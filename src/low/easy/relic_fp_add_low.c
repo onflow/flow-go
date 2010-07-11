6becb5ca77c66e884759dab3853d6fafbbf12a04
dfaranha <dfaranha@gmail.com>
2010-07-11 03:13:42 +0000

    Doubling uses the same method as addition.

--- src/low/easy/relic_fp_add_low.c
+++ src/low/easy/relic_fp_add_low.c
@@ -88,20 +88,22 @@ void fp_addm_low(dig_t *c, dig_t *a, dig_t *b) {
 	}
 }
 
-dig_t fp_addd_low(dig_t *c, dig_t *a, dig_t *b) {
+void fp_addd_low(dig_t *c, dig_t *a, dig_t *b) {
 	int i;
 	dig_t carry, c0, c1, r0, r1;
 
 	carry = 0;
-	for (i = 0; i < 2 * FP_DIGS; i++, a++, b++, c++) {
+	for (i = 0; i < 2 * FP_DIGS; i++, a++, b++) {
 		r0 = (*a) + (*b);
 		c0 = (r0 < (*a));
 		r1 = r0 + carry;
 		c1 = (r1 < r0);
 		carry = c0 | c1;
-		(*c) = r1;
+		c[i] = r1;
 	}
-	return carry;
+	if (carry) {
+		fp_subn_low(c + FP_DIGS, c + FP_DIGS, fp_prime_get());
+	}
 }
 
 dig_t fp_sub1_low(dig_t *c, dig_t *a, dig_t digit) {
@@ -149,17 +151,70 @@ void fp_subm_low(dig_t *c, dig_t *a, dig_t *b) {
 	}
 }
 
-dig_t fp_subd_low(dig_t *c, dig_t *a, dig_t *b) {
+void fp_subc_low(dig_t *c, dig_t *a, dig_t *b) {
 	int i;
 	dig_t carry, r0, diff;
 
 	/* Zero the carry. */
 	carry = 0;
-	for (i = 0; i < 2 * FP_DIGS; i++, a++, b++, c++) {
+	for (i = 0; i < 2 * FP_DIGS; i++, a++, b++) {
 		diff = (*a) - (*b);
 		r0 = diff - carry;
 		carry = ((*a) < (*b)) || (carry && !diff);
-		(*c) = r0;
+		c[i] = r0;
+	}
+	if (carry) {
+		fp_addn_low(c + FP_DIGS, c + FP_DIGS, fp_prime_get());
+	}
+}
+
+void fp_subd_low(dig_t *c, dig_t *a, dig_t *b) {
+	int i;
+	dig_t carry, r0, diff;
+
+	/* Zero the carry. */
+	carry = 0;
+	for (i = 0; i < 2 * FP_DIGS; i++, a++, b++) {
+		diff = (*a) - (*b);
+		r0 = diff - carry;
+		carry = ((*a) < (*b)) || (carry && !diff);
+		c[i] = r0;
+	}
+	if (carry) {
+		fp_addn_low(c + FP_DIGS, c + FP_DIGS, fp_prime_get());
+	}
+}
+
+dig_t fp_dbln_low(dig_t *c, dig_t *a) {
+	int i;
+	dig_t carry, c0, c1, r0, r1;
+
+	carry = 0;
+	for (i = 0; i < FP_DIGS; i++, a++, c++) {
+		r0 = (*a) + (*a);
+		c0 = (r0 < (*a));
+		r1 = r0 + carry;
+		c1 = (r1 < r0);
+		carry = c0 | c1;
+		(*c) = r1;
 	}
 	return carry;
+}
+
+void fp_dblm_low(dig_t *c, dig_t *a) {
+	int i;
+	dig_t carry, c0, c1, r0, r1;
+
+	carry = 0;
+	for (i = 0; i < FP_DIGS; i++, a++) {
+		r0 = (*a) + (*a);
+		c0 = (r0 < (*a));
+		r1 = r0 + carry;
+		c1 = (r1 < r0);
+		carry = c0 | c1;
+		c[i] = r1;
+	}
+	if (carry || (fp_cmpn_low(c, fp_prime_get()) != CMP_LT)) {
+		carry = fp_subn_low(c, c, fp_prime_get());
+	}
 }
