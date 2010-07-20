d7d0fa87f6f9e1c805f0da5af3e696fb9626dd87
dfaranha <dfaranha@gmail.com>
2010-07-20 17:42:48 +0000

    Quadratic extension field arithmetic now can be overriden at lower level.

--- cmake/pp.cmake
+++ cmake/pp.cmake
@@ -5,19 +5,24 @@ message("      PP_PARAL=[off|on] Parallel implementation.\n")
 
 option(PP_PARAL "Parallel implementation." on)
 
-message("   ** Available bilinear pairing methods (default = X-ATE):")
+message("   ** Available bilinear pairing methods (default = BASIC;O-ATE):")
+
+message("      PP_METHD=BASIC    Basic extension field arithmetic.")    
+message("      PP_METHD=INTEG    Extension field arithmetic with embedded reductions.\n")
+
 message("      PP_METHD=R_ATE    R-ate pairing.")
 message("      PP_METHD=O_ATE    O-ate pairing.")
 message("      PP_METHD=X_ATE    X-ate pairing.\n")
 
 # Choose the arithmetic methods.
 if (NOT PP_METHD)
-	set(PP_METHD "R_ATE")
+	set(PP_METHD "BASIC;O_ATE")
 endif(NOT PP_METHD)
 list(LENGTH PP_METHD PP_LEN)
-if (PP_LEN LESS 1)
+if (PP_LEN LESS 2)
 	message(FATAL_ERROR "Incomplete PP_METHD specification: ${PP_METHD}")
-endif(PP_LEN LESS 1)
+endif(PP_LEN LESS 2)
 
-list(GET PP_METHD 0 PP_MAP)
+list(GET PP_METHD 0 PP_EXT)
+list(GET PP_METHD 1 PP_MAP)
 set(PP_METHD ${PP_METHD} CACHE STRING "Bilinear pairings arithmetic method.")
