9f5893e7e7fbb92a8670e0b42abc85e4ef618eb2
dfaranha <dfaranha@gmail.com>
2010-07-25 18:35:52 +0000

    Implemented point compression on prime curves.

---
+++ src/ep/relic_ep_pck.c
@@ -0,0 +1,119 @@
+/*
+ * RELIC is an Efficient LIbrary for Cryptography
+ * Copyright (C) 2007, 2008, 2009 RELIC Authors
+ *
+ * This file is part of RELIC. RELIC is legal property of its developers,
+ * whose names are not listed here. Please refer to the COPYRIGHT file
+ * for contact information.
+ *
+ * RELIC is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * RELIC is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with RELIC. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+/**
+ * @file
+ *
+ * Implementation of point compression on prime elliptic curves.
+ *
+ * @version $Id: relic_ep_pck.c 466 2010-07-16 02:24:36Z dfaranha $
+ * @ingroup ep
+ */
+
+#include "string.h"
+
+#include "relic_core.h"
+#include "relic_ep.h"
+#include "relic_error.h"
+
+/*============================================================================*/
+/* Public definitions                                                         */
+/*============================================================================*/
+
+void ep_pck(ep_t r, ep_t p) {
+	int b = fp_get_bit(p->y, 0);
+	fp_copy(r->x, p->x);
+	fp_set_bit(r->y, 0, b);
+	r->norm = 1;
+}
+
+void ep_upk(ep_t r, ep_t p) {
+	fp_t t0, t1, t2;
+
+	fp_null(t0);
+	fp_null(t1);
+	fp_null(t2);
+
+	TRY {
+		fp_new(t0);
+		fp_new(t1);
+		fp_new(t2);
+
+		/* t0 = x1^2. */
+		fp_sqr(t0, p->x);
+		/* t1 = x1^3. */
+		fp_mul(t1, t0, p->x);
+
+		/* t1 = x1^3 + a * x1 + b. */
+		switch (ep_curve_opt_a()) {
+			case OPT_ZERO:
+				break;
+			case OPT_ONE:
+				fp_add(t1, t1, p->x);
+				break;
+			case OPT_DIGIT:
+				fp_mul_dig(t2, p->x, ep_curve_get_a()[0]);
+				fp_add(t1, t1, t2);
+				break;
+			default:
+				fp_mul(t2, p->x, ep_curve_get_a());
+				fp_add(t1, t1, t2);
+				break;
+		}
+
+		switch (ep_curve_opt_b()) {
+			case OPT_ZERO:
+				break;
+			case OPT_ONE:
+				fp_add_dig(t1, t1, 1);
+				break;
+			case OPT_DIGIT:
+				fp_add_dig(t1, t1, ep_curve_get_b()[0]);
+				break;
+			default:
+				fp_add(t1, t1, ep_curve_get_b());
+				break;
+		}
+
+		/* t0 = sqrt(x1^3 + a * x1 + b). */
+		fp_srt(t0, t1);
+
+		/* Verify if least significant bit of the result matches the
+		 * compressed y-coordinate. */
+		if (fp_get_bit(t0, 0) != fp_get_bit(p->y, 0)) {
+			fp_neg(t0, t0);
+		}
+		fp_copy(r->x, p->x);
+		fp_copy(r->y, t0);
+		fp_set_dig(r->z, 1);
+
+		r->norm = 1;
+	}
+	CATCH_ANY {
+		THROW(ERR_CAUGHT);
+	}
+	FINALLY {
+		fp_free(t0);
+		fp_free(t1);
+		fp_free(t2);
+	}
+}
