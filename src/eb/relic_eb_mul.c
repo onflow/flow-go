1813ce6891d0ca885ea04b8c9f3a0d8c3ff48e66
dfaranha <dfaranha@gmail.com>
2011-08-10 16:37:57 +0000

    Cleanup in new version of Lopez-Dahab scalar multiplication.

--- src/eb/relic_eb_mul.c
+++ src/eb/relic_eb_mul.c
@@ -32,6 +32,7 @@
 #include "string.h"
 
 #include "relic_core.h"
+#include "relic_fb_low.h"
 #include "relic_eb.h"
 #include "relic_error.h"
 
@@ -661,189 +662,195 @@ void eb_mul_basic(eb_t r, eb_t p, bn_t k) {
 #if EB_MUL == LODAH || !defined(STRIP)
 
 void eb_mul_lodah(eb_t r, eb_t p, bn_t k) {
-        int i, t;
-        dv_t x1, z1, x2, z2, r1, r2, r3, r4;
-        dig_t *b;
+	int i, t;
+	dv_t x1, z1, x2, z2, r1, r2, r3, r4, r5;
+	dig_t *b;
 
-        if (eb_curve_is_super()) {
-                THROW(ERR_INVALID);
-        }
+	if (eb_curve_is_super()) {
+		THROW(ERR_INVALID);
+	}
 
-        dv_null(x1);
-        dv_null(z1);
-        dv_null(x2);
-        dv_null(z2);
-        dv_null(r1);
-        dv_null(r2);
-        dv_null(r3);
-        dv_null(r4);
+	dv_null(x1);
+	dv_null(z1);
+	dv_null(x2);
+	dv_null(z2);
+	dv_null(r1);
+	dv_null(r2);
+	dv_null(r3);
+	dv_null(r4);
+	dv_null(r5);
 
-        TRY {
-                dv_new(x1);
-                dv_new(z1);
-                dv_new(x2);
-                dv_new(z2);
-                dv_new(r1);
-                dv_new(r2);
-                dv_new(r3);
-                dv_new(r4);
+	TRY {
+		dv_new(x1);
+		dv_new(z1);
+		dv_new(x2);
+		dv_new(z2);
+		dv_new(r1);
+		dv_new(r2);
+		dv_new(r3);
+		dv_new(r4);
+		dv_new(r5);
 
-                fb_copy(x1, p->x);
-                fb_zero(z1);
-                fb_set_bit(z1, 0, 1);
-                fb_sqr(z2, p->x);
-                fb_sqr(x2, z2);
+		fb_copy(x1, p->x);
+		fb_zero(z1);
+		fb_set_bit(z1, 0, 1);
+		fb_sqr(z2, p->x);
+		fb_sqr(x2, z2);
+		dv_zero(r5, 2 * FB_DIGS);
 
-                b = eb_curve_get_b();
+		b = eb_curve_get_b();
 
-                switch (eb_curve_opt_b()) {
-                        case OPT_ZERO:
-                                break;
-                        case OPT_ONE:
-                                fb_add_dig(x2, x2, (dig_t)1);
-                                break;
-                        case OPT_DIGIT:
-                                fb_add_dig(x2, x2, b[0]);
-                                break;
-                        default:
-                                fb_add(x2, x2, b);
-                                break;
-                }
+		switch (eb_curve_opt_b()) {
+			case OPT_ZERO:
+				break;
+			case OPT_ONE:
+				fb_add_dig(x2, x2, (dig_t)1);
+				break;
+			case OPT_DIGIT:
+				fb_add_dig(x2, x2, b[0]);
+				break;
+			default:
+				fb_add(x2, x2, b);
+				break;
+		}
 
-                t = bn_bits(k);
-                for (i = t - 2; i >= 0; i--) {
-                        fb_mul(r1, x1, z2);
-                        fb_mul(r2, x2, z1);
-                        fb_add(r3, r1, r2);
-                        fb_muln_low(r4, r1, r2);
-                        if (bn_test_bit(k, i) == 1) {
-                                fb_sqr(z1, r3);
-                                fb_muln_low(r1, z1, p->x);
-                                fb_addd_low(x1, r1, r4, 2 * FB_DIGS);
-                                fb_rdcn_low(x1, x1);
-                                fb_sqr(r1, z2);
-                                fb_sqr(r2, x2);
-                                fb_mul(z2, r1, r2);
-                                switch (eb_curve_opt_b()) {
-                                        case OPT_ZERO:
-                                                fb_sqr(x2, r2);
-                                                break;
-                                        case OPT_ONE:
-                                                fb_add(r1, r1, r2);
-                                                fb_sqr(x2, r1);
-                                                break;
-                                        case OPT_DIGIT:
-                                            	fb_sqr(r1, r1);
-                                                fb_sqrl_low(x2, r2);
-                                                fb_mul1_low(r2, r1, b[0]);
-                                                fb_addd_low(x2, x2, r2, FB_DIGS + 1);
-                                                fb_rdcn_low(x2, x2);
-                                                break;
-                                        default:
-                                                fb_sqr(x2, r2);
-                                                fb_sqr(r1, r1);
-                                                fb_mul(r2, r1, b);
-                                                fb_add(x2, x2, r2);
-                                                break;
-                                }
-                        } else {
-                                fb_sqr(z2, r3);
-                                fb_muln_low(r1, z2, p->x);
-                                fb_addd_low(x2, r1, r4, 2 * FB_DIGS);
-                                fb_rdcn_low(x2, x2);
-                                fb_sqr(r1, z1);
-                                fb_sqr(r2, x1);
-                                fb_mul(z1, r1, r2);
-                                switch (eb_curve_opt_b()) {
-                                        case OPT_ZERO:
-                                                fb_sqr(x1, r2);
-                                                break;
-                                        case OPT_ONE:
-                                                fb_add(r1, r1, r2);
-                                                fb_sqr(x1, r1);
-                                                break;
-                                        case OPT_DIGIT:
-                                            	fb_sqr(r1, r1);
-                                                fb_sqrl_low(x1, r2);
-                                                fb_mul1_low(r2, r1, b[0]);
-                                                fb_addd_low(x1, x1, r2, FB_DIGS + 1);
-                                                fb_rdcn_low(x1, x1);
-                                                break;
-                                        default:
-                                                fb_sqr(x1, r2);
-                                                fb_sqr(r1, r1);
-                                                fb_mul(r2, r1, b);
-                                                fb_add(x1, x1, r2);
-                                                break;
-                                }
-                        }
-                }
+		t = bn_bits(k);
+		for (i = t - 2; i >= 0; i--) {
+			fb_mul(r1, x1, z2);
+			fb_mul(r2, x2, z1);
+			fb_add(r3, r1, r2);
+			fb_muln_low(r4, r1, r2);
+			if (bn_test_bit(k, i) == 1) {
+				fb_sqr(z1, r3);
+				fb_muln_low(r1, z1, p->x);
+				fb_addd_low(x1, r1, r4, 2 * FB_DIGS);
+				fb_rdcn_low(x1, x1);
+				fb_sqr(r1, z2);
+				fb_sqr(r2, x2);
+				fb_mul(z2, r1, r2);
+				switch (eb_curve_opt_b()) {
+					case OPT_ZERO:
+						fb_sqr(x2, r2);
+						break;
+					case OPT_ONE:
+						fb_add(r1, r1, r2);
+						fb_sqr(x2, r1);
+						break;
+					case OPT_DIGIT:
+						fb_sqr(r1, r1);
+						fb_sqrl_low(x2, r2);
+						fb_mul1_low(r5, r1, b[0]);
+						fb_addd_low(x2, x2, r5, FB_DIGS + 1);
+						fb_rdcn_low(x2, x2);
+						break;
+					default:
+						fb_sqr(r1, r1);
+						fb_sqrl_low(x2, r2);
+						fb_muln_low(r5, r1, b);
+						fb_addd_low(x2, x2, r5, 2 * FB_DIGS);
+						fb_rdcn_low(x2, x2);
+						break;
+				}
+			} else {
+				fb_sqr(z2, r3);
+				fb_muln_low(r1, z2, p->x);
+				fb_addd_low(x2, r1, r4, 2 * FB_DIGS);
+				fb_rdcn_low(x2, x2);
+				fb_sqr(r1, z1);
+				fb_sqr(r2, x1);
+				fb_mul(z1, r1, r2);
+				switch (eb_curve_opt_b()) {
+					case OPT_ZERO:
+						fb_sqr(x1, r2);
+						break;
+					case OPT_ONE:
+						fb_add(r1, r1, r2);
+						fb_sqr(x1, r1);
+						break;
+					case OPT_DIGIT:
+						fb_sqr(r1, r1);
+						fb_sqrl_low(x1, r2);
+						fb_mul1_low(r5, r1, b[0]);
+						fb_addd_low(x1, x1, r5, FB_DIGS + 1);
+						fb_rdcn_low(x1, x1);
+						break;
+					default:
+						fb_sqr(r1, r1);
+						fb_sqrl_low(x1, r2);
+						fb_muln_low(r5, r1, b);
+						fb_addd_low(x1, x1, r5, 2 * FB_DIGS);
+						fb_rdcn_low(x1, x1);
+						break;
+				}
+			}
+		}
 
-                if (fb_is_zero(z1)) {
-                        /* The point q is at infinity. */
-                        eb_set_infty(r);
-                } else {
-                        if (fb_is_zero(z2)) {
-                                fb_copy(r->x, p->x);
-                                fb_add(r->y, p->x, p->y);
-                                fb_zero(r->z);
-                                fb_set_bit(r->z, 0, 1);
-                        } else {
-                                /* r3 = z1 * z2. */
-                                fb_mul(r3, z1, z2);
-                                /* z1 = (x1 + x * z1). */
-                                fb_mul(z1, z1, p->x);
-                                fb_add(z1, z1, x1);
-                                /* z2 = x * z2. */
-                                fb_mul(z2, z2, p->x);
-                                /* x1 = x1 * z2. */
-                                fb_mul(x1, x1, z2);
-                                /* z2 = (x2 + x * z2)(x1 + x * z1). */
-                                fb_add(z2, z2, x2);
-                                fb_mul(z2, z2, z1);
+		if (fb_is_zero(z1)) {
+			/* The point q is at infinity. */
+			eb_set_infty(r);
+		} else {
+			if (fb_is_zero(z2)) {
+				fb_copy(r->x, p->x);
+				fb_add(r->y, p->x, p->y);
+				fb_zero(r->z);
+				fb_set_bit(r->z, 0, 1);
+			} else {
+				/* r3 = z1 * z2. */
+				fb_mul(r3, z1, z2);
+				/* z1 = (x1 + x * z1). */
+				fb_mul(z1, z1, p->x);
+				fb_add(z1, z1, x1);
+				/* z2 = x * z2. */
+				fb_mul(z2, z2, p->x);
+				/* x1 = x1 * z2. */
+				fb_mul(x1, x1, z2);
+				/* z2 = (x2 + x * z2)(x1 + x * z1). */
+				fb_add(z2, z2, x2);
+				fb_mul(z2, z2, z1);
 
-                                /* r4 = (x^2 + y) * z1 * z2 + (x2 + x * z2)(x1 + x * z1). */
-                                fb_sqr(r4, p->x);
-                                fb_add(r4, r4, p->y);
-                                fb_mul(r4, r4, r3);
-                                fb_add(r4, r4, z2);
+				/* r4 = (x^2 + y) * z1 * z2 + (x2 + x * z2)(x1 + x * z1). */
+				fb_sqr(r4, p->x);
+				fb_add(r4, r4, p->y);
+				fb_mul(r4, r4, r3);
+				fb_add(r4, r4, z2);
 
-                                /* r3 = (z1 * z2 * x)^{-1}. */
-                                fb_mul(r3, r3, p->x);
-                                fb_inv(r3, r3);
-                                /* r4 = (x^2 + y) * z1 * z2 + (x2 + x * z2)(x1 + x * z1) * r3. */
-                                fb_mul(r4, r4, r3);
-                                /* x2 = x1 * x * z2 * (z1 * z2 * x)^{-1} = x1/z1. */
-                                fb_mul(x2, x1, r3);
-                                /* z2 = x + x1/z1. */
-                                fb_add(z2, x2, p->x);
+				/* r3 = (z1 * z2 * x)^{-1}. */
+				fb_mul(r3, r3, p->x);
+				fb_inv(r3, r3);
+				/* r4 = (x^2 + y) * z1 * z2 + (x2 + x * z2)(x1 + x * z1) * r3. */
+				fb_mul(r4, r4, r3);
+				/* x2 = x1 * x * z2 * (z1 * z2 * x)^{-1} = x1/z1. */
+				fb_mul(x2, x1, r3);
+				/* z2 = x + x1/z1. */
+				fb_add(z2, x2, p->x);
 
-                                /* z2 = z2 * r4 + y. */
-                                fb_mul(z2, z2, r4);
-                                fb_add(z2, z2, p->y);
+				/* z2 = z2 * r4 + y. */
+				fb_mul(z2, z2, r4);
+				fb_add(z2, z2, p->y);
 
-                                fb_copy(r->x, x2);
-                                fb_copy(r->y, z2);
-                                fb_zero(r->z);
-                                fb_set_bit(r->z, 0, 1);
+				fb_copy(r->x, x2);
+				fb_copy(r->y, z2);
+				fb_zero(r->z);
+				fb_set_bit(r->z, 0, 1);
 
-                                r->norm = 1;
-                        }
-                }
-        }
-        CATCH_ANY {
-                THROW(ERR_CAUGHT);
-        }
-        FINALLY {
-                dv_free(x1);
-                dv_free(z1);
-                dv_free(x2);
-                dv_free(z2);
-                dv_free(r1);
-                dv_free(r2);
-                dv_free(r3);
-                dv_free(r4);
-        }
+				r->norm = 1;
+			}
+		}
+	}
+	CATCH_ANY {
+		THROW(ERR_CAUGHT);
+	}
+	FINALLY {
+		dv_free(x1);
+		dv_free(z1);
+		dv_free(x2);
+		dv_free(z2);
+		dv_free(r1);
+		dv_free(r2);
+		dv_free(r3);
+		dv_free(r4);
+		dv_free(r5);
+	}
 }
 
 #endif /* EB_ORDIN || EB_KBLTZ */
