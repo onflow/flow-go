1acf32a8118982221a9ba6b9c944f57e91b077b8
dfaranha <dfaranha@gmail.com>
2011-12-02 07:17:36 +0000

    Release of the GF(2^353) AVR backend for pairing computation.

---
+++ src/low/avr-asm-353/relic_fb_mul_low.c
@@ -0,0 +1,135 @@
+/*
+ * RELIC is an Efficient LIbrary for Cryptography
+ * Copyright (C) 2007-2011 RELIC Authors
+ *
+ * This file is part of RELIC. RELIC is legal property of its developers,
+ * whose names are not listed here. Please refer to the COPYRIGHT file
+ * for contact information.
+ *
+ * RELIC is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * RELIC is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with RELIC. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+/**
+ * @file
+ *
+ * Implementation of the low-level binary field multiplication functions.
+ *
+ * @version $Id$
+ * @ingroup fb
+ */
+
+#include "relic_fb.h"
+#include "relic_bn_low.h"
+#include "relic_fb_low.h"
+#include "relic_util.h"
+
+void fb_mulk_low(dig_t *c, dig_t *a, dig_t *b);
+
+void fb_mul1_low(dig_t *c, dig_t *a, dig_t digit) {
+	dig_t carry;
+
+	fb_zero(c);
+	fb_zero(c + FB_DIGS);
+	for (int i = FB_DIGIT - 1; i >= 0; i--) {
+		if (digit & ((dig_t)1 << i)) {
+			fb_addn_low(c, c, a);
+		}
+		if (i != 0) {
+			carry = fb_lsh1_low(c, c);
+			c[FB_DIGS] = (c[FB_DIGS] << 1) | carry;
+		}
+	}
+}
+
+void fb_mulm_low(dig_t *c, dig_t *a, dig_t *b) {
+	dv_t t;
+
+	dv_new(t);
+	fb_muln_low(t, a, b);
+	fb_rdcn_low(c, t);
+	dv_free(t);
+}
+
+void fb_muld_low(dig_t *c, dig_t *a, dig_t *b, int size) {
+	dv_t table[16];
+	dig_t u, *tmpa, *tmpc, r0, r1, r2, r4, r8;
+	int i, j;
+
+	for (i = 0; i < 16; i++) {
+		dv_null(table[i]);
+	}
+
+	dv_zero(c, 2 * size);
+
+	for (i = 0; i < 16; i++) {
+		dv_new(table[i]);
+		dv_zero(table[i], size + 1);
+	}
+
+	u = 0;
+	for (i = 0; i < size; i++) {
+		r1 = r0 = b[i];
+		r2 = (r0 << 1) | (u >> (FB_DIGIT - 1));
+		r4 = (r0 << 2) | (u >> (FB_DIGIT - 2));
+		r8 = (r0 << 3) | (u >> (FB_DIGIT - 3));
+		table[0][i] = 0;
+		table[1][i] = r1;
+		table[2][i] = r2;
+		table[3][i] = r1 ^ r2;
+		table[4][i] = r4;
+		table[5][i] = r1 ^ r4;
+		table[6][i] = r2 ^ r4;
+		table[7][i] = r1 ^ r2 ^ r4;
+		table[8][i] = r8;
+		table[9][i] = r1 ^ r8;
+		table[10][i] = r2 ^ r8;
+		table[11][i] = r1 ^ r2 ^ r8;
+		table[12][i] = r4 ^ r8;
+		table[13][i] = r1 ^ r4 ^ r8;
+		table[14][i] = r2 ^ r4 ^ r8;
+		table[15][i] = r1 ^ r2 ^ r4 ^ r8;
+		u = r1;
+	}
+
+	if (u > 0) {
+		r2 = u >> (FB_DIGIT - 1);
+		r4 = u >> (FB_DIGIT - 2);
+		r8 = u >> (FB_DIGIT - 3);
+		table[0][size] = table[1][size] = 0;
+		table[2][size] = table[3][size] = r2;
+		table[4][size] = table[5][size] = r4;
+		table[6][size] = table[7][size] = r2 ^ r4;
+		table[8][size] = table[9][size] = r8;
+		table[10][size] = table[11][size] = r2 ^ r8;
+		table[12][size] = table[13][size] = r4 ^ r8;
+		table[14][size] = table[15][size] = r2 ^ r4 ^ r8;
+	}
+
+	for (i = FB_DIGIT - 4; i > 0; i -= 4) {
+		tmpa = a;
+		tmpc = c;
+		for (j = 0; j < size; j++, tmpa++, tmpc++) {
+			u = (*tmpa >> i) & 0x0F;
+			fb_addd_low(tmpc, tmpc, table[u], size + 1);
+		}
+		bn_lshb_low(c, c, 2 * size, 4);
+	}
+	for (j = 0; j < size; j++, a++, c++) {
+		u = *a & 0x0F;
+		fb_addd_low(c, c, table[u], size + 1);
+	}
+	for (i = 0; i < 16; i++) {
+		dv_free(table[i]);
+	}
+}
---
+++ src/low/avr-asm-353/relic_fb_srt_low.s
@@ -0,0 +1,244 @@
+/*
+ * RELIC is an Efficient LIbrary for Cryptography
+ * Copyright (C) 2007-2011 RELIC Authors
+ *
+ * This file is part of RELIC. RELIC is legal property of its developers,
+ * whose names are not listed here. Please refer to the COPYRIGHT file
+ * for contact information.
+ *
+ * RELIC is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * RELIC is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with RELIC. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+/**
+ * @file
+ *
+ * Implementation of the low-level binary field square root.
+ *
+ * @version $Id$
+ * @ingroup fb
+ */
+
+#include "relic_fb_low.h"
+
+.arch atmega128
+
+.text
+
+.global fb_srti_low
+
+.macro MACRO
+	ld		r18, x+
+	bst		r18, 0
+	bld		r19, 0
+	bst		r18, 2
+	bld		r19, 1
+	bst		r18, 4
+	bld		r19, 2
+	bst		r18, 6
+	bld		r19, 3
+
+	bst		r18, 1
+	bld		r20, 0
+	bst		r18, 3
+	bld		r20, 1
+	bst		r18, 5
+	bld		r20, 2
+	bst		r18, 7
+	bld		r20, 3
+
+	ld		r18, x+
+	bst		r18, 0
+	bld		r19, 4
+	bst		r18, 2
+	bld		r19, 5
+	bst		r18, 4
+	bld		r19, 6
+	bst		r18, 6
+	bld		r19, 7
+
+	bst		r18, 1
+	bld		r20, 4
+	bst		r18, 3
+	bld		r20, 5
+	bst		r18, 5
+	bld		r20, 6
+	bst		r18, 7
+	bld		r20, 7
+.endm
+
+fb_srti_low:
+	push r28
+	push r29
+
+	movw	r30,r24				; Copy c to Z
+	movw	r28,r22				; Copy t to Y
+	movw	r26,r20				; Copy a to X
+
+	clr		r22
+	MACRO
+	std		z+0, r19
+	mov		r21, r20
+	lsl		r21
+	std		y+22,r21
+	bst		r20, 7
+	bld		r22, 0
+	mov		r21, r20
+	lsl		r21
+	lsl		r21
+	lsl		r21
+	std		y+4, r21
+	swap	r20
+	andi	r20, 0x0F
+	lsr		r20
+	mov		r23, r20
+
+	MACRO
+	std		z+1, r19
+	mov		r21, r20
+	lsl		r21
+	eor		r21, r22
+	std		y+23,r21
+	bst		r20, 7
+	bld		r22, 0
+	mov		r21, r20
+	lsl		r21
+	lsl		r21
+	lsl		r21
+	eor		r21, r23
+	std		y+5, r21
+	swap	r20
+	andi	r20, 0x0F
+	lsr		r20
+	mov		r23, r20
+
+	MACRO
+	std		z+2, r19
+	mov		r21, r20
+	lsl		r21
+	eor		r21, r22
+	std		y+24,r21
+	bst		r20, 7
+	bld		r22, 0
+	mov		r21, r20
+	lsl		r21
+	lsl		r21
+	lsl		r21
+	eor		r21, r23
+	std		y+6, r21
+	swap	r20
+	andi	r20, 0x0F
+	lsr		r20
+	mov		r23, r20
+
+	MACRO
+	std		z+3, r19
+	mov		r21, r20
+	lsl		r21
+	eor		r21, r22
+	std		y+25,r21
+	bst		r20, 7
+	bld		r22, 0
+	mov		r21, r20
+	lsl		r21
+	lsl		r21
+	lsl		r21
+	eor		r21, r23
+	std		y+7, r21
+	swap	r20
+	andi	r20, 0x0F
+	lsr		r20
+	mov		r23, r20
+
+	.irp i, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16
+		MACRO
+		ldd		r21, y+\i
+		eor		r19, r21
+		std		z+\i, r19
+		mov		r21, r20
+		lsl		r21
+		eor		r21, r22
+		std		y+22+\i,r21
+		bst		r20, 7
+		bld		r22, 0
+		mov		r21, r20
+		lsl		r21
+		lsl		r21
+		lsl		r21
+		eor		r21, r23
+		std		y+4+\i, r21
+		swap	r20
+		andi	r20, 0x0F
+		lsr		r20
+		mov		r23, r20
+	.endr
+
+	MACRO
+	ldd		r21, y+17
+	eor		r19, r21
+	std		z+17, r19
+	mov		r21, r20
+	lsl		r21
+	eor		r21, r22
+	std		y+22+17,r21
+	bst		r20, 7
+	bld		r22, 0
+	mov		r21, r20
+	lsl		r21
+	lsl		r21
+	lsl		r21
+	eor		r21, r23
+	std		y+4+17, r21
+	swap	r20
+	andi	r20, 0x0F
+	lsr		r20
+	mov		r23, r20
+
+	.irp i, 18, 19, 20, 21
+		MACRO
+		ldd		r21, y+\i
+		eor		r19, r21
+		std		z+\i, r19
+		mov		r21, r20
+		lsl		r21
+		eor		r21, r22
+		std		y+22+\i,r21
+		bst		r20, 7
+		bld		r22, 0
+		mov		r21, r20
+		lsl		r21
+		lsl		r21
+		lsl		r21
+		eor		r21, r23
+		ldd		r19, y+4+\i
+		eor		r21, r19
+		std		y+4+\i, r21
+		swap	r20
+		andi	r20, 0x0F
+		lsr		r20
+		mov		r23, r20
+	.endr
+
+	std		y+44, r22
+	ldd		r19, y+26
+	eor		r23, r19
+	std		y+26, r23
+
+	ld		r19, x+
+	ldd		r21, y+22
+	eor		r19, r21
+	std		z+22, r19
+
+	pop r29
+	pop r28
+	ret
