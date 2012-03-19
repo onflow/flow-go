2dd07bf921c8f907a3178b93439b1d8cee22fbe4
dfaranha <dfaranha@gmail.com>
2012-03-19 00:16:37 +0000

    Further breaking down fb*_mul() functions.

---
+++ src/fbx/relic_fb12_mul.c
@@ -0,0 +1,96 @@
+/*
+ * RELIC is an Efficient LIbrary for Cryptography
+ * Copyright (C) 2007-2012 RELIC Authors
+ *
+ * This file is part of RELIC. RELIC is legal property of its developers,
+ * whose names are not listed here. Please refer to the COPYRIGHT file
+ * for contact information.
+ *
+ * RELIC is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * RELIC is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with RELIC. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+/**
+ * @file
+ *
+ * Implementation of multiplication in dodecic extensions of binary fields.
+ *
+ * @version $Id: relic_fbx_mul.c 1137 2012-03-18 13:35:48Z dfaranha $
+ * @ingroup fbx
+ */
+
+#include <string.h>
+
+#include "relic_core.h"
+#include "relic_conf.h"
+#include "relic_util.h"
+#include "relic_fbx.h"
+#include "relic_fb_low.h"
+
+/*============================================================================*/
+/* Public definitions                                                         */
+/*============================================================================*/
+
+void fb12_mul(fb12_t c, fb12_t a, fb12_t b) {
+	fb6_t t0, t1, t2;
+
+	fb6_null(t0);
+	fb6_null(t1);
+	fb6_null(t2);
+
+	TRY {
+		fb6_new(t0);
+		fb6_new(t1);
+		fb6_new(t2);
+
+		fb6_add(t0, a[0], a[1]);
+		fb6_add(t1, b[0], b[1]);
+		fb6_mul(t2, t0, t1);
+		fb6_mul(t0, a[0], b[0]);
+		fb6_mul(t1, a[1], b[1]);
+		fb6_mul_nor(t1, t1);
+		fb6_add(c[0], t0, t1);
+		fb6_add(c[1], t0, t2);
+	} CATCH_ANY {
+		THROW(ERR_CAUGHT);
+	} FINALLY {
+		fb6_free(t0);
+		fb6_free(t1);
+		fb6_free(t2);
+	}
+}
+
+void fb12_mul_dxs(fb12_t c, fb12_t a, fb12_t b) {
+	fb6_t t0, t1;
+
+	fb6_null(t0);
+	fb6_null(t1);
+
+	TRY {
+		fb6_new(t0);
+		fb6_new(t1);
+
+		fb6_mul_dxs(t0, a[0], b[0]);
+		fb6_mul_nor(t1, a[1]);
+		fb_add_dig(b[0][0], b[0][0], 1);
+		fb6_mul_dxs(c[1], a[1], b[0]);
+		fb_add_dig(b[0][0], b[0][0], 1);
+		fb6_add(c[1], c[1], a[0]);
+		fb6_add(c[0], t0, t1);
+	} CATCH_ANY {
+		THROW(ERR_CAUGHT);
+	} FINALLY {
+		fb6_free(t0);
+		fb6_free(t1);
+	}
+}
