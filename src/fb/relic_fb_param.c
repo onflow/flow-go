27e52d7d9379a6efa073007d3a7f25024482a6d6
dfaranha <dfaranha@gmail.com>
2012-04-01 01:23:40 +0000

    Added missing include.

--- src/fb/relic_fb_param.c
+++ src/fb/relic_fb_param.c
@@ -37,6 +37,7 @@
 #include "relic_conf.h"
 #include "relic_dv.h"
 #include "relic_fb.h"
+#include "relic_util.h"
 #include "relic_error.h"
 
 /*============================================================================*/
