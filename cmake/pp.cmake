19134a0d52463bce16487c6828b33b9e8074c039
dfaranha <dfaranha@gmail.com>
2012-04-08 19:17:14 +0000

    Renamed pairings.

--- cmake/pp.cmake
+++ cmake/pp.cmake
@@ -5,7 +5,7 @@ message("      PP_PARAL=[off|on] Parallel implementation.\n")
 
 option(PP_PARAL "Parallel implementation." off)
 
-message("   ** Available bilinear pairing methods (default = BASIC;BASIC;BASIC;O_ATE):")
+message("   ** Available bilinear pairing methods (default = BASIC;BASIC;BASIC;OATEP):")
 
 message("      PP_METHD=BASIC    Basic quadratic extension field arithmetic.")    
 message("      PP_METHD=INTEG    Quadratic extension field arithmetic with embedded modular reduction.\n")
@@ -16,11 +16,13 @@ message("      PP_METHD=INTEG    Cubic extension field arithmetic with embedded 
 message("      PP_METHD=BASIC    Basic extension field arithmetic.")    
 message("      PP_METHD=LAZYR    Lazy reduced extension field arithmetic.\n")
 
-message("      PP_METHD=O_ATE    Optimal ate pairing.\n")
+message("      PP_METHD=TATEP    Tate pairing.")
+message("      PP_METHD=WEILP    Weil pairing.")
+message("      PP_METHD=OATEP    Optimal ate pairing.\n")
 
 # Choose the arithmetic methods.
 if (NOT PP_METHD)
-	set(PP_METHD "BASIC;BASIC;BASIC;O_ATE")
+	set(PP_METHD "BASIC;BASIC;BASIC;OATEP")
 endif(NOT PP_METHD)
 list(LENGTH PP_METHD PP_LEN)
 if (PP_LEN LESS 4)
