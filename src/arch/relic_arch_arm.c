051a26fcfdb837f65d791c2a9665567a7d90a62f
dfaranha@gmail.com <dfaranha@gmail.com>
2012-10-01 02:49:40 +0000

    Added support for recent ARM architectures.

---
+++ src/arch/relic_arch_arm.c
@@ -0,0 +1,48 @@
+/*
+ * RELIC is an Efficient LIbrary for Cryptography
+ * Copyright (C) 2007-2012 RELIC Authors
+ *
+ * This file is part of RELIC. RELIC is legal property of its developers,
+ * whose names are not listed here. Please refer to the COPYRIGHT file
+ * for contact information.
+ *
+ * RELIC is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * RELIC is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with RELIC. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+/**
+ * @file
+ *
+ * Implementation of architecture-dependent routines.
+ *
+ * @version $Id$
+ * @ingroup arch
+ */
+
+#include "relic_util.h"
+
+/*============================================================================*/
+/* Public definitions                                                         */
+/*============================================================================*/
+
+void arch_init(void) {
+}
+
+void arch_clean(void) {
+}
+
+unsigned long long arch_cycles(void) {
+	unsigned int value;
+	asm ("MRC p15, 0, %0, c9, c13, 0\t\n": "=r"(value));  
+	return value;
+}
