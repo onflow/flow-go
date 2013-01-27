46284d394a8503f90bbc78bc4b7ab7606fa11dcb
dfaranha <dfaranha@gmail.com>
2013-01-27 02:28:52 +0000

    Massive commit updating Copyright clause in entire project.

--- src/fbx/relic_fbx_slv.c
+++ src/fbx/relic_fbx_slv.c
@@ -1,6 +1,6 @@
 /*
  * RELIC is an Efficient LIbrary for Cryptography
- * Copyright (C) 2007-2012 RELIC Authors
+ * Copyright (C) 2007-2013 RELIC Authors
  *
  * This file is part of RELIC. RELIC is legal property of its developers,
  * whose names are not listed here. Please refer to the COPYRIGHT file
@@ -43,7 +43,7 @@ void fb2_slv(fb2_t c, fb2_t a) {
 	fb_slv(c[1], a[1]);
 	/* Compute c_0 = a_0 + a_1 + c_1 + Tr(c_1). */
 	fb_add(c[0], c[0], c[1]);
-	//fb_add_dig(c[0], c[0], fb_trc(c[1]));
+	fb_add_dig(c[0], c[0], fb_trc(c[1]));
 	/* Make Tr(c_0) = 0. */
 	fb_slv(c[0], c[0]);
 	/* Compute c_0^2 + c_0 = c_0. */
