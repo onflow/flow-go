e83793ca53a2c29091d60e26b5f089d2dba81113
dfaranha <dfaranha@gmail.com>
2013-03-22 17:19:48 +0000

    Low-level backends should avoid allocating memory through *_new() macros.

--- src/low/easy/relic_fb_mul_low.c
+++ src/low/easy/relic_fb_mul_low.c
@@ -74,19 +74,16 @@ void fb_mul1_low(dig_t *c, dig_t *a, dig_t digit) {
 }
 
 void fb_muln_low(dig_t *c, dig_t *a, dig_t *b) {
-	dv_t table[16];
+	align dig_t t[16][FB_DIGS + 1];
 	dig_t r0, r1, r2, r4, r8, u, carry, *tmpa, *tmpc;
 	int i, j;
 
-	for (i = 0; i < 16; i++) {
-		dv_null(table[i]);
-	}
 	for (i = 0; i < 2 * FB_DIGS; i++) {
 		c[i] = 0;
 	}
+
 	for (i = 0; i < 16; i++) {
-		dv_new(table[i]);
-		dv_zero(table[i], FB_DIGS + 1);
+		dv_zero(t[i], FB_DIGS + 1);
 	}
 
 	u = 0;
@@ -95,22 +92,22 @@ void fb_muln_low(dig_t *c, dig_t *a, dig_t *b) {
 		r2 = (r0 << 1) | (u >> (FB_DIGIT - 1));
 		r4 = (r0 << 2) | (u >> (FB_DIGIT - 2));
 		r8 = (r0 << 3) | (u >> (FB_DIGIT - 3));
-		table[0][i] = 0;
-		table[1][i] = r1;
-		table[2][i] = r2;
-		table[3][i] = r1 ^ r2;
-		table[4][i] = r4;
-		table[5][i] = r1 ^ r4;
-		table[6][i] = r2 ^ r4;
-		table[7][i] = r1 ^ r2 ^ r4;
-		table[8][i] = r8;
-		table[9][i] = r1 ^ r8;
-		table[10][i] = r2 ^ r8;
-		table[11][i] = r1 ^ r2 ^ r8;
-		table[12][i] = r4 ^ r8;
-		table[13][i] = r1 ^ r4 ^ r8;
-		table[14][i] = r2 ^ r4 ^ r8;
-		table[15][i] = r1 ^ r2 ^ r4 ^ r8;
+		t[0][i] = 0;
+		t[1][i] = r1;
+		t[2][i] = r2;
+		t[3][i] = r1 ^ r2;
+		t[4][i] = r4;
+		t[5][i] = r1 ^ r4;
+		t[6][i] = r2 ^ r4;
+		t[7][i] = r1 ^ r2 ^ r4;
+		t[8][i] = r8;
+		t[9][i] = r1 ^ r8;
+		t[10][i] = r2 ^ r8;
+		t[11][i] = r1 ^ r2 ^ r8;
+		t[12][i] = r4 ^ r8;
+		t[13][i] = r1 ^ r4 ^ r8;
+		t[14][i] = r2 ^ r4 ^ r8;
+		t[15][i] = r1 ^ r2 ^ r4 ^ r8;
 		u = r1;
 	}
 
@@ -118,14 +115,14 @@ void fb_muln_low(dig_t *c, dig_t *a, dig_t *b) {
 		r2 = u >> (FB_DIGIT - 1);
 		r4 = u >> (FB_DIGIT - 2);
 		r8 = u >> (FB_DIGIT - 3);
-		table[0][FB_DIGS] = table[1][FB_DIGS] = 0;
-		table[2][FB_DIGS] = table[3][FB_DIGS] = r2;
-		table[4][FB_DIGS] = table[5][FB_DIGS] = r4;
-		table[6][FB_DIGS] = table[7][FB_DIGS] = r2 ^ r4;
-		table[8][FB_DIGS] = table[9][FB_DIGS] = r8;
-		table[10][FB_DIGS] = table[11][FB_DIGS] = r2 ^ r8;
-		table[12][FB_DIGS] = table[13][FB_DIGS] = r4 ^ r8;
-		table[14][FB_DIGS] = table[15][FB_DIGS] = r2 ^ r4 ^ r8;
+		t[0][FB_DIGS] = t[1][FB_DIGS] = 0;
+		t[2][FB_DIGS] = t[3][FB_DIGS] = r2;
+		t[4][FB_DIGS] = t[5][FB_DIGS] = r4;
+		t[6][FB_DIGS] = t[7][FB_DIGS] = r2 ^ r4;
+		t[8][FB_DIGS] = t[9][FB_DIGS] = r8;
+		t[10][FB_DIGS] = t[11][FB_DIGS] = r2 ^ r8;
+		t[12][FB_DIGS] = t[13][FB_DIGS] = r4 ^ r8;
+		t[14][FB_DIGS] = t[15][FB_DIGS] = r2 ^ r4 ^ r8;
 	}
 
 	for (i = FB_DIGIT - 4; i > 0; i -= 4) {
@@ -133,8 +130,8 @@ void fb_muln_low(dig_t *c, dig_t *a, dig_t *b) {
 		tmpc = c;
 		for (j = 0; j < FB_DIGS; j++, tmpa++, tmpc++) {
 			u = (*tmpa >> i) & 0x0F;
-			fb_addn_low(tmpc, tmpc, table[u]);
-			*(tmpc + FB_DIGS) ^= table[u][FB_DIGS];
+			fb_addn_low(tmpc, tmpc, t[u]);
+			*(tmpc + FB_DIGS) ^= t[u][FB_DIGS];
 		}
 		carry = fb_lshb_low(c, c, 4);
 		fb_lshb_low(c + FB_DIGS, c + FB_DIGS, 4);
@@ -143,29 +140,20 @@ void fb_muln_low(dig_t *c, dig_t *a, dig_t *b) {
 
 	for (j = 0; j < FB_DIGS; j++, a++, c++) {
 		u = *a & 0x0F;
-		fb_addn_low(c, c, table[u]);
-		*(c + FB_DIGS) ^= table[u][FB_DIGS];
-	}
-
-	for (i = 0; i < 16; i++) {
-		dv_free(table[i]);
+		fb_addn_low(c, c, t[u]);
+		*(c + FB_DIGS) ^= t[u][FB_DIGS];
 	}
 }
 
 void fb_muld_low(dig_t *c, dig_t *a, dig_t *b, int size) {
-	dv_t table[16];
+	align dig_t t[16][size + 1];
 	dig_t u, *tmpa, *tmpc, r0, r1, r2, r4, r8;
 	int i, j;
-
-	for (i = 0; i < 16; i++) {
-		dv_null(table[i]);
-	}
 
 	dv_zero(c, 2 * size);
 
 	for (i = 0; i < 16; i++) {
-		dv_new(table[i]);
-		dv_zero(table[i], size + 1);
+		dv_zero(t[i], size + 1);
 	}
 
 	u = 0;
@@ -174,22 +162,22 @@ void fb_muld_low(dig_t *c, dig_t *a, dig_t *b, int size) {
 		r2 = (r0 << 1) | (u >> (FB_DIGIT - 1));
 		r4 = (r0 << 2) | (u >> (FB_DIGIT - 2));
 		r8 = (r0 << 3) | (u >> (FB_DIGIT - 3));
-		table[0][i] = 0;
-		table[1][i] = r1;
-		table[2][i] = r2;
-		table[3][i] = r1 ^ r2;
-		table[4][i] = r4;
-		table[5][i] = r1 ^ r4;
-		table[6][i] = r2 ^ r4;
-		table[7][i] = r1 ^ r2 ^ r4;
-		table[8][i] = r8;
-		table[9][i] = r1 ^ r8;
-		table[10][i] = r2 ^ r8;
-		table[11][i] = r1 ^ r2 ^ r8;
-		table[12][i] = r4 ^ r8;
-		table[13][i] = r1 ^ r4 ^ r8;
-		table[14][i] = r2 ^ r4 ^ r8;
-		table[15][i] = r1 ^ r2 ^ r4 ^ r8;
+		t[0][i] = 0;
+		t[1][i] = r1;
+		t[2][i] = r2;
+		t[3][i] = r1 ^ r2;
+		t[4][i] = r4;
+		t[5][i] = r1 ^ r4;
+		t[6][i] = r2 ^ r4;
+		t[7][i] = r1 ^ r2 ^ r4;
+		t[8][i] = r8;
+		t[9][i] = r1 ^ r8;
+		t[10][i] = r2 ^ r8;
+		t[11][i] = r1 ^ r2 ^ r8;
+		t[12][i] = r4 ^ r8;
+		t[13][i] = r1 ^ r4 ^ r8;
+		t[14][i] = r2 ^ r4 ^ r8;
+		t[15][i] = r1 ^ r2 ^ r4 ^ r8;
 		u = r1;
 	}
 
@@ -197,14 +185,14 @@ void fb_muld_low(dig_t *c, dig_t *a, dig_t *b, int size) {
 		r2 = u >> (FB_DIGIT - 1);
 		r4 = u >> (FB_DIGIT - 2);
 		r8 = u >> (FB_DIGIT - 3);
-		table[0][size] = table[1][size] = 0;
-		table[2][size] = table[3][size] = r2;
-		table[4][size] = table[5][size] = r4;
-		table[6][size] = table[7][size] = r2 ^ r4;
-		table[8][size] = table[9][size] = r8;
-		table[10][size] = table[11][size] = r2 ^ r8;
-		table[12][size] = table[13][size] = r4 ^ r8;
-		table[14][size] = table[15][size] = r2 ^ r4 ^ r8;
+		t[0][size] = t[1][size] = 0;
+		t[2][size] = t[3][size] = r2;
+		t[4][size] = t[5][size] = r4;
+		t[6][size] = t[7][size] = r2 ^ r4;
+		t[8][size] = t[9][size] = r8;
+		t[10][size] = t[11][size] = r2 ^ r8;
+		t[12][size] = t[13][size] = r4 ^ r8;
+		t[14][size] = t[15][size] = r2 ^ r4 ^ r8;
 	}
 
 	for (i = FB_DIGIT - 4; i > 0; i -= 4) {
@@ -212,16 +200,13 @@ void fb_muld_low(dig_t *c, dig_t *a, dig_t *b, int size) {
 		tmpc = c;
 		for (j = 0; j < size; j++, tmpa++, tmpc++) {
 			u = (*tmpa >> i) & 0x0F;
-			fb_addd_low(tmpc, tmpc, table[u], size + 1);
+			fb_addd_low(tmpc, tmpc, t[u], size + 1);
 		}
 		bn_lshb_low(c, c, 2 * size, 4);
 	}
 	for (j = 0; j < size; j++, a++, c++) {
 		u = *a & 0x0F;
-		fb_addd_low(c, c, table[u], size + 1);
-	}
-	for (i = 0; i < 16; i++) {
-		dv_free(table[i]);
+		fb_addd_low(c, c, t[u], size + 1);
 	}
 }
 
--- src/low/easy/relic_ft_mul_low.c
+++ src/low/easy/relic_ft_mul_low.c
@@ -39,54 +39,18 @@
 /*============================================================================*/
 /* Public definitions                                                         */
 /*============================================================================*/
-/*
- * void ft_mul1_low(dig_t *c, dig_t *a, dig_t digit) {
- * int j, k;
- * dig_t b1, b2;
- *
- * if (digit == 0) {
- * dv_zero(c, FT_DIGS + 1);
- * return;
- * }
- * if (digit == 1) {
- * ft_copy(c, a);
- * return;
- * }
- * c[FT_DIGS] = ft_lshb_low(c, a, util_bits_dig(digit) - 1);
- * for (int i = util_bits_dig(digit) - 2; i > 0; i--) {
- * if (digit & ((dig_t)1 << i)) {
- * j = FT_DIGIT - i;
- * b1 = a[0];
- * c[0] ^= (b1 << i);
- * for (k = 1; k < FT_DIGS; k++) {
- * b2 = a[k];
- * c[k] ^= ((b2 << i) | (b1 >> j));
- * b1 = b2;
- * }
- * c[FT_DIGS] ^= (b1 >> j);
- * }
- * }
- * if (digit & (dig_t)1) {
- * ft_add(c, c, a);
- * }
- * }
- */
 
 void ft_muln_low(dig_t *c, dig_t *a, dig_t *b) {
-	dv_t table[81];
-	dig_t r0, r1, r2, r4, r8, u, carry;
+	align dig_t t[81][2 * FT_DIGS + 1];
+	dig_t r0, r1, r2, r4, u, carry;
 	unsigned char k[FT_TRITS] = { 0 };
 	int i, l;
 
 	for (i = 0; i < 81; i++) {
-		dv_null(table[i]);
+		dv_zero(t[i], 2 * FT_DIGS + 1);
 	}
 	for (i = 0; i < 2 * FT_DIGS; i++) {
 		c[i] = 0;
-	}
-	for (i = 0; i < 81; i++) {
-		dv_new(table[i]);
-		dv_zero(table[i], 2 * FT_DIGS);
 	}
 
 	u = 0;
@@ -94,73 +58,69 @@ void ft_muln_low(dig_t *c, dig_t *a, dig_t *b) {
 		r1 = r0 = b[i];
 		r2 = (r0 << 1) | (u >> (FT_DIGIT - 1));
 		r4 = (r0 << 2) | (u >> (FT_DIGIT - 2));
-		r8 = (r0 << 3) | (u >> (FT_DIGIT - 3));
-		table[0][i] = 0;
-		table[1][i] = r1;
-		table[3][i] = r2;
-		table[9][i] = r4;
+		t[0][i] = 0;
+		t[1][i] = r1;
+		t[3][i] = r2;
+		t[9][i] = r4;
 		u = r1;
 	}
 	if (u > 0) {
 		r2 = u >> (FT_DIGIT - 1);
 		r4 = u >> (FT_DIGIT - 2);
-		r8 = u >> (FT_DIGIT - 3);
-		table[0][FT_DIGS / 2] = table[1][FT_DIGS / 2] = 0;
-		table[3][FT_DIGS / 2] = r2;
-		table[9][FT_DIGS / 2] = r4;
+		t[0][FT_DIGS / 2] = t[1][FT_DIGS / 2] = 0;
+		t[3][FT_DIGS / 2] = r2;
+		t[9][FT_DIGS / 2] = r4;
 	}
 
 	for (i = FT_DIGS / 2; i < FT_DIGS; i++) {
 		r1 = r0 = b[i];
 		r2 = (r0 << 1) | (u >> (FT_DIGIT - 1));
 		r4 = (r0 << 2) | (u >> (FT_DIGIT - 2));
-		r8 = (r0 << 3) | (u >> (FT_DIGIT - 3));
-		table[0][i + FT_DIGS/2] = 0;
-		table[1][i + FT_DIGS/2] = r1;
-		table[3][i + FT_DIGS/2] = r2;
-		table[9][i + FT_DIGS/2] = r4;
+		t[0][i + FT_DIGS/2] = 0;
+		t[1][i + FT_DIGS/2] = r1;
+		t[3][i + FT_DIGS/2] = r2;
+		t[9][i + FT_DIGS/2] = r4;
 		u = r1;
 	}
 	if (u > 0) {
 		r2 = u >> (FT_DIGIT - 1);
 		r4 = u >> (FT_DIGIT - 2);
-		r8 = u >> (FT_DIGIT - 3);
-		table[0][FT_DIGS + FT_DIGS/2] = table[1][FT_DIGS + FT_DIGS/2] = 0;
-		table[3][FT_DIGS + FT_DIGS/2] = r2;
-		table[9][FT_DIGS + FT_DIGS/2] = r4;
+		t[0][FT_DIGS + FT_DIGS/2] = t[1][FT_DIGS + FT_DIGS/2] = 0;
+		t[3][FT_DIGS + FT_DIGS/2] = r2;
+		t[9][FT_DIGS + FT_DIGS/2] = r4;
 	}
 
-	dv_copy(table[2] + FT_DIGS, table[1], FT_DIGS);
-	dv_copy(table[2], table[1] + FT_DIGS, FT_DIGS);
-	ft_addd_low(table[4], table[3], table[1], 2 * FT_DIGS, 2 * FT_DIGS);
-	ft_addd_low(table[5], table[3], table[2], 2 * FT_DIGS, 2 * FT_DIGS);
-	dv_copy(table[6] + FT_DIGS, table[3], FT_DIGS);
-	dv_copy(table[6], table[3] + FT_DIGS, FT_DIGS);
-	dv_copy(table[7] + FT_DIGS, table[5], FT_DIGS);
-	dv_copy(table[7], table[5] + FT_DIGS, FT_DIGS);
-	dv_copy(table[8] + FT_DIGS, table[4], FT_DIGS);
-	dv_copy(table[8], table[4] + FT_DIGS, FT_DIGS);
+	dv_copy(t[2] + FT_DIGS, t[1], FT_DIGS);
+	dv_copy(t[2], t[1] + FT_DIGS, FT_DIGS);
+	ft_addd_low(t[4], t[3], t[1], 2 * FT_DIGS, 2 * FT_DIGS);
+	ft_addd_low(t[5], t[3], t[2], 2 * FT_DIGS, 2 * FT_DIGS);
+	dv_copy(t[6] + FT_DIGS, t[3], FT_DIGS);
+	dv_copy(t[6], t[3] + FT_DIGS, FT_DIGS);
+	dv_copy(t[7] + FT_DIGS, t[5], FT_DIGS);
+	dv_copy(t[7], t[5] + FT_DIGS, FT_DIGS);
+	dv_copy(t[8] + FT_DIGS, t[4], FT_DIGS);
+	dv_copy(t[8], t[4] + FT_DIGS, FT_DIGS);
 	for (i = 1; i < 9; i++) {
-		ft_addd_low(table[9 + i], table[9], table[i], 2 * FT_DIGS, 2 * FT_DIGS);
+		ft_addd_low(t[9 + i], t[9], t[i], 2 * FT_DIGS, 2 * FT_DIGS);
 	}
-	dv_copy(table[18] + FT_DIGS, table[9], FT_DIGS);
-	dv_copy(table[18], table[9] + FT_DIGS, FT_DIGS);
-	dv_copy(table[19] + FT_DIGS, table[11], FT_DIGS);
-	dv_copy(table[19], table[11] + FT_DIGS, FT_DIGS);
-	dv_copy(table[20] + FT_DIGS, table[10], FT_DIGS);
-	dv_copy(table[20], table[10] + FT_DIGS, FT_DIGS);
-	dv_copy(table[21] + FT_DIGS, table[15], FT_DIGS);
-	dv_copy(table[21], table[15] + FT_DIGS, FT_DIGS);
-	dv_copy(table[22] + FT_DIGS, table[17], FT_DIGS);
-	dv_copy(table[22], table[17] + FT_DIGS, FT_DIGS);
-	dv_copy(table[23] + FT_DIGS, table[16], FT_DIGS);
-	dv_copy(table[23], table[16] + FT_DIGS, FT_DIGS);
-	dv_copy(table[24] + FT_DIGS, table[12], FT_DIGS);
-	dv_copy(table[24], table[12] + FT_DIGS, FT_DIGS);
-	dv_copy(table[25] + FT_DIGS, table[14], FT_DIGS);
-	dv_copy(table[25], table[14] + FT_DIGS, FT_DIGS);
-	dv_copy(table[26] + FT_DIGS, table[13], FT_DIGS);
-	dv_copy(table[26], table[13] + FT_DIGS, FT_DIGS);
+	dv_copy(t[18] + FT_DIGS, t[9], FT_DIGS);
+	dv_copy(t[18], t[9] + FT_DIGS, FT_DIGS);
+	dv_copy(t[19] + FT_DIGS, t[11], FT_DIGS);
+	dv_copy(t[19], t[11] + FT_DIGS, FT_DIGS);
+	dv_copy(t[20] + FT_DIGS, t[10], FT_DIGS);
+	dv_copy(t[20], t[10] + FT_DIGS, FT_DIGS);
+	dv_copy(t[21] + FT_DIGS, t[15], FT_DIGS);
+	dv_copy(t[21], t[15] + FT_DIGS, FT_DIGS);
+	dv_copy(t[22] + FT_DIGS, t[17], FT_DIGS);
+	dv_copy(t[22], t[17] + FT_DIGS, FT_DIGS);
+	dv_copy(t[23] + FT_DIGS, t[16], FT_DIGS);
+	dv_copy(t[23], t[16] + FT_DIGS, FT_DIGS);
+	dv_copy(t[24] + FT_DIGS, t[12], FT_DIGS);
+	dv_copy(t[24], t[12] + FT_DIGS, FT_DIGS);
+	dv_copy(t[25] + FT_DIGS, t[14], FT_DIGS);
+	dv_copy(t[25], t[14] + FT_DIGS, FT_DIGS);
+	dv_copy(t[26] + FT_DIGS, t[13], FT_DIGS);
+	dv_copy(t[26], t[13] + FT_DIGS, FT_DIGS);
 
 	i = l = 0;
 	while (i < ft_trits(a)) {
@@ -169,7 +129,7 @@ void ft_muln_low(dig_t *c, dig_t *a, dig_t *b) {
 	}
 
 	for (i = l - 1; i > 0; i--) {
-		ft_addd_low(c, c, table[k[i]], 2 * FT_DIGS, 2 * FT_DIGS);
+		ft_addd_low(c, c, t[k[i]], 2 * FT_DIGS, 2 * FT_DIGS);
 
 		carry = ft_lshb_low(c, c, 3);
 		ft_lshb_low(c + FT_DIGS / 2, c + FT_DIGS / 2, 3);
@@ -179,85 +139,8 @@ void ft_muln_low(dig_t *c, dig_t *a, dig_t *b) {
 		ft_lshb_low(c + FT_DIGS + FT_DIGS / 2, c + FT_DIGS + FT_DIGS / 2, 3);
 		c[FT_DIGS + FT_DIGS / 2] |= carry;
 	}
-	ft_addd_low(c, c, table[k[0]], 2 * FT_DIGS, 2 * FT_DIGS);
-
-	for (i = 0; i < 81; i++) {
-		dv_free(table[i]);
-	}
+	ft_addd_low(c, c, t[k[0]], 2 * FT_DIGS, 2 * FT_DIGS);
 }
-
-//void ft_muld_low(dig_t *c, dig_t *a, dig_t *b, int size) {
-//  dv_t table[16];
-//  dig_t u, *tmpa, *tmpc, r0, r1, r2, r4, r8;
-//  int i, j;
-//
-//  for (i = 0; i < 16; i++) {
-//      dv_null(table[i]);
-//  }
-//
-//  dv_zero(c, 2 * size);
-//
-//  for (i = 0; i < 16; i++) {
-//      dv_new(table[i]);
-//      dv_zero(table[i], size + 1);
-//  }
-//
-//  u = 0;
-//  for (i = 0; i < size; i++) {
-//      r1 = r0 = b[i];
-//      r2 = (r0 << 1) | (u >> (FT_DIGIT - 1));
-//      r4 = (r0 << 2) | (u >> (FT_DIGIT - 2));
-//      r8 = (r0 << 3) | (u >> (FT_DIGIT - 3));
-//      table[0][i] = 0;
-//      table[1][i] = r1;
-//      table[2][i] = r2;
-//      table[3][i] = r1 ^ r2;
-//      table[4][i] = r4;
-//      table[5][i] = r1 ^ r4;
-//      table[6][i] = r2 ^ r4;
-//      table[7][i] = r1 ^ r2 ^ r4;
-//      table[8][i] = r8;
-//      table[9][i] = r1 ^ r8;
-//      table[10][i] = r2 ^ r8;
-//      table[11][i] = r1 ^ r2 ^ r8;
-//      table[12][i] = r4 ^ r8;
-//      table[13][i] = r1 ^ r4 ^ r8;
-//      table[14][i] = r2 ^ r4 ^ r8;
-//      table[15][i] = r1 ^ r2 ^ r4 ^ r8;
-//      u = r1;
-//  }
-//
-//  if (u > 0) {
-//      r2 = u >> (FT_DIGIT - 1);
-//      r4 = u >> (FT_DIGIT - 2);
-//      r8 = u >> (FT_DIGIT - 3);
-//      table[0][size] = table[1][size] = 0;
-//      table[2][size] = table[3][size] = r2;
-//      table[4][size] = table[5][size] = r4;
-//      table[6][size] = table[7][size] = r2 ^ r4;
-//      table[8][size] = table[9][size] = r8;
-//      table[10][size] = table[11][size] = r2 ^ r8;
-//      table[12][size] = table[13][size] = r4 ^ r8;
-//      table[14][size] = table[15][size] = r2 ^ r4 ^ r8;
-//  }
-//
-//  for (i = FT_DIGIT - 4; i > 0; i -= 4) {
-//      tmpa = a;
-//      tmpc = c;
-//      for (j = 0; j < size; j++, tmpa++, tmpc++) {
-//          u = (*tmpa >> i) & 0x0F;
-//          ft_addd_low(tmpc, tmpc, table[u], size + 1);
-//      }
-//      bn_lshb_low(c, c, 2 * size, 4);
-//  }
-//  for (j = 0; j < size; j++, a++, c++) {
-//      u = *a & 0x0F;
-//      ft_addd_low(c, c, table[u], size + 1);
-//  }
-//  for (i = 0; i < 16; i++) {
-//      dv_free(table[i]);
-//  }
-//}
 
 void ft_mulm_low(dig_t *c, dig_t *a, dig_t *b) {
 	dig_t align t[2 * FT_DIGS];
