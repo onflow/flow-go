47ddd34de99326d8e8d4d766408b84cb528e6cb0
dfaranha <dfaranha@gmail.com>
2013-09-03 08:18:08 +0000

    Fix pairing types based on field size.

--- include/relic_pc.h
+++ include/relic_pc.h
@@ -259,8 +259,15 @@ typedef CAT(GT_LOWER, t) gt_t;
  * @{
  */
 #if PC_CUR == PRIME
+
+#if FP_PRIME < 1536
+#define pc_map_is_type1()	(1)
+#define pc_map_is_type3()	(0)
+#else
 #define pc_map_is_type1()	(0)
 #define pc_map_is_type3()	(1)
+#endif
+
 #elif PC_CUR == CHAR2
 #define pc_map_is_type1()	(1)
 #define pc_map_is_type3()	(0)
