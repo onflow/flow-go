9c87b9ef0e4b7d4da9687c22a175c0fac6756404
dfaranha <dfaranha@gmail.com>
2013-12-11 17:16:46 +0000

    Add implementation of HMAC and corresponding test vectors for its SHA256 version.

---
+++ src/md/relic_md_hmac.c
@@ -0,0 +1,71 @@
+/*
+ * RELIC is an Efficient LIbrary for Cryptography
+ * Copyright (C) 2007-2013 RELIC Authors
+ *
+ * This file is part of RELIC. RELIC is legal property of its developers,
+ * whose names are not listed here. Please refer to the COPYRIGHT file
+ * for contact information.
+ *
+ * RELIC is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * RELIC is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with RELIC. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+/**
+ * @file
+ *
+ * Implementation of Hash-based Message Authentication Code.
+ *
+ * @version $Id$
+ * @ingroup md
+ */
+
+#include <string.h>
+
+#include "relic_conf.h"
+#include "relic_core.h"
+#include "relic_util.h"
+#include "relic_md.h"
+
+/*============================================================================*/
+/* Public definitions                                                         */
+/*============================================================================*/
+
+void md_hmac(unsigned char *mac, unsigned char *in, int in_len,
+		unsigned char *key, int key_len) {
+#if MD_MAP == SHONE || MD_MAP == SH224 || MD_MAP == SH256
+	int block_size = 64;
+#elif MD_MAP == SH384 || MD_MAP == SH512
+	int block_size = 128;
+#endif
+	unsigned char opad[block_size + MD_LEN], ipad[block_size + in_len];
+
+	if (key_len > block_size) {
+		unsigned char _key[MD_LEN];
+		md_map(_key, key, key_len);
+		key = _key;
+		key_len = MD_LEN;
+	}
+	if (key_len < block_size) {
+		unsigned char _key[block_size];
+		memcpy(_key, key, key_len);
+		memset(_key + key_len, 0, block_size - key_len);
+		key = _key;
+	}
+	for (int i = 0; i < block_size; i++) {
+		opad[i] = 0x5C ^ key[i];
+		ipad[i] = 0x36 ^ key[i];
+	}
+	memcpy(ipad + block_size, in, in_len);
+	md_map(opad + block_size, ipad, block_size + in_len);
+	md_map(mac, opad, block_size + MD_LEN);
+}
