647b693112aa75e3f67daf6768b499322aa02fed
dfaranha <dfaranha@gmail.com>
2014-02-05 17:23:50 +0000

    Update tests with serialization functions.

--- test/test_ec.c
+++ test/test_ec.c
@@ -61,8 +61,9 @@ static int memory(void) {
 }
 
 int util(void) {
-	int code = STS_ERR;
+	int l, code = STS_ERR;
 	ec_t a, b, c;
+	uint8_t bin[2 * FC_BYTES + 1];
 
 	ec_null(a);
 	ec_null(b);
@@ -120,10 +121,33 @@ int util(void) {
 		TEST_BEGIN("validity test is correct") {
 			ec_rand(a);
 			TEST_ASSERT(ec_is_valid(a), end);
-			dv_zero(a->x, EC_DIGS);
+			dv_zero(a->x, FC_DIGS);
 			TEST_ASSERT(!ec_is_valid(a), end);
 		}
 		TEST_END;
+
+		TEST_BEGIN("reading and writing a point are consistent") {
+			for (int j = 0; j < 2; j++) {
+				ec_set_infty(a);
+				ec_size_bin(&l, a, j);
+				ec_write_bin(bin, l, a, j);
+				ec_read_bin(b, bin, l);
+				TEST_ASSERT(ec_cmp(a, b) == CMP_EQ, end);
+				ec_rand(a);
+				ec_size_bin(&l, a, j);
+				ec_write_bin(bin, l, a, j);
+				ec_read_bin(b, bin, l);
+				TEST_ASSERT(ec_cmp(a, b) == CMP_EQ, end);
+				ec_rand(a);
+				ec_dbl(a, a);
+				ec_size_bin(&l, a, j);
+				ec_norm(a, a);
+				ec_write_bin(bin, l, a, j);
+				ec_read_bin(b, bin, l);
+				TEST_ASSERT(ec_cmp(a, b) == CMP_EQ, end);						
+			}
+		}
+		TEST_END;		
 	}
 	CATCH_ANY {
 		util_print("FATAL ERROR!\n");
