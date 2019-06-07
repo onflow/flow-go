659663506b6e207400f39914adaf1479c41267e1
Brian Ho <brian.ho@dapperlabs.com>
2019-06-07 15:26:17 -0700

    initial boilerplate structure

---
+++ main.go
@@ -0,0 +1,14 @@
+package main
+
+import (
+	"github.com/dapperlabs/bamboo-emulator/server"
+	// "github.com/dapperlabs/bamboo-emulator/client"
+	// "github.com/dapperlabs/bamboo-emulator/data"
+	// "github.com/dapperlabs/bamboo-emulator/nodes/access"
+	// "github.com/dapperlabs/bamboo-emulator/nodes/security"
+	// "github.com/dapperlabs/bamboo-emulator/nodes/execution"
+)
+
+func main() {
+	server.Start()
+}
