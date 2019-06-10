11b1c16d2293a8dfd07e6387a5f091d9fa17ed4e
Peter Siemens <peterjsiemens@gmail.com>
2019-06-10 15:29:33 -0700

    restructure protobuf files

--- server/server.go
+++ server/server.go
@@ -8,43 +8,43 @@ import (
 
 	"google.golang.org/grpc"
 
-	"github.com/dapperlabs/bamboo-emulator/gen/grpc/access/apiv1"
+	"github.com/dapperlabs/bamboo-emulator/gen/grpc/services/accessv1"
 )
 
 type bambooAccessAPIServer struct{}
 
 // SendTransaction submits a transaction to the network.
-func (s *bambooAccessAPIServer) SendTransaction(context.Context, *apiv1.SendTransactionRequest) (*apiv1.SendTransactionResponse, error) {
+func (s *bambooAccessAPIServer) SendTransaction(context.Context, *accessv1.SendTransactionRequest) (*accessv1.SendTransactionResponse, error) {
 	return nil, nil
 }
 
 // GetBlockByHash gets a block by hash.
-func (s *bambooAccessAPIServer) GetBlockByHash(context.Context, *apiv1.GetBlockByHashRequest) (*apiv1.GetBlockByHashResponse, error) {
+func (s *bambooAccessAPIServer) GetBlockByHash(context.Context, *accessv1.GetBlockByHashRequest) (*accessv1.GetBlockByHashResponse, error) {
 	return nil, nil
 }
 
 // GetBlockByNumber gets a block by number.
-func (s *bambooAccessAPIServer) GetBlockByNumber(context.Context, *apiv1.GetBlockByNumberRequest) (*apiv1.GetBlockByNumberResponse, error) {
+func (s *bambooAccessAPIServer) GetBlockByNumber(context.Context, *accessv1.GetBlockByNumberRequest) (*accessv1.GetBlockByNumberResponse, error) {
 	return nil, nil
 }
 
 // GetLatestBlock gets the latest sealed block.
-func (s *bambooAccessAPIServer) GetLatestBlock(context.Context, *apiv1.GetLatestBlockRequest) (*apiv1.GetLatestBlockResponse, error) {
+func (s *bambooAccessAPIServer) GetLatestBlock(context.Context, *accessv1.GetLatestBlockRequest) (*accessv1.GetLatestBlockResponse, error) {
 	return nil, nil
 }
 
 // GetTransactions gets a transaction by hash.
-func (s *bambooAccessAPIServer) GetTransaction(context.Context, *apiv1.GetTransactionRequest) (*apiv1.GetTransactionResponse, error) {
+func (s *bambooAccessAPIServer) GetTransaction(context.Context, *accessv1.GetTransactionRequest) (*accessv1.GetTransactionResponse, error) {
 	return nil, nil
 }
 
 // GetBalance returns the balance of an address.
-func (s *bambooAccessAPIServer) GetBalance(context.Context, *apiv1.GetBalanceRequest) (*apiv1.GetBalanceResponse, error) {
+func (s *bambooAccessAPIServer) GetBalance(context.Context, *accessv1.GetBalanceRequest) (*accessv1.GetBalanceResponse, error) {
 	return nil, nil
 }
 
 // CallContract performs a contract call.
-func (s *bambooAccessAPIServer) CallContract(context.Context, *apiv1.CallContractRequest) (*apiv1.CallContractResponse, error) {
+func (s *bambooAccessAPIServer) CallContract(context.Context, *accessv1.CallContractRequest) (*accessv1.CallContractResponse, error) {
 	return nil, nil
 }
 
@@ -57,7 +57,7 @@ func Start(port int) {
 
 	grpcServer := grpc.NewServer()
 
-	apiv1.RegisterBambooAccessAPIServer(grpcServer, &bambooAccessAPIServer{})
+	accessv1.RegisterBambooAccessAPIServer(grpcServer, &bambooAccessAPIServer{})
 
 	grpcServer.Serve(lis)
 }
