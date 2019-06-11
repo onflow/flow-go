510a6db2b63ea1d22c2c94471d2b6ac43b284e8b
Brian Ho <brian.ho@dapperlabs.com>
2019-06-11 11:47:34 -0700

    add crypto package + implement hashing functionality

--- data/hash.go
+++ data/hash.go
@@ -1,8 +1,45 @@
 package data
 
+import (
+	"encoding/hex"
+
+	"github.com/dapperlabs/bamboo-emulator/crypto"
+)
+
 const (
 	HashLength = 32
 )
 
 // Hash represents the 32 byte Keccak256 hash of arbitrary data.
 type Hash [HashLength]byte
+
+// BytesToHash sets b to hash.
+// If b is larger than len(h), b will be cropped from the left.
+func BytesToHash(b []byte) Hash {
+	var h Hash
+	h.SetBytes(b)
+	return h
+}
+
+// SetBytes sets the hash to the value of b.
+// If b is larger than len(h), b will be cropped from the left.
+func (h *Hash) SetBytes(b []byte) {
+	if len(b) > len(h) {
+		b = b[len(b)-HashLength:]
+	}
+
+	copy(h[HashLength-len(b):], b)
+}
+
+// Bytes gets the byte representation of the underlying hash.
+func (h Hash) Bytes() []byte { return h[:] }
+
+// NewHash computes the Keccak256 hash of some arbitrary set of data.
+func NewHash(data []byte) Hash {
+	return BytesToHash(crypto.ComputeHash(data))
+}
+
+// ToString encodes Hash as a readable string for logging purposes.
+func (h Hash) ToString() string {
+	return hex.EncodeToString(h.Bytes())
+}
--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/dapperlabs/bamboo-emulator
 
 go 1.12
+
+require golang.org/x/crypto v0.0.0-20190605123033-f99c8df09eb5
