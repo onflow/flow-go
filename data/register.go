724e68d522c98417940256ae30b3611ac8d45b1d
Peter Siemens <peterjsiemens@gmail.com>
2019-06-12 15:46:54 -0700

    Add doc comments to registers.go

--- data/register.go
+++
@@ -1,9 +0,0 @@
-package data
-
-type Registers map[string][]byte
-
-func (r *Registers) Update(registers Registers) {
-	for id, value := range registers {
-		(*r)[id] = value
-	}
-}
---
+++ data/registers.go
@@ -0,0 +1,14 @@
+package data
+
+// Registers is a map of register values.
+type Registers map[string][]byte
+
+// Update merges this register map with the values of another register map.
+//
+// If the other map contains keys that already exist in this map, the values
+// in this map are overwritten.
+func (r *Registers) Update(registers Registers) {
+	for id, value := range registers {
+		(*r)[id] = value
+	}
+}
