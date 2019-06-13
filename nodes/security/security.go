ceb758b087a02cab091d93924b0c97ea935d2938
Brian Ho <brian.ho@dapperlabs.com>
2019-06-13 15:10:37 -0700

    outline security node functionality

--- nodes/security/security.go
+++ nodes/security/security.go
@@ -1,1 +1,39 @@
 package security
+
+import (
+	"time"
+
+	"github.com/dapperlabs/bamboo-emulator/crypto"
+	"github.com/dapperlabs/bamboo-emulator/data"
+)
+
+// Node simulates the behaviour of a Bamboo access node.
+type Node struct {
+	state *data.WorldState
+}
+
+// NewNode returns a new simulated security node.
+func NewNode(state *data.WorldState) *Node {
+	return &Node{
+		state: state,
+	}
+}
+
+func (n *Node) MintNoOpBlock() *data.Block {
+	latestBlock := n.state.GetLatestBlock()
+
+	newBlock := data.Block{
+		Number:            latestBlock.Number,
+		Timestamp:         time.Now(),
+		PrevBlockHash:     latestBlock.Hash(),
+		Status:            data.BlockPending,
+		CollectionHashes:  []crypto.Hash{},
+		TransactionHashes: []crypto.Hash{},
+	}
+
+	return &newBlock
+}
+
+func (n *Node) SealBlock(hash crypto.Hash) error {
+	return n.state.SealBlock(hash)
+}
