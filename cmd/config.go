6cb173da03a4dd64eb74ffe76ca6c117bb32ee86
Peter Siemens <peterjsiemens@gmail.com>
2019-06-14 15:44:30 -0700

    Update config.go

--- cmd/config.go
+++ cmd/config.go
@@ -4,6 +4,6 @@ import "time"
 
 type Config struct {
 	Port               int           `default:"5000" flag:"port"`
-	CollectionInterval time.Duration `default:"2s"`
+	CollectionInterval time.Duration `default:"1s"`
 	BlockInterval      time.Duration `default:"5s"`
 }
