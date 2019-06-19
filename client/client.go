87ca62ed1b60b0a6b468e79090d8515646680661
Brian Ho <brian.ho@dapperlabs.com>
2019-06-19 11:38:01 -0700

    initial client boilerplate

--- client/client.go
+++ client/client.go
@@ -1,1 +1,29 @@
 package client
+
+import (
+	"fmt"
+
+	"github.com/dapperlabs/bamboo-emulator/server"
+)
+
+type Client struct {
+	Server server.Server
+}
+
+// Connect connects to a locally running Bamboo Emulator server.
+func Connect() {
+	// TODO: connect to Bamboo Local Server
+	// also creates an HD wallet for client
+	fmt.Println("connected to access node!")
+}
+
+// LogCommands displays all the usable commands to a client. 
+func LogCommands() {
+	// TODO: log all help commands available to Client
+	fmt.Println("here are all the commands you can use!")
+}
+
+// CreateAccount creates an account for the client.
+func CreateAccount() {
+	// TODO: create a new account in the client's wallet
+}
