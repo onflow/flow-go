8ea187b3b265053bde6f93fcf12175ffdf130867
Bastian Müller <bastian@axiomzen.co>
2019-07-01 06:17:52 -0700

    add tests for underscores in integer literals

--- execution/strictus/parser_test.go
+++ execution/strictus/parser_test.go
@@ -1111,6 +1111,7 @@ func TestParseIntegerLiterals(t *testing.T) {
 		const octal = 0o32
         const hex = 0xf2
         const binary = 0b101010
+        const decimal = 1234567890
 	`)
 
 	Expect(errors).Should(BeEmpty())
@@ -1151,11 +1152,234 @@ func TestParseIntegerLiterals(t *testing.T) {
 		IdentifierPos: &Position{Offset: 61, Line: 4, Column: 14},
 	}
 
+	decimal := &VariableDeclaration{
+		Identifier: "decimal",
+		IsConst:    true,
+		Value: &IntExpression{
+			Value: big.NewInt(1234567890),
+			Pos:   &Position{Offset: 103, Line: 5, Column: 24},
+		},
+		StartPos:      &Position{Offset: 87, Line: 5, Column: 8},
+		EndPos:        &Position{Offset: 103, Line: 5, Column: 24},
+		IdentifierPos: &Position{Offset: 93, Line: 5, Column: 14},
+	}
+
 	expected := &Program{
-		Declarations: []Declaration{octal, hex, binary},
+		Declarations: []Declaration{octal, hex, binary, decimal},
 	}
 
 	Expect(actual).Should(Equal(expected))
+}
+
+func TestParseIntegerLiteralsWithUnderscores(t *testing.T) {
+	RegisterTestingT(t)
+
+	actual, errors := parser.Parse(`
+		const octal = 0o32_45
+        const hex = 0xf2_09
+        const binary = 0b101010_101010
+        const decimal = 1_234_567_890
+	`)
+
+	Expect(errors).Should(BeEmpty())
+
+	octal := &VariableDeclaration{
+		Identifier: "octal",
+		IsConst:    true,
+		Value: &IntExpression{
+			Value: big.NewInt(1701),
+			Pos:   &Position{Offset: 17, Line: 2, Column: 16},
+		},
+		StartPos:      &Position{Offset: 3, Line: 2, Column: 2},
+		EndPos:        &Position{Offset: 17, Line: 2, Column: 16},
+		IdentifierPos: &Position{Offset: 9, Line: 2, Column: 8},
+	}
+
+	hex := &VariableDeclaration{
+		Identifier: "hex",
+		IsConst:    true,
+		Value: &IntExpression{
+			Value: big.NewInt(61961),
+			Pos:   &Position{Offset: 45, Line: 3, Column: 20},
+		},
+		StartPos:      &Position{Offset: 33, Line: 3, Column: 8},
+		EndPos:        &Position{Offset: 45, Line: 3, Column: 20},
+		IdentifierPos: &Position{Offset: 39, Line: 3, Column: 14},
+	}
+
+	binary := &VariableDeclaration{
+		Identifier: "binary",
+		IsConst:    true,
+		Value: &IntExpression{
+			Value: big.NewInt(2730),
+			Pos:   &Position{Offset: 76, Line: 4, Column: 23},
+		},
+		StartPos:      &Position{Offset: 61, Line: 4, Column: 8},
+		EndPos:        &Position{Offset: 76, Line: 4, Column: 23},
+		IdentifierPos: &Position{Offset: 67, Line: 4, Column: 14},
+	}
+
+	decimal := &VariableDeclaration{
+		Identifier: "decimal",
+		IsConst:    true,
+		Value: &IntExpression{
+			Value: big.NewInt(1234567890),
+			Pos:   &Position{Offset: 116, Line: 5, Column: 24},
+		},
+		StartPos:      &Position{Offset: 100, Line: 5, Column: 8},
+		EndPos:        &Position{Offset: 116, Line: 5, Column: 24},
+		IdentifierPos: &Position{Offset: 106, Line: 5, Column: 14},
+	}
+
+	expected := &Program{
+		Declarations: []Declaration{octal, hex, binary, decimal},
+	}
+
+	Expect(actual).Should(Equal(expected))
+}
+
+func TestParseInvalidOctalIntegerLiteralWithLeadingUnderscore(t *testing.T) {
+	RegisterTestingT(t)
+
+	actual, errors := parser.Parse(`
+		const octal = 0o_32_45
+	`)
+
+	Expect(actual).Should(BeNil())
+
+	Expect(errors).Should(HaveLen(1))
+	syntaxError := errors[0].(*parser.InvalidIntegerLiteralError)
+	Expect(*syntaxError).To(MatchFields(IgnoreExtras, Fields{
+		"Pos":                       Equal(&Position{Offset: 17, Line: 2, Column: 16}),
+		"IntegerLiteralKind":        Equal(parser.IntegerLiteralKindOctal),
+		"InvalidIntegerLiteralKind": Equal(parser.InvalidIntegerLiteralKindLeadingUnderscore),
+	}))
+}
+
+func TestParseInvalidOctalIntegerLiteralWithTrailingUnderscore(t *testing.T) {
+	RegisterTestingT(t)
+
+	actual, errors := parser.Parse(`
+		const octal = 0o32_45_
+	`)
+
+	Expect(actual).Should(BeNil())
+
+	Expect(errors).Should(HaveLen(1))
+	syntaxError := errors[0].(*parser.InvalidIntegerLiteralError)
+	Expect(*syntaxError).To(MatchFields(IgnoreExtras, Fields{
+		"Pos":                       Equal(&Position{Offset: 17, Line: 2, Column: 16}),
+		"IntegerLiteralKind":        Equal(parser.IntegerLiteralKindOctal),
+		"InvalidIntegerLiteralKind": Equal(parser.InvalidIntegerLiteralKindTrailingUnderscore),
+	}))
+}
+
+func TestParseInvalidBinaryIntegerLiteralWithLeadingUnderscore(t *testing.T) {
+	RegisterTestingT(t)
+
+	actual, errors := parser.Parse(`
+		const binary = 0b_101010_101010
+	`)
+
+	Expect(actual).Should(BeNil())
+
+	Expect(errors).Should(HaveLen(1))
+	syntaxError := errors[0].(*parser.InvalidIntegerLiteralError)
+	Expect(*syntaxError).To(MatchFields(IgnoreExtras, Fields{
+		"Pos":                       Equal(&Position{Offset: 18, Line: 2, Column: 17}),
+		"IntegerLiteralKind":        Equal(parser.IntegerLiteralKindBinary),
+		"InvalidIntegerLiteralKind": Equal(parser.InvalidIntegerLiteralKindLeadingUnderscore),
+	}))
+}
+
+func TestParseInvalidBinaryIntegerLiteralWithTrailingUnderscore(t *testing.T) {
+	RegisterTestingT(t)
+
+	actual, errors := parser.Parse(`
+		const binary = 0b101010_101010_
+	`)
+
+	Expect(actual).Should(BeNil())
+
+	Expect(errors).Should(HaveLen(1))
+	syntaxError := errors[0].(*parser.InvalidIntegerLiteralError)
+	Expect(*syntaxError).To(MatchFields(IgnoreExtras, Fields{
+		"Pos":                       Equal(&Position{Offset: 18, Line: 2, Column: 17}),
+		"IntegerLiteralKind":        Equal(parser.IntegerLiteralKindBinary),
+		"InvalidIntegerLiteralKind": Equal(parser.InvalidIntegerLiteralKindTrailingUnderscore),
+	}))
+}
+
+func TestParseInvalidDecimalIntegerLiteralWithTrailingUnderscore(t *testing.T) {
+	RegisterTestingT(t)
+
+	actual, errors := parser.Parse(`
+		const decimal = 1_234_567_890_
+	`)
+
+	Expect(actual).Should(BeNil())
+
+	Expect(errors).Should(HaveLen(1))
+	syntaxError := errors[0].(*parser.InvalidIntegerLiteralError)
+	Expect(*syntaxError).To(MatchFields(IgnoreExtras, Fields{
+		"Pos":                       Equal(&Position{Offset: 19, Line: 2, Column: 18}),
+		"IntegerLiteralKind":        Equal(parser.IntegerLiteralKindDecimal),
+		"InvalidIntegerLiteralKind": Equal(parser.InvalidIntegerLiteralKindTrailingUnderscore),
+	}))
+}
+
+func TestParseInvalidHexadecimalIntegerLiteralWithLeadingUnderscore(t *testing.T) {
+	RegisterTestingT(t)
+
+	actual, errors := parser.Parse(`
+		const hex = 0x_f2_09
+	`)
+
+	Expect(actual).Should(BeNil())
+
+	Expect(errors).Should(HaveLen(1))
+	syntaxError := errors[0].(*parser.InvalidIntegerLiteralError)
+	Expect(*syntaxError).To(MatchFields(IgnoreExtras, Fields{
+		"Pos":                       Equal(&Position{Offset: 15, Line: 2, Column: 14}),
+		"IntegerLiteralKind":        Equal(parser.IntegerLiteralKindHexadecimal),
+		"InvalidIntegerLiteralKind": Equal(parser.InvalidIntegerLiteralKindLeadingUnderscore),
+	}))
+}
+
+func TestParseInvalidHexadecimalIntegerLiteralWithTrailingUnderscore(t *testing.T) {
+	RegisterTestingT(t)
+
+	actual, errors := parser.Parse(`
+		const hex = 0xf2_09_
+	`)
+
+	Expect(actual).Should(BeNil())
+
+	Expect(errors).Should(HaveLen(1))
+	syntaxError := errors[0].(*parser.InvalidIntegerLiteralError)
+	Expect(*syntaxError).To(MatchFields(IgnoreExtras, Fields{
+		"Pos":                       Equal(&Position{Offset: 15, Line: 2, Column: 14}),
+		"IntegerLiteralKind":        Equal(parser.IntegerLiteralKindHexadecimal),
+		"InvalidIntegerLiteralKind": Equal(parser.InvalidIntegerLiteralKindTrailingUnderscore),
+	}))
+}
+
+func TestParseInvalidIntegerLiteral(t *testing.T) {
+	RegisterTestingT(t)
+
+	actual, errors := parser.Parse(`
+		const hex = 0z123
+	`)
+
+	Expect(actual).Should(BeNil())
+
+	Expect(errors).Should(HaveLen(1))
+	syntaxError := errors[0].(*parser.InvalidIntegerLiteralError)
+	Expect(*syntaxError).To(MatchFields(IgnoreExtras, Fields{
+		"Pos":                       Equal(&Position{Offset: 15, Line: 2, Column: 14}),
+		"IntegerLiteralKind":        Equal(parser.IntegerLiteralKindUnknown),
+		"InvalidIntegerLiteralKind": Equal(parser.InvalidIntegerLiteralKindUnknownPrefix),
+	}))
 }
 
 func TestParseIntegerTypes(t *testing.T) {
