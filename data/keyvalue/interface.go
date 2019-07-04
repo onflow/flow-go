ba7076b2af056bb335ef2ac1b11224c9de2785d9
pazams <maor@pazams.com>
2019-07-04 10:57:46 -0700

    Add delete

--- data/keyvalue/interface.go
+++ data/keyvalue/interface.go
@@ -8,8 +8,9 @@ type DBConnection interface {
 // QueryBuilder builds a key value query
 type QueryBuilder interface {
 	InTransaction()
-	Set(namespace string, key string)
 	Get(namespace string, key string)
+	Set(namespace string, key string)
+	Delete(namespace string, key string)
 	MustBuild() // MustBuild is intended to be called once per query on server startup for performance considerations of some providers.
 	Execute() (result string, err error)
 }
