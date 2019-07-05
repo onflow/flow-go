b5380af212b777af7c768b1bf6d34a18925ad3cb
pazams <maor@pazams.com>
2019-07-05 16:26:38 -0700

    Update interface

--- data/keyvalue/interface.go
+++ data/keyvalue/interface.go
@@ -3,13 +3,15 @@ package keyvalue
 // DBConnection abstracts a db connection
 type DBConnection interface {
 	NewQuery() QueryBuilder
+	// migrateup
+	// migratedowne
 }
 
 // QueryBuilder builds a key value query
 type QueryBuilder interface {
 	InTransaction()
 	Get(namespace string, key string)
-	Set(namespace string, key string)
+	Set(namespace string, key string, value string)
 	Delete(namespace string, key string)
 	MustBuild() // MustBuild is intended to be called once per query on server startup for performance considerations of some providers.
 	Execute() (result string, err error)
