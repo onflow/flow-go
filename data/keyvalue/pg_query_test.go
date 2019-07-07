42c1e334ba79ac35a2ba625ef3bf29f3516e12fc
pazams <maor@pazams.com>
2019-07-07 14:46:17 -0700

    Refactor test assertions

--- data/keyvalue/pg_query_test.go
+++ data/keyvalue/pg_query_test.go
@@ -4,9 +4,12 @@ import (
 	"testing"
 
 	"github.com/dapperlabs/bamboo-node/utils/unittest"
+	. "github.com/onsi/gomega"
 )
 
 func TestGet(t *testing.T) {
+	gomega := NewWithT(t)
+
 	table := "tableA"
 	key := "keyA"
 
@@ -15,11 +18,13 @@ func TestGet(t *testing.T) {
 	q.MustBuild()
 	query, params := q.debug()
 
-	unittest.ExpectString(query, "SELECT value FROM ?0 WHERE key=?1 ; ", t)
-	unittest.ExpectStrings(params, []string{table, key}, t)
+	gomega.Expect(query).To(Equal("SELECT value FROM ?0 WHERE key=?1 ; "))
+	gomega.Expect(params).To(Equal([]string{table, key}))
 }
 
 func TestSet(t *testing.T) {
+	gomega := NewWithT(t)
+
 	table := "tableA"
 	key := "keyA"
 	value := "valueA"
@@ -28,14 +33,16 @@ func TestSet(t *testing.T) {
 	q.Set(table, key)
 	q.MustBuild()
 	err := q.mergeSetParams([]string{value})
+	gomega.Expect(err).ToNot(HaveOccurred())
 	query, params := q.debug()
 
-	unittest.ExpectNoError(err, t)
-	unittest.ExpectString(query, "INSERT INTO ?0 (key, value) VALUES ('?1', '?2') ON CONFLICT (key) DO UPDATE SET value = ?2 ; ", t)
-	unittest.ExpectStrings(params, []string{table, key, value}, t)
+	gomega.Expect(query).To(Equal("INSERT INTO ?0 (key, value) VALUES ('?1', '?2') ON CONFLICT (key) DO UPDATE SET value = ?2 ; "))
+	gomega.Expect(params).To(Equal([]string{table, key, value}))
 }
 
 func TestSetWithExtraSetParams(t *testing.T) {
+	gomega := NewWithT(t)
+
 	table := "tableA"
 	key := "keyA"
 	value := "valueA"
@@ -44,13 +51,13 @@ func TestSetWithExtraSetParams(t *testing.T) {
 	q.Set(table, key)
 	q.MustBuild()
 	err := q.mergeSetParams([]string{value, "extra"})
-	q.debug()
-
-	unittest.ExpectError(err, t)
-	unittest.ExpectString(err.Error(), "Expected to substituted 1 set params, but received 2", t)
+	gomega.Expect(err).To(HaveOccurred())
+	gomega.Expect(err.Error()).To(Equal("Expected to substituted 1 set params, but received 2"))
 }
 
 func TestSetWithMissingSetParams(t *testing.T) {
+	gomega := NewWithT(t)
+
 	table := "tableA"
 	key := "keyA"
 	//value := "valueA"
@@ -59,34 +66,30 @@ func TestSetWithMissingSetParams(t *testing.T) {
 	q.Set(table, key)
 	q.MustBuild()
 	err := q.mergeSetParams([]string{})
-	q.debug()
-
-	unittest.ExpectError(err, t)
-	unittest.ExpectString(err.Error(), "Expected to substituted 1 set params, but received 0", t)
+	gomega.Expect(err).To(HaveOccurred())
+	gomega.Expect(err.Error()).To(Equal("Expected to substituted 1 set params, but received 0"))
 }
 
 func TestMultiSetNoTx(t *testing.T) {
 
-	defer unittest.ExpectPanic("Must use a transaction when changing more than one key", t)
-
 	table1 := "tableA"
 	key1 := "keyA"
-	value1 := "valueA"
+	// value1 := "valueA"
 
 	table2 := "tableB"
 	key2 := "keyB"
-	value2 := "valueB"
+	// value2 := "valueB"
 
 	q := pgSQLQuery{}
 	q.Set(table1, key1)
 	q.Set(table2, key2)
+	defer unittest.ExpectPanic("Must use a transaction when changing more than one key", t)
 	q.MustBuild()
-	err := q.mergeSetParams([]string{value1, value2})
-	unittest.ExpectNoError(err, t)
 
 }
 
 func TestMultiSetWithTx(t *testing.T) {
+	gomega := NewWithT(t)
 
 	table1 := "tableA"
 	key1 := "keyA"
@@ -102,14 +105,15 @@ func TestMultiSetWithTx(t *testing.T) {
 	q.InTransaction()
 	q.MustBuild()
 	err := q.mergeSetParams([]string{value1, value2})
+	gomega.Expect(err).ToNot(HaveOccurred())
 	query, params := q.debug()
 
-	unittest.ExpectNoError(err, t)
-	unittest.ExpectString(query, "BEGIN; INSERT INTO ?0 (key, value) VALUES ('?1', '?2') ON CONFLICT (key) DO UPDATE SET value = ?2 ; INSERT INTO ?3 (key, value) VALUES ('?4', '?5') ON CONFLICT (key) DO UPDATE SET value = ?5 ;  COMMIT;", t)
-	unittest.ExpectStrings(params, []string{table1, key1, value1, table2, key2, value2}, t)
+	gomega.Expect(query).To(Equal("BEGIN; INSERT INTO ?0 (key, value) VALUES ('?1', '?2') ON CONFLICT (key) DO UPDATE SET value = ?2 ; INSERT INTO ?3 (key, value) VALUES ('?4', '?5') ON CONFLICT (key) DO UPDATE SET value = ?5 ;  COMMIT;"))
+	gomega.Expect(params).To(Equal([]string{table1, key1, value1, table2, key2, value2}))
 }
 
 func TestMustBuildBeforeExecute(t *testing.T) {
+	gomega := NewWithT(t)
 
 	table := "tableA"
 	key := "keyA"
@@ -117,15 +121,14 @@ func TestMustBuildBeforeExecute(t *testing.T) {
 	q := pgSQLQuery{}
 	q.Get(table, key)
 	_, err := q.Execute()
-	unittest.ExpectError(err, t)
+	gomega.Expect(err).To(HaveOccurred())
 
 }
 
 func TestMustBuildWithInvalidQuery(t *testing.T) {
 
-	defer unittest.ExpectPanic("Empty query. must have at least one get/set/delete", t)
-
 	q := pgSQLQuery{}
+	defer unittest.ExpectPanic("Empty query. must have at least one get/set/delete", t)
 	q.MustBuild()
 
 }
--- utils/unittest/unittest.go
+++ utils/unittest/unittest.go
@@ -4,47 +4,12 @@ import (
 	"testing"
 )
 
-func ExpectNoError(err error, t *testing.T) {
-	if err != nil {
-		t.Error(err)
-	}
-}
-
-func ExpectError(err error, t *testing.T) {
-	if err == nil {
-		t.Error("Expected error but got err=nil")
-	}
-}
-
-func ExpectBool(actual, expected bool, t *testing.T) {
-	if actual != expected {
-		t.Errorf("expected %v to be %v", actual, expected)
-	}
-}
-
-func ExpectInt(actual, expected int, t *testing.T) {
-	if actual != expected {
-		t.Errorf("expected %v to be %v", actual, expected)
-	}
-}
-
-func ExpectString(actual, expected string, t *testing.T) {
-	if actual != expected {
-		t.Errorf("expected %v to be %v", actual, expected)
-	}
-}
-
-func ExpectStrings(actual, expected []string, t *testing.T) {
-	ExpectInt(len(actual), len(expected), t)
-	for i := range actual {
-		ExpectString(actual[i], expected[i], t)
-	}
-}
-
 func ExpectPanic(expectedMsg string, t *testing.T) {
 	if r := recover(); r != nil {
 		err := r.(error)
-		ExpectString(err.Error(), expectedMsg, t)
+		if err.Error() != expectedMsg {
+			t.Errorf("expected %v to be %v", err, expectedMsg)
+		}
 		return
 	}
 	t.Errorf("Expected to panic with `%s`, but did not panic", expectedMsg)
