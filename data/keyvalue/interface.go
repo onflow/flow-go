04e067436c562fdc6b4a180a8d6be1f9dc992d19
Maor Zamski <pazams@users.noreply.github.com>
2019-07-10 10:43:06 -0700

    Update data/keyvalue/interface.go
    Co-Authored-By: Peter Siemens <peterjsiemens@gmail.com>

--- data/keyvalue/interface.go
+++ data/keyvalue/interface.go
@@ -13,6 +13,7 @@ type QueryBuilder interface {
 	Get(namespace string, key string) QueryBuilder
 	Set(namespace string, key string) QueryBuilder
 	Delete(namespace string, key string) QueryBuilder
-	MustBuild() QueryBuilder // MustBuild is intended to be called once per query on server startup for performance considerations of some providers.
+      // MustBuild is intended to be called once per query on server startup for performance considerations of some providers.
+	MustBuild() QueryBuilder 
 	Execute(setParams ...string) (result string, err error)
 }
