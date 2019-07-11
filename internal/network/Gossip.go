c57c678b137b6bae60ee03a234f460db0d0fa1ea
Yahya Hassanzadeh <14860644+yhassanzadeh@users.noreply.github.com>
2019-07-11 10:14:34 -0700

    Yahya/602 gossip interface (#144)
    * Update networking.md
    
    * Subsections added
    
    * Update networking.md
    
    * RPC part updated in doc
    
    * The gossip interface for v1 added
    
    * RPC finalized
    
    * go-libp2p added
    
    * assumptions updated
    
    * Interface updated
    
    * One to one gossip mode updated
    
    * one to all updated
    
    * One-to-many mode added
    
    * One to all gossip mode updated
    
    * Terminologies updated
    
    * Readme.md added for the network package
    
    * Readme updated
    
    * Update readme
    
    * Updated the documentation
    
    * Apply suggestions from code review
    
    Co-Authored-By: Bastian Müller <bastian@axiomzen.co>
    
    * Applied gofmt
    
    * Apply suggestions from code review
    
    Co-Authored-By: Peter Siemens <peterjsiemens@gmail.com>
    
    * Gossip signature and GossipModes updated

---
+++ internal/network/Gossip.go
@@ -0,0 +1,37 @@
+// Package network implements the functionality to epidemically dessiminate a message (e.g., transaction, collection, or block) within the network.
+package network
+
+import (
+	"context"
+	"github.com/golang/protobuf/proto"
+)
+
+// Defines mode of the gossip based on the set of recipients
+type GossipMode int
+
+// The legitimate values for the GossipMode
+const (
+	ONE_TO_ONE GossipMode = iota
+	ONE_TO_MANY
+	ONE_TO_ALL
+)
+
+func (gm GossipMode) String() string {
+	return [...]string{"one_to_one", "one_to_many", "one_to_all"}[gm]
+}
+
+// Message models a single message that is supposed to get exchanged by the gossip network
+type Message struct {
+	Payload    *proto.Message // The payload of the message, i.e., the data to be exchanged
+	Method     string         // Name of RPC method to be invoked on the message
+	Recipients []string       // Address of the recipients to which the message should be delivered
+	Sender     string         // Address of the sender of this message
+	Path       []string       // Address of the nodes that this message visited so far
+	TTL        int            // The time to live of the message, i.e., maximum number of hops it should be gossiped
+}
+
+// GossipService is the interface of the network package and hence the networking streams with all
+// other streams of the system. It defines the function call, the type of input arguments, and the output result
+type GossipService interface {
+	Gossip(ctx context.Context, request *Message) (reply *Message, err error)
+}
