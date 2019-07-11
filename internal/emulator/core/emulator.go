4f298266f47189f0e9e63e1a15af32bd65d244fc
Brian Ho <mr.brianhobo@gmail.com>
2019-07-11 10:50:29 -0700

    initial EmulatedBlockchain structure

---
+++ internal/emulator/core/emulator.go
@@ -0,0 +1,13 @@
+package core
+
+import (
+	"github.com/dapperlabs/bamboo-node/internal/emulator/types"
+	"github.com/dapperlabs/bamboo-node/pkg/crypto"
+)
+
+type EmulatedBlockchain struct {
+	worldStateStore map[crypto.Hash]WorldState
+	worldState      WorldState
+	blockchain      []*types.Block
+	txPool          map[crypto.Hash]*types.Transaction
+}
---
+++ internal/emulator/core/state.go
@@ -0,0 +1,17 @@
+package core
+
+import (
+	"sync"
+
+	"github.com/dapperlabs/bamboo-node/internal/emulator/types"
+	"github.com/dapperlabs/bamboo-node/pkg/crypto"
+)
+
+type WorldState struct {
+	accounts          map[crypto.Address]crypto.Account
+	accountsMutex     sync.RWMutex
+	blocks            map[crypto.Hash]*types.Block
+	blocksMutex       sync.RWMutex
+	transactions      map[crypto.Hash]*types.Transaction
+	transactionsMutex sync.RWMutex
+}
