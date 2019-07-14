0f1a98f77e639311f3b077ac01b051f715a3ac6e
pazams <maor@pazams.com>
2019-07-14 16:36:35 -0700

    Refactor implementation

--- pkg/data/keyvalue/pg_connection.go
+++ pkg/data/keyvalue/pg_connection.go
@@ -8,14 +8,19 @@ import (
 )
 
 type postgresDB struct {
-	db *pg.DB
+	db          *pg.DB
+	getQuery    Query
+	setQuery    Query
+	deleteQuery Query
 }
 
+/*
 const (
 	getQuery    = "SELECT value FROM ?0 WHERE key=?1 ;"
 	setQuery    = "INSERT INTO ?0 (key, value) VALUES ('?1', '?2') ON CONFLICT (key) DO UPDATE SET value = ?2 ;"
 	deleteQuery = "DELETE FROM ?0 WHERE key=?1 ;"
 )
+*/
 
 // NewpostgresDB returns a DBConnector interface backed by a postgres DB
 func NewpostgresDB(addr, user, password, dbname string) DBConnector {
@@ -27,9 +32,13 @@ func NewpostgresDB(addr, user, password, dbname string) DBConnector {
 	}
 
 	db := pg.Connect(options)
+	qBuilder := &pgQueryBuilder{db: db}
 
 	return &postgresDB{
-		db: db,
+		db:          db,
+		getQuery:    qBuilder.AddGet().MustBuild(),
+		setQuery:    qBuilder.AddSet().MustBuild(),
+		deleteQuery: qBuilder.AddDelete().MustBuild(),
 	}
 }
 
@@ -38,19 +47,19 @@ func (d *postgresDB) NewQueryBuilder() QueryBuilder {
 	return &pgQueryBuilder{db: d.db}
 }
 
-// GetQuery returns a pre-built QueryBuilder instance ready to be executed as a get statement
+// GetQuery returns a Query instance ready to be executed as a get statement
 func (d *postgresDB) GetQuery() Query {
-	return &simpleQuery{d.db, getQuery, true}
+	return d.getQuery
 }
 
-// SetQuery returns a pre-built QueryBuilder instance ready to be executed as a get statement
+// SetQuery returns a Query instance ready to be executed as a set statement
 func (d *postgresDB) SetQuery() Query {
-	return &simpleQuery{d.db, setQuery, false}
+	return d.setQuery
 }
 
-// DeleteQuery returns a pre-built QueryBuilder instance ready to be executed as a delete statement
+// DeleteQuery returns a Query instance ready to be executed as a delete statement
 func (d *postgresDB) DeleteQuery() Query {
-	return &simpleQuery{db: d.db, query: deleteQuery, isGet: false}
+	return d.deleteQuery
 }
 
 // MigrateUp performs all the steps required to bring the backing DB into an initialised state
--- pkg/data/keyvalue/pg_simple_query.go
+++ pkg/data/keyvalue/pg_simple_query.go
@@ -1,23 +1,44 @@
 package keyvalue
 
 import (
+	"fmt"
+
 	"github.com/go-pg/pg"
 )
 
-// simpleQuery implements the Query interface.
-type simpleQuery struct {
-	db    *pg.DB
-	query string
-	isGet bool
+// pgQuery implements the Query interface.
+type pgQuery struct {
+	db         *pg.DB
+	query      string
+	isGet      bool
+	paramCount int
 }
 
-func (sq *simpleQuery) Execute(params ...string) (result string, err error) {
-	if sq.isGet {
+func (pg *pgQuery) Execute(params ...string) (result string, err error) {
+	if err := pg.checkParams(params); err != nil {
+		return "", err
+	}
+
+	if pg.isGet {
 		var value string
-		_, err := sq.db.QueryOne(&value, sq.query, params)
+		_, err := pg.db.QueryOne(&value, pg.query, params)
 		return value, err
 	}
 
-	_, execErr := sq.db.Exec(sq.query, params)
+	_, execErr := pg.db.Exec(pg.query, params)
 	return "", execErr
+}
+
+func (pg *pgQuery) checkParams(params []string) error {
+	if len(params) != pg.paramCount {
+		return fmt.Errorf("Expected to substituted %d params, but received %d", pg.paramCount, len(params))
+	}
+	return nil
+}
+
+func (pg *pgQuery) debug(params []string) (string, error) {
+	if err := pg.checkParams(params); err != nil {
+		return "", err
+	}
+	return pg.query, nil
 }
