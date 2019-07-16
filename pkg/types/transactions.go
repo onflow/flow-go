e34c00da6d3c95b4bc69630f1f0e929c53c840fd
Peter Siemens <peterjsiemens@gmail.com>
2019-07-16 13:14:50 -0700

    Move transaction types to pkg/types

--- pkg/types/transactions.go
+++ pkg/types/transactions.go
@@ -1,9 +1,28 @@
 package types
 
 import (
-	"github.com/dapperlabs/bamboo-node/pkg/crypto"
 	"github.com/dapperlabs/bamboo-node/internal/emulator/data"
+	"github.com/dapperlabs/bamboo-node/pkg/crypto"
 )
+
+// TransactionStatus represents the status of a Transaction.
+type TransactionStatus int
+
+const (
+	// TransactionPending is the status of a pending transaction.
+	TransactionPending TransactionStatus = iota
+	// TransactionFinalized is the status of a finalized transaction.
+	TransactionFinalized
+	// TransactionReverted is the status of a reverted transaction.
+	TransactionReverted
+	// TransactionSealed is the status of a sealed transaction.
+	TransactionSealed
+)
+
+// String returns the string representation of a transaction status.
+func (s TransactionStatus) String() string {
+	return [...]string{"PENDING", "FINALIZED", "REVERTED", "SEALED"}[s]
+}
 
 // RawTransaction is an unsigned transaction.
 type RawTransaction struct {
@@ -24,6 +43,9 @@ func (tx *RawTransaction) Hash() crypto.Hash {
 	return crypto.NewHash(bytes)
 }
 
+// Sign signs a transaction with the given account and keypair.
+//
+// The function returns a new SignedTransaction that includes the generated signature.
 func (tx *RawTransaction) Sign(account crypto.Address, keyPair *crypto.KeyPair) *SignedTransaction {
 	hash := tx.Hash()
 	sig := crypto.Sign(hash, account, keyPair)
@@ -45,7 +67,7 @@ type SignedTransaction struct {
 	ComputeLimit   uint64
 	ComputeUsed    uint64
 	PayerSignature *crypto.Signature
-	Status         data.TxStatus
+	Status         TransactionStatus
 }
 
 // Hash computes the hash over the necessary transaction data.
