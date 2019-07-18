5a064e9795e1a158e7ed8fd93f6db4948fd1bccb
Peter Siemens <peterjsiemens@gmail.com>
2019-07-18 12:26:36 -0700

    Update Dockerfiles to work with go mod

--- cmd/consensus/main.go
+++ cmd/consensus/main.go
@@ -1,11 +1,17 @@
 package main
 
-import "github.com/dapperlabs/bamboo-node/internal/roles/consensus"
+import (
+	"fmt"
+
+	"github.com/dapperlabs/bamboo-node/internal/roles/consensus"
+)
 
 func main() {
 	server, err := consensus.InitializeServer()
 	if err != nil {
 		panic(err)
 	}
+	fmt.Println("TEST")
+
 	server.Start()
 }
