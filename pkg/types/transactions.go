b3ab7769ecb5bdd2300268cef79951dcbb975821
Brian Ho <mr.brianhobo@gmail.com>
2019-07-18 15:24:12 -0700

    refactor oldcrypto imports

--- pkg/types/transactions.go
+++ pkg/types/transactions.go
@@ -3,7 +3,7 @@ package types
 import (
 	"time"
 
-	"github.com/dapperlabs/bamboo-node/internal/emulator/data"
+	"github.com/dapperlabs/bamboo-node/internal/emulator/utils"
 	"github.com/dapperlabs/bamboo-node/pkg/crypto"
 )
 
@@ -37,7 +37,7 @@ type RawTransaction struct {
 
 // Hash computes the hash over the necessary transaction data.
 func (tx *RawTransaction) Hash() crypto.Hash {
-	bytes := data.EncodeAsBytes(
+	bytes := utils.EncodeAsBytes(
 		tx.ToAddress,
 		tx.Script,
 		tx.Nonce,
@@ -78,7 +78,7 @@ type SignedTransaction struct {
 
 // Hash computes the hash over the necessary transaction data.
 func (tx *SignedTransaction) Hash() crypto.Hash {
-	bytes := data.EncodeAsBytes(
+	bytes := utils.EncodeAsBytes(
 		tx.ToAddress,
 		tx.Script,
 		tx.Nonce,
