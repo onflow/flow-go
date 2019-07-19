b28b17f9a35419e86b6828fd9665c5ecf993d8d4
pazams <maor@pazams.com>
2019-07-18 18:31:58 -0700

    Add models

--- internal/pkg/types/execute.go
+++ internal/pkg/types/execute.go
@@ -2,6 +2,7 @@ package types
 
 import (
 	"github.com/dapperlabs/bamboo-node/pkg/crypto"
+	"github.com/dapperlabs/bamboo-node/utils/slices"
 )
 
 type ExecutionReceipt struct {
@@ -12,12 +13,30 @@ type ExecutionReceipt struct {
 	Signatures                []crypto.Signature
 }
 
+// Encode is defined to match crypto.Encoder interface
+func (m *ExecutionReceipt) Encode() []byte {
+	var b [][]byte
+	// todo: fix once crypto package is merged
+	b = [][]byte{
+		m.PreviousReceiptHash[:],
+	}
+	return slices.Concat(b)
+
+}
+
 type InvalidExecutionReceiptChallenge struct {
 	ExecutionReceiptHash      crypto.Hash
 	ExecutionReceiptSignature crypto.Signature
 	PartIndex                 uint64
 	PartTransactions          []IntermediateRegisters
 	Signature                 crypto.Signature
+}
+
+// TODO: this type should be defined inside the compute library.
+// It is given here in the meanwhile.
+type BlockPartExecutionResult struct {
+	PartIndex        uint64
+	PartTransactions []IntermediateRegisters
 }
 
 type ResultApproval struct {
