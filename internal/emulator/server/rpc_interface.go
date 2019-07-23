5deb2a99cee79fd717091ebb3f81c88de2182150
brian <mr.brianhobo@gmail.com>
2019-07-23 14:50:55 -0700

    implement SendTransaction, GetTransaction rpc call

--- internal/emulator/server/rpc_interface.go
+++ internal/emulator/server/rpc_interface.go
@@ -2,11 +2,15 @@ package server
 
 import (
 	"context"
+	"time"
 
 	"google.golang.org/grpc/codes"
 	"google.golang.org/grpc/status"
 
 	"github.com/dapperlabs/bamboo-node/grpc/services/observe"
+	"github.com/dapperlabs/bamboo-node/pkg/types"
+
+	crypto "github.com/dapperlabs/bamboo-node/pkg/crypto/oldcrypto"
 )
 
 func (s *EmulatorServer) Ping(ctx context.Context, req *observe.PingRequest) (*observe.PingResponse, error) {
@@ -15,7 +19,28 @@ func (s *EmulatorServer) Ping(ctx context.Context, req *observe.PingRequest) (*o
 
 // SendTransaction submits a transaction to the network.
 func (s *EmulatorServer) SendTransaction(ctx context.Context, req *observe.SendTransactionRequest) (*observe.SendTransactionResponse, error) {
-	return nil, status.Error(codes.Unimplemented, "")
+	txMsg := req.GetTransaction()
+	payerSig := txMsg.GetPayerSignature()
+
+	tx := &types.SignedTransaction{
+		Script:       txMsg.GetScript(),
+		Nonce:        txMsg.GetNonce(),
+		ComputeLimit: txMsg.GetComputeLimit(),
+		Timestamp:    time.Now(),
+		PayerSignature: crypto.Signature{
+			Account: crypto.BytesToAddress(payerSig.GetAccountAddress()),
+			// TODO: update this (default signature for now)
+			Sig: crypto.Sig{},
+		},
+		Status: types.TransactionPending,
+	}
+
+	s.transactionsIn <- tx
+	response := &observe.SendTransactionResponse{
+		Hash: tx.Hash().Bytes(),
+	}
+
+	return response, nil
 }
 
 // GetBlockByHash gets a block by hash.
@@ -35,7 +60,28 @@ func (s *EmulatorServer) GetLatestBlock(ctx context.Context, req *observe.GetLat
 
 // GetTransaction gets a transaction by hash.
 func (s *EmulatorServer) GetTransaction(ctx context.Context, req *observe.GetTransactionRequest) (*observe.GetTransactionResponse, error) {
-	return nil, status.Error(codes.Unimplemented, "")
+	hash := crypto.BytesToHash(req.GetHash())
+	tx := s.blockchain.GetTransaction(hash)
+
+	txMsg := &observe.GetTransactionResponse_Transaction{
+		ToAddress:    tx.ToAddress.Bytes(),
+		Script:       tx.Script,
+		Nonce:        tx.Nonce,
+		ComputeLimit: tx.ComputeLimit,
+		ComputeUsed:  tx.ComputeUsed,
+		PayerSignature: &observe.Signature{
+			AccountAddress: tx.PayerSignature.Account.Bytes(),
+			// TODO: update this (default signature bytes for now)
+			Signature: tx.PayerSignature.Sig[:],
+		},
+		Status: observe.GetTransactionResponse_Transaction_Status(tx.Status),
+	}
+
+	response := &observe.GetTransactionResponse{
+		Transaction: txMsg,
+	}
+
+	return response, nil
 }
 
 // GetAccount returns the info associated with an address.
