8ed4f4a5648c4036ac8253ab94bbb4f9b82a299d
James Key <jameskey@Jamess-MacBook-Pro.local>
2019-07-24 13:25:12 -0700

    Deleting old Message struct

--- pkg/network/gossip/gossip.go
+++ pkg/network/gossip/gossip.go
@@ -22,16 +22,6 @@ func (gm GossipMode) String() string {
 	return [...]string{"one_to_one", "one_to_many", "one_to_all"}[gm]
 }
 
-// // Message models a single message that is supposed to get exchanged by the gossip network
-// type Message struct {
-// 	Payload    proto.Message // The payload of the message, i.e., the data to be exchanged
-// 	Method     string        // Name of RPC method to be invoked on the message
-// 	Recipients []string      // Address of the recipients to which the message should be delivered
-// 	Sender     string        // Address of the sender of this message
-// 	Path       []string      // Address of the nodes that this message visited so far
-// 	TTL        int           // The time to live of the message, i.e., maximum number of hops it should be gossiped
-// }
-
 // GossipService is the interface of the network package and hence the networking streams with all
 // other streams of the system. It defines the function call, the type of input arguments, and the output result
 type GossipService interface {
