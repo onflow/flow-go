95f0a28ddfd6ec772dc8bb34e3c04981917ffb5b
Peter Siemens <peterjsiemens@gmail.com>
2019-07-29 15:13:07 -0700

    Create submit_transaction_test.go

---
+++ internal/roles/collect/test/submit_transaction_test.go
@@ -0,0 +1,56 @@
+package collect_test
+
+import (
+	"context"
+	"testing"
+
+	. "github.com/onsi/gomega"
+	"github.com/sirupsen/logrus"
+
+	svc "github.com/dapperlabs/bamboo-node/pkg/grpc/services/collect"
+	"github.com/dapperlabs/bamboo-node/pkg/types"
+
+	"github.com/dapperlabs/bamboo-node/internal/pkg/mocks"
+	"github.com/dapperlabs/bamboo-node/internal/pkg/proto"
+	"github.com/dapperlabs/bamboo-node/internal/roles/collect/controller"
+)
+
+func TestSubmitTransaction(t *testing.T) {
+	RegisterTestingT(t)
+
+	c := controller.New(logrus.New())
+
+	ctx := context.Background()
+
+	tx := mocks.MockSignedTransaction()
+
+	txMsg, err := proto.SignedTransactionToMessage(tx)
+	Expect(err).ToNot(HaveOccurred())
+
+	_, err = c.SubmitTransaction(ctx, &svc.SubmitTransactionRequest{
+		Transaction: txMsg,
+	})
+	Expect(err).ToNot(HaveOccurred())
+}
+
+func TestSubmitInvalidTransaction(t *testing.T) {
+	RegisterTestingT(t)
+
+	c := controller.New(logrus.New())
+
+	ctx := context.Background()
+
+	// transaction missing script and compute_limit fields
+	tx := types.SignedTransaction{
+		Nonce:          10,
+		PayerSignature: mocks.MockAccountSignature(),
+	}
+
+	txMsg, err := proto.SignedTransactionToMessage(tx)
+	Expect(err).ToNot(HaveOccurred())
+
+	_, err = c.SubmitTransaction(ctx, &svc.SubmitTransactionRequest{
+		Transaction: txMsg,
+	})
+	Expect(err).To(HaveOccurred())
+}
