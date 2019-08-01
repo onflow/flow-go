03f1141f2a3025a54bd066e082141cd933b123fa
Peter Siemens <peterjsiemens@gmail.com>
2019-07-31 21:36:39 -0700

    Add documentation to storage

--- internal/roles/collect/storage/mock.go
+++ internal/roles/collect/storage/mock.go
@@ -5,21 +5,26 @@ import (
 	"github.com/dapperlabs/bamboo-node/pkg/types"
 )
 
+// MockStorage is an in-memory storage implementation.
 type MockStorage struct {
 	transactions map[crypto.Hash]types.SignedTransaction
 }
 
+// NewMockStorage initializes and returns a new MockStorage.
 func NewMockStorage() Storage {
 	return &MockStorage{
 		transactions: make(map[crypto.Hash]types.SignedTransaction),
 	}
 }
 
+// InsertTransaction inserts a signed transaction into storage.
 func (d *MockStorage) InsertTransaction(tx types.SignedTransaction) error {
 	d.transactions[tx.Hash()] = tx
 	return nil
 }
 
+// ContainsTransaction returns true if a transaction with the given hash exists
+// in storage and false otherwise.
 func (d *MockStorage) ContainsTransaction(hash crypto.Hash) bool {
 	_, exists := d.transactions[hash]
 	return exists
--- internal/roles/collect/storage/storage.go
+++ internal/roles/collect/storage/storage.go
@@ -6,15 +6,34 @@ import (
 	"github.com/dapperlabs/bamboo-node/pkg/types"
 )
 
+// Storage provides methods to store and retrieve data required by a collection node.
 type Storage interface {
+	// InsertTransaction inserts a signed transaction into storage.
 	InsertTransaction(types.SignedTransaction) error
+	// ContainsTransaction returns true if a transaction with the given hash exists
+	// in storage and false otherwise.
 	ContainsTransaction(crypto.Hash) bool
 }
 
+// DatabaseStorage is a storage implementation backed by a key-value database.
 type DatabaseStorage struct {
 	db keyvalue.DBConnector
 }
 
+// NewDatabaseStorage returns a DatabaseStorage instance backed by the provided database.
 func NewDatabaseStorage(db keyvalue.DBConnector) Storage {
 	return &DatabaseStorage{db}
+}
+
+// InsertTransaction inserts a signed transaction into storage.
+func (d *DatabaseStorage) InsertTransaction(tx types.SignedTransaction) error {
+	// TODO: implement InsertTransaction
+	return nil
+}
+
+// ContainsTransaction returns true if a transaction with the given hash exists
+// in storage and false otherwise.
+func (d *DatabaseStorage) ContainsTransaction(hash crypto.Hash) bool {
+	// TODO: implement ContainsTransaction
+	return false
 }
