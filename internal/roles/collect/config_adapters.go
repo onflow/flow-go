6d8f2a7f806cc0a75b45af9a672f1df3fcb6d40f
Peter Siemens <peterjsiemens@gmail.com>
2019-07-31 22:35:06 -0700

    Update config_adapters.go

--- internal/roles/collect/config_adapters.go
+++ internal/roles/collect/config_adapters.go
@@ -5,6 +5,7 @@ import (
 	"github.com/dapperlabs/bamboo-node/pkg/data/keyvalue"
 )
 
+// NewDatabaseConnector constructs a keyvalue.DBConnector instance from the provided configuration.
 func NewDatabaseConnector(conf *config.Config) keyvalue.DBConnector {
 	return keyvalue.NewpostgresDB(
 		conf.PostgresAddr,
