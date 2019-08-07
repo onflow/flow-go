fcb1161541ce9d21a00cff82e0e041a520b89789
Peter Siemens <peterjsiemens@gmail.com>
2019-08-07 11:05:08 -0700

    Add tests for CreateAccount and UpdateAccountCode transaction generators

--- client/account_test.go
+++ client/account_test.go
@@ -7,6 +7,7 @@ import (
 	. "github.com/onsi/gomega"
 
 	"github.com/dapperlabs/bamboo-node/client"
+	"github.com/dapperlabs/bamboo-node/pkg/types"
 )
 
 func TestLoadAccount(t *testing.T) {
@@ -34,4 +35,47 @@ func TestLoadAccount(t *testing.T) {
 
 	a, err = client.LoadAccount(r)
 	Expect(err).To(HaveOccurred())
+}
+
+func TestCreateAccount(t *testing.T) {
+	RegisterTestingT(t)
+
+	publicKey := []byte{4, 136, 178, 30, 0, 0, 0, 0, 0, 0, 0, 0, 0, 111, 117, 56, 107, 245, 122, 184, 40, 127, 172, 19, 175, 225, 131, 184, 22, 122, 23, 90, 172, 214, 144, 150, 92, 69, 119, 218, 11, 191, 120, 226, 74, 2, 217, 156, 75, 44, 44, 121, 152, 143, 47, 180, 169, 205, 18, 77, 47, 135, 146, 34, 34, 157, 69, 149, 177, 141, 80, 99, 66, 186, 33, 25, 73, 179, 224, 166, 205, 172}
+
+	// create account with no code
+	txA := client.CreateAccount(publicKey, []byte{})
+
+	Expect(txA.Script).To(Equal([]byte(`
+		fun main() {
+			let publicKey = [4,136,178,30,0,0,0,0,0,0,0,0,0,111,117,56,107,245,122,184,40,127,172,19,175,225,131,184,22,122,23,90,172,214,144,150,92,69,119,218,11,191,120,226,74,2,217,156,75,44,44,121,152,143,47,180,169,205,18,77,47,135,146,34,34,157,69,149,177,141,80,99,66,186,33,25,73,179,224,166,205,172]
+			let code = []
+			createAccount(publicKey, code)
+		}
+	`)))
+
+	// create account with code
+	txB := client.CreateAccount(publicKey, []byte("fun main() {}"))
+
+	Expect(txB.Script).To(Equal([]byte(`
+		fun main() {
+			let publicKey = [4,136,178,30,0,0,0,0,0,0,0,0,0,111,117,56,107,245,122,184,40,127,172,19,175,225,131,184,22,122,23,90,172,214,144,150,92,69,119,218,11,191,120,226,74,2,217,156,75,44,44,121,152,143,47,180,169,205,18,77,47,135,146,34,34,157,69,149,177,141,80,99,66,186,33,25,73,179,224,166,205,172]
+			let code = [102,117,110,32,109,97,105,110,40,41,32,123,125]
+			createAccount(publicKey, code)
+		}
+	`)))
+}
+
+func TestUpdateAccountCode(t *testing.T) {
+	RegisterTestingT(t)
+
+	address := types.HexToAddress("0000000000000000000000000000000000000001")
+	tx := client.UpdateAccountCode(address, []byte("fun main() {}"))
+
+	Expect(tx.Script).To(Equal([]byte(`
+		fun main() {
+			let account = [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1]
+			let code = [102,117,110,32,109,97,105,110,40,41,32,123,125]
+			updateAccountCode(account, code)
+		}
+	`)))
 }
--- client/client_test.go
+++ client/client_test.go
@@ -11,9 +11,7 @@ import (
 
 	"github.com/dapperlabs/bamboo-node/client"
 	"github.com/dapperlabs/bamboo-node/client/mocks"
-	crypto "github.com/dapperlabs/bamboo-node/pkg/crypto/oldcrypto"
 	"github.com/dapperlabs/bamboo-node/pkg/grpc/services/observe"
-	"github.com/dapperlabs/bamboo-node/pkg/types"
 	"github.com/dapperlabs/bamboo-node/pkg/utils/unittest"
 )
 
@@ -55,36 +53,4 @@ func TestSendTransaction(t *testing.T) {
 	// error should be passed to user
 	err = c.SendTransaction(ctx, tx)
 	Expect(err).To(HaveOccurred())
-}
-
-func TestCreateAccount(t *testing.T) {
-	RegisterTestingT(t)
-
-	mockCtrl := gomock.NewController(t)
-	defer mockCtrl.Finish()
-
-	mockRPC := mocks.NewMockRPCClient(mockCtrl)
-
-	c := client.NewFromRPCClient(mockRPC)
-	ctx := context.Background()
-
-	keyPair, _ := crypto.GenerateKeyPair("elephant ears")
-
-	tx := client.CreateAccount(keyPair.PublicKey, []byte{})
-
-	tx.SetNonce(client.RandomNonce())
-	tx.SetComputeLimit(100)
-
-	signedTx := tx.SignPayer(defaultAccount.Account, defaultAccount.KeyPair)
-
-	client.UpdateAccountCode(types.HexToAddress("0000000000000000000000000000000000000002"), []byte("fun main() {}"))
-
-	// client should return non-error if RPC call succeeds
-	mockRPC.EXPECT().
-		SendTransaction(ctx, gomock.Any()).
-		Return(&observe.SendTransactionResponse{Hash: signedTx.Hash().Bytes()}, nil).
-		Times(1)
-
-	err := c.SendTransaction(ctx, *signedTx)
-	Expect(err).ToNot(HaveOccurred())
 }
