d1ac0fe8b1d01b840a279012527cbeca6a21a6f3
Bastian Müller <bastian@axiomzen.co>
2019-08-09 12:02:45 -0700

    add trampoline test for flatMap on Done

--- pkg/language/runtime/trampoline/trampoline_test.go
+++ pkg/language/runtime/trampoline/trampoline_test.go
@@ -6,7 +6,20 @@ import (
 	"testing"
 )
 
-func TestFlatMap(t *testing.T) {
+func TestFlatMapDone(t *testing.T) {
+	RegisterTestingT(t)
+
+	trampoline := Done{23}.
+		FlatMap(func(value interface{}) Trampoline {
+			number := value.(int)
+			return Done{number * 42}
+		})
+
+	Expect(Run(trampoline)).
+		To(Equal(23 * 42))
+}
+
+func TestFlatMapMore(t *testing.T) {
 	RegisterTestingT(t)
 
 	trampoline :=
