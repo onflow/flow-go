343e7c480c01f08812e87dacc4d7a7d159c7c2b4
Peter Siemens <peterjsiemens@gmail.com>
2019-08-11 18:16:21 -0700

    Add tests for CallScript method

--- sdk/client/client_test.go
+++ sdk/client/client_test.go
@@ -2,6 +2,7 @@ package client_test
 
 import (
 	"context"
+	"encoding/json"
 	"errors"
 	"strings"
 	"testing"
@@ -53,5 +54,59 @@ func TestSendTransaction(t *testing.T) {
 
 	// error should be passed to user
 	err = c.SendTransaction(ctx, tx)
+	Expect(err).To(HaveOccurred())
+}
+
+func TestCallScript(t *testing.T) {
+	RegisterTestingT(t)
+
+	mockCtrl := gomock.NewController(t)
+	defer mockCtrl.Finish()
+
+	mockRPC := mocks.NewMockRPCClient(mockCtrl)
+
+	c := client.NewFromRPCClient(mockRPC)
+	ctx := context.Background()
+
+	valueBytes, _ := json.Marshal(1)
+
+	// client should return non-error if RPC call succeeds
+	mockRPC.EXPECT().
+		CallScript(ctx, gomock.Any()).
+		Return(&observe.CallScriptResponse{Value: valueBytes}, nil).
+		Times(1)
+
+	value, err := c.CallScript(ctx, []byte("fun main(): Int { return 1 }"))
+	Expect(err).ToNot(HaveOccurred())
+	Expect(value).To(Equal(float64(1)))
+
+	// client should return error if RPC call fails
+	mockRPC.EXPECT().
+		CallScript(ctx, gomock.Any()).
+		Return(nil, errors.New("dummy error")).
+		Times(1)
+
+	// error should be passed to user
+	_, err = c.CallScript(ctx, []byte("fun main(): Int { return 1 }"))
+	Expect(err).To(HaveOccurred())
+
+	// client should return error if value is empty
+	mockRPC.EXPECT().
+		CallScript(ctx, gomock.Any()).
+		Return(&observe.CallScriptResponse{Value: []byte{}}, nil).
+		Times(1)
+
+	// error should be passed to user
+	_, err = c.CallScript(ctx, []byte("fun main(): Int { return 1 }"))
+	Expect(err).To(HaveOccurred())
+
+	// client should return error if value is malformed
+	mockRPC.EXPECT().
+		CallScript(ctx, gomock.Any()).
+		Return(&observe.CallScriptResponse{Value: []byte("asdfafa")}, nil).
+		Times(1)
+
+	// error should be passed to user
+	_, err = c.CallScript(ctx, []byte("fun main(): Int { return 1 }"))
 	Expect(err).To(HaveOccurred())
 }
