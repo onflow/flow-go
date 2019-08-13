1c9bd3370157f536f038fc6505cd3a85336db52d
Bastian Müller <bastian@axiomzen.co>
2019-08-13 14:24:34 -0700

    add assert function, in new stdlib package

---
+++ pkg/language/runtime/stdlib/stdlib_test.go
@@ -0,0 +1,31 @@
+package stdlib
+
+import (
+	"testing"
+
+	. "github.com/onsi/gomega"
+
+	"github.com/dapperlabs/bamboo-node/pkg/language/runtime/ast"
+	"github.com/dapperlabs/bamboo-node/pkg/language/runtime/interpreter"
+)
+
+func TestAssert(t *testing.T) {
+	RegisterTestingT(t)
+
+	inter := interpreter.NewInterpreter(&ast.Program{})
+	for _, function := range BuiltIns {
+		Expect(inter.ImportFunction(function.Name, function.Function)).
+			To(Not(HaveOccurred()))
+	}
+
+	_, err := inter.Invoke("assert", false, "oops")
+	Expect(err).
+		To(Equal(AssertionError{
+			Message:  "oops",
+			Position: ast.Position{},
+		}))
+
+	_, err = inter.Invoke("assert", true, "oops")
+	Expect(err).
+		To(Not(HaveOccurred()))
+}
