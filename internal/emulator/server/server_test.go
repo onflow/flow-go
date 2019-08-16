3dcc77905e4b60c7be0ee634aa2015b4ff599e4a
brian <mr.brianhobo@gmail.com>
2019-08-16 14:55:18 -0700

    add WrappedServer test (needs improvement)

---
+++ internal/emulator/server/server_test.go
@@ -0,0 +1,24 @@
+package server
+
+import (
+	"testing"
+	"time"
+	
+	. "github.com/onsi/gomega"
+	log "github.com/sirupsen/logrus"
+)
+
+func TestWrappedServer(t *testing.T) {
+	RegisterTestingT(t)
+
+	logger := log.New()
+	logger.SetLevel(log.DebugLevel)
+
+	config := &Config{
+		Port:          5000,
+		WrappedPort:   9090,
+		BlockInterval: time.Second * 5,
+	}
+
+	StartServer(logger, config)
+}
