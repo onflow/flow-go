5c812f8520b76877ff0b34d74db9de59c59ddf8a
Alexander Hentschel <alex.hentschel@axiomzen.co>
2019-08-19 19:50:01 -0700

    finalized implementation ... still needs tests

--- internal/identity/identity.go
+++ internal/identity/identity.go
@@ -6,16 +6,16 @@ import (
 	"fmt"
 	"math/big"
 	"sort"
-	"strconv"
 )
 
 type NodeRole string
+
 const (
 	CollectorRole NodeRole = "collector"
 	ConsensusRole NodeRole = "consensus"
-	ExecutorRole NodeRole = "executor"
-	VerifierRole NodeRole = "verifier"
-	ObserverRole NodeRole = "observer"
+	ExecutorRole  NodeRole = "executor"
+	VerifierRole  NodeRole = "verifier"
+	ObserverRole  NodeRole = "observer"
 )
 
 type NodeIdentity interface {
@@ -26,52 +26,44 @@ type NodeIdentity interface {
 	Index() uint
 }
 
-type IdentityTable interface {
-	Count() int
+type Table interface {
+	Count() uint
 	Nodes() []NodeIdentity
 
+	GetByIndex(uint) (NodeIdentity, error)
 	GetByID(uint) (NodeIdentity, error)
 	GetByAddress(string) (NodeIdentity, error)
 
 	TotalStake() *big.Int
 
-	FilterByID([]uint) IdentityTable
-	FilterByAddress([]string) IdentityTable
-	FilterByRole(NodeRole) IdentityTable
-	FilterByIndex([]uint) IdentityTable
+	FilterByID([]uint) (Table, error)
+	FilterByAddress([]string) (Table, error)
+	FilterByRole(NodeRole) (Table, error)
+	FilterByIndex([]uint) (Table, error)
 }
 
-// Information about one Node that is independent of potential other nodes that this node is grouped together with.
+// NodeRecord provides information about one Node that (independent of potential other nodes)
 type NodeRecord struct {
-	ID uint
+	ID      uint
 	Address string
-	Role NodeRole
-	Stake *big.Int
+	Role    NodeRole
+	Stake   *big.Int
 }
-
 
 // NodeRecords is a slice of *NodeRecord which implements sort.Interface
 // Sorting is based solely on NodeRecord.ID
 type NodeRecords []*NodeRecord
 
-func (ns NodeRecords) Len() int {
-	return len(ns)
-}
-func (ns NodeRecords) Less(i, j int) bool {
-	return ns[i].ID < ns[j].ID
-}
-func (ns NodeRecords) Swap(i, j int) {
-	ns[i], ns[j] = ns[j], ns[i]
-}
+func (ns NodeRecords) Len() int           { return len(ns) }
+func (ns NodeRecords) Less(i, j int) bool { return ns[i].ID < ns[j].ID }
+func (ns NodeRecords) Swap(i, j int)      { ns[i], ns[j] = ns[j], ns[i] }
 
-// Compile-time check that nodeIdentity implements interface NodeIdentity
-var _ sort.Interface = (*NodeRecords)(nil)
 
 
 // Implementation of NodeIdentity interface
 type nodeIdentity struct {
 	coreID *NodeRecord
-	index uint
+	index  uint
 }
 
 func (i nodeIdentity) ID() uint {
@@ -94,33 +86,42 @@ func (i nodeIdentity) Index() uint {
 	return i.index
 }
 
-// Compile-time check that nodeIdentity implements interface NodeIdentity
-var _ NodeIdentity = (*nodeIdentity)(nil)
+// nodeIdentities is a slice of *nodeIdentity which implements sort.Interface
+// Sorting is based solely on nodeIdentity.ID
+type nodeIdentities []*nodeIdentity
 
+func (ns nodeIdentities) Len() int           { return len(ns) }
+func (ns nodeIdentities) Less(i, j int) bool { return ns[i].coreID.ID < ns[j].coreID.ID }
+func (ns nodeIdentities) Swap(i, j int)      { ns[i], ns[j] = ns[j], ns[i] }
 
-
-// In-memory Implementation of the interface IdentityTable
+// InMemoryIdentityTable is a In-memory implementation of the interface identity.Table
 type InMemoryIdentityTable struct {
-	nodes []*nodeIdentity
+	nodes      []*nodeIdentity
 	addressMap map[string]*nodeIdentity
-	idMap map[uint]*nodeIdentity
-
+	idMap      map[uint]*nodeIdentity
 }
 
-func (t InMemoryIdentityTable) Count() int {
-	return len(t.nodes)
+func (t InMemoryIdentityTable) Count() uint {
+	return uint(len(t.nodes))
 }
 
 func (t InMemoryIdentityTable) Nodes() []NodeIdentity {
 	identities := make([]NodeIdentity, len(t.nodes))
-	for i, n := range t.nodes {
+	for i, n := range t.nodes { // converting explicitly from nodeIdentity ti interface NodeIdentity
 		identities[i] = n
 	}
 	return identities
 }
 
+func (t InMemoryIdentityTable) GetByIndex(idx uint) (NodeIdentity, error) {
+	if int(idx) > len(t.nodes) {
+		return nil, &NodeNotFoundError{fmt.Sprint(idx)}
+	}
+	return t.nodes[idx], nil
+}
+
 func (t InMemoryIdentityTable) GetByID(id uint) (NodeIdentity, error) {
-	value, found := t.idMap[id];
+	value, found := t.idMap[id]
 	if !found {
 		return nil, &NodeNotFoundError{fmt.Sprint(id)}
 	}
@@ -128,7 +129,7 @@ func (t InMemoryIdentityTable) GetByID(id uint) (NodeIdentity, error) {
 }
 
 func (t InMemoryIdentityTable) GetByAddress(address string) (NodeIdentity, error) {
-	value, found := t.addressMap[address];
+	value, found := t.addressMap[address]
 	if !found {
 		return nil, &NodeNotFoundError{address}
 	}
@@ -143,8 +144,9 @@ func (t InMemoryIdentityTable) TotalStake() *big.Int {
 	return s
 }
 
-func (t InMemoryIdentityTable) FilterByID(ids []uint) IdentityTable {
+func (t InMemoryIdentityTable) FilterByID(ids []uint) (Table, error) {
 	nodes := make([]*NodeRecord, len(ids))
+	missing := []uint{}
 	var n *nodeIdentity
 	var found bool
 	var idx int = 0
@@ -152,14 +154,20 @@ func (t InMemoryIdentityTable) FilterByID(ids []uint) IdentityTable {
 		n, found = t.idMap[id]
 		if found {
 			nodes[idx] = n.coreID
-			idx += 1
+			idx++
+		} else {
+			missing = append(missing, id)
 		}
 	}
-	return NewInMemoryIdentityTable(nodes[0:idx])
+	if len(missing) > 0 {
+		return NewInMemoryIdentityTable(nodes[0:idx]), &NodeNotFoundError{fmt.Sprint(missing)}
+	}
+	return NewInMemoryIdentityTable(nodes[0:idx]), nil
 }
 
-func (t InMemoryIdentityTable) FilterByAddress(addresses []string) IdentityTable {
+func (t InMemoryIdentityTable) FilterByAddress(addresses []string) (Table, error) {
 	nodes := make([]*NodeRecord, len(addresses))
+	missing := []string{}
 	var n *nodeIdentity
 	var found bool
 	var idx int = 0
@@ -167,18 +175,50 @@ func (t InMemoryIdentityTable) FilterByAddress(addresses []string) IdentityTable
 		n, found = t.addressMap[addr]
 		if found {
 			nodes[idx] = n.coreID
-			idx += 1
+			idx++
+		} else {
+			missing = append(missing, addr)
 		}
 	}
-	return NewInMemoryIdentityTable(nodes[0:idx])
+	if len(missing) > 0 {
+		return NewInMemoryIdentityTable(nodes[0:idx]), &NodeNotFoundError{fmt.Sprint(missing)}
+	}
+	return NewInMemoryIdentityTable(nodes[0:idx]), nil
 }
 
-func (t InMemoryIdentityTable) FilterByRole(role NodeRole) IdentityTable {
-	panic("implement me")
+func (t InMemoryIdentityTable) FilterByRole(role NodeRole) (Table, error) {
+	nodes := make([]*NodeRecord, t.Count())
+	var idx int = 0
+	for _, n := range t.nodes {
+		if n.Role() == role {
+			nodes[idx] = n.coreID
+			idx++
+		}
+	}
+	if idx == 0 {
+		return nil, &NodeNotFoundError{fmt.Sprint(role)}
+	}
+	return NewInMemoryIdentityTable(nodes[0:idx]), nil
 }
 
-func (t InMemoryIdentityTable) FilterByIndex([]uint) IdentityTable {
-	panic("implement me")
+func (t InMemoryIdentityTable) FilterByIndex(indices []uint) (Table, error) {
+	nodes := make([]*NodeRecord, len(indices))
+	missing := []uint{}
+	var idx int = 0
+	_nodeCount := uint(t.Count())
+	_allNodes := t.nodes
+	for _, i := range indices {
+		if i < _nodeCount {
+			nodes[idx] = _allNodes[i].coreID
+			idx++
+		} else {
+			missing = append(missing, i)
+		}
+	}
+	if len(missing) > 0 {
+		return NewInMemoryIdentityTable(nodes[0:idx]), &NodeNotFoundError{fmt.Sprint(missing)}
+	}
+	return NewInMemoryIdentityTable(nodes[0:idx]), nil
 }
 
 type NodeNotFoundError struct {
@@ -189,43 +229,37 @@ func (e *NodeNotFoundError) Error() string {
 	return fmt.Sprintf("node with '%s' not found", e.key)
 }
 
-
-
-
-
-
 func NewInMemoryIdentityTable(nodes []*NodeRecord) *InMemoryIdentityTable {
+	nidentities := newSortedNodeIdentities(nodes)
 
-
-	// While the slice `nodes` is copied, the data in the slice is not sufficient to sort the slice without mutating the underlying array
-	// For more details, see https://blog.golang.org/go-slices-usage-and-internals
-
-	nds := make([]*NodeRecord, len(nodes))
-
-	//sort.Sort(ByAge(family))
-	nds := make([]*NodeRecord, len(nodes))
-	copy(nds, nodes)
-	sort.Sort(NodeRecords(nds))
-
-	nidentities := make([]*nodeIdentity, len(nodes))
 	addressMap := make(map[string]*nodeIdentity)
 	idMap := make(map[uint]*nodeIdentity)
-
-	for i, r := range nodes {
-		nodeId := &nodeIdentity{
-			coreID: r,
-			index: uint(i),
+	var _last *NodeRecord = nil // reference to previous nodeIdentity to detect duplicates
+	for i, n := range nidentities {
+		if _last == n.coreID {
+			panic("Duplicate NodeRecord not supported")
 		}
-		nidentities[i] = nodeId
-		addressMap[r.Address] = nodeId
-		idMap[r.ID] = nodeId
+		_last = n.coreID
+		n.index = uint(i)
+		addressMap[n.coreID.Address] = n
+		idMap[n.coreID.ID] = n
 	}
 
-	return &InMemoryIdentityTable{
-		nidentities,
-		addressMap,
-		idMap,
-	}
+	return &InMemoryIdentityTable{nidentities, addressMap, idMap}
 }
 
-
+// Wraps each NodeRecord as into a nodeIdentity type with default `index=0` and sorts the elements
+// Checks for nil elements in
+func newSortedNodeIdentities(nodes []*NodeRecord) []*nodeIdentity {
+	// While the slice `nodes` is copied, the data in the slice is not sufficient to sort the slice without mutating the underlying array
+	// For more details, see https://blog.golang.org/go-slices-usage-and-internals
+	nidentities := make([]*nodeIdentity, len(nodes))
+	for i, n := range nodes {
+		if n == nil {
+			panic("NodeRecord cannot be nil")
+		}
+		nidentities[i] = &nodeIdentity{coreID: n}
+	}
+	sort.Sort(nodeIdentities(nidentities))
+	return nidentities
+}
--- internal/identity/identity_test.go
+++ internal/identity/identity_test.go
@@ -1,31 +1,96 @@
-package identity_test
+package identity
 
 import (
-	"github.com/dapperlabs/bamboo-node/internal/identity"
 	"sort"
 	"testing"
 
 	. "github.com/onsi/gomega"
 )
 
-func TestNewInMemoryIdentityTable(t *testing.T) {
+// Compile-time check that nodeIdentity implements interface identity.NodeIdentity
+func TestNodeIdentitiesImplementsInterface(t *testing.T) {
+	var _ NodeIdentity = (*nodeIdentity)(nil)
+}
+
+// Compile-time check that InMemoryIdentityTable implements interface identity.Table
+func TestInMemoryIdentityTableImplementsTable(t *testing.T) {
+	var _ Table = (*InMemoryIdentityTable)(nil)
+}
+
+// Compile-time check that []nodeIdentity, aka nodeIdentities, implements interface sort.Interface
+func TestNodeIdentitiesImplementsSortable(t *testing.T) {
+	var _ sort.Interface = (*nodeIdentities)(nil)
+}
+
+// Compile-time check that []NodeRecord, aka NodeRecords, implements interface sort.Interface
+func TestNodeRecordsImplementsSortable(t *testing.T) {
+	var _ sort.Interface = (*NodeRecords)(nil)
+}
+
+func TestNodeRecordsSort(t *testing.T) {
 	RegisterTestingT(t)
 
-	nodes := identity.NodeRecords{
-		&identity.NodeRecord{ID: 1},
-		&identity.NodeRecord{ID: 3},
-		&identity.NodeRecord{ID: 2},
-		&identity.NodeRecord{ID: 5},
+	nodes := NodeRecords{
+		&NodeRecord{ID: 1},
+		&NodeRecord{ID: 3},
+		&NodeRecord{ID: 2},
+		&NodeRecord{ID: 5},
 	}
 
 	sort.Sort(nodes)
 
 	Expect(nodes).To(Equal(
-		identity.NodeRecords{
-			&identity.NodeRecord{ID: 1},
-			&identity.NodeRecord{ID: 2},
-			&identity.NodeRecord{ID: 3},
-			&identity.NodeRecord{ID: 5},
+		NodeRecords{
+			&NodeRecord{ID: 1},
+			&NodeRecord{ID: 2},
+			&NodeRecord{ID: 3},
+			&NodeRecord{ID: 5},
 		},
 	))
 }
+
+func TestNodeIdentitiesSort(t *testing.T) {
+	RegisterTestingT(t)
+
+	nodes := nodeIdentities{
+		&nodeIdentity{coreID: &NodeRecord{ID: 1}, index:11},
+		&nodeIdentity{coreID: &NodeRecord{ID: 3}, index:33},
+		&nodeIdentity{coreID: &NodeRecord{ID: 2}, index:22},
+		&nodeIdentity{coreID: &NodeRecord{ID: 5}, index:55},
+	}
+
+	sort.Sort(nodes)
+
+	Expect(nodes).To(Equal(
+		nodeIdentities{
+			&nodeIdentity{coreID: &NodeRecord{ID: 1}, index:11},
+			&nodeIdentity{coreID: &NodeRecord{ID: 2}, index:22},
+			&nodeIdentity{coreID: &NodeRecord{ID: 3}, index:33},
+			&nodeIdentity{coreID: &NodeRecord{ID: 5}, index:55},
+		},
+	))
+}
+
+func TestNewInMemoryIdentityTable(t *testing.T) {
+	RegisterTestingT(t)
+
+	nodes := nodeIdentities{
+		&nodeIdentity{coreID: &NodeRecord{ID: 1}, index:11},
+		&nodeIdentity{coreID: &NodeRecord{ID: 3}, index:33},
+		&nodeIdentity{coreID: &NodeRecord{ID: 2}, index:22},
+		&nodeIdentity{coreID: &NodeRecord{ID: 5}, index:55},
+	}
+
+	sort.Sort(nodes)
+
+	Expect(nodes).To(Equal(
+		nodeIdentities{
+			&nodeIdentity{coreID: &NodeRecord{ID: 1}, index:11},
+			&nodeIdentity{coreID: &NodeRecord{ID: 2}, index:22},
+			&nodeIdentity{coreID: &NodeRecord{ID: 3}, index:33},
+			&nodeIdentity{coreID: &NodeRecord{ID: 5}, index:55},
+		},
+	))
+}
+
+
