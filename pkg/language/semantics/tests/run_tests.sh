cff873d4bf3ddf627446a12872a00df270f232c5
Brandon Moore <brandon.moore@runtimeverification.com>
2019-08-29 15:13:08 -0500

    Language Syntax in K
    Initial syntax and tests in K.
    Test cases were extracted from the interpreter unit tests.
    The resource, structure, and interface syntax is poorly covered.

---
+++ pkg/language/semantics/tests/run_tests.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+export OPAMROOT=/usr/lib/kframework/lib/opamroot
+cd $(dirname $0)
+export KRUN_COMPILED_DEF="$(cd ..;pwd)"
+echo $KRUN_COMPILED_DEF
+test_parse () { kast "$1"; }
+
+if [ -t 1 ]; then
+  FANCY=true
+else
+  FANCY=false
+fi
+
+for f in parser/valid/*.bpl; do
+  if [ "$FANCY" = true ]; then
+    printf "\e[2K\rRUNNING %.$(($(tput cols)-8))s" "$f"
+  fi
+  if ! test_parse $f &> /dev/null; then
+    if [ "$FANCY" = true ]; then printf "\e[2K\r"; fi
+    printf "FAIL %s\n" "$f" 
+  fi
+done
+
+for f in parser/invalid/*.bpl; do
+  if [ "$FANCY" = true ]; then
+    printf "\e[2K\rRUNNING %.$(($(tput cols)-8))s" "$f"
+  fi
+  if test_parse $f &> /dev/null; then
+    if [ "$FANCY" = true ]; then printf "\e[2K\r"; fi
+    printf "FAIL %s\n" "$f" 
+  fi
+done
+echo DONE
