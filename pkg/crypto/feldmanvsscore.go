df6b0ab838ffcf892d91be4040819de05247411e
Tarak Ben Youssef <tarak.benyoussef@dapperlabs.com>
2019-09-17 19:33:51 -0700

    more polynomial related changes

--- pkg/crypto/feldmanvsscore.go
+++ pkg/crypto/feldmanvsscore.go
@@ -4,7 +4,10 @@ package crypto
 // #cgo LDFLAGS: -Lrelic/build/lib -l relic_s
 // #include "dkg_include.h"
 import "C"
-import "fmt"
+import (
+	"fmt"
+	"unsafe"
+)
 
 // TODO: remove -wall after reaching a stable version
 // TDOO: enable QUIET in relic
@@ -14,8 +17,8 @@ func (s *feldmanVSSstate) generateShares(seed []byte) *DKGoutput {
 	// Generate a polyomial P in Zr[X]
 	s.a = make([]scalar, s.threshold+1)
 	for i := 0; i < s.threshold+1; i++ {
-		randZr(&(s.a[i]))
-		//s.a[i].setInt(1)
+		//randZr(&(s.a[i]))
+		s.a[i].setInt(1)
 	}
 
 	// prepare the DKGToSend slice and compute the shares
@@ -29,14 +32,30 @@ func (s *feldmanVSSstate) generateShares(seed []byte) *DKGoutput {
 		toSend := &(out.action[i])
 		toSend.broadcast = false
 		toSend.dest = index(s.currentIndex, i)
-		share := index(s.currentIndex, i) //:= polynomialImage(a []scalar, i)
-		toSend.data = DKGmsg([]byte{byte(FeldmanVSSshare), byte(share)})
+		shareSize := s.blsContext.PrKeySize()
+		data := make([]byte, shareSize+1)
+		data[0] = byte(FeldmanVSSshare)
+		index := index(s.currentIndex, i)
+		copy(data[1:], polynomialImage(s.a, index, shareSize))
+		toSend.data = DKGmsg(data)
 	}
 	return out
 }
 
 func (s *feldmanVSSstate) receiveShare(origin int, data []byte) (dkgResult, error) {
-	fmt.Printf("%d Receiving a share from %d", s.currentIndex, origin)
-	fmt.Printf("the share is %d\n", data[0])
+	fmt.Printf("%d Receiving a share from %d, the share is %d\n", s.currentIndex, origin, data[0])
 	return valid, nil
+}
+
+// computes P(x) = a_0 + a_1*x + .. + a_n x^n (mod p)
+// p being the order of G1
+// x being a small integer
+func polynomialImage(a []scalar, x int, outputSize int) []byte {
+	s := make([]byte, outputSize)
+
+	C._polynomialImage((*C.uchar)((unsafe.Pointer)(&s[0])),
+		(*C.bn_st)(&a[0]), (C.int)(len(a)),
+		(C.int)(x),
+	)
+	return s
 }
