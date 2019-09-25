057bf1f4621637c7a3457f5ba305af3130bc5f76
Bastian Müller <bastian@axiomzen.co>
2019-09-25 08:01:39 -0700

    Remove unnecessary test registration

--- pkg/language/runtime/tests/checker_test.go
+++ pkg/language/runtime/tests/checker_test.go
@@ -6903,7 +6903,6 @@ func TestCheckMissingReturnStatementStructFunction(t *testing.T) {
 }
 
 func TestCheckFunctionDeclarationParameterWithMoveAnnotation(t *testing.T) {
-	RegisterTestingT(t)
 
 	for _, kind := range common.CompositeKinds {
 		t.Run(kind.Keyword(), func(t *testing.T) {
