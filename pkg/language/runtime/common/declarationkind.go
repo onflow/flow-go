f5185a865a1b5cf89e43bc4e2c21f1b373ea2705
Bastian Müller <bastian@axiomzen.co>
2019-09-27 15:15:44 -0700

    add separate declaration kind for `self`

--- pkg/language/runtime/common/declarationkind.go
+++ pkg/language/runtime/common/declarationkind.go
@@ -24,6 +24,7 @@ const (
 	DeclarationKindResourceInterface
 	DeclarationKindContractInterface
 	DeclarationKindImport
+	DeclarationKindSelf
 )
 
 func (k DeclarationKind) Name() string {
@@ -60,6 +61,8 @@ func (k DeclarationKind) Name() string {
 		return "contract interface"
 	case DeclarationKindImport:
 		return "import"
+	case DeclarationKindSelf:
+		return "self"
 	}
 
 	panic(&errors.UnreachableError{})
