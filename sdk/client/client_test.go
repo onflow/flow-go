4758248a807d8157d22ea4533159db80388858f5
Jordan Schalm <jordan.schalm@gmail.com>
2019-10-10 16:50:26 -0700

    Refactor client tests
    * Use testify rather than gomega
    * Use t.Run to separate success/failure cases

--- sdk/client/client_test.go
+++ sdk/client/client_test.go
@@ -9,7 +9,7 @@ import (
 	"testing"
 
 	"github.com/golang/mock/gomock"
-	. "github.com/onsi/gomega"
+	"github.com/stretchr/testify/assert"
 
 	"github.com/dapperlabs/flow-go/pkg/grpc/services/observe"
 	"github.com/dapperlabs/flow-go/pkg/types"
@@ -20,8 +20,6 @@ import (
 )
 
 func TestSendTransaction(t *testing.T) {
-	RegisterTestingT(t)
-
 	mockCtrl := gomock.NewController(t)
 	defer mockCtrl.Finish()
 
@@ -32,29 +30,32 @@ func TestSendTransaction(t *testing.T) {
 
 	tx := unittest.TransactionFixture()
 
-	// client should return non-error if RPC call succeeds
-	mockRPC.EXPECT().
-		SendTransaction(ctx, gomock.Any()).
-		Return(&observe.SendTransactionResponse{Hash: tx.Hash()}, nil).
-		Times(1)
+	t.Run("Success", func(t *testing.T) {
+		// client should return non-error if RPC call succeeds
+		mockRPC.EXPECT().
+			SendTransaction(ctx, gomock.Any()).
+			Return(&observe.SendTransactionResponse{Hash: tx.Hash()}, nil).
+			Times(1)
 
-	err := c.SendTransaction(ctx, tx)
-	Expect(err).ToNot(HaveOccurred())
+		err := c.SendTransaction(ctx, tx)
+		assert.Nil(t, err)
+	})
 
-	// client should return error if RPC call fails
-	mockRPC.EXPECT().
-		SendTransaction(ctx, gomock.Any()).
-		Return(nil, errors.New("dummy error")).
-		Times(1)
 
-	// error should be passed to user
-	err = c.SendTransaction(ctx, tx)
-	Expect(err).To(HaveOccurred())
+	t.Run("Server error", func(t *testing.T) {
+		// client should return error if RPC call fails
+		mockRPC.EXPECT().
+			SendTransaction(ctx, gomock.Any()).
+			Return(nil, errors.New("dummy error")).
+			Times(1)
+
+		// error should be passed to user
+		err := c.SendTransaction(ctx, tx)
+		assert.Error(t, err)
+	})
 }
 
 func TestGetLatestBlock(t *testing.T) {
-	RegisterTestingT(t)
-
 	mockCtrl := gomock.NewController(t)
 	defer mockCtrl.Finish()
 
@@ -67,31 +68,33 @@ func TestGetLatestBlock(t *testing.T) {
 		Block: proto.BlockHeaderToMessage(unittest.BlockHeaderFixture()),
 	}
 
-	// client should return non-error if RPC call succeeds
-	mockRPC.EXPECT().
-		GetLatestBlock(ctx, gomock.Any()).
-		Return(res, nil).
-		Times(1)
+	t.Run("Success", func(t *testing.T) {
+		// client should return non-error if RPC call succeeds
+		mockRPC.EXPECT().
+			GetLatestBlock(ctx, gomock.Any()).
+			Return(res, nil).
+			Times(1)
 
-	blockHeaderA, err := c.GetLatestBlock(ctx, true)
-	Expect(err).ToNot(HaveOccurred())
+		blockHeaderA, err := c.GetLatestBlock(ctx, true)
+		assert.Nil(t, err)
 
-	blockHeaderB := proto.MessageToBlockHeader(res.GetBlock())
-	Expect(*blockHeaderA).To(Equal(blockHeaderB))
+		blockHeaderB := proto.MessageToBlockHeader(res.GetBlock())
+		assert.Equal(t, *blockHeaderA, blockHeaderB)
+	})
 
-	// client should return error if RPC call fails
-	mockRPC.EXPECT().
-		GetLatestBlock(ctx, gomock.Any()).
-		Return(nil, errors.New("dummy error")).
-		Times(1)
+	t.Run("Server error", func(t *testing.T) {
+		// client should return error if RPC call fails
+		mockRPC.EXPECT().
+			GetLatestBlock(ctx, gomock.Any()).
+			Return(nil, errors.New("dummy error")).
+			Times(1)
 
-	_, err = c.GetLatestBlock(ctx, true)
-	Expect(err).To(HaveOccurred())
+		_, err := c.GetLatestBlock(ctx, true)
+		assert.Error(t, err)
+	})
 }
 
 func TestCallScript(t *testing.T) {
-	RegisterTestingT(t)
-
 	mockCtrl := gomock.NewController(t)
 	defer mockCtrl.Finish()
 
@@ -102,50 +105,56 @@ func TestCallScript(t *testing.T) {
 
 	valueBytes, _ := json.Marshal(1)
 
-	// client should return non-error if RPC call succeeds
-	mockRPC.EXPECT().
-		CallScript(ctx, gomock.Any()).
-		Return(&observe.CallScriptResponse{Value: valueBytes}, nil).
-		Times(1)
+	t.Run("Success", func(t *testing.T) {
+		// client should return non-error if RPC call succeeds
+		mockRPC.EXPECT().
+			CallScript(ctx, gomock.Any()).
+			Return(&observe.CallScriptResponse{Value: valueBytes}, nil).
+			Times(1)
 
-	value, err := c.CallScript(ctx, []byte("fun main(): Int { return 1 }"))
-	Expect(err).ToNot(HaveOccurred())
-	Expect(value).To(Equal(float64(1)))
+		value, err := c.CallScript(ctx, []byte("fun main(): Int { return 1 }"))
+		assert.Nil(t, err)
+		assert.Equal(t, value, float64(1))
+	})
 
-	// client should return error if RPC call fails
-	mockRPC.EXPECT().
-		CallScript(ctx, gomock.Any()).
-		Return(nil, errors.New("dummy error")).
-		Times(1)
+	t.Run("Server error", func(t *testing.T) {
+		// client should return error if RPC call fails
+		mockRPC.EXPECT().
+			CallScript(ctx, gomock.Any()).
+			Return(nil, errors.New("dummy error")).
+			Times(1)
 
-	// error should be passed to user
-	_, err = c.CallScript(ctx, []byte("fun main(): Int { return 1 }"))
-	Expect(err).To(HaveOccurred())
+		// error should be passed to user
+		_, err := c.CallScript(ctx, []byte("fun main(): Int { return 1 }"))
+		assert.Error(t, err)
+	})
 
-	// client should return error if value is empty
-	mockRPC.EXPECT().
-		CallScript(ctx, gomock.Any()).
-		Return(&observe.CallScriptResponse{Value: []byte{}}, nil).
-		Times(1)
+	t.Run("Error - empty return value", func(t *testing.T) {
+		// client should return error if value is empty
+		mockRPC.EXPECT().
+			CallScript(ctx, gomock.Any()).
+			Return(&observe.CallScriptResponse{Value: []byte{}}, nil).
+			Times(1)
 
-	// error should be passed to user
-	_, err = c.CallScript(ctx, []byte("fun main(): Int { return 1 }"))
-	Expect(err).To(HaveOccurred())
+		// error should be passed to user
+		_, err := c.CallScript(ctx, []byte("fun main(): Int { return 1 }"))
+		assert.Error(t, err)
+	})
 
-	// client should return error if value is malformed
-	mockRPC.EXPECT().
-		CallScript(ctx, gomock.Any()).
-		Return(&observe.CallScriptResponse{Value: []byte("asdfafa")}, nil).
-		Times(1)
+	t.Run("Error - malformed return value", func(t *testing.T) {
+		// client should return error if value is malformed
+		mockRPC.EXPECT().
+			CallScript(ctx, gomock.Any()).
+			Return(&observe.CallScriptResponse{Value: []byte("asdfafa")}, nil).
+			Times(1)
 
-	// error should be passed to user
-	_, err = c.CallScript(ctx, []byte("fun main(): Int { return 1 }"))
-	Expect(err).To(HaveOccurred())
+		// error should be passed to user
+		_, err := c.CallScript(ctx, []byte("fun main(): Int { return 1 }"))
+		assert.Error(t, err)
+	})
 }
 
 func TestGetEvents(t *testing.T) {
-	RegisterTestingT(t)
-
 	mockCtrl := gomock.NewController(t)
 	defer mockCtrl.Finish()
 
@@ -167,37 +176,43 @@ func TestGetEvents(t *testing.T) {
 
 	var buf bytes.Buffer
 	err := json.NewEncoder(&buf).Encode(events)
-	Expect(err).ToNot(HaveOccurred())
+	assert.Nil(t, err)
 
-	// Set up the mock to return a mocked event response
-	mockRPC.EXPECT().
-		GetEvents(ctx, gomock.Any()).
-		Return(&observe.GetEventsResponse{EventsJson: buf.Bytes()}, nil).
-		Times(1)
+	t.Run("Success", func(t *testing.T) {
+		// Set up the mock to return a mocked event response
+		mockRPC.EXPECT().
+			GetEvents(ctx, gomock.Any()).
+			Return(&observe.GetEventsResponse{EventsJson: buf.Bytes()}, nil).
+			Times(1)
 
-	// The client should pass the response to the client
-	res, err := c.GetEvents(ctx, &types.EventQuery{})
-	Expect(err).ToNot(HaveOccurred())
-	Expect(len(res)).To(Equal(1))
-	Expect(res[0].ID).To(Equal(mockEvent.ID))
+		// The client should pass the response to the client
+		res, err := c.GetEvents(ctx, &types.EventQuery{})
+		assert.Nil(t, err)
+		assert.Equal(t, len(res), 1)
+		assert.Equal(t, res[0].ID, mockEvent.ID)
+	})
 
-	// Set up the mock to return an error
-	mockRPC.EXPECT().
-		GetEvents(ctx, gomock.Any()).
-		Return(nil, fmt.Errorf("dummy error")).
-		Times(1)
+	t.Run("Server error", func(t *testing.T) {
+		// Set up the mock to return an error
+		mockRPC.EXPECT().
+			GetEvents(ctx, gomock.Any()).
+			Return(nil, fmt.Errorf("dummy error")).
+			Times(1)
 
-	// The client should pass along the error
-	_, err = c.GetEvents(ctx, &types.EventQuery{})
-	Expect(err).To(HaveOccurred())
+		// The client should pass along the error
+		_, err = c.GetEvents(ctx, &types.EventQuery{})
+		assert.Error(t, err)
+	})
 
-	// Set up the mock to return a malformed eventsJSON response
-	mockRPC.EXPECT().
-		GetEvents(ctx, gomock.Any()).
-		Return(&observe.GetEventsResponse{EventsJson: []byte{1,2,3,4}}, nil).
-		Times(1)
+	t.Run("Error - malformed return value", func(t *testing.T) {
+		// Set up the mock to return a malformed eventsJSON response
+		mockRPC.EXPECT().
+			GetEvents(ctx, gomock.Any()).
+			Return(&observe.GetEventsResponse{EventsJson: []byte{1,2,3,4}}, nil).
+			Times(1)
 
-	// The client should return an error because it should fail to decode
-	_, err = c.GetEvents(ctx, &types.EventQuery{})
-	Expect(err).To(HaveOccurred())
+		// The client should return an error because it should fail to decode
+		_, err = c.GetEvents(ctx, &types.EventQuery{})
+		assert.Error(t, err)
+	})
 }
