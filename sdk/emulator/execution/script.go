01d24c164d2c2191b5aea611b1415677dd231e33
Peter Siemens <peterjsiemens@gmail.com>
2019-10-18 11:57:40 -0700

    Update script.go

--- sdk/emulator/execution/script.go
+++ sdk/emulator/execution/script.go
@@ -2,6 +2,7 @@ package execution
 
 import "github.com/dapperlabs/flow-go/pkg/crypto"
 
+// ScriptHash computes the content hash of a Cadence script.
 func ScriptHash(script []byte) crypto.Hash {
 	hasher, _ := crypto.NewHasher(crypto.SHA3_256)
 	return hasher.ComputeHash(script)
