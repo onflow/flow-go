37f90413c2a387ca02c45099420096ab86c1519b
Jordan Schalm <jordan.schalm@gmail.com>
2019-10-21 15:45:23 -0700

    Add --init flag to start command
    Allows first-run to work in one command rather than two.

--- internal/cli/initialize/init.go
+++ internal/cli/initialize/init.go
@@ -26,30 +26,38 @@ var Cmd = &cobra.Command{
 	Short: "Initialize a new account profile",
 	Run: func(cmd *cobra.Command, args []string) {
 		if !project.ConfigExists() || conf.Reset {
-			prKey, _ := crypto.GeneratePrivateKey(crypto.ECDSA_P256, []byte{})
-			prKeyBytes, _ := prKey.Encode()
-			prKeyHex := hex.EncodeToString(prKeyBytes)
-			address := types.HexToAddress("01").Hex()
-
-			conf := &project.Config{
-				Accounts: map[string]*project.AccountConfig{
-					"root": &project.AccountConfig{
-						Address:    address,
-						PrivateKey: prKeyHex,
-					},
-				},
-			}
-
-			project.SaveConfig(conf)
+			pconf := InitializeProject()
+			rootAcct := pconf.Accounts["root"]
 
 			fmt.Printf("⚙️   Flow client initialized with root account:\n\n")
-			fmt.Printf("👤  Address: 0x%s\n", address)
-			fmt.Printf("🔑  PrivateKey: %s\n\n", prKeyHex)
+			fmt.Printf("👤  Address: 0x%s\n", rootAcct.Address)
+			fmt.Printf("🔑  PrivateKey: %s\n\n", rootAcct.PrivateKey)
 			fmt.Printf("ℹ️   Start the emulator with this root account by running: flow emulator start\n")
 		} else {
 			fmt.Printf("⚠️   Flow configuration file already exists! Begin by running: flow emulator start\n")
 		}
 	},
+}
+
+// InitializeProject generates a new root key and saves project config.
+func InitializeProject() *project.Config {
+	prKey, _ := crypto.GeneratePrivateKey(crypto.ECDSA_P256, []byte{})
+	prKeyBytes, _ := prKey.Encode()
+	prKeyHex := hex.EncodeToString(prKeyBytes)
+	address := types.HexToAddress("01").Hex()
+
+	conf := &project.Config{
+		Accounts: map[string]*project.AccountConfig{
+			"root": &project.AccountConfig{
+				Address:    address,
+				PrivateKey: prKeyHex,
+			},
+		},
+	}
+
+	project.SaveConfig(conf)
+
+	return conf
 }
 
 func init() {
