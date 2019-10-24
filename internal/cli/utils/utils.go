1499591d9ca503f7933239c951b1e476bb8dc854
Peter Siemens <peterjsiemens@gmail.com>
2019-10-24 12:34:31 -0700

    Remove unneeded key utilities

--- internal/cli/utils/utils.go
+++ internal/cli/utils/utils.go
@@ -1,11 +1,8 @@
 package utils
 
 import (
-	"encoding/hex"
 	"fmt"
 	"os"
-
-	"github.com/dapperlabs/flow-go/pkg/crypto"
 )
 
 func Exit(code int, msg string) {
@@ -16,26 +13,4 @@ func Exit(code int, msg string) {
 func Exitf(code int, msg string, args ...interface{}) {
 	fmt.Printf(msg+"\n", args...)
 	os.Exit(code)
-}
-
-func DecodePrivateKey(derHex string) (crypto.PrivateKey, error) {
-	prKeyDer, err := hex.DecodeString(derHex)
-	if err != nil {
-		return nil, err
-	}
-
-	return crypto.DecodePrivateKey(crypto.ECDSA_P256, prKeyDer)
-}
-
-func EncodePrivateKey(sk crypto.PrivateKey) (string, error) {
-	skBytes, err := sk.Encode()
-	if err != nil {
-		return "", err
-	}
-
-	return hex.EncodeToString(skBytes), nil
-}
-
-func EncodePublicKey(pubKey crypto.PublicKey) ([]byte, error) {
-	return pubKey.Encode()
 }
