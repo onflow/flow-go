d698ec93f5dae11bc73ccc8e79a52b2f491fbc32
Peter Siemens <peterjsiemens@gmail.com>
2019-10-25 11:00:35 -0700

    Types do not compute hashes

--- pkg/types/chunk.go
+++ pkg/types/chunk.go
@@ -8,9 +8,9 @@ import (
 
 // Chunk is a container of transactions with total compute limit less than chunkTotalGasLimit
 type Chunk struct {
+	Hash                  crypto.Hash
 	Transactions          []*Transaction
 	TotalComputationLimit uint64
-	Hash                  crypto.Hash
 }
 
 func (c *Chunk) String() string {
@@ -19,29 +19,20 @@ func (c *Chunk) String() string {
 		return fmt.Sprintf("An empty chunk")
 	case 1:
 		return fmt.Sprintf("Chunk %v includes a transaction (totalComputationLimit: %v):\n %v",
-			c.Hash, c.TotalComputationLimit, c.Transactions[0].Hash())
+			c.Hash, c.TotalComputationLimit, c.Transactions[0].Hash)
 	default:
 		return fmt.Sprintf("Chunk %v includes %v transactions (totalComputationLimit: %v):\n %v\n ...\n %v  ", c.Hash,
-			len(c.Transactions), c.TotalComputationLimit, c.Transactions[0].Hash(),
-			c.Transactions[len(c.Transactions)-1].Hash())
+			len(c.Transactions), c.TotalComputationLimit, c.Transactions[0].Hash,
+			c.Transactions[len(c.Transactions)-1].Hash)
 	}
-}
-
-// GenerateHash generates and returns hash for the given chunk
-func (c *Chunk) GenerateHash() crypto.Hash {
-	hasher, _ := crypto.NewHasher(crypto.SHA3_256)
-	for _, tx := range c.Transactions {
-		hasher.Add(tx.CanonicalEncoding())
-	}
-	c.Hash = hasher.SumHash()
-	return c.Hash
 }
 
 // NewChunk creates a new chunk
 func NewChunk(transactions []*Transaction, totalComputationLimit uint64) *Chunk {
-	newChunk := &Chunk{Transactions: transactions, TotalComputationLimit: totalComputationLimit}
-	newChunk.GenerateHash()
-	return newChunk
+	return &Chunk{
+		Transactions:          transactions,
+		TotalComputationLimit: totalComputationLimit,
+	}
 }
 
 // Chunker knows how to group a collection of transactions into chunks
--- pkg/types/collection.go
+++ pkg/types/collection.go
@@ -5,13 +5,6 @@ import (
 )
 
 type Collection struct {
+	Hash         crypto.Hash
 	Transactions []*Transaction
-}
-
-func (col *Collection) Hash() crypto.Hash {
-	hasher, _ := crypto.NewHasher(crypto.SHA3_256)
-	for _, tx := range col.Transactions {
-		hasher.Add(tx.CanonicalEncoding())
-	}
-	return hasher.SumHash()
 }
