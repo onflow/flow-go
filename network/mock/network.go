41475267ca4b62fa77f4b369562fbbbb7b5db6d5
Leo Zhang (zhangchiqing) <zhangchiqing@gmail.com>
2019-10-29 15:21:57 -0700

    add PeerHasSeen

--- network/mock/network.go
+++ network/mock/network.go
@@ -5,6 +5,8 @@ import (
 
 	"github.com/dapperlabs/flow-go/module"
 	"github.com/dapperlabs/flow-go/network"
+	"github.com/dapperlabs/flow-go/network/trickle"
+	"github.com/dapperlabs/flow-go/network/trickle/state"
 )
 
 // Network is a mocked network layer made for testing Engine's behavior.
@@ -15,16 +17,23 @@ type Network struct {
 	hub     *Hub
 	com     module.Committee
 	engines map[uint8]network.Engine
+	state   trickle.State
 }
 
 // NewNetwork create a mocked network.
 // The committee has the identity of the node already, so only `committee` is needed
 // in order for a mock hub to find each other.
 func NewNetwork(com module.Committee, hub *Hub) (*Network, error) {
+	state, err := state.New()
+	if err != nil {
+		return nil, err
+	}
+
 	o := &Network{
 		com:     com,
 		hub:     hub,
 		engines: make(map[uint8]network.Engine),
+		state:   state,
 	}
 	// Plug the network to a hub so that networks can find each other.
 	hub.Plug(o)
@@ -34,19 +43,41 @@ func NewNetwork(com module.Committee, hub *Hub) (*Network, error) {
 // submit is called when an Engine is sending an event to an Engine on another node or nodes.
 func (mn *Network) submit(engineID uint8, event interface{}, targetIDs ...string) error {
 	var err error
+	engine, ok := mn.engines[engineID]
+	if !ok {
+		return errors.Errorf("missing engine for engineID %v", engineID)
+	}
+	eventID, err := engine.Identify(event)
+	if err != nil {
+		return err
+	}
+
 	for _, nodeID := range targetIDs {
 		// Find the network of the targeted node
 		receiverNetwork, ok := mn.hub.Networks[nodeID]
 		if !ok || receiverNetwork == nil {
 			return errors.Errorf("Network can not find a node by ID %v", nodeID)
 		}
-		// Find the engine of the targeted network
+
 		receiverEngine, ok := receiverNetwork.engines[engineID]
 		if !ok {
 			return errors.Errorf("Network can not find engine ID: %v for node: %v", engineID, nodeID)
 		}
+
+		// Make sure the peer is up
+		receiverNetwork.state.Up(nodeID)
+
+		if receiverNetwork.state.PeerHaveSeen(nodeID, eventID) {
+			continue
+		}
+
+		// mark the peer has seen the event
+		receiverNetwork.state.Seen(nodeID, eventID)
+
+		// Find the engine of the targeted network
 		// Call `Process` to let receiver engine receive the event directly.
 		err = receiverEngine.Process(mn.GetID(), event)
+
 		if err != nil {
 			return errors.Wrapf(err, "senderEngine failed to process event: %v", event)
 		}
