48f0f759e581d20c58974d9975b980abdae28b14
Leo Zhang (zhangchiqing) <zhangchiqing@gmail.com>
2019-10-29 15:59:59 -0700

    try 50 times

--- engine/consensus/propagation/tests/network_mocked_test.go
+++ engine/consensus/propagation/tests/network_mocked_test.go
@@ -287,8 +287,8 @@ func TestSubmitCollection(t *testing.T) {
 		testConcrrencyOnce)
 
 	// will take roughly 6 seconds
-	t.Run("run the above tests for 10 times", func(t *testing.T) {
-		for i := 0; i < 10; i++ {
+	t.Run("run the above tests for 50 times", func(t *testing.T) {
+		for i := 0; i < 50; i++ {
 			testConcrrencyOnce(t)
 		}
 	})
