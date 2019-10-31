4c05ce813c9066c89b5e321969f535dd8c49c5ef
Peter Siemens <peterjsiemens@gmail.com>
2019-10-31 13:40:05 -0700

    Remove models from encoding package

--- model/encoding/encoder.go
+++ model/encoding/encoder.go
@@ -2,24 +2,13 @@ package encoding
 
 import (
 	"github.com/dapperlabs/flow-go/model/encoding/rlp"
-	"github.com/dapperlabs/flow-go/model/flow"
 )
 
 // Encoder encodes and decodes values to and from bytes.
 type Encoder interface {
 	Encode(interface{}) ([]byte, error)
 	Decode(interface{}, []byte) error
-
-	// EncodeAccountPublicKey encodes an account public key to bytes.
-	EncodeAccountPublicKey(flow.AccountPublicKey) ([]byte, error)
-	// DecodeAccountPublicKey decodes an account public key from bytes.
-	DecodeAccountPublicKey([]byte) (flow.AccountPublicKey, error)
-
-	// EncodeAccountPrivateKey encodes an account private key to bytes.
-	EncodeAccountPrivateKey(flow.AccountPrivateKey) ([]byte, error)
-	// DecodeAccountPrivateKey decodes an account private key from bytes.
-	DecodeAccountPrivateKey([]byte) (flow.AccountPrivateKey, error)
 }
 
-// DefaultEncoder is the default model encoder used by Flow.
+// DefaultEncoder is the default encoder used by Flow.
 var DefaultEncoder = rlp.NewEncoder()
--- model/encoding/rlp/rlp.go
+++ model/encoding/rlp/rlp.go
@@ -2,9 +2,6 @@ package rlp
 
 import (
 	"github.com/ethereum/go-ethereum/rlp"
-
-	"github.com/dapperlabs/flow-go/crypto"
-	"github.com/dapperlabs/flow-go/model/flow"
 )
 
 type Encoder struct{}
@@ -19,82 +16,4 @@ func (e *Encoder) Encode(val interface{}) ([]byte, error) {
 
 func (e *Encoder) Decode(b []byte, val interface{}) error {
 	return rlp.DecodeBytes(b, val)
-}
-
-func (e *Encoder) EncodeAccountPublicKey(a flow.AccountPublicKey) ([]byte, error) {
-	publicKey, err := a.PublicKey.Encode()
-	if err != nil {
-		return nil, err
-	}
-
-	w := accountPublicKeyWrapper{
-		PublicKey: publicKey,
-		SignAlgo:  uint(a.SignAlgo),
-		HashAlgo:  uint(a.HashAlgo),
-		Weight:    uint(a.Weight),
-	}
-
-	return rlp.EncodeToBytes(&w)
-}
-
-func (e *Encoder) DecodeAccountPublicKey(b []byte) (a flow.AccountPublicKey, err error) {
-	var w accountPublicKeyWrapper
-
-	err = rlp.DecodeBytes(b, &w)
-	if err != nil {
-		return a, err
-	}
-
-	signAlgo := crypto.SigningAlgorithm(w.SignAlgo)
-	hashAlgo := crypto.HashingAlgorithm(w.HashAlgo)
-
-	publicKey, err := crypto.DecodePublicKey(signAlgo, w.PublicKey)
-	if err != nil {
-		return a, err
-	}
-
-	return flow.AccountPublicKey{
-		PublicKey: publicKey,
-		SignAlgo:  signAlgo,
-		HashAlgo:  hashAlgo,
-		Weight:    int(w.Weight),
-	}, nil
-}
-
-func (e *Encoder) EncodeAccountPrivateKey(a flow.AccountPrivateKey) ([]byte, error) {
-	privateKey, err := a.PrivateKey.Encode()
-	if err != nil {
-		return nil, err
-	}
-
-	w := accountPrivateKeyWrapper{
-		PrivateKey: privateKey,
-		SignAlgo:   uint(a.SignAlgo),
-		HashAlgo:   uint(a.HashAlgo),
-	}
-
-	return rlp.EncodeToBytes(&w)
-}
-
-func (e *Encoder) DecodeAccountPrivateKey(b []byte) (a flow.AccountPrivateKey, err error) {
-	var w accountPrivateKeyWrapper
-
-	err = rlp.DecodeBytes(b, &w)
-	if err != nil {
-		return a, err
-	}
-
-	signAlgo := crypto.SigningAlgorithm(w.SignAlgo)
-	hashAlgo := crypto.HashingAlgorithm(w.HashAlgo)
-
-	privateKey, err := crypto.DecodePrivateKey(signAlgo, w.PrivateKey)
-	if err != nil {
-		return a, err
-	}
-
-	return flow.AccountPrivateKey{
-		PrivateKey: privateKey,
-		SignAlgo:   signAlgo,
-		HashAlgo:   hashAlgo,
-	}, nil
 }
