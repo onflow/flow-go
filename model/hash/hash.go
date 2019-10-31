24178c6a50a939806b29b17182911ad577c683e3
Peter Siemens <peterjsiemens@gmail.com>
2019-10-31 13:39:56 -0700

    Update hash.go

--- model/hash/hash.go
+++ model/hash/hash.go
@@ -2,37 +2,15 @@ package hash
 
 import (
 	"github.com/dapperlabs/flow-go/crypto"
-	"github.com/dapperlabs/flow-go/model/encoding"
-	"github.com/dapperlabs/flow-go/model/flow"
 )
 
 // DefaultHasher is the default hasher used by Flow.
-var DefaultHasher, _ = crypto.NewHasher(crypto.SHA3_256)
+var DefaultHasher crypto.Hasher
 
-// SetTransactionHash computes a hash and saves it on the transaction.
-func SetTransactionHash(tx *flow.Transaction) error {
-	b, err := encoding.DefaultEncoder.EncodeTransaction(*tx)
+func init() {
+	var err error
+	DefaultHasher, err = crypto.NewHasher(crypto.SHA3_256)
 	if err != nil {
-		return err
+		panic(err)
 	}
-
-	hash := DefaultHasher.ComputeHash(b)
-
-	tx.Hash = hash
-
-	return nil
-}
-
-// SetChunkHash computes a hash and saves it on the chunk.
-func SetChunkHash(c *flow.Chunk) error {
-	b, err := encoding.DefaultEncoder.EncodeChunk(*c)
-	if err != nil {
-		return err
-	}
-
-	hash := DefaultHasher.ComputeHash(b)
-
-	c.Hash = hash
-
-	return nil
 }
