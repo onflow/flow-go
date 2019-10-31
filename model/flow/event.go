66196bd2aadd5e8f17c81994124e16e88351eaeb
Jordan Schalm <jordan.schalm@gmail.com>
2019-10-31 14:32:51 -0700

    Update event model
    Add placeholder Encode, ID methods. These will use the hashing/encoding
    defined in https://github.com/dapperlabs/flow-go/pull/1057

--- model/flow/event.go
+++ model/flow/event.go
@@ -4,14 +4,21 @@ package flow
 
 import (
 	"fmt"
+	"github.com/dapperlabs/flow-go/crypto"
 	"strings"
 )
 
 type Event struct {
+	// TxHash is the hash of the transaction this event was emitted from.
+	TxHash crypto.Hash
+	// Type is the qualified event type.
 	Type string
 	// Values is a map of all the parameters to the event, keys are parameter
 	// names, values are the parameter values and must be primitive types.
 	Values map[string]interface{}
+	// Index defines the ordering of events in a transaction. The first event
+	// emitted has index 0, the second has index 1, and so on.
+	Index int
 }
 
 // String returns the string representation of this event.
@@ -29,4 +36,32 @@ func (e Event) String() string {
 	}
 
 	return fmt.Sprintf("%s(%s)", e.Type, values.String())
+}
+
+// ID returns a canonical identifier that is guaranteed to be unique.
+func (e Event) ID() string {
+	b, _ := e.Encode()
+	_ = b
+	// TODO hash here
+	return ""
+}
+
+func (e Event) Encode() ([]byte, error) {
+	w := wrapEvent(e)
+	_ = w
+	// TODO encode here
+	return nil, nil
+}
+
+// Defines only the fields needed to uniquely identify an event.
+type eventWrapper struct {
+	TxHash []byte
+	Index  int
+}
+
+func wrapEvent(e Event) eventWrapper {
+	return eventWrapper{
+		TxHash: e.TxHash,
+		Index:  e.Index,
+	}
 }
