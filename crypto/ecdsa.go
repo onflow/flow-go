33a759d1934ad9760505b71ec9ab36951e511e00
Tarak Ben Youssef <tarak.benyoussef@dapperlabs.com>
2019-10-31 19:32:11 -0700

    ecdsa ken gen is deterministic - add raw encoding/decoding for ecdsa

--- crypto/ecdsa.go
+++ crypto/ecdsa.go
@@ -5,10 +5,12 @@ package crypto
 // This is different from the ECDSA version implemented in some blockchains.
 
 import (
+	"bytes"
 	goecdsa "crypto/ecdsa"
 	"crypto/elliptic"
 	"crypto/rand"
 	"crypto/x509"
+	"fmt"
 	"math/big"
 )
 
@@ -75,31 +77,84 @@ func (pk *PubKeyECDSA) Verify(sig Signature, data []byte, alg Hasher) (bool, err
 // GeneratePrKey generates a private key for ECDSA
 // This is only a test function!
 func (a *ECDSAalgo) generatePrivateKey(seed []byte) (PrivateKey, error) {
-	sk, err := goecdsa.GenerateKey(a.curve, rand.Reader)
+	Nlen := bitsToBytes((a.curve.Params().N).BitLen())
+	if len(seed) < Nlen+8 {
+		return nil, cryptoError{fmt.Sprintf("the seed should be at least %d bytes.", Nlen+8)}
+	}
+	sk, err := goecdsa.GenerateKey(a.curve, bytes.NewReader(seed))
 	if err != nil {
 		return nil, cryptoError{"The ECDSA key generation has failed"}
 	}
 	return &PrKeyECDSA{a, sk}, nil
 }
 
-func (a *ECDSAalgo) decodePrivateKey(der []byte) (PrivateKey, error) {
-	sk, err := x509.ParseECPrivateKey(der)
-	if err != nil {
-		return nil, err
+func (a *ECDSAalgo) rawDecodePrivateKey(der []byte) (PrivateKey, error) {
+	Nlen := bitsToBytes((a.curve.Params().N).BitLen())
+	if len(der) != Nlen {
+		return nil, cryptoError{"the raw private key is not valid."}
 	}
-	return &PrKeyECDSA{a, sk}, nil
+	var d big.Int
+	d.SetBytes(der)
+
+	priv := goecdsa.PrivateKey{
+		D: &d,
+	}
+	priv.PublicKey.Curve = a.curve
+	priv.PublicKey.X, priv.PublicKey.Y = a.curve.ScalarBaseMult(der)
+	return &PrKeyECDSA{a, &priv}, nil
+}
+
+func (a *ECDSAalgo) decodePrivateKey(der []byte) (PrivateKey, error) {
+	// NIST curves case
+	if a.algo == ECDSA_P256 {
+		sk, err := x509.ParseECPrivateKey(der)
+		if err != nil {
+			return nil, err
+		}
+		return &PrKeyECDSA{a, sk}, nil
+	}
+	// non-NIST curves
+	if a.algo == ECDSA_SECp256k1 {
+		return a.rawDecodePrivateKey(der)
+	}
+	return nil, cryptoError{"The curve is not supported."}
+}
+
+func (a *ECDSAalgo) rawDecodePublicKey(der []byte) (PublicKey, error) {
+	Plen := bitsToBytes((a.curve.Params().N).BitLen())
+	if len(der) != 2*Plen {
+		return nil, cryptoError{"the raw public key is not valid."}
+	}
+	var x, y big.Int
+	x.SetBytes(der[:Plen])
+	y.SetBytes(der[Plen:])
+
+	pk := goecdsa.PublicKey{
+		Curve: a.curve,
+		X:     &x,
+		Y:     &y,
+	}
+	return &PubKeyECDSA{a, &pk}, nil
 }
 
 func (a *ECDSAalgo) decodePublicKey(der []byte) (PublicKey, error) {
-	i, err := x509.ParsePKIXPublicKey(der)
-	if err != nil {
-		return nil, err
+	// NIST curves case
+	if a.algo == ECDSA_P256 {
+		i, err := x509.ParsePKIXPublicKey(der)
+		if err != nil {
+			return nil, err
+		}
+		goecdsaPk := i.(*goecdsa.PublicKey)
+		return &PubKeyECDSA{
+			alg:      a,
+			goPubKey: goecdsaPk,
+		}, nil
 	}
-	goecdsaPk := i.(*goecdsa.PublicKey)
-	return &PubKeyECDSA{
-		alg:      a,
-		goPubKey: goecdsaPk,
-	}, nil
+	// non-NIST curves
+	if a.algo == ECDSA_SECp256k1 {
+		return a.rawDecodePublicKey(der)
+	}
+	return nil, cryptoError{"The curve is not supported."}
 }
 
 // PrKeyECDSA is the private key of ECDSA, it implements PrivateKey
@@ -128,8 +183,29 @@ func (sk *PrKeyECDSA) PublicKey() PublicKey {
 	}
 }
 
+func (sk *PrKeyECDSA) rawEncode() ([]byte, error) {
+	skBytes := sk.goPrKey.D.Bytes()
+	Nlen := bitsToBytes((sk.alg.curve.Params().N).BitLen())
+	skEncoded := make([]byte, Nlen)
+	// pad sk with zeroes
+	i := 0
+	for ; i < Nlen-len(skBytes); i++ {
+		skEncoded[i] = 0
+	}
+	copy(skEncoded[i:], skBytes)
+	return skEncoded, nil
+}
+
 func (sk *PrKeyECDSA) Encode() ([]byte, error) {
-	return x509.MarshalECPrivateKey(sk.goPrKey)
+	// NIST curves case
+	if sk.Algorithm() == ECDSA_P256 {
+		return x509.MarshalECPrivateKey(sk.goPrKey)
+	}
+	// non-NIST curves
+	if sk.Algorithm() == ECDSA_SECp256k1 {
+		return sk.rawEncode()
+	}
+	return nil, cryptoError{"The curve is not supported."}
 }
 
 // PubKeyECDSA is the public key of ECDSA, it implements PublicKey
@@ -150,6 +226,32 @@ func (pk *PubKeyECDSA) KeySize() int {
 	return 2 * bitsToBytes((pk.goPubKey.Params().P).BitLen())
 }
 
+func (pk *PubKeyECDSA) rawEncode() ([]byte, error) {
+	xBytes := pk.goPubKey.X.Bytes()
+	yBytes := pk.goPubKey.Y.Bytes()
+	Plen := bitsToBytes((pk.alg.curve.Params().P).BitLen())
+	pkEncoded := make([]byte, 2*Plen)
+	// pad the public key coordinates with zeroes
+	i := 0
+	for ; i < Plen-len(xBytes); i++ {
+		pkEncoded[i] = 0
+	}
+	i += copy(pkEncoded[i:], xBytes)
+	for ; i < 2*Plen-len(yBytes); i++ {
+		pkEncoded[i] = 0
+	}
+	copy(pkEncoded[i:], yBytes)
+	return pkEncoded, nil
+}
+
 func (pk *PubKeyECDSA) Encode() ([]byte, error) {
-	return x509.MarshalPKIXPublicKey(pk.goPubKey)
+	// NIST curves case
+	if pk.Algorithm() == ECDSA_P256 {
+		return x509.MarshalPKIXPublicKey(pk.goPubKey)
+	}
+	// non-NIST curves
+	if pk.Algorithm() == ECDSA_SECp256k1 {
+		return pk.rawEncode()
+	}
+	return nil, cryptoError{"The curve is not supported."}
 }
--- crypto/secg.go
+++ crypto/secg.go
@@ -10,6 +10,7 @@ package crypto
 
 import (
 	goec "crypto/elliptic"
+	"encoding/asn1"
 	"math/big"
 )
 
@@ -221,3 +222,5 @@ func secp256k1() *SECCurve {
 	secp256k1Curve.BitSize = 256
 	return secp256k1Curve
 }
+
+var oidNamedCurveSECp256k1 = asn1.ObjectIdentifier{1, 3, 132, 0, 10}
