a5c3e652a306f56bceadaf214de7837f40d6e30e
Jordan Schalm <jordan.schalm@gmail.com>
2019-11-07 14:02:15 -0800

    Add Store interface

---
+++ sdk/emulator/store/store.go
@@ -0,0 +1,21 @@
+package store
+
+import (
+	"crypto"
+
+	"github.com/dapperlabs/flow-go/model/flow"
+)
+
+type Store interface {
+	GetBlockByHash(crypto.Hash) (flow.Block, error)
+	GetBlockByNumber(blockNumber uint64) (flow.Block, error)
+	GetLatestBlock() (flow.Block, error)
+
+	InsertBlock(flow.Block) error
+
+	GetTransaction(crypto.Hash) (flow.Transaction, error)
+	InsertTransaction(flow.Transaction) error
+
+	GetRegistersView(blockNumber uint64) flow.RegistersView
+	SetRegisters(blockNumber uint64, registers flow.Registers) error
+}
