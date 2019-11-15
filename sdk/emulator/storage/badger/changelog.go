794e58f1eac80365027b5d68162a76044b1e82c5
Jordan Schalm <jordan.schalm@gmail.com>
2019-11-14 19:01:18 -0800

    Fix a bad copy-paste

--- sdk/emulator/storage/badger/changelog.go
+++ sdk/emulator/storage/badger/changelog.go
@@ -9,7 +9,7 @@ import (
 // a given block height.
 const notFound uint64 = ^uint64(0)
 
-// An ordered list of Blocks at which a register changed value. This type
+// An ordered list of blocks at which a register changed value. This type
 // implements sort.Interface for efficient searching and sorting.
 //
 // Users should NEVER interact with the backing slice directly, as it must
@@ -44,8 +44,8 @@ func (c changelist) search(n uint64) uint64 {
 	return c.Blocks[foundIndex-1]
 }
 
-// add adds the block number to the Blocks, ensuring the Blocks remains sorted. If
-// n already exists in the Blocks, this is a no-op.
+// add adds the block number to the list, ensuring the list remains sorted. If
+// n already exists in the list, this is a no-op.
 func (c *changelist) add(n uint64) {
 	if n == notFound {
 		return
@@ -61,7 +61,7 @@ func (c *changelist) add(n uint64) {
 }
 
 // The changelog describes the change history of each register in a ledger.
-// For each register, the changelog contains a Blocks of all the block numbers at
+// For each register, the changelog contains a list of all the block numbers at
 // which the register's value changed. This enables quick lookups of the latest
 // register state change for a given block.
 //
@@ -71,7 +71,7 @@ type changelog struct {
 	// Maps register IDs to an ordered slice of all the block numbers at which
 	// the register value changed.
 	registers map[string]changelist
-	// Guards the register Blocks from concurrent writes.
+	// Guards the register list from concurrent writes.
 	sync.RWMutex
 }
 
