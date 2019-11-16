a45247ff345de7cde0509a8233f6794573b0722a
Leo Zhang (zhangchiqing) <zhangchiqing@gmail.com>
2019-11-15 16:10:21 -0800

    simplify Protocol

--- network/gossip/protocol.go
+++ network/gossip/protocol.go
@@ -2,17 +2,7 @@ package gossip
 
 import (
 	"context"
-
-	"github.com/dapperlabs/flow-go/proto/gossip/messages"
 )
-
-// ProtocolMessage represents the data received from a peer or to be delivered to a peer
-type ProtocolMessage struct {
-	// Message is the message data
-	Message messages.GossipMessage
-	// Broadcast indicate whether the receiver will need to broadcast it
-	Broadcast bool
-}
 
 // Protocol represents the protocol layer that provides the network implementation for establishing
 // connections with peers, starting a service for accepting incoming connection request, as well
@@ -20,9 +10,10 @@ type ProtocolMessage struct {
 type Protocol interface {
 	// Handle accepts a callback function which will be called when the server receives a message from
 	// a peer.
-	Handle(onReceive func(sender string, msg ProtocolMessage)) error
-	// Start starts the server with a given address and port
-	Start(address string, port string) error
+	Handle(onReceive OnReceiveCallback) error
+	// Start starts the server with a given address. The address should contain the port info.
+	// For instance: "198.51.100.1:80"
+	Start(address string) error
 	// Stop stops the server and drops all connections
 	Stop() error
 
@@ -30,12 +21,15 @@ type Protocol interface {
 	Dial(address string) (Connection, error)
 }
 
+// OnReceiveCallback is the callback type for receiving incoming messages from a peer
+type OnReceiveCallback func(sender string, msg []byte)
+
 // Connection represents a client connection to a peer through which a node can send messages
 // to a peer.
 type Connection interface {
 	// Send sends a message to the peer through the alive connection.
-	Send(ctx context.Context, msg *ProtocolMessage) error
-	// onClosed takes a callback to be notified when the peer closed the connection
+	Send(ctx context.Context, msg []byte) error
+	// OnClosed takes a callback to be notified when the peer closed the connection
 	OnClosed(func()) error
 	// Close close the connection
 	Close() error
