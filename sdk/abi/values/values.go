75d7433401f3c0183e2737e7a8d272d860d32889
Peter Siemens <peterjsiemens@gmail.com>
2019-11-27 23:39:55 -0800

    Add types to all values

--- sdk/abi/values/values.go
+++ sdk/abi/values/values.go
@@ -9,233 +9,264 @@ import (
 type Value interface {
 	isValue()
 	Type() types.Type
-	setType(p types.Type)
 }
 
-// WithType annotates a value with a type.
-func WithType(t types.Type, v Value) Value {
-	v.setType(t)
+type TypeAssignableValue interface {
+	Value
+	SetType(types.Type)
+}
+
+type typeAssignableValue struct {
+	typ types.Type
+}
+
+func (typeAssignableValue) isValue() {}
+
+func (v typeAssignableValue) Type() types.Type {
+	return v.typ
+}
+
+func (v typeAssignableValue) SetType(typ types.Type) {
+	v.typ = typ
+}
+
+// WithType assigns a type to a type-assignable value.
+func WithType(typ types.Type, v TypeAssignableValue) Value {
+	v.SetType(typ)
 	return v
 }
 
-type baseValue struct {
-	valueType types.Type
+type Void struct{}
+
+func NewVoid() Void {
+	return Void{}
 }
 
-func (baseValue) isValue() {}
+func (Void) isValue() {}
 
-func (v baseValue) Type() types.Type { return v.valueType }
-func (v baseValue) setType(t types.Type) {
-	v.valueType = t
+func (Void) Type() types.Type {
+	return types.Void{}
 }
 
-type Void struct{ baseValue }
+type Nil struct{}
 
-type Nil struct{ baseValue }
-
-type Bool struct {
-	baseValue
-	val bool
+func NewNil() Nil {
+	return Nil{}
 }
+
+func (Nil) isValue() {}
+
+func (Nil) Type() types.Type {
+	return nil
+}
+
+type Bool bool
 
 func NewBool(b bool) Bool {
-	return Bool{val: b}
+	return Bool(b)
 }
 
-func (v Bool) Bool() bool {
-	return v.val
+func (Bool) isValue() {}
+
+func (Bool) Type() types.Type {
+	return types.Bool{}
 }
 
-type String struct {
-	baseValue
-	val string
-}
+type String string
 
 func NewString(s string) String {
-	return String{val: s}
+	return String(s)
 }
 
-func (v String) String() string {
-	return v.val
+func (String) isValue() {}
+
+func (String) Type() types.Type {
+	return types.String{}
 }
 
-type Bytes struct {
-	baseValue
-	val []byte
-}
+type Bytes []byte
 
 func NewBytes(b []byte) Bytes {
-	return Bytes{val: b}
+	return b
 }
 
-func (v Bytes) Bytes() []byte {
-	return v.val
+func (Bytes) isValue() {}
+
+func (Bytes) Type() types.Type {
+	return types.Bytes{}
+}
+
+const AddressLength = 20
+
+type Address [AddressLength]byte
+
+func NewAddress(b [AddressLength]byte) Address {
+	return b
+}
+
+func NewAddressFromBytes(b []byte) Address {
+	var a Address
+	copy(a[:], b)
+	return a
+}
+
+func (Address) isValue() {}
+
+func (Address) Type() types.Type {
+	return types.Address{}
+}
+
+func (v Address) Bytes() []byte {
+	return v[:]
 }
 
 type Int struct {
-	baseValue
-	val *big.Int
+	Value *big.Int
 }
 
 func NewInt(i int) Int {
-	return Int{
-		val: big.NewInt(int64(i)),
-	}
+	return Int{big.NewInt(int64(i))}
 }
 
 func NewIntFromBig(i *big.Int) Int {
-	return Int{val: i}
+	return Int{i}
+}
+
+func (Int) isValue() {}
+
+func (Int) Type() types.Type {
+	return nil
 }
 
 func (v Int) Int() int {
-	return int(v.val.Int64())
+	return int(v.Value.Int64())
 }
 
 func (v Int) Big() *big.Int {
-	return v.val
+	return v.Value
 }
 
-type Int8 struct {
-	baseValue
-	val int8
-}
+type Int8 int8
 
 func NewInt8(i int8) Int8 {
-	return Int8{val: i}
+	return Int8(i)
 }
 
-func (v Int8) Int8() int8 {
-	return v.val
+func (Int8) isValue() {}
+
+func (Int8) Type() types.Type {
+	return types.Int8{}
 }
 
-type Int16 struct {
-	baseValue
-	val int16
-}
+type Int16 int16
 
 func NewInt16(i int16) Int16 {
-	return Int16{val: i}
+	return Int16(i)
 }
 
-func (v Int16) Int16() int16 {
-	return v.val
+func (Int16) isValue() {}
+
+func (Int16) Type() types.Type {
+	return types.Int16{}
 }
 
-type Int32 struct {
-	baseValue
-	val int32
-}
+type Int32 int32
 
 func NewInt32(i int32) Int32 {
-	return Int32{val: i}
+	return Int32(i)
 }
 
-func (v Int32) Int32() int32 {
-	return v.val
+func (Int32) isValue() {}
+
+func (Int32) Type() types.Type {
+	return types.Int32{}
 }
 
-type Int64 struct {
-	baseValue
-	val int64
-}
+type Int64 int64
 
 func NewInt64(i int64) Int64 {
-	return Int64{val: i}
+	return Int64(i)
 }
 
-func (v Int64) Int64() int64 {
-	return v.val
+func (Int64) isValue() {}
+
+func (Int64) Type() types.Type {
+	return types.Int64{}
 }
 
-type Uint8 struct {
-	baseValue
-	val uint8
-}
+type Uint8 uint8
 
 func NewUint8(i uint8) Uint8 {
-	return Uint8{val: i}
+	return Uint8(i)
 }
 
-func (v Uint8) Uint8() uint8 {
-	return v.val
+func (Uint8) isValue() {}
+
+func (Uint8) Type() types.Type {
+	return types.Uint8{}
 }
 
-type Uint16 struct {
-	baseValue
-	val uint16
-}
+type Uint16 uint16
 
 func NewUint16(i uint16) Uint16 {
-	return Uint16{val: i}
+	return Uint16(i)
 }
 
-func (v Uint16) Uint16() uint16 {
-	return v.val
+func (Uint16) isValue() {}
+
+func (Uint16) Type() types.Type {
+	return types.Uint16{}
 }
 
-type Uint32 struct {
-	baseValue
-	val uint32
-}
+type Uint32 uint32
 
 func NewUint32(i uint32) Uint32 {
-	return Uint32{val: i}
+	return Uint32(i)
 }
 
-func (v Uint32) Uint32() uint32 {
-	return v.val
+func (Uint32) isValue() {}
+
+func (Uint32) Type() types.Type {
+	return types.Uint32{}
 }
 
-type Uint64 struct {
-	baseValue
-	val uint64
-}
+type Uint64 uint64
 
 func NewUint64(i uint64) Uint64 {
-	return Uint64{val: i}
+	return Uint64(i)
 }
 
-func (v Uint64) Uint64() uint64 {
-	return v.val
+func (Uint64) isValue() {}
+
+func (Uint64) Type() types.Type {
+	return types.Uint64{}
 }
 
 type VariableSizedArray struct {
-	baseValue
-	val []Value
+	typeAssignableValue
+	Values []Value
 }
 
 func NewVariableSizedArray(values []Value) VariableSizedArray {
-	return VariableSizedArray{val: values}
-}
-
-func (v VariableSizedArray) Values() []Value {
-	return v.val
+	return VariableSizedArray{Values: values}
 }
 
 type ConstantSizedArray struct {
-	baseValue
-	val []Value
+	typeAssignableValue
+	Values []Value
 }
 
 func NewConstantSizedArray(values []Value) ConstantSizedArray {
-	return ConstantSizedArray{val: values}
-}
-
-func (v ConstantSizedArray) Values() []Value {
-	return v.val
+	return ConstantSizedArray{Values: values}
 }
 
 type Dictionary struct {
-	baseValue
-	pairs []KeyValuePair
+	typeAssignableValue
+	Pairs []KeyValuePair
 }
 
 func NewDictionary(pairs []KeyValuePair) Dictionary {
-	return Dictionary{pairs: pairs}
-}
-
-func (v Dictionary) Pairs() []KeyValuePair {
-	return v.pairs
+	return Dictionary{Pairs: pairs}
 }
 
 type KeyValuePair struct {
@@ -243,43 +274,20 @@ type KeyValuePair struct {
 	Value Value
 }
 
-type Composite struct {
-	baseValue
-	fields []Value
-}
-
-func NewComposite(fields []Value) Composite {
-	return Composite{fields: fields}
-}
-
-func (v Composite) Fields() []Value {
-	return v.fields
-}
-
 type Event struct {
-	baseValue
-	fields []Value
+	typeAssignableValue
+	Fields []Value
 }
 
 func NewEvent(fields []Value) Event {
-	return Event{fields: fields}
+	return Event{Fields: fields}
 }
 
-func (v Event) Fields() []Value {
-	return v.fields
+type Composite struct {
+	typeAssignableValue
+	Fields []Value
 }
 
-type Address struct {
-	baseValue
-	val [20]byte
-}
-
-func (v Address) Bytes() []byte {
-	return v.val[:]
-}
-
-func NewAddress(b []byte) Address {
-	var a [20]byte
-	copy(a[:], b)
-	return Address{val: a}
+func NewComposite(fields []Value) Composite {
+	return Composite{Fields: fields}
 }
