64c71935bfc042624daf13684167d3c1a5d2d222
Jordan Schalm <jordan@dapperlabs.com>
2019-12-12 13:20:18 -0800

    Add storage-level test

--- storage/badger/operation/transactions.go
+++ storage/badger/operation/transactions.go
@@ -6,10 +6,12 @@ import (
 	"github.com/dgraph-io/badger/v2"
 )
 
+// InsertTransaction inserts a transaction keyed by transaction hash.
 func InsertTransaction(hash crypto.Hash, tx *flow.Transaction) func(*badger.Txn) error {
 	return insert(makePrefix(codeTransaction, hash), tx)
 }
 
+// RetrieveTransactions retrieves a transaction by hash.
 func RetrieveTransaction(hash crypto.Hash, tx *flow.Transaction) func(*badger.Txn) error {
 	return retrieve(makePrefix(codeTransaction, hash), tx)
 }
---
+++ storage/badger/transactions_test.go
@@ -0,0 +1,32 @@
+package badger_test
+
+import (
+	"fmt"
+	"math/rand"
+	"os"
+	"path/filepath"
+	"testing"
+
+	storage "github.com/dapperlabs/flow-go/storage/badger"
+	"github.com/dapperlabs/flow-go/utils/unittest"
+	"github.com/dgraph-io/badger/v2"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+)
+
+func TestTransactionsInsertRetrieve(t *testing.T) {
+	dir := filepath.Join(os.TempDir(), fmt.Sprintf("flow-test-db-%d", rand.Uint64()))
+	db, err := badger.Open(badger.DefaultOptions(dir).WithLogger(nil))
+	require.Nil(t, err)
+
+	store := storage.NewTransactions(db)
+
+	expected := unittest.TransactionFixture()
+	err = store.Insert(&expected)
+	require.Nil(t, err)
+
+	actual, err := store.ByHash(expected.Hash())
+	require.Nil(t, err)
+
+	assert.Equal(t, &expected, actual)
+}
