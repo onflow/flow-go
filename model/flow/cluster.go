111a59884e3e98c9f227a3d7531ccd18fd9d4cd8
Jordan Schalm <jordan@dapperlabs.com>
2019-12-13 17:12:20 -0800

    Add implementation and tests for cluster routing

--- model/flow/cluster.go
+++ model/flow/cluster.go
@@ -1,13 +1,11 @@
-package collection
-
-import "github.com/dapperlabs/flow-go/model/flow"
+package flow
 
 // ClusterID is the unique ID of a given collection node cluster.
 type ClusterID int
 
 // ClusterList is a set of clusters, keyed by ID. Each cluster must contain at
 // least one node. All nodes are collection nodes.
-type ClusterList []flow.IdentityList
+type ClusterList []IdentityList
 
 // Size returns the number of clusters.
 func (cl ClusterList) Size() int {
@@ -16,10 +14,10 @@ func (cl ClusterList) Size() int {
 
 // Get returns the cluster with the given ID. Returns an empty cluster if
 // no cluster with the given ID exists.
-func (cl ClusterList) Get(id ClusterID) flow.IdentityList {
+func (cl ClusterList) Get(id ClusterID) IdentityList {
 
 	if id < 0 || int(id) >= len(cl) {
-		return flow.IdentityList{}
+		return IdentityList{}
 	}
 
 	return cl[id]
@@ -29,7 +27,7 @@ func (cl ClusterList) Get(id ClusterID) flow.IdentityList {
 // in any cluster. Otherwise returns -1.
 //
 // TODO this is O(n), optimize if # of collection nodes gets large
-func (cl ClusterList) ClusterIDFor(nodeID flow.Identifier) ClusterID {
+func (cl ClusterList) ClusterIDFor(nodeID Identifier) ClusterID {
 
 	for clusterID, cluster := range cl {
 		for _, identity := range cluster {
