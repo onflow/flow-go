28a787ec1498daa03c43e5b6df5b6283d8e8d6cc
Jordan Schalm <jordan@dapperlabs.com>
2019-12-13 17:07:12 -0800

    Add testutils package for engine tests
    The idea is this package will contain mocked nodes of all types. Since
    soon tests will involve multiple node types, it makes sense for the
    different node type mocks to be accessible for testing in different
    packages.

---
+++ engine/testutil/cluster.go
@@ -0,0 +1,26 @@
+package testutil
+
+import (
+	"fmt"
+
+	"github.com/dapperlabs/flow-go/model/flow"
+	"github.com/dapperlabs/flow-go/protocol"
+	"github.com/dapperlabs/flow-go/utils/unittest"
+)
+
+// TransactionForCluster generates a transaction that will be assigned to the
+// target cluster ID.
+func TransactionForCluster(nClusters int, target flow.ClusterID) *flow.Transaction {
+	tx := unittest.TransactionFixture()
+
+	// Bound to avoid infinite loop in case the routing algorithm is broken
+	for i := 0; i < 10000; i++ {
+		tx.Nonce++
+		id := protocol.Route(nClusters, tx.Hash())
+		if id == target {
+			return &tx
+		}
+	}
+
+	panic(fmt.Sprintf("unable to find transaction for target (%d) with %d clusters", target, nClusters))
+}
--- model/flow/cluster.go
+++ model/flow/cluster.go
@@ -1,8 +1,43 @@
-package flow
+package collection
+
+import "github.com/dapperlabs/flow-go/model/flow"
 
 // ClusterID is the unique ID of a given collection node cluster.
-type ClusterID uint
+type ClusterID int
 
 // ClusterList is a set of clusters, keyed by ID. Each cluster must contain at
 // least one node. All nodes are collection nodes.
-type ClusterList []IdentityList
+type ClusterList []flow.IdentityList
+
+// Size returns the number of clusters.
+func (cl ClusterList) Size() int {
+	return len(cl)
+}
+
+// Get returns the cluster with the given ID. Returns an empty cluster if
+// no cluster with the given ID exists.
+func (cl ClusterList) Get(id ClusterID) flow.IdentityList {
+
+	if id < 0 || int(id) >= len(cl) {
+		return flow.IdentityList{}
+	}
+
+	return cl[id]
+}
+
+// ClusterIDFor returns the cluster ID for the given node ID, if the node exists
+// in any cluster. Otherwise returns -1.
+//
+// TODO this is O(n), optimize if # of collection nodes gets large
+func (cl ClusterList) ClusterIDFor(nodeID flow.Identifier) ClusterID {
+
+	for clusterID, cluster := range cl {
+		for _, identity := range cluster {
+			if identity.NodeID == nodeID {
+				return ClusterID(clusterID)
+			}
+		}
+	}
+
+	return -1
+}
