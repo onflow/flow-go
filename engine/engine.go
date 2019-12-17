e8dba7c99c68e4400b8fce7d317995923ca298c8
Jordan Schalm <jordan@dapperlabs.com>
2019-12-17 15:24:36 -0800

    Change engine interface to just include Submit
    This is the same interface as network.Engine, but without origin ID
    (since it's always the same). I've re-added the error return because it
    is useful for this particular use-case and I think it will be useful in
    the future as well. The caller can always log the error if necessary, or
    the engine can expose another method that logs errors if that behaviour
    is desired.

--- engine/engine.go
+++ engine/engine.go
@@ -1,21 +1,9 @@
 package engine
 
-import "github.com/dapperlabs/flow-go/network"
-
-// Engine defines the interface for engines. This extends `network.Engine`
-// with methods used only within the engine's process.
+// Engine defines the internal interface for engines. This is the interface
+// used by other components in the same node as the engine.
 type Engine interface {
-	network.Engine
-
-	// Submit submits a local event to the engine. Any errors should be
-	// logged internally.
-	Submit(event interface{})
-
-	// Ready returns a channel that is closed when the engine has fully
-	// started up.
-	Ready() <-chan struct{}
-
-	// Done returns a channel that is closed when the engine has fully stopped.
-	// Calling Done has the effect of starting the shut-down process.
-	Done() <-chan struct{}
+	// Submit submits a local event to the engine. It is identical to
+	// `network#Engine.Process` but sets the origin ID to the node ID.
+	Submit(event interface{}) error
 }
