371c024ac9cc8803b66c10a4d9f59a2a38805f1d
Peter Siemens <peterjsiemens@gmail.com>
2019-12-24 15:12:25 -0600

    Implement Computer.ExecuteTransaction

--- engine/execution/execution/components/computer/computer.go
+++ engine/execution/execution/components/computer/computer.go
@@ -1,8 +1,10 @@
 package computer
 
 import (
+	"errors"
 	"fmt"
 
+	"github.com/dapperlabs/flow-go/crypto"
 	"github.com/dapperlabs/flow-go/engine/execution/execution/modules/context"
 	"github.com/dapperlabs/flow-go/language/runtime"
 	"github.com/dapperlabs/flow-go/model/flow"
@@ -10,10 +12,9 @@ import (
 
 // A TransactionResult is the result of executing a transaction.
 type TransactionResult struct {
-	TxID      flow.Identifier
-	Succeeded bool
-	Events    []*flow.Event
-	GasUsed   uint64
+	TxHash  crypto.Hash
+	Error   error
+	GasUsed uint64
 }
 
 // A Computer uses the Cadence runtime to compute transaction results.
@@ -30,7 +31,28 @@ func New(runtime runtime.Runtime, contextProvider context.Provider) *Computer {
 	}
 }
 
-// ComputeTransaction computes the result of a transaction.
-func (c *Computer) ComputeTransaction(tx *flow.Transaction) (*TransactionResult, error) {
-	return nil, nil
+// ExecuteTransaction computes the result of a transaction.
+func (c *Computer) ExecuteTransaction(tx *flow.Transaction) (*TransactionResult, error) {
+	ctx := c.contextProvider.NewTransactionContext(tx)
+
+	location := runtime.TransactionLocation(tx.Hash())
+
+	err := c.runtime.ExecuteTransaction(tx.Script, ctx, location)
+	if err != nil {
+		if errors.As(err, &runtime.Error{}) {
+			// runtime errors occur when the execution reverts
+			return &TransactionResult{
+				TxHash: tx.Hash(),
+				Error:  err,
+			}, nil
+		}
+
+		// other errors are unexpected and should be treated as fatal
+		return nil, fmt.Errorf("failed to execute transaction: %w", err)
+	}
+
+	return &TransactionResult{
+		TxHash: tx.Hash(),
+		Error:  nil,
+	}, nil
 }
