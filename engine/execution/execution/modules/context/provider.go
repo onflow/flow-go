866776e0d9707ed512a7d15371dab8f0b39503bf
Peter Siemens <peterjsiemens@gmail.com>
2020-01-02 18:08:33 -0800

    Remove sdk/abi/values from transaction context

--- engine/execution/execution/modules/context/provider.go
+++ engine/execution/execution/modules/context/provider.go
@@ -2,8 +2,8 @@ package context
 
 import (
 	"github.com/dapperlabs/flow-go/engine/execution/execution/modules/ledger"
+	"github.com/dapperlabs/flow-go/language/runtime"
 	"github.com/dapperlabs/flow-go/model/flow"
-	"github.com/dapperlabs/flow-go/sdk/abi/values"
 )
 
 // NewProvider returns a new context provider.
@@ -14,9 +14,9 @@ func NewProvider() Provider {
 type provider struct{}
 
 func (p *provider) NewTransactionContext(tx *flow.Transaction, ledger *ledger.View) TransactionContext {
-	signingAccounts := make([]values.Address, len(tx.ScriptAccounts))
+	signingAccounts := make([]runtime.Address, len(tx.ScriptAccounts))
 	for i, addr := range tx.ScriptAccounts {
-		signingAccounts[i] = values.Address(addr)
+		signingAccounts[i] = runtime.Address(addr)
 	}
 
 	return &transactionContext{
