7f0ccb928950cd82060d524962e4602825b3eb4e
Leo Zhang (zhangchiqing) <zhangchiqing@gmail.com>
2020-01-07 15:27:51 -0800

    rename to NetworkSender

--- engine/consensus/hotstuff/network_sender.go
+++ engine/consensus/hotstuff/network_sender.go
@@ -2,7 +2,7 @@ package hotstuff
 
 import "github.com/dapperlabs/flow-go/engine/consensus/hotstuff/types"
 
-type Network interface {
+type NetworkSender interface {
 	SendVote(vote *types.Vote, identityIdx uint32)
 	BroadcastProposal(b *types.BlockProposal)
 	RespondBlockProposalRequest(req *types.BlockProposalRequest, b *types.BlockProposal)
