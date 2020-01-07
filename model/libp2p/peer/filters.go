2c82798ca3b1e41a1e34f695b25ddca6743f604a
Yahya <yahya@dapperlabs.com>
2020-01-06 18:12:18 -0800

    Adds filters for peers

--- model/libp2p/peer/filters.go
+++ model/libp2p/peer/filters.go
@@ -5,20 +5,20 @@ package peer
 import (
 	"encoding/hex"
 
-	"github.com/dapperlabs/flow-go/network/gossip/libp2p"
+	libp2p2 "github.com/dapperlabs/flow-go/model/libp2p"
 )
 
 // Not negates the wrapped filter.
-func Not(filter libp2p.PeerFilter) libp2p.PeerFilter {
-	return func(p *libp2p.Peer) bool {
+func Not(filter libp2p2.PeerFilter) libp2p2.PeerFilter {
+	return func(p *libp2p2.Peer) bool {
 		return !filter(p)
 	}
 }
 
 // HasSeen filters for peers that have seen the event with the given ID.
-func HasSeen(eventID []byte) libp2p.PeerFilter {
+func HasSeen(eventID []byte) libp2p2.PeerFilter {
 	key := hex.EncodeToString(eventID)
-	return func(p *libp2p.Peer) bool {
+	return func(p *libp2p2.Peer) bool {
 		_, ok := p.Seen[key]
 		return ok
 	}
@@ -26,13 +26,13 @@ func HasSeen(eventID []byte) libp2p.PeerFilter {
 
 // HasSeenAllOf filters for peers that have seen all of the events with the given
 // IDs.
-func HasSeenAllOf(eventIDs ...[]byte) libp2p.PeerFilter {
+func HasSeenAllOf(eventIDs ...[]byte) libp2p2.PeerFilter {
 	keys := make([]string, 0, len(eventIDs))
 	for _, eventID := range eventIDs {
 		key := hex.EncodeToString(eventID)
 		keys = append(keys, key)
 	}
-	return func(p *libp2p.Peer) bool {
+	return func(p *libp2p2.Peer) bool {
 		for _, key := range keys {
 			_, ok := p.Seen[key]
 			if !ok {
@@ -45,13 +45,13 @@ func HasSeenAllOf(eventIDs ...[]byte) libp2p.PeerFilter {
 
 // HasSeenOneOf filters for peers that have seen at least one of the events with the
 // given IDs.
-func HasSeenOneOf(eventIDs ...[]byte) libp2p.PeerFilter {
+func HasSeenOneOf(eventIDs ...[]byte) libp2p2.PeerFilter {
 	keys := make([]string, 0, len(eventIDs))
 	for _, eventID := range eventIDs {
 		key := hex.EncodeToString(eventID)
 		keys = append(keys, key)
 	}
-	return func(i *libp2p.Peer) bool {
+	return func(i *libp2p2.Peer) bool {
 		for _, key := range keys {
 			_, ok := i.Seen[key]
 			if ok {
