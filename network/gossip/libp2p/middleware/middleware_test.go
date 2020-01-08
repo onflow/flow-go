10188b5ca659b2da96a128bce1906f2a1aad9a04
Yahya <yahya@dapperlabs.com>
2020-01-08 15:42:41 -0800

    Converts middleware tests into test suite

--- network/gossip/libp2p/middleware/middleware_test.go
+++ network/gossip/libp2p/middleware/middleware_test.go
@@ -10,17 +10,36 @@ import (
 	"github.com/rs/zerolog/log"
 	mockery "github.com/stretchr/testify/mock"
 	"github.com/stretchr/testify/require"
+	"github.com/stretchr/testify/suite"
 
 	"github.com/dapperlabs/flow-go/model/flow"
 	"github.com/dapperlabs/flow-go/network/codec/json"
 	"github.com/dapperlabs/flow-go/network/gossip/libp2p/mock"
 )
 
-func TestSendAndReceive(t *testing.T) {
+type MiddlewareTestSuit struct {
+	suite.Suite
+	size int           // used to determine number of middlewares under test
+	mws  []*Middleware // used to keep track of middlewares under test
+	ids  []flow.Identifier
+}
+
+// TestMiddlewareTestSuit runs all the test methods in this test suit
+func TestMiddlewareTestSuit(t *testing.T) {
+	suite.Run(t, new(MiddlewareTestSuit))
+}
+
+// SetupTest initiates the test setups prior to each test
+func (m *MiddlewareTestSuit) SetupTest() {
+	m.size = 2 // operates on two middlewares
+	m.ids, m.mws = m.createAndStartMiddleWares(m.size)
+}
+
+func (m *MiddlewareTestSuit) TestSendAndReceive() {
 	count := 2
-	ids, mws := createAndStartMiddleWares(t, count)
-	require.Len(t, ids, count)
-	require.Len(t, mws, count)
+	ids, mws := m.createAndStartMiddleWares(count)
+	require.Len(m.Suite.T(), ids, count)
+	require.Len(m.Suite.T(), mws, count)
 	msg := []byte("hello")
 	time.Sleep(4 * time.Second)
 	mws[0].Send(ids[count-1], msg)
@@ -30,25 +49,31 @@ func TestSendAndReceive(t *testing.T) {
 	time.Sleep(time.Minute * 10)
 }
 
-func createAndStartMiddleWares(t *testing.T, count int) ([]flow.Identifier, []*Middleware) {
+func (m *MiddlewareTestSuit) createAndStartMiddleWares(count int) ([]flow.Identifier, []*Middleware) {
 	var mws []*Middleware
 	var ids []flow.Identifier
-	for i := 0; i < count; i++ {
 
+	// creates the middlewares
+	for i := 0; i < count; i++ {
+		// generating ids of the nodes
+		// as [32]byte{(i+1),0,...,0}
 		var target [32]byte
 		target[0] = byte(i + 1)
 		targetID := flow.Identifier(target)
 		ids = append(ids, targetID)
 
+		// generates logger and coder of the nodes
 		logger := log.Output(zerolog.ConsoleWriter{Out: os.Stderr}).With().Caller().Logger()
 		codec := json.NewCodec()
 
+		// creates new middleware
 		mw, err := New(logger, codec, uint(count-1), "0.0.0.0:0", targetID)
-		require.NoError(t, err)
+		require.NoError(m.Suite.T(), err)
 
 		mws = append(mws, mw)
 	}
 
+	// mocks an overlay (i.e., network) for each middleware
 	var overlays []*mock.Overlay
 	for i := 0; i < count; i++ {
 		overlay := &mock.Overlay{}
@@ -57,20 +82,26 @@ func createAndStartMiddleWares(t *testing.T, count int) ([]flow.Identifier, []*M
 			target = 0
 		}
 		ip, port := mws[target].libP2PNode.GetIPPort()
-		flowID := flow.Identity{NodeID: ids[target], Address: fmt.Sprintf("%s:%s", ip, port), Role: flow.RoleCollection}
-		overlay.On("Identity").Return(flowID, nil)
-		// overlay.On("Handshake", mockery.Anything).Return(flowID.NodeID, nil)
-		overlay.On("Receive", mockery.Anything).Return(nil).Once().Run(func(args mockery.Arguments) {
-			fmt.Printf(" Recd: %s from %s", args[1], args[0])
-		})
+
+		// mocks an identity
+		flowID := flow.Identity{
+			NodeID:  ids[target],
+			Address: fmt.Sprintf("%s:%s", ip, port),
+			Role:    flow.RoleCollection,
+		}
+
+		// mocks Overlay.Identity for middleware.Overlay.Identity()
+		overlay.On("Identity").Return(flowID, nil).Once()
+
+		// mocks Overlay.Receive for  middleware.Overlay.Receive(*nodeID, payload)
+		overlay.On("Receive", mockery.Anything).Return(nil).Once()
 		overlays = append(overlays, overlay)
 	}
 
+	// starting the middleware
 	for i := 0; i < count; i++ {
 		mws[i].Start(overlays[i])
 		time.Sleep(1 * time.Second)
 	}
-
 	return ids, mws
-
 }
