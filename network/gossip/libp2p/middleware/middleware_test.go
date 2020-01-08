ad0e074bd0965ff4d90e3dda574a653925527665
Vishal Changrani <vishal.changrani@dapperlabs.com>
2020-01-07 17:01:30 -0800

    adding exists

--- network/gossip/libp2p/middleware/middleware_test.go
+++ network/gossip/libp2p/middleware/middleware_test.go
@@ -11,6 +11,7 @@ import (
 	"github.com/stretchr/testify/require"
 	"os"
 	"testing"
+	"time"
 )
 
 func TestSendAndReceive(t *testing.T) {
@@ -19,7 +20,7 @@ func TestSendAndReceive(t *testing.T) {
 	require.Len(t, ids, count)
 	require.Len(t, mws, count)
 	msg := json.Envelope{ Code: json.CodeRequest, Data: []byte(`{"key": "hello", "value": "world"}`)}
-	//time.Sleep(10 * time.Minute)
+	time.Sleep(10 * time.Minute)
 	mws[0].Send(ids[count-1], msg)
 }
 
