9268f116cd58e5133e653f399a600aa0050110aa
Vishal Changrani <vishal.changrani@dapperlabs.com>
2020-01-09 14:27:38 -0800

    fixing middleware test

--- network/gossip/libp2p/middleware/middleware_test.go
+++ network/gossip/libp2p/middleware/middleware_test.go
@@ -6,6 +6,8 @@ import (
 	"testing"
 	"time"
 
+	"github.com/stretchr/testify/assert"
+
 	"github.com/rs/zerolog"
 	"github.com/rs/zerolog/log"
 	mockery "github.com/stretchr/testify/mock"
@@ -33,11 +35,11 @@ func TestMiddlewareTestSuit(t *testing.T) {
 // SetupTest initiates the test setups prior to each test
 func (m *MiddlewareTestSuit) SetupTest() {
 	m.size = 2 // operates on two middlewares
-	m.ids, m.mws = m.createAndStartMiddleWares(m.size)
+	// create the middlewares
+	m.ids, m.mws = m.createMiddleWares(m.size)
 	require.Len(m.Suite.T(), m.ids, m.size)
 	require.Len(m.Suite.T(), m.mws, m.size)
-
-	// starts middlewares
+	// starts the middlewares
 	m.StartMiddlewares()
 }
 
@@ -59,7 +61,7 @@ func (m *MiddlewareTestSuit) StartMiddlewares() {
 		}
 
 		// mocks Overlay.Identity for middleware.Overlay.Identity()
-		m.ov[i].On("Identity").Return(flowID, nil).Once()
+		m.ov[i].On("Identity").Return(flowID, nil)
 
 	}
 
@@ -75,24 +77,31 @@ func (m *MiddlewareTestSuit) StartMiddlewares() {
 }
 
 func (m *MiddlewareTestSuit) TestSendOneMessage() {
+
+	ch := make(chan struct{})
 	// mocks Overlay.Receive for  middleware.Overlay.Receive(*nodeID, payload)
-	m.ov[m.size-1].On("Receive", mockery.Anything, mockery.Anything).Return(nil).Once()
+	m.ov[m.size-1].On("Receive", mockery.Anything, mockery.Anything).Return(nil).Once().
+		Run(func(args mockery.Arguments) {
+			ch <- struct{}{}
+		})
 
 	msg := []byte("hello")
 	err := m.mws[0].Send(m.ids[m.size-1], msg)
 	require.NoError(m.Suite.T(), err)
 
-	//
-	//err = m.mws[m.size-1].Send(m.ids[0], msg)
-	//require.NoError(m.Suite.T(), err)
+	select {
+	case <-ch:
+	case <-time.After(3 * time.Second):
+		assert.Fail(m.T(), "peer 1 failed to send a message to peer 2")
+	}
 
 	// evaluates the mock calls
-	for i := 0; i < m.size; i++ {
+	for i := 1; i < m.size; i++ {
 		m.ov[i].AssertExpectations(m.T())
 	}
 }
 
-func (m *MiddlewareTestSuit) createAndStartMiddleWares(count int) ([]flow.Identifier, []*Middleware) {
+func (m *MiddlewareTestSuit) createMiddleWares(count int) ([]flow.Identifier, []*Middleware) {
 	var mws []*Middleware
 	var ids []flow.Identifier
 
@@ -116,14 +125,9 @@ func (m *MiddlewareTestSuit) createAndStartMiddleWares(count int) ([]flow.Identi
 		mws = append(mws, mw)
 	}
 
-	// mocks an overlay (i.e., network) for each middleware
+	// create the mock overlay (i.e., network) for each middleware
 	for i := 0; i < count; i++ {
 		overlay := &mock.Overlay{}
-		//// mocks Overlay.Identity for middleware.Overlay.Identity()
-		//overlay.On("Identity").Return(flowID, nil).Once()
-		//
-		//// mocks Overlay.Receive for  middleware.Overlay.Receive(*nodeID, payload)
-		//overlay.On("Receive", mockery.Anything).Return(nil).Once()
 		m.ov = append(m.ov, overlay)
 	}
 
