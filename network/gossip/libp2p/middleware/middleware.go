b3c28efc930a44c9384c2ca57d5d6a2a2fe7d11b
Vishal Changrani <vishal.changrani@dapperlabs.com>
2020-01-10 14:22:51 -0800

    implementation of the overlay network, middleware, connection and related tests for libp2p (co-author: Yahya)

---
+++ network/gossip/libp2p/middleware/middleware.go
@@ -0,0 +1,271 @@
+// (c) 2019 Dapper Labs - ALL RIGHTS RESERVED
+
+package middleware
+
+import (
+	"context"
+	"fmt"
+	"net"
+	"sync"
+	"time"
+
+	libp2pnetwork "github.com/libp2p/go-libp2p-core/network"
+	"github.com/pkg/errors"
+	"github.com/rs/zerolog"
+
+	"github.com/dapperlabs/flow-go/model/flow"
+	"github.com/dapperlabs/flow-go/network"
+	"github.com/dapperlabs/flow-go/network/gossip/libp2p"
+)
+
+// Middleware handles the input & output on the direct connections we have to
+// our neighbours on the peer-to-peer network.
+type Middleware struct {
+	sync.Mutex
+	log        zerolog.Logger
+	codec      network.Codec
+	ov         libp2p.Overlay
+	slots      chan struct{} // semaphore for outgoing connection slots
+	cc         *ConnectionCache
+	wg         *sync.WaitGroup
+	libP2PNode *libp2p.P2PNode
+	stop       chan struct{}
+	me         flow.Identifier
+}
+
+// New creates a new middleware instance with the given config and using the
+// given codec to encode/decode messages to our peers.
+func New(log zerolog.Logger, codec network.Codec, conns uint, address string, flowID flow.Identifier) (*Middleware, error) {
+	ip, port, err := net.SplitHostPort(address)
+	if err != nil {
+		return nil, err
+	}
+
+	nodeAddress := libp2p.NodeAddress{Name: flowID.String(), IP: ip, Port: port}
+	p2p := &libp2p.P2PNode{}
+
+	// create the node entity and inject dependencies & config
+	m := &Middleware{
+		log:        log,
+		codec:      codec,
+		slots:      make(chan struct{}, conns),
+		cc:         NewConnectionCache(),
+		libP2PNode: p2p,
+		wg:         &sync.WaitGroup{},
+		stop:       make(chan struct{}),
+		me:         flowID,
+	}
+
+	// Start the libp2p node
+	err = p2p.Start(context.Background(), nodeAddress, log, m.handleIncomingStream)
+
+	return m, err
+}
+
+// Start will start the middleware.
+func (m *Middleware) Start(ov libp2p.Overlay) {
+	m.ov = ov
+	m.wg.Add(1)
+	go m.rotate()
+}
+
+// Stop will end the execution of the middleware and wait for it to end.
+func (m *Middleware) Stop() {
+	close(m.stop)
+	m.libP2PNode.Stop()
+	for _, conn := range m.cc.GetAll() {
+		conn.stop()
+	}
+	m.wg.Wait()
+}
+
+// Send will try to send the given message to the given peer.
+func (m *Middleware) Send(nodeID flow.Identifier, msg interface{}) error {
+	m.Lock()
+	defer m.Unlock()
+
+	// get the conn from the conn cache
+	conn, ok := m.cc.Get(nodeID)
+	if !ok {
+		return errors.Errorf("connection not found (node_id: %s)", nodeID)
+	}
+
+	// check if the peer is still running
+	select {
+	case <-conn.done:
+		return errors.Errorf("connection already closed (node_id: %s)", nodeID)
+	default:
+	}
+
+	pmsg := m.createOutboundMessage(msg)
+
+	// whichever comes first, sending the message or ending the provided context
+	select {
+	case <-conn.done:
+		return errors.Errorf("connection has closed (node_id: %s)", nodeID)
+	case conn.outbound <- pmsg:
+		return nil
+	}
+}
+
+func (m *Middleware) createOutboundMessage(msg interface{}) *libp2p.Message {
+	// Compose the message payload
+	message := &libp2p.Message{
+		SenderID: m.me[:],
+		Event:    msg.([]byte),
+	}
+	return message
+}
+
+func (m *Middleware) createInboundMessage(msg *libp2p.Message) (*flow.Identifier, interface{}, error) {
+
+	// Extract sender id
+	if len(msg.SenderID) < 32 {
+		m.log.Debug().
+			Bytes("sender", msg.SenderID).
+			Msg(" invalid sender id")
+		err := fmt.Errorf("invalid sender id")
+		return nil, nil, err
+	}
+	var senderID [32]byte
+	copy(senderID[:], msg.SenderID)
+	var id flow.Identifier
+	id = senderID
+	return &id, msg.Event, nil
+}
+
+// rotate periodically creates connection to peers
+func (m *Middleware) rotate() {
+	defer m.wg.Done()
+
+Loop:
+	for {
+		select {
+
+		// for each free connection slot, we create a new connection
+		case m.slots <- struct{}{}:
+
+			// launch connection attempt
+			m.wg.Add(1)
+			go m.connect()
+
+			// TODO: add proper rate limiter
+			time.Sleep(time.Second)
+
+		case <-m.stop:
+			break Loop
+		}
+	}
+}
+
+// connect creates an outbound stream
+func (m *Middleware) connect() {
+	defer m.wg.Done()
+
+	log := m.log.With().Logger()
+
+	// make sure we free up the connection slot once we drop the peer
+	defer m.release(m.slots)
+
+	// get an identity to connect to
+	flowIdentity, err := m.ov.Identity()
+	if err != nil {
+		log.Error().Err(err).Msg("could not get flow ID")
+		return
+	}
+
+	// If this node is already added, noop
+	if m.cc.Exists(flowIdentity.NodeID) {
+		log.Debug().Str("flowIdentity", flowIdentity.String()).Msg("node already added")
+		return
+	}
+
+	log = log.With().Str("flowIdentity", flowIdentity.String()).Str("address", flowIdentity.Address).Logger()
+
+	ip, port, err := net.SplitHostPort(flowIdentity.Address)
+	if err != nil {
+		log.Error().Err(err).Msg("could not parse address")
+		return
+	}
+	// Create a new NodeAddress
+	nodeAddress := libp2p.NodeAddress{Name: flowIdentity.NodeID.String(), IP: ip, Port: port}
+	// Add it as a peer
+	stream, err := m.libP2PNode.CreateStream(context.Background(), nodeAddress)
+	if err != nil {
+		log.Error().Err(err).Msgf("failed to create stream for %s", nodeAddress.Name)
+		return
+	}
+
+	// make sure we close the stream once the handling is done
+	defer stream.Close()
+
+	clog := m.log.With().
+		Str("local_addr", stream.Conn().LocalPeer().String()).
+		Str("remote_addr", stream.Conn().RemotePeer().String()).
+		Logger()
+
+	// create the write connection handler
+	conn := NewWriteConnection(clog, stream)
+
+	// cache the connection against the node id and remove it when done
+	m.cc.Add(flowIdentity.NodeID, conn)
+	defer m.cc.Remove(flowIdentity.NodeID)
+
+	log.Info().Msg("connection established")
+
+	// kick off the send loop
+	conn.SendLoop()
+}
+
+// handleIncomingStream handles an incoming stream from a remote peer
+// this is a blocking call, so that the deferred resource cleanup happens after
+// we are done handling the connection
+func (m *Middleware) handleIncomingStream(s libp2pnetwork.Stream) {
+	m.wg.Add(1)
+	defer m.wg.Done()
+
+	// make sure we close the connection when we are done handling the peer
+	defer s.Close()
+
+	log := m.log.With().
+		Str("local_addr", s.Conn().LocalPeer().String()).
+		Str("remote_addr", s.Conn().RemotePeer().String()).
+		Logger()
+
+	// initialize the encoder/decoder and create the connection handler
+	conn := NewReadConnection(log, s)
+
+	log.Info().Msg("incoming connection established")
+
+	// start processing messages in the background
+	go conn.ReceiveLoop()
+
+	// process incoming messages for as long as the peer is running
+ProcessLoop:
+	for {
+		select {
+		case <-conn.done:
+			log.Info().Msg("breaking loop")
+			break ProcessLoop
+		case msg := <-conn.inbound:
+			log.Info().Msg("got message")
+			nodeID, payload, err := m.createInboundMessage(msg)
+			if err != nil {
+				log.Error().Err(err).Msg("could not extract payload")
+				continue ProcessLoop
+			}
+			err = m.ov.Receive(*nodeID, payload)
+			if err != nil {
+				log.Error().Err(err).Msg("could not deliver payload")
+				continue ProcessLoop
+			}
+		}
+	}
+
+	log.Info().Msg("connection closed")
+}
+
+// release will release one resource on the given semaphore.
+func (m *Middleware) release(slots chan struct{}) {
+	<-slots
+}
---
+++ network/gossip/libp2p/network.go
@@ -0,0 +1,265 @@
+package libp2p
+
+import (
+	"fmt"
+	"hash"
+	"math/rand"
+	"sync"
+
+	"github.com/dchest/siphash"
+	"github.com/hashicorp/go-multierror"
+	"github.com/pkg/errors"
+	"github.com/rs/zerolog"
+
+	"github.com/dapperlabs/flow-go/model/flow"
+	"github.com/dapperlabs/flow-go/model/flow/identity"
+	networkmodel "github.com/dapperlabs/flow-go/model/libp2p/network"
+	"github.com/dapperlabs/flow-go/model/libp2p/peer"
+	"github.com/dapperlabs/flow-go/module"
+	"github.com/dapperlabs/flow-go/network"
+	"github.com/dapperlabs/flow-go/network/gossip/libp2p/cache"
+	"github.com/dapperlabs/flow-go/network/gossip/libp2p/topology"
+	"github.com/dapperlabs/flow-go/protocol"
+)
+
+// Network represents the overlay network of our peer-to-peer network, including
+// the protocols for handshakes, authentication, gossiping and heartbeats.
+type Network struct {
+	sync.RWMutex
+	logger  zerolog.Logger
+	codec   network.Codec
+	state   protocol.State
+	me      module.Local
+	mw      Middleware
+	cache   *cache.Cache
+	top     *topology.Topology
+	sip     hash.Hash
+	engines map[uint8]network.Engine
+}
+
+// NewNetwork creates a new naive overlay network, using the given middleware to
+// communicate to direct peers, using the given codec for serialization, and
+// using the given state & cache interfaces to track volatile information.
+func NewNetwork(log zerolog.Logger, codec network.Codec, state protocol.State, me module.Local, mw Middleware) (*Network, error) {
+
+	top, err := topology.New()
+	if err != nil {
+		return nil, errors.Wrap(err, "could not initialize topology")
+	}
+
+	ca, err := cache.New()
+	if err != nil {
+		return nil, errors.Wrap(err, "could not initialize cache")
+	}
+
+	o := &Network{
+		logger:  log,
+		codec:   codec,
+		state:   state,
+		me:      me,
+		mw:      mw,
+		top:     top,
+		cache:   ca,
+		sip:     siphash.New([]byte("daflowtrickleson")),
+		engines: make(map[uint8]network.Engine),
+	}
+
+	return o, nil
+}
+
+// Ready returns a channel that will close when the network stack is ready.
+func (n *Network) Ready() <-chan struct{} {
+	ready := make(chan struct{})
+	n.mw.Start(n)
+	go func() {
+		close(ready)
+	}()
+	return ready
+}
+
+// Done returns a channel that will close when shutdown is complete.
+func (n *Network) Done() <-chan struct{} {
+	done := make(chan struct{})
+	go func() {
+		n.mw.Stop()
+		close(done)
+	}()
+	return done
+}
+
+// Register will register the given engine with the given unique engine engineID,
+// returning a conduit to directly submit messages to the message bus of the
+// engine.
+func (n *Network) Register(channelID uint8, engine network.Engine) (network.Conduit, error) {
+	n.Lock()
+	defer n.Unlock()
+
+	// check if the engine engineID is already taken
+	_, ok := n.engines[channelID]
+	if ok {
+		return nil, errors.Errorf("engine already registered (%d)", engine)
+	}
+
+	// add the channel ID to the cache
+	n.cache.Add(channelID)
+
+	// create the conduit
+	conduit := &Conduit{
+		channelID: channelID,
+		submit:    n.submit,
+	}
+
+	// register engine with provided channelID
+	n.engines[channelID] = engine
+
+	return conduit, nil
+}
+
+// Address implements a callback to provide the overlay layer with the
+// addresses we want to establish new connections to.
+func (n *Network) Identity() (flow.Identity, error) {
+
+	// get a list of other nodes that are not us, and we are not connected to
+	nodeIDs := n.top.Peers().NodeIDs()
+	nodeIDs = append(nodeIDs, n.me.NodeID())
+	ids, err := n.state.Final().Identities(identity.Not(identity.HasNodeID(nodeIDs...)))
+	if err != nil {
+		return flow.Identity{}, errors.Wrap(err, "could not get identities")
+	}
+
+	// if we don't have nodes available, we can't do anything
+	if len(ids) == 0 {
+		return flow.Identity{}, errors.New("no identities available")
+	}
+
+	// select a random identity from the list
+	id := ids[rand.Int()%len(ids)]
+
+	return id, nil
+}
+
+// Cleanup implements a callback to handle peers that have been dropped
+// by the middleware layer.
+func (n *Network) Cleanup(nodeID flow.Identifier) error {
+	// drop the peer state using the ID we registered
+	n.top.Down(nodeID)
+	return nil
+}
+
+func (n *Network) Receive(nodeID flow.Identifier, payload interface{}) error {
+
+	// TODO do type assettion
+
+	var err error
+	// Convert message payload to a known message type
+	msg, err := n.codec.Decode(payload.([]byte))
+	if err != nil {
+		return errors.Wrap(err, "could not decode event")
+	}
+
+	switch m := msg.(type) {
+	case *networkmodel.NetworkMessage:
+		n.processNetworkMessage(nodeID, m)
+	default:
+		err = errors.Errorf("invalid message type (%T)", m)
+	}
+	if err != nil {
+		return errors.Wrap(err, "could not process message")
+	}
+	return nil
+
+	return errors.New("method not implemented")
+}
+
+func (n *Network) processNetworkMessage(nodeID flow.Identifier, message *networkmodel.NetworkMessage) error {
+
+	// Extract channel id and find the registered engine
+	en, found := n.engines[message.ChannelID]
+	if !found {
+		n.logger.Debug().Str("sender", nodeID.String()).
+			Uint8("channel", uint8(message.ChannelID)).
+			Msg(" dropping message since no engine to receive it was found")
+		return fmt.Errorf("could not find the engine: %d", message.ChannelID)
+	}
+	// call the engine with the message payload
+	err := en.Process(message.OriginID, message.Payload)
+	if err != nil {
+		n.logger.Error().
+			Uint8("channel", uint8(message.ChannelID)).Str("sender", nodeID.String()).Err(err)
+		return err
+	}
+	return nil
+}
+
+// genNetworkMessage uses the codec to encode an event into a NetworkMessage
+func (n *Network) genNetworkMessage(channelID uint8, event interface{}, targetIDs ...flow.Identifier) (*networkmodel.NetworkMessage, error) {
+	// encode the payload using the configured codec
+	payload, err := n.codec.Encode(event)
+	if err != nil {
+		return nil, errors.Wrap(err, "could not encode event")
+	}
+
+	// use a hash with an engine-specific salt to get the payload hash
+	sip := siphash.New([]byte("libp2ppacking" + fmt.Sprintf("%03d", channelID)))
+
+	// casting event structure
+	e := &networkmodel.NetworkMessage{
+		ChannelID: channelID,
+		EventID:   sip.Sum(payload),
+		OriginID:  n.me.NodeID(),
+		TargetIDs: targetIDs,
+		Payload:   payload,
+	}
+
+	return e, nil
+}
+
+// submit method submits the given event for the given channel to the overlay layer
+// for processing; it is used by engines through conduits.
+// The Target needs to be added as a peer before submitting the message.
+func (n *Network) submit(channelID uint8, event interface{}, targetIDs ...flow.Identifier) error {
+	// genNetworkMessage the event to get payload and event ID
+	message, err := n.genNetworkMessage(channelID, event)
+	if err != nil {
+		return errors.Wrap(err, "could not cast the event into NetworkMessage")
+	}
+	// gossip
+
+	// checks if the event is already in the cache
+	ok := n.cache.Has(channelID, message.EventID)
+	if ok {
+		// returns nil and terminates sending the message since
+		// the message already submitted to the network
+		return nil
+	}
+	// storing event in the cache
+	n.cache.Set(channelID, message.EventID, message)
+
+	// get all peers that haven't seen it yet
+	nodeIDs := n.top.Peers(peer.Not(peer.HasSeen(message.EventID))).NodeIDs()
+	if len(nodeIDs) == 0 {
+		return nil
+	}
+	err = n.send(message, nodeIDs...)
+	if err != nil {
+		return errors.Wrap(err, "could not gossip event")
+	}
+
+	return nil
+}
+
+// send sends the message to the set of target ids through the middleware
+// send is the last method within the pipeline of message shipping in network layer
+// once it is called, the message slips through the network layer towards the middleware
+func (n *Network) send(msg interface{}, nodeIDs ...flow.Identifier) error {
+	// send the message through the peer connection
+	var result *multierror.Error
+	for _, nodeID := range nodeIDs {
+		err := n.mw.Send(nodeID, msg)
+		if err != nil {
+			result = multierror.Append(result, err)
+		}
+	}
+
+	return result.ErrorOrNil()
+}
