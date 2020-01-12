f4facf6e09c4d56b7ff41516e68da73a00fa6584
Alexander Hentschel <alex.hentschel@axiomzen.co>
2020-01-12 14:22:07 -0800

    restored voter; updated Block definition to contain needed fields that where already present in removed implementation

--- engine/consensus/hotstuff/types/block.go
+++ engine/consensus/hotstuff/types/block.go
@@ -1,10 +1,16 @@
 package types
 
+import "github.com/golang/protobuf/ptypes/timestamp"
+
 type Block struct {
 	View        uint64
 	QC          *QuorumCertificate
+	Height     uint64
+	ChainID    string
 	PayloadHash []byte
+	Timestamp  *timestamp.Timestamp
 }
+
 
 func (b *Block) BlockMRH() []byte {
 	panic("TODO")
--- engine/consensus/hotstuff/voter.go
+++ engine/consensus/hotstuff/voter.go
@@ -6,6 +6,7 @@ type Voter struct {
 	viewState ViewState
 }
 
-func (v *Voter) ConsiderVoting(block *types.BlockProposal, curView uint64) *types.Vote {
+// TODO: api unclear. requires documentation to specify expected behaviour
+func (v *Voter) ShouldVoteForNewProposal(b *types.BlockProposal, curView uint64) (myVote *types.Vote, voteCollectorIdx uint32) {
 	panic("TODO")
 }
