2e3193ac54b344a68a21d033c88320592b612cf1
Yahya <yahya@dapperlabs.com>
2020-01-13 13:19:37 -0800

    Decouples stubengine

--- network/gossip/libp2p/test/stubengine.go
+++ network/gossip/libp2p/test/stubengine.go
@@ -1,1 +1,49 @@
 package test
+
+import (
+	"fmt"
+	"testing"
+
+	"github.com/stretchr/testify/require"
+
+	"github.com/dapperlabs/flow-go/model/flow"
+	libp2p2 "github.com/dapperlabs/flow-go/network/gossip/libp2p"
+)
+
+// StubEngine is a simple engine that is used for testing the correctness of
+// driving the engines with libp2p
+type StubEngine struct {
+	t        *testing.T
+	net      libp2p2.Network // used to communicate with the network layer
+	originID flow.Identifier
+	event    interface{}   // used to keep track of the events that the node receives
+	received chan struct{} // used as an indicator on reception of messages for testing
+}
+
+// SubmitLocal is implemented for a valid type assertion to Engine
+// any call to it fails the test
+func (te *StubEngine) SubmitLocal(event interface{}) {
+	require.Fail(te.t, "not implemented")
+}
+
+// Submit is implemented for a valid type assertion to Engine
+// any call to it fails the test
+func (te *StubEngine) Submit(originID flow.Identifier, event interface{}) {
+	require.Fail(te.t, "not implemented")
+}
+
+// ProcessLocal is implemented for a valid type assertion to Engine
+// any call to it fails the test
+func (te *StubEngine) ProcessLocal(event interface{}) error {
+	require.Fail(te.t, "not implemented")
+	return fmt.Errorf(" unexpected method called")
+}
+
+// Process receives an originID and an event and casts them into the corresponding fields of the
+// StubEngine. It then flags the received channel on reception of an event
+func (te *StubEngine) Process(originID flow.Identifier, event interface{}) error {
+	te.originID = originID
+	te.event = event
+	te.received <- struct{}{}
+	return nil
+}
--- network/gossip/libp2p/test/stubengine_test.go
+++ network/gossip/libp2p/test/stubengine_test.go
@@ -21,14 +21,10 @@ import (
 	protocol "github.com/dapperlabs/flow-go/protocol/mock"
 )
 
-// StubEngine is a simple engine that is used for testing the correctness of
-// driving the engines with libp2p
-type StubEngine struct {
-	t        *testing.T
-	net      libp2p2.Network // used to communicate with the network layer
-	originID flow.Identifier
-	event    interface{}   // used to keep track of the events that the node receives
-	received chan struct{} // used as an indicator on reception of messages for testing
+type StubEngineTestSuite struct {
+	suite.Suite
+	state *protocol.State
+	me    *mock.Local
 }
 
 type Local struct {
@@ -41,40 +37,6 @@ func (l Local) NodeID() flow.Identifier {
 
 func (l Local) Address() string {
 	return ""
-}
-
-type StubEngineTestSuite struct {
-	suite.Suite
-	state *protocol.State
-	me    *mock.Local
-}
-
-// SubmitLocal is implemented for a valid type assertion to Engine
-// any call to it fails the test
-func (te *StubEngine) SubmitLocal(event interface{}) {
-	require.Fail(te.t, "not implemented")
-}
-
-// Submit is implemented for a valid type assertion to Engine
-// any call to it fails the test
-func (te *StubEngine) Submit(originID flow.Identifier, event interface{}) {
-	require.Fail(te.t, "not implemented")
-}
-
-// ProcessLocal is implemented for a valid type assertion to Engine
-// any call to it fails the test
-func (te *StubEngine) ProcessLocal(event interface{}) error {
-	require.Fail(te.t, "not implemented")
-	return fmt.Errorf(" unexpected method called")
-}
-
-// Process receives an originID and an event and casts them into the corresponding fields of the
-// StubEngine. It then flags the received channel on reception of an event
-func (te *StubEngine) Process(originID flow.Identifier, event interface{}) error {
-	te.originID = originID
-	te.event = event
-	te.received <- struct{}{}
-	return nil
 }
 
 // TestLibP2PNodesTestSuite runs all the test methods in this test suit
