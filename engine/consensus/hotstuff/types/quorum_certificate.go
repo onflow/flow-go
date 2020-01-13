db30295835fa89aba5608beb0f23e77ca9202d7a
Fangyu Gai <greferry@gmail.com>
2020-01-13 15:50:49 -0800

    fixed merge conflicts

--- engine/consensus/hotstuff/types/quorum_certificate.go
+++ engine/consensus/hotstuff/types/quorum_certificate.go
@@ -10,15 +10,12 @@ type QuorumCertificate struct {
 	AggregatedSignature *AggregatedSignature
 }
 
-<<<<<<< HEAD
 func NewQC(block *Block, sigs []*Signature, signersBitfieldLength uint32) *QuorumCertificate {
 	qc := &QuorumCertificate{
-		BlockMRH:            block.BlockMRH,
+		BlockMRH:            block.BlockMRH(),
 		View:                block.View,
 		AggregatedSignature: crypto.AggregateSigs(sigs, signersBitfieldLength),
 	}
 
 	return qc
 }
-=======
->>>>>>> 1a39716e7ed772903729afe55815eab9dc5c55dc
