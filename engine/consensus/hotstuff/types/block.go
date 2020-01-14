d17610fa57f784d610c36e0d51194cf9ded28fbf
Yahya <yahya@dapperlabs.com>
2020-01-14 13:22:54 -0800

    Adds echo functionality to stubengine

--- engine/consensus/hotstuff/types/block.go
+++ engine/consensus/hotstuff/types/block.go
@@ -5,12 +5,11 @@ import "github.com/golang/protobuf/ptypes/timestamp"
 type Block struct {
 	View        uint64
 	QC          *QuorumCertificate
-	Height     uint64
-	ChainID    string
+	Height      uint64
+	ChainID     string
 	PayloadHash []byte
-	Timestamp  *timestamp.Timestamp
+	Timestamp   *timestamp.Timestamp
 }
-
 
 func (b *Block) BlockMRH() []byte {
 	panic("TODO")
--- network/gossip/libp2p/test/stubengine.go
+++ network/gossip/libp2p/test/stubengine.go
@@ -15,9 +15,11 @@ import (
 type StubEngine struct {
 	t        *testing.T
 	net      libp2p2.Network // used to communicate with the network layer
+	con      libp2p2.Conduit // used to directly communicate with the network
 	originID flow.Identifier
 	event    chan interface{} // used to keep track of the events that the node receives
 	received chan struct{}    // used as an indicator on reception of messages for testing
+	echomsg  string           // used as a fix string to be included in the reply echos
 }
 
 // SubmitLocal is implemented for a valid type assertion to Engine
@@ -40,10 +42,22 @@ func (te *StubEngine) ProcessLocal(event interface{}) error {
 }
 
 // Process receives an originID and an event and casts them into the corresponding fields of the
-// StubEngine. It then flags the received channel on reception of an event
+// StubEngine. It then flags the received channel on reception of an event.
+// It also sends back an echo of the message to the origin ID
 func (te *StubEngine) Process(originID flow.Identifier, event interface{}) error {
+	// stores the message locally
 	te.originID = originID
 	te.event <- event
 	te.received <- struct{}{}
+
+	// asserting event as string
+	strEvent, ok := event.(string)
+	require.True(te.t, ok, "could not assert event as string")
+
+	// sends a echo back
+	msg := fmt.Sprintf("%s: %s", te.echomsg, strEvent)
+	err := te.con.Submit(msg, originID)
+	require.NoError(te.t, err, "could not submit echo back to network ")
+
 	return nil
 }
