1cf4d4d646a78513389f3e9e771f4f96daf28c04
Quantaf1re <37821211+quantaf1re@users.noreply.github.com>
2020-01-15 13:41:24 -0800

    Adding BytesForSig

--- engine/consensus/hotstuff/types/unsigned_vote.go
+++ engine/consensus/hotstuff/types/unsigned_vote.go
@@ -2,21 +2,19 @@ package types
 
 import "encoding/binary"
 
-type Vote struct {
-	View      uint64
-	BlockMRH  MRH
-	Signature *Signature
+type UnsignedVote struct {
+	View     uint64
+	BlockMRH MRH
 }
 
-func NewVote(view uint64, blockMRH MRH, sig *Signature) *Vote {
+func NewUnsignedVote(view uint64, blockMRH MRH) *Vote {
 	return &Vote{
-		View:      view,
-		BlockMRH:  blockMRH,
-		Signature: sig,
+		View:     view,
+		BlockMRH: blockMRH,
 	}
 }
 
-func (uv *Vote) BytesForSig() []byte {
+func (uv *UnsignedVote) BytesForSig() []byte {
 	// TODO there's probably a cleaner way to do this whole fcn
 	viewBytes := make([]byte, 8)
 	binary.LittleEndian.PutUint64(viewBytes, uv.View)
@@ -25,6 +23,7 @@ func (uv *Vote) BytesForSig() []byte {
 
 	length := len(viewBytes) + len(uv.BlockMRH) + len(voteStrBytes)
 	bytesForSig := make([]byte, 0, length)
+
 	bytesForSig = append(bytesForSig, viewBytes...)
 	bytesForSig = append(bytesForSig, uv.BlockMRH...)
 	bytesForSig = append(bytesForSig, voteStrBytes...)
