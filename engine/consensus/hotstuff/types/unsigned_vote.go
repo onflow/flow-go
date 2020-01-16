cec72d6fbc8aeab5a36e309ddd73cd4d67dbd08d
Quantaf1re <37821211+quantaf1re@users.noreply.github.com>
2020-01-16 15:22:22 -0800

    Changing order of generation of bytes for signing

--- engine/consensus/hotstuff/types/unsigned_vote.go
+++ engine/consensus/hotstuff/types/unsigned_vote.go
@@ -16,17 +16,16 @@ func NewUnsignedVote(view uint64, blockMRH []byte) *Vote {
 
 func (uv *UnsignedVote) BytesForSig() []byte {
 	// TODO there's probably a cleaner way to do this whole fcn
+	voteStrBytes := []byte("vote")
+
 	viewBytes := make([]byte, 8)
 	binary.LittleEndian.PutUint64(viewBytes, uv.View)
 
-	voteStrBytes := []byte("vote")
-
 	length := len(viewBytes) + len(uv.BlockMRH) + len(voteStrBytes)
 	bytesForSig := make([]byte, 0, length)
-
+	bytesForSig = append(bytesForSig, voteStrBytes...)
 	bytesForSig = append(bytesForSig, viewBytes...)
 	bytesForSig = append(bytesForSig, uv.BlockMRH...)
-	bytesForSig = append(bytesForSig, voteStrBytes...)
 
 	return bytesForSig
 }
--- engine/consensus/hotstuff/types/vote.go
+++ engine/consensus/hotstuff/types/vote.go
@@ -18,16 +18,16 @@ func NewVote(view uint64, blockMRH []byte, sig *Signature) *Vote {
 
 func (uv *Vote) BytesForSig() []byte {
 	// TODO there's probably a cleaner way to do this whole fcn
+	voteStrBytes := []byte("vote")
+
 	viewBytes := make([]byte, 8)
 	binary.LittleEndian.PutUint64(viewBytes, uv.View)
 
-	voteStrBytes := []byte("vote")
-
 	length := len(viewBytes) + len(uv.BlockMRH) + len(voteStrBytes)
 	bytesForSig := make([]byte, 0, length)
+	bytesForSig = append(bytesForSig, voteStrBytes...)
 	bytesForSig = append(bytesForSig, viewBytes...)
 	bytesForSig = append(bytesForSig, uv.BlockMRH...)
-	bytesForSig = append(bytesForSig, voteStrBytes...)
 
 	return bytesForSig
 }
