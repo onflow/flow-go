ab5dcbf9b006959f17916f51edd1b9dbf582cf69
Leo Zhang <zhangchiqing@gmail.com>
2020-01-16 10:13:45 -0800

    Update engine/consensus/hotstuff/voter.go
    Co-Authored-By: Alexander Hentschel <alex.hentschel@axiomzen.co>

--- engine/consensus/hotstuff/voter.go
+++ engine/consensus/hotstuff/voter.go
@@ -11,7 +11,7 @@ type Voter struct {
 // and boolean indicates whether to vote or not.
 // In order to ensure that only safe node will be voted, Voter will ask Forks whether a vote is a safe node or not.
 // The curView is taken as input to ensure Voter will only vote for proposals at current view.
-// Calling this method the second time for the same inputs will return the same vote as returned for the first time.
+// This method will only ever _once_ return a `non-nil, true` vote: the very first time it encounters a safe block of the current view to vote for. Subsequently, voter does _not_ vote for any other block with the same (or lower) view. (including repeated calls with the initial block we voted for also return `nil,false`). 
 func (v *Voter) ShouldVoteForCurProposal(b *types.BlockProposal, curView uint64) (*types.Vote, bool) {
 	panic("TODO")
 }
