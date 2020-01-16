e19771dbcac28f3c306d1ee47e5d25205caaab62
Peter Siemens <peterjsiemens@gmail.com>
2020-01-15 16:21:32 -0800

    Update execution_test.go

--- engine/execution/execution_test.go
+++ engine/execution/execution_test.go
@@ -1,7 +1,6 @@
 package execution_test
 
 import (
-	"fmt"
 	"testing"
 
 	"github.com/stretchr/testify/require"
@@ -110,65 +109,4 @@ func TestExecutionFlow(t *testing.T) {
 	// 	  return 5
 	// 	}
 	// `))
-}
-
-const counterScript = `
-
-  pub contract Counting {
-
-      pub resource Counter {
-          pub var count: Int
-
-          init() {
-              self.count = 0
-          }
-
-          pub fun add(_ count: Int) {
-              self.count = self.count + count
-          }
-      }
-
-      pub fun createCounter(): @Counter {
-          return <-create Counter()
-      }
-  }
-`
-
-// generateAddTwoToCounterScript generates a script that increments a counter.
-// If no counter exists, it is created.
-func generateAddTwoToCounterScript(counterAddress flow.Address) string {
-	return fmt.Sprintf(
-		`
-            import 0x%s
-
-            transaction {
-
-                prepare(signer: Account) {
-                    if signer.storage[Counting.Counter] == nil {
-                        let existing <- signer.storage[Counting.Counter] <- Counting.createCounter()
-                        destroy existing
-
-                        signer.published[&Counting.Counter] = &signer.storage[Counting.Counter] as Counting.Counter
-                    }
-
-                    signer.published[&Counting.Counter]?.add(2)
-                }
-            }
-        `,
-		counterAddress,
-	)
-}
-
-func generateGetCounterCountScript(counterAddress flow.Address, accountAddress flow.Address) string {
-	return fmt.Sprintf(
-		`
-            import 0x%s
-
-            pub fun main(): Int {
-                return getAccount(0x%s).published[&Counting.Counter]?.count ?? 0
-            }
-        `,
-		counterAddress,
-		accountAddress,
-	)
 }
