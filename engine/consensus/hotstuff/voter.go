8e5017da426568eca6c972406f967ddadb17eed8
Quantaf1re <37821211+quantaf1re@users.noreply.github.com>
2020-01-17 14:50:27 -0800

    Adding logging to Voter

--- engine/consensus/hotstuff/voter.go
+++ engine/consensus/hotstuff/voter.go
@@ -2,6 +2,7 @@ package hotstuff
 
 import (
 	"github.com/dapperlabs/flow-go/engine/consensus/hotstuff/types"
+	"github.com/rs/zerolog"
 )
 
 type Voter struct {
@@ -12,6 +13,18 @@ type Voter struct {
 	isConActor bool
 	// Need to keep track of the last view we voted for so we don't double vote accidentally
 	lastVotedView uint64
+	log           zerolog.Logger
+}
+
+func (v *Voter) NewVoter(signer Signer, viewState ViewState, forks Forks, isConActor bool, log zerolog.Logger) *Voter {
+	return &Voter{
+		signer:        signer,
+		viewState:     viewState,
+		forks:         forks,
+		isConActor:    isConActor,
+		lastVotedView: 0,
+		log:           log,
+	}
 }
 
 // ProduceVoteIfVotable will make a decision on whether it will vote for the given proposal, the returned
@@ -22,23 +35,29 @@ type Voter struct {
 //  current view to vote for. Subsequently, voter does _not_ vote for any other block with the same (or lower) view.
 // (including repeated calls with the initial block we voted for also return `nil, false`).
 func (v *Voter) ProduceVoteIfVotable(bp *types.BlockProposal, curView uint64) (*types.Vote, bool) {
+	log := v.log.With().
+		Hex("block_id", bp.Block.BlockMRH()).
+		Logger()
+
 	if !v.isConActor {
-		// TODO: log this reason
+		log.Debug().Msg("we're not a consensus actor, don't vote")
 		return nil, false
 	}
 
 	if v.forks.IsSafeNode(bp) {
-		// TODO: log this reason
+		log.Info().Msg("received block is not a safe node, don't vote")
 		return nil, false
 	}
 
 	if curView != bp.Block.View {
-		// TODO: log this reason
+		log.Info().Uint64("view", bp.Block.View).Uint64("curView", curView).
+			Msg("received block's view is not our current view, don't vote")
 		return nil, false
 	}
 
 	if curView <= v.lastVotedView {
-		// TODO: log this reason
+		log.Info().Uint64("lastVotedView", v.lastVotedView).Uint64("curView", curView).
+			Msg("received block's view is <= lastVotedView, don't vote")
 		return nil, false
 	}
 
@@ -46,9 +65,15 @@ func (v *Voter) ProduceVoteIfVotable(bp *types.BlockProposal, curView uint64) (*
 }
 
 func (v *Voter) produceVote(bp *types.BlockProposal) *types.Vote {
+	log := v.log.With().
+		Hex("block_id", bp.Block.BlockMRH()).
+		Logger()
+
 	signerIdx := v.viewState.GetSelfIdxForView(bp.Block.View)
 	unsignedVote := types.NewUnsignedVote(bp.Block.View, bp.Block.BlockMRH())
 	sig := v.signer.SignVote(unsignedVote, signerIdx)
+	vote := types.NewVote(bp.Block.View, bp.Block.BlockMRH(), sig)
+	log.Info().Msg("successfully produced vote")
 
-	return types.NewVote(bp.Block.View, bp.Block.BlockMRH(), sig)
+	return vote
 }
