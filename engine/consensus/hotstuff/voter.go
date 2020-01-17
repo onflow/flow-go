df7297be6e5274ec01873e066e958f4701d01ddd
Quantaf1re <37821211+quantaf1re@users.noreply.github.com>
2020-01-16 16:23:36 -0800

    Adding back comment

--- engine/consensus/hotstuff/voter.go
+++ engine/consensus/hotstuff/voter.go
@@ -14,6 +14,13 @@ type Voter struct {
 	lastVotedView uint64
 }
 
+// ProduceVoteIfVotable will make a decision on whether it will vote for the given proposal, the returned
+// boolean indicates whether to vote or not.
+// In order to ensure that only a safe node will be voted, Voter will ask Forks whether a vote is a safe node or not.
+// The curView is taken as input to ensure Voter will only vote for proposals at current view and prevent double voting.
+// This method will only ever _once_ return a `non-nil, true` vote: the very first time it encounters a safe block of the
+//  current view to vote for. Subsequently, voter does _not_ vote for any other block with the same (or lower) view.
+// (including repeated calls with the initial block we voted for also return `nil, false`).
 func (v *Voter) ProduceVoteIfVotable(bp *types.BlockProposal, curView uint64) (*types.Vote, bool) {
 	if !v.isConActor {
 		// TODO: log this reason
