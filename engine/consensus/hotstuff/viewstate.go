1259a344ccd13ba4ed67847c26a52a7f56906e26
Quantaf1re <37821211+quantaf1re@users.noreply.github.com>
2020-01-17 17:13:17 -0800

    Implementing GetSelfIdxForBlockID and GetIdxOfPubKeyForBlockID

--- engine/consensus/hotstuff/viewstate.go
+++ engine/consensus/hotstuff/viewstate.go
@@ -1,11 +1,14 @@
 package hotstuff
 
-import "github.com/dapperlabs/flow-go/engine/consensus/hotstuff/types"
+import (
+	"github.com/dapperlabs/flow-go/engine/consensus/hotstuff/types"
+	"github.com/dapperlabs/flow-go/model/flow"
+)
 
 type ViewState interface {
 	IsSelf(id types.ID) bool
 	IsSelfLeaderForView(view uint64) bool
-	GetSelfIdxForView(view uint64) uint32
-	GetIdxOfPubKeyForView(view uint64) uint32
 	LeaderForView(view uint64) types.ID
+	GetSelfIdxForBlockID(blockID flow.Identifier) uint32
+	GetIdxOfPubKeyForBlockID(blockID flow.Identifier) uint32
 }
--- engine/consensus/hotstuff/viewstate/viewstate.go
+++ engine/consensus/hotstuff/viewstate/viewstate.go
@@ -1,29 +1,59 @@
 package viewstate
 
 import (
+	"errors"
+
 	"github.com/dapperlabs/flow-go/model/flow"
+	"github.com/dapperlabs/flow-go/model/flow/identity"
 	"github.com/dapperlabs/flow-go/module"
 	"github.com/dapperlabs/flow-go/protocol"
+	"github.com/rs/zerolog"
 )
 
 type ViewState struct {
-	ps protocol.State
-	me module.Local
+	ps  protocol.State
+	me  module.Local
+	log zerolog.Logger
 }
 
 // IsSelf(id types.ID) bool
 // IsSelfLeaderForView(view uint64) bool
-// GetSelfIdxForView(view uint64) uint32
-// GetIdxOfPubKeyForView(view uint64) uint32
 // LeaderForView(view uint64) types.ID
+// GetSelfIdxForBlockID(blockID flow.Identifier) uint32
+// GetIdxOfPubKeyForBlockID(blockID flow.Identifier) uint32
 
 func (vs *ViewState) IsSelf(id flow.Identifier) bool {
 	return id == vs.me.NodeID()
 }
 
-func (vs *ViewState) GetIdentitiesByRole(role flow.IdentityList) bool {
-	vs.ps
-	func(node *flow.Identity) {
-		node.Role == flow.RoleConsensus
+func (vs *ViewState) getIdentitiesByViewAndRole(view uint64, role flow.Role) (flow.IdentityList, error) {
+	identities, err := vs.ps.AtNumber(view).Identities(identity.HasRole(role))
+
+	return identities, err
+}
+
+func (vs *ViewState) GetSelfIdxForBlockID(blockID flow.Identifier) (uint32, error) {
+	return vs.GetIdxOfPubKeyForBlockID(blockID, vs.me.NodeID())
+}
+
+func (vs *ViewState) GetIdxOfPubKeyForBlockID(blockID flow.Identifier, pubkey flow.Identifier) (uint32, error) {
+	log := vs.log.With().
+		Hex("block_id", blockID[:]).
+		Logger()
+
+	identities, err := vs.ps.AtBlockID(blockID).Identities(identity.HasRole(flow.RoleConsensus))
+	if err != nil {
+		log.Error().Err(err).Msg("couldn't get consensus identities while doing GetSelfIdxForBlockID")
+		return 0, err
 	}
+
+	for i, identity := range identities {
+		if identity.NodeID == pubkey {
+			return uint32(i), nil
+		}
+	}
+
+	err = errors.New("couldn't find self in consensus identities while doing GetSelfIdxForBlockID")
+	log.Error().Err(err)
+	return 0, err
 }
