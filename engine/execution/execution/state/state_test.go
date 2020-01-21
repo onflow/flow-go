5e44fbc316cb0d678a7adec4f87fce49bb6632c5
Peter Siemens <peterjsiemens@gmail.com>
2020-01-21 14:07:46 -0800

    Add test for GetChunkRegisters function

--- engine/execution/execution/state/state_test.go
+++ engine/execution/execution/state/state_test.go
@@ -1,6 +1,7 @@
 package state_test
 
 import (
+	"fmt"
 	"testing"
 
 	"github.com/golang/mock/gomock"
@@ -136,4 +137,77 @@ func TestExecutionStateWithTrieStorage(t *testing.T) {
 		assert.Equal(t, []byte("apple"), b1)
 		assert.Empty(t, b2)
 	}))
+}
+
+func TestState_GetChunkRegisters(t *testing.T) {
+	t.Run("non-existent chunk", func(t *testing.T) {
+		ls := new(storage.Ledger)
+		sc := new(storage.StateCommitments)
+		ch := new(storage.ChunkHeaders)
+
+		chunkID := unittest.IdentifierFixture()
+
+		ch.On("ByID", chunkID).Return(nil, fmt.Errorf("storage error"))
+
+		es := state.NewExecutionState(ls, sc, ch)
+
+		ledger, err := es.GetChunkRegisters(chunkID)
+		assert.Nil(t, ledger)
+		assert.Error(t, err)
+	})
+
+	t.Run("ledger storage error", func(t *testing.T) {
+		ls := new(storage.Ledger)
+		sc := new(storage.StateCommitments)
+		ch := new(storage.ChunkHeaders)
+
+		chunkHeader := unittest.ChunkHeaderFixture()
+		chunkID := chunkHeader.ChunkID
+
+		registerIDs := chunkHeader.RegisterIDs
+
+		ch.On("ByID", chunkID).Return(&chunkHeader, nil)
+		ls.On("GetRegisters", registerIDs, chunkHeader.StartState).
+			Return(nil, fmt.Errorf("storage error"))
+
+		es := state.NewExecutionState(ls, sc, ch)
+
+		registers, err := es.GetChunkRegisters(chunkID)
+		assert.Error(t, err)
+		assert.Nil(t, registers)
+
+		ch.AssertExpectations(t)
+		ls.AssertExpectations(t)
+	})
+
+	t.Run("existing chunk", func(t *testing.T) {
+		ls := new(storage.Ledger)
+		sc := new(storage.StateCommitments)
+		ch := new(storage.ChunkHeaders)
+
+		chunkHeader := unittest.ChunkHeaderFixture()
+		chunkID := chunkHeader.ChunkID
+
+		registerIDs := chunkHeader.RegisterIDs
+		registerValues := []flow.RegisterValue{{1}, {2}, {3}}
+
+		ch.On("ByID", chunkID).Return(&chunkHeader, nil)
+		ls.On("GetRegisters", registerIDs, chunkHeader.StartState).Return(registerValues, nil)
+
+		es := state.NewExecutionState(ls, sc, ch)
+
+		actualRegisters, err := es.GetChunkRegisters(chunkID)
+		assert.NoError(t, err)
+
+		expectedRegisters := flow.Ledger{
+			string(registerIDs[0]): registerValues[0],
+			string(registerIDs[1]): registerValues[1],
+			string(registerIDs[2]): registerValues[2],
+		}
+
+		assert.Equal(t, expectedRegisters, actualRegisters)
+
+		ch.AssertExpectations(t)
+		ls.AssertExpectations(t)
+	})
 }
