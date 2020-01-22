24687b7beb66ee060e090db9f7125335b7601d14
Peter Siemens <peterjsiemens@gmail.com>
2020-01-22 12:21:06 -0800

    Refactor language package to remove flow-go dependency

--- utils/unittest/unittest.go
+++ utils/unittest/unittest.go
@@ -5,12 +5,9 @@ import (
 	"math/rand"
 	"os"
 	"path/filepath"
-	"strings"
 	"testing"
 
 	"github.com/dgraph-io/badger/v2"
-	"github.com/go-test/deep"
-	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
 
 	"github.com/dapperlabs/flow-go/storage/ledger/databases/leveldb"
@@ -25,37 +22,6 @@ func ExpectPanic(expectedMsg string, t *testing.T) {
 		return
 	}
 	t.Errorf("Expected to panic with `%s`, but did not panic", expectedMsg)
-}
-
-// AssertEqualWithDiff asserts that two objects are equal.
-//
-// If the objects are not equal, this function prints a human-readable diff.
-func AssertEqualWithDiff(t *testing.T, expected, actual interface{}) {
-	// the maximum levels of a struct to recurse into
-	// this prevents infinite recursion from circular references
-	deep.MaxDepth = 100
-
-	diff := deep.Equal(expected, actual)
-
-	if len(diff) != 0 {
-		s := strings.Builder{}
-
-		for i, d := range diff {
-			if i == 0 {
-				s.WriteString("diff    : ")
-			} else {
-				s.WriteString("          ")
-			}
-
-			s.WriteString(d)
-			s.WriteString("\n")
-		}
-
-		assert.Fail(t, fmt.Sprintf("Not equal: \n"+
-			"expected: %s\n"+
-			"actual  : %s\n\n"+
-			"%s", expected, actual, s.String()))
-	}
 }
 
 func RunWithBadgerDB(t *testing.T, f func(*badger.DB)) {
