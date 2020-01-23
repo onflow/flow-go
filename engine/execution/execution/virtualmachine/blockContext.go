710df4869cdeceb61fe8bc53c1a3b7231a67cae0
Peter Siemens <peterjsiemens@gmail.com>
2020-01-22 16:38:18 -0800

    Update virtualmachine to use block header for block context

--- engine/execution/execution/virtualmachine/blockContext.go
+++ engine/execution/execution/virtualmachine/blockContext.go
@@ -18,20 +18,20 @@ type BlockContext interface {
 }
 
 // NewBlockContext creates a new block context given a runtime and block.
-func NewBlockContext(rt runtime.Runtime, block *flow.Block) BlockContext {
+func NewBlockContext(rt runtime.Runtime, header *flow.Header) BlockContext {
 	vm := &virtualMachine{
 		rt: rt,
 	}
 
 	return &blockContext{
-		vm:    vm,
-		block: block,
+		vm:     vm,
+		header: header,
 	}
 }
 
 type blockContext struct {
-	vm    *virtualMachine
-	block *flow.Block
+	vm     *virtualMachine
+	header *flow.Header
 }
 
 func (bc *blockContext) newTransactionContext(ledger Ledger, tx *flow.TransactionBody) *transactionContext {
