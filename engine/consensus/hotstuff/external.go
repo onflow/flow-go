f57e572a89db7dc2a6f5a5b1e4f83113d965310a
Jordan Schalm <jordan@dapperlabs.com>
2020-02-03 10:36:43 -0800

    update module.Builder comment

--- engine/consensus/hotstuff/external.go
+++ engine/consensus/hotstuff/external.go
@@ -18,33 +18,6 @@ type NetworkSender interface {
 	BroadcastProposal(proposal *types.BlockProposal) error
 }
 
-// Builder defines the interface between the HotStuff core algorithm and a
-// payload validation layer.
-//
-// The payload validation layer is responsible for gathering items for
-// inclusion in blocks, tracking payloads in un-finalized blocks, and enforcing
-// application-specific payload validation rules.
-type Builder interface {
-
-	// BuildOn generates a new payload that is valid with respect to the parent
-	// being built upon and returns the hash of the generated payload.
-
-	// TODO decide between 2 following:
-
-	// BuildOn1 just generate the payload and return the hash. Store the hash
-	// to payload mapping. When the block is eventually broadcasted via
-	// NetworkSender, get the payload via block.payloadID and store the full
-	// block.
-	BuildOn1(parentID flow.Identifier) flow.Identifier
-
-	// BuildOn2 accepts a closure so that storing the full block can be completed
-	// all in one go here.
-	//
-	// create function creates the full block including payload hash, signs it
-	// and returns the full block. Builder can then store the block.
-	BuildOn2(parentID flow.Identifier, create func(payloadID flow.Identifier) *types.Block)
-}
-
 // Pruner allows components external to HotStuff to keep their un-finalized
 // block tree in sync with HotStuff.
 type Pruner interface {
