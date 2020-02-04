b1e9ed0a0fbb3bc096e074d119bc68862eede661
Fangyu Gai <greferry@gmail.com>
2020-02-03 22:32:16 -0800

    finished testing

--- engine/consensus/hotstuff/vote_aggregator_test.go
+++ engine/consensus/hotstuff/vote_aggregator_test.go
@@ -20,73 +20,191 @@ import (
 // meaning that it is enough to build a QC when receiving 5 votes
 const VALIDATOR_SIZE = 7
 
-// receive 6 valid incorporated votes in total
-// a QC will be generated on receiving the 5th vote
-// when receiving the 6th vote, the same QC should be returned
-func TestHappyPathForIncorporatedVotes(t *testing.T) {
+// HAPPY PATH (votes are valid and the block always arrives before votes)
+// assume there are 7 nodes, meaning that the threshold is 5
+// a QC should not be built when receiving the block because of insufficient votes
+func TestOnlyReceiveBlock(t *testing.T) {
 	va := newMockVoteAggregator(t)
 	testView := uint64(5)
 	block := newMockBlock(testView)
-	var lastQC *types.QuorumCertificate
-	for i := 0; i < 5; i++ {
+	qc, err := va.BuildQCOnReceivingBlock(block)
+	require.Nil(t, qc)
+	require.NotNil(t, err)
+	require.True(t, errors.Is(err, types.ErrInsufficientVotes{}))
+}
+
+// HAPPY PATH (votes are valid and the block always arrives before votes)
+// assume there are 7 nodes, meaning that the threshold is 5
+// a QC should not be built when receiving insufficient votes
+func TestReceiveBlockBeforeInsufficientVotes(t *testing.T) {
+	va := newMockVoteAggregator(t)
+	testView := uint64(5)
+	block := newMockBlock(testView)
+	qc, err := va.BuildQCOnReceivingBlock(block)
+	require.Nil(t, qc)
+	require.NotNil(t, err)
+	for i := 0; i < 3; i++ {
 		vote := newMockVote(testView, block.BlockID(), uint32(i))
-		qc, err := va.StoreVoteAndBuildQC(vote, block)
-		if i < 4 {
+		qc, err = va.StoreVoteAndBuildQC(vote, block)
+		require.Nil(t, qc)
+		require.NotNil(t, err)
+		require.True(t, errors.Is(err, types.ErrInsufficientVotes{}))
+	}
+}
+
+// HAPPY PATH (votes are valid and the block always arrives before votes)
+// assume there are 7 nodes, meaning that the threshold is 5
+// a QC should be built when receiving the 5th vote (the block is counted as one vote)
+func TestReceiveBlockBeforeSufficientVotes(t *testing.T) {
+	va := newMockVoteAggregator(t)
+	testView := uint64(5)
+	block := newMockBlock(testView)
+	qc, err := va.BuildQCOnReceivingBlock(block)
+	require.Nil(t, qc)
+	require.NotNil(t, err)
+	for i := 0; i < 4; i++ {
+		vote := newMockVote(testView, block.BlockID(), uint32(i))
+		qc, err = va.StoreVoteAndBuildQC(vote, block)
+		if i < 3 {
+			require.Nil(t, qc)
 			require.NotNil(t, err)
 			require.True(t, errors.Is(err, types.ErrInsufficientVotes{}))
-			fmt.Println(err.Error())
 		} else {
 			require.Nil(t, err)
 			require.NotNil(t, qc)
 			require.Equal(t, block.BlockID(), qc.BlockID)
-			lastQC = qc
-			fmt.Println("QC generated")
 		}
 	}
-	lastVote := newMockVote(testView, block.BlockID(), uint32(5))
-	newQC, err := va.StoreVoteAndBuildQC(lastVote, block)
-	require.Nil(t, err)
-	require.Equal(t, lastQC, newQC)
 }
 
-// receive 4 valid pending votes and QC should not be built
-// on receiving the block proposal, a QC should be built because of the primary vote
-func TestBuildQCOnReceivingBlock(t *testing.T) {
+// HAPPY PATH (votes are valid and the block always arrives before votes)
+// assume there are 7 nodes, meaning that the threshold is 5
+// the same QC should be returned when receiving the 6th vote
+func TestReceiveVoteAfterQCBuilt(t *testing.T) {
 	va := newMockVoteAggregator(t)
 	testView := uint64(5)
 	block := newMockBlock(testView)
-	for i := 0; i < 4; i++ {
-		vote := newMockVote(testView, block.BlockID(), uint32(i))
-		err := va.StorePendingVote(vote)
-		require.Nil(t, err)
-	}
-	qc, err := va.BuildQCOnReceivingBlock(block)
-	require.Nil(t, err)
-	require.Equal(t, block.BlockID(), qc.BlockID)
-}
-
-// receive 3 pending votes and then receive the block,
-// a QC should not be built because of insufficient votes,
-// when receiving the 5th vote, a QC should be generated
-func TestBuildQCAfterReceivingBlock(t *testing.T) {
-	va := newMockVoteAggregator(t)
-	testView := uint64(5)
-	block := newMockBlock(testView)
-	fmt.Println(block.BlockID().String())
-	for i := 0; i < 3; i++ {
-		vote := newMockVote(testView, block.BlockID(), uint32(i))
-		err := va.StorePendingVote(vote)
-		require.Nil(t, err)
-	}
 	qc, err := va.BuildQCOnReceivingBlock(block)
 	require.Nil(t, qc)
 	require.NotNil(t, err)
-	lastVote := newMockVote(testView, block.BlockID(), uint32(3))
-	qc, err = va.StoreVoteAndBuildQC(lastVote, block)
+	for i := 0; i < 4; i++ {
+		vote := newMockVote(testView, block.BlockID(), uint32(i))
+		qc, err = va.StoreVoteAndBuildQC(vote, block)
+		if i < 3 {
+			require.Nil(t, qc)
+			require.NotNil(t, err)
+			require.True(t, errors.Is(err, types.ErrInsufficientVotes{}))
+		} else {
+			require.Nil(t, err)
+			require.NotNil(t, qc)
+			require.Equal(t, block.BlockID(), qc.BlockID)
+		}
+	}
+	finalVote := newMockVote(testView, block.BlockID(), uint32(4))
+	finalQC, err := va.StoreVoteAndBuildQC(finalVote, block)
 	require.Nil(t, err)
-	require.NotNil(t, qc)
+	require.NotNil(t, finalQC)
+	require.Equal(t, qc, finalQC)
 }
 
+// PENDING PATH (votes are valid and the block arrives after votes)
+// receive 3 votes first, a QC should not be built when receiving the block because of insufficient votes
+func TestReceiveInsufficientVotesBeforeBlock(t *testing.T) {
+	va := newMockVoteAggregator(t)
+	testView := uint64(5)
+	block := newMockBlock(testView)
+	qc, err := va.BuildQCOnReceivingBlock(block)
+	require.Nil(t, qc)
+	require.NotNil(t, err)
+	for i := 0; i < 3; i++ {
+		vote := newMockVote(testView, block.BlockID(), uint32(i))
+		err = va.StorePendingVote(vote)
+		require.Nil(t, err)
+	}
+	qc, err = va.BuildQCOnReceivingBlock(block)
+	require.Nil(t, qc)
+	require.NotNil(t, err)
+	require.True(t, errors.Is(err, types.ErrInsufficientVotes{}))
+}
+
+// PENDING PATH (votes are valid and the block arrives after votes)
+// receive 4 votes first, a QC should be built when receiving the block
+func TestReceiveSufficientVotesBeforeBlock(t *testing.T) {
+	va := newMockVoteAggregator(t)
+	testView := uint64(5)
+	block := newMockBlock(testView)
+	qc, err := va.BuildQCOnReceivingBlock(block)
+	require.Nil(t, qc)
+	require.NotNil(t, err)
+	for i := 0; i < 4; i++ {
+		vote := newMockVote(testView, block.BlockID(), uint32(i))
+		err = va.StorePendingVote(vote)
+		require.Nil(t, err)
+	}
+	qc, err = va.BuildQCOnReceivingBlock(block)
+	require.Nil(t, err)
+	require.NotNil(t, qc)
+	require.Equal(t, block.BlockID(), qc.BlockID)
+}
+
+// UNHAPPY PATH (votes are invalid and the block arrives before votes)
+// ErrStaleVote should be raised when receiving stale votes (vote.view <= lastPruneView)
+func TestErrStaleVote(t *testing.T) {
+	va := newMockVoteAggregator(t)
+	testView := uint64(0)
+	block := newMockBlock(testView)
+	vote := newMockVote(testView, block.BlockID(), uint32(1))
+	err := va.StorePendingVote(vote)
+	require.NotNil(t, err)
+	require.True(t, errors.Is(err, types.ErrStaleVote{
+		Vote:          vote,
+		FinalizedView: va.lastPrunedView,
+	}))
+	require.Equal(t, 0, len(va.pendingVoteMap))
+}
+
+// UNHAPPY PATH (votes with invalid view and the block arrives after votes)
+// there should not be error when being stored as pending votes, but
+// error should be raised when converting from pending votes
+func TestErrInvalidView(t *testing.T) {
+	va := newMockVoteAggregator(t)
+	testView := uint64(5)
+	block := newMockBlock(testView)
+	vote := newMockVote(testView-1, block.BlockID(), uint32(1))
+	err := va.StorePendingVote(vote)
+	require.Nil(t, err)
+	qc, err := va.BuildQCOnReceivingBlock(block)
+	require.Nil(t, qc)
+	require.NotNil(t, err)
+}
+
+// UNHAPPY PATH (double voting)
+// store one vote in the memory
+// receive another vote with the same voter and the same view
+// should trigger ErrDoubleVote
+func TestErrDoubleVote(t *testing.T) {
+	va := newMockVoteAggregator(t)
+	// mock blocks and votes
+	b1 := newMockBlock(10)
+	b2 := newMockBlock(10)
+	vote1 := newMockVote(10, b1.BlockID(), uint32(1))
+	_, err := va.StoreVoteAndBuildQC(vote1, b1)
+	// vote2 is double voting
+	vote2 := newMockVote(10, b2.BlockID(), uint32(1))
+	_, err = va.StoreVoteAndBuildQC(vote2, b2)
+	if err != nil {
+		switch err.(type) {
+		case types.ErrDoubleVote:
+			fmt.Printf("double vote detected %v", err)
+		default:
+			fmt.Printf("error detected %v", err)
+		}
+	} else {
+		t.Errorf("double vote not detected")
+	}
+}
+
+// UNHAPPY PATH (votes with invalid view)
 // receive 7 votes in total
 // 3 of them are invalid and located every other valid vote
 // i.e., 1.valid, 2. invalid, ..., 6. valid, 7. invalid
@@ -109,6 +227,7 @@ func TestUnHappyPathForIncorporatedVotes(t *testing.T) {
 	}
 }
 
+// UNHAPPY PATH
 // receive 7 votes in total
 // 1. all 7 votes are pending and invalid (cannot pass ValidatePendingVotes), no votes should be stored
 // 2. all 7 votes are pending and can pass ValidatePendingVotes, but cannot pass ValidateIncorporatedVotes
@@ -139,32 +258,6 @@ func TestUnHappyPathForPendingVotes(t *testing.T) {
 		require.Nil(t, qc)
 		require.NotNil(t, err)
 		fmt.Println(err.Error())
-	}
-}
-
-// store one vote in the memory
-// receive another vote with the same voter and the same view
-// should trigger ErrDoubleVote
-func TestErrDoubleVote(t *testing.T) {
-	va := newMockVoteAggregator(t)
-	// mock blocks and votes
-	b1 := newMockBlock(10)
-	b2 := newMockBlock(10)
-	vote1 := newMockVote(10, b1.BlockID(), uint32(1))
-	_, err := va.StoreVoteAndBuildQC(vote1, b1)
-	require.NoError(t, err)
-	// vote2 is double voting
-	vote2 := newMockVote(10, b2.BlockID(), uint32(1))
-	_, err = va.StoreVoteAndBuildQC(vote2, b2)
-	if err != nil {
-		switch err.(type) {
-		case types.ErrDoubleVote:
-			fmt.Printf("double vote detected %v", err)
-		default:
-			fmt.Printf("error detected %v", err)
-		}
-	} else {
-		t.Errorf("double vote not detected")
 	}
 }
 
