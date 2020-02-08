a253c30ca2c48462d600e38c9877f0653da4ca43
Leo Zhang (zhangchiqing) <zhangchiqing@gmail.com>
2020-02-07 22:28:26 -0800

    update comments

--- engine/consensus/hotstuff/signature/sig_verifier.go
+++ engine/consensus/hotstuff/signature/sig_verifier.go
@@ -7,22 +7,12 @@ import (
 // SigVerifier provides functions to verify single signature and aggregated signature.
 type SigVerifier interface {
 	// VerifySig verifies a single signature.
-	// sig - the signature to be verified
-	// message - the message that was signed
-	// blockID - the blockID of the data (vote) to be signed.
-	// The signer index in the signature and the blockID determines the public key, which will
-	// be used to verify the signature.
-	// blockID also determines the group public key to verify threshold signature.
+	// It reads the public key from the signature's identity field, and verifies the raw sig against
+	// the given message.
 	VerifySig(sig *types.Signature, message types.VoteBytes) (bool, error)
 
 	// VerifyAggregatedSig verifies the aggregated signature.
-	// VerifyAggregatedSig checks if the given aggregated signature is aggregated from signatures
-	// that are signed for a certain view by the given public keys over a message
-	//
-	// aggsig: the aggregated signature to be verified
-	// blockID: the ID of the block that was signed.
-	// Note: blockID is needed when the aggregated signature contains threshold signature, in which case
-	// the blockID is needed to find the group public key.
-	// The group public key is needed to verify threshold signature.
+	// aggsig - the aggregated signature to be verified
+	// message - the message that each signer was signed.
 	VerifyAggregatedSig(aggsig *types.AggregatedSignature, message types.VoteBytes) (bool, error)
 }
