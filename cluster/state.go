8970cc9933e41bc338b6a3c913ad3f75ad87f2db
Jordan Schalm <jordan@dapperlabs.com>
2020-02-10 15:45:01 -0800

    use different files, as in protocol.State

--- cluster/state.go
+++ cluster/state.go
@@ -1,7 +1,6 @@
 package cluster
 
 import (
-	"github.com/dapperlabs/flow-go/model/cluster"
 	"github.com/dapperlabs/flow-go/model/flow"
 )
 
@@ -30,29 +29,4 @@ type State interface {
 	// extending the cluster state in a consistent manner that preserves
 	// integrity, validity, and functionality of the database.
 	Mutate() Mutator
-}
-
-// Snapshot represents an immutable snapshot at a specific point in the cluster
-// state history.
-type Snapshot interface {
-
-	// Collection returns the collection generated in this step of the cluster
-	// state history.
-	Collection() (*flow.Collection, error)
-}
-
-// Mutator represents an interface to modify the persistent cluster state in a
-// way that conserves its integrity. It enforces a number of invariants on the
-// input data to ensure internal bookkeeping mechanisms remain functional and
-// valid.
-type Mutator interface {
-
-	// Bootstrap initializes the persistent cluster state with a genesis block.
-	// The genesis block must have number 0, a parent hash of 32 zero bytes,
-	// and an empty collection as payload.
-	Bootstrap(genesis *cluster.Block) error
-
-	// Extend introduces the block with the given ID into the persistent
-	// cluster state without modifying the current finalized state.
-	Extend(blockID flow.Identifier) error
 }
