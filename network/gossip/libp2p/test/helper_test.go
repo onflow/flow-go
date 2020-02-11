9514c70a3c474fa7dd3e81386570efc7e97d2d18
Yahya <yahya@dapperlabs.com>
2020-02-11 11:29:05 -0800

    Adds .TestTenNodesTwoLinkedSubNet

--- network/gossip/libp2p/test/helper_test.go
+++ network/gossip/libp2p/test/helper_test.go
@@ -44,19 +44,19 @@ Single Subnet tests
 // TestOneNodeOneSubNet evaluates CreateSubnets for creating a single subnet of one node
 func (s *SubNetGeneratorTestSuite) TestOneNodeOneSubNet() {
 	// single subnet of size 1 nodes
-	s.SubNetSizeTestHelper(1, 1)
+	s.SubNetSizeTestHelper(1, 1, 0)
 }
 
 // TestTwoNodeOneSubNet evaluates CreateSubnets for creating a single subnet of two nodes
 func (s *SubNetGeneratorTestSuite) TestTwoNodesOneSubNet() {
 	// single subnet of size 2 nodes
-	s.SubNetSizeTestHelper(2, 1)
+	s.SubNetSizeTestHelper(2, 1, 0)
 }
 
 // TestMultiNodeOneSubNet evaluates CreateSubnets for a single subnet of multiple nodes
 func (s *SubNetGeneratorTestSuite) TestMultiNodesOneSubNet() {
 	// single subnet of size 10 nodes
-	s.SubNetSizeTestHelper(10, 1)
+	s.SubNetSizeTestHelper(10, 1, 0)
 }
 
 /*
@@ -66,13 +66,13 @@ Two Subnets tests
 // TestTwoNodesTwoSubNet evaluates CreateSubnets for creating two subnets of two nodes
 func (s *SubNetGeneratorTestSuite) TestTwoNodesTwoSubNet() {
 	// 2 subsets of size 2
-	s.SubNetSizeTestHelper(2, 2)
+	s.SubNetSizeTestHelper(2, 2, 0)
 }
 
 // TestTenNodesTwoSubNet evaluates CreateSubnets for creating two subnets of 10 nodes
 func (s *SubNetGeneratorTestSuite) TestTenNodesTwoSubNet() {
 	// two subnet 10 nodes
-	s.SubNetSizeTestHelper(10, 2)
+	s.SubNetSizeTestHelper(10, 2, 0)
 }
 
 /*
@@ -83,36 +83,48 @@ Multiple subnets
 // number of subnets, which should make all subnets receive at least one node
 func (s *SubNetGeneratorTestSuite) TestMultiNodeMultiSubNet() {
 	// divides 5 nodes into 5 subnets
-	s.SubNetSizeTestHelper(5, 5)
+	s.SubNetSizeTestHelper(5, 5, 0)
 }
 
 // TestDoubleNodeMultiSubNet evaluates CreateSubnets for dividing double nodes into subnets
 func (s *SubNetGeneratorTestSuite) TestDoubleNodeMultiSubNet() {
 	// divides 10 nodes into 5 subnets
-	s.SubNetSizeTestHelper(10, 5)
+	s.SubNetSizeTestHelper(10, 5, 0)
+}
+
+/*
+Liked subnets
+*/
+// TestTenNodesTwoSubNet evaluates CreateSubnets for creating two subnets of 10 nodes linked with one node from
+// each one
+func (s *SubNetGeneratorTestSuite) TestTenNodesTwoLinkedSubNet() {
+	// two subnet 10 nodes
+	s.SubNetSizeTestHelper(10, 2, 1)
 }
 
 // SubNetSizeTestHelper creates subnets of different sizes
 // and validates their correct correction
-func (s *SubNetGeneratorTestSuite) SubNetSizeTestHelper(nodesNum, subsNum int) {
-	subnets, idMap, err := CreateSubnets(nodesNum, subsNum, 0)
+func (s *SubNetGeneratorTestSuite) SubNetSizeTestHelper(nodeNum, subNum, linkNum int) {
+	subnets, idMap, err := CreateSubnets(nodeNum, subNum, linkNum)
 	require.NoError(s.Suite.T(), err)
 
-	// iterates on the subnet ids (not nodes)
+	// iterates on the subnet ids (not nodes) in the ids map
 	for subnetID := range subnets {
 		// all subnet ids should lay in range [0, subnetID-1]
-		if subnetID < 0 || subnetID >= nodesNum {
+		if subnetID < 0 || subnetID >= nodeNum {
 			require.Fail(s.Suite.T(), fmt.Sprintf("unidentified subnet id: %d", subnetID))
 		}
-		require.Equal(s.Suite.T(), len(subnets[subnetID]), nodesNum/subsNum)
+		// size of each subnet should be equal to nodeNum/subNum + external links between subnets
+		require.Equal(s.Suite.T(), nodeNum/subNum+(linkNum), len(idMap[subnetID]))
 	}
 
-	// iterates on the subnet ids (not nodes)
+	// iterates on the subnet ids (not nodes) in the nets map
 	for subnetID := range idMap {
 		// all subnet ids should lay in range [0, subnetID-1]
-		if subnetID < 0 || subnetID >= nodesNum {
+		if subnetID < 0 || subnetID >= nodeNum {
 			require.Fail(s.Suite.T(), fmt.Sprintf("unidentified subnet id: %d", subnetID))
 		}
-		require.Equal(s.Suite.T(), len(idMap[subnetID]), nodesNum/subsNum)
+		// size of each subnet should be equal to nodeNum/subNum + external links between subnets
+		require.Equal(s.Suite.T(), (nodeNum/subNum)+(linkNum), len(idMap[subnetID]))
 	}
 }
