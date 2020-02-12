eb21d0a12d573ea3cee042bb504121542dafe0c9
Alexander Hentschel <alex.hentschel@axiomzen.co>
2020-02-11 17:14:44 -0800

    initial commit

---
+++ engine/consensus/hotstuff/follower.go
@@ -0,0 +1,12 @@
+package hotstuff
+
+
+// Follower runs in non-consensus nodes. It informs other components within the node
+// about finalization of blocks. The consensus Follower consumes all block proposals
+// broadcasts by the consensus node, verifies the block header and locally evaluates
+// the finalization rules.
+//
+// CAUTION: Follower is NOT CONCURRENCY safe
+type Follower struct {
+
+}
