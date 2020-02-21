93258e4f3fbd55e8cbd8fd86af094728de5a0774
Max Wolter <awishformore@gmail.com>
2020-02-21 18:02:21 +0100

    CON: populate missing fields on proposal broadcast (#2621)

--- engine/consensus/consensus/engine.go
+++ engine/consensus/consensus/engine.go
@@ -158,6 +158,16 @@ func (e *Engine) BroadcastProposal(header *flow.Header) error {
 		return fmt.Errorf("cannot broadcast proposal with non-local proposer (%x)", header.ProposerID)
 	}
 
+	// get the parent of the block
+	parent, err := e.headers.ByBlockID(header.ParentID)
+	if err != nil {
+		return fmt.Errorf("could not retrieve proposal parent: %w", err)
+	}
+
+	// fill in the fields that can't be populated by HotStuff
+	header.ChainID = parent.ChainID
+	header.Height = parent.Height + 1
+
 	// retrieve the payload for the block
 	payload, err := e.payloads.ByPayloadHash(header.PayloadHash)
 	if err != nil {
