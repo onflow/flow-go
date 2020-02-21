a940627e4b25e932cc8c1b0c27fcd66d9b730949
Jordan Schalm <jordan@dapperlabs.com>
2020-02-20 17:45:26 -0800

    add test stubs

---
+++ module/builder/collection/builder_test.go
@@ -0,0 +1,15 @@
+package collection
+
+import "testing"
+
+func TestNonexistentParent(t *testing.T) {
+	t.Skip()
+}
+
+func TestEmptyMempool(t *testing.T) {
+	t.Skip()
+}
+
+func TestFork(t *testing.T) {
+	t.Skip()
+}
