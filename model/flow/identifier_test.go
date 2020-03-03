706d109db5cb68901726feb0516f3f1082db6316
Jordan Schalm <jordan@dapperlabs.com>
2020-03-02 16:46:22 -0800

    implement fmt.Formatter for flow.Identifier
    This change overrides the default formatting for flow.Identifier and
    allows us to handle %x and %s formatting verbs as hex-encoding the
    identifier.

---
+++ model/flow/identifier_test.go
@@ -0,0 +1,39 @@
+package flow_test
+
+import (
+	"fmt"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+
+	"github.com/dapperlabs/flow-go/utils/unittest"
+)
+
+func TestIdentifierFormat(t *testing.T) {
+	id := unittest.IdentifierFixture()
+
+	// should print hex representation with %x formatting verb
+	t.Run("%x", func(t *testing.T) {
+		formatted := fmt.Sprintf("%x", id)
+		assert.Equal(t, id.String(), formatted)
+	})
+
+	// should print hex representation with %s formatting verb
+	t.Run("%s", func(t *testing.T) {
+		formatted := fmt.Sprintf("%s", id)
+		assert.Equal(t, id.String(), formatted)
+	})
+
+	// should print hex representation with default formatting verb
+	t.Run("%v", func(t *testing.T) {
+		formatted := fmt.Sprintf("%v", id)
+		assert.Equal(t, id.String(), formatted)
+	})
+
+	// should handle unsupported verbs
+	t.Run("unsupported formatting verb", func(t *testing.T) {
+		formatted := fmt.Sprintf("%d", id)
+		expected := fmt.Sprintf("%%!d(flow.Identifier=%s)", id)
+		assert.Equal(t, expected, formatted)
+	})
+}
