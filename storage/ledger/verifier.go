969606ac3aa67a03c77515f237e0b1106566c67a
Ramtin Mehdizadeh Seraj <ramtin@axiomzen.co>
2020-03-03 15:43:55 -0800

    Comment Verifier tests

--- storage/ledger/verifier.go
+++ storage/ledger/verifier.go
@@ -1,12 +1,5 @@
 package ledger
 
-import (
-	"errors"
-
-	"github.com/dapperlabs/flow-go/model/flow"
-	"github.com/dapperlabs/flow-go/storage/ledger/trie"
-)
-
 type TrieVerifier struct {
 	height        int
 	defaultHashes [257][]byte
@@ -22,29 +15,30 @@ func NewTrieVerifier(height int, defaultHashes [257][]byte) *TrieVerifier {
 	}
 }
 
-// VerifyRegistersProof takes in an encoded proof along with registers, state, and values,
-// and verifies if the proofs are correct
-func (v *TrieVerifier) VerifyRegistersProof(
-	registerIDs []flow.RegisterID,
-	stateCommitment flow.StateCommitment,
-	values []flow.RegisterValue,
-	proof []flow.StorageProof,
-) (verified bool, err error) {
-	proofHldr := trie.DecodeProof(proof)
-	length := proofHldr.GetSize()
-	verified = true
-	var verify bool
-	for i := 0; i < length; i++ {
-		flag, singleProof, inclusion, size := proofHldr.ExportProof(i)
-		if inclusion {
-			verify = trie.VerifyInclusionProof(registerIDs[i], values[i], flag, singleProof, size, stateCommitment, v.height)
-		} else {
-			verify = trie.VerifyNonInclusionProof(registerIDs[i], values[i], flag, singleProof, size, stateCommitment, v.height)
-		}
-		if !verify {
-			return verify, errors.New("Incorrect Proof")
-		}
-	}
+// TODO (Ramtin) this is going to be updated in the next PRs
+// // VerifyRegistersProof takes in an encoded proof along with registers, state, and values,
+// // and verifies if the proofs are correct
+// func (v *TrieVerifier) VerifyRegistersProof(
+// 	registerIDs []flow.RegisterID,
+// 	stateCommitment flow.StateCommitment,
+// 	values []flow.RegisterValue,
+// 	proof []flow.StorageProof,
+// ) (verified bool, err error) {
+// 	proofHldr := trie.DecodeProof(proof)
+// 	length := proofHldr.GetSize()
+// 	verified = true
+// 	var verify bool
+// 	for i := 0; i < length; i++ {
+// 		flag, singleProof, inclusion, size := proofHldr.ExportProof(i)
+// 		if inclusion {
+// 			verify = trie.VerifyInclusionProof(registerIDs[i], values[i], flag, singleProof, size, stateCommitment, v.height)
+// 		} else {
+// 			verify = trie.VerifyNonInclusionProof(registerIDs[i], values[i], flag, singleProof, size, stateCommitment, v.height)
+// 		}
+// 		if !verify {
+// 			return verify, errors.New("Incorrect Proof")
+// 		}
+// 	}
 
-	return verified, nil
-}
+// 	return verified, nil
+// }
