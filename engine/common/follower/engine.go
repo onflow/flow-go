1c0281b84853a5775a0e89f9eda6f1f0e870b211
Jordan Schalm <jordan@dapperlabs.com>
2020-03-04 15:35:17 -0800

    add block handling loop for follower engine

--- engine/common/follower/engine.go
+++ engine/common/follower/engine.go
@@ -1,8 +1,10 @@
 package follower
 
 import (
+	"errors"
 	"fmt"
 
+	"github.com/hashicorp/go-multierror"
 	"github.com/rs/zerolog"
 
 	"github.com/dapperlabs/flow-go/engine"
@@ -11,6 +13,7 @@ import (
 	"github.com/dapperlabs/flow-go/module"
 	"github.com/dapperlabs/flow-go/network"
 	protocol "github.com/dapperlabs/flow-go/protocol/badger"
+	"github.com/dapperlabs/flow-go/storage"
 )
 
 type Engine struct {
@@ -19,9 +22,19 @@ type Engine struct {
 	me    module.Local
 	state protocol.State
 	con   network.Conduit
-	//cache map[flow.Identifier][]cacheItem
+
+	headers  storage.Headers
+	payloads storage.Payloads
+
+	cache      map[flow.Identifier][]cacheItem // pending block cache, keyed by parent ID
+	cacheDedup map[flow.Identifier]struct{}    // prevent dupes in cache
 
 	follower module.HotStuffFollower
+}
+
+type cacheItem struct {
+	OriginID flow.Identifier
+	Proposal *messages.ClusterBlockProposal
 }
 
 func New(
@@ -29,14 +42,21 @@ func New(
 	net module.Network,
 	me module.Local,
 	state protocol.State,
+	headers storage.Headers,
+	payloads storage.Payloads,
 	follower module.HotStuffFollower,
 ) (*Engine, error) {
 
 	e := &Engine{
-		unit:  engine.NewUnit(),
-		log:   log.With().Str("engine", "follower").Logger(),
-		me:    me,
-		state: state,
+		unit:       engine.NewUnit(),
+		log:        log.With().Str("engine", "follower").Logger(),
+		me:         me,
+		state:      state,
+		headers:    headers,
+		payloads:   payloads,
+		follower:   follower,
+		cache:      make(map[flow.Identifier][]cacheItem),
+		cacheDedup: make(map[flow.Identifier]struct{}),
 	}
 
 	return e, nil
@@ -87,13 +107,94 @@ func (e *Engine) Process(originID flow.Identifier, event interface{}) error {
 
 func (e *Engine) process(originID flow.Identifier, event interface{}) error {
 	switch ev := event.(type) {
-	case *messages.BlockProposal:
-		return e.onBlockProposal(originID, ev)
+	case *flow.Block:
+		return e.onBlock(originID, ev)
 	default:
 		return fmt.Errorf("invalid event type (%T)", event)
 	}
 }
 
-func (e *Engine) onBlockProposal(originID flow.Identifier, proposal *messages.BlockProposal) error {
-	panic("TODO")
+func (e *Engine) onBlock(originID flow.Identifier, block *flow.Block) error {
+
+	// retrieve the parent for the block, cache if not found
+	parent, err := e.headers.ByBlockID(block.ParentID)
+	if errors.Is(err, storage.ErrNotFound) {
+		return e.processPendingBlock(originID, block)
+	}
+	if err != nil {
+		return fmt.Errorf("could not retrieve block parent: %w", err)
+	}
+
+	// store the block payload
+	err = e.payloads.Store(&block.Header, &block.Payload)
+	if err != nil {
+		return fmt.Errorf("could not store block payload: %w", err)
+	}
+
+	// insert the block header
+	err = e.headers.Store(&block.Header)
+	if err != nil {
+		return fmt.Errorf("could not store block header: %w", err)
+	}
+
+	// ensure the block is a valid extension of protocol state
+	err = e.state.Mutate().Extend(block.ID())
+	if err != nil {
+		return fmt.Errorf("could not extend protocol state: %w", err)
+	}
+
+	// submit the model to hotstuff finalization logic
+	e.follower.SubmitProposal(&block.Header, parent.View)
+
+	// check for any descendants of the block that are now processable
+	children, ok := e.cache[block.ID()]
+	if !ok {
+		return nil
+	}
+
+	// then try to process children only this once
+	var result *multierror.Error
+	for _, child := range children {
+		err := e.onBlock(child.OriginID, child.Proposal)
+	}
+
+	return nil
+}
+
+func (e *Engine) processPendingBlock(originID flow.Identifier, block *flow.Block) error {
+
+}
+
+// Caches a pending proposal in the block buffer cache, keyed by the block's
+// parent ID.
+func (e *Engine) cachePendingProposal(originID flow.Identifier, proposal *messages.ClusterBlockProposal) {
+
+	blockID := proposal.Header.ID()
+	parentID := proposal.Header.ParentID
+
+	item := cacheItem{
+		OriginID: originID,
+		Proposal: proposal,
+	}
+
+	e.cache[parentID] = append(e.cache[parentID], item)
+	e.cacheDedup[blockID] = struct{}{}
+}
+
+// Returns true if the proposal with the given block ID has been cached.
+func (e *Engine) isPendingProposalCached(blockID flow.Identifier) bool {
+	_, cached := e.cacheDedup[blockID]
+	return cached
+}
+
+// Removes from the pending proposal cache all the children of the block with
+// the given ID. Since buffered blocks are keyed by parent, this function
+// should be called when the parent for a set of children is received.
+func (e *Engine) dropPendingProposalsWithParent(blockID flow.Identifier) {
+
+	children := e.cache[blockID]
+	for _, child := range children {
+		delete(e.cacheDedup, child.Proposal.Header.ID())
+	}
+	delete(e.cache, blockID)
 }
