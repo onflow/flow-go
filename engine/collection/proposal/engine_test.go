6f71c185aea8aa20d315603e075aa9284b510fdd
Jordan Schalm <jordan@dapperlabs.com>
2020-03-05 12:06:54 -0800

    stub out proposal tests, use suite

--- engine/collection/proposal/engine_test.go
+++ engine/collection/proposal/engine_test.go
@@ -8,6 +8,7 @@ import (
 	"github.com/rs/zerolog"
 	"github.com/stretchr/testify/mock"
 	"github.com/stretchr/testify/require"
+	"github.com/stretchr/testify/suite"
 
 	"github.com/dapperlabs/flow-go/consensus/coldstuff"
 	"github.com/dapperlabs/flow-go/model/flow"
@@ -21,8 +22,9 @@ import (
 	"github.com/dapperlabs/flow-go/utils/unittest"
 )
 
-// testcontext contains the context for a test case.
-type testcontext struct {
+type Suite struct {
+	suite.Suite
+
 	state        *protocol.State
 	snapshot     *protocol.Snapshot
 	me           *module.Local
@@ -36,59 +38,47 @@ type testcontext struct {
 	finalizer    *module.Finalizer
 }
 
-// WithEngine initializes the dependencies for a test case, then runs the test
-// case with the dependencies and initialized engine.
-func WithEngine(t *testing.T, run func(testcontext, *Engine)) {
-	var ctx testcontext
+func TestProposalEngine(t *testing.T) {
+	suite.Run(t, new(Suite))
+}
 
+func (suite *Suite) SetupTest() {
 	log := zerolog.New(os.Stderr)
 	tracer, err := trace.NewTracer(log)
-	require.NoError(t, err)
+	require.NoError(suite.T(), err)
 
-	ctx.state = new(protocol.State)
-	ctx.snapshot = new(protocol.Snapshot)
-	ctx.state.On("Final").Return(ctx.snapshot)
-	ctx.snapshot.On("Head").Return(&flow.Header{}, nil)
-	ctx.snapshot.On("Identities", mock.Anything).Return(unittest.IdentityListFixture(1), nil)
-	ctx.me = new(module.Local)
-	ctx.me.On("NodeID").Return(flow.Identifier{})
+	suite.state = new(protocol.State)
+	suite.snapshot = new(protocol.Snapshot)
+	suite.state.On("Final").Return(suite.snapshot)
+	suite.snapshot.On("Head").Return(&flow.Header{}, nil)
+	suite.snapshot.On("Identities", mock.Anything).Return(unittest.IdentityListFixture(1), nil)
+	suite.me = new(module.Local)
+	suite.me.On("NodeID").Return(flow.Identifier{})
 
 	hub := stub.NewNetworkHub()
-	ctx.net = stub.NewNetwork(ctx.state, ctx.me, hub)
+	suite.net = stub.NewNetwork(suite.state, suite.me, hub)
 
-	ctx.provider = new(network.Engine)
-	ctx.pool = new(mempool.Transactions)
-	ctx.transactions = new(storage.Transactions)
-	ctx.headers = new(storage.Headers)
-	ctx.payloads = new(storage.ClusterPayloads)
-	ctx.builder = new(module.Builder)
-	ctx.finalizer = new(module.Finalizer)
+	suite.provider = new(network.Engine)
+	suite.pool = new(mempool.Transactions)
+	suite.transactions = new(storage.Transactions)
+	suite.headers = new(storage.Headers)
+	suite.payloads = new(storage.ClusterPayloads)
+	suite.builder = new(module.Builder)
+	suite.finalizer = new(module.Finalizer)
 
-	eng, err := New(log, ctx.net, ctx.me, ctx.state, tracer, ctx.provider, ctx.pool, ctx.transactions, ctx.headers, ctx.payloads)
-	require.NoError(t, err)
+	eng, err := New(log, suite.net, suite.me, suite.state, tracer, suite.provider, suite.pool, suite.transactions, suite.headers, suite.payloads)
+	require.NoError(suite.T(), err)
 
-	cold, err := coldstuff.New(log, ctx.state, ctx.me, eng, ctx.builder, ctx.finalizer, time.Second, time.Second)
-	require.NoError(t, err)
+	cold, err := coldstuff.New(log, suite.state, suite.me, eng, suite.builder, suite.finalizer, time.Second, time.Second)
+	require.NoError(suite.T(), err)
 
-	run(ctx, eng.WithConsensus(cold))
+	eng.coldstuff = cold
 }
 
-func TestStartStop(t *testing.T) {
-	WithEngine(t, func(_ testcontext, e *Engine) {
-		ready := e.Ready()
+func (suite *Suite) TestHandleProposal() {}
 
-		select {
-		case <-ready:
-		case <-time.After(time.Second):
-			t.Fail()
-		}
+func (suite *Suite) TestHandlePendingProposal() {}
 
-		done := e.Done()
+func (suite *Suite) TestHandleProposalWithPendingChildren() {}
 
-		select {
-		case <-done:
-		case <-time.After(time.Second):
-			t.Fail()
-		}
-	})
-}
+func (suite *Suite) TestReceiveVote() {}
