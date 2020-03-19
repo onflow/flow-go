2b130b817bf9bb6fd762e802cc241d60a9bfc2da
Kay-Zee <kan@axiomzen.co>
2020-03-18 17:10:37 -0700

    Add comment/reference

--- utils/unittest/unittest.go
+++ utils/unittest/unittest.go
@@ -51,6 +51,7 @@ func TempBadgerDB(t *testing.T) *badger.DB {
 	dir, err := tempDBDir()
 	require.Nil(t, err)
 
+	// Ref: https://github.com/dgraph-io/badger#memory-usage
 	db, err := badger.Open(badger.DefaultOptions(dir).WithLogger(nil).WithValueLogLoadingMode(options.FileIO))
 	require.Nil(t, err)
 
