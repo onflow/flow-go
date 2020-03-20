7d24cd77b69eb6f65507a81b48e3ef64c9a1c207
Jordan Schalm <jordan@dapperlabs.com>
2020-03-20 12:15:39 -0700

    add fixture for sdk transaction

--- utils/unittest/fixtures.go
+++ utils/unittest/fixtures.go
@@ -5,6 +5,8 @@ import (
 	"math/rand"
 	"time"
 
+	sdk "github.com/dapperlabs/flow-go-sdk"
+
 	"github.com/dapperlabs/flow-go/crypto"
 	"github.com/dapperlabs/flow-go/engine/execution"
 	"github.com/dapperlabs/flow-go/engine/execution/state"
@@ -361,6 +363,22 @@ func TransactionBodyFixture(opts ...func(*flow.TransactionBody)) flow.Transactio
 	}
 
 	return tb
+}
+
+func SDKTransactionFixture(opts ...func(*sdk.Transaction)) sdk.Transaction {
+	tx := sdk.Transaction{
+		Script:             []byte("fun main() {}"),
+		ReferenceBlockHash: []byte{1, 2, 3, 4},
+		Nonce:              rand.Uint64(),
+		ComputeLimit:       10,
+		PayerAccount:       sdk.RootAddress,
+	}
+
+	for _, apply := range opts {
+		apply(&tx)
+	}
+
+	return tx
 }
 
 // CompleteExecutionResultFixture returns complete execution result with an
