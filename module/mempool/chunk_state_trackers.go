d42b9c21f1013ebbd1e267ec4579d74b9e8b5aba
Yahya <yahya@dapperlabs.com>
2020-03-25 11:36:58 -0700

    Adds chunk state tracker

---
+++ module/mempool/chunk_state_trackers.go
@@ -0,0 +1,25 @@
+package mempool
+
+import (
+	"github.com/dapperlabs/flow-go/model/flow"
+	"github.com/dapperlabs/flow-go/model/verification/tracker"
+)
+
+// ChunkStateTrackers represents a concurrency-safe memory pool of chunk states
+type ChunkStatePackTrackers interface {
+
+	// Add will add the given chunk state tracker.
+	Add(cst *tracker.ExecutionStateTracker) error
+
+	// Has checks if the given chunkID has a tracker in mempool.
+	Has(chunkID flow.Identifier) bool
+
+	// Rem removes tracker with the given chunk ID.
+	Rem(chunkID flow.Identifier) bool
+
+	// ByChunkID returns the chunk state tracker for the given chunk ID.
+	ByChunkID(chunkID flow.Identifier) (*tracker.ExecutionStateTracker, error)
+
+	// All will return a list of chunk state trackers in mempool.
+	All() []*tracker.ExecutionStateTracker
+}
