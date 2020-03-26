42bd54a8d3fea22b2873a0cd21b718175d7fdf8a
Leo Zhang <zhangchiqing@gmail.com>
2020-03-25 17:28:17 -0700

    [HotStuff] add storage functions to find unfinalized blocks (#2974)
    * add FindDecendants
    
    * add test cases for FindDescendants
    
    * add more test cases
    
    * handle error
    
    * findDescendants should start from finalized block's view + 1
    
    * Apply suggestions from code review
    
    Co-Authored-By: Jordan Schalm <jordan@dapperlabs.com>
    
    * replace iterateKey with keyonly
    
    * rename function name
    
    Co-authored-by: Jordan Schalm <jordan@dapperlabs.com>

--- storage/badger/operation/guarantees.go
+++ storage/badger/operation/guarantees.go
@@ -1,6 +1,8 @@
 package operation
 
 import (
+	"math"
+
 	"github.com/dgraph-io/badger/v2"
 
 	"github.com/dapperlabs/flow-go/model/flow"
@@ -36,4 +38,13 @@ func VerifyGuaranteePayload(height uint64, blockID flow.Identifier, collIDs []fl
 // set that already exist in an ancestor block.
 func CheckGuaranteePayload(height uint64, blockID flow.Identifier, candidateIDs []flow.Identifier, invalidIDs *map[flow.Identifier]struct{}) func(*badger.Txn) error {
 	return iterate(makePrefix(codeIndexGuarantee, height), makePrefix(codeIndexGuarantee, uint64(0)), searchduplicates(blockID, candidateIDs, invalidIDs))
+}
+
+// FindDescendants uses guarantee payload index to find all the incorporated descendants for a given block.
+// Useful for finding all the unfinalized and incorporated blocks
+// "Incorporated" means a block connects to a given block through a certain number of intermediate blocks
+// Note: the descendants doesn't include the blockID itself
+func FindDescendants(height uint64, blockID flow.Identifier, descendants *[]flow.Identifier) func(*badger.Txn) error {
+	// height + 1 to exclude the blockID, and all blocks at the same height
+	return iterate(makePrefix(codeIndexGuarantee, height+1), makePrefix(codeIndexGuarantee, uint64(math.MaxUint64)), keyonly(finddescendant(blockID, descendants)))
 }
