a9b4967e55c8c59c99ca0c9dc7ce1a2837f7404e
Jordan Schalm <jordan@dapperlabs.com>
2020-03-31 13:16:23 -0700

    add note on %v replacement

--- cmd/bootstrap/cmd/constants.go
+++ cmd/bootstrap/cmd/constants.go
@@ -7,7 +7,7 @@ const (
 
 	DirnameExecutionState         = "execution-state"
 	FilenameGenesisBlock          = "genesis-block.json"
-	FilenameGenesisClusterBlock   = "%v.genesis-cluster-block.json"
+	FilenameGenesisClusterBlock   = "%v.genesis-cluster-block.json" // %v will be replaced by cluster ID
 	FilenameNodeInfosPub          = "node-infos.pub.json"
 	FilenameNodeInfoPriv          = "%v.node-info.priv.json"     // %v will be replaced by NodeID
 	FilenameNodeInfoPub           = "%v.node-info.pub.json"      // %v will be replaced by NodeID
@@ -16,5 +16,5 @@ const (
 	FilenameDKGDataPub            = "dkg-data.pub.json"
 	FilenameAccount0Priv          = "account-0.priv.json"
 	FilenameGenesisQC             = "genesis-qc.json"
-	FilenameGenesisClusterQC      = "%v.genesis-cluster-qc.json"
+	FilenameGenesisClusterQC      = "%v.genesis-cluster-qc.json" // %v will be replaced by cluster ID
 )
