205d1946ed224a5a49cc9aac143ffc8183799ed3
Jordan Schalm <jordan@dapperlabs.com>
2020-03-31 16:52:45 -0700

    add enough nodes to mvp test (for dkg)

--- integration/tests/mvp_test.go
+++ integration/tests/mvp_test.go
@@ -16,14 +16,15 @@ import (
 
 func TestMVP_Network(t *testing.T) {
 
-	t.Skip()
-
 	colNode := testnet.NewNodeConfig(flow.RoleCollection)
+	exeNode := testnet.NewNodeConfig(flow.RoleExecution)
 
 	net := []*testnet.NodeConfig{
 		colNode,
+		exeNode,
 		testnet.NewNodeConfig(flow.RoleConsensus),
-		testnet.NewNodeConfig(flow.RoleExecution),
+		testnet.NewNodeConfig(flow.RoleConsensus),
+		testnet.NewNodeConfig(flow.RoleConsensus),
 		testnet.NewNodeConfig(flow.RoleVerification),
 	}
 
@@ -43,7 +44,7 @@ func TestMVP_Network(t *testing.T) {
 	colNodeAPIPort := colContainer.Ports[testnet.ColNodeAPIPort]
 	require.NotEqual(t, "", colNodeAPIPort)
 
-	exeContainer, ok := flowNetwork.ContainerByID(colNode.Identifier)
+	exeContainer, ok := flowNetwork.ContainerByID(exeNode.Identifier)
 	require.True(t, ok)
 	exeNodeAPIPort := exeContainer.Ports[testnet.ExeNodeAPIPort]
 	require.NotEqual(t, "", exeNodeAPIPort)
