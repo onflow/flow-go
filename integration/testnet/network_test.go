b3351e12ef90cc10f32ba5bce8abdfe94c5a5926
Jordan Schalm <jordan@dapperlabs.com>
2020-04-01 17:56:34 -0700

    fix testnet unit tests

--- integration/testnet/network_test.go
+++ integration/testnet/network_test.go
@@ -23,6 +23,8 @@ func TestNetworkSetupBasic(t *testing.T) {
 	net := []*testnet.NodeConfig{
 		testnet.NewNodeConfig(flow.RoleCollection),
 		testnet.NewNodeConfig(flow.RoleConsensus),
+		testnet.NewNodeConfig(flow.RoleConsensus),
+		testnet.NewNodeConfig(flow.RoleConsensus),
 		testnet.NewNodeConfig(flow.RoleExecution),
 		testnet.NewNodeConfig(flow.RoleVerification),
 	}
@@ -38,6 +40,8 @@ func TestNetworkSetupBasic(t *testing.T) {
 	expectedData := []nodeInfo{
 		{image: "gcr.io/dl-flow/collection:latest", name: "collection_1", address: "collection_1:2137"},
 		{image: "gcr.io/dl-flow/consensus:latest", name: "consensus_1", address: "consensus_1:2137"},
+		{image: "gcr.io/dl-flow/consensus:latest", name: "consensus_2", address: "consensus_2:2137"},
+		{image: "gcr.io/dl-flow/consensus:latest", name: "consensus_3", address: "consensus_3:2137"},
 		{image: "gcr.io/dl-flow/execution:latest", name: "execution_1", address: "execution_1:2137"},
 		{image: "gcr.io/dl-flow/verification:latest", name: "verification_1", address: "verification_1:2137"},
 	}
@@ -51,6 +55,9 @@ func TestNetworkSetupMultipleNodes(t *testing.T) {
 		testnet.NewNodeConfig(flow.RoleCollection),
 		testnet.NewNodeConfig(flow.RoleCollection),
 		testnet.NewNodeConfig(flow.RoleCollection),
+		testnet.NewNodeConfig(flow.RoleConsensus),
+		testnet.NewNodeConfig(flow.RoleConsensus),
+		testnet.NewNodeConfig(flow.RoleConsensus),
 		testnet.NewNodeConfig(flow.RoleVerification),
 		testnet.NewNodeConfig(flow.RoleVerification),
 		testnet.NewNodeConfig(flow.RoleVerification),
@@ -69,6 +76,9 @@ func TestNetworkSetupMultipleNodes(t *testing.T) {
 		{image: "gcr.io/dl-flow/collection:latest", name: "collection_1", address: "collection_1:2137"},
 		{image: "gcr.io/dl-flow/collection:latest", name: "collection_2", address: "collection_2:2137"},
 		{image: "gcr.io/dl-flow/collection:latest", name: "collection_3", address: "collection_3:2137"},
+		{image: "gcr.io/dl-flow/consensus:latest", name: "consensus_1", address: "consensus_1:2137"},
+		{image: "gcr.io/dl-flow/consensus:latest", name: "consensus_2", address: "consensus_2:2137"},
+		{image: "gcr.io/dl-flow/consensus:latest", name: "consensus_3", address: "consensus_3:2137"},
 		{image: "gcr.io/dl-flow/verification:latest", name: "verification_1", address: "verification_1:2137"},
 		{image: "gcr.io/dl-flow/verification:latest", name: "verification_2", address: "verification_2:2137"},
 		{image: "gcr.io/dl-flow/verification:latest", name: "verification_3", address: "verification_3:2137"},
@@ -84,7 +94,7 @@ func getNodeInfos(flowNetwork *testnet.FlowNetwork) []nodeInfo {
 	for i, container := range flowNetwork.Containers {
 		realData[i] = nodeInfo{
 			image:   container.Image,
-			name:    container.Name,
+			name:    container.Name(),
 			address: container.Identity.Address,
 		}
 	}
