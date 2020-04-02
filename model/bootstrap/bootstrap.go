48aa76d36e0accfc232cdbfd1df9d9a50afc96f5
Jordan Schalm <jordan@dapperlabs.com>
2020-04-02 13:07:14 -0700

    create bootstrap.NodeInfo as canonical bootstrap node
    move key tests over

--- model/bootstrap/bootstrap.go
+++ model/bootstrap/bootstrap.go
@@ -3,11 +3,13 @@ package bootstrap
 
 import (
 	"github.com/dapperlabs/flow-go/consensus/hotstuff"
+	"github.com/dapperlabs/flow-go/crypto"
 	"github.com/dapperlabs/flow-go/model/flow"
 )
 
-// TODO consolidate with bootstrap model and integration NodeConfig
-type NodeConfig struct {
+// Defines the canonical structure for private node info.
+// NOTE: should be used only for encoding.
+type nodeInfoPriv struct {
 	Role           flow.Role
 	Address        string
 	NodeID         flow.Identifier
@@ -15,37 +17,100 @@ type NodeConfig struct {
 	StakingPrivKey EncodableStakingPrivKey
 }
 
-// TODO consolidate with bootstrap model ( and maybe hotstuff dkg )
-type DKGParticipantPub struct {
-	ID          flow.Identifier
-	PubKeyShare EncodableRandomBeaconPubKey
-	GroupIndex  int
+// Defines the canonical structure for public node info.
+// NOTE: should be used only for encoding.
+type nodeInfoPub struct {
+	Role          flow.Role
+	Address       string
+	NodeID        flow.Identifier
+	Stake         uint64
+	NetworkPubKey EncodableNetworkPubKey
+	StakingPubKey EncodableStakingPubKey
 }
 
-type DKGParticipantPriv struct {
-	ID           flow.Identifier
-	PrivKeyShare EncodableRandomBeaconPrivKey
-	GroupIndex   int
+// NodeInfo contains configuration information for a node, including private
+// information. This is used during the bootstrapping process to represent each
+// node. When writing node information to disk, use `Public` or `Private`.
+type NodeInfo struct {
+	NodeID         flow.Identifier
+	Role           flow.Role
+	Address        string
+	Stake          uint64
+	NetworkPrivKey crypto.PrivateKey
+	StakingPrivKey crypto.PrivateKey
 }
 
-type DKGPubData struct {
-	GroupPubKey  EncodableRandomBeaconPubKey
-	Participants []DKGParticipantPub
+func (node NodeInfo) Private() nodeInfoPriv {
+	return nodeInfoPriv{
+		Role:           node.Role,
+		Address:        node.Address,
+		NodeID:         node.NodeID,
+		NetworkPrivKey: EncodableNetworkPrivKey{PrivateKey: node.NetworkPrivKey},
+		StakingPrivKey: EncodableStakingPrivKey{PrivateKey: node.StakingPrivKey},
+	}
 }
 
-func (pub *DKGPubData) ForHotStuff() *hotstuff.DKGPublicData {
+// Public returns the canonical public structure
+func (node NodeInfo) Public() nodeInfoPub {
+	return nodeInfoPub{
+		Role:          node.Role,
+		Address:       node.Address,
+		NodeID:        node.NodeID,
+		Stake:         node.Stake,
+		NetworkPubKey: EncodableNetworkPubKey{PublicKey: node.NetworkPrivKey.PublicKey()},
+		StakingPubKey: EncodableStakingPubKey{PublicKey: node.StakingPrivKey.PublicKey()},
+	}
+}
+
+// Identity returns the node info as a public Flow identity.
+func (node NodeInfo) Identity() *flow.Identity {
+	identity := &flow.Identity{
+		NodeID:        node.NodeID,
+		Address:       node.Address,
+		Role:          node.Role,
+		Stake:         node.Stake,
+		StakingPubKey: node.StakingPrivKey.PublicKey(),
+		NetworkPubKey: node.NetworkPrivKey.PublicKey(),
+	}
+	return identity
+}
+
+// NodeInfoFromIdentity converts a public Flow identity to a node info.
+func NodeInfoFromIdentity(identity *flow.Identity) NodeInfo {
+	return NodeInfo{
+		NodeID:         identity.NodeID,
+		Role:           identity.Role,
+		Address:        identity.Address,
+		Stake:          identity.Stake,
+		NetworkPrivKey: nil,
+		StakingPrivKey: nil,
+	}
+}
+
+type DKGParticipant struct {
+	NodeID     flow.Identifier
+	KeyShare   crypto.PrivateKey
+	GroupIndex int
+}
+
+type DKGData struct {
+	GroupPubKey  crypto.PublicKey
+	Participants []DKGParticipant
+}
+
+func (dkg *DKGData) ForHotStuff() *hotstuff.DKGPublicData {
 
 	participantLookup := make(map[flow.Identifier]*hotstuff.DKGParticipant)
-	for _, part := range pub.Participants {
-		participantLookup[part.ID] = &hotstuff.DKGParticipant{
-			Id:             part.ID,
-			PublicKeyShare: part.PubKeyShare.PublicKey,
+	for _, part := range dkg.Participants {
+		participantLookup[part.NodeID] = &hotstuff.DKGParticipant{
+			Id:             part.NodeID,
+			PublicKeyShare: part.KeyShare.PublicKey(),
 			DKGIndex:       part.GroupIndex,
 		}
 	}
 
 	return &hotstuff.DKGPublicData{
-		GroupPubKey:           pub.GroupPubKey.PublicKey,
+		GroupPubKey:           dkg.GroupPubKey,
 		IdToDKGParticipantMap: participantLookup,
 	}
 }
---
+++ model/bootstrap/keys_test.go
@@ -0,0 +1,188 @@
+package bootstrap
+
+import (
+	"crypto/rand"
+	"encoding/json"
+	"testing"
+
+	"github.com/stretchr/testify/require"
+
+	"github.com/dapperlabs/flow-go/crypto"
+)
+
+func TestEncodableNetworkPubKey(t *testing.T) {
+	netw, err := crypto.GeneratePrivateKey(crypto.ECDSA_SECp256k1, generateRandomSeed(t))
+	require.NoError(t, err)
+	key := EncodableNetworkPubKey{netw.PublicKey()}
+
+	enc, err := json.Marshal(key)
+	require.NoError(t, err)
+	require.NotEmpty(t, enc)
+
+	var dec EncodableNetworkPubKey
+	err = json.Unmarshal(enc, &dec)
+	require.NoError(t, err)
+	require.True(t, key.Equals(dec.PublicKey))
+}
+
+func TestEncodableNetworkPubKeyNil(t *testing.T) {
+	key := EncodableNetworkPubKey{}
+
+	enc, err := json.Marshal(key)
+	require.NoError(t, err)
+	require.Equal(t, "null", string(enc))
+
+	var dec EncodableNetworkPubKey
+	err = json.Unmarshal(enc, &dec)
+	require.NoError(t, err)
+	require.Equal(t, key, dec)
+}
+
+func TestEncodableNetworkPrivKey(t *testing.T) {
+	netw, err := crypto.GeneratePrivateKey(crypto.ECDSA_P256, generateRandomSeed(t))
+	require.NoError(t, err)
+	key := EncodableNetworkPrivKey{netw}
+
+	enc, err := json.Marshal(key)
+	require.NoError(t, err)
+	require.NotEmpty(t, enc)
+
+	var dec EncodableNetworkPrivKey
+	err = json.Unmarshal(enc, &dec)
+	require.NoError(t, err)
+	require.True(t, key.Equals(dec.PrivateKey))
+}
+
+func TestEncodableNetworkPrivKeyNil(t *testing.T) {
+	key := EncodableNetworkPrivKey{}
+
+	enc, err := json.Marshal(key)
+	require.NoError(t, err)
+	require.Equal(t, "null", string(enc))
+
+	var dec EncodableNetworkPrivKey
+	err = json.Unmarshal(enc, &dec)
+	require.NoError(t, err)
+	require.Equal(t, key, dec)
+}
+
+func TestEncodableStakingPubKey(t *testing.T) {
+	stak, err := crypto.GeneratePrivateKey(crypto.BLS_BLS12381, generateRandomSeed(t))
+	require.NoError(t, err)
+	key := EncodableStakingPubKey{stak.PublicKey()}
+
+	enc, err := json.Marshal(key)
+	require.NoError(t, err)
+	require.NotEmpty(t, enc)
+
+	var dec EncodableStakingPubKey
+	err = json.Unmarshal(enc, &dec)
+	require.NoError(t, err)
+	require.True(t, key.Equals(dec.PublicKey))
+}
+
+func TestEncodableStakingPubKeyNil(t *testing.T) {
+	key := EncodableStakingPubKey{}
+
+	enc, err := json.Marshal(key)
+	require.NoError(t, err)
+	require.Equal(t, "null", string(enc))
+
+	var dec EncodableStakingPubKey
+	err = json.Unmarshal(enc, &dec)
+	require.NoError(t, err)
+	require.Equal(t, key, dec)
+}
+
+func TestEncodableStakingPrivKey(t *testing.T) {
+	stak, err := crypto.GeneratePrivateKey(crypto.BLS_BLS12381, generateRandomSeed(t))
+	require.NoError(t, err)
+	key := EncodableStakingPrivKey{stak}
+
+	enc, err := json.Marshal(key)
+	require.NoError(t, err)
+	require.NotEmpty(t, enc)
+
+	var dec EncodableStakingPrivKey
+	err = json.Unmarshal(enc, &dec)
+	require.NoError(t, err)
+
+	require.True(t, key.Equals(dec.PrivateKey), "encoded/decoded key equality check failed")
+}
+
+func TestEncodableStakingPrivKeyNil(t *testing.T) {
+	key := EncodableStakingPrivKey{}
+
+	enc, err := json.Marshal(key)
+	require.NoError(t, err)
+	require.Equal(t, "null", string(enc))
+
+	var dec EncodableStakingPrivKey
+	err = json.Unmarshal(enc, &dec)
+	require.NoError(t, err)
+	require.Equal(t, key, dec)
+}
+
+func TestEncodableRandomBeaconPubKey(t *testing.T) {
+	randbeac, err := crypto.GeneratePrivateKey(crypto.BLS_BLS12381, generateRandomSeed(t))
+	require.NoError(t, err)
+	key := EncodableRandomBeaconPubKey{randbeac.PublicKey()}
+
+	enc, err := json.Marshal(key)
+	require.NoError(t, err)
+	require.NotEmpty(t, enc)
+
+	var dec EncodableRandomBeaconPubKey
+	err = json.Unmarshal(enc, &dec)
+	require.NoError(t, err)
+	require.True(t, key.Equals(dec.PublicKey))
+}
+
+func TestEncodableRandomBeaconPubKeyNil(t *testing.T) {
+	key := EncodableRandomBeaconPubKey{}
+
+	enc, err := json.Marshal(key)
+	require.NoError(t, err)
+	require.Equal(t, "null", string(enc))
+
+	var dec EncodableRandomBeaconPubKey
+	err = json.Unmarshal(enc, &dec)
+	require.NoError(t, err)
+	require.Equal(t, key, dec)
+}
+
+func TestEncodableRandomBeaconPrivKey(t *testing.T) {
+	randbeac, err := crypto.GeneratePrivateKey(crypto.BLS_BLS12381, generateRandomSeed(t))
+	require.NoError(t, err)
+	key := EncodableRandomBeaconPrivKey{randbeac}
+
+	enc, err := json.Marshal(key)
+	require.NoError(t, err)
+	require.NotEmpty(t, enc)
+
+	var dec EncodableRandomBeaconPrivKey
+	err = json.Unmarshal(enc, &dec)
+	require.NoError(t, err)
+
+	require.True(t, key.Equals(dec.PrivateKey), "encoded/decoded key equality check failed")
+}
+
+func TestEncodableRandomBeaconPrivKeyNil(t *testing.T) {
+	key := EncodableRandomBeaconPrivKey{}
+
+	enc, err := json.Marshal(key)
+	require.NoError(t, err)
+	require.Equal(t, "null", string(enc))
+
+	var dec EncodableRandomBeaconPrivKey
+	err = json.Unmarshal(enc, &dec)
+	require.NoError(t, err)
+	require.Equal(t, key, dec)
+}
+
+func generateRandomSeed(t *testing.T) []byte {
+	seed := make([]byte, 48)
+	_, err := rand.Read(seed)
+	require.Nil(t, err)
+	return seed
+}
