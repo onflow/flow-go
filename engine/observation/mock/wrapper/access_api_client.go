654bfa76a074242495c183d8fa016c4fd09b69ac
vishalchangraniaxiom <56943838+vishalchangraniaxiom@users.noreply.github.com>
2020-04-06 22:30:39 -0700

    renaming observation node to access node (#3170)

--- engine/observation/mock/wrapper/access_api_client.go
+++
@@ -1,9 +0,0 @@
-package wrapper
-
-import "github.com/dapperlabs/flow/protobuf/go/flow/access"
-
-// AccessAPIClient allows for generation of a mock (via mockery) for the real AccessAPIClient imported as a dependency
-// from the flow repo
-type AccessAPIClient interface {
-	access.AccessAPIClient
-}
