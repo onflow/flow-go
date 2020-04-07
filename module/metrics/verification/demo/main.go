446eb791018cc3e509284a82b69a15ab05149593
Yahya <yahya@dapperlabs.com>
2020-04-06 17:34:18 -0700

    Fixes comment

--- module/metrics/verification/demo/main.go
+++ module/metrics/verification/demo/main.go
@@ -14,7 +14,6 @@ func main() {
 	logger := zerolog.New(os.Stderr).With().Timestamp().Logger()
 	port := 3030
 
-	// to notify the s
 	exitSig := make(chan os.Signal, 1)
 	signal.Notify(exitSig, os.Interrupt, syscall.SIGTERM)
 	verification.Demo(logger, port, exitSig)
