d5586533cca080d83aa54c61a3b8ee7865385a12
Philip Stanislaus <6912756+philipstanislaus@users.noreply.github.com>
2020-04-18 00:56:04 +0200

    Move signing tag of execution result and result approval into constants file

--- engine/execution/utils/hasher.go
+++ engine/execution/utils/hasher.go
@@ -3,11 +3,12 @@ package utils
 import (
 	"github.com/dapperlabs/flow-go/crypto"
 	"github.com/dapperlabs/flow-go/crypto/hash"
+	"github.com/dapperlabs/flow-go/model/encoding"
 )
 
 // NewExecutionReceiptHasher generates and returns a hasher for signing
 // and verification of execution receipts
 func NewExecutionReceiptHasher() hash.Hasher {
-	h := crypto.NewBLS_KMAC("execution receipt")
+	h := crypto.NewBLS_KMAC(encoding.ExecutionReceiptTag)
 	return h
 }
