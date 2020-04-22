3fc0d80cac398fb96a97e19ab07430678668557d
Yahya <yahya@dapperlabs.com>
2020-04-22 14:00:10 -0700

    Changes log to debug

--- engine/verification/ingest/engine.go
+++ engine/verification/ingest/engine.go
@@ -388,7 +388,7 @@ func (e *Engine) requestChunkDataPack(chunkID flow.Identifier, blockID flow.Iden
 		return fmt.Errorf("could not submit request for collection (id=%s): %w", chunkID, err)
 	}
 
-	e.log.Info().
+	e.log.Debug().
 		Hex("chunk_id", logging.ID(chunkID)).
 		Msg("chunk data pack request submitted")
 
