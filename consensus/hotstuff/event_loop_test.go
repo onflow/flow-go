0885bc04cfb66d17ec986d26e7ede7cb5eba5a38
Leo Zhang (zhangchiqing) <zhangchiqing@gmail.com>
2020-04-23 15:41:59 -0700

    add event loop ready done test

--- consensus/hotstuff/event_loop_test.go
+++ consensus/hotstuff/event_loop_test.go
@@ -1,21 +1,20 @@
 package hotstuff
 
 import (
-	"fmt"
 	"io/ioutil"
 	"sync"
 	"testing"
 	"time"
 
+	"github.com/stretchr/testify/mock"
+	"github.com/stretchr/testify/require"
+
 	"github.com/dapperlabs/flow-go/consensus/hotstuff/mocks"
 	module "github.com/dapperlabs/flow-go/module/mock"
 	"github.com/rs/zerolog"
-	"github.com/stretchr/testify/mock"
-	"github.com/stretchr/testify/require"
 )
 
 func TestReadyDone(t *testing.T) {
-	fmt.Printf("What\n")
 	eh := &mocks.EventHandler{}
 	eh.On("Start").Return(nil)
 	eh.On("TimeoutChannel").Return(time.NewTimer(10 * time.Second).C)
@@ -25,6 +24,7 @@ func TestReadyDone(t *testing.T) {
 	metrics.On("HotStuffBusyDuration", mock.Anything, mock.Anything)
 	metrics.On("HotStuffIdleDuration", mock.Anything, mock.Anything)
 	metrics.On("HotStuffWaitDuration", mock.Anything, mock.Anything)
+
 	log := zerolog.New(ioutil.Discard)
 
 	eventLoop, err := NewEventLoop(log, metrics, eh)
@@ -38,6 +38,7 @@ func TestReadyDone(t *testing.T) {
 		<-eventLoop.Done()
 		wg.Done()
 	}()
+	<-eventLoop.Done()
 
 	// wait until Wait returns
 	wg.Wait()
