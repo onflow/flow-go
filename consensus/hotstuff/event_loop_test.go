97ef2f67fb898a452e71d8b1b44ce66790e2572b
Leo Zhang (zhangchiqing) <zhangchiqing@gmail.com>
2020-04-23 15:41:59 -0700

    add event loop ready done test

--- consensus/hotstuff/event_loop_test.go
+++ consensus/hotstuff/event_loop_test.go
@@ -1,11 +1,38 @@
 package hotstuff
 
 import (
-	"fmt"
+	"io/ioutil"
+	"sync"
 	"testing"
+	"time"
+
+	"github.com/dapperlabs/flow-go/consensus/hotstuff/mocks"
+	module "github.com/dapperlabs/flow-go/module/mock"
+	"github.com/rs/zerolog"
+	"github.com/stretchr/testify/require"
 )
 
-func TestEventLoop(t *testing.T) {
-	eventLoop := &EventLoop{}
-	fmt.Printf("%v", eventLoop)
+func TestReadyDone(t *testing.T) {
+	eh := &mocks.EventHandler{}
+	eh.On("Start").Return(nil)
+	eh.On("TimeoutChannel").Return(time.NewTimer(10 * time.Second).C)
+
+	metrics := &module.Metrics{}
+	log := zerolog.New(ioutil.Discard)
+
+	eventLoop, err := NewEventLoop(log, metrics, eh)
+	require.NoError(t, err)
+
+	<-eventLoop.Ready()
+	time.Sleep(1 * time.Second)
+	var wg sync.WaitGroup
+	go func() {
+		wg.Add(1)
+		<-eventLoop.Wait()
+		wg.Done()
+	}()
+	<-eventLoop.Done()
+
+	// wait until Wait returns
+	wg.Wait()
 }
