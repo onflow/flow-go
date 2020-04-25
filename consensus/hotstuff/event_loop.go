9f54268072d560c0a3f316195b37a02021f21277
Max Wolter <awishformore@gmail.com>
2020-04-25 18:43:50 +0200

    cleaned up sync engine a bit

--- consensus/hotstuff/event_loop.go
+++ consensus/hotstuff/event_loop.go
@@ -42,6 +42,11 @@ func NewEventLoop(log zerolog.Logger, metrics module.Metrics, eventHandler Event
 
 func (el *EventLoop) loop() {
 
+	err := el.eventHandler.Start()
+	if err != nil {
+		el.log.Fatal().Err(err).Msg("could not start event handler")
+	}
+
 	// hotstuff will run in an event loop to process all events synchronously. And this is what will happen when hitting errors:
 	// if hotstuff hits a known critical error, it will exit the loop (for instance, there is a conflicting block with a QC against finalized blocks
 	// if hotstuff hits a known error indicating some assumption between components is broken, it will exit the loop (for instance, hotstuff receives a block whose parent is missing)
@@ -178,19 +183,10 @@ func (el *EventLoop) SubmitVote(originID flow.Identifier, blockID flow.Identifie
 // Multiple calls are handled gracefully and the event loop will only start
 // once.
 func (el *EventLoop) Ready() <-chan struct{} {
-	err := el.eventHandler.Start()
-	if err != nil {
-		el.log.Fatal().Err(err).Msg("could not start event handler")
-	}
 	return el.runner.Start(el.loop)
 }
 
 // Done implements interface module.ReadyDoneAware
 func (el *EventLoop) Done() <-chan struct{} {
 	return el.runner.Abort()
-}
-
-// Wait implements a function to wait for the event loop to exit.
-func (el *EventLoop) Wait() <-chan struct{} {
-	return el.runner.Completed()
 }
