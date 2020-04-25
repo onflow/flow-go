946c4f7e78798685eb63a43139fd7ededea88ddc
Jordan Schalm <jordan@dapperlabs.com>
2020-04-25 16:05:31 -0700

    testnet improvements
    update to use explicit removal of containers, as added to testingdock.
    this requires using the most up-to-date version.
    
    include network name in container name, to avoid naming collisions
    between tests

--- integration/testnet/network.go
+++ integration/testnet/network.go
@@ -100,6 +100,10 @@ func (net *FlowNetwork) Stop() {
 
 // Cleanup cleans up all temporary files used by the network.
 func (net *FlowNetwork) Cleanup() {
+	// remove stopped containers
+	err := net.suite.Remove()
+	require.Nil(net.t, err, "failed to remove network")
+
 	// remove data directories
 	for _, c := range net.Containers {
 		err := os.RemoveAll(c.datadir)
@@ -492,7 +496,7 @@ func setupKeys(t *testing.T, networkConf NetworkConfig) []ContainerConfig {
 	for i, conf := range networkConf.Nodes {
 
 		// define the node's name <role>_<n> and address <name>:<port>
-		name := fmt.Sprintf("%s_%d", conf.Role.String(), roleCounter[conf.Role]+1)
+		name := fmt.Sprintf("%s_%s_%d", networkConf.Name, conf.Role.String(), roleCounter[conf.Role]+1)
 
 		addr := fmt.Sprintf("%s:%d", name, 2137)
 		roleCounter[conf.Role]++
