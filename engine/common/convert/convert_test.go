b6f240593f74a07d2f3979da2b18bed1082b9ea4
Jordan Schalm <jordan@dapperlabs.com>
2020-04-25 16:44:11 -0700

    revert convert test changes

--- engine/common/convert/convert_test.go
+++ engine/common/convert/convert_test.go
@@ -1,13 +1,9 @@
 package convert_test
 
 import (
-	"fmt"
 	"testing"
 
-	sdk "github.com/onflow/flow-go-sdk"
-	sdkconvert "github.com/onflow/flow-go-sdk/client/convert"
 	"github.com/stretchr/testify/assert"
-	"github.com/stretchr/testify/require"
 
 	"github.com/dapperlabs/flow-go/engine/common/convert"
 	"github.com/dapperlabs/flow-go/utils/unittest"
@@ -22,27 +18,4 @@ func TestConvertTransaction(t *testing.T) {
 
 	assert.Equal(t, tx, converted)
 	assert.Equal(t, tx.ID(), converted.ID())
-}
-
-// Make sure we are compatible with SDK transactions.
-func TestSDK(t *testing.T) {
-	sdkTx := sdk.NewTransaction().
-		SetReferenceBlockID(sdk.Identifier{0xff}).
-		SetScript([]byte("asdf")).
-		SetPayer(sdk.RootAddress).
-		SetProposalKey(sdk.RootAddress, 0, 0)
-
-	protoTx := sdkconvert.TransactionToMessage(*sdkTx)
-
-	flowTx, err := convert.MessageToTransaction(protoTx)
-	require.Nil(t, err)
-
-	assert.Equal(t, sdkTx.Encode(), flowTx.Encode())
-	assert.Equal(t, sdkTx.ID().String(), flowTx.ID().String())
-
-	fmt.Printf("%x\n", sdkTx.Encode())
-	fmt.Printf("%x\n", flowTx.Encode())
-
-	fmt.Println(sdkTx.ID().String())
-	fmt.Println(flowTx.ID().String())
 }
