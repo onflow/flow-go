23f9febb43e7c6679d0a2b110bba45963bb82fee
Vishal <vishal.changrani@dapperlabs.com>
2020-04-24 19:33:27 -0700

    adding glue code to translate between flow and flow-go-sdk

--- integration/testnet/client.go
+++ integration/testnet/client.go
@@ -4,8 +4,11 @@ import (
 	"context"
 	"fmt"
 
+	"github.com/onflow/cadence"
+	flowgosdk "github.com/onflow/flow-go-sdk"
+	"github.com/onflow/flow-go-sdk/client"
+
 	"github.com/dapperlabs/flow-go/crypto/hash"
-	"github.com/dapperlabs/flow-go/integration/client"
 	"github.com/dapperlabs/flow-go/model/flow"
 	"github.com/dapperlabs/flow-go/utils/dsl"
 	"github.com/dapperlabs/flow-go/utils/unittest"
@@ -15,7 +18,7 @@ import (
 // NOTE: we use integration/client rather than sdk/client as a stopgap until
 // the SDK client is updated with the latest protobuf definitions.
 type Client struct {
-	client *client.AccessClient
+	client *client.Client
 	key    *flow.AccountPrivateKey
 }
 
@@ -23,7 +26,7 @@ type Client struct {
 // address, using the given account key for signing transactions.
 func NewClientWithKey(addr string, key *flow.AccountPrivateKey) (*Client, error) {
 
-	client, err := client.NewAccessClient(addr)
+	client, err := client.New(addr)
 	if err != nil {
 		return nil, err
 	}
@@ -82,7 +85,7 @@ func (c *Client) SignTransaction(tx flow.TransactionBody) (flow.TransactionBody,
 // SendTransaction submits the transaction to the Access API. The caller must
 // set up the transaction, including signing it.
 func (c *Client) SendTransaction(ctx context.Context, tx flow.TransactionBody) error {
-	return c.client.SendTransaction(ctx, tx)
+	return c.client.SendTransaction(ctx, flowGoSDKTransaction(tx))
 }
 
 // SignAndSendTransaction signs, then sends, a transaction. I bet you didn't
@@ -96,14 +99,64 @@ func (c *Client) SignAndSendTransaction(ctx context.Context, tx flow.Transaction
 	return c.SendTransaction(ctx, tx)
 }
 
-func (c *Client) ExecuteScript(ctx context.Context, script dsl.Main) ([]byte, error) {
+func (c *Client) ExecuteScript(ctx context.Context, script dsl.Main) (cadence.Value, error) {
 
 	code := script.ToCadence()
 
-	res, err := c.client.ExecuteScript(ctx, []byte(code))
+	res, err := c.client.ExecuteScriptAtLatestBlock(ctx, []byte(code))
 	if err != nil {
 		return nil, fmt.Errorf("could not execute script: %w", err)
 	}
 
 	return res, nil
+}
+
+func flowGoSDKTransaction(tx flow.TransactionBody) flowgosdk.Transaction {
+	flowgosdkProposalKey := flowgosdk.ProposalKey{
+		Address:        flowgosdk.Address(tx.ProposalKey.Address),
+		KeyID:          tx.ProposalKey.KeyID,
+		SequenceNumber: tx.ProposalKey.SequenceNumber,
+	}
+
+	flowGoSDKTx := flowgosdk.Transaction{
+		Script:             tx.Script,
+		ReferenceBlockID:   flowgosdk.Identifier(tx.ReferenceBlockID),
+		GasLimit:           tx.GasLimit,
+		ProposalKey:        flowgosdkProposalKey,
+		Payer:              flowgosdk.Address(tx.Payer),
+		Authorizers:        flowGoSDKAddresses(tx.Authorizers),
+		PayloadSignatures:  flowGoSDKSignatures(tx.PayloadSignatures),
+		EnvelopeSignatures: flowGoSDKSignatures(tx.EnvelopeSignatures),
+	}
+
+	return flowGoSDKTx
+}
+
+func flowGoSDKAddress(address flow.Address) flowgosdk.Address {
+	return flowgosdk.Address(address)
+}
+
+func flowGoSDKAddresses(addresses []flow.Address) []flowgosdk.Address {
+	fgsAddrs := make([]flowgosdk.Address, len(addresses))
+	for i, a := range addresses {
+		fgsAddrs[i] = flowGoSDKAddress(a)
+	}
+	return fgsAddrs
+}
+
+func flowGoSDKSignature(sig flow.TransactionSignature) flowgosdk.TransactionSignature {
+	return flowgosdk.TransactionSignature{
+		Address:     flowGoSDKAddress(sig.Address),
+		SignerIndex: sig.SignerIndex,
+		KeyID:       sig.KeyID,
+		Signature:   sig.Signature,
+	}
+}
+
+func flowGoSDKSignatures(sigs []flow.TransactionSignature) []flowgosdk.TransactionSignature {
+	fgsSigs := make([]flowgosdk.TransactionSignature, len(sigs))
+	for i, s := range sigs {
+		fgsSigs[i] = flowGoSDKSignature(s)
+	}
+	return fgsSigs
 }
