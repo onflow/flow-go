e5c25c4fd903501ac980484fb87787d0e410e0c4
AndrewBurian <andrew@dapperlabs.com>
2020-04-27 15:19:21 -0700

    Adding crypto tests

---
+++ cmd/bootstrap/transit/crypto_test.go
@@ -0,0 +1,52 @@
+package main
+
+import (
+	"testing"
+	"os"
+	"path/filepath"
+	"io/ioutil"
+	"fmt"
+
+	"github.com/dapperlabs/flow-go/model/bootstrap"
+)
+
+const nodeID string = "0000000000000000000000000000000000000000000000000000000000000001"
+
+func TestEndToEnd(t *testing.T) {
+
+
+	// Create a temp directory to work as "bootstrap"
+	bootdir, err := ioutil.TempDir("", "bootstrap.*")
+	if err != nil {
+		t.Fatal(err)
+	}
+	defer os.RemoveAll(bootdir)
+
+	t.Logf("Created dir %s", bootdir)
+
+	// Create test files
+	//bootcmd.WriteText(filepath.Join(bootdir, bootstrap.FilenameNodeId), []byte(nodeID)
+	randomBeaconPath := filepath.Join(bootdir, fmt.Sprintf(bootstrap.FilenameRandomBeaconPriv, nodeID))
+	ioutil.WriteFile(randomBeaconPath, []byte("test data"), 0644)
+
+	// Client:
+	// create the transit keys
+	err = generateKeys(bootdir, nodeID)
+	if err != nil {
+		t.Fatalf("Error generating keys: %s", err)
+	}
+
+	// Dapper:
+	// Wrap a file with transit keys
+	err = wrapFile(bootdir, nodeID)
+	if err != nil {
+		t.Fatalf("Error wrapping files: %s", err)
+	}
+
+	// Client:
+	// Unwrap files
+	err = unwrapFile(bootdir, nodeID)
+	if err != nil {
+		t.Fatalf("Error unwrapping response: %s", err)
+	}
+}
