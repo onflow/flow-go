f5000c856ed418d45208ae02aa1f89bd3e4d126e
Leo Zhang (zhangchiqing) <zhangchiqing@gmail.com>
2020-04-27 15:55:30 -0700

    update tests

--- consensus/hotstuff/event_loop.go
+++ consensus/hotstuff/event_loop.go
@@ -191,8 +191,3 @@ func (el *EventLoop) Ready() <-chan struct{} {
 func (el *EventLoop) Done() <-chan struct{} {
 	return el.unit.Done()
 }
-
-// Wait implements a function to wait for the event loop to exit.
-func (el *EventLoop) Wait() <-chan struct{} {
-	return el.unit.Quit()
-}
