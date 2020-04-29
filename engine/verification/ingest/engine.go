207b1dd4e683c94dbb8bdb0121d7f6ead9294edf
Kay-Zee <kan@axiomzen.co>
2020-04-29 16:37:34 -0700

    Use random nonce instead

--- engine/verification/ingest/engine.go
+++ engine/verification/ingest/engine.go
@@ -2,6 +2,7 @@ package ingest
 
 import (
 	"fmt"
+	"math/rand"
 	"sync"
 	"time"
 
@@ -345,8 +346,8 @@ func (e *Engine) requestCollection(collID flow.Identifier) error {
 	}
 
 	req := &messages.CollectionRequest{
-		ID:        collID,
-		Requester: e.me.NodeID(),
+		ID:    collID,
+		Nonce: rand.Uint64(),
 	}
 
 	// TODO we should only submit to cluster which owns the collection
