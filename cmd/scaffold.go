42328f7fa84b20f6a28b18190923a5bf4f9699e1
Max Wolter <awishformore@gmail.com>
2020-04-30 02:49:45 +0200

    using lsm trie for big values

--- cmd/scaffold.go
+++ cmd/scaffold.go
@@ -217,7 +217,8 @@ func (fnb *FlowNodeBuilder) initDatabase() {
 	err := os.MkdirAll(fnb.BaseConfig.datadir, 0700)
 	fnb.MustNot(err).Msgf("could not create datadir %s", fnb.BaseConfig.datadir)
 
-	opts := badger.DefaultOptions(fnb.BaseConfig.datadir).WithLogger(nil)
+	opts := badger.LSMOnlyOptions(fnb.BaseConfig.datadir).WithLogger(nil)
+
 	db, err := badger.Open(opts)
 	fnb.MustNot(err).Msg("could not open key-value store")
 	fnb.DB = db
--- consensus/integration/counter_test.go
+++ consensus/integration/counter_test.go
@@ -15,6 +15,7 @@ type CounterConsumer struct {
 	next      time.Time
 	count     uint
 	total     uint
+	ma        []uint
 	finalized func(uint)
 }
 
@@ -33,8 +34,44 @@ func (c *CounterConsumer) OnFinalizedBlock(block *model.Block) {
 		return
 	}
 
+	// append the values to the moving average and shorten as makes sense
+	c.ma = append(c.ma, c.count)
+	if len(c.ma) > 300 {
+		c.ma = c.ma[1:]
+	}
+
+	// calculate 60 second moving average
+	var ma60 float32
+	end60 := len(c.ma)
+	start60 := len(c.ma) - 60
+	if start60 < 0 {
+		start60 = 0
+	}
+	for _, count := range c.ma[start60:end60] {
+		ma60 += float32(count)
+	}
+	ma60 = ma60 / float32(end60-start60)
+
+	// calculate 300 second moving average
+	var ma300 float32
+	end300 := len(c.ma)
+	start300 := len(c.ma) - 300
+	if start300 < 0 {
+		start300 = 0
+	}
+	for _, count := range c.ma[start300:end300] {
+		ma300 += float32(count)
+	}
+	ma300 = ma300 / float32(end300-start300)
+
 	// otherwise, print number of finalized count and reset
-	c.log.Info().Uint("count", c.count).Dur("ms", diff).Uint("total", c.total).Msg("finalized blocks")
+	c.log.Info().
+		Uint("rate", c.count).
+		Float32("ma_60", ma60).
+		Float32("ma_300", ma300).
+		Uint("total", c.total).Msg("finalized blocks")
+
+	// reset count and next log time
 	c.next = c.next.Add(c.interval)
 	c.count = 0
 }
