e50b8bd225978c2b7c7acd918615d722c93624a6
Jordan Schalm <jordan@dapperlabs.com>
2020-04-30 20:30:18 -0700

    add reference block ID

--- model/cluster/block.go
+++ model/cluster/block.go
@@ -9,19 +9,18 @@ import (
 func Genesis() *Block {
 	header := flow.Header{
 		View:      0,
-		ChainID:   "",
+		ChainID:   "cluster",
 		Timestamp: flow.GenesisTime(),
 		ParentID:  flow.ZeroID,
 	}
 
-	payload := EmptyPayload()
-
-	header.PayloadHash = payload.Hash()
+	payload := EmptyPayload(flow.ZeroID)
 
 	block := &Block{
 		Header:  header,
 		Payload: payload,
 	}
+	block.SetPayload(payload)
 
 	return block
 }
@@ -42,15 +41,29 @@ func (b *Block) SetPayload(payload Payload) {
 // Payload is the payload for blocks in collection node cluster consensus.
 // It contains only a single collection.
 type Payload struct {
+
+	// Collection is the collection being created.
 	Collection flow.Collection
+
+	// ReferenceBlockID is the ID of a reference block on the main chain. It
+	// is defined as the ID of the reference block with the lowest height
+	// from all transactions within the collection.
+	//
+	// This determines when the collection expires, using the same expiry rules
+	// as transactions. It is also used as the reference point for committee
+	// state (staking, etc.) when validating the containing block.
+	ReferenceBlockID flow.Identifier
 }
 
-func EmptyPayload() Payload {
-	return PayloadFromTransactions()
+// EmptyPayload returns a payload with an empty collection and the given
+// reference block ID.
+func EmptyPayload(refID flow.Identifier) Payload {
+	return PayloadFromTransactions(refID)
 }
 
-// PayloadFromTransactions creates a payload given a list of transaction hashes.
-func PayloadFromTransactions(transactions ...*flow.TransactionBody) Payload {
+// PayloadFromTransactions creates a payload given a reference block ID and a
+// list of transaction hashes.
+func PayloadFromTransactions(refID flow.Identifier, transactions ...*flow.TransactionBody) Payload {
 	// avoid a nil transaction list
 	if len(transactions) == 0 {
 		transactions = []*flow.TransactionBody{}
@@ -59,12 +72,13 @@ func PayloadFromTransactions(transactions ...*flow.TransactionBody) Payload {
 		Collection: flow.Collection{
 			Transactions: transactions,
 		},
+		ReferenceBlockID: refID,
 	}
 }
 
 // Hash returns the hash of the payload, simply the ID of the collection.
 func (p Payload) Hash() flow.Identifier {
-	return p.Collection.ID()
+	return flow.MakeID(p)
 }
 
 // PendingBlock is a wrapper type representing a block that cannot yet be
