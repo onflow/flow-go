34d7919b1b2c71347b5102a0af7e92d4793f6fee
Leo Zhang (zhangchiqing) <zhangchiqing@gmail.com>
2020-04-30 21:46:11 -0700

    fix error check

--- cmd/consensus/main.go
+++ cmd/consensus/main.go
@@ -4,6 +4,7 @@ package main
 
 import (
 	"encoding/json"
+	"errors"
 	"fmt"
 	"io/ioutil"
 	"path/filepath"
@@ -13,6 +14,7 @@ import (
 
 	"github.com/dapperlabs/flow-go/consensus/hotstuff/committee"
 	"github.com/dapperlabs/flow-go/state/protocol"
+	"github.com/dapperlabs/flow-go/storage"
 
 	"github.com/dapperlabs/flow-go/cmd"
 	"github.com/dapperlabs/flow-go/consensus"
@@ -34,8 +36,7 @@ import (
 	"github.com/dapperlabs/flow-go/module/mempool/stdmap"
 	consensusMetrics "github.com/dapperlabs/flow-go/module/metrics/consensus"
 	"github.com/dapperlabs/flow-go/module/signature"
-	sto "github.com/dapperlabs/flow-go/storage"
-	storage "github.com/dapperlabs/flow-go/storage/badger"
+	badger "github.com/dapperlabs/flow-go/storage/badger"
 )
 
 func main() {
@@ -91,7 +92,7 @@ func main() {
 			return err
 		}).
 		Component("matching engine", func(node *cmd.FlowNodeBuilder) (module.ReadyDoneAware, error) {
-			results := storage.NewExecutionResults(node.DB)
+			results := badger.NewExecutionResults(node.DB)
 			return matching.New(node.Logger, node.Network, node.State, node.Me, results, receipts, approvals, seals)
 		}).
 		Component("provider engine", func(node *cmd.FlowNodeBuilder) (module.ReadyDoneAware, error) {
@@ -115,13 +116,13 @@ func main() {
 			// TODO: we should probably find a way to initialize mutually dependent engines separately
 
 			// initialize the entity database accessors
-			cleaner := storage.NewCleaner(node.Logger, node.DB)
-			headersDB := storage.NewHeaders(node.DB)
-			payloadsDB := storage.NewPayloads(node.DB)
-			blocksDB := storage.NewBlocks(node.DB)
-			guaranteesDB := storage.NewGuarantees(node.DB)
-			sealsDB := storage.NewSeals(node.DB)
-			viewsDB := storage.NewViews(node.DB)
+			cleaner := badger.NewCleaner(node.Logger, node.DB)
+			headersDB := badger.NewHeaders(node.DB)
+			payloadsDB := badger.NewPayloads(node.DB)
+			blocksDB := badger.NewBlocks(node.DB)
+			guaranteesDB := badger.NewGuarantees(node.DB)
+			sealsDB := badger.NewSeals(node.DB)
+			viewsDB := badger.NewViews(node.DB)
 
 			// initialize the pending blocks cache
 			cache := buffer.NewPendingBlocks()
@@ -206,12 +207,12 @@ func loadDKGPrivateData(path string, myID flow.Identifier) (*bootstrap.DKGPartic
 	return &priv, nil
 }
 
-func findLatest(state protocol.State, headers *storage.Headers, rootHeader *flow.Header) (*flow.Header, []*flow.Header, error) {
+func findLatest(state protocol.State, headers *badger.Headers, rootHeader *flow.Header) (*flow.Header, []*flow.Header, error) {
 	// find finalized block
 	finalized, err := state.Final().Head()
 
 	// when bootstrap, there is no block finalized, use the rootHeader instead
-	if err == sto.ErrNotFound {
+	if errors.Is(err, storage.ErrNotFound) {
 		finalized = rootHeader
 	}
 
