6c61a2e84a41a63d8a55301a9ca8c72b27150b2e
Leo Zhang (zhangchiqing) <zhangchiqing@gmail.com>
2020-05-04 17:04:40 -0700

    add comments

--- consensus/integration/integration_test.go
+++ consensus/integration/integration_test.go
@@ -105,6 +105,9 @@ func TestMessagesLostAcrossNetwork(t *testing.T) {
 }
 
 // verify if each receiver receive delayed messages, the network can still reach consensus
+// the delay might skip some blocks, so should expect to see some gaps in the finalized views
+// like this:
+// [1 2 3 4 10 11 12 17 20 21 22 23 28 31 33 36 39 44 47 53 58 61 62 79 80 88 89 98 101 106 108 111 115 116 119 120 122 123 124 126 127 128 129 130 133 134 135 138 141 142 143 144]
 func TestDelay(t *testing.T) {
 
 	nodes, stopper, hub := createNodes(t, 5, 150, 1500)
