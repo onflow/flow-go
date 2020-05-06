8cd87669db6848a5ec8f5152f1ff6c3a11f4f6eb
Jordan Schalm <jordan@dapperlabs.com>
2020-05-06 12:12:13 -0700

    fix comment

--- cmd/collection/main.go
+++ cmd/collection/main.go
@@ -281,7 +281,7 @@ func main() {
 		Run(flow.RoleCollection.String())
 }
 
-// initClusterCommittee initializetestnet/network.gos tcmd/bootstrap/run/cluster_qc.gohe Collector cluster's HotStuff committee state
+// initClusterCommittee initializes the collector cluster's HotStuff committee state
 func initClusterCommittee(node *cmd.FlowNodeBuilder, colPayloads *badger.ClusterPayloads) (hotstuff.Committee, error) {
 	blockTranslator := func(blockID *flow.Identifier) (*flow.Identifier, error) {
 		payload, err := colPayloads.ByBlockID(*blockID)
