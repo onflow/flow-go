d291dbb0654080a1eae125d1242ab78ff168a328
Benjamin Van Meter <benjaminkvm@icloud.com>
2020-05-07 19:35:09 -0400

    Refactor contract deploy transaction fixture

--- engine/execution/testutil/fixtures.go
+++ engine/execution/testutil/fixtures.go
@@ -9,87 +9,17 @@ import (
 	"github.com/dapperlabs/flow-go/model/flow"
 )
 
-func DeployCounterContractTransaction() flow.TransactionBody {
-	encoded := hex.EncodeToString([]byte(`
-			access(all) contract Container {
-				access(all) resource Counter {
-					pub var count: Int
-
-					init(_ v: Int) {
-						self.count = v
-					}
-					pub fun add(_ count: Int) {
-						self.count = self.count + count
-					}
-				}
-				pub fun createCounter(_ v: Int): @Counter {
-					return <-create Counter(v)
-				}
-			}`))
-
+// Create a transaction that will deploy the provided contract
+// to the specified address.
+func CreateContractDeploymentTransaction(contract string, address flow.Address) flow.TransactionBody {
+	encoded := hex.EncodeToString([]byte(contract))
 	return flow.TransactionBody{
 		Script: []byte(fmt.Sprintf(`transaction {
               prepare(signer: AuthAccount) {
                 signer.setCode("%s".decodeHex())
               }
             }`, encoded)),
-		Authorizers: []flow.Address{flow.RootAddress},
-	}
-}
-
-func CreateCounterTransaction() flow.TransactionBody {
-	return flow.TransactionBody{
-		Script: []byte(`
-			import 0x01
-			
-			transaction {
-				prepare(acc: AuthAccount) {
-					var maybeCounter <- acc.load<@Container.Counter>(from: /storage/counter)
-			
-					if maybeCounter == nil {
-						maybeCounter <-! Container.createCounter(3)		
-					}
-			
-					acc.save(<-maybeCounter!, to: /storage/counter)
-				}   	
-			}`),
-		Authorizers: []flow.Address{flow.RootAddress},
-	}
-}
-
-// CreateCounterPanicTransaction returns a transaction that will manipulate state by writing a new counter into storage
-// and then panic. It can be used to test whether execution state stays untouched/will revert
-func CreateCounterPanicTransaction() flow.TransactionBody {
-	return flow.TransactionBody{
-		Script: []byte(`
-
-			import 0x01
-
-			transaction {
-				prepare(acc: AuthAccount) {
-					if let existing <- acc.load<@Container.Counter>(from: /storage/counter) {
-						destroy existing
-            		}
-
-					panic("fail for testing purposes")
-              	}
-            }`),
-		Authorizers: []flow.Address{flow.RootAddress},
-	}
-}
-
-func AddToCounterTransaction() flow.TransactionBody {
-	return flow.TransactionBody{
-		Script: []byte(`
-			import 0x01
-			
-			transaction {
-				prepare(acc: AuthAccount) {
-					let counter = acc.borrow<&Container.Counter>(from: /storage/counter)
-					counter?.add(2)
-				}
-			}`),
-		Authorizers: []flow.Address{flow.RootAddress},
+		Authorizers: []flow.Address{address},
 	}
 }
 
---
+++ engine/execution/testutil/fixtures_counter.go
@@ -0,0 +1,80 @@
+package testutil
+
+import (
+	"github.com/dapperlabs/flow-go/model/flow"
+)
+
+func DeployCounterContractTransaction() flow.TransactionBody {
+	return CreateContractDeploymentTransaction(`
+		access(all) contract Container {
+			access(all) resource Counter {
+				pub var count: Int
+
+				init(_ v: Int) {
+					self.count = v
+				}
+				pub fun add(_ count: Int) {
+					self.count = self.count + count
+				}
+			}
+			pub fun createCounter(_ v: Int): @Counter {
+				return <-create Counter(v)
+			}
+		}`, flow.RootAddress)
+}
+
+func CreateCounterTransaction() flow.TransactionBody {
+	return flow.TransactionBody{
+		Script: []byte(`
+			import 0x01
+
+			transaction {
+				prepare(acc: AuthAccount) {
+					var maybeCounter <- acc.load<@Container.Counter>(from: /storage/counter)
+
+					if maybeCounter == nil {
+						maybeCounter <-! Container.createCounter(3)
+					}
+
+					acc.save(<-maybeCounter!, to: /storage/counter)
+				}
+			}`),
+		Authorizers: []flow.Address{flow.RootAddress},
+	}
+}
+
+// CreateCounterPanicTransaction returns a transaction that will manipulate state by writing a new counter into storage
+// and then panic. It can be used to test whether execution state stays untouched/will revert
+func CreateCounterPanicTransaction() flow.TransactionBody {
+	return flow.TransactionBody{
+		Script: []byte(`
+
+			import 0x01
+
+			transaction {
+				prepare(acc: AuthAccount) {
+					if let existing <- acc.load<@Container.Counter>(from: /storage/counter) {
+						destroy existing
+            		}
+
+					panic("fail for testing purposes")
+              	}
+            }`),
+		Authorizers: []flow.Address{flow.RootAddress},
+	}
+}
+
+func AddToCounterTransaction() flow.TransactionBody {
+	return flow.TransactionBody{
+		Script: []byte(`
+			import 0x01
+
+			transaction {
+				prepare(acc: AuthAccount) {
+					let counter = acc.borrow<&Container.Counter>(from: /storage/counter)
+					counter?.add(2)
+				}
+			}`),
+		Authorizers: []flow.Address{flow.RootAddress},
+	}
+}
