5e100737018a1d8034dff3d0a92e71fb072c3e45
Ramtin Mehdizadeh Seraj <ramtin@axiomzen.co>
2020-05-06 17:30:51 -0700

    Add insert tests

--- storage/ledger/mtrie/forest_test.go
+++ storage/ledger/mtrie/forest_test.go
@@ -14,7 +14,265 @@ import (
 	"github.com/dapperlabs/flow-go/utils/unittest"
 )
 
-func TestSameInsert(t *testing.T) {
+func TestEmptyInsert(t *testing.T) {
+	trieHeight := 17
+	fStore := mtrie.NewMForest(trieHeight)
+	rootHash := fStore.GetEmptyRootHash()
+
+	k1 := []byte([]uint8{uint8(53), uint8(74)})
+	v1 := []byte{'A'}
+	keys := [][]byte{k1}
+	values := [][]byte{v1}
+	rootHash, err := fStore.Update(keys, values, rootHash)
+	require.NoError(t, err)
+	retKeys, retValues, err := fStore.Read(keys, rootHash)
+	require.True(t, bytes.Equal(retKeys[0], keys[0]))
+	require.True(t, bytes.Equal(retValues[0], values[0]))
+}
+
+func TestLeftEmptyInsert(t *testing.T) {
+	//////////////////////
+	//     insert X     //
+	//       ()         //
+	//      /  \        //
+	//    (X)  [~]      //
+	//////////////////////
+	trieHeight := 17
+	fStore := mtrie.NewMForest(trieHeight)
+	rootHash1 := fStore.GetEmptyRootHash()
+
+	// key: 1000...
+	k1 := []byte([]uint8{uint8(129), uint8(1)})
+	// key: 1100....
+	k2 := []byte([]uint8{uint8(193), uint8(1)})
+
+	v1 := []byte{'A'}
+	v2 := []byte{'B'}
+	keys := [][]byte{k1, k2}
+	values := [][]byte{v1, v2}
+	rootHash2, err := fStore.Update(keys, values, rootHash1)
+	require.NoError(t, err)
+	// expected trie:
+	// 16: ([],)[]
+	// 		15: ([],)[1]
+	// 			14: ([129 1],41)[10]
+	// 			14: ([193 1],42)[11]
+
+	k3 := []byte([]uint8{uint8(1), uint8(1)})
+	v3 := []byte{'C'}
+	keys = [][]byte{k3}
+	values = [][]byte{v3}
+	rootHash3, err := fStore.Update(keys, values, rootHash2)
+	require.NoError(t, err)
+	// expected trie:
+	// 16: ([],)[]
+	// 		15: ([1 1],43)[0]
+	// 		15: ([],)[1]
+	// 			14: ([129 1],41)[10]
+	// 			14: ([193 1],42)[11]
+
+	// order matters
+	keys = [][]byte{k3, k1, k2}
+	values = [][]byte{v3, v1, v2}
+	retKeys, retValues, err := fStore.Read(keys, rootHash3)
+	for i := range keys {
+		require.True(t, bytes.Equal(retKeys[i], keys[i]))
+		require.True(t, bytes.Equal(retValues[i], values[i]))
+	}
+}
+
+func TestRightEmptyInsert(t *testing.T) {
+	///////////////////////
+	//     insert X      //
+	//       ()          //
+	//      /  \         //
+	//    [~]  (X)       //
+	///////////////////////
+	trieHeight := 17
+	fStore := mtrie.NewMForest(trieHeight)
+	rootHash1 := fStore.GetEmptyRootHash()
+
+	// key: 1000...
+	k1 := []byte([]uint8{uint8(129), uint8(1)})
+	// key: 1100....
+	k2 := []byte([]uint8{uint8(193), uint8(1)})
+
+	v1 := []byte{'A'}
+	v2 := []byte{'B'}
+	keys := [][]byte{k1, k2}
+	values := [][]byte{v1, v2}
+	rootHash2, err := fStore.Update(keys, values, rootHash1)
+	require.NoError(t, err)
+	// expected trie:
+	// 16: ([],)[]
+	// 		15: ([],)[1]
+	// 			14: ([129 1],41)[10]
+	// 			14: ([193 1],42)[11]
+
+	k3 := []byte([]uint8{uint8(1), uint8(1)})
+	v3 := []byte{'C'}
+	keys = [][]byte{k3}
+	values = [][]byte{v3}
+	rootHash3, err := fStore.Update(keys, values, rootHash2)
+	require.NoError(t, err)
+	// expected trie:
+	// 16: ([],)[]
+	// 		15: ([1 1],43)[0]
+	// 		15: ([],)[1]
+	// 			14: ([129 1],41)[10]
+	// 			14: ([193 1],42)[11]
+
+	// order matters
+	keys = [][]byte{k3, k1, k2}
+	values = [][]byte{v3, v1, v2}
+	retKeys, retValues, err := fStore.Read(keys, rootHash3)
+	for i := range keys {
+		require.True(t, bytes.Equal(retKeys[i], keys[i]))
+		require.True(t, bytes.Equal(retValues[i], values[i]))
+	}
+}
+
+func TestExpansionInsert(t *testing.T) {
+	//////////////////////
+	//  insert ~'       //
+	//       ()         //
+	//      /  \        //
+	//     ()  [~]      //
+	//////////////////////
+
+	trieHeight := 17
+	fStore := mtrie.NewMForest(trieHeight)
+	rootHash1 := fStore.GetEmptyRootHash()
+
+	// key: 1000...
+	k1 := []byte([]uint8{uint8(129), uint8(1)})
+	v1 := []byte{'A'}
+	keys := [][]byte{k1}
+	values := [][]byte{v1}
+	rootHash2, err := fStore.Update(keys, values, rootHash1)
+	require.NoError(t, err)
+	// expected trie:
+	// 16: ([129 1],41)[]
+
+	// key: 1111....
+	k2 := []byte([]uint8{uint8(130), uint8(1)})
+	v2 := []byte{'B'}
+	keys = [][]byte{k2}
+	values = [][]byte{v2}
+	rootHash3, err := fStore.Update(keys, values, rootHash2)
+	require.NoError(t, err)
+	// expected trie:
+	// 16: ([],)[]
+	// 		15: ([],)[1]
+	// 			14: ([],)[10]
+	// 				13: ([],)[100]
+	// 					12: ([],)[1000]
+	// 						11: ([],)[10000]
+	// 							10: ([],)[100000]
+	// 								9: ([129 1],41)[1000000]
+	// 								9: ([130 1],42)[1000001]
+
+	keys = [][]byte{k1, k2}
+	values = [][]byte{v1, v2}
+	retKeys, retValues, err := fStore.Read(keys, rootHash3)
+	for i := range keys {
+		require.True(t, bytes.Equal(retKeys[i], keys[i]))
+		require.True(t, bytes.Equal(retValues[i], values[i]))
+	}
+}
+
+func TestFullHouseInsert(t *testing.T) {
+	///////////////////////
+	//   insert ~1<X<~2  //
+	//       ()          //
+	//      /  \         //
+	//    [~1]  [~2]     //
+	///////////////////////
+
+	trieHeight := 17
+	fStore := mtrie.NewMForest(trieHeight)
+	rootHash1 := fStore.GetEmptyRootHash()
+
+	// key: 1000...
+	k1 := []byte([]uint8{uint8(129), uint8(1)})
+	// key: 1100....
+	k2 := []byte([]uint8{uint8(193), uint8(1)})
+
+	v1 := []byte{'A'}
+	v2 := []byte{'B'}
+	keys := [][]byte{k1, k2}
+	values := [][]byte{v1, v2}
+	rootHash2, err := fStore.Update(keys, values, rootHash1)
+	require.NoError(t, err)
+	// expected trie:
+	// 16: ([],)[]
+	// 		15: ([],)[1]
+	// 			14: ([129 1],41)[10]
+	// 			14: ([193 1],42)[11]
+
+	k3 := []byte([]uint8{uint8(160), uint8(1)})
+	v3 := []byte{'C'}
+	keys = [][]byte{k3}
+	values = [][]byte{v3}
+	rootHash3, err := fStore.Update(keys, values, rootHash2)
+	require.NoError(t, err)
+	// expected trie:
+	// 16: ([],)[]
+	// 		15: ([],)[1]
+	// 			14: ([],)[10]
+	// 				13: ([129 1],41)[100]
+	// 				13: ([160 1],43)[101]
+	// 			14: ([193 1],42)[11]
+
+	// order matters
+	keys = [][]byte{k1, k3, k2}
+	values = [][]byte{v1, v3, v2}
+	retKeys, retValues, err := fStore.Read(keys, rootHash3)
+	for i := range keys {
+		require.True(t, bytes.Equal(retKeys[i], keys[i]))
+		require.True(t, bytes.Equal(retValues[i], values[i]))
+	}
+}
+
+func TestLeafInsert(t *testing.T) {
+	///////////////////////
+	//   insert 1, 2     //
+	//       ()          //
+	//      /  \         //
+	//     ()   ...      //
+	//          /  \     //
+	//         ()  ()    //
+	///////////////////////
+	trieHeight := 17
+	fStore := mtrie.NewMForest(trieHeight)
+	rootHash1 := fStore.GetEmptyRootHash()
+
+	k1 := []byte([]uint8{uint8(1), uint8(0)})
+	k2 := []byte([]uint8{uint8(1), uint8(1)})
+
+	v1 := []byte{'A'}
+	v2 := []byte{'B'}
+	keys := [][]byte{k1, k2}
+	values := [][]byte{v1, v2}
+	rootHash2, err := fStore.Update(keys, values, rootHash1)
+	require.NoError(t, err)
+	// expected trie:
+	// 16: ([],)[]
+	// 		15: ([],)[0]
+	// 			14: ([],)[00]
+	//               ...
+	// 					1: ([],)[000000010000000]
+	// 						0: ([1 0],41)[0000000100000000]
+	// 						0: ([1 1],42)[0000000100000001]
+
+	retKeys, retValues, err := fStore.Read(keys, rootHash2)
+	for i := range keys {
+		require.True(t, bytes.Equal(retKeys[i], keys[i]))
+		require.True(t, bytes.Equal(retValues[i], values[i]))
+	}
+}
+
+func TestSameKeyInsert(t *testing.T) {
 	trieHeight := 17 // should be key size (in bits) + 1
 	fStore := mtrie.NewMForest(trieHeight)
 	rootHash := fStore.GetEmptyRootHash()
@@ -31,11 +289,17 @@ func TestSameInsert(t *testing.T) {
 	v3 := []byte{'C'}
 	keys = [][]byte{k3}
 	values = [][]byte{v3}
-	_, err = fStore.Update(keys, values, rootHash)
+	rootHash, err = fStore.Update(keys, values, rootHash)
 	require.NoError(t, err)
+
+	retKeys, retValues, err := fStore.Read(keys, rootHash)
+	require.True(t, bytes.Equal(retKeys[0], keys[0]))
+	require.True(t, bytes.Equal(retValues[0], values[0]))
 }
 
-func TestForestAccuracy(t *testing.T) {
+// TODO test duplicated keys
+
+func TestMForestAccuracy(t *testing.T) {
 	trieHeight := 17 // should be key size (in bits) + 1
 	experimentRep := 10
 	fStore := mtrie.NewMForest(trieHeight)
