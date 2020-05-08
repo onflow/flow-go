80a7397344d8c47d7265ad602f15021d18d17b37
Leo Zhang <zhangchiqing@gmail.com>
2020-05-08 16:13:40 -0700

    Consensus: integration tests for liveness (#3385)
    * liveness integration tests
    
    * createNodes
    
    * startup nodes
    
    * add genesis to headers
    
    * move integration tests
    
    * making progress
    
    * add tests
    
    * fix chainID
    
    * fix consensus
    
    * 2 nodes
    
    * remove chain ID
    
    * stop at view 100
    
    * 4 nodes
    
    * final state are the same
    
    * log signer count only
    
    * update tests
    
    * log
    
    * add message detect
    
    * add event loop ready done test
    
    * add synchroinization Ready Done test
    
    * replace single runner with unit
    
    * fix test case
    
    * add slowness_test
    
    * slow down tests
    
    * update log
    
    * add assertSafety and assertLiveness
    
    * add more logging
    
    * fix test case
    
    * update tests
    
    * update tests
    
    * remove single runner test
    
    * update tests
    
    * fix tests
    
    * add test
    
    * add more tests
    
    * add delay tests
    
    * remove to different files
    
    * remove test
    
    * add more logging
    
    * update tests
    
    * split into different files
    
    * add comments
    
    * fix merge conflict for committee
    
    * update tests
    
    * fix test case
    
    * move hubs to createNodes
    
    * fix tests
    
    * fix test
    
    * fix test
    
    * rename
    
    * revert change
    
    * fix merge conflict
    
    * fix merge conflict
    
    * add Test delay
    
    * test delay
    
    * add comments
    
    * test one node behind
    
    * remove formatting
    
    * make tests robust for ci
    
    * relaxing the assert condition
    
    * fix nodes_test
    
    * fix merge conflict
    
    * extend the test wait
    
    * move tests to slow test
    
    * fix lint
    
    * fix comment

--- engine/consensus/compliance/engine.go
+++ engine/consensus/compliance/engine.go
@@ -113,8 +113,11 @@ func (e *Engine) Ready() <-chan struct{} {
 // For the consensus engine, we wait for hotstuff to finish.
 func (e *Engine) Done() <-chan struct{} {
 	return e.unit.Done(func() {
+		e.log.Debug().Msg("shutting down synchronization engine")
 		<-e.sync.Done()
+		e.log.Debug().Msg("shutting down hotstuff eventloop")
 		<-e.hotstuff.Done()
+		e.log.Debug().Msg("all components have been shut down")
 	})
 }
 
@@ -130,7 +133,7 @@ func (e *Engine) Submit(originID flow.Identifier, event interface{}) {
 	e.unit.Launch(func() {
 		err := e.Process(originID, event)
 		if err != nil {
-			e.log.Error().Err(err).Msg("could not process submitted event")
+			e.log.Error().Err(err).Msg("compliance could not process submitted event")
 		}
 	})
 }
@@ -430,7 +433,7 @@ func (e *Engine) processBlockProposal(proposal *messages.BlockProposal) error {
 	}
 	err := e.state.Mutate().Extend(block)
 	if err != nil {
-		return fmt.Errorf("could not extend protocol state: %w", err)
+		return fmt.Errorf("could not extend protocol state for block (%v) at view %v: %w", header.ID(), header.View, err)
 	}
 
 	// retrieve the parent
