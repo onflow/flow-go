eba29764a5e4e6285c1d21959ae1af9faee041b4
Peter Siemens <peterjsiemens@gmail.com>
2020-05-11 12:23:58 -0700

    ledger: Cleap up LevelDB storage implementation

--- storage/ledger/databases/database.go
+++ storage/ledger/databases/database.go
@@ -5,33 +5,31 @@ import "errors"
 var ErrNotFound = errors.New("trie kv: not found")
 
 type DAL interface {
-	// PutIntoBatcher puts key-value pairs into a batcher.
-	//PutIntoBatcher(key []byte, value []byte)
-	//
-
-	// NewBatcher creates a new batcher to be used while updating
-	NewBatcher() Batcher
-
 	// UpdateTrieDB updates the TrieDB according to the batcher.
 	UpdateTrieDB(batcher Batcher) error
 
-	// UpdateKVDB updates the key-value database according to the KV pair.
+	// UpdateKVDB updates the key-value database according to the key-value pairs.
 	UpdateKVDB(keys [][]byte, values [][]byte) error
 
-	// GetTrieDB gets the key from the TrieDB.
+	// GetTrieDB gets the key from the trie database.
 	GetTrieDB(key []byte) ([]byte, error)
 
-	// GetKVDB gets the key from the KVDB.
+	// GetKVDB gets the key from the key-value database.
 	GetKVDB(key []byte) ([]byte, error)
 
-	CopyTo(db DAL) error
+	// NewBatcher creates a new batcher to be used while updating.
+	NewBatcher() Batcher
+
+	// CopyTo copies the contents of this database to the provided database.
+	CopyTo(dest DAL) error
 
 	// PruneDB removes all values from this database that also exist in the provided database.
 	PruneDB(next DAL) error
 
-	// SafeClose attempts to safely close the databases.
+	// SafeClose attempts to safely close the trie and key-value databases.
 	SafeClose() (error, error)
 }
+
 type Batcher interface {
 	Put(key []byte, value []byte)
 }
