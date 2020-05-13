7d4bcd16721c47756a53d29fb074a1dec0cd8474
Benjamin Van Meter <benjaminkvm@icloud.com>
2020-05-13 19:28:23 -0400

    Benchmark AST program cache

--- engine/execution/computation/virtualmachine/astCache_test.go
+++ engine/execution/computation/virtualmachine/astCache_test.go
@@ -5,6 +5,7 @@ import (
 	"testing"
 
 	"github.com/onflow/cadence/runtime"
+	"github.com/onflow/cadence/runtime/ast"
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
 
@@ -90,7 +91,7 @@ func TestScriptASTCache(t *testing.T) {
 	})
 }
 
-func TestTransactionWithImportASTCache(t *testing.T) {
+func TestTransactionWithProgramASTCache(t *testing.T) {
 	rt := runtime.NewInterpreterRuntime()
 	h := unittest.BlockHeaderFixture()
 
@@ -159,4 +160,136 @@ func TestTransactionWithImportASTCache(t *testing.T) {
 	program, err := vm.ASTCache().GetProgram(location)
 	assert.NotNil(t, program)
 	assert.NoError(t, err)
+}
+
+func BenchmarkTransactionWithProgramASTCache(t *testing.B) {
+	rt := runtime.NewInterpreterRuntime()
+	h := unittest.BlockHeaderFixture()
+
+	vm, _ := virtualmachine.New(rt)
+	bc := vm.NewBlockContext(&h)
+
+	// Create a number of account private keys.
+	privateKeys, _ := execTestutil.GenerateAccountPrivateKeys(3)
+
+	// Bootstrap a ledger, creating accounts with the provided private keys and the root account.
+	ledger, accounts, _ := execTestutil.BootstrappedLedger(make(virtualmachine.MapLedger), privateKeys)
+
+	// Create FungibleToken deployment transaction.
+	deployFungibleTokenContractTx := execTestutil.CreateDeployFungibleTokenContractInterfaceTransaction(accounts[0])
+	_ = execTestutil.SignTransaction(&deployFungibleTokenContractTx, accounts[0], flow.RootAccountPrivateKey, 0)
+
+	// Create FlowToken deployment transaction.
+	deployFlowTokenContractTx := execTestutil.CreateDeployFlowTokenContractTransaction(accounts[1], accounts[0])
+	_ = execTestutil.SignTransaction(&deployFlowTokenContractTx, accounts[1], privateKeys[0], 0)
+
+	// Deploy the FungibleToken contract interface.
+	_, _ = bc.ExecuteTransaction(ledger, &deployFungibleTokenContractTx)
+
+	// Deploy the FlowToken contract.
+	_, _ = bc.ExecuteTransaction(ledger, &deployFlowTokenContractTx)
+
+	// Create many transactions that imports the FlowToken contract.
+	var txs []flow.TransactionBody
+	for i := 0; i < 10000; i++ {
+		tx := flow.TransactionBody{
+			Authorizers: []flow.Address{accounts[2]},
+			Script: []byte(fmt.Sprintf(`
+				import FlowToken from 0x%s
+				transaction {
+					prepare(signer: AuthAccount) {}
+					execute {
+						let v <- FlowToken.createEmptyVault()
+						destroy v
+					}
+				}
+			`, accounts[1])),
+		}
+		_ = execTestutil.SignTransaction(&tx, accounts[2], privateKeys[1], 0)
+		txs = append(txs, tx)
+	}
+
+	t.ResetTimer()
+
+	for i := 0; i < t.N; i++ {
+		for _, tx := range txs {
+			// Run the Use import (FT Vault resource) transaction.
+			_, err := bc.ExecuteTransaction(ledger, &tx)
+			if err != nil {
+				fmt.Println(err)
+			}
+		}
+	}
+
+}
+
+type nonFunctioningCache struct{}
+
+func (cache *nonFunctioningCache) GetProgram(location ast.Location) (*ast.Program, error) {
+	return nil, nil
+}
+
+func (cache *nonFunctioningCache) SetProgram(location ast.Location, program *ast.Program) error {
+	return nil
+}
+
+func BenchmarkTransactionWithoutProgramASTCache(t *testing.B) {
+	rt := runtime.NewInterpreterRuntime()
+	h := unittest.BlockHeaderFixture()
+
+	vm, _ := virtualmachine.NewWithCache(rt, &nonFunctioningCache{})
+	bc := vm.NewBlockContext(&h)
+
+	// Create a number of account private keys.
+	privateKeys, _ := execTestutil.GenerateAccountPrivateKeys(3)
+
+	// Bootstrap a ledger, creating accounts with the provided private keys and the root account.
+	ledger, accounts, _ := execTestutil.BootstrappedLedger(make(virtualmachine.MapLedger), privateKeys)
+
+	// Create FungibleToken deployment transaction.
+	deployFungibleTokenContractTx := execTestutil.CreateDeployFungibleTokenContractInterfaceTransaction(accounts[0])
+	_ = execTestutil.SignTransaction(&deployFungibleTokenContractTx, accounts[0], flow.RootAccountPrivateKey, 0)
+
+	// Create FlowToken deployment transaction.
+	deployFlowTokenContractTx := execTestutil.CreateDeployFlowTokenContractTransaction(accounts[1], accounts[0])
+	_ = execTestutil.SignTransaction(&deployFlowTokenContractTx, accounts[1], privateKeys[0], 0)
+
+	// Deploy the FungibleToken contract interface.
+	_, _ = bc.ExecuteTransaction(ledger, &deployFungibleTokenContractTx)
+
+	// Deploy the FlowToken contract.
+	_, _ = bc.ExecuteTransaction(ledger, &deployFlowTokenContractTx)
+
+	// Create many transactions that imports the FlowToken contract.
+	var txs []flow.TransactionBody
+	for i := 0; i < 10000; i++ {
+		tx := flow.TransactionBody{
+			Authorizers: []flow.Address{accounts[2]},
+			Script: []byte(fmt.Sprintf(`
+				import FlowToken from 0x%s
+				transaction {
+					prepare(signer: AuthAccount) {}
+					execute {
+						let v <- FlowToken.createEmptyVault()
+						destroy v
+					}
+				}
+			`, accounts[1])),
+		}
+		_ = execTestutil.SignTransaction(&tx, accounts[2], privateKeys[1], 0)
+		txs = append(txs, tx)
+	}
+
+	t.ResetTimer()
+
+	for i := 0; i < t.N; i++ {
+		for _, tx := range txs {
+			// Run the Use import (FT Vault resource) transaction.
+			_, err := bc.ExecuteTransaction(ledger, &tx)
+			if err != nil {
+				fmt.Println(err)
+			}
+		}
+	}
+
 }
