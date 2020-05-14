e75950195fb0708f7a840b18d2a00303ee5fe610
Alexander Hentschel <alex.hentschel@axiomzen.co>
2020-05-13 23:39:16 -0700

    rollback of unnecessary comment change

--- storage/ledger/mtrie/common/hash.go
+++ storage/ledger/mtrie/common/hash.go
@@ -61,7 +61,7 @@ func HashInterNode(hash1 []byte, hash2 []byte) []byte {
 	return hasher.SumHash()
 }
 
-// ComputeCompactValue computes the value for the node considering the sub tree to only include this value and default Values.
+// ComputeCompactValue computes the value for the node considering the sub tree to only include this value and default values.
 func ComputeCompactValue(key []byte, value []byte, nodeHeight int) []byte {
 	// if value is nil return default hash
 	if len(value) == 0 {
