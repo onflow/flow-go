09b7b458d86fc7cc808880e28d062be200bc2d24
Max Wolter <awishformore@gmail.com>
2020-05-15 20:17:47 +0200

    switched to mtrie for localnet (#3754)

--- integration/localnet/bootstrap.go
+++ integration/localnet/bootstrap.go
@@ -254,6 +254,7 @@ func prepareExecutionService(container testnet.ContainerConfig, i int) Service {
 	service.Command = append(
 		service.Command,
 		fmt.Sprintf("--rpc-addr=%s:%d", container.ContainerName, RPCPort),
+		"--mtrie=true",
 	)
 
 	return service
