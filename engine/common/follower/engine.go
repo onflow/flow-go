2b26e96b9216b220dad9b69237bfd570f74dd8f5
Jordan Schalm <jordan@dapperlabs.com>
2020-05-18 19:03:35 -0700

    fix nil pointer bug

--- engine/common/follower/engine.go
+++ engine/common/follower/engine.go
@@ -227,16 +227,18 @@ func (e *Engine) onBlockProposal(originID flow.Identifier, proposal *messages.Bl
 
 		// go to the first missing ancestor
 		ancestorID := ancestor.Header.ParentID
+		ancestorHeight := ancestor.Header.Height + 1
 		for {
 			ancestor, found = e.pending.ByID(ancestorID)
 			if !found {
 				break
 			}
 			ancestorID = ancestor.Header.ParentID
+			ancestorHeight = ancestor.Header.Height + 1
 		}
 
 		log.Debug().
-			Uint64("ancestor_height", ancestor.Header.Height).
+			Uint64("ancestor_height", ancestorHeight).
 			Hex("ancestor_id", ancestorID[:]).
 			Msg("requesting missing ancestor for proposal")
 
