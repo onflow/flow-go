2b83f3a2627751701f6afc6942e0a9d2cea52510
Alexander Hentschel <alex.hentschel@axiomzen.co>
2020-05-20 23:05:33 -0700

    fixed naming and goDoc

--- consensus/hotstuff/verification/metrics_wrapper.go
+++ consensus/hotstuff/verification/metrics_wrapper.go
@@ -9,43 +9,42 @@ import (
 	"github.com/dapperlabs/flow-go/module"
 )
 
-// CommitteeMetricsWrapper measures the time which the HotStuff's core logic
-// spends in the hotstuff.Committee component, i.e. the time determining consensus
-// committee relations.
-type CommitteeMetricsWrapper struct {
+// SignerMetricsWrapper measures the time which the HotStuff's core logic
+// spends in the hotstuff.Signer component, i.e. the with crypto-related operations.
+type SignerMetricsWrapper struct {
 	signer  hotstuff.Signer
 	metrics module.HotstuffMetrics
 }
 
-func (w CommitteeMetricsWrapper) VerifyVote(voterID flow.Identifier, sigData []byte, block *model.Block) (bool, error) {
+func (w SignerMetricsWrapper) VerifyVote(voterID flow.Identifier, sigData []byte, block *model.Block) (bool, error) {
 	processStart := time.Now()
 	valid, err := w.signer.VerifyVote(voterID, sigData, block)
 	w.metrics.SignerProcessingDuration(time.Since(processStart))
 	return valid, err
 }
 
-func (w CommitteeMetricsWrapper) VerifyQC(voterIDs []flow.Identifier, sigData []byte, block *model.Block) (bool, error) {
+func (w SignerMetricsWrapper) VerifyQC(voterIDs []flow.Identifier, sigData []byte, block *model.Block) (bool, error) {
 	processStart := time.Now()
 	valid, err := w.signer.VerifyQC(voterIDs, sigData, block)
 	w.metrics.SignerProcessingDuration(time.Since(processStart))
 	return valid, err
 }
 
-func (w CommitteeMetricsWrapper) CreateProposal(block *model.Block) (*model.Proposal, error) {
+func (w SignerMetricsWrapper) CreateProposal(block *model.Block) (*model.Proposal, error) {
 	processStart := time.Now()
 	proposal, err := w.signer.CreateProposal(block)
 	w.metrics.SignerProcessingDuration(time.Since(processStart))
 	return proposal, err
 }
 
-func (w CommitteeMetricsWrapper) CreateVote(block *model.Block) (*model.Vote, error) {
+func (w SignerMetricsWrapper) CreateVote(block *model.Block) (*model.Vote, error) {
 	processStart := time.Now()
 	vote, err := w.signer.CreateVote(block)
 	w.metrics.SignerProcessingDuration(time.Since(processStart))
 	return vote, err
 }
 
-func (w CommitteeMetricsWrapper) CreateQC(votes []*model.Vote) (*model.QuorumCertificate, error) {
+func (w SignerMetricsWrapper) CreateQC(votes []*model.Vote) (*model.QuorumCertificate, error) {
 	processStart := time.Now()
 	qc, err := w.signer.CreateQC(votes)
 	w.metrics.SignerProcessingDuration(time.Since(processStart))
