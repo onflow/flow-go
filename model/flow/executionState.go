4fc7e897eda27d5471e5928de70efc6708e05f65
Peter Siemens <peterjsiemens@gmail.com>
2020-05-23 21:59:04 -0700

    Update executionState.go

--- model/flow/executionState.go
+++ model/flow/executionState.go
@@ -29,7 +29,7 @@ type StateCommitment = []byte
 const RootAccountPrivateKeyHex = "e3a08ae3d0461cfed6d6f49bfc25fa899351c39d1bd21fdba8c87595b6c49bb4cc430201"
 
 // Pre-calculated state commitment with root account with the above private key
-const GenesistStateCommitmentHex = "71a3275f197df8bd959f3641c562e42df2247d2c2a25050f901eee0a80c14bb1"
+const GenesistStateCommitmentHex = "03674d6f4d362edd3407f44962b6b2d5901a3121452a5804f26031d500458c79"
 
 var GenesisStateCommitment StateCommitment
 var RootAccountPrivateKey AccountPrivateKey
