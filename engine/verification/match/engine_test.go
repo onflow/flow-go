d3d9c5a80bc2b8a16db5dd10d3313c1ca5143e8d
Leo Zhang (zhangchiqing) <zhangchiqing@gmail.com>
2020-05-26 17:25:58 -0700

    move test cases

--- engine/verification/match/engine_test.go
+++ engine/verification/match/engine_test.go
@@ -27,22 +27,6 @@ import (
 	"github.com/dapperlabs/flow-go/utils/unittest"
 )
 
-// when maxAttempt is set to 3, CanTry will only return true for the first 3 times.
-func TestCanTry(t *testing.T) {
-	t.Run("maxAttempt=3", func(t *testing.T) {
-		maxAttempt := 3
-		chunks := NewChunks(10)
-		chunk := NewChunkStatus(ChunkWithIndex(flow.Identifier{0x11}, 0), flow.Identifier{0xaa}, flow.Identifier{0xbb})
-		chunks.Add(chunk)
-		results := []bool{}
-		for i := 0; i < 5; i++ {
-			results = append(results, CanTry(maxAttempt, chunk))
-			chunks.IncrementAttempt(chunk.ID())
-		}
-		require.Equal(t, []bool{true, true, true, false, false}, results)
-	})
-}
-
 func FinalizedProtocolStateWithParticipants(participants flow.IdentityList) (
 	*flow.Block, *protocol.Snapshot, *protocol.State) {
 	block := unittest.BlockFixture()
