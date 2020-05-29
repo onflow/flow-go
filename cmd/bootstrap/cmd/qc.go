eaede9e59d8579f255128bf22e0d61765307d954
Tarak Ben Youssef <tarak.benyoussef@dapperlabs.com>
2020-05-28 19:00:59 -0700

    fix a bug in DKG data for consensus

--- cmd/bootstrap/cmd/qc.go
+++ cmd/bootstrap/cmd/qc.go
@@ -61,10 +61,19 @@ func GenerateQCParticipantData(allNodes, internalNodes []model.NodeInfo, dkgData
 		})
 	}
 
+	// add the external nodes to the look-up table
+	for i := len(internalNodes); i < len(allNodes); i++ {
+		// assign a node to a DGKdata entry, using the canonical ordering
+		node := allNodes[i]
+		participantLookup[node.NodeID] = &dkg.Participant{
+			PublicKeyShare: dkgData.PubKeyShares[i],
+			Index:          uint(i),
+		}
+	}
+
 	sd.DKGState = wrapper.NewState(&dkg.PublicData{
 		GroupPubKey:     dkgData.PubGroupKey,
 		IDToParticipant: participantLookup,
 	})
-
 	return sd
 }
