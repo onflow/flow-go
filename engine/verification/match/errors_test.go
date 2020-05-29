9ff32755c56cece4d8e8049638238cc5d36bbe36
Leo Zhang <zhangchiqing@gmail.com>
2020-05-29 10:43:37 -0700

    [VER] Match engine (#3865)
    * add match engine
    
    * add pending results
    
    * add type check
    
    * add tests
    
    * refactor SetupTest
    
    * fix ByID
    
    * add happy case
    
    * fix TestMultiAssignment
    
    * fix duplicate
    
    * tmp
    
    * fix max retry
    
    * refactor into CanTry
    
    * fix max retry test
    
    * test concurrency
    
    * fix concurrency
    
    * fix TestProcessChunkDataPackConcurrently
    
    * reduce retryInterval
    
    * update go.mod
    
    * move test cases
    
    * tidy
    
    * refactor test cases
    
    * fix TestProcessExecutionResultConcurrently
    
    * refactor tests
    
    * add identity check
    
    * handle collection
    
    * fix lint
    
    * update verifier engine
    
    * move pending_results to mempool
    
    * refactor error handling
    
    * fix tests
    
    * fix error handling
    
    * tidy
    
    * fix error handling

---
+++ engine/verification/match/errors_test.go
@@ -0,0 +1,63 @@
+package match
+
+import (
+	"errors"
+	"fmt"
+	"testing"
+
+	"github.com/stretchr/testify/require"
+
+	"github.com/dapperlabs/flow-go/model/flow"
+)
+
+func ErrorExecutionResultExist(resultID flow.Identifier) error {
+	return InvalidInput{
+		Msg: fmt.Sprintf("execution result already exists in mempool, execution result ID: %v", resultID),
+	}
+}
+
+func TestCustomizedError(t *testing.T) {
+	var err error
+	err = ErrorExecutionResultExist(flow.Identifier{0x11})
+
+	require.True(t, errors.Is(err, InvalidInput{}))
+
+	err = fmt.Errorf("could not process, because: %w", err)
+	require.True(t, errors.Is(err, InvalidInput{}))
+}
+
+type CustomizedError struct {
+	Msg string
+}
+
+func (e CustomizedError) Error() string {
+	return e.Msg
+}
+
+func (e CustomizedError) Is(other error) bool {
+	_, ok := other.(CustomizedError)
+	return ok
+}
+
+func TestErrorWrapping(t *testing.T) {
+	var err error
+	err = CustomizedError{
+		Msg: "customized",
+	}
+
+	err = InvalidInput{
+		Msg: fmt.Sprintf("invalid input: %v", err),
+	}
+	require.True(t, errors.Is(err, InvalidInput{}))
+	require.False(t, errors.Is(err, CustomizedError{}))
+	require.Equal(t, "invalid input: customized", err.Error())
+
+	err = InvalidInput{
+		Msg: "invalid input",
+		Err: CustomizedError{
+			Msg: "customized",
+		},
+	}
+	require.True(t, errors.Is(err, InvalidInput{}))
+	require.True(t, errors.Is(err, CustomizedError{}))
+}
--- engine/verification/messages.go
+++ engine/verification/messages.go
@@ -7,10 +7,18 @@ import (
 // VerifiableChunk represents a ready-to-verify chunk
 // It contains the execution result as well as all resources needed to verify it
 type VerifiableChunk struct {
-	ChunkIndex    uint64                 // index of the chunk to be verified
-	EndState      flow.StateCommitment   // state commitment at the end of this chunk
-	Block         *flow.Block            // block that contains this chunk
+	ChunkIndex    uint64               // index of the chunk to be verified
+	EndState      flow.StateCommitment // state commitment at the end of this chunk
+	Block         *flow.Block
 	Receipt       *flow.ExecutionReceipt // execution receipt of this block
 	Collection    *flow.Collection       // collection corresponding to the chunk
 	ChunkDataPack *flow.ChunkDataPack    // chunk data package needed to verify this chunk
+}
+
+type VerifiableChunkData struct {
+	Chunk         *flow.Chunk           // the chunk to be verified
+	Header        *flow.Header          // BlockHeader that contains this chunk
+	Result        *flow.ExecutionResult // execution result of this block
+	Collection    *flow.Collection      // collection corresponding to the chunk
+	ChunkDataPack *flow.ChunkDataPack   // chunk data package needed to verify this chunk
 }
