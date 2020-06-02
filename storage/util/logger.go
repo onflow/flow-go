4172347a9dacdd2bd9ccd4270e455751b20061f0
Jordan Schalm <jordan@dapperlabs.com>
2020-06-02 15:15:15 -0700

    lint
    pass variadic args to logger correctly

--- storage/util/logger.go
+++ storage/util/logger.go
@@ -16,17 +16,17 @@ func NewLogger(logger zerolog.Logger) *Logger {
 }
 
 func (l *Logger) Errorf(msg string, args ...interface{}) {
-	l.log.Error().Msgf(msg, args)
+	l.log.Error().Msgf(msg, args...)
 }
 
 func (l *Logger) Warningf(msg string, args ...interface{}) {
-	l.log.Warn().Msgf(msg, args)
+	l.log.Warn().Msgf(msg, args...)
 }
 
 func (l *Logger) Infof(msg string, args ...interface{}) {
-	l.log.Info().Msgf(msg, args)
+	l.log.Info().Msgf(msg, args...)
 }
 
 func (l *Logger) Debugf(msg string, args ...interface{}) {
-	l.log.Debug().Msgf(msg, args)
+	l.log.Debug().Msgf(msg, args...)
 }
