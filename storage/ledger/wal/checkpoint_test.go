3992e849e3bd8e6fbd5b95f72b32615489aa9b64
Maks Pawlak <120831+m4ksio@users.noreply.github.com>
2020-06-02 10:38:46 -0700

    Test cleanup

--- storage/ledger/wal/checkpoint_test.go
+++ storage/ledger/wal/checkpoint_test.go
@@ -114,199 +114,219 @@ func Test_WAL(t *testing.T) {
 
 func Test_Checkpointing(t *testing.T) {
 
-	//t.Skip()
-
 	numInsPerStep := 2
 	keyByteSize := 4
-	valueMaxByteSize := 2 << 16 //16kB
+	valueMaxByteSize := 2 << 16 //64kB
 	size := 10
 	metricsCollector := &metrics.NoopCollector{}
 
-	//unittest.RunWithTempDir(t, func(dir string) {
+	unittest.RunWithTempDir(t, func(dir string) {
 
-	//f, err := ledger.NewMTrieStorage(dir, size*10, metricsCollector, nil)
-	f, err := mtrie.NewMForest(33, dir, size*10, metricsCollector, func(tree *mtrie.MTrie) error { return nil })
-	require.NoError(t, err)
-
-	wal, err := realWAL.NewWAL(nil, nil, dir, size*10, 33)
-	require.NoError(t, err)
-
-	var stateCommitment = f.GetEmptyRootHash()
-
-	//saved data after updates
-	savedData := make(map[string]map[string][]byte)
-
-	// WAL segments are 32kB, so here we generate 2 keys 16kB each, times `size`
-	// so we should get at least `size` segments
-
-	// Generate the tree and create WAL
-	for i := 0; i < size; i++ {
-
-		keys := utils.GetRandomKeysFixedN(numInsPerStep, keyByteSize)
-		values := utils.GetRandomValues(len(keys), valueMaxByteSize, valueMaxByteSize)
-
-		err = wal.RecordUpdate(stateCommitment, keys, values)
+		f, err := mtrie.NewMForest(33, dir, size*10, metricsCollector, func(tree *mtrie.MTrie) error { return nil })
 		require.NoError(t, err)
 
-		stateCommitment, err = f.Update(keys, values, stateCommitment)
-		require.NoError(t, err)
+		var stateCommitment = f.GetEmptyRootHash()
 
-		fmt.Printf("Updated with %x\n", stateCommitment)
+		//saved data after updates
+		savedData := make(map[string]map[string][]byte)
 
-		data := make(map[string][]byte, len(keys))
-		for j, key := range keys {
-			data[string(key)] = values[j]
-		}
+		t.Run("create WAL and inital trie", func(t *testing.T) {
 
-		savedData[string(stateCommitment)] = data
-	}
-	err = wal.Close()
-	require.NoError(t, err)
+			wal, err := realWAL.NewWAL(nil, nil, dir, size*10, 33)
+			require.NoError(t, err)
 
-	require.FileExists(t, path.Join(dir, "00000010")) //make sure we have enough segments saved
+			// WAL segments are 32kB, so here we generate 2 keys 64kB each, times `size`
+			// so we should get at least `size` segments
 
-	// create a new forest and reply WAL
-	f2, err := mtrie.NewMForest(33, dir, size*10, metricsCollector, func(tree *mtrie.MTrie) error { return nil })
-	require.NoError(t, err)
+			// Generate the tree and create WAL
+			for i := 0; i < size; i++ {
 
-	wal2, err := realWAL.NewWAL(nil, nil, dir, size*10, 33)
-	require.NoError(t, err)
+				keys := utils.GetRandomKeysFixedN(numInsPerStep, keyByteSize)
+				values := utils.GetRandomValues(len(keys), valueMaxByteSize, valueMaxByteSize)
 
-	err = wal2.Replay(
-		func(nodes []*mtrie.StorableNode, tries []*mtrie.StorableTrie) error {
-			return fmt.Errorf("I should fail as there should be no checkpoints")
-		},
-		func(commitment flow.StateCommitment, keys [][]byte, values [][]byte) error {
-			_, err := f2.Update(keys, values, commitment)
-			return err
-		},
-		func(commitment flow.StateCommitment) error {
-			return fmt.Errorf("I should fail as there should be no deletions")
-		},
-	)
-	require.NoError(t, err)
+				err = wal.RecordUpdate(stateCommitment, keys, values)
+				require.NoError(t, err)
 
-	checkpointer, err := wal2.Checkpointer()
-	require.NoError(t, err)
+				stateCommitment, err = f.Update(keys, values, stateCommitment)
+				require.NoError(t, err)
 
-	err = checkpointer.Checkpoint(10, func() (io.WriteCloser, error) {
-		return checkpointer.CheckpointWriter(10)
-	})
+				fmt.Printf("Updated with %x\n", stateCommitment)
 
-	require.FileExists(t, path.Join(dir, "checkpoint.00000010")) //make sure we have checkpoint file
+				data := make(map[string][]byte, len(keys))
+				for j, key := range keys {
+					data[string(key)] = values[j]
+				}
 
-	err = wal2.Close()
-	require.NoError(t, err)
-
-	f3, err := mtrie.NewMForest(33, dir, size*10, metricsCollector, func(tree *mtrie.MTrie) error { return nil })
-	require.NoError(t, err)
-
-	wal3, err := realWAL.NewWAL(nil, nil, dir, size*10, 33)
-	require.NoError(t, err)
-
-	err = wal3.Replay(
-		func(nodes []*mtrie.StorableNode, tries []*mtrie.StorableTrie) error {
-			return f3.LoadStorables(nodes, tries)
-		},
-		func(commitment flow.StateCommitment, keys [][]byte, values [][]byte) error {
-			return fmt.Errorf("I should fail as there should be no updates")
-		},
-		func(commitment flow.StateCommitment) error {
-			return fmt.Errorf("I should fail as there should be no deletions")
-		},
-	)
-	require.NoError(t, err)
-
-	err = wal3.Close()
-	require.NoError(t, err)
-
-	// random map iteration order is a benefit here
-	// make sure the tries has been rebuilt from WAL and another from from Checkpoint
-	// f1, f2 and f3 should be identical
-	for stateCommitment, data := range savedData {
-
-		keys := make([][]byte, 0, len(data))
-		for keyString := range data {
-			key := []byte(keyString)
-			keys = append(keys, key)
-		}
-
-		fmt.Printf("Querying with %x\n", stateCommitment)
-
-		registerValues, err := f.Read(keys, []byte(stateCommitment))
-		require.NoError(t, err)
-
-		registerValues2, err := f2.Read(keys, []byte(stateCommitment))
-		require.NoError(t, err)
-
-		registerValues3, err := f3.Read(keys, []byte(stateCommitment))
-		require.NoError(t, err)
-
-		for i, key := range keys {
-			require.Equal(t, data[string(key)], registerValues[i])
-			require.Equal(t, data[string(key)], registerValues2[i])
-			require.Equal(t, data[string(key)], registerValues3[i])
-		}
-	}
-
-	//generate one more segment
-	wal4, err := realWAL.NewWAL(nil, nil, dir, size*10, 33)
-	require.NoError(t, err)
-
-	keys2 := utils.GetRandomKeysFixedN(numInsPerStep, keyByteSize)
-	values2 := utils.GetRandomValues(len(keys2), valueMaxByteSize, valueMaxByteSize)
-
-	err = wal4.RecordUpdate(stateCommitment, keys2, values2)
-	require.NoError(t, err)
-
-	stateCommitment, err = f.Update(keys2, values2, stateCommitment)
-	require.NoError(t, err)
-
-	err = wal4.Close()
-	require.NoError(t, err)
-
-	require.FileExists(t, path.Join(dir, "00000011")) //make sure we have extra segment
-
-	f5, err := mtrie.NewMForest(33, dir, size*10, metricsCollector, func(tree *mtrie.MTrie) error { return nil })
-	require.NoError(t, err)
-
-	wal5, err := realWAL.NewWAL(nil, nil, dir, size*10, 33)
-	require.NoError(t, err)
-
-	updatesLeft := 1 // there should be only one update
-
-	err = wal5.Replay(
-		func(nodes []*mtrie.StorableNode, tries []*mtrie.StorableTrie) error {
-			return f5.LoadStorables(nodes, tries)
-		},
-		func(commitment flow.StateCommitment, keys [][]byte, values [][]byte) error {
-			if updatesLeft == 0 {
-				return fmt.Errorf("more updates called then expected")
+				savedData[string(stateCommitment)] = data
 			}
-			_, err := f5.Update(keys, values, commitment)
-			updatesLeft--
-			return err
-		},
-		func(commitment flow.StateCommitment) error {
-			return fmt.Errorf("I should fail as there should be no deletions")
-		},
-	)
-	require.NoError(t, err)
+			err = wal.Close()
+			require.NoError(t, err)
 
-	err = wal5.Close()
-	require.NoError(t, err)
+			require.FileExists(t, path.Join(dir, "00000010")) //make sure we have enough segments saved
+		})
 
-	registerValues, err := f.Read(keys2, []byte(stateCommitment))
-	require.NoError(t, err)
+		// create a new forest and reply WAL
+		f2, err := mtrie.NewMForest(33, dir, size*10, metricsCollector, func(tree *mtrie.MTrie) error { return nil })
+		require.NoError(t, err)
 
-	registerValues5, err := f5.Read(keys2, []byte(stateCommitment))
-	require.NoError(t, err)
+		t.Run("replay WAL and create checkpoint", func(t *testing.T) {
 
-	for i, _ := range keys2 {
-		require.Equal(t, values2[i], registerValues[i])
-		require.Equal(t, values2[i], registerValues5[i])
-	}
-	//})
+			require.NoFileExists(t, path.Join(dir, "checkpoint.00000010"))
+
+			wal2, err := realWAL.NewWAL(nil, nil, dir, size*10, 33)
+			require.NoError(t, err)
+
+			err = wal2.Replay(
+				func(nodes []*mtrie.StorableNode, tries []*mtrie.StorableTrie) error {
+					return fmt.Errorf("I should fail as there should be no checkpoints")
+				},
+				func(commitment flow.StateCommitment, keys [][]byte, values [][]byte) error {
+					_, err := f2.Update(keys, values, commitment)
+					return err
+				},
+				func(commitment flow.StateCommitment) error {
+					return fmt.Errorf("I should fail as there should be no deletions")
+				},
+			)
+			require.NoError(t, err)
+
+			checkpointer, err := wal2.Checkpointer()
+			require.NoError(t, err)
+
+			err = checkpointer.Checkpoint(10, func() (io.WriteCloser, error) {
+				return checkpointer.CheckpointWriter(10)
+			})
+			require.NoError(t, err)
+
+			require.FileExists(t, path.Join(dir, "checkpoint.00000010")) //make sure we have checkpoint file
+
+			err = wal2.Close()
+			require.NoError(t, err)
+		})
+
+		f3, err := mtrie.NewMForest(33, dir, size*10, metricsCollector, func(tree *mtrie.MTrie) error { return nil })
+		require.NoError(t, err)
+
+		t.Run("read checkpoint", func(t *testing.T) {
+			wal3, err := realWAL.NewWAL(nil, nil, dir, size*10, 33)
+			require.NoError(t, err)
+
+			err = wal3.Replay(
+				func(nodes []*mtrie.StorableNode, tries []*mtrie.StorableTrie) error {
+					return f3.LoadStorables(nodes, tries)
+				},
+				func(commitment flow.StateCommitment, keys [][]byte, values [][]byte) error {
+					return fmt.Errorf("I should fail as there should be no updates")
+				},
+				func(commitment flow.StateCommitment) error {
+					return fmt.Errorf("I should fail as there should be no deletions")
+				},
+			)
+			require.NoError(t, err)
+
+			err = wal3.Close()
+			require.NoError(t, err)
+		})
+
+		t.Run("all forests contain the same data", func(t *testing.T) {
+			// random map iteration order is a benefit here
+			// make sure the tries has been rebuilt from WAL and another from from Checkpoint
+			// f1, f2 and f3 should be identical
+			for stateCommitment, data := range savedData {
+
+				keys := make([][]byte, 0, len(data))
+				for keyString := range data {
+					key := []byte(keyString)
+					keys = append(keys, key)
+				}
+
+				fmt.Printf("Querying with %x\n", stateCommitment)
+
+				registerValues, err := f.Read(keys, []byte(stateCommitment))
+				require.NoError(t, err)
+
+				registerValues2, err := f2.Read(keys, []byte(stateCommitment))
+				require.NoError(t, err)
+
+				registerValues3, err := f3.Read(keys, []byte(stateCommitment))
+				require.NoError(t, err)
+
+				for i, key := range keys {
+					require.Equal(t, data[string(key)], registerValues[i])
+					require.Equal(t, data[string(key)], registerValues2[i])
+					require.Equal(t, data[string(key)], registerValues3[i])
+				}
+			}
+		})
+
+		keys2 := utils.GetRandomKeysFixedN(numInsPerStep, keyByteSize)
+		values2 := utils.GetRandomValues(len(keys2), valueMaxByteSize, valueMaxByteSize)
+
+		t.Run("create segment after checkpoint", func(t *testing.T) {
+
+			require.NoFileExists(t, path.Join(dir, "00000011"))
+
+			//generate one more segment
+			wal4, err := realWAL.NewWAL(nil, nil, dir, size*10, 33)
+			require.NoError(t, err)
+
+			err = wal4.RecordUpdate(stateCommitment, keys2, values2)
+			require.NoError(t, err)
+
+			stateCommitment, err = f.Update(keys2, values2, stateCommitment)
+			require.NoError(t, err)
+
+			err = wal4.Close()
+			require.NoError(t, err)
+
+			require.FileExists(t, path.Join(dir, "00000011")) //make sure we have extra segment
+		})
+
+		f5, err := mtrie.NewMForest(33, dir, size*10, metricsCollector, func(tree *mtrie.MTrie) error { return nil })
+		require.NoError(t, err)
+
+		t.Run("replay both checkpoint and updates after checkpoint", func(t *testing.T) {
+			wal5, err := realWAL.NewWAL(nil, nil, dir, size*10, 33)
+			require.NoError(t, err)
+
+			updatesLeft := 1 // there should be only one update
+
+			err = wal5.Replay(
+				func(nodes []*mtrie.StorableNode, tries []*mtrie.StorableTrie) error {
+					return f5.LoadStorables(nodes, tries)
+				},
+				func(commitment flow.StateCommitment, keys [][]byte, values [][]byte) error {
+					if updatesLeft == 0 {
+						return fmt.Errorf("more updates called then expected")
+					}
+					_, err := f5.Update(keys, values, commitment)
+					updatesLeft--
+					return err
+				},
+				func(commitment flow.StateCommitment) error {
+					return fmt.Errorf("I should fail as there should be no deletions")
+				},
+			)
+			require.NoError(t, err)
+
+			err = wal5.Close()
+			require.NoError(t, err)
+		})
+
+		t.Run("extra updates were applied correctly", func(t *testing.T) {
+			registerValues, err := f.Read(keys2, []byte(stateCommitment))
+			require.NoError(t, err)
+
+			registerValues5, err := f5.Read(keys2, []byte(stateCommitment))
+			require.NoError(t, err)
+
+			for i, _ := range keys2 {
+				require.Equal(t, values2[i], registerValues[i])
+				require.Equal(t, values2[i], registerValues5[i])
+			}
+		})
+
+	})
 }
 
 func Test_emptyDir(t *testing.T) {
