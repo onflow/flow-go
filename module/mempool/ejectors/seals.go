c416537803103439a1fa25a4de413b792eb53e6b
Jordan Schalm <jordan@dapperlabs.com>
2020-06-08 15:43:01 -0700

    add ejector for seal mempool

---
+++ module/mempool/ejectors/seals.go
@@ -0,0 +1,43 @@
+package ejectors
+
+import (
+	"errors"
+
+	"github.com/dapperlabs/flow-go/model/flow"
+	"github.com/dapperlabs/flow-go/storage"
+)
+
+// LatestSeal is a mempool ejector for block seals that ejects newest-first.
+// NOTE: should be initialized with its own headers instance with cache size
+// equal to the mempool size.
+type LatestSeal struct {
+	headers storage.Headers
+}
+
+func NewLatestSeal(headers storage.Headers) *LatestSeal {
+	ejector := &LatestSeal{
+		headers: headers,
+	}
+	return ejector
+}
+
+func (ls *LatestSeal) Eject(entities map[flow.Identifier]flow.Entity) (flow.Identifier, flow.Entity) {
+	maxHeight := uint64(0)
+	maxID := flow.ZeroID
+
+	for id := range entities {
+		block, err := ls.headers.ByBlockID(id)
+		if errors.Is(err, storage.ErrNotFound) {
+			return id, entities[id]
+		}
+		if err != nil {
+			continue
+		}
+		if block.Height > maxHeight {
+			maxHeight = block.Height
+			maxID = id
+		}
+	}
+
+	return maxID, entities[maxID]
+}
