4dc72e8606038bb132844ff451a37027fd8177a1
Yahya <yahya@dapperlabs.com>
2020-06-11 16:07:38 -0700

    Completely removes VerifiableChunk

--- engine/verification/messages.go
+++ engine/verification/messages.go
@@ -4,17 +4,8 @@ import (
 	"github.com/dapperlabs/flow-go/model/flow"
 )
 
-// VerifiableChunk represents a ready-to-verify chunk
+// VerifiableChunkData represents a ready-to-verify chunk
 // It contains the execution result as well as all resources needed to verify it
-type VerifiableChunk struct {
-	ChunkIndex    uint64               // index of the chunk to be verified
-	EndState      flow.StateCommitment // state commitment at the end of this chunk
-	Block         *flow.Block
-	Receipt       *flow.ExecutionReceipt // execution receipt of this block
-	Collection    *flow.Collection       // collection corresponding to the chunk
-	ChunkDataPack *flow.ChunkDataPack    // chunk data package needed to verify this chunk
-}
-
 type VerifiableChunkData struct {
 	Chunk         *flow.Chunk           // the chunk to be verified
 	Header        *flow.Header          // BlockHeader that contains this chunk
