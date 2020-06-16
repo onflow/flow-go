9478210194ce8aa58a35c065f9eb243c4914049f
Yahya <yahya@dapperlabs.com>
2020-06-16 10:49:55 -0700

    adds test for creating and appending id

--- module/mempool/stdmap/identifier_map_test.go
+++ module/mempool/stdmap/identifier_map_test.go
@@ -1,1 +1,29 @@
 package stdmap
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/require"
+
+	"github.com/dapperlabs/flow-go/utils/unittest"
+)
+
+func TestIdentiferMap(t *testing.T) {
+	idMap, err := NewIdentifierMap(10)
+
+	t.Run("no error on creating a new mempool", func(t *testing.T) {
+		require.NoError(t, err)
+	})
+
+	key := unittest.IdentifierFixture()
+	id1 := unittest.IdentifierFixture()
+	t.Run("appending an id to a new key", func(t *testing.T) {
+		err = idMap.Append(key, id1)
+		require.NoError(t, err)
+
+		// checks the existence of id1 for key
+		ids, ok := idMap.Get(key)
+		require.True(t, ok)
+		require.Contains(t, ids, id1)
+	})
+}
