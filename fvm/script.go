996378ae6a49cbe7710ec8bf267d29ef6a64a4df
Peter Siemens <peterjsiemens@gmail.com>
2020-06-16 22:35:51 -0700

    fvm: Add invokable scripts

---
+++ fvm/script.go
@@ -0,0 +1,41 @@
+package fvm
+
+import (
+	"github.com/onflow/cadence/runtime"
+
+	"github.com/dapperlabs/flow-go/model/flow"
+	"github.com/dapperlabs/flow-go/model/hash"
+)
+
+func Script(script []byte) *InvokableScript {
+	return &InvokableScript{
+		script: script,
+	}
+}
+
+type InvokableScript struct {
+	script []byte
+	args   [][]byte
+}
+
+func (i *InvokableScript) WithArguments(args [][]byte) *InvokableScript {
+	i.args = args
+	return i
+}
+
+func (i *InvokableScript) Parse(ctx Context, ledger Ledger) (Invokable, error) {
+	panic("implement me")
+}
+
+func (i *InvokableScript) Invoke(ctx Context, ledger Ledger) (*InvocationResult, error) {
+	env := ctx.Environment(ledger)
+
+	scriptHash := hash.DefaultHasher.ComputeHash(i.script)
+	location := runtime.ScriptLocation(scriptHash)
+
+	scriptID := flow.HashToID(scriptHash)
+
+	value, err := ctx.Runtime().ExecuteScript(i.script, env, location)
+
+	return createInvocationResult(scriptID, value, env.getEvents(), env.getLogs(), err)
+}
