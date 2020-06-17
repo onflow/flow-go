ef3112953c020ec57d5a7d8f96ff7519ac067176
Peter Siemens <peterjsiemens@gmail.com>
2020-06-16 22:15:00 -0700

    fvm: Define new VirtualMachine interface

--- fvm/fvm.go
+++ fvm/fvm.go
@@ -1,77 +1,28 @@
 package fvm
 
 import (
-	"fmt"
-
-	"github.com/onflow/cadence"
 	"github.com/onflow/cadence/runtime"
-
-	"github.com/dapperlabs/flow-go/model/flow"
 )
 
-const (
-	AccountKeyWeightThreshold = 1000
-	MaxProgramASTCacheSize    = 256
-)
-
-// VirtualMachine augments the Cadence runtime with the Flow host functionality required
-// to execute transactions.
+// VirtualMachine augments the Cadence runtime with Flow host functionality.
 type VirtualMachine interface {
-	// NewBlockContext creates a new block context for executing transactions.
-	NewBlockContext(b *flow.Header, blocks Blocks) BlockContext
-	// GetCache returns the program AST cache.
-	ASTCache() ASTCache
+	NewContext(opts ...Option) Context
+}
+
+type Invokable interface {
+	Parse(ctx Context, ledger Ledger) (Invokable, error)
+	Invoke(ctx Context, ledger Ledger) (*Result, error)
 }
 
 // New creates a new virtual machine instance with the provided runtime.
-func New(rt runtime.Runtime) (VirtualMachine, error) {
-	cache, err := NewLRUASTCache(MaxProgramASTCacheSize)
-	if err != nil {
-		return nil, fmt.Errorf("failed to create vm ast cache, %w", err)
-	}
-	return &virtualMachine{
-		rt:    rt,
-		cache: cache,
-	}, nil
-}
-
-func NewWithCache(rt runtime.Runtime, cache ASTCache) (VirtualMachine, error) {
-	return &virtualMachine{
-		rt:    rt,
-		cache: cache,
-	}, nil
+func New(rt runtime.Runtime) VirtualMachine {
+	return &virtualMachine{rt: rt}
 }
 
 type virtualMachine struct {
-	rt    runtime.Runtime
-	cache ASTCache
+	rt runtime.Runtime
 }
 
-func (vm *virtualMachine) NewBlockContext(header *flow.Header, blocks Blocks) BlockContext {
-	return &blockContext{
-		vm:     vm,
-		header: header,
-		blocks: blocks,
-	}
-}
-
-func (vm *virtualMachine) ASTCache() ASTCache {
-	return vm.cache
-}
-
-func (vm *virtualMachine) executeTransaction(
-	script []byte,
-	arguments [][]byte,
-	runtimeInterface runtime.Interface,
-	location runtime.Location,
-) error {
-	return vm.rt.ExecuteTransaction(script, arguments, runtimeInterface, location)
-}
-
-func (vm *virtualMachine) executeScript(
-	script []byte,
-	runtimeInterface runtime.Interface,
-	location runtime.Location,
-) (cadence.Value, error) {
-	return vm.rt.ExecuteScript(script, runtimeInterface, location)
+func (vm *virtualMachine) NewContext(opts ...Option) Context {
+	return newContext(vm.rt, defaultOptions(), opts...)
 }
