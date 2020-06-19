2351c7024a0c1325fc855a12e6c272a893ed6d91
Ramtin Mehdizadeh Seraj <ramtin@axiomzen.co>
2020-06-19 09:47:42 -0700

    add a benchmakr for local nets

---
+++ integration/benchmark/main/main.go
@@ -0,0 +1,58 @@
+package main
+
+import (
+	"encoding/hex"
+	"fmt"
+
+	flowsdk "github.com/onflow/flow-go-sdk"
+	"github.com/onflow/flow-go-sdk/client"
+	"google.golang.org/grpc"
+
+	"github.com/dapperlabs/flow-go/integration/utils"
+	"github.com/dapperlabs/flow-go/model/flow"
+	"github.com/dapperlabs/flow-go/utils/unittest"
+)
+
+func main() {
+
+	chainID := flowsdk.Testnet
+	accessNodeAddress := "localhost:3569"
+	serviceAccountPrivateKeyHex := unittest.ServiceAccountPrivateKeyHex
+
+	addressGen := flowsdk.NewAddressGenerator(chainID)
+	serviceAccountAddress := addressGen.NextAddress()
+	fmt.Println("Root Service Address:", serviceAccountAddress)
+	fungibleTokenAddress := addressGen.NextAddress()
+	fmt.Println("Fungible Address:", fungibleTokenAddress)
+	flowTokenAddress := addressGen.NextAddress()
+	fmt.Println("Flow Address:", flowTokenAddress)
+
+	serviceAccountPrivateKeyBytes, err := hex.DecodeString(serviceAccountPrivateKeyHex)
+	if err != nil {
+		panic("error while hex decoding hardcoded root key")
+	}
+
+	// RLP decode the key
+	ServiceAccountPrivateKey, err := flow.DecodeAccountPrivateKey(serviceAccountPrivateKeyBytes)
+	if err != nil {
+		panic("error while decoding hardcoded root key bytes")
+	}
+
+	// get the private key string
+	priv := hex.EncodeToString(ServiceAccountPrivateKey.PrivateKey.Encode())
+
+	flowClient, err := client.New(accessNodeAddress, grpc.WithInsecure())
+	lg, err := utils.NewLoadGenerator(flowClient, priv, &serviceAccountAddress, &fungibleTokenAddress, &flowTokenAddress, 100, false)
+	if err != nil {
+		panic(err)
+	}
+
+	rounds := 5
+	// extra 3 is for setup
+	for i := 0; i < rounds+3; i++ {
+		lg.Next()
+	}
+
+	fmt.Println(lg.Stats())
+	lg.Close()
+}
