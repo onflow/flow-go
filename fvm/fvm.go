937cdc9981fcde6c2f6687faea5cc7e649fb4364
Peter Siemens <peterjsiemens@gmail.com>
2020-06-22 17:24:10 -0700

    fvm: Remove VirtualMachine interface

--- fvm/fvm.go
+++ fvm/fvm.go
@@ -4,25 +4,22 @@ import (
 	"github.com/onflow/cadence/runtime"
 )
 
-// VirtualMachine augments the Cadence runtime with Flow host functionality.
-type VirtualMachine interface {
-	NewContext(opts ...Option) Context
-}
-
 type Invokable interface {
 	Parse(ctx Context, ledger Ledger) (Invokable, error)
 	Invoke(ctx Context, ledger Ledger) (*InvocationResult, error)
 }
 
-// New creates a new virtual machine instance with the provided runtime.
-func New(rt runtime.Runtime) VirtualMachine {
-	return &virtualMachine{rt: rt}
-}
-
-type virtualMachine struct {
+// A VirtualMachine augments the Cadence runtime with Flow host functionality.
+type VirtualMachine struct {
 	rt runtime.Runtime
 }
 
-func (vm *virtualMachine) NewContext(opts ...Option) Context {
+// New creates a new virtual machine instance with the provided runtime.
+func New(rt runtime.Runtime) *VirtualMachine {
+	return &VirtualMachine{rt: rt}
+}
+
+// NewContext initializes a new execution context with the provided options.
+func (vm *VirtualMachine) NewContext(opts ...Option) Context {
 	return newContext(vm.rt, defaultOptions(), opts...)
 }
