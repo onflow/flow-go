b76e2493dba75c7013f062c58ceb174334f03fbb
Tarak Ben Youssef <tarak.benyoussef@dapperlabs.com>
2020-06-22 18:17:13 -0700

    update KMAC domain separation tags

--- model/encoding/signing_tags.go
+++ model/encoding/signing_tags.go
@@ -1,14 +1,21 @@
 package encoding
 
-// List of string constants, aka 'tags' for signature typing
-// Each Protocol-level signature, essentially signs the hash of an entity. To prevent
-// type-malleability attacks, the signed data must also include a tag which specifies the
-// type of the hashed-and-signed object.
+// List of domain separation tags for protocol signatures.
+//
+// Each Protocol-level signature involves hashing an entity.
+// To prevent domain malleability attacks and to simulate multiple
+// independent random oracles, the hashing process includes
+// a domain tag that specifies the type of the signed object.
 
 const (
-	RandomBeaconTag     = "RandomBeacon"
-	ConsensusVoteTag    = "ConsensusVote"
-	CollectorVoteTag    = "CollectorVote"
-	ExecutionReceiptTag = "ExecutionReceipt"
-	ResultApprovalTag   = "ResultApproval"
+	// RandomBeaconTag is used for threshold signatures in the random beacon
+	RandomBeaconTag = "FLOW-V0.0-RandomBeacon"
+	// ConsensusVoteTag is used for Consensus Hotstuff votes
+	ConsensusVoteTag = "FLOW-V0.0-ConsensusVote"
+	// CollectorVoteTag is used for Collection Hotstuff votes
+	CollectorVoteTag = "FLOW-V0.0-CollectorVote"
+	// ExecutionReceiptTag is used for execution receipts
+	ExecutionReceiptTag = "FLOW-V0.0-ExecutionReceipt"
+	// ResultApprovalTag is used for result approvals
+	ResultApprovalTag = "FLOW-V0.0-ResultApproval"
 )
