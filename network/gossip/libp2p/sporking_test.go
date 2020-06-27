93df4183e93223526c34d83f5931735d3e33d0df
Vishal <1117327+vishalchangrani@users.noreply.github.com>
2020-06-26 22:34:45 -0700

    suffixing libp2p topic ids with genesis hash id

--- network/gossip/libp2p/sporking_test.go
+++ network/gossip/libp2p/sporking_test.go
@@ -6,32 +6,34 @@ import (
 
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/suite"
+
+	"github.com/dapperlabs/flow-go/utils/unittest"
 )
 
-// HardSpooningTestSuite tests that the network layer behaves as expected after a hard spoon.
-// All network related hard spooning requirements can be supported via libp2p directly,
-// without needing any additional support in Flow.
-// Hard-spooning can be supported by two ways:
+// SporkingTestSuite tests that the network layer behaves as expected after a spork.
+// All network related sporking requirements can be supported via libp2p directly,
+// without needing any additional support in Flow other than providing the new root block ID.
+// Sporking can be supported by two ways:
 // 1. Updating the network key of a node after it is moved from the old chain to the new chain
 // 2. Updating the Flow Libp2p protocol ID in the code
 // 1 and 2 both can independently ensure that nodes from the old chain cannot communicate with nodes on the new chain
-// These tests are just to reconfirm the network behaviour and provide a test bed for future tests for hard-spooning, if needed
-type HardSpooningTestSuite struct {
+// These tests are just to reconfirm the network behaviour and provide a test bed for future tests for sporking, if needed
+type SporkingTestSuite struct {
 	LibP2PNodeTestSuite
 }
 
 func TestHardSpooningTestSuite(t *testing.T) {
-	suite.Run(t, new(HardSpooningTestSuite))
+	suite.Run(t, new(SporkingTestSuite))
 }
 
 // TestNetworkKeyChangedAfterHardSpoon tests that a node from the old chain cannot talk to a node in the new chain if it's
 // network key is updated even if the libp2p protocol id remains the same
-func (h *HardSpooningTestSuite) TestNetworkKeyChangedAfterHardSpoon() {
+func (h *SporkingTestSuite) TestNetworkKeyChangedAfterHardSpoon() {
 
 	// create and start node 1 on localhost and random port
 	node1key, err := generateNetworkingKey("abc")
 	assert.NoError(h.T(), err)
-	node1, address1 := h.CreateNode("node1", node1key, "0.0.0.0", "0")
+	node1, address1 := h.CreateNode("node1", node1key, "0.0.0.0", "0", rootID)
 	defer h.StopNode(node1)
 	h.T().Logf(" %s node started on %s:%s", node1.name, address1.IP, address1.Port)
 	h.T().Logf("libp2p ID for %s: %s", node1.name, node1.libP2PHost.ID())
@@ -39,7 +41,7 @@ func (h *HardSpooningTestSuite) TestNetworkKeyChangedAfterHardSpoon() {
 	// create and start node 2 on localhost and random port
 	node2key, err := generateNetworkingKey("def")
 	assert.NoError(h.T(), err)
-	node2, address2 := h.CreateNode("node2", node2key, "0.0.0.0", "0")
+	node2, address2 := h.CreateNode("node2", node2key, "0.0.0.0", "0", rootID)
 	h.T().Logf(" %s node started on %s:%s", node2.name, address2.IP, address2.Port)
 	h.T().Logf("libp2p ID for %s: %s", node2.name, node2.libP2PHost.ID())
 
@@ -59,7 +61,7 @@ func (h *HardSpooningTestSuite) TestNetworkKeyChangedAfterHardSpoon() {
 	assert.False(h.T(), node2key.Equals(node2keyNew))
 
 	// start node2 with the same name, ip and port but with the new key
-	node2, address2New := h.CreateNode(node2.name, node2keyNew, address2.IP, address2.Port)
+	node2, address2New := h.CreateNode(node2.name, node2keyNew, address2.IP, address2.Port, rootID)
 	defer h.StopNode(node2)
 	h.T().Logf(" %s node again started on %s:%s", node2.name, address2New.IP, address2New.Port)
 	h.T().Logf("new libp2p ID for %s: %s", node2.name, node2.libP2PHost.ID())
@@ -80,15 +82,15 @@ func (h *HardSpooningTestSuite) TestNetworkKeyChangedAfterHardSpoon() {
 
 // TestProtocolChangeAfterHardSpoon tests that a node from the old chain cannot talk to a node in the new chain if
 // the Flow libp2p protocol ID is updated but the network keys are kept the same.
-func (h *HardSpooningTestSuite) TestProtocolChangeAfterHardSpoon() {
+func (h *SporkingTestSuite) TestProtocolChangeAfterHardSpoon() {
 
-	// set protocol id to the current default
-	flowLibP2PProtocolID = FlowLibP2PProtocolIDVersion1
+	// root id before spork
+	rootID1 := unittest.BlockFixture().ID().String()
 
 	// create and start node 1 on localhost and random port
 	node1key, err := generateNetworkingKey("abc")
 	assert.NoError(h.T(), err)
-	node1, address1 := h.CreateNode("node1", node1key, "0.0.0.0", "0")
+	node1, address1 := h.CreateNode("node1", node1key, "0.0.0.0", "0", rootID1)
 	defer h.StopNode(node1)
 	h.T().Logf(" %s node started on %s:%s", node1.name, address1.IP, address1.Port)
 	h.T().Logf("libp2p ID for %s: %s", node1.name, node1.libP2PHost.ID())
@@ -96,7 +98,7 @@ func (h *HardSpooningTestSuite) TestProtocolChangeAfterHardSpoon() {
 	// create and start node 2 on localhost and random port
 	node2key, err := generateNetworkingKey("def")
 	assert.NoError(h.T(), err)
-	node2, address2 := h.CreateNode("node2", node2key, "0.0.0.0", "0")
+	node2, address2 := h.CreateNode("node2", node2key, "0.0.0.0", "0", rootID1)
 	h.T().Logf(" %s node started on %s:%s", node2.name, address2.IP, address2.Port)
 	h.T().Logf("libp2p ID for %s: %s", node2.name, node2.libP2PHost.ID())
 
@@ -110,11 +112,11 @@ func (h *HardSpooningTestSuite) TestProtocolChangeAfterHardSpoon() {
 	// stop node 2 and start it again with a different libp2p protocol id to listen for
 	h.StopNode(node2)
 
-	// update the flow libp2p protocol id for node2. node1 is still listening on the old protocol
-	flowLibP2PProtocolID = "/flow/push/2"
+	// update the flow root id for node 2. node1 is still listening on the old protocol
+	rootID2 := unittest.BlockFixture().ID().String()
 
 	// start node2 with the same name, ip and port but with the new key
-	node2, address2New := h.CreateNode(node2.name, node2key, address2.IP, address2.Port)
+	node2, address2New := h.CreateNode(node2.name, node2key, address2.IP, address2.Port, rootID2)
 	defer h.StopNode(node2)
 	h.T().Logf(" %s node again started on %s:%s", node2.name, address2New.IP, address2New.Port)
 	h.T().Logf("new libp2p ID for %s: %s", node2.name, node2.libP2PHost.ID())
