be797c16be0687a11749f728e97797abb1962157
Tarak Ben Youssef <tarak.benyoussef@dapperlabs.com>
2020-07-06 13:09:16 -0700

    update test comments

--- crypto/spock_test.go
+++ crypto/spock_test.go
@@ -11,10 +11,11 @@ import (
 	"github.com/stretchr/testify/require"
 )
 
+// tests of happy and unhappy paths of SPOCKVerifyAgainstData
 func TestSPOCKProveVerifyAgainstData(t *testing.T) {
 	// test the consistency with different inputs
 	seed := make([]byte, KeyGenSeedMinLenBLSBLS12381)
-	input := make([]byte, 100)
+	data := make([]byte, 100)
 
 	loops := 1
 	for j := 0; j < loops; j++ {
@@ -23,46 +24,47 @@ func TestSPOCKProveVerifyAgainstData(t *testing.T) {
 		require.NoError(t, err)
 		sk, err := GeneratePrivateKey(BLSBLS12381, seed)
 		require.NoError(t, err)
-		_, err = rand.Read(input)
+		_, err = rand.Read(data)
 		require.NoError(t, err)
-		// SPoCK proof
+		// generate a SPoCK proof
 		kmac := NewBLSKMAC("spock test")
-		s, err := SPOCKProve(sk, input, kmac)
+		s, err := SPOCKProve(sk, data, kmac)
 		require.NoError(t, err)
 		pk := sk.PublicKey()
-		// SPoCK verify against the data
-		result, err := SPOCKVerifyAgainstData(pk, s, input, kmac)
+		// SPoCK verify against the data (happy path)
+		result, err := SPOCKVerifyAgainstData(pk, s, data, kmac)
 		require.NoError(t, err)
 		assert.True(t, result, fmt.Sprintf(
-			"Verification should succeed:\n signature:%s\n message:%s\n private key:%s", s, input, sk))
-		// test with a different message
-		input[0] ^= 1
-		result, err = SPOCKVerifyAgainstData(pk, s, input, kmac)
+			"Verification should succeed:\n signature:%s\n message:%s\n private key:%s", s, data, sk))
+		// test with a different message (unhappy path)
+		data[0] ^= 1
+		result, err = SPOCKVerifyAgainstData(pk, s, data, kmac)
 		require.NoError(t, err)
 		assert.False(t, result, fmt.Sprintf(
-			"Verification should fail:\n signature:%s\n message:%s\n private key:%s", s, input, sk))
-		input[0] ^= 1
-		// test with a valid but different key
+			"Verification should fail:\n signature:%s\n message:%s\n private key:%s", s, data, sk))
+		data[0] ^= 1
+		// test with a valid but different key (unhappy path)
 		seed[0] ^= 1
 		wrongSk, err := GeneratePrivateKey(BLSBLS12381, seed)
 		require.NoError(t, err)
-		result, err = SPOCKVerifyAgainstData(wrongSk.PublicKey(), s, input, kmac)
+		result, err = SPOCKVerifyAgainstData(wrongSk.PublicKey(), s, data, kmac)
 		require.NoError(t, err)
 		assert.False(t, result, fmt.Sprintf(
-			"Verification should fail:\n signature:%s\n message:%s\n private key:%s", s, input, sk))
+			"Verification should fail:\n signature:%s\n message:%s\n private key:%s", s, data, sk))
 	}
 }
 
+// tests of happy and unhappy paths of SPOCKVerify
 func TestSPOCKProveVerify(t *testing.T) {
 	// test the consistency with different inputs
 	seed1 := make([]byte, KeyGenSeedMinLenBLSBLS12381)
 	seed2 := make([]byte, KeyGenSeedMinLenBLSBLS12381)
-	input := make([]byte, 100)
+	data := make([]byte, 100)
 
 	loops := 1
 	for j := 0; j < loops; j++ {
 		// data
-		_, err := rand.Read(input)
+		_, err := rand.Read(data)
 		require.NoError(t, err)
 		// sk1
 		n, err := rand.Read(seed1)
@@ -77,36 +79,38 @@ func TestSPOCKProveVerify(t *testing.T) {
 		sk2, err := GeneratePrivateKey(BLSBLS12381, seed2)
 		require.NoError(t, err)
 
-		// SPoCK proofs
+		// generate SPoCK proofs
 		kmac := NewBLSKMAC("spock test")
-		pr1, err := SPOCKProve(sk1, input, kmac)
+		pr1, err := SPOCKProve(sk1, data, kmac)
 		require.NoError(t, err)
-		pr2, err := SPOCKProve(sk2, input, kmac)
+		pr2, err := SPOCKProve(sk2, data, kmac)
 		require.NoError(t, err)
-		// SPoCK verify against the data
+		// SPoCK verify against the data, happy path
 		result, err := SPOCKVerify(sk1.PublicKey(), pr1, sk2.PublicKey(), pr2)
 		require.NoError(t, err)
 		assert.True(t, result, fmt.Sprintf(
 			"Verification should succeed:\n proofs:%s\n %s\n private keys:%s\n %s\n data:%s",
-			pr1, pr2, sk1, sk2, input))
-		// test with a different message
-		input[0] ^= 1
-		pr2bis, err := SPOCKProve(sk2, input, kmac)
+			pr1, pr2, sk1, sk2, data))
+		// test with a different message, verification should fail for proofs
+		// of different messages.
+		data[0] ^= 1 // alter the data
+		pr2bis, err := SPOCKProve(sk2, data, kmac)
 		require.NoError(t, err)
 		result, err = SPOCKVerify(sk1.PublicKey(), pr1, sk2.PublicKey(), pr2bis)
 		require.NoError(t, err)
 		assert.False(t, result, fmt.Sprintf(
-			"Verification should succeed:\n proofs:%s\n %s\n private keys:%s\n %s \n data:%s",
-			pr1, pr2bis, sk1, sk2, input))
-		input[0] ^= 1
-		// test with a different key
-		seed2[0] ^= 1
+			"Verification should fail:\n proofs:%s\n %s\n private keys:%s\n %s \n data:%s",
+			pr1, pr2bis, sk1, sk2, data))
+		data[0] ^= 1 // restore the data
+		// test with a different key, verification should fail if the public keys are not
+		// matching the private keys used to generate the proofs.
+		seed2[0] ^= 1 // alter the seed
 		sk2bis, err := GeneratePrivateKey(BLSBLS12381, seed2)
 		require.NoError(t, err)
 		result, err = SPOCKVerify(sk1.PublicKey(), pr1, sk2bis.PublicKey(), pr2)
 		require.NoError(t, err)
 		assert.False(t, result, fmt.Sprintf(
 			"Verification should succeed:\n proofs:%s\n %s\n private keys:%s\n %s \n data:%s",
-			pr1, pr2, sk1, sk2bis, input))
+			pr1, pr2, sk1, sk2bis, data))
 	}
 }
