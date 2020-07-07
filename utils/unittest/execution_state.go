198b985fd236005c1247bcf2e0184e4504eef30a
Peter Siemens <peterjsiemens@gmail.com>
2020-07-07 09:03:43 -0700

    Update utils/unittest/execution_state.go
    Co-authored-by: Bastian Müller <bastian@axiomzen.co>

--- utils/unittest/execution_state.go
+++ utils/unittest/execution_state.go
@@ -22,7 +22,13 @@ const GenesisStateCommitmentHex = "729ab6bd97c3a948062b650a6dd990fc46d9df47887e9
 
 var GenesisStateCommitment flow.StateCommitment
 
-var GenesisTokenSupply, _ = cadence.NewUFix64("10000000.00000000")
+var GenesisTokenSupply = func() cadence.UFix64 { 
+	value, err := cadence.NewUFix64("10000000.00000000")
+	if err != nil {
+		panic(fmt.Errorf("invalid genesis token supply: %w", err))
+	}
+	return value
+}()
 
 var ServiceAccountPrivateKey flow.AccountPrivateKey
 var ServiceAccountPublicKey flow.AccountPublicKey
