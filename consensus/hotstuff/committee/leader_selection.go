ddcf03d0f5c3a891d5ebec04818a79cf3c05dd46
Leo Zhang (zhangchiqing) <zhangchiqing@gmail.com>
2020-07-08 15:36:47 -0700

    add leader selection and tests

---
+++ consensus/hotstuff/committee/leader_selection.go
@@ -0,0 +1,118 @@
+package committee
+
+import (
+	"fmt"
+
+	"github.com/dapperlabs/flow-go/crypto/random"
+	"github.com/dapperlabs/flow-go/model/flow"
+)
+
+// LeaderSelection caches the pre-generated leader selections for a certain number of
+// views starting from the epoch start view.
+type LeaderSelection struct {
+	// leaderIndexesForView caches leader selections that were pre-generated for a
+	// certain number of views.
+	leaderIndexesForView []int
+	// The leader selection randomness varies for each epoch.
+	// Leader selection only returns the correct leader selection for the corresponding epoch.
+	// epochStartView specifies the start view of the current epoch
+	epochStartView uint64
+}
+
+// LeaderIndexForView returns the leader index for given view.
+// If the view is smaller than the epochStartView, an error will be returned.
+func (l LeaderSelection) LeaderIndexForView(view uint64) (int, error) {
+	if view < l.epochStartView {
+		return 0, fmt.Errorf("view (%v) is smaller than the epochStartView (%v)", view, l.epochStartView)
+	}
+
+	index := int(view - l.epochStartView)
+	if index >= len(l.leaderIndexesForView) {
+		return 0, fmt.Errorf("view out of cached range: %v", view)
+	}
+	return l.leaderIndexesForView[index], nil
+}
+
+// ComputeLeaderSelectionFromSeed pre-generates a certain number of leader selections, and returns a
+// leader selection instance for querying the leader indexes for certain views.
+// epochStartView - the start view of the epoch, the generated leader selections start from this view.
+// seed - the random seed for leader selection
+// count - the number of leader selections to be pre-generated and cached.
+// identities - the identities that contain the stake info, which is used as weight for the chance of
+// 							the identity to be selected as leader.
+func ComputeLeaderSelectionFromSeed(epochStartView uint64, seed []byte, count int, identities flow.IdentityList) (*LeaderSelection, error) {
+	weights := make([]uint64, 0, len(identities))
+	for _, id := range identities {
+		weights = append(weights, id.Stake)
+	}
+
+	leaders, err := WeightedRandomSelection(seed, count, weights)
+	if err != nil {
+		return nil, fmt.Errorf("could not select leader: %w", err)
+	}
+
+	return &LeaderSelection{
+		leaderIndexesForView: leaders,
+		epochStartView:       epochStartView,
+	}, nil
+}
+
+// WeightedRandomSelection - given a seed and a given count, pre-generate the indexs of leader.
+// The chance to be selected as leader is proportional to its weight.
+func WeightedRandomSelection(seed []byte, count int, weights []uint64) ([]int, error) {
+	// create random number generator from the seed
+	rng, err := random.NewRand(seed)
+	if err != nil {
+		return nil, fmt.Errorf("can not create rng: %w", err)
+	}
+
+	// create an array of weight ranges for each identity.
+	// an i-th identity is selected as the leader if the random number falls into its weight range.
+	weightSum := make([]uint64, 0, len(weights))
+	var sum uint64
+	for _, weight := range weights {
+		sum += weight
+		weightSum = append(weightSum, sum)
+	}
+
+	// after accumulating the weights, the last item is the total weight
+	// total weight is used to specify the range of the random number.
+	totalWeight := weightSum[len(weightSum)-1]
+
+	leaders := make([]int, 0, count)
+	for i := 0; i < count; i++ {
+		// make a random number by specifying the range.
+		// the range should be from 0 (inclusive) to totalWeight (exclusive). Or [0, totalWeight)
+		randomness, err := rng.IntN(int(totalWeight))
+		if err != nil {
+			return nil, fmt.Errorf("could not generate randomness: %w", err)
+		}
+
+		// binary search to find the leader index by the random number
+		leader, err := binarySearch(uint64(randomness), weightSum)
+		if err != nil {
+			return nil, fmt.Errorf("could not generate leader index: %w", err)
+		}
+
+		leaders = append(leaders, leader)
+	}
+	return leaders, nil
+}
+
+// binary search to find the index of the first item in the given array that is
+// bigger or equal to the given value.
+func binarySearch(value uint64, arr []uint64) (int, error) {
+	return bsearch(0, len(arr)-1, value, arr), nil
+}
+
+func bsearch(left, right int, value uint64, arr []uint64) int {
+	if left == right {
+		return left
+	}
+
+	mid := (left + right) / 2
+	if value < arr[mid] {
+		return bsearch(left, mid, value, arr)
+	}
+	return bsearch(mid+1, right, value, arr)
+}
---
+++ consensus/hotstuff/committee/leader_selection_test.go
@@ -0,0 +1,166 @@
+package committee
+
+import (
+	"fmt"
+	"testing"
+
+	"github.com/dapperlabs/flow-go/model/flow"
+	"github.com/stretchr/testify/require"
+)
+
+var someSeed = []uint8{0x6A, 0x23, 0x41, 0xB7, 0x80, 0xE1, 0x64, 0x59,
+	0x6A, 0x53, 0x40, 0xB7, 0x80, 0xE4, 0x64, 0x5C,
+	0x66, 0x53, 0x41, 0xB7, 0x80, 0xE1, 0x64, 0x51,
+	0xAA, 0x53, 0x40, 0xB7, 0x80, 0xE4, 0x64, 0x50}
+
+// Test binary search implementation
+func TestBsearch(t *testing.T) {
+	sums := []uint64{1, 3, 6, 9}
+	sel := make([]int, 0, 9)
+	for i := 0; i < 9; i++ {
+		index, err := binarySearch(uint64(i), sums)
+		require.NoError(t, err)
+		sel = append(sel, index)
+	}
+	require.Equal(t, []int{0, 1, 1, 2, 2, 2, 3, 3, 3}, sel)
+}
+
+// compare the result of binary search with the brute force search,
+// should be the same.
+func TestBsearchWithNormalSearch(t *testing.T) {
+	count := 100
+	sums := make([]uint64, 0, count)
+	sum := 0
+	for i := 0; i < count; i++ {
+		sum += i
+		sums = append(sums, uint64(sum))
+	}
+
+	var value uint64
+	total := sums[len(sums)-1]
+	for value = 0; value < total; value++ {
+		expected, err := bruteSearch(value, sums)
+		require.NoError(t, err)
+
+		actual, err := binarySearch(value, sums)
+		require.NoError(t, err)
+
+		require.Equal(t, expected, actual)
+	}
+}
+
+func bruteSearch(value uint64, arr []uint64) (int, error) {
+	// value ranges from [arr[0], arr[len(arr) -1 ]) exclusive
+	for i, a := range arr {
+		if a > value {
+			return i, nil
+		}
+	}
+	return 0, fmt.Errorf("not found")
+}
+
+// Test given the same seed, the leader selection will produce the same selection
+func TestDeterminstic(t *testing.T) {
+	identities := []*flow.Identity{
+		&flow.Identity{Stake: 1},
+		&flow.Identity{Stake: 2},
+		&flow.Identity{Stake: 3},
+		&flow.Identity{Stake: 4},
+	}
+
+	count := 100
+
+	leaders1, err := ComputeLeaderSelectionFromSeed(0, someSeed, count, identities)
+	require.NoError(t, err)
+
+	leaders2, err := ComputeLeaderSelectionFromSeed(0, someSeed, count, identities)
+	require.NoError(t, err)
+
+	for i := 0; i < count; i++ {
+		l1, err := leaders1.LeaderIndexForView(uint64(i))
+		require.NoError(t, err)
+
+		l2, err := leaders2.LeaderIndexForView(uint64(i))
+		require.NoError(t, err)
+
+		require.Equal(t, l1, l2)
+	}
+}
+
+func TestDifferentSeedWillProduceDifferentSelection(t *testing.T) {
+	identities := []*flow.Identity{
+		&flow.Identity{Stake: 1},
+		&flow.Identity{Stake: 2},
+		&flow.Identity{Stake: 3},
+		&flow.Identity{Stake: 4},
+	}
+
+	count := 100
+
+	seed1 := make([]byte, 16)
+	seed1[0] = 34
+
+	seed2 := make([]byte, 16)
+	seed2[0] = 8
+
+	leaders1, err := ComputeLeaderSelectionFromSeed(0, seed1, count, identities)
+	require.NoError(t, err)
+
+	leaders2, err := ComputeLeaderSelectionFromSeed(0, seed2, count, identities)
+	require.NoError(t, err)
+
+	diff := 0
+	for view := 0; view < count; view++ {
+		l1, err := leaders1.LeaderIndexForView(uint64(view))
+		require.NoError(t, err)
+
+		l2, err := leaders2.LeaderIndexForView(uint64(view))
+		require.NoError(t, err)
+
+		if l1 != l2 {
+			diff++
+		}
+	}
+
+	require.True(t, diff > 0)
+}
+
+// given a random seed and certain weights, measure the chance each identity selected as leader.
+// The number of time being selected as leader might not exactly match their weight, but also
+// won't go too far from that.
+func TestLeaderSelectionAreWeighted(t *testing.T) {
+	count := 100000
+	identities := []*flow.Identity{
+		&flow.Identity{Stake: 1},
+		&flow.Identity{Stake: 2},
+		&flow.Identity{Stake: 3},
+		&flow.Identity{Stake: 4},
+	}
+
+	leaders, err := ComputeLeaderSelectionFromSeed(0, someSeed, count, identities)
+	require.NoError(t, err)
+
+	selected := make([]uint64, 4)
+	for view := 0; view < count; view++ {
+		index, err := leaders.LeaderIndexForView(uint64(view))
+		require.NoError(t, err)
+
+		selected[index]++
+	}
+
+	fmt.Printf("selected for weights [1,2,3,4]: %v\n", selected)
+	for i, selectedCount := range selected {
+		target := uint64(count) * identities[i].Stake / 10
+
+		var diff uint64
+		if selectedCount > target {
+			diff = selectedCount - target
+		} else {
+			diff = target - selectedCount
+		}
+
+		// difference should be less than 2%
+		stdDiff := count / 10 * 2 / 100
+		require.Less(t, diff, uint64(stdDiff))
+	}
+}
