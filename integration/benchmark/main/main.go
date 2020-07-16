802c16561d4ee0b0e8134f74ae88f036f143d02a
Ramtin <ramtin@axiomzen.co>
2020-07-16 01:06:46 +0000

    revert temp changes

--- integration/benchmark/main/main.go
+++ integration/benchmark/main/main.go
@@ -16,11 +16,9 @@ import (
 
 func main() {
 
-	verbose := flag.Bool("verbose", true, "print verbose information")
+	verbose := flag.Bool("verbose", false, "print verbose information")
 	chainIDStr := flag.String("chain", string(flowsdk.Testnet), "chain ID")
-	fmt.Println(chainIDStr)
 	chainID := flowsdk.ChainID([]byte(*chainIDStr))
-	fmt.Println(chainID)
 	accessNodeAddress := flag.String("access", "localhost:3569", "access node address")
 	serviceAccountPrivateKeyHex := flag.String("servPrivHex", unittest.ServiceAccountPrivateKeyHex, "service account private key hex")
 
@@ -47,7 +45,7 @@ func main() {
 	priv := hex.EncodeToString(ServiceAccountPrivateKey.PrivateKey.Encode())
 
 	flowClient, err := client.New(*accessNodeAddress, grpc.WithInsecure())
-	lg, err := utils.NewLoadGenerator(flowClient, priv, &serviceAccountAddress, &fungibleTokenAddress, &flowTokenAddress, 10, *verbose)
+	lg, err := utils.NewLoadGenerator(flowClient, priv, &serviceAccountAddress, &fungibleTokenAddress, &flowTokenAddress, 25, *verbose)
 	if err != nil {
 		panic(err)
 	}
