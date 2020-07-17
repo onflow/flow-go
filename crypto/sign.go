780e14aa2bdbc4095d588353f7e1e8b01c422957
Tarak Ben Youssef <tarak.benyoussef@dapperlabs.com>
2020-07-17 15:31:15 -0700

    add PoP for ECDSA keys and implement tests

--- crypto/sign.go
+++ crypto/sign.go
@@ -92,6 +92,9 @@ type PrivateKey interface {
 	// unequal or if their encoded representations are unequal. If the encoding of either key fails, they are considered
 	// unequal as well.
 	Equals(PrivateKey) bool
+	// GeneratePOP returns a proof of possession (PoP) for the receiver private key
+	// using the given hasher.
+	GeneratePOP(hash.Hasher) (Signature, error)
 }
 
 // PublicKey is an unspecified signature scheme public key.
@@ -110,4 +113,7 @@ type PublicKey interface {
 	// unequal or if their encoded representations are unequal. If the encoding of either key fails, they are considered
 	// unequal as well.
 	Equals(PublicKey) bool
+	// VerifyPOP verifies a proof of possession (PoP) for the receiver public key
+	// using the given hasher.
+	VerifyPOP(Signature, hash.Hasher) (bool, error)
 }
