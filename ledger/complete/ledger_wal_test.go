ab451ce95b1501eb6e7bc8e2b987215f24d8f20b
Ramtin Mehdizadeh Seraj <ramtin@axiomzen.co>
2020-07-20 15:40:47 -0700

    fix wal test

--- ledger/complete/ledger_wal_test.go
+++ ledger/complete/ledger_wal_test.go
@@ -1,84 +1,91 @@
 package complete_test
 
-// import (
-// 	"fmt"
-// 	"testing"
+import (
+	"fmt"
+	"testing"
 
-// 	"github.com/stretchr/testify/assert"
-// 	"github.com/stretchr/testify/require"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
 
-// 	"github.com/dapperlabs/flow-go/ledger"
-// 	"github.com/dapperlabs/flow-go/ledger/utils"
-// 	"github.com/dapperlabs/flow-go/module/metrics"
-// 	"github.com/dapperlabs/flow-go/utils/unittest"
-// )
+	"github.com/dapperlabs/flow-go/ledger"
+	"github.com/dapperlabs/flow-go/ledger/common"
+	"github.com/dapperlabs/flow-go/ledger/complete"
+	"github.com/dapperlabs/flow-go/module/metrics"
+	"github.com/dapperlabs/flow-go/utils/unittest"
+)
 
-// func Test_WAL(t *testing.T) {
+func Test_WAL(t *testing.T) {
+	numInsPerStep := 2
+	keyNumberOfParts := 10
+	keyPartMinByteSize := 1
+	keyPartMaxByteSize := 100
+	valueMaxByteSize := 2 << 16 //16kB
+	size := 10
+	metricsCollector := &metrics.NoopCollector{}
 
-// 	numInsPerStep := 2
-// 	keyByteSize := 32
-// 	valueMaxByteSize := 2 << 16
-// 	size := 10
-// 	metricsCollector := &metrics.NoopCollector{}
+	unittest.RunWithTempDir(t, func(dir string) {
 
-// 	unittest.RunWithTempDir(t, func(dir string) {
+		// cache size intentionally is set to size to test deletion
+		led, err := complete.NewLedger(dir, size, metricsCollector, nil)
+		require.NoError(t, err)
 
-// 		// cache size intentionally is set to size to test deletion
-// 		f, err := ledger.NewMTrieStorage(dir, size, metricsCollector, nil)
-// 		require.NoError(t, err)
+		var stateCommitment = led.EmptyStateCommitment()
 
-// 		var stateCommitment = f.EmptyStateCommitment()
+		//saved data after updates
+		savedData := make(map[string]map[string]ledger.Value)
 
-// 		//saved data after updates
-// 		savedData := make(map[string]map[string][]byte)
+		for i := 0; i < size; i++ {
 
-// 		for i := 0; i < size; i++ {
-// 			keys := utils.GetRandomKeysFixedN(numInsPerStep, keyByteSize)
-// 			values := utils.GetRandomValues(len(keys), 0, valueMaxByteSize)
+			keys := common.RandomUniqueKeys(numInsPerStep, keyNumberOfParts, keyPartMinByteSize, keyPartMaxByteSize)
+			values := common.RandomValues(numInsPerStep, 1, valueMaxByteSize)
+			update, err := ledger.NewUpdate(stateCommitment, keys, values)
+			assert.NoError(t, err)
+			stateCommitment, err = led.Set(update)
+			require.NoError(t, err)
+			fmt.Printf("Updated with %x\n", stateCommitment)
 
-// 			stateCommitment, err = f.UpdateRegisters(keys, values, stateCommitment)
-// 			require.NoError(t, err)
+			data := make(map[string]ledger.Value, len(keys))
+			for j, key := range keys {
+				encKey := common.EncodeKey(&key)
+				data[string(encKey)] = values[j]
+			}
 
-// 			fmt.Printf("Updated with %x\n", stateCommitment)
+			savedData[string(stateCommitment)] = data
+		}
 
-// 			data := make(map[string][]byte, len(keys))
-// 			for j, key := range keys {
-// 				data[string(key)] = values[j]
-// 			}
+		<-led.Done()
 
-// 			savedData[string(stateCommitment)] = data
-// 		}
+		led2, err := complete.NewLedger(dir, size+10, metricsCollector, nil)
+		require.NoError(t, err)
 
-// 		<-f.Done()
+		// random map iteration order is a benefit here
+		for stateCommitment, data := range savedData {
 
-// 		f2, err := ledger.NewMTrieStorage(dir, size+10, metricsCollector, nil)
-// 		require.NoError(t, err)
+			keys := make([]ledger.Key, 0, len(data))
+			for encKey := range data {
+				key, err := common.DecodeKey([]byte(encKey))
+				assert.NoError(t, err)
+				keys = append(keys, *key)
+			}
 
-// 		// random map iteration order is a benefit here
-// 		for stateCommitment, data := range savedData {
+			fmt.Printf("Querying with %x\n", stateCommitment)
 
-// 			keys := make([][]byte, 0, len(data))
-// 			for keyString := range data {
-// 				key := []byte(keyString)
-// 				keys = append(keys, key)
-// 			}
+			query, err := ledger.NewQuery(ledger.StateCommitment(stateCommitment), keys)
+			assert.NoError(t, err)
+			registerValues, err := led2.Get(query)
+			require.NoError(t, err)
 
-// 			fmt.Printf("Querying with %x\n", stateCommitment)
+			for i, key := range keys {
+				registerValue := registerValues[i]
+				encKey := common.EncodeKey(&key)
+				assert.True(t, data[string(encKey)].Equals(registerValue))
+			}
+		}
 
-// 			registerValues, err := f2.GetRegisters(keys, []byte(stateCommitment))
-// 			require.NoError(t, err)
+		// test deletion
+		s := led2.ForestSize()
+		assert.Equal(t, s, size)
 
-// 			for i, key := range keys {
-// 				registerValue := registerValues[i]
-
-// 				assert.Equal(t, data[string(key)], registerValue)
-// 			}
-// 		}
-
-// 		// test deletion
-// 		s := f2.ForestSize()
-// 		assert.Equal(t, s, size)
-
-// 		<-f2.Done()
-// 	})
-// }
+		<-led2.Done()
+	})
+}
