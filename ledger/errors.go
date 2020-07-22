ac805d8a675147ac0ef6b819c078061b7aed763e
Ramtin Mehdizadeh Seraj <ramtin@axiomzen.co>
2020-07-21 17:15:47 -0700

    very first version of ledger

--- ledger/errors.go
+++ ledger/errors.go
@@ -1,5 +1,24 @@
 package ledger
 
+// ErrLedgerConstruction returned when there is a failure with ledger creation
+type ErrLedgerConstruction struct {
+	err error
+}
+
+func (e ErrLedgerConstruction) Error() string {
+	return e.err.Error()
+}
+
+// Is return true if the type of errors are the same
+func (e ErrLedgerConstruction) Is(other error) bool {
+	_, ok := other.(ErrLedgerConstruction)
+	return ok
+}
+
+func NewErrLedgerConstruction(err error) *ErrLedgerConstruction {
+	return &ErrLedgerConstruction{err}
+}
+
 // ErrKeyNotFound returned when there are some missing keys in the ledger
 type ErrKeyNotFound struct {
 	missingKeys []Key
--- ledger/partial/ledger.go
+++ ledger/partial/ledger.go
@@ -1,8 +1,6 @@
 package partial
 
 import (
-	"fmt"
-
 	"github.com/dapperlabs/flow-go/ledger"
 	"github.com/dapperlabs/flow-go/ledger/common"
 	"github.com/dapperlabs/flow-go/ledger/partial/ptrie"
@@ -28,10 +26,10 @@ func NewLedger(proof ledger.Proof, sc ledger.StateCommitment) (*Ledger, error) {
 
 	if err != nil {
 		// TODO provide more details based on the error type
-		return chmodels.NewCFInvalidVerifiableChunk("error constructing partial trie", err, chIndex, execResID), nil
+		return nil, ledger.NewErrLedgerConstruction(err)
 	}
 
-	return &Ledger{ptrie: psmt}
+	return &Ledger{ptrie: psmt}, nil
 }
 
 // Ready implements interface module.ReadyDoneAware
@@ -62,7 +60,7 @@ func (l *Ledger) Get(query *ledger.Query) (values []ledger.Value, err error) {
 		return nil, err
 	}
 	// TODO deal with failedPaths
-	payloads, failedPaths, err := l.ptrie.Get(paths)
+	payloads, _, err := l.ptrie.Get(paths)
 	if err != nil {
 		return nil, err
 	}
@@ -87,34 +85,20 @@ func (l *Ledger) Set(update *ledger.Update) (newStateCommitment ledger.StateComm
 		return nil, err
 	}
 
-	// TODO here update the ptrie
+	// TODO handle failed paths
+	newRootHash, _, err := l.ptrie.Update(trieUpdate.Paths, trieUpdate.Payloads)
+	if err != nil {
+		return nil, err
+	}
 
 	// TODO log info state commitments
 	return ledger.StateCommitment(newRootHash), nil
 }
 
 // Prove provides proofs for a ledger query and errors (if any)
+// TODO implement this by iterating over initial proofs to find the ones for the query
 func (l *Ledger) Prove(query *ledger.Query) (proof ledger.Proof, err error) {
-
-	// TODO iterate over proofs to find the ones for the query
-	paths, err := common.KeysToPaths(query.Keys(), 0)
-	if err != nil {
-		return nil, err
-	}
-
-	trieRead := &ledger.TrieRead{RootHash: ledger.RootHash(query.StateCommitment()), Paths: paths}
-	batchProof, err := l.forest.Proofs(trieRead)
-	if err != nil {
-		return nil, fmt.Errorf("could not get proofs: %w", err)
-	}
-
-	proofToGo := common.EncodeTrieBatchProof(batchProof)
-
-	if len(paths) > 0 {
-		l.metrics.ProofSize(uint32(len(proofToGo) / len(paths)))
-	}
-
-	return ledger.Proof(proofToGo), err
+	return nil, err
 }
 
 // TODO implement an approximate MemSize method
@@ -126,18 +110,3 @@ func (l *Ledger) MemSize() (int64, error) {
 func (l *Ledger) DiskSize() (int64, error) {
 	return 0, nil
 }
-
-// // chunk view construction
-// // unknown register tracks access to parts of the partial trie which
-// // are not expanded and values are unknown.
-// unknownRegTouch := make(map[string]bool)
-// regMap := vc.ChunkDataPack.GetRegisterValues()
-// getRegister := func(key flow.RegisterID) (flow.RegisterValue, error) {
-// 	// check if register has been provided in the chunk data pack
-// 	val, ok := regMap[string(key)]
-// 	if !ok {
-// 		unknownRegTouch[string(key)] = true
-// 		return nil, fmt.Errorf("missing register")
-// 	}
-// 	return val, nil
-// }
