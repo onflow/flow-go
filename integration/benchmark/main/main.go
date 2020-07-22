f370af7374dbd8bcc7fddd5dad16294bf529a659
Philip Stanislaus <6912756+philipstanislaus@users.noreply.github.com>
2020-07-22 20:58:26 +0000

    Fix flags, make sleep accept duration

--- integration/benchmark/main/main.go
+++ integration/benchmark/main/main.go
@@ -18,15 +18,14 @@ import (
 
 func main() {
 
-	sleep := flag.Int64("sleep", 0, "seconds to sleep before benchmarking starts")
-	time.Sleep(time.Duration(*sleep) * time.Second)
-
+	sleep := flag.Duration("sleep", 0, "duration to sleep before benchmarking starts")
 	verbose := flag.Bool("verbose", false, "print verbose information")
 	chainIDStr := flag.String("chain", string(flowsdk.Testnet), "chain ID")
 	chainID := flowsdk.ChainID([]byte(*chainIDStr))
 	access := flag.String("access", "localhost:3569", "access node address")
 	serviceAccountPrivateKeyHex := flag.String("servPrivHex", unittest.ServiceAccountPrivateKeyHex, "service account private key hex")
 	accessNodeAddrs := strings.Split(*access, ",")
+	flag.Parse()
 
 	addressGen := flowsdk.NewAddressGenerator(chainID)
 	serviceAccountAddress := addressGen.NextAddress()
@@ -50,9 +49,15 @@ func main() {
 	// get the private key string
 	priv := hex.EncodeToString(ServiceAccountPrivateKey.PrivateKey.Encode())
 
+	// sleep in order to ensure the testnet is up and running
+	if *sleep > 0 {
+		fmt.Printf("Sleeping for %v before starting benchmark\n", sleep)
+		time.Sleep(*sleep)
+	}
+
 	flowClient, err := client.New(accessNodeAddrs[0], grpc.WithInsecure())
 	lg, err := utils.NewLoadGenerator(flowClient, priv, &serviceAccountAddress, &fungibleTokenAddress,
-		&flowTokenAddress, 2000, *verbose)
+		&flowTokenAddress, 100, *verbose)
 	if err != nil {
 		panic(err)
 	}
