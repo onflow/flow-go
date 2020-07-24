d85f9ce54d329e60ab13285d71442a9f4be1e94b
Max Wolter <max.wolter@dapperlabs.com>
2020-07-24 17:06:05 +0200

    added compliance checks for block views

--- state/protocol/badger/mutator.go
+++ state/protocol/badger/mutator.go
@@ -207,6 +207,10 @@ func (m *Mutator) Bootstrap(root *flow.Block, result *flow.ExecutionResult, seal
 		if err != nil {
 			return fmt.Errorf("could not insert epoch counter: %w", err)
 		}
+		err = operation.IndexEpochStart(setup.Counter, root.Header.View)(tx)
+		if err != nil {
+			return fmt.Errorf("could not index epoch start: %w", err)
+		}
 		err = operation.InsertEpochSetup(setup.Counter, setup)(tx)
 		if err != nil {
 			return fmt.Errorf("could not insert epoch seed: %w", err)
@@ -455,6 +459,11 @@ func (m *Mutator) Extend(candidate *flow.Block) error {
 	if err != nil {
 		return fmt.Errorf("could not retrieve epoch counter: %w", err)
 	}
+	var start uint64
+	err = m.state.db.View(operation.LookupEpochStart(counter, &start))
+	if err != nil {
+		return fmt.Errorf("could not retrieve epoch start: %w", err)
+	}
 	var setup flow.EpochSetup
 	err = m.state.db.View(operation.RetrieveEpochSetup(counter, &setup))
 	if err != nil {
@@ -482,7 +491,7 @@ func (m *Mutator) Extend(candidate *flow.Block) error {
 	for _, seal := range payload.Seals {
 		for _, event := range seal.ServiceEvents {
 
-			// convert the event into a strongly typed service event
+			// Convert the event into a strongly typed service event.
 			// TODO: We might want to do this upon creation of the seals
 			// instead; the only question is how to encode it over the
 			// network nicely, as a slice of interfaces needs additional
@@ -493,52 +502,89 @@ func (m *Mutator) Extend(candidate *flow.Block) error {
 				return fmt.Errorf("could not decode service event: %w", err)
 			}
 
-			// type assert the event to know what to do about it
 			switch ev := service.(type) {
 
-			// handle epoch setup events
 			case *flow.EpochSetup:
 
-				// we should only have a single epoch setup event per epoch
+				// We should only have a single epoch setup event per epoch.
 				if didSetup {
 					return fmt.Errorf("duplicate epoch setup service event")
 				}
 
-				// the setup event should have the counter increased by one
+				// The setup event should have the counter increased by one.
 				if ev.Counter != counter+1 {
 					return fmt.Errorf("next epoch setup has invalid counter (%d => %d)", counter, ev.Counter)
 				}
 
-				// make sure to disallow multiple commit events per payload
+				// The final view needs to be after the current epoch final view.
+				// NOTE: This kind of operates as an overflow check for the other checks.
+				if ev.FinalView <= setup.FinalView {
+					return fmt.Errorf("next epoch must be after current epoch (%d <= %d)", ev.FinalView, setup.FinalView)
+				}
+
+				// The total length of the epoch must be longer than the auction window plus twice the grace period.
+				// This leaves at least one grace period for the actual processing of the events.
+				length := setup.FinalView - ev.FinalView
+				minimum := m.state.cfg.auctionWindow + 2*m.state.cfg.gracePeriod
+				if length < minimum {
+					return fmt.Errorf("next epoch has insufficient length (%d < %d)", length, minimum)
+				}
+
+				// The setup event can not happen until after the auction window.
+				auction := start + m.state.cfg.auctionWindow
+				if candidate.Header.View <= auction {
+					return fmt.Errorf("next epoch setup inside auction window (%d <= %d)", candidate.Header.View, auction)
+				}
+
+				// The setup event can not happen after the current epoch end minus the grace period.
+				// NOTE: This is a sanity check; at this point, the chain should have halted anyway, as
+				// we can't make progress unless the next epoch is set up on time.
+				grace := setup.FinalView - m.state.cfg.gracePeriod
+				if candidate.Header.View >= grace {
+					return fmt.Errorf("next epoch setup inside grace peried (%d >= %d)", candidate.Header.View, grace)
+				}
+
+				// Make sure to disallow multiple commit events per payload.
 				didSetup = true
 
-			// handle epoch commit events
 			case *flow.EpochCommit:
 
-				// we should only have a single epoch commit event per epoch
+				// We should only have a single epoch commit event per epoch.
 				if didCommit {
 					return fmt.Errorf("duplicate epoch commit service event")
 				}
 
-				// the epoch setup event needs to happen before the commit
-				// NOTE: once we add the check for the view windows within
-				// which each event is valid, this check turns into a mere
-				// sanity check, because the chain shouldn't make progress
-				// unless the setup event has happened
+				// The epoch setup event needs to happen before the commit.
 				if !didSetup {
 					return fmt.Errorf("missing epoch setup for epoch commit")
 				}
 
-				// the commit event should have the counter increased by one
+				// The commit event should have the counter increased by one.
 				if ev.Counter != counter+1 {
 					return fmt.Errorf("next epoch commit has invalid counter (%d => %d)", counter, ev.Counter)
 				}
 
-				// make sure to disallow multiple commit events per payload
+				// The commit event can not happen until after the auction window.
+				// NOTE: This is a sanity check; as the commit event can't happen before the setup event,
+				// the auction period must have passed anyway.
+				auction := start + m.state.cfg.auctionWindow
+				if candidate.Header.View <= auction {
+					return fmt.Errorf("next epoch commit inside auction window (%d <= %d)", candidate.Header.View, auction)
+				}
+
+				// The commit event can not happen after the current epoch end minus the grace period.
+				// NOTE: This is a sanity check; at this point, the chain should have halted anyway, as
+				// we can't make progress unless the next epoch is set up on time.
+				grace := setup.FinalView - m.state.cfg.gracePeriod
+				if candidate.Header.View >= grace {
+					return fmt.Errorf("next epoch commit inside grace peried (%d >= %d)", candidate.Header.View, grace)
+				}
+
+				// Make sure to disallow multiple commit events per payload.
 				didCommit = true
 
 			default:
-				// NOTE: this is already handled as error in the decoding; if
+				// NOTE: This is already handled as error in the decoding; if
 				// we decode an event successfully, it should be processed, so
 				// all we need to do here is do a warning log that there are
 				// unprocessed service events.
