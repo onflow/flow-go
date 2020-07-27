b41bb55c063a12893a8ec75c046bece05ab1017a
Ramtin Mehdizadeh Seraj <ramtin@axiomzen.co>
2020-07-27 13:59:17 -0700

    clean up

--- ledger/complete/ledger_test.go
+++ ledger/complete/ledger_test.go
@@ -2,7 +2,10 @@ package complete_test
 
 import (
 	"bytes"
+	"fmt"
+	"math/rand"
 	"testing"
+	"time"
 
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
@@ -12,6 +15,7 @@ import (
 	"github.com/dapperlabs/flow-go/ledger/common/encoding"
 	"github.com/dapperlabs/flow-go/ledger/common/utils"
 	"github.com/dapperlabs/flow-go/ledger/complete"
+	"github.com/dapperlabs/flow-go/ledger/partial/ptrie"
 	"github.com/dapperlabs/flow-go/module/metrics"
 	"github.com/dapperlabs/flow-go/utils/unittest"
 )
@@ -174,4 +178,193 @@ func TestLedger_Proof(t *testing.T) {
 			assert.True(t, common.VerifyTrieBatchProof(proof, newSc))
 		})
 	})
+}
+
+func Test_WAL(t *testing.T) {
+	numInsPerStep := 2
+	keyNumberOfParts := 10
+	keyPartMinByteSize := 1
+	keyPartMaxByteSize := 100
+	valueMaxByteSize := 2 << 16 //16kB
+	size := 10
+	metricsCollector := &metrics.NoopCollector{}
+
+	unittest.RunWithTempDir(t, func(dir string) {
+
+		// cache size intentionally is set to size to test deletion
+		led, err := complete.NewLedger(dir, size, metricsCollector, nil)
+		require.NoError(t, err)
+
+		var state = led.InitState()
+
+		//saved data after updates
+		savedData := make(map[string]map[string]ledger.Value)
+
+		for i := 0; i < size; i++ {
+
+			keys := utils.RandomUniqueKeys(numInsPerStep, keyNumberOfParts, keyPartMinByteSize, keyPartMaxByteSize)
+			values := utils.RandomValues(numInsPerStep, 1, valueMaxByteSize)
+			update, err := ledger.NewUpdate(state, keys, values)
+			assert.NoError(t, err)
+			state, err = led.Set(update)
+			require.NoError(t, err)
+			fmt.Printf("Updated with %x\n", state)
+
+			data := make(map[string]ledger.Value, len(keys))
+			for j, key := range keys {
+				encKey := encoding.EncodeKey(&key)
+				data[string(encKey)] = values[j]
+			}
+
+			savedData[string(state)] = data
+		}
+
+		<-led.Done()
+
+		led2, err := complete.NewLedger(dir, size+10, metricsCollector, nil)
+		require.NoError(t, err)
+
+		// random map iteration order is a benefit here
+		for state, data := range savedData {
+
+			keys := make([]ledger.Key, 0, len(data))
+			for encKey := range data {
+				key, err := encoding.DecodeKey([]byte(encKey))
+				assert.NoError(t, err)
+				keys = append(keys, *key)
+			}
+
+			query, err := ledger.NewQuery(ledger.State(state), keys)
+			assert.NoError(t, err)
+			registerValues, err := led2.Get(query)
+			require.NoError(t, err)
+
+			for i, key := range keys {
+				registerValue := registerValues[i]
+				encKey := encoding.EncodeKey(&key)
+				assert.True(t, data[string(encKey)].Equals(registerValue))
+			}
+		}
+
+		// test deletion
+		s := led2.ForestSize()
+		assert.Equal(t, s, size)
+
+		<-led2.Done()
+	})
+}
+
+func TestLedgerFunctionality(t *testing.T) {
+	rand.Seed(time.Now().UnixNano())
+	// You can manually increase this for more coverage
+	experimentRep := 2
+
+	metricsCollector := &metrics.NoopCollector{}
+
+	for e := 0; e < experimentRep; e++ {
+		numInsPerStep := 100
+		numHistLookupPerStep := 10
+		pathByteSize := 32
+		keyNumberOfParts := 10
+		keyPartMinByteSize := 1
+		keyPartMaxByteSize := 100
+		stateComSize := 32
+		valueMaxByteSize := 2 << 16 //16kB
+		activeTries := 1000
+		steps := 40                                  // number of steps
+		histStorage := make(map[string]ledger.Value) // historic storage string(key, state) -> value
+		latestValue := make(map[string]ledger.Value) // key to value
+		unittest.RunWithTempDir(t, func(dbDir string) {
+			led, err := complete.NewLedger(dbDir, activeTries, metricsCollector, nil)
+			assert.NoError(t, err)
+			state := led.InitState()
+			for i := 0; i < steps; i++ {
+				// add new keys
+				// TODO update some of the existing keys and shuffle them
+				keys := utils.RandomUniqueKeys(numInsPerStep, keyNumberOfParts, keyPartMinByteSize, keyPartMaxByteSize)
+				values := utils.RandomValues(numInsPerStep, 1, valueMaxByteSize)
+				update, err := ledger.NewUpdate(state, keys, values)
+				assert.NoError(t, err)
+				newState, err := led.Set(update)
+				assert.NoError(t, err)
+
+				// capture new values for future query
+				for j, k := range keys {
+					encKey := encoding.EncodeKey(&k)
+					histStorage[string(newState)+string(encKey)] = values[j]
+					latestValue[string(encKey)] = values[j]
+				}
+
+				// read values and compare values
+				query, err := ledger.NewQuery(newState, keys)
+				assert.NoError(t, err)
+				retValues, err := led.Get(query)
+				assert.NoError(t, err)
+				// byte{} is returned as nil
+				assert.True(t, valuesMatches(values, retValues))
+
+				// validate proofs (check individual proof and batch proof)
+				proof, err := led.Prove(query)
+				assert.NoError(t, err)
+
+				bProof, err := encoding.DecodeTrieBatchProof(proof)
+				assert.NoError(t, err)
+
+				// validate batch proofs
+				isValid := common.VerifyTrieBatchProof(bProof, newState)
+				assert.True(t, isValid)
+
+				// validate proofs as a batch
+				_, err = ptrie.NewPSMT(newState, pathByteSize, bProof)
+				assert.NoError(t, err)
+
+				// query all exising keys (check no drop)
+				for ek, v := range latestValue {
+					k, err := encoding.DecodeKey([]byte(ek))
+					assert.NoError(t, err)
+					query, err := ledger.NewQuery(newState, []ledger.Key{*k})
+					assert.NoError(t, err)
+					rv, err := led.Get(query)
+					assert.NoError(t, err)
+					assert.True(t, v.Equals(rv[0]))
+				}
+
+				// query some of historic values (map return is random)
+				j := 0
+				for s := range histStorage {
+					value := histStorage[s]
+					state := []byte(s[:stateComSize])
+					enk := []byte(s[stateComSize:])
+					key, err := encoding.DecodeKey([]byte(enk))
+					assert.NoError(t, err)
+					query, err := ledger.NewQuery(state, []ledger.Key{*key})
+					assert.NoError(t, err)
+					rv, err := led.Get(query)
+					assert.NoError(t, err)
+					assert.True(t, value.Equals(rv[0]))
+					j++
+					if j >= numHistLookupPerStep {
+						break
+					}
+				}
+				state = newState
+			}
+		})
+	}
+}
+
+func valuesMatches(expected []ledger.Value, got []ledger.Value) bool {
+	if len(expected) != len(got) {
+		return false
+	}
+	// replace nils
+	for i, v := range got {
+		if v == nil {
+			got[i] = []byte{}
+		}
+		if !bytes.Equal(expected[i], got[i]) {
+			return false
+		}
+	}
+	return true
 }
