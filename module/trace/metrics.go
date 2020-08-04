758fadc5df416aa554b2eb8e1aadd7f13eb4aca2
Jordan Schalm <jordan@dapperlabs.com>
2020-08-04 14:52:41 -0700

    add buckets to span metric

--- module/trace/metrics.go
+++ module/trace/metrics.go
@@ -7,7 +7,8 @@ import (
 
 var (
 	spanDurationMetric = promauto.NewHistogramVec(prometheus.HistogramOpts{
-		Name: "span_duration_s",
-		Help: "The duration of the Jaeger span in seconds",
+		Name:    "span_duration_s",
+		Help:    "The duration of the Jaeger span in seconds",
+		Buckets: []float64{.001, .005, .01, .05, .1, .25, .5, 1, 2.5, 5, 10, 30},
 	}, []string{"name"})
 )
