fe98f361e39f1d8a9baa4a3c1efa9eeaeba15958
Kay-Zee <kan@axiomzen.co>
2020-08-05 19:40:02 -0700

    Add retry delay for badger db conflicts

--- storage/badger/operation/modifiers.go
+++ storage/badger/operation/modifiers.go
@@ -2,11 +2,15 @@ package operation
 
 import (
 	"errors"
+	"time"
 
 	"github.com/dgraph-io/badger/v2"
 
 	"github.com/dapperlabs/flow-go/storage"
 )
+
+const initialRetryDelay = 10 * time.Millisecond
+const maxRetryDelay = 250 * time.Millisecond
 
 func SkipDuplicates(op func(*badger.Txn) error) func(tx *badger.Txn) error {
 	return func(tx *badger.Txn) error {
@@ -19,9 +23,15 @@ func SkipDuplicates(op func(*badger.Txn) error) func(tx *badger.Txn) error {
 }
 
 func RetryOnConflict(action func(func(*badger.Txn) error) error, op func(tx *badger.Txn) error) error {
+	delay := initialRetryDelay
 	for {
 		err := action(op)
 		if errors.Is(err, badger.ErrConflict) {
+			time.Sleep(delay)
+			delay = delay * 2
+			if delay > maxRetryDelay {
+				delay = maxRetryDelay
+			}
 			continue
 		}
 		return err
