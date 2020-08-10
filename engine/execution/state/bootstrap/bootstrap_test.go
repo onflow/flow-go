3641348c246d3b267feca4beb22f2b8e764cd72f
Bastian Müller <bastian@axiomzen.co>
2020-08-10 15:31:53 -0700

    update state commitment hashes in test

--- engine/execution/state/bootstrap/bootstrap_test.go
+++ engine/execution/state/bootstrap/bootstrap_test.go
@@ -38,8 +38,7 @@ func TestBootstrapLedger(t *testing.T) {
 }
 
 func TestBootstrapLedger_ZeroTokenSupply(t *testing.T) {
-	var expectedStateCommitment, _ = hex.DecodeString("0189af4919ddc65db33edf6156aa6c9ad8aab77ba632095f078f22a65c68658a")
-
+	var expectedStateCommitment, _ = hex.DecodeString("c9d4c845c0e159c1f3b3bc7c4dc570abd77d83870e5b2312be44e476f6715664")
 	unittest.RunWithTempDir(t, func(dbDir string) {
 
 		chain := flow.Mainnet.Chain()
