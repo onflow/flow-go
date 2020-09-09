cd908808d8c377f1b5548a72507afdb34fbe20c9
Jordan Schalm <jordan@dapperlabs.com>
2020-09-09 09:34:34 -0700

    add events interface for protocol state

---
+++ state/protocol/events.go
@@ -0,0 +1,43 @@
+package protocol
+
+import (
+	"github.com/dapperlabs/flow-go/model/flow"
+)
+
+// Events defines a set of events that occur within the protocol state, that
+// can be propagated to other components via an implementation of this interface.
+//
+// NOTE: These methods are only called once the fork containing the relevant
+// event has been finalized.
+type Events interface {
+
+	// BlockFinalized is called when a block is finalized.
+	BlockFinalized(block *flow.Header)
+
+	// EpochTransition is called when we transition to a new epoch. This is
+	// equivalent to the beginning of the new epoch's staking phase and the end
+	// of the previous epoch's epoch committed phase.
+	//
+	// The block parameter is the first block of the new epoch.
+	//
+	// NOTE: Only called once the transition has been finalized.
+	EpochTransition(newEpoch uint64, first *flow.Header)
+
+	// EpochSetupPhaseStarted is called when we begin the epoch setup phase for
+	// the current epoch. This is equivalent to the end of the epoch staking
+	// phase for the current epoch.
+	//
+	// The block parameter is the first block of the epoch setup phase.
+	//
+	// NOTE: Only called once the phase transition has been finalized.
+	EpochSetupPhaseStarted(epoch uint64, first *flow.Header)
+
+	// EpochCommittedPhase is called when we begin the epoch committed phase
+	// for the current epoch. This is equivalent to the end of the epoch setup
+	// phase for the current epoch.
+	//
+	// The block parameter is the first block of the epoch setup phase.
+	//
+	// NOTE: Only called once the phase transition has been finalized.
+	EpochCommittedPhaseStarted(epoch uint64, first *flow.Header)
+}
