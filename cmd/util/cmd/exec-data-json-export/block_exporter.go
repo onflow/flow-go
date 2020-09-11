d5571de104c160c40226983d334c99f526124009
Ramtin Mehdizadeh Seraj <ramtin@axiomzen.co>
2020-09-11 15:15:06 -0700

    add json export for exec node data

---
+++ cmd/util/cmd/exec-data-json-export/block_exporter.go
@@ -0,0 +1,115 @@
+package jsonexporter
+
+import (
+	"bufio"
+	"encoding/hex"
+	"encoding/json"
+	"fmt"
+	"os"
+	"path/filepath"
+	"time"
+
+	"github.com/dapperlabs/flow-go/cmd/util/cmd/common"
+	"github.com/dapperlabs/flow-go/model/flow"
+	"github.com/dapperlabs/flow-go/module/metrics"
+	"github.com/dapperlabs/flow-go/storage/badger"
+)
+
+type blockSummary struct {
+	BlockHeight    uint64   `json:"block_height"`
+	BlockID        string   `json:"block_id"`
+	ParentBlockID  string   `json:"parent_block_id"`
+	ParentVoterIDs []string `json:"parent_voter_ids"`
+	// ParentVoterSig []string  `json:"parent_voter_sig"`
+	ProposerID string `json:"proposer_id"`
+	// ProposerSig    string  `json:"proposer_sig"`
+	Timestamp     time.Time `json:"timestamp"`
+	CollectionIDs []string  `json:"collection_ids"`
+	SealedBlocks  []string  `json:"sealed_blocks"`
+}
+
+// ExportBlocks exports blocks (note this only export blocks of the main chain and doesn't export forks)
+func ExportBlocks(blockID flow.Identifier, dbPath string, outputPath string) error {
+
+	// traverse backward from the given block (parent block) and fetch by blockHash
+	db := common.InitStorage(dbPath)
+	defer db.Close()
+
+	cacheMetrics := &metrics.NoopCollector{}
+	headers := badger.NewHeaders(cacheMetrics, db)
+	index := badger.NewIndex(cacheMetrics, db)
+	identities := badger.NewIdentities(cacheMetrics, db)
+	guarantees := badger.NewGuarantees(cacheMetrics, db)
+	seals := badger.NewSeals(cacheMetrics, db)
+	payloads := badger.NewPayloads(db, index, identities, guarantees, seals)
+	blocks := badger.NewBlocks(db, headers, payloads)
+
+	activeBlockID := blockID
+	outputFile := filepath.Join(outputPath, "blocks.jsonl")
+
+	fi, err := os.Create(outputFile)
+	if err != nil {
+		return fmt.Errorf("could not create block output file %w", err)
+	}
+	defer fi.Close()
+
+	blockWriter := bufio.NewWriter(fi)
+	defer blockWriter.Flush()
+
+	for {
+		header, err := headers.ByBlockID(activeBlockID)
+		if err != nil {
+			// no more header is available
+			return nil
+		}
+
+		block, err := blocks.ByID(activeBlockID)
+		if err != nil {
+			// log.Fatal().Err(err).Msg("could not load block")
+			return nil
+		}
+
+		parentVoterIDs := make([]string, 0)
+		for _, v := range header.ParentVoterIDs {
+			parentVoterIDs = append(parentVoterIDs, hex.EncodeToString(v[:]))
+		}
+
+		cols := make([]string, 0)
+		for _, g := range block.Payload.Guarantees {
+			cols = append(cols, hex.EncodeToString(g.CollectionID[:]))
+		}
+
+		seals := make([]string, 0)
+		for _, s := range block.Payload.Seals {
+			seals = append(seals, hex.EncodeToString(s.BlockID[:]))
+		}
+
+		pvIDs := make([]string, 0)
+		for _, i := range header.ParentVoterIDs {
+			pvIDs = append(pvIDs, hex.EncodeToString(i[:]))
+		}
+
+		b := blockSummary{
+			BlockID:        hex.EncodeToString(activeBlockID[:]),
+			BlockHeight:    header.Height,
+			ParentBlockID:  hex.EncodeToString(header.ParentID[:]),
+			ParentVoterIDs: pvIDs,
+			ProposerID:     hex.EncodeToString(header.ProposerID[:]),
+			Timestamp:      header.Timestamp,
+			CollectionIDs:  cols,
+			SealedBlocks:   seals,
+		}
+
+		jsonData, err := json.Marshal(b)
+		if err != nil {
+			return fmt.Errorf("could not create a json obj for a block: %w", err)
+		}
+		_, err = blockWriter.WriteString(string(jsonData) + "\n")
+		if err != nil {
+			return fmt.Errorf("could not write block json to the file: %w", err)
+		}
+		blockWriter.Flush()
+
+		activeBlockID = header.ParentID
+	}
+}
---
+++ cmd/util/cmd/exec-data-json-export/ledger_exporter.go
@@ -0,0 +1,114 @@
+package jsonexporter
+
+import (
+	"bytes"
+	"encoding/hex"
+	"errors"
+	"fmt"
+	"path/filepath"
+	"time"
+
+	"github.com/dapperlabs/flow-go/cmd/util/cmd/common"
+	"github.com/dapperlabs/flow-go/model/flow"
+	"github.com/dapperlabs/flow-go/module/metrics"
+	"github.com/dapperlabs/flow-go/storage/badger"
+	"github.com/dapperlabs/flow-go/storage/ledger"
+	"github.com/dapperlabs/flow-go/storage/ledger/mtrie"
+	"github.com/dapperlabs/flow-go/storage/ledger/mtrie/flattener"
+	"github.com/dapperlabs/flow-go/storage/ledger/mtrie/trie"
+	"github.com/dapperlabs/flow-go/storage/ledger/wal"
+	"github.com/rs/zerolog/log"
+)
+
+// ExportLedger exports ledger key value pairs at the given blockID
+func ExportLedger(blockID flow.Identifier, dbPath string, ledgerPath string, outputPath string) error {
+	db := common.InitStorage(dbPath)
+	defer db.Close()
+
+	cache := &metrics.NoopCollector{}
+	commits := badger.NewCommits(cache, db)
+
+	targetHash, err := commits.ByBlockID(blockID)
+	if err != nil {
+		return fmt.Errorf("cannot get state commitment for block: %w", err)
+	}
+
+	w, err := wal.NewWAL(nil, nil, ledgerPath, ledger.CacheSize, ledger.RegisterKeySize, wal.SegmentSize)
+	if err != nil {
+		return fmt.Errorf("cannot create WAL: %w", err)
+	}
+	defer func() {
+		_ = w.Close()
+	}()
+
+	// TODO port this to use new forest
+	mForest, err := mtrie.NewMForest(ledger.RegisterKeySize, outputPath, 1000, &metrics.NoopCollector{}, func(evictedTrie *trie.MTrie) error { return nil })
+	if err != nil {
+		return fmt.Errorf("cannot create mForest: %w", err)
+	}
+
+	i := 0
+	valuesSize := 0
+	valuesCount := 0
+	startTime := time.Now()
+	found := false
+	FoundHashError := fmt.Errorf("found hash %s", targetHash)
+
+	err = w.ReplayLogsOnly(
+		func(forestSequencing *flattener.FlattenedForest) error {
+			rebuiltTries, err := flattener.RebuildTries(forestSequencing)
+			if err != nil {
+				return fmt.Errorf("rebuilding forest from sequenced nodes failed: %w", err)
+			}
+			err = mForest.AddTries(rebuiltTries)
+			if err != nil {
+				return fmt.Errorf("adding rebuilt tries to forest failed: %w", err)
+			}
+			return nil
+		},
+		func(stateCommitment flow.StateCommitment, keys [][]byte, values [][]byte) error {
+
+			newTrie, err := mForest.Update(stateCommitment, keys, values)
+
+			for _, value := range values {
+				valuesSize += len(value)
+			}
+
+			valuesCount += len(values)
+
+			if err != nil {
+				return fmt.Errorf("error while updating mForest: %w", err)
+			}
+
+			if bytes.Equal(targetHash, newTrie.RootHash()) {
+				found = true
+				return FoundHashError
+			}
+
+			i++
+			if i%1000 == 0 {
+				log.Info().Int("values_count", valuesCount).Int("values_size_bytes", valuesSize).Int("updates_count", i).Msg("progress")
+			}
+
+			return err
+		},
+		func(commitment flow.StateCommitment) error {
+			return nil
+		})
+
+	duration := time.Since(startTime)
+
+	if !errors.Is(err, FoundHashError) {
+		return fmt.Errorf("error while processing WAL: %w", err)
+	}
+
+	if !found {
+		return fmt.Errorf("no value found: %w", err)
+	}
+
+	log.Info().Int("values_count", valuesCount).Int("values_size_bytes", valuesSize).Int("updates_count", i).Float64("total_time_s", duration.Seconds()).Msg("finished seeking")
+	log.Info().Msg("writing root checkpoint")
+
+	mForest.DumpTrieAsJSON(targetHash, filepath.Join(outputPath, hex.EncodeToString(targetHash)+".trie.jsonl"))
+	return nil
+}
