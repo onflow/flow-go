534b2d25538f1685fbf7de7543c11501ed94570c
Jordan Schalm <jordan@dapperlabs.com>
2020-09-24 14:58:27 -0700

    add mock for event gadgets

--- state/protocol/events/gadgets.go
+++ state/protocol/events/gadgets.go
@@ -1,6 +1,7 @@
 package events
 
-// Heights enables subscribing to finalized heights.
+// Heights enables subscribing to specific heights. The callback is invoked
+// when the given height is finalized.
 type Heights interface {
 
 	// OnHeight registers the callback for the given height.
