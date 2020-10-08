d2e2df55bba8ec356da1cffd042fa048ed72f80a
arrivets <arrivets@gmail.com>
2020-10-08 15:01:05 +0100

    Disable require-one-approval in integration/execution tests

--- integration/testnet/network.go
+++ integration/testnet/network.go
@@ -487,9 +487,6 @@ func (net *FlowNetwork) AddNode(t *testing.T, bootstrapDir string, nodeConf Cont
 			// tests only start 1 verification node
 			nodeContainer.addFlag("chunk-alpha", "1")
 
-			// require one approval per chunk when sealing
-			nodeContainer.addFlag("require-one-approval", "true")
-
 		case flow.RoleVerification:
 			// use 1 here instead of the default 5, because the integration
 			// tests only start 1 verification node
