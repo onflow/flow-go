ef910e56ba730a8e49a42ffd5fa3c5285e349735
vishal <1117327+vishalchangrani@users.noreply.github.com>
2020-10-13 08:23:35 -0700

    wip

---
+++ network/gossip/libp2p/peermgr/peerManager.go
@@ -0,0 +1,21 @@
+package peermgr
+
+import "time"
+
+var PeerUpdateInterval = 1 * time.Second
+
+type PeerManager struct {
+
+}
+
+func (pm *PeerManager) RequestPeerUpdate() {
+
+}
+
+func (pm *PeerManager) UpdatePeers() {
+
+}
+
+func (pm *PeerManager) updateLoop() {
+
+}
