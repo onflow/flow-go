ce7ec7a7775b3b7d19491c8ab85a313cb2fa7686
Yahya <yahya@dapperlabs.com>
2020-10-13 14:17:51 -0700

    completes body of Subset

--- network/gossip/libp2p/topology/topicAwareTopology.go
+++ network/gossip/libp2p/topology/topicAwareTopology.go
@@ -2,7 +2,6 @@ package topology
 
 import (
 	"fmt"
-	"math"
 
 	"github.com/onflow/flow-go/model/flow"
 	"github.com/onflow/flow-go/model/flow/filter"
@@ -13,6 +12,9 @@ type TopicAwareTopology struct {
 	roleByTopic map[string]flow.RoleList // used to map between topics and roles subscribed to topics
 }
 
+// Subset receives an identity list and a topic. It then extracts list of nodes subscribed to the topic from the
+// identity list, and samples and returns`(k+1)/2` of them uniformly where `k` is the number of nodes subscribed to the
+// topic.
 func (t *TopicAwareTopology) Subset(idList flow.IdentityList, _ uint, topic string) (flow.IdentityList, error) {
 	// extracts flow roles subscribed to topic.
 	roles, ok := t.roleByTopic[topic]
@@ -22,10 +24,15 @@ func (t *TopicAwareTopology) Subset(idList flow.IdentityList, _ uint, topic stri
 
 	// extracts node ids with specified roles.
 	ids := idList.Filter(filter.HasRole(roles...))
-	fanout := math.Ceil((float64)(len(ids)+1) / 2)
-	randomSample, err := t.RandPermTopology.Subset(ids, uint(fanout), DummyTopic)
 
+	// determines fanout concerning connectedness.
+	fanout := uint((len(ids) + 1) / 2)
+
+	// samples uniformly among the ids of specified topic.
+	randomSample, err := t.RandPermTopology.Subset(ids, fanout, DummyTopic)
 	if err != nil {
-		return nil, fmt.Errorf("could not ")
+		return nil, fmt.Errorf("could not sample from subscribed nodes to the topic: %w", err)
 	}
+
+	return randomSample, nil
 }
