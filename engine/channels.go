bcd69ec6ff21d0ec96beba08490ccfdd8c8e5e94
Yahya <yahya@dapperlabs.com>
2020-10-14 14:41:45 -0700

    adds initial list of topic map

--- engine/channels.go
+++ engine/channels.go
@@ -9,6 +9,8 @@ import (
 	"github.com/onflow/flow-go/model/flow"
 )
 
+var TopicMap map[string]flow.RoleList
+
 // channel IDs
 const (
 
@@ -48,6 +50,44 @@ const (
 	ProvideChunks            = RequestChunks
 	ProvideReceiptsByBlockID = RequestReceiptsByBlockID
 )
+
+func Init() {
+
+}
+
+func initializeTopicMap() {
+	TopicMap := make(map[string]flow.RoleList)
+	// Channels for consensus protocols
+	TopicMap[ConsensusCommittee] = flow.RoleList{flow.RoleConsensus}
+
+	// Channels for protocols actively synchronizing state across nodes
+	TopicMap[SyncCommittee] = flow.RoleList{flow.RoleConsensus}
+	TopicMap[SyncExecution] = flow.RoleList{flow.RoleExecution}
+
+	// Channels for actively pushing entities to subscribers
+	TopicMap[PushTransactions] = flow.RoleList{flow.RoleCollection}
+	TopicMap[PushGuarantees] = flow.RoleList{flow.RoleCollection, flow.RoleConsensus}
+	TopicMap[PushBlocks] = flow.RoleList{flow.RoleCollection, flow.RoleConsensus, flow.RoleExecution, flow.RoleVerification}
+	TopicMap[PushReceipts] = flow.RoleList{flow.RoleConsensus, flow.RoleExecution, flow.RoleVerification}
+	TopicMap[PushApprovals] = flow.RoleList{flow.RoleConsensus, flow.RoleVerification}
+
+	// Channels for actively requesting missing entities
+	TopicMap[RequestCollections] = flow.RoleList{flow.RoleCollection, flow.RoleExecution}
+	TopicMap[RequestChunks] = flow.RoleList{flow.RoleExecution, flow.RoleVerification}
+	TopicMap[RequestReceiptsByBlockID] = flow.RoleList{flow.RoleConsensus}
+
+	// Channel aliases to make the code more readable / more robust to errors
+	TopicMap[ReceiveGuarantees] = flow.RoleList{flow.RoleConsensus}
+	TopicMap[ReceiveBlocks] = flow.RoleList{flow.RoleCollection, flow.RoleExecution,
+		flow.RoleVerification, flow.RoleAccess}
+	TopicMap[ReceiveReceipts] = flow.RoleList{flow.RoleAccess, flow.RoleConsensus, flow.RoleVerification}
+	TopicMap[ReceiveApprovals] = flow.RoleList{flow.RoleVerification}
+
+	TopicMap[ProvideCollections] = flow.RoleList{flow.RoleCollection}
+	TopicMap[ProvideChunks] = flow.RoleList{flow.RoleExecution}
+	TopicMap[ProvideReceiptsByBlockID] = flow.RoleList{flow.RoleExecution}
+
+}
 
 // FullyQualifiedChannelName returns the unique channel name made up of channel name string suffixed with root block id
 // The root block id is used to prevent cross talks between nodes on different sporks
