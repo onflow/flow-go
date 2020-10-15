001419c0b5fa8b1639f46144c28c3e941288d782
Yahya <yahya@dapperlabs.com>
2020-10-15 13:13:33 -0700

    defines topic aware topology

--- network/gossip/libp2p/topology/topicAwareTopology.go
+++ network/gossip/libp2p/topology/topicAwareTopology.go
@@ -3,59 +3,44 @@ package topology
 import (
 	"fmt"
 
+	"github.com/onflow/flow-go/engine"
 	"github.com/onflow/flow-go/model/flow"
 	"github.com/onflow/flow-go/model/flow/filter"
 )
 
-type TopicAwareOpt func(*TopicAwareTopology)
-
-// WithTopic is an option function for TopicAwareTopology that adds
-// a topic as well as its related engaged roles to the topology.
-func WithTopic(name string, roles flow.RoleList) TopicAwareOpt {
-	return func(t *TopicAwareTopology) {
-		t.roleByTopic[name] = roles
-	}
-}
-
 type TopicAwareTopology struct {
 	RandPermTopology
-	roleByTopic map[string]flow.RoleList // used to map between topics and roles subscribed to topics
+	seed int64
 }
 
 // NewTopicAwareTopology returns an instance of the TopicAwareTopology
-func NewTopicAwareTopology(topics ...TopicAwareOpt) (*TopicAwareTopology, error) {
-	t := &TopicAwareTopology{
-		roleByTopic: make(map[string]flow.RoleList),
+func NewTopicAwareTopology(myId flow.Identifier) (*TopicAwareTopology, error) {
+	seed, err := seedFromID(myId)
+	if err != nil {
+		return nil, fmt.Errorf("failed to seed topology: %w", err)
 	}
-
-	for _, apply := range topics {
-		apply(t)
+	t := &TopicAwareTopology{
+		seed: seed,
 	}
 
 	return t, nil
 }
 
-// Subset receives an identity list and a topic. It then extracts list of nodes subscribed to the topic from the
-// identity list, and samples and returns`(k+1)/2` of them uniformly where `k` is the number of nodes subscribed to the
-// topic.
+// Subset samples and returns a connected graph fanout of the subscribers to the topic from the idList.
+// A connected graph fanout means that the subset of ids returned by this method on different nodes collectively
+// construct a connected graph component among all the subscribers to the topic.
 func (t *TopicAwareTopology) Subset(idList flow.IdentityList, _ uint, topic string) (flow.IdentityList, error) {
 	// extracts flow roles subscribed to topic.
-	roles, ok := t.roleByTopic[topic]
+	roles, ok := engine.GetRolesByTopic(topic)
 	if !ok {
 		return nil, fmt.Errorf("unknown topic with no subscribed roles: %s", topic)
 	}
 
-	// extracts node ids with specified roles.
-	ids := idList.Filter(filter.HasRole(roles...))
+	// extract ids of subscribers to the topic
+	subscribers := idList.Filter(filter.HasRole(roles...))
 
-	// determines fanout concerning connectedness.
-	fanout := uint((len(ids) + 1) / 2)
+	// samples subscribers of a connected graph
+	subscriberSample, _ := connectedGraphSample(subscribers, t.seed)
 
-	// samples uniformly among the ids of specified topic.
-	randomSample, err := t.RandPermTopology.Subset(ids, fanout, DummyTopic)
-	if err != nil {
-		return nil, fmt.Errorf("could not sample from subscribed nodes to the topic: %w", err)
-	}
-
-	return randomSample, nil
+	return subscriberSample, nil
 }
