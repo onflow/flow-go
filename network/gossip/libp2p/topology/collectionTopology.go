fed2db93b10a0b871188ca0ed00e51d97091a8df
Yahya <yahya@dapperlabs.com>
2020-10-15 13:16:24 -0700

    refactors collection topology to use topic aware one

--- network/gossip/libp2p/topology/collectionTopology.go
+++ network/gossip/libp2p/topology/collectionTopology.go
@@ -2,8 +2,8 @@ package topology
 
 import (
 	"fmt"
-	"strings"
 
+	"github.com/onflow/flow-go/engine"
 	"github.com/onflow/flow-go/model/flow"
 	"github.com/onflow/flow-go/model/flow/filter"
 	"github.com/onflow/flow-go/state/protocol"
@@ -12,63 +12,61 @@ import (
 // CollectionTopology builds on top of RandPermTopology and generates a deterministic random topology for collection node
 // such that nodes withing the same collection cluster form a connected graph.
 type CollectionTopology struct {
-	RandPermTopology
+	*TopicAwareTopology
 	nodeID flow.Identifier
 	state  protocol.ReadOnlyState
 	seed   int64
 }
 
 func NewCollectionTopology(nodeID flow.Identifier, state protocol.ReadOnlyState) (CollectionTopology, error) {
-	rpt, err := NewRandPermTopology(flow.RoleCollection, nodeID)
+	tat, err := NewTopicAwareTopology(nodeID)
 	if err != nil {
 		return CollectionTopology{}, err
 	}
 	return CollectionTopology{
-		RandPermTopology: rpt,
-		nodeID:           nodeID,
-		state:            state,
+		TopicAwareTopology: tat,
+		nodeID:             nodeID,
+		state:              state,
 	}, nil
 }
 
 // Subset samples the idList and returns a list of nodes to connect with such that:
 // a. this node is directly or indirectly connected to all other nodes in the same cluster
-// b. to all other nodes
-// c. to at least one node of each type and
-///d. to all other nodes of the same type.
+// b. to all other nodes that it shares a subscription topic
 // The collection nodes within a collection cluster need to form a connected graph among themselves independent of any
 // other nodes to ensure reliable dissemination of cluster specific topic messages. e.g ClusterBlockProposal
-// Similarly, all nodes of network need to form a connected graph, to ensure reliable dissemination of messages for
-// topics subscribed by all node types e.g. BlockProposals
-// Each node should be connected to at least one node of each type to ensure nodes don't form an island of a specific
-// role, specially since some node types are represented by a very small number of nodes (e.g. few access nodes compared
-//to tens or hundreds of collection nodes)
-// Finally, all nodes of the same type should form a connected graph for exchanging messages for role specific topics
-// e.g. Transaction
-func (c CollectionTopology) Subset(idList flow.IdentityList, fanout uint, topic string) (flow.IdentityList, error) {
+// Similarly, they should maintain a connected graph component with each node they share a topic,
+// to ensure reliable dissemination of messages.
+func (c CollectionTopology) Subset(idList flow.IdentityList, fanout uint, _ string) (flow.IdentityList, error) {
+	var subset flow.IdentityList
 
-	if !strings.EqualFold(topic, DummyTopic) {
-		return nil, fmt.Errorf("could not support topics, expected: %s, got %s", DummyTopic, topic)
-	}
-
-	randPermSample, err := c.RandPermTopology.Subset(idList, fanout, DummyTopic)
-	if err != nil {
-		return nil, err
-	}
-
+	// extracts cluster peer ids to which the node belongs to
 	clusterPeers, err := c.clusterPeers()
 	if err != nil {
 		return nil, fmt.Errorf("failed to find cluster peers for node %s", c.nodeID.String())
 	}
+
+	// samples a connected graph fanout from the cluster peers set
 	clusterSample, _ := connectedGraphSample(clusterPeers, c.seed)
+	subset = append(subset, clusterSample...)
 
-	// include only those cluster peers which have not already been chosen by RandPermTopology
-	uniqueClusterSample := clusterSample.Filter(filter.Not(filter.In(randPermSample)))
+	// extracts topics to which this collection node belongs to
+	topics := engine.GetTopicsByRole(flow.RoleCollection)
 
-	// add those to the earlier sample from randPerm
-	randPermSample = append(randPermSample, uniqueClusterSample...)
+	for _, topic := range topics {
+		topicSample, err := c.TopicAwareTopology.Subset(idList, 0, topic)
+		if err != nil {
+			return nil, fmt.Errorf("could not sample topology for topic %s", topic)
+		}
 
-	// return the aggregated set
-	return randPermSample, nil
+		// extracts unique samples not existing in subset
+		uniqueSample := topicSample.Filter(filter.Not(filter.In(subset)))
+
+		// merges unique samples with the subset
+		subset = append(subset, uniqueSample...)
+	}
+
+	return subset, nil
 }
 
 // clusterPeers returns the list of other nodes within the same cluster as this node
