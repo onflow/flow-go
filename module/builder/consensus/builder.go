744191b7678e42e9b1ec1a7717687d1a835bc562
Jordan Schalm <jordan@dapperlabs.com>
2020-10-16 20:26:43 -0700

    adding consistency checks: wip

--- module/builder/consensus/builder.go
+++ module/builder/consensus/builder.go
@@ -232,12 +232,38 @@ func (b *Builder) BuildOn(parentID flow.Identifier, setter func(*flow.Header) er
 	// roadmap (https://github.com/dapperlabs/flow-go/issues/4872)
 
 	// create a mapping of block to seal for all seals in our pool
-	byBlock := make(map[flow.Identifier]*flow.Seal)
-	for _, seal := range b.sealPool.All() {
-		byBlock[seal.Seal.BlockID] = seal.Seal
+	encounteredInconsistentSealsForSameBlock := false
+	byBlock := make(map[flow.Identifier]*flow.SealContainer)
+	for _, sealContainer := range b.sealPool.All() {
+		seal := sealContainer.Seal
+		if sc2, found := byBlock[seal.BlockID]; found {
+			if len(sealContainer.ExecutionResult.Chunks) < 1 {
+				return nil, fmt.Errorf("ExecutionResult without chunks: %v", sealContainer.ExecutionResult.ID())
+			}
+			if len(sc2.ExecutionResult.Chunks) < 1 {
+				return nil, fmt.Errorf("ExecutionResult without chunks: %v", sc2.ExecutionResult.ID())
+			}
+			// only continue if both seals have same start AND end state:
+			if !bytes.Equal(sealContainer.Seal.FinalState, sc2.Seal.FinalState) ||
+				!bytes.Equal(sealContainer.ExecutionResult.Chunks[0].StartState, sc2.ExecutionResult.Chunks[0].StartState) {
+				sc1json, err := json.Marshal(sealContainer)
+				if err != nil {
+					return nil, err
+				}
+				sc2json, err := json.Marshal(sc2)
+				if err != nil {
+					return nil, err
+				}
+
+				fmt.Printf("ERROR: multiple seals for the same block %v: %s and %s", seal.BlockID, string(sc1json), string(sc2json))
+				encounteredInconsistentSealsForSameBlock = true
+			}
+		} else {
+			byBlock[seal.BlockID] = sealContainer
+		}
 	}
-	if int(b.sealPool.Size()) > len(byBlock) {
-		return nil, fmt.Errorf("multiple seals for the same block")
+	if encounteredInconsistentSealsForSameBlock {
+		byBlock = make(map[flow.Identifier]*flow.SealContainer)
 	}
 
 	// get the parent's block seal, which constitutes the beginning of the
@@ -261,7 +287,6 @@ func (b *Builder) BuildOn(parentID flow.Identifier, setter func(*flow.Header) er
 	var seals []*flow.Seal
 	var sealCount uint
 	for height := sealed.Height + 1; height <= finalized; height++ {
-
 		if len(byBlock) == 0 {
 			break
 		}
@@ -283,10 +308,19 @@ func (b *Builder) BuildOn(parentID flow.Identifier, setter func(*flow.Header) er
 			break
 		}
 
-		seals = append(seals, next)
+		nextErToBeSealed := next.ExecutionResult
+		if len(nextErToBeSealed.Chunks) < 1 {
+			return nil, fmt.Errorf("ExecutionResult without chunks: %v", nextErToBeSealed.ID())
+		}
+		initialState := nextErToBeSealed.Chunks[0].StartState
+		if !bytes.Equal(initialState, last.FinalState) {
+			return nil, fmt.Errorf("seal execution states do not connect in finalized")
+		}
+
+		seals = append(seals, next.Seal)
 		sealCount++
 		delete(byBlock, blockID)
-		last = next
+		last = next.Seal
 	}
 
 	// NOTE: We should only run the next part in case we did not use up all
@@ -327,9 +361,10 @@ func (b *Builder) BuildOn(parentID flow.Identifier, setter func(*flow.Header) er
 			break
 		}
 
-		seals = append(seals, next)
+		seals = append(seals, next.Seal)
+		sealCount++
 		delete(byBlock, pendingID)
-		last = next
+		last = next.Seal
 	}
 
 	b.tracer.FinishSpan(parentID, trace.CONBuildOnCreatePayloadSeals)
