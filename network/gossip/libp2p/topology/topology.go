2207e3f426663c16104a7dbb9303b12d3f7fa9d9
Yahya <yahya@dapperlabs.com>
2020-10-20 14:46:46 -0700

    removes dummy topic

--- network/gossip/libp2p/topology/topology.go
+++ network/gossip/libp2p/topology/topology.go
@@ -2,11 +2,6 @@ package topology
 
 import "github.com/onflow/flow-go/model/flow"
 
-// Todo: deprecate non-topic-aware Topology implementations.
-// DummyTopic represents an empty topic name that is used to pass to Subset method of
-// Topology implementations that do not support topics.
-const DummyTopic = ""
-
 // Topology provides a subset of nodes which a given node should directly connect to for 1-k messaging
 type Topology interface {
 	// Subset returns a random subset of the identity list that is passed
