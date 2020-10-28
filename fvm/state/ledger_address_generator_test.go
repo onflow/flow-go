620b21acb39bc81cab8c9b063d8d356c88a4fc6e
Janez Podhostnik <janez.podhostnik@gmail.com>
2020-10-28 21:22:53 +0100

    linter fixes

--- fvm/state/ledger_address_generator_test.go
+++ fvm/state/ledger_address_generator_test.go
@@ -1,10 +1,12 @@
 package state_test
 
 import (
+	"testing"
+
+	"github.com/stretchr/testify/require"
+
 	"github.com/onflow/flow-go/fvm/state"
 	"github.com/onflow/flow-go/model/flow"
-	"github.com/stretchr/testify/require"
-	"testing"
 )
 
 func Test_NewLedgerBoundAddressGenerator_NoError(t *testing.T) {
@@ -24,6 +26,7 @@ func Test_NewLedgerBoundAddressGenerator_GeneratingUpdatesState(t *testing.T) {
 	require.NoError(t, err)
 
 	stateBytes, err := ledger.Get("", "", "account_address_state")
+	require.NoError(t, err)
 
 	require.Equal(t, flow.BytesToAddress(stateBytes), flow.HexToAddress("01"))
 }
@@ -39,6 +42,7 @@ func Test_NewLedgerBoundAddressGenerator_UsesLedgerState(t *testing.T) {
 	require.NoError(t, err)
 
 	stateBytes, err := ledger.Get("", "", "account_address_state")
+	require.NoError(t, err)
 
 	require.Equal(t, flow.BytesToAddress(stateBytes), flow.HexToAddress("02"))
 }
