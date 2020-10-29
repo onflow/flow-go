5b80183f9c6b62e6dabea5ffc812bab1698b139c
Yahya <yahya@dapperlabs.com>
2020-10-29 11:29:49 -0700

    refactors topology test affected by helper

--- network/gossip/libp2p/test/topology_test.go
+++ network/gossip/libp2p/test/topology_test.go
@@ -8,7 +8,6 @@ import (
 	golog "github.com/ipfs/go-log"
 	"github.com/rs/zerolog"
 	"github.com/rs/zerolog/log"
-	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
 	"github.com/stretchr/testify/suite"
 
@@ -77,26 +76,18 @@ func (suite *TopologyTestSuite) testTopology(total int, minorityRole flow.Role) 
 		require.NoError(suite.T(), err)
 
 		// evaluates size of topology
-		checkTopologySize(suite.T(), total, top)
+		topology.CheckTopologySize(suite.T(), total, top)
 
 		// evaluates all ids in topology are valid
-		checkMembership(suite.T(), top, ids)
+		topology.CheckMembership(suite.T(), top, ids)
 		adjencyMap[ids[i].NodeID] = top
 	}
 
 	// evaluates subgraph of nodes subscribed to a channelID is connected.
 	for _, channelID := range engine.ChannelIDs() {
-		checkConnectednessByChannelID(suite.T(), adjencyMap, ids, channelID)
-
-		if engine.IsClusterChannelID(channelID) {
-			checkConnectedness()
-		}
+		topology.CheckConnectednessByChannelID(suite.T(), adjencyMap, ids, channelID)
 	}
 
-	// evaluates graph as whole is connected
-	// this implies that follower engines on distinct nodes can exchange messages
-	// and follow the consensus.
-	checkConnectedness(suite.T(), adjencyMap, ids)
 }
 
 // createDistribution creates a count distribution of ~total number of nodes with 2% minority node count
@@ -121,80 +112,4 @@ func createDistribution(total int, minority flow.Role) map[flow.Role]int {
 		}
 	}
 	return countMap
-}
-
-// checkConnectedness verifies graph as a whole is connected.
-func checkConnectedness(t *testing.T, adjMap map[flow.Identifier]flow.IdentityList, ids flow.IdentityList) {
-	checkGraphConnected(t, adjMap, ids, filter.Any)
-}
-
-// checkConnectednessByChannelID verifies that the subgraph of nodes subscribed to a channelID is connected.
-func checkConnectednessByChannelID(t *testing.T, adjMap map[flow.Identifier]flow.IdentityList, ids flow.IdentityList,
-	channelID string) {
-	roles, ok := engine.RolesByChannelID(channelID)
-	require.True(t, ok)
-	checkGraphConnected(t, adjMap, ids, filter.HasRole(roles...))
-}
-
-// checkGraphConnected checks if the graph represented by the adjacency matrix is connected.
-// It traverses the adjacency map starting from an arbitrary node and checks if all nodes that satisfy the filter
-// were visited.
-func checkGraphConnected(t *testing.T, adjMap map[flow.Identifier]flow.IdentityList, ids flow.IdentityList, f flow.IdentityFilter) {
-
-	// filter the ids and find the expected node count
-	expectedIDs := ids.Filter(f)
-	expectedCount := len(expectedIDs)
-
-	// start with an arbitrary node which satisfies the filter
-	startID := expectedIDs.Sample(1)[0].NodeID
-
-	visited := make(map[flow.Identifier]bool)
-	dfs(startID, adjMap, visited, f)
-
-	// assert that expected number of nodes were visited by DFS
-	assert.Equal(t, expectedCount, len(visited))
-}
-
-// checkMembership checks each identity in a top list belongs to all identity list
-func checkMembership(t *testing.T, top flow.IdentityList, all flow.IdentityList) {
-	for _, id := range top {
-		require.Contains(t, all, id.NodeID)
-	}
-}
-
-// TODO: fix this test after we have fanout optimized.
-// checkTopologySize evaluates that overall topology size of a node is bound by the fanout of system.
-func checkTopologySize(t *testing.T, total int, top flow.IdentityList) {
-	t.Skip("this test requires optimizing the fanout per topic")
-	fanout := (total + 1) / 2
-	require.True(t, len(top) <= fanout)
-}
-
-func clusterNum(t *testing.T, ids flow.IdentityList, size int) int {
-	collectors := ids.Filter(filter.HasRole(flow.RoleCollection))
-
-	// we need at least two collector nodes to generate a cluster
-	// and check the connectedness
-	require.True(t, len(collectors) >= 2)
-	require.True(t, size > 0)
-
-	clusterNum := len(collectors) / size
-	return int(math.Max(float64(clusterNum), 1))
-}
-
-// dfs to check graph connectedness
-func dfs(currentID flow.Identifier,
-	adjMap map[flow.Identifier]flow.IdentityList,
-	visited map[flow.Identifier]bool,
-	filter flow.IdentityFilter) {
-
-	if visited[currentID] {
-		return
-	}
-
-	visited[currentID] = true
-
-	for _, id := range adjMap[currentID].Filter(filter) {
-		dfs(id.NodeID, adjMap, visited, filter)
-	}
 }
