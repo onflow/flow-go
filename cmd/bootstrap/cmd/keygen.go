2d3cefeef41f7036e90afc9af82b3b3f3a323069
danuio <me@danu.io>
2020-10-29 10:36:11 +0000

    Check if `keys` dir exists and print summary log

--- cmd/bootstrap/cmd/keygen.go
+++ cmd/bootstrap/cmd/keygen.go
@@ -1,9 +1,14 @@
 package cmd
 
 import (
+	"fmt"
+	"io"
+	"os"
+
 	"github.com/spf13/cobra"
 
 	model "github.com/onflow/flow-go/model/bootstrap"
+	"github.com/onflow/flow-go/model/flow"
 )
 
 // keygenCmd represents the key gen command
@@ -12,9 +17,49 @@ var keygenCmd = &cobra.Command{
 	Short: "Create all keys on ansible machine for the new network",
 	Long:  ``,
 	Run: func(cmd *cobra.Command, args []string) {
+		// check if out directory exists
+		exists, err := exists(flagOutdir)
+		if err != nil {
+			log.Error().Msg("could not check if directory exists")
+			return
+		}
+
+		// check if the out directory is empty or has contents
+		if exists {
+			empty, err := isEmptyDir(flagOutdir)
+			if err != nil {
+				log.Error().Msg("could not check if directory as empty")
+				return
+			}
+
+			if !empty {
+				log.Error().Msg("output directory already exists and has content. delete and try again.")
+				return
+			}
+		}
+
+		// create keys
 		log.Info().Msg("generating internal private networking and staking keys")
 		_ = genNetworkAndStakingKeys([]model.NodeInfo{})
 		log.Info().Msg("")
+
+		// count roles
+		var nodeConfigs []model.NodeConfig
+		readJSON(flagConfig, &nodeConfigs)
+		roleCounts := map[flow.Role]uint32{
+			flow.RoleCollection:   0,
+			flow.RoleConsensus:    0,
+			flow.RoleExecution:    0,
+			flow.RoleVerification: 0,
+			flow.RoleAccess:       0,
+		}
+		for _, node := range nodeConfigs {
+			roleCounts[node.Role] = roleCounts[node.Role] + 1
+		}
+
+		for role, count := range roleCounts {
+			log.Info().Msg(fmt.Sprintf("created keys for %d %s nodes", count, role.String()))
+		}
 	},
 }
 
@@ -29,4 +74,31 @@ func init() {
 	// optional parameters
 	keygenCmd.Flags().
 		BoolVar(&flagFastKG, "fast-kg", false, "use fast (centralized) random beacon key generation instead of DKG")
+}
+
+// isEmptyDir returns True if the directory contains children
+func isEmptyDir(path string) (bool, error) {
+	f, err := os.Open(path)
+	if err != nil {
+		return false, err
+	}
+	defer f.Close()
+
+	_, err = f.Readdirnames(1)
+	if err == io.EOF {
+		return true, nil
+	}
+	return false, err // Either not empty or error, suits both cases
+}
+
+// exists
+func exists(path string) (bool, error) {
+	_, err := os.Stat(path)
+	if err == nil {
+		return true, nil
+	}
+	if os.IsNotExist(err) {
+		return false, nil
+	}
+	return false, err
 }
