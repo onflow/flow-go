9a46077948044257427675042ace5eb7ffa043d4
Yahya <yahya@dapperlabs.com>
2020-11-04 13:08:43 -0800

    replaces with stopNetworks in echo_engine

--- network/gossip/libp2p/test/echoengine_test.go
+++ network/gossip/libp2p/test/echoengine_test.go
@@ -18,7 +18,6 @@ import (
 	"github.com/onflow/flow-go/model/flow"
 	"github.com/onflow/flow-go/model/libp2p/message"
 	"github.com/onflow/flow-go/network/gossip/libp2p"
-	"github.com/onflow/flow-go/utils/unittest"
 )
 
 // EchoEngineTestSuite tests the correctness of the entire pipeline of network -> middleware -> libp2p
@@ -48,9 +47,7 @@ func (suite *EchoEngineTestSuite) SetupTest() {
 
 // TearDownTest closes the networks within a specified timeout
 func (suite *EchoEngineTestSuite) TearDownTest() {
-	for _, net := range suite.nets {
-		unittest.RequireCloseBefore(suite.T(), net.Done(), 3*time.Second, "could not stop the network")
-	}
+	stopNetworks(suite.T(), suite.nets, 3*time.Second)
 }
 
 // TestUnknownChannelID evaluates that registering an engine with an unknown channel ID returns an error.
