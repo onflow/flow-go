7f1e9ec5cc2d5c7e1509145e6a276ab4cf101172
Yahya <yahya@dapperlabs.com>
2020-11-03 16:39:05 -0800

    fixes sparse topology tests

--- network/gossip/libp2p/test/sparsetopology_test.go
+++ network/gossip/libp2p/test/sparsetopology_test.go
@@ -184,8 +184,8 @@ type IndexBoundTopology struct {
 }
 
 // Returns a subset of ids bounded by [minIndex, maxIndex) for the SparseTopology
-func (ibt IndexBoundTopology) Subset(idList flow.IdentityList, _ uint, _ string) (flow.IdentityList, error) {
-	sub := idList[ibt.minIndex:ibt.maxIndex]
+func (ibt IndexBoundTopology) Subset(ids flow.IdentityList, shouldHave flow.IdentityList, topic string, fanout uint) (flow.IdentityList, error) {
+	sub := ids[ibt.minIndex:ibt.maxIndex]
 	return sub, nil
 }
 
