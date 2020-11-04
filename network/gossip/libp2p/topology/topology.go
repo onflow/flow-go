b9ac837e0f75b8a3739d2801743d604b211fda54
Yahya <yahya@dapperlabs.com>
2020-11-03 16:33:15 -0800

    refactors subset method of topology

--- network/gossip/libp2p/topology/topology.go
+++ network/gossip/libp2p/topology/topology.go
@@ -4,6 +4,7 @@ import "github.com/onflow/flow-go/model/flow"
 
 // Topology provides a subset of nodes which a given node should directly connect to for 1-k messaging
 type Topology interface {
-	// Subset returns a random subset of the identity list that is passed
-	Subset(ids flow.IdentityList, fanout uint, topic string) (flow.IdentityList, error)
+	// Subset returns a random subset of the identity list that is passed. shouldHave represents set of
+	// identities that should be included in the returned subset.
+	Subset(ids flow.IdentityList, shouldHave flow.IdentityList, topic string, fanout uint) (flow.IdentityList, error)
 }
