b263e5ac97e83d983976b0269a0a299568e37cb2
danuio <me@danu.io>
2020-11-09 12:20:25 +0000

    Add contraints check

--- cmd/bootstrap/cmd/final_list.go
+++ cmd/bootstrap/cmd/final_list.go
@@ -42,11 +42,14 @@ func addFinalListFlags() {
 func finalList(cmd *cobra.Command, args []string) {
 	// read public partner node infos
 	log.Info().Msgf("reading parnter public node information: %s", flagPartnerNodeInfoDir)
-	partnerNodes := readPartnerNodes()
+	partnerNodes := assemblePartnerNodesWithoutStake()
 
 	// read internal private node infos
 	log.Info().Msgf("reading internal/flow private node information: %s", flagInternalNodePrivInfoDir)
-	flowNodes := readInternalNodes()
+	flowNodes := assembleInternalNodesWithoutStake()
+
+	log.Info().Msg("checking constraints on consensus/cluster nodes")
+	checkConstraints(partnerNodes, flowNodes)
 
 	log.Info().Msgf("reading staking contract node information: %s", flagStakingNodesDir)
 	stakingNodes := readStakingContractDetails()
@@ -58,15 +61,89 @@ func finalList(cmd *cobra.Command, args []string) {
 	reconcileNodes(allNodes, stakingNodes)
 }
 
-func readStakingContractDetails() []model.NodeInfoPub {
-	var nodes []model.NodeInfoPub
+func readStakingContractDetails() []model.NodeInfo {
+	var stakingNodes []model.NodeInfoPub
 	path := filepath.Join(flagStakingNodesDir, "node-infos.pub.json")
-	readJSON(path, &nodes)
+	readJSON(path, &stakingNodes)
+
+	var nodes []model.NodeInfo
+	for _, staking := range stakingNodes {
+		validateAddressFormat(staking.Address)
+
+		// validate every single partner node
+		nodeID := validateNodeID(staking.NodeID)
+		networkPubKey := validateNetworkPubKey(staking.NetworkPubKey)
+		stakingPubKey := validateStakingPubKey(staking.StakingPubKey)
+
+		node := model.NewPublicNodeInfo(
+			nodeID,
+			staking.Role,
+			staking.Address,
+			0,
+			networkPubKey,
+			stakingPubKey,
+		)
+		nodes = append(nodes, node)
+	}
+
 	return nodes
 }
 
-func reconcileNodes(nodes []model.NodeInfo, stakingNodes model.NodeInfo) {
+func reconcileNodes(nodes []model.NodeInfo, stakingNodes []model.NodeInfo) {
 	// check node count
 	// check node id mismatch
 	// check node type mismathc
+}
+
+func assembleInternalNodesWithoutStake() []model.NodeInfo {
+	privInternals := readInternalNodes()
+	log.Info().Msgf("read %v internal private node-info files", len(privInternals))
+
+	var nodes []model.NodeInfo
+	for _, internal := range privInternals {
+		// check if address is valid format
+		validateAddressFormat(internal.Address)
+
+		// validate every single internal node
+		nodeID := validateNodeID(internal.NodeID)
+		node := model.NewPrivateNodeInfo(
+			nodeID,
+			internal.Role,
+			internal.Address,
+			0,
+			internal.NetworkPrivKey,
+			internal.StakingPrivKey,
+		)
+
+		nodes = append(nodes, node)
+	}
+
+	return nodes
+}
+
+func assemblePartnerNodesWithoutStake() []model.NodeInfo {
+	partners := readPartnerNodes()
+	log.Info().Msgf("read %v partner node configuration files", len(partners))
+
+	var nodes []model.NodeInfo
+	for _, partner := range partners {
+		validateAddressFormat(partner.Address)
+
+		// validate every single partner node
+		nodeID := validateNodeID(partner.NodeID)
+		networkPubKey := validateNetworkPubKey(partner.NetworkPubKey)
+		stakingPubKey := validateStakingPubKey(partner.StakingPubKey)
+
+		node := model.NewPublicNodeInfo(
+			nodeID,
+			partner.Role,
+			partner.Address,
+			0,
+			networkPubKey,
+			stakingPubKey,
+		)
+		nodes = append(nodes, node)
+	}
+
+	return nodes
 }
