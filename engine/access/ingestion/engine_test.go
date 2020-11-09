a00df0515aa24d61ad040a3face302983d8b3f24
Kay-Zee <kan@axiomzen.co>
2020-11-08 16:56:58 -0800

    Allow querying older access nodes for tx results
    Fix tests
    
    go sum
    
    Add test for GetTransaction
    
    No more pending from historical
    
    access-metrics
    
    Add access node RPC metrics
    
    Add metrics flag
    
    Only add interceptors if metrics enabled
    
    lint

--- engine/access/ingestion/engine_test.go
+++ engine/access/ingestion/engine_test.go
@@ -89,8 +89,8 @@ func (suite *Suite) SetupTest() {
 	blocksToMarkExecuted, err := stdmap.NewTimes(100)
 	require.NoError(suite.T(), err)
 
-	rpcEng := rpc.New(log, suite.proto.state, rpc.Config{}, nil, nil, suite.blocks, suite.headers, suite.collections,
-		suite.transactions, flow.Testnet, metrics.NewNoopCollector(), 0, false)
+	rpcEng := rpc.New(log, suite.proto.state, rpc.Config{}, nil, nil, nil, suite.blocks, suite.headers, suite.collections,
+		suite.transactions, flow.Testnet, metrics.NewNoopCollector(), 0, false, false)
 
 	eng, err := New(log, net, suite.proto.state, suite.me, suite.request, suite.blocks, suite.headers, suite.collections,
 		suite.transactions, metrics.NewNoopCollector(), collectionsToMarkFinalized, collectionsToMarkExecuted,
