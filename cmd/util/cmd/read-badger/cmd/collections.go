bf4b580476696bf3f312a73e7a51585d19fcea35
Leo Zhang (zhangchiqing) <zhangchiqing@gmail.com>
2020-11-10 10:58:05 -0800

    add collection and transactions

--- cmd/util/cmd/read-badger/cmd/collections.go
+++ cmd/util/cmd/read-badger/cmd/collections.go
@@ -9,32 +9,56 @@ import (
 )
 
 var flagCollectionID string
+var flagTransactionID string
 
 func init() {
 	rootCmd.AddCommand(collectionsCmd)
 
 	collectionsCmd.Flags().StringVar(&flagCollectionID, "collection-id", "", "the ID of the collection")
-	_ = collectionsCmd.MarkFlagRequired("collection-id")
+	collectionsCmd.Flags().StringVar(&flagTransactionID, "transaction-id", "", "the ID of the transaction")
 }
 
 var collectionsCmd = &cobra.Command{
 	Use:   "collections",
-	Short: "get collection by collection ID",
+	Short: "get collection by collection ID (--colleciton-id) or transaction ID (--transaction-id)",
 	Run: func(cmd *cobra.Command, args []string) {
 		storages := InitStorages()
 
-		collectionID, err := flow.HexStringToIdentifier(flagCollectionID)
-		if err != nil {
-			log.Fatal().Err(err).Msg("malformed collection ID")
+		if flagCollectionID != "" {
+			log.Info().Msgf("flag collection id: %v", flagCollectionID)
+			collectionID, err := flow.HexStringToIdentifier(flagCollectionID)
+			if err != nil {
+				log.Fatal().Err(err).Msg("malformed collection ID")
+			}
+
+			log.Info().Msgf("getting collections by collection id: %v", collectionID)
+
+			collection, err := storages.Collections.ByID(collectionID)
+			if err != nil {
+				log.Fatal().Err(err).Msgf("could not get collection")
+			}
+
+			common.PrettyPrintEntity(collection)
+			return
 		}
 
-		log.Info().Msgf("getting collections by block id: %v", collectionID)
+		if flagTransactionID != "" {
+			transactionID, err := flow.HexStringToIdentifier(flagTransactionID)
+			if err != nil {
+				log.Fatal().Err(err).Msg("malformed transaction ID")
+			}
 
-		collection, err := storages.Collections.ByID(collectionID)
-		if err != nil {
-			log.Fatal().Err(err).Msgf("could not get collection")
+			log.Info().Msgf("getting collections by transaction id: %v", transactionID)
+
+			collections, err := storages.Collections.LightByTransactionID(transactionID)
+			if err != nil {
+				log.Fatal().Err(err).Msg("could not get collections")
+			}
+
+			common.PrettyPrintEntity(collections)
+			return
 		}
 
-		common.PrettyPrintEntity(collection)
+		log.Fatal().Msg("missing flags --collection-id or --transaction-id")
 	},
 }
