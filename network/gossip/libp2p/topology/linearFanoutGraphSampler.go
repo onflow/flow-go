43281944139cc002e856bb9003f3b31bb1b4a676
Yahya <yahya@dapperlabs.com>
2020-11-09 16:39:55 -0800

    migrates methods to linear fanout graph sampler

--- network/gossip/libp2p/topology/linearFanoutGraphSampler.go
+++ network/gossip/libp2p/topology/linearFanoutGraphSampler.go
@@ -1,1 +1,67 @@
 package topology
+
+import (
+	"fmt"
+	"math"
+
+	"github.com/onflow/flow-go/model/flow"
+	"github.com/onflow/flow-go/model/flow/filter"
+)
+
+type LinearFanoutGraphSampler struct {
+	seed       int64
+	fanoutFunc FanoutFunc
+}
+
+// NewLinearFanoutGraphSampler creates and returns a LinearFanoutGraphSampler
+func NewLinearFanoutGraphSampler(id flow.Identifier) (*LinearFanoutGraphSampler, error) {
+	seed, err := seedFromID(id)
+	if err != nil {
+		return nil, fmt.Errorf("could not generate seed from id:%w", err)
+	}
+	return &LinearFanoutGraphSampler{
+		seed:       seed,
+		fanoutFunc: LinearFanoutFunc,
+	}, nil
+}
+
+// SampleConnectedGraph receives two lists: all and shouldHave. It then samples a connected fanout
+// for the caller that includes the shouldHave set. Independent invocations of this method over
+// different nodes, should create a connected graph.
+// Fanout is the set of nodes that this instance should get connected to in order to create a
+// connected graph.
+func (l *LinearFanoutGraphSampler) SampleConnectedGraph(all flow.IdentityList,
+	shouldHave flow.IdentityList) flow.IdentityList {
+	var result flow.IdentityList
+	if shouldHave == nil {
+		result = l.connectedGraph(all, l.seed)
+	} else {
+		result = l.conditionalConnectedGraph(all, shouldHave, l.seed)
+	}
+	return result
+}
+
+// conditionalConnectedGraph returns a random subset of length (n+1)/2, which includes the shouldHave
+// set of identifiers.
+// If each node connects to the nodes returned by connectedGraph, the graph of such nodes is connected.
+func (l *LinearFanoutGraphSampler) conditionalConnectedGraph(all, shouldHave flow.IdentityList, seed int64) flow.IdentityList {
+	// total sample size
+	totalSize := int(math.Ceil(float64(len(all)+1) / 2))
+
+	// subset size excluding should have ones
+	subsetSize := totalSize - len(shouldHave)
+
+	// others are all excluding should have ones
+	others := all.Filter(filter.Not(filter.In(shouldHave)))
+	others = others.DeterministicSample(uint(subsetSize), seed)
+	return others.Union(shouldHave)
+}
+
+// connectedGraph returns a random subset of length (n+1)/2.
+// If each node connects to the nodes returned by connectedGraph, the graph of such nodes is connected.
+func (l *LinearFanoutGraphSampler) connectedGraph(ids flow.IdentityList, seed int64) flow.IdentityList {
+	// choose (n+1)/2 random nodes so that each node in the graph will have a degree >= (n+1) / 2,
+	// guaranteeing a connected graph.
+	size := uint(math.Ceil(float64(len(ids)+1) / 2))
+	return ids.DeterministicSample(size, seed)
+}
--- network/gossip/libp2p/topology/topology_utils.go
+++ network/gossip/libp2p/topology/topology_utils.go
@@ -3,53 +3,17 @@ package topology
 import (
 	"bytes"
 	"encoding/binary"
-	"math"
+	"fmt"
 
 	"github.com/onflow/flow-go/model/flow"
-	"github.com/onflow/flow-go/model/flow/filter"
 )
-
-// connectedGraph returns a random subset of length (n+1)/2.
-// If each node connects to the nodes returned by connectedGraph, the graph of such nodes is connected.
-func connectedGraph(ids flow.IdentityList, seed int64) flow.IdentityList {
-	// choose (n+1)/2 random nodes so that each node in the graph will have a degree >= (n+1) / 2,
-	// guaranteeing a connected graph.
-	size := uint(math.Ceil(float64(len(ids)+1) / 2))
-	return ids.DeterministicSample(size, seed)
-}
-
-// conditionalConnectedGraph returns a random subset of length (n+1)/2, which includes the shouldHave
-// set of identifiers.
-// If each node connects to the nodes returned by connectedGraph, the graph of such nodes is connected.
-func conditionalConnectedGraph(all, shouldHave flow.IdentityList, seed int64) flow.IdentityList {
-	// total sample size
-	totalSize := int(math.Ceil(float64(len(all)+1) / 2))
-
-	// subset size excluding should have ones
-	subsetSize := totalSize - len(shouldHave)
-
-	// others are all excluding should have ones
-	others := all.Filter(filter.Not(filter.In(shouldHave)))
-	return others.DeterministicSample(uint(subsetSize), seed)
-}
-
-func connectedGraphSample(all flow.IdentityList, shouldHave flow.IdentityList, seed int64) (flow.IdentityList,
-	flow.IdentityList) {
-	var result flow.IdentityList
-	if shouldHave == nil {
-		result = connectedGraph(all, seed)
-	} else {
-		result = conditionalConnectedGraph(all, shouldHave, seed)
-	}
-
-	remainder := all.Filter(filter.Not(filter.In(result)))
-	return result, remainder
-}
 
 // seedFromID generates a int64 seed from a flow.Identifier
 func seedFromID(id flow.Identifier) (int64, error) {
 	var seed int64
 	buf := bytes.NewBuffer(id[:])
-	err := binary.Read(buf, binary.LittleEndian, &seed)
-	return seed, err
+	if err := binary.Read(buf, binary.LittleEndian, &seed); err != nil {
+		return -1, fmt.Errorf("could not random bytes: %w", err)
+	}
+	return seed, nil
 }
