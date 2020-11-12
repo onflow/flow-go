48a35885f59a255c71c09b5574ab019f85d4a832
Jordan Schalm <jordan@dapperlabs.com>
2020-11-12 14:23:31 -0800

    update networking tests

--- network/gossip/libp2p/nodeIDRefresher.go
+++ network/gossip/libp2p/nodeIDRefresher.go
@@ -1,20 +1,18 @@
 package libp2p
 
 import (
-	"fmt"
-
 	"github.com/rs/zerolog"
+	"github.com/rs/zerolog/log"
 
 	"github.com/onflow/flow-go/model/flow"
 	"github.com/onflow/flow-go/model/flow/filter"
 	"github.com/onflow/flow-go/state/protocol"
-	"github.com/onflow/flow-go/state/protocol/events"
 )
 
-// NodeIDRefresher derives the latest list of flow Identities with which the network should be communicating based on the
-// current epoch phase
+// NodeIDRefresher derives the latest list of flow identities with which the
+// network should be communicating based on identity table changes in the
+// protocol state.
 type NodeIDRefresher struct {
-	events.Noop
 	logger   zerolog.Logger
 	state    protocol.ReadOnlyState
 	callBack func(flow.IdentityList) error // callBack to call when the id list has changed
@@ -28,61 +26,45 @@ func NewNodeIDRefresher(logger zerolog.Logger, state protocol.ReadOnlyState, cal
 	}
 }
 
-func (listener *NodeIDRefresher) EpochSetupPhaseStarted(newEpoch uint64, first *flow.Header) {
-	log := listener.logger.With().Uint64("new_epoch", newEpoch).Logger()
+// OnIdentityTableChanged updates the networking layer's list of nodes to connect
+// to when the identity table changes in the protocol state.
+func (listener *NodeIDRefresher) OnIdentityTableChanged() {
+
+	final, err := listener.state.Final().Head()
+	if err != nil {
+		log.Error().Err(err).Msg("failed to get finalized height")
+		return
+	}
+	phase, err := listener.state.Final().Phase()
+	if err != nil {
+		log.Error().Err(err).Msg("failed to get epoch phase")
+	}
+
+	log := listener.logger.
+		With().
+		Uint64("final_height", final.Height).
+		Str("epoch_phase", phase.String()).
+		Logger()
 
 	// get the new set of IDs
 	newIDs, err := IDsFromState(listener.state)
 	if err != nil {
-		log.Err(err).Msg("failed to update network ids on epoch transition")
+		log.Err(err).Msg("failed to update network ids on identity table change")
 		return
 	}
 
 	// call the registered callback
 	err = listener.callBack(newIDs)
 	if err != nil {
-		log.Err(err).Msg("failed to update network ids on epoch transition")
+		log.Err(err).Msg("failed to update network ids on identity table change")
 		return
 	}
 }
 
-// IDsFromState returns the identities that the network should be using based on the current epoch phase as following:
-// ----------------------------------------------------------------------------------------------------
-// |       Epoch Phase   |               IDs included                                                  |
-// |---------------------------------------------------------------------------------------------------
-// | EpochPhaseStaking   | All IDs from current epoch are included (TODO: add ids from preivous state) |
-// |---------------------------------------------------------------------------------------------------|
-// | EpochPhaseSetup     | All IDs from current and next epoch are included                            |
-// |---------------------------------------------------------------------------------------------------|
-// | EpochPhaseCommitted | All IDs from current and next epoch are included                            |
-// |---------------------------------------------------------------------------------------------------|
+// IDsFromState returns which nodes should be connected to based on the latest
+// finalized state. The protocol state includes nodes from the previous/next
+// epoch that should be included in network communication. We omit any nodes
+// that have been ejected.
 func IDsFromState(state protocol.ReadOnlyState) (flow.IdentityList, error) {
-
-	// epoch ids from this epoch
-	ids, err := state.Final().Identities(filter.Any)
-	if err != nil {
-		return nil, fmt.Errorf("failed to get current epoch ids: %w", err)
-	}
-
-	// current epoch phase
-	phase, err := state.Final().Phase()
-	if err != nil {
-		return nil, fmt.Errorf("failed to retrieve epoch phase: %w", err)
-	}
-
-	//TODO: Add epoch ids from previous phase
-	//if phase == flow.EpochPhaseStaking {
-	//	ids = ids.Union(state.Final().Epochs().Previous().InitialIdentities())
-	//}
-
-	// if node is in epoch setup or epoch committed phase, include the next epoch identities as well
-	if phase == flow.EpochPhaseSetup || phase == flow.EpochPhaseCommitted {
-		nextEpochIDs, err := state.Final().Epochs().Next().InitialIdentities()
-		if err != nil {
-			return nil, fmt.Errorf("failed to get next epoch ids: %w", err)
-		}
-		ids = ids.Union(nextEpochIDs)
-	}
-
-	return ids, err
+	return state.Final().Identities(filter.Not(filter.Ejected))
 }
