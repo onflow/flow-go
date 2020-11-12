b3d17e21f761b56ed029afa52e7fa9b6c34139ac
Ramtin Mehdizadeh Seraj <ramtin@axiomzen.co>
2020-11-12 15:02:06 -0800

    create migration dir

--- cmd/util/cmd/execution-state-extract/execution_state_extract.go
+++ cmd/util/cmd/execution-state-extract/execution_state_extract.go
@@ -6,6 +6,7 @@ import (
 
 	"github.com/rs/zerolog"
 
+	"github.com/onflow/flow-go/cmd/util/migrations"
 	"github.com/onflow/flow-go/ledger"
 	"github.com/onflow/flow-go/ledger/complete"
 	"github.com/onflow/flow-go/model/flow"
@@ -22,14 +23,10 @@ func extractExecutionState(dir string, targetHash flow.StateCommitment, outputDi
 	led, err := complete.NewLedger(dir, 1000, &metrics.NoopCollector{}, log, nil, complete.DefaultPathFinderVersion)
 
 	filePath := path.Join(outputDir, "root.checkpoint")
-	newState, err := led.ExportCheckpointAt(targetHash, []ledger.Migration{noOpMigration}, complete.DefaultPathFinderVersion, filePath)
+	newState, err := led.ExportCheckpointAt(targetHash, []ledger.Migration{migrations.NoOpMigration}, complete.DefaultPathFinderVersion, filePath)
 	if err != nil {
 		return fmt.Errorf("cannot create WAL: %w", err)
 	}
 	log.Info().Msg("New state commitment for the exported state is :" + newState.String())
 	return nil
-}
-
-func noOpMigration(p []ledger.Payload) ([]ledger.Payload, error) {
-	return p, nil
 }
