794e94c975aef0a19b631573a2dadd36733c25fa
Yahya <yahya@dapperlabs.com>
2020-11-16 12:18:31 -0800

    refactors fanout function

--- network/gossip/libp2p/topology/fanout.go
+++ network/gossip/libp2p/topology/fanout.go
@@ -11,6 +11,6 @@ type FanoutFunc func(size int) int
 // LinearFanoutFunc guarantees full network connectivity in a deterministic way.
 // Given system of `size` nodes, it returns `size+1/2`.
 var LinearFanoutFunc FanoutFunc = func(size int) int {
-	fanout := math.Ceil(float64(size) / 2)
-	return int(fanout) + 1
+	fanout := math.Ceil(float64(size+1) / 2)
+	return int(fanout)
 }
