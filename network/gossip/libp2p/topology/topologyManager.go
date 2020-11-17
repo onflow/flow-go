2e2f89a528b589d044d51c4b3a78a36d055d7406
Yahya <yahya@dapperlabs.com>
2020-11-17 15:25:19 -0800

    fixes comment of topology manager

--- network/gossip/libp2p/topology/topologyManager.go
+++ network/gossip/libp2p/topology/topologyManager.go
@@ -6,7 +6,8 @@ import (
 
 type Manager interface {
 	// MakeTopology receives identity list of entire network and constructs identity list of topology
-	// of this instance. A node directly communicates with its topology identity list on epidemic dissemination
+	// of this instance. The returned identity list from MakeTopology is also called fanout of the node.
+	// A node directly communicates with its fanout on epidemic dissemination
 	// of the messages (i.e., publish and multicast).
 	// Independent invocations of MakeTopology on different nodes collaboratively
 	// constructs a connected graph of nodes that enables them talking to each other.
