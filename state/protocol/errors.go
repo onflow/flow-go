60746d477db19226be5dec184911380604f53c7a
Jordan Schalm <jordan@dapperlabs.com>
2020-11-19 19:53:19 -0800

    remove ByCounter from Epochs API

--- state/protocol/errors.go
+++ state/protocol/errors.go
@@ -7,15 +7,19 @@ import (
 	"github.com/onflow/flow-go/model/flow"
 )
 
-type IdentityNotFoundErr struct {
+// ErrNoPreviousEpoch is a sentinel error returned when a previous epoch is
+// queried from a snapshot within the first epoch after the root block.
+var ErrNoPreviousEpoch = fmt.Errorf("no previous epoch exists")
+
+type IdentityNotFoundError struct {
 	NodeID flow.Identifier
 }
 
-func (e IdentityNotFoundErr) Error() string {
+func (e IdentityNotFoundError) Error() string {
 	return fmt.Sprintf("identity not found (%x)", e.NodeID)
 }
 
 func IsIdentityNotFound(err error) bool {
-	var errIdentityNotFound IdentityNotFoundErr
+	var errIdentityNotFound IdentityNotFoundError
 	return errors.As(err, &errIdentityNotFound)
 }
