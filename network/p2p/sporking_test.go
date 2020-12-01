c095863c267a2f4b128b4e26b977c76b0eff44a3
Yahya <yahya@dapperlabs.com>
2020-12-01 12:27:14 -0800

    refactors suite in sporking test

--- network/p2p/sporking_test.go
+++ network/p2p/sporking_test.go
@@ -35,101 +35,101 @@ func TestSporkingTestSuite(t *testing.T) {
 
 // TestCrosstalkPreventionOnNetworkKeyChange tests that a node from the old chain cannot talk to a node in the new chain
 // if it's network key is updated while the libp2p protocol ID remains the same
-func (h *SporkingTestSuite) TestCrosstalkPreventionOnNetworkKeyChange() {
+func (suite *SporkingTestSuite) TestCrosstalkPreventionOnNetworkKeyChange() {
 
 	// create and start node 1 on localhost and random port
-	node1key := generateNetworkingKey(h.T())
-	node1, address1 := h.CreateNode("node1", node1key, "0.0.0.0", "0", rootBlockID, nil, false)
-	defer h.StopNode(node1)
-	h.T().Logf(" %s node started on %s:%s", node1.name, address1.IP, address1.Port)
-	h.T().Logf("libp2p ID for %s: %s", node1.name, node1.libP2PHost.Host().ID())
+	node1key := generateNetworkingKey(suite.T())
+	node1, address1 := suite.CreateNode("node1", node1key, "0.0.0.0", "0", rootBlockID, nil, false)
+	defer suite.StopNode(node1)
+	suite.T().Logf(" %s node started on %s:%s", node1.name, address1.IP, address1.Port)
+	suite.T().Logf("libp2p ID for %s: %s", node1.name, node1.libP2PHost.Host().ID())
 
 	// create and start node 2 on localhost and random port
-	node2key := generateNetworkingKey(h.T())
-	node2, address2 := h.CreateNode("node2", node2key, "0.0.0.0", "0", rootBlockID, nil, false)
+	node2key := generateNetworkingKey(suite.T())
+	node2, address2 := suite.CreateNode("node2", node2key, "0.0.0.0", "0", rootBlockID, nil, false)
 
 	// create stream from node 1 to node 2
-	testOneToOneMessagingSucceeds(h.T(), node1, address2)
+	testOneToOneMessagingSucceeds(suite.T(), node1, address2)
 
 	// Simulate a hard-spoon: node1 is on the old chain, but node2 is moved from the old chain to the new chain
 
 	// stop node 2 and start it again with a different networking key but on the same IP and port
-	h.StopNode(node2)
+	suite.StopNode(node2)
 
 	// generate a new key
-	node2keyNew := generateNetworkingKey(h.T())
-	assert.False(h.T(), node2key.Equals(node2keyNew))
+	node2keyNew := generateNetworkingKey(suite.T())
+	assert.False(suite.T(), node2key.Equals(node2keyNew))
 
 	// start node2 with the same name, ip and port but with the new key
-	node2, address2New := h.CreateNode(node2.name, node2keyNew, address2.IP, address2.Port, rootBlockID, nil, false)
-	defer h.StopNode(node2)
+	node2, address2New := suite.CreateNode(node2.name, node2keyNew, address2.IP, address2.Port, rootBlockID, nil, false)
+	defer suite.StopNode(node2)
 
 	// make sure the node2 indeed came up on the old ip and port
-	assert.Equal(h.T(), address2.IP, address2New.IP)
-	assert.Equal(h.T(), address2.Port, address2New.Port)
+	assert.Equal(suite.T(), address2.IP, address2New.IP)
+	assert.Equal(suite.T(), address2.Port, address2New.Port)
 
 	// attempt to create a stream from node 1 (old chain) to node 2 (new chain)
 	// this time it should fail since node 2 is using a different public key
 	// (and therefore has a different libp2p node id)
-	testOneToOneMessagingFails(h.T(), node1, address2)
+	testOneToOneMessagingFails(suite.T(), node1, address2)
 }
 
 // TestOneToOneCrosstalkPrevention tests that a node from the old chain cannot talk directly to a node in the new chain
 // if the Flow libp2p protocol ID is updated while the network keys are kept the same.
-func (h *SporkingTestSuite) TestOneToOneCrosstalkPrevention() {
+func (suite *SporkingTestSuite) TestOneToOneCrosstalkPrevention() {
 
 	// root id before spork
 	rootID1 := unittest.BlockFixture().ID().String()
 
 	// create and start node 1 on localhost and random port
-	node1key := generateNetworkingKey(h.T())
-	node1, address1 := h.CreateNode("node1", node1key, "0.0.0.0", "0", rootID1, nil, false)
-	defer h.StopNode(node1)
+	node1key := generateNetworkingKey(suite.T())
+	node1, address1 := suite.CreateNode("node1", node1key, "0.0.0.0", "0", rootID1, nil, false)
+	defer suite.StopNode(node1)
 
 	// create and start node 2 on localhost and random port
-	node2key := generateNetworkingKey(h.T())
-	node2, address2 := h.CreateNode("node2", node2key, "0.0.0.0", "0", rootID1, nil, false)
+	node2key := generateNetworkingKey(suite.T())
+	node2, address2 := suite.CreateNode("node2", node2key, "0.0.0.0", "0", rootID1, nil, false)
 
 	// create stream from node 2 to node 1
-	testOneToOneMessagingSucceeds(h.T(), node2, address1)
+	testOneToOneMessagingSucceeds(suite.T(), node2, address1)
 
 	// Simulate a hard-spoon: node1 is on the old chain, but node2 is moved from the old chain to the new chain
 
 	// stop node 2 and start it again with a different libp2p protocol id to listen for
-	h.StopNode(node2)
+	suite.StopNode(node2)
 
 	// update the flow root id for node 2. node1 is still listening on the old protocol
 	rootID2 := unittest.BlockFixture().ID().String()
 
 	// start node2 with the same name, ip and port but with the new key
-	node2, address2New := h.CreateNode(node2.name, node2key, address2.IP, address2.Port, rootID2, nil, false)
-	defer h.StopNode(node2)
+	node2, address2New := suite.CreateNode(node2.name, node2key, address2.IP, address2.Port, rootID2, nil, false)
+	defer suite.StopNode(node2)
 
 	// make sure the node2 indeed came up on the old ip and port
-	assert.Equal(h.T(), address2.IP, address2New.IP)
-	assert.Equal(h.T(), address2.Port, address2New.Port)
+	assert.Equal(suite.T(), address2.IP, address2New.IP)
+	assert.Equal(suite.T(), address2.Port, address2New.Port)
 
 	// attempt to create a stream from node 2 (new chain) to node 1 (old chain)
 	// this time it should fail since node 2 is listening on a different protocol
-	testOneToOneMessagingFails(h.T(), node2, address1)
+	testOneToOneMessagingFails(suite.T(), node2, address1)
 }
 
 // TestOneToKCrosstalkPrevention tests that a node from the old chain cannot talk to a node in the new chain via PubSub
 // if the channel ID is updated while the network keys are kept the same.
-func (h *SporkingTestSuite) TestOneToKCrosstalkPrevention() {
+func (suite *SporkingTestSuite) TestOneToKCrosstalkPrevention() {
 
 	// root id before spork
 	rootIDBeforeSpork := unittest.BlockFixture().ID().String()
 
 	// create and start node 1 on localhost and random port
-	node1key := generateNetworkingKey(h.T())
-	node1, _ := h.CreateNode("node1", node1key, "0.0.0.0", "0", rootIDBeforeSpork, nil, false)
-	defer h.StopNode(node1)
+	node1key := generateNetworkingKey(suite.T())
+	node1, _ := suite.CreateNode("node1", node1key, "0.0.0.0", "0", rootIDBeforeSpork, nil, false)
+	defer suite.StopNode(node1)
 
 	// create and start node 2 on localhost and random port with the same root block ID
-	node2key := generateNetworkingKey(h.T())
-	node2, addr2 := h.CreateNode("node1", node2key, "0.0.0.0", "0", rootIDBeforeSpork, nil, false)
-	defer h.StopNode(node2)
+	node2key := generateNetworkingKey(suite.T())
+	node2, addr2 := suite.CreateNode("node1", node2key, "0.0.0.0", "0", rootIDBeforeSpork, nil, false)
+	defer suite.StopNode(node2)
 
 	ctx, cancel := context.WithCancel(context.Background())
 	defer cancel()
@@ -139,19 +139,19 @@ func (h *SporkingTestSuite) TestOneToKCrosstalkPrevention() {
 
 	// both nodes are initially on the same spork and subscribed to the same topic
 	_, err := node1.Subscribe(ctx, topicBeforeSpork)
-	require.NoError(h.T(), err)
+	require.NoError(suite.T(), err)
 	sub2, err := node2.Subscribe(ctx, topicBeforeSpork)
-	require.NoError(h.T(), err)
+	require.NoError(suite.T(), err)
 
 	// add node 2 as a peer of node 1
 	err = node1.AddPeer(ctx, addr2)
-	require.NoError(h.T(), err)
+	require.NoError(suite.T(), err)
 
 	// let the two nodes form the mesh
 	time.Sleep(time.Second)
 
 	// assert that node 1 can successfully send a message to node 2 via PubSub
-	testOneToKMessagingSucceeds(ctx, h.T(), node1, sub2, topicBeforeSpork)
+	testOneToKMessagingSucceeds(ctx, suite.T(), node1, sub2, topicBeforeSpork)
 
 	// new root id after spork
 	rootIDAfterSpork := unittest.BlockFixture().ID().String()
@@ -163,12 +163,12 @@ func (h *SporkingTestSuite) TestOneToKCrosstalkPrevention() {
 	// by unsubscribing node1 from 'topicBeforeSpork' and subscribing it to 'topicAfterSpork'
 	// and keeping node2 subscribed to topic 'topicBeforeSpork'
 	err = node1.UnSubscribe(topicBeforeSpork)
-	require.NoError(h.T(), err)
+	require.NoError(suite.T(), err)
 	_, err = node1.Subscribe(ctx, topicAfterSpork)
-	require.NoError(h.T(), err)
+	require.NoError(suite.T(), err)
 
 	// assert that node 1 can no longer send a message to node 2 via PubSub
-	testOneToKMessagingFails(ctx, h.T(), node1, sub2, topicAfterSpork)
+	testOneToKMessagingFails(ctx, suite.T(), node1, sub2, topicAfterSpork)
 }
 
 func testOneToOneMessagingSucceeds(t *testing.T, sourceNode *Node, dstnAddress NodeAddress) {
