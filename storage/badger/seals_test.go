c0fca0fdce8046f01dee22ff1c513b770171fb3f
danuio <me@danu.io>
2020-12-03 10:43:54 +0000

    Fix linting

--- storage/badger/seals_test.go
+++ storage/badger/seals_test.go
@@ -6,11 +6,11 @@ import (
 	"testing"
 
 	"github.com/dgraph-io/badger/v2"
-	"github.com/onflow/flow-go/storage/badger/operation"
 	"github.com/stretchr/testify/require"
 
 	"github.com/onflow/flow-go/module/metrics"
 	"github.com/onflow/flow-go/storage"
+	"github.com/onflow/flow-go/storage/badger/operation"
 	"github.com/onflow/flow-go/utils/unittest"
 
 	badgerstorage "github.com/onflow/flow-go/storage/badger"
