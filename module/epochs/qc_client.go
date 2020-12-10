c05926b1e9730f5a3aed124886766f2137127df5
danuio <me@danu.io>
2020-12-10 10:59:04 +0000

    Add initial template for QC client

---
+++ module/epochs/qc_client.go
@@ -0,0 +1,10 @@
+package epochs
+
+// QCContractClient is a client to the QC Contract
+type QCContractClient struct {
+}
+
+// NewQCContractClient returns a new client to the QC contract
+func NewQCContractClient() *QCContractClient {
+	return &QCContractClient{}
+}
