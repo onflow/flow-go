80de1e114538f16d96700609d5764bea7f8c4a5a
Tarak Ben Youssef <tarak.benyoussef@dapperlabs.com>
2020-12-10 18:52:09 -0800

    update the random beacon to isolate the single consensus node case

--- module/signature/threshold.go
+++ module/signature/threshold.go
@@ -78,13 +78,26 @@ func (tp *ThresholdProvider) Sign(msg []byte) (crypto.Signature, error) {
 	return tp.priv.Sign(msg, tp.hasher)
 }
 
+// check if there are enough shares for the threshold signature reconstruction
+func enoughThresholdShares(size int, shares int) (bool, error) {
+	// isolate the case with one node
+	if size == 1 {
+		return shares > 0, nil
+	}
+	enoughShares, err := crypto.EnoughShares(RandomBeaconThreshold(size), shares)
+	if err != nil {
+		return false, fmt.Errorf("could not check the threshold: %w", err)
+	}
+	return enoughShares, nil
+}
+
 // Combine will combine the provided public signature shares to attempt and reconstruct a threshold
 // signature for the group of the given size. The indices represent the index for ech signature share
 // within the DKG algorithm.
 func (tp *ThresholdProvider) Combine(size uint, shares []crypto.Signature, indices []uint) (crypto.Signature, error) {
 
 	// check that we have sufficient shares to reconstruct the threshold signature
-	enoughShares, err := crypto.EnoughShares(RandomBeaconThreshold(int(size)), len(shares))
+	enoughShares, err := enoughThresholdShares(int(size), len(shares))
 	if err != nil {
 		return nil, fmt.Errorf("error in combine: %w", err)
 	}
@@ -99,6 +112,9 @@ func (tp *ThresholdProvider) Combine(size uint, shares []crypto.Signature, indic
 	}
 
 	// try to reconstruct the threshold signature using the given shares & indices
+	if size == 1 {
+		return shares[0], nil
+	}
 	thresSig, err := crypto.ReconstructThresholdSignature(int(size), RandomBeaconThreshold(int(size)), shares, converted)
 	if err != nil {
 		return nil, fmt.Errorf("could not reconstruct threshold signature: %w", err)
