4eaac392e1a3606a092794a7fe1fee557aaea1f7
Yurii Oleksyshyn <yuraolex@gmail.com>
2020-12-14 17:26:36 +0200

    Removed commented code

--- state/protocol/util/testing.go
+++ state/protocol/util/testing.go
@@ -67,32 +67,3 @@ func RunWithFullProtocolStateAndConsumer(t testing.TB, stateRoot *pbadger.StateR
 		f(db, fullState)
 	})
 }
-
-// use this function if you'd like to create your own protocol state with customized dependencies
-//func RunWithProtocolStateDeps(t testing.TB, create createState, f func(*badger.DB, *pbadger.State)) {
-//	unittest.RunWithBadgerDB(t, func(db *badger.DB) {
-//		proto := WithProtocolState(t, db, create)
-//		f(db, proto)
-//	})
-//}
-
-//func RunWithProtocolStateAndConsumer(t testing.TB, consumer protocol.Consumer, f func(*badger.DB, *pbadger.State)) {
-//	RunWithProtocolStateDeps(t, func(
-//		metrics module.ComplianceMetrics,
-//		tracer module.Tracer,
-//		db *badger.DB,
-//		headers storage.Headers,
-//		seals storage.Seals,
-//		index storage.Index,
-//		payloads storage.Payloads,
-//		blocks storage.Blocks,
-//		setups storage.EpochSetups,
-//		commits storage.EpochCommits,
-//		statuses storage.EpochStatuses,
-//		c protocol.Consumer,
-//	) (*pbadger.State, error) {
-//		proto, err := pbadger.NewState(metrics, tracer, db, headers, seals, index, payloads, blocks, setups, commits, statuses, consumer)
-//		require.NoError(t, err)
-//		return proto, nil
-//	}, f)
-//}
