5cb27021fc1a87e8536bc7076d849a30e1a384e2
Ramtin Mehdizadeh Seraj <ramtin@axiomzen.co>
2020-12-18 09:49:49 -0800

    expand the logs

--- fvm/transaction.go
+++ fvm/transaction.go
@@ -1,7 +1,6 @@
 package fvm
 
 import (
-	"fmt"
 	"strings"
 
 	"github.com/davecgh/go-spew/spew"
@@ -11,8 +10,6 @@ import (
 	"github.com/onflow/flow-go/fvm/state"
 	"github.com/onflow/flow-go/model/flow"
 )
-
-const TopShotContractAddress = "0b2a3299cc857e29"
 
 func Transaction(tx *flow.TransactionBody, txIndex uint32) *TransactionProcedure {
 	return &TransactionProcedure{
@@ -104,17 +101,17 @@ func (i *TransactionInvocator) Process(
 // checking failures in this contract indicate the unexpected computation happening.
 // This is a temporary measure.
 func (i *TransactionInvocator) topshotSafetyErrorCheck(err error) {
-	fmt.Println("ERROR", err)
 	e := err.Error()
-	if strings.Contains(e, TopShotContractAddress) && strings.Contains(e, "checking") {
+	i.logger.Info().Str("error", e).Msg("TEMP LOGGING: Cadence Execution ERROR")
+	if strings.Contains(e, "checking") {
 		re, isRuntime := err.(runtime.Error)
 		if !isRuntime {
-			i.logger.Err(err).Msg("found checking error for TopShot contract but exception is not RuntimeError")
+			i.logger.Err(err).Msg("found checking error for a contract but exception is not RuntimeError")
 			return
 		}
 		ee, is := re.Err.(*runtime.ParsingCheckingError)
 		if !is {
-			i.logger.Err(err).Msg("found checking error for TopShot contract but exception is not ExtendedParsingCheckingError")
+			i.logger.Err(err).Msg("found checking error for a contract but exception is not ExtendedParsingCheckingError")
 			return
 		}
 
@@ -123,6 +120,6 @@ func (i *TransactionInvocator) topshotSafetyErrorCheck(err error) {
 		spew.Config.DisableMethods = true
 		dump := spew.Sdump(ee)
 
-		i.logger.Error().Str("extended_error", dump).Msg("TopShot contract checking failed")
+		i.logger.Error().Str("extended_error", dump).Msg("contract checking failed")
 	}
 }
