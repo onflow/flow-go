7df476031a73739993f1d157ac07312f8e69d4b3
Jordan Schalm <jordan@dapperlabs.com>
2021-01-08 16:19:47 -0800

    update inmem snapshot to use state root

--- utils/unittest/fixtures.go
+++ utils/unittest/fixtures.go
@@ -6,10 +6,10 @@ import (
 	"math/rand"
 	"time"
 
+	sdk "github.com/onflow/flow-go-sdk"
+
 	"github.com/onflow/flow-go/model/chunks"
 	"github.com/onflow/flow-go/module/signature"
-
-	sdk "github.com/onflow/flow-go-sdk"
 
 	hotstuff "github.com/onflow/flow-go/consensus/hotstuff/model"
 	"github.com/onflow/flow-go/crypto"
