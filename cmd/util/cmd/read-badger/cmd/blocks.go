95fc972158534e18427fdd2247e8b73b0cf78738
Leo Zhang (zhangchiqing) <zhangchiqing@gmail.com>
2021-01-12 17:26:45 -0800

    add parents flag to read n number of ancestor from a block

--- cmd/util/cmd/read-badger/cmd/blocks.go
+++ cmd/util/cmd/read-badger/cmd/blocks.go
@@ -9,12 +9,15 @@ import (
 )
 
 var flagBlockID string
+var flagNParents int
 
 func init() {
 	rootCmd.AddCommand(blocksCmd)
 
 	blocksCmd.Flags().StringVarP(&flagBlockID, "id", "i", "", "the id of the block")
 	_ = blocksCmd.MarkFlagRequired("id")
+
+	blocksCmd.Flags().IntVarP(&flagNParents, "parents", "p", 0, "the number of parent blocks to fetch")
 }
 
 var blocksCmd = &cobra.Command{
@@ -34,7 +37,19 @@ var blocksCmd = &cobra.Command{
 		if err != nil {
 			log.Fatal().Err(err).Msgf("could not get block with id: %v", blockID)
 		}
-
 		common.PrettyPrintEntity(block)
+
+		parentID := block.Header.ParentID
+		n := flagNParents
+		for ; n > 0; n-- {
+			log.Info().Msgf("getting parent block by id: %v (%v/%v)", parentID, flagNParents-n+1, flagNParents)
+			parent, err := storages.Blocks.ByID(parentID)
+			if err != nil {
+				log.Fatal().Err(err).Msgf("could not get parent with id: %v", parentID)
+			}
+			common.PrettyPrintEntity(parent)
+			parentID = parent.Header.ParentID
+		}
+
 	},
 }
