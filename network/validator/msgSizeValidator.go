0df1c9f7c64d09e338da8b7b9b0455857a97264a
vishal <1117327+vishalchangrani@users.noreply.github.com>
2021-01-14 14:14:01 -0800

    fixing condition in message size validator

--- network/validator/msgSizeValidator.go
+++ network/validator/msgSizeValidator.go
@@ -14,14 +14,14 @@ type maxMsgSizeLimitLookupFunc func(msg *message.Message) int
 
 // MsgSizeValidator validates the incoming unicast message size
 type MsgSizeValidator struct {
-	log zerolog.Logger
+	log        zerolog.Logger
 	lookupFunc maxMsgSizeLimitLookupFunc
 }
 
 // NewMsgSizeValidator creates and returns a new MsgSizeValidator
 func NewMsgSizeValidator(log zerolog.Logger, lookupFunc maxMsgSizeLimitLookupFunc) *MsgSizeValidator {
 	msgSizeValidator := &MsgSizeValidator{
-		log: log,
+		log:        log,
 		lookupFunc: lookupFunc,
 	}
 	return msgSizeValidator
@@ -29,8 +29,8 @@ func NewMsgSizeValidator(log zerolog.Logger, lookupFunc maxMsgSizeLimitLookupFun
 
 // Validate returns true if the message size is less than or equal to the maximum permissible message size
 func (msv *MsgSizeValidator) Validate(msg message.Message) bool {
-	size := msv.lookupFunc(&msg)
-	if size <= msg.Size() {
+	maxSize := msv.lookupFunc(&msg)
+	if msg.Size() <= maxSize {
 		return true
 	}
 
@@ -39,8 +39,8 @@ func (msv *MsgSizeValidator) Validate(msg message.Message) bool {
 		Hex("event_id", msg.EventID).
 		Str("event_type", msg.Type).
 		Str("channel_id", msg.ChannelID).
-		Int("size", size).
-		Msg("received message exceeded permissible message size")
+		Int("maxSize", maxSize).
+		Msg("received message exceeded permissible message maxSize")
 
 	return false
 }
