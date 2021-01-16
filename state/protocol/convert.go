9bfc912df634dcc43ec7297ddd668f629ecb1f36
Jordan Schalm <jordan@dapperlabs.com>
2021-01-15 16:14:45 -0800

    error handling and docs in service event <-> Epoch conversion

--- state/protocol/convert.go
+++ state/protocol/convert.go
@@ -7,18 +7,34 @@ import (
 	"github.com/onflow/flow-go/model/flow/filter"
 )
 
-// TODO error handling
+// ToEpochSetup converts an Epoch interface instance to the underlying
+// concrete epoch setup service event.
 func ToEpochSetup(epoch Epoch) (*flow.EpochSetup, error) {
 	counter, err := epoch.Counter()
 	if err != nil {
-		return nil, err
+		return nil, fmt.Errorf("could not get epoch counter: %w", err)
 	}
 	firstView, err := epoch.FirstView()
+	if err != nil {
+		return nil, fmt.Errorf("could not get epoch first view: %w", err)
+	}
 	finalView, err := epoch.FinalView()
+	if err != nil {
+		return nil, fmt.Errorf("could not get epoch final view: %w", err)
+	}
 	participants, err := epoch.InitialIdentities()
+	if err != nil {
+		return nil, fmt.Errorf("could not get epoch participants: %w", err)
+	}
 	clustering, err := epoch.Clustering()
+	if err != nil {
+		return nil, fmt.Errorf("could not get epoch clustering: %w", err)
+	}
 	assignments := clustering.Assignments()
-	src, err := epoch.RandomSource()
+	randomSource, err := epoch.RandomSource()
+	if err != nil {
+		return nil, fmt.Errorf("could not get epoch random source: %w", err)
+	}
 
 	setup := &flow.EpochSetup{
 		Counter:      counter,
@@ -26,29 +42,42 @@ func ToEpochSetup(epoch Epoch) (*flow.EpochSetup, error) {
 		FinalView:    finalView,
 		Participants: participants,
 		Assignments:  assignments,
-		RandomSource: src,
+		RandomSource: randomSource,
 	}
 	return setup, nil
 }
 
-// TODO error handling
+// ToEpochCommit converts an Epoch interface instance to the underlying
+// concrete epoch commit service event. The epoch must have been committed.
 func ToEpochCommit(epoch Epoch) (*flow.EpochCommit, error) {
 	counter, err := epoch.Counter()
 	if err != nil {
-		return nil, err
+		return nil, fmt.Errorf("could not get epoch counter: %w", err)
 	}
 	clustering, err := epoch.Clustering()
+	if err != nil {
+		return nil, fmt.Errorf("could not get epoch clustering: %w", err)
+	}
 	qcs := make([]*flow.QuorumCertificate, 0, len(clustering))
 	for i := range clustering {
 		cluster, err := epoch.Cluster(uint(i))
 		if err != nil {
-			return nil, err
+			return nil, fmt.Errorf("could not get epoch cluster (index=%d): %w", i, err)
 		}
 		qcs = append(qcs, cluster.RootQC())
 	}
 	participants, err := epoch.InitialIdentities()
+	if err != nil {
+		return nil, fmt.Errorf("could not get epoch participants: %w", err)
+	}
 	dkg, err := epoch.DKG()
+	if err != nil {
+		return nil, fmt.Errorf("could not get epoch dkg: %w", err)
+	}
 	dkgParticipants, err := ToDKGParticipantLookup(dkg, participants.Filter(filter.HasRole(flow.RoleConsensus)))
+	if err != nil {
+		return nil, fmt.Errorf("could not compute dkg participant lookup: %w", err)
+	}
 
 	commit := &flow.EpochCommit{
 		Counter:         counter,
@@ -59,7 +88,8 @@ func ToEpochCommit(epoch Epoch) (*flow.EpochCommit, error) {
 	return commit, nil
 }
 
-// TODO doc
+// ToDKGParticipantLookup computes the nodeID -> DKGParticipant lookup for a
+// DKG instance. The participants must exactly match the DKG instance configuration.
 func ToDKGParticipantLookup(dkg DKG, participants flow.IdentityList) (map[flow.Identifier]flow.DKGParticipant, error) {
 
 	lookup := make(map[flow.Identifier]flow.DKGParticipant)
