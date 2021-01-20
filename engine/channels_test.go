3dd9b62c330fe31be5db140a859ec196552d8750
Yahya Hassanzadeh <yhassanzadeh13@ku.edu.tr>
2021-01-20 10:01:59 -0800

    NET: Yahya/5147-deduplicate-channels-part-2 (#302)
    * makes rolelist sortable
    
    * adds tests for identifier of rolelist
    
    * adds unique channel ids by role
    
    * simplifies unique channel ids by role implementation
    
    * adds TestUniqueChannelIDsByRole_Uniqueness
    
    * defines channel type
    
    * defines channel list
    
    * refactors subscription manager with channels
    
    * refactors topologies with channels
    
    * refactors fully qualified channel id
    
    * refactors channels in middleware
    
    * refactors channels
    
    * refactors channel in conduit
    
    * refactors channels
    
    * refactors channels and removes unique channel ids method
    
    * cleans up channel id from channel
    
    * fixes channels test
    
    * refactors network interface with channel
    
    * refactors event priority
    
    * refactors channel id in network
    
    * refactors channel on stub conduit
    
    * refactors stub for channel ids
    
    * generates mocks for subscription manager and middleware
    
    * refactors channel in unittest
    
    * refactors topology helper
    
    * refactors randomized topology
    
    * refactors topic based topology
    
    * refactors echo engine and mesh engine tests
    
    * refactors some comments
    
    * refactors consensus package
    
    * refactors synchronization test
    
    * refactors provider engine
    
    * refactors requester engine
    
    * refactors consensus compliance engine
    
    * refactors ghost node
    
    * refactors verification tests
    
    * refactors metrics examples
    
    * casts constant channels as types
    
    * generates mocks
    
    * fixes sync test
    
    * fixes ghost node send issue
    
    * removes channel id of topic based topology
    
    * removes channel id in receive cache
    
    * removes channel id in randomized topology
    
    * removes channel id from echo engine test
    
    * refactors channel id in comments and messages
    
    * refactors channel id
    
    * refactors channel to topic conversion
    
    * fixes go docs
    
    * refactors topic in libp2p node
    
    * refactors topic in middleware
    
    * fixes lint in tests
    
    * fixes cyclic dependency
    
    * adds exists method for channels
    
    * adds channel list
    
    * adds TestUniqueChannels_Uniqueness
    
    * fixes a comment
    
    * adds TestUniqueChannels_ClusterChannels
    
    * adds deduplication to topic based fanout
    
    * adds unique channels to randomized topology
    
    * refactors test parameters
    
    * fixes a godoc
    
    * fixes lint
    
    * simplifies a test
    
    * fixes a comment
    
    * Update engine/channels_test.go
    
    Co-authored-by: Vishal <1117327+vishalchangrani@users.noreply.github.com>
    
    * Update engine/channels.go
    
    Co-authored-by: Vishal <1117327+vishalchangrani@users.noreply.github.com>
    
    * refactors cluster channel method
    
    * fixes lint issue
    
    * fixes flakey topology test
    
    * Update engine/channels.go
    
    Co-authored-by: Jordan Schalm <jordan@dapperlabs.com>
    
    * Update engine/channels_test.go
    
    Co-authored-by: Jordan Schalm <jordan@dapperlabs.com>
    
    Co-authored-by: Vishal <1117327+vishalchangrani@users.noreply.github.com>
    Co-authored-by: Jordan Schalm <jordan@dapperlabs.com>

--- engine/channels_test.go
+++ engine/channels_test.go
@@ -67,23 +67,62 @@ func TestGetChannelByRole(t *testing.T) {
 	assert.Contains(t, topics, TestNetwork)
 }
 
-// TestIsClusterChannel verifies the correctness of IsClusterChannel method
+// TestIsClusterChannel verifies the correctness of ClusterChannel method
 // against cluster and non-cluster channel.
 func TestIsClusterChannel(t *testing.T) {
 	// creates a consensus cluster channel and verifies it
 	conClusterChannel := ChannelConsensusCluster("some-consensus-cluster-id")
-	clusterChannel, ok := IsClusterChannel(conClusterChannel)
+	clusterChannel, ok := ClusterChannel(conClusterChannel)
 	require.True(t, ok)
 	require.Equal(t, clusterChannel, consensusClusterPrefix)
 
 	// creates a sync cluster channel and verifies it
 	syncClusterChannel := ChannelSyncCluster("some-sync-cluster-id")
-	clusterChannel, ok = IsClusterChannel(syncClusterChannel)
+	clusterChannel, ok = ClusterChannel(syncClusterChannel)
 	require.True(t, ok)
 	require.Equal(t, clusterChannel, syncClusterPrefix)
 
 	// non-cluster channel should not be verified
-	clusterChannel, ok = IsClusterChannel("non-cluster-channel-id")
+	clusterChannel, ok = ClusterChannel("non-cluster-channel-id")
 	require.False(t, ok)
 	require.Empty(t, clusterChannel)
+}
+
+// TestUniqueChannels_Uniqueness verifies that non-cluster channels returned by
+// UniqueChannels are unique based on their set of involved roles.
+// We use the identifier of RoleList to determine their uniqueness.
+func TestUniqueChannels_Uniqueness(t *testing.T) {
+	for _, role := range flow.Roles() {
+		uniques := UniqueChannels(ChannelsByRole(role))
+
+		visited := make(map[flow.Identifier]struct{})
+		for _, channel := range uniques {
+
+			if _, ok := ClusterChannel(channel); ok {
+				continue //only considering non-cluster channel in this test case
+			}
+
+			// non-cluster channels should be unique based on their RoleList identifier.
+			id := channelRoleMap[channel].ID()
+			_, duplicate := visited[id]
+			require.False(t, duplicate)
+
+			visited[id] = struct{}{}
+		}
+	}
+}
+
+// TestUniqueChannels_ClusterChannels verifies that if cluster channels have the RoleList the same as
+// single non-cluster channel, then all cluster channels as well as the one non-cluster channel are returned
+// by the UniqueChannels. In other words, neither cluster channels nor non-cluster ones are de-duplicated in the
+// favor of each other.
+// We use the identifier of RoleList to determine their uniqueness.
+func TestUniqueChannels_ClusterChannels(t *testing.T) {
+	channels := ChannelsByRole(flow.RoleCollection)
+	uniques := UniqueChannels(channels)
+	// collection role has two cluster and one non-cluster channels all with the same RoleList.
+	// Hence all of them should be returned as unique channels.
+	require.Contains(t, uniques, syncClusterPrefix)      // cluster channel
+	require.Contains(t, uniques, consensusClusterPrefix) // cluster channel
+	require.Contains(t, uniques, PushTransactions)       // non-cluster channel
 }
