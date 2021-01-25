4ba6c0ab9bf783629e13dbf2bfe54f5fdbcebdd4
Yahya <yahya@dapperlabs.com>
2021-01-25 09:38:06 -0800

    adds channels to input of generate fanout in network

--- network/p2p/network.go
+++ network/p2p/network.go
@@ -167,7 +167,7 @@ func (n *Network) Identity() (map[flow.Identifier]flow.Identity, error) {
 func (n *Network) Topology() (flow.IdentityList, error) {
 	n.Lock()
 	defer n.Unlock()
-	top, err := n.top.GenerateFanout(n.ids, nil)
+	top, err := n.top.GenerateFanout(n.ids, n.subMngr.Channels())
 	if err != nil {
 		return nil, fmt.Errorf("could not generate topology: %w", err)
 	}
