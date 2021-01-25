ec4ce2a32cb95d7114770d26e8831217fc1ac3f4
Yahya <yahya@dapperlabs.com>
2021-01-25 10:30:34 -0800

    implements sortable interface for channellist

--- network/conduit.go
+++ network/conduit.go
@@ -5,6 +5,7 @@ package network
 import (
 	"errors"
 	"fmt"
+	"sort"
 
 	"github.com/onflow/flow-go/model/flow"
 )
@@ -17,6 +18,32 @@ type ChannelList []Channel
 
 func (c Channel) String() string {
 	return string(c)
+}
+
+// Len returns length of the ChannelList in the number of stored Channels.
+// It satisfies the sort.Interface making the ChannelList sortable.
+func (cl ChannelList) Len() int {
+	return len(cl)
+}
+
+// Less returns true if element i in the ChannelList  is less than j based on the numerical value of its Channel.
+// Otherwise it returns true.
+// It satisfies the sort.Interface making the ChannelList sortable.
+func (cl ChannelList) Less(i, j int) bool {
+	return cl[i] < cl[j]
+}
+
+// Swap swaps the element i and j in the ChannelList.
+// It satisfies the sort.Interface making the ChannelList sortable.
+func (cl ChannelList) Swap(i, j int) {
+	cl[i], cl[j] = cl[j], cl[i]
+}
+
+// ID returns hash of the content of ChannelList. It first sorts the ChannelList and then takes its
+// hash value.
+func (cl ChannelList) ID() flow.Identifier {
+	sort.Sort(cl)
+	return flow.MakeID(cl)
 }
 
 // Conduit represents the interface for engines to communicate over the
