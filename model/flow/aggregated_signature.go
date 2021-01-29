049365fbda28390b81e90acf3f68415e34898738
Alexander Hentschel <alex.hentschel@axiomzen.co>
2021-01-28 20:18:06 -0800

    fixed aggregated signature

--- model/flow/aggregated_signature.go
+++ model/flow/aggregated_signature.go
@@ -13,3 +13,18 @@ type AggregatedSignature struct {
 	// List of signer identifiers
 	SignerIDs []Identifier
 }
+
+// NumberSigners returns the number of signers that contributed to the AggregatedSignature
+func (a *AggregatedSignature) NumberSigners() int {
+	return len(a.VerifierSignatures)
+}
+
+// HasSigner returns true if and only if signer's signature is part of this aggregated signature
+func (a *AggregatedSignature) HasSigner(signerID Identifier) bool {
+	for _, id := range a.SignerIDs {
+		if id == signerID {
+			return true
+		}
+	}
+	return false
+}
