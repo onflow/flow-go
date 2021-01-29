f54a1e6ebe411b6155a6aff6bed4f5381c4987da
Bastian Müller <bastian@axiomzen.co>
2021-01-29 12:23:15 -0800

    only commit draft programs with address locations

--- fvm/programs.go
+++ fvm/programs.go
@@ -7,15 +7,21 @@ import (
 	"github.com/onflow/cadence/runtime/interpreter"
 )
 
+type ProgramEntry struct {
+	Location common.Location
+	Program *interpreter.Program
+}
+
 type Programs struct {
 	lock     sync.RWMutex
-	programs map[common.LocationID]*interpreter.Program
-	draft    map[common.LocationID]*interpreter.Program
+	programs map[common.LocationID]ProgramEntry
+	draft    map[common.LocationID]ProgramEntry
 }
 
 func NewPrograms() *Programs {
 	return &Programs{
-		programs: map[common.LocationID]*interpreter.Program{},
+		programs: map[common.LocationID]ProgramEntry{},
+		draft: map[common.LocationID]ProgramEntry{},
 	}
 }
 
@@ -23,41 +29,50 @@ func (p *Programs) Get(locationID common.LocationID) *interpreter.Program {
 	p.lock.RLock()
 	defer p.lock.RUnlock()
 
-	// check draft first
-	if prog, ok := p.draft[locationID]; ok {
-		return prog
+	// Check is the program is in the drafts
+	programEntry, ok := p.draft[locationID]
+	if ok {
+		return programEntry.Program
 	}
 
-	return p.programs[locationID]
+	return p.programs[locationID].Program
 }
 
-func (p *Programs) Set(locationID common.LocationID, program *interpreter.Program) {
+func (p *Programs) Set(location common.Location, program *interpreter.Program) {
 	p.lock.Lock()
 	defer p.lock.Unlock()
 
-	p.draft[locationID] = program
+	p.draft[location.ID()] = ProgramEntry{
+		Location: location,
+		Program: program,
+	}
 }
 
 func (p *Programs) Commit() error {
 	p.lock.Lock()
 	defer p.lock.Unlock()
 
-	for loc, prog := range p.draft {
-		if _, ok := loc.(common.AddressLocation); !ok {
-			// ignore cases that are not addresslocation
+	// Commit all drafts
+
+	for locationID, programEntry := range p.draft {
+
+		// Only commit programs with address locations,
+		// i.e. programs for contracts,
+		// and not programs for transactions or scripts
+
+		if _, ok := programEntry.Location.(common.AddressLocation); !ok {
 			continue
 		}
-		// TODO make me smarter
-		// if loc already exist this is an update - purge the cache
-		if _, ok := p.programs[loc]; ok {
-			p.programs = make(map[common.LocationID]*interpreter.Program)
-			break
-		}
-		p.programs[loc] = prog
+
+		p.programs[locationID] = programEntry
 	}
 
-	// reset draft
-	p.draft = make(map[common.LocationID]*interpreter.Program)
+	// Clear drafts
+
+	for locationID := range p.draft {
+		delete(p.draft, locationID)
+	}
+
 	return nil
 }
 
@@ -65,7 +80,11 @@ func (p *Programs) Rollback() error {
 	p.lock.Lock()
 	defer p.lock.Unlock()
 
-	// reset draft
-	p.draft = make(map[common.LocationID]*interpreter.Program)
+	// Clear drafts
+
+	for locationID := range p.draft {
+		delete(p.draft, locationID)
+	}
+
 	return nil
 }
