59b99ffd8659788fb4c24d988fd53e31182a916f
Alexander Hentschel <alex.hentschel@axiomzen.co>
2021-01-28 18:03:11 -0800

    removed mutability from ggregated Sig

--- model/flow/aggregated_signature.go
+++ model/flow/aggregated_signature.go
@@ -1,8 +1,6 @@
 package flow
 
 import (
-	"sync"
-
 	"github.com/onflow/flow-go/crypto"
 )
 
@@ -10,7 +8,6 @@ import (
 // to the validity of an execution result chunk.
 // TODO: this will be replaced with BLS aggregation
 type AggregatedSignature struct {
-	sync.Mutex
 	// List of signatures
 	VerifierSignatures []crypto.Signature
 	// List of signer identifiers
@@ -19,45 +16,15 @@ type AggregatedSignature struct {
 
 // Len returns the number of signatures in the AggregatedSignature
 func (a *AggregatedSignature) Len() int {
-	a.Lock()
-	defer a.Unlock()
 	return len(a.VerifierSignatures)
 }
 
 // BySigner returns a signer's signature if it exists
 func (a *AggregatedSignature) BySigner(signerID Identifier) (*crypto.Signature, bool) {
-	a.Lock()
-	defer a.Unlock()
 	for index, id := range a.SignerIDs {
 		if id == signerID {
 			return &a.VerifierSignatures[index], true
 		}
 	}
 	return nil, false
-}
-
-// Add appends a signature.
-// ATTENTION: it does not check for duplicates
-func (a *AggregatedSignature) Add(signerID Identifier, signature crypto.Signature) {
-	a.Lock()
-	defer a.Unlock()
-	a.SignerIDs = append(a.SignerIDs, signerID)
-	a.VerifierSignatures = append(a.VerifierSignatures, signature)
-}
-
-// Copy returns a deep copy of the AggregatedSignature
-func (a *AggregatedSignature) Copy() AggregatedSignature {
-	a.Lock()
-	defer a.Unlock()
-
-	signatures := make([]crypto.Signature, len(a.VerifierSignatures))
-	copy(signatures, a.VerifierSignatures)
-
-	signers := make([]Identifier, len(a.SignerIDs))
-	copy(signers, a.SignerIDs)
-
-	return AggregatedSignature{
-		VerifierSignatures: signatures,
-		SignerIDs:          signers,
-	}
 }
--- model/flow/incorporated_result.go
+++ model/flow/incorporated_result.go
@@ -19,21 +19,21 @@ type IncorporatedResult struct {
 	// incorporated in the payload of IncorporatedBlockID.
 	Result *ExecutionResult
 
-	// aggregatedSignatures is a placeholder for attestation signatures
+	// chunkApprovals is a placeholder for attestation signatures
 	// collected for each chunk. It gets populated by the consensus matching
 	// engine when approvals are matched to execution results.
 	// This field is not exported (name doesn't start with a capital letter), so
 	// it is not used in calculating the ID and Checksum of the Incorporated
 	// Result (RLP encoding ignores private fields).
-	aggregatedSignatures     map[uint64]*AggregatedSignature
-	aggregatedSignaturesLock sync.Mutex
+	chunkApprovals     map[uint64]*approverSignatures
+	chunkApprovalsLock sync.Mutex
 }
 
 func NewIncorporatedResult(incorporatedBlockID Identifier, result *ExecutionResult) *IncorporatedResult {
 	return &IncorporatedResult{
-		IncorporatedBlockID:  incorporatedBlockID,
-		Result:               result,
-		aggregatedSignatures: make(map[uint64]*AggregatedSignature),
+		IncorporatedBlockID: incorporatedBlockID,
+		Result:              result,
+		chunkApprovals:      make(map[uint64]*approverSignatures),
 	}
 }
 
@@ -51,22 +51,21 @@ func (ir *IncorporatedResult) Checksum() Identifier {
 
 // GetChunkSignatures returns the AggregatedSignature for a specific chunk
 func (ir *IncorporatedResult) GetChunkSignatures(chunkIndex uint64) (*AggregatedSignature, bool) {
-	ir.aggregatedSignaturesLock.Lock()
-	defer ir.aggregatedSignaturesLock.Unlock()
-	ar, ok := ir.aggregatedSignatures[chunkIndex]
+	ir.chunkApprovalsLock.Lock()
+	defer ir.chunkApprovalsLock.Unlock()
+	s, ok := ir.chunkApprovals[chunkIndex]
 	if !ok {
 		return nil, false
 	}
-	sigCopy := ar.Copy()
-	return &sigCopy, true
+	return s.ToAggregatedSignature(), true
 }
 
 // GetSignature returns a signature by chunk index and signer ID
 func (ir *IncorporatedResult) GetSignature(chunkIndex uint64, signerID Identifier) (*crypto.Signature, bool) {
-	ir.aggregatedSignaturesLock.Lock()
-	defer ir.aggregatedSignaturesLock.Unlock()
+	ir.chunkApprovalsLock.Lock()
+	defer ir.chunkApprovalsLock.Unlock()
 
-	as, ok := ir.aggregatedSignatures[chunkIndex]
+	as, ok := ir.chunkApprovals[chunkIndex]
 	if !ok {
 		return nil, false
 	}
@@ -75,13 +74,13 @@ func (ir *IncorporatedResult) GetSignature(chunkIndex uint64, signerID Identifie
 
 // AddSignature adds a signature to the collection of AggregatedSignatures
 func (ir *IncorporatedResult) AddSignature(chunkIndex uint64, signerID Identifier, signature crypto.Signature) {
-	ir.aggregatedSignaturesLock.Lock()
-	defer ir.aggregatedSignaturesLock.Unlock()
+	ir.chunkApprovalsLock.Lock()
+	defer ir.chunkApprovalsLock.Unlock()
 
-	as, ok := ir.aggregatedSignatures[chunkIndex]
+	as, ok := ir.chunkApprovals[chunkIndex]
 	if !ok {
-		as = &AggregatedSignature{}
-		ir.aggregatedSignatures[chunkIndex] = as
+		as = NewApproverSignatures()
+		ir.chunkApprovals[chunkIndex] = as
 	}
 
 	as.Add(signerID, signature)
@@ -90,19 +89,75 @@ func (ir *IncorporatedResult) AddSignature(chunkIndex uint64, signerID Identifie
 // GetAggregatedSignatures returns all the aggregated signatures orderd by chunk
 // index
 func (ir *IncorporatedResult) GetAggregatedSignatures() []AggregatedSignature {
-	ir.aggregatedSignaturesLock.Lock()
-	defer ir.aggregatedSignaturesLock.Unlock()
+	ir.chunkApprovalsLock.Lock()
+	defer ir.chunkApprovalsLock.Unlock()
 
 	result := make([]AggregatedSignature, 0, len(ir.Result.Chunks))
 
 	for _, chunk := range ir.Result.Chunks {
-		as, ok := ir.aggregatedSignatures[chunk.Index]
+		as, ok := ir.chunkApprovals[chunk.Index]
 		if ok {
-			result = append(result, as.Copy())
+			result = append(result, *as.ToAggregatedSignature())
 		} else {
 			result = append(result, AggregatedSignature{})
 		}
 	}
 
 	return result
+}
+
+/* ************************************************************************ */
+
+// approverSignatures contains a set of of signatures from verifiers attesting
+// to the validity of an execution result chunk.
+// TODO: this will be replaced with BLS aggregation
+type approverSignatures struct {
+	// List of signatures
+	verifierSignatures []crypto.Signature
+	// List of signer identifiers
+	signerIDs []Identifier
+
+	signerIDSet map[Identifier]struct{}
+}
+
+func NewApproverSignatures() *approverSignatures {
+	return &approverSignatures{
+		verifierSignatures: nil,
+		signerIDs:          nil,
+		signerIDSet:        make(map[Identifier]struct{}),
+	}
+}
+
+func (as *approverSignatures) ToAggregatedSignature() *AggregatedSignature {
+	signatures := make([]crypto.Signature, len(as.verifierSignatures))
+	copy(signatures, as.verifierSignatures)
+
+	signers := make([]Identifier, len(as.signerIDs))
+	copy(signers, as.signerIDs)
+
+	return &AggregatedSignature{
+		VerifierSignatures: signatures,
+		SignerIDs:          signers,
+	}
+}
+
+// BySigner returns a signer's signature if it exists
+func (as *approverSignatures) BySigner(signerID Identifier) (*crypto.Signature, bool) {
+	for index, id := range as.signerIDs {
+		if id == signerID {
+			return &as.verifierSignatures[index], true
+		}
+	}
+	return nil, false
+}
+
+// Add appends a signature.
+// ATTENTION: it does not check for duplicates
+func (as *approverSignatures) Add(signerID Identifier, signature crypto.Signature) {
+	if _, found := as.signerIDSet[signerID]; found {
+		return
+	}
+	as.signerIDSet[signerID] = struct{}{}
+	as.signerIDs = append(as.signerIDs, signerID)
+	as.verifierSignatures = append(as.verifierSignatures, signature)
 }
