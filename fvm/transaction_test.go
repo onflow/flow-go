d46efc3f1f2e544c9a3a26f6a3ef29eaad6f9f29
Maks Pawlak <120831+m4ksio@users.noreply.github.com>
2021-01-29 17:36:53 -0800

    Test for cache storage in case of failed tx

--- fvm/transaction_test.go
+++ fvm/transaction_test.go
@@ -2,12 +2,14 @@ package fvm
 
 import (
 	"bytes"
+	"encoding/hex"
 	"fmt"
 	"sort"
 	"testing"
 
 	"github.com/fxamacker/cbor/v2"
 	"github.com/onflow/cadence/runtime"
+	"github.com/onflow/cadence/runtime/common"
 	"github.com/rs/zerolog"
 	"github.com/stretchr/testify/require"
 
@@ -200,7 +202,105 @@ func TestSafetyCheck(t *testing.T) {
 		require.NotContains(t, buffer.String(), "programs")
 		require.NotContains(t, buffer.String(), "codes")
 	})
+}
 
+func TestFailedTransactionsDontModifyProgramsCache(t *testing.T) {
+
+	ledger := state.NewMapLedger()
+	st := state.NewState(ledger)
+
+	address := flow.HexToAddress("1234")
+
+	//create accounts
+	accounts := state.NewAccounts(st)
+	err := accounts.Create(nil, address)
+	require.NoError(t, err)
+
+	err = st.Commit()
+	require.NoError(t, err)
+
+	contractName := "Cherry"
+
+	contractACode := fmt.Sprintf(`
+			pub contract %s {
+				pub fun say() {
+					log("a")
+				}
+			}
+		`, contractName)
+
+	contractBCode := fmt.Sprintf(`
+			pub contract %s {
+				pub fun shout() {
+					log("a")
+				}
+			}
+		`, contractName)
+
+	deployContract := func(contractName, contractCode string) []byte {
+		return []byte(fmt.Sprintf(
+			`
+			 transaction {
+			   prepare(signer: AuthAccount) {
+				   signer.contracts.add(name: "%s", code: "%s".decodeHex())
+			   }
+			 }
+	   `, contractName, hex.EncodeToString([]byte(contractCode)),
+		))
+	}
+
+	updateContract := func(contractName, contractCode string) []byte {
+		return []byte(fmt.Sprintf(
+			`
+			 transaction {
+			   prepare(signer: AuthAccount) {
+				   signer.contracts.update__experimental(name: "%s", code: "%s".decodeHex())
+			   }
+			 }
+	   `, contractName, hex.EncodeToString([]byte(contractCode)),
+		))
+	}
+
+	procA := Transaction(&flow.TransactionBody{Script: deployContract(contractName, contractACode), Authorizers: []flow.Address{address}, Payer: address}, 0)
+	deployTxInvocator := NewTransactionInvocator(zerolog.Nop())
+	deployContext := NewContext(zerolog.Nop(), WithTransactionProcessors(deployTxInvocator), WithRestrictedDeployment(false))
+	deployRt := runtime.NewInterpreterRuntime()
+
+	deployVm := New(deployRt)
+
+	err = deployVm.Run(deployContext, procA, st)
+	require.NoError(t, err)
+	require.NoError(t, procA.Err)
+
+	programBefore := deployContext.Programs.Get(common.LocationID(fmt.Sprintf("A.%s.%s", address, contractName)))
+	require.NotNil(t, programBefore) //make sure we get some real program
+
+	procB := Transaction(&flow.TransactionBody{Script: updateContract(contractName, contractBCode), Authorizers: []flow.Address{address}, Payer: address}, 0)
+
+	deployContextButFailing := NewContextFromParent(deployContext, WithTransactionProcessors(deployTxInvocator, &AlwaysFailingTransactionProcessor{}))
+	err = deployVm.Run(deployContextButFailing, procB, st)
+	require.NoError(t, err)
+	require.Error(t, procB.Err)
+
+	programAfter := deployContextButFailing.Programs.Get(common.LocationID(fmt.Sprintf("A.%s.%s", address, contractName)))
+
+	require.Equal(t, programBefore, programAfter)
+}
+
+type AlwaysFailingTransactionProcessor struct{}
+
+func (a *AlwaysFailingTransactionProcessor) Process(*VirtualMachine, Context, *TransactionProcedure, *state.State) error {
+	return &FvmError{}
+}
+
+type FvmError struct{}
+
+func (f *FvmError) Code() uint32 {
+	return 6
+}
+
+func (f *FvmError) Error() string {
+	return "I am FVM error"
 }
 
 func encodeContractNames(contractNames []string) ([]byte, error) {
