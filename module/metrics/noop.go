e6e2e00eb89bc89f5e844d0dae98fc1a63826293
Yahya <yahya@dapperlabs.com>
2021-02-01 11:52:24 -0800

    adds inbound processing time as a noop metric

--- module/metrics/noop.go
+++ module/metrics/noop.go
@@ -20,6 +20,7 @@ func (nc *NoopCollector) NetworkDuplicateMessagesDropped(topic string, messageTy
 func (nc *NoopCollector) MessageAdded(priority int)                                              {}
 func (nc *NoopCollector) MessageRemoved(priority int)                                            {}
 func (nc *NoopCollector) QueueDuration(duration time.Duration, priority int)                     {}
+func (nc *NoopCollector) InboundProcessDuration(topic string, duration time.Duration)            {}
 func (nc *NoopCollector) MessageSent(engine string, message string)                              {}
 func (nc *NoopCollector) MessageReceived(engine string, message string)                          {}
 func (nc *NoopCollector) MessageHandled(engine string, message string)                           {}
@@ -45,7 +46,7 @@ func (nc *NoopCollector) CacheEntries(resource string, entries uint)            
 func (nc *NoopCollector) CacheHit(resource string)                                               {}
 func (nc *NoopCollector) CacheMiss(resource string)                                              {}
 func (nc *NoopCollector) MempoolEntries(resource string, entries uint)                           {}
-func (nm *NoopCollector) Register(resource string, entriesFunc EntriesFunc) error                { return nil }
+func (nc *NoopCollector) Register(resource string, entriesFunc EntriesFunc) error                { return nil }
 func (nc *NoopCollector) HotStuffBusyDuration(duration time.Duration, event string)              {}
 func (nc *NoopCollector) HotStuffIdleDuration(duration time.Duration)                            {}
 func (nc *NoopCollector) HotStuffWaitDuration(duration time.Duration, event string)              {}
@@ -83,7 +84,7 @@ func (nc *NoopCollector) ExecutionStateReadsPerBlock(reads uint64)              
 func (nc *NoopCollector) ExecutionStateStorageDiskTotal(bytes int64)                             {}
 func (nc *NoopCollector) ExecutionStorageStateCommitment(bytes int64)                            {}
 func (nc *NoopCollector) ExecutionLastExecutedBlockHeight(height uint64)                         {}
-func (ec *NoopCollector) ExecutionTotalExecutedTransactions(numberOfTx int)                      {}
+func (nc *NoopCollector) ExecutionTotalExecutedTransactions(numberOfTx int)                      {}
 func (nc *NoopCollector) ForestApproxMemorySize(bytes uint64)                                    {}
 func (nc *NoopCollector) ForestNumberOfTrees(number uint64)                                      {}
 func (nc *NoopCollector) LatestTrieRegCount(number uint64)                                       {}
