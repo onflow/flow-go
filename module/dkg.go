58bffbec1124f064c9d3aa07b339f3f94ccfbe66
arrivets <arrivets@gmail.com>
2021-02-02 13:24:32 +0000

    Update DKGContractClient interface

--- module/dkg.go
+++ module/dkg.go
@@ -1,6 +1,10 @@
 package module
 
-import "github.com/onflow/flow-go/model/messages"
+import (
+	"github.com/onflow/flow-go/crypto"
+	"github.com/onflow/flow-go/model/flow"
+	"github.com/onflow/flow-go/model/messages"
+)
 
 // DKGContractClient enables interacting with the DKG smart contract. This
 // contract is deployed to the service account as part of a collection of
@@ -15,20 +19,17 @@ type DKGContractClient interface {
 	// should be re-submitted.
 	Broadcast(msg messages.DKGMessage) error
 
-	// ReadBroadcast reads the broadcast messages from the smart contract for
-	// a particular phase. All messages for the given phase that have been
-	// successfully broadcast will be returned, regardless of whether those
-	// messages have already been read by a previous call to ReadBroadcast.
-	//
+	// ReadBroadcast reads the broadcast messages from the smart contract.
 	// Messages are returned in the order in which they were broadcast (received
-	// and stored in the smart contract).
+	// and stored in the smart contract). The parameters are:
+	//
+	// * fromIndex: return messages with index >= fromIndex
+	// * referenceBlock: a marker for the state against which the query should
+	//   be executed
 	//
 	// DKG nodes should call ReadBroadcast one final time once they have
 	// observed the phase deadline trigger to guarantee they receive all
 	// messages for that phase.
-	//
-	// OPTIONAL: add a messageIndex parameter to only receive messages we have
-	// not already received in previous calls
 	ReadBroadcast(fromIndex uint, referenceBlock flow.Identifier) ([]messages.DKGMessage, error)
 
 	// SubmitResult submits the final public result of the DKG protocol. This
@@ -36,7 +37,5 @@ type DKGContractClient interface {
 	// DKG participant and the group public key.
 	//
 	// SubmitResult must be called strictly after the final phase has ended.
-	//
-	// TODO type of DKGResult?
 	SubmitResult([]crypto.PublicKey) error
 }
