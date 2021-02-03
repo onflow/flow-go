87d482d18382db5f42a61a06246e6b1f55fd877d
Leo Zhang (zhangchiqing) <zhangchiqing@gmail.com>
2021-02-02 17:02:01 -0800

    add assigner and fetcher engine

---
+++ engine/verification/assigner/engine.go
@@ -0,0 +1,95 @@
+package assigner
+
+import (
+	"github.com/rs/zerolog"
+
+	"github.com/onflow/flow-go/engine"
+	"github.com/onflow/flow-go/model/flow"
+	"github.com/onflow/flow-go/module"
+	"github.com/onflow/flow-go/state/protocol"
+	"github.com/onflow/flow-go/storage"
+)
+
+type Engine struct {
+	unit    *engine.Unit
+	log     zerolog.Logger
+	metrics module.VerificationMetrics
+	me      module.Local
+	state   protocol.State
+
+	headerStorage    storage.Headers       // used to check block existence before verifying
+	assigner         module.ChunkAssigner  // used to determine chunks this node needs to verify
+	chunksQueue      storage.ChunksQueue   // to store chunks to be verified
+	newChunkListener module.NewJobListener // to notify about a new chunk
+	finishProcessing finishProcessing      // to report a block has been processed
+}
+
+func New(
+	log zerolog.Logger,
+	metrics module.VerificationMetrics,
+	tracer module.Tracer,
+	me module.Local,
+	state protocol.State,
+	headerStorage storage.Headers,
+	assigner module.ChunkAssigner,
+	chunksQueue storage.ChunksQueue,
+	newChunkListener module.NewJobListener,
+) *Engine {
+	e := &Engine{
+		unit:             engine.NewUnit(),
+		log:              log.With().Str("engine", "assigner").Logger(),
+		metrics:          metrics,
+		me:               me,
+		state:            state,
+		headerStorage:    headerStorage,
+		assigner:         assigner,
+		chunksQueue:      chunksQueue,
+		newChunkListener: newChunkListener,
+	}
+
+	return e
+}
+
+func (e *Engine) withFinishProcessing(finishProcessing finishProcessing) {
+	e.finishProcessing = finishProcessing
+}
+
+func (e *Engine) Ready() <-chan struct{} {
+	return e.unit.Ready()
+}
+
+func (e *Engine) Done() <-chan struct{} {
+	return e.unit.Done()
+}
+
+func (e *Engine) SubmitLocal(event interface{}) {
+	e.Submit(e.me.NodeID(), event)
+}
+
+func (e *Engine) Submit(originID flow.Identifier, event interface{}) {
+	e.unit.Launch(func() {
+		err := e.Process(originID, event)
+		if err != nil {
+			engine.LogError(e.log, err)
+		}
+	})
+}
+
+func (e *Engine) ProcessLocal(event interface{}) error {
+	return e.Process(e.me.NodeID(), event)
+}
+
+func (e *Engine) Process(originID flow.Identifier, event interface{}) error {
+	return e.unit.Do(func() error {
+		return e.process(originID, event)
+	})
+}
+
+func (e *Engine) process(originID flow.Identifier, event interface{}) error {
+	return nil
+}
+
+func (e *Engine) ProcessFinalizedBlock(block *flow.Block) {
+	// the block consumer will pull as many finalized blocks as
+	// it can consume to process
+}
---
+++ engine/verification/fetcher/chunks_test.go
@@ -0,0 +1,27 @@
+package fetcher
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/require"
+
+	"github.com/onflow/flow-go/engine/verification/match"
+	"github.com/onflow/flow-go/model/flow"
+)
+
+// when maxAttempt is set to 3, CanTry will only return true for the first 3 times.
+func TestCanTry(t *testing.T) {
+	t.Run("maxAttempt=3", func(t *testing.T) {
+		maxAttempt := 3
+		chunks := match.NewChunks(10)
+		c := ChunkWithIndex(flow.Identifier{0x11}, 0)
+		chunk := match.NewChunkStatus(c, flow.Identifier{0xaa}, flow.Identifier{0xbb})
+		chunks.Add(chunk)
+		results := []bool{}
+		for i := 0; i < 5; i++ {
+			results = append(results, match.CanTry(maxAttempt, chunk))
+			chunks.IncrementAttempt(chunk.ID())
+		}
+		require.Equal(t, []bool{true, true, true, false, false}, results)
+	})
+}
