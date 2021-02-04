55088de8e571544014ad2fedd2deb2286a45b1b3
Ramtin Mehdizadeh Seraj <ramtin@axiomzen.co>
2021-02-04 15:24:41 -0800

    add more tokens

--- integration/marketplace_simulator/marketplaceSimulator.go
+++ integration/marketplace_simulator/marketplaceSimulator.go
@@ -334,7 +334,7 @@ func (m *MarketPlaceSimulator) createAccounts(serviceAcc *flowAccount, num int) 
 	count := cadence.NewInt(num)
 
 	initialTokenAmount, err := cadence.NewUFix64FromParts(
-		24*60*60,
+		24*60*60*100,
 		0,
 	)
 	if err != nil {
