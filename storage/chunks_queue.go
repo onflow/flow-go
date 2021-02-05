25a32cc163425aaa1ff86aab80abcc959ee4562f
Yahya <yahya@dapperlabs.com>
2021-02-05 11:27:26 -0800

    refactors storing chunk locator interface

--- storage/chunks_queue.go
+++ storage/chunks_queue.go
@@ -2,11 +2,10 @@ package storage
 
 import (
 	"github.com/onflow/flow-go/model/chunks"
-	"github.com/onflow/flow-go/model/flow"
 )
 
 type ChunkLocatorQueue interface {
-	StoreChunkLocator(chunkID flow.Identifier, locator *chunks.ChunkLocator) (bool, error)
+	StoreChunkLocator(locator *chunks.ChunkLocator) (bool, error)
 
 	LatestIndex() (int64, error)
 
