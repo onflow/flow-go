8e106f54e1fe5b5a9cac2982771d3288e6e96504
Yahya <yahya@dapperlabs.com>
2021-02-05 10:46:43 -0800

    refactors inserting and retrieving chunk locators

--- storage/badger/operation/chunks.go
+++ storage/badger/operation/chunks.go
@@ -3,13 +3,14 @@ package operation
 import (
 	"github.com/dgraph-io/badger/v2"
 
+	"github.com/onflow/flow-go/model/chunks"
 	"github.com/onflow/flow-go/model/flow"
 )
 
-func InsertChunk(chunk *flow.Chunk) func(*badger.Txn) error {
-	return insert(makePrefix(codeChunk, chunk.ID()), chunk)
+func InsertChunkLocator(chunkID flow.Identifier, locator *chunks.ChunkLocator) func(*badger.Txn) error {
+	return insert(makePrefix(codeChunk, chunkID), locator)
 }
 
-func RetrieveChunk(chunkID flow.Identifier, chunk *flow.Chunk) func(*badger.Txn) error {
-	return retrieve(makePrefix(codeChunk, chunkID), chunk)
+func RetrieveChunkLocator(chunkID flow.Identifier, locator *chunks.ChunkLocator) func(*badger.Txn) error {
+	return retrieve(makePrefix(codeChunk, chunkID), locator)
 }
