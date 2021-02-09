d081fddbaff28bf9564c553b7e462fc6fc8b2a40
Jordan Schalm <jordan@dapperlabs.com>
2021-02-08 17:22:11 -0800

    move bootstrap tests to state_test

--- state/protocol/badger/state_test.go
+++ state/protocol/badger/state_test.go
@@ -1,6 +1,8 @@
 package badger_test
 
 import (
+	"fmt"
+	"os"
 	"testing"
 
 	"github.com/dgraph-io/badger/v2"
@@ -9,14 +11,17 @@ import (
 
 	"github.com/onflow/flow-go/model/flow"
 	"github.com/onflow/flow-go/module/metrics"
+	"github.com/onflow/flow-go/state/protocol"
 	bprotocol "github.com/onflow/flow-go/state/protocol/badger"
-	"github.com/onflow/flow-go/state/protocol/util"
+	"github.com/onflow/flow-go/state/protocol/inmem"
+	protoutil "github.com/onflow/flow-go/state/protocol/util"
 	storagebadger "github.com/onflow/flow-go/storage/badger"
+	storutil "github.com/onflow/flow-go/storage/util"
 	"github.com/onflow/flow-go/utils/unittest"
 )
 
-// TestBootstrapAndOpen verifies after bootstraping with a state root,
-// we should be able to open it and got the same state root
+// TestBootstrapAndOpen verifies after bootstrapping with a root snapshot
+// we should be able to open it and got the same state.
 func TestBootstrapAndOpen(t *testing.T) {
 
 	// create a state root and bootstrap the protocol state with it
@@ -25,11 +30,9 @@ func TestBootstrapAndOpen(t *testing.T) {
 		block.Header.ParentID = unittest.IdentifierFixture()
 	})
 
-	util.RunWithBootstrapState(t, rootSnapshot, func(db *badger.DB, state *bprotocol.State) {
-
-		// protocol state has been bootstrapped, now open a protocol state with
-		// the database
-		metrics := &metrics.NoopCollector{}
+	protoutil.RunWithBootstrapState(t, rootSnapshot, func(db *badger.DB, state *bprotocol.State) {
+		// protocol state has been bootstrapped, now open a protocol state with the database
+		metrics := new(metrics.NoopCollector)
 		all := storagebadger.InitAll(metrics, db)
 		state, err := bprotocol.OpenState(
 			metrics,
@@ -43,12 +46,119 @@ func TestBootstrapAndOpen(t *testing.T) {
 			all.Statuses)
 		require.NoError(t, err)
 
-		expected, err := rootSnapshot.Head()
-		require.NoError(t, err)
-		actual, err := state.Final().Head()
-		require.NoError(t, err)
-
-		assert.Equal(t, expected, actual)
+		unittest.AssertSnapshotsEqual(t, rootSnapshot, state.Final())
 	})
+}
 
+// TestBootstrapNonRoot tests bootstrapping the protocol state
+// from arbitrary states.
+func TestBootstrapNonRoot(t *testing.T) {
+
+	// start with a regular post-spork root snapshot
+	participants := unittest.CompleteIdentitySet()
+	rootSnapshot := unittest.RootSnapshotFixture(participants)
+	rootBlock, err := rootSnapshot.Head()
+	require.NoError(t, err)
+
+	t.Run("with one block built", func(t *testing.T) {
+		after := snapshotAfter(t, rootSnapshot, func(state *bprotocol.FollowerState) {
+			block1 := unittest.BlockWithParentFixture(rootBlock)
+			err = state.Extend(&block1)
+			require.NoError(t, err)
+		})
+		bootstrap(t, after, func(state *bprotocol.State, err error) {
+			assert.NoError(t, err)
+		})
+	})
+}
+
+func TestBootstrapDuplicateID(t *testing.T) {
+	participants := flow.IdentityList{
+		{NodeID: flow.Identifier{0x01}, Address: "a1", Role: flow.RoleCollection, Stake: 1},
+		{NodeID: flow.Identifier{0x02}, Address: "a2", Role: flow.RoleConsensus, Stake: 2},
+		{NodeID: flow.Identifier{0x03}, Address: "a3", Role: flow.RoleExecution, Stake: 3},
+		{NodeID: flow.Identifier{0x04}, Address: "a4", Role: flow.RoleVerification, Stake: 4},
+		{NodeID: flow.Identifier{0x04}, Address: "a4", Role: flow.RoleVerification, Stake: 4}, // dupe
+	}
+	root := unittest.RootSnapshotFixture(participants)
+	bootstrap(t, root, func(state *bprotocol.State, err error) {
+		assert.Error(t, err)
+	})
+}
+
+func TestBootstrapZeroStake(t *testing.T) {
+	participants := flow.IdentityList{
+		{NodeID: flow.Identifier{0x01}, Address: "a1", Role: flow.RoleCollection, Stake: 0},
+		{NodeID: flow.Identifier{0x02}, Address: "a2", Role: flow.RoleConsensus, Stake: 2},
+		{NodeID: flow.Identifier{0x03}, Address: "a3", Role: flow.RoleExecution, Stake: 3},
+		{NodeID: flow.Identifier{0x04}, Address: "a4", Role: flow.RoleVerification, Stake: 4},
+	}
+	root := unittest.RootSnapshotFixture(participants)
+	bootstrap(t, root, func(state *bprotocol.State, err error) {
+		assert.Error(t, err)
+	})
+}
+
+func TestBootstrapMissingRole(t *testing.T) {
+	requiredRoles := []flow.Role{
+		flow.RoleConsensus,
+		flow.RoleCollection,
+		flow.RoleExecution,
+		flow.RoleVerification,
+	}
+
+	for _, role := range requiredRoles {
+		t.Run(fmt.Sprintf("no %s nodes", role.String()), func(t *testing.T) {
+			participants := unittest.IdentityListFixture(5, unittest.WithAllRolesExcept(role))
+			root := unittest.RootSnapshotFixture(participants)
+			bootstrap(t, root, func(state *bprotocol.State, err error) {
+				assert.Error(t, err)
+			})
+		})
+	}
+}
+
+func TestBootstrapExistingAddress(t *testing.T) {
+	participants := flow.IdentityList{
+		{NodeID: flow.Identifier{0x01}, Address: "a1", Role: flow.RoleCollection, Stake: 1},
+		{NodeID: flow.Identifier{0x02}, Address: "a1", Role: flow.RoleConsensus, Stake: 2}, // dupe address
+		{NodeID: flow.Identifier{0x03}, Address: "a3", Role: flow.RoleExecution, Stake: 3},
+		{NodeID: flow.Identifier{0x04}, Address: "a4", Role: flow.RoleVerification, Stake: 4},
+	}
+
+	root := unittest.RootSnapshotFixture(participants)
+	bootstrap(t, root, func(state *bprotocol.State, err error) {
+		assert.Error(t, err)
+	})
+}
+
+// bootstraps protocol state with the given snapshot and invokes the callback
+// with the result of the constructor
+func bootstrap(t *testing.T, rootSnapshot protocol.Snapshot, f func(*bprotocol.State, error)) {
+	metrics := metrics.NewNoopCollector()
+	dir := unittest.TempDir(t)
+	defer os.RemoveAll(dir)
+	db := unittest.BadgerDB(t, dir)
+	defer db.Close()
+	headers, _, seals, _, _, blocks, setups, commits, statuses, results := storutil.StorageLayer(t, db)
+	state, err := bprotocol.Bootstrap(metrics, db, headers, seals, results, blocks, setups, commits, statuses, rootSnapshot)
+	f(state, err)
+}
+
+// snapshotAfter returns an in-memory state snapshot of the FINALIZED state after
+// bootstrapping the protocol state from the root snapshot, applying the state
+// mutating function f, and clearing the on-disk protocol state.
+//
+// This is used for generating valid snapshots to use when testing bootstrapping
+// from non-root states.
+func snapshotAfter(t *testing.T, rootSnapshot protocol.Snapshot, f func(*bprotocol.FollowerState)) protocol.Snapshot {
+	var after protocol.Snapshot
+	protoutil.RunWithFollowerProtocolState(t, rootSnapshot, func(db *badger.DB, state *bprotocol.FollowerState) {
+		f(state)
+		final := state.Final()
+		var err error
+		after, err = inmem.FromSnapshot(final)
+		require.NoError(t, err)
+	})
+	return after
 }
--- state/protocol/badger/validity_test.go
+++ state/protocol/badger/validity_test.go
@@ -1,96 +1,17 @@
 package badger
 
 import (
-	"fmt"
-	"os"
 	"testing"
 
-	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
 
 	"github.com/onflow/flow-go/crypto"
 	"github.com/onflow/flow-go/model/flow"
 	"github.com/onflow/flow-go/model/flow/filter"
-	"github.com/onflow/flow-go/module/metrics"
-	"github.com/onflow/flow-go/state/protocol"
-	"github.com/onflow/flow-go/storage/util"
 	"github.com/onflow/flow-go/utils/unittest"
 )
 
 var participants = unittest.IdentityListFixture(5, unittest.WithAllRoles())
-
-// bootstraps protocol state with the given snapshot and invokes the callback
-// with the result of the constructor
-func bootstrap(t *testing.T, rootSnapshot protocol.Snapshot, f func(*State, error)) {
-	metrics := metrics.NewNoopCollector()
-	dir := unittest.TempDir(t)
-	defer os.RemoveAll(dir)
-	db := unittest.BadgerDB(t, dir)
-	defer db.Close()
-	headers, _, seals, _, _, blocks, setups, commits, statuses, results := util.StorageLayer(t, db)
-	state, err := Bootstrap(metrics, db, headers, seals, results, blocks, setups, commits, statuses, rootSnapshot)
-	f(state, err)
-}
-
-func TestBootstrapDuplicateID(t *testing.T) {
-	participants := flow.IdentityList{
-		{NodeID: flow.Identifier{0x01}, Address: "a1", Role: flow.RoleCollection, Stake: 1},
-		{NodeID: flow.Identifier{0x02}, Address: "a2", Role: flow.RoleConsensus, Stake: 2},
-		{NodeID: flow.Identifier{0x03}, Address: "a3", Role: flow.RoleExecution, Stake: 3},
-		{NodeID: flow.Identifier{0x04}, Address: "a4", Role: flow.RoleVerification, Stake: 4},
-		{NodeID: flow.Identifier{0x04}, Address: "a4", Role: flow.RoleVerification, Stake: 4}, // dupe
-	}
-	root := unittest.RootSnapshotFixture(participants)
-	bootstrap(t, root, func(state *State, err error) {
-		assert.Error(t, err)
-	})
-}
-
-func TestBootstrapZeroStake(t *testing.T) {
-	participants := flow.IdentityList{
-		{NodeID: flow.Identifier{0x01}, Address: "a1", Role: flow.RoleCollection, Stake: 0},
-		{NodeID: flow.Identifier{0x02}, Address: "a2", Role: flow.RoleConsensus, Stake: 2},
-		{NodeID: flow.Identifier{0x03}, Address: "a3", Role: flow.RoleExecution, Stake: 3},
-		{NodeID: flow.Identifier{0x04}, Address: "a4", Role: flow.RoleVerification, Stake: 4},
-	}
-	root := unittest.RootSnapshotFixture(participants)
-	bootstrap(t, root, func(state *State, err error) {
-		assert.Error(t, err)
-	})
-}
-
-func TestBootstrapMissingRole(t *testing.T) {
-	requiredRoles := []flow.Role{
-		flow.RoleConsensus,
-		flow.RoleCollection,
-		flow.RoleExecution,
-		flow.RoleVerification,
-	}
-
-	for _, role := range requiredRoles {
-		t.Run(fmt.Sprintf("no %s nodes", role.String()), func(t *testing.T) {
-			participants := unittest.IdentityListFixture(5, unittest.WithAllRolesExcept(role))
-			root := unittest.RootSnapshotFixture(participants)
-			bootstrap(t, root, func(state *State, err error) {
-				assert.Error(t, err)
-			})
-		})
-	}
-}
-
-func TestBootstrapExistingAddress(t *testing.T) {
-	participants := flow.IdentityList{
-		{NodeID: flow.Identifier{0x01}, Address: "a1", Role: flow.RoleCollection, Stake: 1},
-		{NodeID: flow.Identifier{0x02}, Address: "a1", Role: flow.RoleConsensus, Stake: 2}, // dupe address
-		{NodeID: flow.Identifier{0x03}, Address: "a3", Role: flow.RoleExecution, Stake: 3},
-		{NodeID: flow.Identifier{0x04}, Address: "a4", Role: flow.RoleVerification, Stake: 4},
-	}
-
-	root := unittest.RootSnapshotFixture(participants)
-	bootstrap(t, root, func(state *State, err error) {
-		assert.Error(t, err)
-	})
-}
 
 func TestEpochSetupValidity(t *testing.T) {
 	t.Run("invalid first/final view", func(t *testing.T) {
