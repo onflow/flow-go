b1f11457e54ec92408cf93ce09299dcc1a3a74af
Yurii Oleksyshyn <yuraolex@gmail.com>
2021-02-09 17:29:08 +0200

    lint godoc

--- module/approval_validator.go
+++ module/approval_validator.go
@@ -7,8 +7,9 @@ import "github.com/onflow/flow-go/model/flow"
 //	to current protocol state.
 // Returns the following:
 // * nil - in case of success
-// * sentinel engine.InvalidInputError when approval is invalid 
+// * sentinel engine.InvalidInputError when approval is invalid
 // * sentinel engine.OutdatedInputError if the corresponding block has a finalized seal
+// * sentinel engine.UnverifiableInputError if approval cannot be validation because of missing data
 // * exception in case of any other error, usually this is not expected.
 type ApprovalValidator interface {
 	Validate(approval *flow.ResultApproval) error
