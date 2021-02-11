5c9c8f752335f678737363ed7b7eafce6c8245b2
Vishal <1117327+vishalchangrani@users.noreply.github.com>
2021-02-11 09:46:54 -0800

    reporting rtt with the ping metric

--- module/metrics/ping.go
+++ module/metrics/ping.go
@@ -1,6 +1,8 @@
 package metrics
 
 import (
+	"time"
+
 	"github.com/prometheus/client_golang/prometheus"
 	"github.com/prometheus/client_golang/prometheus/promauto"
 
@@ -23,14 +25,17 @@ func NewPingCollector() *PingCollector {
 	return pc
 }
 
-func (pc *PingCollector) NodeReachable(node *flow.Identity, nodeInfo string, reachable bool) {
-	var val float64
-	if reachable {
-		val = 1
+func (pc *PingCollector) NodeReachable(node *flow.Identity, nodeInfo string, rtt time.Duration) {
+	var rttValue float64
+	if rtt > 0 {
+		rttValue = float64(rtt.Milliseconds())
+	} else {
+		rttValue = -1
 	}
+
 	pc.reachable.With(prometheus.Labels{
 		LabelNodeID:   node.String(),
 		LabelNodeRole: node.Role.String(),
 		LabelNodeInfo: nodeInfo}).
-		Set(val)
+		Set(rttValue)
 }
