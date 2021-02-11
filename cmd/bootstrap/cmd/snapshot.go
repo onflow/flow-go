7337ed15fe0b7ffd9b72cd57c34ad239ef6ea5fb
danuio <me@danu.io>
2021-02-11 16:50:03 +0000

    temp

--- cmd/bootstrap/cmd/snapshot.go
+++ cmd/bootstrap/cmd/snapshot.go
@@ -2,6 +2,7 @@ package cmd
 
 import (
 	"context"
+	"encoding/json"
 
 	"github.com/spf13/cobra"
 	"google.golang.org/grpc"
@@ -9,9 +10,11 @@ import (
 	"github.com/onflow/flow-go-sdk/client"
 
 	model "github.com/onflow/flow-go/model/bootstrap"
+	"github.com/onflow/flow-go/state/protocol/inmem"
 )
 
 var flagAccessAddress string
+var flagNodeID string
 
 // snapshotCmd represents the command to download the latest protocol snapshot to bootstrap from
 var snapshotCmd = &cobra.Command{
@@ -26,7 +29,9 @@ func init() {
 
 	// required parameters
 	snapshotCmd.Flags().StringVar(&flagAccessAddress, "access-address", "", "the address of an access node")
-	_ = snapshotCmd.MarkFlagRequired("config")
+	_ = snapshotCmd.MarkFlagRequired("access-address")
+	snapshotCmd.Flags().StringVar(&flagNodeID, "node-id", "", "")
+	_ = snapshotCmd.MarkFlagRequired("access-address")
 }
 
 func snapshot(cmd *cobra.Command, args []string) {
@@ -40,6 +45,12 @@ func snapshot(cmd *cobra.Command, args []string) {
 	bytes, err := flowClient.GetLatestProtocolStateSnapshot(ctx)
 	if err != nil {
 		log.Fatal().Msg("could not get latest protocol snapshot from access node")
+	}
+
+	var snapshot inmem.Snapshot
+	err = json.Unmarshal(bytes, &snapshot)
+	if err != nil {
+		log.Fatal().Msg("could not unmarshal snapshot data")
 	}
 
 	writeText(model.PathRootProtocolStateSnapshot, bytes)
