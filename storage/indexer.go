edf9747c51d50561a3f7b12f491bf3b315c7a98b
Yahya <yahya@dapperlabs.com>
2021-02-17 16:04:07 -0800

    refactors indexer implementation to satisfy the interface

--- storage/indexer.go
+++ storage/indexer.go
@@ -7,5 +7,5 @@ import (
 // Indexer indexes all execution receipts in a finalized container block based on
 // their reference block ID and the executor ID.
 type Indexer interface {
-	IndexReceipts(blockID flow.Identifier)
+	IndexReceipts(blockID flow.Identifier) error
 }
