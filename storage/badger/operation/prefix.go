69f80d5aabfbac4fe5c053bf064d9e51b8a6e991
Leo Zhang (zhangchiqing) <zhangchiqing@gmail.com>
2021-02-18 19:37:24 -0800

    update prefix

--- storage/badger/operation/prefix.go
+++ storage/badger/operation/prefix.go
@@ -52,7 +52,7 @@ const (
 	codeBlockExecutionReceipt       = 55 // index mapping block ID to execution receipt ID
 	codeBlockEpochStatus            = 56 // index mapping block ID to epoch status
 	codePayloadReceipts             = 57 // index mapping block ID  to payload receipts
-	codeExecutionIDExecutionReceipt = 58 // index mapping block ID, execution ID to execution receipt ID
+	codeExecutionIDExecutionReceipt = 59 // index mapping block ID, execution ID to execution receipt ID
 
 	// codes related to epoch information
 	codeEpochSetup  = 60 // EpochSetup service event, keyed by ID
