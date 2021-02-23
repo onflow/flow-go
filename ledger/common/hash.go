60fc950d4b0aa304c7d67e5cd57ccb705eca5177
Ramtin Mehdizadeh Seraj <ramtin@axiomzen.co>
2021-02-22 16:53:19 -0800

    introducing ledger hasher

--- ledger/common/hash.go
+++ ledger/common/hash.go
@@ -6,40 +6,55 @@ import (
 	"github.com/onflow/flow-go/ledger/common/utils"
 )
 
-// default value and default hash value for a default node
-var emptySlice []byte
-var defaultLeafHash = HashLeaf([]byte("default:"), emptySlice)
+// hash method 1 mean using sha3-256
+// hash method 2 means using blake2s
+const defaultHashMethod = 1
 
-// we are currently supporting paths of a size up to 32 bytes. I.e. path length from the rootNode of a fully expanded tree to the leaf node is 256. A path of length k is comprised of k+1 vertices. Hence, we need 257 default hashes.
-var defaultHashes [257][]byte
+type LedgerHasher struct {
+	hashMethod      int
+	defaultLeafHash []byte
+	// we are currently supporting paths of a size up to 32 bytes. I.e. path length from the rootNode of a fully expanded tree to the leaf node is 256. A path of length k is comprised of k+1 vertices. Hence, we need 257 default hashes.
+	defaultHashes [257][]byte
+}
 
-func init() {
-	// Creates the Default hashes from base to level height
-	defaultHashes[0] = defaultLeafHash
-	for i := 1; i < len(defaultHashes); i++ {
-		defaultHashes[i] = HashInterNode(defaultHashes[i-1], defaultHashes[i-1])
+func newLedgerHasher(hashMethod int) *LedgerHasher {
+	lh := &LedgerHasher{
+		hashMethod: hashMethod,
 	}
+
+	// default value and default hash value for a default node
+	var emptySlice []byte
+	lh.defaultLeafHash = lh.HashLeaf([]byte("default:"), emptySlice)
+
+	var defaultHashes [257][]byte
+	// Creates the Default hashes from base to level height
+	defaultHashes[0] = lh.defaultLeafHash
+	for i := 1; i < len(defaultHashes); i++ {
+		defaultHashes[i] = lh.HashInterNode(defaultHashes[i-1], defaultHashes[i-1])
+	}
+	lh.defaultHashes = defaultHashes
+	return lh
 }
 
 // GetDefaultHashes returns the default hashes of the SMT.
 //
 // For each tree level N, there is a default hash equal to the chained
 // hashing of the default value N times.
-func GetDefaultHashes() [257][]byte {
-	return defaultHashes
+func (lh *LedgerHasher) GetDefaultHashes() [257][]byte {
+	return lh.defaultHashes
 }
 
 // GetDefaultHashForHeight returns the default hashes of the SMT at a specified height.
 //
 // For each tree level N, there is a default hash equal to the chained
 // hashing of the default value N times.
-func GetDefaultHashForHeight(height int) []byte {
-	return defaultHashes[height]
+func (lh *LedgerHasher) GetDefaultHashForHeight(height int) []byte {
+	return lh.defaultHashes[height]
 }
 
 // HashLeaf generates hash value for leaf nodes (SHA3-256).
 // note that we don't include the keys here as they are already included in the path
-func HashLeaf(path []byte, value []byte) []byte {
+func (lh *LedgerHasher) HashLeaf(path []byte, value []byte) []byte {
 	hasher := hash.NewSHA3_256()
 	_, err := hasher.Write(path)
 	if err != nil {
@@ -54,7 +69,7 @@ func HashLeaf(path []byte, value []byte) []byte {
 }
 
 // HashInterNode generates hash value for intermediate nodes (SHA3-256).
-func HashInterNode(hash1 []byte, hash2 []byte) []byte {
+func (lh *LedgerHasher) HashInterNode(hash1 []byte, hash2 []byte) []byte {
 	hasher := hash.NewSHA3_256()
 	_, err := hasher.Write(hash1)
 	if err != nil {
@@ -68,18 +83,18 @@ func HashInterNode(hash1 []byte, hash2 []byte) []byte {
 }
 
 // ComputeCompactValue computes the value for the node considering the sub tree to only include this value and default values.
-func ComputeCompactValue(path []byte, payload *ledger.Payload, nodeHeight int) []byte {
+func (lh *LedgerHasher) ComputeCompactValue(path []byte, payload *ledger.Payload, nodeHeight int) []byte {
 	// if register is unallocated: return default hash
 	if len(payload.Value) == 0 {
-		return GetDefaultHashForHeight(nodeHeight)
+		return lh.GetDefaultHashForHeight(nodeHeight)
 	}
 
 	// register is allocated
 	treeHeight := 8 * len(path)
 	// TODO Change this later to include the key as well
 	// for now is just the value to make it compatible with previous code
-	computedHash := HashLeaf(path, payload.Value) // we first compute the hash of the fully-expanded leaf
-	for h := 1; h <= nodeHeight; h++ {            // then, we hash our way upwards towards the root until we hit the specified nodeHeight
+	computedHash := lh.HashLeaf(path, payload.Value) // we first compute the hash of the fully-expanded leaf
+	for h := 1; h <= nodeHeight; h++ {               // then, we hash our way upwards towards the root until we hit the specified nodeHeight
 		// h is the height of the node, whose hash we are computing in this iteration.
 		// The hash is computed from the node's children at height h-1.
 		bitIsSet, err := utils.IsBitSet(path, treeHeight-h)
@@ -87,9 +102,9 @@ func ComputeCompactValue(path []byte, payload *ledger.Payload, nodeHeight int) [
 			panic(err)
 		}
 		if bitIsSet { // right branching
-			computedHash = HashInterNode(GetDefaultHashForHeight(h-1), computedHash)
+			computedHash = lh.HashInterNode(lh.GetDefaultHashForHeight(h-1), computedHash)
 		} else { // left branching
-			computedHash = HashInterNode(computedHash, GetDefaultHashForHeight(h-1))
+			computedHash = lh.HashInterNode(computedHash, lh.GetDefaultHashForHeight(h-1))
 		}
 	}
 	return computedHash
