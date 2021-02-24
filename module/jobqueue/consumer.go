3c6e6daa40e3b339284eaa4c77d4b658bd1c8bd0
Leo Zhang (zhangchiqing) <zhangchiqing@gmail.com>
2021-02-24 12:25:26 -0800

    block Stop until the existing jobs are finished

--- module/jobqueue/consumer.go
+++ module/jobqueue/consumer.go
@@ -33,6 +33,7 @@ type Consumer struct {
 	// until the workers are ready
 	isChecking *atomic.Bool // allow only one process checking job processable
 	// are ready, and stop when shutting down.
+	runningJobs sync.WaitGroup // to wait for all existing jobs to finish for graceful shutdown
 
 	processedIndex   int64
 	processings      map[int64]*jobStatus   // keep track of the status of each on going job
@@ -109,12 +110,16 @@ func (c *Consumer) Start(defaultIndex int64) error {
 }
 
 // Stop stops consuming jobs from the job queue.
+// It blocks until the existing worker finish processing the job
 // Note, it won't stop the existing worker from finishing their job
 func (c *Consumer) Stop() {
 	c.mu.Lock()
-	defer c.mu.Unlock()
-
 	c.running = false
+	// not to use `defer`, otherwise runningJobs.Wait will hold the lock and cause deadlock
+	c.mu.Unlock()
+
+	c.log.Info().Msg("stopping consumer")
+	c.runningJobs.Wait()
 	c.log.Info().Msg("consumer stopped")
 }
 
@@ -126,6 +131,7 @@ func (c *Consumer) NotifyJobIsDone(jobID module.JobID) {
 	c.log.Debug().Str("job_id", string(jobID)).Msg("finishing job")
 
 	if c.doneJob(jobID) {
+		c.runningJobs.Done()
 		c.checkProcessable()
 	}
 }
@@ -197,6 +203,7 @@ func (c *Consumer) run() (int64, error) {
 			jobID: jobID,
 			done:  false,
 		}
+		c.runningJobs.Add(1)
 
 		go c.worker.Run(jobAtIndex.job)
 	}
