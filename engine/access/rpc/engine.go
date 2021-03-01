783f5792c434985393f10c6529ebb1e47aa30b28
vishal <1117327+vishalchangrani@users.noreply.github.com>
2021-03-01 11:04:27 -0800

    lint

--- engine/access/rpc/engine.go
+++ engine/access/rpc/engine.go
@@ -29,14 +29,14 @@ import (
 
 // Config defines the configurable options for the access node server
 type Config struct {
-	GRPCListenAddr          string // the GRPC server address as ip:port
-	HTTPListenAddr          string // the HTTP web proxy address as ip:port
-	ExecutionAddr           string // the address of the upstream execution node
-	CollectionAddr          string // the address of the upstream collection node
-	HistoricalAccessAddrs   string // the list of all access nodes from previous spork
-	MaxMsgSize              int    // GRPC max message size
-	ExecutionClientTimeout  time.Duration   // execution API GRPC client timeout
-	CollectionClientTimeout time.Duration   // collection API GRPC client timeout
+	GRPCListenAddr          string        // the GRPC server address as ip:port
+	HTTPListenAddr          string        // the HTTP web proxy address as ip:port
+	ExecutionAddr           string        // the address of the upstream execution node
+	CollectionAddr          string        // the address of the upstream collection node
+	HistoricalAccessAddrs   string        // the list of all access nodes from previous spork
+	MaxMsgSize              int           // GRPC max message size
+	ExecutionClientTimeout  time.Duration // execution API GRPC client timeout
+	CollectionClientTimeout time.Duration // collection API GRPC client timeout
 }
 
 // Engine implements a gRPC server with a simplified version of the Observation API.
