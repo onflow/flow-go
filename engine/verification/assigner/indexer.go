bb251ca9e7ef5d31c571796a0713942344dfd4f5
Yahya <yahya@dapperlabs.com>
2021-03-01 16:32:03 -0800

    adds indexer to assigner package

--- engine/verification/assigner/indexer.go
+++ engine/verification/assigner/indexer.go
@@ -1,1 +1,10 @@
 package assigner
+
+import (
+	"github.com/onflow/flow-go/model/flow"
+)
+
+// Indexer indexes the provided execution receipts. Each Receipt is indexed by the block the receipt pertains to.
+type Indexer interface {
+	Index(Receipts []*flow.ExecutionReceipt) error
+}
