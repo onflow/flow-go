c8fe442ef3c1c31bbc869d375c845f8fbd0bf556
Ramtin Mehdizadeh Seraj <ramtin@axiomzen.co>
2021-03-05 12:58:51 -0800

    merge uuids and uuid generator

--- fvm/state/uuids.go
+++ fvm/state/uuids.go
@@ -2,23 +2,24 @@ package state
 
 import (
 	"encoding/binary"
+	"fmt"
 
 	"github.com/onflow/flow-go/utils/slices"
 )
 
 const keyUUID = "uuid"
 
-type UUIDs struct {
+type UUIDGenerator struct {
 	stateManager *StateManager
 }
 
-func NewUUIDs(stateManager *StateManager) *UUIDs {
-	return &UUIDs{
+func NewUUIDGenerator(stateManager *StateManager) *UUIDGenerator {
+	return &UUIDGenerator{
 		stateManager: stateManager,
 	}
 }
 
-func (u *UUIDs) GetUUID() (uint64, error) {
+func (u *UUIDGenerator) GetUUID() (uint64, error) {
 	stateBytes, err := u.stateManager.State().Get("", "", keyUUID)
 	if err != nil {
 		return 0, err
@@ -28,12 +29,21 @@ func (u *UUIDs) GetUUID() (uint64, error) {
 	return binary.BigEndian.Uint64(bytes), nil
 }
 
-func (u *UUIDs) SetUUID(uuid uint64) {
+func (u *UUIDGenerator) SetUUID(uuid uint64) error {
 	bytes := make([]byte, 8)
 	binary.BigEndian.PutUint64(bytes, uuid)
-	err := u.stateManager.State().Set("", "", keyUUID, bytes)
-	// TODO return the error instead
+	return u.stateManager.State().Set("", "", keyUUID, bytes)
+}
+
+func (u *UUIDGenerator) GenerateUUID() (uint64, error) {
+	uuid, err := u.GetUUID()
 	if err != nil {
-		panic(err)
+		return 0, fmt.Errorf("cannot get UUID: %w", err)
 	}
+
+	err = u.SetUUID(uuid + 1)
+	if err != nil {
+		return 0, fmt.Errorf("cannot set UUID: %w", err)
+	}
+	return uuid, nil
 }
--- fvm/state/uuids_test.go
+++ fvm/state/uuids_test.go
@@ -11,18 +11,50 @@ import (
 func TestUUIDs_GetAndSetUUID(t *testing.T) {
 	ledger := state.NewMapLedger()
 	stm := state.NewStateManager(state.NewState(ledger))
-	uuidsA := state.NewUUIDs(stm)
+	uuidsA := state.NewUUIDGenerator(stm)
 
 	uuid, err := uuidsA.GetUUID() // start from zero
 	require.NoError(t, err)
-
 	require.Equal(t, uint64(0), uuid)
-	uuidsA.SetUUID(5)
+
+	err = uuidsA.SetUUID(5)
+	require.NoError(t, err)
 
 	// create new UUIDs instance
-	uuidsB := state.NewUUIDs(stm)
+	uuidsB := state.NewUUIDGenerator(stm)
 	uuid, err = uuidsB.GetUUID() // should read saved value
 	require.NoError(t, err)
 
 	require.Equal(t, uint64(5), uuid)
+}
+
+func Test_GenerateUUID(t *testing.T) {
+	ledger := state.NewMapLedger()
+	stm := state.NewStateManager(state.NewState(ledger))
+	genA := state.NewUUIDGenerator(stm)
+
+	uuidA, err := genA.GenerateUUID()
+	require.NoError(t, err)
+	uuidB, err := genA.GenerateUUID()
+	require.NoError(t, err)
+	uuidC, err := genA.GenerateUUID()
+	require.NoError(t, err)
+
+	require.Equal(t, uint64(0), uuidA)
+	require.Equal(t, uint64(1), uuidB)
+	require.Equal(t, uint64(2), uuidC)
+
+	// Create new generator instance from same ledger
+	genB := state.NewUUIDGenerator(stm)
+
+	uuidD, err := genB.GenerateUUID()
+	require.NoError(t, err)
+	uuidE, err := genB.GenerateUUID()
+	require.NoError(t, err)
+	uuidF, err := genB.GenerateUUID()
+	require.NoError(t, err)
+
+	require.Equal(t, uint64(3), uuidD)
+	require.Equal(t, uint64(4), uuidE)
+	require.Equal(t, uint64(5), uuidF)
 }
