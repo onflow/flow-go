0788af6ce21962691ed017de760783d9b29a4d0a
Yahya <yahya@dapperlabs.com>
2021-03-15 11:01:46 -0700

    packages block job into a separate file

---
+++ engine/verification/assigner/blockjob.go
@@ -0,0 +1,31 @@
+package assigner
+
+import (
+	"fmt"
+
+	"github.com/onflow/flow-go/model/flow"
+	"github.com/onflow/flow-go/module"
+)
+
+// BlockJob implements the Job interface. It converts a Block into a Job to be used by job queue.
+type BlockJob struct {
+	Block *flow.Block
+}
+
+// ID converts block id into job id, which guarantees uniqueness
+func (j BlockJob) ID() module.JobID {
+	return blockIDToJobID(j.Block.ID())
+}
+
+func blockIDToJobID(blockID flow.Identifier) module.JobID {
+	return module.JobID(fmt.Sprintf("%v", blockID))
+}
+
+func blockToJob(block *flow.Block) *BlockJob {
+	return &BlockJob{Block: block}
+}
+
+func jobToBlock(job module.Job) *flow.Block {
+	blockJob, _ := job.(*BlockJob)
+	return blockJob.Block
+}
--- engine/verification/assigner/consumer.go
+++ engine/verification/assigner/consumer.go
@@ -13,29 +13,6 @@ import (
 	"github.com/onflow/flow-go/storage"
 )
 
-// BlockJob converts a Block into a Job to be used by job queue
-type BlockJob struct {
-	Block *flow.Block
-}
-
-// ID converts block id into job id, which guarantees uniqueness
-func (j BlockJob) ID() module.JobID {
-	return blockIDToJobID(j.Block.ID())
-}
-
-func blockIDToJobID(blockID flow.Identifier) module.JobID {
-	return module.JobID(fmt.Sprintf("%v", blockID))
-}
-
-func blockToJob(block *flow.Block) *BlockJob {
-	return &BlockJob{Block: block}
-}
-
-func jobToBlock(job module.Job) *flow.Block {
-	blockJob, _ := job.(*BlockJob)
-	return blockJob.Block
-}
-
 // FinalizedBlockReader provides an abstraction for consumers to read blocks
 // as job
 type FinalizedBlockReader struct {
