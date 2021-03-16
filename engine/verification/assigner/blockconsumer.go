e15d81c41456c116296b0275c927c93eb432f869
Yahya <yahya@dapperlabs.com>
2021-03-16 14:23:58 -0700

    adds block consumer interface

--- engine/verification/assigner/blockconsumer.go
+++ engine/verification/assigner/blockconsumer.go
@@ -1,1 +1,11 @@
 package assigner
+
+import (
+	"github.com/onflow/flow-go/model/flow"
+)
+
+// FinalizedBlockWorker should be implemented by the verification node engine responsible
+// for processing execution receipts included in a finalized block.
+type FinalizedBlockWorker interface {
+	ProcessFinalizedBlock(*flow.Block)
+}
