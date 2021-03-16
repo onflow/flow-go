6d9cd814951875e44bf531d9e0ef01f09e3d609b
Yahya <yahya@dapperlabs.com>
2021-03-16 15:37:02 -0700

    adds with block consumer to finalized block processor

--- engine/verification/assigner/blockconsumer.go
+++ engine/verification/assigner/blockconsumer.go
@@ -2,10 +2,17 @@ package assigner
 
 import (
 	"github.com/onflow/flow-go/model/flow"
+	"github.com/onflow/flow-go/module"
 )
 
-// FinalizedBlockWorker should be implemented by the verification node engine responsible
+// FinalizedBlockProcessor should be implemented by the verification node engine responsible
 // for processing execution receipts included in a finalized block.
-type FinalizedBlockWorker interface {
+type FinalizedBlockProcessor interface {
 	ProcessFinalizedBlock(*flow.Block)
+
+	// WithBlockConsumerNotifier sets the notifier of the block processor.
+	// The notifier is called by the internal logic of the processor to let the consumer know that
+	// the processor is done by processing a block so that the next block may be passed to the processor
+	// by the consumer through invoking ProcessFinalizedBlock of this processor.
+	WithBlockConsumerNotifier(module.ProcessingNotifier)
 }
