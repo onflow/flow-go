41cc0b51e741bddcaacfe0583d390c5a50fea8ff
Yahya <yahya@dapperlabs.com>
2021-03-16 15:51:11 -0700

    Implements with consumer helper

--- engine/verification/assigner/blockconsumer/consumer_test.go
+++ engine/verification/assigner/blockconsumer/consumer_test.go
@@ -3,8 +3,16 @@ package blockconsumer
 import (
 	"testing"
 
+	"github.com/dgraph-io/badger/v2"
 	"github.com/stretchr/testify/require"
 
+	"github.com/onflow/flow-go/engine/testutil"
+	"github.com/onflow/flow-go/engine/verification/assigner"
+	"github.com/onflow/flow-go/module"
+	"github.com/onflow/flow-go/module/metrics"
+	"github.com/onflow/flow-go/module/trace"
+	"github.com/onflow/flow-go/storage"
+	bstorage "github.com/onflow/flow-go/storage/badger"
 	"github.com/onflow/flow-go/utils/unittest"
 )
 
@@ -24,4 +32,30 @@ func TestBlockToJob(t *testing.T) {
 // 3. pushing 100 jobs concurrently, could end up having 100
 // jobs processed by the consumer
 func TestProduceConsume(t *testing.T) {
+}
+
+func WithConsumer(
+	t *testing.T,
+	blockProcessor assigner.FinalizedBlockProcessor,
+	withConsumer func(*BlockConsumer, storage.Blocks),
+) {
+	unittest.RunWithBadgerDB(t, func(db *badger.DB) {
+		maxProcessing := int64(3)
+
+		processedHeight := bstorage.NewConsumerProgress(db, module.ConsumeProgressVerificationBlockHeight)
+		collector := &metrics.NoopCollector{}
+		tracer := &trace.NoopTracer{}
+		ids := unittest.IdentityListFixture(1)
+		s := testutil.CompleteStateFixture(t, collector, tracer, ids)
+
+		consumer, _, err := NewBlockConsumer(unittest.Logger(),
+			processedHeight,
+			s.Storage.Blocks,
+			s.State,
+			blockProcessor,
+			maxProcessing)
+		require.NoError(t, err)
+
+		withConsumer(consumer, s.Storage.Blocks)
+	})
 }
