5ae45f8e188bdfb9c281eeb6712989230fc7a142
Yurii Oleksyshyn <yuraolex@gmail.com>
2021-03-17 17:19:54 +0200

    Fixed more tests

--- engine/consensus/matching/engine_test.go
+++ engine/consensus/matching/engine_test.go
@@ -105,7 +105,7 @@ func (ms *MatchingEngineSuite) TestProcessValidReceipt() {
 		unittest.WithResult(unittest.ExecutionResultFixture(unittest.WithBlock(&ms.UnfinalizedBlock))),
 	)
 
-	ms.receiptValidator.On("Validate", []*flow.ExecutionReceipt{receipt}).Return(nil).Once()
+	ms.receiptValidator.On("Validate", receipt).Return(nil).Once()
 	// we expect that receipt is persisted in storage
 	ms.ReceiptsDB.On("Store", receipt).Return(nil).Once()
 	// we expect that receipt is added to mempool
@@ -137,7 +137,7 @@ func (ms *MatchingEngineSuite) TestMultipleProcessingItems() {
 			unittest.WithExecutorID(originID),
 			unittest.WithResult(unittest.ExecutionResultFixture(unittest.WithBlock(&ms.UnfinalizedBlock))),
 		)
-		ms.receiptValidator.On("Validate", []*flow.ExecutionReceipt{receipt}).Return(nil).Once()
+		ms.receiptValidator.On("Validate", receipt).Return(nil).Once()
 		// we expect that receipt is persisted in storage
 		ms.ReceiptsDB.On("Store", receipt).Return(nil).Once()
 		// we expect that receipt is added to mempool
