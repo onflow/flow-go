dd37b156b9e9a83cf5b4fdb5b3cfdb2c6e833533
Yurii Oleksyshyn <yuraolex@gmail.com>
2021-03-18 22:56:29 +0200

    Linted

--- module/validation/seal_validator.go
+++ module/validation/seal_validator.go
@@ -2,13 +2,13 @@ package validation
 
 import (
 	"fmt"
-	"github.com/onflow/flow-go/state"
 
 	"github.com/rs/zerolog/log"
 
 	"github.com/onflow/flow-go/engine"
 	"github.com/onflow/flow-go/model/flow"
 	"github.com/onflow/flow-go/module"
+	"github.com/onflow/flow-go/state"
 	"github.com/onflow/flow-go/state/protocol"
 	"github.com/onflow/flow-go/storage"
 )
