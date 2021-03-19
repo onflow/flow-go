d31aea8065b012ed7b82191129c2797bd803c8d9
danuio <me@danu.io>
2021-03-19 11:44:55 +0000

    Add Prepare command

--- cmd/bootstrap/transit/cmd/prepare.go
+++ cmd/bootstrap/transit/cmd/prepare.go
@@ -1,1 +1,50 @@
 package cmd
+
+import (
+	"github.com/onflow/flow-go/model/flow"
+	"github.com/spf13/cobra"
+)
+
+// prepareCmd represents a command to generate transit keys for push command
+var prepareCmd = &cobra.Command{
+	Use:   "prepare",
+	Short: "Generate transit keys for push command",
+	Long:  `Generate transit keys for push command`,
+	Run:   prepare,
+}
+
+func init() {
+	rootCmd.AddCommand(prepareCmd)
+	addPrepareCmdFlags()
+}
+
+func addPrepareCmdFlags() {
+	pullCmd.Flags().StringVar(&flagNodeRole, "role", "", `node role (can be "collection", "consensus", "execution", "verification" or "access")`)
+
+	_ = pullCmd.MarkFlagRequired("role")
+}
+
+// prepare generates transit keys for push command
+func prepare(cmd *cobra.Command, args []string) {
+	log.Info().Msg("running prepare")
+
+	nodeID, err := readNodeID()
+	if err != nil {
+		log.Fatal().Err(err).Msg("could not read node ID")
+	}
+
+	role, err := flow.ParseRole(flagNodeRole)
+	if err != nil {
+		log.Fatal().Err(err).Msg("could not parse Flow role")
+	}
+
+	if role == flow.RoleConsensus {
+		err := generateKeys(nodeID)
+		if err != nil {
+			log.Fatal().Err(err).Msg("failed to prepare")
+		}
+		return
+	}
+
+	log.Info().Str("role", role.String()).Msg("no preparation needed for role")
+}
