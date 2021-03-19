48e7c66e17e52ef52d2bdcf926112d95de7e8b41
Yahya <yahya@dapperlabs.com>
2021-03-19 10:39:00 -0700

    revises comments on worker

--- engine/verification/assigner/processor/worker.go
+++ engine/verification/assigner/processor/worker.go
@@ -6,44 +6,45 @@ import (
 	"github.com/onflow/flow-go/module"
 )
 
-// Worker receives job from job consumer and converts it back to Block
-// for engine to process
-// Worker is stateless, it acts as a middleman to convert the job into
-// the entity that the engine is expecting, and translating the id of
-// the entity back to JobID notify the consumer a job is done.
-type Worker struct {
-	blockWorker assigner.FinalizedBlockProcessor
-	consumer    *BlockConsumer
+// worker is an internal type of this package.
+// It receives block jobs from job consumer and converts it to Block and passes it to the
+// finalized block processor (i.e., assigner engine) to process.
+// In this sense, worker acts as a broker between the block consumer and block processor.
+// The worker is stateless, and is solely responsible for converting block jobs to blocks, passing them
+// to the processor and notifying consumer when the block is processed.
+type worker struct {
+	processor assigner.FinalizedBlockProcessor
+	consumer  *BlockConsumer
 }
 
-func newWorker(blockWorker assigner.FinalizedBlockProcessor) *Worker {
-	return &Worker{
-		blockWorker: blockWorker,
+func newWorker(processor assigner.FinalizedBlockProcessor) *worker {
+	return &worker{
+		processor: processor,
 	}
 }
 
-func (w *Worker) withBlockConsumer(consumer *BlockConsumer) {
+func (w *worker) withBlockConsumer(consumer *BlockConsumer) {
 	w.consumer = consumer
 }
 
 // Run is a block worker that receives a job corresponding to a finalized block.
 // It then converts the job to a block and passes it to the underlying engine
 // for processing.
-func (w *Worker) Run(job module.Job) error {
+func (w *worker) Run(job module.Job) error {
 	block, err := toBlock(job)
 	if err != nil {
 		return err
 	}
-	w.blockWorker.ProcessFinalizedBlock(block)
+	w.processor.ProcessFinalizedBlock(block)
 
 	return nil
 }
 
 // Notify is a callback for engine to notify a block has been
-// processed by the given blockID
-// the worker will translate the block ID into jobID and notify the consumer
+// processed by the given blockID.
+// The worker translates the block ID into job ID and notifies the consumer
 // that the job is done.
-func (w *Worker) Notify(blockID flow.Identifier) {
+func (w *worker) Notify(blockID flow.Identifier) {
 	jobID := jobID(blockID)
 	w.consumer.NotifyJobIsDone(jobID)
 }
