fac70e476581f1d673febb554c4b3e23daab44b7
Yahya <yahya@dapperlabs.com>
2021-03-18 17:28:27 -0700

    adds returning error to block job

--- engine/verification/assigner/processor/blockjob.go
+++ engine/verification/assigner/processor/blockjob.go
@@ -8,20 +8,28 @@ import (
 )
 
 // BlockJob implements the Job interface. It converts a Block into a Job to be used by job queue.
+//
+// In current architecture, BlockJob represents a finalized block enqueued to be processed by the BlockConsumer that implements the
+// JobQueue interface.
 type BlockJob struct {
 	Block *flow.Block
 }
 
-// ID converts block id into job id, which guarantees uniqueness
+// ID converts block id into job id, which guarantees uniqueness.
 func (j BlockJob) ID() module.JobID {
-	return blockIDToJobID(j.Block.ID())
+	return jobID(j.Block.ID())
 }
 
-func blockIDToJobID(blockID flow.Identifier) module.JobID {
+// jobID returns the corresponding unique job id of the BlockJob for this job.
+func jobID(blockID flow.Identifier) module.JobID {
 	return module.JobID(fmt.Sprintf("%v", blockID))
 }
 
-func jobToBlock(job module.Job) *flow.Block {
-	blockJob, _ := job.(*BlockJob)
-	return blockJob.Block
+// block converts a block job into its corresponding block.
+func toBlock(job module.Job) (*flow.Block, error) {
+	blockJob, ok := job.(*BlockJob)
+	if !ok {
+		return nil, fmt.Errorf("could not assert job to block, job id: %x", job.ID())
+	}
+	return blockJob.Block, nil
 }
