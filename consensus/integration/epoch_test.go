db99d60acbeaa3de168abafccb1154118b4ca9cc
Jordan Schalm <jordan@dapperlabs.com>
2021-03-25 18:11:56 -0700

    refactor process of setting up next epoch

--- consensus/integration/epoch_test.go
+++ consensus/integration/epoch_test.go
@@ -1,7 +1,6 @@
 package integration_test
 
 import (
-	"fmt"
 	"testing"
 	"time"
 
@@ -23,31 +22,10 @@ func TestUnweightedNode(t *testing.T) {
 	stopper := NewStopper(2, 0)
 	rootSnapshot := createRootSnapshot(t, 3)
 
-	// convert to encodable form to add an un-staked consensus node
-	enc := rootSnapshot.Encodable()
-
-	// add a consensus to next epoch (it will have 0 weight)
+	// add a consensus node to next epoch (it will have 0 weight)
 	unweightedIdentity := unittest.IdentityFixture(unittest.WithRole(flow.RoleConsensus))
 	nextEpochIdentities := unittest.CompleteIdentitySet(unweightedIdentity)
-
-	currEpoch := enc.Epochs.Current
-	enc.Epochs.Next = &inmem.EncodableEpoch{
-		Counter:           currEpoch.Counter,
-		FirstView:         currEpoch.FinalView + 1,
-		FinalView:         currEpoch.FinalView + 1 + 10000,
-		RandomSource:      unittest.SeedFixture(flow.EpochSetupRandomSourceLength),
-		InitialIdentities: nextEpochIdentities,
-		Clustering:        unittest.ClusterList(1, nextEpochIdentities),
-	}
-	enc.LatestSeal.ResultID = enc.LatestResult.ID()
-	enc.Phase = flow.EpochPhaseCommitted
-	enc.Identities = append(
-		enc.Identities,
-		nextEpochIdentities.Map(mapfunc.WithStake(0))...,
-	)
-
-	// convert back to protocol state snapshot
-	rootSnapshot = inmem.SnapshotFromEncodable(enc)
+	rootSnapshot = withNextEpoch(rootSnapshot, nextEpochIdentities, 10_000)
 
 	nodes, hub := createNodes(t, stopper, rootSnapshot)
 
@@ -69,29 +47,13 @@ func TestStaticEpochTransition(t *testing.T) {
 	stopper := NewStopper(8, 0)
 	rootSnapshot := createRootSnapshot(t, 3)
 
-	// convert to encodable form to add an un-staked consensus node
-	enc := rootSnapshot.Encodable()
+	firstEpochCounter, err := rootSnapshot.Epochs().Current().Counter()
+	require.NoError(t, err)
 
-	// same identities -> same consensus committee in next epoch
-	nextEpochIdentities := enc.Identities
-
-	currEpoch := &enc.Epochs.Current              // take pointer so assignments apply
-	currEpoch.FinalView = currEpoch.FirstView + 4 // first epoch lasts 5 views
-	enc.Epochs.Next = &inmem.EncodableEpoch{
-		Counter:           currEpoch.Counter + 1,
-		FirstView:         currEpoch.FinalView + 1,
-		FinalView:         currEpoch.FinalView + 1 + 10000,
-		RandomSource:      unittest.SeedFixture(flow.EpochSetupRandomSourceLength),
-		InitialIdentities: nextEpochIdentities,
-		Clustering:        unittest.ClusterList(1, nextEpochIdentities),
-		Clusters:          currEpoch.Clusters,
-		DKG:               currEpoch.DKG,
-	}
-	enc.LatestSeal.ResultID = enc.LatestResult.ID()
-	enc.Phase = flow.EpochPhaseCommitted
-
-	// convert back to protocol state snapshot
-	rootSnapshot = inmem.SnapshotFromEncodable(enc)
+	// set up next epoch beginning in 4 views, with same identities as first epoch
+	nextEpochIdentities, err := rootSnapshot.Identities(filter.Any)
+	require.NoError(t, err)
+	rootSnapshot = withNextEpoch(rootSnapshot, nextEpochIdentities, 4)
 
 	nodes, hub := createNodes(t, stopper, rootSnapshot)
 
@@ -105,9 +67,9 @@ func TestStaticEpochTransition(t *testing.T) {
 
 	// confirm that we have transitioned to the new epoch
 	pstate := nodes[0].state
-	counter, err := pstate.Final().Epochs().Current().Counter()
+	afterCounter, err := pstate.Final().Epochs().Current().Counter()
 	require.NoError(t, err)
-	assert.Equal(t, enc.Epochs.Next.Counter, counter)
+	assert.Equal(t, firstEpochCounter+1, afterCounter)
 
 	cleanupNodes(nodes)
 }
@@ -119,38 +81,21 @@ func TestEpochTransition_IdentitiesOverlap(t *testing.T) {
 	stopper := NewStopper(8, 0)
 	rootSnapshot := createRootSnapshot(t, 3)
 
-	// convert to encodable form to add an un-staked consensus node
-	enc := rootSnapshot.Encodable()
+	firstEpochCounter, err := rootSnapshot.Epochs().Current().Counter()
+	require.NoError(t, err)
 
-	// 2 overlapping and 1 new consensus node in next epoch
-	removedIdentity := enc.Identities.Filter(filter.HasRole(flow.RoleConsensus)).Sample(1)[0]
+	// set up next epoch with 1 new consensus nodes and 2 consensus nodes from first epoch
+	// 1 consensus node is removed after the first epoch
+	firstEpochIdentities, err := rootSnapshot.Identities(filter.Any)
+	require.NoError(t, err)
+
+	removedIdentity := firstEpochIdentities.Filter(filter.HasRole(flow.RoleConsensus)).Sample(1)[0]
 	newIdentity := unittest.IdentityFixture(unittest.WithRole(flow.RoleConsensus))
 	nextEpochIdentities := append(
-		enc.Identities.Filter(filter.Not(filter.HasNodeID(removedIdentity.NodeID))),
+		firstEpochIdentities.Filter(filter.Not(filter.HasNodeID(removedIdentity.NodeID))),
 		newIdentity,
 	)
-
-	currEpoch := &enc.Epochs.Current              // take pointer so assignments apply
-	currEpoch.FinalView = currEpoch.FirstView + 4 // first epoch lasts 5 views
-	enc.Epochs.Next = &inmem.EncodableEpoch{
-		Counter:           currEpoch.Counter + 1,
-		FirstView:         currEpoch.FinalView + 1,
-		FinalView:         currEpoch.FinalView + 1 + 10000,
-		RandomSource:      unittest.SeedFixture(flow.EpochSetupRandomSourceLength),
-		InitialIdentities: nextEpochIdentities,
-		Clustering:        unittest.ClusterList(1, nextEpochIdentities),
-		Clusters:          currEpoch.Clusters,
-		DKG: &inmem.EncodableDKG{
-			GroupKey:     encodable.RandomBeaconPubKey{unittest.KeyFixture(crypto.BLSBLS12381).PublicKey()},
-			Participants: unittest.DKGParticipantLookup(nextEpochIdentities),
-		},
-	}
-	enc.LatestSeal.ResultID = enc.LatestResult.ID()
-	enc.Phase = flow.EpochPhaseCommitted
-	enc.Identities = append(enc.Identities, newIdentity)
-
-	// convert back to protocol state snapshot
-	rootSnapshot = inmem.SnapshotFromEncodable(enc)
+	rootSnapshot = withNextEpoch(rootSnapshot, nextEpochIdentities, 4)
 
 	nodes, hub := createNodes(t, stopper, rootSnapshot)
 
@@ -164,58 +109,34 @@ func TestEpochTransition_IdentitiesOverlap(t *testing.T) {
 
 	// confirm that we have transitioned to the new epoch
 	pstate := nodes[0].state
-	counter, err := pstate.Final().Epochs().Current().Counter()
+	afterCounter, err := pstate.Final().Epochs().Current().Counter()
 	require.NoError(t, err)
-	assert.Equal(t, enc.Epochs.Next.Counter, counter)
+	assert.Equal(t, firstEpochCounter+1, afterCounter)
 
 	cleanupNodes(nodes)
 }
 
 // test consensus across an epoch boundary, where the identity table in the new
 // epoch is disjoint from the identity table in the first epoch.
-// TODO copied from above - needs implementation
 func TestEpochTransition_IdentitiesDisjoint(t *testing.T) {
 	// must finalize 8 blocks, we specify the epoch transition after 4 views
 	stopper := NewStopper(8, 0)
 	rootSnapshot := createRootSnapshot(t, 3)
 
-	// convert to encodable form to add an un-staked consensus node
-	enc := rootSnapshot.Encodable()
+	firstEpochCounter, err := rootSnapshot.Epochs().Current().Counter()
+	require.NoError(t, err)
 
-	fmt.Println("epoch1 sns: ", enc.Identities.Filter(filter.HasRole(flow.RoleConsensus)).NodeIDs())
+	// prepare a next epoch with a completely different consensus committee
+	// (no overlapping consensus nodes)
+	firstEpochIdentities, err := rootSnapshot.Identities(filter.Any)
+	require.NoError(t, err)
 
-	// 2 overlapping and 1 new consensus node in next epoch
-	removedIdentity := enc.Identities.Filter(filter.HasRole(flow.RoleConsensus)).Sample(1)[0]
-	newIdentity := unittest.IdentityFixture(unittest.WithRole(flow.RoleConsensus))
+	newIdentities := unittest.IdentityListFixture(3, unittest.WithRole(flow.RoleConsensus))
 	nextEpochIdentities := append(
-		enc.Identities.Filter(filter.Not(filter.HasNodeID(removedIdentity.NodeID))),
-		newIdentity,
+		firstEpochIdentities.Filter(filter.Not(filter.HasRole(flow.RoleConsensus))), // remove all consensus nodes
+		newIdentities..., // add new consensus nodes
 	)
-	fmt.Println("epoch1 sns: ", nextEpochIdentities.Filter(filter.HasRole(flow.RoleConsensus)).NodeIDs())
-	fmt.Println("removed: ", removedIdentity.NodeID)
-	fmt.Println("added: ", newIdentity.NodeID)
-
-	currEpoch := &enc.Epochs.Current              // take pointer so assignments apply
-	currEpoch.FinalView = currEpoch.FirstView + 4 // first epoch lasts 5 views
-	enc.Epochs.Next = &inmem.EncodableEpoch{
-		Counter:           currEpoch.Counter + 1,
-		FirstView:         currEpoch.FinalView + 1,
-		FinalView:         currEpoch.FinalView + 1 + 10000,
-		RandomSource:      unittest.SeedFixture(flow.EpochSetupRandomSourceLength),
-		InitialIdentities: nextEpochIdentities,
-		Clustering:        unittest.ClusterList(1, nextEpochIdentities),
-		Clusters:          currEpoch.Clusters,
-		DKG: &inmem.EncodableDKG{
-			GroupKey:     encodable.RandomBeaconPubKey{unittest.KeyFixture(crypto.BLSBLS12381).PublicKey()},
-			Participants: unittest.DKGParticipantLookup(nextEpochIdentities),
-		},
-	}
-	enc.LatestSeal.ResultID = enc.LatestResult.ID()
-	enc.Phase = flow.EpochPhaseCommitted
-	enc.Identities = append(enc.Identities, newIdentity)
-
-	// convert back to protocol state snapshot
-	rootSnapshot = inmem.SnapshotFromEncodable(enc)
+	rootSnapshot = withNextEpoch(rootSnapshot, nextEpochIdentities, 4)
 
 	nodes, hub := createNodes(t, stopper, rootSnapshot)
 
@@ -229,9 +150,58 @@ func TestEpochTransition_IdentitiesDisjoint(t *testing.T) {
 
 	// confirm that we have transitioned to the new epoch
 	pstate := nodes[0].state
-	counter, err := pstate.Final().Epochs().Current().Counter()
+	afterCounter, err := pstate.Final().Epochs().Current().Counter()
 	require.NoError(t, err)
-	assert.Equal(t, enc.Epochs.Next.Counter, counter)
+	assert.Equal(t, firstEpochCounter+1, afterCounter)
 
 	cleanupNodes(nodes)
+}
+
+// withNextEpoch adds a valid next epoch with the given identities to the input
+// snapshot. Also sets the length of the first (current) epoch to curEpochViews.
+//
+// We make the first (current) epoch start in committed phase so we can transition
+// to the next epoch upon reaching the appropriate view without any further changes
+// to the protocol state.
+func withNextEpoch(snapshot *inmem.Snapshot, nextEpochIdentities flow.IdentityList, curEpochViews uint64) *inmem.Snapshot {
+
+	// convert to encodable representation for simple modification
+	encodableSnapshot := snapshot.Encodable()
+
+	currEpoch := &encodableSnapshot.Epochs.Current                // take pointer so assignments apply
+	currEpoch.FinalView = currEpoch.FirstView + curEpochViews - 1 // first epoch lasts curEpochViews
+	encodableSnapshot.Epochs.Next = &inmem.EncodableEpoch{
+		Counter:           currEpoch.Counter + 1,
+		FirstView:         currEpoch.FinalView + 1,
+		FinalView:         currEpoch.FinalView + 1 + 10000,
+		RandomSource:      unittest.SeedFixture(flow.EpochSetupRandomSourceLength),
+		InitialIdentities: nextEpochIdentities,
+		// must include info corresponding to EpochCommit event, since we are
+		// starting in committed phase
+		Clustering: unittest.ClusterList(1, nextEpochIdentities),
+		Clusters:   currEpoch.Clusters,
+		DKG: &inmem.EncodableDKG{
+			GroupKey: encodable.RandomBeaconPubKey{
+				PublicKey: unittest.KeyFixture(crypto.BLSBLS12381).PublicKey(),
+			},
+			Participants: unittest.DKGParticipantLookup(nextEpochIdentities),
+		},
+	}
+
+	// we must start the current epoch in committed phase so we can transition to the next epoch
+	encodableSnapshot.Phase = flow.EpochPhaseCommitted
+	encodableSnapshot.LatestSeal.ResultID = encodableSnapshot.LatestResult.ID()
+
+	// set identities for root snapshot to include next epoch identities,
+	// since we are in committed phase
+	encodableSnapshot.Identities = append(
+		// all the current epoch identities
+		encodableSnapshot.Identities,
+		// and all the NEW identities in next epoch, with 0 weight
+		nextEpochIdentities.
+			Filter(filter.Not(filter.In(encodableSnapshot.Identities))).
+			Map(mapfunc.WithStake(0))...,
+	)
+
+	return inmem.SnapshotFromEncodable(encodableSnapshot)
 }
