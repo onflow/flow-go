8795dca9d8d05d05b91e02e0e1a2de348dc836a4
Yurii Oleksyshyn <yuraolex@gmail.com>
2021-03-31 20:28:59 +0300

    Added ApprovalCollector. Added basic implementation

--- engine/consensus/approvals/approval_collector.go
+++ engine/consensus/approvals/approval_collector.go
@@ -1,1 +1,32 @@
 package approvals
+
+import (
+	"fmt"
+	"github.com/onflow/flow-go/engine"
+	"github.com/onflow/flow-go/model/chunks"
+	"github.com/onflow/flow-go/model/flow"
+)
+
+type ApprovalCollector struct {
+	incorporatedBlockID           flow.Identifier
+	chunkCollectors               map[uint64]*ChunkApprovalCollector
+	assignmentAuthorizedVerifiers map[flow.Identifier]struct{}
+}
+
+func (c *ApprovalCollector) ProcessApproval(approval *flow.ResultApproval) error {
+	chunkIndex := approval.Body.ChunkIndex
+	collector, ok := c.chunkCollectors[chunkIndex]
+	if !ok {
+		return engine.NewInvalidInputErrorf("approval collector chunk index out of range: %v", chunkIndex)
+	}
+
+	err := collector.ProcessApproval(approval)
+	if err != nil {
+		return fmt.Errorf("could not process approval: %w", err)
+	}
+	return nil
+}
+
+func NewApprovalCollector(assignment chunks.Assignment, ...) *ApprovalCollector {
+
+}
