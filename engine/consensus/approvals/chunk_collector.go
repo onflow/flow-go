78653874f3d51ce44c4a6c8f0c3d7f5b52d0b875
Yurii Oleksyshyn <yuraolex@gmail.com>
2021-03-31 20:22:33 +0300

    Added chunk and approvals collectors. Added basic implementation for chunk collector

---
+++ engine/consensus/approvals/chunk_collector.go
@@ -0,0 +1,47 @@
+package approvals
+
+import (
+	"sync"
+
+	"github.com/onflow/flow-go/model/flow"
+)
+
+type ChunkProcessingStatus struct {
+	numberOfApprovals uint
+	approvalProcessed bool
+}
+
+// ChunkApprovalCollector implements logic for checking chunks against assignments as
+// well as accumulating signatures of already checked approvals.
+type ChunkApprovalCollector struct {
+	assignment          map[flow.Identifier]struct{}
+	authorizedVerifiers map[flow.Identifier]struct{}
+	chunkApprovals      flow.SignatureCollector
+	lock                sync.Mutex // lock to protect `chunkApprovals`
+}
+
+// ProcessApproval performs processing and bookkeeping of single approval
+func (c *ChunkApprovalCollector) ProcessApproval(approval *flow.ResultApproval) error {
+	approverID := approval.Body.ApproverID
+	if _, ok := c.assignment[approverID]; !ok {
+		return nil
+	}
+	if _, ok := c.authorizedVerifiers[approverID]; !ok {
+		return nil
+	}
+
+	c.lock.Lock()
+	defer c.lock.Unlock()
+	c.chunkApprovals.Add(approverID, approval.Body.AttestationSignature)
+	return nil
+}
+
+func NewChunkApprovalCollector(assignment map[flow.Identifier]struct{},
+	authorizedVerifiers map[flow.Identifier]struct{}) *ChunkApprovalCollector {
+	return &ChunkApprovalCollector{
+		assignment:          assignment,
+		authorizedVerifiers: authorizedVerifiers,
+		chunkApprovals:      flow.SignatureCollector{},
+		lock:                sync.Mutex{},
+	}
+}
