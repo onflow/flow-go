ef02bce16d89fc1d97efe930172a38a79ace97da
Yahya <yahya@dapperlabs.com>
2021-04-01 13:07:34 -0700

    moves sample targets out of chunk status

--- engine/verification/fetcher/requester.go
+++ engine/verification/fetcher/requester.go
@@ -2,13 +2,12 @@ package fetcher
 
 import (
 	"github.com/onflow/flow-go/model/flow"
-	"github.com/onflow/flow-go/model/flow/filter"
 )
 
 // ChunkDataPackRequester encapsulates the logic of requesting a chunk data pack from an execution node.
 type ChunkDataPackRequester interface {
 	// Request makes the request of chunk data pack for the specified chunk ID from the target IDs.
-	Request(request *ChunkDataPackRequest)
+	Request(request *ChunkDataPackRequest, targets flow.IdentityList)
 }
 
 // ChunkDataPackHandler encapsulates the logic of handling a requested chunk data pack upon its arrival.
@@ -32,22 +31,4 @@ type ChunkDataPackRequest struct {
 	Height    uint64            // block height of execution result of the chunk, used to drop chunk requests of sealed heights.
 	Agrees    []flow.Identifier // execution node ids that generated the result of chunk.
 	Disagrees []flow.Identifier // execution node ids that generated a conflicting result with result of chunk.
-}
-
-// SampleTargets returns identifier of execution nodes that can be asked for the chunk data pack, based on
-// the agree and disagree execution nodes of the chunk data pack request.
-func (c ChunkDataPackRequest) SampleTargets(all flow.IdentityList, count int) flow.IdentifierList {
-	// if there are enough receipts produced the same result (agrees), we sample from them.
-	if len(c.Agrees) >= count {
-		return all.Filter(filter.HasNodeID(c.Agrees...)).Sample(uint(count)).NodeIDs()
-	}
-
-	// since there is at least one agree, then usually, we just need `count - 1` extra nodes as backup.
-	// We pick these extra nodes randomly from the rest nodes who we haven't received its receipt.
-	// In the case where all other execution nodes has produced different results, then we will only
-	// fetch from the one produced the same result (the only agree)
-	need := uint(count - len(c.Agrees))
-
-	nonResponders := all.Filter(filter.Not(filter.HasNodeID(c.Disagrees...))).Sample(need).NodeIDs()
-	return append(c.Agrees, nonResponders...)
 }
