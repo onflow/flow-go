7feb724eae126e5e123deef9a76258205cd27767
Yurii Oleksyshyn <yuraolex@gmail.com>
2021-04-05 22:55:22 +0300

    Added collecting logic of collecting aggregated signature

--- engine/consensus/approvals/approval_collector.go
+++ engine/consensus/approvals/approval_collector.go
@@ -4,25 +4,19 @@ import (
 	"github.com/onflow/flow-go/engine"
 	"github.com/onflow/flow-go/model/chunks"
 	"github.com/onflow/flow-go/model/flow"
+	"sync"
 )
 
 type ApprovalCollector struct {
-	incorporatedBlockID           flow.Identifier
-	chunkCollectors               []*ChunkApprovalCollector
-	assignmentAuthorizedVerifiers map[flow.Identifier]struct{}
+	incorporatedBlockID                  flow.Identifier
+	chunkCollectors                      []*ChunkApprovalCollector
+	assignmentAuthorizedVerifiers        map[flow.Identifier]struct{}
+	requiredApprovalsForSealConstruction uint                       // min number of approvals required for constructing a candidate seal
+	aggregatedSignatures                 []flow.AggregatedSignature // aggregated signature for each chunk
+	lock                                 sync.RWMutex               // lock for modifying aggregatedSignatures
 }
 
-func (c *ApprovalCollector) ProcessApproval(approval *flow.ResultApproval) error {
-	chunkIndex := approval.Body.ChunkIndex
-	if chunkIndex >= uint64(len(c.chunkCollectors)) {
-		return engine.NewInvalidInputErrorf("approval collector chunk index out of range: %v", chunkIndex)
-	}
-	collector := c.chunkCollectors[chunkIndex]
-	_ = collector.ProcessApproval(approval)
-	return nil
-}
-
-func NewApprovalCollector(result *flow.IncorporatedResult, assignment *chunks.Assignment, authorizedVerifiers map[flow.Identifier]struct{}) *ApprovalCollector {
+func NewApprovalCollector(result *flow.IncorporatedResult, assignment *chunks.Assignment, authorizedVerifiers map[flow.Identifier]struct{}, requiredApprovalsForSealConstruction uint) *ApprovalCollector {
 	chunkCollectors := make([]*ChunkApprovalCollector, 0, result.Result.Chunks.Len())
 	for _, chunk := range result.Result.Chunks {
 		chunkAssignment := make(map[flow.Identifier]struct{})
@@ -33,8 +27,45 @@ func NewApprovalCollector(result *flow.IncorporatedResult, assignment *chunks.As
 		chunkCollectors = append(chunkCollectors, collector)
 	}
 	return &ApprovalCollector{
-		incorporatedBlockID:           result.IncorporatedBlockID,
-		chunkCollectors:               chunkCollectors,
-		assignmentAuthorizedVerifiers: authorizedVerifiers,
+		incorporatedBlockID:                  result.IncorporatedBlockID,
+		chunkCollectors:                      chunkCollectors,
+		assignmentAuthorizedVerifiers:        authorizedVerifiers,
+		requiredApprovalsForSealConstruction: requiredApprovalsForSealConstruction,
 	}
+}
+
+func (c *ApprovalCollector) hasEnoughApprovals(chunkIndex uint64) bool {
+	c.lock.RLock()
+	defer c.lock.RUnlock()
+	return uint(c.aggregatedSignatures[chunkIndex].NumberSigners()) >= c.requiredApprovalsForSealConstruction
+}
+
+func (c *ApprovalCollector) ProcessApproval(approval *flow.ResultApproval) error {
+	chunkIndex := approval.Body.ChunkIndex
+	if chunkIndex >= uint64(len(c.chunkCollectors)) {
+		return engine.NewInvalidInputErrorf("approval collector chunk index out of range: %v", chunkIndex)
+	}
+	// there is no need to process approval if we have already enough info for sealing
+	if c.hasEnoughApprovals(chunkIndex) {
+		return nil
+	}
+
+	collector := c.chunkCollectors[chunkIndex]
+	status := collector.ProcessApproval(approval)
+	if status.numberOfApprovals >= c.requiredApprovalsForSealConstruction {
+		c.collectAggregatedSignature(chunkIndex, collector)
+	}
+
+	return nil
+}
+
+func (c *ApprovalCollector) collectAggregatedSignature(chunkIndex uint64, collector *ChunkApprovalCollector) {
+	if c.hasEnoughApprovals(chunkIndex) {
+		return
+	}
+
+	aggregatedSignature := collector.GetAggregatedSignature()
+	c.lock.Lock()
+	defer c.lock.Unlock()
+	c.aggregatedSignatures[chunkIndex] = aggregatedSignature
 }
