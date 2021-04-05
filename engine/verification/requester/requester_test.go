1ad5b151a27f1722f7a97759c4733abe2e3c7e55
Yahya <yahya@dapperlabs.com>
2021-04-05 14:48:20 -0700

    adds happy path test for requester on handling chunk data packs

--- engine/verification/requester/requester_test.go
+++ engine/verification/requester/requester_test.go
@@ -9,9 +9,9 @@ import (
 	"github.com/stretchr/testify/require"
 
 	"github.com/onflow/flow-go/engine"
-	"github.com/onflow/flow-go/engine/verification/fetcher"
 	mockfetcher "github.com/onflow/flow-go/engine/verification/fetcher/mock"
 	"github.com/onflow/flow-go/model/flow"
+	"github.com/onflow/flow-go/model/verification"
 	mempool "github.com/onflow/flow-go/module/mempool/mock"
 	"github.com/onflow/flow-go/module/mock"
 	"github.com/onflow/flow-go/network/mocknetwork"
@@ -24,11 +24,11 @@ import (
 type RequesterEngineTestSuite struct {
 	// modules
 	log             zerolog.Logger
-	handler         fetcher.ChunkDataPackHandler // contains callbacks for handling received chunk data packs.
-	retryInterval   time.Duration                // determines time in milliseconds for retrying chunk data requests.
-	pendingRequests mempool.ChunkRequests        // used to store all the pending chunks that assigned to this node
-	state           *protocol.State              // used to check the last sealed height
-	con             *mocknetwork.Conduit         // used to send chunk data request, and receive the response
+	handler         *mockfetcher.ChunkDataPackHandler // contains callbacks for handling received chunk data packs.
+	retryInterval   time.Duration                     // determines time in milliseconds for retrying chunk data requests.
+	pendingRequests *mempool.ChunkRequests            // used to store all the pending chunks that assigned to this node
+	state           *protocol.State                   // used to check the last sealed height
+	con             *mocknetwork.Conduit              // used to send chunk data request, and receive the response
 
 	// identities
 	verIdentity *flow.Identity // verification node
@@ -40,7 +40,7 @@ func setupTest() *RequesterEngineTestSuite {
 		log:             unittest.Logger(),
 		handler:         &mockfetcher.ChunkDataPackHandler{},
 		retryInterval:   100 * time.Millisecond,
-		pendingRequests: mempool.ChunkRequests{},
+		pendingRequests: &mempool.ChunkRequests{},
 		state:           &protocol.State{},
 		verIdentity:     unittest.IdentityFixture(unittest.WithRole(flow.RoleVerification)),
 	}
@@ -56,10 +56,32 @@ func newRequesterEngine(t *testing.T, s *RequesterEngineTestSuite) *Engine {
 		Return(s.con, nil).
 		Once()
 
-	e, err := New(s.log, s.state, net, s.retryInterval, s.handler)
+	e, err := New(s.log, s.state, net, s.retryInterval, s.pendingRequests, s.handler)
 	require.NoError(t, err)
 
 	testifymock.AssertExpectationsForObjects(t, net)
 
 	return e
+}
+
+// TestHandleChunkDataPack_HappyPath evaluates the happy path of receiving a requested chunk data pack.
+// The chunk data pack should be passed to the registered handler, and the resources should be cleaned up.
+func TestHandleChunkDataPack_HappyPath(t *testing.T) {
+	s := setupTest()
+	e := newRequesterEngine(t, s)
+
+	response := unittest.ChunkDataResponseFixture()
+	originID := unittest.IdentifierFixture()
+
+	// we have a request pending for this response chunk ID
+	s.pendingRequests.On("ByID", response.ChunkDataPack.ChunkID).Return(&verification.ChunkRequestStatus{}, true).Once()
+	// we remove pending request on receiving this response
+	s.pendingRequests.On("Rem", response.ChunkDataPack.ChunkID).Return(true).Once()
+
+	s.handler.On("HandleChunkDataPack", originID, response.ChunkDataPack, response.Collection).Return().Once()
+
+	err := e.Process(originID, response)
+	require.Nil(t, err)
+
+	testifymock.AssertExpectationsForObjects(t, s.pendingRequests, s.con, s.handler)
 }
