9e28b6d21c86c1bec28da5db76f4065b322735d9
Janez Podhostnik <67895329+janezpodhostnik@users.noreply.github.com>
2021-04-07 17:12:55 +0200

    Turn on storage limitig for mainnet (#599)

--- cmd/scaffold.go
+++ cmd/scaffold.go
@@ -543,12 +543,12 @@ func (fnb *FlowNodeBuilder) initFvmOptions() {
 	vmOpts := []fvm.Option{
 		fvm.WithChain(fnb.RootChainID.Chain()),
 		fvm.WithBlocks(blockFinder),
+		fvm.WithAccountStorageLimit(true),
 	}
 	if fnb.RootChainID == flow.Testnet {
 		vmOpts = append(vmOpts,
 			fvm.WithRestrictedAccountCreation(false),
 			fvm.WithRestrictedDeployment(false),
-			fvm.WithAccountStorageLimit(true),
 		)
 	}
 	fnb.FvmOptions = vmOpts
