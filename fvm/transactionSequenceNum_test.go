a9456909bad4cfe3d2a20a03d5a95df8b7c48d32
Ramtin Mehdizadeh Seraj <ramtin@axiomzen.co>
2021-04-08 14:07:40 -0700

    don't handle tx invocation failures at computer

---
+++ fvm/transactionSequenceNum_test.go
@@ -0,0 +1,17 @@
+package fvm_test
+
+import (
+	"testing"
+)
+
+func TestTransactionSequenceNumProcess(t *testing.T) {
+	t.Run("invalid sequence number", func(t *testing.T) {
+
+	})
+	t.Run("invalid signature", func(t *testing.T) {
+
+	})
+	t.Run("sequence number update", func(t *testing.T) {
+
+	})
+}
