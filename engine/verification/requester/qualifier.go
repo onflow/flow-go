eafc48b0d5468f793bd956cdfe55eb289e2fd722
Yahya <yahya@dapperlabs.com>
2021-04-20 10:08:39 -0700

    adds godoc for qualifier

--- engine/verification/requester/qualifier.go
+++ engine/verification/requester/qualifier.go
@@ -4,7 +4,15 @@ import (
 	"github.com/onflow/flow-go/model/verification"
 )
 
+// ChunkDataRequestQualifier encapsulates the logic of deciding whether to dispatch a chunk data request
+// or to hold on it.
+// It also encapsulates the bookkeeping logic of dispatched chunk data requests.
 type ChunkDataRequestQualifier interface {
-	CanTry(status verification.ChunkRequestStatus) bool
-	UpdateStatus(status *verification.ChunkDataPackRequest) bool
+	// CanDispatchRequest returns true if the chunk request can be dispatched to the network, otherwise
+	// it returns false.
+	CanDispatchRequest(request verification.ChunkRequestStatus) bool
+
+	// OnRequestDispatched encapsulates the bookkeeping logic after dispatching the chunk request
+	// is done successfully.
+	OnRequestDispatched(request *verification.ChunkDataPackRequest) bool
 }
