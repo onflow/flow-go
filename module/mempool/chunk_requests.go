7df540c3fa426180e8690fd76fb8418ad4f672b6
Yahya <yahya@dapperlabs.com>
2021-04-20 17:15:49 -0700

    refactors chunk requests interface to only deal with chunk data requests

--- module/mempool/chunk_requests.go
+++ module/mempool/chunk_requests.go
@@ -5,29 +5,30 @@ import (
 	"github.com/onflow/flow-go/model/verification"
 )
 
-// ChunkRequests is an in-memory storage for maintaining chunk requests data objects.
+// ChunkRequests is an in-memory storage for maintaining chunk data pack requests.
 type ChunkRequests interface {
-	// ByID returns a chunk request status by its chunk ID.
+	// ByID returns a chunk request by its chunk ID.
 	// There is a one-to-one correspondence between the chunk requests in memory, and
 	// their chunk ID.
-	ByID(chunkID flow.Identifier) (*verification.ChunkRequestStatus, bool)
+	ByID(chunkID flow.Identifier) (*verification.ChunkDataPackRequest, bool)
 
 	// Add provides insertion functionality into the memory pool.
 	// The insertion is only successful if there is no duplicate chunk request with the same
 	// chunk ID in the memory. Otherwise, it aborts the insertion and returns false.
-	Add(request *verification.ChunkRequestStatus) bool
+	Add(request *verification.ChunkDataPackRequest) bool
 
 	// Rem provides deletion functionality from the memory pool.
 	// If there is a chunk request with this ID, Rem removes it and returns true.
 	// Otherwise it returns false.
 	Rem(chunkID flow.Identifier) bool
 
-	// IncrementAttempt increments the Attempt field of the chunk request in memory pool that
-	// has the specified chunk ID. If such chunk ID does not exist in the memory pool, it returns
-	// false.
+	// IncrementAttempt increments the Attempt field of the corresponding status of the
+	// chunk request in memory pool that has the specified chunk ID.
+	// If such chunk ID does not exist in the memory pool, it returns false.
+	//
 	// The increments are done atomically, thread-safe, and in isolation.
 	IncrementAttempt(chunkID flow.Identifier) bool
 
 	// All returns all chunk requests stored in this memory pool.
-	All() []*verification.ChunkRequestStatus
+	All() []*verification.ChunkDataPackRequest
 }
--- module/mempool/stdmap/chunk_requests.go
+++ module/mempool/stdmap/chunk_requests.go
@@ -9,6 +9,10 @@ import (
 )
 
 // ChunkRequests is an implementation of in-memory storage for maintaining chunk requests data objects.
+//
+// In this implementation, the ChunkRequests
+// wraps the ChunkDataPackRequests around an internal ChunkRequestStatus data object, and maintains the wrapped
+// version in memory.
 type ChunkRequests struct {
 	*Backend
 }
@@ -27,23 +31,26 @@ func chunkRequestStatus(entity flow.Entity) *verification.ChunkRequestStatus {
 	return chunk
 }
 
-// ByID returns a chunk request status by its chunk ID.
+// ByID returns a chunk request by its chunk ID.
 // There is a one-to-one correspondence between the chunk requests in memory, and
 // their chunk ID.
-func (cs *ChunkRequests) ByID(chunkID flow.Identifier) (*verification.ChunkRequestStatus, bool) {
+func (cs *ChunkRequests) ByID(chunkID flow.Identifier) (*verification.ChunkDataPackRequest, bool) {
 	entity, exists := cs.Backend.ByID(chunkID)
 	if !exists {
 		return nil, false
 	}
 	request := chunkRequestStatus(entity)
-	return request, true
+	return request.ChunkDataPackRequest, true
 }
 
 // Add provides insertion functionality into the memory pool.
 // The insertion is only successful if there is no duplicate chunk request with the same
 // chunk ID in the memory. Otherwise, it aborts the insertion and returns false.
-func (cs *ChunkRequests) Add(request *verification.ChunkRequestStatus) bool {
-	return cs.Backend.Add(request)
+func (cs *ChunkRequests) Add(request *verification.ChunkDataPackRequest) bool {
+	status := verification.ChunkRequestStatus{
+		ChunkDataPackRequest: request,
+	}
+	return cs.Backend.Add(status)
 }
 
 // Rem provides deletion functionality from the memory pool.
@@ -53,9 +60,10 @@ func (cs *ChunkRequests) Rem(chunkID flow.Identifier) bool {
 	return cs.Backend.Rem(chunkID)
 }
 
-// IncrementAttempt increments the Attempt field of the chunk request in memory pool that
-// has the specified chunk ID. If such chunk ID does not exist in the memory pool, it returns
-// false.
+// IncrementAttempt increments the Attempt field of the corresponding status of the
+// chunk request in memory pool that has the specified chunk ID.
+// If such chunk ID does not exist in the memory pool, it returns false.
+//
 // The increments are done atomically, thread-safe, and in isolation.
 func (cs *ChunkRequests) IncrementAttempt(chunkID flow.Identifier) bool {
 	err := cs.Backend.Run(func(backdata map[flow.Identifier]flow.Entity) error {
@@ -73,12 +81,12 @@ func (cs *ChunkRequests) IncrementAttempt(chunkID flow.Identifier) bool {
 }
 
 // All returns all chunk requests stored in this memory pool.
-func (cs *ChunkRequests) All() []*verification.ChunkRequestStatus {
+func (cs *ChunkRequests) All() []*verification.ChunkDataPackRequest {
 	all := cs.Backend.All()
-	requests := make([]*verification.ChunkRequestStatus, 0, len(all))
+	requests := make([]*verification.ChunkDataPackRequest, 0, len(all))
 	for _, entity := range all {
-		chunk := chunkRequestStatus(entity)
-		requests = append(requests, chunk)
+		status := chunkRequestStatus(entity)
+		requests = append(requests, status.ChunkDataPackRequest)
 	}
 	return requests
 }
