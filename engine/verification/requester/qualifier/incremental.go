c5367c96184969e877d05a42e86dbcb26241358e
Yahya <yahya@dapperlabs.com>
2021-04-23 15:43:42 -0700

    adds qualifier functions

--- engine/verification/requester/qualifier/incremental.go
+++ engine/verification/requester/qualifier/incremental.go
@@ -1,28 +1,55 @@
 package qualifier
 
 import (
+	"time"
+
 	"github.com/onflow/flow-go/model/flow"
 	"github.com/onflow/flow-go/module/mempool"
 )
 
-// IncrementalQualifier always qualifies a request for dispatching. On request getting dispatched, it
+// ChunkDataRequestQualifier always qualifies a request for dispatching. On request getting dispatched, it
 // increments the attempt field of request on its underlying mempool.
-type IncrementalQualifier struct {
+type ChunkDataRequestQualifier struct {
 	pendingRequests mempool.ChunkRequests // used to track requested chunks.
+	qualifierFunc   RequestQualifierFunc
 }
 
-func NewIncrementalQualifier(requests mempool.ChunkRequests) *IncrementalQualifier {
-	return &IncrementalQualifier{pendingRequests: requests}
+func NewIncrementalQualifier(requests mempool.ChunkRequests) *ChunkDataRequestQualifier {
+	return &ChunkDataRequestQualifier{pendingRequests: requests}
 }
 
-// CanDispatchRequest always returns true. Its sole purpose is to satisfy the interface implementation.
-func (i *IncrementalQualifier) CanDispatchRequest(chunkID flow.Identifier) bool {
-	return true
+// CanDispatchRequest returns true if the chunk request can be dispatched to the network, otherwise
+// it returns false.
+func (i ChunkDataRequestQualifier) CanDispatchRequest(chunkID flow.Identifier) bool {
+	attempts, lastAttempt, retryAfter, exists := i.pendingRequests.RequestInfo(chunkID)
+	if !exists {
+		return false
+	}
+
+	return i.qualifierFunc(attempts, lastAttempt, retryAfter)
 }
 
 // OnRequestDispatched encapsulates the bookkeeping logic after dispatching the chunk request
 // is done successfully. On request getting dispatched, it
 // increments the attempt field of request on its underlying mempool.
-func (i *IncrementalQualifier) OnRequestDispatched(chunkID flow.Identifier) bool {
+func (i *ChunkDataRequestQualifier) OnRequestDispatched(chunkID flow.Identifier) bool {
 	return i.pendingRequests.IncrementAttempt(chunkID)
+}
+
+type RequestQualifierFunc func(uint64, time.Time, time.Duration) bool
+
+func MaxAttemptQualifier(maxAttempts uint64) RequestQualifierFunc {
+	return func(attempts uint64, _ time.Time, _ time.Duration) bool {
+		return attempts < maxAttempts
+	}
+}
+
+func RetryAfterQualifier() RequestQualifierFunc {
+	return func(_ uint64, lastAttempt time.Time, retryAfter time.Duration) bool {
+		cutoff := lastAttempt.Add(retryAfter)
+		if cutoff.After(time.Now()) {
+			return true
+		}
+		return false
+	}
 }
