160c9d385603d0fc2d723c8262ddc5583fd2694d
Ramtin Mehdizadeh Seraj <ramtin@axiomzen.co>
2021-04-24 11:41:13 -0700

    make exec state extract test less randomized

--- cmd/util/cmd/execution-state-extract/execution_state_extract_test.go
+++ cmd/util/cmd/execution-state-extract/execution_state_extract_test.go
@@ -1,6 +1,7 @@
 package extract
 
 import (
+	"crypto/rand"
 	"path"
 	"testing"
 
@@ -10,7 +11,6 @@ import (
 	"github.com/onflow/flow-go/cmd/util/cmd/common"
 	"github.com/onflow/flow-go/ledger"
 	"github.com/onflow/flow-go/ledger/common/pathfinder"
-	"github.com/onflow/flow-go/ledger/common/utils"
 	"github.com/onflow/flow-go/ledger/complete"
 	"github.com/onflow/flow-go/ledger/complete/wal"
 	"github.com/onflow/flow-go/model/bootstrap"
@@ -73,8 +73,6 @@ func TestExtractExecutionState(t *testing.T) {
 
 			// generate some oldLedger data
 			size := 10
-			keyMaxByteSize := 64
-			valueMaxByteSize := 1024
 
 			diskWal, err := wal.NewDiskWAL(zerolog.Nop(), nil, metrics.NewNoopCollector(), execdir, size, pathfinder.PathByteSize, wal.SegmentSize)
 			require.NoError(t, err)
@@ -89,11 +87,7 @@ func TestExtractExecutionState(t *testing.T) {
 			blocksInOrder := make([]flow.Identifier, size)
 
 			for i := 0; i < size; i++ {
-				//keys := utils.GetRandomRegisterIDs(4)
-				//values := utils.GetRandomValues(len(keys), 10, valueMaxByteSize)
-
-				keys := utils.RandomUniqueKeys(4, 3, 10, keyMaxByteSize)
-				values := utils.RandomValues(len(keys), 10, valueMaxByteSize)
+				keys, values := getSampleKeyValues(i)
 
 				update, err := ledger.NewUpdate(stateCommitment, keys, values)
 				require.NoError(t, err)
@@ -182,6 +176,81 @@ func TestExtractExecutionState(t *testing.T) {
 			}
 		})
 	})
+}
+
+func getSampleKeyValues(i int) ([]ledger.Key, []ledger.Value) {
+	switch i {
+	case 0:
+		return []ledger.Key{getKey("", "", "uuid"), getKey("", "", "account_address_state")},
+			[]ledger.Value{[]byte{'1'}, []byte{'A'}}
+	case 1:
+		return []ledger.Key{getKey("ADDRESS", "ADDRESS", "public_key_count"),
+				getKey("ADDRESS", "ADDRESS", "public_key_0"),
+				getKey("ADDRESS", "", "exists"),
+				getKey("ADDRESS", "", "storage_used")},
+			[]ledger.Value{[]byte{1}, []byte("PUBLICKEYXYZ"), []byte{1}, []byte{100}}
+	case 2:
+		// TODO change the contract_names to CBOR encoding
+		return []ledger.Key{getKey("ADDRESS", "ADDRESS", "contract_names"), getKey("ADDRESS", "ADDRESS", "code.mycontract")},
+			[]ledger.Value{[]byte("mycontract"), []byte("CONTRACT Content")}
+	default:
+		keys := make([]ledger.Key, 0)
+		values := make([]ledger.Value, 0)
+		for j := 0; j < 10; j++ {
+			address := make([]byte, 32)
+			rand.Read(address)
+			keys = append(keys, getKey(string(address), "", "test"))
+			values = append(values, getRandomCadenceValue())
+		}
+		return keys, values
+	}
+}
+
+func getKey(owner, controller, key string) ledger.Key {
+	return ledger.Key{KeyParts: []ledger.KeyPart{
+		{Type: uint16(0), Value: []byte(owner)},
+		{Type: uint16(1), Value: []byte(controller)},
+		{Type: uint16(2), Value: []byte(key)},
+	},
+	}
+}
+
+func getRandomCadenceValue() ledger.Value {
+
+	randomPart := make([]byte, 10)
+	rand.Read(randomPart)
+
+	valueBytes := []byte{
+		// magic prefix
+		0x0, 0xca, 0xde, 0x0, 0x4,
+		// tag
+		0xd8, 132,
+		// array, 5 items follow
+		0x85,
+
+		// tag
+		0xd8, 193,
+		// UTF-8 string, length 4
+		0x64,
+		// t, e, s, t
+		0x74, 0x65, 0x73, 0x74,
+
+		// nil
+		0xf6,
+
+		// positive integer 1
+		0x1,
+
+		// array, 0 items follow
+		0x80,
+
+		// UTF-8 string, length 10
+		0x6a,
+		0x54, 0x65, 0x73, 0x74, 0x53, 0x74, 0x72, 0x75, 0x63, 0x74,
+	}
+
+	valueBytes = append(valueBytes, randomPart...)
+	return ledger.Value(valueBytes)
 }
 
 func withDirs(t *testing.T, f func(datadir, execdir, outdir string)) {
