d1a3d4d40d635a07ec01e157e909fcb86f16e2ef
Tarak Ben Youssef <tarak.benyoussef@dapperlabs.com>
2021-04-30 14:23:10 -0700

    add a hash len sanity test

--- ledger/common/hash/hash_test.go
+++ ledger/common/hash/hash_test.go
@@ -12,44 +12,14 @@ import (
 	"github.com/onflow/flow-go/ledger/common/hash"
 )
 
-// Test_GetDefaultHashForHeight tests getting default hash for given heights
-func Test_GetDefaultHashForHeight(t *testing.T) {
-	hasher := cryhash.NewSHA3_256()
-	defaultLeafHash := hasher.ComputeHash([]byte("default:"))
-	expected := ledger.GetDefaultHashForHeight(0)
-	assert.Equal(t, []byte(expected[:]), []byte(defaultLeafHash))
-
-	l1 := hash.HashInterNode(ledger.GetDefaultHashForHeight(0), ledger.GetDefaultHashForHeight(0))
-	assert.Equal(t, l1, ledger.GetDefaultHashForHeight(1))
-
-	l2 := hash.HashInterNode(l1, l1)
-	assert.Equal(t, l2, ledger.GetDefaultHashForHeight(2))
-}
-
-func Test_ComputeCompactValue(t *testing.T) {
-	v := []byte{'A'}
-
-	// 00000101...00000000
-	var path hash.Hash
-	path[0] = 5
-	nodeHeight := 251
-	h := hash.HashLeaf(path, v)
-	l := 0
-	// exclude last 3 level
-	for ; l < nodeHeight-3; l++ {
-		h = hash.HashInterNode(h, ledger.GetDefaultHashForHeight(l))
-	}
-	l1 := hash.HashInterNode(ledger.GetDefaultHashForHeight(l), h)
-	l2 := hash.HashInterNode(l1, ledger.GetDefaultHashForHeight(l+1))
-	l3 := hash.HashInterNode(ledger.GetDefaultHashForHeight(l+2), l2)
-	result := ledger.ComputeCompactValue(path, v, nodeHeight)
-	assert.Equal(t, l3, result)
-}
-
 func TestHash(t *testing.T) {
 	r := time.Now().UnixNano()
 	rand.Seed(r)
 	t.Logf("math rand seed is %d", r)
+
+	t.Run("lengthSanity", func(t *testing.T) {
+		assert.Equal(t, 32, hash.HashLen)
+	})
 
 	t.Run("HashLeaf", func(t *testing.T) {
 		var path hash.Hash
@@ -83,6 +53,40 @@ func TestHash(t *testing.T) {
 			assert.Equal(t, []byte(expected), []byte(h[:]))
 		}
 	})
+}
+
+// Test_GetDefaultHashForHeight tests getting default hash for given heights
+func Test_GetDefaultHashForHeight(t *testing.T) {
+	hasher := cryhash.NewSHA3_256()
+	defaultLeafHash := hasher.ComputeHash([]byte("default:"))
+	expected := ledger.GetDefaultHashForHeight(0)
+	assert.Equal(t, []byte(expected[:]), []byte(defaultLeafHash))
+
+	l1 := hash.HashInterNode(ledger.GetDefaultHashForHeight(0), ledger.GetDefaultHashForHeight(0))
+	assert.Equal(t, l1, ledger.GetDefaultHashForHeight(1))
+
+	l2 := hash.HashInterNode(l1, l1)
+	assert.Equal(t, l2, ledger.GetDefaultHashForHeight(2))
+}
+
+func Test_ComputeCompactValue(t *testing.T) {
+	v := []byte{'A'}
+
+	// 00000101...00000000
+	var path hash.Hash
+	path[0] = 5
+	nodeHeight := 251
+	h := hash.HashLeaf(path, v)
+	l := 0
+	// exclude last 3 level
+	for ; l < nodeHeight-3; l++ {
+		h = hash.HashInterNode(h, ledger.GetDefaultHashForHeight(l))
+	}
+	l1 := hash.HashInterNode(ledger.GetDefaultHashForHeight(l), h)
+	l2 := hash.HashInterNode(l1, ledger.GetDefaultHashForHeight(l+1))
+	l3 := hash.HashInterNode(ledger.GetDefaultHashForHeight(l+2), l2)
+	result := ledger.ComputeCompactValue(path, v, nodeHeight)
+	assert.Equal(t, l3, result)
 }
 
 func BenchmarkHash(b *testing.B) {
