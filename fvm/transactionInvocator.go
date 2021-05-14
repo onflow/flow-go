f8e6e0a5e8fdd6abe4700c08aac5c6a65c0fe6cf
Jordan Schalm <jordan@dapperlabs.com>
2021-05-13 17:23:55 -0700

    log transaction error

--- fvm/transactionInvocator.go
+++ fvm/transactionInvocator.go
@@ -169,6 +169,7 @@ func (i *TransactionInvocator) Process(
 		// if tx fails just do clean up
 		programs.Cleanup(nil)
 		i.logger.Info().
+			Err(txError).
 			Str("txHash", proc.ID.String()).
 			Uint64("blockHeight", blockHeight).
 			Uint64("ledgerInteractionUsed", sth.State().InteractionUsed()).
