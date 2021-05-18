fb641e9ac8c3b2fcaba8fcb744b2b8e058d03017
Yahya <yahya@dapperlabs.com>
2021-05-18 11:36:50 -0700

    removes checking handler fatal logs

--- engine/verification/fetcher/engine.go
+++ engine/verification/fetcher/engine.go
@@ -94,9 +94,6 @@ func (e *Engine) WithChunkConsumerNotifier(notifier module.ProcessingNotifier) {
 
 // Ready initializes the engine and returns a channel that is closed when the initialization is done
 func (e *Engine) Ready() <-chan struct{} {
-	if e.chunkConsumerNotifier == nil {
-		e.log.Fatal().Msg("missing chunk consumer notifier callback in verification fetcher engine")
-	}
 	return e.unit.Ready()
 }
 
