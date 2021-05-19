7cac16076010c6c05e6e710ebb2863a3495a8648
Jordan Schalm <jordan@dapperlabs.com>
2021-05-19 15:34:49 -0700

    Revert "rename refactored signer as mock"
    This reverts commit c7d9b45269e9506fc7749adab8b3c5a96352869d.

--- consensus/hotstuff/verification/mock_signer.go
+++ consensus/hotstuff/verification/mock_signer.go
@@ -5,27 +5,27 @@ import (
 	"github.com/onflow/flow-go/model/flow"
 )
 
-type MockSigner struct {
-	LocalID flow.Identifier
+type Signer struct {
+	localID flow.Identifier
 }
 
-func (*MockSigner) CreateProposal(block *model.Block) (*model.Proposal, error) {
+func (*Signer) CreateProposal(block *model.Block) (*model.Proposal, error) {
 	proposal := &model.Proposal{
 		Block:   block,
 		SigData: nil,
 	}
 	return proposal, nil
 }
-func (s *MockSigner) CreateVote(block *model.Block) (*model.Vote, error) {
+func (s *Signer) CreateVote(block *model.Block) (*model.Vote, error) {
 	vote := &model.Vote{
 		View:     block.View,
 		BlockID:  block.BlockID,
-		SignerID: s.LocalID,
+		SignerID: s.localID,
 		SigData:  nil,
 	}
 	return vote, nil
 }
-func (*MockSigner) CreateQC(votes []*model.Vote) (*flow.QuorumCertificate, error) {
+func (*Signer) CreateQC(votes []*model.Vote) (*flow.QuorumCertificate, error) {
 	voterIDs := make([]flow.Identifier, 0, len(votes))
 	for _, vote := range votes {
 		voterIDs = append(voterIDs, vote.SignerID)
@@ -39,10 +39,10 @@ func (*MockSigner) CreateQC(votes []*model.Vote) (*flow.QuorumCertificate, error
 	return qc, nil
 }
 
-func (*MockSigner) VerifyVote(voterID *flow.Identity, sigData []byte, block *model.Block) (bool, error) {
+func (*Signer) VerifyVote(voterID *flow.Identity, sigData []byte, block *model.Block) (bool, error) {
 	return true, nil
 }
 
-func (*MockSigner) VerifyQC(voters flow.IdentityList, sigData []byte, block *model.Block) (bool, error) {
+func (*Signer) VerifyQC(voters flow.IdentityList, sigData []byte, block *model.Block) (bool, error) {
 	return true, nil
 }
