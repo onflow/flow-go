05a9ef952cd53f66d09e77c2262c82463f2872f6
Jordan Schalm <jordan@dapperlabs.com>
2021-05-18 17:45:52 -0700

    programatically specify verbose unittest logging

--- utils/unittest/logging.go
+++ utils/unittest/logging.go
@@ -9,14 +9,14 @@ import (
 	"github.com/rs/zerolog"
 )
 
-var verbose = flag.Bool("vv", false, "print debugging logs")
+var LogVerbose = flag.Bool("vv", false, "print debugging logs")
 
 // Logger returns a zerolog
 // use -vv flag to print debugging logs for tests
 func Logger() zerolog.Logger {
 	writer := ioutil.Discard
 
-	if *verbose {
+	if *LogVerbose {
 		writer = os.Stderr
 	}
 	zerolog.TimestampFunc = func() time.Time { return time.Now().UTC() }
