fa03ba29345251340ed32564ac272924663f84da
danuio <me@danu.io>
2021-05-25 10:38:22 +0100

    Update tests

--- cmd/bootstrap/cmd/machine_account_key_test.go
+++ cmd/bootstrap/cmd/machine_account_key_test.go
@@ -3,7 +3,8 @@ package cmd
 import (
 	"fmt"
 	"os"
-	"path"
+
+	"path/filepath"
 	"regexp"
 	"strconv"
 	"strings"
@@ -37,26 +38,35 @@ func TestMachineAccountKeyFileExists(t *testing.T) {
 	hook.logs.Reset()
 
 	// require log regex to match
-	require.DirExists(t, path.Join(flagOutdir, bootstrap.DirnamePublicBootstrap))
-	require.DirExists(t, path.Join(flagOutdir, bootstrap.DirPrivateRoot))
+	require.DirExists(t, filepath.Join(flagOutdir, bootstrap.DirnamePublicBootstrap))
+	require.DirExists(t, filepath.Join(flagOutdir, bootstrap.DirPrivateRoot))
 
-	nodeIDPath := path.Join(flagOutdir, bootstrap.PathNodeID)
+	nodeIDPath := filepath.Join(flagOutdir, bootstrap.PathNodeID)
 	require.FileExists(t, nodeIDPath)
 	b, err := ioutils.ReadFile(nodeIDPath)
 	require.NoError(t, err)
 	nodeID := strings.TrimSpace(string(b))
 
 	// make sure file exists
-	machineKeyFilePath := path.Join(flagOutdir, fmt.Sprintf(model.PathNodeMachineAccountPrivateKey, nodeID))
+	machineKeyFilePath := filepath.Join(flagOutdir, fmt.Sprintf(model.PathNodeMachineAccountPrivateKey, nodeID))
 	require.FileExists(t, machineKeyFilePath)
 
-	// read file priv key file
+	// read file priv key file before command
+	var machineAccountPrivBefore model.NodeMachineAccountPriv
+	readJSON(machineKeyFilePath, &machineAccountPrivBefore)
 
 	// run command with flags
 	machineAccountKeyRun(nil, nil)
 
 	// make sure regex matches
 	require.Regexp(t, regex, hook.logs.String())
+
+	// read machine account key file again
+	var machineAccountPrivAfter model.NodeMachineAccountPriv
+	readJSON(machineKeyFilePath, &machineAccountPrivAfter)
+
+	// check if key was modified
+	assert.Equal(t, machineAccountPrivBefore, machineAccountPrivAfter)
 }
 
 func TestMachineAccountKeyFileCreated(t *testing.T) {
@@ -81,18 +91,18 @@ func TestMachineAccountKeyFileCreated(t *testing.T) {
 	hook.logs.Reset()
 
 	// require log regex to match
-	require.DirExists(t, path.Join(flagOutdir, bootstrap.DirnamePublicBootstrap))
-	require.DirExists(t, path.Join(flagOutdir, bootstrap.DirPrivateRoot))
+	require.DirExists(t, filepath.Join(flagOutdir, bootstrap.DirnamePublicBootstrap))
+	require.DirExists(t, filepath.Join(flagOutdir, bootstrap.DirPrivateRoot))
 
 	// read in node ID
-	nodeIDPath := path.Join(flagOutdir, bootstrap.PathNodeID)
+	nodeIDPath := filepath.Join(flagOutdir, bootstrap.PathNodeID)
 	require.FileExists(t, nodeIDPath)
 	b, err := ioutils.ReadFile(nodeIDPath)
 	require.NoError(t, err)
 	nodeID := strings.TrimSpace(string(b))
 
 	// delete machine account key file
-	machineKeyFilePath := path.Join(flagOutdir, fmt.Sprintf(model.PathNodeMachineAccountPrivateKey, nodeID))
+	machineKeyFilePath := filepath.Join(flagOutdir, fmt.Sprintf(model.PathNodeMachineAccountPrivateKey, nodeID))
 	err = os.Remove(machineKeyFilePath)
 	require.NoError(t, err)
 
