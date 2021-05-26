ad2c5e74c83f42808a314886c65b4cc5bcc1954d
danuio <me@danu.io>
2021-05-26 15:36:51 +0100

    Address PR comments

--- cmd/bootstrap/cmd/machine_account_test.go
+++ cmd/bootstrap/cmd/machine_account_test.go
@@ -5,10 +5,8 @@ import (
 
 	"path/filepath"
 	"regexp"
-	"strconv"
 	"strings"
 	"testing"
-	"time"
 
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
@@ -16,105 +14,108 @@ import (
 	"github.com/onflow/flow-go/model/bootstrap"
 	model "github.com/onflow/flow-go/model/bootstrap"
 	ioutils "github.com/onflow/flow-go/utils/io"
+	"github.com/onflow/flow-go/utils/unittest"
 )
 
 func TestMachineAccountHappyPath(t *testing.T) {
-	var machineAccountHappyPath = `^read machine account private key json` +
-		`encoded public machine account key` +
-		`wrote file /tmp/%s/private-root-information/private-node-info_\S+/node-machine-account-info.priv.json`
 
-	dirName := strconv.FormatInt(time.Now().UnixNano(), 10)
-	regex := regexp.MustCompile(fmt.Sprintf(machineAccountHappyPath, dirName))
+	unittest.RunWithTempDir(t, func(bootDir string) {
+		var machineAccountHappyPath = `^read machine account private key json` +
+			`encoded public machine account key` +
+			`wrote file %s/private-root-information/private-node-info_\S+/node-machine-account-info.priv.json`
+		regex := regexp.MustCompile(fmt.Sprintf(machineAccountHappyPath, bootDir))
 
-	// command flags
-	flagOutdir = fmt.Sprintf("/tmp/%s", dirName)
-	flagRole = "consensus"
-	flagAddress = "189.123.123.42:3869"
+		// command flags
+		flagOutdir = bootDir
+		flagRole = "consensus"
+		flagAddress = "189.123.123.42:3869"
 
-	hook := zeroLoggerHook{logs: &strings.Builder{}}
-	log = log.Hook(hook)
+		hook := zeroLoggerHook{logs: &strings.Builder{}}
+		log = log.Hook(hook)
 
-	// run keys command to generate all keys and bootstrap files
-	keyCmdRun(nil, nil)
-	hook.logs.Reset()
+		// run keys command to generate all keys and bootstrap files
+		keyCmdRun(nil, nil)
+		hook.logs.Reset()
 
-	// require log regex to match
-	require.DirExists(t, filepath.Join(flagOutdir, bootstrap.DirnamePublicBootstrap))
-	require.DirExists(t, filepath.Join(flagOutdir, bootstrap.DirPrivateRoot))
+		// require log regex to match
+		require.DirExists(t, filepath.Join(flagOutdir, bootstrap.DirnamePublicBootstrap))
+		require.DirExists(t, filepath.Join(flagOutdir, bootstrap.DirPrivateRoot))
 
-	// read in nodeID
-	nodeIDPath := filepath.Join(flagOutdir, bootstrap.PathNodeID)
-	require.FileExists(t, nodeIDPath)
-	b, err := ioutils.ReadFile(nodeIDPath)
-	require.NoError(t, err)
-	nodeID := strings.TrimSpace(string(b))
+		// read in nodeID
+		nodeIDPath := filepath.Join(flagOutdir, bootstrap.PathNodeID)
+		require.FileExists(t, nodeIDPath)
+		b, err := ioutils.ReadFile(nodeIDPath)
+		require.NoError(t, err)
+		nodeID := strings.TrimSpace(string(b))
 
-	// make sure key file exists (sanity check)
-	machineKeyFilePath := filepath.Join(flagOutdir, fmt.Sprintf(model.PathNodeMachineAccountPrivateKey, nodeID))
-	require.FileExists(t, machineKeyFilePath)
+		// make sure key file exists (sanity check)
+		machineKeyFilePath := filepath.Join(flagOutdir, fmt.Sprintf(model.PathNodeMachineAccountPrivateKey, nodeID))
+		require.FileExists(t, machineKeyFilePath)
 
-	// sanity check if machine account info file exists
-	machineInfoFilePath := filepath.Join(flagOutdir, fmt.Sprintf(model.PathNodeMachineAccountInfoPriv, nodeID))
-	require.NoFileExists(t, machineInfoFilePath)
+		// sanity check if machine account info file exists
+		machineInfoFilePath := filepath.Join(flagOutdir, fmt.Sprintf(model.PathNodeMachineAccountInfoPriv, nodeID))
+		require.NoFileExists(t, machineInfoFilePath)
 
-	// make sure regex matches and file was created
-	machineAccountRun(nil, nil)
-	require.Regexp(t, regex, hook.logs.String())
-	require.FileExists(t, machineInfoFilePath)
+		// make sure regex matches and file was created
+		machineAccountRun(nil, nil)
+		require.Regexp(t, regex, hook.logs.String())
+		require.FileExists(t, machineInfoFilePath)
+	})
 }
 
 func TestMachineAccountInfoFileExists(t *testing.T) {
-	var machineAccountInfoFileExistsRegex = `^node matching account info file already exists`
 
-	dirName := strconv.FormatInt(time.Now().UnixNano(), 10)
-	regex := regexp.MustCompile(machineAccountInfoFileExistsRegex)
+	unittest.RunWithTempDir(t, func(bootDir string) {
+		var machineAccountInfoFileExistsRegex = `^node matching account info file already exists`
+		regex := regexp.MustCompile(machineAccountInfoFileExistsRegex)
 
-	// command flags
-	flagOutdir = fmt.Sprintf("/tmp/%s", dirName)
-	flagRole = "consensus"
-	flagAddress = "189.123.123.42:3869"
+		// command flags
+		flagOutdir = bootDir
+		flagRole = "consensus"
+		flagAddress = "189.123.123.42:3869"
 
-	hook := zeroLoggerHook{logs: &strings.Builder{}}
-	log = log.Hook(hook)
+		hook := zeroLoggerHook{logs: &strings.Builder{}}
+		log = log.Hook(hook)
 
-	// run keys command to generate all keys and bootstrap files
-	keyCmdRun(nil, nil)
-	hook.logs.Reset()
+		// run keys command to generate all keys and bootstrap files
+		keyCmdRun(nil, nil)
+		hook.logs.Reset()
 
-	// require log regex to match
-	require.DirExists(t, filepath.Join(flagOutdir, bootstrap.DirnamePublicBootstrap))
-	require.DirExists(t, filepath.Join(flagOutdir, bootstrap.DirPrivateRoot))
+		// require log regex to match
+		require.DirExists(t, filepath.Join(flagOutdir, bootstrap.DirnamePublicBootstrap))
+		require.DirExists(t, filepath.Join(flagOutdir, bootstrap.DirPrivateRoot))
 
-	// read in nodeID
-	nodeIDPath := filepath.Join(flagOutdir, bootstrap.PathNodeID)
-	require.FileExists(t, nodeIDPath)
-	b, err := ioutils.ReadFile(nodeIDPath)
-	require.NoError(t, err)
-	nodeID := strings.TrimSpace(string(b))
+		// read in nodeID
+		nodeIDPath := filepath.Join(flagOutdir, bootstrap.PathNodeID)
+		require.FileExists(t, nodeIDPath)
+		b, err := ioutils.ReadFile(nodeIDPath)
+		require.NoError(t, err)
+		nodeID := strings.TrimSpace(string(b))
 
-	// make sure key file exists (sanity check)
-	machineKeyFilePath := filepath.Join(flagOutdir, fmt.Sprintf(model.PathNodeMachineAccountPrivateKey, nodeID))
-	require.FileExists(t, machineKeyFilePath)
+		// make sure key file exists (sanity check)
+		machineKeyFilePath := filepath.Join(flagOutdir, fmt.Sprintf(model.PathNodeMachineAccountPrivateKey, nodeID))
+		require.FileExists(t, machineKeyFilePath)
 
-	// sanity check if machine account info file exists
-	machineInfoFilePath := filepath.Join(flagOutdir, fmt.Sprintf(model.PathNodeMachineAccountInfoPriv, nodeID))
-	require.NoFileExists(t, machineInfoFilePath)
+		// sanity check if machine account info file exists
+		machineInfoFilePath := filepath.Join(flagOutdir, fmt.Sprintf(model.PathNodeMachineAccountInfoPriv, nodeID))
+		require.NoFileExists(t, machineInfoFilePath)
 
-	// run machine account to create info file
-	machineAccountRun(nil, nil)
-	require.FileExists(t, machineInfoFilePath)
-	hook.logs.Reset()
+		// run machine account to create info file
+		machineAccountRun(nil, nil)
+		require.FileExists(t, machineInfoFilePath)
+		hook.logs.Reset()
 
-	// read in info file
-	var machineAccountInfoBefore model.NodeMachineAccountInfo
-	readJSON(machineInfoFilePath, &machineAccountInfoBefore)
+		// read in info file
+		var machineAccountInfoBefore model.NodeMachineAccountInfo
+		readJSON(machineInfoFilePath, &machineAccountInfoBefore)
 
-	// run again and make sure info file was not changed
-	machineAccountRun(nil, nil)
-	require.Regexp(t, regex, hook.logs.String())
+		// run again and make sure info file was not changed
+		machineAccountRun(nil, nil)
+		require.Regexp(t, regex, hook.logs.String())
 
-	var machineAccountInfoAfter model.NodeMachineAccountInfo
-	readJSON(machineInfoFilePath, &machineAccountInfoAfter)
+		var machineAccountInfoAfter model.NodeMachineAccountInfo
+		readJSON(machineInfoFilePath, &machineAccountInfoAfter)
 
-	assert.Equal(t, machineAccountInfoBefore, machineAccountInfoAfter)
+		assert.Equal(t, machineAccountInfoBefore, machineAccountInfoAfter)
+	})
 }
