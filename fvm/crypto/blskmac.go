0d2cae3d0ff8f5801fa46f1130fa4428190b98d1
Tarak Ben Youssef <50252200+tarakby@users.noreply.github.com>
2021-06-08 15:40:56 -0700

    minor edit

--- fvm/crypto/blskmac.go
+++ fvm/crypto/blskmac.go
@@ -5,5 +5,5 @@ package crypto
 import "github.com/onflow/flow-go/crypto/hash"
 
 func NewBLSKMAC(_ string) hash.Hasher {
-	panic("BLSKMAC not supported when flow-go is build without relic")
+	panic("BLSKMAC not supported when flow-go is built without relic")
 }
