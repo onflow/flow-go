35686c1b37b643d9cd2146788949044c56c8983c
Leo Zhang (zhangchiqing) <zhangchiqing@gmail.com>
2021-06-11 14:43:53 -0700

    fix error handling

--- engine/consensus/sealing/core.go
+++ engine/consensus/sealing/core.go
@@ -493,7 +493,7 @@ func (c *Core) ProcessFinalizedBlock(finalizedBlockID flow.Identifier) error {
 
 	err = c.sealsMempool.PruneUpToHeight(lastSealed.Height)
 	if err != nil {
-		return fmt.Errorf("could not prune seals mempool at block %v, by height: %v", finalizedBlockID, lastSealed.Height)
+		return fmt.Errorf("could not prune seals mempool at block %v, by height: %v: %w", finalizedBlockID, lastSealed.Height, err)
 	}
 
 	requestPendingApprovalsSpan := c.tracer.StartSpanFromParent(processFinalizedBlockSpan, trace.CONSealingRequestingPendingApproval)
