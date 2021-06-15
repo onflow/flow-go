3d20da950e25f4db0463fd4abf9e76cfd0644d95
Jordan Schalm <jordan@dapperlabs.com>
2021-06-15 15:53:35 -0700

    fix OBO in dkg phase final view computation

--- integration/testnet/network.go
+++ integration/testnet/network.go
@@ -667,9 +667,9 @@ func BootstrapNetwork(networkConf NetworkConfig, bootstrapDir string) (*flow.Blo
 	epochSetup := &flow.EpochSetup{
 		Counter:            epochCounter,
 		FirstView:          root.Header.View,
-		DKGPhase1FinalView: root.Header.View + networkConf.ViewsInStakingAuction + networkConf.ViewsInDKGPhase,
-		DKGPhase2FinalView: root.Header.View + networkConf.ViewsInStakingAuction + networkConf.ViewsInDKGPhase*2,
-		DKGPhase3FinalView: root.Header.View + networkConf.ViewsInStakingAuction + networkConf.ViewsInDKGPhase*3,
+		DKGPhase1FinalView: root.Header.View + networkConf.ViewsInStakingAuction + networkConf.ViewsInDKGPhase - 1,
+		DKGPhase2FinalView: root.Header.View + networkConf.ViewsInStakingAuction + networkConf.ViewsInDKGPhase*2 - 1,
+		DKGPhase3FinalView: root.Header.View + networkConf.ViewsInStakingAuction + networkConf.ViewsInDKGPhase*3 - 1,
 		FinalView:          root.Header.View + networkConf.ViewsInEpoch - 1,
 		Participants:       participants,
 		Assignments:        clusterAssignments,
