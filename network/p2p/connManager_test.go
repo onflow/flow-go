ea0b0753daf771644a674f9fd9331a458b379423
vishal <1117327+vishalchangrani@users.noreply.github.com>
2021-06-14 22:39:27 -0700

    improving the unit test

--- network/p2p/connManager_test.go
+++ network/p2p/connManager_test.go
@@ -1,9 +1,11 @@
 package p2p
 
 import (
+	"fmt"
 	"os"
 	"testing"
 
+	"github.com/libp2p/go-libp2p-core/peer"
 	"github.com/rs/zerolog"
 	"github.com/stretchr/testify/require"
 
@@ -11,20 +13,74 @@ import (
 	"github.com/onflow/flow-go/utils/unittest"
 )
 
-// TestProtectUnprotect tests that ProtectPeer marks a peer as protected and UnprotectPeer marks it as unprotected
-func TestProtectUnprotect(t *testing.T) {
+const (
+	protectF     = "protect"
+	unprotectF   = "unprotect"
+	isProtectedF = "isprotected"
+)
+
+type fun struct {
+	funName     string
+	expectation bool
+}
+
+var protect = fun{
+	protectF,
+	false,
+}
+var unprotect = fun{
+	unprotectF,
+	false,
+}
+var isProtected = fun{
+	isProtectedF,
+	true,
+}
+var isNotProtected = fun{
+	isProtectedF,
+	false,
+}
+
+// TestConnectionManagerProtection tests that multiple protect and unprotect calls result in the correct IsProtected
+// status for a peer ID
+func TestConnectionManagerProtection(t *testing.T) {
+
 	log := zerolog.New(os.Stderr).Level(zerolog.ErrorLevel)
 	noopMetrics := metrics.NewNoopCollector()
-
 	connManager := NewConnManager(log, noopMetrics)
 
+	testCases := [][]fun{
+		// single stream created on a connection
+		{protect, isProtected, unprotect, isNotProtected},
+		// two streams created on a connection at the same time
+		{protect, protect, unprotect, isProtected, unprotect, isNotProtected},
+		// two streams created on a connection one after another
+		{protect, unprotect, isNotProtected, protect, unprotect, isNotProtected},
+	}
+
+	for _, testCase := range testCases {
+		testSequence(t, testCase, connManager)
+	}
+}
+
+func testSequence(t *testing.T, sequence []fun, connMgr *ConnManager) {
+	pID := generatePeerInfo(t)
+	for _, s := range sequence {
+		switch s.funName {
+		case protectF:
+			connMgr.ProtectPeer(pID)
+		case unprotectF:
+			connMgr.UnprotectPeer(pID)
+		case isProtectedF:
+			require.Equal(t, connMgr.IsProtected(pID, ""), s.expectation, fmt.Sprintf("failed sequence: %v", sequence))
+		}
+	}
+}
+
+func generatePeerInfo(t *testing.T) peer.ID {
 	key := generateNetworkingKey(t)
 	identity := unittest.IdentityFixture(unittest.WithNetworkingKey(key.PublicKey()), unittest.WithAddress("1.1.1.1:0"))
 	pInfo, err := PeerAddressInfo(*identity)
 	require.NoError(t, err)
-
-	connManager.ProtectPeer(pInfo.ID)
-	require.True(t, connManager.IsProtected(pInfo.ID, ""))
-	connManager.UnprotectPeer(pInfo.ID)
-	require.False(t, connManager.IsProtected(pInfo.ID, ""))
+	return pInfo.ID
 }
