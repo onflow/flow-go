7186efe6406ea768b77f419c1ee8e27d3ef99f7a
Jordan Schalm <jordan@dapperlabs.com>
2021-06-17 15:17:15 -0700

    use shared key writing methods

--- cmd/bootstrap/cmd/keygen.go
+++ cmd/bootstrap/cmd/keygen.go
@@ -5,13 +5,10 @@ import (
 	"io"
 	"os"
 
+	"github.com/onflow/flow-go/cmd/bootstrap/run"
 	"github.com/spf13/cobra"
 
-	"github.com/onflow/flow-go-sdk/crypto"
-	"github.com/onflow/flow-go/model/bootstrap"
 	model "github.com/onflow/flow-go/model/bootstrap"
-	"github.com/onflow/flow-go/model/flow"
-	"github.com/onflow/flow-go/model/flow/order"
 )
 
 var flagDefaultMachineAccount bool
@@ -48,12 +45,28 @@ var keygenCmd = &cobra.Command{
 		nodes := genNetworkAndStakingKeys()
 		log.Info().Msg("")
 
+		// write key files
+		writeFile := func(relativePath string, val interface{}) error {
+			writeJSON(relativePath, val)
+			return nil
+		}
+		log.Info().Msg("writing internal private key files")
+		err = run.WriteStakingNetworkingKeyFiles(nodes, writeFile)
+		if err != nil {
+			log.Fatal().Err(err).Msg("failed to write internal private key files")
+		}
+		log.Info().Msg("")
+
 		// if specified, write machine account key files
 		// this should be only be used on non-production networks and immediately after
 		// bootstrapping from a fresh execution state (eg. benchnet)
 		if flagDefaultMachineAccount {
+			chainID := parseChainID(flagRootChain)
 			log.Info().Msg("writing default machine account files")
-			genDefaultMachineAccountKeys(nodes)
+			err = run.WriteMachineAccountFiles(chainID, nodes, writeFile)
+			if err != nil {
+				log.Fatal().Err(err).Msg("failed to write machine account key files")
+			}
 			log.Info().Msg("")
 		}
 
@@ -74,7 +87,10 @@ func init() {
 	// required parameters
 	keygenCmd.Flags().StringVar(&flagConfig, "config", "node-config.json", "path to a JSON file containing multiple node configurations (Role, Address, Stake)")
 	_ = keygenCmd.MarkFlagRequired("config")
+
+	// optional parameters, used for generating machine account files
 	keygenCmd.Flags().BoolVar(&flagDefaultMachineAccount, "machine-account", false, "whether or not to generate a default (same as networking key) machine account key file")
+	keygenCmd.Flags().StringVar(&flagRootChain, "root-chain", "emulator", "chain ID for the root block (can be \"main\", \"test\" or \"emulator\"")
 }
 
 // isEmptyDir returns True if the directory contains children
@@ -98,45 +114,4 @@ func genNodePubInfo(nodes []model.NodeInfo) {
 		pubNodes = append(pubNodes, node.Public())
 	}
 	writeJSON(model.PathInternalNodeInfosPub, pubNodes)
-}
-
-// Generates machine account key files using the node's networking key and assuming
-// a fresh empty execution state
-// TODO copied from testnet/network.go
-func genDefaultMachineAccountKeys(nodes []model.NodeInfo) {
-	nodes = model.Sort(nodes, order.Canonical)
-
-	addressIndex := uint64(4)
-	for _, nodeInfo := range nodes {
-
-		if nodeInfo.Role == flow.RoleCollection || nodeInfo.Role == flow.RoleConsensus {
-			addressIndex += 2
-		} else {
-			addressIndex += 1
-			continue
-		}
-
-		private, err := nodeInfo.Private()
-		if err != nil {
-			log.Fatal().Err(err).Msg("could not get node info private keys")
-		}
-
-		accountAddress, err := flow.Testnet.Chain().AddressAtIndex(addressIndex)
-		if err != nil {
-			log.Fatal().Err(err).Msg("failed to get address")
-		}
-
-		info := model.NodeMachineAccountInfo{
-			Address:           accountAddress.HexWithPrefix(),
-			EncodedPrivateKey: private.NetworkPrivKey.Encode(),
-			KeyIndex:          0,
-			SigningAlgorithm:  private.NetworkPrivKey.Algorithm(),
-			HashAlgorithm:     crypto.SHA3_256,
-		}
-
-		writeJSON(fmt.Sprintf(bootstrap.PathNodeMachineAccountInfoPriv, nodeInfo.NodeID), info)
-		if err != nil {
-			log.Fatal().Err(err).Msg("could not write machine account")
-		}
-	}
 }
