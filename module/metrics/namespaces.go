2be74636c23ebeaa8fc7e6ae020102185008e223
Yahya <yahya@dapperlabs.com>
2021-06-18 10:17:40 -0700

    adds job consumer name spaces

--- module/metrics/namespaces.go
+++ module/metrics/namespaces.go
@@ -61,6 +61,8 @@ const (
 	subsystemFetcherEngine   = "fetcher"
 	subsystemRequesterEngine = "requester"
 	subsystemVerifierEngine  = "verifier"
+	subsystemBlockConsumer   = "block_consumer"
+	subsystemChunkConsumer   = "chunk_consumer"
 )
 
 // METRIC NAMING GUIDELINES
