18c8d15e78fed2b60cdacc1fea2c13e2138fa981
Yurii Oleksyshyn <yuraolex@gmail.com>
2021-06-22 12:24:18 +0300

    Work in progress on adding pruning by height for pending receipts cache

--- module/mempool/stdmap/pending_receipts.go
+++ module/mempool/stdmap/pending_receipts.go
@@ -2,22 +2,40 @@ package stdmap
 
 import (
 	"fmt"
+	"github.com/onflow/flow-go/module/mempool"
+	"github.com/onflow/flow-go/storage"
 
 	"github.com/onflow/flow-go/model/flow"
 )
+
+type receiptsSet map[flow.Identifier]struct{}
 
 // PendingReceipts stores pending receipts indexed by the id.
 // It also maintains a secondary index on the previous result id.
 // in order to allow to find receipts by the previous result id.
 type PendingReceipts struct {
 	*Backend
+	headers storage.Headers
 	// secondary index by parent result id, since multiple receipts could
 	// have the same parent result, (even if they have different result)
 	byPreviousResultID map[flow.Identifier][]flow.Identifier
+	// secondary index by height, we need this to prune pending receipts to some height
+	// it's safe to cleanup this index only when pruning. Even if some receipts are deleted manually,
+	// eventually index will be cleaned up.
+	byHeight     map[uint64]receiptsSet
+	lowestHeight uint64
 }
 
 func indexByPreviousResultID(receipt *flow.ExecutionReceipt) flow.Identifier {
 	return receipt.ExecutionResult.PreviousResultID
+}
+
+func (r *PendingReceipts) indexByHeight(receipt *flow.ExecutionReceipt) (uint64, error) {
+	header, err := r.headers.ByBlockID(receipt.ExecutionResult.BlockID)
+	if err != nil {
+		return 0, fmt.Errorf("could not retreieve block by ID %v: %w", receipt.ExecutionResult.BlockID, err)
+	}
+	return header.Height, nil
 }
 
 // NewPendingReceipts creates a new memory pool for execution receipts.
@@ -71,6 +89,16 @@ func (r *PendingReceipts) Add(receipt *flow.ExecutionReceipt) bool {
 			return nil
 		}
 
+		height, err := r.indexByHeight(receipt)
+		if err != nil {
+			return err
+		}
+
+		// skip elements below the pruned
+		if height < r.lowestHeight {
+			return nil
+		}
+
 		entities[receiptID] = receipt
 
 		// update index AND the backdata in one "transaction"
@@ -81,6 +109,14 @@ func (r *PendingReceipts) Add(receipt *flow.ExecutionReceipt) bool {
 		} else {
 			r.byPreviousResultID[previousResultID] = append(siblings, receiptID)
 		}
+
+		sameHeight, ok := r.byHeight[height]
+		if !ok {
+			sameHeight = make(receiptsSet)
+			r.byHeight[height] = sameHeight
+		}
+		sameHeight[receiptID] = struct{}{}
+
 		added = true
 		return nil
 	})
@@ -141,4 +177,49 @@ func (r *PendingReceipts) ByPreviousResultID(previousResultID flow.Identifier) [
 // Size will return the total number of pending receipts
 func (r *PendingReceipts) Size() uint {
 	return r.Backend.Size()
+}
+
+// PruneUpToHeight remove all receipts for blocks whose height is strictly
+// smaller that height. Note: receipts for blocks at height are retained.
+// After pruning, receipts below for blocks below the given height are dropped.
+//
+// Monotonicity Requirement:
+// The pruned height cannot decrease, as we cannot recover already pruned elements.
+// If `height` is smaller than the previous value, the previous value is kept
+// and the sentinel mempool.DecreasingPruningHeightError is returned.
+func (r *PendingReceipts) PruneUpToHeight(height uint64) error {
+	return r.Backend.Run(func(entities map[flow.Identifier]flow.Entity) error {
+		if height < r.lowestHeight {
+			return mempool.NewDecreasingPruningHeightErrorf(
+				"pruning height: %d, existing height: %d", height, r.lowestHeight)
+		}
+
+		if len(entities) == 0 {
+			r.lowestHeight = height
+			return nil
+		}
+		// Optimization: if there are less height in the index than the height range to prune,
+		// range to prune, then just go through each seal.
+		// Otherwise, go through each height to prune.
+		if uint64(len(r.byHeight)) < height-r.lowestHeight {
+			for h := range r.byHeight {
+				if h < height {
+					r.removeByHeight(h)
+				}
+			}
+		} else {
+			for h := r.lowestHeight; h < height; h++ {
+				r.removeByHeight(h)
+			}
+		}
+		r.lowestHeight = height
+		return nil
+	})
+}
+
+func (r *PendingReceipts) removeByHeight(height uint64) {
+	for receiptID := range r.byHeight[height] {
+		r.Rem(receiptID)
+	}
+	delete(r.byHeight, height)
 }
