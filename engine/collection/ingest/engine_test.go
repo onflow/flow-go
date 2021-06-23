ccc1df0a33ef238a78f497214b96695647da78fd
ramtinms <ramtin@axiomzen.co>
2021-06-23 16:40:26 -0700

    apply PR's comments

--- engine/collection/ingest/engine_test.go
+++ engine/collection/ingest/engine_test.go
@@ -166,6 +166,19 @@ func (suite *Suite) TestInvalidTransaction() {
 	})
 
 	suite.Run("invalid signature format", func() {
+		signer := flow.Testnet.Chain().ServiceAddress()
+		keyIndex := uint64(0)
+
+		sig1 := unittest.TransactionSignatureFixture()
+		sig1.KeyIndex = keyIndex
+		sig1.Address = signer
+		sig1.SignerIndex = 0
+
+		sig2 := unittest.TransactionSignatureFixture()
+		sig2.KeyIndex = keyIndex
+		sig2.Address = signer
+		sig2.SignerIndex = 1
+
 		suite.Run("invalid format of an enveloppe signature", func() {
 			invalidSig := unittest.InvalidFormatSignature()
 			tx := unittest.TransactionBodyFixture()
@@ -191,8 +204,7 @@ func (suite *Suite) TestInvalidTransaction() {
 		suite.Run("duplicated signature (envelope only)", func() {
 			tx := unittest.TransactionBodyFixture()
 			tx.ReferenceBlockID = suite.root.ID()
-			tx.EnvelopeSignatures = []flow.TransactionSignature{unittest.TransactionSignatureFixture(), unittest.TransactionSignatureFixture()}
-
+			tx.EnvelopeSignatures = []flow.TransactionSignature{sig1, sig2}
 			err := suite.engine.ProcessLocal(&tx)
 			suite.Assert().Error(err)
 			suite.Assert().True(errors.As(err, &access.DuplicatedSignatureError{}))
@@ -201,7 +213,7 @@ func (suite *Suite) TestInvalidTransaction() {
 		suite.Run("duplicated signature (payload only)", func() {
 			tx := unittest.TransactionBodyFixture()
 			tx.ReferenceBlockID = suite.root.ID()
-			tx.PayloadSignatures = []flow.TransactionSignature{unittest.TransactionSignatureFixture(), unittest.TransactionSignatureFixture()}
+			tx.PayloadSignatures = []flow.TransactionSignature{sig1, sig2}
 
 			err := suite.engine.ProcessLocal(&tx)
 			suite.Assert().Error(err)
@@ -211,8 +223,8 @@ func (suite *Suite) TestInvalidTransaction() {
 		suite.Run("duplicated signature (cross case)", func() {
 			tx := unittest.TransactionBodyFixture()
 			tx.ReferenceBlockID = suite.root.ID()
-			tx.PayloadSignatures = []flow.TransactionSignature{unittest.TransactionSignatureFixture()}
-			tx.EnvelopeSignatures = []flow.TransactionSignature{unittest.TransactionSignatureFixture()}
+			tx.PayloadSignatures = []flow.TransactionSignature{sig1}
+			tx.EnvelopeSignatures = []flow.TransactionSignature{sig2}
 
 			err := suite.engine.ProcessLocal(&tx)
 			suite.Assert().Error(err)
