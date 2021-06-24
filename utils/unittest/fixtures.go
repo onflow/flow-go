fef3bdd1c11935816f4cbdcc1c5dd87c9b03b4d7
Ramtin M. Seraj <ramtinms@users.noreply.github.com>
2021-06-23 17:09:17 -0700

    Reject duplicated signatures (#890)

--- utils/unittest/fixtures.go
+++ utils/unittest/fixtures.go
@@ -1011,7 +1011,6 @@ func TransactionBodyFixture(opts ...func(*flow.TransactionBody)) flow.Transactio
 		ProposalKey:        ProposalKeyFixture(),
 		Payer:              AddressFixture(),
 		Authorizers:        []flow.Address{AddressFixture()},
-		PayloadSignatures:  []flow.TransactionSignature{TransactionSignatureFixture()},
 		EnvelopeSignatures: []flow.TransactionSignature{TransactionSignatureFixture()},
 	}
 
