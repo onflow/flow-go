857e1a8a7f857f4c35be842fdfd9dee98339a8be
Yurii Oleksyshyn <yuraolex@gmail.com>
2021-06-29 15:51:48 +0300

    Updated tests

--- engine/common/synchronization/request_queue_test.go
+++ engine/common/synchronization/request_queue_test.go
@@ -34,3 +34,58 @@ func TestRequestQueue_Get(t *testing.T) {
 	_, ok := q.Get()
 	require.False(t, ok)
 }
+
+// TestRequestQueue_Put tests that putting an item into queue overwrites previous one
+func TestRequestQueue_Put(t *testing.T) {
+	q := NewRequestQueue(100)
+	msg := &engine.Message{
+		OriginID: unittest.IdentifierFixture(),
+		Payload:  unittest.IdentifierFixture(),
+	}
+	require.True(t, q.Put(msg))
+	require.Equal(t, msg, q.requests[msg.OriginID])
+
+	newMsg := &engine.Message{
+		OriginID: msg.OriginID,
+		Payload:  unittest.BlockFixture(),
+	}
+
+	// this should overwrite message
+	require.True(t, q.Put(newMsg))
+	require.Equal(t, newMsg, q.requests[msg.OriginID])
+}
+
+// TestRequestQueue_PutAtMaxCapacity tests that putting an item over max capacity results in successful eject and put
+func TestRequestQueue_PutAtMaxCapacity(t *testing.T) {
+	limit := uint(10)
+	q := NewRequestQueue(limit)
+	messages := make(map[flow.Identifier]*engine.Message)
+	for i := uint(0); i < limit; limit++ {
+		msg := &engine.Message{
+			OriginID: unittest.IdentifierFixture(),
+			Payload:  unittest.IdentifierFixture(),
+		}
+		require.True(t, q.Put(msg))
+		messages[msg.OriginID] = msg
+	}
+	newMsg := &engine.Message{
+		OriginID: unittest.IdentifierFixture(),
+		Payload:  unittest.BlockFixture(),
+	}
+	require.True(t, q.Put(newMsg))
+
+	ejectedCount := 0
+	for {
+		m, ok := q.Get()
+		if !ok {
+			break
+		}
+		expectedMessage, found := messages[m.OriginID]
+		if !found {
+			ejectedCount++
+		} else {
+			require.Equal(t, m, expectedMessage)
+		}
+	}
+	require.Equal(t, 1, ejectedCount)
+}
