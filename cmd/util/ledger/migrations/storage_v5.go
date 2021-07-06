5020bacbafc5e196b194aafc2e3123b79f9b7c02
Bastian Müller <bastian@axiomzen.co>
2021-07-05 18:42:52 -0700

    load programs for composite types

--- cmd/util/ledger/migrations/storage_v5.go
+++ cmd/util/ledger/migrations/storage_v5.go
@@ -3,33 +3,47 @@ package migrations
 import (
 	"bytes"
 	"encoding/hex"
+	"errors"
 	"fmt"
 	"math"
-	"runtime"
 
 	"github.com/fxamacker/cbor/v2"
+	"github.com/onflow/cadence"
+	"github.com/onflow/cadence/runtime"
+	"github.com/onflow/cadence/runtime/ast"
 	"github.com/onflow/cadence/runtime/common"
 	"github.com/onflow/cadence/runtime/interpreter"
-
+	"github.com/onflow/cadence/runtime/sema"
+	"github.com/onflow/flow-go/fvm/programs"
 	"github.com/onflow/flow-go/fvm/state"
 	"github.com/onflow/flow-go/ledger"
+	"github.com/onflow/flow-go/model/flow"
 )
+
+type storageFormatV5MigrationResult struct {
+	key string
+	ledger.Payload
+	error
+}
 
 func StorageFormatV5Migration(payloads []ledger.Payload) ([]ledger.Payload, error) {
 
 	migratedPayloads := make([]ledger.Payload, 0, len(payloads))
 
 	jobs := make(chan ledger.Payload)
-	results := make(chan struct {
-		key string
-		ledger.Payload
-		error
-	})
+	results := make(chan storageFormatV5MigrationResult)
 
-	workerCount := runtime.NumCPU()
+	// TODO: runtime.NumCPU()
+	workerCount := 1
+
+	l := newView(payloads)
+	st := state.NewState(l)
+	sth := state.NewStateHolder(st)
+	accounts := state.NewAccounts(sth)
+	programs := programs.NewEmptyPrograms()
 
 	for i := 0; i < workerCount; i++ {
-		go storageFormatV5MigrationWorker(jobs, results)
+		go storageFormatV5MigrationWorker(jobs, results, accounts, programs)
 	}
 
 	go func() {
@@ -53,13 +67,14 @@ func StorageFormatV5Migration(payloads []ledger.Payload) ([]ledger.Payload, erro
 	return migratedPayloads, nil
 }
 
-func storageFormatV5MigrationWorker(jobs <-chan ledger.Payload, results chan<- struct {
-	key string
-	ledger.Payload
-	error
-}) {
+func storageFormatV5MigrationWorker(
+	jobs <-chan ledger.Payload,
+	results chan<- storageFormatV5MigrationResult,
+	accounts *state.Accounts,
+	programs *programs.Programs,
+) {
 	for payload := range jobs {
-		migratedPayload, err := reencodePayloadV5(payload)
+		migratedPayload, err := reencodePayloadV5(payload, accounts, programs)
 		result := struct {
 			key string
 			ledger.Payload
@@ -79,6 +94,20 @@ func storageFormatV5MigrationWorker(jobs <-chan ledger.Payload, results chan<- s
 	}
 }
 
+func checkStorageFormatV5(payload ledger.Payload) error {
+
+	if !bytes.HasPrefix(payload.Value, []byte{0x0, 0xca, 0xde}) {
+		return nil
+	}
+
+	_, version := interpreter.StripMagic(payload.Value)
+	if version != interpreter.CurrentEncodingVersion {
+		return fmt.Errorf("invalid version for key %s: %d", payload.Key.String(), version)
+	}
+
+	return nil
+}
+
 var storageMigrationV5DecMode = func() cbor.DecMode {
 	decMode, err := cbor.DecOptions{
 		IntDec:           cbor.IntDecConvertNone,
@@ -92,7 +121,11 @@ var storageMigrationV5DecMode = func() cbor.DecMode {
 	return decMode
 }()
 
-func reencodePayloadV5(payload ledger.Payload) (ledger.Payload, error) {
+func reencodePayloadV5(
+	payload ledger.Payload,
+	accounts *state.Accounts,
+	programs *programs.Programs,
+) (ledger.Payload, error) {
 
 	keyParts := payload.Key.KeyParts
 
@@ -130,7 +163,14 @@ func reencodePayloadV5(payload ledger.Payload) (ledger.Payload, error) {
 
 	owner := common.BytesToAddress(rawOwner)
 
-	newValue, err := reencodeValueV5(value, owner, string(rawKey), version)
+	newValue, err := reencodeValueV5(
+		value,
+		owner,
+		string(rawKey),
+		version,
+		accounts,
+		programs,
+	)
 	if err != nil {
 		return ledger.Payload{},
 			fmt.Errorf(
@@ -147,7 +187,14 @@ func reencodePayloadV5(payload ledger.Payload) (ledger.Payload, error) {
 	return payload, nil
 }
 
-func reencodeValueV5(data []byte, owner common.Address, key string, version uint16) ([]byte, error) {
+func reencodeValueV5(
+	data []byte,
+	owner common.Address,
+	key string,
+	version uint16,
+	accounts *state.Accounts,
+	programs *programs.Programs,
+) ([]byte, error) {
 
 	// Decode the value
 
@@ -161,6 +208,8 @@ func reencodeValueV5(data []byte, owner common.Address, key string, version uint
 				err, hex.Dump(data),
 			)
 	}
+
+	// Force decoding of all container values
 
 	interpreter.InspectValue(
 		value,
@@ -176,6 +225,21 @@ func reencodeValueV5(data []byte, owner common.Address, key string, version uint
 			return true
 		},
 	)
+
+	// TODO: inspect composites, load type,
+	//    - recursively walk each field, use field type
+
+	ok, err := inferContainerTypeStaticTypes(value, accounts, programs)
+	if err != nil {
+		return nil, err
+	}
+	// If the types could not be inferred,
+	// then return the data as-is, unmigrated
+	if !ok {
+		return data, nil
+	}
+
+	// TODO: inspect arrays and dictionaries, check static type info (dict key type and dict value type != nil)
 
 	// Encode the value using the new encoder
 
@@ -239,16 +303,358 @@ func reencodeValueV5(data []byte, owner common.Address, key string, version uint
 	return newData, nil
 }
 
-func checkStorageFormatV5(payload ledger.Payload) error {
+func inferContainerTypeStaticTypes(
+	value interpreter.Value,
+	accounts *state.Accounts,
+	programs *programs.Programs,
+) (bool, error) {
+	var err error
+	var typeLoadFailure bool
 
-	if !bytes.HasPrefix(payload.Value, []byte{0x0, 0xca, 0xde}) {
+	interpreter.InspectValue(
+		value,
+		func(value interpreter.Value) bool {
+			compositeValue, ok := value.(*interpreter.CompositeValue)
+			if !ok {
+				return true
+			}
+
+			typeID := compositeValue.TypeID()
+
+			var program *interpreter.Program
+			program, err = loadProgram(compositeValue.Location(), accounts, programs)
+			if err != nil {
+				var parsingCheckingError *runtime.ParsingCheckingError
+				if errors.As(err, &parsingCheckingError) {
+					fmt.Printf(
+						"Failed to parse and check program for: %s: %s\n",
+						typeID, err.Error(),
+					)
+					typeLoadFailure = true
+					err = nil
+				}
+
+				return false
+			}
+
+			compositeType := program.Elaboration.CompositeTypes[typeID]
+			fields := compositeValue.Fields()
+			for pair := fields.Oldest(); pair != nil; pair = pair.Next() {
+				fieldName := pair.Key
+				fieldValue := pair.Value
+
+				member, ok := compositeType.Members.Get(fieldName)
+				if !ok {
+					err = fmt.Errorf("missing type for composite field: %s.%s", typeID, fieldName)
+					return false
+				}
+
+				err = inferContainerTypeStaticType(fieldValue, member.TypeAnnotation.Type)
+				if err != nil {
+					return false
+				}
+			}
+
+			return true
+		},
+	)
+	if err != nil {
+		return false, err
+	}
+	if typeLoadFailure {
+		return false, nil
+	}
+
+	return true, nil
+}
+
+func inferContainerTypeStaticType(value interpreter.Value, t sema.Type) error {
+
+	// Only infer static type for arrays and dictionaries
+	switch value.(type) {
+	case *interpreter.ArrayValue,
+		*interpreter.DictionaryValue:
+		break
+	default:
 		return nil
 	}
 
-	_, version := interpreter.StripMagic(payload.Value)
-	if version != interpreter.CurrentEncodingVersion {
-		return fmt.Errorf("invalid version for key %s: %d", payload.Key.String(), version)
-	}
+	// TODO: infer type recursively
 
 	return nil
+}
+
+func loadProgram(
+	location common.Location,
+	accounts *state.Accounts,
+	programs *programs.Programs,
+) (
+	*interpreter.Program,
+	error,
+) {
+	program, _, ok := programs.Get(location)
+	if ok {
+		return program, nil
+	}
+
+	addressLocation, ok := location.(common.AddressLocation)
+	if !ok {
+		return nil, fmt.Errorf(
+			"cannot load program for unsupported non-address location: %s",
+			addressLocation,
+		)
+	}
+
+	contractCode, err := accounts.GetContract(
+		addressLocation.Name,
+		flow.Address(addressLocation.Address),
+	)
+	if err != nil {
+		return nil, err
+	}
+
+	rt := runtime.NewInterpreterRuntime()
+	program, err = rt.ParseAndCheckProgram(
+		contractCode,
+		runtime.Context{
+			Interface: migrationRuntimeInterface{accounts, programs},
+			Location:  location,
+		},
+	)
+	if err != nil {
+		return nil, err
+	}
+
+	programs.Set(location, program, nil)
+
+	return program, nil
+}
+
+type migrationRuntimeInterface struct {
+	accounts *state.Accounts
+	programs *programs.Programs
+}
+
+func (m migrationRuntimeInterface) ResolveLocation(
+	identifiers []runtime.Identifier,
+	location runtime.Location,
+) ([]runtime.ResolvedLocation, error) {
+
+	addressLocation, isAddress := location.(common.AddressLocation)
+
+	// if the location is not an address location, e.g. an identifier location (`import Crypto`),
+	// then return a single resolved location which declares all identifiers.
+	if !isAddress {
+		return []runtime.ResolvedLocation{
+			{
+				Location:    location,
+				Identifiers: identifiers,
+			},
+		}, nil
+	}
+
+	// if the location is an address,
+	// and no specific identifiers where requested in the import statement,
+	// then fetch all identifiers at this address
+	if len(identifiers) == 0 {
+		address := flow.Address(addressLocation.Address)
+
+		contractNames, err := m.accounts.GetContractNames(address)
+		if err != nil {
+			return nil, fmt.Errorf("ResolveLocation failed: %w", err)
+		}
+
+		// if there are no contractNames deployed,
+		// then return no resolved locations
+		if len(contractNames) == 0 {
+			return nil, nil
+		}
+
+		identifiers = make([]ast.Identifier, len(contractNames))
+
+		for i := range identifiers {
+			identifiers[i] = runtime.Identifier{
+				Identifier: contractNames[i],
+			}
+		}
+	}
+
+	// return one resolved location per identifier.
+	// each resolved location is an address contract location
+	resolvedLocations := make([]runtime.ResolvedLocation, len(identifiers))
+	for i := range resolvedLocations {
+		identifier := identifiers[i]
+		resolvedLocations[i] = runtime.ResolvedLocation{
+			Location: common.AddressLocation{
+				Address: addressLocation.Address,
+				Name:    identifier.Identifier,
+			},
+			Identifiers: []runtime.Identifier{identifier},
+		}
+	}
+
+	return resolvedLocations, nil
+}
+
+func (m migrationRuntimeInterface) GetCode(location runtime.Location) ([]byte, error) {
+	contractLocation, ok := location.(common.AddressLocation)
+	if !ok {
+		return nil, fmt.Errorf("GetCode failed: expected AddressLocation")
+	}
+
+	add, err := m.accounts.GetContract(contractLocation.Name, flow.Address(contractLocation.Address))
+	if err != nil {
+		return nil, fmt.Errorf("GetCode failed: %w", err)
+	}
+
+	return add, nil
+}
+
+func (m migrationRuntimeInterface) GetProgram(location runtime.Location) (*interpreter.Program, error) {
+	program, _, ok := m.programs.Get(location)
+	if ok {
+		return program, nil
+	}
+
+	return nil, nil
+}
+
+func (m migrationRuntimeInterface) SetProgram(location runtime.Location, program *interpreter.Program) error {
+	m.programs.Set(location, program, nil)
+	return nil
+}
+
+func (m migrationRuntimeInterface) GetValue(_, _ []byte) (value []byte, err error) {
+	panic("unexpected GetValue call")
+}
+
+func (m migrationRuntimeInterface) SetValue(_, _, _ []byte) (err error) {
+	panic("unexpected SetValue call")
+}
+
+func (m migrationRuntimeInterface) CreateAccount(_ runtime.Address) (address runtime.Address, err error) {
+	panic("unexpected CreateAccount call")
+}
+
+func (m migrationRuntimeInterface) AddEncodedAccountKey(_ runtime.Address, _ []byte) error {
+	panic("unexpected AddEncodedAccountKey call")
+}
+
+func (m migrationRuntimeInterface) RevokeEncodedAccountKey(_ runtime.Address, _ int) (publicKey []byte, err error) {
+	panic("unexpected RevokeEncodedAccountKey call")
+}
+
+func (m migrationRuntimeInterface) AddAccountKey(
+	_ runtime.Address,
+	_ *runtime.PublicKey,
+	_ runtime.HashAlgorithm,
+	_ int,
+) (*runtime.AccountKey, error) {
+	panic("unexpected AddAccountKey call")
+}
+
+func (m migrationRuntimeInterface) GetAccountKey(_ runtime.Address, _ int) (*runtime.AccountKey, error) {
+	panic("unexpected GetAccountKey call")
+}
+
+func (m migrationRuntimeInterface) RevokeAccountKey(_ runtime.Address, _ int) (*runtime.AccountKey, error) {
+	panic("unexpected RevokeAccountKey call")
+}
+
+func (m migrationRuntimeInterface) UpdateAccountContractCode(_ runtime.Address, _ string, _ []byte) (err error) {
+	panic("unexpected UpdateAccountContractCode call")
+}
+
+func (m migrationRuntimeInterface) GetAccountContractCode(
+	address runtime.Address,
+	name string,
+) (code []byte, err error) {
+	return m.accounts.GetContract(name, flow.Address(address))
+}
+
+func (m migrationRuntimeInterface) RemoveAccountContractCode(_ runtime.Address, _ string) (err error) {
+	panic("unexpected RemoveAccountContractCode call")
+}
+
+func (m migrationRuntimeInterface) GetSigningAccounts() ([]runtime.Address, error) {
+	panic("unexpected GetSigningAccounts call")
+}
+
+func (m migrationRuntimeInterface) ProgramLog(_ string) error {
+	panic("unexpected ProgramLog call")
+}
+
+func (m migrationRuntimeInterface) EmitEvent(_ cadence.Event) error {
+	panic("unexpected EmitEvent call")
+}
+
+func (m migrationRuntimeInterface) ValueExists(_, _ []byte) (exists bool, err error) {
+	panic("unexpected ValueExists call")
+}
+
+func (m migrationRuntimeInterface) GenerateUUID() (uint64, error) {
+	panic("unexpected GenerateUUID call")
+}
+
+func (m migrationRuntimeInterface) GetComputationLimit() uint64 {
+	panic("unexpected GetComputationLimit call")
+}
+
+func (m migrationRuntimeInterface) SetComputationUsed(_ uint64) error {
+	panic("unexpected SetComputationUsed call")
+}
+
+func (m migrationRuntimeInterface) DecodeArgument(_ []byte, _ cadence.Type) (cadence.Value, error) {
+	panic("unexpected DecodeArgument call")
+}
+
+func (m migrationRuntimeInterface) GetCurrentBlockHeight() (uint64, error) {
+	panic("unexpected GetCurrentBlockHeight call")
+}
+
+func (m migrationRuntimeInterface) GetBlockAtHeight(_ uint64) (block runtime.Block, exists bool, err error) {
+	panic("unexpected GetBlockAtHeight call")
+}
+
+func (m migrationRuntimeInterface) UnsafeRandom() (uint64, error) {
+	panic("unexpected UnsafeRandom call")
+}
+
+func (m migrationRuntimeInterface) VerifySignature(
+	_ []byte,
+	_ string,
+	_ []byte,
+	_ []byte,
+	_ runtime.SignatureAlgorithm,
+	_ runtime.HashAlgorithm,
+) (bool, error) {
+	panic("unexpected VerifySignature call")
+}
+
+func (m migrationRuntimeInterface) Hash(_ []byte, _ string, _ runtime.HashAlgorithm) ([]byte, error) {
+	panic("unexpected Hash call")
+}
+
+func (m migrationRuntimeInterface) GetAccountBalance(_ common.Address) (value uint64, err error) {
+	panic("unexpected GetAccountBalance call")
+}
+
+func (m migrationRuntimeInterface) GetAccountAvailableBalance(_ common.Address) (value uint64, err error) {
+	panic("unexpected GetAccountAvailableBalance call")
+}
+
+func (m migrationRuntimeInterface) GetStorageUsed(_ runtime.Address) (value uint64, err error) {
+	panic("unexpected GetStorageUsed call")
+}
+
+func (m migrationRuntimeInterface) GetStorageCapacity(_ runtime.Address) (value uint64, err error) {
+	panic("unexpected GetStorageCapacity call")
+}
+
+func (m migrationRuntimeInterface) ImplementationDebugLog(_ string) error {
+	panic("unexpected ImplementationDebugLog call")
+}
+
+func (m migrationRuntimeInterface) ValidatePublicKey(_ *runtime.PublicKey) (bool, error) {
+	panic("unexpected ValidatePublicKey call")
 }
