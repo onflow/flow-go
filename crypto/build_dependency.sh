021f468d9d29b22f73f04ced378761a7941661e0
Tarak Ben Youssef <tarak.benyoussef@dapperlabs.com>
2021-07-07 01:15:54 -0700

    update relic master branch and add permission grant

--- crypto/build_dependency.sh
+++ crypto/build_dependency.sh
@@ -1,6 +1,13 @@
 
 #!/bin/bash
 
+PKG_DIR="./"
+
+# grant permissions if not existant
+if [[ ! -r ${PKG_DIR}  || ! -w ${PKG_DIR} || ! -x ${PKG_DIR} ]]; then
+   sudo chmod -R 755 ${PKG_DIR}
+fi
+
 # relic version or tag
 relic_version="7a9bba7f"
 
@@ -10,10 +17,12 @@ rm -rf relic
 # git clone --branch $(relic_version) --single-branch --depth 1 git@github.com:relic-toolkit/relic.git
 
 # clone all the history if the version is only defined by a commit hash.
-git clone --branch master --single-branch git@github.com:relic-toolkit/relic.git
+git clone --branch main --single-branch https://github.com/relic-toolkit/relic.git
 cd relic
 git checkout $relic_version
 cd ..
 
 # build relic
 bash relic_build.sh
+
+
