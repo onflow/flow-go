0448a3e52f50daffc887e3aaf6e589d4efbe0455
Yahya <yahya@dapperlabs.com>
2021-07-12 14:54:52 -0700

    implements entity interface for stored chunk data pack

--- storage/model/storedChunkDataPack.go
+++ storage/model/storedChunkDataPack.go
@@ -4,10 +4,20 @@ import (
 	"github.com/onflow/flow-go/model/flow"
 )
 
-// StoredChunkDataPack is an internal representation of chunk data pack specific to the execution node to be stored on persistent storage.
+// StoredChunkDataPack is an in-storage representation of chunk data pack.
+// Its prime difference is instead of an actual collection, it keeps a collection ID hence relying on maintaining
+// the collection on a secondary storage.
 type StoredChunkDataPack struct {
 	ChunkID      flow.Identifier
 	StartState   flow.StateCommitment
 	Proof        flow.StorageProof
 	CollectionID flow.Identifier
+}
+
+func (s StoredChunkDataPack) ID() flow.Identifier {
+	return s.ChunkID
+}
+
+func (s StoredChunkDataPack) Checksum() flow.Identifier {
+	return flow.MakeID(s)
 }
