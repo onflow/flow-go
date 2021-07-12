08867e41b7fd79a3dfe229920f2aa6311e218af8
Simon Zhu <simon.zsiyan@gmail.com>
2021-07-12 12:12:08 -0700

    Updated engine to keep a separate set of all engines

---
+++ engine/common/multiplexer/engine_test.go
@@ -0,0 +1,5 @@
+// test happy path
+// test failure in downstream engine
+// test submitlocal and processlocal
+// test processing message on channel that has no subscriptions
+// test registering same engine twice on same channel
