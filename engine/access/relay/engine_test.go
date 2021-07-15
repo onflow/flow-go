e4be54de7583776bc7066fc428150c9c0ee53e15
Simon Zhu <simon.zsiyan@gmail.com>
2021-07-15 16:26:06 -0700

    Added tests

--- engine/access/relay/engine_test.go
+++ engine/access/relay/engine_test.go
@@ -3,18 +3,47 @@ package relay
 import (
 	"testing"
 
+	"github.com/onflow/flow-go/module"
+	mockmodule "github.com/onflow/flow-go/module/mock"
+	"github.com/onflow/flow-go/network"
+	"github.com/onflow/flow-go/network/mocknetwork"
+	"github.com/onflow/flow-go/utils/unittest"
+	"github.com/rs/zerolog"
+	"github.com/stretchr/testify/mock"
 	"github.com/stretchr/testify/suite"
 )
 
 type Suite struct {
 	suite.Suite
 
-	engine *Engine
+	engine   *Engine
+	me       module.Local
+	channels network.ChannelList
+	conduits map[network.Channel]*mocknetwork.Conduit
 }
 
 func (suite *Suite) SetupTest() {
+	suite.channels = network.ChannelList{
+		network.Channel("test-channel-1"),
+	}
+	net := new(mockmodule.Network)
+	unstakedNet := new(mockmodule.Network)
+	suite.me = new(mockmodule.Local)
+	suite.conduits = make(map[network.Channel]*mocknetwork.Conduit)
+
+	for _, channel := range suite.channels {
+		con := new(mocknetwork.Conduit)
+		suite.conduits[channel] = con
+		net.On("Register", channel, mock.Anything).Return(new(mocknetwork.Conduit), nil).Once()
+		unstakedNet.On("Register", channel, mock.Anything).Return(con, nil).Once()
+	}
+
 	eng, err := New(
-	// TODO
+		zerolog.Logger{},
+		suite.channels,
+		net,
+		unstakedNet,
+		suite.me,
 	)
 	suite.Require().Nil(err)
 
@@ -23,4 +52,28 @@ func (suite *Suite) SetupTest() {
 
 func TestRelayEngine(t *testing.T) {
 	suite.Run(t, new(Suite))
+}
+
+func getEvent() interface{} {
+	return struct {
+		foo string
+	}{
+		foo: "bar",
+	}
+}
+
+// TestHappyPath tests that the relay engine relays events for each
+// channel that it was created with
+func (suite *Suite) TestHappyPath() {
+	for channel, conduit := range suite.conduits {
+		id := unittest.IdentifierFixture()
+		event := getEvent()
+
+		conduit.On("Publish", event).Return(nil).Once()
+
+		suite.engine.Process(channel, id, event)
+
+		conduit.AssertNumberOfCalls(suite.T(), "Publish", 1)
+		conduit.AssertExpectations(suite.T())
+	}
 }
