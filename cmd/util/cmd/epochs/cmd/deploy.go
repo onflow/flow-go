1cde73bb5bd6150a83f86c94781a1d8bf8657ddc
danuio <me@danu.io>
2021-07-19 14:08:22 +0100

    Extract `deploy_epoch` transaction arguments

--- cmd/util/cmd/epochs/cmd/deploy.go
+++ cmd/util/cmd/epochs/cmd/deploy.go
@@ -7,8 +7,12 @@ import (
 
 	"github.com/spf13/cobra"
 
+	"github.com/onflow/flow-core-contracts/lib/go/contracts"
+
 	"github.com/onflow/flow-go/engine/common/rpc/convert"
+	"github.com/onflow/flow-go/fvm/systemcontracts"
 	"github.com/onflow/flow-go/model/bootstrap"
+	"github.com/onflow/flow-go/state/protocol/inmem"
 	"github.com/onflow/flow-go/utils/io"
 )
 
@@ -28,11 +32,11 @@ func init() {
 }
 
 func addDeployCmdFlags() {
+	// TODO: convert `fungibleTokenAddress`, `flowTokenAddress`, `flowTokenAddress`, `idTableContract`, `FLOWsupplyIncreasePercentage` to flags
 }
 
 // deployRun ...
 // Contract: https://github.com/onflow/flow-core-contracts/blob/master/contracts/epochs/FlowEpoch.cdc
-// Transaction: https://github.com/onflow/flow-core-contracts/blob/master/transactions/epoch/admin/deploy_epoch.cdc
 func deployRun(cmd *cobra.Command, args []string) {
 
 	// path to the root protocol snapshot json file
@@ -69,4 +73,56 @@ func deployRun(cmd *cobra.Command, args []string) {
 	}
 
 	fmt.Printf("%v, %v", argsPath, snapshot)
+}
+
+// getDeployEpochTransactionArguments pulls out required arguments for the `deploy_epoch` transaction from the root
+// protocol snapshot and takes into any required ajustments to align the state of the contract with the protocol state.
+// Transaction: https://github.com/onflow/flow-core-contracts/blob/master/transactions/epoch/admin/deploy_epoch.cdc
+func getDeployEpochTransactionArguments(snapshot inmem.Snapshot) {
+
+	// current epoch
+	currentEpoch := snapshot.Epochs().Current()
+
+	head, err := snapshot.Head()
+	if err != nil {
+		log.Fatal().Err(err).Msgf("could not get head from snapshot")
+	}
+	cu
+
+	// root chain id and system contractsRegister
+	chainID := head.ChainID
+	systemContracts, err := systemcontracts.SystemContractsForChain(chainID)
+	if err != nil {
+		log.Fatal().Err(err).Msgf("could not get system contracts for chainID")
+	}
+
+	// epoch contract name and get code for contract
+	epochContractName := "FlowEpoch"
+	epochContractCode := contracts.FlowEpoch("", "", "", systemContracts.ClusterQC.Address.Hex(), systemContracts.DKG.Address.Hex())
+
+	// get final view from snapshot
+	finalView, err := currentEpoch.FinalView()
+	if err != nil {
+		log.Fatal().Err(err).Msgf("could not get finalView for current epoch from snapshot")
+	}
+
+	// calculate numViewInEpoch
+	numViewsInEpoch := (finalView + 1) - head.View
+
+	// TODO: numViewsInStakingAuction, numViewsInDKGPhase
+
+	// number of collectors clusters
+	clustering, err := currentEpoch.Clustering()
+	if err != nil {
+		log.Fatal().Err(err).Msgf("could not get clustering for current epoch from snapshot")
+	}
+	numCollectorClusters := len(clustering)
+
+	// random source
+	randomSource, err := currentEpoch.RandomSource()
+	if err != nil {
+		log.Fatal().Err(err).Msgf("could not get randomSource for current epoch from snapshot")
+	}
+
+	// TODO: collector clusters, DKG, cluster QC
 }
