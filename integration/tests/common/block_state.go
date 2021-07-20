44b75feaeb6fededae4db96397dd663ef7f1098f
Yahya <yahya@dapperlabs.com>
2021-07-20 11:10:47 -0700

    adds more log to highest finalized for debugging

--- integration/tests/common/block_state.go
+++ integration/tests/common/block_state.go
@@ -114,6 +114,7 @@ func (bs *BlockState) processAncestors(b *messages.BlockProposal, confirmsHeight
 func (bs *BlockState) WaitForHighestFinalizedProgress(t *testing.T) *messages.BlockProposal {
 	currentFinalized := bs.highestFinalized
 	require.Eventually(t, func() bool {
+		fmt.Printf("=> checking highest finalized: %d \n", bs.highestFinalized)
 		return bs.highestFinalized > currentFinalized
 	}, blockStateTimeout, 100*time.Millisecond,
 		fmt.Sprintf("did not receive progress on highest finalized height (%v) from (%v) within %v seconds",
