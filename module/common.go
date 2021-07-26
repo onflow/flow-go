8605ee7b610d3a80d8ea59cfe9827ab619d6adb6
Simon Zhu <simon.zsiyan@gmail.com>
2021-07-26 16:38:48 -0700

    Update common.go

--- module/common.go
+++ module/common.go
@@ -1,7 +1,15 @@
 package module
 
-// ReadyDoneAware provides easy interface to wait for module startup and shutdown
+// ReadyDoneAware provides easy interface to wait for module startup and shutdown.
+// Modules that implement this interface only support a single start-stop cycle,
+// and if Ready() is called again after shutdown has already commenced, the module
+// will not restart.
 type ReadyDoneAware interface {
+	// Ready commences startup of the module, and returns a ready channel that is closed once
+	// startup has completed.
 	Ready() <-chan struct{}
+
+	// Done commences shutdown of the module, and returns a done channel that is closed once
+	// shutdown has completed.
 	Done() <-chan struct{}
 }
