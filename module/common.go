a39d30110c40398753db15af75fd6e6274806328
Simon Zhu <simon.zsiyan@gmail.com>
2021-07-26 17:05:22 -0700

    Update common.go

--- module/common.go
+++ module/common.go
@@ -2,14 +2,13 @@ package module
 
 // ReadyDoneAware provides easy interface to wait for module startup and shutdown.
 // Modules that implement this interface only support a single start-stop cycle,
-// and if Ready() is called again after shutdown has already commenced, the module
-// will not restart.
+// and will not restart if Ready() is called again after shutdown has already commenced.
 type ReadyDoneAware interface {
 	// Ready commences startup of the module, and returns a ready channel that is closed once
-	// startup has completed.
+	// startup has completed. This is an idempotent method.
 	Ready() <-chan struct{}
 
 	// Done commences shutdown of the module, and returns a done channel that is closed once
-	// shutdown has completed.
+	// shutdown has completed. This is an idempotent method.
 	Done() <-chan struct{}
 }
