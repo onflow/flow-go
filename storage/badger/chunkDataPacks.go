72d31de2170880eccb782b7687f0f20cf20f5a9e
Yahya <yahya@dapperlabs.com>
2021-07-29 16:45:47 -0700

    fixes lint

--- storage/badger/chunkDataPacks.go
+++ storage/badger/chunkDataPacks.go
@@ -9,6 +9,7 @@ import (
 	"github.com/onflow/flow-go/module"
 	"github.com/onflow/flow-go/module/metrics"
 	"github.com/onflow/flow-go/storage"
+	badgermodel "github.com/onflow/flow-go/storage/badger/model"
 	"github.com/onflow/flow-go/storage/badger/operation"
 	"github.com/onflow/flow-go/storage/badger/transaction"
 )
@@ -19,29 +20,19 @@ type ChunkDataPacks struct {
 	byChunkIDCache *Cache
 }
 
-// storedChunkDataPack is an in-storage representation of chunk data pack.
-// Its prime difference is instead of an actual collection, it keeps a collection ID hence relying on maintaining
-// the collection on a secondary storage.
-type storedChunkDataPack struct {
-	ChunkID      flow.Identifier
-	StartState   flow.StateCommitment
-	Proof        flow.StorageProof
-	CollectionID *flow.Identifier
-}
-
 func NewChunkDataPacks(collector module.CacheMetrics, db *badger.DB, collections storage.Collections, byChunkIDCacheSize uint) *ChunkDataPacks {
 
 	store := func(key interface{}, val interface{}) func(*transaction.Tx) error {
-		chdp := val.(*storedChunkDataPack)
+		chdp := val.(*badgermodel.StoredChunkDataPack)
 		return transaction.WithTx(operation.SkipDuplicates(operation.InsertChunkDataPack(chdp)))
 	}
 
 	retrieve := func(key interface{}) func(tx *badger.Txn) (interface{}, error) {
 		chunkID := key.(flow.Identifier)
 
-		var c storedChunkDataPack
+		var c *badgermodel.StoredChunkDataPack
 		return func(tx *badger.Txn) (interface{}, error) {
-			err := operation.RetrieveChunkDataPack(chunkID, &c)(tx)
+			err := operation.RetrieveChunkDataPack(chunkID, c)(tx)
 			return &c, err
 		}
 	}
@@ -115,18 +106,18 @@ func (ch *ChunkDataPacks) ByChunkID(chunkID flow.Identifier) (*flow.ChunkDataPac
 	return chdp, nil
 }
 
-func (ch *ChunkDataPacks) retrieveCHDP(chunkID flow.Identifier) func(*badger.Txn) (*storedChunkDataPack, error) {
-	return func(tx *badger.Txn) (*storedChunkDataPack, error) {
+func (ch *ChunkDataPacks) retrieveCHDP(chunkID flow.Identifier) func(*badger.Txn) (*badgermodel.StoredChunkDataPack, error) {
+	return func(tx *badger.Txn) (*badgermodel.StoredChunkDataPack, error) {
 		val, err := ch.byChunkIDCache.Get(chunkID)(tx)
 		if err != nil {
 			return nil, err
 		}
-		return val.(*storedChunkDataPack), nil
+		return val.(*badgermodel.StoredChunkDataPack), nil
 	}
 }
 
-func toStoredChunkDataPack(c *flow.ChunkDataPack) *storedChunkDataPack {
-	sc := &storedChunkDataPack{
+func toStoredChunkDataPack(c *flow.ChunkDataPack) *badgermodel.StoredChunkDataPack {
+	sc := &badgermodel.StoredChunkDataPack{
 		ChunkID:    c.ChunkID,
 		StartState: c.StartState,
 		Proof:      c.Proof,
--- storage/badger/operation/chunkDataPacks.go
+++ storage/badger/operation/chunkDataPacks.go
@@ -4,21 +4,21 @@ import (
 	"github.com/dgraph-io/badger/v2"
 
 	"github.com/onflow/flow-go/model/flow"
-	storagemodel "github.com/onflow/flow-go/storage/badger/model"
+	badgermodel "github.com/onflow/flow-go/storage/badger/model"
 )
 
 // InsertChunkDataPack inserts a chunk data pack keyed by chunk ID.
-func InsertChunkDataPack(c *storagemodel.StoredChunkDataPack) func(*badger.Txn) error {
+func InsertChunkDataPack(c *badgermodel.StoredChunkDataPack) func(*badger.Txn) error {
 	return insert(makePrefix(codeChunkDataPack, c.ChunkID), c)
 }
 
 // BatchInsertChunkDataPack inserts a chunk data pack keyed by chunk ID into a batch
-func BatchInsertChunkDataPack(c *storagemodel.StoredChunkDataPack) func(batch *badger.WriteBatch) error {
+func BatchInsertChunkDataPack(c *badgermodel.StoredChunkDataPack) func(batch *badger.WriteBatch) error {
 	return batchInsert(makePrefix(codeChunkDataPack, c.ChunkID), c)
 }
 
 // RetrieveChunkDataPack retrieves a chunk data pack by chunk ID.
-func RetrieveChunkDataPack(chunkID flow.Identifier, c *storagemodel.StoredChunkDataPack) func(*badger.Txn) error {
+func RetrieveChunkDataPack(chunkID flow.Identifier, c *badgermodel.StoredChunkDataPack) func(*badger.Txn) error {
 	return retrieve(makePrefix(codeChunkDataPack, chunkID), c)
 }
 
