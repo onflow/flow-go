7822a97d86a4d4263e575545b4d44467fa9b6272
simonhf <simonhf@gmail.com>
2021-07-29 14:57:58 -0700

    Merciless refactor require.Nil to require.NoError

--- model/flow/service_event_test.go
+++ model/flow/service_event_test.go
@@ -37,19 +37,19 @@ func TestEncodeDecode(t *testing.T) {
 	t.Run("json", func(t *testing.T) {
 		t.Run("specific event types", func(t *testing.T) {
 			b, err := json.Marshal(setup)
-			require.Nil(t, err)
+			require.NoError(t, err)
 
 			gotSetup := new(flow.EpochSetup)
 			err = json.Unmarshal(b, gotSetup)
-			require.Nil(t, err)
+			require.NoError(t, err)
 			assert.DeepEqual(t, setup, gotSetup)
 
 			b, err = json.Marshal(commit)
-			require.Nil(t, err)
+			require.NoError(t, err)
 
 			gotCommit := new(flow.EpochCommit)
 			err = json.Unmarshal(b, gotCommit)
-			require.Nil(t, err)
+			require.NoError(t, err)
 			assert.DeepEqual(t, commit, gotCommit, cmp.FilterValues(func(a, b crypto.PublicKey) bool {
 				return true
 			}, cmp.Comparer(func(a, b crypto.PublicKey) bool {
@@ -59,24 +59,24 @@ func TestEncodeDecode(t *testing.T) {
 
 		t.Run("generic type", func(t *testing.T) {
 			b, err := json.Marshal(setup.ServiceEvent())
-			require.Nil(t, err)
+			require.NoError(t, err)
 
 			outer := new(flow.ServiceEvent)
 			err = json.Unmarshal(b, outer)
-			require.Nil(t, err)
+			require.NoError(t, err)
 			gotSetup, ok := outer.Event.(*flow.EpochSetup)
 			require.True(t, ok)
 			assert.DeepEqual(t, setup, gotSetup)
 
 			t.Logf("- debug: setup.ServiceEvent()=%+v\n", setup.ServiceEvent())
 			b, err = json.Marshal(commit.ServiceEvent())
-			require.Nil(t, err)
+			require.NoError(t, err)
 
 			outer = new(flow.ServiceEvent)
 			t.Logf("- debug: outer=%+v <-- before .Unmarshal()\n", outer)
 			err = json.Unmarshal(b, outer)
 			t.Logf("- debug: outer=%+v <-- after .Unmarshal()\n", outer)
-			require.Nil(t, err)
+			require.NoError(t, err)
 			gotCommit, ok := outer.Event.(*flow.EpochCommit)
 			require.True(t, ok)
 			assert.DeepEqual(t, commit, gotCommit, cmp.FilterValues(func(a, b crypto.PublicKey) bool {
@@ -90,19 +90,19 @@ func TestEncodeDecode(t *testing.T) {
 	t.Run("msgpack", func(t *testing.T) {
 		t.Run("specific event types", func(t *testing.T) {
 			b, err := msgpack.Marshal(setup)
-			require.Nil(t, err)
+			require.NoError(t, err)
 
 			gotSetup := new(flow.EpochSetup)
 			err = msgpack.Unmarshal(b, gotSetup)
-			require.Nil(t, err)
+			require.NoError(t, err)
 			assert.DeepEqual(t, setup, gotSetup)
 
 			b, err = msgpack.Marshal(commit)
-			require.Nil(t, err)
+			require.NoError(t, err)
 
 			gotCommit := new(flow.EpochCommit)
 			err = msgpack.Unmarshal(b, gotCommit)
-			require.Nil(t, err)
+			require.NoError(t, err)
 			assert.DeepEqual(t, commit, gotCommit, cmp.FilterValues(func(a, b crypto.PublicKey) bool {
 				return true
 			}, cmp.Comparer(func(a, b crypto.PublicKey) bool {
@@ -112,24 +112,24 @@ func TestEncodeDecode(t *testing.T) {
 
 		t.Run("generic type", func(t *testing.T) {
 			b, err := msgpack.Marshal(setup.ServiceEvent())
-			require.Nil(t, err)
+			require.NoError(t, err)
 
 			outer := new(flow.ServiceEvent)
 			err = msgpack.Unmarshal(b, outer)
-			require.Nil(t, err)
+			require.NoError(t, err)
 			gotSetup, ok := outer.Event.(*flow.EpochSetup)
 			require.True(t, ok)
 			assert.DeepEqual(t, setup, gotSetup)
 
 			t.Logf("- debug: setup.ServiceEvent()=%+v\n", setup.ServiceEvent())
 			b, err = msgpack.Marshal(commit.ServiceEvent())
-			require.Nil(t, err)
+			require.NoError(t, err)
 
 			outer = new(flow.ServiceEvent)
 			t.Logf("- debug: outer=%+v <-- before .Unmarshal()\n", outer)
 			err = msgpack.Unmarshal(b, outer)
 			t.Logf("- debug: outer=%+v <-- after .Unmarshal()\n", outer)
-			require.Nil(t, err)
+			require.NoError(t, err)
 			gotCommit, ok := outer.Event.(*flow.EpochCommit)
 			require.True(t, ok)
 			assert.DeepEqual(t, commit, gotCommit, cmp.FilterValues(func(a, b crypto.PublicKey) bool {
@@ -143,19 +143,19 @@ func TestEncodeDecode(t *testing.T) {
 	t.Run("cbor", func(t *testing.T) {
 		t.Run("specific event types", func(t *testing.T) {
 			b, err := cborEncMode.Marshal(setup)
-			require.Nil(t, err)
+			require.NoError(t, err)
 
 			gotSetup := new(flow.EpochSetup)
 			err = cbor.Unmarshal(b, gotSetup)
-			require.Nil(t, err)
+			require.NoError(t, err)
 			assert.DeepEqual(t, setup, gotSetup)
 
 			b, err = cborEncMode.Marshal(commit)
-			require.Nil(t, err)
+			require.NoError(t, err)
 
 			gotCommit := new(flow.EpochCommit)
 			err = cbor.Unmarshal(b, gotCommit)
-			require.Nil(t, err)
+			require.NoError(t, err)
 			assert.DeepEqual(t, commit, gotCommit, cmp.FilterValues(func(a, b crypto.PublicKey) bool {
 				return true
 			}, cmp.Comparer(func(a, b crypto.PublicKey) bool {
@@ -166,23 +166,23 @@ func TestEncodeDecode(t *testing.T) {
 		t.Run("generic type", func(t *testing.T) {
 			t.Logf("- debug: setup.ServiceEvent()=%+v\n", setup.ServiceEvent())
 			b, err := cborEncMode.Marshal(setup.ServiceEvent())
-			require.Nil(t, err)
+			require.NoError(t, err)
 
 			outer := new(flow.ServiceEvent)
 			t.Logf("- debug: outer=%+v <-- before .Unmarshal()\n", outer)
 			err = cbor.Unmarshal(b, outer)
 			t.Logf("- debug: outer=%+v <-- after .Unmarshal()\n", outer)
-			require.Nil(t, err)
+			require.NoError(t, err)
 			gotSetup, ok := outer.Event.(*flow.EpochSetup)
 			require.True(t, ok)
 			assert.DeepEqual(t, setup, gotSetup)
 
 			b, err = cborEncMode.Marshal(commit.ServiceEvent())
-			require.Nil(t, err)
+			require.NoError(t, err)
 
 			outer = new(flow.ServiceEvent)
 			err = cbor.Unmarshal(b, outer)
-			require.Nil(t, err)
+			require.NoError(t, err)
 			gotCommit, ok := outer.Event.(*flow.EpochCommit)
 			require.True(t, ok)
 			assert.DeepEqual(t, commit, gotCommit, cmp.FilterValues(func(a, b crypto.PublicKey) bool {
