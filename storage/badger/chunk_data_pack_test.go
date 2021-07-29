8c21c114fe3089fb66a0a2f18cb38f7c888e5e13
Yahya <yahya@dapperlabs.com>
2021-07-29 11:39:41 -0700

    fixes tests

--- storage/badger/chunk_data_pack_test.go
+++ storage/badger/chunk_data_pack_test.go
@@ -9,7 +9,6 @@ import (
 	"github.com/onflow/flow-go/module/metrics"
 	"github.com/onflow/flow-go/storage"
 	badgerstorage "github.com/onflow/flow-go/storage/badger"
-	storagemodel "github.com/onflow/flow-go/storage/badger/model"
 	"github.com/onflow/flow-go/utils/unittest"
 
 	"github.com/stretchr/testify/assert"
@@ -18,7 +17,9 @@ import (
 
 func TestChunkDataPack(t *testing.T) {
 	unittest.RunWithBadgerDB(t, func(db *badger.DB) {
-		store := badgerstorage.NewChunkDataPacks(&metrics.NoopCollector{}, db, 100)
+		transactions := badgerstorage.NewTransactions(&metrics.NoopCollector{}, db)
+		collections := badgerstorage.NewCollections(db, transactions)
+		store := badgerstorage.NewChunkDataPacks(&metrics.NoopCollector{}, db, collections, 100)
 
 		// attempt to get an invalid
 		_, err := store.ByChunkID(unittest.IdentifierFixture())
@@ -27,22 +28,19 @@ func TestChunkDataPack(t *testing.T) {
 		// store in db
 		chunkID := unittest.IdentifierFixture()
 		expected := unittest.ChunkDataPackFixture(chunkID)
+		collection := expected.Collection
+
+		// stores collection in Collections storage (which ChunkDataPacks store uses internally)
+		err = collections.Store(collection)
+		require.NoError(t, err)
+
 		err = store.Store(expected)
 		require.NoError(t, err)
 
 		// retrieve the transaction by ID
 		actual, err := store.ByChunkID(chunkID)
 		require.NoError(t, err)
-
-		// Note that ChunkDataPack is stored and retrieved as StoredChunkDataPack.
-		collID := expected.Collection.ID()
-		expectedStored := &storagemodel.StoredChunkDataPack{
-			ChunkID:      expected.ChunkID,
-			StartState:   expected.StartState,
-			Proof:        expected.Proof,
-			CollectionID: &collID,
-		}
-		assert.Equal(t, expectedStored, actual)
+		assert.Equal(t, expected, actual)
 
 		// re-insert - should be idempotent
 		err = store.Store(expected)
