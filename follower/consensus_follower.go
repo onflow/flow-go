7026a7ac5659f1d1d620cb32101f0a3de0c09510
Simon Zhu <simon.zsiyan@gmail.com>
2021-07-30 11:25:13 -0700

    Add godoc

--- follower/consensus_follower.go
+++ follower/consensus_follower.go
@@ -8,21 +8,34 @@ import (
 	"github.com/onflow/flow-go/module"
 )
 
+// ConsensusFollower is a standalone module run by third parties which provides
+// a mechanism for observing the block chain. It maintains a set of subscribers
+// and delivers block proposals broadcasted by the consensus nodes to each one.
 type ConsensusFollower interface {
+	// Run starts the consensus follower.
 	Run()
-	Subscribe(module.HotStuffFollower)
+
+	// Subscribe adds a new consensus subscriber.
+	Subscribe(ConsensusFollowerSubscriber)
+}
+
+// ConsensusFollowerSubscriber is the interface subscribers must implement to subscribe to the ConsensusFollower.
+type ConsensusFollowerSubscriber interface {
+	// NewBlockProposal is called by ConsensusFollower upon each new block proposal it receives from consensus.
+	NewBlockProposal(*flow.Header, uint64)
 }
 
 type ConsensusFollowerImpl struct {
+	ConsensusFollower
 	relayer blockProposalRelayer
 	options ConsensusFollowerOptions
 }
 
 type ConsensusFollowerOptions struct {
-	upstreamNodeID string
-	bindAddr       string
-	networkKey     crypto.PrivateKey
-	datadir        string
+	upstreamNodeID string            // node ID of the upstream access node
+	bindAddr       string            // network address to bind on
+	networkKey     crypto.PrivateKey // network private key
+	datadir        string            // directory to store the protocol state
 }
 
 func NewConsensusFollower(opts ConsensusFollowerOptions) ConsensusFollower {
@@ -40,7 +53,7 @@ func (cf *ConsensusFollowerImpl) Run() {
 	// startup sync engine
 }
 
-func (cf *ConsensusFollowerImpl) Subscribe(subscriber module.HotStuffFollower) {
+func (cf *ConsensusFollowerImpl) Subscribe(subscriber ConsensusFollowerSubscriber) {
 	cf.relayer.AddFollower(subscriber)
 }
 
@@ -51,22 +64,27 @@ type blockProposalRelayer struct {
 	module.HotStuffFollower
 
 	followersMu sync.RWMutex
-	followers   map[module.HotStuffFollower]struct{} // stores registered followers
+	followers   map[ConsensusFollowerSubscriber]struct{} // stores registered followers
 }
 
-func (r *blockProposalRelayer) AddFollower(follower module.HotStuffFollower) {
+// AddFollower registers a new follower with the block proposal relayer.
+func (r *blockProposalRelayer) AddFollower(follower ConsensusFollowerSubscriber) {
 	r.followersMu.Lock()
 	defer r.followersMu.Unlock()
 
 	r.followers[follower] = struct{}{}
 }
 
+// SubmitProposal feeds a new block proposal into the block proposal relayer.
+// The relayer will forward this block proposal to all registered followers.
 func (r *blockProposalRelayer) SubmitProposal(proposal *flow.Header, parentView uint64) {
 	r.followersMu.RLock()
 	for follower := range r.followers {
 		r.followersMu.RUnlock()
-		// TODO: introduce a queueing mechanism or make this call asynchronously
-		follower.SubmitProposal(proposal, parentView)
+
+		// TODO: introduce a queueing mechanism or make this call asynchronous
+		follower.NewBlockProposal(proposal, parentView)
+
 		r.followersMu.RLock()
 	}
 	r.followersMu.RUnlock()
