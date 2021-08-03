b16eb2f23a5e6079da0f69849e5dbb921b2c0f60
Yahya <yahya@dapperlabs.com>
2021-08-03 13:53:52 -0700

    adds factory

--- network/p2p/dns/resolver.go
+++ network/p2p/dns/resolver.go
@@ -5,10 +5,23 @@ import (
 	"net"
 
 	madns "github.com/multiformats/go-multiaddr-dns"
+	"github.com/rs/zerolog"
+
+	"github.com/onflow/flow-go/module/metrics"
 )
 
 type Resolver struct {
-	res madns.BasicResolver
+	res       madns.BasicResolver
+	collector metrics.NetworkCollector
+	logger    zerolog.Logger
+}
+
+func NewResolver(res madns.BasicResolver, collector metrics.NetworkCollector, logger zerolog.Logger) *Resolver {
+	return &Resolver{
+		res:       res,
+		collector: collector,
+		logger:    logger.With().Str("module", "dns-resolver").Logger(),
+	}
 }
 
 func (r *Resolver) LookupIPAddr(ctx context.Context, domain string) ([]net.IPAddr, error) {
