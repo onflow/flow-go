cf1b75636a202f20cd6d573a45b051476d271b5a
Yahya <yahya@dapperlabs.com>
2021-08-04 13:59:43 -0700

    adds dns lookup time collector for resolver

--- network/p2p/dns/resolver.go
+++ network/p2p/dns/resolver.go
@@ -3,6 +3,7 @@ package dns
 import (
 	"context"
 	"net"
+	"time"
 
 	madns "github.com/multiformats/go-multiaddr-dns"
 	"github.com/rs/zerolog"
@@ -16,18 +17,23 @@ type Resolver struct {
 	logger    zerolog.Logger
 }
 
-func NewResolver(res madns.BasicResolver, collector metrics.NetworkCollector, logger zerolog.Logger) *Resolver {
+func NewResolver(res madns.BasicResolver, collector metrics.NetworkCollector) *Resolver {
 	return &Resolver{
 		res:       res,
 		collector: collector,
-		logger:    logger.With().Str("module", "dns-resolver").Logger(),
 	}
 }
 
 func (r *Resolver) LookupIPAddr(ctx context.Context, domain string) ([]net.IPAddr, error) {
+	started := time.Now()
+	defer r.collector.DNSLookupDuration(time.Since(started))
+
 	return r.res.LookupIPAddr(ctx, domain)
 }
 
 func (r *Resolver) LookupTXT(ctx context.Context, txt string) ([]string, error) {
+	started := time.Now()
+	defer r.collector.DNSLookupDuration(time.Since(started))
+
 	return r.res.LookupTXT(ctx, txt)
 }
