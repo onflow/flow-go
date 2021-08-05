6f4071544316bd69eca9ad1d8868d598138dbac8
Yahya <yahya@dapperlabs.com>
2021-08-04 18:23:47 -0700

    fixes defer issue

--- network/p2p/dns/resolver.go
+++ network/p2p/dns/resolver.go
@@ -24,14 +24,20 @@ func NewResolver(collector module.NetworkMetrics) (*madns.Resolver, error) {
 
 func (r *Resolver) LookupIPAddr(ctx context.Context, domain string) ([]net.IPAddr, error) {
 	started := time.Now()
-	defer r.collector.DNSLookupDuration(time.Since(started))
 
-	return r.res.LookupIPAddr(ctx, domain)
+	addr, err := r.res.LookupIPAddr(ctx, domain)
+
+	r.collector.DNSLookupDuration(time.Since(started))
+
+	return addr, err
 }
 
 func (r *Resolver) LookupTXT(ctx context.Context, txt string) ([]string, error) {
 	started := time.Now()
-	defer r.collector.DNSLookupDuration(time.Since(started))
 
-	return r.res.LookupTXT(ctx, txt)
+	addr, err := r.res.LookupTXT(ctx, txt)
+
+	r.collector.DNSLookupDuration(time.Since(started))
+
+	return addr, err
 }
