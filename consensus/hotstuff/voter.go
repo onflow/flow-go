bd16abb3d4f2e76e44b8e419a5e3a79bf3f79907
Alexander Hentschel <alex.hentschel@axiomzen.co>
2021-08-04 21:32:40 -0700

    suggestions for VoteAggregator

--- consensus/hotstuff/voter.go
+++ consensus/hotstuff/voter.go
@@ -6,9 +6,10 @@ import (
 
 // Voter produces votes for the given block according to voting rules.
 type Voter interface {
-	// Voter takes a block and current view, and decide whether to vote for the block.
+	// ProduceVoteIfVotable takes a block and current view, and decides whether to vote for the block.
 	// If it decides to vote, it returns (&vote, nil). The call will call into VoteAggregator's GetVoteCreator method to get a
 	// `createVote` function and create a vote with it,
 	// If it decides not to vote, it returns (nil, NoVoteError)
+	// ToDo: document sentinel errors expected during normal operation
 	ProduceVoteIfVotable(block *model.Block, curView uint64) (*model.Vote, error)
 }
