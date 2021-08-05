36bb1c3f02470ab7c9cff42ab2949a760f2dfd0f
Yahya <yahya@dapperlabs.com>
2021-08-05 14:52:21 -0700

    adds resolving txt cache

--- network/p2p/dns/resolver.go
+++ network/p2p/dns/resolver.go
@@ -26,7 +26,7 @@ type ipCacheEntry struct {
 }
 
 type txtCacheEntry struct {
-	addresses []net.IPAddr
+	addresses []string
 	timestamp time.Time
 }
 
@@ -71,6 +71,26 @@ func (r *Resolver) resolveIPCache(domain string) ([]net.IPAddr, bool) {
 	if time.Now().After(entry.timestamp.Add(r.ttl)) {
 		// invalidates cache entry
 		delete(r.ipCache, domain)
+		return nil, false
+	}
+
+	return entry.addresses, true
+}
+
+// resolveIPCache resolves the txt through the cache if it is available.
+func (r *Resolver) resolveTXTCache(txt string) ([]string, bool) {
+	r.Lock()
+	defer r.Unlock()
+
+	entry, ok := r.txtCache[txt]
+
+	if !ok {
+		return nil, false
+	}
+
+	if time.Now().After(entry.timestamp.Add(r.ttl)) {
+		// invalidates cache entry
+		delete(r.txtCache, txt)
 		return nil, false
 	}
 
