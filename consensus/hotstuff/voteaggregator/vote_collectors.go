5b20228a7a9bd2b746822dfe5df8bec2063cfaf1
Yurii Oleksyshyn <yuraolex@gmail.com>
2021-08-06 14:14:00 +0300

    Implementing pruning by view

--- consensus/hotstuff/voteaggregator/vote_collectors.go
+++ consensus/hotstuff/voteaggregator/vote_collectors.go
@@ -14,12 +14,19 @@ type voteCollectors struct {
 	collectors       map[flow.Identifier]hotstuff.VoteCollector // blockID -> VoteCollector
 }
 
+// GetOrCreateCollector performs lazy initialization of collectors based on their view and blockID
 func (v *voteCollectors) GetOrCreateCollector(view uint64, blockID flow.Identifier) (hotstuff.VoteCollector, error) {
 	panic("implement me")
 }
 
+// PruneByView prunes all collectors which target same view
 func (v *voteCollectors) PruneByView(view uint64) error {
-	panic("implement me")
+	v.lock.Lock()
+	defer v.lock.Unlock()
+	for blockID := range v.viewToBlockIDSet[view] {
+		delete(v.collectors, blockID)
+	}
+	return nil
 }
 
 func (v *voteCollectors) ProcessBlock(block *model.Block) error {
