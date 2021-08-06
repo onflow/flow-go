0e8861e53cc27be5e3c736d59cd30275823bcb77
Yahya <yahya@dapperlabs.com>
2021-08-06 16:53:25 -0700

    adds net IP address fixture

--- network/p2p/dns/resolver_test.go
+++ network/p2p/dns/resolver_test.go
@@ -1,6 +1,8 @@
 package dns
 
 import (
+	"math/rand"
+	"net"
 	"testing"
 
 	"github.com/stretchr/testify/require"
@@ -14,4 +16,17 @@ func TestResolver(t *testing.T) {
 	_, err := NewResolver(metrics.NewNoopCollector(), WithBasicResolver(&basicResolver))
 	require.NoError(t, err)
 
+	basicResolver.On("LookupIPAddr", "example1.com").Return([]net.IPAddr{})
+}
+
+func netIPAddrFixture() []net.IPAddr {
+	token := make([]byte, 4)
+	rand.Read(token)
+
+	ip := net.IPAddr{
+		IP:   net.IPv4(token[0], token[1], token[2], token[3]),
+		Zone: "flow",
+	}
+
+	return []net.IPAddr{ip}
 }
