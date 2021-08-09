c15e966523f7d82d4b9daf55e5e17bd88cf581fe
vishal <1117327+vishalchangrani@users.noreply.github.com>
2021-08-08 18:43:47 -0700

    adding a NotValidator; changing the meaning of the SenderValidator

--- network/validator/notValidator.go
+++ network/validator/notValidator.go
@@ -7,11 +7,12 @@ import (
 
 var _ network.MessageValidator = &NotValidator{}
 
+// NotValidator returns the opposite result of the given validator for the Validate call
 type NotValidator struct {
 	validator network.MessageValidator
 }
 
-func NewNotValidator(validator network.MessageValidator) *NotValidator {
+func NewNotValidator(validator network.MessageValidator) network.MessageValidator {
 	return &NotValidator{
 		validator: validator,
 	}
@@ -20,4 +21,3 @@ func NewNotValidator(validator network.MessageValidator) *NotValidator {
 func (n NotValidator) Validate(msg message.Message) bool {
 	return !n.validator.Validate(msg)
 }
-
