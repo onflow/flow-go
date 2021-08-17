5101b318bb418cd07c1ee09a36d5d1d1bdfd4d6e
Yahya <yahya@dapperlabs.com>
2021-08-17 12:26:03 -0700

    keeps only the collection

--- engine/execution/ingestion/engine.go
+++ engine/execution/ingestion/engine.go
@@ -1122,11 +1122,12 @@ func (e *Engine) saveExecutionResults(
 	for _, cdp := range chdps {
 		lg := e.log.With().
 			Hex("chunk_id", logging.ID(cdp.ChunkID)).
-			Hex("start_state", cdp.StartState[:]).Logger()
+			// Hex("start_state", cdp.StartState[:]).
+			Logger()
 
-		if cdp.Proof != nil && len(cdp.Proof) > 0 {
-			lg = lg.With().Hex("proof", cdp.Proof[:]).Logger()
-		}
+		//if cdp.Proof != nil && len(cdp.Proof) > 0 {
+		//	lg = lg.With().Hex("proof", cdp.Proof[:]).Logger()
+		//}
 
 		if cdp.Collection != nil {
 			lg = lg.With().Interface("collection", cdp.Collection).Logger()
