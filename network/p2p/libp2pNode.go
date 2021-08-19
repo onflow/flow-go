91ed9287fa0cec599387454823be2caafaa17bce
Yahya <yahya@dapperlabs.com>
2021-08-19 11:07:08 -0700

    adds a todo

--- network/p2p/libp2pNode.go
+++ network/p2p/libp2pNode.go
@@ -56,10 +56,12 @@ func DefaultLibP2PNodeFactory(ctx context.Context, log zerolog.Logger, me flow.I
 
 	connGater := NewConnGater(log)
 
+	// TODO: uncomment following lines to activate dns caching
 	//resolver, err := dns.NewResolver(metrics)
 	//if err != nil {
 	//	return nil, fmt.Errorf("could not create dns resolver: %w", err)
 	//}
+
 	return func() (*Node, error) {
 		return NewDefaultLibP2PNodeBuilder(me, address, flowKey).
 			SetRootBlockID(rootBlockID).
