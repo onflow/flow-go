f5af698283a383e27fac34ae4e80bb269cfa9408
Danu <me@danu.io>
2021-08-19 17:12:53 +0100

    Update module/signature/invalid_threshold.go
    Co-authored-by: Leo Zhang <zhangchiqing@gmail.com>

--- module/signature/invalid_threshold.go
+++ module/signature/invalid_threshold.go
@@ -5,8 +5,8 @@ package signature
 import "github.com/onflow/flow-go/crypto"
 
 // This implementation of `ThresholdProvider` always produces invalid signatures
-// (that are inexpensively detectable as invalid) as we cannot produce valid signatures.
-// Used when this node fails to generate a random beacon key during the last DKG.
+// (that are inexpensively detectable as invalid) as we cannot produce valid signatures
+// due to some failure in generating random beacon key during the last DKG.
 // This will be replaced by consensus voting v2, which will be able to gracefully
 // handle consensus nodes submitting votes with only one signature.
 type InvalidThresholdProvider struct {
