d40158e7dd0edaec6693ce7c7193dd86e0158551
Simon Zhu <simon.zsiyan@gmail.com>
2021-08-20 16:05:51 -0700

    add test for hierchical translator

--- network/p2p/hierarchical_translator_test.go
+++ network/p2p/hierarchical_translator_test.go
@@ -3,13 +3,56 @@ package p2p
 import (
 	"testing"
 
+	"github.com/libp2p/go-libp2p-core/peer"
+	"github.com/onflow/flow-go/model/flow"
+	"github.com/onflow/flow-go/utils/unittest"
+	"github.com/stretchr/testify/require"
 	"github.com/stretchr/testify/suite"
+	"gotest.tools/assert"
 )
 
 type HierarchicalTranslatorTestSuite struct {
 	suite.Suite
+	translator *HierarchicalIDTranslator
+	ids        flow.IdentityList
+}
+
+func (suite *HierarchicalTranslatorTestSuite) SetupTest() {
+	suite.ids = unittest.IdentityListFixture(2, unittest.WithKeys)
+	t1, err := NewFixedTableIdentityTranslator(suite.ids[:1])
+	require.NoError(suite.T(), err)
+	t2, err := NewFixedTableIdentityTranslator(suite.ids[1:])
+	require.NoError(suite.T(), err)
+
+	suite.translator = NewHierarchicalIDTranslator(t1, t2)
 }
 
 func TestHierarchicalTranslator(t *testing.T) {
 	suite.Run(t, new(HierarchicalTranslatorTestSuite))
+}
+
+func (suite *HierarchicalTranslatorTestSuite) TestFirstTranslatorSuccess() {
+	pid, err := suite.translator.GetPeerID(suite.ids[0].NodeID)
+	require.NoError(suite.T(), err)
+	fid, err := suite.translator.GetFlowID(pid)
+	assert.Equal(suite.T(), fid, suite.ids[0].NodeID)
+}
+
+func (suite *HierarchicalTranslatorTestSuite) TestSecondTranslatorSuccess() {
+	pid, err := suite.translator.GetPeerID(suite.ids[1].NodeID)
+	require.NoError(suite.T(), err)
+	fid, err := suite.translator.GetFlowID(pid)
+	assert.Equal(suite.T(), fid, suite.ids[1].NodeID)
+}
+
+func (suite *HierarchicalTranslatorTestSuite) TestTranslationFailure() {
+	fid := unittest.IdentifierFixture()
+	_, err := suite.translator.GetPeerID(fid)
+	require.Error(suite.T(), err)
+
+	key, err := LibP2PPrivKeyFromFlow(generateNetworkingKey(suite.T()))
+	require.NoError(suite.T(), err)
+	pid, err := peer.IDFromPrivateKey(key)
+	_, err = suite.translator.GetFlowID(pid)
+	require.Error(suite.T(), err)
 }
