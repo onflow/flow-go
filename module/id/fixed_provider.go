bf93947a74de844c7dd3d34977dc876e50e688e0
Simon Zhu <simon.zsiyan@gmail.com>
2021-08-20 10:13:31 -0700

    Update libp2pNode_test.go
    Update middleware_test.go
    
    Update testUtil.go
    
    fixed identity provider
    
    Update testUtil.go
    
    [network] Fix the compilation of a bunch of remaining tests

--- module/id/fixed_provider.go
+++ module/id/fixed_provider.go
@@ -15,3 +15,15 @@ func NewFixedIdentifierProvider(identifiers flow.IdentifierList) *FixedIdentifie
 func (p *FixedIdentifierProvider) Identifiers() flow.IdentifierList {
 	return p.identifiers
 }
+
+type FixedIdentityProvider struct {
+	identities flow.IdentityList
+}
+
+func NewFixedIdentityProvider(identities flow.IdentityList) *FixedIdentityProvider {
+	return &FixedIdentityProvider{identities}
+}
+
+func (p *FixedIdentityProvider) Identities(flow.IdentityFilter) flow.IdentityList {
+	return p.identities
+}
