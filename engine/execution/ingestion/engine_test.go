31aa432c0f61c4c6d00fa7775e6c18e82009ee1f
Yahya <yahya@dapperlabs.com>
2021-08-20 12:37:15 -0700

    fixes tests

--- engine/execution/ingestion/engine_test.go
+++ engine/execution/ingestion/engine_test.go
@@ -231,7 +231,6 @@ func (ctx *testingContext) assertSuccessfulBlockComputation(
 	mocked := ctx.executionState.
 		On("PersistExecutionState",
 			mock.Anything,
-			mock.Anything,
 			executableBlock.Block.Header,
 			newStateCommitment,
 			mock.MatchedBy(func(fs []*flow.ChunkDataPack) bool {
@@ -257,8 +256,8 @@ func (ctx *testingContext) assertSuccessfulBlockComputation(
 			//lock.Lock()
 			//defer lock.Unlock()
 
-			blockID := args[2].(*flow.Header).ID()
-			commit := args[3].(flow.StateCommitment)
+			blockID := args[1].(*flow.Header).ID()
+			commit := args[2].(flow.StateCommitment)
 			commits[blockID] = commit
 			onPersisted(blockID, commit)
 		}
@@ -902,15 +901,7 @@ func TestExecutionGenerationResultsAreChained(t *testing.T) {
 		Return(previousExecutionResultID, nil)
 
 	execState.
-		On("PersistExecutionState",
-			mock.Anything,
-			mock.Anything,
-			executableBlock.Block.Header,
-			mock.Anything,
-			mock.Anything,
-			mock.Anything,
-			mock.Anything,
-			mock.Anything, mock.Anything).
+		On("PersistExecutionState", mock.Anything, executableBlock.Block.Header, mock.Anything, mock.Anything, mock.Anything, mock.Anything, mock.Anything, mock.Anything).
 		Return(nil)
 
 	e := Engine{
