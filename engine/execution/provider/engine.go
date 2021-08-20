5b483cdf7a1d745330747c8f8fc7715b52b37e24
Yahya Hassanzadeh <yhassanzadeh13@ku.edu.tr>
2021-08-20 15:24:41 -0700

    Update engine/execution/provider/engine.go
    Co-authored-by: Leo Zhang <zhangchiqing@gmail.com>

--- engine/execution/provider/engine.go
+++ engine/execution/provider/engine.go
@@ -203,7 +203,7 @@ func (e *Engine) onChunkDataRequest(
 		lg.Warn().Msgf("chunk data pack query takes longer than %v secs", e.chdpQueryTimeout.Seconds())
 	}
 
-	lg.Info().Msg("chunk data pack response lunched to dispatch")
+	lg.Debug().Msg("chunk data pack response lunched to dispatch")
 
 	// sends requested chunk data pack to the requester
 	e.unit.Launch(func() {
