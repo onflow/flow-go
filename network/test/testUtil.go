2f6b4d2d9c59ac998598521dc9bb7e51f5a02045
Simon Zhu <simon.zsiyan@gmail.com>
2021-08-19 20:03:22 -0700

    Update testUtil.go

--- network/test/testUtil.go
+++ network/test/testUtil.go
@@ -142,7 +142,8 @@ func GenerateMiddlewares(t *testing.T, logger zerolog.Logger, identities flow.Id
 			p2p.DefaultPeerUpdateInterval,
 			p2p.DefaultUnicastTimeout,
 			true,
-			true)
+			true,
+		)
 	}
 	return mws
 }
