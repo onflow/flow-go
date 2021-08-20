cbb7724207b19504fcbe32de4000fe7ff0cd641a
Janez Podhostnik <janez.podhostnik@gmail.com>
2021-08-20 14:35:29 +0200

    backport github.com/onflow/flow-go/pull/1156

--- fvm/transactionStorageLimiter.go
+++ fvm/transactionStorageLimiter.go
@@ -3,6 +3,7 @@ package fvm
 import (
 	"fmt"
 
+	"github.com/hashicorp/go-multierror"
 	"github.com/onflow/cadence/runtime/common"
 
 	errors "github.com/onflow/flow-go/fvm/errors"
@@ -18,7 +19,7 @@ func NewTransactionStorageLimiter() *TransactionStorageLimiter {
 func (d *TransactionStorageLimiter) CheckLimits(
 	env Enviornment,
 	addresses []flow.Address,
-) error {
+) (err error) {
 	if !env.Context().LimitAccountStorage {
 		return nil
 	}
@@ -26,19 +27,25 @@ func (d *TransactionStorageLimiter) CheckLimits(
 	for _, address := range addresses {
 		commonAddress := common.BytesToAddress(address.Bytes())
 
-		capacity, err := env.GetStorageCapacity(commonAddress)
-		if err != nil {
-			return fmt.Errorf("storage limit check failed: %w", err)
+		capacity, aerr := env.GetStorageCapacity(commonAddress)
+		if aerr != nil {
+			aerr = fmt.Errorf("storage limit check failed: %w", aerr)
+			err = multierror.Append(err, aerr)
+			continue
 		}
 
-		usage, err := env.GetStorageUsed(commonAddress)
-		if err != nil {
-			return fmt.Errorf("storage limit check failed: %w", err)
+		usage, aerr := env.GetStorageUsed(commonAddress)
+		if aerr != nil {
+			aerr = fmt.Errorf("storage limit check failed: %w", aerr)
+			err = multierror.Append(err, aerr)
+			continue
 		}
 
 		if usage > capacity {
-			return errors.NewStorageCapacityExceededError(address, usage, capacity)
+			err = multierror.Append(err, errors.NewStorageCapacityExceededError(address, usage, capacity))
+			continue
 		}
 	}
-	return nil
+
+	return
 }
