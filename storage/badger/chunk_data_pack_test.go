021346334f0444f7fd9f95523b7d64a88fd03e78
Yahya <yahya@dapperlabs.com>
2021-08-20 14:34:34 -0700

    adds more tests for chunk data packs

--- storage/badger/chunk_data_pack_test.go
+++ storage/badger/chunk_data_pack_test.go
@@ -1,6 +1,7 @@
 package badger_test
 
 import (
+	"errors"
 	"sync"
 	"testing"
 	"time"
@@ -9,6 +10,7 @@ import (
 
 	"github.com/onflow/flow-go/model/flow"
 	"github.com/onflow/flow-go/module/metrics"
+	"github.com/onflow/flow-go/storage"
 	badgerstorage "github.com/onflow/flow-go/storage/badger"
 	"github.com/onflow/flow-go/utils/unittest"
 
@@ -16,8 +18,9 @@ import (
 	"github.com/stretchr/testify/require"
 )
 
-// TestChunkDataPack_Store evaluates correct storage and retrieval of chunk data packs in the storage.
-func TestChunkDataPack_Store(t *testing.T) {
+// TestChunkDataPacks_Store evaluates correct storage and retrieval of chunk data packs in the storage.
+// It also evaluates that re-inserting is idempotent.
+func TestChunkDataPacks_Store(t *testing.T) {
 	WithChunkDataPacks(t, 100, func(t *testing.T, chunkDataPacks []*flow.ChunkDataPack, chunkDataPackStore *badgerstorage.ChunkDataPacks, _ *badger.DB) {
 		wg := sync.WaitGroup{}
 		wg.Add(len(chunkDataPacks))
@@ -31,11 +34,17 @@ func TestChunkDataPack_Store(t *testing.T) {
 		}
 
 		unittest.RequireReturnsBefore(t, wg.Wait, 1*time.Second, "could not store chunk data packs on time")
+
+		// re-insert - should be idempotent
+		for _, chunkDataPack := range chunkDataPacks {
+			err := chunkDataPackStore.Store(chunkDataPack)
+			require.NoError(t, err)
+		}
 	})
 }
 
 // TestChunkDataPack_BatchStore evaluates correct batch storage and retrieval of chunk data packs in the storage.
-func TestChunkDataPack_BatchStore(t *testing.T) {
+func TestChunkDataPacks_BatchStore(t *testing.T) {
 	WithChunkDataPacks(t, 100, func(t *testing.T, chunkDataPacks []*flow.ChunkDataPack, chunkDataPackStore *badgerstorage.ChunkDataPacks, db *badger.DB) {
 		batch := badgerstorage.NewBatch(db)
 
@@ -57,15 +66,27 @@ func TestChunkDataPack_BatchStore(t *testing.T) {
 	})
 }
 
-func WithChunkDataPacks(t *testing.T, chunks int, storeFunc func(*testing.T, []*flow.ChunkDataPack, *badgerstorage.ChunkDataPacks, *badger.DB)) {
+// TestChunkDataPacks_MissingItem evaluates querying a missing item returns a storage.ErrNotFound error.
+func TestChunkDataPacks_MissingItem(t *testing.T) {
 	unittest.RunWithBadgerDB(t, func(db *badger.DB) {
 		transactions := badgerstorage.NewTransactions(&metrics.NoopCollector{}, db)
 		collections := badgerstorage.NewCollections(db, transactions)
 		store := badgerstorage.NewChunkDataPacks(&metrics.NoopCollector{}, db, collections, 1)
 
-		//// attempt to get an invalid
-		//_, err := store.ByChunkID(unittest.IdentifierFixture())
-		//assert.True(t, errors.Is(err, storage.ErrNotFound))
+		// attempt to get an invalid
+		_, err := store.ByChunkID(unittest.IdentifierFixture())
+		assert.True(t, errors.Is(err, storage.ErrNotFound))
+	})
+}
+
+// WithChunkDataPacks is a test helper that generates specified number of chunk data packs, store them using the storeFunc, and
+// then evaluates whether they are successfully retrieved from storage.
+func WithChunkDataPacks(t *testing.T, chunks int, storeFunc func(*testing.T, []*flow.ChunkDataPack, *badgerstorage.ChunkDataPacks, *badger.DB)) {
+	unittest.RunWithBadgerDB(t, func(db *badger.DB) {
+		transactions := badgerstorage.NewTransactions(&metrics.NoopCollector{}, db)
+		collections := badgerstorage.NewCollections(db, transactions)
+		// keep the cache size at 1 to make sure that entries are written and read from storage itself.
+		store := badgerstorage.NewChunkDataPacks(&metrics.NoopCollector{}, db, collections, 1)
 
 		chunkDataPacks := unittest.ChunkDataPacksFixture(chunks)
 		for _, chunkDataPack := range chunkDataPacks {
@@ -76,9 +97,6 @@ func WithChunkDataPacks(t *testing.T, chunks int, storeFunc func(*testing.T, []*
 
 		// stores chunk data packs in the memory using provided store function.
 		storeFunc(t, chunkDataPacks, store, db)
-		//batch := badgerstorage.NewBatch(db)
-		//err = store.BatchStore(expected, batch)
-		//require.NoError(t, err)
 
 		// stored chunk data packs should be retrieved successfully.
 		for _, expected := range chunkDataPacks {
@@ -87,9 +105,5 @@ func WithChunkDataPacks(t *testing.T, chunks int, storeFunc func(*testing.T, []*
 
 			assert.Equal(t, expected, actual)
 		}
-
-		//// re-insert - should be idempotent
-		//err = store.Store(expected)
-		//require.NoError(t, err)
 	})
 }
