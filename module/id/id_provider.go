9097a46b55d54d040646829101b4acee5d8ececc
Simon Zhu <simon.zsiyan@gmail.com>
2021-08-23 15:18:13 -0700

    add comments

--- module/id/id_provider.go
+++ module/id/id_provider.go
@@ -4,10 +4,14 @@ import (
 	"github.com/onflow/flow-go/model/flow"
 )
 
+// IdentifierProvider provides an interface to get a list of Identifiers representing
+// a specific set of nodes in the network.
 type IdentifierProvider interface {
 	Identifiers() flow.IdentifierList
 }
 
+// IdentifierProvider provides an interface to get a list of Identities representing
+// the set of staked participants in the Flow protocol.
 type IdentityProvider interface {
 	Identities(flow.IdentityFilter) flow.IdentityList
 }
