23a5486a9e40eaf2c83225a35ca72ee2554adfe0
Leo Zhang (zhangchiqing) <zhangchiqing@gmail.com>
2021-08-24 17:21:40 -0700

    implement single sig encoding and decoding

--- consensus/hotstuff/signature/consensus.go
+++ consensus/hotstuff/signature/consensus.go
@@ -1,14 +1,26 @@
 package signature
 
 import (
+	"fmt"
+
 	"github.com/onflow/flow-go/consensus/hotstuff"
 	"github.com/onflow/flow-go/crypto"
 )
 
 func EncodeSingleSig(sigType hotstuff.SigType, sig crypto.Signature) []byte {
-	panic("to be implemented")
+	t := byte(sigType)
+	encoded := make([]byte, 0, len(sig)+1)
+	encoded = append(encoded, t)
+	encoded = append(encoded, sig[:]...)
+	return encoded
 }
 
 func DecodeSingleSig(sigData []byte) (hotstuff.SigType, crypto.Signature, error) {
-	panic("to be implemented")
+	if len(sigData) == 0 {
+		return hotstuff.SigTypeInvalid, nil, fmt.Errorf("empty sig data")
+	}
+
+	sigType := hotstuff.SigType(sigData[0])
+	sig := crypto.Signature(sigData[1:])
+	return sigType, sig, nil
 }
