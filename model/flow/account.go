9d2ed80e770bbdfa938f106bdd59b494652a7bdb
ramtinms <ramtin@axiomzen.co>
2021-08-24 17:12:54 -0700

    adding compact encoding to accounts

--- model/flow/account.go
+++ model/flow/account.go
@@ -3,7 +3,9 @@
 package flow
 
 import (
+	"encoding/binary"
 	"encoding/json"
+	"fmt"
 
 	"github.com/pkg/errors"
 
@@ -74,6 +76,120 @@ func (a *AccountPublicKey) UnmarshalJSON(data []byte) error {
 	a.SeqNumber = temp.SeqNumber
 	a.Weight = temp.Weight
 	return nil
+}
+
+func (a AccountPublicKey) CompactEncodeWithDefaultVersion() ([]byte, error) {
+	return a.compactEncodeV0()
+}
+
+func (a AccountPublicKey) CompactEncode(version uint8) ([]byte, error) {
+	switch version {
+	case 0:
+		return a.compactEncodeV0()
+	default:
+		return nil, fmt.Errorf("cannot encode account public key: encoding version %d is not supported", version)
+	}
+}
+
+func (a *AccountPublicKey) CompactDecode(data []byte) error {
+	// read version and redirect
+	// first 5 bits are encoding version
+	b := uint8(data[0]) & byte(248)  // first 5 bits are encoding version
+	encodingVersion := uint8(b) >> 3 // (mask of 11111000 and left shift 3)
+
+	switch encodingVersion {
+	case 0:
+		return a.compactDecodeV0(data)
+	default:
+		return fmt.Errorf("cannot decode account public key: encoding version %d is not supported", encodingVersion)
+	}
+}
+
+func (a *AccountPublicKey) compactDecodeV0(data []byte) error {
+
+	dataSize := len(data)
+	minDataSize := 15
+	// we need at least minDataSize bytes
+	if dataSize < minDataSize {
+		return fmt.Errorf("cannot compact decode public key: data size is too small to be decodable (expected: at least %d, got: %d)", minDataSize, dataSize)
+	}
+
+	// first 5 bits were encoding version so we have already processed them
+
+	// next bit is revoked flag
+	a.Revoked = (data[0] & byte(4)) > 0 // (mask of 00000100 and equality check (might optimize in the future))
+
+	// the last 2 remaining bits of first byte are the most significant bits of key weight
+	a.Weight = int(uint16(data[0]&byte(3))<<8 + uint16(data[1]))
+
+	// right now is omitted for speed reasons and since user has no control over these values
+
+	// the next byte is the signing algorithm
+	a.SignAlgo = crypto.SigningAlgorithm(uint8(data[2]))
+
+	// the next byte is the hashing algorithm
+	a.HashAlgo = hash.HashingAlgorithm(uint8(data[3]))
+
+	// the next 8 bytes are sequence number
+	a.SeqNumber = binary.BigEndian.Uint64(data[4:12])
+
+	// the next 2 bytes captures the size of the encoded public key
+	publicKeyLen := binary.BigEndian.Uint16(data[12:14])
+
+	allowedDataSize := publicKeyLen + 14
+	// to stay canonical if there are extra bytes we return error
+	if dataSize != int(allowedDataSize) {
+		return fmt.Errorf("cannot compact decode public key: encoded public key size doesn't match (expected: %d, got: %d)", allowedDataSize, dataSize)
+	}
+
+	PublicKey, err := crypto.DecodePublicKey(a.SignAlgo, data[14:allowedDataSize])
+	if err != nil {
+		return fmt.Errorf("cannot compact decode public key: %w", err)
+	}
+
+	a.PublicKey = PublicKey
+	return nil
+}
+
+func (a AccountPublicKey) compactEncodeV0() ([]byte, error) {
+	var encoded [14]byte
+
+	// first 5 bits	are encoding version
+	// you might set it initial int according to the version
+	// e.g. uint8(8) for version 1, uint8(16) for v2, uint8(24) for v3, ...
+	firstByte := uint8(0)
+	// next bit is set flag for revoked key
+	if a.Revoked {
+		firstByte = firstByte | uint8(4)
+	}
+
+	if a.Weight >= 1024 {
+		return nil, fmt.Errorf("cannot compact encode account public key: key weight larger than 1023")
+	}
+	// rest bits of first byte are the most significant bits of key weight
+	encoded[0] = firstByte | uint8(uint16(a.Weight)>>8)
+	encoded[1] = uint8(a.Weight)
+
+	// TODO (maybe) add checks for enforcing uint8 of signing algo and hashing algo
+	// right now is omitted for speed reasons and since user has no control over these values
+	// the next byte is the signing algorithm
+	encoded[2] = uint8(a.SignAlgo)
+
+	// the next byte is the hashing algorithm
+	encoded[3] = uint8(a.HashAlgo)
+
+	// the next 8 bytes captures sequence number
+	binary.BigEndian.PutUint64(encoded[4:12], a.SeqNumber)
+
+	encodedPublicKey := a.PublicKey.Encode()
+
+	if len(encodedPublicKey) >= 65536 {
+		return nil, fmt.Errorf("cannot compact encode account public key: encoded public key size is larger than 65535 bytes")
+	}
+	binary.BigEndian.PutUint16(encoded[12:14], uint16(len(encodedPublicKey)))
+
+	// and append the encoded public key as the rest of the bytes
+	return append(encoded[:], encodedPublicKey...), nil
 }
 
 // Validate returns an error if this account key is invalid.
--- model/flow/account_test.go
+++ model/flow/account_test.go
@@ -26,3 +26,136 @@ func TestAccountPublicKey_MarshalJSON(t *testing.T) {
 
 	assert.Equal(t, accountKeyA, accountKeyB)
 }
+
+func TestAccountPublicKey_CompactEncodingV0(t *testing.T) {
+	// test with revoke flag
+	accountPrivateKey, err := unittest.AccountKeyDefaultFixture()
+	assert.NoError(t, err)
+	accountKeyA := accountPrivateKey.PublicKey(42)
+
+	t.Run("too short data", func(t *testing.T) {
+		encAccountKey, err := accountKeyA.CompactEncode(0)
+		assert.NoError(t, err)
+
+		var accountKeyB flow.AccountPublicKey
+		err = accountKeyB.CompactDecode(encAccountKey[:10])
+		assert.Error(t, err)
+	})
+
+	t.Run("invalid version", func(t *testing.T) {
+		encAccountKey, err := accountKeyA.CompactEncode(0)
+		assert.NoError(t, err)
+
+		var accountKeyB flow.AccountPublicKey
+		err = accountKeyB.CompactDecode(encAccountKey)
+		assert.NoError(t, err)
+		assert.Equal(t, accountKeyA, accountKeyB)
+
+		encAccountKey, err = accountKeyA.CompactEncode(1)
+		assert.Error(t, err)
+	})
+
+	t.Run("with different revoke flags", func(t *testing.T) {
+		accountKeyA.Revoked = true
+		encAccountKey, err := accountKeyA.CompactEncode(0)
+		assert.NoError(t, err)
+
+		var accountKeyB flow.AccountPublicKey
+		err = accountKeyB.CompactDecode(encAccountKey)
+		assert.NoError(t, err)
+
+		assert.True(t, accountKeyB.Revoked)
+		assert.Equal(t, accountKeyA, accountKeyB)
+
+		accountKeyA.Revoked = false
+		encAccountKey, err = accountKeyA.CompactEncode(0)
+		assert.NoError(t, err)
+
+		err = accountKeyB.CompactDecode(encAccountKey)
+		assert.NoError(t, err)
+		assert.False(t, accountKeyB.Revoked)
+		assert.Equal(t, accountKeyA, accountKeyB)
+	})
+
+	t.Run("test with various key weights", func(t *testing.T) {
+		accountKeyA.Weight = 10
+		encAccountKey, err := accountKeyA.CompactEncode(0)
+		assert.NoError(t, err)
+
+		var accountKeyB flow.AccountPublicKey
+		err = accountKeyB.CompactDecode(encAccountKey)
+		assert.NoError(t, err)
+
+		assert.Equal(t, accountKeyB.Weight, 10)
+		assert.Equal(t, accountKeyA, accountKeyB)
+
+		accountKeyA.Weight = 1000
+		encAccountKey, err = accountKeyA.CompactEncode(0)
+		assert.NoError(t, err)
+
+		err = accountKeyB.CompactDecode(encAccountKey)
+		assert.NoError(t, err)
+		assert.Equal(t, accountKeyB.Weight, 1000)
+		assert.Equal(t, accountKeyA, accountKeyB)
+
+		accountKeyA.Weight = 2000
+		encAccountKey, err = accountKeyA.CompactEncode(0)
+		assert.Error(t, err)
+
+		accountKeyA.Weight = 1000
+	})
+
+	t.Run("test with payload size tampered", func(t *testing.T) {
+		encAccountKey, err := accountKeyA.CompactEncode(0)
+		assert.NoError(t, err)
+
+		// add extra bytes
+		encAccountKey = append(encAccountKey, []byte("extra")...)
+
+		var accountKeyB flow.AccountPublicKey
+		err = accountKeyB.CompactDecode(encAccountKey)
+		assert.Error(t, err)
+
+		encAccountKey, err = accountKeyA.CompactEncode(0)
+		assert.NoError(t, err)
+
+		// remove some data
+		shortEncodedData := encAccountKey[:len(encAccountKey)-2]
+		err = accountKeyB.CompactDecode(shortEncodedData)
+		assert.Error(t, err)
+	})
+}
+
+func BenchmarkAccountPublicKeyDecode_JSON(b *testing.B) {
+	accountPrivateKey, err := unittest.AccountKeyDefaultFixture()
+	assert.NoError(b, err)
+	accountKeyA := accountPrivateKey.PublicKey(1000)
+
+	var accountKeyB flow.AccountPublicKey
+	encAccountKey, _ := accountKeyA.MarshalJSON()
+	for i := 0; i < b.N; i++ {
+		_ = accountKeyB.UnmarshalJSON(encAccountKey)
+	}
+}
+func BenchmarkAccountPublicKeyDecode_Compact(b *testing.B) {
+	accountPrivateKey, err := unittest.AccountKeyDefaultFixture()
+	assert.NoError(b, err)
+	accountKeyA := accountPrivateKey.PublicKey(1000)
+
+	var accountKeyB flow.AccountPublicKey
+	encAccountKey, _ := accountKeyA.CompactEncode(0)
+	for i := 0; i < b.N; i++ {
+		_ = accountKeyB.CompactDecode(encAccountKey)
+	}
+}
+
+func BenchmarkAccountPublicKeyDecode_RLP(b *testing.B) {
+	accountPrivateKey, err := unittest.AccountKeyDefaultFixture()
+	assert.NoError(b, err)
+	accountKeyA := accountPrivateKey.PublicKey(1000)
+
+	encAccountKey, _ := flow.EncodeAccountPublicKey(accountKeyA)
+	for i := 0; i < b.N; i++ {
+		_, _ = flow.DecodeAccountPublicKey(encAccountKey, 0)
+	}
+}
