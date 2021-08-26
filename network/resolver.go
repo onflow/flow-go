aded9e32ce1272396834f1bf192a46fd4f53f67d
Yahya <yahya@dapperlabs.com>
2021-08-26 14:43:13 -0700

    refactors resolver interface

--- network/resolver.go
+++ network/resolver.go
@@ -12,17 +12,7 @@ import (
 // We keep a copy of it here for mock generation.
 // https://github.com/multiformats/go-multiaddr-dns/blob/master/resolve.go
 type BasicResolver interface {
+	module.ReadyDoneAware
 	LookupIPAddr(context.Context, string) ([]net.IPAddr, error)
 	LookupTXT(context.Context, string) ([]string, error)
-}
-
-type DNSIPHandlerFunc func([]net.IPAddr, error)
-type DNSTXTHandlerFunc func(func([]string, error))
-
-type ResolverRequester interface {
-	module.ReadyDoneAware
-	RequestIPAddr(context.Context, string) bool
-	RequestTXT(context.Context, string) bool
-	WithIPHandler(DNSIPHandlerFunc)
-	WithTXTHandler(DNSTXTHandlerFunc)
 }
