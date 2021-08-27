c686f955c91d7e0625b9f4cb3c3c018fb4c68234
ramtinms <ramtin@axiomzen.co>
2021-08-26 18:28:03 -0700

    handle the gas that context gas limit is also zero

--- fvm/transactionEnv.go
+++ fvm/transactionEnv.go
@@ -530,7 +530,12 @@ func (e *TransactionEnv) GenerateUUID() (uint64, error) {
 func (e *TransactionEnv) GetComputationLimit() uint64 {
 	// if gas limit is set to zero return the default one
 	if e.tx.GasLimit == 0 {
-		return e.ctx.GasLimit
+		// if ctx gasLimit is bigger than 0 return it
+		if e.ctx.GasLimit > 0 {
+			return e.ctx.GasLimit
+		}
+		// otherwise return the very default gas limit
+		return DefaultGasLimit
 	}
 	return e.tx.GasLimit
 }
