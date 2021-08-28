6b8f7661b3ab311fee5e417d6fb005903992aef3
Khalil Claybon <kclaybon1@gmail.com>
2021-08-28 16:16:01 -0400

    Update grpc_test.go

--- cmd/util/cmd/common/grpc_test.go
+++ cmd/util/cmd/common/grpc_test.go
@@ -1,6 +1,8 @@
 package common
 
 import (
+	"encoding/hex"
+	"github.com/onflow/flow-go/utils/unittest"
 	"testing"
 
 	"github.com/stretchr/testify/require"
@@ -9,37 +11,39 @@ import (
 func TestGetGRPCDialOption(t *testing.T) {
 	t.Run("should return valid secured GRPC connection when called with access-address and access-api-node-id", func(t *testing.T) {
 		accessAddress := "17.123.255.123:2353"
-		accessApiNodeID := "02880abb813f1646952edb0a919d60444ebb34b92ce53e00868d526b80cf3621"
+		nk, err := unittest.NetworkingKey()
+		require.NoError(t, err)
+		accessApiNodePubKey := hex.EncodeToString(nk.PublicKey().Encode())
 		insecureAccessAPI := false
 
-		_, err := GetGRPCDialOption(accessAddress, accessApiNodeID, insecureAccessAPI)
+		_, err = GetGRPCDialOption(accessAddress, accessApiNodePubKey, insecureAccessAPI)
 		require.NoError(t, err)
 	})
 
 	t.Run("should return valid insecure GRPC connection when called with insecure-api true", func(t *testing.T) {
 		accessAddress := "17.123.255.123:2353"
-		accessApiNodeID := "02880abb813f1646952edb0a919d60444ebb34b92ce53e00868d526b80cf3621"
+		accessApiNodePubKey := ""
 		insecureAccessAPI := true
 
-		_, err := GetGRPCDialOption(accessAddress, accessApiNodeID, insecureAccessAPI)
+		_, err := GetGRPCDialOption(accessAddress, accessApiNodePubKey, insecureAccessAPI)
 		require.NoError(t, err)
 	})
 
 	t.Run("should return error when called with invalid access address", func(t *testing.T) {
 		accessAddress := ""
-		accessApiNodeID := "02880abb813f1646952edb0a919d60444ebb34b92ce53e00868d526b80cf3621"
+		accessApiNodePubKey := "02880abb813f1646952edb0a919d60444ebb34b92ce53e00868d526b80cf3621"
 		insecureAccessAPI := false
 
-		_, err := GetGRPCDialOption(accessAddress, accessApiNodeID, insecureAccessAPI)
+		_, err := GetGRPCDialOption(accessAddress, accessApiNodePubKey, insecureAccessAPI)
 		require.Error(t, err)
 	})
 
 	t.Run("should return error when called with invalid access api node ID", func(t *testing.T) {
 		accessAddress := "17.123.255.123:2353"
-		accessApiNodeID := ""
+		accessApiNodePubKey := ""
 		insecureAccessAPI := false
 
-		_, err := GetGRPCDialOption(accessAddress, accessApiNodeID, insecureAccessAPI)
+		_, err := GetGRPCDialOption(accessAddress, accessApiNodePubKey, insecureAccessAPI)
 		require.Error(t, err)
 	})
 }
