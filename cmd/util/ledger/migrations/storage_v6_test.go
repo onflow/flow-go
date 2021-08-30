c8592e56b955b82272123fd792106942471997af
Supun Setunga <supun.setunga@gmail.com>
2021-08-30 13:33:40 -0700

    Refactor

--- cmd/util/ledger/migrations/storage_v6_test.go
+++ cmd/util/ledger/migrations/storage_v6_test.go
@@ -11,7 +11,9 @@ import (
 	"github.com/onflow/atree"
 	"github.com/onflow/cadence/runtime/common"
 	newInter "github.com/onflow/cadence/runtime/interpreter"
+
 	oldInter "github.com/onflow/cadence/v18/runtime/interpreter"
+	"github.com/onflow/cadence/v18/runtime/tests/utils"
 )
 
 func TestValueConversion(t *testing.T) {
@@ -101,6 +103,66 @@ func TestEncodeDecodeRoundTrip(t *testing.T) {
 
 		value = array.GetIndex(nil, 2)
 		require.NoError(t, err)
+		assert.Equal(t, newInter.BoolValue(true), value)
+	})
+
+	t.Run("Dictionary", func(t *testing.T) {
+		inter, err := oldInter.NewInterpreter(nil, utils.TestLocation)
+		require.NoError(t, err)
+
+		// Get the bytes in old format
+		oldArray := oldInter.NewDictionaryValueUnownedNonCopying(
+			inter,
+			oldInter.DictionaryStaticType{
+				KeyType:   oldInter.PrimitiveStaticTypeString,
+				ValueType: oldInter.PrimitiveStaticTypeAnyStruct,
+			},
+			oldInter.NewStringValue("key1"),
+			oldInter.NewStringValue("foo"),
+			oldInter.NewStringValue("key2"),
+			oldInter.BoolValue(true),
+		)
+
+		encoded, _, err := oldInter.EncodeValue(oldArray, nil, false, nil)
+		require.NoError(t, err)
+
+		migration := StorageFormatV6Migration{
+			Log: zerolog.Logger{},
+		}
+
+		migration.initStorage()
+
+		address := common.Address{1, 2}
+
+		err = migration.reencodeValue(encoded, address, "", oldInter.CurrentEncodingVersion)
+		assert.NoError(t, err)
+
+		migration.storage.Commit()
+
+		encodedValues := migration.baseStorage.Payloads
+		require.Len(t, encodedValues, 1)
+
+		storageId := atree.NewStorageID(
+			[8]byte(address),
+			[8]byte{0, 0, 0, 0, 0, 0, 0, 1},
+		)
+
+		slab, ok, err := migration.storage.Retrieve(storageId)
+		require.NoError(t, err)
+		require.True(t, ok)
+
+		newValue, err := newInter.StoredValue(slab, migration.storage)
+		require.NoError(t, err)
+
+		assert.IsType(t, &newInter.DictionaryValue{}, newValue)
+		dictionary := newValue.(*newInter.DictionaryValue)
+
+		value, _, ok := dictionary.GetKey(newInter.NewStringValue("key1"))
+		require.True(t, ok)
+		assert.Equal(t, newInter.NewStringValue("foo"), value)
+
+		value, _, ok = dictionary.GetKey(newInter.NewStringValue("key2"))
+		require.True(t, ok)
 		assert.Equal(t, newInter.BoolValue(true), value)
 	})
 }
