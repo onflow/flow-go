1d7775b7a79d379e8cc8538ea43bc44253ba374d
Jordan Schalm <jordan@dapperlabs.com>
2021-08-31 15:26:11 -0700

    add method to check machine account config
    and test stubs

---
+++ module/epochs/machine_account_test.go
@@ -0,0 +1,32 @@
+package epochs
+
+import (
+	"testing"
+)
+
+func TestMachineAccountChecking(t *testing.T) {
+	t.Run("consistent machine account", func(t *testing.T) {
+
+	})
+	t.Run("inconsistent address", func(t *testing.T) {
+
+	})
+	t.Run("inconsistent key", func(t *testing.T) {
+
+	})
+	t.Run("inconsistent hash algo", func(t *testing.T) {
+
+	})
+	t.Run("inconsistent sig algo", func(t *testing.T) {
+
+	})
+	t.Run("account without keys", func(t *testing.T) {
+
+	})
+	t.Run("account with < minimum balance", func(t *testing.T) {
+
+	})
+	t.Run("invalid role", func(t *testing.T) {
+
+	})
+}
