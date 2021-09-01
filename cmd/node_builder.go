8e0bfa1fba52ca31c254e615e766dddbc3660067
vishal <1117327+vishalchangrani@users.noreply.github.com>
2021-08-31 23:38:40 -0700

    wip

--- cmd/node_builder.go
+++ cmd/node_builder.go
@@ -114,6 +114,7 @@ type BaseConfig struct {
 	guaranteesCacheSize   uint
 	receiptsCacheSize     uint
 	db                    *badger.DB
+	IdProvider            id.IdentityProvider
 }
 
 // NodeConfig contains all the derived parameters such the NodeID, private keys etc. and initialized instances of
