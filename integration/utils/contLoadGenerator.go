47222733be1ebf0379913033eb44f53da06a1c53
Simon Zhu <simon.zsiyan@gmail.com>
2021-09-03 13:41:51 -0400

    Update contLoadGenerator.go

--- integration/utils/contLoadGenerator.go
+++ integration/utils/contLoadGenerator.go
@@ -7,9 +7,8 @@ import (
 	"time"
 
 	"github.com/onflow/cadence"
-	"github.com/rs/zerolog"
-
 	flowsdk "github.com/onflow/flow-go-sdk"
+	"github.com/rs/zerolog"
 
 	"github.com/onflow/flow-go/module/metrics"
 
