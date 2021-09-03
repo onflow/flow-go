57f617a0f7175cae917307072bb281252e460976
Simon Zhu <simon.zsiyan@gmail.com>
2021-09-03 11:34:19 -0400

    update fixed_provider

--- module/id/fixed_provider.go
+++ module/id/fixed_provider.go
@@ -1,7 +1,10 @@
 package id
 
 import (
+	"github.com/libp2p/go-libp2p-core/peer"
 	"github.com/onflow/flow-go/model/flow"
+	"github.com/onflow/flow-go/model/flow/filter"
+	"github.com/onflow/flow-go/network/p2p"
 )
 
 // FixedIdentifierProvider implements an IdentifierProvider which provides a fixed list
@@ -30,4 +33,21 @@ func NewFixedIdentityProvider(identities flow.IdentityList) *FixedIdentityProvid
 
 func (p *FixedIdentityProvider) Identities(filter flow.IdentityFilter) flow.IdentityList {
 	return p.identities.Filter(filter)
+}
+
+func (p *FixedIdentityProvider) ByNodeID(nodeID flow.Identifier) (*flow.Identity, bool) {
+	results := p.identities.Filter(filter.HasNodeID(nodeID))
+	if len(results) > 0 {
+		return results[0], true
+	}
+	return nil, false
+}
+
+func (p *FixedIdentityProvider) ByPeerID(pid peer.ID) (*flow.Identity, bool) {
+	for _, id := range p.identities {
+		if peerID, err := p2p.PeerIDFromFlowPublicKey(id.NetworkPubKey); err == nil && peerID == pid {
+			return id, true
+		}
+	}
+	return nil, false
 }
