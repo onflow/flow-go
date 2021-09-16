9ec092664d4ce382f658ecfad90684a232e70838
Jeff Winkler <jwinkler2083233@users.noreply.github.com>
2021-09-16 10:27:06 -0500

    Update compactor_test.go

--- ledger/complete/wal/compactor_test.go
+++ ledger/complete/wal/compactor_test.go
@@ -113,7 +113,7 @@ func Test_Compactor(t *testing.T) {
 			select {
 			case <-co.done:
 				// continue
-			case <-time.After(70 * time.Second):
+			case <-time.After(60 * time.Second):
 				assert.FailNow(t, "timed out")
 			}
 
