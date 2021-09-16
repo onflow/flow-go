328f3c86d94a3291c144d9e220cd0ce0210466c2
Supun Setunga <supun.setunga@gmail.com>
2021-09-16 10:58:01 -0700

    Add more tests

--- cmd/util/ledger/migrations/storage_v6_test.go
+++ cmd/util/ledger/migrations/storage_v6_test.go
@@ -19,7 +19,11 @@ import (
 
 func TestValueConversion(t *testing.T) {
 
+	t.Parallel()
+
 	t.Run("Array", func(t *testing.T) {
+		t.Parallel()
+
 		oldArray := oldInter.NewArrayValueUnownedNonCopying(
 			oldInter.VariableSizedStaticType{
 				Type: oldInter.PrimitiveStaticTypeAnyStruct,
@@ -57,11 +61,121 @@ func TestValueConversion(t *testing.T) {
 			array.GetIndex(migration.newInter, nil, 1),
 		)
 	})
+
+	t.Run("Dictionary", func(t *testing.T) {
+		t.Parallel()
+
+		inter, err := oldInter.NewInterpreter(nil, utils.TestLocation)
+		require.NoError(t, err)
+
+		oldDictionary := oldInter.NewDictionaryValueUnownedNonCopying(
+			inter,
+			oldInter.DictionaryStaticType{
+				KeyType:   oldInter.PrimitiveStaticTypeString,
+				ValueType: oldInter.PrimitiveStaticTypeAnyStruct,
+			},
+			oldInter.NewStringValue("key1"),
+			oldInter.NewStringValue("foo"),
+			oldInter.NewStringValue("key2"),
+			oldInter.BoolValue(true),
+		)
+
+		address := &common.Address{1, 2}
+		oldDictionary.SetOwner(address)
+
+		migration := &StorageFormatV6Migration{}
+		baseStorage := newEncodingBaseStorage()
+
+		migration.initPersistentSlabStorage(baseStorage)
+		migration.initNewInterpreter()
+
+		oldInterpreter, err := oldInter.NewInterpreter(nil, nil)
+		assert.NoError(t, err)
+
+		converter := NewValueConverter(migration.newInter, oldInterpreter, migration.storage)
+		newValue := converter.Convert(oldDictionary)
+
+		assert.IsType(t, &newInter.DictionaryValue{}, newValue)
+		dictionary := newValue.(*newInter.DictionaryValue)
+
+		value, _, ok := dictionary.GetKey(newInter.NewStringValue("key1"))
+		require.True(t, ok)
+		assert.Equal(t, newInter.NewStringValue("foo"), value)
+
+		value, _, ok = dictionary.GetKey(newInter.NewStringValue("key2"))
+		require.True(t, ok)
+		assert.Equal(t, newInter.BoolValue(true), value)
+	})
+
+	t.Run("Composite", func(t *testing.T) {
+		t.Parallel()
+
+		inter, err := oldInter.NewInterpreter(nil, utils.TestLocation)
+		require.NoError(t, err)
+
+		owner := common.Address{1, 2}
+
+		oldDictionary := oldInter.NewDictionaryValueUnownedNonCopying(
+			inter,
+			oldInter.DictionaryStaticType{
+				KeyType:   oldInter.PrimitiveStaticTypeString,
+				ValueType: oldInter.PrimitiveStaticTypeAnyStruct,
+			},
+			oldInter.NewStringValue("key1"),
+			oldInter.NewStringValue("value1"),
+			oldInter.NewStringValue("key2"),
+			oldInter.BoolValue(true),
+		)
+		oldDictionary.SetOwner(&owner)
+
+		fields := oldInter.NewStringValueOrderedMap()
+		fields.Set("foo", oldDictionary)
+
+		oldComposite := oldInter.NewCompositeValue(
+			utils.TestLocation,
+			"Test",
+			common.CompositeKindContract,
+			fields,
+			&owner,
+		)
+
+		migration := &StorageFormatV6Migration{}
+		baseStorage := newEncodingBaseStorage()
+
+		migration.initPersistentSlabStorage(baseStorage)
+		migration.initNewInterpreter()
+
+		oldInterpreter, err := oldInter.NewInterpreter(nil, nil)
+		assert.NoError(t, err)
+
+		converter := NewValueConverter(migration.newInter, oldInterpreter, migration.storage)
+		newValue := converter.Convert(oldComposite)
+
+		assert.IsType(t, &newInter.CompositeValue{}, newValue)
+		composite := newValue.(*newInter.CompositeValue)
+
+		fieldValue := composite.GetField("foo")
+
+		assert.IsType(t, &newInter.DictionaryValue{}, fieldValue)
+		dictionary := fieldValue.(*newInter.DictionaryValue)
+
+		value, _, ok := dictionary.GetKey(newInter.NewStringValue("key1"))
+		require.True(t, ok)
+		assert.Equal(t, newInter.NewStringValue("value1"), value)
+
+		value, _, ok = dictionary.GetKey(newInter.NewStringValue("key2"))
+		require.True(t, ok)
+		assert.Equal(t, newInter.BoolValue(true), value)
+	})
 }
 
 func TestEncoding(t *testing.T) {
 
+	t.Parallel()
+
 	t.Run("Array", func(t *testing.T) {
+		t.Parallel()
+
 		// Get the bytes in old format
 		oldArray := oldInter.NewArrayValueUnownedNonCopying(
 			oldInter.VariableSizedStaticType{
@@ -121,11 +235,13 @@ func TestEncoding(t *testing.T) {
 	})
 
 	t.Run("Dictionary", func(t *testing.T) {
+		t.Parallel()
+
 		inter, err := oldInter.NewInterpreter(nil, utils.TestLocation)
 		require.NoError(t, err)
 
 		// Get the bytes in old format
-		oldArray := oldInter.NewDictionaryValueUnownedNonCopying(
+		oldDictionary := oldInter.NewDictionaryValueUnownedNonCopying(
 			inter,
 			oldInter.DictionaryStaticType{
 				KeyType:   oldInter.PrimitiveStaticTypeString,
@@ -137,7 +253,7 @@ func TestEncoding(t *testing.T) {
 			oldInter.BoolValue(true),
 		)
 
-		encoded, _, err := oldInter.EncodeValue(oldArray, nil, false, nil)
+		encoded, _, err := oldInter.EncodeValue(oldDictionary, nil, false, nil)
 		require.NoError(t, err)
 
 		migration := &StorageFormatV6Migration{}
@@ -180,11 +296,113 @@ func TestEncoding(t *testing.T) {
 		require.True(t, ok)
 		assert.Equal(t, newInter.BoolValue(true), value)
 	})
+
+	t.Run("Composite", func(t *testing.T) {
+		t.Parallel()
+
+		inter, err := oldInter.NewInterpreter(nil, utils.TestLocation)
+		require.NoError(t, err)
+
+		owner := common.Address{1, 2}
+
+		oldDictionary := oldInter.NewDictionaryValueUnownedNonCopying(
+			inter,
+			oldInter.DictionaryStaticType{
+				KeyType:   oldInter.PrimitiveStaticTypeString,
+				ValueType: oldInter.PrimitiveStaticTypeAnyStruct,
+			},
+			oldInter.NewStringValue("key1"),
+			oldInter.NewStringValue("value1"),
+			oldInter.NewStringValue("key2"),
+			oldInter.BoolValue(true),
+		)
+		oldDictionary.SetOwner(&owner)
+
+		fields := oldInter.NewStringValueOrderedMap()
+		fields.Set("foo", oldDictionary)
+
+		oldComposite := oldInter.NewCompositeValue(
+			utils.TestLocation,
+			"Test",
+			common.CompositeKindContract,
+			fields,
+			&owner,
+		)
+
+		encoded, _, err := oldInter.EncodeValue(oldComposite, nil, false, nil)
+		require.NoError(t, err)
+
+		migration := &StorageFormatV6Migration{}
+		baseStorage := newEncodingBaseStorage()
+
+		migration.initPersistentSlabStorage(baseStorage)
+		migration.initNewInterpreter()
+		migration.migratedPayloadPaths = make(map[storagePath]bool, 0)
+
+		address := common.Address{1, 2}
+
+		err = migration.decodeAndConvert(encoded, address, "", oldInter.CurrentEncodingVersion)
+		assert.NoError(t, err)
+
+		err = migration.storage.Commit()
+		assert.NoError(t, err)
+
+		encodedValues := baseStorage.ReencodedPayloads
+		require.Len(t, encodedValues, 3)
+
+		// Check composite value in storage
+
+		storageId := atree.NewStorageID(
+			atree.Address(address),
+			atree.StorageIndex{0, 0, 0, 0, 0, 0, 0, 3},
+		)
+
+		slab, ok, err := migration.storage.Retrieve(storageId)
+		require.NoError(t, err)
+		require.True(t, ok)
+
+		storedValue := newInter.StoredValue(slab, migration.storage)
+
+		assert.IsType(t, &newInter.CompositeValue{}, storedValue)
+		composite := storedValue.(*newInter.CompositeValue)
+
+		fieldValue := composite.GetField("foo")
+
+		assert.IsType(t, &newInter.DictionaryValue{}, fieldValue)
+		dictionary := fieldValue.(*newInter.DictionaryValue)
+
+		value, _, ok := dictionary.GetKey(newInter.NewStringValue("key1"))
+		require.True(t, ok)
+		assert.Equal(t, newInter.NewStringValue("value1"), value)
+
+		value, _, ok = dictionary.GetKey(newInter.NewStringValue("key2"))
+		require.True(t, ok)
+		assert.Equal(t, newInter.BoolValue(true), value)
+
+		// Check whether the separately stored value
+		// is same as the composite's field value.
+
+		storageId = atree.NewStorageID(
+			atree.Address(address),
+			atree.StorageIndex{0, 0, 0, 0, 0, 0, 0, 1},
+		)
+
+		slab, ok, err = migration.storage.Retrieve(storageId)
+		require.NoError(t, err)
+		require.True(t, ok)
+
+		storedValue = newInter.StoredValue(slab, migration.storage)
+		assert.Equal(t, dictionary, storedValue)
+	})
 }
 
 func TestRoundTrip(t *testing.T) {
 
+	t.Parallel()
+
 	t.Run("Array", func(t *testing.T) {
+		t.Parallel()
+
 		// Get the bytes in old format
 		oldArray := oldInter.NewArrayValueUnownedNonCopying(
 			oldInter.VariableSizedStaticType{
@@ -218,6 +436,8 @@ func TestRoundTrip(t *testing.T) {
 	})
 
 	t.Run("Dictionary", func(t *testing.T) {
+		t.Parallel()
+
 		inter, err := oldInter.NewInterpreter(nil, utils.TestLocation)
 		require.NoError(t, err)
 
@@ -292,6 +512,8 @@ func stripSlabPrefix(slabData []byte) []byte {
 }
 
 func TestPayloadsMigration(t *testing.T) {
+	t.Parallel()
+
 	inter, err := oldInter.NewInterpreter(nil, utils.TestLocation)
 	require.NoError(t, err)
 
@@ -351,6 +573,8 @@ func TestPayloadsMigration(t *testing.T) {
 
 	migratedPayloads, err := storageFormatV6Migration.migrate(payloads)
 	require.NoError(t, err)
+
+	assert.Len(t, migratedPayloads, 3)
 
 	// Check whether the query works with new ledger
 
