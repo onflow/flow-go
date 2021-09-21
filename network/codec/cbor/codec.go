1fd613d3b792ebb0d335bbcb72d36591260bc69e
simonhf <simonhf@gmail.com>
2021-09-20 18:39:29 -0700

    Size conditional compression of CBOR encoded network messages

--- network/codec/cbor/codec.go
+++ network/codec/cbor/codec.go
@@ -4,8 +4,10 @@ package cbor
 
 import (
 	"bytes"
+	"compress/gzip"
 	"fmt"
 	"io"
+	"io/ioutil"
 
 	"github.com/fxamacker/cbor/v2"
 
@@ -36,6 +38,10 @@ func (c *Codec) NewDecoder(r io.Reader) network.Decoder {
 	return &Decoder{dec: dec}
 }
 
+const IS_COMPRESSED uint8 = 1
+const IS_NOT_COMPRESSED uint8 = 0
+const MINIMUM_BYTES_TO_COMPRESS = 8192
+
 // Given a Golang interface 'v', return a []byte 'envelope'.
 // Return an error if packing the envelope fails.
 // NOTE: 'v' is the network message payload in unserialized form.
@@ -57,6 +63,7 @@ func (c *Codec) Encode(v interface{}) ([]byte, error) {
 	// encode / append the envelope code
 	//bs1 := binstat.EnterTime(binstat.BinNet + ":wire<1(cbor)envelope2payload")
 	var data bytes.Buffer
+	data.WriteByte(IS_NOT_COMPRESSED)
 	data.WriteByte(code)
 	//binstat.LeaveVal(bs1, int64(data.Len()))
 
@@ -69,9 +76,29 @@ func (c *Codec) Encode(v interface{}) ([]byte, error) {
 		return nil, fmt.Errorf("could not encode CBOR payload with envelope code %d AKA %s: %w", code, what, err) // e.g. 2, "CodeBlockProposal", <CBOR error>
 	}
 
-	dataBytes := data.Bytes()
+	if data.Len() < MINIMUM_BYTES_TO_COMPRESS {
+		// come here if encoded message is considered too small to compress
+		return data.Bytes(), nil
+	}
 
-	return dataBytes, nil
+	// compress the encoded payload
+	//bs3 := binstat.EnterTime(fmt.Sprintf("%s%s%s:%d(size=saved)", binstat.BinNet, ":wire<2.5(cbor)compress:", what, code)) // e.g. ~3net::wire<1(cbor)CodeEntityRequest:23
+	var data2gzip bytes.Buffer
+	data2gzip.WriteByte(IS_COMPRESSED)
+	zw, err := gzip.NewWriterLevel(&data2gzip, gzip.BestSpeed)
+	if err != nil {
+		return nil, fmt.Errorf("gzip.NewWriterLevel() failed: %w", err)
+	}
+	_, err = zw.Write(data.Bytes()[1:]) // all but first byte
+	if err != nil {
+		return nil, fmt.Errorf("gzip.NewWriterLevel().Write() failed: %w", err)
+	}
+	if err := zw.Close(); err != nil {
+		return nil, fmt.Errorf("gzip.NewWriterLevel().Close() failed: %w", err)
+	}
+	//binstat.LeaveVal(bs3, int64(data.Len()-data2gzip.Len()))
+
+	return data2gzip.Bytes(), nil
 }
 
 // Given a []byte 'envelope', eturn a Golang interface 'v'.
@@ -81,7 +108,30 @@ func (c *Codec) Encode(v interface{}) ([]byte, error) {
 // NOTE: 'what' is the 'code' name for debugging / instrumentation.
 // NOTE: 'envelope' contains 'code' & serialized / encoded 'v'.
 // i.e.  1st byte is 'code' and remaining bytes are CBOR encoded 'v'.
-func (c *Codec) Decode(data []byte) (interface{}, error) {
+func (c *Codec) Decode(maybeGzipBytes []byte) (interface{}, error) {
+
+	var data []byte
+	var err error
+
+	if IS_COMPRESSED == maybeGzipBytes[0] {
+		// uncompress the encoded payload
+		//bs0 := binstat.EnterTime(binstat.BinNet + ":wire>2.5(cbor)uncompress")
+		gzip2data := bytes.NewBuffer(maybeGzipBytes[1:])
+		zr, err := gzip.NewReader(gzip2data)
+		if err != nil {
+			return nil, fmt.Errorf("gzip.NewReader() failed: %w", err)
+		}
+		data, err = ioutil.ReadAll(zr)
+		if err != nil {
+			return nil, fmt.Errorf("ioutil.ReadAll() failed: %w", err)
+		}
+		if err := zr.Close(); err != nil {
+			return nil, fmt.Errorf("gzip.NewReader().Close() failed: %w", err)
+		}
+		//binstat.Leave(bs0)
+	} else {
+		data = maybeGzipBytes[1:]
+	}
 
 	// decode the envelope
 	//bs1 := binstat.EnterTime(binstat.BinNet + ":wire>3(cbor)payload2envelope")
