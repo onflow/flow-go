45a750afdbf775b2aba13e6cdf83d230cd9a30ac
Jordan Schalm <jordan@dapperlabs.com>
2021-09-22 11:45:35 -0400

    remove snowflake type, update comments

--- storage/badger/init.go
+++ storage/badger/init.go
@@ -22,20 +22,6 @@ func InitPublic(opts badger.Options) (*badger.DB, error) {
 	return db, nil
 }
 
-func InitSnowflake(opts badger.Options) (*badger.DB, error) {
-
-	db, err := badger.Open(opts)
-	if err != nil {
-		return nil, fmt.Errorf("could not open db: %w", err)
-	}
-	err = db.Update(operation.InsertSnowflakeDBMarker)
-	if err != nil {
-		return nil, fmt.Errorf("could not assert db type: %w", err)
-	}
-
-	return db, nil
-}
-
 func InitSecret(opts badger.Options) (*badger.DB, error) {
 
 	db, err := badger.Open(opts)
