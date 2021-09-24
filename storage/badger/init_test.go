0cea4ea39304626a656d03a194d860c8572ac542
Jordan Schalm <jordan@dapperlabs.com>
2021-09-24 18:13:33 -0400

    opening db with wrong key should fail

--- storage/badger/init_test.go
+++ storage/badger/init_test.go
@@ -28,3 +28,29 @@ func TestInitSecret(t *testing.T) {
 		require.Error(t, err)
 	})
 }
+
+// opening a database which has previously been opened with encryption enabled,
+// using a different encryption key, should fail
+func TestEncryptionKeyMismatch(t *testing.T) {
+	unittest.RunWithTempDir(t, func(dir string) {
+
+		// open a database with encryption enabled
+		key1 := unittest.SeedFixture(32)
+		db := unittest.TypedBadgerDB(t, dir, func(options badger.Options) (*badger.DB, error) {
+			options = options.WithEncryptionKey(key1)
+			return badger.Open(options)
+		})
+		db.Close()
+
+		// open the same database with a different key
+		key2 := unittest.SeedFixture(32)
+		opts := badger.
+			DefaultOptions(dir).
+			WithKeepL0InMemory(true).
+			WithEncryptionKey(key2).
+			WithLogger(nil)
+		_, err := badger.Open(opts)
+		// opening the database should return an error
+		require.Error(t, err)
+	})
+}
