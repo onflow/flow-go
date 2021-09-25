746b198e24789aea76bbc087f11eff22fbd69739
Tarak Ben Youssef <tarak.benyoussef@dapperlabs.com>
2021-09-25 18:22:24 +0100

    use capital S to detect C macros in assmbly + fix header issue

--- crypto/hash/internal/keccakAVX512/keccak_amd64_AVX512.go
+++ crypto/hash/internal/keccakAVX512/keccak_amd64_AVX512.go
@@ -10,8 +10,8 @@ package keccakAVX512
 /*
 // check if the amd64 machine supports AVX512 instructions at build time and call
 // an assembly function using AVX512 if so.
-#if ((__AVX512CD__ |  __AVX512ER__ | __AVX512F__ | __AVX512PF__) == 1)
-	void keccakF1600_AVX512(void *state);
+#if (defined __AVX512CD__ ||  defined __AVX512ER__  || defined __AVX512F__ || defined__AVX512PF__)
+	void keccakF1600_AVX512(unsigned long* state);
 #else
 	void keccakF1600_AVX512(unsigned long* state) {};
 #endif
