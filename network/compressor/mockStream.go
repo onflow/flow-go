3ec5da39e9a2e46173b35332bf99f0d75eb3629e
Yahya <yahya@dapperlabs.com>
2021-09-28 14:45:09 -0700

    adds mock stream

--- network/compressor/mockStream.go
+++ network/compressor/mockStream.go
@@ -2,8 +2,10 @@ package compressor
 
 import (
 	"bufio"
+	"time"
 
 	"github.com/libp2p/go-libp2p-core/network"
+	"github.com/libp2p/go-libp2p-core/protocol"
 )
 
 type mockStream struct {
@@ -25,4 +27,48 @@ func (m *mockStream) Read(p []byte) (n int, err error) {
 
 func (m *mockStream) Write(p []byte) (n int, err error) {
 	return m.w.Write(p)
+}
+
+func (m *mockStream) Close() error {
+	return nil
+}
+
+func (m *mockStream) CloseRead() error {
+	return nil
+}
+
+func (m *mockStream) Reset() error {
+	return nil
+}
+
+func (m *mockStream) SetDeadline(_ time.Time) error {
+	return nil
+}
+
+func (m *mockStream) SetReadDeadline(_ time.Time) error {
+	return nil
+}
+
+func (m *mockStream) SetWriteDeadline(_ time.Time) error {
+	return nil
+}
+
+func (m *mockStream) ID() string {
+	return ""
+}
+
+func (m *mockStream) Protocol() protocol.ID {
+	return ""
+}
+
+func (m *mockStream) SetProtocol(id protocol.ID) {
+
+}
+
+func (m *mockStream) Stat() network.Stat {
+	return network.Stat{}
+}
+
+func (m *mockStream) Conn() network.Conn {
+	return nil
 }
