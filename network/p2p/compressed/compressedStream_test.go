e8255033207ad744b1c14efbd5386653ac53e6bd
Yahya <yahya@dapperlabs.com>
2021-09-28 15:38:18 -0700

    adds test roundtri for compressed stream

--- network/p2p/compressed/compressedStream_test.go
+++ network/p2p/compressed/compressedStream_test.go
@@ -1,1 +1,34 @@
-package compressedStream
+package compressed
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/require"
+)
+
+// TestRoundTrip evaluates that (1) reading what has been written by compressor yields in same result,
+// and (2) data is compressed when written.
+func TestRoundTrip(t *testing.T) {
+	// text := "hello world, hello world!"
+
+	_, err := NewCompressedStream(newMockStream())
+	require.NoError(t, err)
+
+	//w, err := gzipComp.NewWriter(buf)
+	//require.NoError(t, err)
+	//
+	//n, err := w.Write([]byte(text))
+	//require.NoError(t, err)
+	//require.Equal(t, n, len(text))
+	//// written data on buffer should be compressed in size.
+	//require.Less(t, buf.Len(), len(text))
+	//
+	//require.NoError(t, w.Close())
+	//
+	//r, err := gzipComp.NewReader(buf)
+	//require.NoError(t, err)
+	//
+	//b, err := io.ReadAll(r)
+	//require.NoError(t, err)
+	//require.Equal(t, b, []byte(text))
+}
--- network/p2p/compressed/mockStream.go
+++ network/p2p/compressed/mockStream.go
@@ -1,4 +1,4 @@
-package compressedStream
+package compressed
 
 import (
 	"bufio"
