66aa788d38226ec6f250350014ddbd13a8ff13af
ramtinms <ramtin@axiomzen.co>
2021-09-28 21:22:13 -0700

    another try

--- cmd/util/ledger/validators/account_balance_validator.go
+++ cmd/util/ledger/validators/account_balance_validator.go
@@ -7,8 +7,9 @@ import (
 
 	"github.com/onflow/cadence"
 	jsoncdc "github.com/onflow/cadence/encoding/json"
-
-	oldFvm "github.com/onflow/flow-go/v21/fvm"
+	oldCommon "github.com/onflow/cadence/runtime/common"
+	oldInterpreter "github.com/onflow/cadence/runtime/interpreter"
+	oldRuntime "github.com/onflow/cadence/v19/runtime"
 	"github.com/rs/zerolog"
 
 	"github.com/onflow/flow-go/fvm"
@@ -42,6 +43,7 @@ func newNewVersionCollector(
 	logger zerolog.Logger,
 	chain flow.Chain,
 	view state.View) *newVersionCollector {
+
 	vm := fvm.NewVirtualMachine(fvm.NewInterpreterRuntime())
 	ctx := fvm.NewContext(zerolog.Nop(), fvm.WithChain(chain))
 	prog := programs.NewEmptyPrograms()
@@ -100,78 +102,6 @@ func (c *newVersionCollector) CollectBalances(addresses chan uint64, balances ch
 	c.wg.Done()
 }
 
-type oldVersionCollector struct {
-	wg     *sync.WaitGroup
-	logger zerolog.Logger
-	vm     *oldFvm.VirtualMachine
-	ctx    oldFvm.Context
-	view   state.View
-	prog   *programs.Programs
-	script []byte
-}
-
-func newOldVersionCollector(wg *sync.WaitGroup,
-	logger zerolog.Logger,
-	chain flow.Chain,
-	view state.View) *oldVersionCollector {
-	vm := oldFvm.NewVirtualMachine(oldFvm.NewInterpreterRuntime())
-	ctx := oldFvm.NewContext(zerolog.Nop(), oldFvm.WithChain(chain))
-	prog := programs.NewEmptyPrograms()
-	script := []byte(fmt.Sprintf(`
-				import FungibleToken from 0x%s
-				import FlowToken from 0x%s
-				
-				pub fun main(account: Address): UFix64 {
-					let acct = getAccount(account)
-					let vaultRef = acct.getCapability(/public/flowTokenBalance)
-						.borrow<&FlowToken.Vault{FungibleToken.Balance}>()
-						?? panic("Could not borrow Balance reference to the Vault")
-				
-					return vaultRef.balance
-				}
-			`, fvm.FungibleTokenAddress(ctx.Chain), fvm.FlowTokenAddress(ctx.Chain)))
-
-	return &oldVersionCollector{
-		wg:     wg,
-		logger: logger,
-		vm:     vm,
-		ctx:    ctx,
-		view:   view.NewChild(),
-		prog:   prog,
-		script: script}
-}
-
-// change this to incoming channel and outgoing channel
-func (c *oldVersionCollector) CollectBalances(addresses chan uint64, balances chan accountBalance) {
-	for indx := range addresses {
-		var balance uint64
-		address, err := c.ctx.Chain.AddressAtIndex(indx)
-		if err != nil {
-			c.logger.Err(err).Msgf("Error collecting balance for account %s", address.String())
-		}
-
-		script := oldFvm.Script(c.script).WithArguments(
-			jsoncdc.MustEncode(cadence.NewAddress(address)),
-		)
-
-		err = c.vm.Run(c.ctx, script, c.view, c.prog)
-		if err != nil {
-			c.logger.Err(err).Msgf("Error collecting balance for account %s", address.String())
-		}
-
-		if script.Err == nil && script.Value != nil {
-			balance = script.Value.ToGoValue().(uint64)
-		} else {
-			c.logger.Err(script.Err).Msgf("Error collecting balance for account %s", address.String())
-		}
-
-		balances <- accountBalance{addressIndex: indx, balance: balance}
-		// for debugging
-		c.logger.Info().Msgf("collected balance for %s (%d)", address.String(), balance)
-	}
-	c.wg.Done()
-}
-
 type AccountBalanceValidator struct {
 	chain            flow.Chain
 	logger           zerolog.Logger
@@ -207,7 +137,7 @@ func (v *AccountBalanceValidator) Setup(oldPayloads []ledger.Payload) error {
 
 	for i := 0; i < workerCount; i++ {
 		wg.Add(1)
-		worker := newOldVersionCollector(wg, v.logger, v.chain, mainView)
+		worker := newOldVersionCollector(wg, oldPayloads, v.logger, v.chain)
 		go worker.CollectBalances(addressIndices, balances)
 	}
 
@@ -263,4 +193,210 @@ func (v *AccountBalanceValidator) Validate(newPayloads []ledger.Payload) (isVali
 	}
 
 	return true, nil
+}
+
+type oldVersionCollector struct {
+	wg      *sync.WaitGroup
+	logger  zerolog.Logger
+	chain   flow.Chain
+	runtime oldRuntime.Runtime
+	env     *oldInter
+	script  []byte
+}
+
+func newOldVersionCollector(
+	wg *sync.WaitGroup,
+	payloads []ledger.Payload,
+	logger zerolog.Logger,
+	chain flow.Chain) *oldVersionCollector {
+
+	env := newOldInter(payloads)
+	runtime := oldRuntime.NewInterpreterRuntime()
+	script := []byte(fmt.Sprintf(`
+				import FungibleToken from 0x%s
+				import FlowToken from 0x%s
+				
+				pub fun main(account: Address): UFix64 {
+					let acct = getAccount(account)
+					let vaultRef = acct.getCapability(/public/flowTokenBalance)
+						.borrow<&FlowToken.Vault{FungibleToken.Balance}>()
+						?? panic("Could not borrow Balance reference to the Vault")
+				
+					return vaultRef.balance
+				}
+			`, fvm.FungibleTokenAddress(chain), fvm.FlowTokenAddress(chain)))
+
+	return &oldVersionCollector{
+		wg:      wg,
+		logger:  logger,
+		chain:   chain,
+		runtime: runtime,
+		env:     env,
+		script:  script}
+}
+
+// change this to incoming channel and outgoing channel
+func (c *oldVersionCollector) CollectBalances(addresses chan uint64, balances chan accountBalance) {
+	for indx := range addresses {
+		var balance uint64
+		address, err := c.chain.AddressAtIndex(indx)
+		if err != nil {
+			c.logger.Err(err).Msgf("Error collecting balance for account %s", address.String())
+		}
+
+		// add := oldCadence.NewAddress(address)
+		value, err := c.runtime.ExecuteScript(
+			oldRuntime.Script{
+				Source:    c.script,
+				Arguments: nil,
+			},
+			oldRuntime.Context{
+				Interface: c.env,
+				Location:  oldCommon.ScriptLocation("LOCATION"),
+			},
+		)
+
+		if err == nil && value != nil {
+			balance = value.ToGoValue().(uint64)
+		} else {
+			c.logger.Err(err).Msgf("Error collecting balance for account %s", address.String())
+		}
+
+		balances <- accountBalance{addressIndex: indx, balance: balance}
+		// for debugging
+		c.logger.Info().Msgf("collected balance for %s (%d)", address.String(), balance)
+	}
+	c.wg.Done()
+}
+
+type oldInter struct {
+	payloads map[string][]byte
+	programs map[oldRuntime.Location]*oldInterpreter.Program
+}
+
+func newOldInter(data []ledger.Payload) *oldInter {
+	payloads := make(map[string][]byte, len(data))
+	for _, p := range data {
+		fk := string(p.Key.KeyParts[0].Value) + "-" + string(p.Key.KeyParts[2].Value)
+		payloads[fk] = p.Value
+	}
+
+	return &oldInter{
+		payloads: payloads,
+		programs: make(map[oldRuntime.Location]*oldInterpreter.Program),
+	}
+}
+
+func (i *oldInter) ResolveLocation(identifiers []oldRuntime.Identifier, location oldRuntime.Location) ([]oldRuntime.ResolvedLocation, error) {
+	return []oldRuntime.ResolvedLocation{
+		{
+			Location:    location,
+			Identifiers: identifiers,
+		},
+	}, nil
+}
+func (i *oldInter) SetProgram(location oldRuntime.Location, program *oldInterpreter.Program) error {
+	i.programs[location] = program
+	return nil
+}
+func (i *oldInter) GetProgram(location oldCommon.Location) (*oldInterpreter.Program, error) {
+	return i.programs[location], nil
+}
+func (i *oldInter) GetCode(_ oldRuntime.Location) ([]byte, error) { return nil, nil }
+func (i *oldInter) ValueExists(owner, key []byte) (exists bool, err error) {
+	fk := string(owner) + "-" + string(key)
+	return len(i.payloads[fk]) > 0, nil
+}
+func (i *oldInter) GetValue(owner, key []byte) (value []byte, err error) {
+	fk := string(owner) + "-" + string(key)
+	return i.payloads[fk], nil
+}
+func (i *oldInter) SetValue(owner, key, value []byte) error {
+	fk := string(owner) + "-" + string(key)
+	i.payloads[fk] = value
+	return nil
+}
+func (i *oldInter) CreateAccount(_ oldRuntime.Address) (address oldRuntime.Address, err error) {
+	return oldRuntime.Address{}, nil
+}
+func (i *oldInter) AddEncodedAccountKey(_ oldRuntime.Address, _ []byte) error {
+	return nil
+}
+func (i *oldInter) RevokeEncodedAccountKey(_ oldRuntime.Address, _ int) ([]byte, error) {
+	return nil, nil
+}
+func (i *oldInter) AddAccountKey(_ oldRuntime.Address, _ *oldRuntime.PublicKey, _ oldRuntime.HashAlgorithm, _ int) (*oldRuntime.AccountKey, error) {
+	return nil, nil
+}
+func (i *oldInter) RevokeAccountKey(_ oldRuntime.Address, _ int) (*oldRuntime.AccountKey, error) {
+	return nil, nil
+}
+func (i *oldInter) GetAccountKey(_ oldRuntime.Address, _ int) (*oldRuntime.AccountKey, error) {
+	return nil, nil
+}
+func (i *oldInter) UpdateAccountContractCode(_ oldRuntime.Address, _ string, _ []byte) (err error) {
+	return nil
+}
+func (i *oldInter) GetAccountContractCode(_ oldRuntime.Address, _ string) (code []byte, err error) {
+	return nil, nil
+}
+func (i *oldInter) RemoveAccountContractCode(_ oldRuntime.Address, _ string) (err error) {
+	return nil
+}
+func (i *oldInter) GetSigningAccounts() ([]oldRuntime.Address, error) {
+	return nil, nil
+}
+func (i *oldInter) ProgramLog(_ string) error {
+	return nil
+}
+func (i *oldInter) EmitEvent(_ cadence.Event) error {
+	return nil
+}
+func (i *oldInter) GenerateUUID() (uint64, error) {
+	return 0, nil
+}
+func (i *oldInter) GetComputationLimit() uint64 {
+	return 0
+}
+func (i *oldInter) SetComputationUsed(uint64) error {
+	return nil
+}
+func (i *oldInter) DecodeArgument(_ []byte, _ cadence.Type) (cadence.Value, error) {
+	return nil, nil
+}
+func (i *oldInter) GetCurrentBlockHeight() (uint64, error) {
+	return 0, nil
+}
+func (i *oldInter) GetBlockAtHeight(_ uint64) (block oldRuntime.Block, exists bool, err error) {
+	return
+}
+func (i *oldInter) UnsafeRandom() (uint64, error) {
+	return 0, nil
+}
+func (i *oldInter) ImplementationDebugLog(_ string) error {
+	return nil
+}
+func (i *oldInter) VerifySignature(_ []byte, _ string, _ []byte, _ []byte, _ oldRuntime.SignatureAlgorithm, _ oldRuntime.HashAlgorithm) (bool, error) {
+	return false, nil
+}
+func (i *oldInter) Hash(_ []byte, _ string, _ oldRuntime.HashAlgorithm) ([]byte, error) {
+	return nil, nil
+}
+func (i oldInter) GetAccountBalance(_ oldRuntime.Address) (uint64, error) {
+	return 0, nil
+}
+func (i oldInter) GetAccountAvailableBalance(_ oldRuntime.Address) (uint64, error) {
+	return 0, nil
+}
+func (i oldInter) GetStorageUsed(_ oldRuntime.Address) (uint64, error) {
+	return 0, nil
+}
+func (i oldInter) GetStorageCapacity(_ oldRuntime.Address) (uint64, error) {
+	return 0, nil
+}
+func (i *oldInter) ValidatePublicKey(_ *oldRuntime.PublicKey) (bool, error) {
+	return false, nil
+}
+func (i *oldInter) GetAccountContractNames(_ oldRuntime.Address) ([]string, error) {
+	return nil, nil
 }
