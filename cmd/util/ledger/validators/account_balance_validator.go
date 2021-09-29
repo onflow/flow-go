2d581d0a7764046b24e9a45867f6aed53390ad93
ramtinms <ramtin@axiomzen.co>
2021-09-28 18:46:24 -0700

    atempt to rewrite

--- cmd/util/ledger/validators/account_balance_validator.go
+++ cmd/util/ledger/validators/account_balance_validator.go
@@ -8,7 +8,8 @@ import (
 	"github.com/onflow/cadence"
 	jsoncdc "github.com/onflow/cadence/encoding/json"
 
-	// oldFvm "github.com/onflow/flow-go/v21/fvm"
+	oldFvm "github.com/onflow/flow-go/v21/fvm"
+	oldState "github.com/onflow/flow-go/v21/fvm/"
 	"github.com/rs/zerolog"
 
 	"github.com/onflow/flow-go/fvm"
@@ -19,106 +20,15 @@ import (
 )
 
 type accountBalance struct {
-	account flow.Address
-	balance uint64
+	addressIndex uint64
+	balance      uint64
 }
 
-type AccountBalanceValidator struct {
-	chain            flow.Chain
-	logger           zerolog.Logger
-	numberOfAccounts uint64
-	addresses        chan flow.Address
-	balances         chan accountBalance
+type balanceCollector interface {
+	CollectBalances(addressIndices chan uint64, balances chan accountBalance)
 }
 
-func NewAccountBalanceValidator(logger zerolog.Logger, chain flow.Chain) *AccountBalanceValidator {
-	return &AccountBalanceValidator{
-		chain:  chain,
-		logger: logger,
-	}
-}
-
-func (v *AccountBalanceValidator) Setup(oldPayloads []ledger.Payload) error {
-
-	mainView := newView(oldPayloads)
-	st := state.NewState(mainView)
-	sth := state.NewStateHolder(st)
-
-	gen := state.NewStateBoundAddressGenerator(sth, v.chain)
-	v.numberOfAccounts = gen.AddressCount()
-	v.addresses = make(chan flow.Address, v.numberOfAccounts)
-	v.balances = make(chan accountBalance, v.numberOfAccounts)
-
-	fvmContext := fvm.NewContext(
-		v.logger,
-		fvm.WithChain(v.chain),
-	)
-
-	for i := uint64(0); i < v.numberOfAccounts; i++ {
-		add, err := v.chain.AddressAtIndex(i)
-		if err != nil {
-			return err
-		}
-		v.addresses <- add
-	}
-
-	workerCount := runtime.NumCPU()
-	wg := &sync.WaitGroup{}
-
-	for i := 0; i < workerCount; i++ {
-		wg.Add(1)
-		// TODO switch to old runtime
-		vm := fvm.NewVirtualMachine(fvm.NewInterpreterRuntime())
-		worker := newWorker(wg, vm, fvmContext, mainView.NewChild(), v.logger)
-		go worker.Collect(v.addresses, v.balances)
-	}
-
-	close(v.addresses)
-	wg.Wait()
-	return nil
-}
-
-func (v *AccountBalanceValidator) Validate(newPayloads []ledger.Payload) (isValid bool, err error) {
-
-	mainView := newView(newPayloads)
-	st := state.NewState(mainView)
-	sth := state.NewStateHolder(st)
-
-	gen := state.NewStateBoundAddressGenerator(sth, v.chain)
-	if v.numberOfAccounts != gen.AddressCount() {
-		return false, fmt.Errorf("number of accounts doesn't match after migration (before:%d after: %d)", v.numberOfAccounts, gen.AddressCount())
-	}
-
-	fvmContext := fvm.NewContext(
-		v.logger,
-		fvm.WithChain(v.chain),
-	)
-
-	workerCount := runtime.NumCPU()
-	wg := &sync.WaitGroup{}
-
-	notVerified := make(chan flow.Address, v.numberOfAccounts)
-
-	for i := 0; i < workerCount; i++ {
-		wg.Add(1)
-		// new runtime
-		rt := fvm.NewInterpreterRuntime()
-		worker := newWorker(wg, fvm.NewVirtualMachine(rt), fvmContext, mainView.NewChild(), v.logger)
-		go worker.CollectAndVerify(v.balances, notVerified)
-	}
-	//
-
-	close(v.balances)
-	wg.Wait()
-	close(notVerified)
-
-	if len(notVerified) != 0 {
-		return false, fmt.Errorf("some balances doesn't match")
-	}
-	return true, nil
-}
-
-type worker struct {
+type newVersionCollector struct {
 	wg     *sync.WaitGroup
 	logger zerolog.Logger
 	vm     *fvm.VirtualMachine
@@ -128,12 +38,13 @@ type worker struct {
 	script []byte
 }
 
-func newWorker(wg *sync.WaitGroup,
-	vm *fvm.VirtualMachine,
-	ctx fvm.Context,
-	view state.View,
+func newNewVersionCollector(
+	wg *sync.WaitGroup,
 	logger zerolog.Logger,
-) *worker {
+	chain flow.Chain,
+	view state.View) *newVersionCollector {
+	vm := fvm.NewVirtualMachine(fvm.NewInterpreterRuntime())
+	ctx := fvm.NewContext(zerolog.Nop(), fvm.WithChain(chain))
 	prog := programs.NewEmptyPrograms()
 	script := []byte(fmt.Sprintf(`
 				import FungibleToken from 0x%s
@@ -149,63 +60,208 @@ func newWorker(wg *sync.WaitGroup,
 				}
 			`, fvm.FungibleTokenAddress(ctx.Chain), fvm.FlowTokenAddress(ctx.Chain)))
 
-	return &worker{wg: wg,
+	return &newVersionCollector{
+		wg:     wg,
 		logger: logger,
 		vm:     vm,
 		ctx:    ctx,
-		view:   view,
+		view:   view.NewChild(),
 		prog:   prog,
 		script: script}
 }
 
 // change this to incoming channel and outgoing channel
-func (w *worker) Collect(addresses chan flow.Address, balances chan accountBalance) {
-	for add := range addresses {
-		balance, err := w.collectBalance(add)
+func (c *newVersionCollector) CollectBalances(addresses chan uint64, balances chan accountBalance) {
+	for indx := range addresses {
+		var balance uint64
+		address, err := c.ctx.Chain.AddressAtIndex(indx)
 		if err != nil {
-			w.logger.Err(err).Msgf("Error collecting balance for account %s", add.String())
+			c.logger.Err(err).Msgf("Error collecting balance for account %s", address.String())
 		}
-		balances <- accountBalance{account: add, balance: balance}
+
+		script := fvm.Script(c.script).WithArguments(
+			jsoncdc.MustEncode(cadence.NewAddress(address)),
+		)
+
+		err = c.vm.Run(c.ctx, script, c.view, c.prog)
+		if err != nil {
+			c.logger.Err(err).Msgf("Error collecting balance for account %s", address.String())
+		}
+
+		if script.Err == nil && script.Value != nil {
+			balance = script.Value.ToGoValue().(uint64)
+		} else {
+			c.logger.Err(script.Err).Msgf("Error collecting balance for account %s", address.String())
+		}
+
+		balances <- accountBalance{addressIndex: indx, balance: balance}
 		// for debugging
-		w.logger.Info().Msgf("collected balance for %s (%d)", add.String(), balance)
+		c.logger.Info().Msgf("collected balance for %s (%d)", address.String(), balance)
 	}
-	w.wg.Done()
+	c.wg.Done()
 }
 
-func (w *worker) CollectAndVerify(balances chan accountBalance, notVerified chan flow.Address) {
-	for b := range balances {
-
-		balance, _ := w.collectBalance(b.account)
-		// TODO log errors to a file
-		// if err != nil {
-		// 	w.logger.Warn(err).Msgf("Error collecting balance for account %s", b.account.String())
-		// }
-		if balance != b.balance {
-			w.logger.Warn().Msgf("balance for account %s doesn't match the one before migration (before: %d, after: %d)", b.account.String(), b.balance, balance)
-			notVerified <- b.account
-		}
-		// for debugging
-		w.logger.Info().Msgf("collected and verified balance for %s (%d)", b.account.String(), balance)
-	}
-	w.wg.Done()
+type oldVersionCollector struct {
+	wg     *sync.WaitGroup
+	logger zerolog.Logger
+	vm     *oldFvm.VirtualMachine
+	ctx    oldFvm.Context
+	view   oldState.View
+	prog   *programs.Programs
+	script []byte
 }
 
-func (w *worker) collectBalance(address flow.Address) (uint64, error) {
-	script := fvm.Script(w.script).WithArguments(
-		jsoncdc.MustEncode(cadence.NewAddress(address)),
-	)
+func newOldVersionCollector(wg *sync.WaitGroup,
+	logger zerolog.Logger,
+	chain flow.Chain,
+	view oldState.View) *oldVersionCollector {
+	vm := oldFvm.NewVirtualMachine(oldFvm.NewInterpreterRuntime())
+	ctx := oldFvm.NewContext(zerolog.Nop(), oldFvm.WithChain(chain))
+	prog := programs.NewEmptyPrograms()
+	script := []byte(fmt.Sprintf(`
+				import FungibleToken from 0x%s
+				import FlowToken from 0x%s
+				
+				pub fun main(account: Address): UFix64 {
+					let acct = getAccount(account)
+					let vaultRef = acct.getCapability(/public/flowTokenBalance)
+						.borrow<&FlowToken.Vault{FungibleToken.Balance}>()
+						?? panic("Could not borrow Balance reference to the Vault")
+				
+					return vaultRef.balance
+				}
+			`, fvm.FungibleTokenAddress(ctx.Chain), fvm.FlowTokenAddress(ctx.Chain)))
 
-	err := w.vm.Run(w.ctx, script, w.view, w.prog)
-	if err != nil {
-		return 0, err
-	}
+	return &oldVersionCollector{
+		wg:     wg,
+		logger: logger,
+		vm:     vm,
+		ctx:    ctx,
+		view:   view.NewChild(),
+		prog:   prog,
+		script: script}
+}
 
-	if script.Err == nil {
-		if script.Value != nil {
-			return script.Value.ToGoValue().(uint64), nil
+// change this to incoming channel and outgoing channel
+func (c *oldVersionCollector) CollectBalances(addresses chan uint64, balances chan accountBalance) {
+	for indx := range addresses {
+		var balance uint64
+		address, err := c.ctx.Chain.AddressAtIndex(indx)
+		if err != nil {
+			c.logger.Err(err).Msgf("Error collecting balance for account %s", address.String())
 		}
-		// account might not have flow token receiver
-		return 0, nil
+
+		script := oldFvm.Script(c.script).WithArguments(
+			jsoncdc.MustEncode(cadence.NewAddress(address)),
+		)
+
+		err = c.vm.Run(c.ctx, script, c.view, c.prog)
+		if err != nil {
+			c.logger.Err(err).Msgf("Error collecting balance for account %s", address.String())
+		}
+
+		if script.Err == nil && script.Value != nil {
+			balance = script.Value.ToGoValue().(uint64)
+		} else {
+			c.logger.Err(script.Err).Msgf("Error collecting balance for account %s", address.String())
+		}
+
+		balances <- accountBalance{addressIndex: indx, balance: balance}
+		// for debugging
+		c.logger.Info().Msgf("collected balance for %s (%d)", address.String(), balance)
 	}
-	return 0, script.Err
+	c.wg.Done()
+}
+
+type AccountBalanceValidator struct {
+	chain            flow.Chain
+	logger           zerolog.Logger
+	numberOfAccounts uint64
+	originalBalances map[uint64]uint64
+}
+
+func NewAccountBalanceValidator(logger zerolog.Logger, chain flow.Chain) *AccountBalanceValidator {
+	return &AccountBalanceValidator{
+		chain:            chain,
+		logger:           logger,
+		originalBalances: make(map[uint64]uint64),
+	}
+}
+
+func (v *AccountBalanceValidator) Setup(oldPayloads []ledger.Payload) error {
+
+	mainView := newView(oldPayloads)
+	st := state.NewState(mainView)
+	sth := state.NewStateHolder(st)
+	gen := state.NewStateBoundAddressGenerator(sth, v.chain)
+	v.numberOfAccounts = gen.AddressCount()
+
+	addressIndices := make(chan uint64, v.numberOfAccounts)
+	balances := make(chan accountBalance, v.numberOfAccounts)
+
+	for i := uint64(0); i < v.numberOfAccounts; i++ {
+		addressIndices <- i
+	}
+
+	workerCount := runtime.NumCPU()
+	wg := &sync.WaitGroup{}
+
+	for i := 0; i < workerCount; i++ {
+		wg.Add(1)
+		worker := newOldVersionCollector(wg, v.logger, v.chain, mainView)
+		go worker.CollectBalances(addressIndices, balances)
+	}
+
+	close(addressIndices)
+	wg.Wait()
+	close(balances)
+
+	for balance := range balances {
+		v.originalBalances[balance.addressIndex] = balance.balance
+	}
+
+	return nil
+}
+
+func (v *AccountBalanceValidator) Validate(newPayloads []ledger.Payload) (isValid bool, err error) {
+
+	mainView := newView(newPayloads)
+	st := state.NewState(mainView)
+	sth := state.NewStateHolder(st)
+
+	gen := state.NewStateBoundAddressGenerator(sth, v.chain)
+	if v.numberOfAccounts != gen.AddressCount() {
+		return false, fmt.Errorf("number of accounts doesn't match after migration (before:%d after: %d)", v.numberOfAccounts, gen.AddressCount())
+	}
+
+	addressIndices := make(chan uint64, v.numberOfAccounts)
+	newBalances := make(chan accountBalance, v.numberOfAccounts)
+
+	for i := uint64(0); i < v.numberOfAccounts; i++ {
+		addressIndices <- i
+	}
+
+	workerCount := runtime.NumCPU()
+	wg := &sync.WaitGroup{}
+
+	for i := 0; i < workerCount; i++ {
+		wg.Add(1)
+		worker := newNewVersionCollector(wg, v.logger, v.chain, mainView)
+		go worker.CollectBalances(addressIndices, newBalances)
+	}
+
+	close(addressIndices)
+	wg.Wait()
+	close(newBalances)
+
+	// comparing the balances
+	for newb := range newBalances {
+		orgBalance := v.originalBalances[newb.addressIndex]
+		if orgBalance != newb.balance {
+			add, _ := v.chain.AddressAtIndex(newb.addressIndex)
+			return false, fmt.Errorf("balances are not matching for account %s (before: %d, after: %d)", add.String(), orgBalance, newb.balance)
+		}
+	}
+
+	return true, nil
 }
