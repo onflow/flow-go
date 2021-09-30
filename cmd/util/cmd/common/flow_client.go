43da870cde0cab47866da497b364fddb8d7c602d
Khalil Claybon <khalil.claybon@dapperlabs.com>
2021-09-30 12:07:51 -0400

    update integration tests
    - update integration tests flags and container configs (from #1323)

--- cmd/util/cmd/common/flow_client.go
+++ cmd/util/cmd/common/flow_client.go
@@ -82,40 +82,57 @@ func insecureFlowClient(accessAddress string) (*client.Client, error) {
 // PrepareFlowClientOpts will assemble connection options for the flow client for each access node id
 func PrepareFlowClientOpts(accessNodeIDS []string, insecureAccessAPI bool, snapshot protocol.Snapshot) ([]*FlowClientOpt, error) {
 	flowClientOpts := make([]*FlowClientOpt, 0)
-	for i, id := range accessNodeIDS {
-		nodeID, err := flow.HexStringToIdentifier(id)
+
+	// convert all IDS to flow.Identifier type, fail early if ID is invalid
+	anIDS := make([]flow.Identifier, 0)
+	for _, anID := range accessNodeIDS {
+		id, err := flow.HexStringToIdentifier(anID)
 		if err != nil {
 			return nil, fmt.Errorf("could not get flow identifer from secured access node id: %s", id)
 		}
 
-		identities, err := snapshot.Identities(filter.HasNodeID(nodeID))
-		if err != nil {
-			return nil, fmt.Errorf("could not get identity of secure access node: %s", id)
-		}
+		anIDS = append(anIDS, id)
+	}
 
-		if len(identities) < 1 {
-			return nil, fmt.Errorf("could not find identity of secure access node: %s", id)
-		}
+	identities, err := snapshot.Identities(filter.HasNodeID(anIDS...))
+	if err != nil {
+		return nil, fmt.Errorf("failed get identities access node IDs from snapshot: %v", accessNodeIDS)
+	}
 
-		// remove gossip port from access address and add respective secure or insecure port
-		var accessAddress strings.Builder
-		accessAddress.WriteString(strings.Split(identities[0].Address, ":")[0])
+	// make sure we have identities for all the access node IDs provided
+	if len(identities) != len(accessNodeIDS) {
+		return nil, fmt.Errorf("failed to get identity for all the access node IDs provided: %v, got %s", accessNodeIDS, identities)
+	}
 
-		if insecureAccessAPI {
-			accessAddress.WriteString(fmt.Sprintf(":%s", DefaultAccessAPIPort))
-		} else {
-			accessAddress.WriteString(fmt.Sprintf(":%s", DefaultAccessAPISecurePort))
-		}
+	// build a FlowClientOpt for each access node identity, these will be used to manage multiple flow client connections
+	for i, identity := range identities {
+		accessAddress := ConvertAccessAddrFromState(identity.Address, insecureAccessAPI)
 
 		// remove the 0x prefix from network public keys
 		networkingPubKey := identities[0].NetworkPubKey.String()[2:]
 
-		opt, err := NewFlowClientOpt(accessAddress.String(), networkingPubKey, insecureAccessAPI)
+		opt, err := NewFlowClientOpt(accessAddress, networkingPubKey, insecureAccessAPI)
 		if err != nil {
-			return nil, fmt.Errorf("failed to get flow client connection option for access node ID (%x): %s %w", i, id, err)
+			return nil, fmt.Errorf("failed to get flow client connection option for access node ID (%x): %s %w", i, identity, err)
 		}
 
 		flowClientOpts = append(flowClientOpts, opt)
 	}
 	return flowClientOpts, nil
+}
+
+// ConvertAccessAddrFromState takes raw network address from the protocol state in the for of [DNS/IP]:PORT, removes the port and applies the appropriate
+// port number depending on the insecureAccessAPI arg.
+func ConvertAccessAddrFromState(address string, insecureAccessAPI bool) string  {
+	// remove gossip port from access address and add respective secure or insecure port
+	var accessAddress strings.Builder
+	accessAddress.WriteString(strings.Split(address, ":")[0])
+
+	if insecureAccessAPI {
+		accessAddress.WriteString(fmt.Sprintf(":%s", DefaultAccessAPIPort))
+	} else {
+		accessAddress.WriteString(fmt.Sprintf(":%s", DefaultAccessAPISecurePort))
+	}
+
+	return accessAddress.String()
 }
