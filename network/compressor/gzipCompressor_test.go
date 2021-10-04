21d3c10ec42e23923b8c4e0696ffeaf73d64d638
Yahya <yahya@dapperlabs.com>
2021-10-04 16:27:14 -0700

    refactors tests

--- network/compressor/gzipCompressor_test.go
+++ network/compressor/gzipCompressor_test.go
@@ -14,6 +14,8 @@ import (
 // and (2) data is compressed when written.
 func TestRoundTrip(t *testing.T) {
 	text := "hello world, hello world!"
+	textBytes := []byte(text)
+	textBytesLen := len(textBytes)
 	buf := new(bytes.Buffer)
 
 	gzipComp := compressor.GzipStreamCompressor{}
@@ -21,18 +23,21 @@ func TestRoundTrip(t *testing.T) {
 	w, err := gzipComp.NewWriter(buf)
 	require.NoError(t, err)
 
-	n, err := w.Write([]byte(text))
+	// testing write
+	//
+	n, err := w.Write(textBytes)
 	require.NoError(t, err)
-	require.Equal(t, n, len(text))
+	// written bytes should match original data
+	require.Equal(t, n, textBytesLen)
 	// written data on buffer should be compressed in size.
-	require.Less(t, buf.Len(), len(text))
-
+	require.Less(t, buf.Len(), textBytesLen)
 	require.NoError(t, w.Close())
 
+	// testing read
+	//
 	r, err := gzipComp.NewReader(buf)
 	require.NoError(t, err)
-
 	b, err := io.ReadAll(r)
 	require.NoError(t, err)
-	require.Equal(t, b, []byte(text))
+	require.Equal(t, b, textBytes)
 }
