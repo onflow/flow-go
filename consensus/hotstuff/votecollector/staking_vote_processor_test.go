4ec022905c1f052c153eb06c7ad66b577b9a2932
Yurii Oleksyshyn <yuraolex@gmail.com>
2021-10-04 15:27:37 +0300

    Linted

--- consensus/hotstuff/votecollector/staking_vote_processor_test.go
+++ consensus/hotstuff/votecollector/staking_vote_processor_test.go
@@ -2,17 +2,19 @@ package votecollector
 
 import (
 	"errors"
+	"sync"
+	"testing"
+
+	"github.com/stretchr/testify/mock"
+	"github.com/stretchr/testify/require"
+	"github.com/stretchr/testify/suite"
+
 	"github.com/onflow/flow-go/consensus/hotstuff"
 	mockhotstuff "github.com/onflow/flow-go/consensus/hotstuff/mocks"
 	"github.com/onflow/flow-go/consensus/hotstuff/model"
 	"github.com/onflow/flow-go/model/flow"
 	msig "github.com/onflow/flow-go/module/signature"
 	"github.com/onflow/flow-go/utils/unittest"
-	"github.com/stretchr/testify/mock"
-	"github.com/stretchr/testify/require"
-	"github.com/stretchr/testify/suite"
-	"sync"
-	"testing"
 )
 
 func TestStakingVoteProcessor(t *testing.T) {
