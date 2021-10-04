ff77e291dd145b63f61e8204dca582383a74de92
Simon Zhu <simon.zsiyan@gmail.com>
2021-10-04 13:14:56 -0700

    port sync engine changes

--- module/synchronization/core.go
+++ module/synchronization/core.go
@@ -106,7 +106,7 @@ func (c *Core) HandleHeight(final *flow.Header, height uint64) {
 		c.mu.Lock()
 		defer c.mu.Unlock()
 		for h := final.Height + 1; h <= height; h++ {
-			c.queueByHeight(h)
+			c.requeueHeight(h)
 		}
 	}
 }
@@ -129,6 +129,12 @@ func (c *Core) RequestHeight(height uint64) {
 	c.mu.Lock()
 	defer c.mu.Unlock()
 
+	c.requeueHeight(height)
+}
+
+// requeueHeight queues the given height, ignoring any previously received
+// blocks at that height
+func (c *Core) requeueHeight(height uint64) {
 	// if we already received this block, reset the status so we can re-queue
 	status := c.heights[height]
 	if status.WasReceived() {
