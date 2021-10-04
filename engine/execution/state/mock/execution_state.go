f09e3ca78b4deee97b4bb30e5f341a3ed605db1c
ramtinms <ramtin@axiomzen.co>
2021-10-03 23:02:22 -0700

    mock update

--- engine/execution/state/mock/execution_state.go
+++ engine/execution/state/mock/execution_state.go
@@ -202,6 +202,20 @@ func (_m *ExecutionState) NewView(_a0 flow.StateCommitment) *delta.View {
 	return r0
 }
 
+// PurgeChunkDataPacksByBlockID provides a mock function with given fields: _a0, _a1
+func (_m *ExecutionState) PurgeChunkDataPacksByBlockID(_a0 context.Context, _a1 flow.Identifier) error {
+	ret := _m.Called(_a0, _a1)
+
+	var r0 error
+	if rf, ok := ret.Get(0).(func(context.Context, flow.Identifier) error); ok {
+		r0 = rf(_a0, _a1)
+	} else {
+		r0 = ret.Error(0)
+	}
+
+	return r0
+}
+
 // RetrieveStateDelta provides a mock function with given fields: _a0, _a1
 func (_m *ExecutionState) RetrieveStateDelta(_a0 context.Context, _a1 flow.Identifier) (*messages.ExecutionStateDelta, error) {
 	ret := _m.Called(_a0, _a1)
