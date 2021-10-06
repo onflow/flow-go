5f187298fad4b777216b5d49df8fc2dfa567a68e
ramtinms <ramtin@axiomzen.co>
2021-10-05 17:08:53 -0700

    update state commitments

--- utils/unittest/execution_state.go
+++ utils/unittest/execution_state.go
@@ -19,7 +19,7 @@ import (
 const ServiceAccountPrivateKeyHex = "e3a08ae3d0461cfed6d6f49bfc25fa899351c39d1bd21fdba8c87595b6c49bb4cc430201"
 
 // Pre-calculated state commitment with root account with the above private key
-const GenesisStateCommitmentHex = "09c48daae12a1a23fe9c41f9d836a56785a89404d7c792d99c37289981f5c56f"
+const GenesisStateCommitmentHex = "0a05e884b52abd732a09a917b24138dffc1f83d1e92f9f6e2f2fb115381292af"
 
 var GenesisStateCommitment flow.StateCommitment
 
