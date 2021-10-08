e136f19d2e9f76f46b5af0d0f80ba92aab4ac291
Yahya <yahya@dapperlabs.com>
2021-10-08 13:41:52 -0700

    renames receivers

--- network/compressor/gzipCompressor.go
+++ network/compressor/gzipCompressor.go
@@ -9,11 +9,11 @@ import (
 
 type GzipStreamCompressor struct{}
 
-func (g GzipStreamCompressor) NewReader(r io.Reader) (io.ReadCloser, error) {
+func (gzipStreamComp GzipStreamCompressor) NewReader(r io.Reader) (io.ReadCloser, error) {
 	return gzip.NewReader(r)
 }
 
-func (g GzipStreamCompressor) NewWriter(w io.Writer) (network.WriteCloseFlusher, error) {
+func (gzipStreamComp GzipStreamCompressor) NewWriter(w io.Writer) (network.WriteCloseFlusher, error) {
 	return &gzipWriteCloseFlusher{w: gzip.NewWriter(w)}, nil
 }
 
@@ -21,14 +21,14 @@ type gzipWriteCloseFlusher struct {
 	w *gzip.Writer
 }
 
-func (g *gzipWriteCloseFlusher) Write(p []byte) (int, error) {
-	return g.w.Write(p)
+func (gzipW *gzipWriteCloseFlusher) Write(p []byte) (int, error) {
+	return gzipW.w.Write(p)
 }
 
-func (g *gzipWriteCloseFlusher) Close() error {
-	return g.w.Close()
+func (gzipW *gzipWriteCloseFlusher) Close() error {
+	return gzipW.w.Close()
 }
 
-func (g *gzipWriteCloseFlusher) Flush() error {
-	return g.w.Flush()
+func (gzipW *gzipWriteCloseFlusher) Flush() error {
+	return gzipW.w.Flush()
 }
