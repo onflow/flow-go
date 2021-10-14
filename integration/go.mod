28485b4c9c13b4b8d484a099ee5b45c4844549c7
Khalil Claybon <khalil.claybon@dapperlabs.com>
2021-10-14 11:48:37 -0400

    update testing dock version, use container by ID, remove return of suitecontainer

--- integration/go.mod
+++ integration/go.mod
@@ -5,7 +5,7 @@ go 1.15
 require (
 	github.com/DataDog/zstd v1.4.8 // indirect
 	github.com/HdrHistogram/hdrhistogram-go v1.0.1 // indirect
-	github.com/dapperlabs/testingdock v0.4.3-0.20200626075145-ea23fc16bb90
+	github.com/dapperlabs/testingdock v0.4.3
 	github.com/desertbit/timer v0.0.0-20180107155436-c41aec40b27f // indirect
 	github.com/dgraph-io/badger/v2 v2.2007.2
 	github.com/dgraph-io/ristretto v0.0.3 // indirect
--- integration/tests/epochs/epoch_test.go
+++ integration/tests/epochs/epoch_test.go
@@ -2,18 +2,18 @@ package epochs
 
 import (
 	"context"
-	"testing"
 
 	"github.com/onflow/cadence"
-	"github.com/onflow/flow-go/integration/testnet"
 	"github.com/onflow/flow-go/integration/utils"
 	"github.com/onflow/flow-go/model/encodable"
+	"github.com/stretchr/testify/suite"
+	"testing"
+
+	"github.com/onflow/flow-go/integration/testnet"
 	"github.com/onflow/flow-go/model/flow"
 	"github.com/onflow/flow-go/state/protocol"
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
-	"github.com/stretchr/testify/suite"
-	"log"
 )
 
 func TestEpochs(t *testing.T) {
@@ -164,13 +164,26 @@ func (s *Suite) TestEpochJoin() {
 	}
 
 	require.True(s.T(), found, "node id for new node not found in approved list after setting the approved list")
-	nodeConfig := testnet.NewNodeConfig(role)
+
+	nodeConfig := testnet.NewNodeConfig(role, testnet.WithID(info.NodeID))
 	testContainerConfig := testnet.NewContainerConfig("epochs-test-container", nodeConfig, info.NetworkingKey, info.StakingAccountKey)
 	err := testContainerConfig.WriteKeyFiles(s.net.BootstrapDir, flow.Localnet, info.MachineAccountAddress, encodable.MachineAccountPrivKey{PrivateKey: info.MachineAccountKey})
-	testContainer, err := s.net.AddNode(s.T(), s.net.BootstrapDir, testContainerConfig)
-	require.NoError(s.T(), err, "failed to add container to network")
-	log.Println("THIS IS A IMAGE MAN", testContainer.Image)
 	require.NoError(s.T(), err)
 
-	s.net.StopContainers()
+	// download root snapshot from access node
+	snapshot, err := s.client.GetLatestProtocolSnapshot(ctx)
+	require.NoError(s.T(), err)
+
+	// write updated root snapshot
+	s.net.WriteRootSnapshot(snapshot)
+
+	// add our container to the network
+	err = s.net.AddNode(s.T(), s.net.BootstrapDir, testContainerConfig)
+	require.NoError(s.T(), err, "failed to add container to network")
+
+	// start our test container
+	testContainer := s.net.ContainerByID(info.NodeID)
+	testContainer.Container.Start(ctx)
+
+	//s.net.StopContainers()
 }
