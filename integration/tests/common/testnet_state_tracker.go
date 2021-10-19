4db2acfdaa06d61ff9db72b91f467baa5e234404
Simon Zhu <simon.zsiyan@gmail.com>
2021-10-19 10:11:12 -0700

    Update testnet_state_tracker.go

--- integration/tests/common/testnet_state_tracker.go
+++ integration/tests/common/testnet_state_tracker.go
@@ -100,7 +100,7 @@ func (tst *TestnetStateTracker) Track(t *testing.T, ctx context.Context, ghost *
 				finalState, err := m.ExecutionResult.FinalStateCommitment()
 				require.NoError(t, err)
 
-				tst.ReceiptState.Add(t, m)
+				tst.ReceiptState.Add(m)
 				t.Logf("execution receipts received from %s for block ID %x by executor ID %x with SC %x resultID %x\n",
 					sender,
 					m.ExecutionResult.BlockID,
