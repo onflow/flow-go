2bbbd35edcb19ab7791f98928793c6e8c126982d
Leo Zhang (zhangchiqing) <zhangchiqing@gmail.com>
2021-10-22 10:22:09 -0700

    add concurrency test

--- engine/consensus/approvals/approvals_lru_cache_test.go
+++ engine/consensus/approvals/approvals_lru_cache_test.go
@@ -1,6 +1,7 @@
 package approvals
 
 import (
+	"sync"
 	"testing"
 
 	"github.com/stretchr/testify/require"
@@ -20,5 +21,29 @@ func TestApprovalsLRUCacheSecondaryIndexPurge(t *testing.T) {
 		cache.Put(approval)
 		require.Equal(t, approval, cache.Get(approval.Body.PartialID()))
 	}
-	require.Equal(t, int(numElements), len(cache.byResultID))
+
+	// LRU kept the last 10 approvals
+	for i := 10; i < 20; i++ {
+		approval := approvals[i]
+		require.Equal(t, approval, cache.Get(approval.Body.PartialID()))
+	}
+	require.Len(t, cache.byResultID, int(numElements))
+}
+
+func TestApprovalsLRUCacheSecondaryIndexPurgeConcurrently(t *testing.T) {
+	numElements := 100
+	cache := NewApprovalsLRUCache(uint(numElements))
+
+	var wg sync.WaitGroup
+
+	for i := 0; i < 2*numElements; i++ {
+		wg.Add(1)
+		go func() {
+			defer wg.Done()
+			approval := unittest.ResultApprovalFixture()
+			cache.Put(approval)
+		}()
+	}
+	wg.Wait()
+	require.Len(t, cache.byResultID, int(numElements))
 }
