e41cd4686b67b70c4b17db7ae8c926a7823d774f
Yahya <yahya@dapperlabs.com>
2021-10-25 13:57:20 -0700

    adds test contains returns true

---
+++ model/verification/chunkDataPackRequest_test.go
@@ -0,0 +1,26 @@
+package verification_test
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/require"
+
+	"github.com/onflow/flow-go/model/chunks"
+	"github.com/onflow/flow-go/model/verification"
+	"github.com/onflow/flow-go/utils/unittest"
+)
+
+func TestChunkDataRequestList_Contain(t *testing.T) {
+	list := unittest.ChunkDataPackRequestListFixture(10)
+
+	t.Run("contains returns true for all existing elements", func(t *testing.T) {
+		for _, req := range list {
+			require.True(t, list.Contains(&verification.ChunkDataPackRequest{
+				Locator: chunks.Locator{
+					ResultID: req.ResultID,
+					Index:    req.Index,
+				},
+			}))
+		}
+	})
+}
