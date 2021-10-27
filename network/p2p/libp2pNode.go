e5d88547269e1cee718cf081457b4327240bc317
Simon Zhu <simon.zsiyan@gmail.com>
2021-10-26 21:13:10 -0700

    move protocol peer cache to new file and add test

--- network/p2p/libp2pNode.go
+++ network/p2p/libp2pNode.go
@@ -13,7 +13,6 @@ import (
 
 	"github.com/hashicorp/go-multierror"
 	"github.com/libp2p/go-libp2p"
-	"github.com/libp2p/go-libp2p-core/event"
 	"github.com/libp2p/go-libp2p-core/host"
 	libp2pnet "github.com/libp2p/go-libp2p-core/network"
 	"github.com/libp2p/go-libp2p-core/peer"
@@ -342,95 +341,6 @@ func (builder *DefaultLibP2PNodeBuilder) Build(ctx context.Context) (*Node, erro
 		Msg("libp2p node started successfully")
 
 	return node, nil
-}
-
-// protocolPeerCache store a mapping from protocol ID to peers who support that protocol
-type protocolPeerCache struct {
-	protocolPeers map[protocol.ID]map[peer.ID]struct{}
-	sync.RWMutex
-}
-
-func newProtocolPeerCache(logger zerolog.Logger, h host.Host) (*protocolPeerCache, error) {
-	sub, err := h.EventBus().
-		Subscribe([]interface{}{new(event.EvtPeerIdentificationCompleted), new(event.EvtPeerProtocolsUpdated)})
-	if err != nil {
-		return nil, fmt.Errorf("could not subscribe to peer protocol update events: %w", err)
-	}
-	p := &protocolPeerCache{protocolPeers: make(map[protocol.ID]map[peer.ID]struct{})}
-	h.Network().Notify(&libp2pnet.NotifyBundle{
-		DisconnectedF: func(n libp2pnet.Network, c libp2pnet.Conn) {
-			peer := c.RemotePeer()
-			if len(n.ConnsToPeer(peer)) == 0 {
-				p.removePeer(peer)
-			}
-		},
-	})
-	go p.consumeSubscription(logger, h, sub)
-
-	return p, nil
-}
-
-func (p *protocolPeerCache) removePeer(peerID peer.ID) {
-	p.Lock()
-	defer p.Unlock()
-	for pid, peers := range p.protocolPeers {
-		delete(peers, peerID)
-		if len(peers) == 0 {
-			delete(p.protocolPeers, pid)
-		}
-	}
-}
-
-func (p *protocolPeerCache) addProtocols(peerID peer.ID, protocols []protocol.ID) {
-	p.Lock()
-	defer p.Unlock()
-	for _, pid := range protocols {
-		peers, ok := p.protocolPeers[pid]
-		if !ok {
-			peers = make(map[peer.ID]struct{})
-			p.protocolPeers[pid] = peers
-		}
-		peers[peerID] = struct{}{}
-	}
-}
-
-func (p *protocolPeerCache) removeProtocols(peerID peer.ID, protocols []protocol.ID) {
-	p.Lock()
-	defer p.Unlock()
-	for _, pid := range protocols {
-		peers := p.protocolPeers[pid]
-		delete(peers, peerID)
-		if len(peers) == 0 {
-			delete(p.protocolPeers, pid)
-		}
-	}
-}
-
-func (p *protocolPeerCache) getPeers(pid protocol.ID) map[peer.ID]struct{} {
-	p.RLock()
-	defer p.RUnlock()
-	return p.protocolPeers[pid]
-}
-
-func (p *protocolPeerCache) consumeSubscription(logger zerolog.Logger, h host.Host, sub event.Subscription) {
-	defer sub.Close()
-	logger.Debug().Msg("starting peer protocol event subscription loop")
-	for e := range sub.Out() {
-		logger.Debug().Interface("event", e).Msg("received new peer protocol event")
-		switch evt := e.(type) {
-		case event.EvtPeerIdentificationCompleted:
-			protocols, err := h.Peerstore().GetProtocols(evt.Peer)
-			if err != nil {
-				logger.Err(err).Str("peer", evt.Peer.String()).Msg("failed to get protocols for peer")
-				continue
-			}
-			p.addProtocols(evt.Peer, protocol.ConvertFromStrings(protocols))
-		case event.EvtPeerProtocolsUpdated:
-			p.addProtocols(evt.Peer, evt.Added)
-			p.removeProtocols(evt.Peer, evt.Removed)
-		}
-	}
-	logger.Debug().Msg("exiting peer protocol event subscription loop")
 }
 
 // Node is a wrapper around the LibP2P host.
---
+++ network/p2p/protocolPeerCache_test.go
@@ -0,0 +1,70 @@
+package p2p
+
+import (
+	"context"
+	"testing"
+	"time"
+
+	"github.com/libp2p/go-libp2p-core/host"
+	"github.com/libp2p/go-libp2p-core/network"
+	"github.com/libp2p/go-libp2p-core/protocol"
+	fcrypto "github.com/onflow/flow-go/crypto"
+	"github.com/onflow/flow-go/utils/unittest"
+	"github.com/rs/zerolog"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+)
+
+func TestProtocolPeerCache(t *testing.T) {
+	ctx, cancel := context.WithCancel(context.Background())
+	defer cancel()
+
+	h1, err := DefaultLibP2PHost(ctx, "0.0.0.0:0", unittest.KeyFixture(fcrypto.ECDSASecp256k1))
+	require.NoError(t, err)
+	pcache, err := newProtocolPeerCache(zerolog.Nop(), h1)
+	require.NoError(t, err)
+
+	h2, err := DefaultLibP2PHost(ctx, "0.0.0.0:0", unittest.KeyFixture(fcrypto.ECDSASecp256k1))
+	require.NoError(t, err)
+
+	h3, err := DefaultLibP2PHost(ctx, "0.0.0.0:0", unittest.KeyFixture(fcrypto.ECDSASecp256k1))
+	require.NoError(t, err)
+
+	p1 := protocol.ID("p1")
+	p2 := protocol.ID("p2")
+	p3 := protocol.ID("p3")
+
+	noopHandler := func(s network.Stream) {}
+	h1.SetStreamHandler(p1, noopHandler)
+	h2.SetStreamHandler(p2, noopHandler)
+	h3.SetStreamHandler(p3, noopHandler)
+
+	require.NoError(t, h1.Connect(ctx, *host.InfoFromHost(h2)))
+	require.NoError(t, h1.Connect(ctx, *host.InfoFromHost(h3)))
+	require.NoError(t, h2.Connect(ctx, *host.InfoFromHost(h3)))
+
+	assert.Eventually(t, func() bool {
+		peers2 := pcache.getPeers(p2)
+		peers3 := pcache.getPeers(p3)
+		_, ok2 := peers2[h2.ID()]
+		_, ok3 := peers3[h3.ID()]
+		return len(peers2) == 1 && len(peers3) == 1 && ok2 && ok3
+	}, 3*time.Second, 50*time.Millisecond)
+
+	h2.RemoveStreamHandler(p2)
+
+	assert.Eventually(t, func() bool {
+		return len(pcache.getPeers(p2)) == 0
+	}, 3*time.Second, 50*time.Millisecond)
+
+	p4 := protocol.ID("p4")
+	h2.SetStreamHandler(p4, noopHandler)
+	h3.SetStreamHandler(p4, noopHandler)
+
+	assert.Eventually(t, func() bool {
+		peers4 := pcache.getPeers(p4)
+		_, ok2 := peers4[h2.ID()]
+		_, ok3 := peers4[h3.ID()]
+		return len(peers4) == 2 && ok2 && ok3
+	}, 3*time.Second, 50*time.Millisecond)
+}
