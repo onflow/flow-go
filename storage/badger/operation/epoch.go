57d9a411f56b40fa4ab8794b39e932ac40d6b1da
Jordan Schalm <jordan@dapperlabs.com>
2021-11-03 11:15:17 -0400

    add storage methods for flagging EECC

--- storage/badger/operation/epoch.go
+++ storage/badger/operation/epoch.go
@@ -29,3 +29,16 @@ func InsertEpochStatus(blockID flow.Identifier, status *flow.EpochStatus) func(*
 func RetrieveEpochStatus(blockID flow.Identifier, status *flow.EpochStatus) func(*badger.Txn) error {
 	return retrieve(makePrefix(codeBlockEpochStatus, blockID), status)
 }
+
+// InsertEpochEmergencyFallbackTriggered sets a flag in the DB indicating that
+// epoch emergency fallback has been triggered. Calling this function multiple
+// is a no-op and returns no expected errors.
+func InsertEpochEmergencyFallbackTriggered() func(txn *badger.Txn) error {
+	return SkipDuplicates(insert(makePrefix(codeEpochEmergencyFallbackTriggered), true))
+}
+
+// RetrieveEpochEmergencyFallbackTriggered retrieves the value of the flag indicating
+// whether epoch emergency fallback has been triggered.
+func RetrieveEpochEmergencyFallbackTriggered(triggered *bool) func(*badger.Txn) error {
+	return retrieve(makePrefix(codeEpochEmergencyFallbackTriggered), &triggered)
+}
--- storage/badger/operation/prefix.go
+++ storage/badger/operation/prefix.go
@@ -83,7 +83,8 @@ const (
 	codeIndexResultApprovalByChunk   = 204
 
 	// internal failure information that should be preserved across restarts
-	codeExecutionFork = 254
+	codeExecutionFork                   = 254
+	codeEpochEmergencyFallbackTriggered = 255
 )
 
 func makePrefix(code byte, keys ...interface{}) []byte {
