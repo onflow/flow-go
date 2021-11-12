6602307f1af9769c02a5a2b1deeb51a170acc8cd
Yurii Oleksyshyn <yuraolex@gmail.com>
2021-11-12 18:30:13 +0200

    Updated tests for random beacon reconsutrctor

--- consensus/hotstuff/signature/randombeacon_reconstructor_test.go
+++ consensus/hotstuff/signature/randombeacon_reconstructor_test.go
@@ -4,10 +4,13 @@ import (
 	"errors"
 	"testing"
 
+	"github.com/stretchr/testify/mock"
 	"github.com/stretchr/testify/require"
 
 	"github.com/onflow/flow-go/consensus/hotstuff/helper"
 	mockhotstuff "github.com/onflow/flow-go/consensus/hotstuff/mocks"
+	"github.com/onflow/flow-go/engine"
+	"github.com/onflow/flow-go/state/protocol"
 	"github.com/onflow/flow-go/utils/unittest"
 )
 
@@ -42,7 +45,58 @@ func TestRandomBeaconReconstructor_InvalidSignerID(t *testing.T) {
 	dkg.AssertExpectations(t)
 }
 
-// TestNewRandomBeaconReconstructor
+// TestNewRandomBeaconReconstructor tests construction process for RandomBeaconReconstructor
+// All errors need to be correctly handled
 func TestNewRandomBeaconReconstructor(t *testing.T) {
+	committee := unittest.IdentityListFixture(5)
+	msg := unittest.RandomBytes(32)
+	t.Run("no-key-share", func(t *testing.T) {
+		dkg := &mockhotstuff.DKG{}
+		dkg.On("KeyShare", mock.Anything).Return(nil, protocol.IdentityNotFoundError{NodeID: unittest.IdentifierFixture()})
+		reconstructor, err := NewRandomBeaconReconstructor(committee, msg, dkg)
+		require.True(t, protocol.IsIdentityNotFound(err))
+		require.Nil(t, reconstructor)
+	})
+	t.Run("invalid-group-key", func(t *testing.T) {
+		dkg := &mockhotstuff.DKG{}
+		dkg.On("Size").Return(uint(len(committee)))
+		for _, identity := range committee {
+			keyShare := helper.MakeBLSKey(t).PublicKey()
+			dkg.On("KeyShare", identity.NodeID).Return(keyShare, nil).Once()
+		}
+		// not bls key
+		dkg.On("GroupKey").Return(unittest.NetworkingPrivKeyFixture().PublicKey()).Once()
+		reconstructor, err := NewRandomBeaconReconstructor(committee, msg, dkg)
+		require.True(t, engine.IsInvalidInputError(err))
+		require.Nil(t, reconstructor)
+	})
+	t.Run("invalid-key-share", func(t *testing.T) {
+		dkg := &mockhotstuff.DKG{}
+		dkg.On("Size").Return(uint(len(committee)))
+		for _, identity := range committee {
+			// not bls key
+			dkg.On("KeyShare", identity.NodeID).Return(identity.NetworkPubKey, nil).Once()
+		}
+		// valid group key
+		dkg.On("GroupKey").Return(helper.MakeBLSKey(t).PublicKey()).Once()
+		reconstructor, err := NewRandomBeaconReconstructor(committee, msg, dkg)
+		require.True(t, engine.IsInvalidInputError(err))
+		require.Nil(t, reconstructor)
+	})
+	t.Run("invalid-size", func(t *testing.T) {
+		dkg := &mockhotstuff.DKG{}
+		// invalid size
+		dkg.On("Size").Return(uint(0))
+		// valid shares
+		for _, identity := range committee {
+			keyShare := helper.MakeBLSKey(t).PublicKey()
+			dkg.On("KeyShare", identity.NodeID).Return(keyShare, nil).Once()
 
+		}
+		// valid group key
+		dkg.On("GroupKey").Return(helper.MakeBLSKey(t).PublicKey()).Once()
+		reconstructor, err := NewRandomBeaconReconstructor(committee, msg, dkg)
+		require.True(t, engine.IsInvalidInputError(err))
+		require.Nil(t, reconstructor)
+	})
 }
