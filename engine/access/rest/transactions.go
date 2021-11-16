7f6bf9e4798f0dc7ab9d185799c7f725da0d0914
Gregor Gololicic <gregor.gololicic@dapperlabs.com>
2021-11-16 19:04:54 +0100

    add transaction handlers

---
+++ engine/access/rest/transactions.go
@@ -0,0 +1,58 @@
+package rest
+
+import (
+	"github.com/onflow/flow-go/access"
+	"github.com/onflow/flow-go/engine/access/rest/generated"
+	"github.com/rs/zerolog"
+	"net/http"
+)
+
+// GetTransactionByID gets a transaction by requested ID.
+func GetTransactionByID(
+	w http.ResponseWriter,
+	r *http.Request,
+	vars map[string]string,
+	backend access.API,
+	logger zerolog.Logger,
+) (interface{}, StatusError) {
+
+	id, err := toID(vars["id"])
+	if err != nil {
+		return nil, NewBadRequest("invalid ID", err)
+	}
+
+	tx, err := backend.GetTransaction(r.Context(), id)
+	if err != nil {
+		return nil, NewBadRequest("transaction fetching error", err)
+	}
+
+	return transactionResponse(tx), nil
+}
+
+// CreateTransaction creates a new transaction from provided payload.
+func CreateTransaction(
+	w http.ResponseWriter,
+	r *http.Request,
+	vars map[string]string,
+	backend access.API,
+	logger zerolog.Logger,
+) (interface{}, StatusError) {
+
+	var txBody generated.TransactionsBody
+	err := jsonDecode(r.Body, &txBody)
+	if err != nil {
+		return nil, NewBadRequest("invalid transaction request", err)
+	}
+
+	tx, err := toTransaction(&txBody)
+	if err != nil {
+		return nil, NewBadRequest("invalid transaction request", err)
+	}
+
+	err = backend.SendTransaction(r.Context(), &tx)
+	if err != nil {
+		return nil, NewBadRequest("failed to send transaction", err)
+	}
+
+	return tx, nil
+}
