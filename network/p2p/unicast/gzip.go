28d97024c982be7ae8b2235f1c6f71a35493817e
Yahya <yahya@dapperlabs.com>
2021-11-16 11:02:58 -0800

    adds factory for gzip protocol

--- network/p2p/unicast/gzip.go
+++ network/p2p/unicast/gzip.go
@@ -4,14 +4,25 @@ import (
 	libp2pnet "github.com/libp2p/go-libp2p-core/network"
 	"github.com/libp2p/go-libp2p-core/protocol"
 
+	"github.com/onflow/flow-go/model/flow"
 	"github.com/onflow/flow-go/network/compressor"
+	"github.com/onflow/flow-go/network/p2p"
 	"github.com/onflow/flow-go/network/p2p/compressed"
 )
 
+const GzipCompressionUnicast = ProtocolName("gzip-compression")
+
 // GzipStream is a stream compression creates and returns a gzip-compressed stream out of input stream.
 type GzipStream struct {
-	protocolId protocol.ID
-	handler    libp2pnet.StreamHandler
+	protocolId     protocol.ID
+	defaultHandler libp2pnet.StreamHandler
+}
+
+func NewGzipCompressedUnicast(rootBlockID flow.Identifier, defaultHandler libp2pnet.StreamHandler) *GzipStream {
+	return &GzipStream{
+		protocolId:     p2p.GzipCompressedProtocolId(rootBlockID),
+		defaultHandler: defaultHandler,
+	}
 }
 
 func (g GzipStream) NewStream(s libp2pnet.Stream) (libp2pnet.Stream, error) {
