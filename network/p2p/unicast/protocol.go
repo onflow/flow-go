7a86c58d6fe8887c2e76c4cb73d835d0b45c5231
Yahya <yahya@dapperlabs.com>
2021-11-16 11:03:12 -0800

    adds protocol name translator

--- network/p2p/unicast/protocol.go
+++ network/p2p/unicast/protocol.go
@@ -1,11 +1,27 @@
 package unicast
 
 import (
+	"fmt"
+
 	libp2pnet "github.com/libp2p/go-libp2p-core/network"
 	"github.com/libp2p/go-libp2p-core/protocol"
+
+	"github.com/onflow/flow-go/model/flow"
 )
 
 type ProtocolName string
+type ProtocolFactory func(flow.Identifier, libp2pnet.StreamHandler) Protocol
+
+func ToProtocolFactory(name ProtocolName) (ProtocolFactory, error) {
+	switch name {
+	case GzipCompressionUnicast:
+		return func(rootBlockID flow.Identifier, handler libp2pnet.StreamHandler) Protocol {
+			return NewGzipCompressedUnicast(rootBlockID, handler)
+		}, nil
+	default:
+		return nil, fmt.Errorf("unknown unicast protocol name: %s", name)
+	}
+}
 
 type Protocol interface {
 	NewStream(s libp2pnet.Stream) (libp2pnet.Stream, error)
