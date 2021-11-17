b2fcc294f6c3757b4330c31d2ba9d237f19926f6
Yahya <yahya@dapperlabs.com>
2021-11-16 16:12:02 -0800

    refactors protocol factory function

--- network/p2p/unicast/protocol.go
+++ network/p2p/unicast/protocol.go
@@ -5,18 +5,19 @@ import (
 
 	libp2pnet "github.com/libp2p/go-libp2p-core/network"
 	"github.com/libp2p/go-libp2p-core/protocol"
+	"github.com/rs/zerolog"
 
 	"github.com/onflow/flow-go/model/flow"
 )
 
 type ProtocolName string
-type ProtocolFactory func(flow.Identifier, libp2pnet.StreamHandler) Protocol
+type ProtocolFactory func(zerolog.Logger, flow.Identifier, libp2pnet.StreamHandler) Protocol
 
 func ToProtocolFactory(name ProtocolName) (ProtocolFactory, error) {
 	switch name {
 	case GzipCompressionUnicast:
-		return func(rootBlockID flow.Identifier, handler libp2pnet.StreamHandler) Protocol {
-			return NewGzipCompressedUnicast(rootBlockID, handler)
+		return func(logger zerolog.Logger, rootBlockID flow.Identifier, handler libp2pnet.StreamHandler) Protocol {
+			return NewGzipCompressedUnicast(logger, rootBlockID, handler)
 		}, nil
 	default:
 		return nil, fmt.Errorf("unknown unicast protocol name: %s", name)
