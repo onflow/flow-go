f12800c4f7856b26010c3f5cd4bc23c2b1501d60
Simon Zhu <simon.zsiyan@gmail.com>
2021-11-19 01:55:07 -0500

    add blob io tests

---
+++ module/state_synchronization/blob_io_test.go
@@ -0,0 +1,25 @@
+package state_synchronization
+
+import (
+	"io"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+)
+
+func TestEmptyRead(t *testing.T) {
+	bcr, bw := OutgoingBlobChannel()
+	require.NoError(t, bw.Close())
+	var buf [1]byte
+	n, err := bcr.Read(buf[:])
+	assert.Equal(t, 0, n)
+	assert.ErrorIs(t, err, io.EOF)
+}
+
+func TestEmptyWrite(t *testing.T) {
+	bcw, br := IncomingBlobChannel(defaultMaxBlobSize)
+	require.NoError(t, bcw.Close())
+	_, err := br.Receive()
+	assert.ErrorIs(t, err, ErrClosedBlobChannel)
+}
