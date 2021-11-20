baf37747e2e69be1f54f3d848861f57570f5c637
Alexey Ivanov <rbtz@dropbox.com>
2021-11-20 15:58:25 -0800

    JSON: do not use pointer receiver for MarshalJSON
    Using a pointer receiver for `MarshalJSON` is error-prone since custom marshal logic would only be used if addressable variable is passed, otherwise the default implementation is used.
    
    See: https://github.com/golang/go/issues/22967

--- model/flow/payload_test.go
+++ model/flow/payload_test.go
@@ -12,16 +12,41 @@ import (
 	"github.com/onflow/flow-go/utils/unittest"
 )
 
+func TestPayloadEncodeEmptyJSON(t *testing.T) {
+	// nil slices
+	payload := unittest.PayloadFixture()
+	payloadHash1 := payload.Hash()
+	encoded1, err := json.Marshal(payload)
+	require.NoError(t, err)
+	var decoded flow.Payload
+	err = json.Unmarshal(encoded1, &decoded)
+	require.NoError(t, err)
+	assert.Equal(t, payloadHash1, decoded.Hash())
+	assert.Equal(t, payload, decoded)
+
+	// empty slices converted to nil
+	payload.Seals = []*flow.Seal{}
+	payloadHash2 := payload.Hash()
+	assert.Equal(t, payloadHash2, payloadHash1)
+	encoded2, err := json.Marshal(payload)
+	require.NoError(t, err)
+	err = json.Unmarshal(encoded2, &decoded)
+	require.NoError(t, err)
+	require.Nil(t, decoded.Seals)
+	assert.Equal(t, payloadHash2, decoded.Hash())
+	assert.NotEqual(t, payload, encoded2)
+}
+
 func TestPayloadEncodeJSON(t *testing.T) {
 	payload := unittest.PayloadFixture()
+	payload.Seals = []*flow.Seal{{}}
 	payloadHash := payload.Hash()
 	data, err := json.Marshal(payload)
 	require.NoError(t, err)
 	var decoded flow.Payload
 	err = json.Unmarshal(data, &decoded)
 	require.NoError(t, err)
-	decodedHash := decoded.Hash()
-	assert.Equal(t, payloadHash, decodedHash)
+	assert.Equal(t, payloadHash, decoded.Hash())
 	assert.Equal(t, payload, decoded)
 }
 
