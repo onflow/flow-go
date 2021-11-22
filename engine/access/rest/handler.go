2a57249304f46d64366cd50687cbce8eb756f79e
Gregor Gololicic <gregor.gololicic@dapperlabs.com>
2021-11-22 16:26:55 +0100

    add body parsing to request object

--- engine/access/rest/handler.go
+++ engine/access/rest/handler.go
@@ -1,16 +1,10 @@
 package rest
 
 import (
-	"context"
 	"encoding/json"
-	"errors"
-	"fmt"
 	"github.com/gorilla/mux"
-	"io"
-	"net/http"
-	"strings"
-
 	"github.com/rs/zerolog"
+	"net/http"
 
 	"github.com/onflow/flow-go/access"
 	"github.com/onflow/flow-go/engine/access/rest/generated"
@@ -124,81 +118,6 @@ func (h *Handler) errorResponse(
 	if err != nil {
 		logger.Error().Err(err).Msg("failed to send error response")
 	}
-}
-
-type Request struct {
-	r        *http.Request
-	context  context.Context
-	params   map[string]string
-	expand   []string
-	selected []string
-	body     io.ReadCloser
-	route    *mux.Route
-}
-
-func (r *Request) getParam(name string) string {
-	return r.params[name] // todo(sideninja) handle missing
-}
-
-func (r *Request) expands(name string) bool {
-	for _, v := range r.expand {
-		if v == name {
-			return true
-		}
-	}
-	return false
-}
-
-// jsonDecode provides safe JSON decoding with sufficient erro handling.
-func jsonDecode(body io.ReadCloser, dst interface{}) error {
-	// validate size
-
-	dec := json.NewDecoder(body)
-	dec.DisallowUnknownFields()
-
-	err := dec.Decode(&dst)
-	if err != nil {
-		var syntaxError *json.SyntaxError
-		var unmarshalTypeError *json.UnmarshalTypeError
-
-		switch {
-		case errors.As(err, &syntaxError):
-			msg := fmt.Sprintf("Request body contains badly-formed JSON (at position %d)", syntaxError.Offset)
-			return NewBadRequestError(msg, err)
-
-		case errors.Is(err, io.ErrUnexpectedEOF):
-			msg := "Request body contains badly-formed JSON"
-			return NewBadRequestError(msg, err)
-
-		case errors.As(err, &unmarshalTypeError):
-			msg := fmt.Sprintf("Request body contains an invalid value for the %q field (at position %d)", unmarshalTypeError.Field, unmarshalTypeError.Offset)
-			return NewBadRequestError(msg, err)
-
-		case strings.HasPrefix(err.Error(), "json: unknown field "):
-			fieldName := strings.TrimPrefix(err.Error(), "json: unknown field ")
-			msg := fmt.Sprintf("Request body contains unknown field %s", fieldName)
-			return NewBadRequestError(msg, err)
-
-		case errors.Is(err, io.EOF):
-			msg := "Request body must not be empty"
-			return NewBadRequestError(msg, err)
-
-		case err.Error() == "http: request body too large":
-			msg := "Request body must not be larger than 1MB"
-			return NewRestError(http.StatusRequestEntityTooLarge, msg, err)
-
-		default:
-			return err
-		}
-	}
-
-	err = dec.Decode(&struct{}{})
-	if err != io.EOF {
-		msg := "Request body must only contain a single JSON object"
-		return NewBadRequestError(msg, err)
-	}
-
-	return nil
 }
 
 // NotImplemented handler returns an error explaining the endpoint is not yet implemented
--- engine/access/rest/request_decorator.go
+++ engine/access/rest/request_decorator.go
@@ -1,8 +1,12 @@
 package rest
 
 import (
+	"encoding/json"
+	"errors"
 	"fmt"
+	"io"
 	"net/http"
+	"strings"
 
 	"github.com/gorilla/mux"
 
@@ -37,6 +41,57 @@ func (rd *requestDecorator) selects(field string) bool {
 func (rd *requestDecorator) getParam(name string) string {
 	vars := mux.Vars(rd.Request)
 	return vars[name] // todo(sideninja) check if exists
+}
+
+func (rd *requestDecorator) bodyAs(dst interface{}) error {
+	//todo(sideninja) validate size
+
+	dec := json.NewDecoder(rd.Body)
+	dec.DisallowUnknownFields()
+
+	err := dec.Decode(&dst)
+	if err != nil {
+		var syntaxError *json.SyntaxError
+		var unmarshalTypeError *json.UnmarshalTypeError
+
+		switch {
+		case errors.As(err, &syntaxError):
+			msg := fmt.Sprintf("Request body contains badly-formed JSON (at position %d)", syntaxError.Offset)
+			return NewBadRequestError(msg, err)
+
+		case errors.Is(err, io.ErrUnexpectedEOF):
+			msg := "Request body contains badly-formed JSON"
+			return NewBadRequestError(msg, err)
+
+		case errors.As(err, &unmarshalTypeError):
+			msg := fmt.Sprintf("Request body contains an invalid value for the %q field (at position %d)", unmarshalTypeError.Field, unmarshalTypeError.Offset)
+			return NewBadRequestError(msg, err)
+
+		case strings.HasPrefix(err.Error(), "json: unknown field "):
+			fieldName := strings.TrimPrefix(err.Error(), "json: unknown field ")
+			msg := fmt.Sprintf("Request body contains unknown field %s", fieldName)
+			return NewBadRequestError(msg, err)
+
+		case errors.Is(err, io.EOF):
+			msg := "Request body must not be empty"
+			return NewBadRequestError(msg, err)
+
+		case err.Error() == "http: request body too large":
+			msg := "Request body must not be larger than 1MB"
+			return NewRestError(http.StatusRequestEntityTooLarge, msg, err)
+
+		default:
+			return err
+		}
+	}
+
+	err = dec.Decode(&struct{}{})
+	if err != io.EOF {
+		msg := "Request body must only contain a single JSON object"
+		return NewBadRequestError(msg, err)
+	}
+
+	return nil
 }
 
 func (rd *requestDecorator) ids() ([]flow.Identifier, error) {
