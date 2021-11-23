a884a24d7a9196f5e9cf7f66633b74661126ca33
vishal <1117327+vishalchangrani@users.noreply.github.com>
2021-11-23 10:12:02 -0800

    unskipping the TestGhostNodeExample tests

--- integration/tests/common/ghost_node_example_test.go
+++ integration/tests/common/ghost_node_example_test.go
@@ -22,7 +22,6 @@ import (
 // TestGhostNodeExample_Subscribe demonstrates how to emulate a node and receive all inbound events for it
 func TestGhostNodeExample_Subscribe(t *testing.T) {
 
-	t.Skip()
 	var (
 		// one collection node
 		collNode = testnet.NewNodeConfig(flow.RoleCollection, testnet.WithLogLevel(zerolog.FatalLevel), testnet.WithIDInt(1))
@@ -93,7 +92,6 @@ func TestGhostNodeExample_Subscribe(t *testing.T) {
 // TestGhostNodeExample_Send demonstrates how to emulate a node and send an event from it
 func TestGhostNodeExample_Send(t *testing.T) {
 
-	t.Skip()
 	var (
 		// one real collection node
 		realCollNode = testnet.NewNodeConfig(flow.RoleCollection, testnet.WithLogLevel(zerolog.DebugLevel), testnet.WithIDInt(1))
