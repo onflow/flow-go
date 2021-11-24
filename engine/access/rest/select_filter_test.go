cbb7021ecaa5a8335642b3d0d9222c750a86c161
vishal <1117327+vishalchangrani@users.noreply.github.com>
2021-11-23 22:04:20 -0800

    adding a select filter to filter out elements from the json response

---
+++ engine/access/rest/select_filter_test.go
@@ -0,0 +1,144 @@
+package rest
+
+import (
+	"encoding/json"
+	"fmt"
+	"time"
+
+	"github.com/onflow/flow-go/engine/access/rest/generated"
+)
+
+func ExampleSelectFilter() {
+
+	block, err := generateBlock()
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	selectKeys := []string{
+		"header.id",
+		"payload.collection_guarantees.signature",
+		"payload.block_seal.aggregated_approval_signatures.signer_ids",
+		"payload.collection_guarantees.signer_ids",
+		"execution_result.events.event_index",
+	}
+
+	filteredBlock, err := SelectFilter(block, selectKeys)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	marshalled, err := json.MarshalIndent(filteredBlock, "", "\t")
+	if err != nil {
+		panic(err.Error())
+	}
+	fmt.Println(string(marshalled))
+	// Output:
+	//{
+	//	"execution_result": {
+	//		"events": [
+	//			{
+	//				"event_index": 2
+	//			},
+	//			{
+	//				"event_index": 3
+	//			}
+	//		]
+	//	},
+	//	"header": {
+	//		"id": "abcd"
+	//	},
+	//	"payload": {
+	//		"block_seals": [
+	//			{
+	//				"aggregated_approval_signatures": [
+	//					{
+	//						"signer_ids": [
+	//							"abcdef0123456789",
+	//							"abcdef0123456789"
+	//						]
+	//					}
+	//				]
+	//			}
+	//		],
+	//		"collection_guarantees": [
+	//			{
+	//				"signature": "abcdef0123456789",
+	//				"signer_ids": [
+	//					"abcdef0123456789",
+	//					"abcdef0123456789"
+	//				]
+	//			}
+	//		]
+	//	}
+	//}
+}
+
+func generateBlock() (generated.Block, error) {
+
+	dummySignature := "abcdef0123456789"
+	multipleDummySignatures := []string{dummySignature, dummySignature}
+	dummyID := "abcd"
+	dateString := "2021-11-20T11:45:26.371Z"
+	time, err := time.Parse(time.RFC3339, dateString)
+	if err != nil {
+		return generated.Block{}, err
+	}
+
+	return generated.Block{
+		Header: &generated.BlockHeader{
+			Id:                   dummyID,
+			ParentId:             dummyID,
+			Height:               100,
+			Timestamp:            time,
+			ParentVoterSignature: dummySignature,
+		},
+		Payload: &generated.BlockPayload{
+			CollectionGuarantees: []generated.CollectionGuarantee{
+				{
+					CollectionId: "abcdef0123456789",
+					SignerIds:    multipleDummySignatures,
+					Signature:    dummySignature,
+				},
+			},
+			BlockSeals: []generated.BlockSeal{
+				{
+					BlockId:    dummyID,
+					ResultId:   dummyID,
+					FinalState: "final",
+					AggregatedApprovalSignatures: []generated.AggregatedSignature{
+						{
+							VerifierSignatures: multipleDummySignatures,
+							SignerIds:          multipleDummySignatures,
+						},
+					},
+				},
+			},
+		},
+		ExecutionResult: &generated.ExecutionResult{
+			Id:      dummyID,
+			BlockId: dummyID,
+			Events: []generated.Event{
+				{
+					Type_:            "type",
+					TransactionId:    dummyID,
+					TransactionIndex: 1,
+					EventIndex:       2,
+					Payload:          "payload",
+				},
+				{
+					Type_:            "type",
+					TransactionId:    dummyID,
+					TransactionIndex: 1,
+					EventIndex:       3,
+					Payload:          "payload",
+				},
+			},
+			Links: &generated.Links{
+				Self: "link",
+			},
+		},
+	}, nil
+}
