0e49480772676dda8d1aebca62129aa9cd7ae571
Yurii Oleksyshyn <yuraolex@gmail.com>
2021-11-24 11:07:34 +0200

    Updated mocks. Updated event loop interface

--- consensus/hotstuff/event_loop.go
+++ consensus/hotstuff/event_loop.go
@@ -1,11 +1,13 @@
 package hotstuff
 
-import "github.com/onflow/flow-go/model/flow"
+import (
+	"github.com/onflow/flow-go/model/flow"
+	"github.com/onflow/flow-go/module"
+)
 
 // EventLoop performs buffer and processing of incoming proposals and QCs.
 type EventLoop interface {
-	// SubmitProposal accepts proposal for processing. Proposal will be dispatched on worker thread.
-	SubmitProposal(proposalHeader *flow.Header, parentView uint64)
+	module.HotStuff
 
 	// SubmitTrustedQC accepts QC for processing. QC will be dispatched on worker thread.
 	// CAUTION: QC is trusted (_not_ validated again), as it's built by ourselves.
--- consensus/hotstuff/mocks/event_loop.go
+++ consensus/hotstuff/mocks/event_loop.go
@@ -5,6 +5,8 @@ package mocks
 import (
 	flow "github.com/onflow/flow-go/model/flow"
 
+	irrecoverable "github.com/onflow/flow-go/module/irrecoverable"
+
 	mock "github.com/stretchr/testify/mock"
 )
 
@@ -13,9 +15,46 @@ type EventLoop struct {
 	mock.Mock
 }
 
-// SubmitProposal provides a mock function with given fields: proposalHeader, parentView
-func (_m *EventLoop) SubmitProposal(proposalHeader *flow.Header, parentView uint64) {
-	_m.Called(proposalHeader, parentView)
+// Done provides a mock function with given fields:
+func (_m *EventLoop) Done() <-chan struct{} {
+	ret := _m.Called()
+
+	var r0 <-chan struct{}
+	if rf, ok := ret.Get(0).(func() <-chan struct{}); ok {
+		r0 = rf()
+	} else {
+		if ret.Get(0) != nil {
+			r0 = ret.Get(0).(<-chan struct{})
+		}
+	}
+
+	return r0
+}
+
+// Ready provides a mock function with given fields:
+func (_m *EventLoop) Ready() <-chan struct{} {
+	ret := _m.Called()
+
+	var r0 <-chan struct{}
+	if rf, ok := ret.Get(0).(func() <-chan struct{}); ok {
+		r0 = rf()
+	} else {
+		if ret.Get(0) != nil {
+			r0 = ret.Get(0).(<-chan struct{})
+		}
+	}
+
+	return r0
+}
+
+// Start provides a mock function with given fields: _a0
+func (_m *EventLoop) Start(_a0 irrecoverable.SignalerContext) {
+	_m.Called(_a0)
+}
+
+// SubmitProposal provides a mock function with given fields: proposal, parentView
+func (_m *EventLoop) SubmitProposal(proposal *flow.Header, parentView uint64) {
+	_m.Called(proposal, parentView)
 }
 
 // SubmitTrustedQC provides a mock function with given fields: qc
