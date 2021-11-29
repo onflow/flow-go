ea18c46067cf168edc6c559519ddd1f596e476b7
Supun Setunga <supun.setunga@gmail.com>
2021-11-29 12:40:19 -0800

    Use internal migrate() method in tests

--- cmd/util/ledger/migrations/storage_v6_test.go
+++ cmd/util/ledger/migrations/storage_v6_test.go
@@ -1167,7 +1167,7 @@ func TestDeferredValues(t *testing.T) {
 		nil,
 	)
 
-	_, err = migration.Migrate(payloads)
+	_, err = migration.migrate(payloads)
 	require.NoError(t, err)
 
 	var result newInter.Value = migration.newInter.ReadStored(
