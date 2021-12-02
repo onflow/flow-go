22e988cd476d7b685b0858060557806009fe7ced
Khalil Claybon <khalil.claybon@dapperlabs.com>
2021-12-02 13:58:36 -0500

    Update epoch_test.go

--- integration/tests/epochs/epoch_test.go
+++ integration/tests/epochs/epoch_test.go
@@ -10,6 +10,7 @@ import (
 	"github.com/onflow/flow-go/model/encodable"
 	"github.com/onflow/flow-go/model/flow"
 	"github.com/onflow/flow-go/state/protocol"
+	"github.com/onflow/flow-go/utils/unittest"
 	"github.com/rs/zerolog"
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
