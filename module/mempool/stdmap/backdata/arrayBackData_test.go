dac9376ee27be8a45237d247acde91fed7045b8f
Yahya <yahya@dapperlabs.com>
2021-12-01 17:07:36 -0800

    adds key index sanity check

--- module/mempool/stdmap/backdata/arrayBackData_test.go
+++ module/mempool/stdmap/backdata/arrayBackData_test.go
@@ -1,6 +1,7 @@
 package backdata
 
 import (
+	"fmt"
 	"testing"
 
 	"github.com/stretchr/testify/require"
@@ -23,10 +24,12 @@ func TestArrayBackData_Add(t *testing.T) {
 		require.Equal(t, bd.Size(), uint(i+1))
 
 		// entity should be placed at index i in back data
-		require.Equal(t, bd.entities[i].entity, e)
+		require.Equal(t, e, bd.entities[i].entity, fmt.Sprintf("mismatching stored entity: %d", i))
 
 		// sanity checks
-		require.Equal(t, bd.buckets[0][0].keyIndex, uint64(1))
+		// since we are below limit, elements should be added sequentially at bucket 0.
+		// first added element has a key index of 1, since 0 means unused key index in implementation.
+		require.Equal(t, bd.buckets[0][i].keyIndex, uint64(i+1))
 	}
 
 }
