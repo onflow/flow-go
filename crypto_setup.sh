ffde1ef8345a1194b85b4f255b7066e5f66b6244
Tarak Ben Youssef <tarak.benyoussef@dapperlabs.com>
2021-12-08 09:28:18 -0800

    automate the version extraction

--- crypto_setup.sh
+++ crypto_setup.sh
@@ -1,21 +1,33 @@
 
 #!/bin/bash
 
-# go get the package
-go get github.com/onflow/flow-go/crypto
+# crypto package 
+PKG_NAME="github.com/onflow/flow-go/crypto"
 
-# the version of flow-go/crypto used for now is v0.23.3
-# till the script is automatized, the version is hardcoded.
-VERSION="v0.23.3"
-PKG_DIR="${GOPATH}/pkg/mod/github.com/onflow/flow-go/crypto@${VERSION}"
+# go get the package
+go get ${PKG_NAME}
+
+# go.mod
+MOD_FILE="./go.mod"
+
+# the version of onflow/flow-go/crypto used in the project is read from the go.mod file
+if [ -f "${MOD_FILE}" ]
+then
+    # extract the version from the go.mod file
+    VERSION="$(grep ${PKG_NAME} < ${MOD_FILE} | cut -d' ' -f 2)"
+    # using the right version, get the package directory path
+    PKG_DIR="$(go env GOPATH)/pkg/mod/${PKG_NAME}@${VERSION}"
+else 
+   { echo "couldn't find go.mod file - make sure the script is in the project root directory"; exit 1; }
+fi
 
 # grant permissions if not existant
 if [[ ! -r ${PKG_DIR}  || ! -w ${PKG_DIR} || ! -x ${PKG_DIR} ]]; then
    sudo chmod -R 755 "${PKG_DIR}"
 fi
 
+# get into the package directory and set up the external dependencies
 (
     cd "${PKG_DIR}" || { echo "cd into the GOPATH package folder failed"; exit 1; }
-
     go generate
 )
