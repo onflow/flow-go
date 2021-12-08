df82c1fc161050e0e78302b46d108458b35b1c9f
Leo Zhang <zhangchiqing@gmail.com>
2021-12-07 20:09:29 -0800

    Update consensus/hotstuff/signer.go
    Co-authored-by: Alexander Hentschel <alex.hentschel@axiomzen.co>

--- consensus/hotstuff/signer.go
+++ consensus/hotstuff/signer.go
@@ -6,7 +6,8 @@ import (
 
 // Signer is responsible for creating votes, proposals for a given block.
 type Signer interface {
-	// CreateProposal creates a proposal for the given block.
+	// CreateProposal creates a proposal for the given block. No error returns
+	// are expected during normal operations (incl. presence of byz. actors).
 	CreateProposal(block *model.Block) (*model.Proposal, error)
 
 	// CreateVote creates a vote for the given block. No error returns are
