ad2409688723cf946410f4407866f19064d3e010
ramtinms <ramtin@axiomzen.co>
2021-12-09 14:02:09 -0800

    increase system chunk interaction limit

--- engine/execution/computation/computer/computer.go
+++ engine/execution/computation/computer/computer.go
@@ -26,9 +26,9 @@ import (
 	"github.com/onflow/flow-go/utils/logging"
 )
 
-const SystemChunkEventCollectionMaxSize = 256_000_000 // ~256MB
-const SystemChunkLedgerIntractionLimit = 500_000_000  // ~500MB
-const MaxTransactionErrorStringSize = 1000            // 1000 chars
+const SystemChunkEventCollectionMaxSize = 256_000_000  // ~256MB
+const SystemChunkLedgerIntractionLimit = 1_000_000_000 // ~1GB
+const MaxTransactionErrorStringSize = 1000             // 1000 chars
 
 // VirtualMachine runs procedures
 type VirtualMachine interface {
