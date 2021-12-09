f6503315d2be4cbef0fff1a2943ded056f515396
Yahya <yahya@dapperlabs.com>
2021-12-09 09:54:06 -0800

    adds test add entity

--- module/mempool/stdmap/backdata/arrayBackData_test.go
+++ module/mempool/stdmap/backdata/arrayBackData_test.go
@@ -49,3 +49,39 @@ func TestArrayBackData_BelowLimit(t *testing.T) {
 		require.Equal(t, expected, actual)
 	}
 }
+
+func TestArrayBackData_WriteHeavy(t *testing.T) {
+	limit := 100_000
+
+	bd := NewArrayBackData(uint32(limit), 8, arraylinkedlist.LRUEjection)
+
+	entities := unittest.EntityListFixture(uint(limit))
+
+	// adds all entities to backdata
+	testAddEntities(t, bd, entities)
+
+	// getting inserted elements
+	for _, expected := range entities {
+		actual, ok := bd.ByID(expected.ID())
+		require.True(t, ok)
+		require.Equal(t, expected, actual)
+	}
+}
+
+// testAddEntities is a test helper that checks entities are added successfully to the backdata.
+// and each entity is retrievable right after it is written to backdata.
+func testAddEntities(t *testing.T, bd *ArrayBackData, entities []*unittest.MockEntity) {
+	// adding elements
+	for i, e := range entities {
+		// adding each element must be successful.
+		require.True(t, bd.Add(e.ID(), e))
+
+		// total of back data should be incremented by each addition.
+		require.Equal(t, bd.Size(), uint(i+1))
+
+		// entity should be placed at index i in back data
+		id, entity, _ := bd.entities.Get(uint32(i))
+		require.Equal(t, e.ID(), id)
+		require.Equal(t, e, entity)
+	}
+}
