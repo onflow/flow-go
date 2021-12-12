4a9ea7c0f317e78ac3dcfa410a0b95f772b26681
Tarak Ben Youssef <tarak.benyoussef@dapperlabs.com>
2021-12-11 19:07:11 -0800

    switch to a bytes counter

--- crypto/random/chacha20.go
+++ crypto/random/chacha20.go
@@ -33,8 +33,13 @@ import (
 // TODO: update
 type chacha20s struct {
 	state chacha20.Cipher
+
 	// Only used for State/Restore functionality
-	counter      uint64 // TODO: update to 32
+
+	// counter of bytes encrypted by the sream cipher
+	// Note this is different than the internal counter of the chacha state
+	// that counts the encrypted blocks of 512 bits.
+	bytesCounter uint64
 	initialBytes []byte
 }
 
@@ -78,6 +83,7 @@ func NewChacha20(seed []byte, streamID []byte) (*chacha20s, error) {
 	// init the chacha20s
 	rand := &chacha20s{
 		state:        *chacha,
+		bytesCounter: 0,
 		initialBytes: append(seed, streamID...),
 	}
 	return rand, nil
@@ -87,14 +93,15 @@ func NewChacha20(seed []byte, streamID []byte) (*chacha20s, error) {
 
 // UintN returns an uint64 pseudo-random number in [0,n-1]
 // using the chacha20 state.
-func (x *chacha20s) UintN(n uint64) uint64 {
+func (c *chacha20s) UintN(n uint64) uint64 {
 	// empty message to encrypt
 	// TODO: use a single array per chacha  - precise concurrency assumptions in GoDoc
 	// TODO: optimization: encrypt the entire block (512 bits) instead of of 64 bits
+	// TODO: improve unioform distribution of UintN: for loop or higher sample
 	bytes := make([]byte, 8)
-	x.state.XORKeyStream(bytes, bytes)
+	c.state.XORKeyStream(bytes, bytes)
 	// increase the counter
-	x.counter++
+	c.bytesCounter += 8
 
 	random := binary.LittleEndian.Uint64(bytes)
 	return random % n
@@ -107,13 +114,13 @@ func (x *chacha20s) UintN(n uint64) uint64 {
 // the output space grows very fast with (!n) so that input (n) and the seed length
 // (which fixes the internal state length of xorshifts ) should be chosen carefully
 // O(n) space and O(n) time
-func (x *chacha20s) Permutation(n int) ([]int, error) {
+func (c *chacha20s) Permutation(n int) ([]int, error) {
 	if n < 0 {
 		return nil, fmt.Errorf("population size cannot be negative")
 	}
 	items := make([]int, n)
 	for i := 0; i < n; i++ {
-		j := x.UintN(uint64(i + 1))
+		j := c.UintN(uint64(i + 1))
 		items[i] = items[j]
 		items[j] = i
 	}
@@ -125,7 +132,7 @@ func (x *chacha20s) Permutation(n int) ([]int, error) {
 // SubPermutation returns the m first elements of a permutation of [0,n-1]
 // It implements Fisher-Yates Shuffle using x as a source of randoms
 // O(n) space and O(n) time
-func (x *chacha20s) SubPermutation(n int, m int) ([]int, error) {
+func (c *chacha20s) SubPermutation(n int, m int) ([]int, error) {
 	if m < 0 {
 		return nil, fmt.Errorf("sample size cannot be negative")
 	}
@@ -133,7 +140,7 @@ func (x *chacha20s) SubPermutation(n int, m int) ([]int, error) {
 		return nil, fmt.Errorf("sample size (%d) cannot be larger than entire population (%d)", m, n)
 	}
 	// condition n >= 0 is enforced by function Permutation(n)
-	items, _ := x.Permutation(n)
+	items, _ := c.Permutation(n)
 	return items[:m], nil
 }
 
@@ -142,12 +149,12 @@ func (x *chacha20s) SubPermutation(n int, m int) ([]int, error) {
 // Shuffle permutes the given slice in place
 // It implements Fisher-Yates Shuffle using x as a source of randoms
 // O(1) space and O(n) time
-func (x *chacha20s) Shuffle(n int, swap func(i, j int)) error {
+func (c *chacha20s) Shuffle(n int, swap func(i, j int)) error {
 	if n < 0 {
 		return fmt.Errorf("population size cannot be negative")
 	}
 	for i := n - 1; i > 0; i-- {
-		j := x.UintN(uint64(i + 1))
+		j := c.UintN(uint64(i + 1))
 		swap(i, int(j))
 	}
 	return nil
@@ -159,7 +166,7 @@ func (x *chacha20s) Shuffle(n int, swap func(i, j int)) error {
 // in random order at indices [0,m-1]. The swapping is done in place
 // It implements the first (m) elements of Fisher-Yates Shuffle using x as a source of randoms
 // O(1) space and O(m) time
-func (x *chacha20s) Samples(n int, m int, swap func(i, j int)) error {
+func (c *chacha20s) Samples(n int, m int, swap func(i, j int)) error {
 	if m < 0 {
 		return fmt.Errorf("inputs cannot be negative")
 	}
@@ -167,7 +174,7 @@ func (x *chacha20s) Samples(n int, m int, swap func(i, j int)) error {
 		return fmt.Errorf("sample size (%d) cannot be larger than entire population (%d)", m, n)
 	}
 	for i := 0; i < m; i++ {
-		j := x.UintN(uint64(n - i))
+		j := c.UintN(uint64(n - i))
 		swap(i, i+int(j))
 	}
 	return nil
@@ -176,16 +183,16 @@ func (x *chacha20s) Samples(n int, m int, swap func(i, j int)) error {
 // State returns the internal state of the concatenated Chacha20s
 // (this is used for serde purposes)
 // TODO: update the name (serialize, encode, marshall ?)
-func (x *chacha20s) State() []byte {
-	counterBytes := make([]byte, 8)
-	binary.LittleEndian.PutUint64(counterBytes, x.counter)
-	bytes := append(x.initialBytes, counterBytes...)
+func (c *chacha20s) State() []byte {
+	counter := make([]byte, 8)
+	binary.LittleEndian.PutUint64(counter, c.bytesCounter)
+	bytes := append(c.initialBytes, counter...)
 	// output is seed || streamID || counter
 	return bytes
 }
 
 func Restore(stateBytes []byte) (*chacha20s, error) {
-	// inpout should be seed (32 bytes) || streamID (12 bytes) || counter (8 bytes)
+	// inpout should be seed (32 bytes) || streamID (12 bytes) || bytesCounter (8 bytes)
 	const expectedLen = KeySize + NonceSize + 8
 
 	// check input length
@@ -195,27 +202,27 @@ func Restore(stateBytes []byte) (*chacha20s, error) {
 
 	seed := stateBytes[:KeySize]
 	streamID := stateBytes[KeySize : KeySize+NonceSize]
-	counterBytes := stateBytes[KeySize+NonceSize:]
+	bytesCounter := binary.LittleEndian.Uint64(stateBytes[KeySize+NonceSize:])
 
 	// create the Chacha20 instance with seed and streamID
 	chacha, err := chacha20.NewUnauthenticatedCipher(seed, streamID)
 	if err != nil {
 		return nil, fmt.Errorf("Chacha20 instance creation failed: %w", err)
 	}
-	// set the counter
-	counter := binary.LittleEndian.Uint64(counterBytes)
+	// set the bytes counter
 
-	// TODO : replace 8 by constants
-	full512Count := uint32(counter / 8)
-	remaining64Count := counter % 8
-	chacha.SetCounter(full512Count)
-	// query the remaining bits and discard the result to catch the stored chacha state
-	remainderStream := make([]byte, remaining64Count*8)
+	// each chacha internal block is 512 bits
+	const bytesPerBlock = 512 / 8
+	blockCount := uint32(bytesCounter / bytesPerBlock)
+	remainingBytes := bytesCounter % bytesPerBlock
+	chacha.SetCounter(blockCount)
+	// query the remaining bytes and to catch the stored chacha state
+	remainderStream := make([]byte, remainingBytes)
 	chacha.XORKeyStream(remainderStream, remainderStream)
 
 	rand := &chacha20s{
 		state:        *chacha,
-		counter:      counter,
+		bytesCounter: bytesCounter,
 		initialBytes: stateBytes[:KeySize+NonceSize],
 	}
 	return rand, nil
