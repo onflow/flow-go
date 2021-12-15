6f76cef7f6d9dfd862f87e553dfbb411e648da44
Gregor Gololicic <gregor.gololicic@dapperlabs.com>
2021-12-15 16:41:01 +0100

    added address model

---
+++ engine/access/rest/models/address.go
@@ -0,0 +1,27 @@
+package models
+
+import (
+	"fmt"
+	"github.com/onflow/flow-go/model/flow"
+	"regexp"
+)
+
+type Address flow.Address
+
+func (a *Address) Parse(raw string) error {
+	if raw == "" { // allow empty
+		return nil
+	}
+
+	valid, _ := regexp.MatchString(`^[0-9a-fA-F]{16}$`, raw)
+	if !valid {
+		return fmt.Errorf("invalid address")
+	}
+
+	*a = Address(flow.HexToAddress(raw))
+	return nil
+}
+
+func (a Address) Flow() flow.Address {
+	return flow.Address(a)
+}
