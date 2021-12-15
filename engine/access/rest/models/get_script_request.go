23f395f379070d3c67ef3f32acc89fc3a92cf66d
Gregor Gololicic <gregor.gololicic@dapperlabs.com>
2021-12-15 19:57:39 +0100

    get script request fix

--- engine/access/rest/models/get_script_request.go
+++ engine/access/rest/models/get_script_request.go
@@ -21,15 +21,9 @@ func (g *GetScriptRequest) Build(r *rest.Request) error {
 		r.GetQueryParam(blockIDQuery),
 		r.Body,
 	)
-	if err != nil {
-		return err
-	}
 
-	if len(g.BlockID) == 0 && g.BlockHeight == 0 {
-		return rest.NewBadRequestError(fmt.Errorf("either block ID or block height must be provided"))
-	}
-	if len(g.BlockID) > 0 && g.BlockHeight != 0 {
-		return rest.NewBadRequestError(fmt.Errorf("can not provide both block ID and block height"))
+	if err != nil {
+		return rest.NewBadRequestError(err)
 	}
 
 	return nil
@@ -41,22 +35,28 @@ func (g *GetScriptRequest) Parse(rawHeight string, rawID string, rawScript io.Re
 	if err != nil {
 		return err
 	}
+	g.BlockHeight = height.Flow()
 
 	var id ID
 	err = id.Parse(rawID)
 	if err != nil {
 		return err
 	}
+	g.BlockID = id.Flow()
 
 	var script Script
 	err = script.Parse(rawScript)
 	if err != nil {
 		return err
 	}
-
-	g.BlockHeight = uint64(height)
-	g.BlockID = flow.Identifier(id)
 	g.Script = script
+
+	if len(g.BlockID) == 0 && g.BlockHeight == 0 {
+		return fmt.Errorf("either block ID or block height must be provided")
+	}
+	if len(g.BlockID) > 0 && g.BlockHeight != 0 {
+		return fmt.Errorf("can not provide both block ID and block height")
+	}
 
 	return nil
 }
