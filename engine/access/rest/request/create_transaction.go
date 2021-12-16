4bc011b35b7eb03fda3c1e8e90f811cce877769b
Gregor Gololicic <gregor.gololicic@dapperlabs.com>
2021-12-16 13:43:27 +0100

    create transaction structure

--- engine/access/rest/request/create_transaction.go
+++ engine/access/rest/request/create_transaction.go
@@ -1,14 +1,26 @@
 package request
 
-import "github.com/onflow/flow-go/engine/access/rest"
+import (
+	"github.com/onflow/flow-go/engine/access/rest"
+	"github.com/onflow/flow-go/model/flow"
+	"io"
+)
 
 type CreateTransaction struct {
+	Transaction flow.TransactionBody
 }
 
 func (c *CreateTransaction) Build(r *rest.Request) error {
 
 }
 
-func (c *CreateTransaction) Parse() error {
+func (c *CreateTransaction) Parse(rawTransaction io.Reader) error {
 
+	var tx Transaction
+	err := tx.Parse(rawTransaction)
+	if err != nil {
+		return err
+	}
+
+	c.Transaction = tx.Flow()
 }
