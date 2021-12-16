e0e7f58b364c80a33e1a4240657877c477d1f24e
Gregor Gololicic <gregor.gololicic@dapperlabs.com>
2021-12-16 12:49:21 +0100

    add tx req structure

--- engine/access/rest/request/create_transaction.go
+++ engine/access/rest/request/create_transaction.go
@@ -1,1 +1,14 @@
 package request
+
+import "github.com/onflow/flow-go/engine/access/rest"
+
+type CreateTransaction struct {
+}
+
+func (c *CreateTransaction) Build(r *rest.Request) error {
+
+}
+
+func (c *CreateTransaction) Parse() error {
+
+}
