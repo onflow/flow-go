61d012ce4673220f025be28d7372265ab2968834
Gregor Gololicic <gregor.gololicic@dapperlabs.com>
2021-12-16 16:16:33 +0100

    remove unused code from req

--- engine/access/rest/request_decorator.go
+++ engine/access/rest/request_decorator.go
@@ -1,18 +1,13 @@
 package rest
 
 import (
-	"encoding/json"
-	"errors"
-	"fmt"
 	"github.com/onflow/flow-go/engine/access/rest/request"
-	"io"
 	"net/http"
 	"strings"
 
 	"github.com/gorilla/mux"
 
 	"github.com/onflow/flow-go/engine/access/rest/middleware"
-	"github.com/onflow/flow-go/model/flow"
 )
 
 // Request a convenience wrapper around the http request to make it easy to read request query params
@@ -134,63 +129,4 @@ func (rd *Request) GetQueryParams(name string) []string {
 		return strings.Split(param, ",")
 	}
 	return strings.Fields(param)
-}
-
-func (rd *Request) bodyAs(dst interface{}) error {
-	//todo(sideninja) validate size
-
-	dec := json.NewDecoder(rd.Body)
-	dec.DisallowUnknownFields()
-
-	err := dec.Decode(&dst)
-	if err != nil {
-		var syntaxError *json.SyntaxError
-		var unmarshalTypeError *json.UnmarshalTypeError
-
-		switch {
-		case errors.As(err, &syntaxError):
-			err := fmt.Errorf("request body contains badly-formed JSON (at position %d)", syntaxError.Offset)
-			return NewBadRequestError(err)
-
-		case errors.Is(err, io.ErrUnexpectedEOF):
-			err := fmt.Errorf("request body contains badly-formed JSON")
-			return NewBadRequestError(err)
-
-		case errors.As(err, &unmarshalTypeError):
-			err := fmt.Errorf("request body contains an invalid value for the %q field (at position %d)", unmarshalTypeError.Field, unmarshalTypeError.Offset)
-			return NewBadRequestError(err)
-
-		case strings.HasPrefix(err.Error(), "json: unknown field "):
-			fieldName := strings.TrimPrefix(err.Error(), "json: unknown field ")
-			err := fmt.Errorf("Request body contains unknown field %s", fieldName)
-			return NewBadRequestError(err)
-
-		case errors.Is(err, io.EOF):
-			err := fmt.Errorf("request body must not be empty")
-			return NewBadRequestError(err)
-
-		default:
-			return err
-		}
-	}
-
-	if dst == nil {
-		return NewBadRequestError(fmt.Errorf("request body must not be empty"))
-	}
-
-	err = dec.Decode(&struct{}{})
-	if err != io.EOF {
-		err := fmt.Errorf("request body must only contain a single JSON object")
-		return NewBadRequestError(err)
-	}
-
-	return nil
-}
-
-func (rd *Request) ids() ([]flow.Identifier, error) {
-	return toIDs(rd.GetVar("id"))
-}
-
-func (rd *Request) id() (flow.Identifier, error) {
-	return toID(rd.GetVar("id"))
 }
