1404adf1adea47ac3a284d95b9849ec1890aad88
Gregor Gololicic <gregor.gololicic@dapperlabs.com>
2021-12-16 11:26:37 +0100

    extract errors

--- engine/access/rest/models/get_block_request.go
+++ engine/access/rest/models/get_block_request.go
@@ -10,6 +10,12 @@ const startHeightQuery = "start_height"
 const endHeightQuery = "end_height"
 const maxAllowedHeights = 50
 
+const mustProvideHeightOrRangeErr = "must provide either heights or start and end height range"
+const onlyProvideHeightOrRangeErr = "can only provide either heights or start and end height range"
+const startHeightBiggerThanEndErr = "start height must be less than or equal to end height"
+const heightRangeExceedMaxErr = "height range %d exceeds maximum allowed of %d"
+const cantMixValuesWithHeightErr = "can not provide '%s' or '%s' values with other height values"
+
 type GetBlockRequest struct {
 	Heights      []uint64
 	StartHeight  uint64
@@ -59,27 +65,27 @@ func (g *GetBlockRequest) Parse(rawHeights []string, rawStart string, rawEnd str
 
 	// if both height and one or both of start and end height are provided
 	if len(g.Heights) > 0 && (g.StartHeight != 0 || g.EndHeight != 0) {
-		return fmt.Errorf("can only provide either heights or start and end height range")
+		return fmt.Errorf(onlyProvideHeightOrRangeErr)
 	}
 
 	// if neither height nor start and end height are provided
 	if len(heights) == 0 && (g.StartHeight == 0 || g.EndHeight == 0) {
-		return fmt.Errorf("must provide either heights or start and end height range")
+		return fmt.Errorf(mustProvideHeightOrRangeErr)
 	}
 
 	if g.StartHeight > g.EndHeight {
-		return fmt.Errorf("start height must be less than or equal to end height")
+		return fmt.Errorf(startHeightBiggerThanEndErr)
 	}
 
 	if g.EndHeight-g.StartHeight > maxAllowedHeights {
-		return fmt.Errorf("height range %d exceeds maximum allowed of %d", g.EndHeight-g.StartHeight, maxAllowedHeights)
+		return fmt.Errorf(heightRangeExceedMaxErr, g.EndHeight-g.StartHeight, maxAllowedHeights)
 	}
 
 	// check that if sealed or final are used they are provided as only value as mix and matching heights with sealed is not encouraged
 	if len(heights) > 0 {
 		for _, h := range heights {
 			if h == SealedHeight || h == FinalHeight {
-				return fmt.Errorf("can not provide '%s' or '%s' values with other height values", final, sealed)
+				return fmt.Errorf(cantMixValuesWithHeightErr, final, sealed)
 			}
 		}
 	}
