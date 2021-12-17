e4b98937e1c7926ff69a53366b4df1d82517db65
Yurii Oleksyshyn <yuraolex@gmail.com>
2021-12-17 18:18:32 +0200

    Update consensus/integration/epoch_test.go
    Co-authored-by: Jordan Schalm <jordan@dapperlabs.com>

--- consensus/integration/epoch_test.go
+++ consensus/integration/epoch_test.go
@@ -23,6 +23,8 @@ import (
 func TestUnweightedNode(t *testing.T) {
 	// stop after building 2 blocks to ensure we can tolerate 0-weight (joining next
 	// epoch) identities, but don't cross an epoch boundary
+	// stop after building 2 blocks to ensure we can tolerate 0-weight (joining next
+	// epoch) identities, but don't cross an epoch boundary
 	stopper := NewStopper(2, 0)
 	participantsData := createConsensusIdentities(t, 3)
 	rootSnapshot := createRootSnapshot(t, participantsData)
