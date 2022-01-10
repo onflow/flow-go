77b914fb532d6c197f67c109348745be9514b500
Khalil Claybon <khalil.claybon@dapperlabs.com>
2022-01-10 12:48:17 -0500

    remove dkg & qc assertion from test run, leave util func to be used for debugging later

--- integration/tests/epochs/epoch_test.go
+++ integration/tests/epochs/epoch_test.go
@@ -183,10 +183,6 @@ func (s *Suite) runTestEpochJoinAndLeave(role flow.Role, checkNetworkHealth node
 	//make sure container to replace removed from smart contract state
 	s.assertNodeNotApprovedOrProposed(ctx, env, containerToReplace.Config.NodeID)
 
-	// check if QC and DKG have completed successfully
-	s.assertQCVotingSuccessful(ctx, env)
-	s.assertDKGSuccessful(ctx, env)
-
 	// assert transition to second epoch happened as expected
 	// if counter is still 0, epoch emergency fallback was triggered and we can fail early
 	s.assertEpochCounter(ctx, 1)
