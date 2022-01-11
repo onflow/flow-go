169120222281f7071c3c92416a43ae3f669be847
Khalil Claybon <khalil.claybon@dapperlabs.com>
2022-01-11 11:06:29 -0500

    add test resource intensive flag to LN test

--- integration/tests/epochs/epoch_test.go
+++ integration/tests/epochs/epoch_test.go
@@ -145,7 +145,7 @@ func (s *Suite) TestEpochJoinAndLeaveVN() {
 
 // TestEpochJoinAndLeaveLN should update collection nodes and assert healthy network conditions related to the node change
 func (s *Suite) TestEpochJoinAndLeaveLN() {
-	//unittest.SkipUnless(s.T(), unittest.TEST_RESOURCE_INTENSIVE, "epochs VN tests should be run on an machine with adequate resources")
+	unittest.SkipUnless(s.T(), unittest.TEST_RESOURCE_INTENSIVE, "epochs VN tests should be run on an machine with adequate resources")
 	s.runTestEpochJoinAndLeave(flow.RoleCollection, s.assertNetworkHealthyAfterLNChange)
 }
 
