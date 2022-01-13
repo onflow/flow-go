f2167f0f5b49ee15daac4656fcac58efb0637a68
Gregor Gololicic <gregor.gololicic@dapperlabs.com>
2022-01-13 15:44:46 +0100

    script test

---
+++ engine/access/rest/request/script_test.go
@@ -0,0 +1,46 @@
+package request
+
+import (
+	"fmt"
+	"github.com/onflow/flow-go/engine/access/rest/models"
+	"github.com/stretchr/testify/assert"
+	"strings"
+	"testing"
+)
+
+const validBody = "pub fun main() { }"
+
+var validBodyEncoded = models.ToBase64([]byte(validBody))
+
+func TestScript_InvalidParse(t *testing.T) {
+	test := map[string]string{
+		"":                                     "request body must not be empty",
+		"foo":                                  "request body contains badly-formed JSON (at position 2)",
+		`{ "script": "123", "arguments": [] }`: "invalid script source encoding",
+		fmt.Sprintf(`{ "script": "%s", "arguments": [123] }`, validBodyEncoded): `request body contains an invalid value for the "arguments" field (at position 57)`,
+	}
+
+	for in, errOut := range test {
+		body := strings.NewReader(in)
+		var script Script
+		err := script.Parse(body)
+		assert.EqualError(t, err, errOut, in)
+	}
+}
+
+func TestScript_ValidParse(t *testing.T) {
+	arg1 := []byte(`{"type": "String", "value": "hello" }`)
+	body := strings.NewReader(fmt.Sprintf(
+		`{ "script": "%s", "arguments": ["%s"] }`,
+		validBodyEncoded,
+		models.ToBase64(arg1),
+	))
+
+	var script Script
+	err := script.Parse(body)
+
+	assert.NoError(t, err)
+	assert.Equal(t, 1, len(script.Args))
+	assert.Equal(t, arg1, script.Args[0])
+	assert.Equal(t, validBody, string(script.Source))
+}
