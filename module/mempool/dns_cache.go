f8e3c31f0bc82d7531e4d51807e16432d73978fb
Yahya <yahya@dapperlabs.com>
2022-01-18 10:32:35 -0800

    adds dns cache interface

--- module/mempool/dns_cache.go
+++ module/mempool/dns_cache.go
@@ -1,1 +1,37 @@
 package mempool
+
+import (
+	"net"
+)
+
+// DNSCache provides an in-memory cache for storing dns entries.
+type DNSCache interface {
+	// PutIpDomain adds the given ip domain into cache.
+	PutIpDomain(string, []net.IPAddr) error
+
+	// PutTxtDomain adds the given txt domain into the cache.
+	PutTxtDomain(string, []string) error
+
+	// GetIpDomain returns the ip domain if exists in the cache.
+	// The second return value determines the timestamp of adding the
+	// domain to the cache.
+	// The boolean return value determines if domain exists in the cache.
+	GetIpDomain(string) ([]net.IPAddr, int64, bool)
+
+	// GetTxtDomain returns the txt domain if exists in the cache.
+	// The second return value determines the timestamp of adding the
+	// domain to the cache.
+	// The boolean return value determines if domain exists in the cache.
+	GetTxtDomain(string) ([]string, int64, bool)
+
+	// RemoveIp removes an ip domain from cache.
+	RemoveIp(string) bool
+
+	// RemoveTxt removes a txt domain from cache.
+	RemoveTxt(string) bool
+
+	// Size returns total domains maintained into this cache.
+	// The first returned value determines number of ip domains.
+	// The second returned value determines number of txt domains.
+	Size() (uint, uint)
+}
