811dd81a6c8e1328b97a5bbe179c7988b149605e
Gregor Gololicic <gregor.gololicic@dapperlabs.com>
2022-01-18 14:58:43 +0100

    add get script tests

---
+++ engine/access/rest/request/get_script_test.go
@@ -0,0 +1,44 @@
+package request
+
+import (
+	"fmt"
+	"github.com/onflow/flow-go/engine/access/rest/util"
+	"github.com/stretchr/testify/assert"
+	"strings"
+	"testing"
+)
+
+func TestGetScript_InvalidParse(t *testing.T) {
+	var getScript GetScript
+
+	validScript := fmt.Sprintf(`{ "script": "%s", "arguments": [] }`, util.ToBase64([]byte(`pub fun main() {}`)))
+	tests := []struct {
+		height string
+		id     string
+		script string
+		err    string
+	}{
+		{"", "", "", "request body must not be empty"},
+		{"1", "7bc42fe85d32ca513769a74f97f7e1a7bad6c9407f0d934c2aa645ef9cf613c7", validScript, "can not provide both block ID and block height"},
+		{"final", "7bc42fe85d32ca513769a74f97f7e1a7bad6c9407f0d934c2aa645ef9cf613c7", validScript, "can not provide both block ID and block height"},
+		{"", "2", validScript, "invalid ID format"},
+		{"1", "", `{ "foo": "zoo" }`, `Request body contains unknown field "foo"`},
+	}
+
+	for i, test := range tests {
+		err := getScript.Parse(test.height, test.id, strings.NewReader(test.script))
+		assert.EqualError(t, err, test.err, fmt.Sprintf("test #%d failed", i))
+	}
+}
+
+func TestGetScript_ValidParse(t *testing.T) {
+	var getScript GetScript
+
+	source := "pub fun main() {}"
+	validScript := strings.NewReader(fmt.Sprintf(`{ "script": "%s", "arguments": [] }`, util.ToBase64([]byte(source))))
+
+	err := getScript.Parse("1", "", validScript)
+	assert.NoError(t, err)
+	assert.Equal(t, getScript.BlockHeight, uint64(1))
+	assert.Equal(t, fmt.Sprintf("%s", getScript.Script.Source), source)
+}
