1d5f7c6e946ddeeb058b08430c6087eba8959ef3
Yahya <yahya@dapperlabs.com>
2022-01-21 12:54:46 -0800

    implements factories for the topologies

---
+++ network/topology/factory.go
@@ -0,0 +1,26 @@
+package topology
+
+import (
+	"github.com/rs/zerolog"
+
+	"github.com/onflow/flow-go/model/flow"
+	"github.com/onflow/flow-go/network"
+	"github.com/onflow/flow-go/state/protocol"
+)
+
+type Name string
+
+type FactoryFunction func(flow.Identifier, zerolog.Logger, protocol.State, float64) (network.Topology, error)
+
+func Factory(name Name) (FactoryFunction, error) {
+	switch name {
+	case FullyConnected:
+		return FullyConnectedTopologyFactory(), nil
+	case FixedList:
+		return FixedListTopologyFactory(), nil
+	case Randomized:
+		return RandomizedTopologyFactory(), nil
+	case TopicBased:
+		return TopicBasedTopologyFactory(), nil
+	}
+}
