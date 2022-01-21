539bbeea96b10cebf2a1323e53b0dc85875ce392
ramtinms <ramtin@axiomzen.co>
2022-01-21 12:01:27 -0800

    make format validation more readable

--- storage/merkle/errors.go
+++ storage/merkle/errors.go
@@ -25,3 +25,23 @@ func (e MalformedProofError) Error() string {
 func (e MalformedProofError) Unwrap() error {
 	return e.err
 }
+
+// InvalidProofError is returned when the proof format is right but
+// verification has failed given other data parts (e.g. it doesn't match the given root hash)
+type InvalidProofError struct {
+	err error
+}
+
+// NewInvalidProofErrorf constructs a new InvalidProofError
+func NewInvalidProofErrorf(msg string, args ...interface{}) *InvalidProofError {
+	return &InvalidProofError{err: fmt.Errorf(msg, args...)}
+}
+
+func (e InvalidProofError) Error() string {
+	return fmt.Sprintf("invalid proof, %s", e.err.Error())
+}
+
+// Unwrap unwraps the error
+func (e InvalidProofError) Unwrap() error {
+	return e.err
+}
--- storage/merkle/proof.go
+++ storage/merkle/proof.go
@@ -23,12 +23,13 @@ type Proof struct {
 	SiblingHashes [][]byte
 }
 
-// Verify verifies the proof by constructing the hash values bottom up and cross check
-// the constructed root hash with the given one.
-// if the proof is valid it returns true and false otherwise
-func (p *Proof) Verify(expectedRootHash []byte) (bool, error) {
+// formatIsValid validates the format and size of elements of the proof
+func (p *Proof) formatIsValid() (bool, error) {
 
-	// first check the key size to be smaller than max allowed
+	// validate the key size
+	if len(p.Key) == 0 {
+		return false, NewMalformedProofErrorf("key is empty")
+	}
 	if len(p.Key) > maxKeyLength {
 		return false, NewMalformedProofErrorf("key length is larger than max key lenght allowed (%d > %d)", len(p.Key), maxKeyLength)
 	}
@@ -37,33 +38,59 @@ func (p *Proof) Verify(expectedRootHash []byte) (bool, error) {
 	steps := len(p.SkipBits) + len(p.SiblingHashes)
 	// number of steps should be smaller than max key length
 	if steps > maxKeyLength {
-		return false, NewMalformedProofErrorf("length of SkipBits pluse length of SiblingHashes is larger than max key lenght allowed (%d > %d)", steps, maxKeyLength)
+		return false, NewMalformedProofErrorf("length of SkipBits plus length of SiblingHashes is larger than max key lenght allowed (%d > %d)", steps, maxKeyLength)
 	}
 	// number of steps should be smaller than number of bits in the InterimNodeTypes
 	if len(p.InterimNodeTypes) != (steps+7)>>3 {
 		return false, NewMalformedProofErrorf("the length of InterimNodeTypes doesn't match the length of SkipBits and SiblingHashes")
 	}
 
+	// For deterministic proof: require that tailing auxiliary bits (to make a complete full byte) are all zero
+	for i := len(p.InterimNodeTypes) - 1; i >= steps; i-- {
+		if bitutils.ReadBit(p.InterimNodeTypes, i) != 0 {
+			return false, NewMalformedProofErrorf("tailing auxiliary bits in InterimNodeTypes should all be zero")
+		}
+	}
+
+	// validate number of bits that is going to be checked matches the size of the given key
+	keyBitCount := len(p.SiblingHashes)
+	for _, sc := range p.SkipBits {
+		keyBitCount += int(sc)
+	}
+
+	if len(p.Key)*8 != keyBitCount {
+		return false, NewMalformedProofErrorf("key length doesn't match the length of SkipBits and SiblingHashes")
+	}
+
+	return true, nil
+}
+
+// Verify verifies the proof by constructing the hash values bottom up and cross check
+// the constructed root hash with the given one.
+// if the proof is valid it returns true and false otherwise
+func (p *Proof) Verify(expectedRootHash []byte) (bool, error) {
+
+	// first validate the format of the proof
+	if valid, err := p.formatIsValid(); !valid {
+		return valid, err
+	}
+
 	// an index to consume interim hashes from the last element to the first element
-	interimHashIndex := len(p.SiblingHashes) - 1
+	siblingHashIndex := len(p.SiblingHashes) - 1
+
 	// an index to consume shortBits from the last element to the first element
 	skipBitIndex := len(p.SkipBits) - 1
 
 	// keyIndex keeps track of the largest index of the key that is unchecked.
 	// note that traverse bottom up here, so we start with the largest key index
 	// build hashes until we reach to the root.
-	keyIndex := len(p.SiblingHashes)
-	for _, sc := range p.SkipBits {
-		keyIndex += int(sc)
-	}
-	keyIndex-- // consider index starts from zero
-
-	if len(p.Key)*8 != keyIndex+1 {
-		return false, NewMalformedProofErrorf("key length doesn't match the length of SkipBits and SiblingHashes")
-	}
+	keyIndex := len(p.Key)*8 - 1
 
 	// compute the hash value of the leaf
 	currentHash := computeLeafHash(p.Value)
+
+	// number of steps
+	steps := len(p.SkipBits) + len(p.SiblingHashes)
 
 	// for each step (level from bottom to top) check if its a full node or a short node and compute the
 	// hash value accordingly; for full node having the sibling hash helps to compute the hash value
@@ -75,11 +102,11 @@ func (p *Proof) Verify(expectedRootHash []byte) (bool, error) {
 		if bitutils.ReadBit(p.InterimNodeTypes, interimNodeTypesIndex) == 0 {
 
 			// read and pop the sibling hash value from SiblingHashes
-			if interimHashIndex < 0 {
+			if siblingHashIndex < 0 {
 				return false, NewMalformedProofErrorf("no more SiblingHashes available to read")
 			}
-			sibling := p.SiblingHashes[interimHashIndex]
-			interimHashIndex--
+			sibling := p.SiblingHashes[siblingHashIndex]
+			siblingHashIndex--
 
 			// based on the bit at keyIndex, compute the hash
 			if bitutils.ReadBit(p.Key, keyIndex) == 0 { // left branching
@@ -115,14 +142,14 @@ func (p *Proof) Verify(expectedRootHash []byte) (bool, error) {
 		currentHash = computeShortHash(skipBits, commonPath, currentHash)
 	}
 
-	// in the end we should have used all the path space available
+	// in the end we should have checked all the bits of the key
 	if keyIndex >= 0 {
 		return false, NewMalformedProofErrorf("a subset of the key is not checked (keyIndex: %d)", keyIndex)
 	}
 
 	// the final hash value should match whith what was expected
 	if !bytes.Equal(currentHash, expectedRootHash) {
-		return false, NewMalformedProofErrorf("root hash doesn't match, expected %X, computed %X", expectedRootHash, currentHash)
+		return false, NewInvalidProofErrorf("root hash doesn't match, expected %X, computed %X", expectedRootHash, currentHash)
 	}
 
 	return true, nil
