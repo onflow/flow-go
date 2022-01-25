876c22f4cda9f9424919c64c1bb38189eb544889
Yahya <yahya@dapperlabs.com>
2022-01-25 10:46:37 -0800

    encapsulates test add in a separate method

--- module/mempool/stdmap/dns_cache_test.go
+++ module/mempool/stdmap/dns_cache_test.go
@@ -1,7 +1,6 @@
 package stdmap_test
 
 import (
-	"fmt"
 	"sync"
 	"testing"
 	"time"
@@ -14,10 +13,12 @@ import (
 )
 
 func TestDNSCache(t *testing.T) {
-	total := 501
-	sizeLimit := uint32(500)
+	total := 700             // total entries to store (i.e., 700 ip and 700 txt domains)
+	sizeLimit := uint32(500) // cache size limit (i.e., 500 ip and 500 txt domains)
+
 	ipFixtures := network.IpLookupFixture(total)
 	txtFixtures := network.TxtLookupFixture(total)
+
 	cache := stdmap.NewDNSCache(sizeLimit, unittest.Logger())
 
 	// cache must be initially empty
@@ -25,10 +26,29 @@ func TestDNSCache(t *testing.T) {
 	require.Equal(t, uint(0), ips)
 	require.Equal(t, uint(0), txts)
 
-	wg := sync.WaitGroup{}
-	wg.Add(2 * total)
+	// adding 700 txt and 700 ip domains to cache
+	testAddToCache(t, cache, ipFixtures, txtFixtures)
 
-	for _, fixture := range ipFixtures {
+	// cache must be full up to its limit
+	ips, txts = cache.Size()
+	require.Equal(t, uint(sizeLimit), ips)
+	require.Equal(t, uint(sizeLimit), txts)
+
+	// only 500 txt and 500 ip domains must be retrievable
+	testRetrievalMatchCount(t, cache, ipFixtures, txtFixtures, int(sizeLimit))
+}
+
+// testAddToCache is a test helper that adds ip and txt domains concurrently to the cache and evaluates
+// each domain is retrievable right after it has been added.
+func testAddToCache(t *testing.T,
+	cache *stdmap.DNSCache,
+	ipTestCases map[string]*network.IpLookupTestCase,
+	txtTestCases map[string]*network.TxtLookupTestCase) {
+
+	wg := sync.WaitGroup{}
+	wg.Add(len(ipTestCases) + len(txtTestCases))
+
+	for _, fixture := range ipTestCases {
 		go func(fixture *network.IpLookupTestCase) {
 			require.True(t, cache.PutIpDomain(fixture.Domain, fixture.TimeStamp, fixture.Result))
 			addresses, timestamp, ok := cache.GetIpDomain(fixture.Domain)
@@ -41,7 +61,7 @@ func TestDNSCache(t *testing.T) {
 		}(fixture)
 	}
 
-	for _, fixture := range txtFixtures {
+	for _, fixture := range txtTestCases {
 		go func(fixture *network.TxtLookupTestCase) {
 			require.True(t, cache.PutTxtDomain(fixture.Domain, fixture.TimeStamp, fixture.Result))
 
@@ -55,20 +75,11 @@ func TestDNSCache(t *testing.T) {
 	}
 
 	unittest.RequireReturnsBefore(t, wg.Wait, 1*time.Second, "could not write all test cases on time")
-
-	// cache must be full up to its limit
-	ips, txts = cache.Size()
-	fmt.Println("ips", ips, "txts", txts)
-	require.Equal(t, uint(sizeLimit), ips)
-	require.Equal(t, uint(sizeLimit), txts)
-
-	// 500 txt and 500 ip domains must be retrievable
-	testMatchCount(t, cache, ipFixtures, txtFixtures, int(sizeLimit))
 }
 
 // testMatchCount is a test helper that checks specified number of txt and ip domains are retrievable from the cache.
 // The `count` parameter specifies number of expected matches from txt and ip domains, separately.
-func testMatchCount(t *testing.T,
+func testRetrievalMatchCount(t *testing.T,
 	cache *stdmap.DNSCache,
 	ipTestCases map[string]*network.IpLookupTestCase,
 	txtTestCases map[string]*network.TxtLookupTestCase,
