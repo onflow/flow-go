45014b3ff5405c30d2f39dbd3e5e6c70eff17c2b
Yahya <yahya@dapperlabs.com>
2022-01-25 10:07:05 -0800

    improves test with retrieval

--- module/mempool/stdmap/dns_cache_test.go
+++ module/mempool/stdmap/dns_cache_test.go
@@ -1,9 +1,12 @@
 package stdmap_test
 
 import (
+	"fmt"
 	"sync"
 	"testing"
 	"time"
+
+	"github.com/stretchr/testify/require"
 
 	"github.com/onflow/flow-go/module/mempool/stdmap"
 	"github.com/onflow/flow-go/utils/unittest"
@@ -16,22 +19,45 @@ func TestDNSCache(t *testing.T) {
 	txtFixtures := network.TxtLookupFixture(count)
 	cache := stdmap.NewDNSCache(500, unittest.Logger())
 
+	// cache must be initially empty
+	ips, txts := cache.Size()
+	require.Equal(t, uint(0), ips)
+	require.Equal(t, uint(0), txts)
+
 	wg := sync.WaitGroup{}
 	wg.Add(2 * count)
 
 	for _, fixture := range ipFixtures {
 		go func(fixture *network.IpLookupTestCase) {
-			cache.PutIpDomain(fixture.Domain, 1, fixture.Result)
+			require.True(t, cache.PutIpDomain(fixture.Domain, 1, fixture.Result))
+			addresses, timestamp, ok := cache.GetIpDomain(fixture.Domain)
+			require.True(t, ok)
+
+			require.Equal(t, timestamp, int64(1))
+			require.Equal(t, fixture.Result, addresses)
+
 			wg.Done()
 		}(fixture)
 	}
 
 	for _, fixture := range txtFixtures {
 		go func(fixture *network.TxtLookupTestCase) {
-			cache.PutTxtDomain(fixture.Domain, 1, fixture.Result)
+			require.True(t, cache.PutTxtDomain(fixture.Domain, 1, fixture.Result))
+
+			addresses, timestamp, ok := cache.GetTxtDomain(fixture.Domain)
+			require.True(t, ok)
+
+			require.Equal(t, timestamp, int64(1))
+			require.Equal(t, fixture.Result, addresses)
 			wg.Done()
 		}(fixture)
 	}
 
-	unittest.RequireReturnsBefore(t, wg.Done, 1*time.Second, "could not write all test cases on time")
+	unittest.RequireReturnsBefore(t, wg.Wait, 1*time.Second, "could not write all test cases on time")
+
+	// cache must be full up to its limit
+	ips, txts = cache.Size()
+	fmt.Println("ips", ips, "txts", txts)
+	require.Equal(t, uint(500), ips)
+	require.Equal(t, uint(500), txts)
 }
