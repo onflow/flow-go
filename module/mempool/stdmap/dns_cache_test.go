f4ba103a8e2a9b3b5c15def5cf902b77d4acdf9f
Yahya <yahya@dapperlabs.com>
2022-01-24 16:13:50 -0800

    adds skeleton of cache test

--- module/mempool/stdmap/dns_cache_test.go
+++ module/mempool/stdmap/dns_cache_test.go
@@ -1,12 +1,37 @@
 package stdmap_test
 
 import (
+	"sync"
 	"testing"
+	"time"
 
-	"github.com/onflow/flow-go/utils/unittest/unittestnet"
+	"github.com/onflow/flow-go/module/mempool/stdmap"
+	"github.com/onflow/flow-go/utils/unittest"
+	"github.com/onflow/flow-go/utils/unittest/network"
 )
 
 func TestDNSCache(t *testing.T) {
-	ipFixtures := network.IpLookupFixture(501)
+	count := 501
+	ipFixtures := network.IpLookupFixture(count)
+	txtFixtures := network.TxtLookupFixture(count)
+	cache := stdmap.NewDNSCache(500, unittest.Logger())
 
+	wg := sync.WaitGroup{}
+	wg.Add(2 * count)
+
+	for _, fixture := range ipFixtures {
+		go func(fixture *network.IpLookupTestCase) {
+			cache.PutIpDomain(fixture.Domain, 1, fixture.Result)
+			wg.Done()
+		}(fixture)
+	}
+
+	for _, fixture := range txtFixtures {
+		go func(fixture *network.TxtLookupTestCase) {
+			cache.PutTxtDomain(fixture.Domain, 1, fixture.Result)
+			wg.Done()
+		}(fixture)
+	}
+
+	unittest.RequireReturnsBefore(t, wg.Done, 1*time.Second, "could not write all test cases on time")
 }
