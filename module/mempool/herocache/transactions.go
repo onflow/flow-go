6f53890b273d96c66da7573d1437f76b03f35520
Yahya <yahya@dapperlabs.com>
2022-01-26 13:11:10 -0800

    implements more methods of transactions

--- module/mempool/herocache/transactions.go
+++ module/mempool/herocache/transactions.go
@@ -13,26 +13,35 @@ import (
 const DefaultOversizeFactor = uint32(8)
 
 type Transactions struct {
-	*herocache.Cache
+	c *herocache.Cache
 }
 
 // NewTransactions implements a transactions mempool based on hero cache.
 func NewTransactions(limit uint32, logger zerolog.Logger) *Transactions {
 	t := &Transactions{
-		Cache: herocache.NewCache(limit, DefaultOversizeFactor, heropool.LRUEjection, logger),
+		c: herocache.NewCache(limit,
+			DefaultOversizeFactor,
+			heropool.LRUEjection,
+			logger.With().Str("mempool", "transactions").Logger()),
 	}
 
 	return t
 }
 
+// Has checks whether the transaction with the given hash is currently in
+// the memory pool.
+func (t Transactions) Has(id flow.Identifier) bool {
+	return t.c.Has(id)
+}
+
 // Add adds a transaction to the mempool.
 func (t *Transactions) Add(tx *flow.TransactionBody) bool {
-	return t.Cache.Add(tx.ID(), *tx)
+	return t.c.Add(tx.ID(), *tx)
 }
 
 // ByID returns the transaction with the given ID from the mempool.
-func (t *Transactions) ByID(txID flow.Identifier) (*flow.TransactionBody, bool) {
-	entity, exists := t.Cache.ByID(txID)
+func (t Transactions) ByID(txID flow.Identifier) (*flow.TransactionBody, bool) {
+	entity, exists := t.c.ByID(txID)
 	if !exists {
 		return nil, false
 	}
@@ -43,9 +52,10 @@ func (t *Transactions) ByID(txID flow.Identifier) (*flow.TransactionBody, bool) 
 	return &tx, true
 }
 
-// All returns all transactions from the mempool.
-func (t *Transactions) All() []*flow.TransactionBody {
-	entities := t.Cache.All()
+// All returns all transactions from the mempool. Since it is using the HeroCache, all guarantees returning
+// all transactions in the same order as they are added.
+func (t Transactions) All() []*flow.TransactionBody {
+	entities := t.c.Entities()
 	txs := make([]*flow.TransactionBody, 0, len(entities))
 	for _, entity := range entities {
 		tx, ok := entity.(flow.TransactionBody)
@@ -55,4 +65,26 @@ func (t *Transactions) All() []*flow.TransactionBody {
 		txs = append(txs, &tx)
 	}
 	return txs
+}
+
+// Clear removes all transactions stored in this mempool.
+func (t *Transactions) Clear() {
+	t.c.Clear()
+}
+
+// Size returns total number of stored transactions.
+func (t Transactions) Size() uint {
+	return t.c.Size()
+}
+
+// Rem removes transaction from mempool.
+func (t *Transactions) Rem(id flow.Identifier) bool {
+	_, ok := t.c.Rem(id)
+	return ok
+}
+
+// Hash will return a fingerprint hash representing the contents of the
+// entire memory pool.
+func (t Transactions) Hash() flow.Identifier {
+	return t.c.Hash()
 }
