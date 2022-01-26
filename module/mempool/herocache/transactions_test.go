161549f7b841b2ab340ac55394add8d766c6f6f4
Yahya <yahya@dapperlabs.com>
2022-01-26 14:53:00 -0800

    adds concurrency test

--- module/mempool/herocache/transactions_test.go
+++ module/mempool/herocache/transactions_test.go
@@ -1,11 +1,14 @@
 package herocache_test
 
 import (
+	"sync"
 	"testing"
+	"time"
 
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
 
+	"github.com/onflow/flow-go/model/flow"
 	"github.com/onflow/flow-go/module/mempool/herocache"
 	"github.com/onflow/flow-go/utils/unittest"
 )
@@ -53,6 +56,41 @@ func TestTransactionPool(t *testing.T) {
 		transactions.Clear()
 		assert.Equal(t, uint(0), transactions.Size())
 	})
+}
+
+// TestConcurrentWriteAndRead checks correctness of transactions mempool under concurrent read and write.
+func TestConcurrentWriteAndRead(t *testing.T) {
+	total := 100
+	txs := unittest.TransactionBodyListFixture(total)
+	transactions := herocache.NewTransactions(uint32(total), unittest.Logger())
+
+	wg := sync.WaitGroup{}
+	wg.Add(total)
+
+	// storing all transactions
+	for i := 0; i < total; i++ {
+		go func(tx flow.TransactionBody) {
+			require.True(t, transactions.Add(&tx))
+
+			wg.Done()
+		}(txs[i])
+	}
+
+	unittest.RequireReturnsBefore(t, wg.Wait, 100*time.Millisecond, "could not write all transactions on time")
+	require.Equal(t, transactions.Size(), uint(total))
+
+	wg.Add(total)
+	// reading all transactions
+	for i := 0; i < total; i++ {
+		go func(tx flow.TransactionBody) {
+			actual, ok := transactions.ByID(tx.ID())
+			require.True(t, ok)
+			require.Equal(t, tx, *actual)
+
+			wg.Done()
+		}(txs[i])
+	}
+	unittest.RequireReturnsBefore(t, wg.Wait, 100*time.Millisecond, "could not read all transactions on time")
 }
 
 // TestAllReturnsInOrder checks All method of the HeroCache-based transactions mempool returns all
