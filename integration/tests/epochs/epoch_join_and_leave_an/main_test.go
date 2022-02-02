a6d7a6c0457796537b369cf334f3e885463a1214
Leo Zhang (zhangchiqing) <zhangchiqing@gmail.com>
2022-02-02 15:55:42 -0800

    split epochs tests

---
+++ integration/tests/epochs/epoch_join_and_leave_an/main_test.go
@@ -0,0 +1,12 @@
+package epoch_join_and_leave_an
+
+import (
+	"testing"
+
+	"github.com/onflow/flow-go/integration/tests/epochs"
+	"github.com/stretchr/testify/suite"
+)
+
+func TestEpoch(t *testing.T) {
+	suite.Run(t, new(epochs.EpochJoinAndLeaveVNSuite))
+}
