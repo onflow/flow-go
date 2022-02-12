9cb2a74ae2cc062efb7faeb817fa3d1831c7c80b
Yahya Hassanzadeh <yahya@dapperlabs.com>
2022-02-11 17:02:00 -0800

    [Networking] Implements Pluggable Conduit Factory (#1986)
    * implements network adapter
    
    * moves channel to a separate file
    
    * refactors network to adapt the adapter interface
    
    * implements default conduit methods
    
    * moves p2p conduit to a separate package
    
    * renames conduit package
    
    * refactors networking layer with config facotry
    
    * refactors network factory with conduit factory
    
    * fixes lint in stub conduit
    
    * removes redundant conduit implementation
    
    * replaces stub conduit with default one
    
    * replaces conduit functions with adaptor methods
    
    * removes conduit functions
    
    * renames with network adapter method of conduit
    
    * dips context creation into the conduit factory
    
    * adds documentation for network conduit
    
    * adds documentation for the network adapter
    
    * refactors conduit factory as an option
    
    * implements adapter interface for the networking layer
    
    * fixes compile issue on scaffold
    
    * Update network/network.go
    
    Co-authored-by: Vishal <1117327+vishalchangrani@users.noreply.github.com>
    
    Co-authored-by: Vishal <1117327+vishalchangrani@users.noreply.github.com>

--- cmd/access/node_builder/access_node_builder.go
+++ cmd/access/node_builder/access_node_builder.go
@@ -11,6 +11,8 @@ import (
 	"github.com/rs/zerolog"
 	"github.com/spf13/pflag"
 
+	"github.com/onflow/flow-go/crypto"
+
 	"github.com/onflow/flow-go/cmd"
 	"github.com/onflow/flow-go/consensus"
 	"github.com/onflow/flow-go/consensus/hotstuff"
@@ -19,7 +21,6 @@ import (
 	hotsignature "github.com/onflow/flow-go/consensus/hotstuff/signature"
 	"github.com/onflow/flow-go/consensus/hotstuff/verification"
 	recovery "github.com/onflow/flow-go/consensus/recovery/protocol"
-	"github.com/onflow/flow-go/crypto"
 	"github.com/onflow/flow-go/engine/access/ingestion"
 	"github.com/onflow/flow-go/engine/access/rpc"
 	"github.com/onflow/flow-go/engine/access/rpc/backend"
