767f9033aeb7b6f3479ea145709309c789ce16ce
Leo Zhang (zhangchiqing) <zhangchiqing@gmail.com>
2022-02-13 18:27:53 -0800

    enable read-execution-state

--- cmd/util/cmd/root.go
+++ cmd/util/cmd/root.go
@@ -15,6 +15,7 @@ import (
 	extract "github.com/onflow/flow-go/cmd/util/cmd/execution-state-extract"
 	ledger_json_exporter "github.com/onflow/flow-go/cmd/util/cmd/export-json-execution-state"
 	read_badger "github.com/onflow/flow-go/cmd/util/cmd/read-badger/cmd"
+	read_execution_state "github.com/onflow/flow-go/cmd/util/cmd/read-execution-state"
 	read_protocol_state "github.com/onflow/flow-go/cmd/util/cmd/read-protocol-state/cmd"
 	rollback_executed_height "github.com/onflow/flow-go/cmd/util/cmd/rollback-executed-height/cmd"
 	truncate_database "github.com/onflow/flow-go/cmd/util/cmd/truncate-database"
@@ -60,6 +61,7 @@ func addCommands() {
 	rootCmd.AddCommand(ledger_json_exporter.Cmd)
 	rootCmd.AddCommand(epochs.RootCmd)
 	rootCmd.AddCommand(rollback_executed_height.Cmd)
+	rootCmd.AddCommand(read_execution_state.Cmd)
 }
 
 func initConfig() {
