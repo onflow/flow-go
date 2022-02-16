29ab3c7737a3e07e91a7b18527f590b3101ac595
Leo Zhang <zhangchiqing@gmail.com>
2022-02-16 10:13:56 -0800

    re execute block for mainnet (#2013)
    * re execute block for mainnet
    
    * Apply suggestions from code review

--- engine/execution/computation/manager.go
+++ engine/execution/computation/manager.go
@@ -310,6 +310,13 @@ func (e *Manager) ComputeBlock(
 			}
 			result.ExecutionDataID = resultID60153761
 		}
+	} else if block.Block.Header.ChainID == flow.Mainnet {
+		// ignore the ExecutionDataID to force result ID to be consistent
+		// this code exists for mainnet16 only, it turns out the ExecutionDataID introduced in this spork
+		// is non-deterministic. We decided to exclude this field from this height. We will fix it in the next spork.
+		if block.Block.Header.Height >= 24188133 {
+			result.ExecutionDataID = flow.ZeroID
+		}
 	}
 
 	return result, nil
