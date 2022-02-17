ddb4b26cc1316feb50540bfbd6b8f5282d9dfe44
Tarak Ben Youssef <tarak.benyoussef@dapperlabs.com>
2022-02-17 12:04:32 -0800

    use default flag value for machine key seed

--- cmd/bootstrap/cmd/machine_account_key.go
+++ cmd/bootstrap/cmd/machine_account_key.go
@@ -24,16 +24,11 @@ var machineAccountKeyCmd = &cobra.Command{
 func init() {
 	rootCmd.AddCommand(machineAccountKeyCmd)
 
-	machineAccountKeyCmd.Flags().BytesHexVar(&flagMachineSeed, "seed", []byte{}, fmt.Sprintf("hex encoded machine account seed (min %d bytes)", crypto.KeyGenSeedMinLenECDSAP256))
+	machineAccountKeyCmd.Flags().BytesHexVar(&flagMachineSeed, "seed", GenerateRandomSeed(crypto.KeyGenSeedMinLenECDSAP256), fmt.Sprintf("hex encoded machine account seed (min %d bytes)", crypto.KeyGenSeedMinLenECDSAP256))
 }
 
 // machineAccountKeyRun generate a machine account key and writes it to a default file path.
 func machineAccountKeyRun(_ *cobra.Command, _ []string) {
-
-	// generate seed if not specified via flag
-	if len(flagMachineSeed) == 0 {
-		flagMachineSeed = GenerateRandomSeed(crypto.KeyGenSeedMinLenECDSAP256)
-	}
 
 	// read nodeID written to boostrap dir by `bootstrap key`
 	nodeID, err := readNodeID()
