ae2a743ef8b6f4d92570fe7fcf1621c8ab048c9b
Yahya <yahya@dapperlabs.com>
2022-02-17 12:03:25 -0800

    implements unicast method for corruptible conduit

--- insecure/corruptible/conduit.go
+++ insecure/corruptible/conduit.go
@@ -21,7 +21,11 @@ type Conduit struct {
 	observer insecure.Observer
 }
 
-// Publish sends an event to the network layer for unreliable delivery
+// Publish implements a corruptible publishing service that is it sends all incoming messages
+// directly to the conduit factory to decide what to do next.
+// If the conduit factory gracefully rejects to observe the dispatched message,
+// Publish acts as in the non-corrupted mode and behaves correctly as expected.
+// which is sending the event to the network layer for unreliable delivery
 // to subscribers of the given event on the network layer. It uses a
 // publish-subscribe layer and can thus not guarantee that the specified
 // recipients received the event.
@@ -44,14 +48,30 @@ func (c *Conduit) Publish(event interface{}, targetIDs ...flow.Identifier) error
 	return nil
 }
 
-// Unicast sends an event in a reliable way to the given recipient.
+// Unicast implements a corruptible unicasting service that is it sends all incoming messages
+// directly to the conduit factory to decide what to do next.
+// If the conduit factory gracefully rejects to observe the dispatched message,
+// Unicast acts as in the non-corrupted mode and behaves correctly as expected, which is
+// sending an event in a reliable way to the given recipient.
 // It uses 1-1 direct messaging over the underlying network to deliver the event.
 // It returns an error if the unicast fails.
 func (c *Conduit) Unicast(event interface{}, targetID flow.Identifier) error {
 	if c.ctx.Err() != nil {
 		return fmt.Errorf("conduit for channel %s closed", c.channel)
 	}
-	return c.adapter.UnicastOnChannel(c.channel, event, targetID)
+
+	observed, err := c.observer.Observe(c.ctx, event, c.channel, proto.Protocol_UNICAST, 0, targetID)
+	if err != nil {
+		return fmt.Errorf("factory could not observe the unicast event: %w", err)
+	}
+
+	if !observed {
+		// if corruptible conduit factor gracefully decides not to observe a message, the
+		// normal flow of passing the message to the networking layer should be followed.
+		return c.adapter.UnicastOnChannel(c.channel, event, targetID)
+	}
+
+	return nil
 }
 
 // Multicast unreliably sends the specified event to the specified number of recipients selected from the specified subset.
