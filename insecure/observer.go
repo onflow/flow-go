ebfa143f231c86cf0b25566111404908b1786876
Yahya <yahya@dapperlabs.com>
2022-02-17 10:54:12 -0800

    implements observer interface

--- insecure/observer.go
+++ insecure/observer.go
@@ -1,1 +1,13 @@
 package insecure
+
+import (
+	"context"
+
+	"github.com/onflow/flow-go/insecure/proto"
+	"github.com/onflow/flow-go/model/flow"
+	"github.com/onflow/flow-go/network"
+)
+
+type observer interface {
+	observe(context.Context, interface{}, network.Channel, proto.Protocol, uint32, ...flow.Identifier)
+}
