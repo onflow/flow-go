beed6dcde39984eb7667555fd305d7e8a8915634
Yahya <yahya@dapperlabs.com>
2022-02-18 14:44:43 -0800

    adds test new conduit on happy path

--- insecure/corruptible/factory_test.go
+++ insecure/corruptible/factory_test.go
@@ -1,11 +1,13 @@
 package corruptible_test
 
 import (
+	"context"
 	"testing"
 
 	"github.com/stretchr/testify/require"
 
 	"github.com/onflow/flow-go/insecure/corruptible"
+	"github.com/onflow/flow-go/network"
 	"github.com/onflow/flow-go/network/codec/cbor"
 	"github.com/onflow/flow-go/network/mocknetwork"
 	"github.com/onflow/flow-go/utils/unittest"
@@ -21,4 +23,19 @@ func TestRegisterAdapter(t *testing.T) {
 
 	// second attempt on registering adapter must fail
 	require.Error(t, f.RegisterAdapter(adapter))
+}
+
+// TestNewConduit_HappyPath checks when factory has an adapter registered, it can successfully
+// create conduits.
+func TestNewConduit_HappyPath(t *testing.T) {
+	f := corruptible.NewCorruptibleConduitFactory(unittest.IdentifierFixture(), cbor.NewCodec())
+	channel := network.Channel("test-channel")
+
+	adapter := &mocknetwork.Adapter{}
+
+	require.NoError(t, f.RegisterAdapter(adapter))
+
+	c, err := f.NewConduit(context.Background(), channel)
+	require.NoError(t, err)
+	require.NotNil(t, c)
 }
