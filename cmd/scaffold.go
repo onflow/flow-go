1241aa463bb8e68ca50d31063b395ab41facc03b
Khalil Claybon <khalil.claybon@dapperlabs.com>
2022-02-22 12:27:37 -0500

    add warning log about insecure secrets db

--- cmd/scaffold.go
+++ cmd/scaffold.go
@@ -529,7 +529,9 @@ func (fnb *FlowNodeBuilder) initSecretsDB() {
 
 	opts := badger.DefaultOptions(fnb.BaseConfig.secretsdir).WithLogger(log)
 
-	if !fnb.InsecureSecretsDB {
+	if fnb.InsecureSecretsDB {
+		fnb.Logger.Warn().Msg("starting with secrets database encryption disabled")
+	} else {
 		// attempt to read an encryption key for the secrets DB from the canonical path
 		encryptionKey, err := loadSecretsEncryptionKey(fnb.BootstrapDir, fnb.NodeID)
 		if errors.Is(err, os.ErrNotExist) {
