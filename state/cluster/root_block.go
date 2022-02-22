96b6d96f07da57571b26860b96ec7a069257f9ec
Jeff Winkler <jeff.winkler@dapperlabs.com>
2022-02-22 19:43:32 +0000

    perf: fix for missing cache

--- state/cluster/root_block.go
+++ state/cluster/root_block.go
@@ -2,6 +2,7 @@ package cluster
 
 import (
 	"fmt"
+	"sync"
 
 	"github.com/onflow/flow-go/model/cluster"
 	"github.com/onflow/flow-go/model/flow"
@@ -13,17 +14,24 @@ func CanonicalClusterID(epoch uint64, participants flow.IdentityList) flow.Chain
 	return flow.ChainID(fmt.Sprintf("cluster-%d-%s", epoch, participants.Fingerprint()))
 }
 
+var cachedHash flow.Identifier
+var once sync.Once
+
 // CanonicalRootBlock returns the canonical root block for the given
 // cluster in the given epoch. It contains an empty collection referencing
 func CanonicalRootBlock(epoch uint64, participants flow.IdentityList) *cluster.Block {
 
 	chainID := CanonicalClusterID(epoch, participants)
 	payload := cluster.EmptyPayload(flow.ZeroID)
+
+	once.Do(func() {
+		cachedHash = payload.Hash()
+	})
 	header := &flow.Header{
 		ChainID:            chainID,
 		ParentID:           flow.ZeroID,
 		Height:             0,
-		PayloadHash:        payload.Hash(),
+		PayloadHash:        cachedHash,
 		Timestamp:          flow.GenesisTime,
 		View:               0,
 		ParentVoterIDs:     nil,
