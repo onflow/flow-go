02ed4905e32ddc2581a278ebc12ea7516ea92885
Jeff Winkler <jwinkler2083233@users.noreply.github.com>
2022-02-22 16:56:47 -0600

    Update root_block.go
    avoid initializing empty rootBlockPayload more than once.

--- state/cluster/root_block.go
+++ state/cluster/root_block.go
@@ -14,24 +14,20 @@ func CanonicalClusterID(epoch uint64, participants flow.IdentityList) flow.Chain
 	return flow.ChainID(fmt.Sprintf("cluster-%d-%s", epoch, participants.Fingerprint()))
 }
 
-var cachedHash flow.Identifier
-var once sync.Once
+// these globals are filled by the static initializer
+var rootBlockPayload = cluster.EmptyPayload(flow.ZeroID)
+var rootBlockPayloadHash = rootBlockPayload.Hash()
 
 // CanonicalRootBlock returns the canonical root block for the given
 // cluster in the given epoch. It contains an empty collection referencing
 func CanonicalRootBlock(epoch uint64, participants flow.IdentityList) *cluster.Block {
-
 	chainID := CanonicalClusterID(epoch, participants)
-	payload := cluster.EmptyPayload(flow.ZeroID)
-
-	once.Do(func() {
-		cachedHash = payload.Hash()
-	})
+	
 	header := &flow.Header{
 		ChainID:            chainID,
 		ParentID:           flow.ZeroID,
 		Height:             0,
-		PayloadHash:        cachedHash,
+		PayloadHash:        rootBlockPayloadHash,
 		Timestamp:          flow.GenesisTime,
 		View:               0,
 		ParentVoterIDs:     nil,
@@ -42,6 +38,6 @@ func CanonicalRootBlock(epoch uint64, participants flow.IdentityList) *cluster.B
 
 	return &cluster.Block{
 		Header:  header,
-		Payload: &payload,
+		Payload: &rootBlockPayload,
 	}
 }
