65bee9e3e42986a8c861ea8bc556f2019ca39a1b
Yahya <yahya@dapperlabs.com>
2022-02-25 14:33:16 -0800

    fixes tests

--- insecure/corruptible/conduit_test.go
+++ insecure/corruptible/conduit_test.go
@@ -30,7 +30,7 @@ func TestConduitRelayMessage_Publish(t *testing.T) {
 	event := unittest.MockEntityFixture()
 	targetIds := unittest.IdentifierListFixture(10)
 
-	params := []interface{}{ctx, event, channel, insecure.Protocol_PUBLISH, uint32(0)}
+	params := []interface{}{event, channel, insecure.Protocol_PUBLISH, uint32(0)}
 	for _, id := range targetIds {
 		params = append(params, id)
 	}
@@ -59,7 +59,7 @@ func TestConduitRelayMessage_Multicast(t *testing.T) {
 	event := unittest.MockEntityFixture()
 	targetIds := unittest.IdentifierListFixture(10)
 
-	params := []interface{}{ctx, event, channel, insecure.Protocol_MULTICAST, uint32(num)}
+	params := []interface{}{event, channel, insecure.Protocol_MULTICAST, uint32(num)}
 	for _, id := range targetIds {
 		params = append(params, id)
 	}
@@ -87,7 +87,7 @@ func TestConduitRelayMessage_Unicast(t *testing.T) {
 	event := unittest.MockEntityFixture()
 	targetId := unittest.IdentifierFixture()
 
-	master.On("HandleIncomingEvent", ctx, event, channel, insecure.Protocol_UNICAST, uint32(0), targetId).
+	master.On("HandleIncomingEvent", event, channel, insecure.Protocol_UNICAST, uint32(0), targetId).
 		Return(nil).
 		Once()
 
@@ -112,7 +112,7 @@ func TestConduitReflectError_Unicast(t *testing.T) {
 	event := unittest.MockEntityFixture()
 	targetId := unittest.IdentifierFixture()
 
-	master.On("HandleIncomingEvent", ctx, event, channel, insecure.Protocol_UNICAST, uint32(0), targetId).
+	master.On("HandleIncomingEvent", event, channel, insecure.Protocol_UNICAST, uint32(0), targetId).
 		Return(fmt.Errorf("could not handle event")).
 		Once()
 
@@ -138,7 +138,7 @@ func TestConduitReflectError_Multicast(t *testing.T) {
 	event := unittest.MockEntityFixture()
 	targetIds := unittest.IdentifierListFixture(10)
 
-	params := []interface{}{ctx, event, channel, insecure.Protocol_MULTICAST, uint32(num)}
+	params := []interface{}{event, channel, insecure.Protocol_MULTICAST, uint32(num)}
 	for _, id := range targetIds {
 		params = append(params, id)
 	}
@@ -167,7 +167,7 @@ func TestConduitReflectError_Publish(t *testing.T) {
 	event := unittest.MockEntityFixture()
 	targetIds := unittest.IdentifierListFixture(10)
 
-	params := []interface{}{ctx, event, channel, insecure.Protocol_PUBLISH, uint32(0)}
+	params := []interface{}{event, channel, insecure.Protocol_PUBLISH, uint32(0)}
 	for _, id := range targetIds {
 		params = append(params, id)
 	}
