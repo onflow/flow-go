3803b6efc26b24ecbb3b89133013481c413b9ada
Gregor Gololicic <gregor.gololicic@dapperlabs.com>
2022-03-04 09:57:27 +0100

    add enum mapping

--- engine/access/rest/models/enums.go
+++ engine/access/rest/models/enums.go
@@ -9,3 +9,9 @@ const (
 	SEALED    = SEALED_TransactionStatus
 	EXPIRED   = EXPIRED_TransactionStatus
 )
+
+const (
+	PENDING_RESULT = PENDING_TransactionResultStatus
+	SUCCESS_RESULT = SUCCESS_TransactionResultStatus
+	FAILURE_RESULT = FAILURE_TransactionResultStatus
+)
