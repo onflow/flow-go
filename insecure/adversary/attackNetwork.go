4ec73415be0fc283a05ee1104ec4a4b04d813ab3
Yahya <yahya@dapperlabs.com>
2022-03-04 14:31:52 -0800

    adds component startup to attack network

--- insecure/adversary/attackNetwork.go
+++ insecure/adversary/attackNetwork.go
@@ -6,32 +6,57 @@ import (
 	"net"
 	"strconv"
 
+	"github.com/hashicorp/go-multierror"
+	"github.com/rs/zerolog"
 	"google.golang.org/grpc"
 
 	"github.com/onflow/flow-go/insecure"
 	"github.com/onflow/flow-go/model/flow"
 	"github.com/onflow/flow-go/module/component"
+	"github.com/onflow/flow-go/module/irrecoverable"
 	"github.com/onflow/flow-go/network"
 )
 
 type corruptedNodeConnection insecure.CorruptibleConduitFactory_ProcessAttackerMessageClient
 
+// AttackNetwork represents the networking interface that is available to the attacker for sending messages "through" corrupted nodes
+// "to" the rest of the network.
 type AttackNetwork struct {
 	component.Component
 	corruptedIds   flow.IdentityList
 	corruptedNodes map[flow.Identifier]corruptedNodeConnection
 	codec          network.Codec
+	logger         zerolog.Logger
 }
 
-func NewAttackNetwork(corruptedIds flow.IdentityList) *AttackNetwork {
+func NewAttackNetwork(corruptedIds flow.IdentityList, logger zerolog.Logger) *AttackNetwork {
 	attackNetwork := &AttackNetwork{
 		corruptedIds:   corruptedIds,
 		corruptedNodes: make(map[flow.Identifier]corruptedNodeConnection),
+		logger:         logger,
 	}
+
+	cm := component.NewComponentManagerBuilder().
+		AddWorker(func(ctx irrecoverable.SignalerContext, ready component.ReadyFunc) {
+			err := attackNetwork.start(ctx)
+			if err != nil {
+				ctx.Throw(fmt.Errorf("could not start attack network: %w", err))
+			}
+
+			ready()
+
+			<-ctx.Done()
+			if err := attackNetwork.stop(); err != nil {
+				logger.Err(err).Msg("error happened while closing attack network connections")
+			}
+		}).Build()
+
+	attackNetwork.Component = cm
 
 	return attackNetwork
 }
 
+// RpcUnicastOnChannel enforces unicast-dissemination on the specified channel through a corrupted node.
 func (a *AttackNetwork) RpcUnicastOnChannel(corruptedId flow.Identifier, channel network.Channel, event interface{}, targetId flow.Identifier) error {
 	connection, ok := a.corruptedNodes[corruptedId]
 	if !ok {
@@ -51,7 +76,9 @@ func (a *AttackNetwork) RpcUnicastOnChannel(corruptedId flow.Identifier, channel
 	return nil
 }
 
-func (a AttackNetwork) RpcPublishOnChannel(corruptedId flow.Identifier, channel network.Channel, event interface{}, targetIds ...flow.Identifier) error {
+// RpcPublishOnChannel enforces a publish-dissemination on the specified channel through a corrupted node.
+func (a *AttackNetwork) RpcPublishOnChannel(corruptedId flow.Identifier, channel network.Channel, event interface{},
+	targetIds ...flow.Identifier) error {
 	connection, ok := a.corruptedNodes[corruptedId]
 	if !ok {
 		return fmt.Errorf("no connection available for corrupted conduit factory to node %x: ", corruptedId)
@@ -70,7 +97,9 @@ func (a AttackNetwork) RpcPublishOnChannel(corruptedId flow.Identifier, channel 
 	return nil
 }
 
-func (a AttackNetwork) RpcMulticastOnChannel(corruptedId flow.Identifier, channel network.Channel, event interface{}, num uint32, targetIds ...flow.Identifier) error {
+// RpcMulticastOnChannel enforces a multicast-dissemination on the specified channel through a corrupted node.
+func (a *AttackNetwork) RpcMulticastOnChannel(corruptedId flow.Identifier, channel network.Channel, event interface{}, num uint32,
+	targetIds ...flow.Identifier) error {
 	connection, ok := a.corruptedNodes[corruptedId]
 	if !ok {
 		return fmt.Errorf("no connection available for corrupted conduit factory to node %x: ", corruptedId)
@@ -89,6 +118,7 @@ func (a AttackNetwork) RpcMulticastOnChannel(corruptedId flow.Identifier, channe
 	return nil
 }
 
+// start establishes a connection to individual corrupted conduit factories.
 func (a *AttackNetwork) start(ctx context.Context) error {
 	for _, corruptedId := range a.corruptedIds {
 		corruptibleClient, err := a.corruptibleConduitFactoryClient(ctx, corruptedId.Address)
@@ -99,6 +129,20 @@ func (a *AttackNetwork) start(ctx context.Context) error {
 	}
 
 	return nil
+}
+
+// stop terminates all connections to corrupted nodes.
+func (a *AttackNetwork) stop() error {
+	var errors *multierror.Error
+	for _, connection := range a.corruptedNodes {
+		err := connection.CloseSend()
+
+		if err != nil {
+			errors = multierror.Append(errors, err)
+		}
+	}
+
+	return errors.ErrorOrNil()
 }
 
 // corruptedConduitFactoryAddress generates and returns the gRPC interface address of corruptible conduit factory for given identity.
