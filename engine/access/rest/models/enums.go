56872a5b14779086bdbcec5bb8f8eb11f2476dfd
Gregor Gololicic <gregor.gololicic@dapperlabs.com>
2022-03-04 09:22:15 +0100

    add enum mapping

---
+++ engine/access/rest/models/enums.go
@@ -0,0 +1,11 @@
+package models
+
+// this mapping exists due to generated algorithms containing suffix that can't be removed
+
+const (
+	PENDING   = PENDING_TransactionStatus
+	FINALIZED = FINALIZED_TransactionStatus
+	EXECUTED  = EXECUTED_TransactionStatus
+	SEALED    = SEALED_TransactionStatus
+	EXPIRED   = EXPIRED_TransactionStatus
+)
