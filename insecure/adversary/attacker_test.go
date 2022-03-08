55d29da34ae3eb07a7f5fc28161b0bdcbfa944de
Yahya <yahya@dapperlabs.com>
2022-03-07 16:41:13 -0800

    adds startup test

--- insecure/adversary/attacker_test.go
+++ insecure/adversary/attacker_test.go
@@ -1,1 +1,42 @@
 package adversary
+
+import (
+	"context"
+	"testing"
+
+	"github.com/stretchr/testify/mock"
+	"github.com/stretchr/testify/require"
+
+	mockinsecure "github.com/onflow/flow-go/insecure/mock"
+	"github.com/onflow/flow-go/module/irrecoverable"
+	"github.com/onflow/flow-go/network/codec/cbor"
+	"github.com/onflow/flow-go/utils/unittest"
+)
+
+const attackerAddress = "localhost:0"
+
+func TestAttackerStart(t *testing.T) {
+
+	codec := cbor.NewCodec()
+	orchestrator := &mockinsecure.AttackOrchestrator{}
+	// mocks start up of orchestrator
+	orchestrator.On("Start", mock.AnythingOfType("*irrecoverable.signalerCtx")).Return().Once()
+
+	attacker, err := NewAttacker(unittest.Logger(), attackerAddress, codec, orchestrator)
+	require.NoError(t, err)
+
+	ctx := context.Background()
+	attackCtx, errChan := irrecoverable.WithSignaler(ctx)
+
+	go func() {
+		select {
+		case err := <-errChan:
+			t.Error("attacker startup encountered fatal error", err)
+		case <-ctx.Done():
+			return
+		}
+	}()
+
+	attacker.Start(attackCtx)
+	<-attacker.Ready()
+}
