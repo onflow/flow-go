03dd585e7214df72eff65590ae3e115210d98885
Janez Podhostnik <janez.podhostnik@gmail.com>
2022-03-10 16:00:44 +0000

    remove unnecesary change

--- fvm/extralog/extralog.go
+++ fvm/extralog/extralog.go
@@ -5,10 +5,11 @@ import (
 	"io/ioutil"
 )
 
-// ExtraLogDumpPath is a shared variable with path where to dump extra logs
+// This is a shared variable with path where to dump extra logs
 // Currently we dump them inside Transaction structure and due to temporary nature of this fix
 // it's easier to use global variable rather than pass extra parameters down multiple levels,
 // changing multiple tests etc.
+
 var ExtraLogDumpPath string
 
 func init() {
