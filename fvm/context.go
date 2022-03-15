605a4d88058b3b56c342c13e6cd47453397ef31a
ramtinms <ramtin@axiomzen.co>
2022-03-15 13:19:29 -0700

    move service tx limits to another pr

--- fvm/context.go
+++ fvm/context.go
@@ -64,14 +64,6 @@ const (
 	DefaultMemoryLimit                  = 100_000_000 // 100G
 	DefaultEventCollectionByteSizeLimit = 256_000     // 256KB
 	DefaultMaxNumOfTxRetries            = 3
-
-	// service transaction is a transaction have service account as payer
-	// and signed by service account, higher limits are allowed
-	// for service transactions to consider flexibility for network services
-	// such as governance transactions, etc.
-	ServiceTransactionComputationLimit             = 100_000_000_000 // 100B
-	ServiceTransactionMemoryLimit                  = 100_000_000     // 100G
-	ServiceTransactionEventCollectionByteSizeLimit = 100_000_000     // 100G
 )
 
 func defaultContext(logger zerolog.Logger) Context {
