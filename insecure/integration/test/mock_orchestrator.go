5e973bd755d873bdb681f2cf998ddad1f75eb89d
Yahya <yahya@dapperlabs.com>
2022-03-16 13:56:01 -0700

    implements mock orchestrator for testing

--- insecure/integration/test/mock_orchestrator.go
+++ insecure/integration/test/mock_orchestrator.go
@@ -1,1 +1,19 @@
 package test
+
+import (
+	"github.com/onflow/flow-go/insecure"
+)
+
+type mockOrchestrator struct {
+	attackNetwork  insecure.AttackNetwork
+	eventCorrupter func(event *insecure.Event)
+}
+
+func (m *mockOrchestrator) HandleEventFromCorruptedNode(event *insecure.Event) error {
+	m.eventCorrupter(event)
+	return m.attackNetwork.Send(event)
+}
+
+func (m *mockOrchestrator) WithAttackNetwork(attackNetwork insecure.AttackNetwork) {
+	m.attackNetwork = attackNetwork
+}
