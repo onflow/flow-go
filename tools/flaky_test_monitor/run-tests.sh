4ac23907a15bbf29207bd76b884e1646cef27238
Simon Zhu <simon.zsiyan@gmail.com>
2022-03-22 17:35:16 -0700

    update scripts

--- tools/flaky_test_monitor/run-tests.sh
+++ tools/flaky_test_monitor/run-tests.sh
@@ -1,5 +1,8 @@
 #!/usr/bin/env bash
 
+# This script runs the tests in the category specified by the TEST_CATEGORY environment variable,
+# and saves the output to the file specified by the TEST_OUTPUT_FILE environment variable.
+
 set -e
 shopt -s extglob
 
@@ -13,35 +16,29 @@ case $TEST_CATEGORY in
     ;;
 esac
 
-export COMMIT_DATE=$(git show --no-patch --no-notes --pretty='%cI' $COMMIT_SHA)
-
 make crypto/relic/build
 
 export JSON_OUTPUT=true
-
-OUTPUT_FILE=output.txt
 
 # run tests and process results
 if [[ $TEST_CATEGORY =~ integration-(common|network|epochs|access|collection|consensus|execution|verification)$ ]]
 then
     make docker-build-flow
-    make -C integration -s ${BASH_REMATCH[1]}-tests > $OUTPUT_FILE
+    make -C integration -s ${BASH_REMATCH[1]}-tests > $TEST_OUTPUT_FILE
 else
     case $TEST_CATEGORY in
         unit)
             make install-mock-generators
             make generate-mocks
-            make -s unittest-main > $OUTPUT_FILE
+            make -s unittest-main > $TEST_OUTPUT_FILE
         ;;
         unit-crypto)
             make -C crypto setup
-            make -C crypto -s test-main > $OUTPUT_FILE
+            make -C crypto -s test-main > $TEST_OUTPUT_FILE
         ;;
         unit-integration)
-            make -C integration -s test > $OUTPUT_FILE
+            make -C integration -s test > $TEST_OUTPUT_FILE
         ;;
     esac
 fi
-
-cat $OUTPUT_FILE | go run tools/flaky_test_monitor/level1/process_summary1_results.go
 
