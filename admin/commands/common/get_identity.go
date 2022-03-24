30f4957b802c8621fdae704ed9df3f6218db5df1
Simon Zhu <simon.zsiyan@gmail.com>
2022-03-23 17:28:26 -0700

    fix lint

--- admin/commands/common/get_identity.go
+++ admin/commands/common/get_identity.go
@@ -7,6 +7,7 @@ import (
 	"fmt"
 
 	"github.com/libp2p/go-libp2p-core/peer"
+
 	"github.com/onflow/flow-go/admin"
 	"github.com/onflow/flow-go/admin/commands"
 	"github.com/onflow/flow-go/model/flow"
