e2c4ab31bd4bcc67d153092767ec96da334a5b46
Simon Zhu <simon.zsiyan@gmail.com>
2022-03-25 22:11:55 -0700

    Update run-tests.sh

--- tools/flaky_test_monitor/run-tests.sh
+++ tools/flaky_test_monitor/run-tests.sh
@@ -11,6 +11,7 @@ export JSON_OUTPUT=true
 # run tests and process results
 if [[ $TEST_CATEGORY =~ integration-(common|network|epochs|access|collection|consensus|execution|verification)$ ]]
 then
+    docker kill $(docker ps -q)
     make -C integration -s ${BASH_REMATCH[1]}-tests > $TEST_OUTPUT_FILE
 else
     case $TEST_CATEGORY in
