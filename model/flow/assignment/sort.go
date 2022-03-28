69196e74ca1f6e282239d147759933cf1f80b8d5
Leo Zhang (zhangchiqing) <zhangchiqing@gmail.com>
2022-03-28 11:53:12 -0700

    add tests

--- model/flow/assignment/sort.go
+++ model/flow/assignment/sort.go
@@ -10,8 +10,7 @@ func FromIdentifierLists(identifierLists []flow.IdentifierList) flow.AssignmentL
 	assignments := make(flow.AssignmentList, 0, len(identifierLists))
 	// in place sort to order the assignment in canonical order
 	for _, identities := range identifierLists {
-		assignment := identities[:]
-		flow.IdentifierList(assignment).Sort(order.IdentifierCanonical)
+		assignment := flow.IdentifierList(identities).Sort(order.IdentifierCanonical)
 		assignments = append(assignments, assignment)
 	}
 	return assignments
---
+++ model/flow/assignment/sort_test.go
@@ -0,0 +1,27 @@
+package assignment_test
+
+import (
+	"testing"
+
+	"github.com/onflow/flow-go/model/flow"
+	"github.com/onflow/flow-go/model/flow/assignment"
+	"github.com/stretchr/testify/require"
+)
+
+func TestSort(t *testing.T) {
+	node1, err := flow.HexStringToIdentifier("0000000000000000000000000000000000000000000000000000000000000001")
+	require.NoError(t, err)
+	node2, err := flow.HexStringToIdentifier("0000000000000000000000000000000000000000000000000000000000000002")
+	require.NoError(t, err)
+	node3, err := flow.HexStringToIdentifier("0000000000000000000000000000000000000000000000000000000000000003")
+	require.NoError(t, err)
+
+	unsorted := []flow.IdentifierList{flow.IdentifierList{node2, node1, node3}}
+
+	assignments := assignment.FromIdentifierLists(unsorted)
+	require.Len(t, assignments, 1)
+
+	require.Equal(t, node1, assignments[0][0])
+	require.Equal(t, node2, assignments[0][1])
+	require.Equal(t, node3, assignments[0][2])
+}
