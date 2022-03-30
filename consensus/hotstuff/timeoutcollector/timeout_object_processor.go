3c4588326886596629f3f8d4924e7293d87ed3f0
Yurii Oleksyshyn <yuraolex@gmail.com>
2022-03-30 21:03:46 +0300

    Added timeout object weight tracker. Added skeleton logic

--- consensus/hotstuff/timeoutcollector/timeout_object_processor.go
+++ consensus/hotstuff/timeoutcollector/timeout_object_processor.go
@@ -1,7 +1,9 @@
 package timeoutcollector
 
 import (
+	"github.com/onflow/flow-go/consensus/hotstuff"
 	"github.com/onflow/flow-go/consensus/hotstuff/model"
+	"github.com/onflow/flow-go/model/flow"
 	"go.uber.org/atomic"
 )
 
@@ -11,18 +13,38 @@ type accumulatedWeightTracker struct {
 	onWeightAccumulated func()
 }
 
-func (t *accumulatedWeightTracker) Track(weight uint64) {
+func (t *accumulatedWeightTracker) Track(weight uint64) bool {
 	if weight < t.minRequiredWeight {
-		return
+		return false
 	}
 	if t.done.CAS(false, true) {
 		t.onWeightAccumulated()
+		return true
 	}
+	return false
 }
 
-type TimeoutVoteProcessor struct {
-	partialTCTracker *accumulatedWeightTracker
-	tcTracker        *accumulatedWeightTracker
+type TimeoutObjectProcessor struct {
+	view             uint64
+	highQCViews      map[uint64]struct{}
+	highestQC        *flow.QuorumCertificate
+	partialTCTracker accumulatedWeightTracker
+	tcTracker        accumulatedWeightTracker
+}
+
+func NewTimeoutObjectProcessor(view uint64, totalWeight uint64, onPartialTCCreated hotstuff.OnPartialTCCreated) *TimeoutObjectProcessor {
+	return &TimeoutObjectProcessor{
+		partialTCTracker: accumulatedWeightTracker{
+			minRequiredWeight:   hotstuff.ComputeWeightThresholdForHonestMajority(totalWeight),
+			done:                *atomic.NewBool(false),
+			onWeightAccumulated: func() { onPartialTCCreated(view) },
+		},
+		tcTracker: accumulatedWeightTracker{
+			minRequiredWeight:   hotstuff.ComputeWeightThresholdForBuildingQC(totalWeight),
+			done:                *atomic.NewBool(false),
+			onWeightAccumulated: func() {},
+		},
+	}
 }
 
 // Process performs processing of timeout object in concurrent safe way. This
@@ -35,9 +57,25 @@ type TimeoutVoteProcessor struct {
 // * VoteForIncompatibleViewError - submitted vote for incompatible view
 // * model.InvalidVoteError - submitted vote with invalid signature
 // All other errors should be treated as exceptions.
-func (p *TimeoutVoteProcessor) Process(timeout *model.TimeoutObject) error {
+func (p *TimeoutObjectProcessor) Process(timeout *model.TimeoutObject) error {
 	if p.tcTracker.done.Load() {
 		return nil
 	}
+
+	p.highQCViews[timeout.HighestQC.View] = struct{}{}
+	if p.highestQC.View < timeout.HighestQC.View {
+		p.highestQC = timeout.HighestQC
+	}
+
 	return nil
+}
+
+func (p *TimeoutObjectProcessor) buildTC() (*flow.TimeoutCertificate, error) {
+	return &flow.TimeoutCertificate{
+		View:          p.view,
+		TOHighQCViews: nil,
+		TOHighestQC:   nil,
+		SignerIDs:     nil,
+		SigData:       nil,
+	}, nil
 }
