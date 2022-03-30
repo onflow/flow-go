34d31a464631f8e550c0760c53394c218c06e1a9
Simon Zhu <simon.zsiyan@gmail.com>
2022-03-30 16:02:52 -0700

    comment out test monitor integration tests

--- integration/tests/access/api/main_test.go
+++ integration/tests/access/api/main_test.go
@@ -6,10 +6,8 @@ import (
 	"github.com/stretchr/testify/suite"
 
 	"github.com/onflow/flow-go/integration/tests/access"
-	"github.com/onflow/flow-go/utils/unittest"
 )
 
 func TestAccessSuite(t *testing.T) {
-	unittest.SkipUnless(t, unittest.TEST_TODO, "broken in CI")
 	suite.Run(t, new(access.AccessSuite))
 }
