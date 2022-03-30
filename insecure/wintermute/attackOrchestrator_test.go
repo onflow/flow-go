5d2133a46a576782073021d1ddf456185c4e190f
Yahya <yahya@dapperlabs.com>
2022-03-30 10:50:40 -0700

    wip adds event sanity check

--- insecure/wintermute/attackOrchestrator_test.go
+++ insecure/wintermute/attackOrchestrator_test.go
@@ -21,12 +21,12 @@ import (
 	"github.com/onflow/flow-go/utils/unittest"
 )
 
-// TestOrchestrator_HandleEventFromCorruptedNode_ExecutionReceipt tests that the orchestrator corrupts the execution receipt if the receipt is from a
-// corrupted execution node.
+// TestOrchestrator_HandleEventFromCorruptedNode_SingleExecutionReceipt tests that the orchestrator corrupts the execution receipt if the receipt is
+// from a corrupted execution node.
 // If an execution receipt is coming from a corrupt execution node,
 // then orchestrator tampers with the receipt and generates a counterfeit receipt, and then
 // enforces all corrupted execution nodes to send that counterfeit receipt on their behalf in the flow network.
-func TestOrchestrator_HandleEventFromCorruptedNode_ExecutionReceipt(t *testing.T) {
+func TestOrchestrator_HandleEventFromCorruptedNode_SingleExecutionReceipt(t *testing.T) {
 	rootStateFixture, allIdentityList, corruptedIdentityList := bootstrapWintermuteFlowSystem(t)
 
 	receipt := unittest.ExecutionReceiptFixture()
@@ -63,6 +63,98 @@ func TestOrchestrator_HandleEventFromCorruptedNode_ExecutionReceipt(t *testing.T
 		corruptedReceiptsSentWG.Wait,
 		1*time.Second,
 		"orchestrator could not send corrupted receipts on time")
+}
+
+func TestOrchestrator_HandleEventFromCorruptedNode_MultipleExecutionReceipt(t *testing.T) {
+	rootStateFixture, allIdentityList, corruptedIdentityList := bootstrapWintermuteFlowSystem(t)
+	corruptedExecutionIds := flow.IdentifierList(corruptedIdentityList.Filter(filter.HasRole(flow.RoleExecution)).NodeIDs())
+	// identities of nodes who are expected targets of an execution receipt.
+	receiptTargetIds, err := rootStateFixture.State.Final().Identities(filter.HasRole(flow.RoleAccess, flow.RoleConsensus, flow.RoleVerification))
+	require.NoError(t, err)
+
+	corruptedEnId1 := corruptedExecutionIds[0]
+	receipt1 := unittest.ExecutionReceiptFixture(unittest.WithExecutorID(corruptedEnId1))
+	event1 := &insecure.Event{
+		CorruptedId:       corruptedEnId1,
+		Channel:           engine.PushReceipts,
+		Protocol:          insecure.Protocol_UNICAST,
+		TargetIds:         receiptTargetIds.NodeIDs(),
+		FlowProtocolEvent: receipt1,
+	}
+
+	corruptedEnId2 := corruptedExecutionIds[1]
+	receipt2 := unittest.ExecutionReceiptFixture(unittest.WithExecutorID(corruptedEnId2))
+	event2 := &insecure.Event{
+		CorruptedId:       corruptedEnId2,
+		Channel:           engine.PushReceipts,
+		Protocol:          insecure.Protocol_UNICAST,
+		TargetIds:         receiptTargetIds.NodeIDs(),
+		FlowProtocolEvent: receipt2,
+	}
+
+	mockAttackNetwork := &mockinsecure.AttackNetwork{}
+
+	wintermuteOrchestrator := wintermute.NewOrchestrator(allIdentityList, corruptedIdentityList, unittest.Logger())
+
+	receivedEventList := make([]*insecure.Event, 0)
+	corruptedReceiptsRcvWG := &sync.WaitGroup{}
+	corruptedReceiptsRcvWG.Add(3)
+	mockAttackNetwork.
+		On("Send", mock.Anything).
+		Run(func(args mock.Arguments) {
+			// assert that args passed are correct
+
+			// extract Event sent
+			event, ok := args[0].(*insecure.Event)
+			require.True(t, ok)
+
+			// make sure sender is a corrupted execution node.
+			ok = corruptedExecutionIds.Contains(event.CorruptedId)
+			require.True(t, ok)
+
+			// makes sure sender is unique
+			receivedEventList = append(receivedEventList, event)
+
+			corruptedReceiptsRcvWG.Done()
+		}).Return(nil)
+
+	// register mock network with orchestrator
+	wintermuteOrchestrator.WithAttackNetwork(mockAttackNetwork)
+
+	corruptedReceiptsSentWG := sync.WaitGroup{}
+	corruptedReceiptsSentWG.Add(2)
+	go func() {
+		err = wintermuteOrchestrator.HandleEventFromCorruptedNode(event1)
+		require.NoError(t, err)
+
+		corruptedReceiptsSentWG.Done()
+	}()
+
+	go func() {
+		err = wintermuteOrchestrator.HandleEventFromCorruptedNode(event2)
+		require.NoError(t, err)
+
+		corruptedReceiptsSentWG.Done()
+	}()
+
+	// waits till corrupted receipts dictated to all execution nodes.
+	unittest.RequireReturnsBefore(t,
+		corruptedReceiptsSentWG.Wait,
+		1*time.Second,
+		"orchestrator could not send corrupted receipts on time")
+
+	// waits till corrupted receipts dictated to all execution nodes.
+	unittest.RequireReturnsBefore(t,
+		corruptedReceiptsRcvWG.Wait,
+		1*time.Second,
+		"orchestrator could not receive corrupted receipts on time")
+
+	// checks one receipt gets corrupted and sent to both corrupted execution nodes
+	receivedExecutionReceiptEventsSanityCheck(
+		t,
+		receivedEventList,
+		corruptedExecutionIds,
+		flow.IdentifierList{receipt1.ID(), receipt2.ID()})
 }
 
 // TestHandleEventFromCorruptedNode_HonestVN tests that honest VN will be ignored when they send a chunk data request
@@ -130,6 +222,9 @@ func mockAttackNetworkForCorruptedExecutionReceipt(
 			corruptedReceipt, ok := event.FlowProtocolEvent.(*flow.ExecutionReceipt)
 			require.True(t, ok)
 
+			// make sure sender and executor are the same
+			require.Equal(t, corruptedReceipt.ExecutorID, event.CorruptedId)
+
 			// make sure the original uncorrupted execution receipt is NOT sent to orchestrator
 			require.NotEqual(t, receipt, corruptedReceipt)
 			require.ElementsMatch(t, receiptTargetIds, event.TargetIds)
@@ -139,4 +234,31 @@ func mockAttackNetworkForCorruptedExecutionReceipt(
 		}).Return(nil)
 
 	return wg
+}
+
+func receivedExecutionReceiptEventsSanityCheck(
+	t *testing.T,
+	events []*insecure.Event,
+	corruptedExecutionNodeIds flow.IdentifierList,
+	originalReceiptIds flow.IdentifierList) {
+
+	sendersByReceipts := make(map[flow.Identifier]flow.IdentifierList)
+	for _, submittedEvent := range events {
+		// submitted event must contain an execution receipt.
+		submittedReceipt, ok := submittedEvent.FlowProtocolEvent.(*flow.ExecutionReceipt)
+		require.True(t, ok)
+
+		// sender and executor for this receipt must be the same.
+		require.Equal(t, submittedReceipt.ExecutorID, submittedEvent.CorruptedId)
+
+		receiptId := submittedReceipt.ID()
+		if sendersByReceipts[receiptId] == nil {
+			sendersByReceipts[receiptId] = flow.IdentifierList{}
+		}
+		sendersByReceipts[receiptId].Union(flow.IdentifierList{submittedEvent.CorruptedId})
+	}
+
+	for submittedReceiptId := range sendersByReceipts {
+		require.True(t, originalReceiptIds.Contains(submittedReceiptId))
+	}
 }
