98c6f14917938222a9999a778cf5c1c49e5dbfb7
Yurii Oleksyshyn <yuraolex@gmail.com>
2022-04-01 15:51:45 +0300

    Updated godoc

--- consensus/hotstuff/timeoutcollector/aggregation.go
+++ consensus/hotstuff/timeoutcollector/aggregation.go
@@ -23,15 +23,25 @@ type sigInfo struct {
 }
 
 type MultiMessageSignatureAggregator struct {
+	lock             sync.RWMutex
 	hasher           hash.Hasher
-	ids              flow.IdentityList
+	ids              flow.IdentityList              // all possible ids (only gets updated by constructor)
 	idToInfo         map[flow.Identifier]signerInfo // auxiliary map to lookup signer weight and index by ID (only gets updated by constructor)
 	indexToSignature map[int]sigInfo                // signatures indexed by the signer index
-	publicKeys       []crypto.PublicKey
-	totalWeight      uint64
-	lock             sync.RWMutex
+	publicKeys       []crypto.PublicKey             // keys indexed from 0 to n-1, signer i is assigned to public key i
+	totalWeight      uint64                         // total accumulated weight
 }
 
+// NewMultiMessageSigAggregator returns a multi message signature aggregator initialized with a list of flow
+// identities, their respective public keys and a domain separation tag. The identities
+// represent the list of all possible signers.
+// The constructor errors if:
+// - the list of identities is empty
+// - if the length of keys does not match the length of identities
+// - if one of the keys is not a valid public key.
+//
+// A multi message sig aggregator is used for one aggregation only. A new instance should be used for each
+// signature aggregation task in the protocol.
 func NewMultiMessageSigAggregator(ids flow.IdentityList, // list of all authorized signers
 	pks []crypto.PublicKey, // list of corresponding public keys used for signature verifications
 	dsTag string, // domain separation tag used by the signature) *MultiMessageSignatureAggregator
@@ -68,6 +78,11 @@ func NewMultiMessageSigAggregator(ids flow.IdentityList, // list of all authoriz
 	}, nil
 }
 
+// Verify verifies the signature under the stored public keys.
+// Expected errors during normal operations:
+//  - model.InvalidSignerError if signerID is invalid (not a consensus participant)
+//  - model.ErrInvalidSignature if signerID is valid but signature is cryptographically invalid
+// The function is thread-safe.
 func (a *MultiMessageSignatureAggregator) Verify(signerID flow.Identifier, sig crypto.Signature, msg []byte) error {
 	info, ok := a.idToInfo[signerID]
 	if !ok {
@@ -83,6 +98,15 @@ func (a *MultiMessageSignatureAggregator) Verify(signerID flow.Identifier, sig c
 	return nil
 }
 
+// TrustedAdd adds signature and message to the internal set of signatures and adds the signer's
+// weight to the total collected weight, iff the signature is _not_ a duplicate.
+//
+// The total weight of all collected signatures (excluding duplicates) is returned regardless
+// of any returned error.
+// The function errors with:
+//  - model.InvalidSignerError if signerID is invalid (not a consensus participant)
+//  - model.DuplicatedSignerError if the signer has been already added
+// The function is thread-safe.
 func (a *MultiMessageSignatureAggregator) TrustedAdd(signerID flow.Identifier, sig crypto.Signature, msg []byte) (uint64, error) {
 	info, found := a.idToInfo[signerID]
 	if !found {
@@ -124,10 +148,9 @@ func (a *MultiMessageSignatureAggregator) TotalWeight() uint64 {
 // TODO : When compacting the list of signers, update the return from []flow.Identifier
 //        to a compact bit vector.
 func (a *MultiMessageSignatureAggregator) Aggregate() ([]flow.Identifier, []byte, error) {
-	a.lock.Lock()
-	defer a.lock.Unlock()
+	a.lock.RLock()
+	defer a.lock.RUnlock()
 
-	// compute aggregation result and cache it in `a.cachedSignerIndices`, `a.cachedSignature`
 	sharesNum := len(a.indexToSignature)
 	if sharesNum == 0 {
 		return nil, nil, model.NewInsufficientSignaturesErrorf("cannot aggregate an empty list of signatures\"")
