8fd0e0c2cf899d4541f55a74088764e50ab9f73d
Yahya <yahya@dapperlabs.com>
2022-04-01 11:34:25 -0700

    adds attack state

--- insecure/wintermute/attackState.go
+++ insecure/wintermute/attackState.go
@@ -1,1 +1,13 @@
 package wintermute
+
+import (
+	"github.com/onflow/flow-go/model/flow"
+)
+
+// attackState keeps data structures related to a specific wintermute attack instance.
+type attackState struct {
+	originalResult    *flow.ExecutionResult // original valid execution result
+	corruptedResult   *flow.ExecutionResult // corrupted execution result by orchestrator
+	originalChunkIds  flow.IdentifierList   // list of chunk ids of original result
+	corruptedChunkIds flow.IdentifierFilter // list of chunk ids of corrupted result
+}
