6396ab6be4021075eda5c595fa55b8f576c269eb
Yahya <yahya@dapperlabs.com>
2022-04-01 11:59:28 -0700

    refactors attack state

--- insecure/wintermute/attackState.go
+++ insecure/wintermute/attackState.go
@@ -9,5 +9,5 @@ type attackState struct {
 	originalResult    *flow.ExecutionResult // original valid execution result
 	corruptedResult   *flow.ExecutionResult // corrupted execution result by orchestrator
 	originalChunkIds  flow.IdentifierList   // list of chunk ids of original result
-	corruptedChunkIds flow.IdentifierFilter // list of chunk ids of corrupted result
+	corruptedChunkIds flow.IdentifierList   // list of chunk ids of corrupted result
 }
