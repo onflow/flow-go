cfed5029510f11aeb2a69183b32ebd39c795b1d8
Yurii Oleksyshyn <yuraolex@gmail.com>
2022-04-14 15:09:28 +0300

    Apply suggestions from code review
    Co-authored-by: Alexander Hentschel <alex.hentschel@axiomzen.co>

--- consensus/hotstuff/timeout_aggregator.go
+++ consensus/hotstuff/timeout_aggregator.go
@@ -5,8 +5,8 @@ import (
 	"github.com/onflow/flow-go/module"
 )
 
-// TimeoutAggregator verifies and aggregates timeout objects to build TC.
-// When enough votes have been collected, it builds a TC and send it to the EventLoop
+// TimeoutAggregator verifies and aggregates timeout objects to build timeout certificates [TCs].
+// When enough timeout objects are collected, it builds a TC and sends it to the EventLoop
 // TimeoutAggregator also detects protocol violation, including invalid timeouts, double timeout, etc and
 // notifies a HotStuff consumer for slashing.
 type TimeoutAggregator interface {
@@ -18,7 +18,7 @@ type TimeoutAggregator interface {
 	AddTimeout(timeoutObject *model.TimeoutObject)
 
 	// PruneUpToView deletes all timeouts _below_ to the given view, as well as
-	// related indices. We only retain and process whose view is equal or larger
+	// related indices. We only retain and process timeouts, whose view is equal or larger
 	// than `lowestRetainedView`. If `lowestRetainedView` is smaller than the
 	// previous value, the previous value is kept and the method call is a NoOp.
 	// This value should be set the latest active view maintained by `Pacemaker`.
