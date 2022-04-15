efaa4cf4fce618390cdd7c141bd034a16353cf3a
Yurii Oleksyshyn <yuraolex@gmail.com>
2022-04-15 11:14:11 +0300

    Fixed import

--- model/flow/timeout_certificate.go
+++ model/flow/timeout_certificate.go
@@ -1,11 +1,13 @@
 package flow
 
+import "github.com/onflow/flow-go/crypto"
+
 // TimeoutCertificate proves that a supermajority of consensus participants want to abandon the specified View.
 // At its core, a timeout certificate is an aggregation of TimeoutObjects, which individual nodes send to signal
 // their intent to leave the active view.
 type TimeoutCertificate struct {
 	View uint64
-	// TOHighQCViews lists for each signer (in the same order) the view of the highest QC they supplied 
+	// TOHighQCViews lists for each signer (in the same order) the view of the highest QC they supplied
 	// as part of their TimeoutObject message (specifically TimeoutObject.HighestQC.View).
 	TOHighQCViews []uint64
 	// TOHighestQC is the newest QC from all TimeoutObject that were aggregated for this certificate.
