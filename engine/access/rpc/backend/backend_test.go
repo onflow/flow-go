e7f4a24e75c6b13b4f6952a4f778891f53bad4f2
Amlandeep Bhadra <amlandeep1912@gmail.com>
2022-04-19 00:02:52 -0400

    fix test

--- engine/access/rpc/backend/backend_test.go
+++ engine/access/rpc/backend/backend_test.go
@@ -2080,6 +2080,18 @@ func (suite *Suite) TestExecutionNodesForBlockID() {
 			require.ElementsMatch(suite.T(), actualList, expectedENs)
 		}
 	}
+	// if we don't find sufficient receipts, executionNodesForBlockID should return a list of random ENs
+	suite.Run("insufficient receipts return random ENs in state", func() {
+		// return no receipts at all attempts
+		attempt1Receipts = flow.ExecutionReceiptList{}
+		attempt2Receipts = flow.ExecutionReceiptList{}
+		attempt3Receipts = flow.ExecutionReceiptList{}
+		suite.state.On("AtBlockID", mock.Anything).Return(suite.snapshot)
+		actualList, err := executionNodesForBlockID(context.Background(), block.ID(), suite.receipts, suite.state, suite.log)
+		require.NoError(suite.T(), err)
+		require.Equal(suite.T(), len(actualList), maxExecutionNodesCnt)
+	})
+
 	// if no preferred or fixed ENs are specified, the ExecutionNodesForBlockID function should
 	// return the exe node list without a filter
 	suite.Run("no preferred or fixed ENs", func() {
@@ -2134,20 +2146,6 @@ func (suite *Suite) TestExecutionNodesForBlockID() {
 		preferredENs := allExecutionNodes[0:2]
 		expectedList := preferredENs
 		testExecutionNodesForBlockID(preferredENs, nil, expectedList)
-	})
-
-	suite.Run("insufficient receipts return random ENs in state", func() {
-		// return no receipts at all attempts
-		totalENs := 5
-		attempt1Receipts = flow.ExecutionReceiptList{}
-		attempt2Receipts = flow.ExecutionReceiptList{}
-		attempt3Receipts = flow.ExecutionReceiptList{}
-		availableExecNodes := unittest.IdentityListFixture(totalENs, unittest.WithRole(flow.RoleExecution))
-		suite.snapshot.On("Identities", mock.Anything).Return(availableExecNodes)
-		suite.state.On("AtBlockID", mock.Anything).Return(suite.snapshot)
-		actualList, err := executionNodesForBlockID(context.Background(), block.ID(), suite.receipts, suite.state, suite.log)
-		require.NoError(suite.T(), err)
-		require.Equal(suite.T(), len(actualList), maxExecutionNodesCnt)
 	})
 }
 
