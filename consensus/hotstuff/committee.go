617cc30a0023be73f302f0f212bc9c0dd5ca325e
Jordan Schalm <jordan@dapperlabs.com>
2022-04-19 20:49:45 -0400

    adjust interfaces

--- consensus/hotstuff/committee.go
+++ consensus/hotstuff/committee.go
@@ -5,8 +5,6 @@ import (
 	"github.com/onflow/flow-go/state/protocol"
 )
 
-// commonCommittee defines methods shared between VoterCommittee and ProposerCommittee.
-//
 // A committee provides a subset of the protocol.State, which is restricted to exactly those
 // nodes that participate in the current HotStuff instance: the state of all legitimate HotStuff
 // participants for the specified view. Legitimate HotStuff participants have NON-ZERO WEIGHT.
@@ -18,9 +16,18 @@ import (
 //
 // When validating proposals, we take into account changes to the committee during the course of
 // an epoch. In particular, if a node is ejected, we will immediately reject all future proposals
-// from that node. For these purposes we use the ProposerCommittee and *ByBlock methods.
+// from that node. For these purposes we use the Committee and *ByBlock methods.
+
+// VoterCommittee defines the consensus committee for the purposes of validating votes, timeouts,
+// quorum certificates, and timeout certificates. Any consensus committee member who was legitimate
+// AT THE BEGINNING of the epoch may produce valid votes and timeouts for the entire epoch, even
+// if they are later ejected.
 //
-type commonCommittee interface {
+// Since the voter committee is considered static over an epoch:
+// * we can query identities by view
+// * we don't need the full block ancestry prior to validating messages
+//
+type VoterCommittee interface {
 	// LeaderForView returns the identity of the leader for a given view.
 	// CAUTION: per liveness requirement of HotStuff, the leader must be fork-independent.
 	//          Therefore, a node retains its proposer view slots even if it is slashed.
@@ -37,21 +44,7 @@ type commonCommittee interface {
 	Self() flow.Identifier
 
 	// DKG returns the DKG info for epoch given by the input view.
-	DKG(view uint64) (protocol.DKG, error)
-}
-
-// VoterCommittee defines the consensus committee for the purposes of validating votes, timeouts,
-// quorum certificates, and timeout certificates. Any consensus committee member who was legitimate
-// AT THE BEGINNING of the epoch may produce valid votes and timeouts for the entire epoch, even
-// if they are later ejected.
-//
-// Since the voter committee is considered static over an epoch:
-// * we can query by view
-// * we don't need the full block ancestry prior to validating messages
-//
-// CAUTION: DO NOT use this for validating block proposals.
-type VoterCommittee interface {
-	commonCommittee
+	DKG(view uint64) (DKG, error)
 
 	// IdentitiesByEpoch returns a list of the legitimate HotStuff participants for the epoch
 	// given by the input view. The list of participants is filtered by the provided selector.
@@ -62,6 +55,7 @@ type VoterCommittee interface {
 	//   * contains no duplicates.
 	// The list of all legitimate HotStuff participants for the given epoch can be obtained by using `filter.Any`
 	//
+	// CAUTION: DO NOT use this method for validating block proposals.
 	// TODO: error for unknown view
 	IdentitiesByEpoch(view uint64, selector flow.IdentityFilter) (flow.IdentityList, error)
 
@@ -70,19 +64,20 @@ type VoterCommittee interface {
 	// ERROR conditions:
 	//  * model.InvalidSignerError if participantID does NOT correspond to an authorized HotStuff participant at the specified block.
 	//
+	// CAUTION: DO NOT use this method for validating block proposals.
 	// TODO: error for unknown view
 	IdentityByEpoch(view uint64, participantID flow.Identifier) (*flow.Identity, error)
 }
 
-// ProposerCommittee defines the consensus committee for the purposes of validating proposals.
-// The proposer committee reflects block-to-block changes in the identity table to support
-// immediately rejecting proposals from nodes after they are ejected.
+// Committee extends VoterCommittee to provide the consensus committee for the purposes
+// of validating proposals. The proposer committee reflects block-to-block changes in the
+// identity table to support immediately rejecting proposals from nodes after they are ejected.
 //
 // Since the proposer committee can change at any block:
 // * we query by block ID
 // * we must have incorporated the full block ancestry prior to validating messages
-type ProposerCommittee interface {
-	commonCommittee
+type Committee interface {
+	VoterCommittee
 
 	// IdentitiesByBlock returns a list of the legitimate HotStuff participants for the given block.
 	// The list of participants is filtered by the provided selector.
@@ -100,13 +95,13 @@ type ProposerCommittee interface {
 	// The node must be a legitimate HotStuff participant with NON-ZERO WEIGHT at the specified block.
 	// ERROR conditions:
 	//  * model.InvalidSignerError if participantID does NOT correspond to an authorized HotStuff participant at the specified block.
-	IdentityByBlock(view uint64, participantID flow.Identifier) (*flow.Identity, error)
+	IdentityByBlock(blockID flow.Identifier, participantID flow.Identifier) (*flow.Identity, error)
 }
 
 // TODO remove
-//type DKG interface {
-//	protocol.DKG
-//}
+type DKG interface {
+	protocol.DKG
+}
 
 // ComputeWeightThresholdForBuildingQC returns the weight that is minimally required for building a QC
 func ComputeWeightThresholdForBuildingQC(totalWeight uint64) uint64 {
