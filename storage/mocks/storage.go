3a58231bd28e9b9584bac44dec214a82e5a50a88
gomisha <misha@gomisha.com>
2022-04-21 11:57:17 -0500

    ran make generate-mocks

--- storage/mocks/storage.go
+++ storage/mocks/storage.go
@@ -262,6 +262,20 @@ func (mr *MockHeadersMockRecorder) IndexByChunkID(arg0, arg1 interface{}) *gomoc
 	return mr.mock.ctrl.RecordCallWithMethodType(mr.mock, "IndexByChunkID", reflect.TypeOf((*MockHeaders)(nil).IndexByChunkID), arg0, arg1)
 }
 
+// RollbackExecutedBlock mocks base method
+func (m *MockHeaders) RollbackExecutedBlock(arg0 *flow.Header) error {
+	m.ctrl.T.Helper()
+	ret := m.ctrl.Call(m, "RollbackExecutedBlock", arg0)
+	ret0, _ := ret[0].(error)
+	return ret0
+}
+
+// RollbackExecutedBlock indicates an expected call of RollbackExecutedBlock
+func (mr *MockHeadersMockRecorder) RollbackExecutedBlock(arg0 interface{}) *gomock.Call {
+	mr.mock.ctrl.T.Helper()
+	return mr.mock.ctrl.RecordCallWithMethodType(mr.mock, "RollbackExecutedBlock", reflect.TypeOf((*MockHeaders)(nil).RollbackExecutedBlock), arg0)
+}
+
 // Store mocks base method
 func (m *MockHeaders) Store(arg0 *flow.Header) error {
 	m.ctrl.T.Helper()
@@ -502,6 +516,20 @@ func (m *MockCommits) ByBlockID(arg0 flow.Identifier) (flow.StateCommitment, err
 func (mr *MockCommitsMockRecorder) ByBlockID(arg0 interface{}) *gomock.Call {
 	mr.mock.ctrl.T.Helper()
 	return mr.mock.ctrl.RecordCallWithMethodType(mr.mock, "ByBlockID", reflect.TypeOf((*MockCommits)(nil).ByBlockID), arg0)
+}
+
+// RemoveByBlockID mocks base method
+func (m *MockCommits) RemoveByBlockID(arg0 flow.Identifier) error {
+	m.ctrl.T.Helper()
+	ret := m.ctrl.Call(m, "RemoveByBlockID", arg0)
+	ret0, _ := ret[0].(error)
+	return ret0
+}
+
+// RemoveByBlockID indicates an expected call of RemoveByBlockID
+func (mr *MockCommitsMockRecorder) RemoveByBlockID(arg0 interface{}) *gomock.Call {
+	mr.mock.ctrl.T.Helper()
+	return mr.mock.ctrl.RecordCallWithMethodType(mr.mock, "RemoveByBlockID", reflect.TypeOf((*MockCommits)(nil).RemoveByBlockID), arg0)
 }
 
 // Store mocks base method
@@ -747,4 +775,18 @@ func (m *MockTransactionResults) ByBlockIDTransactionIndex(arg0 flow.Identifier,
 func (mr *MockTransactionResultsMockRecorder) ByBlockIDTransactionIndex(arg0, arg1 interface{}) *gomock.Call {
 	mr.mock.ctrl.T.Helper()
 	return mr.mock.ctrl.RecordCallWithMethodType(mr.mock, "ByBlockIDTransactionIndex", reflect.TypeOf((*MockTransactionResults)(nil).ByBlockIDTransactionIndex), arg0, arg1)
+}
+
+// RemoveByBlockID mocks base method
+func (m *MockTransactionResults) RemoveByBlockID(arg0 flow.Identifier) error {
+	m.ctrl.T.Helper()
+	ret := m.ctrl.Call(m, "RemoveByBlockID", arg0)
+	ret0, _ := ret[0].(error)
+	return ret0
+}
+
+// RemoveByBlockID indicates an expected call of RemoveByBlockID
+func (mr *MockTransactionResultsMockRecorder) RemoveByBlockID(arg0 interface{}) *gomock.Call {
+	mr.mock.ctrl.T.Helper()
+	return mr.mock.ctrl.RecordCallWithMethodType(mr.mock, "RemoveByBlockID", reflect.TypeOf((*MockTransactionResults)(nil).RemoveByBlockID), arg0)
 }
