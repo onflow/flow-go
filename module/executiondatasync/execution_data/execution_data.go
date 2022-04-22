6178ccc794a665c7bc01b37031cbb644b1e02d78
Simon Zhu <simon.zsiyan@gmail.com>
2022-04-22 10:12:30 -0700

    define execution_data

---
+++ module/executiondatasync/execution_data/execution_data.go
@@ -0,0 +1,25 @@
+package execution_data
+
+import (
+	"github.com/ipfs/go-cid"
+
+	"github.com/onflow/flow-go/ledger"
+	"github.com/onflow/flow-go/model/flow"
+)
+
+// ChunkExecutionData represents the execution data of a chunk
+type ChunkExecutionData struct {
+	Collection *flow.Collection
+	Events     flow.EventsList
+	TrieUpdate *ledger.TrieUpdate
+}
+
+type ExecutionDataRoot struct {
+	BlockID               flow.Identifier
+	ChunkExecutionDataIDs []cid.Cid
+}
+
+type ExecutionData struct {
+	BlockID             flow.Identifier
+	ChunkExecutionDatas []*ChunkExecutionData
+}
