c90bfa38e0a07a3e79f56684bd3514bfa0e864ac
Khalil Claybon <khalil.claybon@dapperlabs.com>
2022-05-03 17:09:45 -0400

    Update run_test.sh

--- cadence_script_compression/run_test.sh
+++ cadence_script_compression/run_test.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 
-go test ./brotli/main_test.go -v > brotli.out
-go test ./lz4/main_test.go -v > lz4.out
-go test ./snappy/main_test.go -v > snappy.out
-go test ./zstd/datadog_zstd/main_test.go -v > datadog_zstd.out
-go test ./zstd/klauspost_compress/main_test.go -v > klauspost_zstd.out
-go test ./zstd/valyala_gozstd/main_test.go -v > valyala_gozstd.out
+mkdir ./out
+go test ./brotli/main_test.go -v > ./out/brotli.out
+go test ./lz4/main_test.go -v > ./out/lz4.out
+go test ./snappy/main_test.go -v > ./out/snappy.out
+go test ./zstd/datadog_zstd/main_test.go -v > ./out/datadog_zstd.out
+go test ./zstd/klauspost_compress/main_test.go -v > ./out/klauspost_zstd.out
+go test ./zstd/valyala_gozstd/main_test.go -v > ./out/valyala_gozstd.out
