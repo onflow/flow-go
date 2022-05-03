b5c6e67ba31685417272a6d15dd8f646bf9a6d52
Khalil Claybon <khalil.claybon@dapperlabs.com>
2022-05-03 16:16:52 -0400

    script to run all test

---
+++ cadence_script_compression/run_test.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+go test ./brotli/main_test.go -v > brotli.out
+go test ./lz4/main_test.go -v > lz4.out
+go test ./snappy/main_test.go -v > snappy.out
+go test ./zstd/datadog_zstd/main_test.go -v > datadog_zstd.out
+go test ./zstd/klauspost_zstd/main_test.go -v > klauspost_zstd.out
+go test ./zstd/valyala_gozstd/main_test.go -v > valyala_gozstd.out
