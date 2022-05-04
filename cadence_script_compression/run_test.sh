4a418955bfdc0b979e97114ecf416eabdbaa159c
Khalil Claybon <khalil.claybon@dapperlabs.com>
2022-05-03 22:57:48 -0400

    Update run_test.sh

--- cadence_script_compression/run_test.sh
+++ cadence_script_compression/run_test.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-mkdir ./out
-go test ./brotli/main_test.go -v > ./out/brotli.out
-go test ./lz4/main_test.go -v > ./out/lz4.out
-go test ./snappy/main_test.go -v > ./out/snappy.out
-go test ./zstd/datadog_zstd/main_test.go -v > ./out/datadog_zstd.out
-go test ./zstd/klauspost_compress/main_test.go -v > ./out/klauspost_zstd.out
-go test ./zstd/valyala_gozstd/main_test.go -v > ./out/valyala_gozstd.out
+mkdir ./out"$1"
+go test ./brotli/main_test.go -v > ./out"$1"/brotli.out
+go test ./lz4/main_test.go -v > ./out"$1"/lz4.out
+go test ./snappy/main_test.go -v > ./out"$1"/snappy.out
+go test ./zstd/datadog_zstd/main_test.go -v > ./out"$1"/datadog_zstd.out
+go test ./zstd/klauspost_compress/main_test.go -v > ./out"$1"/klauspost_zstd.out
+go test ./zstd/valyala_gozstd/main_test.go -v > ./out"$1"/valyala_gozstd.out
