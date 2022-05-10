ca0d18e1beacd5a5d6feb383707360839395bd57
Yahya <yahya@dapperlabs.com>
2022-05-10 14:46:34 -0700

    adds logging for corrupted nodes

--- insecure/cmd/corrupted_builder.go
+++ insecure/cmd/corrupted_builder.go
@@ -8,6 +8,7 @@ import (
 	"github.com/onflow/flow-go/cmd"
 	"github.com/onflow/flow-go/insecure/corruptible"
 	"github.com/onflow/flow-go/module"
+	"github.com/onflow/flow-go/utils/logging"
 )
 
 // CorruptibleConduitFactoryPort is the port number that gRPC server of the conduit factory of corrupted nodes is listening on.
@@ -36,12 +37,8 @@ func (cnb *CorruptedNodeBuilder) Initialize() error {
 
 func (cnb *CorruptedNodeBuilder) enqueueCorruptibleConduitFactory() {
 	cnb.FlowNodeBuilder.OverrideComponent(cmd.ConduitFactoryComponent, func(node *cmd.NodeConfig) (module.ReadyDoneAware, error) {
-		host, _, err := net.SplitHostPort(node.BindAddr)
-		if err != nil {
-			return nil, fmt.Errorf("could not extract host address: %w", err)
-		}
+		address := net.JoinHostPort(node.Me.Address(), strconv.Itoa(CorruptibleConduitFactoryPort))
 
-		address := net.JoinHostPort(host, strconv.Itoa(CorruptibleConduitFactoryPort))
 		ccf := corruptible.NewCorruptibleConduitFactory(
 			cnb.FlowNodeBuilder.Logger,
 			cnb.FlowNodeBuilder.RootChainID,
@@ -49,6 +46,7 @@ func (cnb *CorruptedNodeBuilder) enqueueCorruptibleConduitFactory() {
 			cnb.FlowNodeBuilder.CodecFactory(), address)
 
 		cnb.FlowNodeBuilder.ConduitFactory = ccf
+		node.Logger.Info().Hex("node_id", logging.ID(node.NodeID)).Str("address", address).Msg("corrupted conduit factory initiated")
 		return ccf, nil
 	})
 
