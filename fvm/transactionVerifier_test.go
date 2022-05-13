922c3675b0241ae6795851e97acaf9113bb985f9
Tarak Ben Youssef <tarak.benyoussef@dapperlabs.com>
2022-05-13 14:23:39 -0500

    add fvm tests for invalid envelope and payload sigs

--- fvm/transactionVerifier_test.go
+++ fvm/transactionVerifier_test.go
@@ -7,6 +7,8 @@ import (
 	"github.com/stretchr/testify/require"
 
 	"github.com/onflow/flow-go/fvm"
+	"github.com/onflow/flow-go/fvm/crypto"
+	"github.com/onflow/flow-go/fvm/errors"
 	"github.com/onflow/flow-go/fvm/programs"
 	"github.com/onflow/flow-go/fvm/state"
 	"github.com/onflow/flow-go/fvm/utils"
@@ -19,32 +21,35 @@ func TestTransactionVerification(t *testing.T) {
 	sth := state.NewStateHolder(state.NewState(ledger))
 	accounts := state.NewAccounts(sth)
 
-	// create an account
-	address := flow.HexToAddress("1234")
-	privKey, err := unittest.AccountKeyDefaultFixture()
-	require.NoError(t, err)
-	err = accounts.Create([]flow.AccountPublicKey{privKey.PublicKey(1000)}, address)
+	// create 2 accounts
+	address1 := flow.HexToAddress("1234")
+	privKey1, err := unittest.AccountKeyDefaultFixture()
 	require.NoError(t, err)
 
-	sig1 := flow.TransactionSignature{
-		Address:     address,
-		SignerIndex: 0,
-		KeyIndex:    0,
-		Signature:   []byte("SIGNATURE"),
-	}
+	err = accounts.Create([]flow.AccountPublicKey{privKey1.PublicKey(1000)}, address1)
+	require.NoError(t, err)
 
-	sig2 := flow.TransactionSignature{
-		Address:     address,
-		SignerIndex: 1,
-		KeyIndex:    0,
-		Signature:   []byte("SIGNATURE"),
-	}
+	address2 := flow.HexToAddress("1235")
+	privKey2, err := unittest.AccountKeyDefaultFixture()
+	require.NoError(t, err)
+
+	err = accounts.Create([]flow.AccountPublicKey{privKey2.PublicKey(1000)}, address2)
+	require.NoError(t, err)
+
+	tx := flow.TransactionBody{}
 
 	t.Run("duplicated authorization signatures", func(t *testing.T) {
-		tx := flow.TransactionBody{}
-		tx.SetProposalKey(address, 0, 0)
-		tx.SetPayer(address)
-		tx.PayloadSignatures = []flow.TransactionSignature{sig1, sig2}
+
+		sig := flow.TransactionSignature{
+			Address:     address1,
+			SignerIndex: 0,
+			KeyIndex:    0,
+		}
+
+		tx.SetProposalKey(address1, 0, 0)
+		tx.SetPayer(address1)
+
+		tx.PayloadSignatures = []flow.TransactionSignature{sig, sig}
 		proc := fvm.Transaction(&tx, 0)
 
 		txVerifier := fvm.NewTransactionSignatureVerifier(1000)
@@ -53,17 +58,95 @@ func TestTransactionVerification(t *testing.T) {
 		require.True(t, strings.Contains(err.Error(), "duplicate signatures are provided for the same key"))
 	})
 	t.Run("duplicated authorization and envelope signatures", func(t *testing.T) {
-		tx := flow.TransactionBody{}
-		tx.SetProposalKey(address, 0, 0)
-		tx.SetPayer(address)
 
-		tx.PayloadSignatures = []flow.TransactionSignature{sig1}
-		tx.EnvelopeSignatures = []flow.TransactionSignature{sig2}
+		sig := flow.TransactionSignature{
+			Address:     address1,
+			SignerIndex: 0,
+			KeyIndex:    0,
+		}
+
+		tx.SetProposalKey(address1, 0, 0)
+		tx.SetPayer(address1)
+
+		tx.PayloadSignatures = []flow.TransactionSignature{sig}
+		tx.EnvelopeSignatures = []flow.TransactionSignature{sig}
 		proc := fvm.Transaction(&tx, 0)
 
 		txVerifier := fvm.NewTransactionSignatureVerifier(1000)
 		err = txVerifier.Process(nil, &fvm.Context{}, proc, sth, programs.NewEmptyPrograms())
 		require.Error(t, err)
 		require.True(t, strings.Contains(err.Error(), "duplicate signatures are provided for the same key"))
+	})
+
+	t.Run("invalid envelope signature", func(t *testing.T) {
+		tx.SetProposalKey(address1, 0, 0)
+		tx.SetPayer(address2)
+
+		// assign a valid payload signature
+		hasher1, err := crypto.NewPrefixedHashing(privKey1.HashAlgo, flow.TransactionTagString)
+		require.NoError(t, err)
+		validSig, err := privKey1.PrivateKey.Sign(tx.PayloadMessage(), hasher1) // valid signature
+		require.NoError(t, err)
+
+		sig1 := flow.TransactionSignature{
+			Address:     address1,
+			SignerIndex: 0,
+			KeyIndex:    0,
+			Signature:   validSig,
+		}
+
+		sig2 := flow.TransactionSignature{
+			Address:     address2,
+			SignerIndex: 0,
+			KeyIndex:    0,
+			// invalid signature
+		}
+
+		tx.PayloadSignatures = []flow.TransactionSignature{sig1}
+		tx.EnvelopeSignatures = []flow.TransactionSignature{sig2}
+
+		proc := fvm.Transaction(&tx, 0)
+		txVerifier := fvm.NewTransactionSignatureVerifier(1000)
+		err = txVerifier.Process(nil, &fvm.Context{}, proc, sth, programs.NewEmptyPrograms())
+		require.Error(t, err)
+
+		var envelopeError *errors.InvalidEnvelopeSignatureError
+		require.ErrorAs(t, err, &envelopeError)
+	})
+
+	t.Run("invalid payload signature", func(t *testing.T) {
+		tx.SetProposalKey(address1, 0, 0)
+		tx.SetPayer(address2)
+
+		sig1 := flow.TransactionSignature{
+			Address:     address1,
+			SignerIndex: 0,
+			KeyIndex:    0,
+			// invalid signature
+		}
+
+		// assign a valid envelope signature
+		hasher2, err := crypto.NewPrefixedHashing(privKey2.HashAlgo, flow.TransactionTagString)
+		require.NoError(t, err)
+		validSig, err := privKey2.PrivateKey.Sign(tx.EnvelopeMessage(), hasher2) // valid signature
+		require.NoError(t, err)
+
+		sig2 := flow.TransactionSignature{
+			Address:     address2,
+			SignerIndex: 0,
+			KeyIndex:    0,
+			Signature:   validSig,
+		}
+
+		tx.PayloadSignatures = []flow.TransactionSignature{sig1}
+		tx.EnvelopeSignatures = []flow.TransactionSignature{sig2}
+
+		proc := fvm.Transaction(&tx, 0)
+		txVerifier := fvm.NewTransactionSignatureVerifier(1000)
+		err = txVerifier.Process(nil, &fvm.Context{}, proc, sth, programs.NewEmptyPrograms())
+		require.Error(t, err)
+
+		var payloadError *errors.InvalidPayloadSignatureError
+		require.ErrorAs(t, err, &payloadError)
 	})
 }
