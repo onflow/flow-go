c7d6828e553d67bf9672e34bf91a38f658d0c111
gomisha <misha@gomisha.com>
2022-05-16 16:38:48 -0500

    CI debug - test-setup.sh modified for debugging

--- tools/test_monitor/test-setup.sh
+++ tools/test_monitor/test-setup.sh
@@ -1,25 +1,27 @@
 #!/usr/bin/env bash
 
+echo "test-setup start"
+
 set -e
 shopt -s extglob
 
-make crypto_setup_gopath
+# make crypto_setup_gopath
 
 echo "test category (test-setup):" $TEST_CATEGORY
 
 case $TEST_CATEGORY in
-  integration-ghost|integration-mvp|integration-network|integration-epochs|integration-access|integration-collection|integration-consensus|integration-execution|integration-verification)
-    make docker-build-flow
-  ;;
-  integration-bft)
-    make docker-build-flow-corrupted docker-build-flow
-  ;;
-  unit)
-    make install-mock-generators
-    make generate-mocks
-  ;;
-  unit-crypto)
-    make crypto_setup_tests
-  ;;
+ integration-ghost|integration-mvp|integration-network|integration-epochs|integration-access|integration-collection|integration-consensus|integration-execution|integration-verification)
+#    make docker-build-flow
+ ;;
+ integration-bft)
+#    make docker-build-flow-corrupted docker-build-flow
+ ;;
+ unit)
+#    make install-mock-generators
+#    make generate-mocks
+ ;;
+ unit-crypto)
+#    make crypto_setup_tests
+ ;;
 esac
 
