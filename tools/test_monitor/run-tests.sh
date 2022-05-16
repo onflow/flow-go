84c22e68604c5ec61612797db2a014d7f57f0eaa
gomisha <misha@gomisha.com>
2022-05-16 17:36:12 -0500

    CI debug - run-tests.sh modified to send echo statements to standard error

--- tools/test_monitor/run-tests.sh
+++ tools/test_monitor/run-tests.sh
@@ -5,13 +5,13 @@
 set -e
 shopt -s extglob
 
-echo "test category (run-tests):" $TEST_CATEGORY
+echo "test category (run-tests):" $TEST_CATEGORY>&2
 
 # run tests and process results
 
 if [[ $TEST_CATEGORY =~ integration-(bft|ghost|mvp|network|epochs|access|collection|consensus|execution|verification)$ ]]
 then
-  echo "killing and removing orphaned containers from previous run"
+  echo "killing and removing orphaned containers from previous run">&2
 #    # kill and remove orphaned containers from previous run
 #    containers=$(docker ps -a -q)
 #
@@ -21,25 +21,25 @@ then
 #    fi
 #
 
-  echo "running $TEST_CATEGORY tests"
+  echo "running $TEST_CATEGORY tests">&2
 
 #    make -C integration -s ${BASH_REMATCH[1]}-tests
 else
     case $TEST_CATEGORY in
         unit)
-          echo "running unittest-main"
+          echo "running unittest-main">&2
 #          make -s unittest-main
         ;;
         unit-crypto)
-          echo "running crypto unit tests"
+          echo "running crypto unit tests">&2
 #          make -C crypto -s test
         ;;
         unit-integration)
-          echo "running integration unit tests"
+          echo "running integration unit tests">&2
 #          make -C integration -s test
         ;;
         *)
-          echo "unrecognized test category (run-tests):" $TEST_CATEGORY
+          echo "unrecognized test category (run-tests):" $TEST_CATEGORY>&2
           exit 1
         ;;
     esac
