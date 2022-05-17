88d67ce2129847bd191a7057a9c25897c4d1b622
GetElastech <codesubmit@getelastech.com>
2022-05-17 14:00:21 +0000

    [2063] Minor access node style fix Author: Miklos Szegedi

--- cmd/access/main.go
+++ cmd/access/main.go
@@ -15,7 +15,7 @@ func main() {
 	}
 
 	// Please use an observer for unstaked workloads going forward
-	var builder nodebuilder.AccessNodeBuilder = nodebuilder.NewStakedAccessNodeBuilder(anb)
+	builder := nodebuilder.NewStakedAccessNodeBuilder(anb)
 
 	if err := builder.Initialize(); err != nil {
 		anb.Logger.Fatal().Err(err).Send()
