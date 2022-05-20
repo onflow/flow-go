22d429356807466c3fce2d9ba4ad6417896b03af
Leo Zhang (zhangchiqing) <zhangchiqing@gmail.com>
2022-05-20 08:49:03 -0700

    comments

--- model/flow/assignment/sort.go
+++ model/flow/assignment/sort.go
@@ -5,7 +5,8 @@ import (
 	"github.com/onflow/flow-go/model/flow/order"
 )
 
-// FromIdentifierLists creates assignmentlist with canonical ordering from a list of IdentifierList.
+// FromIdentifierLists creates a `flow.AssignmentList` with canonical ordering from
+// the given `identifierLists`.
 func FromIdentifierLists(identifierLists []flow.IdentifierList) flow.AssignmentList {
 	assignments := make(flow.AssignmentList, 0, len(identifierLists))
 	// in place sort to order the assignment in canonical order
