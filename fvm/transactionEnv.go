001695e569cd22b22c3efb88dda876774aa06bd2
ramtinms <ramtin@axiomzen.co>
2022-05-26 15:13:04 -0700

    account freezing disabled by the context

--- fvm/transactionEnv.go
+++ fvm/transactionEnv.go
@@ -744,6 +744,10 @@ func (e *TransactionEnv) MemoryUsed() uint64 {
 
 func (e *TransactionEnv) SetAccountFrozen(address common.Address, frozen bool) error {
 
+	if !e.ctx.AccountFreezeEnabled {
+		return errors.NewOperationNotSupportedError("SetAccountFrozen")
+	}
+
 	flowAddress := flow.Address(address)
 
 	if flowAddress == e.ctx.Chain.ServiceAddress() {
