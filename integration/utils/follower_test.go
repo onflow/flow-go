f57e0239e640f149b145b3b8730dd6fd63cdc549
Alexey Ivanov <SaveTheRbtz@GMail.com>
2022-05-26 18:33:26 -0700

    imporve tests for the follower

--- integration/utils/follower_test.go
+++ integration/utils/follower_test.go
@@ -5,18 +5,33 @@ import (
 	"testing"
 	"time"
 
+	flowsdk "github.com/onflow/flow-go-sdk"
 	"github.com/onflow/flow-go-sdk/client"
+	"github.com/onflow/flow-go/utils/unittest"
 	"github.com/stretchr/testify/require"
 )
 
-// TestFollower creates new follower with a random block height and stops it.
+// TestTxFollower creates new follower with a fixed block height and stops it.
 // TODO(rbtz): test against a mock client
-func TestFollower(t *testing.T) {
+func TestTxFollower(t *testing.T) {
 	f, err := NewTxFollower(context.Background(),
 		&client.Client{},
 		WithBlockHeight(2),
 		WithInteval(1*time.Hour),
 	)
 	require.NoError(t, err)
+	f.Stop()
+}
+
+// TestNopTxFollower creates a new follower with a fixed block height and
+// verifies that it does not block.
+func TestNopTxFollower(t *testing.T) {
+	f, err := NewNopTxFollower(context.Background(),
+		&client.Client{},
+		WithBlockHeight(1),
+		WithInteval(1*time.Hour),
+	)
+	require.NoError(t, err)
+	unittest.AssertClosesBefore(t, f.CompleteChanByID(flowsdk.Identifier{}), 1*time.Second)
 	f.Stop()
 }
