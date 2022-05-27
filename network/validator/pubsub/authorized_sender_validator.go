034bd421915236756cd610a92a0c5b0b640dba4e
Khalil Claybon <khalil.claybon@dapperlabs.com>
2022-05-27 09:40:05 -0400

    fix codec import

--- network/validator/pubsub/authorized_sender_validator.go
+++ network/validator/pubsub/authorized_sender_validator.go
@@ -100,15 +100,15 @@ func getRoles(channel network.Channel, msgTypeCode uint8) (flow.RoleList, error)
 	}
 
 	// cluster channels have a dynamic channel name
-	if msgTypeCode == cborcodec.CodeClusterBlockProposal ||
-		msgTypeCode == cborcodec.CodeClusterBlockVote ||
-		msgTypeCode == cborcodec.CodeClusterBlockResponse {
+	if msgTypeCode == codec.CodeClusterBlockProposal ||
+		msgTypeCode == codec.CodeClusterBlockVote ||
+		msgTypeCode == codec.CodeClusterBlockResponse {
 		return channels.ClusterChannelRoles(channel), nil
 	}
 
 	// check if msg is cluster sync request. The sync request message is used on
 	// sync-cluster prefixed channels as well as the SyncCommittee channel.
-	if msgTypeCode == cborcodec.CodeSyncRequest && channels.IsClusterChannel(channel) {
+	if msgTypeCode == codec.CodeSyncRequest && channels.IsClusterChannel(channel) {
 		return channels.ClusterChannelRoles(channel), nil
 	}
 
