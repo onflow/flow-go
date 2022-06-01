1236aff95c8175e7d1a8637cb31ba741fcbe93a2
Janez Podhostnik <janez.podhostnik@gmail.com>
2022-06-01 21:04:11 +0200

    fix unit tests

--- engine/execution/state/bootstrap/bootstrap_test.go
+++ engine/execution/state/bootstrap/bootstrap_test.go
@@ -47,7 +47,7 @@ func TestBootstrapLedger(t *testing.T) {
 }
 
 func TestBootstrapLedger_ZeroTokenSupply(t *testing.T) {
-	expectedStateCommitmentBytes, _ := hex.DecodeString("cae2f2d6c53582503dad30dba8a8bba098ff8654d19b820a49e1961c1f459a41")
+	expectedStateCommitmentBytes, _ := hex.DecodeString("349715decb402a5f756cbc37372554cb93191458c6e9293a9655464b450754ca")
 	expectedStateCommitment, err := flow.ToStateCommitment(expectedStateCommitmentBytes)
 	require.NoError(t, err)
 
--- utils/unittest/execution_state.go
+++ utils/unittest/execution_state.go
@@ -24,7 +24,7 @@ const ServiceAccountPrivateKeySignAlgo = crypto.ECDSAP256
 const ServiceAccountPrivateKeyHashAlgo = hash.SHA2_256
 
 // Pre-calculated state commitment with root account with the above private key
-const GenesisStateCommitmentHex = "5b8f8283d5e719672cb53c0e20a822bf0782f4345d09df076c14fba4d9e21da0"
+const GenesisStateCommitmentHex = "cfd13767357a5ab910a499b1dcf10b919be205c1bb63593db648c367eee7a71d"
 
 var GenesisStateCommitment flow.StateCommitment
 
