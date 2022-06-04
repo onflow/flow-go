6e691d37c7f86a9d4f784a714e374ea1faefb6ab
GetElastech <codesubmit@getelastech.com>
2022-06-04 04:31:47 +0000

    [2063][2064][2066][2075][State Sync] Final clean up of access node codebase Author: Miklos Szegedi

--- cmd/access/main.go
+++ cmd/access/main.go
@@ -5,25 +5,22 @@ import (
 )
 
 func main() {
-	anb := nodebuilder.FlowAccessNode() // use the generic Access Node builder till it is determined if this is a staked AN or an unstaked AN
+	builder := nodebuilder.FlowAccessNode() // use the generic Access Node builder till it is determined if this is a staked AN or an unstaked AN
 
-	anb.PrintBuildVersionDetails()
+	builder.PrintBuildVersionDetails()
 
 	// parse all the command line args
-	if err := anb.ParseFlags(); err != nil {
-		anb.Logger.Fatal().Err(err).Send()
+	if err := builder.ParseFlags(); err != nil {
+		builder.Logger.Fatal().Err(err).Send()
 	}
 
-	// Please use an observer for unstaked workloads going forward
-	builder := nodebuilder.NewStakedAccessNodeBuilder(anb)
-
 	if err := builder.Initialize(); err != nil {
-		anb.Logger.Fatal().Err(err).Send()
+		builder.Logger.Fatal().Err(err).Send()
 	}
 
 	node, err := builder.Build()
 	if err != nil {
-		anb.Logger.Fatal().Err(err).Send()
+		builder.Logger.Fatal().Err(err).Send()
 	}
 	node.Run()
 }
