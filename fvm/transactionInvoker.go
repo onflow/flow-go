e4fcea92cfc5331a36269ce2f2072acba07ab771
Maks Pawlak <120831+m4ksio@users.noreply.github.com>
2022-06-04 00:20:22 -0700

    Add test for handling encoding errors

--- fvm/transactionInvoker.go
+++ fvm/transactionInvoker.go
@@ -12,6 +12,7 @@ import (
 	"github.com/onflow/cadence/runtime/common"
 	"github.com/onflow/cadence/runtime/interpreter"
 	"github.com/onflow/cadence/runtime/sema"
+	"github.com/onflow/flow-go/fvm/handler"
 	"github.com/opentracing/opentracing-go"
 	traceLog "github.com/opentracing/opentracing-go/log"
 	"github.com/rs/zerolog"
@@ -24,14 +25,30 @@ import (
 	"github.com/onflow/flow-go/module/trace"
 )
 
-type TransactionInvoker struct {
-	logger zerolog.Logger
+type TransactionInvokerOption func(invoker *TransactionInvoker)
+
+func WithFlowEventHandleOptions(feho ...handler.FlowEventHandlerOption) func(inv *TransactionInvoker) {
+	return func(inv *TransactionInvoker) {
+		inv.feho = feho
+	}
+
 }
 
-func NewTransactionInvoker(logger zerolog.Logger) *TransactionInvoker {
-	return &TransactionInvoker{
+type TransactionInvoker struct {
+	logger zerolog.Logger
+	feho   []handler.FlowEventHandlerOption
+}
+
+func NewTransactionInvoker(logger zerolog.Logger, options ...TransactionInvokerOption) *TransactionInvoker {
+	invoker := &TransactionInvoker{
 		logger: logger,
 	}
+
+	for _, o := range options {
+		o(invoker)
+	}
+
+	return invoker
 }
 
 func (i *TransactionInvoker) Process(
@@ -88,7 +105,7 @@ func (i *TransactionInvoker) Process(
 		sth.SetActiveState(parentState)
 	}()
 
-	env, err := NewTransactionEnvironment(*ctx, vm, sth, programs, proc.Transaction, proc.TxIndex, span)
+	env, err := NewTransactionEnvironment(*ctx, vm, sth, programs, proc.Transaction, proc.TxIndex, span, i.feho...)
 	if err != nil {
 		return fmt.Errorf("error creating new environment: %w", err)
 	}
