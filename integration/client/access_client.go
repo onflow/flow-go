75e7cc1532fb4b10fd540f2a1f2c8910670fd320
Bastian Müller <bastian@axiomzen.co>
2022-06-07 17:35:02 -0700

    update to Cadence v0.24.0 and Go SDK 0.26.0

--- integration/client/access_client.go
+++ integration/client/access_client.go
@@ -6,9 +6,6 @@ package client
 import (
 	"context"
 
-	//sdk "github.com/onflow/flow-go-sdk"
-	//sdkConvert "github.com/onflow/flow-go-sdk/client/convert"
-
 	"google.golang.org/grpc"
 
 	"github.com/onflow/flow/protobuf/go/flow/access"
