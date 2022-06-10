fdb5c242aa87a585612dcb2ff7fa549027ff17b7
Khalil Claybon <khalil.claybon@dapperlabs.com>
2022-06-10 17:46:45 -0400

    remove unused JSON codec

--- network/codec/roundTripHeader_test.go
+++ network/codec/roundTripHeader_test.go
@@ -8,7 +8,6 @@ import (
 
 	"github.com/onflow/flow-go/model/messages"
 	"github.com/onflow/flow-go/network"
-	jsoncodec "github.com/onflow/flow-go/network/codec/json"
 	"github.com/onflow/flow-go/utils/unittest"
 )
 
@@ -34,11 +33,6 @@ func roundTripHeaderViaCodec(t *testing.T, codec network.Codec) {
 	messageHeader := fmt.Sprintf("- .Header=%+v\n", message.Header)
 	decodedHeader := fmt.Sprintf("- .Header=%+v\n", decoded.Header)
 	assert.Equal(t, messageHeader, decodedHeader)
-}
-
-func TestRoundTripHeaderViaJSON(t *testing.T) {
-	codec := jsoncodec.NewCodec()
-	roundTripHeaderViaCodec(t, codec)
 }
 
 func TestRoundTripHeaderViaCBOR(t *testing.T) {
