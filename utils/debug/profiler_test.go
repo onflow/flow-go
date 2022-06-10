031b596af68465a1ebd59aecc3c2afebec2e3f22
Alexey Ivanov <SaveTheRbtz@GMail.com>
2022-06-10 14:06:25 -0700

    review comments + less verbose logging

--- utils/debug/profiler_test.go
+++ utils/debug/profiler_test.go
@@ -5,31 +5,31 @@ import (
 	"testing"
 	"time"
 
-	"github.com/rs/zerolog/log"
+	"github.com/rs/zerolog"
 	"github.com/stretchr/testify/require"
 
 	"github.com/onflow/flow-go/utils/debug"
+	"github.com/onflow/flow-go/utils/unittest"
 )
 
 func TestProfiler(t *testing.T) {
 	t.Parallel()
 	t.Run("profilerEnabled", func(t *testing.T) {
-		tempDir, err := os.MkdirTemp("", "profiles-")
-		require.NoError(t, err)
-		defer os.RemoveAll(tempDir)
-
-		p, err := debug.NewAutoProfiler(log.Logger, tempDir, time.Millisecond*100, time.Millisecond*100, true)
-		require.NoError(t, err)
-
-		<-p.Ready()
-		log.Info().Str("dir", tempDir).Msg("profiler ready")
-
-		require.Eventuallyf(t, func() bool {
-			dirEnts, err := os.ReadDir(tempDir)
+		unittest.RunWithTempDir(t, func(tempDir string) {
+			p, err := debug.NewAutoProfiler(zerolog.Nop(), tempDir, time.Millisecond*100, time.Millisecond*100, true)
 			require.NoError(t, err)
-			return len(dirEnts) > 4
-		}, time.Second*5, time.Millisecond*100, "profiler did not generate any profiles in %v", tempDir)
 
-		<-p.Done()
+			unittest.AssertClosesBefore(t, p.Ready(), 5*time.Second)
+
+			t.Logf("profiler ready %s", tempDir)
+
+			require.Eventuallyf(t, func() bool {
+				dirEnts, err := os.ReadDir(tempDir)
+				require.NoError(t, err)
+				return len(dirEnts) > 4
+			}, time.Second*5, time.Millisecond*100, "profiler did not generate any profiles in %v", tempDir)
+
+			unittest.AssertClosesBefore(t, p.Done(), 5*time.Second)
+		})
 	})
 }
