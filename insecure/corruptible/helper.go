87298533b8aa8c61a544556d87d1451ce501e1f9
Yahya <yahya@dapperlabs.com>
2022-06-20 10:11:24 -0700

    adds comments to mockAttackerObserveClient

--- insecure/corruptible/helper.go
+++ insecure/corruptible/helper.go
@@ -14,18 +14,6 @@ type mockAttackerObserveClient struct {
 	closed         chan interface{}
 }
 
-func (m *mockAttackerObserveClient) SetHeader(_ metadata.MD) error {
-	panic("not implemented")
-}
-
-func (m *mockAttackerObserveClient) SendHeader(_ metadata.MD) error {
-	panic("not implemented")
-}
-
-func (m *mockAttackerObserveClient) SetTrailer(_ metadata.MD) {
-	panic("not implemented")
-}
-
 func newMockAttackerObserverClient() *mockAttackerObserveClient {
 	return &mockAttackerObserveClient{
 		incomingBuffer: make(chan *insecure.Message),
@@ -33,11 +21,31 @@ func newMockAttackerObserverClient() *mockAttackerObserveClient {
 	}
 }
 
+// mockAttackerObserveClient must implement CorruptibleConduitFactory_ProcessAttackerMessageClient interface.
+var _ insecure.CorruptibleConduitFactory_ProcessAttackerMessageClient = &mockAttackerObserveClient{}
+
+// SetHeader is only added to satisfy the interface implementation.
+func (m *mockAttackerObserveClient) SetHeader(_ metadata.MD) error {
+	panic("not implemented")
+}
+
+// SendHeader is only added to satisfy the interface implementation.
+func (m *mockAttackerObserveClient) SendHeader(_ metadata.MD) error {
+	panic("not implemented")
+}
+
+// SetTrailer is only added to satisfy the interface implementation.
+func (m *mockAttackerObserveClient) SetTrailer(_ metadata.MD) {
+	panic("not implemented")
+}
+
+// Send puts the incoming message into inbound buffer of the attacker.
 func (m *mockAttackerObserveClient) Send(in *insecure.Message) error {
 	m.incomingBuffer <- in
 	return nil
 }
 
+// CloseAndRecv notifies the attacker that the client is closed.
 func (m *mockAttackerObserveClient) CloseAndRecv() (*empty.Empty, error) {
 	close(m.closed)
 	return &empty.Empty{}, nil
