59a4a2cb032c8360269a0772f587dd7b6e79f535
ramtinms <ramtin@axiomzen.co>
2022-06-21 15:55:30 -0700

    fix linter

--- ledger/complete/ledger.go
+++ ledger/complete/ledger.go
@@ -416,7 +416,7 @@ func (l *Ledger) ExportCheckpointAt(
 	// running post checkpoint reporters
 	for i, reporter := range postCheckpointReporters {
 		l.logger.Info().Msgf("running a post-checkpoint generation reporter: %s, (%v/%v)", reporter.Name(), i, len(postCheckpointReporters))
-		err := runReport(reporter, payloads, statecommitment,  l.logger)
+		err := runReport(reporter, payloads, statecommitment, l.logger)
 		if err != nil {
 			return ledger.State(hash.DummyHash), err
 		}
