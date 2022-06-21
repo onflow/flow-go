02757ab20d7407f8998919080a7e7cc7d9af0650
Janez Podhostnik <janez.podhostnik@gmail.com>
2022-06-21 21:18:25 +0100

    update state tests

--- fvm/state/state_test.go
+++ fvm/state/state_test.go
@@ -1,6 +1,7 @@
 package state_test
 
 import (
+	"math"
 	"testing"
 	"unicode/utf8"
 
@@ -8,6 +9,7 @@ import (
 
 	"github.com/onflow/atree"
 
+	"github.com/onflow/flow-go/fvm/meter/weighted"
 	"github.com/onflow/flow-go/fvm/state"
 	"github.com/onflow/flow-go/fvm/utils"
 )
@@ -84,120 +86,126 @@ func TestState_ChildMergeFunctionality(t *testing.T) {
 
 }
 
-//
-//func TestState_InteractionMeasuring(t *testing.T) {
-//	view := utils.NewSimpleView()
-//	st := state.NewState(view)
-//
-//	key := "key1"
-//	value := createByteArray(1)
-//	err := st.Set("address", "controller", key, value)
-//	keySize := uint64(len("address") + len("controller") + len(key))
-//	size := keySize + uint64(len(value))
-//	require.NoError(t, err)
-//	require.Equal(t, uint64(0), st.ReadCounter)
-//	require.Equal(t, uint64(0), st.TotalBytesRead)
-//	require.Equal(t, uint64(1), st.WriteCounter)
-//	require.Equal(t, size, st.TotalBytesWritten)
-//
-//	// should read from the delta
-//	// should not impact totalBytesRead
-//	v, err := st.Get("address", "controller", key)
-//	require.NoError(t, err)
-//	require.Equal(t, v, value)
-//	require.Equal(t, uint64(0), st.TotalBytesRead)
-//
-//	// non existing key
-//	// should be counted towards reading from the ledger
-//	key2 := "key2"
-//	_, err = st.Get("address", "controller", key2)
-//	require.NoError(t, err)
-//	require.Equal(t, keySize, st.TotalBytesRead)
-//}
-//
-//func TestState_MaxValueSize(t *testing.T) {
-//	view := utils.NewSimpleView()
-//	st := state.NewState(view, state.WithMaxValueSizeAllowed(6))
-//
-//	// update should pass
-//	value := createByteArray(5)
-//	err := st.Set("address", "controller", "key", value)
-//	require.NoError(t, err)
-//
-//	// update shouldn't pass
-//	value = createByteArray(7)
-//	err = st.Set("address", "controller", "key", value)
-//	require.Error(t, err)
-//}
-//
-//func TestState_MaxKeySize(t *testing.T) {
-//	view := utils.NewSimpleView()
-//	st := state.NewState(view, state.WithMaxKeySizeAllowed(6))
-//
-//	// read
-//	_, err := st.Get("1", "2", "3")
-//	require.NoError(t, err)
-//
-//	// read
-//	_, err = st.Get("123", "234", "345")
-//	require.Error(t, err)
-//
-//	// update
-//	err = st.Set("1", "2", "3", []byte{})
-//	require.NoError(t, err)
-//
-//	// read
-//	err = st.Set("123", "234", "345", []byte{})
-//	require.Error(t, err)
-//
-//}
-//
-//func TestState_MaxInteraction(t *testing.T) {
-//	view := utils.NewSimpleView()
-//	st := state.NewState(view, state.WithMaxInteractionSizeAllowed(12))
-//
-//	// read - interaction 3
-//	_, err := st.Get("1", "2", "3")
-//	require.Equal(t, st.InteractionUsed(), uint64(3))
-//	require.NoError(t, err)
-//
-//	// read - interaction 12
-//	_, err = st.Get("123", "234", "345")
-//	require.Equal(t, st.InteractionUsed(), uint64(12))
-//	require.NoError(t, err)
-//
-//	// read - interaction 21
-//	_, err = st.Get("234", "345", "456")
-//	require.Equal(t, st.InteractionUsed(), uint64(21))
-//	require.Error(t, err)
-//
-//	st = state.NewState(view, state.WithMaxInteractionSizeAllowed(9))
-//	stChild := st.NewChild()
-//
-//	// update - 0
-//	err = stChild.Set("1", "2", "3", []byte{'A'})
-//	require.NoError(t, err)
-//	require.Equal(t, st.InteractionUsed(), uint64(0))
-//
-//	// commit
-//	err = st.MergeState(stChild, true)
-//	require.NoError(t, err)
-//	require.Equal(t, st.InteractionUsed(), uint64(4))
-//
-//	// read - interaction 4 (already in read cache)
-//	_, err = st.Get("1", "2", "3")
-//	require.NoError(t, err)
-//	require.Equal(t, st.InteractionUsed(), uint64(4))
-//
-//	// read - interaction 7
-//	_, err = st.Get("2", "3", "4")
-//	require.NoError(t, err)
-//	require.Equal(t, st.InteractionUsed(), uint64(7))
-//
-//	// read - interaction 10
-//	_, err = st.Get("3", "4", "5")
-//	require.Error(t, err)
-//}
+func TestState_InteractionMeasuring(t *testing.T) {
+	view := utils.NewSimpleView()
+	mh := weighted.NewMeteringHandler(0, 0, math.MaxUint64)
+	m := weighted.NewMeter(mh)
+	st := state.NewState(view, state.WithMeter(m))
+
+	key := "key1"
+	value := createByteArray(1)
+	err := st.Set("address", "controller", key, value)
+	keySize := uint64(len("address") + len("controller") + len(key))
+	size := keySize + uint64(len(value))
+	require.NoError(t, err)
+	require.Equal(t, uint64(0), m.ReadCounter())
+	require.Equal(t, uint64(0), m.TotalBytesRead())
+	require.Equal(t, uint64(1), m.WriteCounter())
+	require.Equal(t, size, m.TotalBytesWritten())
+
+	// should read from the delta
+	// should not impact totalBytesRead
+	v, err := st.Get("address", "controller", key)
+	require.NoError(t, err)
+	require.Equal(t, v, value)
+	require.Equal(t, uint64(0), m.TotalBytesRead())
+
+	// non existing key
+	// should be counted towards reading from the ledger
+	key2 := "key2"
+	_, err = st.Get("address", "controller", key2)
+	require.NoError(t, err)
+	require.Equal(t, keySize, m.TotalBytesRead())
+}
+
+func TestState_MaxValueSize(t *testing.T) {
+	view := utils.NewSimpleView()
+	st := state.NewState(view, state.WithMaxValueSizeAllowed(6))
+
+	// update should pass
+	value := createByteArray(5)
+	err := st.Set("address", "controller", "key", value)
+	require.NoError(t, err)
+
+	// update shouldn't pass
+	value = createByteArray(7)
+	err = st.Set("address", "controller", "key", value)
+	require.Error(t, err)
+}
+
+func TestState_MaxKeySize(t *testing.T) {
+	view := utils.NewSimpleView()
+	st := state.NewState(view, state.WithMaxKeySizeAllowed(6))
+
+	// read
+	_, err := st.Get("1", "2", "3")
+	require.NoError(t, err)
+
+	// read
+	_, err = st.Get("123", "234", "345")
+	require.Error(t, err)
+
+	// update
+	err = st.Set("1", "2", "3", []byte{})
+	require.NoError(t, err)
+
+	// read
+	err = st.Set("123", "234", "345", []byte{})
+	require.Error(t, err)
+
+}
+
+func TestState_MaxInteraction(t *testing.T) {
+
+	view := utils.NewSimpleView()
+	mh := weighted.NewMeteringHandler(0, 0, 12)
+	m := weighted.NewMeter(mh)
+	st := state.NewState(view, state.WithMeter(m))
+
+	// read - interaction 3
+	_, err := st.Get("1", "2", "3")
+	require.Equal(t, m.TotalInteractionUsed(), uint64(3))
+	require.NoError(t, err)
+
+	// read - interaction 12
+	_, err = st.Get("123", "234", "345")
+	require.Equal(t, m.TotalInteractionUsed(), uint64(12))
+	require.NoError(t, err)
+
+	// read - interaction 21
+	_, err = st.Get("234", "345", "456")
+	require.Equal(t, m.TotalInteractionUsed(), uint64(21))
+	require.Error(t, err)
+
+	mh = weighted.NewMeteringHandler(0, 0, 9)
+	m = weighted.NewMeter(mh)
+	st = state.NewState(view, state.WithMeter(m))
+	stChild := st.NewChild()
+
+	// update - 0
+	err = stChild.Set("1", "2", "3", []byte{'A'})
+	require.NoError(t, err)
+	require.Equal(t, m.TotalInteractionUsed(), uint64(0))
+
+	// commit
+	err = st.MergeState(stChild)
+	require.NoError(t, err)
+	require.Equal(t, m.TotalInteractionUsed(), uint64(4))
+
+	// read - interaction 4 (already in read cache)
+	_, err = st.Get("1", "2", "3")
+	require.NoError(t, err)
+	require.Equal(t, m.TotalInteractionUsed(), uint64(4))
+
+	// read - interaction 7
+	_, err = st.Get("2", "3", "4")
+	require.NoError(t, err)
+	require.Equal(t, m.TotalInteractionUsed(), uint64(7))
+
+	// read - interaction 10
+	_, err = st.Get("3", "4", "5")
+	require.Error(t, err)
+}
 
 func TestState_IsFVMStateKey(t *testing.T) {
 	require.True(t, state.IsFVMStateKey("", "", "uuid"))
