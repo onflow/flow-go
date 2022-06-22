a59d74f0650100d4edd5c7dc3db7c91e61823e0a
Janez Podhostnik <janez.podhostnik@gmail.com>
2022-06-22 18:57:34 +0100

    cleanup

--- engine/execution/computation/computer/computer.go
+++ engine/execution/computation/computer/computer.go
@@ -3,6 +3,7 @@ package computer
 import (
 	"context"
 	"fmt"
+	"math"
 	"runtime"
 	"sync"
 	"time"
@@ -64,7 +65,8 @@ func SystemChunkContext(vmCtx fvm.Context, logger zerolog.Logger) fvm.Context {
 		fvm.WithTransactionProcessors(fvm.NewTransactionInvoker(logger)),
 		fvm.WithMaxStateInteractionSize(SystemChunkLedgerIntractionLimit),
 		fvm.WithEventCollectionSizeLimit(SystemChunkEventCollectionMaxSize),
-		fvm.WithLoadContextFromState(false),
+		fvm.WithLoadContextFromState(false), // disable reading the memory limit (and computation/memory weights) from the state
+		fvm.WithMemoryLimit(math.MaxUint64), // and set the memory limit to the maximum
 	)
 }
 
