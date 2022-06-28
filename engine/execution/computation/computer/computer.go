6711c5f30089caf225ac45f1e8ac41db6f7642bf
Kay-Zee <kan@axiomzen.co>
2022-06-27 21:41:42 -0700

    Add detailed debug logging in executeBlock

--- engine/execution/computation/computer/computer.go
+++ engine/execution/computation/computer/computer.go
@@ -175,14 +175,23 @@ func (e *blockComputer) executeBlock(
 		},
 	}
 
+	blockID := logging.Entity(block)
+
 	go func() {
 		bc.Run()
 		wg.Done()
+		e.log.Debug().
+			Hex("block_id", blockID).
+			Msg("block committer done")
 	}()
 
 	go func() {
 		eh.Run()
 		wg.Done()
+		e.log.Debug().
+			Hex("block_id", blockID).
+			Msg("event hasher done")
+
 	}()
 
 	collectionIndex := 0
@@ -193,17 +202,34 @@ func (e *blockComputer) executeBlock(
 		if err != nil {
 			return nil, fmt.Errorf("failed to execute collection at txIndex %v: %w", txIndex, err)
 		}
+		collectionID := collection.Guarantee.CollectionID[:]
+		e.log.Debug().
+			Hex("block_id", blockID).
+			Hex("collection_id", collectionID).
+			Msg("collection executed")
 		bc.Commit(colView)
+		e.log.Debug().
+			Hex("block_id", blockID).
+			Hex("collection_id", collectionID).
+			Msg("collection committed")
 		eh.Hash(res.Events[i])
+		e.log.Debug().
+			Hex("block_id", blockID).
+			Hex("collection_id", collectionID).
+			Msg("collection events hashed")
 		err = stateView.MergeView(colView)
 		if err != nil {
 			return nil, fmt.Errorf("cannot merge view: %w", err)
 		}
+		e.log.Debug().
+			Hex("block_id", blockID).
+			Hex("collection_id", collectionID).
+			Msg("collection view merged")
 		collectionIndex++
 	}
 
 	// executing system chunk
-	e.log.Debug().Hex("block_id", logging.Entity(block)).Msg("executing system chunk")
+	e.log.Debug().Hex("block_id", blockID).Msg("executing system chunk")
 	colView := stateView.NewChild()
 	_, err = e.executeSystemCollection(blockSpan, collectionIndex, txIndex, systemChunkCtx, colView, programs, res)
 	if err != nil {
@@ -220,6 +246,9 @@ func (e *blockComputer) executeBlock(
 	close(bc.views)
 	close(eh.data)
 	wg.Wait()
+
+	e.log.Debug().Hex("block_id", blockID).Msg("all views committed")
+
 	res.StateReads = stateView.(*delta.View).ReadsCount()
 	res.StateCommitments = stateCommitments
 	res.Proofs = proofs
