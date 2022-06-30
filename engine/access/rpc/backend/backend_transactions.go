3023c27417210f36d084da491682a020987491cd
danielholmes839 <danielh839@gmail.com>
2022-06-30 12:40:37 -0400

    added a check for 0 execution nodes being passed

--- engine/access/rpc/backend/backend_transactions.go
+++ engine/access/rpc/backend/backend_transactions.go
@@ -767,6 +767,11 @@ func (b *backendTransactions) getTransactionResultByIndexFromAnyExeNode(
 		}
 	}
 	defer logAnyError()
+
+	if len(execNodes) == 0 {
+		errs = multierror.Append(errs, errors.New("zero execution nodes provided"))
+	}
+
 	// try to execute the script on one of the execution nodes
 	for _, execNode := range execNodes {
 		resp, err := b.tryGetTransactionResultByIndex(ctx, execNode, req)
