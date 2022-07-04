fd8b6bcc62695fff1728bf3b7ce033cf4d97639b
Yurii Oleksyshyn <yuraolex@gmail.com>
2022-07-04 18:28:38 +0300

    Linted

--- consensus/hotstuff/timeoutaggregator/timeout_aggregator_test.go
+++ consensus/hotstuff/timeoutaggregator/timeout_aggregator_test.go
@@ -2,7 +2,6 @@ package timeoutaggregator
 
 import (
 	"context"
-	"github.com/onflow/flow-go/model/flow"
 	"sync"
 	"testing"
 	"time"
@@ -15,6 +14,7 @@ import (
 	"github.com/onflow/flow-go/consensus/hotstuff/helper"
 	"github.com/onflow/flow-go/consensus/hotstuff/mocks"
 	"github.com/onflow/flow-go/consensus/hotstuff/model"
+	"github.com/onflow/flow-go/model/flow"
 	"github.com/onflow/flow-go/module/irrecoverable"
 	"github.com/onflow/flow-go/utils/unittest"
 )
