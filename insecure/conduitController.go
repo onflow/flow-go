da9bb80651ffd11b192b565679a630db0f21eeb3
Yahya <yahya@dapperlabs.com>
2022-07-06 09:19:35 +0200

    renames conduit controller to egress controller

--- insecure/conduitController.go
+++ insecure/conduitController.go
@@ -5,8 +5,9 @@ import (
 	"github.com/onflow/flow-go/network"
 )
 
-// ConduitController defines part of the behavior of a corruptible conduit factory that controls the conduits it creates.
-type ConduitController interface {
+// EgressController defines part of the behavior of a corruptible conduit factory that controls outbound traffic of the conduits it creates.
+// By the outbound traffic, we mean the traffic from conduit to networking layer, i.e., egress traffic of the engine.
+type EgressController interface {
 	// HandleIncomingEvent sends an incoming event to the conduit factory to process.
 	HandleIncomingEvent(interface{}, network.Channel, Protocol, uint32, ...flow.Identifier) error
 
