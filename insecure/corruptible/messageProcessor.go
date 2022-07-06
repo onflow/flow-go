5a95d83ba4c52952567e473286c9e99dfcea8283
Yahya <yahya@dapperlabs.com>
2022-07-06 09:21:36 +0200

    adds message processor

--- insecure/corruptible/messageProcessor.go
+++ insecure/corruptible/messageProcessor.go
@@ -1,1 +1,18 @@
 package corruptible
+
+import (
+	"github.com/onflow/flow-go/model/flow"
+	flownet "github.com/onflow/flow-go/network"
+)
+
+type MessageProcessor struct {
+	originalProcessor flownet.MessageProcessor // original message processor
+}
+
+var _ flownet.MessageProcessor = &MessageProcessor{}
+
+func (m *MessageProcessor) Process(channel flownet.Channel, originID flow.Identifier, message interface{}) error {
+	// TODO: instead of passing through the ingress traffic, process should
+	// relay it to the attacker.
+	return m.originalProcessor.Process(channel, originID, message)
+}
