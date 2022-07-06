b539bb41be27c2ee202b83a108daf60e66455404
Yahya <yahya@dapperlabs.com>
2022-07-06 09:37:29 +0200

    adds handle incoming event

--- insecure/controller.go
+++ insecure/controller.go
@@ -15,3 +15,12 @@ type EgressController interface {
 	// use it anymore, hence the channel can be closed.
 	EngineClosingChannel(network.Channel) error
 }
+
+// IngressController defines part of behavior of a corrupted networking layer that controls the inbound traffic of
+// the engines registered to it.
+// By the inbound traffic, we mean the traffic from networking layer to the engine that carries on the
+// messages from remote nodes to this engine.
+type IngressController interface
+    // HandleIncomingEvent sends an incoming event (to an engine) to the corrupted networking layer to proces.
+	HandleIncomingEvent(network.Channel, flow.Identifier, interface{})
+}
