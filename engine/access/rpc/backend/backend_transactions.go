fc5d57990eebe86989d9ff971ba71290eb02da40
danielholmes839 <danielh839@gmail.com>
2022-07-06 11:12:44 -0400

    return errors directly

--- engine/access/rpc/backend/backend_transactions.go
+++ engine/access/rpc/backend/backend_transactions.go
@@ -721,6 +721,17 @@ func (b *backendTransactions) getTransactionResultsByBlockIDFromAnyExeNode(
 ) (*execproto.GetTransactionResultsResponse, error) {
 	var errs *multierror.Error
 
+	defer func() {
+		if err := errs.ErrorOrNil(); err != nil {
+			b.log.Err(errs).Msg("failed to get transaction results from execution nodes")
+		}
+	}()
+
+	// if we were passed 0 execution nodes add a specific error
+	if len(execNodes) == 0 {
+		return nil, errors.New("zero execution nodes")
+	}
+
 	for _, execNode := range execNodes {
 		resp, err := b.tryGetTransactionResultsByBlockID(ctx, execNode, req)
 		if err == nil {
@@ -736,13 +747,7 @@ func (b *backendTransactions) getTransactionResultsByBlockIDFromAnyExeNode(
 		errs = multierror.Append(errs, err)
 	}
 
-	// if we were passed 0 execution nodes add a specific error
-	if len(execNodes) == 0 {
-		errs = multierror.Append(errs, errors.New("zero execution nodes"))
-	}
-
 	// log the errors
-	b.log.Err(errs).Msg("failed to get transaction results from execution nodes")
 	return nil, errs.ErrorOrNil()
 }
 
@@ -778,7 +783,7 @@ func (b *backendTransactions) getTransactionResultByIndexFromAnyExeNode(
 	defer logAnyError()
 
 	if len(execNodes) == 0 {
-		errs = multierror.Append(errs, errors.New("zero execution nodes provided"))
+		return nil, errors.New("zero execution nodes provided")
 	}
 
 	// try to execute the script on one of the execution nodes
@@ -797,6 +802,7 @@ func (b *backendTransactions) getTransactionResultByIndexFromAnyExeNode(
 		}
 		errs = multierror.Append(errs, err)
 	}
+
 	return nil, errs.ErrorOrNil()
 }
 
