45a5ce3256425bd586e7f152f542428f232cf6e5
Yurii Oleksyshyn <yuraolex@gmail.com>
2022-07-07 19:47:27 +0300

    Changed factory location

--- consensus/hotstuff/timeoutcollector/factory.go
+++ consensus/hotstuff/timeoutcollector/factory.go
@@ -40,3 +40,28 @@ func (f *TimeoutProcessorFactory) Create(view uint64) (hotstuff.TimeoutProcessor
 
 	return NewTimeoutProcessor(f.committee, f.validator, sigAggregator, f.notifier)
 }
+
+type TimeoutCollectorFactory struct {
+	notifier          hotstuff.Consumer
+	collectorNotifier hotstuff.TimeoutCollectorConsumer
+	processorFactory  hotstuff.TimeoutProcessorFactory
+}
+
+func NewTimeoutCollectorFactory(notifier hotstuff.Consumer,
+	collectorNotifier hotstuff.TimeoutCollectorConsumer,
+	createProcessor hotstuff.TimeoutProcessorFactory,
+) *TimeoutCollectorFactory {
+	return &TimeoutCollectorFactory{
+		notifier:          notifier,
+		collectorNotifier: collectorNotifier,
+		processorFactory:  createProcessor,
+	}
+}
+
+func (f *TimeoutCollectorFactory) Create(view uint64) (hotstuff.TimeoutCollector, error) {
+	processor, err := f.processorFactory.Create(view)
+	if err != nil {
+		return nil, fmt.Errorf("could not create TimeoutProcessor at view %d: %w", view, err)
+	}
+	return NewTimeoutCollector(view, f.notifier, f.collectorNotifier, processor), nil
+}
