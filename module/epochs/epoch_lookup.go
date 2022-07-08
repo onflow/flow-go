9099525a613e53877f6aeee73c6ffb534aab86fe
Jordan Schalm <jordan@dapperlabs.com>
2022-07-08 11:48:14 -0700

    translate EpochLookup implementation
    * check for EECC
    * split out prev/curr/next epoch checks

--- module/epochs/epoch_lookup.go
+++ module/epochs/epoch_lookup.go
@@ -48,7 +48,7 @@ func (l *EpochLookup) EpochForView(view uint64) (epochCounter uint64, err error)
 	epochs := final.Epochs()
 
 	// common case: current epoch
-	currentEpoch, ok, err := l.getEpochInfo(view, epochs.Current())
+	currentEpoch, ok, err := l.getEpochInfo(epochs.Current())
 	if err != nil {
 		return 0, fmt.Errorf("could not get current epoch info: %w", err)
 	}
@@ -61,7 +61,7 @@ func (l *EpochLookup) EpochForView(view uint64) (epochCounter uint64, err error)
 
 	// check for a previous epoch
 	if view < currentEpoch.firstView {
-		previousEpoch, ok, err := l.getEpochInfo(view, epochs.Previous())
+		previousEpoch, ok, err := l.getEpochInfo(epochs.Previous())
 		if err != nil {
 			return 0, fmt.Errorf("could not get previous epoch info: %w", err)
 		}
@@ -69,28 +69,44 @@ func (l *EpochLookup) EpochForView(view uint64) (epochCounter uint64, err error)
 			if view < previousEpoch.firstView {
 				return 0, model.ErrViewForUnknownEpoch
 			}
-			if previousEpoch.firstView <= view && view <= previousEpoch.finalView {
-				return previousEpoch.counter, nil
-			}
+			return previousEpoch.counter, nil
 		}
+		return 0, model.ErrViewForUnknownEpoch
 	}
 
-	//
+	// at this point, we know that the view is after the current epoch
+
+	// if epoch fallback is triggered, all views beyond the end of the current
+	// epoch should be considered part of the current epoch
 	epochFallbackTriggered, err := l.state.Params().EpochFallbackTriggered()
 	if err != nil {
-		return 0, fmt.Errorf("could not check if currentEpoch fallback triggered: %w", err)
+		return 0, fmt.Errorf("could not check if epoch fallback triggered: %w", err)
+	}
+	if epochFallbackTriggered {
+		return currentEpoch.counter, nil
 	}
 
-	return 0, fmt.Errorf("couldn't get currentEpoch for view %d", view)
+	// check for a next epoch
+	nextEpoch, ok, err := l.getEpochInfo(epochs.Next())
+	if err != nil {
+		return 0, fmt.Errorf("could not get next epoch info: %w", err)
+	}
+	if ok {
+		if view > nextEpoch.finalView {
+			return 0, model.ErrViewForUnknownEpoch
+		}
+		return nextEpoch.counter, nil
+	}
+	return 0, model.ErrViewForUnknownEpoch
 }
 
-// getEpochInfo determines whether the view is within the bounds of the given epoch.
+// getEpochInfo returns the epoch counter and first/final views.
 // No errors are expected during normal operation.
 // Returns:
 // * counter, true, nil - if the view is within the bounds of the epoch
 // * counter, false, nil - if the view is not within the bounds of the epoch
 // * 0, false, error - for any unexpected error conditions
-func (l *EpochLookup) getEpochInfo(view uint64, epoch protocol.Epoch) (*epochInfo, bool, error) {
+func (l *EpochLookup) getEpochInfo(epoch protocol.Epoch) (*epochInfo, bool, error) {
 	counter, err := epoch.Counter()
 	if errors.Is(err, protocol.ErrNoPreviousEpoch) || errors.Is(err, protocol.ErrNextEpochNotSetup) {
 		return nil, false, nil
