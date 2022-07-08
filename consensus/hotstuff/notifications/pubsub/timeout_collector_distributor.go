a505a85454cedb60aa4c2f114f1cc1a7d67f284c
Yurii Oleksyshyn <yuraolex@gmail.com>
2022-07-08 16:01:16 +0300

    Updated noop consumer. Added missing constructor

--- consensus/hotstuff/notifications/pubsub/timeout_collector_distributor.go
+++ consensus/hotstuff/notifications/pubsub/timeout_collector_distributor.go
@@ -12,11 +12,17 @@ import (
 // TODO: investigate if this can be updated using atomics to prevent locking on mutex since we always add all consumers
 // before delivering events.
 type TimeoutCollectorDistributor struct {
-	lock      sync.Mutex
+	lock      sync.RWMutex
 	consumers []hotstuff.TimeoutCollectorConsumer
 }
 
 var _ hotstuff.TimeoutCollectorConsumer = (*TimeoutCollectorDistributor)(nil)
+
+func NewTimeoutCollectorDistributor() *TimeoutCollectorDistributor {
+	return &TimeoutCollectorDistributor{
+		consumers: make([]hotstuff.TimeoutCollectorConsumer, 0),
+	}
+}
 
 func (d *TimeoutCollectorDistributor) AddConsumer(consumer hotstuff.TimeoutCollectorConsumer) {
 	d.lock.Lock()
@@ -25,25 +31,32 @@ func (d *TimeoutCollectorDistributor) AddConsumer(consumer hotstuff.TimeoutColle
 }
 
 func (d *TimeoutCollectorDistributor) OnTcConstructedFromTimeouts(tc *flow.TimeoutCertificate) {
+	d.lock.RLock()
+	defer d.lock.RUnlock()
 	for _, consumer := range d.consumers {
 		consumer.OnTcConstructedFromTimeouts(tc)
 	}
 }
 
 func (d *TimeoutCollectorDistributor) OnPartialTcCreated(view uint64, newestQC *flow.QuorumCertificate, lastViewTC *flow.TimeoutCertificate) {
+	d.lock.RLock()
+	defer d.lock.RUnlock()
 	for _, consumer := range d.consumers {
 		consumer.OnPartialTcCreated(view, newestQC, lastViewTC)
 	}
 }
 
 func (d *TimeoutCollectorDistributor) OnNewQcDiscovered(qc *flow.QuorumCertificate) {
-
+	d.lock.RLock()
+	defer d.lock.RUnlock()
 	for _, consumer := range d.consumers {
 		consumer.OnNewQcDiscovered(qc)
 	}
 }
 
 func (d *TimeoutCollectorDistributor) OnNewTcDiscovered(tc *flow.TimeoutCertificate) {
+	d.lock.RLock()
+	defer d.lock.RUnlock()
 	for _, consumer := range d.consumers {
 		consumer.OnNewTcDiscovered(tc)
 	}
