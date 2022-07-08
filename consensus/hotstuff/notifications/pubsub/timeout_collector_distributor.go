976fd228eadecbe5568515503eb5a7a443f15c1c
Yurii Oleksyshyn <yuraolex@gmail.com>
2022-07-08 15:55:17 +0300

    Removed builder and started using RW lock to provide concurrency safety

--- consensus/hotstuff/notifications/pubsub/timeout_collector_distributor.go
+++ consensus/hotstuff/notifications/pubsub/timeout_collector_distributor.go
@@ -7,64 +7,44 @@ import (
 	"github.com/onflow/flow-go/model/flow"
 )
 
-// timeoutCollectorDistributor subscribes for events from hotstuff and distributes it to subscribers.
-// timeoutCollectorDistributor is created with fixed set of signers using Builder pattern.
-// Once we create an instance consumers never change.
-type timeoutCollectorDistributor struct {
-	consumers []hotstuff.TimeoutCollectorConsumer
-}
-
-var _ hotstuff.TimeoutCollectorConsumer = (*timeoutCollectorDistributor)(nil)
-
-func (d *timeoutCollectorDistributor) OnTcConstructedFromTimeouts(tc *flow.TimeoutCertificate) {
-	for _, consumer := range d.consumers {
-		consumer.OnTcConstructedFromTimeouts(tc)
-	}
-}
-
-func (d *timeoutCollectorDistributor) OnPartialTcCreated(view uint64, newestQC *flow.QuorumCertificate, lastViewTC *flow.TimeoutCertificate) {
-	for _, consumer := range d.consumers {
-		consumer.OnPartialTcCreated(view, newestQC, lastViewTC)
-	}
-}
-
-func (d *timeoutCollectorDistributor) OnNewQcDiscovered(qc *flow.QuorumCertificate) {
-	for _, consumer := range d.consumers {
-		consumer.OnNewQcDiscovered(qc)
-	}
-}
-
-func (d *timeoutCollectorDistributor) OnNewTcDiscovered(tc *flow.TimeoutCertificate) {
-	for _, consumer := range d.consumers {
-		consumer.OnNewTcDiscovered(tc)
-	}
-}
-
-// TimeoutCollectorDistributorBuilder is an exportable type which is used to build the distributor.
-// We use this structure to safely add consumers and create a distributor which won't be modified after creation.
-// This approach is needed to avoid introducing lock for consumers as we don't have guarantees that it won't be modified.
-// Concurrency safe.
-type TimeoutCollectorDistributorBuilder struct {
+// TimeoutCollectorDistributor subscribes for events from hotstuff and distributes it to subscribers.
+// Concurrently safe
+// TODO: investigate if this can be updated using atomics to prevent locking on mutex since we always add all consumers
+// before delivering events.
+type TimeoutCollectorDistributor struct {
 	lock      sync.Mutex
 	consumers []hotstuff.TimeoutCollectorConsumer
 }
 
-func NewTimeoutCollectorDistributorBuilder() *TimeoutCollectorDistributorBuilder {
-	return &TimeoutCollectorDistributorBuilder{
-		consumers: make([]hotstuff.TimeoutCollectorConsumer, 0),
-	}
-}
+var _ hotstuff.TimeoutCollectorConsumer = (*TimeoutCollectorDistributor)(nil)
 
-func (d *TimeoutCollectorDistributorBuilder) AddConsumer(consumer hotstuff.TimeoutCollectorConsumer) {
+func (d *TimeoutCollectorDistributor) AddConsumer(consumer hotstuff.TimeoutCollectorConsumer) {
 	d.lock.Lock()
 	defer d.lock.Unlock()
 	d.consumers = append(d.consumers, consumer)
 }
 
-func (d *TimeoutCollectorDistributorBuilder) Build() hotstuff.TimeoutCollectorConsumer {
-	d.lock.Lock()
-	defer d.lock.Unlock()
-	dis := &timeoutCollectorDistributor{consumers: d.consumers}
-	d.consumers = nil
-	return dis
+func (d *TimeoutCollectorDistributor) OnTcConstructedFromTimeouts(tc *flow.TimeoutCertificate) {
+	for _, consumer := range d.consumers {
+		consumer.OnTcConstructedFromTimeouts(tc)
+	}
+}
+
+func (d *TimeoutCollectorDistributor) OnPartialTcCreated(view uint64, newestQC *flow.QuorumCertificate, lastViewTC *flow.TimeoutCertificate) {
+	for _, consumer := range d.consumers {
+		consumer.OnPartialTcCreated(view, newestQC, lastViewTC)
+	}
+}
+
+func (d *TimeoutCollectorDistributor) OnNewQcDiscovered(qc *flow.QuorumCertificate) {
+
+	for _, consumer := range d.consumers {
+		consumer.OnNewQcDiscovered(qc)
+	}
+}
+
+func (d *TimeoutCollectorDistributor) OnNewTcDiscovered(tc *flow.TimeoutCertificate) {
+	for _, consumer := range d.consumers {
+		consumer.OnNewTcDiscovered(tc)
+	}
 }
