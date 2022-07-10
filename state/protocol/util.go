9cf3d8da010729cd16fc1d176056eff6130bd610
Leo Zhang <zhangchiqing@gmail.com>
2022-07-10 06:27:45 -0230

    Update state/protocol/util.go
    Co-authored-by: Alexander Hentschel <alex.hentschel@axiomzen.co>

--- state/protocol/util.go
+++ state/protocol/util.go
@@ -101,7 +101,8 @@ func FindGuarantors(state State, guarantee *flow.CollectionGuarantee) ([]flow.Id
 // Expected Error returns during normal operations:
 //  * storage.ErrNotFound if block not found for the given header
 //  * signature.InvalidSignerIndicesError if `signerIndices` does not encode a valid subset of the consensus committee
-func DecodeSignerIDs(state State, header *flow.Header) ([]flow.Identifier, error) {
+// TODO: change `protocol.State` to `Replicas` API once active PaceMaker is merged
+func DecodeSignerIDs(committee hotstuff.Committee, header *flow.Header) ([]flow.Identifier, error) {
 	// root block does not have signer indices
 	if header.ParentVoterIndices == nil && header.View == 0 {
 		return []flow.Identifier{}, nil
