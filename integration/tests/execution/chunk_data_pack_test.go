90153656ba9f3baa4dae0487a84fc8c449ff7643
Yurii Oleksyshyn <yuraolex@gmail.com>
2022-07-19 18:04:39 +0300

    Skipped more tests

--- integration/tests/execution/chunk_data_pack_test.go
+++ integration/tests/execution/chunk_data_pack_test.go
@@ -14,11 +14,13 @@ import (
 	"github.com/onflow/flow-go/model/flow"
 	"github.com/onflow/flow-go/model/messages"
 	"github.com/onflow/flow-go/network"
+	"github.com/onflow/flow-go/utils/unittest"
 	"github.com/stretchr/testify/require"
 	"github.com/stretchr/testify/suite"
 )
 
 func TestExecutionChunkDataPacks(t *testing.T) {
+	unittest.SkipUnless(t, unittest.TEST_TODO, "active-pacemaker")
 	suite.Run(t, new(ChunkDataPacksSuite))
 }
 
