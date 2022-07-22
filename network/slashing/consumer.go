f55aeadd5baff9294ece4c8e6d0878a416192be1
Khalil Claybon <khalil.claybon@dapperlabs.com>
2022-07-22 16:10:30 -0400

    use a single slashing violations consumer for all validators
    - add unicast_message info to authorized sender validator logs
    - add violations consumer interface to improve testing
    - update tests

--- network/slashing/consumer.go
+++ network/slashing/consumer.go
@@ -1,20 +1,58 @@
 package slashing
 
 import (
-	"github.com/onflow/flow-go/model/flow"
+	"fmt"
+
 	"github.com/rs/zerolog"
+
+	"github.com/onflow/flow-go/utils/logging"
+
+	"github.com/onflow/flow-go/model/flow"
 )
 
-type ViolationsConsumer interface {
-	// OnUnAuthorizedSenderError logs an error for unauthorized sender error
-	OnUnAuthorizedSenderError(identity *flow.Identity, peerID, msgType string, err error)
+const (
+	unAuthorizedSenderViolation = "unauthorized_sender"
+	unknownMsgTypeViolation     = "unknown_message_type"
+	senderEjectedViolation      = "sender_ejected"
+)
 
-	// OnUnknownMsgTypeError logs an error for unknown message type error
-	OnUnknownMsgTypeError(identity *flow.Identity, peerID, msgType string, err error)
+// Consumer is a struct that logs a message for any slashable offences.
+// This struct will be updated in the future when slashing is implemented.
+type Consumer struct {
+	log zerolog.Logger
+}
 
-	// OnSenderEjectedError logs an error for sender ejected error
-	OnSenderEjectedError(identity *flow.Identity, peerID, msgType string, err error)
+// NewSlashingViolationsConsumer returns a new Consumer
+func NewSlashingViolationsConsumer(log zerolog.Logger) *Consumer {
+	return &Consumer{log}
+}
 
-	// SetLogger overrides the configured logger allowing the user to add more log context
-	SetLogger(logger zerolog.Logger)
+func (c *Consumer) logOffense(identity *flow.Identity, peerID, msgType, channel, offense string, isUnicast bool, err error) {
+	e := c.log.Error().
+		Str("peer_id", peerID).
+		Str("offense", offense).
+		Str("message_type", msgType).
+		Str("channel", channel).
+		Bool("unicast_message", true)
+
+	if identity != nil {
+		e = e.Str("role", identity.Role.String()).Hex("sender_id", logging.ID(identity.NodeID))
+	}
+
+	e.Msg(fmt.Sprintf("potential slashable offense: %s", err))
+}
+
+// OnUnAuthorizedSenderError logs an error for unauthorized sender error
+func (c *Consumer) OnUnAuthorizedSenderError(identity *flow.Identity, peerID, msgType, channel string, isUnicast bool, err error) {
+	c.logOffense(identity, peerID, msgType, channel, unAuthorizedSenderViolation, isUnicast, err)
+}
+
+// OnUnknownMsgTypeError logs an error for unknown message type error
+func (c *Consumer) OnUnknownMsgTypeError(identity *flow.Identity, peerID, msgType, channel string, isUnicast bool, err error) {
+	c.logOffense(identity, peerID, msgType, channel, unknownMsgTypeViolation, isUnicast, err)
+}
+
+// OnSenderEjectedError logs an error for sender ejected error
+func (c *Consumer) OnSenderEjectedError(identity *flow.Identity, peerID, msgType, channel string, isUnicast bool, err error) {
+	c.logOffense(identity, peerID, msgType, channel, senderEjectedViolation, isUnicast, err)
 }
--- network/slashing/violations_consumer.go
+++ network/slashing/violations_consumer.go
@@ -1,64 +1,16 @@
 package slashing
 
 import (
-	"fmt"
-
-	"github.com/rs/zerolog"
-
-	"github.com/onflow/flow-go/utils/logging"
-
 	"github.com/onflow/flow-go/model/flow"
 )
 
-const (
-	unAuthorizedSenderViolation = "unauthorized_sender"
-	unknownMsgTypeViolation     = "unknown_message_type"
-	senderEjectedViolation      = "sender_ejected"
-)
+type ViolationsConsumer interface {
+	// OnUnAuthorizedSenderError logs an error for unauthorized sender error
+	OnUnAuthorizedSenderError(identity *flow.Identity, peerID, msgType, channel string, isUnicast bool, err error)
 
-// Consumer is a struct that logs a message for any slashable offences.
-// This struct will be updated in the future when slashing is implemented.
-type Consumer struct {
-	log zerolog.Logger
-}
+	// OnUnknownMsgTypeError logs an error for unknown message type error
+	OnUnknownMsgTypeError(identity *flow.Identity, peerID, msgType, channel string, isUnicast bool, err error)
 
-// NewSlashingViolationsConsumer returns a new Consumer
-func NewSlashingViolationsConsumer(log zerolog.Logger) *Consumer {
-	return &Consumer{log}
-}
-
-func (c *Consumer) logEvent(identity *flow.Identity, peerID, msgType, offense string, err error) {
-	e := c.log.Error().
-		Str("peer_id", peerID).
-		Str("offense", offense)
-
-	if msgType != "" {
-		e.Str("message_type", msgType)
-	}
-
-	if identity != nil {
-		e.Str("role", identity.Role.String()).Hex("sender_id", logging.ID(identity.NodeID))
-	}
-
-	e.Msg(fmt.Sprintf("potential slashable offense: %s", err))
-}
-
-// OnUnAuthorizedSenderError logs an error for unauthorized sender error
-func (c *Consumer) OnUnAuthorizedSenderError(identity *flow.Identity, peerID, msgType string, err error) {
-	c.logEvent(identity, peerID, msgType, unAuthorizedSenderViolation, err)
-}
-
-// OnUnknownMsgTypeError logs an error for unknown message type error
-func (c *Consumer) OnUnknownMsgTypeError(identity *flow.Identity, peerID, msgType string, err error) {
-	c.logEvent(identity, peerID, msgType, unknownMsgTypeViolation, err)
-}
-
-// OnSenderEjectedError logs an error for sender ejected error
-func (c *Consumer) OnSenderEjectedError(identity *flow.Identity, peerID, msgType string, err error) {
-	c.logEvent(identity, peerID, msgType, senderEjectedViolation, err)
-}
-
-// SetLogger overrides the configured logger allowing the user to add more log context
-func (c *Consumer) SetLogger(logger zerolog.Logger) {
-	c.log = logger
+	// OnSenderEjectedError logs an error for sender ejected error
+	OnSenderEjectedError(identity *flow.Identity, peerID, msgType, channel string, isUnicast bool, err error)
 }
