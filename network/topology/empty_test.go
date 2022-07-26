f6729ad0ddde5bedef5502c23c62f6712c8f4a47
Yahya Hassanzadeh <yhassanzadeh13@ku.edu.tr>
2022-07-26 13:58:29 -0700

    adds test for empty topology

--- network/topology/empty_test.go
+++ network/topology/empty_test.go
@@ -1,1 +1,15 @@
 package topology
+
+import (
+	"testing"
+
+	"github.com/onflow/flow-go/utils/unittest"
+	"github.com/stretchr/testify/require"
+)
+
+// TestEmptyTopology checks that EmptyTopology always creates an empty list of fanout.
+func TestEmptyTopology(t *testing.T) {
+	ids := unittest.IdentityListFixture(10)
+	top := NewEmptyTopology()
+	require.Empty(t, top.Fanout(ids))
+}
