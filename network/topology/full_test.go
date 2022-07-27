b419ecf5f1405e688b9b154f4ceab20ed1053ecc
Yahya Hassanzadeh <yhassanzadeh13@ku.edu.tr>
2022-07-27 10:22:36 -0700

    lint fix

--- network/topology/full_test.go
+++ network/topology/full_test.go
@@ -3,9 +3,10 @@ package topology_test
 import (
 	"testing"
 
+	"github.com/stretchr/testify/require"
+
 	"github.com/onflow/flow-go/network/topology"
 	"github.com/onflow/flow-go/utils/unittest"
-	"github.com/stretchr/testify/require"
 )
 
 // TestEmptyTopology checks that FullyConnectedTopology always returns the input list as the fanout.
