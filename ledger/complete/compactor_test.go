8afb36bc9ae75bae7d5cf83d722f099fd545a0a7
Faye Amacker <33205765+fxamacker@users.noreply.github.com>
2022-07-29 11:28:00 -0500

    Refactor compactor tests

--- ledger/complete/compactor_test.go
+++ ledger/complete/compactor_test.go
@@ -46,6 +46,8 @@ func (co *CompactorObserver) OnComplete() {
 	close(co.done)
 }
 
+// TestCompactor tests creation of WAL segments and checkpoints, and
+// checks if the rebuilt ledger state matches previous ledger state.
 func TestCompactor(t *testing.T) {
 	const (
 		numInsPerStep      = 2
@@ -155,11 +157,8 @@ func TestCompactor(t *testing.T) {
 			require.FileExists(t, path.Join(dir, "checkpoint.00000008"))
 			require.NoFileExists(t, path.Join(dir, "checkpoint.00000009"))
 
-			ledgerDone := l.Done()
-			compactorDone := compactor.Done()
-
-			<-ledgerDone
-			<-compactorDone
+			<-l.Done()
+			<-compactor.Done()
 		})
 
 		time.Sleep(2 * time.Second)
@@ -246,6 +245,111 @@ func TestCompactor(t *testing.T) {
 	})
 }
 
+// TestCompactorSkipCheckpointing tests that only one
+// checkpointing is running at a time.
+func TestCompactorSkipCheckpointing(t *testing.T) {
+	const (
+		numInsPerStep      = 2
+		pathByteSize       = 32
+		minPayloadByteSize = 2 << 15
+		maxPayloadByteSize = 2 << 16
+		size               = 10
+		checkpointDistance = 1 // checkpointDistance 1 triggers checkpointing for every segment.
+		checkpointsToKeep  = 0
+		forestCapacity     = size * 10
+	)
+
+	metricsCollector := &metrics.NoopCollector{}
+
+	unittest.RunWithTempDir(t, func(dir string) {
+
+		var l *Ledger
+
+		// saved data after updates
+		savedData := make(map[ledger.RootHash]map[string]*ledger.Payload)
+
+		wal, err := realWAL.NewDiskWAL(zerolog.Nop(), nil, metrics.NewNoopCollector(), dir, forestCapacity, pathByteSize, 32*1024)
+		require.NoError(t, err)
+
+		l, err = NewLedger(wal, size*10, metricsCollector, zerolog.Logger{}, DefaultPathFinderVersion)
+		require.NoError(t, err)
+
+		// WAL segments are 32kB, so here we generate 2 keys 64kB each, times `size`
+		// so we should get at least `size` segments
+
+		compactor, err := NewCompactor(l, wal, zerolog.Nop(), forestCapacity, checkpointDistance, checkpointsToKeep)
+		require.NoError(t, err)
+
+		co := CompactorObserver{fromBound: 8, done: make(chan struct{})}
+		compactor.Subscribe(&co)
+
+		// Run Compactor in background.
+		<-compactor.Ready()
+
+		rootState := l.InitialState()
+
+		// Generate the tree and create WAL
+		for i := 0; i < size; i++ {
+
+			payloads := utils.RandomPayloads(numInsPerStep, minPayloadByteSize, maxPayloadByteSize)
+
+			keys := make([]ledger.Key, len(payloads))
+			values := make([]ledger.Value, len(payloads))
+			for i, p := range payloads {
+				keys[i] = p.Key
+				values[i] = p.Value
+			}
+
+			update, err := ledger.NewUpdate(rootState, keys, values)
+			require.NoError(t, err)
+
+			newState, _, err := l.Set(update)
+			require.NoError(t, err)
+
+			require.FileExists(t, path.Join(dir, realWAL.NumberToFilenamePart(i)))
+
+			data := make(map[string]*ledger.Payload, len(keys))
+			for j, k := range keys {
+				ks := string(k.CanonicalForm())
+				data[ks] = payloads[j]
+			}
+
+			savedData[ledger.RootHash(newState)] = data
+
+			rootState = newState
+		}
+
+		// wait for the bound-checking observer to confirm checkpoints have been made
+		select {
+		case <-co.done:
+			// continue
+		case <-time.After(60 * time.Second):
+			// Log segment and checkpoint files
+			files, err := ioutil.ReadDir(dir)
+			require.NoError(t, err)
+
+			for _, file := range files {
+				fmt.Printf("%s, size %d\n", file.Name(), file.Size())
+			}
+
+			assert.FailNow(t, "timed out")
+		}
+
+		<-l.Done()
+		<-compactor.Done()
+
+		checkpointer, err := wal.NewCheckpointer()
+		require.NoError(t, err)
+
+		nums, err := checkpointer.Checkpoints()
+		require.NoError(t, err)
+
+		// Check that there are gaps between checkpoints (some checkpoints are skipped)
+		firstNum, lastNum := nums[0], nums[len(nums)-1]
+		require.True(t, len(nums) < lastNum-firstNum+1)
+	})
+}
+
 // TestCompactorAccuracy expects checkpointed tries to match replayed tries.
 // Replayed tries are tries updated by replaying all WAL segments
 // (from segment 0, ignoring prior checkpoints) to the checkpoint number.
@@ -255,17 +359,20 @@ func TestCompactorAccuracy(t *testing.T) {
 	const (
 		numInsPerStep      = 2
 		pathByteSize       = 32
-		minPayloadByteSize = 2 << 15
-		maxPayloadByteSize = 2 << 16
-		size               = 10
+		minPayloadByteSize = 2<<11 - 256 // 3840 bytes
+		maxPayloadByteSize = 2 << 11     // 4096 bytes
+		size               = 20
 		checkpointDistance = 5
-		checkpointsToKeep  = 0
-		forestCapacity     = size * 10
+		checkpointsToKeep  = 0 // keep all
+		forestCapacity     = 500
 	)
 
 	metricsCollector := &metrics.NoopCollector{}
 
 	unittest.RunWithTempDir(t, func(dir string) {
+
+		// There appears to be 1-2 records per segment (according to logs), so
+		// generate size/2 segments.
 
 		lastCheckpointNum := -1
 
@@ -277,19 +384,13 @@ func TestCompactorAccuracy(t *testing.T) {
 			wal, err := realWAL.NewDiskWAL(zerolog.Nop(), nil, metrics.NewNoopCollector(), dir, forestCapacity, pathByteSize, 32*1024)
 			require.NoError(t, err)
 
-			l, err := NewLedger(wal, size*10, metricsCollector, zerolog.Logger{}, DefaultPathFinderVersion)
+			l, err := NewLedger(wal, forestCapacity, metricsCollector, zerolog.Logger{}, DefaultPathFinderVersion)
 			require.NoError(t, err)
-
-			checkpointer, err := wal.NewCheckpointer()
-			require.NoError(t, err)
-
-			// WAL segments are 32kB, so here we generate 2 keys 64kB each, times `size`
-			// so we should get at least `size` segments
 
 			compactor, err := NewCompactor(l, wal, zerolog.Nop(), forestCapacity, checkpointDistance, checkpointsToKeep)
 			require.NoError(t, err)
 
-			fromBound := lastCheckpointNum + int(checkpointDistance)*2
+			fromBound := lastCheckpointNum + (size / 2)
 
 			co := CompactorObserver{fromBound: fromBound, done: make(chan struct{})}
 			compactor.Subscribe(&co)
@@ -298,7 +399,8 @@ func TestCompactorAccuracy(t *testing.T) {
 			<-compactor.Ready()
 
 			// Generate the tree and create WAL
-			for i := 0; i < size; i++ {
+			// size+2 is used to ensure that size/2 segments are finalized.
+			for i := 0; i < size+2; i++ {
 
 				payloads := utils.RandomPayloads(numInsPerStep, minPayloadByteSize, maxPayloadByteSize)
 
@@ -327,33 +429,167 @@ func TestCompactorAccuracy(t *testing.T) {
 			}
 
 			// Shutdown ledger and compactor
-			ledgerDone := l.Done()
-			compactorDone := compactor.Done()
+			<-l.Done()
+			<-compactor.Done()
 
-			<-ledgerDone
-			<-compactorDone
+			checkpointer, err := wal.NewCheckpointer()
+			require.NoError(t, err)
 
 			nums, err := checkpointer.Checkpoints()
 			require.NoError(t, err)
 
 			for _, n := range nums {
-				testCheckpointedTriesMatchReplayedTriesFromSegments(t, checkpointer, n, dir)
+				// TODO:  After the LRU Cache (outside of checkpointing code) is replaced
+				// by a queue, etc. we should make sure the insertion order is preserved.
+
+				checkSequence := false
+				if i == 0 {
+					// Sequence check only works when initial state is blank.
+					// When initial state is from ledger's forest (LRU cache),
+					// its sequence is altered by reads when replaying segment records.
+					// Insertion order is not preserved (which is the way
+					// it is currently on mainnet).  However, with PR 2792, only the
+					// initial values are affected and those would likely
+					// get into insertion order for the next checkpoint.  Once
+					// the LRU Cache (outside of checkpointing code) is replaced,
+					// then we can verify insertion order.
+					checkSequence = true
+				}
+				testCheckpointedTriesMatchReplayedTriesFromSegments(t, checkpointer, n, dir, checkSequence)
 			}
+
+			lastCheckpointNum = nums[len(nums)-1]
 		}
 	})
 }
 
-func testCheckpointedTriesMatchReplayedTriesFromSegments(t *testing.T, checkpointer *realWAL.Checkpointer, checkpointNum int, dir string) {
+// TestCompactorConcurrency expects checkpointed tries to
+// match replayed tries in sequence with concurrent updates.
+// Replayed tries are tries updated by replaying all WAL segments
+// (from segment 0, ignoring prior checkpoints) to the checkpoint number.
+// This verifies that checkpointed tries are snapshopt of segments
+// and at segment boundary.
+// Note: sequence check only works when initial state is blank.
+// When initial state is from ledger's forest (LRU cache), its
+// sequence is altered by reads when replaying segment records.
+func TestCompactorConcurrency(t *testing.T) {
+	const (
+		numInsPerStep      = 2
+		pathByteSize       = 32
+		minPayloadByteSize = 2<<11 - 256 // 3840 bytes
+		maxPayloadByteSize = 2 << 11     // 4096 bytes
+		size               = 20
+		checkpointDistance = 5
+		checkpointsToKeep  = 0 // keep all
+		forestCapacity     = 500
+		numGoroutine       = 4
+		lastCheckpointNum  = -1
+	)
 
-	// Get tries by replaying segments up to checkpoint number
-	triesFromReplayingSegments, err := triesUpToSegment(dir, checkpointNum)
-	require.NoError(t, err)
+	rootState := ledger.State(trie.EmptyTrieRootHash())
 
+	metricsCollector := &metrics.NoopCollector{}
+
+	unittest.RunWithTempDir(t, func(dir string) {
+
+		// There are 1-2 records per segment (according to logs), so
+		// generate size/2 segments.
+
+		wal, err := realWAL.NewDiskWAL(zerolog.Nop(), nil, metrics.NewNoopCollector(), dir, forestCapacity, pathByteSize, 32*1024)
+		require.NoError(t, err)
+
+		l, err := NewLedger(wal, forestCapacity, metricsCollector, zerolog.Logger{}, DefaultPathFinderVersion)
+		require.NoError(t, err)
+
+		compactor, err := NewCompactor(l, wal, zerolog.Nop(), forestCapacity, checkpointDistance, checkpointsToKeep)
+		require.NoError(t, err)
+
+		fromBound := lastCheckpointNum + (size / 2 * numGoroutine)
+
+		co := CompactorObserver{fromBound: fromBound, done: make(chan struct{})}
+		compactor.Subscribe(&co)
+
+		// Run Compactor in background.
+		<-compactor.Ready()
+
+		// Run 4 goroutines and each goroutine updates size+1 tries.
+		for j := 0; j < numGoroutine; j++ {
+			go func(parentState ledger.State) {
+				// size+1 is used to ensure that size/2*numGoroutine segments are finalized.
+				for i := 0; i < size+1; i++ {
+					payloads := utils.RandomPayloads(numInsPerStep, minPayloadByteSize, maxPayloadByteSize)
+
+					keys := make([]ledger.Key, len(payloads))
+					values := make([]ledger.Value, len(payloads))
+					for i, p := range payloads {
+						keys[i] = p.Key
+						values[i] = p.Value
+					}
+
+					update, err := ledger.NewUpdate(parentState, keys, values)
+					require.NoError(t, err)
+
+					newState, _, err := l.Set(update)
+					require.NoError(t, err)
+
+					parentState = newState
+				}
+			}(rootState)
+		}
+
+		// wait for the bound-checking observer to confirm checkpoints have been made
+		select {
+		case <-co.done:
+			// continue
+		case <-time.After(120 * time.Second):
+			assert.FailNow(t, "timed out")
+		}
+
+		// Shutdown ledger and compactor
+		<-l.Done()
+		<-compactor.Done()
+
+		checkpointer, err := wal.NewCheckpointer()
+		require.NoError(t, err)
+
+		nums, err := checkpointer.Checkpoints()
+		require.NoError(t, err)
+
+		for _, n := range nums {
+			// For each created checkpoint:
+			// - get tries by loading checkpoint
+			// - get tries by replaying segments up to checkpoint number (ignoring all prior checkpoints)
+			// - test that these 2 sets of tries match in content and sequence (insertion order).
+			testCheckpointedTriesMatchReplayedTriesFromSegments(t, checkpointer, n, dir, true)
+		}
+	})
+}
+
+func testCheckpointedTriesMatchReplayedTriesFromSegments(
+	t *testing.T,
+	checkpointer *realWAL.Checkpointer,
+	checkpointNum int,
+	dir string,
+	inSequence bool,
+) {
 	// Get tries by loading checkpoint
 	triesFromLoadingCheckpoint, err := checkpointer.LoadCheckpoint(checkpointNum)
 	require.NoError(t, err)
 
-	// Test that checkpointed tries match replayed tries.
+	// Get tries by replaying segments up to checkpoint number (ignoring checkpoints)
+	triesFromReplayingSegments, err := triesUpToSegment(dir, checkpointNum, len(triesFromLoadingCheckpoint))
+	require.NoError(t, err)
+
+	if inSequence {
+		// Test that checkpointed tries match replayed tries in content and sequence (insertion order).
+		require.Equal(t, len(triesFromReplayingSegments), len(triesFromLoadingCheckpoint))
+		for i := 0; i < len(triesFromReplayingSegments); i++ {
+			require.Equal(t, triesFromReplayingSegments[i].RootHash(), triesFromLoadingCheckpoint[i].RootHash())
+		}
+		return
+	}
+
+	// Test that checkpointed tries match replayed tries in content (ignore order).
 	triesSetFromReplayingSegments := make(map[ledger.RootHash]struct{})
 	for _, t := range triesFromReplayingSegments {
 		triesSetFromReplayingSegments[t.RootHash()] = struct{}{}
@@ -367,18 +603,31 @@ func testCheckpointedTriesMatchReplayedTriesFromSegments(t *testing.T, checkpoin
 	require.True(t, reflect.DeepEqual(triesSetFromReplayingSegments, triesSetFromLoadingCheckpoint))
 }
 
-func triesUpToSegment(dir string, to int) ([]*trie.MTrie, error) {
+func triesUpToSegment(dir string, to int, capacity int) ([]*trie.MTrie, error) {
 
-	forest, err := mtrie.NewForest(500, &metrics.NoopCollector{}, nil)
+	// forest is used to create new trie.
+	forest, err := mtrie.NewForest(capacity, &metrics.NoopCollector{}, nil)
 	if err != nil {
 		return nil, fmt.Errorf("cannot create Forest: %w", err)
 	}
+
+	initialTries, err := forest.GetTries()
+	if err != nil {
+		return nil, fmt.Errorf("cannot get tries from forest: %w", err)
+	}
+
+	// TrieQueue is used to store last n tries from segment files in order (n = capacity)
+	tries := realWAL.NewTrieQueueWithValues(uint(capacity), initialTries)
 
 	err = replaySegments(
 		dir,
 		to,
 		func(update *ledger.TrieUpdate) error {
-			_, err := forest.Update(update)
+			t, err := forest.NewTrie(update)
+			if err == nil {
+				forest.AddTrie(t)
+				tries.Push(t)
+			}
 			return err
 		}, func(rootHash ledger.RootHash) error {
 			return nil
@@ -387,7 +636,7 @@ func triesUpToSegment(dir string, to int) ([]*trie.MTrie, error) {
 		return nil, err
 	}
 
-	return forest.GetTries()
+	return tries.Tries(), nil
 }
 
 func replaySegments(
@@ -436,102 +685,4 @@ func replaySegments(
 	}
 
 	return nil
-}
-
-func TestCompactorConcurrency(t *testing.T) {
-	const (
-		numInsPerStep      = 2
-		pathByteSize       = 32
-		minPayloadByteSize = 2 << 15
-		maxPayloadByteSize = 2 << 16
-		size               = 10
-		checkpointDistance = 5
-		checkpointsToKeep  = 0 // keep all
-		forestCapacity     = size * 10
-	)
-
-	metricsCollector := &metrics.NoopCollector{}
-
-	unittest.RunWithTempDir(t, func(dir string) {
-
-		lastCheckpointNum := -1
-
-		rootHash := trie.EmptyTrieRootHash()
-
-		// Create DiskWAL and Ledger repeatedly to test rebuilding ledger state at restart.
-		for i := 0; i < 3; i++ {
-
-			wal, err := realWAL.NewDiskWAL(zerolog.Nop(), nil, metrics.NewNoopCollector(), dir, forestCapacity, pathByteSize, 32*1024)
-			require.NoError(t, err)
-
-			l, err := NewLedger(wal, size*10, metricsCollector, zerolog.Logger{}, DefaultPathFinderVersion)
-			require.NoError(t, err)
-
-			checkpointer, err := wal.NewCheckpointer()
-			require.NoError(t, err)
-
-			// WAL segments are 32kB, so here we generate 2 keys 64kB each, times `size`
-			// so we should get at least `size` segments
-
-			compactor, err := NewCompactor(l, wal, zerolog.Nop(), forestCapacity, checkpointDistance, checkpointsToKeep)
-			require.NoError(t, err)
-
-			fromBound := lastCheckpointNum + int(checkpointDistance)*2
-
-			co := CompactorObserver{fromBound: fromBound, done: make(chan struct{})}
-			compactor.Subscribe(&co)
-
-			// Run Compactor in background.
-			<-compactor.Ready()
-
-			// Generate updates
-			var updates []*ledger.Update
-			for i := 0; i < size; i++ {
-
-				payloads := utils.RandomPayloads(numInsPerStep, minPayloadByteSize, maxPayloadByteSize)
-
-				keys := make([]ledger.Key, len(payloads))
-				values := make([]ledger.Value, len(payloads))
-				for i, p := range payloads {
-					keys[i] = p.Key
-					values[i] = p.Value
-				}
-
-				update, err := ledger.NewUpdate(ledger.State(rootHash), keys, values)
-				require.NoError(t, err)
-
-				updates = append(updates, update)
-			}
-
-			// Generate the tree and create WAL in parallel
-			for _, update := range updates {
-				go func(update *ledger.Update) {
-					_, _, err := l.Set(update)
-					require.NoError(t, err)
-				}(update)
-			}
-
-			// wait for the bound-checking observer to confirm checkpoints have been made
-			select {
-			case <-co.done:
-				// continue
-			case <-time.After(60 * time.Second):
-				assert.FailNow(t, "timed out")
-			}
-
-			// Shutdown ledger and compactor
-			ledgerDone := l.Done()
-			compactorDone := compactor.Done()
-
-			<-ledgerDone
-			<-compactorDone
-
-			nums, err := checkpointer.Checkpoints()
-			require.NoError(t, err)
-
-			for _, n := range nums {
-				testCheckpointedTriesMatchReplayedTriesFromSegments(t, checkpointer, n, dir)
-			}
-		}
-	})
 }
