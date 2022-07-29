354c77d54c91cd5f6d0f25e76668a392c7233980
Yahya Hassanzadeh <yhassanzadeh13@ku.edu.tr>
2022-07-29 10:10:55 -0700

    adds comments for the test

--- network/p2p/peerManager_integration_test.go
+++ network/p2p/peerManager_integration_test.go
@@ -5,15 +5,21 @@ import (
 	"testing"
 	"time"
 
+	"github.com/libp2p/go-libp2p-core/host"
 	"github.com/libp2p/go-libp2p-core/network"
 	"github.com/libp2p/go-libp2p-core/peer"
 	"github.com/libp2p/go-libp2p-core/peerstore"
+	"github.com/onflow/flow-go/model/flow"
 	"github.com/onflow/flow-go/network/p2p"
 	"github.com/onflow/flow-go/utils/unittest"
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
 )
 
+// TestPeerManager_Integration tests the correctness of integration between PeerManager and Libp2pConnector over
+// a fully connected topology.
+// PeerManager should be able to connect to all peers using the connector, and must also tear down the connection to
+// peers that are excluded from its identity provider.
 func TestPeerManager_Integration(t *testing.T) {
 	count := 5
 	ctx, cancel := context.WithCancel(context.Background())
@@ -26,12 +32,14 @@ func TestPeerManager_Integration(t *testing.T) {
 	thisNode := nodes[0]
 	othersId := identities[1:]
 
-	info, invalid := p2p.PeerInfosFromIDs(identities)
+	// adds address of all other nodes into the peer store of this node, so that it can dial them.
+	info, invalid := p2p.PeerInfosFromIDs(othersId)
 	require.Empty(t, invalid)
 	for _, i := range info {
 		thisNode.Host().Peerstore().SetAddrs(i.ID, i.Addrs, peerstore.PermanentAddrTTL)
 	}
 
+	// setup
 	connector, err := p2p.NewLibp2pConnector(unittest.Logger(), thisNode.Host(), p2p.ConnectionPruningEnabled)
 	require.NoError(t, err)
 
@@ -39,6 +47,8 @@ func TestPeerManager_Integration(t *testing.T) {
 	require.NoError(t, err)
 
 	peerManager := p2p.NewPeerManager(unittest.Logger(), func() peer.IDSlice {
+		// peerManager is furnished with a full topology that connects to all nodes
+		// in the othersId.
 		peers := peer.IDSlice{}
 		for _, id := range othersId {
 			peerId, err := idTranslator.GetPeerID(id.NodeID)
@@ -49,32 +59,37 @@ func TestPeerManager_Integration(t *testing.T) {
 		return peers
 	}, connector)
 
+	// initially no node should be in peer store of this node.
 	require.Empty(t, thisNode.Host().Network().Peers())
 	peerManager.ForceUpdatePeers()
 	time.Sleep(1 * time.Second)
+	// after a forced update all other nodes must be added to the peer store of this node.
 	require.Len(t, thisNode.Host().Network().Peers(), count-1)
-
-	// there must be a connection between this node and other nodes
-	for _, other := range othersId {
-		peerId, err := idTranslator.GetPeerID(other.NodeID)
-		require.NoError(t, err)
-		assert.Equal(t, thisNode.Host().Network().Connectedness(peerId), network.Connected)
-	}
+	// after a forced update there must be a connection between this node and other nodes
+	connectedToAll(t, thisNode.Host(), idTranslator, othersId.NodeIDs())
 
 	// kicks one node out of the othersIds; this imitates evicting, ejecting, or unstaking a node
-	evictedId := othersId[0]
-	othersId = othersId[1:]
+	evictedId := othersId[0] // evicted one
+	othersId = othersId[1:]  // updates otherIds list
 	peerManager.ForceUpdatePeers()
 	time.Sleep(1 * time.Second)
+	// after a forced update, the evicted one should be excluded from the peer store.
 	require.Len(t, thisNode.Host().Network().Peers(), count-2)
-	// there must be a connection between this node and other nodes
-	for _, other := range othersId {
-		peerId, err := idTranslator.GetPeerID(other.NodeID)
-		require.NoError(t, err)
-		assert.Equal(t, thisNode.Host().Network().Connectedness(peerId), network.Connected)
-	}
+	// there must be a connection between this node and other nodes (except evicted one).
+	connectedToAll(t, thisNode.Host(), idTranslator, othersId.NodeIDs())
+
 	// there must be no connection between this node and evicted one
 	peerId, err := idTranslator.GetPeerID(evictedId.NodeID)
 	require.NoError(t, err)
 	assert.Equal(t, thisNode.Host().Network().Connectedness(peerId), network.NotConnected)
+}
+
+// connectedToAll is a test helper that fails if there is no connection between this host and at least one of the
+// nodes in "all".
+func connectedToAll(t *testing.T, host host.Host, translator p2p.IDTranslator, all flow.IdentifierList) {
+	for _, id := range all {
+		peerId, err := translator.GetPeerID(id)
+		require.NoError(t, err)
+		assert.Equal(t, host.Network().Connectedness(peerId), network.Connected)
+	}
 }
