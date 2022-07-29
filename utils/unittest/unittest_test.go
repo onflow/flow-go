34727b04325b7cd1b0dbe31cf68256e546691788
gomisha <misha@gomisha.com>
2022-07-29 19:15:12 -0400

    CrashTest - added 3rd unit test - Crash_LoggerFatal

--- utils/unittest/unittest_test.go
+++ utils/unittest/unittest_test.go
@@ -9,7 +9,7 @@ import "os"
 // TestCrashTest_ErrorMessage tests that CrashTest() can check a function that crashed without any messages.
 func TestCrashTest_NoMessage(t *testing.T) {
 	f := func(t *testing.T) {
-		CrashAndBurn_NoMessage(t)
+		Crash_NoMessage(t)
 	}
 
 	CrashTest(t, f, "", "TestCrashTest_NoMessage")
@@ -18,16 +18,33 @@ func TestCrashTest_NoMessage(t *testing.T) {
 // TestCrashTest_ErrorMessage tests that CrashTest() can read standard messages from stdout before a crash.
 func TestCrashTest_ErrorMessage(t *testing.T) {
 	f := func(t *testing.T) {
-		CrashAndBurn_ErrorMessage(t)
+		Crash_ErrorMessage(t)
 	}
 	CrashTest(t, f, "about to crash", "TestCrashTest_ErrorMessage")
 }
 
-func CrashAndBurn_NoMessage(t *testing.T) {
+// TestCrashTest_Logger tests that CrashTest() can read fatal logger messages from stdout before a crash. This test
+// assumes that the logger uses a hook to send fatal messages to stdout.
+func TestCrashTest_Logger(t *testing.T) {
+	f := func(t *testing.T) {
+		Crash_LoggerFatal(t)
+	}
+	CrashTest(t, f, "fatal crash from logger", "TestCrashTest_Logger")
+}
+
+func Crash_NoMessage(t *testing.T) {
 	os.Exit(1)
 }
 
-func CrashAndBurn_ErrorMessage(t *testing.T) {
+func Crash_ErrorMessage(t *testing.T) {
 	fmt.Println("about to crash... crashing in 3...2...1...")
 	os.Exit(1)
+}
+
+func Crash_LoggerFatal(t *testing.T) {
+	// hook sends fatal messages to stdout, so they can be checked by CrashTest()
+	logger, _ := HookedLogger()
+
+	// calling Fatal() causes the process to exit
+	logger.Fatal().Msg("fatal crash from logger... crashing in 3...2...1...")
 }
