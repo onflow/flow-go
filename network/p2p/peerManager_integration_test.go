d1650a667f9161c4953a16dbda478e445a0328b4
Yahya Hassanzadeh <yhassanzadeh13@ku.edu.tr>
2022-07-29 10:10:48 -0700

    adds test for peer manager integration

--- network/p2p/peerManager_integration_test.go
+++ network/p2p/peerManager_integration_test.go
@@ -5,9 +5,12 @@ import (
 	"testing"
 	"time"
 
+	"github.com/libp2p/go-libp2p-core/network"
 	"github.com/libp2p/go-libp2p-core/peer"
+	"github.com/libp2p/go-libp2p-core/peerstore"
 	"github.com/onflow/flow-go/network/p2p"
 	"github.com/onflow/flow-go/utils/unittest"
+	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
 )
 
@@ -21,8 +24,13 @@ func TestPeerManager_Integration(t *testing.T) {
 	defer stopNodes(t, nodes)
 
 	thisNode := nodes[0]
-	// thisId := identities[0]
 	othersId := identities[1:]
+
+	info, invalid := p2p.PeerInfosFromIDs(identities)
+	require.Empty(t, invalid)
+	for _, i := range info {
+		thisNode.Host().Peerstore().SetAddrs(i.ID, i.Addrs, peerstore.PermanentAddrTTL)
+	}
 
 	connector, err := p2p.NewLibp2pConnector(unittest.Logger(), thisNode.Host(), p2p.ConnectionPruningEnabled)
 	require.NoError(t, err)
@@ -43,6 +51,30 @@ func TestPeerManager_Integration(t *testing.T) {
 
 	require.Empty(t, thisNode.Host().Network().Peers())
 	peerManager.ForceUpdatePeers()
-	time.Sleep(10 * time.Second)
+	time.Sleep(1 * time.Second)
 	require.Len(t, thisNode.Host().Network().Peers(), count-1)
+
+	// there must be a connection between this node and other nodes
+	for _, other := range othersId {
+		peerId, err := idTranslator.GetPeerID(other.NodeID)
+		require.NoError(t, err)
+		assert.Equal(t, thisNode.Host().Network().Connectedness(peerId), network.Connected)
+	}
+
+	// kicks one node out of the othersIds; this imitates evicting, ejecting, or unstaking a node
+	evictedId := othersId[0]
+	othersId = othersId[1:]
+	peerManager.ForceUpdatePeers()
+	time.Sleep(1 * time.Second)
+	require.Len(t, thisNode.Host().Network().Peers(), count-2)
+	// there must be a connection between this node and other nodes
+	for _, other := range othersId {
+		peerId, err := idTranslator.GetPeerID(other.NodeID)
+		require.NoError(t, err)
+		assert.Equal(t, thisNode.Host().Network().Connectedness(peerId), network.Connected)
+	}
+	// there must be no connection between this node and evicted one
+	peerId, err := idTranslator.GetPeerID(evictedId.NodeID)
+	require.NoError(t, err)
+	assert.Equal(t, thisNode.Host().Network().Connectedness(peerId), network.NotConnected)
 }
