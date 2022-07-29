061c9b81374bdcdcabcad509095bf312118bd75a
Yahya Hassanzadeh <yhassanzadeh13@ku.edu.tr>
2022-07-29 10:47:21 -0700

    fixes a godoc

--- network/topology.go
+++ network/topology.go
@@ -7,10 +7,10 @@ import (
 // Topology provides a subset of nodes which a given node should directly connect in order to form a connected mesh
 // for epidemic message dissemination (e.g., publisher and subscriber model).
 type Topology interface {
-	// Fanout receives IdentityList of entire network and constructs the fanout identity list of the node.
+	// Fanout receives IdentityList of entire network and constructs the fanout IdentityList of the node.
 	// A node directly communicates with its fanout IdentityList on epidemic dissemination
 	// of the messages (i.e., publish and multicast).
 	//
-	// GenerateFanout is not concurrency safe. It is responsibility of caller to lock for it.
+	// Fanout is not concurrency safe. It is responsibility of caller to lock for it (if needed).
 	Fanout(ids flow.IdentityList) flow.IdentityList
 }
