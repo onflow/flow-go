e894cc441b9383939e098b93986770e3413fd374
Leo Zhang (zhangchiqing) <zhangchiqing@gmail.com>
2022-08-02 16:49:50 -0700

    remove unused method

--- cmd/util/cmd/read-badger/cmd/storages.go
+++ cmd/util/cmd/read-badger/cmd/storages.go
@@ -4,8 +4,6 @@ import (
 	"github.com/dgraph-io/badger/v2"
 
 	"github.com/onflow/flow-go/cmd/util/cmd/common"
-	"github.com/onflow/flow-go/consensus/hotstuff/persister"
-	"github.com/onflow/flow-go/model/flow"
 	"github.com/onflow/flow-go/storage"
 )
 
@@ -13,10 +11,4 @@ func InitStorages() (*storage.All, *badger.DB) {
 	db := common.InitStorage(flagDatadir)
 	storages := common.InitStorages(db)
 	return storages, db
-}
-
-func InitPersister() (*persister.Persister, *badger.DB) {
-	db := common.InitStorage(flagDatadir)
-	chainID := flow.ChainID("flow-mainnet")
-	return persister.New(db, chainID), db
 }
