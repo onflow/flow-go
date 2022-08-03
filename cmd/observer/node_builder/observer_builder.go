185d90992ac524f946db88f8b19b08e560b4726e
danielholmes839 <daniel.holmes@dapperlabs.com>
2022-08-03 15:08:39 -0400

    lint

--- cmd/observer/node_builder/observer_builder.go
+++ cmd/observer/node_builder/observer_builder.go
@@ -11,16 +11,12 @@ import (
 	"strings"
 	"time"
 
-	"github.com/hashicorp/go-multierror"
 	badger "github.com/ipfs/go-ds-badger2"
 	"github.com/libp2p/go-libp2p-core/host"
 	"github.com/libp2p/go-libp2p-core/peer"
 	"github.com/libp2p/go-libp2p-core/routing"
 	dht "github.com/libp2p/go-libp2p-kad-dht"
 	p2ppubsub "github.com/libp2p/go-libp2p-pubsub"
-	accessproto "github.com/onflow/flow/protobuf/go/flow/access"
-	"google.golang.org/grpc"
-	"google.golang.org/grpc/credentials"
 
 	sdkcrypto "github.com/onflow/flow-go-sdk/crypto"
 	"github.com/onflow/flow-go/cmd"
@@ -69,7 +65,6 @@ import (
 	"github.com/onflow/flow-go/state/protocol/events/gadgets"
 	"github.com/onflow/flow-go/storage"
 	bstorage "github.com/onflow/flow-go/storage/badger"
-	"github.com/onflow/flow-go/utils/grpcutils"
 	"github.com/onflow/flow-go/utils/io"
 
 	"github.com/rs/zerolog"
