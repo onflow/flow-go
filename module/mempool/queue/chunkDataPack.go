c6913ca58bce3575226d0bd7fbccdc2b205473c1
Yahya Hassanzadeh <yhassanzadeh13@ku.edu.tr>
2022-08-04 12:10:42 -0700

    adds locks

--- module/mempool/queue/chunkDataPack.go
+++ module/mempool/queue/chunkDataPack.go
@@ -1,6 +1,8 @@
 package queue
 
 import (
+	"sync"
+
 	"github.com/onflow/flow-go/model/flow"
 	"github.com/onflow/flow-go/module"
 	"github.com/onflow/flow-go/module/mempool"
@@ -11,6 +13,7 @@ import (
 )
 
 type ChunkDataPackRequestQueue struct {
+	mu        sync.Mutex
 	logger    zerolog.Logger
 	cache     *herocache.Cache
 	sizeLimit uint
@@ -31,6 +34,9 @@ func NewChunkDataPackRequestQueue(sizeLimit uint32, logger zerolog.Logger, colle
 }
 
 func (c *ChunkDataPackRequestQueue) Push(chunkId flow.Identifier, requesterId flow.Identifier) bool {
+	c.mu.Lock()
+	defer c.mu.Unlock()
+
 	lg := c.logger.With().
 		Hex("chunk_id", logging.ID(chunkId)).
 		Hex("requester_id", logging.ID(requesterId)).Logger()
@@ -49,6 +55,9 @@ func (c *ChunkDataPackRequestQueue) Push(chunkId flow.Identifier, requesterId fl
 }
 
 func (c *ChunkDataPackRequestQueue) Pop() (flow.Identifier, flow.Identifier, bool) {
+	c.mu.Lock()
+	defer c.mu.Unlock()
+
 	head, ok := c.cache.Head()
 	if !ok {
 		// cache is empty, and there is no head yet to pop.
@@ -60,7 +69,10 @@ func (c *ChunkDataPackRequestQueue) Pop() (flow.Identifier, flow.Identifier, boo
 	return request.requesterId, request.chunkId, true
 }
 
-func (c ChunkDataPackRequestQueue) Size() uint {
+func (c *ChunkDataPackRequestQueue) Size() uint {
+	c.mu.Lock()
+	defer c.mu.Unlock()
+
 	return c.cache.Size()
 }
 
