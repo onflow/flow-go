b2d59ca779a261cd111b9d39d814a0d988c2867f
Jordan Schalm <jordan@dapperlabs.com>
2022-08-10 16:40:46 -0700

    complex and byzantine test cases

--- consensus/hotstuff/forks/forks_test.go
+++ consensus/hotstuff/forks/forks_test.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"testing"
 
+	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/mock"
 	"github.com/stretchr/testify/require"
 
@@ -18,7 +19,7 @@ import (
 
 // receives [1,2], [2,3]
 // it should not finalize any block
-func TestFinalize_1Chain(t *testing.T) {
+func TestFinalize_Direct1Chain(t *testing.T) {
 	builder := NewBlockBuilder()
 	builder.Add(1, 2)
 	builder.Add(2, 3)
@@ -91,84 +92,15 @@ func TestFinalize_IndirectDirect2Chain(t *testing.T) {
 	requireNoBlocksFinalized(t, forks)
 }
 
-// receives [1,2], [2,3], [3,4], [4,5], [4,6], [6,8]
-// it should finalize [1,2]
-func TestLocked2(t *testing.T) {
+// receives [1,3] [3,5] [5,6] [6,7] [7,8]
+// it should not finalize any blocks
+func TestFinalize_Direct2ChainOnIndirect(t *testing.T) {
 	builder := NewBlockBuilder()
-	builder.Add(1, 2)
-	builder.Add(2, 3)
-	builder.Add(3, 4)
-	builder.Add(4, 5)
-	builder.Add(4, 6)
-	builder.Add(6, 8)
-
-	blocks, err := builder.Blocks()
-	require.Nil(t, err)
-
-	forks, _ := newForks(t)
-
-	err = addBlocksToForks(forks, blocks)
-	require.Nil(t, err)
-
-	requireLatestFinalizedBlock(t, forks, 1, 2)
-}
-
-// receives [1,2], [2,3], [3,4], [4,5], [4,6], [6,8], [8,10]
-// it should finalize [1,2], it should lock [4,6].
-func TestLocked3(t *testing.T) {
-	builder := NewBlockBuilder()
-	builder.Add(1, 2)
-	builder.Add(2, 3)
-	builder.Add(3, 4)
-	builder.Add(4, 5)
-	builder.Add(4, 6)
-	builder.Add(6, 8)
-	builder.Add(8, 10)
-
-	blocks, err := builder.Blocks()
-	require.Nil(t, err)
-
-	forks, _ := newForks(t)
-
-	err = addBlocksToForks(forks, blocks)
-	require.Nil(t, err)
-
-	requireLatestFinalizedBlock(t, forks, 1, 2)
-}
-
-// receives [1,2], [2,3], [3,4], [4,5], [5,6]
-// it should finalize [2,3], it should lock [3,4]
-func TestFinalizedDirect3builder(t *testing.T) {
-	builder := NewBlockBuilder()
-	builder.Add(1, 2)
-	builder.Add(2, 3)
-	builder.Add(3, 4)
-	builder.Add(4, 5)
-	builder.Add(5, 6)
-
-	blocks, err := builder.Blocks()
-	require.Nil(t, err)
-
-	forks, _ := newForks(t)
-
-	err = addBlocksToForks(forks, blocks)
-	require.Nil(t, err)
-
-	requireLatestFinalizedBlock(t, forks, 2, 3)
-}
-
-// receives [1,2], [2,3], [3,4], [4,5], [5,6], [6,7], [7,8], [8, 9]
-// it should finalize [5,6], it should lock [6,7]
-func TestFinalizedDirect3builder2(t *testing.T) {
-	builder := NewBlockBuilder()
-	builder.Add(1, 2)
-	builder.Add(2, 3)
-	builder.Add(3, 4)
-	builder.Add(4, 5)
+	builder.Add(1, 3)
+	builder.Add(3, 5)
 	builder.Add(5, 6)
 	builder.Add(6, 7)
 	builder.Add(7, 8)
-	builder.Add(8, 9)
 
 	blocks, err := builder.Blocks()
 	require.Nil(t, err)
@@ -181,243 +113,36 @@ func TestFinalizedDirect3builder2(t *testing.T) {
 	requireLatestFinalizedBlock(t, forks, 5, 6)
 }
 
-// receives [1,2], [2,3], [3,4], [4,5], [5,7],
-// it should finalize [2,3], it should lock [3,4]
-func TestFinalizedDirect2builderPlus1builder(t *testing.T) {
+// receives [1,2], [2,3], [3,4], [4,5], [5,6]
+// it should not finalize any blocks
+func TestFinalize_Direct2ChainOnDirect(t *testing.T) {
 	builder := NewBlockBuilder()
 	builder.Add(1, 2)
 	builder.Add(2, 3)
 	builder.Add(3, 4)
 	builder.Add(4, 5)
-	builder.Add(5, 7)
-
-	blocks, err := builder.Blocks()
-	require.Nil(t, err)
-
-	forks, _ := newForks(t)
-
-	err = addBlocksToForks(forks, blocks)
-	require.Nil(t, err)
-
-	requireLatestFinalizedBlock(t, forks, 2, 3)
-}
-
-// receives [1,2], [2,3], [3,4], [4,5], [4,6],
-// it should finalize [1,2], it should lock [2,3]
-func TestUnfinalized(t *testing.T) {
-	builder := NewBlockBuilder()
-	builder.Add(1, 2)
-	builder.Add(2, 3)
-	builder.Add(3, 4)
-	builder.Add(4, 5)
-	builder.Add(4, 6)
-
-	blocks, err := builder.Blocks()
-	require.Nil(t, err)
-
-	forks, _ := newForks(t)
-
-	err = addBlocksToForks(forks, blocks)
-	require.Nil(t, err)
-
-	requireLatestFinalizedBlock(t, forks, 1, 2)
-}
-
-// receives [1,2], [2,3], [3,4], [4,5], [4,7],
-// it should finalize [1,2], it should lock [2,3]
-func TestUnfinalized2(t *testing.T) {
-	builder := NewBlockBuilder()
-	builder.Add(1, 2)
-	builder.Add(2, 3)
-	builder.Add(3, 4)
-	builder.Add(4, 5)
-	builder.Add(4, 7)
-
-	blocks, err := builder.Blocks()
-	require.Nil(t, err)
-
-	forks, _ := newForks(t)
-
-	err = addBlocksToForks(forks, blocks)
-	require.Nil(t, err)
-
-	requireLatestFinalizedBlock(t, forks, 1, 2)
-}
-
-// Tolerable Forks that extend from locked block (1: might change locked block, 2: not change locked block)
-// receives [1,2], [2,3], [3,4], [4,5], [3,6], [6,7], [7,8]
-// it should finalize [1,2], it should lock [3,6]
-func TestTolerableForksExtendsFromLockedBlock(t *testing.T) {
-	builder := NewBlockBuilder()
-	builder.Add(1, 2)
-	builder.Add(2, 3)
-	builder.Add(3, 4)
-	builder.Add(4, 5)
-	builder.Add(3, 6)
-	builder.Add(6, 7)
-	builder.Add(7, 8)
-
-	blocks, err := builder.Blocks()
-	require.Nil(t, err)
-
-	forks, _ := newForks(t)
-
-	err = addBlocksToForks(forks, blocks)
-	require.Nil(t, err)
-
-	requireLatestFinalizedBlock(t, forks, 1, 2)
-}
-
-// receives [1,2], [2,3], [3,4], [4,5], [4,6], [6,7], [7,8]
-// it should finalize [1,2], it should lock [4,6]
-func TestTolerableForksExtendsFromLockedBlock2(t *testing.T) {
-	builder := NewBlockBuilder()
-	builder.Add(1, 2)
-	builder.Add(2, 3)
-	builder.Add(3, 4)
-	builder.Add(4, 5)
-	builder.Add(4, 6)
-	builder.Add(6, 7)
-	builder.Add(7, 8)
-
-	blocks, err := builder.Blocks()
-	require.Nil(t, err)
-
-	forks, _ := newForks(t)
-
-	err = addBlocksToForks(forks, blocks)
-	require.Nil(t, err)
-
-	requireLatestFinalizedBlock(t, forks, 1, 2)
-}
-
-// receives [1,2], [2,3], [3,4], [4,5], [3,6], [6,7], [7,8], [8,9]
-// it should finalize [3,6], it should lock [6,7]
-func TestTolerableForksExtendsFromLockedBlock3(t *testing.T) {
-	builder := NewBlockBuilder()
-	builder.Add(1, 2)
-	builder.Add(2, 3)
-	builder.Add(3, 4)
-	builder.Add(4, 5)
-	builder.Add(3, 6)
-	builder.Add(6, 7)
-	builder.Add(7, 8)
-	builder.Add(8, 9)
-
-	blocks, err := builder.Blocks()
-	require.Nil(t, err)
-
-	forks, _ := newForks(t)
-
-	err = addBlocksToForks(forks, blocks)
-	require.Nil(t, err)
-
-	requireLatestFinalizedBlock(t, forks, 3, 6)
-}
-
-// receives [1,2], [2,3], [3,4], [4,5], [4,6], [6,7], [7,8], [8,9]
-// it should finalize [4,6], it should lock [6,7]
-func TestTolerableForksExtendsFromLockedBlock4(t *testing.T) {
-	builder := NewBlockBuilder()
-	builder.Add(1, 2)
-	builder.Add(2, 3)
-	builder.Add(3, 4)
-	builder.Add(4, 5)
-	builder.Add(4, 6)
-	builder.Add(6, 7)
-	builder.Add(7, 8)
-	builder.Add(8, 9)
-
-	blocks, err := builder.Blocks()
-	require.Nil(t, err)
-
-	forks, _ := newForks(t)
-
-	err = addBlocksToForks(forks, blocks)
-	require.Nil(t, err)
-
-	requireLatestFinalizedBlock(t, forks, 4, 6)
-}
-
-// receives [1,2], [2,3], [3,4], [4,5], [4,6], [6,7], [7,8], [8,10]
-// it should finalize [3,6], it should lock [6,7]
-func TestTolerableForksExtendsFromLockedBlock5(t *testing.T) {
-	builder := NewBlockBuilder()
-	builder.Add(1, 2)
-	builder.Add(2, 3)
-	builder.Add(3, 4)
-	builder.Add(4, 5)
-	builder.Add(3, 6)
-	builder.Add(6, 7)
-	builder.Add(7, 8)
-	builder.Add(8, 10)
-
-	blocks, err := builder.Blocks()
-	require.Nil(t, err)
-
-	forks, _ := newForks(t)
-
-	err = addBlocksToForks(forks, blocks)
-	require.Nil(t, err)
-
-	requireLatestFinalizedBlock(t, forks, 3, 6)
-}
-
-// receives [1,2], [2,3], [3,4], [4,5], [2,6]
-// it should finalize [1,2], it should lock [2,3]
-func TestTolerableForksNotExtendsFromLockedBlock(t *testing.T) {
-	builder := NewBlockBuilder()
-	builder.Add(1, 2)
-	builder.Add(2, 3)
-	builder.Add(3, 4)
-	builder.Add(4, 5)
-	builder.Add(2, 6)
-
-	blocks, err := builder.Blocks()
-	require.Nil(t, err)
-
-	forks, _ := newForks(t)
-
-	err = addBlocksToForks(forks, blocks)
-	require.Nil(t, err)
-
-	requireLatestFinalizedBlock(t, forks, 1, 2)
-}
-
-// receives [1,2], [2,3], [3,4], [4,5], [2,6], [5,6]
-// it should finalize [2,3], it should lock [3,4], because [2,6] is replaced by [5,6]
-func TestTolerableForksNotExtendsFromLockedBlock2(t *testing.T) {
-	builder := NewBlockBuilder()
-	builder.Add(1, 2)
-	builder.Add(2, 3)
-	builder.Add(3, 4)
-	builder.Add(4, 5)
-	builder.Add(2, 6)
 	builder.Add(5, 6)
 
 	blocks, err := builder.Blocks()
 	require.Nil(t, err)
 
-	forks, notifier := newForks(t)
-	notifier.On("OnDoubleProposeDetected", blocks[5].Block, blocks[4].Block).Return(nil)
+	forks, _ := newForks(t)
 
 	err = addBlocksToForks(forks, blocks)
 	require.Nil(t, err)
 
-	requireLatestFinalizedBlock(t, forks, 2, 3)
-	notifier.AssertExpectations(t)
+	requireLatestFinalizedBlock(t, forks, 3, 4)
 }
 
-// receives [1,2], [2,3], [3,4], [4,5], [2,6], [6,7]
-// it should finalize [1,2], it should lock [2,3]
-func TestTolerableForksNotExtendsFromLockedBlock3(t *testing.T) {
+// receives [1,2] [2,3] [3,5] [3,6] [3,7]
+// it should finalize [1,2]
+func TestFinalize_Multiple2Chains(t *testing.T) {
 	builder := NewBlockBuilder()
 	builder.Add(1, 2)
 	builder.Add(2, 3)
-	builder.Add(3, 4)
-	builder.Add(4, 5)
-	builder.Add(2, 6)
-	builder.Add(6, 7)
+	builder.Add(3, 5)
+	builder.Add(3, 6)
+	builder.Add(3, 7)
 
 	blocks, err := builder.Blocks()
 	require.Nil(t, err)
@@ -430,17 +155,15 @@ func TestTolerableForksNotExtendsFromLockedBlock3(t *testing.T) {
 	requireLatestFinalizedBlock(t, forks, 1, 2)
 }
 
-// receives [1,2], [2,3], [3,4], [4,5], [2,6], [6,7],[7,8]
-// it should finalize [1,2], it should lock [2,6]
-func TestTolerableForksNotExtendsFromLockedBlock4(t *testing.T) {
+// receives [1,2] [2,3] [2,4] [4,5] [5,6]
+// it should finalize [2,4]
+func TestFinalize_OrphanedFork(t *testing.T) {
 	builder := NewBlockBuilder()
 	builder.Add(1, 2)
 	builder.Add(2, 3)
-	builder.Add(3, 4)
+	builder.Add(2, 4)
 	builder.Add(4, 5)
-	builder.Add(2, 6)
-	builder.Add(6, 7)
-	builder.Add(7, 8)
+	builder.Add(5, 6)
 
 	blocks, err := builder.Blocks()
 	require.Nil(t, err)
@@ -450,22 +173,20 @@ func TestTolerableForksNotExtendsFromLockedBlock4(t *testing.T) {
 	err = addBlocksToForks(forks, blocks)
 	require.Nil(t, err)
 
-	requireLatestFinalizedBlock(t, forks, 1, 2)
+	requireLatestFinalizedBlock(t, forks, 2, 4)
 }
 
-// receives [1,2], [2,3], [2,3], [3,4], [3,4], [4,5], [4,5], [5,6], [5,6]
-// it should finalize [2,3], it should lock [3,4]
+// receives [1,2], [2,3], [2,3], [3,4], [3,4], [4,5], [4,5]
+// it should finalize [2,3]
 func TestDuplication(t *testing.T) {
 	builder := NewBlockBuilder()
 	builder.Add(1, 2)
 	builder.Add(2, 3)
-	builder.Add(3, 4)
 	builder.Add(2, 3)
 	builder.Add(3, 4)
+	builder.Add(3, 4)
 	builder.Add(4, 5)
-	builder.Add(5, 6)
 	builder.Add(4, 5)
-	builder.Add(5, 6)
 
 	blocks, err := builder.Blocks()
 	require.Nil(t, err)
@@ -478,14 +199,13 @@ func TestDuplication(t *testing.T) {
 	requireLatestFinalizedBlock(t, forks, 2, 3)
 }
 
-// receives [1,2], [2,3], [3,4], [4,5], [1,6]
-// it should finalize [1,2], it should lock [2,3]
+// receives [1,2], [2,3], [3,4], [1,6]
+// it should finalize [1,2]
 func TestIgnoreBlocksBelowFinalizedView(t *testing.T) {
 	builder := NewBlockBuilder()
 	builder.Add(1, 2)
 	builder.Add(2, 3)
 	builder.Add(3, 4)
-	builder.Add(4, 5)
 	builder.Add(1, 6)
 
 	blocks, err := builder.Blocks()
@@ -499,67 +219,61 @@ func TestIgnoreBlocksBelowFinalizedView(t *testing.T) {
 	requireLatestFinalizedBlock(t, forks, 1, 2)
 }
 
-// receives [1,2], [2,3], [3,4], [4,5], [3,6], [5,6'].
-// it should finalize block [2,3], and emits an DoubleProposal event with ([3,6], [5,6'])
+// receives [1,2], [2,3], [3,4], [4,5], [3,5']
+// it should finalize block [2,3], and emits an DoubleProposal event with ([3,5'], [4,5])
 func TestDoubleProposal(t *testing.T) {
 	builder := NewBlockBuilder()
 	builder.Add(1, 2)
 	builder.Add(2, 3)
 	builder.Add(3, 4)
 	builder.Add(4, 5)
-	builder.Add(3, 6)
-	builder.AddVersioned(5, 6, 0, 1)
+	builder.AddVersioned(3, 5, 0, 1)
 
 	blocks, err := builder.Blocks()
 	require.Nil(t, err)
 
 	forks, notifier := newForks(t)
-	notifier.On("OnDoubleProposeDetected", blocks[5].Block, blocks[4].Block).Return(nil)
+	notifier.On("OnDoubleProposeDetected", blocks[4].Block, blocks[3].Block).Once()
 
 	err = addBlocksToForks(forks, blocks)
 	require.Nil(t, err)
 
 	requireLatestFinalizedBlock(t, forks, 2, 3)
-	notifier.AssertExpectations(t)
 }
 
-// receives [1,2], [2,3], [3,4], [3,4'], [4,5], [4',6].
-// it should return fatal error, because conflicting blocks 4 and 4'
-// both received enough votes for QC
-func TestUntolerableForks(t *testing.T) {
+// receives [1,2] [2,3] [2,3'] [3,4] [3',5]
+// it should return fatal error, because conflicting blocks 3 and 3' both received enough votes for QC
+func TestConflictingQCs(t *testing.T) {
 	builder := NewBlockBuilder()
 
 	builder.Add(1, 2)
 	builder.Add(2, 3)
-	builder.Add(3, 4)
-	builder.AddVersioned(3, 4, 0, 1) // make a special view 4
-	builder.Add(4, 5)
-	builder.AddVersioned(4, 6, 1, 0) // make a special view 6 extends from special view 4
+	builder.AddVersioned(2, 3, 0, 1) // make a conflicting proposal at view 3
+	builder.Add(3, 4)                // creates a QC for 3
+	builder.AddVersioned(3, 5, 1, 0) // creates a QC for 3'
 
 	blocks, err := builder.Blocks()
 	require.Nil(t, err)
 
 	forks, notifier := newForks(t)
-	notifier.On("OnDoubleProposeDetected", blocks[3].Block, blocks[2].Block).Return(nil)
+	notifier.On("OnDoubleProposeDetected", blocks[2].Block, blocks[1].Block).Return(nil)
 
 	err = addBlocksToForks(forks, blocks)
 	require.NotNil(t, err)
-	notifier.AssertExpectations(t)
+	assert.True(t, model.IsByzantineThresholdExceededError(err))
 }
 
-// receives [1,2], [2,3], [2,7], [3,4], [4,5], [5,6], [7,8], [8,9], [9,10]
-// It should return fatal error, because a fork below locked block got finalized
-func TestUntolerableForks2(t *testing.T) {
+// receives [1,2] [2,3] [2,6] [3,4] [4,5] [6,7] [7,8]
+// It should return fatal error, because 2 conflicting forks were finalized
+func TestConflictingFinalizedForks(t *testing.T) {
 	builder := NewBlockBuilder()
 	builder.Add(1, 2)
 	builder.Add(2, 3)
 	builder.Add(3, 4)
-	builder.Add(4, 5)
-	builder.Add(5, 6) // this finalizes (2,3)
-	builder.Add(2, 7)
-	builder.Add(7, 8)
-	builder.Add(8, 9)
-	builder.Add(9, 10) // this finalizes (2,7), which is a conflicting fork with (2,3)
+	builder.Add(4, 5) // finalizes (2,3)
+	builder.Add(2, 6)
+	builder.Add(6, 7)
+	builder.Add(7, 8) // finalizes (2,6) conflicts with (2,3)
 
 	blocks, err := builder.Blocks()
 	require.Nil(t, err)
@@ -568,6 +282,7 @@ func TestUntolerableForks2(t *testing.T) {
 
 	err = addBlocksToForks(forks, blocks)
 	require.Error(t, err)
+	assert.True(t, model.IsByzantineThresholdExceededError(err))
 }
 
 // TestNotification tests that notifier gets correct notifications when incorporating block as well as finalization events.
@@ -625,10 +340,10 @@ func TestNewestView(t *testing.T) {
 // ========== internal functions ===============
 
 func newForks(t *testing.T) (*Forks, *mocks.Consumer) {
-	notifier := &mocks.Consumer{}
+	notifier := mocks.NewConsumer(t)
 	notifier.On("OnBlockIncorporated", mock.Anything).Return(nil)
 	notifier.On("OnFinalizedBlock", mock.Anything).Return(nil)
-	finalizationCallback := &mockmodule.Finalizer{}
+	finalizationCallback := mockmodule.NewFinalizer(t)
 	finalizationCallback.On("MakeFinal", mock.Anything).Return(nil)
 	finalizationCallback.On("MakeValid", mock.Anything).Return(nil)
 
