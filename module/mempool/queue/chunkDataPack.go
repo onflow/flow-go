5c34720fd59e4e2051007ad9ed7c8767d63eb7a7
Yahya Hassanzadeh <yhassanzadeh13@ku.edu.tr>
2022-08-10 12:34:36 -0700

    nit chunk data pack request queue

--- module/mempool/queue/chunkDataPack.go
+++ module/mempool/queue/chunkDataPack.go
@@ -10,12 +10,12 @@ import (
 	"github.com/onflow/flow-go/module/mempool"
 	herocache "github.com/onflow/flow-go/module/mempool/herocache/backdata"
 	"github.com/onflow/flow-go/module/mempool/herocache/backdata/heropool"
-	"github.com/onflow/flow-go/utils/logging"
 )
 
+// ChunkDataPackRequestQueue implements a HeroCache-based in-memory queue for storing chunk data pack requests.
+// It is designed to be utilized at Execution Nodes to maintain and respond chunk data pack requests.
 type ChunkDataPackRequestQueue struct {
-	mu        sync.Mutex
-	logger    zerolog.Logger
+	mu        sync.RWMutex
 	cache     *herocache.Cache
 	sizeLimit uint
 }
@@ -34,20 +34,18 @@ func NewChunkDataPackRequestQueue(sizeLimit uint32, logger zerolog.Logger, colle
 	}
 }
 
+// Push stores chunk data pack request into the queue.
+// Boolean returned variable determines whether push was successful, i.e.,
+// push may be dropped if queue is full or already exists.
 func (c *ChunkDataPackRequestQueue) Push(chunkId flow.Identifier, requesterId flow.Identifier) bool {
 	c.mu.Lock()
 	defer c.mu.Unlock()
 
-	lg := c.logger.With().
-		Hex("chunk_id", logging.ID(chunkId)).
-		Hex("requester_id", logging.ID(requesterId)).Logger()
-
 	if c.cache.Size() >= c.sizeLimit {
-		lg.Debug().Msg("cannot push chunk data pack request to queue, queue is full")
 		return false
 	}
 
-	req := chunkDataPackRequest{
+	req := chunkDataPackRequestEntity{
 		ChunkDataPackRequest: mempool.ChunkDataPackRequest{
 			ChunkId:     chunkId,
 			RequesterId: requesterId,
@@ -61,8 +59,8 @@ func (c *ChunkDataPackRequestQueue) Push(chunkId flow.Identifier, requesterId fl
 // Head returns the head of queue.
 // Boolean return value determines whether there is a head available.
 func (c *ChunkDataPackRequestQueue) Head() (*mempool.ChunkDataPackRequest, bool) {
-	c.mu.Lock()
-	defer c.mu.Unlock()
+	c.mu.RLock()
+	defer c.mu.RUnlock()
 
 	head, ok := c.cache.Head()
 	if !ok {
@@ -70,10 +68,12 @@ func (c *ChunkDataPackRequestQueue) Head() (*mempool.ChunkDataPackRequest, bool)
 		return nil, false
 	}
 
-	request := head.(chunkDataPackRequest)
+	request := head.(chunkDataPackRequestEntity)
 	return &mempool.ChunkDataPackRequest{RequesterId: request.RequesterId, ChunkId: request.ChunkId}, true
 }
 
+// Pop removes and returns the head of queue, and updates the head to the next element.
+// Boolean return value determines whether pop is successful, i.e., poping an empty queue returns false.
 func (c *ChunkDataPackRequestQueue) Pop() (*mempool.ChunkDataPackRequest, bool) {
 	c.mu.Lock()
 	defer c.mu.Unlock()
@@ -85,31 +85,33 @@ func (c *ChunkDataPackRequestQueue) Pop() (*mempool.ChunkDataPackRequest, bool) 
 	}
 
 	c.cache.Remove(head.ID())
-	request := head.(chunkDataPackRequest)
+	request := head.(chunkDataPackRequestEntity)
 	return &mempool.ChunkDataPackRequest{RequesterId: request.RequesterId, ChunkId: request.ChunkId}, true
 }
 
 func (c *ChunkDataPackRequestQueue) Size() uint {
-	c.mu.Lock()
-	defer c.mu.Unlock()
+	c.mu.RLock()
+	defer c.mu.RUnlock()
 
 	return c.cache.Size()
 }
 
-type chunkDataPackRequest struct {
+// chunkDataPackRequestEntity is a wrapper around ChunkDataPackRequest that implements Entity interface for it, and
+// also internally caches its identifier.
+type chunkDataPackRequestEntity struct {
 	mempool.ChunkDataPackRequest
 	// caching identifier to avoid cpu overhead
 	// per query.
 	id flow.Identifier
 }
 
-var _ flow.Entity = &chunkDataPackRequest{}
+var _ flow.Entity = &chunkDataPackRequestEntity{}
 
-func (c chunkDataPackRequest) ID() flow.Identifier {
+func (c chunkDataPackRequestEntity) ID() flow.Identifier {
 	return c.id
 }
 
-func (c chunkDataPackRequest) Checksum() flow.Identifier {
+func (c chunkDataPackRequestEntity) Checksum() flow.Identifier {
 	return c.id
 }
 
