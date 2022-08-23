3b9b0ed27bef08160c1adaf6717b5139a1e6eada
Khalil Claybon <khalil.claybon@dapperlabs.com>
2022-08-23 08:44:16 -0400

    rename isAllowed -> peerIDPassesAllFilters

--- network/p2p/connGater.go
+++ network/p2p/connGater.go
@@ -52,7 +52,7 @@ func NewConnGater(log zerolog.Logger, opts ...ConnGaterOption) *ConnGater {
 
 // InterceptPeerDial - a callback which allows or disallows outbound connection
 func (c *ConnGater) InterceptPeerDial(p peer.ID) bool {
-	return c.isAllowed(p, c.onInterceptPeerDialFilters)
+	return c.peerIDPassesAllFilters(p, c.onInterceptPeerDialFilters)
 }
 
 // InterceptAddrDial is not used. Currently, allowlisting is only implemented by Peer IDs and not multi-addresses
@@ -70,7 +70,7 @@ func (c *ConnGater) InterceptAccept(cm network.ConnMultiaddrs) bool {
 func (c *ConnGater) InterceptSecured(dir network.Direction, p peer.ID, addr network.ConnMultiaddrs) bool {
 	switch dir {
 	case network.DirInbound:
-		allowed := c.isAllowed(p, c.onInterceptSecuredFilters)
+		allowed := c.peerIDPassesAllFilters(p, c.onInterceptSecuredFilters)
 		if !allowed {
 			// log the illegal connection attempt from the remote node
 			c.log.Info().
@@ -91,7 +91,7 @@ func (c *ConnGater) InterceptUpgraded(network.Conn) (allow bool, reason control.
 	return true, 0
 }
 
-func (c *ConnGater) isAllowed(p peer.ID, filters []PeerFilter) bool {
+func (c *ConnGater) peerIDPassesAllFilters(p peer.ID, filters []PeerFilter) bool {
 	for _, allowed := range filters {
 		if !allowed(p) {
 			return false
