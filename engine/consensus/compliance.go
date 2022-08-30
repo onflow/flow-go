7e1f09941bfe8d4fd36180660d2b7eac28dc0758
Jordan Schalm <jordan@dapperlabs.com>
2022-08-30 13:11:58 -0700

    refactor: update provider engine
    * no longer conforms to deprecated Engine interface

---
+++ engine/consensus/compliance.go
@@ -0,0 +1,17 @@
+package consensus
+
+import (
+	"github.com/onflow/flow-go/model/messages"
+)
+
+// ProposalProvider provides proposals created by this node to non-consensus nodes.
+type ProposalProvider interface {
+	// ProvideProposal asynchronously submits our proposal to all non-consensus nodes.
+	ProvideProposal(proposal *messages.BlockProposal)
+}
+
+// ComplianceProcessor accepts blocks for ingestion and processing by the compliance layer.
+// It is used to ingest blocks received through other means than the HotStuff happy path,
+// like the block synchronization engine.
+type ComplianceProcessor interface {
+}
