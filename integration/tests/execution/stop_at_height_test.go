87b94ecf74201c1e6a28bdf67dea91876b38e765
Maks Pawlak <120831+m4ksio@users.noreply.github.com>
2022-09-14 13:18:49 -0600

    wip

---
+++ integration/tests/execution/stop_at_height_test.go
@@ -0,0 +1,91 @@
+package execution
+
+import (
+	"testing"
+	"time"
+
+	"github.com/stretchr/testify/require"
+	"github.com/stretchr/testify/suite"
+)
+
+func TestStopAtHeight(t *testing.T) {
+	suite.Run(t, new(TestStopAtHeightSuite))
+}
+
+type TestStopAtHeightSuite struct {
+	Suite
+}
+
+func (s *TestStopAtHeightSuite) TestStopAtHeight() {
+
+	//chainID := s.net.Root().Header.ChainID
+	//chain := chainID.Chain()
+	//serviceAddress := chain.ServiceAddress()
+
+	// wait for next height finalized (potentially first height), called blockA
+	//currentFinalized := s.BlockState.HighestFinalizedHeight()
+
+	enContainer := s.net.ContainerByID(s.exe1ID)
+
+	enContainer.Ports
+
+	err := enContainer.Pause()
+	require.NoError(s.T(), err)
+
+	err = enContainer.WaitForContainerStopped(20 * time.Second)
+	require.NoError(s.T(), err)
+
+	//blockA := s.BlockState.WaitForHighestFinalizedProgress(s.T(), currentFinalized)
+	//s.T().Logf("got blockA height %v ID %v\n", blockA.Header.Height, blockA.Header.ID())
+	//
+	//// send transaction
+	//err = s.AccessClient().DeployContract(context.Background(), sdk.Identifier(s.net.Root().ID()), lib.CounterContract)
+	//require.NoError(s.T(), err, "could not deploy counter")
+	//
+	//// wait until we see a different state commitment for a finalized block, call that block blockB
+	//blockB, erBlockB := lib.WaitUntilFinalizedStateCommitmentChanged(s.T(), s.BlockState, s.ReceiptState)
+	//s.T().Logf("got blockB height %v ID %v\n", blockB.Header.Height, blockB.Header.ID())
+	//
+	//// final states
+	//finalStateBlockB, err := erBlockB.ExecutionResult.FinalStateCommitment()
+	//require.NoError(s.T(), err)
+	//
+	//// send transaction that panics and should revert
+	//tx := lib.SDKTransactionFixture(
+	//	lib.WithTransactionDSL(lib.CreateCounterPanicTx(chain)),
+	//	lib.WithReferenceBlock(sdk.Identifier(s.net.Root().ID())),
+	//	lib.WithChainID(chainID),
+	//)
+	//
+	//err = s.AccessClient().SendTransaction(context.Background(), &tx)
+	//require.NoError(s.T(), err, "could not send tx to create counter that should panic")
+	//
+	//// send transaction that has no sigs and should not execute
+	//tx = lib.SDKTransactionFixture(
+	//	lib.WithTransactionDSL(lib.CreateCounterTx(sdk.Address(serviceAddress))),
+	//	lib.WithReferenceBlock(sdk.Identifier(s.net.Root().ID())),
+	//	lib.WithChainID(chainID),
+	//)
+	//tx.PayloadSignatures = nil
+	//tx.EnvelopeSignatures = nil
+	//
+	//s.BlockState.HighestFinalizedHeight()
+	//
+	//currentProposed := s.BlockState.HighestProposedHeight()
+	//err = s.AccessClient().SendTransaction(context.Background(), &tx)
+	//require.NoError(s.T(), err, "could not send tx to create counter with wrong sig")
+	//
+	//// wait until the next proposed block is finalized, called blockC
+	//blockC := s.BlockState.WaitUntilNextHeightFinalized(s.T(), currentProposed)
+	//s.T().Logf("got blockC height %v ID %v\n", blockC.Header.Height, blockC.Header.ID())
+	//
+	//// wait for execution receipt for blockC from execution node 1
+	//erBlockC := s.ReceiptState.WaitForReceiptFrom(s.T(), blockC.Header.ID(), s.exe1ID)
+	//finalStateBlockC, err := erBlockC.ExecutionResult.FinalStateCommitment()
+	//require.NoError(s.T(), err)
+	//
+	//s.T().Logf("got erBlockC with SC %x\n", finalStateBlockC)
+	//
+	//// assert that state did not change between blockB and blockC
+	//require.Equal(s.T(), finalStateBlockB, finalStateBlockC)
+}
