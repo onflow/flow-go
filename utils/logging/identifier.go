c558e7442dc08195fc41d72a942280195ca29d57
Peter Argue <89119817+peterargue@users.noreply.github.com>
2022-10-07 09:24:38 -0700

    move new const to a separate file

--- utils/logging/identifier.go
+++ utils/logging/identifier.go
@@ -9,10 +9,6 @@ import (
 	"github.com/onflow/flow-go/model/flow"
 )
 
-// KeySuspicious is a logging label that is used to flag the log event as suspicious behavior
-// This is used to add an easily searchable label to the log event
-const KeySuspicious = "suspicious"
-
 func Entity(entity flow.Entity) []byte {
 	id := entity.ID()
 	return id[:]
