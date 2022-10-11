c2abd7b79472750cb5694d1014a18d14374ed777
Khalil Claybon <khalil.claybon@dapperlabs.com>
2022-10-11 12:16:33 -0400

    fix imports

--- utils/unittest/network/p2p.go
+++ utils/unittest/network/p2p.go
@@ -2,12 +2,10 @@ package network
 
 import (
 	"testing"
-	"time"
 
 	"github.com/stretchr/testify/assert"
 
 	"github.com/onflow/flow-go/network/p2p/p2pnode"
-	"github.com/onflow/flow-go/utils/unittest"
 )
 
 // StopNodes stop all nodes in the input slice
@@ -19,7 +17,6 @@ func StopNodes(t *testing.T, nodes []*p2pnode.Node) {
 
 // StopNode stops node
 func StopNode(t *testing.T, node *p2pnode.Node) {
-	done, err := node.Stop()
+	err := node.Stop()
 	assert.NoError(t, err)
-	unittest.RequireCloseBefore(t, done, 1*time.Second, "could not stop node on ime")
 }
