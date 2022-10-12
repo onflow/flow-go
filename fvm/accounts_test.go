d1dd90ed612b6bfa03b557f355cb1ebfba1c6b10
Bastian Müller <bastian@axiomzen.co>
2022-10-12 07:13:10 -0700

    avoid oppressive terminology

--- fvm/accounts_test.go
+++ fvm/accounts_test.go
@@ -266,7 +266,7 @@ transaction {
 			?? panic("Unable to borrow reference to administrator resource")
 	}
 	execute {
-		// Remove account from account creator whitelist.
+		// Remove account from account creator allowlist.
 		//
 		// Will emit AccountCreatorRemoved(accountCreator: accountCreator).
 		//
@@ -286,7 +286,7 @@ transaction {
 			?? panic("Unable to borrow reference to administrator resource")
 	}
 	execute {
-		// Add account to account creator whitelist.
+		// Add account to account creator allowlist.
 		//
 		// Will emit AccountCreatorAdded(accountCreator: accountCreator).
 		//
