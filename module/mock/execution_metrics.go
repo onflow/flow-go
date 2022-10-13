97abb1dc0bbfa9d252e7a8044449f82ccb4e3242
ramtinms <ramtin.seraj@dapperlabs.com>
2022-10-13 14:29:27 -0700

    update mocks

--- module/mock/execution_metrics.go
+++ module/mock/execution_metrics.go
@@ -34,8 +34,8 @@ func (_m *ExecutionMetrics) ExecutionBlockExecuted(dur time.Duration, compUsed u
 	_m.Called(dur, compUsed, memoryUsed, eventCounts, eventSize, txCounts, colCounts)
 }
 
-// ExecutionChunkDataPackGenerated provides a mock function with given fields: dur, proofSize, numberOfTransactions
-func (_m *ExecutionMetrics) ExecutionChunkDataPackGenerated( proofSize int, numberOfTransactions int) {
+// ExecutionChunkDataPackGenerated provides a mock function with given fields: proofSize, numberOfTransactions
+func (_m *ExecutionMetrics) ExecutionChunkDataPackGenerated(proofSize int, numberOfTransactions int) {
 	_m.Called(proofSize, numberOfTransactions)
 }
 
