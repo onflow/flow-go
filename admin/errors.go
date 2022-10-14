38a38aab34e9c0085ae51a0d67bc5b10ba49f45e
Jordan Schalm <jordan@dapperlabs.com>
2022-10-14 12:38:36 +0200

    define expected error for admin commands

--- admin/errors.go
+++ admin/errors.go
@@ -5,35 +5,39 @@ import (
 	"fmt"
 )
 
-var ErrValidatorReqDataFormat error = errors.New("wrong input format: expected JSON")
+// ErrValidatorReqDataFormat is returned when an admin request is not in the expected format.
+// For example, if the input is a JSON number, but the command expects a map, type conversion
+// of the input will fail.
+var ErrValidatorReqDataFormat = NewInvalidAdminReqErrorf("invalid request format")
 
-type InvalidAdminParameterError struct {
-	field     string
-	msg       string
-	actualVal interface{}
+// InvalidAdminReqError indicates that an admin request has failed validation, and
+// the request will not be processed. All Validator functions must return this error
+// if the
+type InvalidAdminReqError struct {
+	Err error
 }
 
-func NewInvalidAdminParameterError(field string, msg string, actualVal any) *InvalidAdminParameterError {
-	return &InvalidAdminParameterError{
-		field:     field,
-		msg:       fmt.Sprintf("invalid value for '%s': %s. Got: %v", field, msg, actualVal),
-		actualVal: actualVal,
+func NewInvalidAdminReqErrorf(msg string, args ...any) InvalidAdminReqError {
+	return InvalidAdminReqError{
+		Err: fmt.Errorf(msg, args...),
 	}
 }
 
+// NewInvalidAdminReqParameterError returns an InvalidAdminReqError indicating that
+// a field of the request has an invalid value.
+func NewInvalidAdminReqParameterError(field string, msg string, actualVal any) InvalidAdminReqError {
+	return NewInvalidAdminReqErrorf("invalid value for '%s': %s. Got: %v", field, msg, actualVal)
+}
+
 func IsInvalidAdminParameterError(err error) bool {
-	var target *InvalidAdminParameterError
+	var target InvalidAdminReqError
 	return errors.As(err, &target)
 }
 
-func (i *InvalidAdminParameterError) Error() string {
-	return i.msg
+func (err InvalidAdminReqError) Error() string {
+	return err.Err.Error()
 }
 
-func (i *InvalidAdminParameterError) Field() string {
-	return i.field
-}
-
-func (i *InvalidAdminParameterError) ActualVal() any {
-	return i.actualVal
+func (err InvalidAdminReqError) Unwrap() error {
+	return err.Err
 }
