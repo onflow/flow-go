1b91e6458168edef3a4b48687270b91c00095b1a
Khalil Claybon <khalil.claybon@dapperlabs.com>
2022-10-14 20:09:51 -0400

    rename BandWidthRateLimiterImpl -> BandWidthRateLimiter
    - rename MessageRateLimiterImpl -> MessageRateLimiter

--- network/p2p/unicast/ratelimit/bandwidth_rate_limiter.go
+++ network/p2p/unicast/ratelimit/bandwidth_rate_limiter.go
@@ -13,9 +13,9 @@ import (
 	"github.com/onflow/flow-go/network/message"
 )
 
-// BandWidthRateLimiterImpl unicast rate limiter that limits the bandwidth that can be sent
+// BandWidthRateLimiter unicast rate limiter that limits the bandwidth that can be sent
 // by a peer per some configured interval.
-type BandWidthRateLimiterImpl struct {
+type BandWidthRateLimiter struct {
 	limiters                 *limiter_map.RateLimiterMap
 	limit                    rate.Limit
 	burst                    int
@@ -23,10 +23,10 @@ type BandWidthRateLimiterImpl struct {
 	rateLimitLockoutDuration time.Duration // the amount of time that has to pass before a peer is allowed to connect
 }
 
-// NewBandWidthRateLimiter returns a new BandWidthRateLimiterImpl. The cleanup loop will be started in a
+// NewBandWidthRateLimiter returns a new BandWidthRateLimiter. The cleanup loop will be started in a
 // separate goroutine and should be stopped by calling Close.
-func NewBandWidthRateLimiter(limit rate.Limit, burst int, lockout time.Duration, opts ...p2p.RateLimiterOpt) *BandWidthRateLimiterImpl {
-	l := &BandWidthRateLimiterImpl{
+func NewBandWidthRateLimiter(limit rate.Limit, burst int, lockout time.Duration, opts ...p2p.RateLimiterOpt) *BandWidthRateLimiter {
+	l := &BandWidthRateLimiter{
 		limiters:                 limiter_map.NewLimiterMap(rateLimiterTTL, cleanUpTickInterval),
 		limit:                    limit,
 		burst:                    burst,
@@ -44,7 +44,7 @@ func NewBandWidthRateLimiter(limit rate.Limit, burst int, lockout time.Duration,
 // Allow checks the cached limiter for the peer and returns limiter.AllowN(msg.Size())
 // which will check if a peer is able to send a message of msg.Size().
 // If a limiter is not cached one is created.
-func (b *BandWidthRateLimiterImpl) Allow(peerID peer.ID, msg *message.Message) bool {
+func (b *BandWidthRateLimiter) Allow(peerID peer.ID, msg *message.Message) bool {
 	limiter := b.getLimiter(peerID)
 	if !limiter.AllowN(b.now(), msg.Size()) {
 		b.limiters.UpdateLastRateLimit(peerID, b.now())
@@ -55,7 +55,7 @@ func (b *BandWidthRateLimiterImpl) Allow(peerID peer.ID, msg *message.Message) b
 }
 
 // IsRateLimited returns true is a peer is currently rate limited.
-func (b *BandWidthRateLimiterImpl) IsRateLimited(peerID peer.ID) bool {
+func (b *BandWidthRateLimiter) IsRateLimited(peerID peer.ID) bool {
 	metadata, ok := b.limiters.Get(peerID)
 	if !ok {
 		return false
@@ -64,23 +64,23 @@ func (b *BandWidthRateLimiterImpl) IsRateLimited(peerID peer.ID) bool {
 }
 
 // SetTimeNowFunc overrides the default time.Now func with the GetTimeNow func provided.
-func (b *BandWidthRateLimiterImpl) SetTimeNowFunc(now p2p.GetTimeNow) {
+func (b *BandWidthRateLimiter) SetTimeNowFunc(now p2p.GetTimeNow) {
 	b.now = now
 }
 
 // Start starts cleanup loop for underlying caches.
-func (b *BandWidthRateLimiterImpl) Start() {
+func (b *BandWidthRateLimiter) Start() {
 	go b.limiters.CleanupLoop()
 }
 
 // Stop sends cleanup signal to underlying rate limiters and rate limited peers maps. After the rate limiter
 // is stopped it can not be reused.
-func (b *BandWidthRateLimiterImpl) Stop() {
+func (b *BandWidthRateLimiter) Stop() {
 	b.limiters.Close()
 }
 
 // getLimiter returns limiter for the peerID, if a limiter does not exist one is created and stored.
-func (b *BandWidthRateLimiterImpl) getLimiter(peerID peer.ID) *rate.Limiter {
+func (b *BandWidthRateLimiter) getLimiter(peerID peer.ID) *rate.Limiter {
 	if metadata, ok := b.limiters.Get(peerID); ok {
 		return metadata.Limiter
 	}
--- network/p2p/unicast/ratelimit/message_rate_limiter.go
+++ network/p2p/unicast/ratelimit/message_rate_limiter.go
@@ -12,10 +12,10 @@ import (
 	"github.com/onflow/flow-go/network/p2p/unicast/ratelimit/internal/limiter_map"
 )
 
-// MessageRateLimiterImpl unicast rate limiter that limits the amount of streams that can
+// MessageRateLimiter unicast rate limiter that limits the amount of streams that can
 // be created per some configured interval. A new stream is created each time a libP2P
 // node sends a direct message.
-type MessageRateLimiterImpl struct {
+type MessageRateLimiter struct {
 	limiters                 *limiter_map.RateLimiterMap
 	limit                    rate.Limit
 	burst                    int
@@ -23,10 +23,10 @@ type MessageRateLimiterImpl struct {
 	rateLimitLockoutDuration time.Duration // the amount of time that has to pass before a peer is allowed to connect
 }
 
-// NewMessageRateLimiter returns a new MessageRateLimiterImpl. The cleanup loop will be started in a
+// NewMessageRateLimiter returns a new MessageRateLimiter. The cleanup loop will be started in a
 // separate goroutine and should be stopped by calling Close.
-func NewMessageRateLimiter(limit rate.Limit, burst, lockoutDuration int, opts ...p2p.RateLimiterOpt) *MessageRateLimiterImpl {
-	l := &MessageRateLimiterImpl{
+func NewMessageRateLimiter(limit rate.Limit, burst, lockoutDuration int, opts ...p2p.RateLimiterOpt) *MessageRateLimiter {
+	l := &MessageRateLimiter{
 		limiters:                 limiter_map.NewLimiterMap(rateLimiterTTL, cleanUpTickInterval),
 		limit:                    limit,
 		burst:                    burst,
@@ -43,7 +43,7 @@ func NewMessageRateLimiter(limit rate.Limit, burst, lockoutDuration int, opts ..
 
 // Allow checks the cached limiter for the peer and returns limiter.Allow().
 // If a limiter is not cached for a peer one is created.
-func (s *MessageRateLimiterImpl) Allow(peerID peer.ID, _ *message.Message) bool {
+func (s *MessageRateLimiter) Allow(peerID peer.ID, _ *message.Message) bool {
 	limiter := s.getLimiter(peerID)
 	if !limiter.AllowN(s.now(), 1) {
 		s.limiters.UpdateLastRateLimit(peerID, s.now())
@@ -54,7 +54,7 @@ func (s *MessageRateLimiterImpl) Allow(peerID peer.ID, _ *message.Message) bool 
 }
 
 // IsRateLimited returns true is a peer is currently rate limited.
-func (s *MessageRateLimiterImpl) IsRateLimited(peerID peer.ID) bool {
+func (s *MessageRateLimiter) IsRateLimited(peerID peer.ID) bool {
 	metadata, ok := s.limiters.Get(peerID)
 	if !ok {
 		return false
@@ -63,23 +63,23 @@ func (s *MessageRateLimiterImpl) IsRateLimited(peerID peer.ID) bool {
 }
 
 // Start starts cleanup loop for underlying caches.
-func (s *MessageRateLimiterImpl) Start() {
+func (s *MessageRateLimiter) Start() {
 	go s.limiters.CleanupLoop()
 }
 
 // Stop sends cleanup signal to underlying rate limiters and rate limited peers maps. After the rate limiter
 // is closed it can not be reused.
-func (s *MessageRateLimiterImpl) Stop() {
+func (s *MessageRateLimiter) Stop() {
 	s.limiters.Close()
 }
 
 // SetTimeNowFunc overrides the default time.Now func with the GetTimeNow func provided.
-func (s *MessageRateLimiterImpl) SetTimeNowFunc(now p2p.GetTimeNow) {
+func (s *MessageRateLimiter) SetTimeNowFunc(now p2p.GetTimeNow) {
 	s.now = now
 }
 
 // getLimiter returns limiter for the peerID, if a limiter does not exist one is created and stored.
-func (s *MessageRateLimiterImpl) getLimiter(peerID peer.ID) *rate.Limiter {
+func (s *MessageRateLimiter) getLimiter(peerID peer.ID) *rate.Limiter {
 	if metadata, ok := s.limiters.Get(peerID); ok {
 		return metadata.Limiter
 	}
