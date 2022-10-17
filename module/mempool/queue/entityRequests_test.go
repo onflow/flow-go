9ad75052cf46070d923d0dbb45ec9eeda3d601d3
Yahya Hassanzadeh <yhassanzadeh13@ku.edu.tr>
2022-10-17 09:57:51 -0700

    adds testings

--- module/mempool/queue/entityRequests_test.go
+++ module/mempool/queue/entityRequests_test.go
@@ -2,7 +2,9 @@ package queue
 
 import (
 	"math/rand"
+	"sync"
 	"testing"
+	"time"
 
 	"github.com/stretchr/testify/require"
 
@@ -58,92 +60,105 @@ func TestEntityRequestQueue_Sequential(t *testing.T) {
 	}
 }
 
-// // TestChunkDataPackRequestQueue_Concurrent evaluates correctness of queue implementation against concurrent push and pop.
-//
-//	func TestChunkDataPackRequestQueue_Concurrent(t *testing.T) {
-//		sizeLimit := 100
-//		q := NewChunkDataPackRequestQueue(uint32(sizeLimit), unittest.Logger(), metrics.NewNoopCollector())
-//
-//		// initially queue must be zero
-//		require.Zero(t, q.Size())
-//
-//		// initially there should be nothing to pop
-//		request, ok := q.pop()
-//		require.False(t, ok)
-//		require.Nil(t, request)
-//
-//		pushWG := &sync.WaitGroup{}
-//		pushWG.Add(sizeLimit)
-//
-//		requests := chunkDataRequestListFixture(sizeLimit)
-//		// pushing requests concurrently.
-//		for _, req := range requests {
-//			req := req // suppress loop variable
-//			go func() {
-//				require.True(t, q.push(req.ChunkId, req.RequesterId))
-//				pushWG.Done()
-//			}()
-//		}
-//		unittest.RequireReturnsBefore(t, pushWG.Wait, 100*time.Millisecond, "could not push all requests on time")
-//
-//		// once queue meets the size limit, any extra push should fail.
-//		pushWG.Add(sizeLimit)
-//		for i := 0; i < sizeLimit; i++ {
-//			go func() {
-//				require.False(t, q.push(unittest.IdentifierFixture(), unittest.IdentifierFixture()))
-//				pushWG.Done()
-//			}()
-//		}
-//		unittest.RequireReturnsBefore(t, pushWG.Wait, 100*time.Millisecond, "could not push all requests on time")
-//
-//		popWG := &sync.WaitGroup{}
-//		popWG.Add(sizeLimit)
-//		matchLock := &sync.Mutex{}
-//
-//		// pop-ing requests concurrently.
-//		for i := 0; i < sizeLimit; i++ {
-//			go func() {
-//				popedReq, ok := q.pop()
-//				require.True(t, ok)
-//
-//				matchLock.Lock()
-//				matchAndRemove(t, requests, popedReq)
-//				matchLock.Unlock()
-//
-//				popWG.Done()
-//			}()
-//		}
-//		unittest.RequireReturnsBefore(t, popWG.Wait, 100*time.Millisecond, "could not pop all requests on time")
-//
-//		// queue must be empty after pop-ing all
-//		require.Zero(t, q.Size())
-//	}
+// TestEntityRequestsQueue_Concurrent evaluates correctness of queue implementation against concurrent push and pop.
+func TestEntityRequestsQueue_Concurrent(t *testing.T) {
+	sizeLimit := 100
+	q := NewEntityRequestStore(uint32(sizeLimit), unittest.Logger(), metrics.NewNoopCollector())
+
+	// initially queue must be zero
+	require.Zero(t, q.Size())
+
+	// initially there should be nothing to pop
+	originId, request, ok := q.pop()
+	require.False(t, ok)
+	require.Equal(t, originId, flow.Identifier{})
+	require.Nil(t, request)
+
+	pushWG := &sync.WaitGroup{}
+	pushWG.Add(sizeLimit)
+
+	requests := entityRequestListFixture(sizeLimit)
+	originIds := unittest.IdentifierListFixture(sizeLimit)
+	// pushing requests concurrently.
+	for i, req := range requests {
+		req := req // suppress loop variable
+		i := i
+		go func() {
+			require.True(t, q.push(originIds[i], req))
+			pushWG.Done()
+		}()
+	}
+	unittest.RequireReturnsBefore(t, pushWG.Wait, 100*time.Millisecond, "could not push all requests on time")
+
+	// once queue meets the size limit, any extra push should fail.
+	pushWG.Add(sizeLimit)
+	for i := 0; i < sizeLimit; i++ {
+		go func() {
+			require.False(t, q.push(unittest.IdentifierFixture(), entityRequestFixture()))
+			pushWG.Done()
+		}()
+	}
+	unittest.RequireReturnsBefore(t, pushWG.Wait, 100*time.Millisecond, "could not push all requests on time")
+
+	popWG := &sync.WaitGroup{}
+	popWG.Add(sizeLimit)
+	matchLock := &sync.Mutex{}
+
+	// pop-ing requests concurrently.
+	for i := 0; i < sizeLimit; i++ {
+		go func() {
+			popedOriginId, popedReq, ok := q.pop()
+			require.True(t, ok)
+
+			matchLock.Lock()
+			matchAndRemoveEntityRequest(t, requests, originIds, popedReq, popedOriginId)
+			matchLock.Unlock()
+
+			popWG.Done()
+		}()
+	}
+	unittest.RequireReturnsBefore(t, popWG.Wait, 100*time.Millisecond, "could not pop all requests on time")
+
+	// queue must be empty after pop-ing all
+	require.Zero(t, q.Size())
+}
 
 func entityRequestListFixture(count int) []*messages.EntityRequest {
 	list := make([]*messages.EntityRequest, count)
 	for i := 0; i < count; i++ {
-		list[i] = &messages.EntityRequest{
-			Nonce:     rand.Uint64(),
-			EntityIDs: unittest.IdentifierListFixture(10),
-		}
+		list[i] = entityRequestFixture()
 	}
 
 	return list
 }
 
-//
-//// matchAndRemove checks existence of the request in the "requests" array, and if a match is found, it is removed.
-//// If no match is found for a request, it fails the test.
-//func matchAndRemove(t *testing.T, requests []*mempool.ChunkDataPackRequest, req *mempool.ChunkDataPackRequest) []*mempool.ChunkDataPackRequest {
-//	for i, r := range requests {
-//		if r.ChunkId == req.ChunkId && r.RequesterId == req.RequesterId {
-//			// removes the matched request from the list
-//			requests = append(requests[:i], requests[i+1:]...)
-//			return requests
-//		}
-//	}
-//
-//	// no request found in the list to match
-//	require.Fail(t, "could not find a match for request")
-//	return nil
-//}
+func entityRequestFixture() *messages.EntityRequest {
+	return &messages.EntityRequest{
+		Nonce:     rand.Uint64(),
+		EntityIDs: unittest.IdentifierListFixture(10),
+	}
+}
+
+// matchAndRemoveEntityRequest checks existence of the request in the "requests" array and "originId" in the corresponding place of "originIds" array.
+// If a match is found, it is removed.
+// If no match is found for a request, it fails the test.
+func matchAndRemoveEntityRequest(t *testing.T, requests []*messages.EntityRequest, originIds flow.IdentifierList, request *messages.EntityRequest, originId flow.Identifier) {
+	for i, r := range requests {
+		if originIds[i] == originId {
+			require.ElementsMatch(t, r.EntityIDs, request.EntityIDs)
+			// removes the matched request from the list
+			if i == len(requests)-1 {
+				requests = requests[:i]
+				originIds = originIds[:i]
+			} else {
+				requests = append(requests[:i], requests[i+1:]...)
+				originIds = append(originIds[:i], originIds[i+1:]...)
+			}
+
+			return
+		}
+	}
+
+	// no request found in the list to match
+	require.Fail(t, "could not find a match for request")
+}
