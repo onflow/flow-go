f1856445ad916d83808991e01e8c818e803e2c87
Misha <misha.rybalov@dapperlabs.com>
2022-10-18 05:35:07 -0400

    new ingress test: `TestConnectorHappyPath_Send_IngressMsg` in `insecure/attackernet/corrupt_connector_test.go`
    small updates to existing egress test `TestConnectorHappyPath_Send_EgressMsg`

--- insecure/attackernet/corrupt_connector_test.go
+++ insecure/attackernet/corrupt_connector_test.go
@@ -39,22 +39,24 @@ func TestConnectorHappyPath_Send_EgressMsg(t *testing.T) {
 		}()
 
 		// goroutine checks mock cn for receiving the message sent over the connection.
-		msg, _, _ := insecure.EgressMessageFixture(t, unittest.NetworkCodec(), insecure.Protocol_MULTICAST, &message.TestMessage{
+		sentMsg, _, _ := insecure.EgressMessageFixture(t, unittest.NetworkCodec(), insecure.Protocol_MULTICAST, &message.TestMessage{
 			Text: fmt.Sprintf("this is a test message from attacker to cn: %d", rand.Int()),
 		})
 		sentMsgReceived := make(chan struct{})
 		go func() {
 			receivedMsg := <-cn.attackerMsg
 
+			require.Nil(t, receivedMsg.Ingress, "should only have egress message")
+
 			// received message should have an exact match on the relevant fields.
 			// Note: only fields filled by test fixtures are checked, as some others
 			// are filled by gRPC on the fly, which are not relevant to the test's sanity.
-			require.Equal(t, receivedMsg.Egress.Payload, msg.Egress.Payload)
-			require.Equal(t, receivedMsg.Egress.Protocol, msg.Egress.Protocol)
-			require.Equal(t, receivedMsg.Egress.CorruptOriginID, msg.Egress.CorruptOriginID)
-			require.Equal(t, receivedMsg.Egress.TargetNum, msg.Egress.TargetNum)
-			require.Equal(t, receivedMsg.Egress.TargetIDs, msg.Egress.TargetIDs)
-			require.Equal(t, receivedMsg.Egress.ChannelID, msg.Egress.ChannelID)
+			require.Equal(t, sentMsg.Egress.Payload, receivedMsg.Egress.Payload)
+			require.Equal(t, sentMsg.Egress.Protocol, receivedMsg.Egress.Protocol)
+			require.Equal(t, sentMsg.Egress.CorruptOriginID, receivedMsg.Egress.CorruptOriginID)
+			require.Equal(t, sentMsg.Egress.TargetNum, receivedMsg.Egress.TargetNum)
+			require.Equal(t, sentMsg.Egress.TargetIDs, receivedMsg.Egress.TargetIDs)
+			require.Equal(t, sentMsg.Egress.ChannelID, receivedMsg.Egress.ChannelID)
 
 			close(sentMsgReceived)
 		}()
@@ -67,13 +69,73 @@ func TestConnectorHappyPath_Send_EgressMsg(t *testing.T) {
 		}()
 
 		// sends a message to cn
-		require.NoError(t, connection.SendMessage(msg))
+		require.NoError(t, connection.SendMessage(sentMsg))
 
 		// checks a timely arrival of the registration and sent message at the cn.
 		unittest.RequireCloseBefore(t, attackerRegistered, 100*time.Millisecond, "cn could not receive attacker registration on time")
 		// imitates sending a message from cn to attacker through corrupted connection.
 		unittest.RequireCloseBefore(t, sentMsgReceived, 100*time.Millisecond, "cn could not receive message sent on connection on time")
 	})
+}
+
+func TestConnectorHappyPath_Send_IngressMsg(t *testing.T) {
+	withMockCorruptNetwork(t, func(corruptedId flow.Identity, ctx irrecoverable.SignalerContext, cn *mockCorruptNetwork) {
+		// extracting port that CN gRPC server is running on.
+		_, cnPortStr, err := net.SplitHostPort(cn.ServerAddress())
+		require.NoError(t, err)
+
+		connector := NewCorruptConnector(unittest.Logger(),
+			flow.IdentityList{&corruptedId},
+			map[flow.Identifier]string{corruptedId.NodeID: cnPortStr})
+
+		// empty incoming handler, as this test does not evaluate receive path
+		connector.WithIncomingMessageHandler(func(i *insecure.Message) {
+			require.Equal(t, 1, 2) // fail for now - need incoming message handler
+		})
+
+		// goroutine checks the mock cn for receiving the attacker registration from connector.
+		// the attacker registration arrives as the connector attempts a connection on to the cn.
+		attackerRegistered := make(chan struct{})
+		go func() {
+			<-cn.attackerRegMsg
+			close(attackerRegistered)
+		}()
+
+		// goroutine checks mock cn for receiving the message sent over the connection.
+		sentMsg := insecure.IngressMessageFixture(t, unittest.NetworkCodec(), insecure.Protocol_PUBLISH, &message.TestMessage{
+			Text: fmt.Sprintf("this is a test message from corrupt network to attacker: %d", rand.Int()),
+		})
+
+		sentMsgReceived := make(chan struct{})
+
+		go func() {
+			receivedMsg := <-cn.attackerMsg
+			require.Nil(t, receivedMsg.Egress, "should only have ingress message")
+
+			require.Equal(t, sentMsg.Ingress.ChannelID, receivedMsg.Ingress.ChannelID)
+			require.Equal(t, sentMsg.Ingress.OriginID, receivedMsg.Ingress.OriginID)
+			require.Equal(t, sentMsg.Ingress.CorruptTargetID, receivedMsg.Ingress.CorruptTargetID)
+			require.Equal(t, sentMsg.Ingress.Payload, receivedMsg.Ingress.Payload)
+
+			close(sentMsgReceived)
+		}()
+
+		// creates a connection to the corrupt network.
+		connection, err := connector.Connect(ctx, corruptedId.NodeID)
+		require.NoError(t, err)
+		defer func() {
+			require.NoError(t, connection.CloseConnection())
+		}()
+
+		// sends a message to cn
+		require.NoError(t, connection.SendMessage(sentMsg))
+
+		// checks a timely arrival of the registration and sent message at the cn.
+		unittest.RequireCloseBefore(t, attackerRegistered, 100*time.Millisecond, "cn could not receive attacker registration on time")
+		// imitates sending a message from cn to attacker through corrupted connection.
+		unittest.RequireCloseBefore(t, sentMsgReceived, 100*time.Millisecond, "cn could not receive message sent on connection on time")
+	})
+
 }
 
 // TestConnectorHappy_Receive checks that a CorruptConnector can successfully create a connection to a remote corrupt network (CN).
