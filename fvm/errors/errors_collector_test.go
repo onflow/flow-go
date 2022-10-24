a75f5dbfe9f57b81b98aa15ccb2ad8963b08a191
Patrick Lee <patrick.lee@dapperlabs.com>
2022-10-24 10:03:41 -0700

    Test ErrorsCollector against SplitError
    Forgot to add test for this during the refactoring.

--- fvm/errors/errors_collector_test.go
+++ fvm/errors/errors_collector_test.go
@@ -19,8 +19,11 @@ func TestErrorsCollector(t *testing.T) {
 		// Collected non-fatal errors
 		require.False(
 			t,
-			collector.Collect(NewOperationNotSupportedError("op1")).
-				CollectedFailure())
+			collector.Collect(
+				fmt.Errorf(
+					"error wrapped: %w",
+					NewOperationNotSupportedError("op1")),
+			).CollectedFailure())
 
 		require.True(t, collector.CollectedError())
 		err := collector.ErrorOrNil()
@@ -28,61 +31,100 @@ func TestErrorsCollector(t *testing.T) {
 		require.ErrorContains(t, err, "op1")
 		require.False(t, IsFailure(err))
 
+		nonFatal, fatal := SplitErrorTypes(err)
+		require.Nil(t, fatal)
+		require.NotNil(t, nonFatal)
+		require.Equal(t, ErrCodeOperationNotSupportedError, nonFatal.Code())
+
 		require.False(
 			t,
-			collector.Collect(NewOperationNotSupportedError("op2")).
-				CollectedFailure())
+			collector.Collect(
+				NewInvalidArgumentErrorf("bad arg"),
+			).CollectedFailure())
 
 		require.True(t, collector.CollectedError())
 		err = collector.ErrorOrNil()
 		require.NotNil(t, err)
+		require.ErrorContains(t, err, "error wrapped")
 		require.ErrorContains(t, err, "op1")
-		require.ErrorContains(t, err, "op2")
+		require.ErrorContains(t, err, "bad arg")
 		require.False(t, IsFailure(err))
+
+		nonFatal, fatal = SplitErrorTypes(err)
+		require.Nil(t, fatal)
+		require.NotNil(t, nonFatal)
+		require.Equal(t, ErrCodeOperationNotSupportedError, nonFatal.Code())
 
 		// Collected fatal error
 		require.True(
 			t,
-			collector.Collect(fmt.Errorf("fatal1")).CollectedFailure())
+			collector.Collect(
+				fmt.Errorf(
+					"failure wrapped: %w",
+					NewLedgerFailure(fmt.Errorf("fatal1"))),
+			).CollectedFailure())
 
 		require.True(t, collector.CollectedError())
 		err = collector.ErrorOrNil()
 		require.NotNil(t, err)
+		require.ErrorContains(t, err, "error wrapped")
 		require.ErrorContains(t, err, "op1")
-		require.ErrorContains(t, err, "op2")
+		require.ErrorContains(t, err, "bad arg")
+		require.ErrorContains(t, err, "failure wrapped")
 		require.ErrorContains(t, err, "fatal1")
 		require.True(t, IsFailure(err))
+
+		nonFatal, fatal = SplitErrorTypes(err)
+		require.Nil(t, nonFatal)
+		require.NotNil(t, fatal)
+		require.Equal(t, FailureCodeLedgerFailure, fatal.Code())
 
 		// Collecting a non-fatal error after a fatal error should still be
 		// fatal
 		require.True(
 			t,
-			collector.Collect(NewOperationNotSupportedError("op3")).
-				CollectedFailure())
+			collector.Collect(
+				NewOperationNotSupportedError("op3"),
+			).CollectedFailure())
 
 		require.True(t, collector.CollectedError())
 		err = collector.ErrorOrNil()
 		require.NotNil(t, err)
+		require.ErrorContains(t, err, "error wrapped")
 		require.ErrorContains(t, err, "op1")
-		require.ErrorContains(t, err, "op2")
+		require.ErrorContains(t, err, "bad arg")
 		require.ErrorContains(t, err, "op3")
+		require.ErrorContains(t, err, "failure wrapped")
 		require.ErrorContains(t, err, "fatal1")
 		require.True(t, IsFailure(err))
+
+		nonFatal, fatal = SplitErrorTypes(err)
+		require.Nil(t, nonFatal)
+		require.NotNil(t, fatal)
+		require.Equal(t, FailureCodeLedgerFailure, fatal.Code())
 
 		// Collected multiple fatal errors (This should never happen, but just
 		// in case)
-		require.True(t, collector.Collect(fmt.Errorf("fatal2")).
-			CollectedFailure())
+		require.True(
+			t,
+			collector.Collect(fmt.Errorf("fatal2")).CollectedFailure())
 
 		require.True(t, collector.CollectedError())
 		err = collector.ErrorOrNil()
 		require.NotNil(t, err)
+		require.ErrorContains(t, err, "error wrapped")
 		require.ErrorContains(t, err, "op1")
-		require.ErrorContains(t, err, "op2")
+		require.ErrorContains(t, err, "bad arg")
 		require.ErrorContains(t, err, "op3")
+		require.ErrorContains(t, err, "failure wrapped")
 		require.ErrorContains(t, err, "fatal1")
 		require.ErrorContains(t, err, "fatal2")
 		require.True(t, IsFailure(err))
+
+		nonFatal, fatal = SplitErrorTypes(err)
+		require.Nil(t, nonFatal)
+		require.NotNil(t, fatal)
+		require.Equal(t, FailureCodeLedgerFailure, fatal.Code())
 	})
 
 	t.Run("failure only", func(t *testing.T) {
@@ -98,5 +140,9 @@ func TestErrorsCollector(t *testing.T) {
 		require.ErrorContains(t, err, "fatal1")
 		require.True(t, IsFailure(err))
 
+		nonFatal, fatal := SplitErrorTypes(err)
+		require.Nil(t, nonFatal)
+		require.NotNil(t, fatal)
+		require.Equal(t, FailureCodeUnknownFailure, fatal.Code())
 	})
 }
