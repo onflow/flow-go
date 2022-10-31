1f042f22d057031178374d9bd7bdc9e7ce04138c
Misha <misha.rybalov@dapperlabs.com>
2022-10-31 07:31:24 -0400

    flaky tests - deleted `tools/test_monitor/test-setup.sh`

--- tools/test_monitor/run-tests.sh
+++ tools/test_monitor/run-tests.sh
@@ -22,29 +22,34 @@ then
         docker rm -f $containers > /dev/null
     fi
 
-  echo "running $TEST_CATEGORY tests">&2
+  echo "preparing $TEST_CATEGORY tests">&2
   make crypto_setup_gopath
   make docker-build-flow
+  echo "running $TEST_CATEGORY tests">&2
   make -C integration -s ${BASH_REMATCH[1]}-tests > test-output
 else
     case $TEST_CATEGORY in
         unit)
+          echo "preparing unit tests">&2
           make install-tools
           make verify-mocks
-          echo "running unit tests (ci)">&2
+          echo "running unit tests">&2
           make -s unittest-main > test-output
         ;;
         unit-crypto)
+          echo "preparing crypto unit tests">&2
           make -C crypto -s setup
           echo "running crypto unit tests">&2
           make -C crypto -s test_main > test-output
         ;;
         unit-insecure)
+          echo "preparing insecure unit tests">&2
           make install-tools
           echo "running insecure unit tests">&2
           make -C insecure -s test > test-output
         ;;
         unit-integration)
+          echo "preparing integration unit tests">&2
           make install-tools
           echo "running integration unit tests">&2
           make -C integration -s test > test-output
