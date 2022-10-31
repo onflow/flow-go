7821e264582f7e80304b96bf605e4a93ddc2a13a
Peter Argue <89119817+peterargue@users.noreply.github.com>
2022-10-31 11:25:16 -0700

    [Network] Stop sending unused message.Message fields

--- network/internal/messageutils/utils.go
+++ network/internal/messageutils/utils.go
@@ -9,11 +9,10 @@ import (
 	libp2pmessage "github.com/onflow/flow-go/model/libp2p/message"
 	"github.com/onflow/flow-go/network/channels"
 	"github.com/onflow/flow-go/network/message"
-	"github.com/onflow/flow-go/network/p2p"
 	"github.com/onflow/flow-go/utils/unittest"
 )
 
-func CreateMessage(t *testing.T, originID flow.Identifier, targetID flow.Identifier, channel channels.Channel, msg string) (*message.Message, interface{}) {
+func CreateMessage(t *testing.T, targetID flow.Identifier, channel channels.Channel, msg string) (*message.Message, interface{}) {
 	payload := &libp2pmessage.TestMessage{
 		Text: msg,
 	}
@@ -22,16 +21,10 @@ func CreateMessage(t *testing.T, originID flow.Identifier, targetID flow.Identif
 	b, err := codec.Encode(payload)
 	require.NoError(t, err)
 
-	eventID, err := p2p.EventId(channel, b)
-	require.NoError(t, err)
-
 	m := &message.Message{
 		ChannelID: channel.String(),
-		EventID:   eventID,
-		OriginID:  originID[:],
 		TargetIDs: [][]byte{targetID[:]},
 		Payload:   b,
-		Type:      p2p.MessageType(payload),
 	}
 
 	return m, payload
