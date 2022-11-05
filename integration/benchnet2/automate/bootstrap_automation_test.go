b42c30295c0edd41c44207bc5fa6c4fed901db8d
Misha <misha.rybalov@dapperlabs.com>
2022-11-05 08:55:32 -0400

    looping with if fully working, tested for access, collection nodes

--- integration/benchnet2/automate/bootstrap_automation_test.go
+++ integration/benchnet2/automate/bootstrap_automation_test.go
@@ -170,41 +170,41 @@ const ExpectedTemplatesPath = "./testdata/expected/"
 
 func TestApply_DataTable(t *testing.T) {
 	testDataMap := map[string]testData{
-		//"simple1": {
-		//	templatePath:     filepath.Join(TemplatesPath, "test1.yml"),
-		//	dataPath:         filepath.Join(DataPath, "test1.json"),
-		//	expectedTemplate: filepath.Join(ExpectedTemplatesPath, "test1.yml"),
-		//},
-		//
-		//"simple2": {
-		//	templatePath:     filepath.Join(TemplatesPath, "access_template.yml"),
-		//	dataPath:         filepath.Join(DataPath, "access_template.json"),
-		//	expectedTemplate: filepath.Join(ExpectedTemplatesPath, "access_template.yml"),
-		//},
-		//
-		//"values1 - original, empty template": {
-		//	templatePath:     filepath.Join(TemplatesPath, "values1-original.yml"),
-		//	dataPath:         filepath.Join(DataPath, "values1-original.json"),
-		//	expectedTemplate: filepath.Join(ExpectedTemplatesPath, "values1.yml"),
-		//},
-		//
-		//"values2 - access nodes - separate node ids": {
-		//	templatePath:     filepath.Join(TemplatesPath, "values2-access-nodes.yml"),
-		//	dataPath:         filepath.Join(DataPath, "values2-access-nodes.json"),
-		//	expectedTemplate: filepath.Join(ExpectedTemplatesPath, "values1.yml"),
-		//},
-		//
-		//"values3 - access nodes - loop": {
-		//	templatePath:     filepath.Join(TemplatesPath, "values3-access-nodes-loop.yml"),
-		//	dataPath:         filepath.Join(DataPath, "values3-access-nodes-loop.json"),
-		//	expectedTemplate: filepath.Join(ExpectedTemplatesPath, "values1.yml"),
-		//},
-		//
-		//"values4 - access nodes - if loop": {
-		//	templatePath:     filepath.Join(TemplatesPath, "values4-access-nodes-if-loop.yml"),
-		//	dataPath:         filepath.Join(DataPath, "values4-access-nodes-if-loop.json"),
-		//	expectedTemplate: filepath.Join(ExpectedTemplatesPath, "values1.yml"),
-		//},
+		"simple1": {
+			templatePath:     filepath.Join(TemplatesPath, "test1.yml"),
+			dataPath:         filepath.Join(DataPath, "test1.json"),
+			expectedTemplate: filepath.Join(ExpectedTemplatesPath, "test1.yml"),
+		},
+
+		"simple2": {
+			templatePath:     filepath.Join(TemplatesPath, "access_template.yml"),
+			dataPath:         filepath.Join(DataPath, "access_template.json"),
+			expectedTemplate: filepath.Join(ExpectedTemplatesPath, "access_template.yml"),
+		},
+
+		"values1 - original, empty template": {
+			templatePath:     filepath.Join(TemplatesPath, "values1-original.yml"),
+			dataPath:         filepath.Join(DataPath, "values1-original.json"),
+			expectedTemplate: filepath.Join(ExpectedTemplatesPath, "values1.yml"),
+		},
+
+		"values2 - access nodes - separate node ids": {
+			templatePath:     filepath.Join(TemplatesPath, "values2-access-nodes.yml"),
+			dataPath:         filepath.Join(DataPath, "values2-access-nodes.json"),
+			expectedTemplate: filepath.Join(ExpectedTemplatesPath, "values1.yml"),
+		},
+
+		"values3 - access nodes - loop": {
+			templatePath:     filepath.Join(TemplatesPath, "values3-access-nodes-loop.yml"),
+			dataPath:         filepath.Join(DataPath, "values3-access-nodes-loop.json"),
+			expectedTemplate: filepath.Join(ExpectedTemplatesPath, "values1.yml"),
+		},
+
+		"values4 - access nodes - if loop": {
+			templatePath:     filepath.Join(TemplatesPath, "values4-access-nodes-if-loop.yml"),
+			dataPath:         filepath.Join(DataPath, "values4-access-nodes-if-loop.json"),
+			expectedTemplate: filepath.Join(ExpectedTemplatesPath, "values1.yml"),
+		},
 
 		"values5 - collection nodes - if loop": {
 			templatePath:     filepath.Join(TemplatesPath, "values5-collection-nodes-if-loop.yml"),
