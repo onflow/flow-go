0f2a83718bdd3e59711fb54850958c7e1eef1fa3
Misha <misha.rybalov@dapperlabs.com>
2022-11-07 08:02:11 -0500

    updated test template with `values1-original.yml`

--- integration/benchnet2/automate/bootstrap_automation_test.go
+++ integration/benchnet2/automate/bootstrap_automation_test.go
@@ -15,23 +15,23 @@ const ExpectedTemplatesPath = "./testdata/expected/"
 
 func TestApply_DataTable(t *testing.T) {
 	testDataMap := map[string]testData{
-		//"simple1": {
-		//	templatePath:     filepath.Join(TemplatesPath, "test1.yml"),
-		//	dataPath:         filepath.Join(DataPath, "test1.json"),
-		//	expectedTemplate: filepath.Join(ExpectedTemplatesPath, "test1.yml"),
-		//},
-		//
-		//"simple2": {
-		//	templatePath:     filepath.Join(TemplatesPath, "access_template.yml"),
-		//	dataPath:         filepath.Join(DataPath, "access_template.json"),
-		//	expectedTemplate: filepath.Join(ExpectedTemplatesPath, "access_template.yml"),
-		//},
-		//
-		//"values1 - original, empty template": {
-		//	templatePath:     filepath.Join(TemplatesPath, "values1-original.yml"),
-		//	dataPath:         filepath.Join(DataPath, "values1-original.json"),
-		//	expectedTemplate: filepath.Join(ExpectedTemplatesPath, "values1.yml"),
-		//},
+		"simple1": {
+			templatePath:     filepath.Join(TemplatesPath, "test1.yml"),
+			dataPath:         filepath.Join(DataPath, "test1.json"),
+			expectedTemplate: filepath.Join(ExpectedTemplatesPath, "test1.yml"),
+		},
+
+		"simple2": {
+			templatePath:     filepath.Join(TemplatesPath, "access_template.yml"),
+			dataPath:         filepath.Join(DataPath, "access_template.json"),
+			expectedTemplate: filepath.Join(ExpectedTemplatesPath, "access_template.yml"),
+		},
+
+		"values1 - original, empty template": {
+			templatePath:     filepath.Join(TemplatesPath, "values1-original.yml"),
+			dataPath:         filepath.Join(DataPath, "values1-original.json"),
+			expectedTemplate: filepath.Join(ExpectedTemplatesPath, "values1.yml"),
+		},
 		//
 		//"values2 - access nodes - separate node ids": {
 		//	templatePath:     filepath.Join(TemplatesPath, "values2-access-nodes.yml"),
