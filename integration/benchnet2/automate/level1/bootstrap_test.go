3b26945ec29f299ed511bf12b4ef1d0ca3cc6a89
Misha <misha.rybalov@dapperlabs.com>
2022-11-09 09:28:04 -0500

    minor docs update

--- integration/benchnet2/automate/level1/bootstrap_test.go
+++ integration/benchnet2/automate/level1/bootstrap_test.go
@@ -14,7 +14,7 @@ const ExpectedOutputPath = "../testdata/level1/expected"
 
 func TestGenerateBootstrap_DataTable(t *testing.T) {
 	testDataMap := map[string]testData{
-		"2AN 6LN 3CN 2 EN 1 VN": {
+		"2 AN, 6 LN, 3 CN, 2 EN, 1 VN": {
 			bootstrapPath:  filepath.Join(BootstrapPath, "root-protocol-state-snapshot1.json"),
 			expectedOutput: filepath.Join(ExpectedOutputPath, "template-data-input1.json"),
 		},
@@ -33,7 +33,7 @@ func TestGenerateBootstrap_DataTable(t *testing.T) {
 			err = json.Unmarshal(expectedDataBytes, &expectedNodeData)
 			require.Nil(t, err)
 
-			// check generated template data file is correct by unmarshalling json into structs
+			// check generated template data file is correct
 			require.Equal(t, len(expectedNodeData), len(actualNodeData))
 			require.ElementsMatch(t, expectedNodeData, actualNodeData)
 		})
