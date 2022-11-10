c756b342117fe434dc236cac63d08cae151162d5
Khalil Claybon <khalil.claybon@dapperlabs.com>
2022-11-10 17:45:06 -0500

    ensure all auth configurations have a single allowed protocol enabled

--- network/message/init.go
+++ network/message/init.go
@@ -1,9 +1,42 @@
 package message
 
+import (
+	"fmt"
+)
+
+var (
+	validationExclusionList = []string{TestMessage}
+)
+
 // init is called first time this package is imported.
 // It creates and initializes AuthorizationConfigs for each message type.
 func init() {
 	initializeMessageAuthConfigsMap()
+	validateMessageAuthConfigsMap(validationExclusionList)
+}
+
+func validateMessageAuthConfigsMap(excludeList []string) {
+	for _, msgAuthConf := range authorizationConfigs {
+		if excludeConfig(msgAuthConf.Name, excludeList) {
+			continue
+		}
+
+		for _, config := range msgAuthConf.Config {
+			if len(config.AllowedProtocols) != 1 {
+				panic(fmt.Errorf("error: message authorization config for message type %s should have a single allowed protocol found %d: %s", msgAuthConf.Name, len(config.AllowedProtocols), config.AllowedProtocols))
+			}
+		}
+	}
+}
+
+func excludeConfig(name string, excludeList []string) bool {
+	for _, s := range excludeList {
+		if s == name {
+			return true
+		}
+	}
+
+	return false
 }
 
 // string constants for all message types sent on the network
