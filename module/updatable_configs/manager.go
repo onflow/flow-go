b218ef62893b474e6d8ba625966c61f3b105bc1f
Jordan Schalm <jordan@dapperlabs.com>
2022-11-11 09:19:41 -0500

    feat: support IDList in config manager

--- module/updatable_configs/manager.go
+++ module/updatable_configs/manager.go
@@ -5,6 +5,9 @@ import (
 	"fmt"
 	"sync"
 	"time"
+
+	"github.com/onflow/flow-go/model/flow"
+	"github.com/onflow/flow-go/module/util"
 )
 
 // ErrAlreadyRegistered is returned when a config field is registered with a name
@@ -48,15 +51,17 @@ type (
 	// Set*ConfigFunc is a setter function for a single updatable config field.
 	// Returns ValidationError if the new config value is invalid.
 
-	SetUintConfigFunc     func(uint) error
-	SetBoolConfigFunc     func(bool) error
-	SetDurationConfigFunc func(time.Duration) error
+	SetUintConfigFunc           func(uint) error
+	SetBoolConfigFunc           func(bool) error
+	SetDurationConfigFunc       func(time.Duration) error
+	SetIdentifierListConfigFunc func(flow.IdentifierList) error
 
 	// Get*ConfigFunc is a getter function for a single updatable config field.
 
-	GetUintConfigFunc     func() uint
-	GetBoolConfigFunc     func() bool
-	GetDurationConfigFunc func() time.Duration
+	GetUintConfigFunc           func() uint
+	GetBoolConfigFunc           func() bool
+	GetDurationConfigFunc       func() time.Duration
+	GetIdentifierListConfigFunc func() flow.IdentifierList
 )
 
 // Field represents one dynamically configurable config field.
@@ -131,8 +136,14 @@ type Registrar interface {
 	// RegisterDurationConfig registers a new duration config.
 	// Returns ErrAlreadyRegistered if a config is already registered with name.
 	RegisterDurationConfig(name string, get GetDurationConfigFunc, set SetDurationConfigFunc) error
+	// RegisterIdentifierListConfig registers a new []Identifier config
+	// Returns ErrAlreadyRegistered if a config is already registered with name.
+	RegisterIdentifierListConfig(name string, get GetIdentifierListConfigFunc, set SetIdentifierListConfigFunc) error
 }
 
+// RegisterBoolConfig registers a new bool config.
+// Setter inputs must be bool-typed values.
+// Returns ErrAlreadyRegistered if a config is already registered with name.
 func (m *Manager) RegisterBoolConfig(name string, get GetBoolConfigFunc, set SetBoolConfigFunc) error {
 	m.mu.Lock()
 	defer m.mu.Unlock()
@@ -159,6 +170,9 @@ func (m *Manager) RegisterBoolConfig(name string, get GetBoolConfigFunc, set Set
 	return nil
 }
 
+// RegisterUintConfig registers a new uint config.
+// Setter inputs must be float64-typed values and will be truncated if not integral.
+// Returns ErrAlreadyRegistered if a config is already registered with name.
 func (m *Manager) RegisterUintConfig(name string, get GetUintConfigFunc, set SetUintConfigFunc) error {
 	m.mu.Lock()
 	defer m.mu.Unlock()
@@ -176,7 +190,7 @@ func (m *Manager) RegisterUintConfig(name string, get GetUintConfigFunc, set Set
 		Set: func(val any) error {
 			fval, ok := val.(float64) // JSON numbers always parse to float64
 			if !ok {
-				return fmt.Errorf("invalid type for bool config: %T", val)
+				return NewValidationErrorf("invalid type for bool config: %T", val)
 			}
 			return set(uint(fval))
 		},
@@ -185,6 +199,9 @@ func (m *Manager) RegisterUintConfig(name string, get GetUintConfigFunc, set Set
 	return nil
 }
 
+// RegisterDurationConfig registers a new duration config.
+// Setter inputs must be duration-parseable string-typed values.
+// Returns ErrAlreadyRegistered if a config is already registered with name.
 func (m *Manager) RegisterDurationConfig(name string, get GetDurationConfigFunc, set SetDurationConfigFunc) error {
 	m.mu.Lock()
 	defer m.mu.Unlock()
@@ -203,13 +220,54 @@ func (m *Manager) RegisterDurationConfig(name string, get GetDurationConfigFunc,
 		Set: func(val any) error {
 			sval, ok := val.(string)
 			if !ok {
-				return fmt.Errorf("invalid type for duration config: %T", val)
+				return NewValidationErrorf("invalid type for duration config: %T", val)
 			}
 			dval, err := time.ParseDuration(sval)
 			if err != nil {
-				return fmt.Errorf("unparseable duration: %s: %w", sval, err)
+				return NewValidationErrorf("unparseable duration: %s: %w", sval, err)
 			}
 			return set(dval)
+		},
+	}
+	m.fields[field.Name] = field
+	return nil
+}
+
+// RegisterIdentifierListConfig registers a new []Identifier config
+// Setter inputs must be []any-typed values, with string elements parseable as Identifier.
+// Returns ErrAlreadyRegistered if a config is already registered with name.
+func (m *Manager) RegisterIdentifierListConfig(name string, get GetIdentifierListConfigFunc, set SetIdentifierListConfigFunc) error {
+	m.mu.Lock()
+	defer m.mu.Unlock()
+
+	if _, exists := m.fields[name]; exists {
+		return fmt.Errorf("can't register config %s: %w", name, ErrAlreadyRegistered)
+	}
+
+	field := Field{
+		Name:     name,
+		TypeName: "IdentifierList",
+		Get: func() any {
+			return util.DetypeSlice(get().Strings())
+		},
+		Set: func(val any) error {
+			gval, ok := val.([]any)
+			if !ok {
+				return NewValidationErrorf("invalid type for duration config: %T", val)
+			}
+			ids := make(flow.IdentifierList, len(gval))
+			for i, gid := range gval {
+				sid, ok := gid.(string)
+				if !ok {
+					return NewValidationErrorf("invalid element type %T for IdentifierList config - should be string", gid)
+				}
+				id, err := flow.HexStringToIdentifier(sid)
+				if err != nil {
+					return NewValidationErrorf("un-parseable id %s found in list: %w", gid, err)
+				}
+				ids[i] = id
+			}
+			return set(ids)
 		},
 	}
 	m.fields[field.Name] = field
--- module/updatable_configs/manager_test.go
+++ module/updatable_configs/manager_test.go
@@ -1,4 +1,4 @@
-package updatable_configs
+package updatable_configs_test
 
 import (
 	"testing"
@@ -7,10 +7,15 @@ import (
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
 	"google.golang.org/protobuf/types/known/structpb"
+
+	"github.com/onflow/flow-go/model/flow"
+	"github.com/onflow/flow-go/module/updatable_configs"
+	"github.com/onflow/flow-go/module/util"
+	"github.com/onflow/flow-go/utils/unittest"
 )
 
 func TestRegisterDuplicateConfig(t *testing.T) {
-	mgr := NewManager()
+	mgr := updatable_configs.NewManager()
 
 	// should be able to register a config the first time
 	err := mgr.RegisterBoolConfig("field",
@@ -22,16 +27,17 @@ func TestRegisterDuplicateConfig(t *testing.T) {
 	err = mgr.RegisterUintConfig("field",
 		func() uint { return 0 },
 		func(_ uint) error { return nil })
-	assert.ErrorIs(t, err, ErrAlreadyRegistered)
+	assert.ErrorIs(t, err, updatable_configs.ErrAlreadyRegistered)
 }
 
 func TestManager_RegisterBoolConfig(t *testing.T) {
-	mgr := NewManager()
+	mgr := updatable_configs.NewManager()
 
 	// should be able to register config
+	fieldSet := make(chan struct{}) // closed when field is successfully set
 	err := mgr.RegisterBoolConfig("field",
 		func() bool { return true },
-		func(_ bool) error { return nil })
+		func(_ bool) error { close(fieldSet); return nil })
 	require.NoError(t, err)
 
 	// should be able to get the field
@@ -48,15 +54,17 @@ func TestManager_RegisterBoolConfig(t *testing.T) {
 	// should succeed setting correct type
 	err = field.Set(true)
 	assert.NoError(t, err)
+	assert.True(t, util.CheckClosed(fieldSet))
 }
 
 func TestManager_RegisterUintConfig(t *testing.T) {
-	mgr := NewManager()
+	mgr := updatable_configs.NewManager()
 
 	// should be able to register config
+	fieldSet := make(chan struct{}) // closed when field is successfully set
 	err := mgr.RegisterUintConfig("field",
 		func() uint { return 0 },
-		func(_ uint) error { return nil })
+		func(_ uint) error { close(fieldSet); return nil })
 	require.NoError(t, err)
 
 	// should be able to get the field
@@ -73,15 +81,17 @@ func TestManager_RegisterUintConfig(t *testing.T) {
 	// should succeed setting correct type
 	err = field.Set(float64(1)) // JSON uints parse to float64
 	assert.NoError(t, err)
+	assert.True(t, util.CheckClosed(fieldSet))
 }
 
 func TestManager_RegisterDurationConfig(t *testing.T) {
-	mgr := NewManager()
+	mgr := updatable_configs.NewManager()
 
 	// should be able to register config
+	fieldSet := make(chan struct{}) // closed when field is successfully set
 	err := mgr.RegisterDurationConfig("field",
 		func() time.Duration { return time.Second },
-		func(_ time.Duration) error { return nil })
+		func(_ time.Duration) error { close(fieldSet); return nil })
 	require.NoError(t, err)
 
 	// should be able to get the field
@@ -101,4 +111,37 @@ func TestManager_RegisterDurationConfig(t *testing.T) {
 	// should succeed setting correct type
 	err = field.Set("1h")
 	assert.NoError(t, err)
+	assert.True(t, util.CheckClosed(fieldSet))
+}
+
+func TestManager_RegisterIdentifierListConfig(t *testing.T) {
+	mgr := updatable_configs.NewManager()
+
+	// should be able to register config
+	fieldSet := make(chan struct{}) // closed when field is successfully set
+	err := mgr.RegisterIdentifierListConfig("field",
+		func() flow.IdentifierList { return nil },
+		func(_ flow.IdentifierList) error { close(fieldSet); return nil })
+	require.NoError(t, err)
+
+	// should be able to get the field
+	field, ok := mgr.GetField("field")
+	assert.True(t, ok)
+	// field must be parseable by structpb (otherwise admin server will error)
+	_, err = structpb.NewValue(field.Get())
+	require.NoError(t, err)
+
+	// should fail to set incorrect type
+	err = field.Set(struct{}{})
+	assert.Error(t, err)
+	// should fail to set with correct type, but un-parseable
+	err = field.Set([]string{"un-parseable"})
+	assert.Error(t, err)
+	err = field.Set([]string{unittest.IdentifierFixture().String(), "un-parseable"})
+	assert.Error(t, err)
+
+	// should succeed setting correct type
+	err = field.Set(util.DetypeSlice(unittest.IdentifierListFixture(5).Strings()))
+	assert.NoError(t, err)
+	assert.True(t, util.CheckClosed(fieldSet))
 }
