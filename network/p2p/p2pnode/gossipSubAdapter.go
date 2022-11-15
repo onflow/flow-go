1a99697516e93b757fb6052157f64be24aaf5559
Yahya Hassanzadeh <yhassanzadeh13@ku.edu.tr>
2022-11-15 12:55:53 -0800

    implements adapter

--- network/p2p/p2pnode/gossipSubAdapter.go
+++ network/p2p/p2pnode/gossipSubAdapter.go
@@ -1,1 +1,38 @@
 package p2pnode
+
+import (
+	pubsub "github.com/libp2p/go-libp2p-pubsub"
+	"github.com/libp2p/go-libp2p/core/peer"
+
+	"github.com/onflow/flow-go/network/p2p"
+)
+
+type GossipSubAdapter struct {
+	gossipSub *pubsub.PubSub
+}
+
+var _ p2p.PubSubAdapter = (*GossipSubAdapter)(nil)
+
+func (g *GossipSubAdapter) RegisterTopicValidator(topic string, val interface{}) error {
+	return g.gossipSub.RegisterTopicValidator(topic, val, pubsub.WithValidatorInline(true))
+}
+
+func (g *GossipSubAdapter) UnregisterTopicValidator(topic string) error {
+	return g.gossipSub.UnregisterTopicValidator(topic)
+}
+
+func (g *GossipSubAdapter) Join(topic string) (p2p.Topic, error) {
+	t, err := g.gossipSub.Join(topic)
+	if err != nil {
+		return nil, err
+	}
+	return NewGossipSubTopic(t), nil
+}
+
+func (g *GossipSubAdapter) GetTopics() []string {
+	return g.gossipSub.GetTopics()
+}
+
+func (g *GossipSubAdapter) ListPeers(topic string) []peer.ID {
+	return g.gossipSub.ListPeers(topic)
+}
