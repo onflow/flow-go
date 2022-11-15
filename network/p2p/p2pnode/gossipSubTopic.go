13ed60cac15cb8642329433ca528141e2936b223
Yahya Hassanzadeh <yhassanzadeh13@ku.edu.tr>
2022-11-15 12:55:59 -0800

    implements topic

--- network/p2p/p2pnode/gossipSubTopic.go
+++ network/p2p/p2pnode/gossipSubTopic.go
@@ -1,1 +1,37 @@
 package p2pnode
+
+import (
+	"context"
+
+	pubsub "github.com/libp2p/go-libp2p-pubsub"
+
+	"github.com/onflow/flow-go/network/p2p"
+)
+
+type GossipSubTopic struct {
+	t *pubsub.Topic
+}
+
+var _ p2p.Topic = (*GossipSubTopic)(nil)
+
+func NewGossipSubTopic(topic *pubsub.Topic) *GossipSubTopic {
+	return &GossipSubTopic{
+		t: topic,
+	}
+}
+
+func (g *GossipSubTopic) String() string {
+	return g.t.String()
+}
+
+func (g *GossipSubTopic) Close() error {
+	return g.t.Close()
+}
+
+func (g *GossipSubTopic) Publish(ctx context.Context, bytes []byte) error {
+	return g.t.Publish(ctx, bytes)
+}
+
+func (g *GossipSubTopic) Subscribe() (p2p.Subscription, error) {
+	return g.t.Subscribe()
+}
