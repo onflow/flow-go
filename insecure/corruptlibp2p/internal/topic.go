51c79914f0aaf59da814d4d6e5d1b5ddba3efddd
Yahya Hassanzadeh <yhassanzadeh13@ku.edu.tr>
2022-11-15 12:57:16 -0800

    implements corrupt topic

--- insecure/corruptlibp2p/internal/topic.go
+++ insecure/corruptlibp2p/internal/topic.go
@@ -1,1 +1,33 @@
 package internal
+
+import (
+	"context"
+
+	corrupt "github.com/yhassanzadeh13/go-libp2p-pubsub"
+
+	"github.com/onflow/flow-go/network/p2p"
+)
+
+type CorruptTopic struct {
+	t *corrupt.Topic
+}
+
+func NewCorruptTopic(t *corrupt.Topic) p2p.Topic {
+	return &CorruptTopic{
+		t: t,
+	}
+}
+
+var _ p2p.Topic = (*CorruptTopic)(nil)
+
+func (c *CorruptTopic) String() string {
+	return c.t.String()
+}
+
+func (c *CorruptTopic) Close() error {
+	return c.t.Close()
+}
+
+func (c *CorruptTopic) Publish(ctx context.Context, bytes []byte) error {
+	return c.t.Publish(ctx, bytes)
+}
