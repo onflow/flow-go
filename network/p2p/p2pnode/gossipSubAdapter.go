8e2446b523f2a466ae3361288a76c33f97086c93
Yahya Hassanzadeh <yhassanzadeh13@ku.edu.tr>
2022-11-16 09:42:20 -0800

    wip

--- network/p2p/p2pnode/gossipSubAdapter.go
+++ network/p2p/p2pnode/gossipSubAdapter.go
@@ -1,17 +1,32 @@
 package p2pnode
 
 import (
+	"context"
+
 	pubsub "github.com/libp2p/go-libp2p-pubsub"
+	"github.com/libp2p/go-libp2p/core/host"
 	"github.com/libp2p/go-libp2p/core/peer"
+	"github.com/libp2p/go-libp2p/core/routing"
 
 	"github.com/onflow/flow-go/network/p2p"
 )
 
 type GossipSubAdapter struct {
 	gossipSub *pubsub.PubSub
+	cr        routing.ContentRouting
 }
 
 var _ p2p.PubSubAdapter = (*GossipSubAdapter)(nil)
+
+func NewGossipSubAdapter(ctx context.Context, h host.Host, opts ...pubsub.Option) (p2p.PubSubAdapter, error) {
+	gossipSub, err := pubsub.NewGossipSub(ctx, h, opts...)
+	if err != nil {
+		return nil, err
+	}
+	return &GossipSubAdapter{
+		gossipSub: gossipSub,
+	}, nil
+}
 
 func (g *GossipSubAdapter) RegisterTopicValidator(topic string, val interface{}) error {
 	return g.gossipSub.RegisterTopicValidator(topic, val, pubsub.WithValidatorInline(true))
@@ -35,4 +50,8 @@ func (g *GossipSubAdapter) GetTopics() []string {
 
 func (g *GossipSubAdapter) ListPeers(topic string) []peer.ID {
 	return g.gossipSub.ListPeers(topic)
+}
+
+func defaultPubsubOptions() []pubsub.Option {
+
 }
