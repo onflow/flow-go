99b36dabcf4415f25f0a90d325577e484c9b095c
Yahya Hassanzadeh <yhassanzadeh13@ku.edu.tr>
2022-11-16 16:20:50 -0800

    adds pubsub adapter config

--- network/p2p/p2pnode/gossipSubAdapterConfig.go
+++ network/p2p/p2pnode/gossipSubAdapterConfig.go
@@ -1,1 +1,55 @@
 package p2pnode
+
+import (
+	pubsub "github.com/libp2p/go-libp2p-pubsub"
+	pb "github.com/libp2p/go-libp2p-pubsub/pb"
+	"github.com/libp2p/go-libp2p/core/routing"
+	discoveryrouting "github.com/libp2p/go-libp2p/p2p/discovery/routing"
+
+	"github.com/onflow/flow-go/network/p2p"
+	"github.com/onflow/flow-go/network/p2p/scoring"
+)
+
+type GossipSubAdapterConfig struct {
+	options []pubsub.Option
+}
+
+var _ p2p.PubSubAdapterConfig = (*GossipSubAdapterConfig)(nil)
+
+func NewGossipSubAdapterConfig(base *p2p.BasePubSubAdapterConfig) *GossipSubAdapterConfig {
+	return &GossipSubAdapterConfig{
+		options: append(defaultPubsubOptions(base)),
+	}
+}
+
+func (g *GossipSubAdapterConfig) WithRoutingDiscovery(routing routing.ContentRouting) {
+	g.options = append(g.options, pubsub.WithDiscovery(discoveryrouting.NewRoutingDiscovery(routing)))
+}
+
+func (g *GossipSubAdapterConfig) WithSubscriptionFilter(filter p2p.SubscriptionFilter) {
+	g.options = append(g.options, pubsub.WithSubscriptionFilter(filter))
+}
+
+func (g *GossipSubAdapterConfig) WithScoreOption(option *scoring.ScoreOption) {
+	g.options = append(g.options, option.BuildFlowPubSubScoreOption())
+}
+
+func (g *GossipSubAdapterConfig) WithMessageIdFunction(f func([]byte) string) {
+	g.options = append(g.options, pubsub.WithMessageIdFn(func(pmsg *pb.Message) string {
+		return f(pmsg.Data)
+	}))
+}
+
+func (g *GossipSubAdapterConfig) Build() []pubsub.Option {
+	return g.options
+}
+
+func defaultPubsubOptions(base *p2p.BasePubSubAdapterConfig) []pubsub.Option {
+	return []pubsub.Option{
+		pubsub.WithMessageSigning(true),
+		pubsub.WithStrictSignatureVerification(true),
+		pubsub.WithMaxMessageSize(base.MaxMessageSize),
+	}
+}
+
+var _ p2p.PubSubAdapterConfig = (*GossipSubAdapterConfig)(nil)
