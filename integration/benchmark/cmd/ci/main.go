b77c26975795f2c1051b046970b882dfb7e91c43
Alexey Ivanov <rbtz@dapperlabs.com>
2022-11-20 15:27:17 -0800

    [Benchmark] Make stat fetch atomic

--- integration/benchmark/cmd/ci/main.go
+++ integration/benchmark/cmd/ci/main.go
@@ -274,17 +274,15 @@ func recordTransactionData(
 
 	for {
 		startTime := time.Now()
-		startExecutedTransactions := workerStatsTracker.GetTxExecuted()
-		startSentTransactions := workerStatsTracker.GetTxSent()
+		startStats := workerStatsTracker.GetStats()
 
 		select {
 		case endTime := <-t.C:
-			endExecutedTransaction := workerStatsTracker.GetTxExecuted()
-			endSentTransactions := workerStatsTracker.GetTxSent()
+			endStats := workerStatsTracker.GetStats()
 
 			// calculate this slice
-			outputTps := float64(endExecutedTransaction-startExecutedTransactions) / sliceDuration.Seconds()
-			inputTps := float64(endSentTransactions-startSentTransactions) / sliceDuration.Seconds()
+			outputTps := float64(endStats.TxsExecuted-startStats.TxsExecuted) / sliceDuration.Seconds()
+			inputTps := float64(endStats.TxsSent-startStats.TxsSent) / sliceDuration.Seconds()
 			dataSlices = append(dataSlices,
 				dataSlice{
 					GitSha:              gitSha,
@@ -294,8 +292,8 @@ func recordTransactionData(
 					EndTime:             endTime,
 					InputTps:            inputTps,
 					OutputTps:           outputTps,
-					StartExecutionCount: startExecutedTransactions,
-					EndExecutionCount:   endExecutedTransaction,
+					StartExecutionCount: startStats.TxsExecuted,
+					EndExecutionCount:   endStats.TxsExecuted,
 					RunStartTime:        runStartTime,
 				})
 
@@ -344,34 +342,36 @@ func adjustTPS(
 	maxInflight uint,
 ) error {
 	targetTPS := minTPS
+
+	// Stats for the last round
 	lastTs := time.Now()
 	lastTPS := float64(minTPS)
-	lastTxs := uint(workerStatsTracker.GetTxExecuted())
-	lastTimedoutTxs := workerStatsTracker.GetTxTimedout()
+	lastStats := workerStatsTracker.GetStats()
+	lastTxsExecuted := uint(lastStats.TxsExecuted)
+	lastTxsTimedout := lastStats.TxsTimedout
 	for {
 		select {
 		// NOTE: not using a ticker here since adjusting worker count in SetTPS
 		// can take a while and lead to uneven feedback intervals.
 		case nowTs := <-time.After(interval):
-			currentSentTxs := workerStatsTracker.GetTxSent()
-			currentTxs := uint(workerStatsTracker.GetTxExecuted())
-			currentTimedoutTxs := workerStatsTracker.GetTxTimedout()
-			// number of timed out transactions in the last interval
-			timedoutTxs := currentTimedoutTxs - lastTimedoutTxs
+			currentStats := workerStatsTracker.GetStats()
 
-			inflight := currentSentTxs - int(currentTxs)
+			// number of timed out transactions in the last interval
+			txsTimedout := currentStats.TxsTimedout - lastTxsTimedout
+
+			inflight := currentStats.TxsSent - currentStats.TxsExecuted
 			inflightPerWorker := inflight / int(targetTPS)
 
 			skip, currentTPS, unboundedTPS := computeTPS(
-				lastTxs,
-				currentTxs,
+				lastTxsExecuted,
+				uint(currentStats.TxsExecuted),
 				lastTs,
 				nowTs,
 				lastTPS,
 				targetTPS,
 				inflight,
 				maxInflight,
-				timedoutTxs > 0,
+				txsTimedout > 0,
 			)
 
 			if skip {
@@ -382,7 +382,7 @@ func adjustTPS(
 					Int("inflightPerWorker", inflightPerWorker).
 					Msg("skipped adjusting TPS")
 
-				lastTxs = currentTxs
+				lastTxsExecuted = uint(currentStats.TxsExecuted)
 				lastTPS = currentTPS
 				lastTs = nowTs
 
@@ -398,7 +398,7 @@ func adjustTPS(
 				Uint("targetTPS", boundedTPS).
 				Int("inflight", inflight).
 				Int("inflightPerWorker", inflightPerWorker).
-				Int("timedoutTxs", timedoutTxs).
+				Int("txsTimedout", txsTimedout).
 				Msg("adjusting TPS")
 
 			err := lg.SetTPS(boundedTPS)
@@ -407,11 +407,11 @@ func adjustTPS(
 			}
 
 			targetTPS = boundedTPS
-			lastTimedoutTxs = currentTimedoutTxs
-			//
+			lastTxsTimedout = currentStats.TxsTimedout
+
 			// SetTPS is a blocking call, so we need to re-fetch the TxExecuted and time.
-			//
-			lastTxs = uint(workerStatsTracker.GetTxExecuted())
+			currentStats = workerStatsTracker.GetStats()
+			lastTxsExecuted = uint(currentStats.TxsExecuted)
 			lastTPS = currentTPS
 			lastTs = time.Now()
 		case <-lg.Done():
