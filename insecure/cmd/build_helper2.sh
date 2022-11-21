aff77effcef6ecabc165ba3d9622672a5e29fc31
Misha <misha.rybalov@dapperlabs.com>
2022-11-21 12:29:20 -0500

    reset `insecure`, main module back to original after building corrupt Docker images

--- insecure/cmd/build_helper2.sh
+++ insecure/cmd/build_helper2.sh
@@ -1,2 +1,5 @@
-# put back module after building corrupt Docker images
+# reset insecure module to original after building corrupt Docker images
 mv ../go2.mod ../go.mod
+
+# reset main module to original after building corrupt Docker images
+mv ../../go2.mod ../../go.mod
