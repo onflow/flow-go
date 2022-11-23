10280157ba4d712099c38942af5ee014cb9f1198
Yahya Hassanzadeh <yhassanzadeh13@ku.edu.tr>
2022-11-23 10:13:25 -0800

    adds documentation to corrupt topic

--- insecure/corruptlibp2p/internal/topic.go
+++ insecure/corruptlibp2p/internal/topic.go
@@ -8,25 +8,26 @@ import (
 	"github.com/onflow/flow-go/network/p2p"
 )
 
+// CorruptTopic is a wrapper around the forked pubsub topic from
+// github.com/yhassanzadeh13/go-libp2p-pubsub that implements the p2p.Topic.
+// This is needed because in order to use the forked pubsub module, we need to
+// use the entire dependency tree of the forked module which is resolved to
+// github.com/yhassanzadeh13/go-libp2p-pubsub. This means that we cannot use
+// the original libp2p pubsub module in the same package.
+// Note: we use the forked pubsub module for sake of BFT testing and attack vector
+// implementation, it is designed to be completely isolated in the "insecure" package, and
+// totally separated from the rest of the codebase.
 type CorruptTopic struct {
 	t *corrupt.Topic
 }
 
-func (c *CorruptTopic) Subscribe() (p2p.Subscription, error) {
-	sub, err := c.t.Subscribe()
-	if err != nil {
-		return nil, err
-	}
-	return NewCorruptSubscription(sub), nil
-}
+var _ p2p.Topic = (*CorruptTopic)(nil)
 
 func NewCorruptTopic(t *corrupt.Topic) p2p.Topic {
 	return &CorruptTopic{
 		t: t,
 	}
 }
-
-var _ p2p.Topic = (*CorruptTopic)(nil)
 
 func (c *CorruptTopic) String() string {
 	return c.t.String()
@@ -38,4 +39,12 @@ func (c *CorruptTopic) Close() error {
 
 func (c *CorruptTopic) Publish(ctx context.Context, bytes []byte) error {
 	return c.t.Publish(ctx, bytes)
+}
+
+func (c *CorruptTopic) Subscribe() (p2p.Subscription, error) {
+	sub, err := c.t.Subscribe()
+	if err != nil {
+		return nil, err
+	}
+	return NewCorruptSubscription(sub), nil
 }
