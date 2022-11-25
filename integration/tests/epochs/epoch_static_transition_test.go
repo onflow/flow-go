f6f7cea6941dccd8bef634782c7cbf9e3a57b987
Jordan Schalm <jordan@dapperlabs.com>
2022-11-25 11:12:14 -0500

    enable ci for static transition test

--- integration/tests/epochs/epoch_static_transition_test.go
+++ integration/tests/epochs/epoch_static_transition_test.go
@@ -7,11 +7,10 @@ import (
 	"github.com/stretchr/testify/suite"
 
 	"github.com/onflow/flow-go/model/flow"
-	"github.com/onflow/flow-go/utils/unittest"
 )
 
 func TestEpochStaticTransition(t *testing.T) {
-	unittest.SkipUnless(t, unittest.TEST_FLAKY, "epochs join/leave tests should be run on an machine with adequate resources")
+	//unittest.SkipUnless(t, unittest.TEST_FLAKY, "epochs join/leave tests should be run on an machine with adequate resources")
 	suite.Run(t, new(StaticEpochTransitionSuite))
 }
 
