53251ec1ad2b4c7cb3ff216dc0df143cd84d5678
ramtinms <ramtin.seraj@dapperlabs.com>
2022-11-29 10:53:18 -0800

    add bloom filter

--- engine/execution/state/delta/oracle_benchmark_test.go
+++ engine/execution/state/delta/oracle_benchmark_test.go
@@ -69,7 +69,8 @@ func benchmarkStorage(steps int, b *testing.B) {
 
 		////// RocksDB
 		bbto := grocksdb.NewDefaultBlockBasedTableOptions()
-		bbto.SetBlockCache(grocksdb.NewLRUCache(3 << 30))
+		bbto.SetBlockCache(grocksdb.NewLRUCache(3 << 30)) // TODO increase the cache size to higher than 3GB
+		bbto.SetFilterPolicy(grocksdb.NewBloomFilter(10)) // TODO maybe increase number of bits for bloomfilter
 
 		opts := grocksdb.NewDefaultOptions()
 		opts.SetBlockBasedTableFactory(bbto)
