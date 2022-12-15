de9dd27af9f9137c8dc1642180ee9853949c6834
Yahya Hassanzadeh <yhassanzadeh13@ku.edu.tr>
2022-12-14 18:05:52 -0800

    adds scaler test

--- network/p2p/p2pbuilder/libp2pscaler.go
+++ network/p2p/p2pbuilder/libp2pscaler.go
@@ -17,18 +17,18 @@ func getNumFDs() (int, error) {
 
 func allowedMemory(scaleFactor float64) (int64, error) {
 	if scaleFactor <= 0 || scaleFactor > 1 {
-		return 0, fmt.Errorf("scale factor must be greater than 0 and less than or equal to 1: %f", scaleFactor)
+		return 0, fmt.Errorf("memory scale factor must be greater than 0 and less than or equal to 1: %f", scaleFactor)
 	}
 	return int64(math.Floor(float64(memory.TotalMemory()) * scaleFactor)), nil
 }
 
 func allowedFileDescriptors(scaleFactor float64) (int, error) {
-	if scaleFactor < 0 || scaleFactor > 1 {
-		return 0, fmt.Errorf("scale factor must be between 0 and 1: %f", scaleFactor)
+	if scaleFactor <= 0 || scaleFactor > 1 {
+		return 0, fmt.Errorf("fd scale factor must be greater than 0 and less than or equal to 1: %f", scaleFactor)
 	}
 	fd, err := getNumFDs()
 	if err != nil {
 		return 0, fmt.Errorf("failed to get file descriptor limit: %w", err)
 	}
-	return int(math.Ceil(float64(fd) * scaleFactor)), nil
+	return int(math.Floor(float64(fd) * scaleFactor)), nil
 }
--- network/p2p/p2pbuilder/libp2pscaler_test.go
+++ network/p2p/p2pbuilder/libp2pscaler_test.go
@@ -45,4 +45,59 @@ func TestAllowedMemoryScale(t *testing.T) {
 	// scaling with factor of 0.001 should return 0.1% of the total memory.
 	s, err = allowedMemory(0.001)
 	require.NoError(t, err)
+	require.Equal(t, int64(m/1000), s)
+	
+	// scaling with factor of 0.0001 should return 0.01% of the total memory.
+	s, err = allowedMemory(0.0001)
+	require.NoError(t, err)
+	require.Equal(t, int64(m/10000), s)
+}
+
+func TestAllowedFileDescriptorsScale(t *testing.T) {
+	// getting actual file descriptor limit.
+	fd, err := getNumFDs()
+	require.NoError(t, err)
+	require.True(t, fd > 0)
+
+	// scaling with factor of 1 should return the total file descriptors.
+	s, err := allowedFileDescriptors(1)
+	require.NoError(t, err)
+	require.Equal(t, fd, s)
+
+	// scaling with factor of 0 should return an error.
+	_, err = allowedFileDescriptors(0)
+	require.Error(t, err)
+
+	// scaling with factor of -1 should return an error.
+	_, err = allowedFileDescriptors(-1)
+	require.Error(t, err)
+
+	// scaling with factor of 2 should return an error.
+	_, err = allowedFileDescriptors(2)
+	require.Error(t, err)
+
+	// scaling with factor of 0.5 should return half the total file descriptors.
+	s, err = allowedFileDescriptors(0.5)
+	require.NoError(t, err)
+	require.Equal(t, fd/2, s)
+
+	// scaling with factor of 0.1 should return 10% of the total file descriptors.
+	s, err = allowedFileDescriptors(0.1)
+	require.NoError(t, err)
+	require.Equal(t, fd/10, s)
+
+	// scaling with factor of 0.01 should return 1% of the total file descriptors.
+	s, err = allowedFileDescriptors(0.01)
+	require.NoError(t, err)
+	require.Equal(t, fd/100, s)
+
+	// scaling with factor of 0.001 should return 0.1% of the total file descriptors.
+	s, err = allowedFileDescriptors(0.001)
+	require.NoError(t, err)
+	require.Equal(t, fd/1000, s)
+
+	// scaling with factor of 0.0001 should return 0.01% of the total file descriptors.
+	s, err = allowedFileDescriptors(0.0001)
+	require.NoError(t, err)
+	require.Equal(t, fd/10000, s)
 }
