0b35550af09c2911dc00dddbf7b3096ce1a4042f
Patrick Lee <patrick.lee@dapperlabs.com>
2023-01-09 11:23:16 -0800

    Remove RecordSpanFromParent from general tracing api
    The record span logic is moved into fvm's program logger (It's only used for
    cadence tracing)

--- fvm/tracing/tracer_span.go
+++ fvm/tracing/tracer_span.go
@@ -1,9 +1,6 @@
 package tracing
 
 import (
-	"time"
-
-	"go.opentelemetry.io/otel/attribute"
 	otelTrace "go.opentelemetry.io/otel/trace"
 
 	"github.com/onflow/flow-go/module"
@@ -32,10 +29,13 @@ func (tracer TracerSpan) isTraceable() bool {
 	return tracer.Tracer != nil && tracer.Span != nil
 }
 
-func (tracer TracerSpan) StartChildSpan(name trace.SpanName) TracerSpan {
+func (tracer TracerSpan) StartChildSpan(
+	name trace.SpanName,
+	options ...otelTrace.SpanStartOption,
+) TracerSpan {
 	child := trace.NoopSpan
 	if tracer.isTraceable() {
-		child = tracer.Tracer.StartSpanFromParent(tracer.Span, name)
+		child = tracer.Tracer.StartSpanFromParent(tracer.Span, name, options...)
 	}
 
 	return TracerSpan{
@@ -47,10 +47,11 @@ func (tracer TracerSpan) StartChildSpan(name trace.SpanName) TracerSpan {
 
 func (tracer TracerSpan) StartExtensiveTracingChildSpan(
 	name trace.SpanName,
+	options ...otelTrace.SpanStartOption,
 ) TracerSpan {
 	child := trace.NoopSpan
 	if tracer.isTraceable() && tracer.ExtensiveTracing {
-		child = tracer.Tracer.StartSpanFromParent(tracer.Span, name)
+		child = tracer.Tracer.StartSpanFromParent(tracer.Span, name, options...)
 	}
 
 	return TracerSpan{
@@ -58,19 +59,4 @@ func (tracer TracerSpan) StartExtensiveTracingChildSpan(
 		Span:             child,
 		ExtensiveTracing: tracer.ExtensiveTracing,
 	}
-}
-
-func (tracer TracerSpan) RecordChildSpan(
-	spanName trace.SpanName,
-	duration time.Duration,
-	attrs []attribute.KeyValue,
-) {
-	if !tracer.isTraceable() {
-		return
-	}
-	tracer.Tracer.RecordSpanFromParent(
-		tracer.Span,
-		spanName,
-		duration,
-		attrs)
 }
