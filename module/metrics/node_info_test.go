9bf66d2cafcf4c7a0c671e383f4db772df3fccd3
Yurii Oleksyshyn <yuraolex@gmail.com>
2023-01-12 15:39:10 +0200

    Implemented metrics for reporting static node info

---
+++ module/metrics/node_info_test.go
@@ -0,0 +1,37 @@
+package metrics
+
+import (
+	"github.com/onflow/flow-go/utils/unittest"
+	"github.com/prometheus/client_golang/prometheus"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+	"testing"
+)
+
+// TestNodeInfoCollector_NodeInfo tests if node info collector reports desired metrics
+func TestNodeInfoCollector_NodeInfo(t *testing.T) {
+	reg := prometheus.NewRegistry()
+	collector := NewNodeInfoCollector(reg)
+	version := "0.29"
+	commit := "63cec231136914941e2358de2054a6ef71ea3c99"
+	sporkID := unittest.IdentifierFixture().String()
+	collector.NodeInfo(version, commit, sporkID)
+	metricsFamilies, err := reg.Gather()
+	require.NoError(t, err)
+
+	assertReported := func(value string) {
+		for _, metric := range metricsFamilies[0].Metric {
+			for _, label := range metric.GetLabel() {
+				if label.GetValue() == value {
+					return
+				}
+			}
+		}
+		assert.Failf(t, "metric not found", "except to find value %s", value)
+	}
+
+	for _, value := range []string{version, commit, sporkID} {
+		assertReported(value)
+	}
+	//t.Logf(proto.MarshalTextString(metricsFamilies[0]))
+}
