c51614fbcdc28dbd3641110c32c8d13e836f9ab4
Yurii Oleksyshyn <yuraolex@gmail.com>
2023-01-12 15:39:10 +0200

    Implemented metrics for reporting static node info

---
+++ module/metrics/node_info.go
@@ -0,0 +1,37 @@
+package metrics
+
+import (
+	"github.com/prometheus/client_golang/prometheus"
+	"github.com/prometheus/client_golang/prometheus/promauto"
+)
+
+// NodeInfoCollector implements metrics to report static information about node.
+// Such information can include: version, commit, sporkID.
+type NodeInfoCollector struct {
+	nodeInfo *prometheus.GaugeVec
+}
+
+const (
+	sporkIDLabel = "spork_id"
+	versionLabel = "version"
+	commitLabel  = "commit"
+)
+
+func NewNodeInfoCollector(register prometheus.Registerer) *NodeInfoCollector {
+	collector := &NodeInfoCollector{
+		nodeInfo: promauto.NewGaugeVec(prometheus.GaugeOpts{
+			Name:      "node_info",
+			Namespace: "general",
+			Help:      "report general information about node, such information can include version, sporkID, commit hash, etc",
+		}, []string{"key", "value"}),
+	}
+	register.MustRegister(collector.nodeInfo)
+
+	return collector
+}
+
+func (sc *NodeInfoCollector) NodeInfo(version, commit, sporkID string) {
+	sc.nodeInfo.WithLabelValues(versionLabel, version)
+	sc.nodeInfo.WithLabelValues(commitLabel, commit)
+	sc.nodeInfo.WithLabelValues(sporkIDLabel, sporkID)
+}
