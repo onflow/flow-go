26abf11b3d739f2916a922aebabe8caad59901b7
Yurii Oleksyshyn <yuraolex@gmail.com>
2023-01-16 22:00:21 +0200

    Added skeleton for new test

--- state/protocol/badger/snapshot_test.go
+++ state/protocol/badger/snapshot_test.go
@@ -536,6 +536,61 @@ func TestSealingSegment(t *testing.T) {
 			assertSealingSegmentBlocksQueryableAfterBootstrap(t, snapshot)
 		})
 	})
+	// Test the case where the reference block of the snapshot contains seals for blocks that are lower than the lowest sealing segment's block.
+	// ROOT <- B1 <- B2 <- B3(S1) <- B4 <- ... <- B5(S2, S3)
+	// Expected sealing segment: [B3, B4, B5], Extra blocks: [B1, B2]
+	t.Run("highest block seals outside segment", func(t *testing.T) {
+		util.RunWithFollowerProtocolState(t, rootSnapshot, func(db *badger.DB, state *bprotocol.FollowerState) {
+
+			// build a block to seal
+			block1 := unittest.BlockWithParentFixture(head)
+			buildBlock(t, state, block1)
+
+			// build a block sealing block1
+			block2 := unittest.BlockWithParentFixture(block1.Header)
+			receipt1, seal1 := unittest.ReceiptAndSealForBlock(block1)
+			block2.SetPayload(unittest.PayloadFixture(unittest.WithReceipts(receipt1)))
+			buildBlock(t, state, block2)
+
+			receipt2, seal2 := unittest.ReceiptAndSealForBlock(block2)
+			block3 := unittest.BlockWithParentFixture(block2.Header)
+			block3.SetPayload(unittest.PayloadFixture(unittest.WithSeals(seal1), unittest.WithReceipts(receipt2)))
+			buildBlock(t, state, block3)
+
+			receipt3, seal3 := unittest.ReceiptAndSealForBlock(block3)
+			block4 := unittest.BlockWithParentFixture(block3.Header)
+			block4.SetPayload(unittest.PayloadFixture(unittest.WithReceipts(receipt3)))
+			buildBlock(t, state, block4)
+
+			// build chain, so it's long enough to not target blocks as inside of flow.DefaultTransactionExpiry window.
+			parent := block4
+			for i := 0; i < flow.DefaultTransactionExpiry; i++ {
+				next := unittest.BlockWithParentFixture(parent.Header)
+				next.Header.View = next.Header.Height + 1 // set view so we are still in the same epoch
+				buildBlock(t, state, next)
+				parent = next
+			}
+
+			block5 := unittest.BlockWithParentFixture(parent.Header)
+			block5.SetPayload(unittest.PayloadFixture(unittest.WithSeals(seal2, seal3)))
+			buildBlock(t, state, block5)
+
+			snapshot := state.AtBlockID(block5.ID())
+
+			// build a valid child to ensure we have a QC
+			buildBlock(t, state, unittest.BlockWithParentFixture(block5.Header))
+
+			segment, err := snapshot.SealingSegment()
+			require.NoError(t, err)
+			// sealing segment should contain B1 and B5
+			// B5 is reference of snapshot, B1 is latest sealed
+			//unittest.AssertEqualBlocksLenAndOrder(t, []*flow.Block{block3, block4, block5}, segment.Blocks)
+			//unittest.AssertEqualBlocksLenAndOrder(t, []*flow.Block{block1, block2}, segment.ExtraBlocks[1:])
+			assert.Len(t, segment.ExecutionResults, 1)
+
+			assertSealingSegmentBlocksQueryableAfterBootstrap(t, snapshot)
+		})
+	})
 }
 
 // TestBootstrapSealingSegmentWithExtraBlocks test sealing segment where the segment blocks contain collection
