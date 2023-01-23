ca725b97c948b6e975a8c19847ce8396e8e9d2fc
Yahya Hassanzadeh <yhassanzadeh13@ku.edu.tr>
2023-01-23 10:18:48 -0800

    wires in basic connection manager of libp2p

--- network/p2p/connection/connManager.go
+++ network/p2p/connection/connManager.go
@@ -1,11 +1,14 @@
 package connection
 
 import (
+	"context"
+	"fmt"
 	"sync"
 
 	"github.com/libp2p/go-libp2p/core/connmgr"
 	"github.com/libp2p/go-libp2p/core/network"
 	"github.com/libp2p/go-libp2p/core/peer"
+	libp2pconnmgr "github.com/libp2p/go-libp2p/p2p/net/connmgr"
 	"github.com/multiformats/go-multiaddr"
 	"github.com/rs/zerolog"
 
@@ -16,31 +19,48 @@ import (
 // It is called back by libp2p when certain events occur such as opening/closing a stream, opening/closing connection etc.
 // This implementation updates networking metrics when a peer connection is added or removed
 type ConnManager struct {
-	connmgr.NullConnMgr                  // a null conn mgr provided by libp2p to allow implementing only the functions needed
-	n                   network.Notifiee // the notifiee callback provided by libp2p
-	log                 zerolog.Logger   // logger to log connection, stream and other statistics about libp2p
-	metrics             module.LibP2PConnectionMetrics
-
-	plk       sync.RWMutex
-	protected map[peer.ID]map[string]struct{}
+	basicConnMgr *libp2pconnmgr.BasicConnMgr
+	n            network.Notifiee // the notifiee callback provided by libp2p
+	log          zerolog.Logger   // logger to log connection, stream and other statistics about libp2p
+	metrics      module.LibP2PConnectionMetrics
+	mu           sync.RWMutex
+	protected    map[peer.ID]map[string]struct{}
 }
 
 var _ connmgr.ConnManager = (*ConnManager)(nil)
 
-func NewConnManager(log zerolog.Logger, metrics module.LibP2PConnectionMetrics) *ConnManager {
-	cn := &ConnManager{
-		log:         log.With().Str("component", "conn_manager").Logger(),
-		NullConnMgr: connmgr.NullConnMgr{},
-		metrics:     metrics,
-		protected:   make(map[peer.ID]map[string]struct{}),
+type ManagerConfig struct {
+	logger zerolog.Logger
+	metric module.LibP2PConnectionMetrics
+
+	// HighWatermark and LowWatermark govern the number of connections are maintained by the ConnManager.
+	// When the peer count exceeds the HighWatermark, as many peers will be pruned (and
+	// their connections terminated) until LowWatermark peers remain.
+	HighWatermark int // naming from libp2p
+	LowWatermark  int // naming from libp2p
+}
+
+func NewConnManager(cfg *ManagerConfig) (*ConnManager, error) {
+	basic, err := libp2pconnmgr.NewConnManager(cfg.LowWatermark, cfg.HighWatermark)
+	if err != nil {
+		return nil, fmt.Errorf("failed to create basic connection manager of libp2p: %w", err)
 	}
-	n := &network.NotifyBundle{ListenCloseF: cn.ListenCloseNotifee,
+
+	cn := &ConnManager{
+		log:          cfg.logger.With().Str("component", "connection_manager").Logger(),
+		metrics:      cfg.metric,
+		protected:    make(map[peer.ID]map[string]struct{}),
+		basicConnMgr: basic,
+	}
+	n := &network.NotifyBundle{
+		ListenCloseF:  cn.ListenCloseNotifee,
 		ListenF:       cn.ListenNotifee,
 		ConnectedF:    cn.Connected,
-		DisconnectedF: cn.Disconnected}
+		DisconnectedF: cn.Disconnected,
+	}
 	cn.n = n
 
-	return cn
+	return cn, nil
 }
 
 func (cm *ConnManager) Notifee() network.Notifiee {
@@ -48,15 +68,15 @@ func (cm *ConnManager) Notifee() network.Notifiee {
 }
 
 // ListenNotifee is called by libp2p when network starts listening on an addr
-func (cm *ConnManager) ListenNotifee(n network.Network, m multiaddr.Multiaddr) {
+func (cm *ConnManager) ListenNotifee(_ network.Network, m multiaddr.Multiaddr) {
 	cm.log.Debug().Str("multiaddress", m.String()).Msg("listen started")
 }
 
 // called by libp2p when network stops listening on an addr
 // * This is never called back by libp2p currently and may be a bug on their side
-func (cm *ConnManager) ListenCloseNotifee(n network.Network, m multiaddr.Multiaddr) {
-	// just log the multiaddress  on which we listen
-	cm.log.Debug().Str("multiaddress", m.String()).Msg("listen stopped ")
+func (cm *ConnManager) ListenCloseNotifee(_ network.Network, m multiaddr.Multiaddr) {
+	// just log the multiaddress on which we listen
+	cm.log.Debug().Str("multiaddress", m.String()).Msg("listen stopped")
 }
 
 // Connected is called by libp2p when a connection opened
@@ -91,17 +111,17 @@ func (cm *ConnManager) updateConnectionMetric(n network.Network) {
 func (cm *ConnManager) logConnectionUpdate(n network.Network, con network.Conn, logMsg string) {
 	cm.log.Debug().
 		Str("remote_peer", con.RemotePeer().String()).
-		Str("remote_addrs", con.RemoteMultiaddr().String()).
+		Str("remote_address", con.RemoteMultiaddr().String()).
 		Str("local_peer", con.LocalPeer().String()).
-		Str("local_addrs", con.LocalMultiaddr().String()).
+		Str("local_address", con.LocalMultiaddr().String()).
 		Str("direction", con.Stat().Direction.String()).
 		Int("total_connections", len(n.Conns())).
 		Msg(logMsg)
 }
 
 func (cm *ConnManager) Protect(id peer.ID, tag string) {
-	cm.plk.Lock()
-	defer cm.plk.Unlock()
+	cm.mu.Lock()
+	defer cm.mu.Unlock()
 
 	tags, ok := cm.protected[id]
 	if !ok {
@@ -112,8 +132,8 @@ func (cm *ConnManager) Protect(id peer.ID, tag string) {
 }
 
 func (cm *ConnManager) Unprotect(id peer.ID, tag string) (protected bool) {
-	cm.plk.Lock()
-	defer cm.plk.Unlock()
+	cm.mu.Lock()
+	defer cm.mu.Unlock()
 
 	tags, ok := cm.protected[id]
 	if !ok {
@@ -127,8 +147,8 @@ func (cm *ConnManager) Unprotect(id peer.ID, tag string) (protected bool) {
 }
 
 func (cm *ConnManager) IsProtected(id peer.ID, tag string) (protected bool) {
-	cm.plk.RLock()
-	defer cm.plk.RUnlock()
+	cm.mu.RLock()
+	defer cm.mu.RUnlock()
 
 	tags, ok := cm.protected[id]
 	if !ok {
@@ -141,4 +161,28 @@ func (cm *ConnManager) IsProtected(id peer.ID, tag string) (protected bool) {
 
 	_, protected = tags[tag]
 	return protected
+}
+
+func (cm *ConnManager) TagPeer(id peer.ID, s string, i int) {
+	cm.basicConnMgr.TagPeer(id, s, i)
+}
+
+func (cm *ConnManager) UntagPeer(p peer.ID, tag string) {
+	cm.basicConnMgr.UntagPeer(p, tag)
+}
+
+func (cm *ConnManager) UpsertTag(p peer.ID, tag string, upsert func(int) int) {
+	cm.basicConnMgr.UpsertTag(p, tag, upsert)
+}
+
+func (cm *ConnManager) GetTagInfo(p peer.ID) *connmgr.TagInfo {
+	return cm.basicConnMgr.GetTagInfo(p)
+}
+
+func (cm *ConnManager) TrimOpenConns(ctx context.Context) {
+	cm.basicConnMgr.TrimOpenConns(ctx)
+}
+
+func (cm *ConnManager) Close() error {
+	return cm.basicConnMgr.Close()
 }
