9e7c852f3ddaaed743be575ed0b72700a27501f0
Yahya Hassanzadeh <yhassanzadeh13@ku.edu.tr>
2023-01-24 08:54:14 -0800

    adds grace and silence period parameters

--- network/p2p/connection/connManager.go
+++ network/p2p/connection/connManager.go
@@ -3,26 +3,32 @@ package connection
 import (
 	"context"
 	"fmt"
-	"sync"
+	"time"
 
 	"github.com/libp2p/go-libp2p/core/connmgr"
 	"github.com/libp2p/go-libp2p/core/network"
 	"github.com/libp2p/go-libp2p/core/peer"
 	libp2pconnmgr "github.com/libp2p/go-libp2p/p2p/net/connmgr"
-	"github.com/multiformats/go-multiaddr"
 	"github.com/rs/zerolog"
 
 	"github.com/onflow/flow-go/module"
+	"github.com/onflow/flow-go/network/p2p/connection/internal"
 )
 
 const (
-	// DefaultHighWatermark is the default value for the high watermark (i.e., max number of connections).
+	// defaultHighWatermark is the default value for the high watermark (i.e., max number of connections).
 	// We assume a complete topology graph with maximum of 500 nodes.
 	defaultHighWatermark = 500
 
-	// DefaultLowWatermark is the default value for the low watermark (i.e., min number of connections).
+	// defaultLowWatermark is the default value for the low watermark (i.e., min number of connections).
 	// We assume a complete topology graph with minimum of 450 nodes.
 	defaultLowWatermark = 450
+
+	// defaultGracePeriod is the default value for the grace period (i.e., time to wait before pruning a new connection).
+	defaultGracePeriod = 1 * time.Minute
+
+	// defaultSilencePeriod is the default value for the silence period (i.e., time to wait before start pruning connections).
+	defaultSilencePeriod = 10 * time.Second
 )
 
 // DefaultConnManagerConfig returns the default configuration for the connection manager.
@@ -30,6 +36,8 @@ func DefaultConnManagerConfig() *ManagerConfig {
 	return &ManagerConfig{
 		HighWatermark: defaultHighWatermark,
 		LowWatermark:  defaultLowWatermark,
+		GracePeriod:   defaultGracePeriod,
+		SilencePeriod: defaultSilencePeriod,
 	}
 }
 
@@ -38,11 +46,10 @@ func DefaultConnManagerConfig() *ManagerConfig {
 // This implementation updates networking metrics when a peer connection is added or removed
 type ConnManager struct {
 	basicConnMgr *libp2pconnmgr.BasicConnMgr
+	basicNotifee network.Notifiee
 	n            network.Notifiee // the notifiee callback provided by libp2p
 	log          zerolog.Logger   // logger to log connection, stream and other statistics about libp2p
 	metrics      module.LibP2PConnectionMetrics
-	mu           sync.RWMutex
-	protected    map[peer.ID]map[string]struct{}
 }
 
 var _ connmgr.ConnManager = (*ConnManager)(nil)
@@ -53,6 +60,11 @@ type ManagerConfig struct {
 	// their connections terminated) until LowWatermark peers remain.
 	HighWatermark int // naming from libp2p
 	LowWatermark  int // naming from libp2p
+
+	// SilencePeriod is the time to wait before start pruning connections.
+	SilencePeriod time.Duration // naming from libp2p
+	// GracePeriod is the time to wait before pruning a new connection.
+	GracePeriod time.Duration // naming from libp2p
 }
 
 type ConnManagerOpt func(*ConnManager)
@@ -68,7 +80,11 @@ func WithNotifyBundle(n *network.NotifyBundle) ConnManagerOpt {
 // The error is not benign, and we should crash the node if it happens.
 // It is a malpractice to start the node without connection manager.
 func NewConnManager(logger zerolog.Logger, metric module.LibP2PConnectionMetrics, cfg *ManagerConfig, opt ...ConnManagerOpt) (*ConnManager, error) {
-	basic, err := libp2pconnmgr.NewConnManager(cfg.LowWatermark, cfg.HighWatermark)
+	basic, err := libp2pconnmgr.NewConnManager(
+		cfg.LowWatermark,
+		cfg.HighWatermark,
+		libp2pconnmgr.WithGracePeriod(cfg.GracePeriod),
+		libp2pconnmgr.WithSilencePeriod(cfg.SilencePeriod))
 	if err != nil {
 		return nil, fmt.Errorf("failed to create basic connection manager of libp2p: %w", err)
 	}
@@ -76,16 +92,11 @@ func NewConnManager(logger zerolog.Logger, metric module.LibP2PConnectionMetrics
 	cn := &ConnManager{
 		log:          logger.With().Str("component", "connection_manager").Logger(),
 		metrics:      metric,
-		protected:    make(map[peer.ID]map[string]struct{}),
 		basicConnMgr: basic,
 	}
-	n := &network.NotifyBundle{
-		ListenCloseF:  cn.ListenCloseNotifee,
-		ListenF:       cn.ListenNotifee,
-		ConnectedF:    cn.Connected,
-		DisconnectedF: cn.Disconnected,
-	}
-	cn.n = n
+
+	// aggregates the notifiee callbacks from libp2p and our own notifiee into one.
+	cn.n = internal.NewRelayNotifee(internal.NewLoggerNotifiee(cn.log, metric), cn.basicConnMgr.Notifee())
 
 	for _, apply := range opt {
 		apply(cn)
@@ -94,6 +105,8 @@ func NewConnManager(logger zerolog.Logger, metric module.LibP2PConnectionMetrics
 	cn.log.Info().
 		Int("low_watermark", cfg.LowWatermark).
 		Int("high_watermark", cfg.HighWatermark).
+		Dur("grace_period", cfg.GracePeriod).
+		Dur("silence_period", cfg.SilencePeriod).
 		Msg("connection manager initialized")
 
 	return cn, nil
@@ -103,100 +116,16 @@ func (cm *ConnManager) Notifee() network.Notifiee {
 	return cm.n
 }
 
-// ListenNotifee is called by libp2p when network starts listening on an addr
-func (cm *ConnManager) ListenNotifee(_ network.Network, m multiaddr.Multiaddr) {
-	cm.log.Debug().Str("multiaddress", m.String()).Msg("listen started")
-}
-
-// called by libp2p when network stops listening on an addr
-// * This is never called back by libp2p currently and may be a bug on their side
-func (cm *ConnManager) ListenCloseNotifee(_ network.Network, m multiaddr.Multiaddr) {
-	// just log the multiaddress on which we listen
-	cm.log.Debug().Str("multiaddress", m.String()).Msg("listen stopped")
-}
-
-// Connected is called by libp2p when a connection opened
-func (cm *ConnManager) Connected(n network.Network, con network.Conn) {
-	cm.logConnectionUpdate(n, con, "connection established")
-	cm.updateConnectionMetric(n)
-}
-
-// Disconnected is called by libp2p when a connection closed
-func (cm *ConnManager) Disconnected(n network.Network, con network.Conn) {
-	cm.logConnectionUpdate(n, con, "connection removed")
-	cm.updateConnectionMetric(n)
-}
-
-func (cm *ConnManager) updateConnectionMetric(n network.Network) {
-	var totalInbound uint = 0
-	var totalOutbound uint = 0
-
-	for _, conn := range n.Conns() {
-		switch conn.Stat().Direction {
-		case network.DirInbound:
-			totalInbound++
-		case network.DirOutbound:
-			totalOutbound++
-		}
-	}
-
-	cm.metrics.InboundConnections(totalInbound)
-	cm.metrics.OutboundConnections(totalOutbound)
-}
-
-func (cm *ConnManager) logConnectionUpdate(n network.Network, con network.Conn, logMsg string) {
-	cm.log.Debug().
-		Str("remote_peer", con.RemotePeer().String()).
-		Str("remote_address", con.RemoteMultiaddr().String()).
-		Str("local_peer", con.LocalPeer().String()).
-		Str("local_address", con.LocalMultiaddr().String()).
-		Str("direction", con.Stat().Direction.String()).
-		Int("total_connections", len(n.Conns())).
-		Msg(logMsg)
-}
-
 func (cm *ConnManager) Protect(id peer.ID, tag string) {
-	cm.mu.Lock()
-	defer cm.mu.Unlock()
-
-	tags, ok := cm.protected[id]
-	if !ok {
-		tags = make(map[string]struct{}, 2)
-		cm.protected[id] = tags
-	}
-	tags[tag] = struct{}{}
+	cm.basicConnMgr.Protect(id, tag)
 }
 
-func (cm *ConnManager) Unprotect(id peer.ID, tag string) (protected bool) {
-	cm.mu.Lock()
-	defer cm.mu.Unlock()
-
-	tags, ok := cm.protected[id]
-	if !ok {
-		return false
-	}
-	if delete(tags, tag); len(tags) == 0 {
-		delete(cm.protected, id)
-		return false
-	}
-	return true
+func (cm *ConnManager) Unprotect(id peer.ID, tag string) bool {
+	return cm.basicConnMgr.Unprotect(id, tag)
 }
 
-func (cm *ConnManager) IsProtected(id peer.ID, tag string) (protected bool) {
-	cm.mu.RLock()
-	defer cm.mu.RUnlock()
-
-	tags, ok := cm.protected[id]
-	if !ok {
-		return false
-	}
-
-	if tag == "" {
-		return true
-	}
-
-	_, protected = tags[tag]
-	return protected
+func (cm *ConnManager) IsProtected(id peer.ID, tag string) bool {
+	return cm.basicConnMgr.IsProtected(id, tag)
 }
 
 func (cm *ConnManager) TagPeer(id peer.ID, s string, i int) {
