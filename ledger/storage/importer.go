1b95bdab8c2b424eb60a7b792ee791c6137de44f
Leo Zhang (zhangchiqing) <zhangchiqing@gmail.com>
2023-01-26 16:14:38 -0800

    disable importing

--- ledger/storage/importer.go
+++ ledger/storage/importer.go
@@ -1,108 +1,103 @@
 package storage
 
 import (
-	"context"
 	"encoding/binary"
 	"fmt"
 
 	"github.com/onflow/flow-go/ledger"
 	"github.com/onflow/flow-go/ledger/common/hash"
 	"github.com/onflow/flow-go/ledger/complete/mtrie/flattener"
-	"github.com/onflow/flow-go/ledger/complete/mtrie/node"
-	"github.com/onflow/flow-go/ledger/complete/wal"
-	"github.com/onflow/flow-go/module/util"
-	"github.com/rs/zerolog"
 )
 
 // ImportLeafNodesFromCheckpoint takes a checkpoint file specified by the dir and fileName,
 // reads all the leaf nodes from the checkpoint file, and store them into the given
 // storage store.
-func ImportLeafNodesFromCheckpoint(dir string, fileName string, logger *zerolog.Logger, store ledger.Storage) error {
-	tries, err := wal.OpenAndReadCheckpointV6(dir, fileName, logger)
-	if err != nil {
-		return fmt.Errorf("could not read tries: %w", err)
-	}
-
-	if len(tries) == 0 {
-		return fmt.Errorf("could not find any trie root")
-	}
-
-	trie := tries[len(tries)-1]
-	leafNodes := trie.AllLeafNodes()
-
-	err = importLeafNodesConcurrently(leafNodes, logger, store)
-	if err != nil {
-		return fmt.Errorf("fail to store the leafNode to store: %w", err)
-	}
-
-	return nil
-}
-
-func importLeafNodesConcurrently(leafNodes []*node.Node, logger *zerolog.Logger, store ledger.Storage) error {
-	jobs := make(chan *node.Node, len(leafNodes))
-	results := make(chan error, len(leafNodes))
-
-	nWorker := 10
-	if len(leafNodes) < nWorker {
-		nWorker = len(leafNodes)
-	}
-
-	ctx, cancel := context.WithCancel(context.Background())
-	defer cancel()
-
-	// create nWorker number of workers
-	for w := 0; w < nWorker; w++ {
-		go func() {
-			scratch := make([]byte, 1024*4)
-			for leafNode := range jobs {
-				err := storeLeafNode(store, leafNode, scratch)
-				results <- err
-
-				if err != nil {
-					cancel()
-					return
-				}
-
-				select {
-				case <-ctx.Done():
-					return
-				default:
-				}
-			}
-		}()
-	}
-
-	// buffer all jobs
-	for _, leafNode := range leafNodes {
-		jobs <- leafNode
-	}
-	close(jobs)
-
-	logProgress := util.LogProgress("importing leaf nodes to storage", len(leafNodes), logger)
-	// waiting for results
-	for i := 0; i < len(leafNodes); i++ {
-		logProgress(i)
-		err := <-results
-		if err != nil {
-			return err
-		}
-	}
-
-	return nil
-}
-
-func storeLeafNode(store ledger.Storage, leafNode *node.Node, scratch []byte) error {
-	hash, encoded, err := EncodeLeafNode(leafNode, scratch)
-	if err != nil {
-		return fmt.Errorf("could not encode leaf node: %w", err)
-	}
-
-	err = store.Set(hash, encoded)
-	if err != nil {
-		return fmt.Errorf("could not store encoded leaf node: %w", err)
-	}
-	return nil
-}
+// func ImportLeafNodesFromCheckpoint(dir string, fileName string, logger *zerolog.Logger, store ledger.Storage) error {
+// 	tries, err := wal.OpenAndReadCheckpointV6(dir, fileName, logger)
+// 	if err != nil {
+// 		return fmt.Errorf("could not read tries: %w", err)
+// 	}
+//
+// 	if len(tries) == 0 {
+// 		return fmt.Errorf("could not find any trie root")
+// 	}
+//
+// 	trie := tries[len(tries)-1]
+// 	leafNodes := trie.AllLeafNodes()
+//
+// 	err = importLeafNodesConcurrently(leafNodes, logger, store)
+// 	if err != nil {
+// 		return fmt.Errorf("fail to store the leafNode to store: %w", err)
+// 	}
+//
+// 	return nil
+// }
+//
+// func importLeafNodesConcurrently(leafNodes []*node.Node, logger *zerolog.Logger, store ledger.Storage) error {
+// 	jobs := make(chan *node.Node, len(leafNodes))
+// 	results := make(chan error, len(leafNodes))
+//
+// 	nWorker := 10
+// 	if len(leafNodes) < nWorker {
+// 		nWorker = len(leafNodes)
+// 	}
+//
+// 	ctx, cancel := context.WithCancel(context.Background())
+// 	defer cancel()
+//
+// 	// create nWorker number of workers
+// 	for w := 0; w < nWorker; w++ {
+// 		go func() {
+// 			scratch := make([]byte, 1024*4)
+// 			for leafNode := range jobs {
+// 				err := storeLeafNode(store, leafNode, scratch)
+// 				results <- err
+//
+// 				if err != nil {
+// 					cancel()
+// 					return
+// 				}
+//
+// 				select {
+// 				case <-ctx.Done():
+// 					return
+// 				default:
+// 				}
+// 			}
+// 		}()
+// 	}
+//
+// 	// buffer all jobs
+// 	for _, leafNode := range leafNodes {
+// 		jobs <- leafNode
+// 	}
+// 	close(jobs)
+//
+// 	logProgress := util.LogProgress("importing leaf nodes to storage", len(leafNodes), logger)
+// 	// waiting for results
+// 	for i := 0; i < len(leafNodes); i++ {
+// 		logProgress(i)
+// 		err := <-results
+// 		if err != nil {
+// 			return err
+// 		}
+// 	}
+//
+// 	return nil
+// }
+//
+// func storeLeafNode(store ledger.Storage, leafNode *node.Node, scratch []byte) error {
+// 	hash, encoded, err := EncodeLeafNode(leafNode, scratch)
+// 	if err != nil {
+// 		return fmt.Errorf("could not encode leaf node: %w", err)
+// 	}
+//
+// 	err = store.Set(hash, encoded)
+// 	if err != nil {
+// 		return fmt.Errorf("could not store encoded leaf node: %w", err)
+// 	}
+// 	return nil
+// }
 
 const (
 	encHeightSize        = 2
@@ -169,82 +164,4 @@ func DecodePayload(encoded []byte) (ledger.Path, *ledger.Payload, error) {
 	}
 
 	return path, payload, nil
-}
-
-// EncodeLeafNode returns the hash and the encoded bytes of the given leaf node.
-// the encoded bytes contains:
-// - height (2 bytes)
-// - path (32 bytes)
-// - payload (4 bytes)
-// - payload (n bytes)
-func EncodeLeafNode(n *node.Node, scratch []byte) (hash.Hash, []byte, error) {
-	encPayloadSize := ledger.EncodedPayloadLengthWithoutPrefix(n.Payload(), flattener.PayloadEncodingVersion)
-
-	encodedNodeSize := encHeightSize +
-		encPathSize +
-		encPayloadLengthSize +
-		encPayloadSize
-
-	buf := scratch
-	if len(scratch) < encodedNodeSize {
-		buf = make([]byte, encodedNodeSize)
-	}
-
-	pos := 0
-
-	// encode height (2 bytes Big Endian)
-	binary.BigEndian.PutUint16(buf[pos:], uint16(n.Height()))
-	pos += encHeightSize
-
-	// encode path (32 bytes path)
-	path := n.Path()
-	copy(buf[pos:], path[:])
-	pos += encPathSize
-
-	// encode payload (4 bytes Big Endian for encoded payload length and n bytes encoded payload)
-	binary.BigEndian.PutUint32(buf[pos:], uint32(encPayloadSize))
-	pos += encPayloadLengthSize
-
-	// EncodeAndAppendPayloadWithoutPrefix appends encoded payload to the resliced buf.
-	// Returned buf is resliced to include appended payload.
-	buf = ledger.EncodeAndAppendPayloadWithoutPrefix(buf[:pos], n.Payload(), flattener.PayloadEncodingVersion)
-	return n.Hash(), buf, nil
-}
-
-// DecodeLeafNode takes the hash and the encoded bytes, returns the decoded leaf node
-func DecodeLeafNode(nodeHash hash.Hash, encoded []byte) (*node.Node, error) {
-	if len(encoded) < encHeightSize+encPathSize+encPayloadLengthSize {
-		return nil, fmt.Errorf("could not decode leaf node, not enough bytes: %v", len(encoded))
-	}
-
-	pos := 0
-
-	// decode height
-	height := binary.BigEndian.Uint16(encoded[pos : pos+encHeightSize])
-	pos += encHeightSize
-
-	// decode path
-	path, err := ledger.ToPath(encoded[pos : pos+encPathSize])
-	if err != nil {
-		return nil, fmt.Errorf("could node decode path: %w", err)
-	}
-	pos += encPathSize
-
-	// decode payload size
-	expectedSize := binary.BigEndian.Uint32(encoded[pos : pos+encPayloadLengthSize])
-	pos += encPayloadLengthSize
-
-	// decode payload
-	actualSize := uint32(len(encoded) - pos)
-	if expectedSize != actualSize {
-		return nil, fmt.Errorf("incorrect payload size, expect %v, actual %v", expectedSize, actualSize)
-	}
-
-	payload, err := ledger.DecodePayloadWithoutPrefix(encoded[pos:], false, flattener.PayloadEncodingVersion)
-	if err != nil {
-		return nil, fmt.Errorf("could not decode payload: %w", err)
-	}
-
-	node := node.NewNode(int(height), nil, nil, path, payload, nodeHash)
-	return node, nil
 }
--- ledger/storage/importer_test.go
+++ ledger/storage/importer_test.go
@@ -4,14 +4,10 @@ import (
 	"fmt"
 	"testing"
 
-	"github.com/onflow/flow-go/ledger"
+	"github.com/stretchr/testify/require"
+
 	"github.com/onflow/flow-go/ledger/common/hash"
 	"github.com/onflow/flow-go/ledger/common/testutils"
-	"github.com/onflow/flow-go/ledger/complete/mtrie/node"
-	"github.com/onflow/flow-go/ledger/complete/mtrie/trie"
-	"github.com/onflow/flow-go/ledger/complete/wal"
-	"github.com/onflow/flow-go/utils/unittest"
-	"github.com/stretchr/testify/require"
 )
 
 func TestEncodeDecodePayload(t *testing.T) {
@@ -38,117 +34,57 @@ func TestEncodeDecodePayload(t *testing.T) {
 	require.Equal(t, encoded, encodedAgain)
 }
 
-func TestEncodeDecodeLeafNode(t *testing.T) {
-	leafNode := leafNodeFixture()
-	scratch := make([]byte, 1024*4)
-
-	// encode the leaf node
-	nodeHash, encoded, err := EncodeLeafNode(leafNode, scratch)
-	require.NoError(t, err)
-
-	// decode the leaf node
-	decoded, err := DecodeLeafNode(nodeHash, encoded)
-	require.NoError(t, err)
-
-	// decoded should be the same as original
-	require.NoError(t, leafNodeEqual(leafNode, decoded))
-
-	// encode again the decoded leaf node
-	fromDecodedHash, encodedFromDecoded, err := EncodeLeafNode(decoded, scratch)
-	require.NoError(t, err)
-
-	// encode again from the decoded leaf node should be the same as originally encoded
-	require.Equal(t, nodeHash, fromDecodedHash)
-	require.Equal(t, encoded, encodedFromDecoded)
-}
-
-func leafNodeEqual(n1 *node.Node, n2 *node.Node) error {
-	if !n1.IsLeaf() {
-		return fmt.Errorf("n1 is not leaf")
-	}
-
-	if !n2.IsLeaf() {
-		return fmt.Errorf("n2 is not leaf")
-	}
-
-	if n1.Height() != n2.Height() {
-		return fmt.Errorf("different height (%v != %v)", n1.Height(), n2.Height())
-	}
-
-	if *n1.Path() != *n2.Path() {
-		return fmt.Errorf("different path (%v != %v)", n1.Path(), n2.Path())
-	}
-
-	if n1.Hash() != n2.Hash() {
-		return fmt.Errorf("different hash (%v != %v)", n1.Hash(), n2.Hash())
-	}
-
-	if !n1.Payload().Equals(n2.Payload()) {
-		return fmt.Errorf("different payload (%v != %v)", n1.Payload(), n2.Payload())
-	}
-
-	return nil
-}
-
-func leafNodeFixture() *node.Node {
-	height := 255
-	path := testutils.PathByUint8(0)
-	payload := testutils.LightPayload8('A', 'a')
-	nodeHash := hash.Hash([32]byte{1, 2, 3})
-	return node.NewNode(height, nil, nil, ledger.Path(path), payload, nodeHash)
-}
-
-func TestImportAndRead(t *testing.T) {
-	unittest.RunWithTempDir(t, func(dir string) {
-		// create a trie for testing
-		tries, err := createTrie()
-		require.NoError(t, err, "could not create trie")
-		logger := unittest.Logger()
-		fileName := "root.checkpoint"
-		store := createMockStore()
-
-		// create a checkfile from the trie
-		require.NoError(t, wal.StoreCheckpointV6Concurrently(tries, dir, fileName, &logger),
-			"fail to store checkpoint")
-
-		// import the checkpoint into storage mock
-		require.NoError(t, ImportLeafNodesFromCheckpoint(dir, fileName, &logger, store),
-			"fail to import leaf nodes from checkpoint")
-
-		// check all leaf nodes are stored in the storage mock
-		leafNodes := tries[0].AllLeafNodes()
-		for _, n := range leafNodes {
-			encoded, err := store.Get(n.Hash())
-			require.NoError(t, err, "could not get node from storage")
-
-			decoded, err := DecodeLeafNode(n.Hash(), encoded)
-			require.NoError(t, leafNodeEqual(n, decoded))
-		}
-
-		// check didn't store extra nodes
-		require.Equal(t, len(leafNodes), store.Count())
-	})
-}
-
-func createTrie() ([]*trie.MTrie, error) {
-	emptyTrie := trie.NewEmptyMTrie()
-
-	p1 := testutils.PathByUint8(0)
-	v1 := testutils.LightPayload8('A', 'a')
-
-	p2 := testutils.PathByUint8(1)
-	v2 := testutils.LightPayload8('B', 'b')
-
-	paths := []ledger.Path{p1, p2}
-	payloads := []ledger.Payload{*v1, *v2}
-
-	updatedTrie, _, err := trie.NewTrieWithUpdatedRegisters(emptyTrie, paths, payloads, true)
-	if err != nil {
-		return nil, err
-	}
-	tries := []*trie.MTrie{emptyTrie, updatedTrie}
-	return tries, nil
-}
+// func TestImportAndRead(t *testing.T) {
+// 	unittest.RunWithTempDir(t, func(dir string) {
+// 		// create a trie for testing
+// 		tries, err := createTrie()
+// 		require.NoError(t, err, "could not create trie")
+// 		logger := unittest.Logger()
+// 		fileName := "root.checkpoint"
+// 		store := createMockStore()
+//
+// 		// create a checkfile from the trie
+// 		require.NoError(t, wal.StoreCheckpointV6Concurrently(tries, dir, fileName, &logger),
+// 			"fail to store checkpoint")
+//
+// 		// import the checkpoint into storage mock
+// 		require.NoError(t, ImportLeafNodesFromCheckpoint(dir, fileName, &logger, store),
+// 			"fail to import leaf nodes from checkpoint")
+//
+// 		// check all leaf nodes are stored in the storage mock
+// 		leafNodes := tries[0].AllLeafNodes()
+// 		for _, n := range leafNodes {
+// 			encoded, err := store.Get(n.Hash())
+// 			require.NoError(t, err, "could not get node from storage")
+//
+// 			decoded, err := DecodeLeafNode(n.Hash(), encoded)
+// 			require.NoError(t, leafNodeEqual(n, decoded))
+// 		}
+//
+// 		// check didn't store extra nodes
+// 		require.Equal(t, len(leafNodes), store.Count())
+// 	})
+// }
+//
+// func createTrie() ([]*trie.MTrie, error) {
+// 	emptyTrie := trie.NewEmptyMTrie()
+//
+// 	p1 := testutils.PathByUint8(0)
+// 	v1 := testutils.LightPayload8('A', 'a')
+//
+// 	p2 := testutils.PathByUint8(1)
+// 	v2 := testutils.LightPayload8('B', 'b')
+//
+// 	paths := []ledger.Path{p1, p2}
+// 	payloads := []ledger.Payload{*v1, *v2}
+//
+// 	updatedTrie, _, err := trie.NewTrieWithUpdatedRegisters(emptyTrie, paths, payloads, true)
+// 	if err != nil {
+// 		return nil, err
+// 	}
+// 	tries := []*trie.MTrie{emptyTrie, updatedTrie}
+// 	return tries, nil
+// }
 
 func createMockStore() *store {
 	return &store{
