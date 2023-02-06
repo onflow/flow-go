7344cff288ff84c827e4be271896207c04913cc2
Yahya Hassanzadeh <yhassanzadeh13@ku.edu.tr>
2023-02-06 15:57:51 -0800

    wires in metrics

--- network/p2p/p2pbuilder/libp2pNodeBuilder.go
+++ network/p2p/p2pbuilder/libp2pNodeBuilder.go
@@ -296,6 +296,8 @@ func (builder *LibP2PNodeBuilder) Build() (p2p.LibP2PNode, error) {
 			Int64("allowed_memory", mem).
 			Int("allowed_file_descriptors", fd).
 			Msg("allowed memory and file descriptors are fetched from the system")
+		limitLogger := newLimitConfigLogger(builder.logger).loggerForLimits(l)
+		limitLogger.Info().Msg("libp2p resource manager limits are set")
 		
 		opts = append(opts, libp2p.ResourceManager(mgr))
 		builder.logger.Info().Msg("libp2p resource manager is set to default with metrics")
