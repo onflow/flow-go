3c676cb7e701e2587ab0b619e11a89fd1cf6c4c2
Yahya Hassanzadeh <yhassanzadeh13@ku.edu.tr>
2023-02-07 15:38:40 -0800

    implements gossipsub notifier

--- network/p2p/consumer.go
+++ network/p2p/consumer.go
@@ -1,6 +1,8 @@
 package p2p
 
 import (
+	"github.com/libp2p/go-libp2p/core/peer"
+
 	"github.com/onflow/flow-go/model/flow"
 )
 
@@ -14,4 +16,27 @@ type NodeBlockListConsumer interface {
 	// Implementation must be concurrency safe; Non-blocking;
 	// and must handle repetition of the same events (with some processing overhead).
 	OnNodeBlockListUpdate(list flow.IdentifierList)
+}
+
+// ControlMessageType is the type of control message, as defined in the libp2p pubsub spec.
+type ControlMessageType string
+
+const (
+	CtrlMsgIHave ControlMessageType = "IHAVE"
+	CtrlMsgIWant ControlMessageType = "IWANT"
+	CtrlMsgGraft ControlMessageType = "GRAFT"
+	CtrlMsgPrune ControlMessageType = "PRUNE"
+)
+
+// GossipSubRpcInspectorConsumer is the interface for a consumer of inspection result for GossipSub RPC messages.
+// Implementations must:
+//   - be concurrency safe
+//   - be non-blocking
+type GossipSubRpcInspectorConsumer interface {
+	// OnInvalidControlMessage is called when a control message is received that is invalid according to the
+	// Flow protocol specification.
+	// The int parameter is the count of invalid messages received from the peer.
+	// Prerequisites:
+	// Implementation must be concurrency safe and non-blocking.
+	OnInvalidControlMessage(peer.ID, ControlMessageType, int)
 }
