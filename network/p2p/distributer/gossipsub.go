ba749b6ee4aa1d6d5978bf79507912177a2a90af
Yahya Hassanzadeh <yhassanzadeh13@ku.edu.tr>
2023-02-07 17:37:12 -0800

    adds notification distributor

--- network/p2p/distributer/gossipsub.go
+++ network/p2p/distributer/gossipsub.go
@@ -1,1 +1,55 @@
 package distributer
+
+import (
+	"github.com/libp2p/go-libp2p/core/peer"
+	"github.com/rs/zerolog"
+
+	"github.com/onflow/flow-go/engine/common/handler"
+	"github.com/onflow/flow-go/model/flow"
+	"github.com/onflow/flow-go/network/p2p"
+)
+
+type GossipSubInspectorNotificationDistributor struct {
+	log       zerolog.Logger
+	handler   *handler.AsyncEventHandler
+	notifiers []p2p.GossipSubRpcInspectorConsumer
+}
+
+func NewGossipSubInspectorNotificationDistributor(log zerolog.Logger, queueSize uint32, workerCount uint) *GossipSubInspectorNotificationDistributor {
+	g := &GossipSubInspectorNotificationDistributor{
+		handler: handler.NewAsyncEventHandler(log, queueSize, workerCount),
+	}
+
+	g.handler.RegisterProcessor(g.ProcessQueuedNotifications)
+
+	return g
+}
+
+var _ p2p.GossipSubRpcInspectorConsumer = (*GossipSubInspectorNotificationDistributor)(nil)
+
+type invalidControlMessage struct {
+	peerID  peer.ID
+	msgType p2p.ControlMessageType
+	count   int
+}
+
+func (g *GossipSubInspectorNotificationDistributor) OnInvalidControlMessage(id peer.ID, messageType p2p.ControlMessageType, i int) {
+	// TODO: handle error
+	_ = g.handler.Process(flow.ZeroID, invalidControlMessage{
+		peerID:  id,
+		msgType: messageType,
+		count:   i,
+	})
+}
+
+func (g *GossipSubInspectorNotificationDistributor) ProcessQueuedNotifications(_ flow.Identifier, notification interface{}) {
+	switch notification := notification.(type) {
+	case invalidControlMessage:
+		for _, notifier := range g.notifiers {
+			notifier.OnInvalidControlMessage(notification.peerID, notification.msgType, notification.count)
+		}
+		break
+	default:
+		g.log.Fatal().Msgf("unknown notification type: %T", notification)
+	}
+}
