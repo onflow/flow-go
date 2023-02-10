28786d7f796174ddaa1dd6bbb26abbe1f8d9b6bb
Yurii Oleksyshyn <yuraolex@gmail.com>
2023-02-10 20:42:55 +0200

    Updated QC storage to include tx API

--- storage/qcs.go
+++ storage/qcs.go
@@ -1,14 +1,17 @@
 package storage
 
-import "github.com/onflow/flow-go/model/flow"
+import (
+	"github.com/onflow/flow-go/model/flow"
+	"github.com/onflow/flow-go/storage/badger/transaction"
+)
 
 // QuorumCertificates represents storage for Quorum Certificates.
 // Quorum Certificates are distributed using blocks, each block incorporates a QC for its parent.
 // This specific storage allows to store and query QCs discovered during following the protocol or participating in.
 type QuorumCertificates interface {
-	// Store stores a quorum certificate, QC is indexed by QC.BlockID.
+	// StoreTx stores a Quorum Certificate as part of database transaction QC is indexed by QC.BlockID.
 	// No errors are expected during normal operations.
-	Store(qc *flow.QuorumCertificate) error
+	StoreTx(qc *flow.QuorumCertificate) func(*transaction.Tx) error
 	// ByBlockID returns QC that certifies block referred by blockID.
 	// * storage.ErrNotFound if no QC for blockID doesn't exist.
 	ByBlockID(blockID flow.Identifier) (*flow.QuorumCertificate, error)
