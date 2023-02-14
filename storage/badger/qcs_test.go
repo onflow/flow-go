7439aea9a5daabaf15af72ee38e99497f4efe9f0
Yurii Oleksyshyn <yuraolex@gmail.com>
2023-02-14 12:36:46 +0200

    Added test for storing & retrieving QCs

--- storage/badger/qcs_test.go
+++ storage/badger/qcs_test.go
@@ -1,1 +1,30 @@
-package badger
+package badger_test
+
+import (
+	"testing"
+
+	"github.com/dgraph-io/badger/v2"
+	"github.com/stretchr/testify/require"
+
+	"github.com/onflow/flow-go/module/metrics"
+	bstorage "github.com/onflow/flow-go/storage/badger"
+	"github.com/onflow/flow-go/storage/badger/operation"
+	"github.com/onflow/flow-go/storage/badger/transaction"
+	"github.com/onflow/flow-go/utils/unittest"
+)
+
+func TestQuorumCertificates_StoreTx(t *testing.T) {
+	unittest.RunWithBadgerDB(t, func(db *badger.DB) {
+		metrics := metrics.NewNoopCollector()
+		store := bstorage.NewQuorumCertificates(metrics, db, 10)
+		qc := unittest.QuorumCertificateFixture()
+
+		err := operation.RetryOnConflictTx(db, transaction.Update, store.StoreTx(qc))
+		require.NoError(t, err)
+
+		actual, err := store.ByBlockID(qc.BlockID)
+		require.NoError(t, err)
+
+		require.Equal(t, qc, actual)
+	})
+}
