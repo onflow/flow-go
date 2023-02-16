c9ffda78db819b8abd13e5ad9143c07ad7b23f4c
Leo Zhang (zhangchiqing) <zhangchiqing@gmail.com>
2023-02-16 15:46:01 -0800

    proof

--- ledger/common/proof/proof.go
+++ ledger/common/proof/proof.go
@@ -18,9 +18,8 @@ func VerifyTrieProof(p *ledger.TrieProof, expectedState ledger.State) bool {
 	}
 	// We start with the leaf and hash our way upwards towards the root
 	proofIndex := len(p.Interims) - 1                                                        // the index of the last non-default value furthest down the tree (-1 if there is none)
-	leafHash := ledger.ComputeCompactValue(hash.Hash(p.Path), p.Payload.Value(), leafHeight) // we first compute the hash of the compact leaf (at height leafHeight)
-	computed := leafHash
-	for h := leafHeight + 1; h <= treeHeight; h++ { // then, we hash our way upwards until we hit the root (at height `treeHeight`)
+	computed := ledger.ComputeCompactValue(hash.Hash(p.Path), p.Payload.Value(), leafHeight) // we first compute the hash of the compact leaf (at height leafHeight)
+	for h := leafHeight + 1; h <= treeHeight; h++ {                                          // then, we hash our way upwards until we hit the root (at height `treeHeight`)
 		// we are currently at a node n (initially the leaf). In this iteration, we want to compute the
 		// parent's hash. Here, h is the height of the parent, whose hash want to compute.
 		// The parent has two children: child n, whose hash we have already computed (aka `computed`);
