ffc03982ffac7b85c4932b1eb717a87d70b6a2f6
Yurii Oleksyshyn <yuraolex@gmail.com>
2023-02-20 10:50:04 +0200

    Apply suggestions from code review
    Co-authored-by: Alexander Hentschel <alex.hentschel@axiomzen.co>

--- storage/qcs.go
+++ storage/qcs.go
@@ -6,14 +6,13 @@ import (
 )
 
 // QuorumCertificates represents storage for Quorum Certificates.
-// Quorum Certificates are distributed using blocks, each block incorporates a QC for its parent.
-// This specific storage allows to store and query QCs discovered from both consensus participants and followers.
-// When stored, QCs are indexed by the ID of the block they reference (not the block they are included within).
+// Quorum Certificates are distributed using blocks, where a block incorporates a QC for its parent.
+// When stored, QCs are indexed by the ID of the block they certify (not the block they are included within).
 // In the example below, `QC_1` is indexed by `Block_1.ID()`
 // Block_1 <- Block_2(QC_1)
 type QuorumCertificates interface {
 	// StoreTx stores a Quorum Certificate as part of database transaction QC is indexed by QC.BlockID.
-	// * storage.ErrAlreadyExists if QC for blockID already exists
+	// * storage.ErrAlreadyExists if any QC for blockID is already stored
 	StoreTx(qc *flow.QuorumCertificate) func(*transaction.Tx) error
 	// ByBlockID returns QC that certifies block referred by blockID.
 	// * storage.ErrNotFound if no QC for blockID doesn't exist.
