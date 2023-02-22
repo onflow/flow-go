f9e994eb999d7f46a507f9f9a7537330254f4f58
Khalil Claybon <khalil.claybon@dapperlabs.com>
2023-02-22 11:15:44 -0500

    Update manager.go

--- network/p2p/unicast/manager.go
+++ network/p2p/unicast/manager.go
@@ -14,6 +14,7 @@ import (
 	"github.com/libp2p/go-libp2p/core/protocol"
 	"github.com/libp2p/go-libp2p/p2p/net/swarm"
 	"github.com/multiformats/go-multiaddr"
+
 	"github.com/onflow/flow-go/model/flow"
 	"github.com/onflow/flow-go/network/p2p"
 	"github.com/onflow/flow-go/network/p2p/unicast/protocols"
