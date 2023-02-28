88c164fbd73aa36b5b7ded7b74a7f6fae197721a
Jordan Schalm <jordan@dapperlabs.com>
2023-02-28 11:21:42 -0500

    extend test cases for beacon key store

--- consensus/hotstuff/signature/randombeacon_signer_store_test.go
+++ consensus/hotstuff/signature/randombeacon_signer_store_test.go
@@ -13,6 +13,7 @@ import (
 	"github.com/onflow/flow-go/crypto"
 	"github.com/onflow/flow-go/module"
 	mockmodule "github.com/onflow/flow-go/module/mock"
+	"github.com/onflow/flow-go/storage"
 	mockstorage "github.com/onflow/flow-go/storage/mock"
 	"github.com/onflow/flow-go/utils/unittest"
 )
@@ -37,6 +38,7 @@ func (suite *BeaconKeyStore) SetupTest() {
 	suite.store = NewEpochAwareRandomBeaconKeyStore(suite.epochLookup, suite.beaconKeys)
 }
 
+// TestHappyPath tests the happy path, where the epoch is known and there is a safe beacon key available.
 func (suite *BeaconKeyStore) TestHappyPath() {
 	view := rand.Uint64()
 	epoch := rand.Uint64()
@@ -49,25 +51,31 @@ func (suite *BeaconKeyStore) TestHappyPath() {
 	assert.Equal(suite.T(), expectedKey, key)
 }
 
-func (suite *BeaconKeyStore) Test_EpochLookup_UnknownEpochError() {
+// Test_EpochLookup_UnknownEpochError tests that when EpochLookup returns
+// model.ErrViewForUnknownEpoch it is propagated to the caller of ByView.
+func (suite *BeaconKeyStore) Test_EpochLookup_ViewForUnknownEpoch() {
 	view := rand.Uint64()
-	suite.epochLookup.On("EpochForViewWithFallback", view).Return(0, model.ErrViewForUnknownEpoch)
+	suite.epochLookup.On("EpochForViewWithFallback", view).Return(uint64(0), model.ErrViewForUnknownEpoch)
 
 	key, err := suite.store.ByView(view)
 	require.ErrorIs(suite.T(), err, model.ErrViewForUnknownEpoch)
 	assert.Nil(suite.T(), key)
 }
 
+// Test_EpochLookup_UnexpectedError tests that an exception from EpochLookup is
+// propagated to the caller of ByView.
 func (suite *BeaconKeyStore) Test_EpochLookup_UnexpectedError() {
 	view := rand.Uint64()
 	exception := errors.New("unexpected error")
-	suite.epochLookup.On("EpochForViewWithFallback", view).Return(0, exception)
+	suite.epochLookup.On("EpochForViewWithFallback", view).Return(uint64(0), exception)
 
 	key, err := suite.store.ByView(view)
 	require.ErrorIs(suite.T(), err, exception)
 	assert.Nil(suite.T(), key)
 }
 
+// Test_BeaconKeys_Unsafe tests that if SafeBeaconKeys reports an unsafe key,
+// ByView returns that no beacon key is available.
 func (suite *BeaconKeyStore) Test_BeaconKeys_Unsafe() {
 	view := rand.Uint64()
 	epoch := rand.Uint64()
@@ -76,12 +84,56 @@ func (suite *BeaconKeyStore) Test_BeaconKeys_Unsafe() {
 
 	key, err := suite.store.ByView(view)
 	require.ErrorIs(suite.T(), err, module.ErrNoBeaconKeyForEpoch)
-	_ = key
+	assert.Nil(suite.T(), key)
 }
 
-// ErrVIewForUnknownEpoch
-// unexpected error
-//
-// key, nil
-// nil, unsafe, nil
-// nil, unsafe, ErrNotFound
+// Test_BeaconKeys_NotFound tests that if SafeBeaconKeys returns storage.ErrNotFound,
+// ByView returns that no beacon key is available.
+func (suite *BeaconKeyStore) Test_BeaconKeys_NotFound() {
+	view := rand.Uint64()
+	epoch := rand.Uint64()
+	suite.epochLookup.On("EpochForViewWithFallback", view).Return(epoch, nil)
+	suite.beaconKeys.On("RetrieveMyBeaconPrivateKey", epoch).Return(nil, false, storage.ErrNotFound)
+
+	key, err := suite.store.ByView(view)
+	require.ErrorIs(suite.T(), err, module.ErrNoBeaconKeyForEpoch)
+	assert.Nil(suite.T(), key)
+}
+
+// Test_BeaconKeys_NotFound tests that if SafeBeaconKeys returns storage.ErrNotFound,
+// ByView initially returns that no beacon key is available. But if SafeBeaconKeys
+// later returns a safe key, ByView should thereafter return the beacon key.
+// In other words, NotFound results should not be cached.
+func (suite *BeaconKeyStore) Test_BeaconKeys_NotFoundThenAvailable() {
+	view := rand.Uint64()
+	epoch := rand.Uint64()
+	suite.epochLookup.On("EpochForViewWithFallback", view).Return(epoch, nil)
+
+	var retKey crypto.PrivateKey
+	var retSafe bool
+	var retErr error
+	suite.beaconKeys.On("RetrieveMyBeaconPrivateKey", epoch).
+		Return(
+			func(_ uint64) crypto.PrivateKey { return retKey },
+			func(_ uint64) bool { return retSafe },
+			func(_ uint64) error { return retErr },
+		)
+
+	// 1 - return storage.ErrNotFound
+	retKey = nil
+	retSafe = false
+	retErr = storage.ErrNotFound
+
+	key, err := suite.store.ByView(view)
+	require.ErrorIs(suite.T(), err, module.ErrNoBeaconKeyForEpoch)
+	assert.Nil(suite.T(), key)
+
+	// 2 - return a safe beacon key
+	retKey = unittest.RandomBeaconPriv().PrivateKey
+	retSafe = true
+	retErr = nil
+
+	key, err = suite.store.ByView(view)
+	require.NoError(suite.T(), err)
+	assert.Equal(suite.T(), retKey, key)
+}
