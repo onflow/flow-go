bdd13e6ff5439be440d30fe8dc9b4dbca3fee011
Yahya Hassanzadeh <yhassanzadeh13@ku.edu.tr>
2023-03-06 10:59:10 -0800

    Update network/p2p/p2pbuilder/libp2pNodeBuilder.go
    Co-authored-by: Khalil Claybon <khalil.claybon@dapperlabs.com>

--- network/p2p/p2pbuilder/libp2pNodeBuilder.go
+++ network/p2p/p2pbuilder/libp2pNodeBuilder.go
@@ -49,7 +49,7 @@ const (
 	// DefaultPeerScoringEnabled is the default value for enabling peer scoring.
 	defaultPeerScoringEnabled = true // enable peer scoring by default on node builder
 
-	// DefaultMeshTracerLoggingInterval is the default interval at which the mesh tracer logs the mesh
+	// defaultMeshTracerLoggingInterval is the default interval at which the mesh tracer logs the mesh
 	// topology. This is used for debugging and forensics purposes.
 	// Note that we purposefully choose this logging interval high enough to avoid spamming the logs. Moreover, the
 	// mesh updates will be logged individually and separately. The logging interval is only used to log the mesh
