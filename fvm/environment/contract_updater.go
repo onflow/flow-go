d3f1654c3800b6c2d58995e2621c30188ac0d7fc
Janez Podhostnik <janez.podhostnik@gmail.com>
2023-03-07 20:40:16 +0100

    Remove Audit Vouchers

--- fvm/environment/contract_updater.go
+++ fvm/environment/contract_updater.go
@@ -164,8 +164,6 @@ type ContractUpdaterStubs interface {
 	RestrictedRemovalEnabled() bool
 
 	GetAuthorizedAccounts(path cadence.Path) []flow.Address
-
-	UseContractAuditVoucher(address flow.Address, code []byte) (bool, error)
 }
 
 type contractUpdaterStubsImpl struct {
@@ -262,18 +260,6 @@ func (impl *contractUpdaterStubsImpl) GetAuthorizedAccounts(
 		return defaultAccounts
 	}
 	return addresses
-}
-
-func (impl *contractUpdaterStubsImpl) UseContractAuditVoucher(
-	address flow.Address,
-	code []byte,
-) (
-	bool,
-	error,
-) {
-	return impl.systemContracts.UseContractAuditVoucher(
-		address,
-		string(code[:]))
 }
 
 type ContractUpdaterImpl struct {
@@ -398,8 +384,7 @@ func (updater *ContractUpdaterImpl) SetContract(
 	code []byte,
 	signingAccounts []flow.Address,
 ) error {
-	// Initial contract deployments must be authorized by signing accounts,
-	// or there must be an audit voucher available.
+	// Initial contract deployments must be authorized by signing accounts.
 	//
 	// Contract updates are always allowed.
 	exists, err := updater.accounts.ContractExists(name, address)
@@ -408,23 +393,13 @@ func (updater *ContractUpdaterImpl) SetContract(
 	}
 
 	if !exists && !updater.isAuthorizedForDeployment(signingAccounts) {
-		// check if there's an audit voucher for the contract
-		voucherAvailable, err := updater.UseContractAuditVoucher(address, code)
-		if err != nil {
-			errInner := errors.NewOperationAuthorizationErrorf(
+		return fmt.Errorf(
+			"deploying contract failed: %w",
+			errors.NewOperationAuthorizationErrorf(
 				"SetContract",
-				"failed to check audit vouchers",
-			)
-			return fmt.Errorf("setting contract failed: %w - %s", errInner, err)
-		}
-		if !voucherAvailable {
-			return fmt.Errorf(
-				"deploying contract failed: %w",
-				errors.NewOperationAuthorizationErrorf(
-					"SetContract",
-					"deploying contracts requires authorization from specific "+
-						"accounts"))
-		}
+				"deploying contracts requires authorization from specific "+
+					"accounts"))
+
 	}
 
 	contractUpdateKey := ContractUpdateKey{
