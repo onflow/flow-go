6cc1ed7380e639221b0d2189da4ce463f28c001b
Yahya Hassanzadeh <yhassanzadeh13@ku.edu.tr>
2023-03-09 13:31:16 -0800

    updates mocks

--- network/p2p/mock/disallow_list_notification_distributor.go
+++ network/p2p/mock/disallow_list_notification_distributor.go
@@ -4,6 +4,8 @@ package mockp2p
 
 import (
 	flow "github.com/onflow/flow-go/model/flow"
+	irrecoverable "github.com/onflow/flow-go/module/irrecoverable"
+
 	mock "github.com/stretchr/testify/mock"
 
 	p2p "github.com/onflow/flow-go/network/p2p"
@@ -31,6 +33,43 @@ func (_m *DisallowListNotificationDistributor) DistributeBlockListNotification(l
 	}
 
 	return r0
+}
+
+// Done provides a mock function with given fields:
+func (_m *DisallowListNotificationDistributor) Done() <-chan struct{} {
+	ret := _m.Called()
+
+	var r0 <-chan struct{}
+	if rf, ok := ret.Get(0).(func() <-chan struct{}); ok {
+		r0 = rf()
+	} else {
+		if ret.Get(0) != nil {
+			r0 = ret.Get(0).(<-chan struct{})
+		}
+	}
+
+	return r0
+}
+
+// Ready provides a mock function with given fields:
+func (_m *DisallowListNotificationDistributor) Ready() <-chan struct{} {
+	ret := _m.Called()
+
+	var r0 <-chan struct{}
+	if rf, ok := ret.Get(0).(func() <-chan struct{}); ok {
+		r0 = rf()
+	} else {
+		if ret.Get(0) != nil {
+			r0 = ret.Get(0).(<-chan struct{})
+		}
+	}
+
+	return r0
+}
+
+// Start provides a mock function with given fields: _a0
+func (_m *DisallowListNotificationDistributor) Start(_a0 irrecoverable.SignalerContext) {
+	_m.Called(_a0)
 }
 
 type mockConstructorTestingTNewDisallowListNotificationDistributor interface {
