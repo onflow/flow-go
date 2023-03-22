a463a1593921ad8d26077f6b4f94f46991d6069a
Yurii Oleksyshyn <yuraolex@gmail.com>
2023-03-22 12:29:47 +0200

    Added an interface for FollowerCore

---
+++ engine/common/follower.go
@@ -0,0 +1,19 @@
+package common
+
+import "github.com/onflow/flow-go/model/flow"
+
+// FollowerCore interface defines the methods that a consensus follower must implement in order to synchronize
+// with the Flow network.
+// FollowerCore processes incoming continuous ranges of blocks by executing consensus follower logic which consists of
+// validation and extending protocol state by applying state changes contained in block's payload.
+// Processing valid ranges of blocks results in extending protocol state and subsequent finalization of pending blocks.
+type FollowerCore interface {
+	// OnBlockRange is called when a batch of blocks is received from the network.
+	// The originID parameter identifies the node that sent the batch of blocks.
+	// The connectedRange parameter contains the blocks, they must form a sequence of connected blocks.
+	// No errors are expected during normal operations.
+	OnBlockRange(originID flow.Identifier, connectedRange []*flow.Block) error
+	// OnFinalizedBlock is called when a new block is finalized by Hotstuff.
+	// FollowerCore updates can update its local state using this information.
+	OnFinalizedBlock(finalized *flow.Header)
+}
