70ce275a5bdbc334f21b4702c1b07f168f06de39
Yurii Oleksyshyn <yuraolex@gmail.com>
2023-03-22 20:03:14 +0200

    Cleanup of follower core.

--- engine/common/follower/core.go
+++ engine/common/follower/core.go
@@ -15,13 +15,10 @@ import (
 	"github.com/onflow/flow-go/module/component"
 	"github.com/onflow/flow-go/module/irrecoverable"
 	"github.com/onflow/flow-go/module/metrics"
-	"github.com/onflow/flow-go/module/trace"
 	"github.com/onflow/flow-go/state"
 	"github.com/onflow/flow-go/state/protocol"
 	"github.com/onflow/flow-go/storage"
-	"github.com/onflow/flow-go/utils/logging"
 	"github.com/rs/zerolog"
-	"github.com/rs/zerolog/log"
 )
 
 type ComplianceOption func(*Core)
@@ -59,9 +56,7 @@ var _ common.FollowerCore = (*Core)(nil)
 func NewCore(log zerolog.Logger,
 	mempoolMetrics module.MempoolMetrics,
 	cleaner storage.Cleaner,
-	payloads storage.Payloads,
 	state protocol.FollowerState,
-	pending module.PendingBlockBuffer,
 	follower module.HotStuffFollower,
 	validator hotstuff.Validator,
 	sync module.BlockRequester,
@@ -95,137 +90,29 @@ func NewCore(log zerolog.Logger,
 	return c
 }
 
-// OnBlockProposal handles incoming block proposals.
-// No errors are expected during normal operations.
-//func (c *Core) OnBlockProposal(originID flow.Identifier, batch []*messages.BlockProposal) error {
-//	block := proposal.Block.ToInternal()
-//	header := block.Header
-//	blockID := header.ID()
-//
-//	span, ctx := c.tracer.StartBlockSpan(context.Background(), blockID, trace.FollowerOnBlockProposal)
-//	defer span.End()
-//
-//	log := c.log.With().
-//		Hex("origin_id", originID[:]).
-//		Str("chain_id", header.ChainID.String()).
-//		Uint64("block_height", header.Height).
-//		Uint64("block_view", header.View).
-//		Hex("block_id", blockID[:]).
-//		Hex("parent_id", header.ParentID[:]).
-//		Hex("payload_hash", header.PayloadHash[:]).
-//		Time("timestamp", header.Timestamp).
-//		Hex("proposer", header.ProposerID[:]).
-//		Logger()
-//
-//	log.Info().Msg("block proposal received")
-//
-//	// first, we reject all blocks that we don't need to process:
-//	// 1) blocks already in the cache; they will already be processed later
-//	// 2) blocks already on disk; they were processed and await finalization
-//	// 3) blocks at a height below finalized height; they can not be finalized
-//
-//	// ignore proposals that are already cached
-//	_, cached := c.pendingCache.ByID(blockID)
-//	if cached {
-//		log.Debug().Msg("skipping already cached proposal")
-//		return nil
-//	}
-//
-//	// ignore proposals that were already processed
-//	_, err := c.headers.ByBlockID(blockID)
-//	if err == nil {
-//		log.Debug().Msg("skipping already processed proposal")
-//		return nil
-//	}
-//	if !errors.Is(err, storage.ErrNotFound) {
-//		return fmt.Errorf("could not check proposal: %w", err)
-//	}
-//
-//	// ignore proposals which are too far ahead of our local finalized state
-//	// instead, rely on sync engine to catch up finalization more effectively, and avoid
-//	// large subtree of blocks to be cached.
-//	final, err := c.state.Final().Head()
-//	if err != nil {
-//		return fmt.Errorf("could not get latest finalized header: %w", err)
-//	}
-//	if header.Height > final.Height && header.Height-final.Height > c.config.SkipNewProposalsThreshold {
-//		log.Debug().
-//			Uint64("final_height", final.Height).
-//			Msg("dropping block too far ahead of locally finalized height")
-//		return nil
-//	}
-//	if header.Height <= final.Height {
-//		log.Debug().
-//			Uint64("final_height", final.Height).
-//			Msg("dropping block below finalized threshold")
-//		return nil
-//	}
-//
-//	// there are two possibilities if the proposal is neither already pendingCache
-//	// processing in the cache, nor has already been processed:
-//	// 1) the proposal is unverifiable because parent or ancestor is unknown
-//	// => we cache the proposal and request the missing link
-//	// 2) the proposal is connected to finalized state through an unbroken chain
-//	// => we verify the proposal and forward it to hotstuff if valid
-//
-//	// if the parent is a pendingCache block (disconnected from the incorporated state), we cache this block as well.
-//	// we don't have to request its parent block or its ancestor again, because as a
-//	// pendingCache block, its parent block must have been requested.
-//	// if there was problem requesting its parent or ancestors, the sync engine's forward
-//	// syncing with range requests for finalized blocks will request for the blocks.
-//	_, found := c.pendingCache.ByID(header.ParentID)
-//	if found {
-//
-//		// add the block to the cache
-//		_ = c.pendingCache.Add(originID, block)
-//		c.mempoolMetrics.MempoolEntries(metrics.ResourceClusterProposal, c.pendingCache.Size())
-//
-//		return nil
-//	}
-//
-//	// if the proposal is connected to a block that is neither in the cache, nor
-//	// in persistent storage, its direct parent is missing; cache the proposal
-//	// and request the parent
-//	_, err = c.headers.ByBlockID(header.ParentID)
-//	if errors.Is(err, storage.ErrNotFound) {
-//
-//		_ = c.pendingCache.Add(originID, block)
-//
-//		log.Debug().Msg("requesting missing parent for proposal")
-//
-//		c.sync.RequestBlock(header.ParentID, header.Height-1)
-//
-//		return nil
-//	}
-//	if err != nil {
-//		return fmt.Errorf("could not check parent: %w", err)
-//	}
-//
-//	// at this point, we should be able to connect the proposal to the finalized
-//	// state and should process it to see whether to forward to hotstuff or not
-//	err = c.processBlockAndDescendants(ctx, block)
-//	if err != nil {
-//		return fmt.Errorf("could not process block proposal (id=%x, height=%d, view=%d): %w", blockID, header.Height, header.View, err)
-//	}
-//
-//	// most of the heavy database checks are done at this point, so this is a
-//	// good moment to potentially kick-off a garbage collection of the DB
-//	// NOTE: this is only effectively run every 1000th calls, which corresponds
-//	// to every 1000th successfully processed block
-//	c.cleaner.RunGC()
-//
-//	return nil
-//}
-
 func (c *Core) OnBlockRange(originID flow.Identifier, batch []*flow.Block) error {
 	if len(batch) < 1 {
 		return nil
 	}
 
-	lastBlock := batch[len(batch)-1]
-	hotstuffProposal := model.ProposalFromFlow(lastBlock.Header)
+	firstBlock := batch[0].Header
+	lastBlock := batch[len(batch)-1].Header
+	hotstuffProposal := model.ProposalFromFlow(lastBlock)
+	log := c.log.With().
+		Hex("origin_id", originID[:]).
+		Str("chain_id", lastBlock.ChainID.String()).
+		Uint64("first_block_height", firstBlock.Height).
+		Uint64("first_block_view", firstBlock.View).
+		Uint64("last_block_height", lastBlock.Height).
+		Uint64("last_block_view", lastBlock.View).
+		Hex("last_block_id", hotstuffProposal.Block.BlockID[:]).
+		Int("range_length", len(batch)).
+		Logger()
+
+	log.Info().Msg("processing block range")
 
 	if c.pendingCache.Peek(hotstuffProposal.Block.BlockID) == nil {
+		log.Debug().Msg("block not found in cache, performing validation")
 		// if last block is in cache it means that we can skip validation since it was already validated
 		// otherwise we must validate it to proof validity of blocks range.
 		err := c.validator.ValidateProposal(hotstuffProposal)
@@ -258,7 +145,14 @@ func (c *Core) OnBlockRange(originID flow.Identifier, batch []*flow.Block) error
 		return fmt.Errorf("could not add a range of pending blocks: %w", err)
 	}
 
-	c.certifiedBlocksChan <- rangeToCertifiedBlocks(certifiedBatch, certifyingQC)
+	log.Debug().Msgf("processing range resulted in %d certified blocks", len(certifiedBatch))
+
+	// in-case we have already stopped our worker we use a select statement to avoid
+	// blocking since there is no active consumer for this channel
+	select {
+	case c.certifiedBlocksChan <- rangeToCertifiedBlocks(certifiedBatch, certifyingQC):
+	case <-c.ComponentManager.ShutdownSignal():
+	}
 	return nil
 }
 
@@ -286,87 +180,18 @@ func (c *Core) processCoreSeqEvents(ctx irrecoverable.SignalerContext, ready com
 	}
 }
 
-// processBlockAndDescendants processes `proposal` and its pendingCache descendants recursively.
-// The function assumes that `proposal` is connected to the finalized state. By induction,
-// any children are therefore also connected to the finalized state and can be processed as well.
-// No errors are expected during normal operations.
-func (c *Core) processBlockAndDescendants(ctx context.Context, proposal *flow.Block) error {
-	header := proposal.Header
-	span, ctx := c.tracer.StartSpanFromContext(ctx, trace.FollowerProcessBlockProposal)
-	defer span.End()
-
-	log := c.log.With().
-		Str("chain_id", header.ChainID.String()).
-		Uint64("block_height", header.Height).
-		Uint64("block_view", header.View).
-		Hex("block_id", logging.Entity(header)).
-		Hex("parent_id", header.ParentID[:]).
-		Hex("payload_hash", header.PayloadHash[:]).
-		Time("timestamp", header.Timestamp).
-		Hex("proposer", header.ProposerID[:]).
-		Logger()
-
-	log.Info().Msg("processing block proposal")
-
-	hotstuffProposal := model.ProposalFromFlow(header)
-	err := c.validator.ValidateProposal(hotstuffProposal)
-	if err != nil {
-		if model.IsInvalidBlockError(err) {
-			// TODO potential slashing
-			log.Err(err).Msgf("received invalid block proposal (potential slashing evidence)")
-			return nil
-		}
-		if errors.Is(err, model.ErrViewForUnknownEpoch) {
-			// We have received a proposal, but we don't know the epoch its view is within.
-			// We know:
-			//  - the parent of this block is valid and inserted (ie. we knew the epoch for it)
-			//  - if we then see this for the child, one of two things must have happened:
-			//    1. the proposer malicious created the block for a view very far in the future (it's invalid)
-			//      -> in this case we can disregard the block
-			//    2. no blocks have been finalized the epoch commitment deadline, and the epoch end
-			//       (breaking a critical assumption - see EpochCommitSafetyThreshold in protocol.Params for details)
-			//      -> in this case, the network has encountered a critical failure
-			//  - we assume in general that Case 2 will not happen, therefore we can discard this proposal
-			log.Err(err).Msg("unable to validate proposal with view from unknown epoch")
-			return nil
-		}
-		return fmt.Errorf("unexpected error validating proposal: %w", err)
-	}
-
-	// check whether the block is a valid extension of the chain.
-	// The follower engine only checks the block's header. The more expensive payload validation
-	// is only done by the consensus committee. For safety, we require that a QC for the extending
-	// block is provided while inserting the block. This ensures that all stored blocks are fully validated
-	// by the consensus committee before being stored here.
-	err = c.state.ExtendCertified(ctx, proposal, nil)
-	if err != nil {
-		// block is outdated by the time we started processing it
-		// => some other node generating the proposal is probably behind is catching up.
-		if state.IsOutdatedExtensionError(err) {
-			log.Info().Err(err).Msg("dropped processing of abandoned fork; this might be an indicator that some consensus node is behind")
-			return nil
-		}
-		// the block is invalid; log as error as we desire honest participation
-		// ToDo: potential slashing
-		if state.IsInvalidExtensionError(err) {
-			log.Warn().
-				Err(err).
-				Msg("received invalid block from other node (potential slashing evidence?)")
-			return nil
-		}
-
-		return fmt.Errorf("could not extend protocol state: %w", err)
-	}
-
-	return nil
-}
-
 // OnFinalizedBlock updates local state of pendingCache tree using received finalized block.
 // Is NOT concurrency safe, has to be used by the same goroutine as processCertifiedBlocks.
 // OnFinalizedBlock and processCertifiedBlocks MUST be sequentially ordered.
 func (c *Core) OnFinalizedBlock(final *flow.Header) {
 	c.pendingCache.PruneUpToView(final.View)
-	c.finalizedBlocksChan <- final
+
+	// in-case we have already stopped our worker we use a select statement to avoid
+	// blocking since there is no active consumer for this channel
+	select {
+	case c.finalizedBlocksChan <- final:
+	case <-c.ComponentManager.ShutdownSignal():
+	}
 }
 
 // processCertifiedBlocks processes batch of certified blocks by applying them to tree of certified blocks.
