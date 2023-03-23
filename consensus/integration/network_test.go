31efb3950fd4ec26ee18d1e0c54af55aa95a44dd
Yahya Hassanzadeh <yhassanzadeh13@ku.edu.tr>
2023-03-23 15:37:51 -0700

    implements misbehavior reporter for tests

--- consensus/integration/network_test.go
+++ consensus/integration/network_test.go
@@ -172,6 +172,13 @@ type Conduit struct {
 	queue   chan message
 }
 
+// ReportMisbehavior reports the misbehavior of a node on sending a message to the current node that appears valid
+// based on the networking layer but is considered invalid by the current node based on the Flow protocol.
+// This method is a no-op in the test helper implementation.
+func (c *Conduit) ReportMisbehavior(_ *network.MisbehaviorReport) {
+	// no-op
+}
+
 var _ network.Conduit = (*Conduit)(nil)
 
 func (c *Conduit) Submit(event interface{}, targetIDs ...flow.Identifier) error {
