8f7b29b0b8ef694fee0625da80dbf1067bd87d06
Yahya Hassanzadeh <yhassanzadeh13@ku.edu.tr>
2023-03-24 16:46:07 -0700

    abstracts misbehavior report

--- network/alsp.go
+++ network/alsp.go
@@ -5,6 +5,15 @@ import (
 	"github.com/onflow/flow-go/network/channels"
 )
 
+// Misbehavior is a type of misbehavior that can be reported by a node.
+// The misbehavior is used to penalize the misbehaving node at the protocol level concerning the messages that the current
+// node has received from the misbehaving node.
+type Misbehavior string
+
+func (m Misbehavior) String() string {
+	return string(m)
+}
+
 // MisbehaviorReporter is an interface that is used to report misbehavior of a node.
 // The misbehavior is reported to the networking layer to penalize the misbehaving node.
 type MisbehaviorReporter interface {
@@ -18,6 +27,12 @@ type MisbehaviorReporter interface {
 type MisbehaviorReport interface {
 	// OriginId returns the ID of the misbehaving node.
 	OriginId() flow.Identifier
+
+	// Reason returns the reason of the misbehavior.
+	Reason() Misbehavior
+
+	// Penalty returns the penalty value of the misbehavior.
+	Penalty() int
 }
 
 type MisbehaviorReportManager interface {
@@ -25,5 +40,5 @@ type MisbehaviorReportManager interface {
 	// The implementation of this function should penalize the misbehaving node and report the node to be
 	// disallow-listed if the overall penalty of the misbehaving node drops below the disallow-listing threshold.
 	// The implementation of this function should be thread-safe and non-blocking.
-	HandleReportedMisbehavior(channels.Channel, *MisbehaviorReport)
+	HandleReportedMisbehavior(channels.Channel, MisbehaviorReport)
 }
