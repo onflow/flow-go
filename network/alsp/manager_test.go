aefb271ff39efd70292a269d8e260ce5e5cd4ba2
Yahya Hassanzadeh <yhassanzadeh13@ku.edu.tr>
2023-03-24 17:38:49 -0700

    adds empty test for manager

--- network/alsp/manager_test.go
+++ network/alsp/manager_test.go
@@ -1,1 +1,17 @@
-package alsp
+package alsp_test
+
+import (
+	"testing"
+)
+
+func TestHandleReportedMisbehavior(t *testing.T) {
+	//
+	//ids, nodes, mws, _, _ := testutils.GenerateIDsAndMiddlewares(
+	//	t,
+	//	1,
+	//	unittest.Logger(),
+	//	unittest.NetworkCodec(),
+	//	unittest.NetworkSlashingViolationsConsumer(unittest.Logger(), metrics.NewNoopCollector()))
+	//sms := testutils.GenerateSubscriptionManagers(t, mws)
+	//networks := testutils.GenerateNetworks(t, unittest.Logger(), ids, mws, sms)
+}
