36857158bbb827296a73caa819218a9b5de1c1a4
Yahya Hassanzadeh <yhassanzadeh13@ku.edu.tr>
2023-03-24 17:42:41 -0700

    lint fix

--- network/alsp/params.go
+++ network/alsp/params.go
@@ -1,7 +1,5 @@
 package alsp
 
-import "time"
-
 // To give a summary with the default value:
 //  1. The penalty of each misbehavior is 0.01 * misbehaviorDisallowListingThreshold = -86.4
 //  2. The penalty of each misbehavior is decayed by a decay value at each decay interval. The default decay value is 1000.
@@ -29,24 +27,4 @@ const (
 	// amplifies the penalty by 10, the number of misbehavior/sec that will result in disallow-listing the node will be
 	// 10 times less than the default penalty value and the node will be disallow-listed after 10 times more misbehavior/sec.
 	defaultPenaltyValue = 0.01 * misbehaviorDisallowListingThreshold
-
-	// misbehaviorDecayHeartbeatInterval is the interval at which the penalty of the misbehaving nodes is decayed.
-	// At each interval, the penalty of the misbehaving nodes is decayed by the deducting the defaultDecayValue from the penalty.
-	misbehaviorDecayHeartBeatInterval = 1 * time.Second
-
-	// defaultDecayValue is the default value that is deducted from the penalty of the misbehaving nodes at each decay interval.
-	// This is called the default decay value because the decay value will be changed per node each time the node goes
-	// below the disallow-listing threshold. Hence, the nodes that are misbehaving more than the others will be decayed
-	// slower than the others.
-	defaultDecayValue = 1000
-
-	// decayValueSpeedPenalty is the penalty for the decay speed. The decay speed is multiplied by this value each time
-	// the node goes below the disallow-listing threshold.
-	decayValueSpeedPenalty = 0.1
-
-	// minimumDecayValue is the minimum decay value that is used to decay the penalty of the misbehaving nodes.
-	// The decay value is multiplied by the decay value speed penalty each time the node goes below the disallow-listing
-	// threshold. The minimum decay value is used to prevent the decay value from becoming too small. If the decay value
-	// becomes too small, the penalty of the misbehaving forever. Hence, the decay value is capped at this value.
-	minimumDecayValue = 1
 )
