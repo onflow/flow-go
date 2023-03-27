b5d60b1125191dcd0f97327114e9b6b78262665e
Yahya Hassanzadeh <yhassanzadeh13@ku.edu.tr>
2023-03-27 13:33:11 -0700

    adds manager test

--- network/alsp/manager_test.go
+++ network/alsp/manager_test.go
@@ -1,17 +1,70 @@
 package alsp_test
 
 import (
+	"context"
+	"sync"
 	"testing"
+	"time"
+
+	"github.com/stretchr/testify/mock"
+	"github.com/stretchr/testify/require"
+
+	"github.com/onflow/flow-go/module/irrecoverable"
+	"github.com/onflow/flow-go/module/metrics"
+	"github.com/onflow/flow-go/network"
+	"github.com/onflow/flow-go/network/channels"
+	"github.com/onflow/flow-go/network/internal/testutils"
+	"github.com/onflow/flow-go/network/mocknetwork"
+	"github.com/onflow/flow-go/network/p2p"
+	"github.com/onflow/flow-go/network/p2p/conduit"
+	"github.com/onflow/flow-go/utils/unittest"
 )
 
 func TestHandleReportedMisbehavior(t *testing.T) {
-	//
-	//ids, nodes, mws, _, _ := testutils.GenerateIDsAndMiddlewares(
-	//	t,
-	//	1,
-	//	unittest.Logger(),
-	//	unittest.NetworkCodec(),
-	//	unittest.NetworkSlashingViolationsConsumer(unittest.Logger(), metrics.NewNoopCollector()))
-	//sms := testutils.GenerateSubscriptionManagers(t, mws)
-	//networks := testutils.GenerateNetworks(t, unittest.Logger(), ids, mws, sms)
+	misbehaviorReportManger := mocknetwork.NewMisbehaviorReportManager(t)
+	conduitFactory := conduit.NewDefaultConduitFactory(unittest.Logger(), conduit.WithMisbehaviorManager(misbehaviorReportManger))
+
+	ids, nodes, mws, _, _ := testutils.GenerateIDsAndMiddlewares(
+		t,
+		1,
+		unittest.Logger(),
+		unittest.NetworkCodec(),
+		unittest.NetworkSlashingViolationsConsumer(unittest.Logger(), metrics.NewNoopCollector()))
+	sms := testutils.GenerateSubscriptionManagers(t, mws)
+	networks := testutils.GenerateNetworks(
+		t,
+		unittest.Logger(),
+		ids,
+		mws,
+		sms,
+		p2p.WithConduitFactory(conduitFactory))
+
+	ctx, cancel := context.WithCancel(context.Background())
+
+	signalerCtx := irrecoverable.NewMockSignalerContext(t, ctx)
+	testutils.StartNodesAndNetworks(signalerCtx, t, nodes, networks, 100*time.Millisecond)
+	defer testutils.StopComponents[p2p.LibP2PNode](t, nodes, 100*time.Millisecond)
+	defer cancel()
+
+	e := mocknetwork.NewEngine(t)
+	con, err := networks[0].Register(channels.TestNetworkChannel, e)
+	require.NoError(t, err)
+
+	reports := testutils.MisbehaviorReportsFixture(t, 10)
+	allReportsManaged := sync.WaitGroup{}
+	allReportsManaged.Add(len(reports))
+	var seenReports []network.MisbehaviorReport
+	misbehaviorReportManger.On("HandleReportedMisbehavior", channels.TestNetworkChannel, mock.Anything).Run(func(args mock.Arguments) {
+		report := args.Get(1).(network.MisbehaviorReport)
+		require.Contains(t, reports, report)                                         // ensures that the report is one of the reports we expect.
+		require.NotContainsf(t, seenReports, report, "duplicate report: %v", report) // ensures that we have not seen this report before.
+		seenReports = append(seenReports, report)                                    // adds the report to the list of seen reports.
+		allReportsManaged.Done()
+	}).Return(nil)
+
+	for _, report := range reports {
+		con.ReportMisbehavior(report) // reports the misbehavior
+	}
+
+	unittest.RequireReturnsBefore(t, allReportsManaged.Wait, 100*time.Millisecond, "did not receive all reports")
 }
