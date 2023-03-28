5d94dd9596eae83a1790a123ee0c709b808d7be6
Yurii Oleksyshyn <yuraolex@gmail.com>
2023-03-28 10:29:52 +0300

    Apply suggestions from code review
    Co-authored-by: Alexander Hentschel <alex.hentschel@axiomzen.co>

--- engine/common/follower/core.go
+++ engine/common/follower/core.go
@@ -116,12 +116,14 @@ func NewCore(log zerolog.Logger,
 	return c, nil
 }
 
-// OnBlockRange performs processing batches of connected blocks. Input batch has to be sequentially ordered forming a chain.
+// OnBlockRange processes a batches of connected blocks. The input batch has to be sequentially ordered forming a chain.
 // Submitting batch with invalid order results in error, such batch will be discarded and exception will be returned.
-// Effectively this function validates incoming batch, adds it to cache of pending blocks and possibly schedules blocks for further
+// Effectively, this function validates incoming batch, adds it to cache of pending blocks and possibly schedules blocks for further
 // processing if they were certified.
-// No errors expected during normal operations.
 // This function is safe to use in concurrent environment.
+// Caution: this function might block if internally too many certified blocks are queued in the channel `certifiedBlocksChan`. 
+// Expected errors during normal operations:
+//   - ErrDisconnectedBatch
 func (c *Core) OnBlockRange(originID flow.Identifier, batch []*flow.Block) error {
 	if len(batch) < 1 {
 		return nil
@@ -145,8 +147,16 @@ func (c *Core) OnBlockRange(originID flow.Identifier, batch []*flow.Block) error
 
 	if c.pendingCache.Peek(hotstuffProposal.Block.BlockID) == nil {
 		log.Debug().Msg("block not found in cache, performing validation")
-		// if last block is in cache it means that we can skip validation since it was already validated
-		// otherwise we must validate it to proof validity of blocks range.
+		// Caution: we are _not_ verifying the proposal's full validity here. Instead, we need to check 
+		// the following two critical properties: 
+		// 1. The block has been signed by the legitimate primary for the view. This is important in case 
+		//    there are multiple blocks for the view. We need to differentiate the following byzantine cases: 
+		//     (i) Some other consensus node that is _not_ primary is trying to publish a block.
+		//         This would result in the validation below failing with and `InvalidBlockError`.
+		//    (ii) The legitimate primary for the view is equivocating. In this case, the validity check
+		//         below would pass. Though, the `PendingTree` would eventually notice this, when we connect 
+		//         the equivocating blocks to the latest finalized block.  
+		// 2. The QC within the block is valid. A valid QC proves validity of all ancestors. 
 		err := c.validator.ValidateProposal(hotstuffProposal)
 		if err != nil {
 			if model.IsInvalidBlockError(err) {
@@ -156,15 +166,19 @@ func (c *Core) OnBlockRange(originID flow.Identifier, batch []*flow.Block) error
 			}
 			if errors.Is(err, model.ErrViewForUnknownEpoch) {
 				// We have received a proposal, but we don't know the epoch its view is within.
-				// We know:
-				//  - the parent of this block is valid and inserted (ie. we knew the epoch for it)
-				//  - if we then see this for the child, one of two things must have happened:
-				//    1. the proposer malicious created the block for a view very far in the future (it's invalid)
-				//      -> in this case we can disregard the block
-				//    2. no blocks have been finalized the epoch commitment deadline, and the epoch end
-				//       (breaking a critical assumption - see EpochCommitSafetyThreshold in protocol.Params for details)
-				//      -> in this case, the network has encountered a critical failure
-				//  - we assume in general that Case 2 will not happen, therefore we can discard this proposal
+				// Conceptually, there are three scenarios that could lead to this edge-case:
+				//  1. the proposer maliciously created the block for a view very far in the future (it's invalid)
+				//     -> in this case we can disregard the block
+				//  2. This node is very far behind and hasn't processed enough blocks to observe the EpochCommit
+				//     service event.
+				//     -> in this case we can disregard the block
+				//     Note: we could eliminate this edge case by dropping future blocks, iff their _view_
+				//           is strictly larger than `V + EpochCommitSafetyThreshold`, where `V` denotes
+				//           the latest finalized block known to this node.
+				//  3. No blocks have been finalized for the last `EpochCommitSafetyThreshold` views. This breaks
+				//     a critical liveness assumption - see EpochCommitSafetyThreshold in protocol.Params for details.
+				//     -> In this case, it is ok for the protocol to halt. Consequently, we can just disregard
+				//        the block, which will probably lead to this node eventually halting.
 				log.Err(err).Msg("unable to validate proposal with view from unknown epoch")
 				return nil
 			}
@@ -176,14 +190,13 @@ func (c *Core) OnBlockRange(originID flow.Identifier, batch []*flow.Block) error
 	if err != nil {
 		return fmt.Errorf("could not add a range of pending blocks: %w", err)
 	}
-
-	log.Debug().Msgf("processing range resulted in %d certified blocks", len(certifiedBatch))
+	log.Debug().Msgf("caching block range resulted in %d certified blocks (possibly including additional cached blocks)", len(certifiedBatch))
 
 	if len(certifiedBatch) < 1 {
 		return nil
 	}
 
-	// in-case we have already stopped our worker we use a select statement to avoid
+	// in case we have already stopped our worker, we use a select statement to avoid
 	// blocking since there is no active consumer for this channel
 	select {
 	case c.certifiedBlocksChan <- rangeToCertifiedBlocks(certifiedBatch, certifyingQC):
@@ -194,6 +207,7 @@ func (c *Core) OnBlockRange(originID flow.Identifier, batch []*flow.Block) error
 
 // processCoreSeqEvents processes events that need to be dispatched on dedicated core's goroutine.
 // Here we process events that need to be sequentially ordered(processing certified blocks and new finalized blocks).
+// Is NOT concurrency safe, has to be used by internal goroutine.
 func (c *Core) processCoreSeqEvents(ctx irrecoverable.SignalerContext, ready component.ReadyFunc) {
 	ready()
 
@@ -219,6 +233,8 @@ func (c *Core) processCoreSeqEvents(ctx irrecoverable.SignalerContext, ready com
 // OnFinalizedBlock updates local state of pendingCache tree using received finalized block and queues finalized block
 // to be processed by internal goroutine.
 // This function is safe to use in concurrent environment.
+// CAUTION: this function blocks and is therefore not compliant with the `FinalizationConsumer.OnFinalizedBlock` 
+// interface. This function should only be executed within the a worker routine.  
 func (c *Core) OnFinalizedBlock(final *flow.Header) {
 	c.pendingCache.PruneUpToView(final.View)
 
@@ -300,10 +316,10 @@ func (c *Core) processFinalizedBlock(ctx context.Context, finalized *flow.Header
 // Pure function.
 func rangeToCertifiedBlocks(certifiedRange []*flow.Block, certifyingQC *flow.QuorumCertificate) CertifiedBlocks {
 	certifiedBlocks := make(CertifiedBlocks, 0, len(certifiedRange))
-	for i := 0; i < len(certifiedRange); i++ {
-		block := certifiedRange[i]
+	lastIndex := len(certifiedRange) - 1
+	for i, block := range certifiedRange {
 		var qc *flow.QuorumCertificate
-		if i < len(certifiedRange)-1 {
+		if i < lastIndex {
 			qc = certifiedRange[i+1].Header.QuorumCertificate()
 		} else {
 			qc = certifyingQC
