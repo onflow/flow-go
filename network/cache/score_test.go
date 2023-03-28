85d3fc272cf6eeecadfeac7e9f28aaabcacfe072
Yahya Hassanzadeh <yhassanzadeh13@ku.edu.tr>
2023-03-28 14:17:33 -0700

    adds score cache test

--- network/cache/score_test.go
+++ network/cache/score_test.go
@@ -14,6 +14,40 @@ import (
 	"github.com/onflow/flow-go/utils/unittest"
 )
 
+// TestAppScoreCache_Update tests the Update method of the AppScoreCache. It tests if the cache
+// can add a new entry, update an existing entry, and fail to add a new entry when the cache is full.
+func TestAppScoreCache_Update(t *testing.T) {
+	// create a new instance of AppScoreCache.
+	cache := netcache.NewAppScoreCache(100, unittest.Logger(), metrics.NewNoopCollector())
+
+	// tests adding a new entry to the cache.
+	require.NoError(t, cache.Update("peer1", 0.1, 0.5))
+
+	// tests updating an existing entry in the cache.
+	require.NoError(t, cache.Update("peer1", 0.2, 0.8))
+
+	// makes the cache full.
+	for i := 0; i < 100; i++ {
+		require.NoError(t, cache.Update(peer.ID(fmt.Sprintf("peer%d", i)), 0.1, 0.5))
+	}
+
+	// adding a new entry to the cache should fail.
+	require.Error(t, cache.Update("peer101", 0.1, 0.5))
+
+	// retrieving an existing entity should work.
+	for i := 0; i < 100; i++ {
+		score, decay, ok := cache.Get(peer.ID(fmt.Sprintf("peer%d", i)))
+		require.True(t, ok)
+
+		require.Equal(t, 0.1, decay)
+		require.Equal(t, 0.5, score)
+	}
+
+	// yet updating an existing entry should still work.
+	require.NoError(t, cache.Update("peer1", 0.2, 0.8))
+
+}
+
 // TestConcurrentUpdateAndGet tests if the cache can be updated and retrieved concurrently.
 // It updates the cache with a number of records concurrently and then checks if the cache
 // can retrieve all records.
