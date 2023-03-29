cb75f72962053946af09b98386e04c6f6bdb3bde
Yahya Hassanzadeh <yhassanzadeh13@ku.edu.tr>
2023-03-29 16:03:51 -0700

    refactors tests with records

--- network/cache/score_test.go
+++ network/cache/score_test.go
@@ -21,31 +21,56 @@ func TestAppScoreCache_Update(t *testing.T) {
 	cache := netcache.NewAppScoreCache(100, unittest.Logger(), metrics.NewNoopCollector())
 
 	// tests adding a new entry to the cache.
-	require.NoError(t, cache.Update("peer1", 0.1, 0.5))
+	require.NoError(t, cache.Update(netcache.AppScoreRecord{
+		PeerID:      "peer1",
+		Decay:       0.1,
+		Score:       0.5,
+		LastUpdated: time.Now(),
+	}))
 
 	// tests updating an existing entry in the cache.
-	require.NoError(t, cache.Update("peer1", 0.2, 0.8))
+	require.NoError(t, cache.Update(netcache.AppScoreRecord{
+		PeerID:      "peer1",
+		Decay:       0.1,
+		Score:       0.5,
+		LastUpdated: time.Now(),
+	}))
 
 	// makes the cache full.
 	for i := 0; i < 100; i++ {
-		require.NoError(t, cache.Update(peer.ID(fmt.Sprintf("peer%d", i)), 0.1, 0.5))
+		require.NoError(t, cache.Update(netcache.AppScoreRecord{
+			PeerID:      peer.ID(fmt.Sprintf("peer%d", i)),
+			Decay:       0.1,
+			Score:       0.5,
+			LastUpdated: time.Now(),
+		}))
 	}
 
 	// adding a new entry to the cache should fail.
-	require.Error(t, cache.Update("peer101", 0.1, 0.5))
+	require.Error(t, cache.Update(netcache.AppScoreRecord{
+		PeerID:      "peer101",
+		Decay:       0.1,
+		Score:       0.5,
+		LastUpdated: time.Now(),
+	}))
 
 	// retrieving an existing entity should work.
 	for i := 0; i < 100; i++ {
-		score, decay, ok := cache.Get(peer.ID(fmt.Sprintf("peer%d", i)))
+		record, ok := cache.Get(peer.ID(fmt.Sprintf("peer%d", i)))
 		require.True(t, ok)
 
-		require.Equal(t, 0.1, decay)
-		require.Equal(t, 0.5, score)
+		require.Equal(t, 0.1, record.Decay)
+		require.Equal(t, 0.5, record.Score)
+		require.GreaterOrEqual(t, time.Now(), record.LastUpdated)
 	}
 
 	// yet updating an existing entry should still work.
-	require.NoError(t, cache.Update("peer1", 0.2, 0.8))
-
+	require.NoError(t, cache.Update(netcache.AppScoreRecord{
+		PeerID:      "peer1",
+		Decay:       0.2,
+		Score:       0.8,
+		LastUpdated: time.Now(),
+	}))
 }
 
 // TestConcurrentUpdateAndGet tests if the cache can be updated and retrieved concurrently.
@@ -66,7 +91,12 @@ func TestConcurrentUpdateAndGet(t *testing.T) {
 		go func(num int) {
 			defer wg.Done()
 			peerID := fmt.Sprintf("peer%d", num)
-			err := cache.Update(peer.ID(peerID), 0.1*float64(num), float64(num))
+			err := cache.Update(netcache.AppScoreRecord{
+				PeerID:      peer.ID(peerID),
+				Decay:       0.1 * float64(num),
+				Score:       float64(num),
+				LastUpdated: time.Now(),
+			})
 			require.NoError(t, err)
 		}(i)
 	}
@@ -76,12 +106,15 @@ func TestConcurrentUpdateAndGet(t *testing.T) {
 	// checks if the cache can retrieve all records.
 	for i := 0; i < numRecords; i++ {
 		peerID := fmt.Sprintf("peer%d", i)
-		score, decay, found := cache.Get(peer.ID(peerID))
+		record, found := cache.Get(peer.ID(peerID))
 		require.True(t, found)
 
 		expectedScore := float64(i)
-		require.Equal(t, expectedScore, score, "Get() returned incorrect score for record %s: expected %f, got %f", peerID, expectedScore, score)
+		require.Equal(t, expectedScore, record.Score,
+			"Get() returned incorrect Score for record %s: expected %f, got %f", peerID, expectedScore, record.Score)
 		expectedDecay := 0.1 * float64(i)
-		require.Equal(t, expectedDecay, decay, "Get() returned incorrect decay for record %s: expected %f, got %f", peerID, expectedDecay, decay)
+		require.Equal(t, expectedDecay, record.Decay,
+			"Get() returned incorrect Decay for record %s: expected %f, got %f", peerID, expectedDecay, record.Decay)
+		require.GreaterOrEqual(t, time.Now(), record.LastUpdated)
 	}
 }
