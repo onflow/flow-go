681be16004b7083e06d7576aa669a0df30fdda90
Peter Argue <89119817+peterargue@users.noreply.github.com>
2023-03-30 11:41:10 -0700

    remove unused LastBlockID from execdata distributor

--- module/state_synchronization/requester/distributer.go
+++ module/state_synchronization/requester/distributer.go
@@ -3,7 +3,6 @@ package requester
 import (
 	"sync"
 
-	"github.com/onflow/flow-go/model/flow"
 	"github.com/onflow/flow-go/module/executiondatasync/execution_data"
 	"github.com/onflow/flow-go/module/state_synchronization"
 )
@@ -11,24 +10,12 @@ import (
 // ExecutionDataDistributor subscribes to execution data received events from the requester and
 // distributes them to subscribers
 type ExecutionDataDistributor struct {
-	// lastBlockID is the block ID of the most recent execution data received
-	lastBlockID flow.Identifier
-
 	consumers []state_synchronization.OnExecutionDataReceivedConsumer
 	lock      sync.Mutex
 }
 
 func NewExecutionDataDistributor() *ExecutionDataDistributor {
 	return &ExecutionDataDistributor{}
-}
-
-// LastBlockID returns the block ID of the most recent execution data received
-// Execution data is guaranteed to be received in height order
-func (p *ExecutionDataDistributor) LastBlockID() flow.Identifier {
-	p.lock.Lock()
-	defer p.lock.Unlock()
-
-	return p.lastBlockID
 }
 
 // AddOnExecutionDataReceivedConsumer adds a consumer to be notified when new execution data is received
@@ -43,8 +30,6 @@ func (p *ExecutionDataDistributor) AddOnExecutionDataReceivedConsumer(consumer s
 func (p *ExecutionDataDistributor) OnExecutionDataReceived(executionData *execution_data.BlockExecutionDataEntity) {
 	p.lock.Lock()
 	defer p.lock.Unlock()
-
-	p.lastBlockID = executionData.BlockID
 
 	for _, consumer := range p.consumers {
 		consumer(executionData)
