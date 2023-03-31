5d7320d018da953f1518f72768fd5e59e296651f
Yahya Hassanzadeh <yhassanzadeh13@ku.edu.tr>
2023-03-31 10:50:37 -0700

    adds new tests

--- network/p2p/scoring/decay_test.go
+++ network/p2p/scoring/decay_test.go
@@ -1,6 +1,7 @@
 package scoring_test
 
 import (
+	"fmt"
 	"math"
 	"testing"
 	"time"
@@ -10,68 +11,114 @@ import (
 	"github.com/onflow/flow-go/network/p2p/scoring"
 )
 
-// TestGeometricDecay_PositiveScore tests the GeometricDecay function with positive score.
-// It tests the normal case, overflow case, and underflow case.
-// The normal case is the most common case where the score is not too large or too small.
-// The overflow case is when the score is too large and the decay factor is too large.
-// The underflow case is when the score is too small and the decay factor is too small.
-func TestGeometricDecay_PositiveScore(t *testing.T) {
-	score := 10.0
-	decay := 0.5
-	lastUpdated := time.Now().Add(-10 * time.Minute)
-
-	// tests normal case.
-	expected := score * math.Pow(decay, time.Since(lastUpdated).Seconds())
-	actual, err := scoring.GeometricDecay(score, decay, lastUpdated)
-	assert.Nil(t, err)
-	assert.Less(t, math.Abs(expected-actual), 1e-10)
-
-	// tests overflow case.
-	score = 1e300
-	decay = 2 // although such decay is not practical, it is still valid mathematically.
-	lastUpdated = time.Now().Add(-10 * time.Minute)
-	actual, err = scoring.GeometricDecay(score, decay, lastUpdated)
-	assert.Errorf(t, err, "decay factor is too large for %f^%f", decay, time.Since(lastUpdated).Seconds())
-	assert.Equal(t, 0.0, actual)
-
-	// tests underflow case.
-	score = 1e-300
-	decay = 0.5
-	lastUpdated = time.Now().Add(-10 * time.Minute)
-	actual, err = scoring.GeometricDecay(score, decay, lastUpdated)
-	assert.NoError(t, err)
-	assert.Equal(t, 0.0, actual)
-}
-
-// TestGeometricDecay_NegativeScore tests the GeometricDecay function with negative score.
-// It tests the normal case, overflow case, and underflow case.
-// The normal case is the most common case where the score is not too large or too small.
-// The overflow case is when the score is too large and the decay factor is too large.
-// The underflow case is when the score is too small and the decay factor is too small.
-func TestGeometricDecay_NegativeScore(t *testing.T) {
-	score := -10.0
-	decay := 0.5
-	lastUpdated := time.Now().Add(-10 * time.Minute)
-
-	// tests normal case.
-	expected := score * math.Pow(decay, time.Since(lastUpdated).Seconds())
-	actual, err := scoring.GeometricDecay(score, decay, lastUpdated)
-	assert.Nil(t, err)
-	assert.Less(t, math.Abs(expected-actual), 1e-10)
-
-	// test overflow case with negative score.
-	score = -1e300
-	decay = 2
-	lastUpdated = time.Now().Add(-10 * time.Minute)
-	expected = math.Inf(-1)
-	actual, err = scoring.GeometricDecay(score, decay, lastUpdated)
-	assert.Equal(t, expected, actual)
-
-	// test underflow case with negative score.
-	score = -1e-300
-	decay = 0.5
-	lastUpdated = time.Now().Add(-10 * time.Minute)
-	expected = 0.0
-	actual, err = scoring.GeometricDecay(score, decay, lastUpdated)
-	assert.Equal(t, expected, actual)
+func TestGeometricDecay(t *testing.T) {
+	type args struct {
+		score       float64
+		decay       float64
+		lastUpdated time.Time
+	}
+	tests := []struct {
+		name    string
+		args    args
+		want    float64
+		wantErr error
+	}{
+		{
+			name: "valid score, decay, and time",
+			args: args{
+				score:       100,
+				decay:       0.9,
+				lastUpdated: time.Now().Add(-10 * time.Second),
+			},
+			want:    100 * math.Pow(0.9, 10),
+			wantErr: nil,
+		},
+		{
+			name: "zero decay factor",
+			args: args{
+				score:       100,
+				decay:       0,
+				lastUpdated: time.Now(),
+			},
+			want:    0,
+			wantErr: nil,
+		},
+		{
+			name: "decay factor of 1",
+			args: args{
+				score:       100,
+				decay:       1,
+				lastUpdated: time.Now().Add(-10 * time.Second),
+			},
+			want:    100,
+			wantErr: nil,
+		},
+		{
+			name: "negative decay factor",
+			args: args{
+				score:       100,
+				decay:       -0.5,
+				lastUpdated: time.Now(),
+			},
+			want:    0,
+			wantErr: fmt.Errorf("decay factor must be in the range [0, 1], got %f", -0.5),
+		},
+		{
+			name: "decay factor greater than 1",
+			args: args{
+				score:       100,
+				decay:       1.2,
+				lastUpdated: time.Now(),
+			},
+			want:    0,
+			wantErr: fmt.Errorf("decay factor must be in the range [0, 1], got %f", 1.2),
+		},
+		{
+			name: "large time value causing overflow",
+			args: args{
+				score:       100,
+				decay:       0.999999999999999,
+				lastUpdated: time.Now().Add(-1e5 * time.Second),
+			},
+			want:    100 * math.Pow(0.999999999999999, 1e5),
+			wantErr: nil,
+		},
+		{
+			name: "large decay factor and time value causing underflow",
+			args: args{
+				score:       100,
+				decay:       0.999999,
+				lastUpdated: time.Now().Add(-1e9 * time.Second),
+			},
+			want:    0,
+			wantErr: nil,
+		},
+		{
+			name: "very small decay factor and time value causing underflow",
+			args: args{
+				score:       100,
+				decay:       0.000001,
+				lastUpdated: time.Now().Add(-1e9 * time.Second),
+			},
+		},
+		{
+			name: "future time value causing an error",
+			args: args{
+				score:       100,
+				decay:       0.999999,
+				lastUpdated: time.Now().Add(+1e9 * time.Second),
+			},
+			want:    0,
+			wantErr: fmt.Errorf("last updated time cannot be in the future"),
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			got, err := scoring.GeometricDecay(tt.args.score, tt.args.decay, tt.args.lastUpdated)
+			if tt.wantErr != nil {
+				assert.Errorf(t, err, tt.wantErr.Error())
+			}
+			assert.Less(t, math.Abs(got-tt.want), 1e-3)
+		})
+	}
 }
