e722c65418851d2a176034935cefa965bf4ecc60
Yahya Hassanzadeh <yhassanzadeh13@ku.edu.tr>
2023-04-11 21:01:40 -0700

    moves all decay tests to the same test file

--- network/p2p/scoring/decay_test.go
+++ network/p2p/scoring/decay_test.go
@@ -8,12 +8,14 @@ import (
 
 	"github.com/stretchr/testify/assert"
 
+	"github.com/onflow/flow-go/network/p2p"
 	"github.com/onflow/flow-go/network/p2p/scoring"
 )
 
+// TestGeometricDecay tests the GeometricDecay function.
 func TestGeometricDecay(t *testing.T) {
 	type args struct {
-		score       float64
+		penalty     float64
 		decay       float64
 		lastUpdated time.Time
 	}
@@ -24,9 +26,9 @@ func TestGeometricDecay(t *testing.T) {
 		wantErr error
 	}{
 		{
-			name: "valid score, decay, and time",
+			name: "valid penalty, decay, and time",
 			args: args{
-				score:       100,
+				penalty:     100,
 				decay:       0.9,
 				lastUpdated: time.Now().Add(-10 * time.Second),
 			},
@@ -36,7 +38,7 @@ func TestGeometricDecay(t *testing.T) {
 		{
 			name: "zero decay factor",
 			args: args{
-				score:       100,
+				penalty:     100,
 				decay:       0,
 				lastUpdated: time.Now(),
 			},
@@ -46,7 +48,7 @@ func TestGeometricDecay(t *testing.T) {
 		{
 			name: "decay factor of 1",
 			args: args{
-				score:       100,
+				penalty:     100,
 				decay:       1,
 				lastUpdated: time.Now().Add(-10 * time.Second),
 			},
@@ -56,7 +58,7 @@ func TestGeometricDecay(t *testing.T) {
 		{
 			name: "negative decay factor",
 			args: args{
-				score:       100,
+				penalty:     100,
 				decay:       -0.5,
 				lastUpdated: time.Now(),
 			},
@@ -66,7 +68,7 @@ func TestGeometricDecay(t *testing.T) {
 		{
 			name: "decay factor greater than 1",
 			args: args{
-				score:       100,
+				penalty:     100,
 				decay:       1.2,
 				lastUpdated: time.Now(),
 			},
@@ -76,7 +78,7 @@ func TestGeometricDecay(t *testing.T) {
 		{
 			name: "large time value causing overflow",
 			args: args{
-				score:       100,
+				penalty:     100,
 				decay:       0.999999999999999,
 				lastUpdated: time.Now().Add(-1e5 * time.Second),
 			},
@@ -86,7 +88,7 @@ func TestGeometricDecay(t *testing.T) {
 		{
 			name: "large decay factor and time value causing underflow",
 			args: args{
-				score:       100,
+				penalty:     100,
 				decay:       0.999999,
 				lastUpdated: time.Now().Add(-1e9 * time.Second),
 			},
@@ -96,7 +98,7 @@ func TestGeometricDecay(t *testing.T) {
 		{
 			name: "very small decay factor and time value causing underflow",
 			args: args{
-				score:       100,
+				penalty:     100,
 				decay:       0.000001,
 				lastUpdated: time.Now().Add(-1e9 * time.Second),
 			},
@@ -104,7 +106,7 @@ func TestGeometricDecay(t *testing.T) {
 		{
 			name: "future time value causing an error",
 			args: args{
-				score:       100,
+				penalty:     100,
 				decay:       0.999999,
 				lastUpdated: time.Now().Add(+1e9 * time.Second),
 			},
@@ -114,11 +116,134 @@ func TestGeometricDecay(t *testing.T) {
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			got, err := scoring.GeometricDecay(tt.args.score, tt.args.decay, tt.args.lastUpdated)
+			got, err := scoring.GeometricDecay(tt.args.penalty, tt.args.decay, tt.args.lastUpdated)
 			if tt.wantErr != nil {
 				assert.Errorf(t, err, tt.wantErr.Error())
 			}
 			assert.Less(t, math.Abs(got-tt.want), 1e-3)
+		})
+	}
+}
+
+// TestDefaultDecayFunction tests the default decay function.
+// The default decay function is used when no custom decay function is provided.
+// The test evaluates the following cases:
+// 1. penalty is non-negative and should not be decayed.
+// 2. penalty is negative and above the skipDecayThreshold and lastUpdated is too recent. In this case, the penalty should not be decayed.
+// 3. penalty is negative and above the skipDecayThreshold and lastUpdated is too old. In this case, the penalty should not be decayed.
+// 4. penalty is negative and below the skipDecayThreshold and lastUpdated is too recent. In this case, the penalty should not be decayed.
+// 5. penalty is negative and below the skipDecayThreshold and lastUpdated is too old. In this case, the penalty should be decayed.
+func TestDefaultDecayFunction(t *testing.T) {
+	type args struct {
+		record      p2p.GossipSubSpamRecord
+		lastUpdated time.Time
+	}
+
+	type want struct {
+		record p2p.GossipSubSpamRecord
+	}
+
+	tests := []struct {
+		name string
+		args args
+		want want
+	}{
+		{
+			// 1. penalty is non-negative and should not be decayed.
+			name: "penalty is non-negative",
+			args: args{
+				record: p2p.GossipSubSpamRecord{
+					Penalty: 5,
+					Decay:   0.8,
+				},
+				lastUpdated: time.Now(),
+			},
+			want: want{
+				record: p2p.GossipSubSpamRecord{
+					Penalty: 5,
+					Decay:   0.8,
+				},
+			},
+		},
+		{ // 2. penalty is negative and above the skipDecayThreshold and lastUpdated is too recent. In this case, the penalty should not be decayed,
+			// since less than a second has passed since last update.
+			name: "penalty is negative and but above skipDecayThreshold and lastUpdated is too recent",
+			args: args{
+				record: p2p.GossipSubSpamRecord{
+					Penalty: -0.09, // -0.09 is above skipDecayThreshold of -0.1
+					Decay:   0.8,
+				},
+				lastUpdated: time.Now(),
+			},
+			want: want{
+				record: p2p.GossipSubSpamRecord{
+					Penalty: 0, // penalty is set to 0
+					Decay:   0.8,
+				},
+			},
+		},
+		{
+			// 3. penalty is negative and above the skipDecayThreshold and lastUpdated is too old. In this case, the penalty should not be decayed,
+			// since penalty is between [skipDecayThreshold, 0] and more than a second has passed since last update.
+			name: "penalty is negative and but above skipDecayThreshold and lastUpdated is too old",
+			args: args{
+				record: p2p.GossipSubSpamRecord{
+					Penalty: -0.09, // -0.09 is above skipDecayThreshold of -0.1
+					Decay:   0.8,
+				},
+				lastUpdated: time.Now().Add(-10 * time.Second),
+			},
+			want: want{
+				record: p2p.GossipSubSpamRecord{
+					Penalty: 0, // penalty is set to 0
+					Decay:   0.8,
+				},
+			},
+		},
+		{
+			// 4. penalty is negative and below the skipDecayThreshold and lastUpdated is too recent. In this case, the penalty should not be decayed,
+			// since less than a second has passed since last update.
+			name: "penalty is negative and below skipDecayThreshold but lastUpdated is too recent",
+			args: args{
+				record: p2p.GossipSubSpamRecord{
+					Penalty: -5,
+					Decay:   0.8,
+				},
+				lastUpdated: time.Now(),
+			},
+			want: want{
+				record: p2p.GossipSubSpamRecord{
+					Penalty: -5,
+					Decay:   0.8,
+				},
+			},
+		},
+		{
+			// 5. penalty is negative and below the skipDecayThreshold and lastUpdated is too old. In this case, the penalty should be decayed.
+			name: "penalty is negative and below skipDecayThreshold but lastUpdated is too old",
+			args: args{
+				record: p2p.GossipSubSpamRecord{
+					Penalty: -15,
+					Decay:   0.8,
+				},
+				lastUpdated: time.Now().Add(-10 * time.Second),
+			},
+			want: want{
+				record: p2p.GossipSubSpamRecord{
+					Penalty: -15 * math.Pow(0.8, 10),
+					Decay:   0.8,
+				},
+			},
+		},
+	}
+
+	decayFunc := scoring.DefaultDecayFunction()
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			got, err := decayFunc(tt.args.record, tt.args.lastUpdated)
+			assert.NoError(t, err)
+			assert.Less(t, math.Abs(got.Penalty-tt.want.record.Penalty), 10e-3)
+			assert.Equal(t, got.Decay, tt.want.record.Decay)
 		})
 	}
 }
--- network/p2p/scoring/registry_test.go
+++ network/p2p/scoring/registry_test.go
@@ -19,131 +19,8 @@ import (
 	"github.com/onflow/flow-go/utils/unittest"
 )
 
-// TestDefaultDecayFunction tests the default decay function used by the peer scorer.
-// The default decay function is used when no custom decay function is provided.
-// The test evaluates the following cases:
-// 1. score is non-negative and should not be decayed.
-// 2. score is negative and above the skipDecayThreshold and lastUpdated is too recent. In this case, the score should not be decayed.
-// 3. score is negative and above the skipDecayThreshold and lastUpdated is too old. In this case, the score should not be decayed.
-// 4. score is negative and below the skipDecayThreshold and lastUpdated is too recent. In this case, the score should not be decayed.
-// 5. score is negative and below the skipDecayThreshold and lastUpdated is too old. In this case, the score should be decayed.
-func TestDefaultDecayFunction(t *testing.T) {
-	type args struct {
-		record      p2p.GossipSubSpamRecord
-		lastUpdated time.Time
-	}
-
-	type want struct {
-		record p2p.GossipSubSpamRecord
-	}
-
-	tests := []struct {
-		name string
-		args args
-		want want
-	}{
-		{
-			// 1. score is non-negative and should not be decayed.
-			name: "score is non-negative",
-			args: args{
-				record: p2p.GossipSubSpamRecord{
-					Penalty: 5,
-					Decay:   0.8,
-				},
-				lastUpdated: time.Now(),
-			},
-			want: want{
-				record: p2p.GossipSubSpamRecord{
-					Penalty: 5,
-					Decay:   0.8,
-				},
-			},
-		},
-		{ // 2. score is negative and above the skipDecayThreshold and lastUpdated is too recent. In this case, the score should not be decayed,
-			// since less than a second has passed since last update.
-			name: "score is negative and but above skipDecayThreshold and lastUpdated is too recent",
-			args: args{
-				record: p2p.GossipSubSpamRecord{
-					Penalty: -0.09, // -0.09 is above skipDecayThreshold of -0.1
-					Decay:   0.8,
-				},
-				lastUpdated: time.Now(),
-			},
-			want: want{
-				record: p2p.GossipSubSpamRecord{
-					Penalty: 0, // score is set to 0
-					Decay:   0.8,
-				},
-			},
-		},
-		{
-			// 3. score is negative and above the skipDecayThreshold and lastUpdated is too old. In this case, the score should not be decayed,
-			// since score is between [skipDecayThreshold, 0] and more than a second has passed since last update.
-			name: "score is negative and but above skipDecayThreshold and lastUpdated is too old",
-			args: args{
-				record: p2p.GossipSubSpamRecord{
-					Penalty: -0.09, // -0.09 is above skipDecayThreshold of -0.1
-					Decay:   0.8,
-				},
-				lastUpdated: time.Now().Add(-10 * time.Second),
-			},
-			want: want{
-				record: p2p.GossipSubSpamRecord{
-					Penalty: 0, // score is set to 0
-					Decay:   0.8,
-				},
-			},
-		},
-		{
-			// 4. score is negative and below the skipDecayThreshold and lastUpdated is too recent. In this case, the score should not be decayed,
-			// since less than a second has passed since last update.
-			name: "score is negative and below skipDecayThreshold but lastUpdated is too recent",
-			args: args{
-				record: p2p.GossipSubSpamRecord{
-					Penalty: -5,
-					Decay:   0.8,
-				},
-				lastUpdated: time.Now(),
-			},
-			want: want{
-				record: p2p.GossipSubSpamRecord{
-					Penalty: -5,
-					Decay:   0.8,
-				},
-			},
-		},
-		{
-			// 5. score is negative and below the skipDecayThreshold and lastUpdated is too old. In this case, the score should be decayed.
-			name: "score is negative and below skipDecayThreshold but lastUpdated is too old",
-			args: args{
-				record: p2p.GossipSubSpamRecord{
-					Penalty: -15,
-					Decay:   0.8,
-				},
-				lastUpdated: time.Now().Add(-10 * time.Second),
-			},
-			want: want{
-				record: p2p.GossipSubSpamRecord{
-					Penalty: -15 * math.Pow(0.8, 10),
-					Decay:   0.8,
-				},
-			},
-		},
-	}
-
-	decayFunc := scoring.DefaultDecayFunction()
-	for _, tt := range tests {
-		t.Run(tt.name, func(t *testing.T) {
-			got, err := decayFunc(tt.args.record, tt.args.lastUpdated)
-			assert.NoError(t, err)
-			assert.Less(t, math.Abs(got.Penalty-tt.want.record.Penalty), 10e-3)
-			assert.Equal(t, got.Decay, tt.want.record.Decay)
-		})
-	}
-}
-
 // TestInit tests when a peer id is queried for the first time by the
-// app specific score function, the score is initialized to the initial state.
+// app specific penalty function, the penalty is initialized to the initial state.
 func TestInitSpamRecords(t *testing.T) {
 	reg, cache := newGossipSubAppSpecificScoreRegistry(t)
 	peerID := peer.ID("peer-1")
@@ -151,16 +28,16 @@ func TestInitSpamRecords(t *testing.T) {
 	// initially, the cache should not have the peer id.
 	assert.False(t, cache.Has(peerID))
 
-	// when the app specific score function is called for the first time, the score should be initialized to the initial state.
+	// when the app specific penalty function is called for the first time, the penalty should be initialized to the initial state.
 	score := reg.AppSpecificScoreFunc()(peerID)
-	assert.Equal(t, score, scoring.InitAppScoreRecordState().Penalty) // score should be initialized to the initial state.
+	assert.Equal(t, score, scoring.InitAppScoreRecordState().Penalty) // penalty should be initialized to the initial state.
 
 	// the cache should now have the peer id.
 	assert.True(t, cache.Has(peerID))
 	record, err, ok := cache.Get(peerID) // get the record from the cache.
 	assert.True(t, ok)
 	assert.NoError(t, err)
-	assert.Equal(t, record.Penalty, scoring.InitAppScoreRecordState().Penalty) // score should be initialized to the initial state.
+	assert.Equal(t, record.Penalty, scoring.InitAppScoreRecordState().Penalty) // penalty should be initialized to the initial state.
 	assert.Equal(t, record.Decay, scoring.InitAppScoreRecordState().Decay)     // decay should be initialized to the initial state.
 }
 
@@ -179,27 +56,31 @@ func TestInitWhenGetGoesFirst(t *testing.T) {
 	})
 }
 
-// testInitWhenGetFirst tests when a peer id is queried for the first time by the
-// app specific score function, the score is initialized to the initial state. Then, the score is reported and the
-// score is updated in the cache. The next time the app specific score function is called, the score should be the
-// updated score.
+// testInitWhenGetFirst tests the state of the app specific penalty transition of the node in this order:
+// (1) initially, there is no spam record for the peer id in the cache.
+// (2) the app specific penalty function is called for the first time for the peer id, and the spam record is initialized in cache.
+// (3) a spam violation is reported for the peer id, causing the spam record to be updated in cache.
+// (4) the app specific penalty function is called for the second time for the peer id, and the updated spam record is retrieved from cache.
 func testInitWhenGetFirst(t *testing.T, messageType p2p.ControlMessageType, expectedPenalty float64) {
-	reg, cache := newGossipSubAppSpecificScoreRegistry(t)
 	peerID := peer.ID("peer-1")
+	reg, cache := newGossipSubAppSpecificScoreRegistry(
+		t,
+		withStakedIdentity(peerID),
+		withValidSubscriptions(peerID))
 
 	// initially, the cache should not have the peer id.
 	assert.False(t, cache.Has(peerID))
 
-	// when the app specific score function is called for the first time, the score should be initialized to the initial state.
+	// when the app specific penalty function is called for the first time, the penalty should be initialized to the initial state.
 	score := reg.AppSpecificScoreFunc()(peerID)
-	assert.Equal(t, score, scoring.InitAppScoreRecordState().Penalty) // score should be initialized to the initial state.
+	assert.Equal(t, score, scoring.InitAppScoreRecordState().Penalty) // penalty should be initialized to the initial state.
 
 	// the cache should now have the peer id.
 	assert.True(t, cache.Has(peerID))
 	record, err, ok := cache.Get(peerID) // get the record from the cache.
 	assert.True(t, ok)
 	assert.NoError(t, err)
-	assert.Equal(t, record.Penalty, scoring.InitAppScoreRecordState().Penalty) // score should be initialized to the initial state.
+	assert.Equal(t, record.Penalty, scoring.InitAppScoreRecordState().Penalty) // penalty should be initialized to the initial state.
 	assert.Equal(t, record.Decay, scoring.InitAppScoreRecordState().Decay)     // decay should be initialized to the initial state.
 
 	// report a misbehavior for the peer id.
@@ -209,16 +90,16 @@ func testInitWhenGetFirst(t *testing.T, messageType p2p.ControlMessageType, expe
 		Count:   1,
 	})
 
-	// the score should now be updated.
+	// the penalty should now be updated.
 	record, err, ok = cache.Get(peerID) // get the record from the cache.
 	assert.True(t, ok)
 	assert.NoError(t, err)
-	assert.Less(t, math.Abs(expectedPenalty-record.Penalty), 10e-3)        // score should be updated to -10.
+	assert.Less(t, math.Abs(expectedPenalty-record.Penalty), 10e-3)        // penalty should be updated to -10.
 	assert.Equal(t, scoring.InitAppScoreRecordState().Decay, record.Decay) // decay should be initialized to the initial state.
 
-	// when the app specific score function is called again, the score should be updated.
+	// when the app specific penalty function is called again, the penalty should be updated.
 	score = reg.AppSpecificScoreFunc()(peerID)
-	assert.Less(t, math.Abs(expectedPenalty-score), 10e-3) // score should be updated to -10.
+	assert.Less(t, math.Abs(expectedPenalty-score), 10e-3) // penalty should be updated to -10.
 }
 
 func TestInitWhenReportGoesFirst(t *testing.T) {
@@ -236,31 +117,36 @@ func TestInitWhenReportGoesFirst(t *testing.T) {
 	})
 }
 
-// testInitWhenReportGoesFirst tests situation where a peer id is reported for the first time
-// before the app specific score function is called for the first time on it.
-// The test expects the score to be initialized to the initial state and then updated by the penalty value.
-// Subsequent calls to the app specific score function should return the updated score.
+// testInitWhenReportGoesFirst tests situation where a peer id is reported for the first time for spam violation,
+// before the app specific penalty function is called for the first time on it.
+// The test expects the penalty to be initialized to the initial state and then updated by the penalty value.
+// Subsequent calls to the app specific penalty function should return the updated penalty.
 func testInitWhenReportGoesFirst(t *testing.T, messageType p2p.ControlMessageType, expectedPenalty float64) {
-	reg, cache := newGossipSubAppSpecificScoreRegistry(t)
 	peerID := peer.ID("peer-1")
+	reg, cache := newGossipSubAppSpecificScoreRegistry(
+		t,
+		withStakedIdentity(peerID),
+		withValidSubscriptions(peerID))
 
 	// report a misbehavior for the peer id.
 	reg.OnInvalidControlMessageNotification(&p2p.InvalidControlMessageNotification{
 		PeerID:  peerID,
-		MsgType: p2p.CtrlMsgGraft,
+		MsgType: messageType,
 		Count:   1,
 	})
 
-	// the score should now be updated.
+	// the penalty should now be updated.
 	record, err, ok := cache.Get(peerID) // get the record from the cache.
 	assert.True(t, ok)
 	assert.NoError(t, err)
-	assert.Less(t, math.Abs(scoring.DefaultGossipSubCtrlMsgPenaltyValue().Graft-record.Penalty), 10e-3) // score should be updated to -10, we account for decay.
-	assert.Equal(t, scoring.InitAppScoreRecordState().Decay, record.Decay)                              // decay should be initialized to the initial state.
+	assert.Less(t, math.Abs(expectedPenalty-record.Penalty), 10e-3)        // penalty should be updated to -10, we account for decay.
+	assert.Equal(t, scoring.InitAppScoreRecordState().Decay, record.Decay) // decay should be initialized to the initial state.
 
-	// when the app specific score function is called for the first time, the score should be updated.
+	// when the app specific penalty function is called for the first time, the penalty should be updated.
+	// note that since there is a spam penalty, the peer is deprived of the base staked identity reward, and
+	// the penalty is only comprised of the spam penalty.
 	score := reg.AppSpecificScoreFunc()(peerID)
-	assert.Less(t, math.Abs(scoring.DefaultGossipSubCtrlMsgPenaltyValue().Graft-score), 10e-3) // score should be updated to -10, we account for decay.
+	assert.Less(t, math.Abs(expectedPenalty-score), 10e-3) // penalty should be updated to -10, we account for decay.
 }
 
 // TestSpamPenaltyDecaysInCache tests that the spam penalty records decay over time in the cache.
@@ -303,9 +189,9 @@ func TestSpamPenaltyDecaysInCache(t *testing.T) {
 
 	time.Sleep(1 * time.Second) // wait for the penalty to decay.
 
-	// when the app specific score function is called for the first time, the decay functionality should be kicked in
-	// the cache, and the score should be updated. Note that since the penalty values are negative, the default staked identity
-	// reward is not applied. Hence, the score is only comprised of the penalties.
+	// when the app specific penalty function is called for the first time, the decay functionality should be kicked in
+	// the cache, and the penalty should be updated. Note that since the penalty values are negative, the default staked identity
+	// reward is not applied. Hence, the penalty is only comprised of the penalties.
 	score := reg.AppSpecificScoreFunc()(peerID)
 	// the upper bound is the sum of the penalties without decay.
 	scoreUpperBound := penaltyValueFixtures().Prune +
@@ -316,13 +202,13 @@ func TestSpamPenaltyDecaysInCache(t *testing.T) {
 	// in reality, the decay is applied 4 times to the first penalty, then 3 times to the second penalty, and so on.
 	scoreLowerBound := scoreUpperBound * math.Pow(scoring.InitAppScoreRecordState().Decay, 4)
 
-	// with decay, the score should be between the upper and lower bounds.
+	// with decay, the penalty should be between the upper and lower bounds.
 	assert.Greater(t, score, scoreUpperBound)
 	assert.Less(t, score, scoreLowerBound)
 }
 
 // TestSpamPenaltyDecayToZero tests that the spam penalty decays to zero over time, and when the spam penalty of
-// a peer is set back to zero, its app specific score is also reset to the initial state.
+// a peer is set back to zero, its app specific penalty is also reset to the initial state.
 func TestSpamPenaltyDecayToZero(t *testing.T) {
 	cache := netcache.NewGossipSubSpamRecordCache(100, unittest.Logger(), metrics.NewNoopCollector(), scoring.DefaultDecayFunction())
 
@@ -358,12 +244,12 @@ func TestSpamPenaltyDecayToZero(t *testing.T) {
 		Count:   1,
 	})
 
-	// decays happen every second, so we wait for 1 second to make sure the score is updated.
+	// decays happen every second, so we wait for 1 second to make sure the penalty is updated.
 	time.Sleep(1 * time.Second)
-	// the score should now be updated, it should be still negative but greater than the penalty value (due to decay).
+	// the penalty should now be updated, it should be still negative but greater than the penalty value (due to decay).
 	score := reg.AppSpecificScoreFunc()(peerID)
-	require.Less(t, score, float64(0))                      // the score should be less than zero.
-	require.Greater(t, score, penaltyValueFixtures().Graft) // the score should be less than the penalty value due to decay.
+	require.Less(t, score, float64(0))                      // the penalty should be less than zero.
+	require.Greater(t, score, penaltyValueFixtures().Graft) // the penalty should be less than the penalty value due to decay.
 
 	require.Eventually(t, func() bool {
 		// the spam penalty should eventually decay to zero.
@@ -372,15 +258,15 @@ func TestSpamPenaltyDecayToZero(t *testing.T) {
 	}, 5*time.Second, 100*time.Millisecond)
 
 	require.Eventually(t, func() bool {
-		// when the spam penalty is decayed to zero, the app specific score of the node should reset back to its initial state (i.e., max reward).
+		// when the spam penalty is decayed to zero, the app specific penalty of the node should reset back to its initial state (i.e., max reward).
 		return reg.AppSpecificScoreFunc()(peerID) == scoring.MaxAppSpecificReward
 	}, 5*time.Second, 100*time.Millisecond)
 
-	// the score should now be zero.
+	// the penalty should now be zero.
 	record, err, ok := cache.Get(peerID) // get the record from the cache.
 	assert.True(t, ok)
 	assert.NoError(t, err)
-	assert.Equal(t, 0.0, record.Penalty) // score should be zero.
+	assert.Equal(t, 0.0, record.Penalty) // penalty should be zero.
 }
 
 // withStakedIdentity returns a function that sets the identity provider to return an staked identity for the given peer id.
