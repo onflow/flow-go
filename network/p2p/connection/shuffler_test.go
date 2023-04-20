5ee38d75a0064e89939f21312cf17b9e575cf0fe
Yahya Hassanzadeh <yhassanzadeh13@ku.edu.tr>
2023-04-20 15:34:42 -0700

    adds test for salted shuffle

--- network/p2p/connection/shuffler_test.go
+++ network/p2p/connection/shuffler_test.go
@@ -32,3 +32,39 @@ func TestShuffle(t *testing.T) {
 	require.NotEqual(t, shuffled1, shuffled2)
 	require.ElementsMatch(t, peerIds, shuffled2)
 }
+
+// TestSaltedShuffle tests that shufflers initialized with the same peer ID return different shuffles each time.
+// It is due to the fact that the seed is salted with a random salt.
+func TestSaltedShuffle(t *testing.T) {
+	pid := p2ptest.PeerIdFixture(t)
+
+	s1, err := connection.NewPeerIdSliceShuffler(pid)
+	require.NoError(t, err)
+
+	s2, err := connection.NewPeerIdSliceShuffler(pid)
+	require.NoError(t, err)
+
+	peerIds := p2ptest.PeerIdSliceFixture(t, 10)
+
+	shuffled1 := s1.Shuffle(peerIds)
+	shuffled2 := s2.Shuffle(peerIds)
+
+	// shuffle should not be in place, so shuffled1 and shuffle2 should not be equal to peerIds
+	require.NotEqual(t, peerIds, shuffled1)
+	require.NotEqual(t, peerIds, shuffled2)
+	// however, element-wise it should be equal
+	require.ElementsMatch(t, peerIds, shuffled1)
+	require.ElementsMatch(t, peerIds, shuffled2)
+
+	// since seeds are salted, even though the same peer id is used, the shuffle should be different
+	require.NotEqual(t, shuffled1, shuffled2)
+
+	// we keep shuffling for 10 times and make sure that shufflers are not returning the same shuffle.
+	for i := 0; i < 10; i++ {
+		shuffled1 = s1.Shuffle(peerIds)
+		shuffled2 = s2.Shuffle(peerIds)
+		require.NotEqual(t, shuffled1, shuffled2)
+		require.ElementsMatch(t, peerIds, shuffled1)
+		require.ElementsMatch(t, peerIds, shuffled2)
+	}
+}
