5393f059aade0fdc7e22e7c4f3a8fa0fa1b6926d
Misha <misha.rybalov@dapperlabs.com>
2023-05-01 16:41:39 -0400

    Update bench.sh
    added debug statements

--- integration/benchmark/server/bench.sh
+++ integration/benchmark/server/bench.sh
@@ -10,6 +10,7 @@ git fetch
 git fetch --tags
 
 while read -r branch_hash; do
+    echo "The current directory (start of loop) is $PWD"
     hash="${branch_hash##*:}"
     branch="${branch_hash%%:*}"
 
@@ -19,20 +20,23 @@ while read -r branch_hash; do
     git log --oneline | head -1
     git describe
 
+    echo "The current directory (middle of loop) is $PWD"
     make -C ../.. crypto_setup_gopath
     make stop
     rm -f docker-compose.nodes.yml
-    sudo rm -rf data profiler trie
+    rm -rf data profiler trie
     make clean-data
-
+    echo "The current directory (middle2 of loop) is $PWD"
     make -e COLLECTION=12 VERIFICATION=12 NCLUSTERS=12 LOGLEVEL=INFO bootstrap
 #    make -e COLLECTION=12 VERIFICATION=12 NCLUSTERS=12 LOGLEVEL=INFO init
-    sudo DOCKER_BUILDKIT=1 COMPOSE_DOCKER_CLI_BUILD=1 docker-compose -f docker-compose.nodes.yml build || continue
-    sudo DOCKER_BUILDKIT=1 COMPOSE_DOCKER_CLI_BUILD=1 docker-compose -f docker-compose.nodes.yml up -d || continue
+    DOCKER_BUILDKIT=1 COMPOSE_DOCKER_CLI_BUILD=1 docker-compose -f docker-compose.nodes.yml build || continue
+    DOCKER_BUILDKIT=1 COMPOSE_DOCKER_CLI_BUILD=1 docker-compose -f docker-compose.nodes.yml up -d || continue
 
+    echo "The current directory (middle3 of loop) is $PWD"
     sleep 30;
     go run -tags relic ../benchmark/cmd/ci -log-level debug -git-repo-path ../../ -tps-initial 800 -tps-min 1 -tps-max 1200 -duration 30m
 
     make stop
     docker system prune -a -f
+    echo "The current directory (end of loop) is $PWD"
 done </opt/master.recent
