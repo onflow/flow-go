a4d51c59dd588a59c0b17680123a46d2e5dea31e
Misha <misha.rybalov@dapperlabs.com>
2023-05-01 09:23:24 -0400

    Update control.sh
    systemd clones flow-go

--- integration/benchmark/server/control.sh
+++ integration/benchmark/server/control.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
 
-# assumes flow-go was already cloned and git was configured to allow systemd to issue git commands with
-# git config --global --add safe.directory /tmp/flow-go
+# if repo was previously cloned, this will fail cleanly and the script will continue
+git clone https://github.com/onflow/flow-go.git
 
 cd flow-go
+
+# temporary - need to use this branch while changes not yet merged to master
+git checkout misha/4015-move-tps-to-new-vm
+
 git fetch
 git log  --merges --first-parent  --format=master:%H origin/master --since '1 week' | sort -R  | tee /tmp/master.recent
 
-echo "Hello. The current date and time is " | tee -a /tmp/hello.txt
-date | tee -a /tmp/hello.txt
+date +"Hello. The current date and time is %a %b %d %T %Z %Y" | tee -a /tmp/hello.txt
