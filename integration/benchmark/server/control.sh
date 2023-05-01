c137f9ec8f100f8c4b505c7e89a39d669904125a
Misha <misha.rybalov@dapperlabs.com>
2023-05-01 09:49:28 -0400

    Update control.sh
    use "git config --system" instead of "git config --global"

--- integration/benchmark/server/control.sh
+++ integration/benchmark/server/control.sh
@@ -1,13 +1,10 @@
 #!/bin/bash
 
-# if repo was previously cloned, this will fail cleanly and the script will continue
-git clone https://github.com/onflow/flow-go.git
-
 # assumes flow-go was already cloned by user
 
 # need to add this, otherwise will get the following error when systemd executes git commands
 # fatal: detected dubious ownership in repository at '/tmp/flow-go'
-git config --global --add safe.directory /tmp/flow-go
+git config --system --add safe.directory /tmp/flow-go
 
 cd flow-go
 
