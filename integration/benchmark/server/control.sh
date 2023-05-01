049986fb7c1bbb338ff20bbbb5096a61622da671
Misha <misha.rybalov@dapperlabs.com>
2023-05-01 09:39:05 -0400

    Update control.sh
    adding git config --global --add safe.directory /tmp/flow-go

--- integration/benchmark/server/control.sh
+++ integration/benchmark/server/control.sh
@@ -3,12 +3,16 @@
 # if repo was previously cloned, this will fail cleanly and the script will continue
 git clone https://github.com/onflow/flow-go.git
 
+# assumes flow-go was already cloned by user
+
+# need to add this, otherwise will get the following error when systemd executes git commands
+# fatal: detected dubious ownership in repository at '/tmp/flow-go'
+git config --global --add safe.directory /tmp/flow-go
+
 cd flow-go
 
-# temporary - need to use this branch while changes not yet merged to master
-git checkout misha/4015-move-tps-to-new-vm
-
 git fetch
+
 git log  --merges --first-parent  --format=master:%H origin/master --since '1 week' | sort -R  | tee /tmp/master.recent
 
 date +"Hello. The current date and time is %a %b %d %T %Z %Y" | tee -a /tmp/hello.txt
