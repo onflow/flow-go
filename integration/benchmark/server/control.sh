d2e8ada1d00ed33a8975630edf20eedaab729034
Misha <misha.rybalov@dapperlabs.com>
2023-05-04 18:05:41 -0400

    update safe.directory for systemd

--- integration/benchmark/server/control.sh
+++ integration/benchmark/server/control.sh
@@ -4,7 +4,7 @@
 
 # need to add this, otherwise will get the following error when systemd executes git commands
 # fatal: detected dubious ownership in repository at '/tmp/flow-go'
-git config --system --add safe.directory /tmp/flow-go
+git config --system --add safe.directory /opt/flow-go
 
 cd flow-go
 
