8578296c4f48e5994d93a78faab5e3e1d4543e3f
Misha <misha.rybalov@dapperlabs.com>
2023-05-04 18:06:52 -0400

    remove need to cd into flow-go

--- integration/benchmark/server/control.sh
+++ integration/benchmark/server/control.sh
@@ -6,8 +6,6 @@
 # fatal: detected dubious ownership in repository at '/tmp/flow-go'
 git config --system --add safe.directory /opt/flow-go
 
-cd flow-go
-
 git fetch
 
 git log  --merges --first-parent  --format=master:%H origin/master --since '1 week' | sort -R  | tee /opt/master.recent
