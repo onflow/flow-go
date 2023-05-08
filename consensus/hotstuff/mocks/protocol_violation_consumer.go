92c7f97831c294c4371be3afdbde9c82f3c008e0
Alexander Hentschel <alex.hentschel@axiomzen.co>
2023-05-07 20:52:54 -0700

    updated mocks

--- consensus/hotstuff/mocks/protocol_violation_consumer.go
+++ consensus/hotstuff/mocks/protocol_violation_consumer.go
@@ -17,9 +17,34 @@ func (_m *ProtocolViolationConsumer) OnDoubleProposeDetected(_a0 *model.Block, _
 	_m.Called(_a0, _a1)
 }
 
+// OnDoubleTimeoutDetected provides a mock function with given fields: _a0, _a1
+func (_m *ProtocolViolationConsumer) OnDoubleTimeoutDetected(_a0 *model.TimeoutObject, _a1 *model.TimeoutObject) {
+	_m.Called(_a0, _a1)
+}
+
+// OnDoubleVotingDetected provides a mock function with given fields: _a0, _a1
+func (_m *ProtocolViolationConsumer) OnDoubleVotingDetected(_a0 *model.Vote, _a1 *model.Vote) {
+	_m.Called(_a0, _a1)
+}
+
 // OnInvalidBlockDetected provides a mock function with given fields: err
 func (_m *ProtocolViolationConsumer) OnInvalidBlockDetected(err model.InvalidBlockError) {
 	_m.Called(err)
+}
+
+// OnInvalidTimeoutDetected provides a mock function with given fields: err
+func (_m *ProtocolViolationConsumer) OnInvalidTimeoutDetected(err model.InvalidTimeoutError) {
+	_m.Called(err)
+}
+
+// OnInvalidVoteDetected provides a mock function with given fields: err
+func (_m *ProtocolViolationConsumer) OnInvalidVoteDetected(err model.InvalidVoteError) {
+	_m.Called(err)
+}
+
+// OnVoteForInvalidBlockDetected provides a mock function with given fields: vote, invalidProposal
+func (_m *ProtocolViolationConsumer) OnVoteForInvalidBlockDetected(vote *model.Vote, invalidProposal *model.Proposal) {
+	_m.Called(vote, invalidProposal)
 }
 
 type mockConstructorTestingTNewProtocolViolationConsumer interface {
