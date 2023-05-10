bc2746ddb8310d77982f7d9e020b118051d827cd
Yahya Hassanzadeh <yhassanzadeh13@ku.edu.tr>
2023-05-09 18:08:31 -0700

    adds nonce value to the misbehavior report

--- network/alsp/internal/reported_misbehavior_work.go
+++ network/alsp/internal/reported_misbehavior_work.go
@@ -6,6 +6,8 @@ import (
 	"github.com/onflow/flow-go/network/channels"
 )
 
+const NonceSize = 8
+
 // ReportedMisbehaviorWork is an internal data structure for "temporarily" storing misbehavior reports in the queue
 // till they are processed by the worker.
 type ReportedMisbehaviorWork struct {
@@ -18,6 +20,17 @@ type ReportedMisbehaviorWork struct {
 	// Reason is the reason of the misbehavior.
 	Reason network.Misbehavior
 
+	// Nonce is a random nonce value that is used to make the key of the struct unique in the queue even when
+	// the same misbehavior report is reported multiple times. This is needed as we expect the same misbehavior report
+	// to be reported multiple times when an attack persists for a while. We don't want to deduplicate the misbehavior
+	// reports in the queue as we want to penalize the misbehaving node for each report.
+	Nonce [NonceSize]byte
+
 	// Penalty is the penalty value of the misbehavior.
-	Penalty float64
+	// We use `rlp:"-"` to ignore this field when serializing the struct to RLP to determine the key of this struct
+	// when storing in the queue. Hence, the penalty value does "not" contribute to the key for storing in the queue.
+	// As RLP encoding does not support float64, we cannot use this field as the key of the
+	// struct. As we use a random nonce value for the key of the struct, we can be sure that we will not have a collision
+	// in the queue, and duplicate reports will be accepted with unique keys.
+	Penalty float64 `rlp:"-"`
 }
