95030ab13c296494a424b9a1cde7f16c2a9d050a
Leo Zhang (zhangchiqing) <zhangchiqing@gmail.com>
2023-05-18 09:31:12 -0700

    update snapshot formatting

--- cmd/util/cmd/read-protocol-state/cmd/snapshot.go
+++ cmd/util/cmd/read-protocol-state/cmd/snapshot.go
@@ -1,14 +1,12 @@
 package cmd
 
 import (
-	"fmt"
-
 	"github.com/rs/zerolog/log"
 	"github.com/spf13/cobra"
 
 	"github.com/onflow/flow-go/cmd/util/cmd/common"
-	"github.com/onflow/flow-go/engine/common/rpc/convert"
 	"github.com/onflow/flow-go/state/protocol"
+	"github.com/onflow/flow-go/state/protocol/inmem"
 )
 
 var SnapshotCmd = &cobra.Command{
@@ -57,11 +55,20 @@ func runSnapshot(*cobra.Command, []string) {
 		snapshot = state.Sealed()
 	}
 
-	data, err := convert.SnapshotToBytes(snapshot)
+	head, err := snapshot.Head()
 	if err != nil {
-		log.Fatal().Err(err).Msg("failed to convert snapshot to bytes: %v")
+		log.Fatal().Err(err).Msg("fail to get block of snapshot")
+	}
+
+	log.Info().Msgf("creating snapshot for block height %v, id %v", head.Height, head.ID())
+
+	serializable, err := inmem.FromSnapshot(snapshot)
+	if err != nil {
+		log.Fatal().Err(err).Msg("fail to serialize snapshot")
 	}
 
 	log.Info().Msgf("snapshot created")
-	fmt.Println(data)
+
+	encoded := serializable.Encodable()
+	common.PrettyPrint(encoded)
 }
