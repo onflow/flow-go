0e05b5952c0ba0e9aadc842b6c52f76b534de99f
Yahya Hassanzadeh <yhassanzadeh13@ku.edu.tr>
2023-05-24 16:12:06 -0700

    adds disallow list cache interface

--- network/p2p/middleware/disallowListCache.go
+++ network/p2p/middleware/disallowListCache.go
@@ -1,1 +1,42 @@
 package middleware
+
+// DisallowListedCause is a type representing the cause of disallow listing. A remote node may be disallow-listed by the
+// current node for a variety of reasons. This type is used to represent the reason for disallow-listing, so that if
+// a node is disallow-listed for reasons X and Y, allow-listing it back for reason X does not automatically allow-list
+// it for reason Y.
+type DisallowListedCause string
+
+const (
+	// DisallowListedCauseAdmin is the cause of disallow-listing a node by an admin command.
+	DisallowListedCauseAdmin DisallowListedCause = "disallow-listed-admin"
+	// DisallowListedCauseAlsp is the cause of disallow-listing a node by the ALSP (Application Layer Spam Prevention).
+	DisallowListedCauseAlsp DisallowListedCause = "disallow-listed-alsp"
+)
+
+// DisallowListCache is an interface for a cache that keeps the list of disallow-listed peers.
+// It is designed to present a centralized interface for keeping track of disallow-listed peers for different reasons.
+type DisallowListCache interface {
+	// IsDisallowedFor returns true if the given peer is disallow-listed for the given cause.
+	IsDisallowedFor(peerID string, cause DisallowListedCause) bool
+
+	// DisallowFor disallow-lists the given peer for the given cause.
+	// If the peer is already disallow-listed for the given cause, this method is a no-op.
+	// Args:
+	// - peerID: the peer to disallow-list.
+	// - cause: the cause for disallow-listing.
+	// Returns:
+	// - nil if the peer was successfully disallow-listed.
+	// - an error if the peer could not be disallow-listed in the cache, the error is irrecoverable and the current node should
+	// crash.
+	DisallowFor(peerID string, cause DisallowListedCause) error
+
+	// AllowFor allow-lists the given peer for the given cause.
+	// If the peer is not disallow-listed for the given cause, this method is a no-op.
+	// Args:
+	// - peerID: the peer to allow-list.
+	// - cause: the cause for allow-listing.
+	// Returns:
+	// - nil if the peer was successfully allow-listed.
+	// - an error if the peer could not be allow-listed in the cache, the error is irrecoverable and the current node should
+	AllowFor(peerID string, cause DisallowListedCause) error
+}
