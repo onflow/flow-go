0ff5df25f499b7e23ba580bf5b793b9154078815
Khalil Claybon <khalil.claybon@dapperlabs.com>
2023-05-24 15:38:58 -0400

    update cluster prefixed tracker cache size to 150

--- network/p2p/inspector/validation/validation_inspector_config.go
+++ network/p2p/inspector/validation/validation_inspector_config.go
@@ -11,7 +11,7 @@ const (
 	// DefaultControlMsgValidationInspectorQueueCacheSize is the default size of the inspect message queue.
 	DefaultControlMsgValidationInspectorQueueCacheSize = 100
 	// DefaultClusterPrefixedControlMsgsReceivedCacheSize is the default size of the cluster prefixed topics received record cache.
-	DefaultClusterPrefixedControlMsgsReceivedCacheSize = 100
+	DefaultClusterPrefixedControlMsgsReceivedCacheSize = 150
 	// DefaultClusterPrefixedControlMsgsReceivedCacheDecay the default cache decay value for cluster prefixed topics received cached counters.
 	DefaultClusterPrefixedControlMsgsReceivedCacheDecay = 0.99
 	// rpcInspectorComponentName the rpc inspector component name.
