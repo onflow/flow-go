7ae989ff20a08480f58c58342f423fe52548dc4c
Leo Zhang (zhangchiqing) <zhangchiqing@gmail.com>
2023-05-25 10:56:42 -0700

    add comment

--- engine/execution/ingestion/engine.go
+++ engine/execution/ingestion/engine.go
@@ -43,6 +43,7 @@ type Engine struct {
 	me                     module.Local
 	request                module.Requester // used to request collections
 	state                  protocol.State
+	headers                storage.Headers // see comments on getHeaderByHeight for why we need it
 	blocks                 storage.Blocks
 	collections            storage.Collections
 	events                 storage.Events
@@ -1251,4 +1252,13 @@ func (e *Engine) fetchCollection(blockID flow.Identifier, height uint64, guarant
 	))
 
 	return nil
+}
+
+// if the EN is dynamically bootstrapped, the finalized blocks at height range:
+// [ sealedRoot.Height, finalizedRoot.Height - 1] can not be retrieved from
+// protocol state, but only from headers
+func (e *Engine) getHeaderByHeight(height uint64) (*flow.Header, error) {
+	// we don't use protocol state because for dynamic boostrapped execution node
+	// the last executed and sealed block is below the root block
+	return e.headers.ByHeight(height)
 }
