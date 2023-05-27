6778d0a52f471c4578a8ddc38a6a9148576f956e
Misha <misha.rybalov@dapperlabs.com>
2023-05-27 08:07:28 -0400

    removed extra orchestrator code

--- integration/tests/bft/gossipsub/spam/orchestrator.go
+++ integration/tests/bft/gossipsub/spam/orchestrator.go
@@ -1,9 +1,6 @@
 package spam
 
 import (
-	"errors"
-	"fmt"
-	"io"
 	"sync"
 	"testing"
 
@@ -11,7 +8,6 @@ import (
 
 	"github.com/onflow/flow-go/insecure"
 	"github.com/onflow/flow-go/integration/tests/bft"
-	"github.com/onflow/flow-go/utils/logging"
 )
 
 // orchestrator represents a simple orchestrator that passes through all incoming events.
@@ -30,28 +26,4 @@ func NewSpamOrchestrator(t *testing.T, Logger zerolog.Logger) *orchestrator {
 		},
 	}
 	return o
-}
-
-// HandleIngressEvent implements logic of processing the incoming (ingress) events to a corrupt node.
-func (o *orchestrator) HandleIngressEvent(event *insecure.IngressEvent) error {
-	lg := o.Logger.With().
-		Hex("origin_id", logging.ID(event.OriginID)).
-		Str("channel", event.Channel.String()).
-		Str("corrupt_target_id", fmt.Sprintf("%v", event.CorruptTargetID)).
-		Str("flow_protocol_event", fmt.Sprintf("%T", event.FlowProtocolEvent)).Logger()
-
-	err := o.OrchestratorNetwork.SendIngress(event)
-
-	if err != nil {
-		if errors.Is(err, io.EOF) {
-			// log a warning and continue for EOF errors
-			lg.Err(err).Msg("could not pass through ingress event")
-			return nil
-		}
-		// since this is used for testing, if we encounter any RPC send error, crash the orchestrator.
-		lg.Error().Err(err).Msg("could not pass through ingress event")
-		return err
-	}
-	lg.Info().Msg("ingress event passed through successfully")
-	return nil
 }
