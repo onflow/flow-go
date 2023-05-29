56ba9be04e0f4a763e17734c532afd12be667b47
Yahya Hassanzadeh <yhassanzadeh13@ku.edu.tr>
2023-05-29 10:47:23 -0700

    adds networking disallow list cache metrics factory

--- module/metrics/labels.go
+++ module/metrics/labels.go
@@ -85,6 +85,7 @@ const (
 	ResourceNetworkingDnsIpCache                             = "networking_dns_ip_cache"     // networking layer
 	ResourceNetworkingDnsTxtCache                            = "networking_dns_txt_cache"    // networking layer
 	ResourceNetworkingDisallowListNotificationQueue          = "networking_disallow_list_notification_queue"
+	ResourceNetworkingDisallowListCache                      = "networking_disallow_list_cache"
 	ResourceNetworkingRpcInspectorNotificationQueue          = "networking_rpc_inspector_notification_queue"
 	ResourceNetworkingRpcValidationInspectorQueue            = "networking_rpc_validation_inspector_queue"
 	ResourceNetworkingRpcMetricsObserverInspectorQueue       = "networking_rpc_metrics_observer_inspector_queue"
