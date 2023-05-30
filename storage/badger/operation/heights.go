f8589127e6511e75dcf51a27403fdb94dcb581a7
Leo Zhang (zhangchiqing) <zhangchiqing@gmail.com>
2023-05-29 17:29:00 -0700

    rename variable

--- storage/badger/operation/heights.go
+++ storage/badger/operation/heights.go
@@ -7,11 +7,11 @@ import (
 )
 
 func InsertRootHeight(height uint64) func(*badger.Txn) error {
-	return insert(makePrefix(codeRootHeight), height)
+	return insert(makePrefix(codeFinalizedRootHeight), height)
 }
 
 func RetrieveRootHeight(height *uint64) func(*badger.Txn) error {
-	return retrieve(makePrefix(codeRootHeight), height)
+	return retrieve(makePrefix(codeFinalizedRootHeight), height)
 }
 
 func InsertSealedRootHeight(height uint64) func(*badger.Txn) error {
