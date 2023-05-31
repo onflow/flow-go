910953e39dca592e41f8e6d94a43fc01317d99f3
Leo Zhang (zhangchiqing) <zhangchiqing@gmail.com>
2023-05-30 18:16:20 -0700

    fix lint

--- state/protocol/mock/instance_params.go
+++ state/protocol/mock/instance_params.go
@@ -79,6 +79,29 @@ func (_m *InstanceParams) Seal() (*flow.Seal, error) {
 	return r0, r1
 }
 
+// SealedRoot provides a mock function with given fields:
+func (_m *InstanceParams) SealedRoot() (*flow.Header, error) {
+	ret := _m.Called()
+
+	var r0 *flow.Header
+	if rf, ok := ret.Get(0).(func() *flow.Header); ok {
+		r0 = rf()
+	} else {
+		if ret.Get(0) != nil {
+			r0 = ret.Get(0).(*flow.Header)
+		}
+	}
+
+	var r1 error
+	if rf, ok := ret.Get(1).(func() error); ok {
+		r1 = rf()
+	} else {
+		r1 = ret.Error(1)
+	}
+
+	return r0, r1
+}
+
 type mockConstructorTestingTNewInstanceParams interface {
 	mock.TestingT
 	Cleanup(func())
--- state/protocol/mock/params.go
+++ state/protocol/mock/params.go
@@ -75,34 +75,8 @@ func (_m *Params) EpochFallbackTriggered() (bool, error) {
 	return r0, r1
 }
 
-<<<<<<< HEAD
-// ProtocolVersion provides a mock function with given fields:
-func (_m *Params) ProtocolVersion() (uint, error) {
-	ret := _m.Called()
-
-	var r0 uint
-	if rf, ok := ret.Get(0).(func() uint); ok {
-		r0 = rf()
-	} else {
-		r0 = ret.Get(0).(uint)
-	}
-
-	var r1 error
-	if rf, ok := ret.Get(1).(func() error); ok {
-		r1 = rf()
-	} else {
-		r1 = ret.Error(1)
-	}
-
-	return r0, r1
-}
-
-// Root provides a mock function with given fields:
-func (_m *Params) Root() (*flow.Header, error) {
-=======
 // FinalizedRoot provides a mock function with given fields:
 func (_m *Params) FinalizedRoot() (*flow.Header, error) {
->>>>>>> 420f5e72d3 (add comment)
 	ret := _m.Called()
 
 	var r0 *flow.Header
@@ -129,16 +103,13 @@ func (_m *Params) ProtocolVersion() (uint, error) {
 	ret := _m.Called()
 
 	var r0 uint
-	var r1 error
-	if rf, ok := ret.Get(0).(func() (uint, error)); ok {
-		return rf()
-	}
 	if rf, ok := ret.Get(0).(func() uint); ok {
 		r0 = rf()
 	} else {
 		r0 = ret.Get(0).(uint)
 	}
 
+	var r1 error
 	if rf, ok := ret.Get(1).(func() error); ok {
 		r1 = rf()
 	} else {
@@ -158,6 +129,29 @@ func (_m *Params) Seal() (*flow.Seal, error) {
 	} else {
 		if ret.Get(0) != nil {
 			r0 = ret.Get(0).(*flow.Seal)
+		}
+	}
+
+	var r1 error
+	if rf, ok := ret.Get(1).(func() error); ok {
+		r1 = rf()
+	} else {
+		r1 = ret.Error(1)
+	}
+
+	return r0, r1
+}
+
+// SealedRoot provides a mock function with given fields:
+func (_m *Params) SealedRoot() (*flow.Header, error) {
+	ret := _m.Called()
+
+	var r0 *flow.Header
+	if rf, ok := ret.Get(0).(func() *flow.Header); ok {
+		r0 = rf()
+	} else {
+		if ret.Get(0) != nil {
+			r0 = ret.Get(0).(*flow.Header)
 		}
 	}
 
