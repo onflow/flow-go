b64766a80d41e2b120fd6a69ccd18568ff74e0e7
Khalil Claybon <khalil.claybon@dapperlabs.com>
2023-06-06 17:21:37 +0400

    Update integration/tests/bft/gossipsub/rpc_inspector/false_positive_test.go
    Co-authored-by: Yahya Hassanzadeh <yhassanzadeh@ieee.org>

--- integration/tests/bft/gossipsub/rpc_inspector/false_positive_test.go
+++ integration/tests/bft/gossipsub/rpc_inspector/false_positive_test.go
@@ -8,7 +8,7 @@ import (
 
 const numOfTestAccounts = 5
 
-type GossipsubRPCInstpectorFalePostiveNotificationsTestSuite struct {
+type GossipSubRpcInspectorFalsePositiveNotificationsTestSuite struct {
 	Suite
 }
 
