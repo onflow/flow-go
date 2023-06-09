f5b3216eb1378b3811f33a67cae321d06a9bf1bc
Leo Zhang (zhangchiqing) <zhangchiqing@gmail.com>
2023-06-09 10:00:46 -0700

    fix lint

--- storage/badger/cluster_blocks_test.go
+++ storage/badger/cluster_blocks_test.go
@@ -4,11 +4,12 @@ import (
 	"testing"
 
 	"github.com/dgraph-io/badger/v2"
+	"github.com/stretchr/testify/require"
+
 	"github.com/onflow/flow-go/module/metrics"
 	"github.com/onflow/flow-go/storage/badger/operation"
 	"github.com/onflow/flow-go/storage/badger/procedure"
 	"github.com/onflow/flow-go/utils/unittest"
-	"github.com/stretchr/testify/require"
 )
 
 func TestClusterBlocksByHeight(t *testing.T) {
