547c133e3d221d39ec792438ace7f1f1a4118b57
Boris Kozlov <bkozlov@C02X84NSJGH6.ad.here.com>
2023-06-12 20:46:44 +0200

    Added a unit test for linked list

---
+++ module/mempool/herocache/backdata/heropool/linkedlist_test.go
@@ -0,0 +1,76 @@
+package heropool
+
+import (
+	"fmt"
+	"testing"
+
+	"github.com/stretchr/testify/require"
+
+	"github.com/onflow/flow-go/utils/unittest"
+)
+
+// TestStoreAndRetrieval_BelowLimit checks health of heroPool for storing and retrieval scenarios that
+// do not involve ejection.
+// The test involves cases for testing the pool below its limit, and also up to its limit. However, it never gets beyond
+// the limit, so no ejection will kick-in.
+func TestAddElementsToList(t *testing.T) {
+	limit_capacity := uint32(4)
+	number_of_entities := uint32(4)
+	t.Run(fmt.Sprintf("%d-limit-%d-entities", limit_capacity, number_of_entities), func(t *testing.T) {
+		withTestScenario(t, limit_capacity, number_of_entities, LRUEjection, []func(*testing.T, *Pool, []*unittest.MockEntity){
+			func(t *testing.T, pool *Pool, entities []*unittest.MockEntity) {
+				testAddingElementsToList(t, pool, entities, LRUEjection)
+			},
+		}...,
+		)
+	})
+}
+
+func checkList(t *testing.T, expectedList []EIndex, pool *Pool, list *state) {
+	node_index := list.head
+	i := 0
+	// check size
+	require.Equal(t, uint32(len(expectedList)), list.size)
+	// check links
+	for ; node_index != pool.free.tail; i++ {
+		require.Equal(t, expectedList[i], node_index)
+		node_index_next := pool.poolEntities[node_index].node.next
+		require.Equal(t, expectedList[i], pool.poolEntities[node_index_next].node.prev)
+		node_index = node_index_next
+	}
+	//check the tail
+	require.Equal(t, expectedList[i], node_index)
+}
+
+func testAddingElementsToList(t *testing.T, pool *Pool, entitiesToBeAdded []*unittest.MockEntity, ejectionMode EjectionMode) {
+
+	//check entities are initalized
+	expectedList := [4]EIndex{0, 1, 2, 3}
+	checkList(t, expectedList[:], pool, &pool.free)
+
+	// remove middle
+	expectedListNoMiddle := [3]EIndex{0, 1, 3}
+	pool.free.removeEntity(pool, 2)
+	checkList(t, expectedListNoMiddle[:], pool, &pool.free)
+
+	// remove head
+	pool.free.removeEntity(pool, 0)
+	checkList(t, expectedListNoMiddle[1:], pool, &pool.free)
+
+	// remove tail
+	pool.free.removeEntity(pool, 3)
+	checkList(t, expectedListNoMiddle[1:2], pool, &pool.free)
+
+	// remove the last element and ty to remove from an empty list
+	pool.free.removeEntity(pool, 1)
+
+	defer func() {
+		if r := recover(); r == nil {
+			t.Errorf("The code did not panic")
+		} else {
+			require.Equal(t, r, "Removing an entity from the empty list")
+		}
+	}()
+
+	pool.free.removeEntity(pool, 1)
+}
