1c1ab1083e835b8a5b9a8cdb13efb2b580168192
Leo Zhang (zhangchiqing) <zhangchiqing@gmail.com>
2023-06-14 09:27:55 -0700

    log admin tool command and small refactor

--- admin/commands/storage/read_range_blocks.go
+++ admin/commands/storage/read_range_blocks.go
@@ -7,9 +7,14 @@ import (
 	"github.com/onflow/flow-go/admin/commands"
 	"github.com/onflow/flow-go/cmd/util/cmd/read-light-block"
 	"github.com/onflow/flow-go/storage"
+	"github.com/rs/zerolog/log"
 )
 
 var _ commands.AdminCommand = (*ReadRangeBlocksCommand)(nil)
+
+// 10001 instead of 10000, because 10000 won't allow a range from 10000 to 20000,
+// which is easier to type than [10001, 20000]
+const Max_Range_Block_Limit = uint64(10001)
 
 type ReadRangeBlocksCommand struct {
 	blocks storage.Blocks
@@ -27,9 +32,10 @@ func (c *ReadRangeBlocksCommand) Handler(ctx context.Context, req *admin.Command
 		return nil, err
 	}
 
-	limit := uint64(10001)
-	if reqData.Range() > limit {
-		return nil, admin.NewInvalidAdminReqErrorf("getting for more than %v blocks at a time might have an impact to node's performance and is not allowed", limit)
+	log.Info().Str("module", "admin-tool").Msgf("read range blocks, data: %v", reqData)
+
+	if reqData.Range() > Max_Range_Block_Limit {
+		return nil, admin.NewInvalidAdminReqErrorf("getting for more than %v blocks at a time might have an impact to node's performance and is not allowed", Max_Range_Block_Limit)
 	}
 
 	lights, err := read.ReadLightBlockByHeightRange(c.blocks, reqData.startHeight, reqData.endHeight)
