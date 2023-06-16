a2ea98024514134cbf5aaa5c69fa49c6a88908c6
Misha <misha.rybalov@dapperlabs.com>
2023-06-16 18:28:09 -0400

    un-exported disallowListingThreshold

--- network/alsp/model/params.go
+++ network/alsp/model/params.go
@@ -1,7 +1,7 @@
 package model
 
 // To give a summary with the default value:
-//  1. The penalty of each misbehavior is 0.01 * DisallowListingThreshold = -864
+//  1. The penalty of each misbehavior is 0.01 * disallowListingThreshold = -864
 //  2. The penalty of each misbehavior is decayed by a decay value at each decay interval. The default decay value is 1000.
 //     This means that by default if a node misbehaves 100 times in a second, it gets disallow-listed, and takes 86.4 seconds to recover.
 //     We emphasize on the default penalty value can be amplified by the engine that reports the misbehavior.
@@ -13,12 +13,12 @@ package model
 //     around a day to recover. From this point on, the decay speed is 1, and it takes around a day to recover from each
 //     disallow-listing.
 const (
-	// DisallowListingThreshold is the threshold for concluding a node behavior is malicious and disallow-listing the node.
+	// disallowListingThreshold is the threshold for concluding a node behavior is malicious and disallow-listing the node.
 	// If the overall penalty of this node drops below this threshold, the node is reported to be disallow-listed by
 	// the networking layer, i.e., existing connections to the node are closed and the node is no longer allowed to connect till
 	// its penalty is decayed back to zero.
 	// maximum block-list period is 1 day
-	DisallowListingThreshold = -24 * 60 * 60 // (Don't change this value)
+	disallowListingThreshold = -24 * 60 * 60 // (Don't change this value)
 
 	// DefaultPenaltyValue is the default penalty value for misbehaving nodes.
 	// By default, each reported infringement will be penalized by this value. However, the penalty can be amplified
@@ -27,7 +27,7 @@ const (
 	// decrease the number of misbehavior/sec that will result in disallow-listing the node. For example, if the engine
 	// amplifies the penalty by 10, the number of misbehavior/sec that will result in disallow-listing the node will be
 	// 10 times less than the default penalty value and the node will be disallow-listed after 10 times more misbehavior/sec.
-	DefaultPenaltyValue = 0.01 * DisallowListingThreshold // (Don't change this value)
+	DefaultPenaltyValue = 0.01 * disallowListingThreshold // (Don't change this value)
 
 	// InitialDecaySpeed is the initial decay speed of the penalty of a misbehaving node.
 	// The decay speed is applied on an arithmetic progression. The penalty value of the node is the first term of the
@@ -47,11 +47,11 @@ const (
 )
 
 type PenaltyParams struct {
-	DisallowListingThreshold int64
-	DefaultPenaltyValue      int64
-	InitialDecaySpeed        int64
+	DisallowListingThreshold float64
+	DefaultPenaltyValue      float64
+	InitialDecaySpeed        int
 }
 
 func DefaultParams() PenaltyParams {
-	return PenaltyParams{DisallowListingThreshold: DisallowListingThreshold, DefaultPenaltyValue: DefaultPenaltyValue, InitialDecaySpeed: InitialDecaySpeed}
+	return PenaltyParams{DisallowListingThreshold: disallowListingThreshold, DefaultPenaltyValue: DefaultPenaltyValue, InitialDecaySpeed: InitialDecaySpeed}
 }
