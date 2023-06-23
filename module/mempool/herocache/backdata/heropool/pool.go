5c71aec12886198a38ce9dc8fb8331a8f8ebbf3c
Boris Kozlov <bkozlov@C02X84NSJGH6.ad.here.com>
2023-06-23 20:48:15 +0200

    Fixed variable names

--- module/mempool/herocache/backdata/heropool/pool.go
+++ module/mempool/herocache/backdata/heropool/pool.go
@@ -78,7 +78,7 @@ func NewHeroPool(sizeLimit uint32, ejectionMode EjectionMode) *Pool {
 	return l
 }
 
-// setDefaultNodeLinkValues sets nodes prev and nextt to InvalidIndex for all cached entity poolEntities.
+// setDefaultNodeLinkValues sets nodes prev and next to InvalidIndex for all cached entity poolEntities.
 func (p *Pool) setDefaultNodeLinkValues() {
 	for i := 0; i < len(p.poolEntities); i++ {
 		p.poolEntities[i].node.next = InvalidIndex
