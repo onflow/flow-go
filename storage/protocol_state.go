81f77ce27d4a1c82d95cb8645b3dc3f6148798f3
Yurii Oleksyshyn <yuraolex@gmail.com>
2023-07-07 13:39:48 +0300

    Added interface for ProtocolState storage

---
+++ storage/protocol_state.go
@@ -0,0 +1,18 @@
+package storage
+
+import (
+	"github.com/onflow/flow-go/model/flow"
+	"github.com/onflow/flow-go/storage/badger/transaction"
+)
+
+// ProtocolState represents persistent storage for protocol state entries.
+type ProtocolState interface {
+	// StoreTx allows us to store protocol state as part of a DB tx, while still going through the caching layer.
+	StoreTx(id flow.Identifier, protocolState *flow.ProtocolStateEntry) func(*transaction.Tx) error
+	// Index indexes the protocol state by block ID.
+	Index(blockID flow.Identifier, protocolStateID flow.Identifier) func(*transaction.Tx) error
+	// ByID returns the protocol state by its ID.
+	ByID(id flow.Identifier) (*flow.ProtocolStateEntry, error)
+	// ByBlockID returns the protocol state by block ID.
+	ByBlockID(blockID flow.Identifier) (*flow.ProtocolStateEntry, error)
+}
