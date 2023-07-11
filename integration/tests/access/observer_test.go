145de0517a1eff4a1d32d87a70531d73e9f56059
UlyanaAndrukhiv <u.andrukhiv@gmail.com>
2023-07-11 17:15:39 +0300

    Updated rest integration test

--- integration/tests/access/observer_test.go
+++ integration/tests/access/observer_test.go
@@ -174,7 +174,7 @@ func (s *ObserverSuite) TestObserverRPC() {
 }
 
 // TestObserverRest runs the following tests:
-// 1. CompareRPCs: verifies that the observer client returns the same errors as the access client for rests proxied to the upstream AN
+// 1. CompareEndpoints: verifies that the observer client returns the same errors as the access client for rests proxied to the upstream AN
 // 2. HandledByUpstream: stops the upstream AN and verifies that the observer client returns errors for all rests handled by the upstream
 // 3. HandledByObserver: stops the upstream AN and verifies that the observer client handles all others queries
 func (s *ObserverSuite) TestObserverRest() {
@@ -184,27 +184,20 @@ func (s *ObserverSuite) TestObserverRest() {
 	observerAddr := s.net.ContainerByName("observer_1").Addr(testnet.RESTPort)
 
 	httpClient := http.DefaultClient
-	makeHttpCall := func(method string, url string) (*http.Response, error) {
+	makeHttpCall := func(method string, url string, body io.Reader) (*http.Response, error) {
 		switch method {
 		case http.MethodGet:
 			return httpClient.Get(url)
 		case http.MethodPost:
-			var body io.Reader
-			if strings.Contains(url, "/transactions") {
-				body = createTx(s.net)
-			} else {
-				body = strings.NewReader("{}")
-			}
-
 			return httpClient.Post(url, "application/json", body)
 		}
 		panic("not supported")
 	}
-	makeObserverCall := func(method string, path string) (*http.Response, error) {
-		return makeHttpCall(method, "http://"+observerAddr+"/v1"+path)
+	makeObserverCall := func(method string, path string, body io.Reader) (*http.Response, error) {
+		return makeHttpCall(method, "http://"+observerAddr+"/v1"+path, body)
 	}
-	makeAccessCall := func(method string, path string) (*http.Response, error) {
-		return makeHttpCall(method, "http://"+accessAddr+"/v1"+path)
+	makeAccessCall := func(method string, path string, body io.Reader) (*http.Response, error) {
+		return makeHttpCall(method, "http://"+accessAddr+"/v1"+path, body)
 	}
 
 	t.Run("CompareEndpoints", func(t *testing.T) {
@@ -215,8 +208,8 @@ func (s *ObserverSuite) TestObserverRest() {
 				continue
 			}
 			t.Run(endpoint.name, func(t *testing.T) {
-				accessResp, accessErr := makeAccessCall(endpoint.method, endpoint.path)
-				observerResp, observerErr := makeObserverCall(endpoint.method, endpoint.path)
+				accessResp, accessErr := makeAccessCall(endpoint.method, endpoint.path, endpoint.body)
+				observerResp, observerErr := makeObserverCall(endpoint.method, endpoint.path, endpoint.body)
 				assert.NoError(t, accessErr)
 				assert.NoError(t, observerErr)
 				assert.Equal(t, accessResp.Status, observerResp.Status)
@@ -236,7 +229,7 @@ func (s *ObserverSuite) TestObserverRest() {
 				continue
 			}
 			t.Run(endpoint.name, func(t *testing.T) {
-				observerResp, observerErr := makeObserverCall(endpoint.method, endpoint.path)
+				observerResp, observerErr := makeObserverCall(endpoint.method, endpoint.path, endpoint.body)
 				require.NoError(t, observerErr)
 				assert.Contains(t, [...]int{
 					http.StatusInternalServerError,
@@ -252,7 +245,7 @@ func (s *ObserverSuite) TestObserverRest() {
 				continue
 			}
 			t.Run(endpoint.name, func(t *testing.T) {
-				observerResp, observerErr := makeObserverCall(endpoint.method, endpoint.path)
+				observerResp, observerErr := makeObserverCall(endpoint.method, endpoint.path, endpoint.body)
 				require.NoError(t, observerErr)
 				assert.Contains(t, [...]int{http.StatusNotFound, http.StatusOK}, observerResp.StatusCode)
 			})
@@ -395,6 +388,7 @@ type RestEndpointTest struct {
 	name   string
 	method string
 	path   string
+	body   io.Reader
 }
 
 func (s *ObserverSuite) getRestEndpoints() []RestEndpointTest {
@@ -415,6 +409,7 @@ func (s *ObserverSuite) getRestEndpoints() []RestEndpointTest {
 			name:   "createTransaction",
 			method: http.MethodPost,
 			path:   "/transactions",
+			body:   createTx(s.net),
 		},
 		{
 			name:   "getTransactionResultByID",
@@ -455,6 +450,7 @@ func (s *ObserverSuite) getRestEndpoints() []RestEndpointTest {
 			name:   "executeScript",
 			method: http.MethodPost,
 			path:   "/scripts",
+			body:   strings.NewReader("{}"),
 		},
 		{
 			name:   "getAccount",
