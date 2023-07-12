a916db0048c4ea9212e025e43182f35f8e5fecaa
Janez Podhostnik <janez.podhostnik@gmail.com>
2023-07-12 19:06:50 +0200

    Remove dual authorizers for system transaction

--- fvm/blueprints/system.go
+++ fvm/blueprints/system.go
@@ -28,19 +28,6 @@ func SystemChunkTransaction(chain flow.Chain) (*flow.TransactionBody, error) {
 		return nil, fmt.Errorf("could not get system contracts for chain: %w", err)
 	}
 
-	// this is only true for testnet, sandboxnet and mainnet.
-	if contracts.Epoch.Address != chain.ServiceAddress() {
-		// Temporary workaround because the heartbeat resources need to be moved
-		// to the service account:
-		//  - the system chunk will attempt to load both Epoch and VersionBeacon
-		//      resources from either the service account or the staking account
-		//  - the service account committee can then safely move the resources
-		//      at any time
-		//  - once the resources are moved, this workaround should be removed
-		//      after version v0.31.0
-		return systemChunkTransactionDualAuthorizers(chain, contracts)
-	}
-
 	tx := flow.NewTransactionBody().
 		SetScript(
 			[]byte(templates.ReplaceAddresses(
@@ -51,35 +38,7 @@ func SystemChunkTransaction(chain flow.Chain) (*flow.TransactionBody, error) {
 				},
 			)),
 		).
-		AddAuthorizer(contracts.Epoch.Address).
-		SetGasLimit(SystemChunkTransactionGasLimit)
-
-	return tx, nil
-}
-
-// systemChunkTransactionTemplateDualAuthorizer is the same as systemChunkTransactionTemplate
-// but it looks for the heartbeat resources on two different accounts.
-//
-//go:embed scripts/systemChunkTransactionTemplateDualAuthorizer.cdc
-var systemChunkTransactionTemplateDualAuthorizer string
-
-func systemChunkTransactionDualAuthorizers(
-	chain flow.Chain,
-	contracts *systemcontracts.SystemContracts,
-) (*flow.TransactionBody, error) {
-
-	tx := flow.NewTransactionBody().
-		SetScript(
-			[]byte(templates.ReplaceAddresses(
-				systemChunkTransactionTemplateDualAuthorizer,
-				templates.Environment{
-					EpochAddress:             contracts.Epoch.Address.Hex(),
-					NodeVersionBeaconAddress: contracts.NodeVersionBeacon.Address.Hex(),
-				},
-			)),
-		).
 		AddAuthorizer(chain.ServiceAddress()).
-		AddAuthorizer(contracts.Epoch.Address).
 		SetGasLimit(SystemChunkTransactionGasLimit)
 
 	return tx, nil
