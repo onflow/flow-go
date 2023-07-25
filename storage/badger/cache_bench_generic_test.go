abf501bf591c17af1a7fe89e580ed5c5df887e36
Nozim Mehrubonov <nozim.mehrubonov@gmail.com>
2023-07-25 18:43:20 +0100

    Remove benchmark file, refactor benchmark test

--- storage/badger/cache_bench_generic_test.go
+++ storage/badger/cache_bench_generic_test.go
@@ -11,10 +11,10 @@ import (
 func BenchmarkInterface(B *testing.B) {
 	key := unittest.IdentifierFixture()
 	cache := newCacheIFace(metrics.NewNoopCollector(), "test")
-
+	value := unittest.RandomBytes(128)
 	for i := 0; i < B.N; i++ {
 		// insert, then remove the item
-		cache.Insert(key, unittest.RandomBytes(128))
+		cache.Insert(key, value)
 		cache.Remove(key)
 
 		_ = cache.IsCached(key)
@@ -25,10 +25,9 @@ func BenchmarkInterface(B *testing.B) {
 func BenchmarkGenerics(B *testing.B) {
 	key := unittest.IdentifierFixture()
 	cache := newCache[flow.Identifier, any](metrics.NewNoopCollector(), "test")
-
+	value := unittest.RandomBytes(128)
 	for i := 0; i < B.N; i++ {
-		// insert, then remove the item
-		cache.Insert(key, unittest.RandomBytes(128))
+		cache.Insert(key, value)
 		cache.Remove(key)
 
 		_ = cache.IsCached(key)
