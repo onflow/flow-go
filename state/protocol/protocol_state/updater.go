6e3f587d7d794a796a666146f4b86b52f6497514
Yurii Oleksyshyn <yuraolex@gmail.com>
2023-08-01 15:16:09 +0300

    Linted

--- state/protocol/protocol_state/updater.go
+++ state/protocol/protocol_state/updater.go
@@ -2,10 +2,10 @@ package protocol_state
 
 import (
 	"fmt"
-	"github.com/onflow/flow-go/state/protocol"
 
 	"github.com/onflow/flow-go/model/flow"
 	"github.com/onflow/flow-go/model/flow/order"
+	"github.com/onflow/flow-go/state/protocol"
 )
 
 // Updater is a dedicated structure that encapsulates all logic for updating protocol state.
