085123b356964480d223f96a5b72b08d52f4d3a4
Gregor Gololicic <gregor.gololicic@dapperlabs.com>
2023-08-21 15:15:21 +0200

    change register interface

--- storage/registers.go
+++ storage/registers.go
@@ -5,8 +5,8 @@ import "github.com/onflow/flow-go/ledger"
 // Registers represent persistent storage for execution data registers.
 type Registers interface {
 
-	// Store inserts trie updates with provided paths and payloads. If update paths exist the payloads are overwritten.
-	Store(update *ledger.TrieUpdate) error
+	// Store insert register payload indexed by path. If update paths exist the payloads are overwritten.
+	Store(path *ledger.Path, payload *ledger.Payload) error
 
 	// ByPaths returns trie update payloads for provided paths.
 	ByPaths(paths []ledger.Path) ([]*ledger.Payload, error)
