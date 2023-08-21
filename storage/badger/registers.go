8eb579681a5cef44957ad328ea56f97f572ec9ac
Gregor Gololicic <gregor.gololicic@dapperlabs.com>
2023-08-21 15:46:39 +0200

    implement register badger

--- storage/badger/registers.go
+++ storage/badger/registers.go
@@ -4,10 +4,13 @@ import (
 	"github.com/dgraph-io/badger/v2"
 	"github.com/onflow/flow-go/ledger"
 	"github.com/onflow/flow-go/storage"
+	"github.com/onflow/flow-go/storage/badger/operation"
+	"github.com/onflow/flow-go/storage/badger/transaction"
 )
 
 type Registers struct {
-	db *badger.DB
+	db    *badger.DB
+	cache *Cache[ledger.Path, *ledger.Payload]
 }
 
 var _ storage.Registers = &Registers{}
@@ -16,12 +19,23 @@ func NewRegisters(db *badger.DB) *Registers {
 	return &Registers{db: db}
 }
 
-func (r Registers) Store(path ledger.Path, payload *ledger.Payload) error {
-	//TODO implement me
-	panic("implement me")
+func (r *Registers) Store(path ledger.Path, payload *ledger.Payload) error {
+	return operation.RetryOnConflictTx(r.db, transaction.Update, r.cache.PutTx(path, payload))
 }
 
-func (r Registers) ByPath(path ledger.Path) (*ledger.Payload, error) {
-	//TODO implement me
-	panic("implement me")
+func (r *Registers) ByPath(path ledger.Path) (*ledger.Payload, error) {
+	tx := r.db.NewTransaction(false)
+	defer tx.Discard()
+	return r.retrieveRegister(path)(tx)
+}
+
+func (r *Registers) retrieveRegister(path ledger.Path) func(tx *badger.Txn) (*ledger.Payload, error) {
+	return func(tx *badger.Txn) (*ledger.Payload, error) {
+		val, err := r.cache.Get(path)(tx)
+		if err != nil {
+			return nil, err
+		}
+
+		return val, nil
+	}
 }
