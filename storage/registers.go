c93d18c36cd4d9c87b9320af3c1c6fd90c5e6a4d
Gregor Gololicic <gregor.gololicic@dapperlabs.com>
2023-08-21 15:05:28 +0200

    change method logic

--- storage/registers.go
+++ storage/registers.go
@@ -5,7 +5,7 @@ import "github.com/onflow/flow-go/ledger"
 // Registers represent persistent storage for execution data registers.
 type Registers interface {
 
-	// Store inserts trie updates with provided paths and payloads. Duplicate is ignored.
+	// Store inserts trie updates with provided paths and payloads. If update paths exist the payloads are overwritten.
 	Store(update *ledger.TrieUpdate) error
 
 	// ByPaths returns trie update payloads for provided paths.
