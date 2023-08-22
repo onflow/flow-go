69d5000c7b6e27d236c1d18289201ea0bf0f417d
Yahya Hassanzadeh <yhassanzadeh13@ku.edu.tr>
2023-08-22 12:57:04 -0700

    reduces the number of messages sent on topic validation test

--- integration/tests/bft/gossipsub/topicvalidator/orchestrator.go
+++ integration/tests/bft/gossipsub/topicvalidator/orchestrator.go
@@ -21,10 +21,10 @@ const (
 	// The numOfAuthorizedEvents allows us to wait for a certain number of authorized messages to be received, this should
 	// give the network enough time to process the unauthorized messages. This ensures us that the unauthorized messages
 	// were indeed dropped and not unprocessed.
-	numOfAuthorizedEvents = 100
+	numOfAuthorizedEvents = 5
 
 	// numOfUnauthorizedEvents the number of unauthorized events per type to send by the test orchestrator.
-	numOfUnauthorizedEvents = 10
+	numOfUnauthorizedEvents = 5
 )
 
 // Orchestrator represents an insecure.AttackOrchestrator track incoming unauthorized messages and authorized messages received by victim nodes.
