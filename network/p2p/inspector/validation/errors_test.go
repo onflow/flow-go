d9c7970098f3756a6149cd6b6d7044ddccb18a09
Khalil Claybon <khalil.claybon@dapperlabs.com>
2023-08-22 13:09:40 -0400

    add round trip tests for new errors

--- network/p2p/inspector/validation/errors_test.go
+++ network/p2p/inspector/validation/errors_test.go
@@ -80,3 +80,35 @@ func TestErrDuplicateTopicRoundTrip(t *testing.T) {
 	dummyErr := fmt.Errorf("dummy error")
 	assert.False(t, IsDuplicateFoundErr(dummyErr), "IsDuplicateFoundErr should return false for non-DuplicateFoundErr error")
 }
+
+// TestIWantCacheMissThresholdErrRoundTrip ensures correct error formatting for IWantCacheMissThresholdErr.
+func TestIWantCacheMissThresholdErrRoundTrip(t *testing.T) {
+	err := NewIWantCacheMissThresholdErr(5, 10, .4)
+
+	// tests the error message formatting.
+	expectedErrMsg := "5/10 iWant cache misses exceeds the allowed threshold: 0.400000"
+	assert.Equal(t, expectedErrMsg, err.Error(), "the error message should be correctly formatted")
+
+	// tests the IsIWantCacheMissThresholdErr function.
+	assert.True(t, IsIWantCacheMissThresholdErr(err), "IsIWantCacheMissThresholdErr should return true for IWantCacheMissThresholdErr error")
+
+	// test IsIWantCacheMissThresholdErr with a different error type.
+	dummyErr := fmt.Errorf("dummy error")
+	assert.False(t, IsIWantCacheMissThresholdErr(dummyErr), "IsIWantCacheMissThresholdErr should return false for non-IWantCacheMissThresholdErr error")
+}
+
+// TestIWantDuplicateMsgIDThresholdErrRoundTrip ensures correct error formatting for IWantDuplicateMsgIDThresholdErr.
+func TestIWantDuplicateMsgIDThresholdErrRoundTrip(t *testing.T) {
+	err := NewIWantDuplicateMsgIDThresholdErr(5, 10, .4)
+
+	// tests the error message formatting.
+	expectedErrMsg := "5/10 iWant duplicate message ids exceeds the allowed threshold: 0.400000"
+	assert.Equal(t, expectedErrMsg, err.Error(), "the error message should be correctly formatted")
+
+	// tests the IsIWantDuplicateMsgIDThresholdErr function.
+	assert.True(t, IsIWantDuplicateMsgIDThresholdErr(err), "IsIWantDuplicateMsgIDThresholdErr should return true for IWantDuplicateMsgIDThresholdErr error")
+
+	// test IsDuplicateFoundErr with a different error type.
+	dummyErr := fmt.Errorf("dummy error")
+	assert.False(t, IsIWantDuplicateMsgIDThresholdErr(dummyErr), "IsIWantDuplicateMsgIDThresholdErr should return false for non-IWantDuplicateMsgIDThresholdErr error")
+}
