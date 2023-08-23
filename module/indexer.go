ab3d3c6a780fb8e0f3fbbec622b3acedaebc9d04
Gregor Gololicic <gregor.gololicic@dapperlabs.com>
2023-08-23 17:09:09 +0200

    divide indexer into two reader and writer

--- module/indexer.go
+++ module/indexer.go
@@ -6,11 +6,18 @@ import (
 )
 
 type Indexer interface {
+	IndexReader
+	IndexWriter
+}
+
+type IndexReader interface {
 	Last() (uint64, error)                                                    // get last indexed sealed block height
 	HeightForBlock(ID flow.Identifier) (uint64, error)                        // get height by block ID
 	Commitment(height uint64) (flow.StateCommitment, error)                   // get state commitment by height
 	Values(IDs flow.RegisterIDs, height uint64) ([]flow.RegisterValue, error) // retrieve register values for register IDs at given height
+}
 
+type IndexWriter interface {
 	StorePayloads(payloads []*ledger.Payload, height uint64) error // index payloads at given height
 	StoreCommitment(commitment flow.StateCommitment, height uint64) error
 	StoreLast(uint64) error
