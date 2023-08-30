5f7a033e5effe3d726ce775bb38485e9c947ff43
Gregor Gololicic <gregor.gololicic@dapperlabs.com>
2023-08-30 15:52:26 +0200

    change the name and interfaces

--- module/execution_indexer/indexer.go
+++ module/execution_indexer/indexer.go
@@ -1,29 +1,25 @@
 package execution_indexer
 
 import (
-	"context"
 	"fmt"
 	"github.com/onflow/flow-go/cmd/util/ledger/migrations"
-	"github.com/onflow/flow-go/engine/execution/scripts"
-	"github.com/onflow/flow-go/fvm/storage/snapshot"
 	"github.com/onflow/flow-go/ledger"
 	"github.com/onflow/flow-go/model/flow"
 	"github.com/onflow/flow-go/module"
 	"github.com/onflow/flow-go/storage"
 )
 
-var _ module.ExecutionStateIndexer = &Indexer{}
-var _ scripts.ScriptExecutionState = &Indexer{}
+var _ module.ExecutionStateIndexer = &ExecutionIndexer{}
 
-type Indexer struct {
+type ExecutionIndexer struct {
 	registers   storage.Registers
 	headers     storage.Headers
 	last        uint64                          // todo persist
 	commitments map[uint64]flow.StateCommitment // todo persist
 }
 
-func New(registers storage.Registers, headers storage.Headers) *Indexer {
-	return &Indexer{
+func New(registers storage.Registers, headers storage.Headers) *ExecutionIndexer {
+	return &ExecutionIndexer{
 		registers:   registers,
 		headers:     headers,
 		last:        0,
@@ -31,65 +27,16 @@ func New(registers storage.Registers, headers storage.Headers) *Indexer {
 	}
 }
 
-func (i *Indexer) NewStorageSnapshot(commitment flow.StateCommitment) snapshot.StorageSnapshot {
-	var height uint64
-	for h, commit := range i.commitments {
-		if commit == commitment {
-			height = h
-		}
-	}
-
-	// TODO what if commitment is not found
-
-	reader := func(id flow.RegisterID) (flow.RegisterValue, error) {
-		entry, err := i.registers.Get(id, height)
-		if err != nil {
-			return nil, err
-		}
-
-		return entry.Value, nil
-	}
-
-	return snapshot.NewReadFuncStorageSnapshot(reader)
-}
-
-func (i *Indexer) StateCommitmentByBlockID(
-	ctx context.Context,
-	identifier flow.Identifier,
-) (flow.StateCommitment, error) {
-	h, err := i.headers.ByBlockID(identifier)
-	if err != nil {
-		return flow.DummyStateCommitment, fmt.Errorf("could not find block by ID %s for state commitment: %w", identifier.String(), err)
-	}
-
-	commit, ok := i.commitments[h.Height]
-	if !ok {
-		return flow.DummyStateCommitment, fmt.Errorf("could not find the state commitment by height %d", h.Height)
-	}
-
-	return commit, nil
-}
-
-func (i *Indexer) HasState(commitment flow.StateCommitment) bool {
-	for _, c := range i.commitments {
-		if c == commitment {
-			return true
-		}
-	}
-
-	return false
-}
-
-func (i *Indexer) Last() (uint64, error) {
+func (i *ExecutionIndexer) Last() (uint64, error) {
 	return i.last, nil
 }
 
-func (i *Indexer) StoreLast(last uint64) error {
+func (i *ExecutionIndexer) StoreLast(last uint64) error {
 	i.last = last
 	return nil
 }
 
-func (i *Indexer) HeightByBlockID(ID flow.Identifier) (uint64, error) {
+func (i *ExecutionIndexer) HeightByBlockID(ID flow.Identifier) (uint64, error) {
 	header, err := i.headers.ByBlockID(ID)
 	if err != nil {
 		return 0, err
@@ -98,7 +45,7 @@ func (i *Indexer) HeightByBlockID(ID flow.Identifier) (uint64, error) {
 	return header.Height, nil
 }
 
-func (i *Indexer) Commitment(height uint64) (flow.StateCommitment, error) {
+func (i *ExecutionIndexer) Commitment(height uint64) (flow.StateCommitment, error) {
 	val, ok := i.commitments[height]
 	if !ok {
 		return flow.DummyStateCommitment, fmt.Errorf("could not find commitment at height %d", height)
@@ -107,12 +54,12 @@ func (i *Indexer) Commitment(height uint64) (flow.StateCommitment, error) {
 	return val, nil
 }
 
-func (i *Indexer) StoreCommitment(commitment flow.StateCommitment, height uint64) error {
+func (i *ExecutionIndexer) StoreCommitment(commitment flow.StateCommitment, height uint64) error {
 	i.commitments[height] = commitment
 	return nil
 }
 
-func (i *Indexer) Values(IDs flow.RegisterIDs, height uint64) ([]flow.RegisterValue, error) {
+func (i *ExecutionIndexer) Values(IDs flow.RegisterIDs, height uint64) ([]flow.RegisterValue, error) {
 	values := make([]flow.RegisterValue, len(IDs))
 
 	for j, id := range IDs {
@@ -127,7 +74,7 @@ func (i *Indexer) Values(IDs flow.RegisterIDs, height uint64) ([]flow.RegisterVa
 	return values, nil
 }
 
-func (i *Indexer) StorePayloads(payloads []*ledger.Payload, height uint64) error {
+func (i *ExecutionIndexer) StorePayloads(payloads []*ledger.Payload, height uint64) error {
 	regEntries := make(flow.RegisterEntries, len(payloads))
 
 	for j, payload := range payloads {
