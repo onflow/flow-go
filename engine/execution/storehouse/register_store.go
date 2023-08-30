1b6b4703f981ea83103ae7a371c5426dbeac3417
Leo Zhang (zhangchiqing) <zhangchiqing@gmail.com>
2023-08-30 16:54:02 -0700

    add test cases

--- engine/execution/storehouse/register_store.go
+++ engine/execution/storehouse/register_store.go
@@ -58,10 +58,10 @@ func (r *RegisterStore) GetRegister(height uint64, blockID flow.Identifier, regi
 	return reg, nil
 }
 
-// SaveRegister saves to InMemoryRegisterStore first, then trigger the same check as OnBlockFinalized
-// Depend on InMemoryRegisterStore.SaveRegister
-func (r *RegisterStore) SaveRegister(header *flow.Header, registers []flow.RegisterEntry) error {
-	err := r.memStore.SaveRegister(header.Height, header.ID(), header.ParentID, registers)
+// SaveRegisters saves to InMemoryRegisterStore first, then trigger the same check as OnBlockFinalized
+// Depend on InMemoryRegisterStore.SaveRegisters
+func (r *RegisterStore) SaveRegisters(header *flow.Header, registers []flow.RegisterEntry) error {
+	err := r.memStore.SaveRegisters(header.Height, header.ID(), header.ParentID, registers)
 	if err != nil {
 		return fmt.Errorf("cannot save register to memStore: %w", err)
 	}
@@ -75,7 +75,7 @@ func (r *RegisterStore) SaveRegister(header *flow.Header, registers []flow.Regis
 
 // Depend on FinalizedReader's GetFinalizedBlockIDAtHeight
 // Depend on ExecutedFinalizedWAL.Append
-// Depend on OnDiskRegisterStore.SaveRegister
+// Depend on OnDiskRegisterStore.SaveRegisters
 // OnBlockFinalized trigger the check of whether a block at the next height becomes finalized and executed.
 // the next height is the existing finalized and executed block's height + 1.
 // If a block at next height becomes finalized and executed, then:
@@ -103,7 +103,7 @@ func (r *RegisterStore) OnBlockFinalized() error {
 		return fmt.Errorf("cannot write %v registers to write ahead logs for height %v: %w", len(regs), next, err)
 	}
 
-	err = r.diskStore.SaveRegister(next, regs)
+	err = r.diskStore.SaveRegisters(next, regs)
 	if err != nil {
 		return fmt.Errorf("cannot save %v registers to disk store for height %v: %w", len(regs), next, err)
 	}
@@ -146,7 +146,7 @@ func (r *RegisterStore) syncDiskStore() (uint64, error) {
 				return 0, fmt.Errorf("cannot read registers from write ahead logs: %w", err)
 			}
 
-			err = r.diskStore.SaveRegister(height, registers)
+			err = r.diskStore.SaveRegisters(height, registers)
 			if err != nil {
 				return 0, fmt.Errorf("cannot save registers to disk store at height %v : %w", height, err)
 			}
