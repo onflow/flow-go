86bc8c9ac27c28c59345dd39f2f02d1a3ffab962
Misha <misha@gomisha.com>
2023-09-01 17:35:36 -0400

    more sub packages testing

--- utils/test_matrix/test_matrix_test.go
+++ utils/test_matrix/test_matrix_test.go
@@ -10,6 +10,7 @@ import (
 func getAllFlowPackages() []string {
 	return []string{
 		flowPackagePrefix + "abc",
+		flowPackagePrefix + "abc/123",
 		flowPackagePrefix + "abc/def",
 		flowPackagePrefix + "abc/def/ghi",
 		flowPackagePrefix + "def",
@@ -25,14 +26,15 @@ func getAllFlowPackages() []string {
 	}
 }
 
+// TestListTargetPackages tests that the target packages are included in the target packages and seen packages.
 func TestListTargetPackages(t *testing.T) {
 	targetPackages, seenPackages := listTargetPackages([]string{"abc", "ghi"}, getAllFlowPackages())
 	require.Equal(t, 2, len(targetPackages))
-	require.Equal(t, 4, len(seenPackages))
 
-	// there should be 3 packages that start with "abc"
-	require.Equal(t, 3, len(targetPackages["abc"]))
+	// there should be 4 packages that start with "abc"
+	require.Equal(t, 4, len(targetPackages["abc"]))
 	require.Contains(t, targetPackages["abc"], flowPackagePrefix+"abc")
+	require.Contains(t, targetPackages["abc"], flowPackagePrefix+"abc/123")
 	require.Contains(t, targetPackages["abc"], flowPackagePrefix+"abc/def")
 	require.Contains(t, targetPackages["abc"], flowPackagePrefix+"abc/def/ghi")
 
@@ -40,7 +42,10 @@ func TestListTargetPackages(t *testing.T) {
 	require.Equal(t, 1, len(targetPackages["ghi"]))
 	require.Contains(t, targetPackages["ghi"], flowPackagePrefix+"ghi")
 
+	// there should be 5 packages that start with "abc"
+	require.Equal(t, 5, len(seenPackages))
 	require.Contains(t, seenPackages, flowPackagePrefix+"abc")
+	require.Contains(t, seenPackages, flowPackagePrefix+"abc/123")
 	require.Contains(t, seenPackages, flowPackagePrefix+"abc/def")
 	require.Contains(t, seenPackages, flowPackagePrefix+"abc/def/ghi")
 	require.Contains(t, seenPackages, flowPackagePrefix+"ghi")
@@ -51,16 +56,17 @@ func TestListTargetPackages(t *testing.T) {
 func TestListTargetSubPackages(t *testing.T) {
 	targetPackages, seenPackages := listTargetPackages([]string{"abc/def"}, getAllFlowPackages())
 	require.Equal(t, 1, len(targetPackages))
-	require.Equal(t, 2, len(seenPackages))
 
 	// there should be 2 packages that starts with "abc/def"
 	require.Equal(t, 2, len(targetPackages["abc/def"]))
 	require.Contains(t, targetPackages["abc/def"], flowPackagePrefix+"abc/def")
 
+	require.Equal(t, 2, len(seenPackages))
 	require.Contains(t, seenPackages, flowPackagePrefix+"abc/def")
 	require.Contains(t, seenPackages, flowPackagePrefix+"abc/def/ghi")
 }
 
+// TestListOtherPackages tests that the remaining packages that don't match any of the target packages are included
 func TestListOtherPackages(t *testing.T) {
 	var seenPackages = make(map[string]string)
 	seenPackages[flowPackagePrefix+"abc/def"] = flowPackagePrefix + "abc/def"
@@ -71,9 +77,10 @@ func TestListOtherPackages(t *testing.T) {
 
 	otherPackages := listOtherPackages(getAllFlowPackages(), seenPackages)
 
-	require.Equal(t, 8, len(otherPackages))
+	require.Equal(t, 9, len(otherPackages))
 
 	require.Contains(t, otherPackages, flowPackagePrefix+"abc")
+	require.Contains(t, otherPackages, flowPackagePrefix+"abc/123")
 	require.Contains(t, otherPackages, flowPackagePrefix+"def")
 	require.Contains(t, otherPackages, flowPackagePrefix+"def/abc")
 	require.Contains(t, otherPackages, flowPackagePrefix+"jkl")
@@ -83,21 +90,27 @@ func TestListOtherPackages(t *testing.T) {
 	require.Contains(t, otherPackages, flowPackagePrefix+"yz")
 }
 
+// TestGenerateTestMatrix tests that the test matrix is generated correctly where the target packages include top level
+// packages as well as sub packages.
 func TestGenerateTestMatrix(t *testing.T) {
-	targetPackages, seenPackages := listTargetPackages([]string{"abc", "ghi"}, getAllFlowPackages())
-	require.Equal(t, 2, len(targetPackages))
-	require.Equal(t, 4, len(seenPackages))
+	targetPackages, seenPackages := listTargetPackages([]string{"abc/def", "def", "ghi"}, getAllFlowPackages())
+	require.Equal(t, 3, len(targetPackages))
+	require.Equal(t, 5, len(seenPackages))
 
 	otherPackages := listOtherPackages(getAllFlowPackages(), seenPackages)
 
 	matrix := generateTestMatrix(targetPackages, otherPackages)
 
-	// should be 3 groups in test matrix: abc, ghi, others
-	require.Equal(t, 3, len(matrix))
+	// should be 3 groups in test matrix: abc/def, def, ghi, others
+	require.Equal(t, 4, len(matrix))
 
 	require.Contains(t, matrix, testMatrix{
-		Name:     "abc",
-		Packages: "github.com/onflow/flow-go/abc github.com/onflow/flow-go/abc/def github.com/onflow/flow-go/abc/def/ghi"},
+		Name:     "abc/def",
+		Packages: "github.com/onflow/flow-go/abc/def github.com/onflow/flow-go/abc/def/ghi"},
+	)
+	require.Contains(t, matrix, testMatrix{
+		Name:     "def",
+		Packages: "github.com/onflow/flow-go/def github.com/onflow/flow-go/def/abc"},
 	)
 	require.Contains(t, matrix, testMatrix{
 		Name:     "ghi",
@@ -105,6 +118,6 @@ func TestGenerateTestMatrix(t *testing.T) {
 	)
 	require.Contains(t, matrix, testMatrix{
 		Name:     "others",
-		Packages: "github.com/onflow/flow-go/def github.com/onflow/flow-go/def/abc github.com/onflow/flow-go/jkl github.com/onflow/flow-go/mno/abc github.com/onflow/flow-go/pqr github.com/onflow/flow-go/stu github.com/onflow/flow-go/vwx github.com/onflow/flow-go/vwx/ghi github.com/onflow/flow-go/yz"},
+		Packages: "github.com/onflow/flow-go/abc github.com/onflow/flow-go/abc/123 github.com/onflow/flow-go/jkl github.com/onflow/flow-go/mno/abc github.com/onflow/flow-go/pqr github.com/onflow/flow-go/stu github.com/onflow/flow-go/vwx github.com/onflow/flow-go/vwx/ghi github.com/onflow/flow-go/yz"},
 	)
 }
