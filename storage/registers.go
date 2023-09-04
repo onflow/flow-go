bb2b4705da642c99bf2c6f439d856fb334cb1b6c
Gregor Gololicic <gregor.gololicic@dapperlabs.com>
2023-09-04 11:52:23 +0200

    add set last height and last height getter methods

--- storage/registers.go
+++ storage/registers.go
@@ -12,19 +12,25 @@ type Registers interface {
 
 // RegisterReader defines read-only operations on the register index.
 type RegisterReader interface {
+	// LastHeight at which we indexed registers.
+	LastHeight() (uint64, error)
 	// Get register by the register ID at a given block height.
 	//
 	// If the register at the given height was not indexed, returns the highest
 	// height the register was indexed at.
-	// An error is returned if the register was not indexed at all. Expected errors:
-	// - storage.ErrNotFound if the register was not found in the db
+	// An error is returned if the register was not indexed at all or if the height is out of bounds.
+	// Expected errors:
+	// - storage.ErrNotFound if the register was not found in the db or is out of bounds.
 	Get(ID flow.RegisterID, height uint64) (flow.RegisterValue, error)
 }
 
 // RegisterWriter defines write-only operations on the register index.
 type RegisterWriter interface {
+	// SetLastHeight at which we indexed registers.
+	// The provided height should either be one higher than the current height or the same to ensure idempotency.
+	// If the height is invalid this will panic!
+	SetLastHeight(height uint64) error
 	// Store batch of register entries at the provided block height.
-	//
 	// If the registers already exists it overwrites them to make this action idempotent.
 	Store(entries flow.RegisterEntries, height uint64) error
 }
