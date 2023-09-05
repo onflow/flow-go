2a27beb0946e6fedb76e99a4cd428f97ec0550dc
Gregor Gololicic <gregor.gololicic@dapperlabs.com>
2023-09-05 14:35:24 +0200

    update interface

--- storage/registers.go
+++ storage/registers.go
@@ -15,6 +15,7 @@ type RegisterReader interface {
 	// LatestHeight at which we indexed registers.
 	LatestHeight() (uint64, error)
 	// FirstHeight at which we started to index the registers.
+	// Returns the first indexed height found in the store.
 	FirstHeight() (uint64, error)
 	// Get register by the register ID at a given block height.
 	//
@@ -31,5 +32,12 @@ type RegisterWriter interface {
 	// Store batch of register entries at the provided block height.
 	// The provided height should either be one higher than the current height or the same to ensure idempotency.
 	// If the height is not within those bounds it will panic!
+	// Store should be used with the SetLatestHeight to progress the indexing.
+	// An error might get returned if there are problems with persisting the registers.
 	Store(entries flow.RegisterEntries, height uint64) error
+	// SetLatestHeight updates the latest height record.
+	// The provided height should either be one higher than the current height or the same to ensure idempotency.
+	// If the height is not within those bounds it will panic!
+	// An error might get returned if there are problems with persisting the height.
+	SetLatestHeight(height uint64) error
 }
