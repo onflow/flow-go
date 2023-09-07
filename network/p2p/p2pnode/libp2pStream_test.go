4c0dd758b47fe00eb4c5fe37b18bc03cd49aaeb8
Yurii Oleksyshyn <yuraolex@gmail.com>
2023-09-07 23:45:21 +0300

    Linted

--- network/p2p/p2pnode/libp2pStream_test.go
+++ network/p2p/p2pnode/libp2pStream_test.go
@@ -55,7 +55,7 @@ func TestStreamClosing(t *testing.T) {
 	p2ptest.StartNodes(t, signalerCtx, nodes)
 	defer p2ptest.StopNodes(t, nodes, cancel)
 
-	nodeInfo1, err := utils.PeerAddressInfo(*identities[1])
+	nodeInfo1, err := utils.PeerAddressInfo(identities[1].IdentitySkeleton)
 	require.NoError(t, err)
 
 	senderWG := sync.WaitGroup{}
@@ -172,7 +172,7 @@ func testCreateStream(t *testing.T, sporkId flow.Identifier, unicasts []protocol
 	allStreamsClosedWg := sync.WaitGroup{}
 	for i := 0; i < streamCount; i++ {
 		allStreamsClosedWg.Add(1)
-		pInfo, err := utils.PeerAddressInfo(*id2)
+		pInfo, err := utils.PeerAddressInfo(id2.IdentitySkeleton)
 		require.NoError(t, err)
 		nodes[0].Host().Peerstore().AddAddrs(pInfo.ID, pInfo.Addrs, peerstore.AddressTTL)
 		go func() {
@@ -244,7 +244,7 @@ func TestCreateStream_FallBack(t *testing.T) {
 	allStreamsClosedWg := sync.WaitGroup{}
 	for i := 0; i < streamCount; i++ {
 		allStreamsClosedWg.Add(1)
-		pInfo, err := utils.PeerAddressInfo(otherId)
+		pInfo, err := utils.PeerAddressInfo(otherId.IdentitySkeleton)
 		require.NoError(t, err)
 		thisNode.Host().Peerstore().AddAddrs(pInfo.ID, pInfo.Addrs, peerstore.AddressTTL)
 
@@ -293,7 +293,7 @@ func TestCreateStreamIsConcurrencySafe(t *testing.T) {
 	p2ptest.StartNodes(t, signalerCtx, nodes)
 	defer p2ptest.StopNodes(t, nodes, cancel)
 
-	nodeInfo1, err := utils.PeerAddressInfo(*identities[1])
+	nodeInfo1, err := utils.PeerAddressInfo(identities[1].IdentitySkeleton)
 	require.NoError(t, err)
 
 	wg := sync.WaitGroup{}
@@ -356,7 +356,7 @@ func TestNoBackoffWhenCreatingStream(t *testing.T) {
 	defer p2ptest.StopNode(t, node1, cancel1)
 
 	id2 := identities[1]
-	pInfo, err := utils.PeerAddressInfo(*id2)
+	pInfo, err := utils.PeerAddressInfo(id2.IdentitySkeleton)
 	require.NoError(t, err)
 	nodes[0].Host().Peerstore().AddAddrs(pInfo.ID, pInfo.Addrs, peerstore.AddressTTL)
 	maxTimeToWait := p2pnode.MaxConnectAttempt * unicast.MaxRetryJitter * time.Millisecond
@@ -516,7 +516,7 @@ func TestCreateStreamTimeoutWithUnresponsiveNode(t *testing.T) {
 		require.NoError(t, listener.Close())
 	}()
 
-	silentNodeInfo, err := utils.PeerAddressInfo(silentNodeId)
+	silentNodeInfo, err := utils.PeerAddressInfo(silentNodeId.IdentitySkeleton)
 	require.NoError(t, err)
 
 	timeout := 1 * time.Second
@@ -555,7 +555,7 @@ func TestCreateStreamIsConcurrent(t *testing.T) {
 	p2ptest.StartNodes(t, signalerCtx, goodNodes)
 	defer p2ptest.StopNodes(t, goodNodes, cancel)
 
-	goodNodeInfo1, err := utils.PeerAddressInfo(*goodNodeIds[1])
+	goodNodeInfo1, err := utils.PeerAddressInfo(goodNodeIds[1].IdentitySkeleton)
 	require.NoError(t, err)
 
 	// create a silent node which never replies
@@ -563,7 +563,7 @@ func TestCreateStreamIsConcurrent(t *testing.T) {
 	defer func() {
 		require.NoError(t, listener.Close())
 	}()
-	silentNodeInfo, err := utils.PeerAddressInfo(silentNodeId)
+	silentNodeInfo, err := utils.PeerAddressInfo(silentNodeId.IdentitySkeleton)
 	require.NoError(t, err)
 
 	// creates a stream to unresponsive node and makes sure that the stream creation is blocked
