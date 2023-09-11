1a5b376f8a9c3df8e5ef4bbb577a8689cb400953
Yahya Hassanzadeh <yhassanzadeh13@ku.edu.tr>
2023-09-11 15:47:34 -0700

    adds a todo

--- network/p2p/p2plogging/internal/peerIdCache.go
+++ network/p2p/p2plogging/internal/peerIdCache.go
@@ -8,6 +8,11 @@ import (
 )
 
 type PeerIdCache struct {
+	// TODO: Note that we use lru.Cache as there is an inherent import cycle when using the HeroCache.
+	//	 Moving forward we should consider moving the HeroCache to a separate repository and transition
+	// 	to using it here.
+	// 	This PeerIdCache is using extensively across the codebase, so any minor import cycle will cause
+	// 	a lot of trouble.
 	peerCache *lru.Cache
 }
 
@@ -21,6 +26,9 @@ func NewPeerIdCache(size int) *PeerIdCache {
 	}
 }
 
+// PeerIdString returns the base58 encoded peer id string, it looks up the peer id in a cache to avoid
+// expensive base58 encoding, and caches the result for future use in case of a cache miss.
+// It is safe to call this method concurrently.
 func (p *PeerIdCache) PeerIdString(pid peer.ID) string {
 	pidStr, ok := p.peerCache.Get(pid)
 	if ok {
@@ -32,10 +40,13 @@ func (p *PeerIdCache) PeerIdString(pid peer.ID) string {
 	return pidStr0
 }
 
+// Size returns the number of entries in the cache; it is mainly used for testing.
 func (p *PeerIdCache) Size() int {
 	return p.peerCache.Len()
 }
 
+// ByPeerId returns the base58 encoded peer id string by directly looking up the peer id in the cache. It is only
+// used for testing and since this is an internal package, it is not exposed to the outside world.
 func (p *PeerIdCache) ByPeerId(pid peer.ID) (string, bool) {
 	pidStr, ok := p.peerCache.Get(pid)
 	if ok {
