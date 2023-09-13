3af93fc6066a432d6670c030f2366f22f7f4c522
Yahya Hassanzadeh <yhassanzadeh13@ku.edu.tr>
2023-09-13 10:48:16 -0700

    extends godo

--- network/p2p/unicast/internal/dialConfigEntity.go
+++ network/p2p/unicast/internal/dialConfigEntity.go
@@ -6,16 +6,19 @@ import (
 	"github.com/onflow/flow-go/model/flow"
 )
 
+// DialConfigEntity is a struct that represents a dial config entry for storing in the dial config cache.
+// It implements the flow.Entity interface.
 type DialConfigEntity struct {
-	PeerId             peer.ID
-	DialBackoff        uint64
-	StreamBackoff      uint64
-	LastSuccessfulDial uint64
-	id                 flow.Identifier
+	PeerId             peer.ID         // remote peer id; used as the "key" in the dial config cache.
+	DialBackoff        uint64          // number of times we have to try to dial the peer before we give up.
+	StreamBackoff      uint64          // number of times we have to try to open a stream to the peer before we give up.
+	LastSuccessfulDial uint64          // timestamp of the last successful dial to the peer.
+	id                 flow.Identifier // cache the id for fast lookup (HeroCache).
 }
 
 var _ flow.Entity = (*DialConfigEntity)(nil)
 
+// ID returns the ID of the dial config entity; it is hash value of the peer id.
 func (d DialConfigEntity) ID() flow.Identifier {
 	if d.id == flow.ZeroID {
 		d.id = flow.MakeIDFromFingerPrint([]byte(d.PeerId))
@@ -23,6 +26,7 @@ func (d DialConfigEntity) ID() flow.Identifier {
 	return d.id
 }
 
+// Checksum acts the same as ID.
 func (d DialConfigEntity) Checksum() flow.Identifier {
 	return d.ID()
 }
