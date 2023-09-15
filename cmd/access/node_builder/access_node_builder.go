5c0f1cfda37c036ad58d384e8f375a456cc76ccb
Gregor Gololicic <gregor.gololicic@dapperlabs.com>
2023-09-15 15:13:04 +0200

    fix building indexer

--- cmd/access/node_builder/access_node_builder.go
+++ cmd/access/node_builder/access_node_builder.go
@@ -612,7 +612,10 @@ func (builder *FlowAccessNodeBuilder) BuildExecutionDataRequester() *FlowAccessN
 			)
 
 			registers := memory.NewRegisters() // temporarily use the in-memory db
-			exeIndexer := indexer.New(registers, builder.Storage.Headers, builder.executionDataConfig.InitialBlockHeight)
+			exeIndexer, err := indexer.New(registers, builder.Storage.Headers, builder.executionDataConfig.InitialBlockHeight)
+			if err != nil {
+				return nil, err
+			}
 
 			// execution state worker uses a jobqueue to process new execution data and indexes it by using the indexer.
 			worker := indexer.NewExecutionStateWorker(
