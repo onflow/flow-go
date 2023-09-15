04f6f7b5b4619ec55ac013206d66db124c3c84a1
Peter Argue <89119817+peterargue@users.noreply.github.com>
2023-09-15 15:10:34 -0700

    add compatibility warning

--- module/executiondatasync/execution_data/internal/execution_data_versions.go
+++ module/executiondatasync/execution_data/internal/execution_data_versions.go
@@ -7,6 +7,10 @@ import (
 
 // This is a collection of data structures from previous versions of ExecutionData.
 // They are maintained here for backwards compatibility testing.
+//
+// Note: the current codebase makes no guarantees about backwards compatibility with previous of
+// execution data. The data structures and tests included are only to help inform of any breaking
+// changes.
 
 // ChunkExecutionDataV1 [deprecated] only use for backwards compatibility testing
 // was used up to block X (TODO: fill in block number after release)
