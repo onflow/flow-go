37f4d0c2399a74fba925311baccba6e97c3ac887
Gregor Gololicic <gregor.gololicic@dapperlabs.com>
2023-09-18 16:23:51 +0200

    update register interface

--- storage/registers.go
+++ storage/registers.go
@@ -6,41 +6,27 @@ import (
 
 // RegisterIndex defines methods for the register index.
 type RegisterIndex interface {
-	RegisterIndexReader
-	RegisterIndexWriter
-}
-
-// RegisterIndexReader defines read-only operations on the register index.
-type RegisterIndexReader interface {
-	// LatestHeight at which we indexed registers.
-	// Expected errors:
-	// - storage.ErrNotFound if the height was not found.
-	LatestHeight() (uint64, error)
-	// FirstHeight at which we started to index the registers.
-	// Returns the first indexed height found in the store.
-	// Expected errors:
-	// - storage.ErrNotFound if the height was not found.
-	FirstHeight() (uint64, error)
 	// Get register by the register ID at a given block height.
 	//
-	// If the register at the given height was not indexed, returns the highest height the register was indexed at.
-	// An error is returned if the register was not indexed at all or if the height is out of bounds.
+	// If the register at the given height was not indexed, returns the highest
+	// height the register was indexed at.
 	// Expected errors:
-	// - storage.ErrNotFound if the register was not found in the db or is out of bounds.
+	// - storage.ErrHeightNotIndexed if the given height was not indexed yet or lower than the first indexed height.
+	// - storage.ErrNotFound if the given height is indexed, but the register does not exist.
 	Get(ID flow.RegisterID, height uint64) (flow.RegisterValue, error)
-}
 
-// RegisterIndexWriter defines write-only operations on the register index.
-type RegisterIndexWriter interface {
+	// LatestHeight returns the latest indexed height.
+	LatestHeight() (uint64, error)
+
+	// FirstHeight at which we started to index. Returns the first indexed height found in the store.
+	FirstHeight() (uint64, error)
+
 	// Store batch of register entries at the provided block height.
-	// The provided height should either be one higher than the current height or the same to ensure idempotency.
-	// If the height is not within those bounds it will panic!
-	// Store should be used with the SetLatestHeight to progress the indexing.
-	// An error might get returned if there are problems with persisting the registers.
+	//
+	// The provided height must either be one higher than the current height or the same to ensure idempotency,
+	// otherwise and error is returned. If the height is not within those bounds there is either a bug
+	// or state corruption.
+	//
+	// No errors are expected during normal operation.
 	Store(entries flow.RegisterEntries, height uint64) error
-	// SetLatestHeight updates the latest height record.
-	// The provided height should either be one higher than the current height or the same to ensure idempotency.
-	// If the height is not within those bounds it will panic!
-	// An error might get returned if there are problems with persisting the height.
-	SetLatestHeight(height uint64) error
 }
