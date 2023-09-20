a2656be852607956e91109d0c65053abe15f73a5
Gregor Gololicic <gregor.gololicic@dapperlabs.com>
2023-09-20 10:36:06 +0200

    memory get

--- storage/memory/registers.go
+++ storage/memory/registers.go
@@ -2,8 +2,11 @@ package memory
 
 import (
 	"fmt"
+	"math"
 
 	"github.com/pkg/errors"
+	"golang.org/x/exp/maps"
+	"golang.org/x/exp/slices"
 
 	"github.com/onflow/flow-go/model/flow"
 	"github.com/onflow/flow-go/storage"
@@ -12,14 +15,14 @@ import (
 var _ storage.RegisterIndex = (*Registers)(nil)
 
 type Registers struct {
-	registers    map[uint64]map[flow.RegisterID]flow.RegisterValue
+	registers    map[flow.RegisterID]map[uint64]flow.RegisterValue
 	latestHeight uint64
 	firstHeight  uint64
 }
 
 func NewRegisters(first uint64, last uint64) *Registers {
 	return &Registers{
-		registers: make(map[uint64]map[flow.RegisterID]flow.RegisterValue),
+		registers: make(map[flow.RegisterID]map[uint64]flow.RegisterValue),
 	}
 }
 
@@ -39,25 +42,37 @@ func (r *Registers) FirstHeight() (uint64, error) {
 }
 
 func (r *Registers) Get(ID flow.RegisterID, height uint64) (flow.RegisterValue, error) {
-	h, ok := r.registers[height]
-	if !ok {
-		return nil, errors.Wrap(storage.ErrNotFound, fmt.Sprintf("height %d for registers not indexed", height))
-	}
-
-	entry, ok := h[ID]
+	reg, ok := r.registers[ID]
 	if !ok {
 		return nil, errors.Wrap(storage.ErrNotFound, fmt.Sprintf("register by ID %s not found", ID.String()))
 	}
 
-	return entry, nil
+	// get all the heights indexed for request register, then iterate over
+	// to find the highest height compared to requested height and return it.
+	indexedHeights := maps.Keys(r.registers[ID])
+	slices.Sort(indexedHeights)
+	highestHeight := uint64(math.MaxUint64)
+	for i := len(indexedHeights) - 1; i >= 0; i-- {
+		if indexedHeights[i] <= height {
+			highestHeight = indexedHeights[i]
+			break
+		}
+	}
+
+	if highestHeight == math.MaxUint64 {
+		return nil, errors.Wrap(storage.ErrNotFound, fmt.Sprintf("register at height %d or lower not found", height))
+	}
+
+	return reg[highestHeight], nil
 }
 
 func (r *Registers) Store(entries flow.RegisterEntries, height uint64) error {
-	if _, ok := r.registers[height]; !ok {
-		r.registers[height] = make(map[flow.RegisterID]flow.RegisterValue)
-	}
 	for _, e := range entries {
-		r.registers[height][e.Key] = e.Value
+		if _, ok := r.registers[e.Key]; !ok {
+			r.registers[e.Key] = make(map[uint64]flow.RegisterValue)
+		}
+
+		r.registers[e.Key][height] = e.Value
 	}
 
 	r.latestHeight = height
