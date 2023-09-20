cde4632bd0d24160ff4baee2dc40b1c1583c5b06
Leo Zhang (zhangchiqing) <zhangchiqing@gmail.com>
2023-09-19 18:37:34 -0700

    fix loader

--- engine/execution/ingestion/loader/loader.go
+++ engine/execution/ingestion/loader/loader.go
@@ -10,7 +10,6 @@ import (
 	"github.com/onflow/flow-go/model/flow"
 	"github.com/onflow/flow-go/state/protocol"
 	"github.com/onflow/flow-go/storage"
-	"github.com/onflow/flow-go/utils/logging"
 )
 
 type Loader struct {
@@ -37,99 +36,18 @@ func NewLoader(
 func (e *Loader) LoadUnexecuted(ctx context.Context) ([]flow.Identifier, error) {
 	lastExecuted := e.execState.GetHighestFinalizedExecuted()
 
-	// TODO: check dynamic bootstrapping
-
-	finalized, pending, err := e.unexecutedBlocks(lastExecuted)
-	if err != nil {
-		return nil, fmt.Errorf("could not reload unexecuted blocks: %w", err)
-	}
-
-	unexecuted := append(finalized, pending...)
-
-	log := e.log.With().
-		Int("total", len(unexecuted)).
-		Int("finalized", len(finalized)).
-		Int("pending", len(pending)).
-		Uint64("last_executed", lastExecuted).
-		Logger()
-
-	log.Info().Msg("reloading unexecuted blocks")
-
-	return unexecuted, nil
-}
-
-func (e *Loader) unexecutedBlocks(ctx context.Context) (
-	finalized []flow.Identifier,
-	pending []flow.Identifier,
-	err error,
-) {
-	// pin the snapshot so that finalizedUnexecutedBlocks and pendingUnexecutedBlocks are based
-	// on the same snapshot.
-	snapshot := e.state.Final()
-
-	finalized, err = e.finalizedUnexecutedBlocks(ctx, snapshot)
-	if err != nil {
-		return nil, nil, fmt.Errorf("could not read finalized unexecuted blocks")
-	}
-
-	pending, err = e.pendingUnexecutedBlocks(ctx, snapshot)
-	if err != nil {
-		return nil, nil, fmt.Errorf("could not read pending unexecuted blocks")
-	}
-
-	return finalized, pending, nil
-}
-
-func (e *Loader) finalizedUnexecutedBlocks(ctx context.Context, finalized protocol.Snapshot) (
-	[]flow.Identifier,
-	error,
-) {
 	// get finalized height
-	final, err := finalized.Head()
+	final, err := e.state.Final().Head()
 	if err != nil {
 		return nil, fmt.Errorf("could not get finalized block: %w", err)
 	}
 
-	// find the first unexecuted and finalized block
-	// We iterate from the last finalized, check if it has been executed,
-	// if not, keep going to the lower height, until we find an executed
-	// block, and then the next height is the first unexecuted.
-	// If there is only one finalized, and it's executed (i.e. root block),
-	// then the firstUnexecuted is a unfinalized block, which is ok,
-	// because the next loop will ensure it only iterates through finalized
-	// blocks.
-	lastExecuted := final.Height
-
-	// dynamically bootstrapped execution node will reload blocks from
-	// [sealedRoot.Height + 1, finalizedRoot.Height] and execute them on startup.
-	rootBlock, err := e.state.Params().SealedRoot()
-	if err != nil {
-		return nil, fmt.Errorf("failed to retrieve root block: %w", err)
-	}
-
-	for ; lastExecuted > rootBlock.Height; lastExecuted-- {
-		header, err := e.getHeaderByHeight(lastExecuted)
-		if err != nil {
-			return nil, fmt.Errorf("could not get header at height: %v, %w", lastExecuted, err)
-		}
-
-		executed, err := state.IsBlockExecuted(ctx, e.execState, header.ID())
-		if err != nil {
-			return nil, fmt.Errorf("could not check whether block is executed: %w", err)
-		}
-
-		if executed {
-			break
-		}
-	}
-
-	firstUnexecuted := lastExecuted + 1
-
+	// TODO: dynamically bootstrapped execution node will reload blocks from
 	unexecuted := make([]flow.Identifier, 0)
 
 	// starting from the first unexecuted block, go through each unexecuted and finalized block
 	// reload its block to execution queues
-	for height := firstUnexecuted; height <= final.Height; height++ {
+	for height := lastExecuted + 1; height <= final.Height; height++ {
 		header, err := e.getHeaderByHeight(height)
 		if err != nil {
 			return nil, fmt.Errorf("could not get header at height: %v, %w", height, err)
@@ -141,36 +59,10 @@ func (e *Loader) finalizedUnexecutedBlocks(ctx context.Context, finalized protoc
 	e.log.Info().
 		Uint64("last_finalized", final.Height).
 		Uint64("last_finalized_executed", lastExecuted).
-		Uint64("sealed_root_height", rootBlock.Height).
-		Hex("sealed_root_id", logging.Entity(rootBlock)).
-		Uint64("first_unexecuted", firstUnexecuted).
+		// Uint64("sealed_root_height", rootBlock.Height).
+		// Hex("sealed_root_id", logging.Entity(rootBlock)).
 		Int("total_finalized_unexecuted", len(unexecuted)).
 		Msgf("finalized unexecuted blocks")
-
-	return unexecuted, nil
-}
-
-func (e *Loader) pendingUnexecutedBlocks(ctx context.Context, finalized protocol.Snapshot) (
-	[]flow.Identifier,
-	error,
-) {
-	pendings, err := finalized.Descendants()
-	if err != nil {
-		return nil, fmt.Errorf("could not get pending blocks: %w", err)
-	}
-
-	unexecuted := make([]flow.Identifier, 0)
-
-	for _, pending := range pendings {
-		executed, err := state.IsBlockExecuted(ctx, e.execState, pending)
-		if err != nil {
-			return nil, fmt.Errorf("could not check block executed or not: %w", err)
-		}
-
-		if !executed {
-			unexecuted = append(unexecuted, pending)
-		}
-	}
 
 	return unexecuted, nil
 }
