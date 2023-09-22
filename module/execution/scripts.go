c426cde8df07034e8952b268641d4a0ff82d6a2c
Gregor Gololicic <gregor.gololicic@dapperlabs.com>
2023-09-22 12:39:28 +0200

    script storage snapshot

--- module/execution/scripts.go
+++ module/execution/scripts.go
@@ -2,14 +2,33 @@ package execution
 
 import (
 	"context"
+	"fmt"
 
 	"github.com/onflow/flow-go/engine/execution/computation/query"
+	"github.com/onflow/flow-go/fvm/storage/snapshot"
+	"github.com/onflow/flow-go/model/flow"
 	"github.com/onflow/flow-go/storage"
 )
 
+// RegistersAtHeight returns register values for provided register IDs at the block height.
+// Even if the register wasn't indexed at the provided height, returns the highest height the register was indexed at.
+// Expected errors:
+// - storage.ErrNotFound if the register by the ID was never indexed
+// - ErrIndexBoundary if the height is out of indexed height boundary
+type RegistersAtHeight func(IDs flow.RegisterIDs, height uint64) ([]flow.RegisterValue, error)
+
 type Scripts struct {
-	executor query.QueryExecutor
-	headers  storage.Headers
+	executor          query.QueryExecutor
+	headers           storage.Headers
+	registersAtHeight RegistersAtHeight
+}
+
+func NewScripts(executor query.QueryExecutor, header storage.Headers, registersAtHeight RegistersAtHeight) *Scripts {
+	return &Scripts{
+		executor:          executor,
+		headers:           header,
+		registersAtHeight: registersAtHeight,
+	}
 }
 
 // ExecuteAtBlockHeight executes provided script against the block height.
@@ -28,5 +47,16 @@ func (s *Scripts) ExecuteAtBlockHeight(
 		return nil, err
 	}
 
-	return s.executor.ExecuteScript(ctx, script, arguments, header, snapshot)
+	storageSnapshot := snapshot.NewReadFuncStorageSnapshot(func(ID flow.RegisterID) (flow.RegisterValue, error) {
+		values, err := s.registersAtHeight(flow.RegisterIDs{ID}, height)
+		if err != nil {
+			return nil, err
+		}
+		if len(values) > 1 {
+			return nil, fmt.Errorf("multiple values returned for a single register")
+		}
+		return values[0], nil
+	})
+
+	return s.executor.ExecuteScript(ctx, script, arguments, header, storageSnapshot)
 }
