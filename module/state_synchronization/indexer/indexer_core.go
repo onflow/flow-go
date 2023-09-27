801124e142454f643f8d38495606bda792158697
Gregor Gololicic <gregor.gololicic@dapperlabs.com>
2023-09-27 21:21:39 +0200

    remove error

--- module/state_synchronization/indexer/indexer_core.go
+++ module/state_synchronization/indexer/indexer_core.go
@@ -62,7 +62,6 @@ func (c *IndexerCore) RegisterValues(IDs flow.RegisterIDs, height uint64) ([]flo
 // IndexBlockData indexes all execution block data by height.
 // This method shouldn't be used concurrently.
 // Expected errors:
-// - ErrIndexValue if height was not incremented in sequence
 // - storage.ErrNotFound if the block for execution data was not found
 func (c *IndexerCore) IndexBlockData(data *execution_data.BlockExecutionDataEntity) error {
 	block, err := c.headers.ByBlockID(data.BlockID)
