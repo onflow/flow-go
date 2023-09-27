fb52463c5483a6b3165a72761da660766b6a9380
Gregor Gololicic <gregor.gololicic@dapperlabs.com>
2023-09-27 20:29:34 +0200

    remove fmt

--- module/state_synchronization/indexer/indexer_core.go
+++ module/state_synchronization/indexer/indexer_core.go
@@ -67,7 +67,6 @@ func (i *IndexerCore) RegisterValues(IDs flow.RegisterIDs, height uint64) ([]flo
 // - ErrIndexValue if height was not incremented in sequence
 // - storage.ErrNotFound if the block for execution data was not found
 func (i *IndexerCore) IndexBlockData(_ context.Context, data *execution_data.BlockExecutionDataEntity) error {
-	fmt.Println("index block data", data.BlockID)
 	block, err := i.headers.ByBlockID(data.BlockID)
 	if err != nil {
 		return fmt.Errorf("could not get the block by ID %s: %w", data.BlockID, err)
