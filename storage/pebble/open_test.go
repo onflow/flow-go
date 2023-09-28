9979dbf273ab106c7d30b017b2e71d73e4cd98e1
Leo Zhang (zhangchiqing) <zhangchiqing@gmail.com>
2023-09-28 12:05:02 -0700

    refactor tests

--- storage/pebble/open_test.go
+++ storage/pebble/open_test.go
@@ -2,7 +2,6 @@ package pebble
 
 import (
 	"errors"
-	"os"
 	"testing"
 
 	"github.com/stretchr/testify/require"
@@ -12,68 +11,65 @@ import (
 
 func TestIsDBNotBootstrapped(t *testing.T) {
 	t.Parallel()
-	dir := unittest.TempPebblePath(t)
-	db, err := OpenRegisterPebbleDB(dir)
-	require.NoError(t, err)
-	notBootstrapped, err := IsDBNotBootstrapped(db)
-	require.NoError(t, err)
-	require.True(t, notBootstrapped)
-	require.NoError(t, db.Close())
-	err = os.RemoveAll(dir)
-	require.NoError(t, err)
+	unittest.RunWithTempDir(t, func(dir string) {
+		db, err := OpenRegisterPebbleDB(dir)
+		require.NoError(t, err)
+		notBootstrapped, err := IsDBNotBootstrapped(db)
+		require.NoError(t, err)
+		require.True(t, notBootstrapped)
+		require.NoError(t, db.Close())
+	})
 }
 
 func TestReadHeightsFromBootstrappedDB(t *testing.T) {
 	t.Parallel()
-	dir := unittest.TempPebblePath(t)
-	db, err := OpenRegisterPebbleDB(dir)
-	require.NoError(t, err)
+	unittest.RunWithTempDir(t, func(dir string) {
+		db, err := OpenRegisterPebbleDB(dir)
+		require.NoError(t, err)
 
-	// init with first height
-	firstHeight := uint64(10)
-	require.NoError(t, initHeights(db, firstHeight))
+		// init with first height
+		firstHeight := uint64(10)
+		require.NoError(t, initHeights(db, firstHeight))
 
-	notBootstrapped, err := IsDBNotBootstrapped(db)
-	require.NoError(t, err)
+		notBootstrapped, err := IsDBNotBootstrapped(db)
+		require.NoError(t, err)
 
-	require.False(t, notBootstrapped)
-	require.NoError(t, db.Close())
+		require.False(t, notBootstrapped)
+		require.NoError(t, db.Close())
 
-	// reopen the db
-	registers, db, err := NewBootstrappedRegistersWithPath(dir)
-	require.NoError(t, err)
+		// reopen the db
+		registers, db, err := NewBootstrappedRegistersWithPath(dir)
+		require.NoError(t, err)
 
-	require.Equal(t, firstHeight, registers.FirstHeight())
-	require.Equal(t, firstHeight, registers.LatestHeight())
+		require.Equal(t, firstHeight, registers.FirstHeight())
+		require.Equal(t, firstHeight, registers.LatestHeight())
 
-	require.NoError(t, db.Close())
-	err = os.RemoveAll(dir)
-	require.NoError(t, err)
+		require.NoError(t, db.Close())
+	})
 }
 
 func TestNewBootstrappedRegistersWithPath(t *testing.T) {
 	t.Parallel()
-	dir := unittest.TempPebblePath(t)
-	_, db, err := NewBootstrappedRegistersWithPath(dir)
-	require.Error(t, err)
-	require.True(t, errors.Is(err, ErrNotBootstrapped))
+	unittest.RunWithTempDir(t, func(dir string) {
+		_, db, err := NewBootstrappedRegistersWithPath(dir)
+		require.Error(t, err)
+		require.True(t, errors.Is(err, ErrNotBootstrapped))
 
-	// verify the db is closed
-	require.True(t, db == nil)
+		// verify the db is closed
+		require.True(t, db == nil)
 
-	// bootstrap the db
-	// init with first height
-	db2, err := OpenRegisterPebbleDB(dir)
-	require.NoError(t, err)
-	firstHeight := uint64(10)
-	require.NoError(t, initHeights(db2, firstHeight))
+		// bootstrap the db
+		// init with first height
+		db2, err := OpenRegisterPebbleDB(dir)
+		require.NoError(t, err)
+		firstHeight := uint64(10)
+		require.NoError(t, initHeights(db2, firstHeight))
 
-	registers, err := NewRegisters(db2)
-	require.NoError(t, err)
-	require.Equal(t, firstHeight, registers.FirstHeight())
-	require.Equal(t, firstHeight, registers.LatestHeight())
+		registers, err := NewRegisters(db2)
+		require.NoError(t, err)
+		require.Equal(t, firstHeight, registers.FirstHeight())
+		require.Equal(t, firstHeight, registers.LatestHeight())
 
-	require.NoError(t, db2.Close())
-	err = os.RemoveAll(dir)
-	require.NoError(t, err)
+		require.NoError(t, db2.Close())
+	})
 }
