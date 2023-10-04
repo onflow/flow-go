ca7798ad76d1d0fcc8bd0b53825f8d9e21409ad2
Janez Podhostnik <janez.podhostnik@gmail.com>
2023-10-04 17:19:43 +0200

    print value type

--- cmd/util/ledger/migrations/cricket_moments.go
+++ cmd/util/ledger/migrations/cricket_moments.go
@@ -316,6 +316,12 @@ func cloneCricketMomentsShardedCollection(
 			return nil, fmt.Errorf("failed to get nft collection: %w", err)
 		}
 
+		fmt.Printf("ownedNFTs value type %s\n"+
+			"cloned value type %s\n",
+			ownedNFTs.Type.ValueType.String(),
+			clonedValue.value.StaticType(mr.Interpreter).String(),
+		)
+
 		err = capturePanic(func() {
 			ownedNFTs.Insert(
 				mr.Interpreter,
