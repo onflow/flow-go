95f7a0b2e1f96d21934d1f6a9d2159fd951d3c1d
Peter Argue <89119817+peterargue@users.noreply.github.com>
2023-10-05 14:56:17 -0700

    uncomment test file

--- engine/access/state_stream/handler_test.go
+++ engine/access/state_stream/handler_test.go
@@ -1,230 +1,234 @@
 package state_stream_test
 
-// import (
-// 	"context"
-// 	"io"
-// 	"sync"
-// 	"testing"
-// 	"time"
+import (
+	"context"
+	"io"
+	"sync"
+	"testing"
+	"time"
 
-// 	"github.com/stretchr/testify/assert"
-// 	"github.com/stretchr/testify/mock"
-// 	"github.com/stretchr/testify/require"
-// 	"google.golang.org/grpc"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/mock"
+	"github.com/stretchr/testify/require"
+	"google.golang.org/grpc"
 
-// 	jsoncdc "github.com/onflow/cadence/encoding/json"
-// 	access "github.com/onflow/flow/protobuf/go/flow/executiondata"
+	jsoncdc "github.com/onflow/cadence/encoding/json"
+	access "github.com/onflow/flow/protobuf/go/flow/executiondata"
 
-// 	"github.com/onflow/flow-go/engine/access/state_stream"
-// 	ssmock "github.com/onflow/flow-go/engine/access/state_stream/mock"
-// 	"github.com/onflow/flow-go/engine/common/rpc/convert"
-// 	"github.com/onflow/flow-go/model/flow"
-// 	"github.com/onflow/flow-go/utils/unittest"
-// 	"github.com/onflow/flow-go/utils/unittest/generator"
-// )
+	"github.com/onflow/flow-go/engine/access/state_stream"
+	ssmock "github.com/onflow/flow-go/engine/access/state_stream/mock"
+	"github.com/onflow/flow-go/engine/common/rpc/convert"
+	"github.com/onflow/flow-go/model/flow"
+	"github.com/onflow/flow-go/utils/unittest"
+	"github.com/onflow/flow-go/utils/unittest/generator"
+)
 
-// func TestExecutionDataStream(t *testing.T) {
-// 	ctx, cancel := context.WithCancel(context.Background())
-// 	defer cancel()
+func TestExecutionDataStream(t *testing.T) {
+	t.Parallel()
 
-// 	api := ssmock.NewAPI(t)
-// 	stream := makeStreamMock[access.SubscribeExecutionDataRequest, access.SubscribeExecutionDataResponse](ctx)
-// 	sub := state_stream.NewSubscription(1)
+	ctx, cancel := context.WithCancel(context.Background())
+	defer cancel()
 
-// 	// generate some events with a payload to include
-// 	// generators will produce identical event payloads (before encoding)
-// 	ccfEventGenerator := generator.EventGenerator(generator.WithEncoding(generator.EncodingCCF))
-// 	jsonEventsGenerator := generator.EventGenerator(generator.WithEncoding(generator.EncodingJSON))
-// 	inputEvents := make([]flow.Event, 0, 3)
-// 	expectedEvents := make([]flow.Event, 0, 3)
-// 	for i := 0; i < 3; i++ {
-// 		inputEvents = append(inputEvents, ccfEventGenerator.New())
-// 		expectedEvents = append(expectedEvents, jsonEventsGenerator.New())
-// 	}
+	api := ssmock.NewAPI(t)
+	stream := makeStreamMock[access.SubscribeExecutionDataRequest, access.SubscribeExecutionDataResponse](ctx)
+	sub := state_stream.NewSubscription(1)
 
-// 	api.On("SubscribeExecutionData", mock.Anything, flow.ZeroID, uint64(0), mock.Anything).Return(sub)
+	// generate some events with a payload to include
+	// generators will produce identical event payloads (before encoding)
+	ccfEventGenerator := generator.EventGenerator(generator.WithEncoding(generator.EncodingCCF))
+	jsonEventsGenerator := generator.EventGenerator(generator.WithEncoding(generator.EncodingJSON))
+	inputEvents := make([]flow.Event, 0, 3)
+	expectedEvents := make([]flow.Event, 0, 3)
+	for i := 0; i < 3; i++ {
+		inputEvents = append(inputEvents, ccfEventGenerator.New())
+		expectedEvents = append(expectedEvents, jsonEventsGenerator.New())
+	}
 
-// 	h := state_stream.NewHandler(api, flow.Localnet.Chain(), state_stream.EventFilterConfig{}, 1)
+	api.On("SubscribeExecutionData", mock.Anything, flow.ZeroID, uint64(0), mock.Anything).Return(sub)
 
-// 	wg := sync.WaitGroup{}
-// 	wg.Add(1)
-// 	go func() {
-// 		wg.Done()
-// 		err := h.SubscribeExecutionData(&access.SubscribeExecutionDataRequest{}, stream)
-// 		require.NoError(t, err)
-// 		t.Log("subscription closed")
-// 	}()
-// 	wg.Wait()
+	h := state_stream.NewHandler(api, flow.Localnet.Chain(), state_stream.EventFilterConfig{}, 1)
 
-// 	// send a single response
-// 	blockHeight := uint64(1)
-// 	executionData := unittest.BlockExecutionDataFixture(
-// 		unittest.WithChunkExecutionDatas(
-// 			unittest.ChunkExecutionDataFixture(t, 1024, unittest.WithChunkEvents(inputEvents)),
-// 			unittest.ChunkExecutionDataFixture(t, 1024, unittest.WithChunkEvents(inputEvents)),
-// 		),
-// 	)
+	wg := sync.WaitGroup{}
+	wg.Add(1)
+	go func() {
+		wg.Done()
+		err := h.SubscribeExecutionData(&access.SubscribeExecutionDataRequest{}, stream)
+		require.NoError(t, err)
+		t.Log("subscription closed")
+	}()
+	wg.Wait()
 
-// 	err := sub.Send(ctx, &state_stream.ExecutionDataResponse{
-// 		Height:        blockHeight,
-// 		ExecutionData: executionData,
-// 	}, 100*time.Millisecond)
-// 	require.NoError(t, err)
+	// send a single response
+	blockHeight := uint64(1)
+	executionData := unittest.BlockExecutionDataFixture(
+		unittest.WithChunkExecutionDatas(
+			unittest.ChunkExecutionDataFixture(t, 1024, unittest.WithChunkEvents(inputEvents)),
+			unittest.ChunkExecutionDataFixture(t, 1024, unittest.WithChunkEvents(inputEvents)),
+		),
+	)
 
-// 	// notify end of data
-// 	sub.Close()
+	err := sub.Send(ctx, &state_stream.ExecutionDataResponse{
+		Height:        blockHeight,
+		ExecutionData: executionData,
+	}, 100*time.Millisecond)
+	require.NoError(t, err)
 
-// 	receivedCount := 0
-// 	for {
-// 		t.Log(receivedCount)
-// 		resp, err := stream.RecvToClient()
-// 		if err == io.EOF {
-// 			break
-// 		}
-// 		require.NoError(t, err)
+	// notify end of data
+	sub.Close()
 
-// 		convertedExecData, err := convert.MessageToBlockExecutionData(resp.GetBlockExecutionData(), flow.Testnet.Chain())
-// 		require.NoError(t, err)
+	receivedCount := 0
+	for {
+		t.Log(receivedCount)
+		resp, err := stream.RecvToClient()
+		if err == io.EOF {
+			break
+		}
+		require.NoError(t, err)
 
-// 		assert.Equal(t, blockHeight, resp.GetBlockHeight())
+		convertedExecData, err := convert.MessageToBlockExecutionData(resp.GetBlockExecutionData(), flow.Testnet.Chain())
+		require.NoError(t, err)
 
-// 		// make sure the payload is valid JSON-CDC
-// 		for _, chunk := range convertedExecData.ChunkExecutionDatas {
-// 			for i, e := range chunk.Events {
-// 				assert.Equal(t, expectedEvents[i], e)
+		assert.Equal(t, blockHeight, resp.GetBlockHeight())
 
-// 				_, err := jsoncdc.Decode(nil, e.Payload)
-// 				require.NoError(t, err)
-// 			}
-// 		}
+		// make sure the payload is valid JSON-CDC
+		for _, chunk := range convertedExecData.ChunkExecutionDatas {
+			for i, e := range chunk.Events {
+				assert.Equal(t, expectedEvents[i], e)
 
-// 		receivedCount++
+				_, err := jsoncdc.Decode(nil, e.Payload)
+				require.NoError(t, err)
+			}
+		}
 
-// 		// shutdown the stream after one response
-// 		close(stream.sentFromServer)
-// 	}
+		receivedCount++
 
-// 	// only expect a single response
-// 	assert.Equal(t, 1, receivedCount)
-// }
+		// shutdown the stream after one response
+		close(stream.sentFromServer)
+	}
 
-// func TestEventStream(t *testing.T) {
-// 	ctx, cancel := context.WithCancel(context.Background())
-// 	defer cancel()
+	// only expect a single response
+	assert.Equal(t, 1, receivedCount)
+}
 
-// 	api := ssmock.NewAPI(t)
-// 	stream := makeStreamMock[access.SubscribeEventsRequest, access.SubscribeEventsResponse](ctx)
-// 	sub := state_stream.NewSubscription(1)
+func TestEventStream(t *testing.T) {
+	t.Parallel()
 
-// 	// generate some events with a payload to include
-// 	// generators will produce identical event payloads (before encoding)
-// 	ccfEventGenerator := generator.EventGenerator(generator.WithEncoding(generator.EncodingCCF))
-// 	jsonEventsGenerator := generator.EventGenerator(generator.WithEncoding(generator.EncodingJSON))
-// 	inputEvents := make([]flow.Event, 0, 3)
-// 	expectedEvents := make([]flow.Event, 0, 3)
-// 	for i := 0; i < 3; i++ {
-// 		inputEvents = append(inputEvents, ccfEventGenerator.New())
-// 		expectedEvents = append(expectedEvents, jsonEventsGenerator.New())
-// 	}
+	ctx, cancel := context.WithCancel(context.Background())
+	defer cancel()
 
-// 	api.On("SubscribeEvents", mock.Anything, flow.ZeroID, uint64(0), mock.Anything).Return(sub)
+	api := ssmock.NewAPI(t)
+	stream := makeStreamMock[access.SubscribeEventsRequest, access.SubscribeEventsResponse](ctx)
+	sub := state_stream.NewSubscription(1)
 
-// 	h := state_stream.NewHandler(api, flow.Localnet.Chain(), state_stream.EventFilterConfig{}, 1)
+	// generate some events with a payload to include
+	// generators will produce identical event payloads (before encoding)
+	ccfEventGenerator := generator.EventGenerator(generator.WithEncoding(generator.EncodingCCF))
+	jsonEventsGenerator := generator.EventGenerator(generator.WithEncoding(generator.EncodingJSON))
+	inputEvents := make([]flow.Event, 0, 3)
+	expectedEvents := make([]flow.Event, 0, 3)
+	for i := 0; i < 3; i++ {
+		inputEvents = append(inputEvents, ccfEventGenerator.New())
+		expectedEvents = append(expectedEvents, jsonEventsGenerator.New())
+	}
 
-// 	wg := sync.WaitGroup{}
-// 	wg.Add(1)
-// 	go func() {
-// 		wg.Done()
-// 		err := h.SubscribeEvents(&access.SubscribeEventsRequest{}, stream)
-// 		require.NoError(t, err)
-// 		t.Log("subscription closed")
-// 	}()
-// 	wg.Wait()
+	api.On("SubscribeEvents", mock.Anything, flow.ZeroID, uint64(0), mock.Anything).Return(sub)
 
-// 	// send a single response
-// 	blockHeight := uint64(1)
-// 	blockID := unittest.IdentifierFixture()
-// 	err := sub.Send(ctx, &state_stream.EventsResponse{
-// 		BlockID: blockID,
-// 		Height:  blockHeight,
-// 		Events:  inputEvents,
-// 	}, 100*time.Millisecond)
-// 	require.NoError(t, err)
+	h := state_stream.NewHandler(api, flow.Localnet.Chain(), state_stream.EventFilterConfig{}, 1)
 
-// 	// notify end of data
-// 	sub.Close()
+	wg := sync.WaitGroup{}
+	wg.Add(1)
+	go func() {
+		wg.Done()
+		err := h.SubscribeEvents(&access.SubscribeEventsRequest{}, stream)
+		require.NoError(t, err)
+		t.Log("subscription closed")
+	}()
+	wg.Wait()
 
-// 	receivedCount := 0
-// 	for {
-// 		t.Log(receivedCount)
-// 		resp, err := stream.RecvToClient()
-// 		if err == io.EOF {
-// 			break
-// 		}
-// 		require.NoError(t, err)
+	// send a single response
+	blockHeight := uint64(1)
+	blockID := unittest.IdentifierFixture()
+	err := sub.Send(ctx, &state_stream.EventsResponse{
+		BlockID: blockID,
+		Height:  blockHeight,
+		Events:  inputEvents,
+	}, 100*time.Millisecond)
+	require.NoError(t, err)
 
-// 		convertedEvents := convert.MessagesToEvents(resp.GetEvents())
+	// notify end of data
+	sub.Close()
 
-// 		assert.Equal(t, blockHeight, resp.GetBlockHeight())
-// 		assert.Equal(t, blockID, convert.MessageToIdentifier(resp.GetBlockId()))
-// 		assert.Equal(t, expectedEvents, convertedEvents)
+	receivedCount := 0
+	for {
+		t.Log(receivedCount)
+		resp, err := stream.RecvToClient()
+		if err == io.EOF {
+			break
+		}
+		require.NoError(t, err)
 
-// 		// make sure the payload is valid JSON-CDC
-// 		for _, e := range convertedEvents {
-// 			_, err := jsoncdc.Decode(nil, e.Payload)
-// 			require.NoError(t, err)
-// 		}
+		convertedEvents := convert.MessagesToEvents(resp.GetEvents())
 
-// 		receivedCount++
+		assert.Equal(t, blockHeight, resp.GetBlockHeight())
+		assert.Equal(t, blockID, convert.MessageToIdentifier(resp.GetBlockId()))
+		assert.Equal(t, expectedEvents, convertedEvents)
 
-// 		// shutdown the stream after one response
-// 		close(stream.sentFromServer)
-// 	}
+		// make sure the payload is valid JSON-CDC
+		for _, e := range convertedEvents {
+			_, err := jsoncdc.Decode(nil, e.Payload)
+			require.NoError(t, err)
+		}
 
-// 	// only expect a single response
-// 	assert.Equal(t, 1, receivedCount)
-// }
+		receivedCount++
 
-// func makeStreamMock[R, T any](ctx context.Context) *StreamMock[R, T] {
-// 	return &StreamMock[R, T]{
-// 		ctx:            ctx,
-// 		recvToServer:   make(chan *R, 10),
-// 		sentFromServer: make(chan *T, 10),
-// 	}
-// }
+		// shutdown the stream after one response
+		close(stream.sentFromServer)
+	}
 
-// type StreamMock[R, T any] struct {
-// 	grpc.ServerStream
-// 	ctx            context.Context
-// 	recvToServer   chan *R
-// 	sentFromServer chan *T
-// }
+	// only expect a single response
+	assert.Equal(t, 1, receivedCount)
+}
 
-// func (m *StreamMock[R, T]) Context() context.Context {
-// 	return m.ctx
-// }
-// func (m *StreamMock[R, T]) Send(resp *T) error {
-// 	m.sentFromServer <- resp
-// 	return nil
-// }
+func makeStreamMock[R, T any](ctx context.Context) *StreamMock[R, T] {
+	return &StreamMock[R, T]{
+		ctx:            ctx,
+		recvToServer:   make(chan *R, 10),
+		sentFromServer: make(chan *T, 10),
+	}
+}
 
-// func (m *StreamMock[R, T]) Recv() (*R, error) {
-// 	req, more := <-m.recvToServer
-// 	if !more {
-// 		return nil, io.EOF
-// 	}
-// 	return req, nil
-// }
+type StreamMock[R, T any] struct {
+	grpc.ServerStream
+	ctx            context.Context
+	recvToServer   chan *R
+	sentFromServer chan *T
+}
 
-// func (m *StreamMock[R, T]) SendFromClient(req *R) error {
-// 	m.recvToServer <- req
-// 	return nil
-// }
+func (m *StreamMock[R, T]) Context() context.Context {
+	return m.ctx
+}
+func (m *StreamMock[R, T]) Send(resp *T) error {
+	m.sentFromServer <- resp
+	return nil
+}
 
-// func (m *StreamMock[R, T]) RecvToClient() (*T, error) {
-// 	response, more := <-m.sentFromServer
-// 	if !more {
-// 		return nil, io.EOF
-// 	}
-// 	return response, nil
-// }
+func (m *StreamMock[R, T]) Recv() (*R, error) {
+	req, more := <-m.recvToServer
+	if !more {
+		return nil, io.EOF
+	}
+	return req, nil
+}
+
+func (m *StreamMock[R, T]) SendFromClient(req *R) error {
+	m.recvToServer <- req
+	return nil
+}
+
+func (m *StreamMock[R, T]) RecvToClient() (*T, error) {
+	response, more := <-m.sentFromServer
+	if !more {
+		return nil, io.EOF
+	}
+	return response, nil
+}
