9beeb8c2d8f0fdd805929774563a90b1cacd7221
Gregor Gololicic <gregor.gololicic@dapperlabs.com>
2023-10-23 16:54:25 +0200

    rewrite test suite

--- module/execution/scripts_test.go
+++ module/execution/scripts_test.go
@@ -3,6 +3,7 @@ package execution
 import (
 	"context"
 	"fmt"
+	"os"
 	"strings"
 	"testing"
 
@@ -10,9 +11,8 @@ import (
 	"github.com/onflow/cadence/encoding/ccf"
 	jsoncdc "github.com/onflow/cadence/encoding/json"
 	"github.com/rs/zerolog"
-	"github.com/stretchr/testify/assert"
 	mocks "github.com/stretchr/testify/mock"
-	"github.com/stretchr/testify/require"
+	"github.com/stretchr/testify/suite"
 
 	"github.com/onflow/flow-go/engine/execution/computation/query/mock"
 	"github.com/onflow/flow-go/engine/execution/testutil"
@@ -20,207 +20,169 @@ import (
 	"github.com/onflow/flow-go/fvm/storage/snapshot"
 	"github.com/onflow/flow-go/model/flow"
 	"github.com/onflow/flow-go/module/metrics"
+	"github.com/onflow/flow-go/module/state_synchronization/indexer"
 	synctest "github.com/onflow/flow-go/module/state_synchronization/requester/unittest"
-	"github.com/onflow/flow-go/module/trace"
 	"github.com/onflow/flow-go/storage"
 	pebbleStorage "github.com/onflow/flow-go/storage/pebble"
 	"github.com/onflow/flow-go/utils/unittest"
 )
 
-var (
-	chain     = flow.Emulator.Chain()
-	collector = metrics.NewExecutionCollector(&trace.NoopTracer{})
-)
+func TestScripts(t *testing.T) {
+	suite.Run(t, new(scriptTestSuite))
+}
 
-func Test_ExecuteScript(t *testing.T) {
-	t.Run("Simple Script Execution", func(t *testing.T) {
-		blockchain := unittest.BlockchainFixture(10)
-		first := blockchain[0]
-		tree := bootstrapFVM(t)
+type scriptTestSuite struct {
+	suite.Suite
+	scripts       *Scripts
+	registerIndex storage.RegisterIndex
+	vm            *fvm.VirtualMachine
+	vmCtx         fvm.Context
+	chain         flow.Chain
+	height        uint64
+	snapshot      snapshot.SnapshotTree
+	dbDir         string
+}
 
-		scripts := newScripts(
-			t,
-			newBlockHeadersStorage(blockchain),
-			treeToRegisterAdapter(tree),
-		)
-
+func (s *scriptTestSuite) TestScriptExecution() {
+	s.Run("Simple Script Execution", func() {
 		number := int64(42)
 		code := []byte(fmt.Sprintf("pub fun main(): Int { return %d; }", number))
 
-		result, err := scripts.ExecuteAtBlockHeight(context.Background(), code, nil, first.Header.Height)
-		require.NoError(t, err)
-		value, err := jsoncdc.Decode(nil, result)
-		require.NoError(t, err)
-		assert.Equal(t, number, value.(cadence.Int).Value.Int64())
+		result, err := s.scripts.ExecuteAtBlockHeight(context.Background(), code, nil, s.height)
+		s.Require().NoError(err)
+		val, err := jsoncdc.Decode(nil, result)
+		s.Require().NoError(err)
+		s.Assert().Equal(number, val.(cadence.Int).Value.Int64())
 	})
 
-	t.Run("Handle not found Register", func(t *testing.T) {
-		blockchain := unittest.BlockchainFixture(10)
-		first := blockchain[0]
-		scripts := newScripts(
-			t,
-			newBlockHeadersStorage(blockchain),
-			IndexRegisterAdapter(
-				func(IDs flow.RegisterIDs, height uint64) ([]flow.RegisterValue, error) {
-					return nil, nil // intentionally return nil to check edge case
-				}),
+	s.Run("Handle Not Found Register", func() {
+		s.scripts.registersAtHeight = IndexRegisterAdapter( // using the adapter checks the error handling in the adapter
+			func(IDs flow.RegisterIDs, height uint64) ([]flow.RegisterValue, error) {
+				return nil, nil // intentionally return nil to check edge case
+			},
 		)
 
 		// use a non-existing address to trigger register get function
 		code := []byte("import Foo from 0x01; pub fun main() { }")
 
-		result, err := scripts.ExecuteAtBlockHeight(context.Background(), code, nil, first.Header.Height)
-		require.True(t, strings.Contains(err.Error(), "invalid number of returned values for a single register"))
-		require.Nil(t, result)
+		result, err := s.scripts.ExecuteAtBlockHeight(context.Background(), code, nil, s.height)
+		s.Assert().True(strings.Contains(err.Error(), "invalid number of returned values for a single register"))
+		s.Assert().Nil(result)
 	})
 }
 
-func Test_GetAccount(t *testing.T) {
-	blockchain := unittest.BlockchainFixture(10)
-	first := blockchain[0]
-	tree := bootstrapFVM(t)
+func (s *scriptTestSuite) TestGetAccount() {
+	s.Run("Get Service Account", func() {
+		address := s.chain.ServiceAddress()
+		account, err := s.scripts.GetAccountAtBlockHeight(context.Background(), address, s.height)
+		s.Require().NoError(err)
+		s.Assert().Equal(address, account.Address)
+		s.Assert().NotZero(account.Balance)
+		s.Assert().NotZero(len(account.Contracts))
+	})
 
-	scripts := newScripts(
-		t,
-		newBlockHeadersStorage(blockchain),
-		treeToRegisterAdapter(tree),
-	)
-
-	address := chain.ServiceAddress()
-	account, err := scripts.GetAccountAtBlockHeight(context.Background(), address, first.Header.Height)
-	require.NoError(t, err)
-	assert.Equal(t, address, account.Address)
-	assert.NotZero(t, account.Balance)
-	assert.NotZero(t, len(account.Contracts))
+	s.Run("Get New Account", func() {
+		address := s.createAccount()
+		account, err := s.scripts.GetAccountAtBlockHeight(context.Background(), address, s.height)
+		s.Require().NoError(err)
+		s.Require().Equal(address, account.Address)
+	})
 }
 
-func Test_NewlyCreatedAccount(t *testing.T) {
-	blockchain := unittest.BlockchainFixture(10)
-	first := blockchain[0]
-	tree := bootstrapFVM(t)
-	tree, _, newAddress := createAccount(t, tree)
-
-	scripts := newScripts(
-		t,
-		newBlockHeadersStorage(blockchain),
-		treeToRegisterAdapter(tree),
-	)
-
-	account, err := scripts.GetAccountAtBlockHeight(context.Background(), newAddress, first.Header.Height)
-	require.NoError(t, err)
-	assert.Equal(t, newAddress, account.Address)
-}
-
-func Test_IntegrationStorage(t *testing.T) {
+func (s *scriptTestSuite) SetupTest() {
+	logger := unittest.LoggerForTest(s.Suite.T(), zerolog.InfoLevel)
 	entropyProvider := testutil.EntropyProviderFixture(nil)
-	entropyBlock := mock.NewEntropyProviderPerBlock(t)
 	blockchain := unittest.BlockchainFixture(10)
 	headers := newBlockHeadersStorage(blockchain)
 
-	pebbleStorage.RunWithRegistersStorageAtInitialHeights(t, 0, blockchain[0].Header.Height, func(registers *pebbleStorage.Registers) {
-		entropyBlock.
-			On("AtBlockID", mocks.AnythingOfType("flow.Identifier")).
-			Return(entropyProvider).
-			Maybe()
+	s.chain = flow.Emulator.Chain()
+	s.snapshot = snapshot.NewSnapshotTree(nil)
+	s.vm = fvm.NewVirtualMachine()
+	s.vmCtx = fvm.NewContext(
+		fvm.WithChain(s.chain),
+		fvm.WithAuthorizationChecksEnabled(false),
+		fvm.WithSequenceNumberCheckAndIncrementEnabled(false),
+	)
+	s.height = blockchain[0].Header.Height
 
-		snap := bootstrapFVM(t)
-		snap, exeSnap, newAddress := createAccount(t, snap)
-
-		newHeight := blockchain[1].Header.Height
-		err := registers.Store(exeSnap.UpdatedRegisters(), newHeight)
-		require.NoError(t, err)
-
-		scripts, err := NewScripts(zerolog.Nop(), collector, flow.Emulator, entropyBlock, headers, func(ID flow.RegisterID, height uint64) (flow.RegisterValue, error) {
-			return registers.Get(ID, height)
-		})
-		require.NoError(t, err)
-
-		acc, err := scripts.GetAccountAtBlockHeight(context.Background(), newAddress, newHeight)
-		require.NoError(t, err)
-		assert.Equal(t, acc.Address, newAddress)
-	})
-}
-
-func newScripts(
-	t *testing.T,
-	headers storage.Headers,
-	registers func(ID flow.RegisterID, height uint64) (flow.RegisterValue, error),
-) *Scripts {
-	entropyProvider := testutil.EntropyProviderFixture(nil)
-	entropyBlock := mock.NewEntropyProviderPerBlock(t)
-
+	entropyBlock := mock.NewEntropyProviderPerBlock(s.T())
 	entropyBlock.
 		On("AtBlockID", mocks.AnythingOfType("flow.Identifier")).
 		Return(entropyProvider).
 		Maybe()
 
-	scripts, err := NewScripts(zerolog.Nop(), collector, flow.Emulator, entropyBlock, headers, registers)
-	require.NoError(t, err)
+	s.dbDir = unittest.TempDir(s.T())
+	db := pebbleStorage.NewBootstrappedRegistersWithPathForTest(s.T(), s.dbDir, s.height, s.height)
+	pebbleRegisters, err := pebbleStorage.NewRegisters(db)
+	s.Require().NoError(err)
+	s.registerIndex = pebbleRegisters
 
-	return scripts
+	index, err := indexer.New(logger, metrics.NewNoopCollector(), nil, s.registerIndex, headers, nil, nil)
+	s.Require().NoError(err)
+
+	scripts, err := NewScripts(
+		logger,
+		metrics.NewNoopCollector(),
+		s.chain.ChainID(),
+		entropyBlock,
+		headers,
+		IndexRegisterAdapter(index.RegisterValues),
+	)
+	s.Require().NoError(err)
+	s.scripts = scripts
+
+	s.bootstrap()
 }
 
-func newBlockHeadersStorage(blocks []*flow.Block) storage.Headers {
-	blocksByHeight := make(map[uint64]*flow.Block)
-	for _, b := range blocks {
-		blocksByHeight[b.Header.Height] = b
-	}
-
-	return synctest.MockBlockHeaderStorage(synctest.WithByHeight(blocksByHeight))
+func (s *scriptTestSuite) TearDownTest() {
+	s.Require().NoError(os.RemoveAll(s.dbDir))
 }
 
-// bootstrapFVM starts up an FVM and run bootstrap procedures and returns the snapshot tree of the state.
-func bootstrapFVM(t *testing.T) snapshot.SnapshotTree {
-	ctx := fvm.NewContext(fvm.WithChain(chain))
-	vm := fvm.NewVirtualMachine()
-
-	snapshotTree := snapshot.NewSnapshotTree(nil)
-
+func (s *scriptTestSuite) bootstrap() {
 	bootstrapOpts := []fvm.BootstrapProcedureOption{
 		fvm.WithInitialTokenSupply(unittest.GenesisTokenSupply),
 	}
 
-	executionSnapshot, out, err := vm.Run(
-		ctx,
+	executionSnapshot, out, err := s.vm.Run(
+		s.vmCtx,
 		fvm.Bootstrap(unittest.ServiceAccountPublicKey, bootstrapOpts...),
-		snapshotTree)
+		s.snapshot)
 
-	require.NoError(t, err)
-	require.NoError(t, out.Err)
+	s.Require().NoError(err)
+	s.Require().NoError(out.Err)
 
-	return snapshotTree.Append(executionSnapshot)
+	s.height++
+	err = s.registerIndex.Store(executionSnapshot.UpdatedRegisters(), s.height)
+	s.Require().NoError(err)
+
+	s.snapshot = s.snapshot.Append(executionSnapshot)
 }
 
-// createAccount on an existing bootstrapped snapshot tree, execution snapshot and return a new snapshot as well as the newly created account address
-func createAccount(t *testing.T, tree snapshot.SnapshotTree) (snapshot.SnapshotTree, *snapshot.ExecutionSnapshot, flow.Address) {
+func (s *scriptTestSuite) createAccount() flow.Address {
 	const createAccountTransaction = `
-	transaction {
-	  prepare(signer: AuthAccount) {
-		let account = AuthAccount(payer: signer)
-	  }
-	}
-	`
-
-	ctx := fvm.NewContext(
-		fvm.WithChain(chain),
-		fvm.WithAuthorizationChecksEnabled(false),
-		fvm.WithSequenceNumberCheckAndIncrementEnabled(false),
-	)
-	vm := fvm.NewVirtualMachine()
+		transaction {
+		  prepare(signer: AuthAccount) {
+			let account = AuthAccount(payer: signer)
+		  }
+		}`
 
 	txBody := flow.NewTransactionBody().
 		SetScript([]byte(createAccountTransaction)).
-		AddAuthorizer(chain.ServiceAddress())
+		AddAuthorizer(s.chain.ServiceAddress())
 
-	executionSnapshot, output, err := vm.Run(
-		ctx,
+	executionSnapshot, output, err := s.vm.Run(
+		s.vmCtx,
 		fvm.Transaction(txBody, 0),
-		tree,
+		s.snapshot,
 	)
-	require.NoError(t, err)
-	require.NoError(t, output.Err)
+	s.Require().NoError(err)
+	s.Require().NoError(output.Err)
 
-	tree = tree.Append(executionSnapshot)
+	s.height++
+	err = s.registerIndex.Store(executionSnapshot.UpdatedRegisters(), s.height)
+	s.Require().NoError(err)
+
+	s.snapshot = s.snapshot.Append(executionSnapshot)
 
 	var accountCreatedEvents []flow.Event
 	for _, event := range output.Events {
@@ -230,18 +192,20 @@ func createAccount(t *testing.T, tree snapshot.SnapshotTree) (snapshot.SnapshotT
 		accountCreatedEvents = append(accountCreatedEvents, event)
 		break
 	}
-	require.Len(t, accountCreatedEvents, 1)
+	s.Require().Len(accountCreatedEvents, 1)
 
 	data, err := ccf.Decode(nil, accountCreatedEvents[0].Payload)
-	require.NoError(t, err)
+	s.Require().NoError(err)
 	address := flow.ConvertAddress(data.(cadence.Event).Fields[0].(cadence.Address))
 
-	return tree, executionSnapshot, address
+	return address
 }
 
-// converts tree get register function to the required script get register function
-func treeToRegisterAdapter(tree snapshot.SnapshotTree) func(ID flow.RegisterID, height uint64) (flow.RegisterValue, error) {
-	return func(ID flow.RegisterID, height uint64) (flow.RegisterValue, error) {
-		return tree.Get(ID)
+func newBlockHeadersStorage(blocks []*flow.Block) storage.Headers {
+	blocksByHeight := make(map[uint64]*flow.Block)
+	for _, b := range blocks {
+		blocksByHeight[b.Header.Height] = b
 	}
+
+	return synctest.MockBlockHeaderStorage(synctest.WithByHeight(blocksByHeight))
 }
