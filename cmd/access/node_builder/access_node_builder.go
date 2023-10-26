1b18d6f76d15f1383ad1fdc3e8b145164dce7cb5
Gregor Gololicic <gregor.gololicic@dapperlabs.com>
2023-10-26 14:53:59 +0200

    replace get registers

--- cmd/access/node_builder/access_node_builder.go
+++ cmd/access/node_builder/access_node_builder.go
@@ -758,7 +758,7 @@ func (builder *FlowAccessNodeBuilder) BuildExecutionSyncComponents() *FlowAccess
 					builder.RootChainID,
 					query.NewProtocolStateWrapper(builder.State),
 					builder.Storage.Headers,
-					execution.IndexRegisterAdapter(builder.ExecutionIndexerCore.RegisterValues),
+					builder.ExecutionIndexerCore.RegisterValue,
 				)
 				if err != nil {
 					return nil, err
