fc8a55f3f7b57d3e505b0434d8c885be25bd1584
Peter Argue <89119817+peterargue@users.noreply.github.com>
2023-10-25 20:14:08 -0700

    fix lint

--- engine/access/ingestion/engine.go
+++ engine/access/ingestion/engine.go
@@ -855,7 +855,7 @@ func (e *Engine) updateLastFullBlockReceivedIndex(ctx context.Context) {
 	}
 
 	// if we've fallen behind by more than a few blocks, try to catch up using execution data
-	if latestFullHeight + defaultExecutionDataCatchupDistance < finalBlk.Height {
+	if latestFullHeight+defaultExecutionDataCatchupDistance < finalBlk.Height {
 		err = e.indexMissingCollectionsFromExecutionData(ctx, latestFullHeight)
 		if err != nil {
 			logError(err)
