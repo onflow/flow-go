b44485f276dbe8f3c0a5850ddc8a9cabe50e09e9
ramtinms <ramtin.seraj@dapperlabs.com>
2023-11-02 10:28:26 -0700

    add new types for emulator db

--- fvm/evm/types/errors.go
+++ fvm/evm/types/errors.go
@@ -18,13 +18,16 @@ var (
 	// ErrInsufficientTotalSupply is returned when flow token
 	// is withdraw request is there but not enough balance is on EVM vault
 	// this should never happen but its a saftey measure to protect Flow against EVM issues.
-	// TODO we might consider this fatal
+	// TODO: we might consider this fatal
 	ErrInsufficientTotalSupply = errors.New("insufficient total supply")
 
 	// ErrBalanceConversion is returned conversion of balance has failed, usually
 	// is returned when the balance presented in attoflow has values that could
 	// be marginally lost on the conversion.
 	ErrBalanceConversion = errors.New("balance converion error")
+
+	// ErrNotImplemented is a fatal error when something is called that is not implemented
+	ErrNotImplemented = NewFatalError(errors.New("a functionality is called that is not implemented"))
 )
 
 // EVMExecutionError is a user-related error,
