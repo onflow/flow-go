77c6c3c8da3fc5cdd31310fa8753e5fb3956a98e
Gregor Gololicic <gregor.gololicic@dapperlabs.com>
2023-11-16 19:48:11 +0100

    add test updating the state

--- fvm/evm/emulator/state_test.go
+++ fvm/evm/emulator/state_test.go
@@ -1,45 +1,65 @@
 package emulator_test
 
 import (
+	"math/big"
 	"testing"
+
+	"github.com/ethereum/go-ethereum/common"
 
 	"github.com/onflow/flow-go/fvm/evm/testutils"
 	"github.com/onflow/flow-go/fvm/evm/types"
 
 	"github.com/onflow/flow-go/model/flow"
 
-	"github.com/ethereum/go-ethereum/common"
 	gethRawDB "github.com/ethereum/go-ethereum/core/rawdb"
 	gethState "github.com/ethereum/go-ethereum/core/state"
 	"github.com/onflow/flow-go/fvm/evm/emulator/database"
 	"github.com/stretchr/testify/require"
 )
 
-func TestStateWrites(t *testing.T) {
+func fullKey(owner, key []byte) string {
+	return string(owner) + "~" + string(key)
+}
 
-	testutils.RunWithTestBackend(t, func(backend types.Backend) {
+func BenchmarkStateBalance(b *testing.B) {
+
+	testutils.RunWithTestBackend(b, func(backend types.Backend) {
+
 		rootAddr := types.Address{0x01}
-		testAddr := types.Address{0x02}
+		testAddr := types.Address{0x02}.ToCommon()
 
-		db, err := database.NewDatabase(backend, (flow.Address)(rootAddr.Bytes()))
-		require.NoError(t, err)
+		//log.Root().SetHandler(log.StreamHandler(os.Stdout, log.LogfmtFormat()))
+
+		db, err := database.NewMeteredDatabase(backend, (flow.Address)(rootAddr.Bytes()))
+		require.NoError(b, err)
 
 		root, err := db.GetRootHash()
-		require.NoError(t, err)
+		require.NoError(b, err)
 
 		stateDB := gethState.NewDatabase(gethRawDB.NewDatabase(db))
 
-		state, err := gethState.New(root, stateDB, nil)
-		require.NoError(t, err)
+		updateAccount := func(root common.Hash, balance *big.Int) common.Hash {
+			state, err := gethState.New(root, stateDB, nil)
+			require.NoError(b, err)
 
-		stateObj := state.GetOrNewStateObject(testAddr.ToCommon())
-		stateObj.SetState(stateDB, common.Hash{0x01}, common.Hash{0x02})
+			state.SetBalance(testAddr, balance)
+			hash, err := state.Commit(true)
+			require.NoError(b, err)
 
-		hash, err := state.Commit(true)
-		require.NoError(t, err)
+			err = state.Database().TrieDB().Commit(hash, true)
+			require.NoError(b, err)
 
-		err = state.Database().TrieDB().Commit(hash, false)
-		require.NoError(t, err)
+			err = db.Commit(hash)
+			require.NoError(b, err)
+
+			return hash
+		}
+
+		hash := root
+		for i := 0; i < 1000; i++ {
+			hash = updateAccount(hash, big.NewInt(int64(i)))
+			b.ReportMetric((float64)(db.BytesStored()), "bytes_used")
+		}
 
 	})
 
