8d49e08e0564671c35cbab51ab49112778be4b6f
Leo Zhang (zhangchiqing) <zhangchiqing@gmail.com>
2023-11-16 13:04:29 -0800

    fix lint

--- engine/execution/storehouse/block_end_snapshot_test.go
+++ engine/execution/storehouse/block_end_snapshot_test.go
@@ -5,12 +5,12 @@ import (
 	"fmt"
 	"testing"
 
-	"github.com/stretchr/testify/mock"
 	"github.com/stretchr/testify/require"
 	"go.uber.org/atomic"
 
 	executionMock "github.com/onflow/flow-go/engine/execution/mock"
 	"github.com/onflow/flow-go/engine/execution/storehouse"
+	"github.com/onflow/flow-go/model/flow"
 	"github.com/onflow/flow-go/storage"
 	"github.com/onflow/flow-go/utils/unittest"
 )
@@ -69,18 +69,18 @@ func TestBlockEndSnapshot(t *testing.T) {
 		// test getting storage.ErrNotExecuted error
 		heightNotExecuted := unittest.MakeOwnerReg("height not executed", "height not executed")
 		counter := atomic.NewInt32(0)
-	store.
-		On("GetRegister", header.Height, header.ID(), heightNotExecuted.Key).
-		Return(func(uint64, flow.Identifier, flow.RegisterID) (flow.RegisterValue, error) {
-			counter.Inc()
-			// the first call should return error
-			if counter.Load() == 1 {
-				return nil, fmt.Errorf("fail: %w", storehouse.ErrNotExecuted)
-			}
-			// the second call, it returns value
-			return heightNotExecuted.Value, nil
-		}).
-		Times(2)
+		store.
+			On("GetRegister", header.Height, header.ID(), heightNotExecuted.Key).
+			Return(func(uint64, flow.Identifier, flow.RegisterID) (flow.RegisterValue, error) {
+				counter.Inc()
+				// the first call should return error
+				if counter.Load() == 1 {
+					return nil, fmt.Errorf("fail: %w", storehouse.ErrNotExecuted)
+				}
+				// the second call, it returns value
+				return heightNotExecuted.Value, nil
+			}).
+			Times(2)
 
 		// first time should return error
 		_, err = snapshot.Get(heightNotExecuted.Key)
