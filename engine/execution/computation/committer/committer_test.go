4aac6bc9e7db1917c6fbb86b477c721435c22cfe
Leo Zhang (zhangchiqing) <zhangchiqing@gmail.com>
2023-11-16 13:27:59 -0800

    update committer tests

--- engine/execution/computation/committer/committer_test.go
+++ engine/execution/computation/committer/committer_test.go
@@ -7,42 +7,83 @@ import (
 	"github.com/stretchr/testify/require"
 
 	"github.com/onflow/flow-go/engine/execution/computation/committer"
+	"github.com/onflow/flow-go/engine/execution/storehouse"
 	"github.com/onflow/flow-go/fvm/storage/snapshot"
-	led "github.com/onflow/flow-go/ledger"
+	"github.com/onflow/flow-go/ledger"
+	"github.com/onflow/flow-go/ledger/common/convert"
+	"github.com/onflow/flow-go/ledger/common/pathfinder"
+	"github.com/onflow/flow-go/ledger/complete"
 	ledgermock "github.com/onflow/flow-go/ledger/mock"
 	"github.com/onflow/flow-go/model/flow"
 	"github.com/onflow/flow-go/module/trace"
-	utils "github.com/onflow/flow-go/utils/unittest"
+	"github.com/onflow/flow-go/utils/unittest"
 )
 
 func TestLedgerViewCommitter(t *testing.T) {
 
-	t.Run("calls to set and prove", func(t *testing.T) {
+	// verify after committing a snapshot, proof will be generated,
+	// and changes are saved in storage snapshot
+	t.Run("CommitView should return proof and statecommitment", func(t *testing.T) {
 
-		ledger := new(ledgermock.Ledger)
-		com := committer.NewLedgerViewCommitter(ledger, trace.NewNoopTracer())
+		l := new(ledgermock.Ledger)
+		committer := committer.NewLedgerViewCommitter(l, trace.NewNoopTracer())
 
-		var expectedStateCommitment led.State
-		copy(expectedStateCommitment[:], []byte{1, 2, 3})
-		ledger.On("Set", mock.Anything).
-			Return(expectedStateCommitment, nil, nil).
+		// CommitDelta will call ledger.Set and ledger.Prove
+
+		reg := flow.NewRegisterID("owner", "key2")
+		value := []byte{1}
+		startState := unittest.StateCommitmentFixture()
+
+		update, err := ledger.NewUpdate(ledger.State(startState), []ledger.Key{convert.RegisterIDToLedgerKey(reg)}, []ledger.Value{value})
+		require.NoError(t, err)
+
+		expectedTrieUpdate, err := pathfinder.UpdateToTrieUpdate(update, complete.DefaultPathFinderVersion)
+		require.NoError(t, err)
+
+		endState := unittest.StateCommitmentFixture()
+		require.NotEqual(t, startState, endState)
+
+		// mock ledger.Set
+		l.On("Set", mock.Anything).
+			Return(ledger.State(endState), expectedTrieUpdate, nil).
 			Once()
 
-		expectedProof := led.Proof([]byte{2, 3, 4})
-		ledger.On("Prove", mock.Anything).
+			// mock ledger.Prove
+		expectedProof := ledger.Proof([]byte{2, 3, 4})
+		l.On("Prove", mock.Anything).
 			Return(expectedProof, nil).
 			Once()
 
-		newState, proof, _, err := com.CommitView(
-			&snapshot.ExecutionSnapshot{
-				WriteSet: map[flow.RegisterID]flow.RegisterValue{
-					flow.NewRegisterID("owner", "key"): []byte{1},
-				},
+			// previous block's storage snapshot
+		previousBlockSnapshot := storehouse.NewExecutingBlockSnapshot(
+			snapshot.MapStorageSnapshot{
+				flow.NewRegisterID("owner", "key1"): []byte{1},
 			},
-			utils.StateCommitmentFixture())
+			flow.StateCommitment(update.State()),
+		)
+
+		// this block's register updates
+		blockUpdates := &snapshot.ExecutionSnapshot{
+			WriteSet: map[flow.RegisterID]flow.RegisterValue{
+				reg: value,
+			},
+		}
+
+		newCommit, proof, trieUpdate, newStorageSnapshot, err := committer.CommitView(
+			blockUpdates,
+			previousBlockSnapshot,
+		)
+
 		require.NoError(t, err)
-		require.Equal(t, flow.StateCommitment(expectedStateCommitment), newState)
+
+		// verify CommitView returns expected proof and statecommitment
+		require.Equal(t, previousBlockSnapshot.Commitment(), flow.StateCommitment(trieUpdate.RootHash))
+		require.Equal(t, newCommit, newStorageSnapshot.Commitment())
+		require.Equal(t, endState, newCommit)
 		require.Equal(t, []uint8(expectedProof), proof)
+		require.True(t, expectedTrieUpdate.Equals(trieUpdate))
+
+		// TOOD(leo): verify ledger.Set and ledger.Prove are called and received expected arguments
 	})
 
 }
