f3af37881ba67ed5d6dddd29ddf3d0840a05e8c1
Gregor Gololicic <gregor.gololicic@dapperlabs.com>
2023-11-21 18:04:04 +0100

    improve metrics reported

--- fvm/evm/emulator/state_test.go
+++ fvm/evm/emulator/state_test.go
@@ -1,6 +1,7 @@
 package emulator_test
 
 import (
+	"fmt"
 	"math/big"
 	"os"
 	"testing"
@@ -27,59 +28,85 @@ var rootAddr = flow.Address{0x01}
 // Accounts will be automatically generated, and the benchmark allows users to determine the frequency of balance
 // updates across all accounts, including in-between state committing.
 func benchmarkStateAccountsBalanceChange(b *testing.B, numberOfAccounts int, numberOfUpdatesPerAccount int, debug bool) {
-	testutils.RunWithTestBackend(b, func(backend *testutils.TestBackend) {
-		if debug {
-			log.Root().SetHandler(log.StreamHandler(os.Stdout, log.LogfmtFormat()))
-		}
+	if debug {
+		log.Root().SetHandler(log.StreamHandler(os.Stdout, log.LogfmtFormat()))
+	}
 
-		db, err := database.NewMeteredDatabase(backend, rootAddr)
+	f, err := os.OpenFile("./bench_10000", os.O_CREATE|os.O_APPEND|os.O_WRONLY, 0644)
+	require.NoError(b, err)
+
+	store := testutils.GetSimpleValueStore()
+	db, err := database.NewMeteredDatabase(store, rootAddr)
+	require.NoError(b, err)
+
+	rootHash, err := db.GetRootHash()
+	require.NoError(b, err)
+
+	stateDB := gethState.NewDatabase(gethRawDB.NewDatabase(db))
+
+	accounts := make([]common.Address, numberOfAccounts)
+	for i := range accounts {
+		accounts[i] = common.Address{byte(i)}
+	}
+
+	updateAccounts := func(root common.Hash, balance *big.Int) common.Hash {
+		state, err := gethState.New(root, stateDB, nil)
 		require.NoError(b, err)
 
-		rootHash, err := db.GetRootHash()
+		var hash common.Hash
+		for _, addr := range accounts {
+			state.SetBalance(addr, balance)
+			hash, err = state.Commit(true)
+			require.NoError(b, err)
+		}
+
+		err = state.Database().TrieDB().Commit(hash, true)
 		require.NoError(b, err)
 
-		stateDB := gethState.NewDatabase(gethRawDB.NewDatabase(db))
+		err = db.Commit(hash)
+		require.NoError(b, err)
 
-		accounts := make([]common.Address, numberOfAccounts)
-		for i := range accounts {
-			accounts[i] = common.Address{byte(i)}
-		}
+		db.DropCache()
 
-		updateAccounts := func(root common.Hash, balance *big.Int) common.Hash {
-			state, err := gethState.New(root, stateDB, nil)
-			require.NoError(b, err)
+		_, count := store.Metrics()
+		_, err = f.WriteString(fmt.Sprintf("%d\n", count))
+		require.NoError(b, err)
 
-			var hash common.Hash
-			for _, addr := range accounts {
-				state.SetBalance(addr, balance)
-				hash, err = state.Commit(true)
-				require.NoError(b, err)
-			}
+		return hash
+	}
 
-			err = state.Database().TrieDB().Commit(hash, true)
-			require.NoError(b, err)
+	for i := 0; i < numberOfUpdatesPerAccount; i++ {
+		rootHash = updateAccounts(rootHash, big.NewInt(int64(i)))
+		size, items := store.Metrics()
 
-			err = db.Commit(hash)
-			require.NoError(b, err)
+		b.ReportMetric(float64(db.BytesWritten()), "bytes_written")
+		b.ReportMetric(float64(db.BytesRead()), "bytes_read")
+		b.ReportMetric(float64(size), "store_size_bytes")
+		b.ReportMetric(float64(size)/1000000, "store_size_mb")
+		b.ReportMetric(float64(items), "store_items_count")
+	}
 
-			db.DropCache()
-
-			return hash
-		}
-
-		for i := 0; i < numberOfUpdatesPerAccount; i++ {
-			rootHash = updateAccounts(rootHash, big.NewInt(int64(i)))
-			b.ReportMetric(float64(db.BytesStored()), "bytes_used")
-			b.ReportMetric(float64(db.BytesRetrieved()), "bytes_read")
-		}
-
-	})
+	f.Close()
 }
 
+/*
+42284067 bytes_read
+29274306 bytes_written
+1593 store_items_count
+1681820 store_size_bytes
+1.68 store_size_mb
+*/
 func BenchmarkStateBalanceSingleAccount(b *testing.B) {
 	benchmarkStateAccountsBalanceChange(b, 1, 10000, false)
 }
 
+/*
+25492266 bytes_read
+17193574 bytes_written
+2515 store_items_count
+2307331 store_size_bytes
+2.30 store_size_mb
+*/
 func BenchmarkStateBalanceMultipleAccount(b *testing.B) {
 	benchmarkStateAccountsBalanceChange(b, 100, 100, false)
 }
