4f4364e2ecf445b4cf331eec49e1a97ce3c7e453
Gregor Gololicic <gregor.gololicic@dapperlabs.com>
2023-11-22 19:37:46 +0100

    add comments change tests from benchmarks to test

--- fvm/evm/emulator/state_test.go
+++ fvm/evm/emulator/state_test.go
@@ -19,83 +19,91 @@ import (
 
 var rootAddr = flow.Address{0x01}
 
-// BenchmarkStateAccountsBalanceChange is designed to evaluate the impact of state modifications on storage size.
+// testBalanceUpdates is designed to evaluate the impact of state modifications on storage size.
 // It measures the bytes used in the underlying storage, aiming to understand how storage size scales with changes in state.
 // During the test, each account balance is updated, with a focus on measuring any consequential changes to the state.
 // While the specific operation details are not crucial for this benchmark, the primary goal is to analyze how the storage
 // size evolves in response to state modifications. Users can specify the number of accounts on which the balance is updated.
 // Accounts will be automatically generated, and the benchmark allows users to determine the frequency of balance
 // updates across all accounts, including in-between state committing.
-func benchmarkStateAccountsBalanceChange(b *testing.B, numberOfAccounts int, numberOfUpdatesPerAccount int, debug bool) {
+func testBalanceUpdates(
+	t *testing.T,
+	numberOfAccounts int,
+	numberOfUpdatesPerAccount int,
+	debug bool,
+) {
+	//t.Skip() // don't run in a normal test suite
+
 	if debug {
 		log.Root().SetHandler(log.StreamHandler(os.Stdout, log.LogfmtFormat()))
 	}
 
-	store := testutils.GetSimpleValueStore()
-	db, err := database.NewMeteredDatabase(store, rootAddr)
-	require.NoError(b, err)
+	backend := testutils.GetSimpleValueStore()
+
+	db, err := database.NewMeteredDatabase(backend, rootAddr)
+	require.NoError(t, err)
 
 	rootHash, err := db.GetRootHash()
-	require.NoError(b, err)
+	require.NoError(t, err)
 
-	stateDB := gethState.NewDatabase(gethRawDB.NewDatabase(db))
+	rawDB := gethRawDB.NewDatabase(db)
+	stateDB := gethState.NewDatabase(rawDB)
 
 	accounts := make([]common.Address, numberOfAccounts)
 	for i := range accounts {
-		accounts[i] = common.Address{byte(i)}
+		accounts[i] = common.Address{byte(i + 100)}
 	}
 
 	updateAccounts := func(root common.Hash, balance *big.Int) common.Hash {
 		state, err := gethState.New(root, stateDB, nil)
-		require.NoError(b, err)
+		require.NoError(t, err)
 
 		var hash common.Hash
 		for _, addr := range accounts {
+			// create account and set code
 			state.SetBalance(addr, balance)
 			hash, err = state.Commit(true)
-			require.NoError(b, err)
+			require.NoError(t, err)
 		}
 
 		err = state.Database().TrieDB().Commit(hash, true)
-		require.NoError(b, err)
+		require.NoError(t, err)
 
 		err = db.Commit(hash)
-		require.NoError(b, err)
+		require.NoError(t, err)
 
 		db.DropCache()
+
 		return hash
 	}
 
 	for i := 0; i < numberOfUpdatesPerAccount; i++ {
 		rootHash = updateAccounts(rootHash, big.NewInt(int64(i)))
-		size, items := store.Metrics()
-
-		b.ReportMetric(float64(db.BytesWritten()), "bytes_written")
-		b.ReportMetric(float64(db.BytesRead()), "bytes_read")
-		b.ReportMetric(float64(size), "store_size_bytes")
-		b.ReportMetric(float64(size)/1000000, "store_size_mb")
-		b.ReportMetric(float64(items), "store_items_count")
 	}
+
+	items, size := backend.Metrics()
+	t.Logf("bytes_written: %d", db.BytesStored())
+	t.Logf("bytes_read: %d", db.BytesRetrieved())
+	t.Logf("storage_items: %d", items)
+	t.Logf("storage_size_bytes: %d", size)
 }
 
 /*
-42284067 bytes_read
-29274306 bytes_written
-1593 store_items_count
-1681820 store_size_bytes
-1.68 store_size_mb
+bytes_written 353897818
+bytes_read 605494369
+storage_items 16855058
+storage_size_bytes 15911
 */
-func BenchmarkStateBalanceSingleAccount(b *testing.B) {
-	benchmarkStateAccountsBalanceChange(b, 1, 10000, false)
+func TestStateBalanceSingleAccount(t *testing.T) {
+	testBalanceUpdates(t, 1, 1000, false)
 }
 
 /*
-25492266 bytes_read
-17193574 bytes_written
-2515 store_items_count
-2307331 store_size_bytes
-2.30 store_size_mb
+bytes_written 44919544
+bytes_read 67108024
+storage_items 6006558
+storage_size_bytes 6673
 */
-func BenchmarkStateBalanceMultipleAccount(b *testing.B) {
-	benchmarkStateAccountsBalanceChange(b, 100, 100, false)
+func TestStateBalanceMultipleAccounts(t *testing.T) {
+	testBalanceUpdates(t, 1000, 100, false)
 }
