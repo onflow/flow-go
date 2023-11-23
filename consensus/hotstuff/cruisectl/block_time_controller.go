4d2d07321975b9c0e9c2f64e07ee366455437958
Jordan Schalm <jordan@dapperlabs.com>
2023-11-23 10:55:41 -0500

    gofmt

--- consensus/hotstuff/cruisectl/block_time_controller.go
+++ consensus/hotstuff/cruisectl/block_time_controller.go
@@ -35,8 +35,8 @@ type TimedBlock struct {
 // the first view of a new epoch, or the EpochSetup phase of the current epoch.
 type epochInfo struct {
 	curEpochFirstView       uint64
-	curEpochFinalView       uint64 // F[v] - the final view of the current epoch
-	curEpochTargetDuration  uint64 // desired total duration of current epoch in seconds
+	curEpochFinalView       uint64  // F[v] - the final view of the current epoch
+	curEpochTargetDuration  uint64  // desired total duration of current epoch in seconds
 	curEpochTargetEndTime   uint64  // T[v] - the target end time of the current epoch, represented as Unix Time [seconds]
 	nextEpochFinalView      *uint64 // the final view of the next epoch
 	nextEpochTargetDuration *uint64 // desired total duration of next epoch in seconds, or nil if epoch has not yet been set up
@@ -390,8 +390,8 @@ func (ctl *BlockTimeController) measureViewDuration(tb TimedBlock) error {
 	// In accordance with this convention, observing the proposal for the last view of an epoch, marks the start of the last view.
 	// By observing the proposal, nodes enter the last view, verify the block, vote for it, the primary aggregates the votes,
 	// constructs the child (for first view of new epoch). The last view of the epoch ends, when the child proposal is published.
-	tau := ctl.targetViewTime()                                // τ: idealized target view time in units of seconds
-	viewDurationsRemaining := ctl.curEpochFinalView + 1 - view // k[v]: views remaining in current epoch
+	tau := ctl.targetViewTime()                                              // τ: idealized target view time in units of seconds
+	viewDurationsRemaining := ctl.curEpochFinalView + 1 - view               // k[v]: views remaining in current epoch
 	durationRemaining := u2t(ctl.curEpochTargetEndTime).Sub(tb.TimeObserved) // Γ[v] = T[v] - t[v], with t[v] ≡ tb.TimeObserved the time when observing the block that trigged the view change
 
 	// Compute instantaneous error term: e[v] = k[v]·τ - T[v] i.e. the projected difference from target switchover
