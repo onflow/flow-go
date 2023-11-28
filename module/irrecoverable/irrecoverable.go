e78bf18a4f1d3f64488dcc3e1f6566dc9b98fbdd
Uliana Andrukhiv <u.andrukhiv@gmail.com>
2023-11-28 18:41:21 +0200

    Updated godoc for SignalerContextKey
    Co-authored-by: Yurii Oleksyshyn <yuraolex@gmail.com>

--- module/irrecoverable/irrecoverable.go
+++ module/irrecoverable/irrecoverable.go
@@ -48,7 +48,7 @@ type SignalerContext interface {
 	sealed()         // private, to constrain builder to using WithSignaler
 }
 
-// SignalerContextKey represents the key type for retrieving a SignalerContext from a context.Context.
+// SignalerContextKey represents the key type for retrieving a SignalerContext from a value `context.Context`.
 type SignalerContextKey struct{}
 
 // private, to force context derivation / WithSignaler
