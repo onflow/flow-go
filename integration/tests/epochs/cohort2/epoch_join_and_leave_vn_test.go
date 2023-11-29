9c4f2c356e8f8963f8a84e4e5b9b01575554c76d
Jordan Schalm <jordan@dapperlabs.com>
2023-11-29 14:59:33 -0500

    Cherry-pick changes from https://github.com/onflow/flow-go/pull/4975
    Backporting VN epoch test flakiness fix from master

--- integration/tests/epochs/cohort2/epoch_join_and_leave_vn_test.go
+++ integration/tests/epochs/cohort2/epoch_join_and_leave_vn_test.go
@@ -2,6 +2,7 @@ package cohort2
 
 import (
 	"testing"
+	"time"
 
 	"github.com/stretchr/testify/suite"
 
@@ -21,6 +22,8 @@ type EpochJoinAndLeaveVNSuite struct {
 func (s *EpochJoinAndLeaveVNSuite) SetupTest() {
 	// require approvals for seals to verify that the joining VN is producing valid seals in the second epoch
 	s.RequiredSealApprovals = 1
+	// slow down consensus, as sealing tends to lag behind
+	s.ConsensusProposalDuration = time.Millisecond * 250
 	// increase epoch length to account for greater sealing lag due to above
 	// NOTE: this value is set fairly aggressively to ensure shorter test times.
 	// If flakiness due to failure to complete staking operations in time is observed,
@@ -29,7 +32,7 @@ func (s *EpochJoinAndLeaveVNSuite) SetupTest() {
 	s.DKGPhaseLen = 100
 	s.EpochLen = 450
 	s.EpochCommitSafetyThreshold = 20
-	s.DynamicEpochTransitionSuite.SetupTest()
+	s.DynamicEpochTransitionSuite.Suite.SetupTest()
 }
 
 // TestEpochJoinAndLeaveVN should update verification nodes and assert healthy network conditions
