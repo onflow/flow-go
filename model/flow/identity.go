a8baaf30d6af64def153bb5d24f89fa5b7a30af9
Tarak Ben Youssef <tarak.benyoussef@dapperlabs.com>
2024-01-17 21:26:51 +0100

    fix more test monitor data and go imports

--- model/flow/identity.go
+++ model/flow/identity.go
@@ -12,10 +12,10 @@ import (
 
 	"github.com/ethereum/go-ethereum/rlp"
 	"github.com/fxamacker/cbor/v2"
+	"github.com/onflow/crypto"
 	"github.com/pkg/errors"
 	"github.com/vmihailenco/msgpack"
 
-	"github.com/onflow/crypto"
 	"github.com/onflow/flow-go/utils/rand"
 )
 
