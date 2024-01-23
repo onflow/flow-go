fb9d5e6a966278dbc2a1094a2d32eb8ea7d8412b
Leo Zhang <zhangchiqing@gmail.com>
2024-01-22 19:42:39 -0800

    Update access/validator.go
    Co-authored-by: Jordan Schalm <jordan@dapperlabs.com>

--- access/validator.go
+++ access/validator.go
@@ -42,7 +42,10 @@ func (b *ProtocolStateBlocks) FinalizedHeader() (*flow.Header, error) {
 	return b.state.Final().Head()
 }
 
-// RateLimiter is an interface for checking if an address is rate limited
+// RateLimiter is an interface for checking if an address is rate limited.
+// By convention, the address used is the payer field of a transaction.
+// This rate limiter is applied when a transaction is first received by a
+// node, meaning that if a transaction is rate-limited it will be dropped.
 type RateLimiter interface {
 	// IsRateLimited returns true if the address is rate limited
 	IsRateLimited(address flow.Address) bool
