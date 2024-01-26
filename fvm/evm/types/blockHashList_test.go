121b1320f5be3b7afbe4b176f5f62498a86d423a
ramtinms <ramtin.seraj@dapperlabs.com>
2024-01-26 00:10:04 -0800

    add block hash type

---
+++ fvm/evm/types/blockHashList_test.go
@@ -0,0 +1,55 @@
+package types_test
+
+import (
+	"testing"
+
+	gethCommon "github.com/ethereum/go-ethereum/common"
+	"github.com/stretchr/testify/require"
+
+	"github.com/onflow/flow-go/fvm/evm/types"
+)
+
+func TestBlockHashList(t *testing.T) {
+
+	capacity := 5
+	bhl := types.NewBlockHashList(capacity)
+	require.True(t, bhl.IsEmpty())
+
+	require.Equal(t, gethCommon.Hash{}, bhl.LastAddedBlockHash())
+
+	found, h := bhl.BlockHashByHeight(0)
+	require.False(t, found)
+	require.Equal(t, gethCommon.Hash{}, h)
+
+	// first full range
+	for i := 0; i < capacity; i++ {
+		err := bhl.Push(i, gethCommon.Hash{byte(i)})
+		require.NoError(t, err)
+	}
+	for i := 0; i < capacity; i++ {
+		found, h := bhl.BlockHashByHeight(i)
+		require.True(t, found)
+		require.Equal(t, gethCommon.Hash{byte(i)}, h)
+	}
+	require.Equal(t, gethCommon.Hash{byte(capacity - 1)}, bhl.LastAddedBlockHash())
+
+	// over border range
+	for i := capacity; i < capacity+3; i++ {
+		bhl.Push(i, gethCommon.Hash{byte(i)})
+	}
+	for i := 0; i < capacity-2; i++ {
+		found, _ := bhl.BlockHashByHeight(i)
+		require.False(t, found)
+	}
+	for i := capacity - 2; i < capacity+3; i++ {
+		found, h := bhl.BlockHashByHeight(i)
+		require.True(t, found)
+		require.Equal(t, gethCommon.Hash{byte(i)}, h)
+	}
+	require.Equal(t, gethCommon.Hash{byte(capacity + 2)}, bhl.LastAddedBlockHash())
+
+	encoded := bhl.Encode()
+	bhl2, err := types.NewBlockHashListFromEncoded(encoded)
+	require.NoError(t, err)
+	require.Equal(t, bhl, bhl2)
+}
