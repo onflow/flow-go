a0761f7c66bda8be2674732888d6cd482c6db289
ramtinms <ramtin.seraj@dapperlabs.com>
2024-01-29 21:11:44 -0800

    improve coa contract

--- fvm/evm/handler/coa.sol
+++ fvm/evm/handler/coa.sol
@@ -39,9 +39,7 @@ function onERC1155BatchReceived(
 }
 contract COA is ERC1155TokenReceiver, ERC777TokensRecipient, ERC721TokenReceiver, IERC165 {
     address constant public cadenceArch = 0x0000000000000000000000010000000000000001;
-    event FlowReceived(address indexed sender, uint256 value);
     receive() external payable  {
-        emit FlowReceived(msg.sender, msg.value);
     }
     function supportsInterface(bytes4 id) external view virtual override returns (bool) {
         return
@@ -93,7 +91,7 @@ contract COA is ERC1155TokenReceiver, ERC777TokensRecipient, ERC721TokenReceiver
     bytes32 _hash,
     bytes memory _sig
     ) external view virtual returns (bytes4){
-        (bool ok, bytes memory data) = cadenceArch.staticcall(abi.encodeWithSignature("verifyCOAOwnershipProof(bytes32, bytes)", _hash, _sig));
+        (bool ok, bytes memory data) = cadenceArch.staticcall(abi.encodeWithSignature("verifyCOAOwnershipProof(address, bytes32, bytes)", address(this), _hash, _sig));
         require(ok);
         bool output = abi.decode(data, (bool));
         if (output) {
