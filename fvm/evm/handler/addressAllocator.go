c8bd1da07ca4fc25013cd6278e54d8aab083c465
Tarak Ben Youssef <tarak.benyoussef@dapperlabs.com>
2024-02-01 23:39:33 +0100

    explain mapAddressIndex logic

--- fvm/evm/handler/addressAllocator.go
+++ fvm/evm/handler/addressAllocator.go
@@ -11,9 +11,10 @@ import (
 
 const (
 	ledgerAddressAllocatorKey = "AddressAllocator"
-	// addressIndexShuffleSeed is used for shuffling address index
-	// shuffling index is used to make address postfixes look random
-	addressIndexShuffleSeed = uint64(0xFFEEDDCCBBAA9987)
+	// addressIndexMultiplierConstant is used for mapping address indices
+	// into a deterministic random-looking address postfixes. 
+	// look at `mapAddressIndex` for more details.
+	addressIndexMultiplierConstant = uint64(0xFFEEDDCCBBAA9977)
 )
 
 type AddressAllocator struct {
@@ -41,7 +42,7 @@ func (aa *AddressAllocator) AllocateCOAAddress(uuid uint64) types.Address {
 }
 
 func MakeCOAAddress(index uint64) types.Address {
-	return makePrefixedAddress(shuffleAddressIndex(index), types.FlowEVMCOAAddressPrefix)
+	return makePrefixedAddress(mapAddressIndex(index), types.FlowEVMCOAAddressPrefix)
 }
 
 func (aa *AddressAllocator) AllocatePrecompileAddress(index uint64) types.Address {
@@ -64,6 +65,18 @@ func makePrefixedAddress(
 	return addr
 }
 
-func shuffleAddressIndex(preShuffleIndex uint64) uint64 {
-	return uint64(preShuffleIndex * addressIndexShuffleSeed)
+// `mapAddressIndex` maps an index of 64 bits to a deterministic random-looking 64 bits.
+//
+// The mapping function must be an injective mapping (in this case bijective) 
+// where every two indices always map to two different results. Multiple injective 
+// mappings are possible.
+// 
+// The current implementation uses a simple modular multiplication by a constant modulo 2^64.
+// The multiplier constant can be any odd number. Since odd numbers are co-prime with 2^64, they
+// have an multiplicative inverse modulo 2^64. 
+// This makes multiplying by an odd number an injective function (and therefore bijective).
+//
+// Multiplying modulo 2^64 is implicitly implemented as a uint64 multiplication with a uin64 result.
+func mapAddressIndex(index uint64) uint64 {
+	return uint64(index * addressIndexMultiplierConstant)
 }
