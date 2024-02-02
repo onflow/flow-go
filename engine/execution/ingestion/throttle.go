844c350c9d5488b9dde3f2102dce585b13c87baa
Leo Zhang (zhangchiqing) <zhangchiqing@gmail.com>
2024-02-01 17:09:14 -0800

    lint

--- engine/execution/ingestion/throttle.go
+++ engine/execution/ingestion/throttle.go
@@ -5,13 +5,14 @@ import (
 	"fmt"
 	"sync"
 
+	"github.com/rs/zerolog"
+
 	"github.com/onflow/flow-go/engine/execution/state"
 	"github.com/onflow/flow-go/model/flow"
 	"github.com/onflow/flow-go/module/component"
 	"github.com/onflow/flow-go/module/irrecoverable"
 	"github.com/onflow/flow-go/state/protocol"
 	"github.com/onflow/flow-go/storage"
-	"github.com/rs/zerolog"
 )
 
 // CatchUpThreshold is the number of blocks that if the execution is far behind
