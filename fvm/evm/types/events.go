9f46beab5fe05288f00bf45236bdc9f45ee36f1d
Gregor Gololicic <gregor.gololicic@dapperlabs.com>
2024-02-06 16:21:12 +0100

    total supply type fix

--- fvm/evm/types/events.go
+++ fvm/evm/types/events.go
@@ -168,7 +168,7 @@ var blockExecutedEventCadenceType = &cadence.EventType{
 	Fields: []cadence.Field{
 		cadence.NewField("height", cadence.UInt64Type),
 		cadence.NewField("hash", cadence.StringType),
-		cadence.NewField("totalSupply", cadence.UInt64Type),
+		cadence.NewField("totalSupply", cadence.IntType),
 		cadence.NewField("parentHash", cadence.StringType),
 		cadence.NewField("receiptRoot", cadence.StringType),
 		cadence.NewField(
