cc7e059146bdb3c05de27639e9290fb0ff8e0ee2
Janez Podhostnik <janez.podhostnik@gmail.com>
2024-02-08 15:54:06 +0100

    add todo

--- engine/execution/state/bootstrap/bootstrap.go
+++ engine/execution/state/bootstrap/bootstrap.go
@@ -51,6 +51,7 @@ func (b *Bootstrapper) BootstrapLedger(
 		fvm.WithLogger(b.logger),
 		fvm.WithMaxStateInteractionSize(ledgerIntractionLimitNeededForBootstrapping),
 		fvm.WithChain(chain),
+		// TODO (JanezP): move this deeper
 		fvm.WithEVMEnabled(true),
 	)
 
