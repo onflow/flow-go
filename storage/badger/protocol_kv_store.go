f68be71e09c81fb8a9201f325aa5d9fbef5203d8
Yurii Oleksyshyn <yuraolex@gmail.com>
2024-02-12 22:24:00 +0200

    Added base test for protocol kv store

--- storage/badger/protocol_kv_store.go
+++ storage/badger/protocol_kv_store.go
@@ -8,13 +8,25 @@ import (
 	"github.com/onflow/flow-go/storage/badger/transaction"
 )
 
+// DefaultProtocolKVStoreCacheSize is the default size for primary protocol KV store cache.
+// KV store is rarely updated, so we will have a limited number of unique states.
+// Lets be generous and assume we have 10 different KV stores used at the same time.
+var DefaultProtocolKVStoreCacheSize uint = 10
+
+// DefaultProtocolKVStoreByBlockIDCacheSize is the default value for secondary byBlockIdCache.
+// We want to be able to cover a broad interval of views without cache misses, so we use a bigger value.
+var DefaultProtocolKVStoreByBlockIDCacheSize uint = 1000
+
 type ProtocolKVStore struct {
 	db *badger.DB
 }
 
 var _ storage.ProtocolKVStore = (*ProtocolKVStore)(nil)
 
-func NewProtocolKVStore(collector module.CacheMetrics) *ProtocolKVStore {
+func NewProtocolKVStore(collector module.CacheMetrics,
+	stateCacheSize uint,
+	stateByBlockIDCacheSize uint,
+) *ProtocolKVStore {
 	return &ProtocolKVStore{}
 }
 
--- storage/badger/protocol_kv_store_test.go
+++ storage/badger/protocol_kv_store_test.go
@@ -1,1 +1,46 @@
 package badger
+
+import (
+	"testing"
+
+	"github.com/dgraph-io/badger/v2"
+	"github.com/onflow/flow-go/module/metrics"
+	"github.com/onflow/flow-go/storage"
+	"github.com/onflow/flow-go/storage/badger/transaction"
+	"github.com/onflow/flow-go/utils/unittest"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+)
+
+// TesKeyValueStoreStorage tests if the KV store is stored, retrieved and indexed correctly
+func TestKeyValueStoreStorage(t *testing.T) {
+	unittest.RunWithBadgerDB(t, func(db *badger.DB) {
+		metrics := metrics.NewNoopCollector()
+		store := NewProtocolKVStore(metrics, DefaultProtocolStateCacheSize, DefaultProtocolStateByBlockIDCacheSize)
+
+		expected := &storage.KeyValueStoreData{
+			Version: 2,
+			Data:    unittest.RandomBytes(32),
+		}
+		stateID := unittest.IdentifierFixture()
+		blockID := unittest.IdentifierFixture()
+
+		// store protocol state and auxiliary info
+		err := transaction.Update(db, func(tx *transaction.Tx) error {
+			err := store.StoreTx(stateID, expected)(tx)
+			require.NoError(t, err)
+			return store.IndexTx(blockID, stateID)(tx)
+		})
+		require.NoError(t, err)
+
+		// fetch protocol state
+		actual, err := store.ByID(stateID)
+		require.NoError(t, err)
+		assert.Equal(t, expected, actual)
+
+		// fetch protocol state by block ID
+		actualByBlockID, err := store.ByBlockID(blockID)
+		require.NoError(t, err)
+		assert.Equal(t, expected, actualByBlockID)
+	})
+}
