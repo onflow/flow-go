784176c74b9fcf96f6cec3adddf10b21b6555721
Yurii Oleksyshyn <yuraolex@gmail.com>
2024-02-15 20:23:44 +0200

    Added extra tests to verify that correct sentinels are returned

--- storage/badger/protocol_kv_store_test.go
+++ storage/badger/protocol_kv_store_test.go
@@ -45,3 +45,64 @@ func TestKeyValueStoreStorage(t *testing.T) {
 		assert.Equal(t, expected, actualByBlockID)
 	})
 }
+
+// TestProtocolKVStore_StoreTx tests that StoreTx returns an error if the KV-store snapshot with the given id is already stored.
+func TestProtocolKVStore_StoreTx(t *testing.T) {
+	unittest.RunWithBadgerDB(t, func(db *badger.DB) {
+		metrics := metrics.NewNoopCollector()
+		store := NewProtocolKVStore(metrics, db, DefaultProtocolKVStoreCacheSize, DefaultProtocolKVStoreByBlockIDCacheSize)
+
+		stateID := unittest.IdentifierFixture()
+		expected := &storage.KeyValueStoreData{
+			Version: 2,
+			Data:    unittest.RandomBytes(32),
+		}
+
+		err := transaction.Update(db, store.StoreTx(stateID, expected))
+		require.NoError(t, err)
+
+		err = transaction.Update(db, store.StoreTx(stateID, expected))
+		require.ErrorIs(t, err, storage.ErrAlreadyExists)
+	})
+}
+
+// TestProtocolKVStore_IndexTx tests that IndexTx returns an error if a KV store for the given blockID has already been indexed.
+func TestProtocolKVStore_IndexTx(t *testing.T) {
+	unittest.RunWithBadgerDB(t, func(db *badger.DB) {
+		metrics := metrics.NewNoopCollector()
+		store := NewProtocolKVStore(metrics, db, DefaultProtocolKVStoreCacheSize, DefaultProtocolKVStoreByBlockIDCacheSize)
+
+		stateID := unittest.IdentifierFixture()
+		blockID := unittest.IdentifierFixture()
+
+		err := transaction.Update(db, store.IndexTx(blockID, stateID))
+		require.NoError(t, err)
+
+		err = transaction.Update(db, store.IndexTx(blockID, stateID))
+		require.ErrorIs(t, err, storage.ErrAlreadyExists)
+	})
+}
+
+// TestProtocolKVStore_ByBlockID tests that ByBlockID returns an error if no snapshot has been indexed for the given block.
+func TestProtocolKVStore_ByBlockID(t *testing.T) {
+	unittest.RunWithBadgerDB(t, func(db *badger.DB) {
+		metrics := metrics.NewNoopCollector()
+		store := NewProtocolKVStore(metrics, db, DefaultProtocolKVStoreCacheSize, DefaultProtocolKVStoreByBlockIDCacheSize)
+
+		blockID := unittest.IdentifierFixture()
+		_, err := store.ByBlockID(blockID)
+		require.ErrorIs(t, err, storage.ErrNotFound)
+	})
+}
+
+// TestProtocolKVStore_ByID tests that ByID returns an error if no snapshot with the given Identifier is known.
+func TestProtocolKVStore_ByID(t *testing.T) {
+	unittest.RunWithBadgerDB(t, func(db *badger.DB) {
+		metrics := metrics.NewNoopCollector()
+		store := NewProtocolKVStore(metrics, db, DefaultProtocolKVStoreCacheSize, DefaultProtocolKVStoreByBlockIDCacheSize)
+
+		stateID := unittest.IdentifierFixture()
+		_, err := store.ByID(stateID)
+		require.ErrorIs(t, err, storage.ErrNotFound)
+	})
+}
