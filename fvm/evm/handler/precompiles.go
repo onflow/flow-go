cac590e9b1f2d417f98e359f0640eee5844bfc1f
ramtinms <ramtin.seraj@dapperlabs.com>
2024-02-14 18:03:32 -0800

    clean up

--- fvm/evm/handler/precompiles.go
+++ fvm/evm/handler/precompiles.go
@@ -7,11 +7,26 @@ import (
 	"github.com/onflow/cadence/runtime/sema"
 
 	"github.com/onflow/flow-go/fvm/environment"
+	"github.com/onflow/flow-go/fvm/evm/precompiles"
 	"github.com/onflow/flow-go/fvm/evm/types"
 	"github.com/onflow/flow-go/model/flow"
 )
 
-func BlockHeightProvider(backend types.Backend) func() (uint64, error) {
+func preparePrecompiles(
+	evmContractAddress flow.Address,
+	addressAllocator types.AddressAllocator,
+	backend types.Backend,
+) []types.Precompile {
+	archAddress := addressAllocator.AllocatePrecompileAddress(1)
+	archContract := precompiles.ArchContract(
+		archAddress,
+		blockHeightProvider(backend),
+		coaOwnershipProofValidator(evmContractAddress, backend),
+	)
+	return []types.Precompile{archContract}
+}
+
+func blockHeightProvider(backend types.Backend) func() (uint64, error) {
 	return func() (uint64, error) {
 		h, err := backend.GetCurrentBlockHeight()
 		// if is a fatal error - panic
@@ -22,7 +37,7 @@ func BlockHeightProvider(backend types.Backend) func() (uint64, error) {
 	}
 }
 
-func COAOwnershipProofValidator(contractAddress flow.Address, backend types.Backend) func(proof *types.COAOwnershipProofInContext) (bool, error) {
+func coaOwnershipProofValidator(contractAddress flow.Address, backend types.Backend) func(proof *types.COAOwnershipProofInContext) (bool, error) {
 	return func(proof *types.COAOwnershipProofInContext) (bool, error) {
 		value, err := backend.Invoke(
 			environment.ContractFunctionSpec{
