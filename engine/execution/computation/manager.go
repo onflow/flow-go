b03c66ee1d5183f767fbcd1759890567fb5b7784
Janez Podhostnik <janez.podhostnik@gmail.com>
2024-02-22 20:16:14 +0100

    use diffeernt approach with fvm ctx

--- engine/execution/computation/manager.go
+++ engine/execution/computation/manager.go
@@ -243,6 +243,7 @@ func DefaultFVMOptions(chainID flow.ChainID, cadenceTracing bool, extensiveTraci
 					CapabilityControllersEnabled: chainID != flow.Mainnet,
 				},
 			)),
+		fvm.WithEVMEnabled(true),
 	}
 
 	if extensiveTracing {
