8521cb19b1c6a93504057c02ad6885ba9c2206f7
Josh Hannan <hannanjoshua19@gmail.com>
2024-02-26 14:41:42 -0600

    add addresses for some contracts

--- cmd/util/ledger/migrations/change_contract_code_migration.go
+++ cmd/util/ledger/migrations/change_contract_code_migration.go
@@ -79,14 +79,20 @@ func SystemContractChanges(chainID flow.ChainID, options SystemContractChangesOp
 	case flow.Mainnet:
 		env.StakingCollectionAddress = "0x8d0e87b65159ae63"
 		env.StakingProxyAddress = "0x62430cf28c26d095"
+		env.ViewResolverAddress = "0x1d7e57aa55817448"
+		env.FungibleTokenMetadataViewsAddress = "0xf233dcee88fe0abe"
 
 	case flow.Testnet:
 		env.StakingCollectionAddress = "0x95e019a17d0e23d7"
 		env.StakingProxyAddress = "0x7aad92e5a0715d21"
+		env.ViewResolverAddress = "0x631e88ae7f1d7c20"
+		env.FungibleTokenMetadataViewsAddress = "0x9a0766d93b6608b7"
 
 	case flow.Emulator, flow.Localnet:
 		env.StakingCollectionAddress = env.ServiceAccountAddress
 		env.StakingProxyAddress = env.ServiceAccountAddress
+		env.ViewResolverAddress = env.ServiceAccountAddress
+		env.FungibleTokenMetadataViewsAddress = env.ServiceAccountAddress
 
 	default:
 		panic(fmt.Errorf("unsupported chain ID: %s", chainID))
