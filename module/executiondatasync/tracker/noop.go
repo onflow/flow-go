0142cd00c1c2f6549e361fba7f989d1a050f0d99
Leo Zhang (zhangchiqing) <zhangchiqing@gmail.com>
2024-02-28 11:19:59 -0800

    fix noop

--- module/executiondatasync/tracker/noop.go
+++ module/executiondatasync/tracker/noop.go
@@ -1,10 +1,17 @@
 package tracker
 
+import "github.com/ipfs/go-cid"
+
 type NoopStorage struct{}
 
 var _ Storage = (*NoopStorage)(nil)
 
-func (s *NoopStorage) Update(UpdateFn) error { return nil }
+func (s *NoopStorage) Update(update UpdateFn) error {
+	return update(func(blockHeight uint64, cids ...cid.Cid) error {
+		return nil
+	})
+}
+
 func (s *NoopStorage) GetFulfilledHeight() (uint64, error) {
 	return 0, nil
 }
