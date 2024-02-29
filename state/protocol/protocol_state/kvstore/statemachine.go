26b016984c48a6664f6a21e812351f8a914d37b3
Yurii Oleksyshyn <yuraolex@gmail.com>
2024-02-29 19:15:33 +0200

    Added basic logic for processing version upgrade in state machine

--- state/protocol/protocol_state/kvstore/statemachine.go
+++ state/protocol/protocol_state/kvstore/statemachine.go
@@ -1,12 +1,14 @@
 package kvstore
 
 import (
+	"fmt"
 	"github.com/onflow/flow-go/model/flow"
 	"github.com/onflow/flow-go/state/protocol/protocol_state"
 )
 
 type ProcessingStateMachine struct {
-	view uint64
+	view        uint64
+	parentState protocol_state.Reader
 }
 
 var _ protocol_state.KeyValueStoreStateMachine = (*ProcessingStateMachine)(nil)
@@ -17,22 +19,37 @@ func NewProcessingStateMachine(view uint64) *ProcessingStateMachine {
 	}
 }
 
-func (ProcessingStateMachine) Build() (updatedState protocol_state.Reader, stateID flow.Identifier, hasChanges bool) {
+func (m *ProcessingStateMachine) Build() (updatedState protocol_state.Reader, stateID flow.Identifier, hasChanges bool) {
 	//TODO implement me
 	panic("implement me")
 }
 
-func (ProcessingStateMachine) ProcessUpdate(update *flow.ServiceEvent) error {
-	//TODO implement me
-	panic("implement me")
+func (m *ProcessingStateMachine) ProcessUpdate(update *flow.ServiceEvent) error {
+	switch update.Type {
+	case flow.ServiceEventProtocolStateVersionUpgrade:
+		versionUpgrade, ok := update.Event.(*flow.ProtocolStateVersionUpgrade)
+		if !ok {
+			return fmt.Errorf("internal invalid type for ProtocolStateVersionUpgrade: %T", update.Event)
+		}
+
+		if m.parentState.GetProtocolStateVersion() >= versionUpgrade.NewProtocolStateVersion {
+			return fmt.Errorf("invalid protocol state version upgrade: %d -> %d",
+				m.parentState.GetProtocolStateVersion(), versionUpgrade.NewProtocolStateVersion)
+		}
+
+		// set new protocol version and activation view.
+
+	default:
+		return nil
+	}
+
+	return nil
 }
 
-func (ProcessingStateMachine) View() uint64 {
-	//TODO implement me
-	panic("implement me")
+func (m *ProcessingStateMachine) View() uint64 {
+	return m.view
 }
 
-func (ProcessingStateMachine) ParentState() protocol_state.Reader {
-	//TODO implement me
-	panic("implement me")
+func (m *ProcessingStateMachine) ParentState() protocol_state.Reader {
+	return m.parentState
 }
