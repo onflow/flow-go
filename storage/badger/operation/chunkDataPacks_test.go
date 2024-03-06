534de8ce23e1ce132824c1c04535fbfa14073227
Leo Zhang (zhangchiqing) <zhangchiqing@gmail.com>
2024-03-05 16:29:11 -0800

    fix codec

--- storage/badger/operation/chunkDataPacks_test.go
+++ storage/badger/operation/chunkDataPacks_test.go
@@ -7,7 +7,7 @@ import (
 	"github.com/ipfs/go-cid"
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
-	"github.com/vmihailenco/msgpack"
+	"github.com/vmihailenco/msgpack/v4"
 
 	"github.com/onflow/flow-go/model/flow"
 	storagemodel "github.com/onflow/flow-go/storage/badger/model"
