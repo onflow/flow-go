d7ffbf8fd6fa9b410f0b2b8990e1bee786c473ca
Bastian Müller <bastian@turbolent.com>
2024-03-08 16:31:41 -0800

    remove known problematic accounts

--- cmd/util/ledger/migrations/account_based_migration.go
+++ cmd/util/ledger/migrations/account_based_migration.go
@@ -315,19 +315,7 @@ func MigrateGroupConcurrently(
 	return migrated, nil
 }
 
-var knownProblematicAccounts = map[common.Address]string{
-	// Testnet accounts with broken contracts
-	mustHexToAddress("434a1f199a7ae3ba"): "Broken contract FanTopPermission",
-	mustHexToAddress("454c9991c2b8d947"): "Broken contract Test",
-	mustHexToAddress("48602d8056ff9d93"): "Broken contract FanTopPermission",
-	mustHexToAddress("5d63c34d7f05e5a4"): "Broken contract FanTopPermission",
-	mustHexToAddress("5e3448b3cffb97f2"): "Broken contract FanTopPermission",
-	mustHexToAddress("7d8c7e050c694eaa"): "Broken contract Test",
-	mustHexToAddress("ba53f16ede01972d"): "Broken contract FanTopPermission",
-	mustHexToAddress("c843c1f5a4805c3a"): "Broken contract FanTopPermission",
-	mustHexToAddress("48d3be92e6e4a973"): "Broken contract FanTopPermission",
-	// Mainnet account
-}
+var knownProblematicAccounts = map[common.Address]string{}
 
 func mustHexToAddress(hex string) common.Address {
 	address, err := common.HexToAddress(hex)
