ba562db1f2569bca0d9ef2a2f18716c394817e4b
Leo Zhang (zhangchiqing) <zhangchiqing@gmail.com>
2024-04-01 16:48:20 -0700

    move deduplicate to flow package

--- model/flow/entity.go
+++ model/flow/entity.go
@@ -1,5 +1,11 @@
 package flow
 
+type IDEntity interface {
+	// ID returns a unique id for this entity using a hash of the immutable
+	// fields of the entity.
+	ID() Identifier
+}
+
 // Entity defines how flow entities should be defined
 // Entities are flat data structures holding multiple data fields.
 // Entities don't include nested entities, they only include pointers to
@@ -7,10 +13,7 @@ package flow
 // of keeping a slice of entity object itself. This simplifies storage, signature and validation
 // of entities.
 type Entity interface {
-
-	// ID returns a unique id for this entity using a hash of the immutable
-	// fields of the entity.
-	ID() Identifier
+	IDEntity
 
 	// Checksum returns a unique checksum for the entity, including the mutable
 	// data such as signatures.
@@ -23,4 +26,22 @@ func EntitiesToIDs[T Entity](entities []T) []Identifier {
 		ids = append(ids, entity.ID())
 	}
 	return ids
+}
+
+// Deduplicate entities in a slice by the ID method
+func Deduplicate[T IDEntity](entities []T) []T {
+	seen := make(map[Identifier]struct{}, len(entities))
+	result := make([]T, 0, len(entities))
+
+	for _, entity := range entities {
+		id := entity.ID()
+		if _, ok := seen[id]; ok {
+			continue
+		}
+
+		seen[id] = struct{}{}
+		result = append(result, entity)
+	}
+
+	return result
 }
