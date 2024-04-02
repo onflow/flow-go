cf8a91fd8424bfd1d2c6956bf061722ec09f52eb
Yurii Oleksyshyn <yuraolex@gmail.com>
2024-04-02 15:31:36 +0300

    Added tests for asserting Build functionality

--- state/protocol/protocol_state/epochs/statemachine_test.go
+++ state/protocol/protocol_state/epochs/statemachine_test.go
@@ -4,6 +4,7 @@ import (
 	"errors"
 	"github.com/onflow/flow-go/module/irrecoverable"
 	"github.com/onflow/flow-go/state/protocol/protocol_state/epochs"
+	"github.com/onflow/flow-go/storage/badger/transaction"
 	"github.com/stretchr/testify/assert"
 	mocks "github.com/stretchr/testify/mock"
 	"testing"
@@ -29,7 +30,6 @@ func TestEpochStateMachine(t *testing.T) {
 type EpochStateMachineSuite struct {
 	suite.Suite
 	epochStateDB                    *storagemock.ProtocolState
-	headersDB                       *storagemock.Headers
 	setupsDB                        *storagemock.EpochSetups
 	commitsDB                       *storagemock.EpochCommits
 	globalParams                    *protocolmock.GlobalParams
@@ -85,7 +85,66 @@ func (s *EpochStateMachineSuite) SetupTest() {
 	require.NoError(s.T(), err)
 }
 
-// TestStateMutator_Constructor tests the behaviour of the StateMutator constructor.
+// TestOnHappyPathNoDbChanges tests that stateMutator doesn't cache any db updates when there are no changes.
+func (s *EpochStateMachineSuite) TestOnHappyPathNoDbChanges() {
+	//s.stateMachine.On("ParentState").Return(parentState)
+	//s.stateMachine.On("Build").Return(parentState.ProtocolStateEntry, parentState.ID(), false)
+
+	err := s.stateMachine.ProcessUpdate(nil)
+	require.NoError(s.T(), err)
+
+	indexTxDeferredUpdate := storagemock.NewDeferredDBUpdate(s.T())
+	indexTxDeferredUpdate.On("Execute", mocks.Anything).Return(nil).Once()
+	storeTxDeferredUpdate := storagemock.NewDeferredDBUpdate(s.T())
+	storeTxDeferredUpdate.On("Execute", mocks.Anything).Return(nil).Once()
+
+	dbUpdates := s.stateMachine.Build()
+	require.Empty(s.T(), dbUpdates)
+}
+
+// TestHappyPathWithDbChanges tests that `stateMutator` returns cached db updates when building protocol state after applying service events.
+// Whenever `stateMutator` successfully processes an epoch setup or epoch commit event, it has to create a deferred db update to store the event.
+// Deferred db updates are cached in `stateMutator` and returned when building protocol state when calling `Build`.
+func (s *EpochStateMachineSuite) TestHappyPathWithDbChanges() {
+	//s.stateMachine.On("ParentState").Return(parentState)
+	//s.stateMachine.On("Build").Return(unittest.ProtocolStateFixture().ProtocolStateEntry,
+	//	unittest.IdentifierFixture(), true)
+
+	epochSetup := unittest.EpochSetupFixture()
+	epochSetupServiceEvent := epochSetup.ServiceEvent()
+	epochCommit := unittest.EpochCommitFixture()
+	epochCommitServiceEvent := epochCommit.ServiceEvent()
+
+	//epochSetupStored := mock.Mock{}
+	//epochSetupStored.On("EpochSetupStored").Return()
+	//s.stateMachine.On("ProcessEpochSetup", epochSetup).Return(true, nil).Once()
+	//s.setupsDB.On("StoreTx", epochSetup).Return(func(*transaction.Tx) error {
+	//	epochSetupStored.MethodCalled("EpochSetupStored")
+	//	return nil
+	//}).Once()
+
+	//epochCommitStored := mock.Mock{}
+	//epochCommitStored.On("EpochCommitStored").Return()
+	//s.stateMachine.On("ProcessEpochCommit", epochCommit).Return(true, nil).Once()
+	//s.commitsDB.On("StoreTx", epochCommit).Return(func(*transaction.Tx) error {
+	//	epochCommitStored.MethodCalled("EpochCommitStored")
+	//	return nil
+	//}).Once()
+
+	err := s.stateMachine.ProcessUpdate([]*flow.ServiceEvent{&epochSetupServiceEvent, &epochCommitServiceEvent})
+	require.NoError(s.T(), err)
+
+	dbUpdates := s.stateMachine.Build()
+	// in next loop we assert that we have received expected deferred db updates by executing them
+	// and expecting that corresponding mock methods will be called
+	tx := &transaction.Tx{}
+	for _, dbUpdate := range dbUpdates {
+		err := dbUpdate(tx)
+		require.NoError(s.T(), err)
+	}
+}
+
+// TestEpochStateMachine_Constructor tests the behavior of the StateMutator constructor.
 // We expect the constructor to select the appropriate state machine constructor, and
 // to handle (pass-through) exceptions from the state machine constructor.
 func (s *EpochStateMachineSuite) TestEpochStateMachine_Constructor() {
@@ -294,6 +353,103 @@ func (s *EpochStateMachineSuite) TestEpochStateMachine_Constructor() {
 			assert.ErrorIs(s.T(), err, exception)
 			assert.Nil(s.T(), stateMachine)
 		})
+	})
+}
+
+// TestProcessUpdate_InvalidEpochSetup tests that handleServiceEvents rejects invalid epoch setup event and sets
+// InvalidEpochTransitionAttempted flag in protocol.ProtocolStateMachine.
+func (s *EpochStateMachineSuite) TestProcessUpdate_InvalidEpochSetup() {
+	s.Run("invalid-epoch-setup", func() {
+		happyPathStateMachineFactory := mock.NewStateMachineFactoryMethod(s.T())
+		happyPathStateMachineFactory.On("Execute", s.candidate.View, s.parentEpochState).Return(s.happyPathStateMachine, nil).Once()
+		fallbackPathStateMachineFactory := mock.NewStateMachineFactoryMethod(s.T())
+		stateMachine, err := epochs.NewEpochStateMachine(
+			s.candidate,
+			s.globalParams,
+			s.setupsDB,
+			s.commitsDB,
+			s.epochStateDB,
+			s.parentState,
+			s.mutator,
+			happyPathStateMachineFactory.Execute,
+			fallbackPathStateMachineFactory.Execute,
+		)
+		require.NoError(s.T(), err)
+
+		epochSetup := unittest.EpochSetupFixture()
+		serviceEvent := epochSetup.ServiceEvent()
+
+		s.happyPathStateMachine.On("ParentState").Return(s.parentEpochState)
+		s.happyPathStateMachine.On("ProcessEpochSetup", epochSetup).
+			Return(false, protocol.NewInvalidServiceEventErrorf("")).Once()
+
+		fallbackStateMachine := mock.NewStateMachine(s.T())
+		fallbackStateMachine.On("ProcessEpochSetup", epochSetup).Return(false, nil).Once()
+		fallbackPathStateMachineFactory.On("Execute", s.candidate.View, s.parentEpochState).Return(fallbackStateMachine, nil).Once()
+
+		err = stateMachine.ProcessUpdate([]*flow.ServiceEvent{&serviceEvent})
+		require.NoError(s.T(), err)
+	})
+	s.Run("process-epoch-setup-exception", func() {
+		epochSetup := unittest.EpochSetupFixture()
+		serviceEvent := epochSetup.ServiceEvent()
+
+		exception := errors.New("exception")
+		s.happyPathStateMachine.On("ProcessEpochSetup", epochSetup).Return(false, exception).Once()
+
+		err := s.stateMachine.ProcessUpdate([]*flow.ServiceEvent{&serviceEvent})
+		require.Error(s.T(), err)
+		require.False(s.T(), protocol.IsInvalidServiceEventError(err))
+	})
+}
+
+// TestProcessUpdate_InvalidEpochCommit tests that handleServiceEvents rejects invalid epoch commit event and sets
+// InvalidEpochTransitionAttempted flag in protocol.ProtocolStateMachine.
+func (s *EpochStateMachineSuite) TestProcessUpdate_InvalidEpochCommit() {
+	s.Run("invalid-epoch-commit", func() {
+		happyPathStateMachineFactory := mock.NewStateMachineFactoryMethod(s.T())
+		happyPathStateMachineFactory.On("Execute", s.candidate.View, s.parentEpochState).Return(s.happyPathStateMachine, nil).Once()
+		fallbackPathStateMachineFactory := mock.NewStateMachineFactoryMethod(s.T())
+		stateMachine, err := epochs.NewEpochStateMachine(
+			s.candidate,
+			s.globalParams,
+			s.setupsDB,
+			s.commitsDB,
+			s.epochStateDB,
+			s.parentState,
+			s.mutator,
+			happyPathStateMachineFactory.Execute,
+			fallbackPathStateMachineFactory.Execute,
+		)
+		require.NoError(s.T(), err)
+
+		epochCommit := unittest.EpochCommitFixture()
+		serviceEvent := epochCommit.ServiceEvent()
+
+		s.happyPathStateMachine.On("ParentState").Return(s.parentEpochState)
+		s.happyPathStateMachine.On("ProcessEpochCommit", epochCommit).
+			Return(false, protocol.NewInvalidServiceEventErrorf("")).Once()
+
+		fallbackStateMachine := mock.NewStateMachine(s.T())
+		fallbackStateMachine.On("ProcessEpochCommit", epochCommit).Return(false, nil).Once()
+		fallbackPathStateMachineFactory.On("Execute", s.candidate.View, s.parentEpochState).Return(fallbackStateMachine, nil).Once()
+
+		err = stateMachine.ProcessUpdate([]*flow.ServiceEvent{&serviceEvent})
+		require.NoError(s.T(), err)
+	})
+	s.Run("process-epoch-commit-exception", func() {
+		//parentState := unittest.ProtocolStateFixture()
+		//s.stateMachine.On("ParentState").Return(parentState)
+
+		epochCommit := unittest.EpochCommitFixture()
+		serviceEvent := epochCommit.ServiceEvent()
+
+		exception := errors.New("exception")
+		s.happyPathStateMachine.On("ProcessEpochCommit", epochCommit).Return(false, exception).Once()
+
+		err := s.stateMachine.ProcessUpdate([]*flow.ServiceEvent{&serviceEvent})
+		require.Error(s.T(), err)
+		require.False(s.T(), protocol.IsInvalidServiceEventError(err))
 	})
 }
 
