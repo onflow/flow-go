658a0fb2609c39f685c8ea157a850c7af0a782cc
Alexander Hentschel <alex.hentschel@axiomzen.co>
2024-04-08 17:20:31 -0700

    added unit test for `AlwaysEvolveStateWrapper`

--- state/protocol/protocol_state/state/always_evolvestate_wrapper.go
+++ state/protocol/protocol_state/state/always_evolvestate_wrapper.go
@@ -10,6 +10,20 @@ import (
 
 var _ ps.OrthogonalStoreStateMachine[any] = (*AlwaysEvolveStateWrapper[any])(nil)
 
+// AlwaysEvolveStateWrapper wraps a `protocol_state.OrthogonalStoreStateMachine`. The wrapper guarantees that
+// the state machine's `EvolveState(…)` is always called before its `Build()` method. If it hasn't been executed
+// before, the wrapper will call the state machine's `EvolveState(…)` method first in its `Build()` step.
+//
+// NOT CONCURRENCY SAFE
+type AlwaysEvolveStateWrapper[P any] struct {
+	ps.OrthogonalStoreStateMachine[P]
+	evolveStateCalled bool
+}
+
+// NewAlwaysEvolveStateWrapper adds a wrapper to the given `stateMachine`, which guarantees that
+// `stateMachine.EvolveState(…)` is always called before `stateMachine.Build()`. When the external logic
+// calls `AlwaysEvolveStateWrapper.Build()`, the wrapper will call `stateMachine.EvolveState( empty )`
+// first, if and only if it hasn't been called before. The input `empty` is an empty list of service events.
 func NewAlwaysEvolveStateWrapper[P any](stateMachine ps.OrthogonalStoreStateMachine[P]) ps.OrthogonalStoreStateMachine[P] {
 	return &AlwaysEvolveStateWrapper[P]{
 		OrthogonalStoreStateMachine: stateMachine,
@@ -17,22 +31,20 @@ func NewAlwaysEvolveStateWrapper[P any](stateMachine ps.OrthogonalStoreStateMach
 	}
 }
 
-type AlwaysEvolveStateWrapper[P any] struct {
-	ps.OrthogonalStoreStateMachine[P]
-	evolveStateCalled bool
-}
-
+// Build calls the state machine's `EvolveState(…)` method first with an empty list
+// of Service Events, if and only if `EvolveState` hasn't been called before.
 func (a *AlwaysEvolveStateWrapper[P]) Build() (*protocol.DeferredBlockPersist, error) {
 	if !a.evolveStateCalled {
 		err := a.OrthogonalStoreStateMachine.EvolveState([]flow.ServiceEvent{})
 		if err != nil {
-			return protocol.NewDeferredBlockPersist(),
-				fmt.Errorf("attempting to execute EvolveState method with empty list of Service Events failed: %w", err)
+			return protocol.NewDeferredBlockPersist(), fmt.Errorf("attempting to execute EvolveState method with empty list of Service Events failed: %w", err)
 		}
 	}
 	return a.OrthogonalStoreStateMachine.Build()
 }
 
+// EvolveState passes the call to the wrapped state machine. Thereafter, the wrapper will abstain
+// from calling `EvolveState` during the `Build` step, irrespective of `EvolveState`'s return here.
 func (a *AlwaysEvolveStateWrapper[P]) EvolveState(sealedServiceEvents []flow.ServiceEvent) error {
 	a.evolveStateCalled = true
 	return a.OrthogonalStoreStateMachine.EvolveState(sealedServiceEvents)
---
+++ state/protocol/protocol_state/state/always_evolvestate_wrapper_test.go
@@ -0,0 +1,141 @@
+package state_test
+
+import (
+	"fmt"
+	"testing"
+
+	"github.com/stretchr/testify/mock"
+	"github.com/stretchr/testify/require"
+	"go.uber.org/atomic"
+
+	"github.com/onflow/flow-go/model/flow"
+	"github.com/onflow/flow-go/state/protocol"
+	ps_mock "github.com/onflow/flow-go/state/protocol/protocol_state/mock"
+	"github.com/onflow/flow-go/state/protocol/protocol_state/state"
+	"github.com/onflow/flow-go/storage/badger/transaction"
+	"github.com/onflow/flow-go/utils/unittest"
+)
+
+// TestView verifies that the AlwaysEvolveStateWrapper correctly report the view from the wrapped state machine
+func TestView(t *testing.T) {
+	view := uint64(127)
+	stateMachine := ps_mock.NewOrthogonalStoreStateMachine[interface{}](t)
+	stateMachine.On("View").Return(view).Once()
+	wrapper := state.NewAlwaysEvolveStateWrapper[interface{}](stateMachine)
+	require.Equal(t, view, wrapper.View())
+}
+
+// TestParentState verifies that the AlwaysEvolveStateWrapper correctly reports the wrapped state machine's ParentState
+func TestParentState(t *testing.T) {
+	type customType string
+	var parentState customType = "some state"
+	stateMachine := ps_mock.NewOrthogonalStoreStateMachine[customType](t)
+	stateMachine.On("ParentState").Return(parentState).Once()
+	wrapper := state.NewAlwaysEvolveStateWrapper[customType](stateMachine)
+	require.Equal(t, parentState, wrapper.ParentState())
+}
+
+// TestEvolveState verifies that the `AlwaysEvolveStateWrapper.EvolveState(serviceEvents)` correctly passes the input
+// Service Events to the wrapped state machine and passes back its respective outputs.
+func TestEvolveState(t *testing.T) {
+	serviceEvents := []flow.ServiceEvent{
+		unittest.ProtocolStateVersionUpgradeFixture().ServiceEvent(),
+		unittest.EpochCommitFixture().ServiceEvent(),
+	}
+
+	t.Run("Input passed to wrapped state machine", func(t *testing.T) {
+		stateMachine := ps_mock.NewOrthogonalStoreStateMachine[interface{}](t)
+		stateMachine.On("EvolveState", serviceEvents).Return(nil).Once()
+		wrapper := state.NewAlwaysEvolveStateWrapper[interface{}](stateMachine)
+		require.NoError(t, wrapper.EvolveState(serviceEvents))
+	})
+
+	t.Run("wrapped state machine errors", func(t *testing.T) {
+		customError := fmt.Errorf("custom error")
+		stateMachine := ps_mock.NewOrthogonalStoreStateMachine[interface{}](t)
+		stateMachine.On("EvolveState", serviceEvents).Return(customError).Once()
+		wrapper := state.NewAlwaysEvolveStateWrapper[interface{}](stateMachine)
+		require.ErrorIs(t, wrapper.EvolveState(serviceEvents), customError, "error should be bubbled up")
+	})
+
+}
+
+// TestBuild verifies that the wrapped state machine's `EvolveState(..)` is called by the wrapper
+// if and only if it wasn't called before. Furthermore, we test that the wrapper's `Build` method bubbles up
+// errors returned by either the state machine's `Build()` or `EvolveState` method.
+func TestBuild(t *testing.T) {
+	//epochSetup := unittest.EpochSetupFixture().ServiceEvent()
+	//epochCommit := unittest.EpochCommitFixture().ServiceEvent()
+	//versionBeacon := unittest.VersionBeaconFixture().ServiceEvent()
+	//protocolVersionUpgrade := unittest.ProtocolStateVersionUpgradeFixture().ServiceEvent()
+
+	serviceEvents := []flow.ServiceEvent{
+		unittest.ProtocolStateVersionUpgradeFixture().ServiceEvent(),
+		unittest.EpochCommitFixture().ServiceEvent(),
+	}
+
+	someIndexingOperation := func(blockID flow.Identifier, tx *transaction.Tx) error { return nil }
+	dbPersist := protocol.NewDeferredBlockPersist().AddIndexingOp(someIndexingOperation)
+
+	t.Run("Happy Path: external logic calls EvolveState before Build", func(t *testing.T) {
+		stateMachine := ps_mock.NewOrthogonalStoreStateMachine[interface{}](t)
+		wrapper := state.NewAlwaysEvolveStateWrapper[interface{}](stateMachine)
+
+		// external logic calls `EvolveState` first, which returns with no error
+		stateMachine.On("EvolveState", serviceEvents).Return(nil).Once()
+		require.NoError(t, wrapper.EvolveState(serviceEvents))
+
+		// external logic subsequently calls `Build`, the wrapper should *not* call EvolveState again:
+		stateMachine.On("Build").Return(dbPersist, nil).Once()
+		deferredDb, err := wrapper.Build()
+		require.NoError(t, err)
+		require.Equal(t, dbPersist, deferredDb)
+	})
+
+	t.Run("Happy Path: external logic calls only Build", func(t *testing.T) {
+		stateMachine := ps_mock.NewOrthogonalStoreStateMachine[interface{}](t)
+		wrapper := state.NewAlwaysEvolveStateWrapper[interface{}](stateMachine)
+
+		// We haven't called `EvolveState` but call `Build` right away. The wrapper should
+		//  (1) execute `EvolveState` on the wrapped state machine with an _empty_ list of service events _first_
+		//  (2) call `Build` on the wrapped state machine
+		evolveStateCalled := atomic.NewBool(false)
+		stateMachine.On("EvolveState", mock.Anything).Run(func(args mock.Arguments) {
+			require.Empty(t, args.Get(0).([]flow.ServiceEvent), "expecting empty list of service events")
+			evolveStateCalled.Store(true)
+		}).Return(nil).Once()
+		stateMachine.On("Build").Run(func(args mock.Arguments) {
+			require.True(t, evolveStateCalled.Load(), "Method EvolveState should have been called first")
+		}).Return(dbPersist, nil).Once()
+
+		deferredDb, err := wrapper.Build()
+		require.NoError(t, err)
+		require.Equal(t, dbPersist, deferredDb)
+	})
+
+	t.Run("Unhappy Path: wrapped state machine errors on EvolveState", func(t *testing.T) {
+		customError := fmt.Errorf("custom error")
+		stateMachine := ps_mock.NewOrthogonalStoreStateMachine[interface{}](t)
+		wrapper := state.NewAlwaysEvolveStateWrapper[interface{}](stateMachine)
+
+		// We haven't called `EvolveState` but call `Build` right away. The wrapper should execute `EvolveState`
+		// on the wrapped state machine. If this errors, the wrapper should return the error, _without_ calling `Build`
+		stateMachine.On("EvolveState", mock.Anything).Return(customError).Once()
+		_, err := wrapper.Build()
+		require.ErrorIs(t, err, customError, "error should be bubbled up")
+	})
+
+	t.Run("Unhappy Path: wrapped state machine errors on Build", func(t *testing.T) {
+		customError := fmt.Errorf("custom error")
+		stateMachine := ps_mock.NewOrthogonalStoreStateMachine[interface{}](t)
+		wrapper := state.NewAlwaysEvolveStateWrapper[interface{}](stateMachine)
+
+		// We haven't called `EvolveState` but call `Build` right away. The wrapper should execute `EvolveState` on
+		// the wrapped state machine first and only then `Build`. Errors return by the second step sould be bubbled up.
+		stateMachine.On("EvolveState", mock.Anything).Return(nil).Once()
+		stateMachine.On("Build").Return(dbPersist, customError).Once()
+
+		_, err := wrapper.Build()
+		require.ErrorIs(t, err, customError, "error should be bubbled up")
+	})
+}
