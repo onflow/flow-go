6ab8300125afbdf45d6d275d8e7628da9c2f0bb9
Faye Amacker <33205765+fxamacker@users.noreply.github.com>
2024-04-09 11:08:21 -0500

    Move reusable migration funcs to migrations/utils.go

--- cmd/util/ledger/migrations/atree_register_migration.go
+++ cmd/util/ledger/migrations/atree_register_migration.go
@@ -14,7 +14,6 @@ import (
 	"github.com/onflow/cadence/runtime"
 	"github.com/onflow/cadence/runtime/common"
 	"github.com/onflow/cadence/runtime/interpreter"
-	"github.com/onflow/cadence/runtime/stdlib"
 
 	"github.com/onflow/flow-go/cmd/util/ledger/reporters"
 	"github.com/onflow/flow-go/fvm/environment"
@@ -178,41 +177,6 @@ func (m *AtreeRegisterMigrator) MigrateAccount(
 	}
 
 	return newPayloads, nil
-}
-
-func checkStorageHealth(
-	address common.Address,
-	storage *runtime.Storage,
-	payloads []*ledger.Payload,
-) error {
-
-	for _, payload := range payloads {
-		registerID, _, err := convert.PayloadToRegister(payload)
-		if err != nil {
-			return fmt.Errorf("failed to convert payload to register: %w", err)
-		}
-
-		if !registerID.IsSlabIndex() {
-			continue
-		}
-
-		// Convert the register ID to a storage ID.
-		slabID := atree.NewStorageID(
-			atree.Address([]byte(registerID.Owner)),
-			atree.StorageIndex([]byte(registerID.Key[1:])))
-
-		// Retrieve the slab.
-		_, _, err = storage.Retrieve(slabID)
-		if err != nil {
-			return fmt.Errorf("failed to retrieve slab %s: %w", slabID, err)
-		}
-	}
-
-	for _, domain := range domains {
-		_ = storage.GetStorageMap(address, domain, false)
-	}
-
-	return storage.CheckHealth()
 }
 
 func (m *AtreeRegisterMigrator) migrateAccountStorage(
@@ -492,25 +456,6 @@ func capturePanic(f func()) (err error) {
 	f()
 
 	return
-}
-
-// convert all domains
-var domains = []string{
-	common.PathDomainStorage.Identifier(),
-	common.PathDomainPrivate.Identifier(),
-	common.PathDomainPublic.Identifier(),
-	runtime.StorageDomainContract,
-	stdlib.InboxStorageDomain,
-	stdlib.CapabilityControllerStorageDomain,
-}
-
-var domainsLookupMap = map[string]struct{}{
-	common.PathDomainStorage.Identifier():    {},
-	common.PathDomainPrivate.Identifier():    {},
-	common.PathDomainPublic.Identifier():     {},
-	runtime.StorageDomainContract:            {},
-	stdlib.InboxStorageDomain:                {},
-	stdlib.CapabilityControllerStorageDomain: {},
 }
 
 // migrationProblem is a struct for reporting errors
