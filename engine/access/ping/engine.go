e665d5b970fd877bd584c1d6fd5d9195ac4c5051
Peter Argue <89119817+peterargue@users.noreply.github.com>
2024-04-11 12:15:27 -0700

    improve jitter, limit concurrent pings

--- engine/access/ping/engine.go
+++ engine/access/ping/engine.go
@@ -2,11 +2,10 @@ package ping
 
 import (
 	"context"
-	"encoding/binary"
-	"sync"
 	"time"
 
 	"github.com/rs/zerolog"
+	"golang.org/x/sync/errgroup"
 
 	"github.com/onflow/flow-go/model/flow"
 	"github.com/onflow/flow-go/model/flow/filter"
@@ -23,6 +22,12 @@ const (
 
 	// PingInterval is the interval between pings to remote nodes
 	PingInterval = time.Minute
+
+	// MaxConcurrentPings is the maximum number of ping requests that can be sent concurrently
+	MaxConcurrentPings = 100
+
+	// MaxJitter is the maximum time to pause between nodes during ping
+	MaxJitter = 5 * time.Second
 )
 
 type Engine struct {
@@ -101,31 +106,29 @@ func (e *Engine) pingLoop(ctx irrecoverable.SignalerContext, ready component.Rea
 }
 
 func (e *Engine) pingAllNodes(ctx context.Context) {
-	ctx, cancel := context.WithTimeout(ctx, PingTimeout)
-	defer cancel()
+	g := new(errgroup.Group)
 
-	wg := sync.WaitGroup{}
+	// restrict the number of concurrently running ping requests.
+	g.SetLimit(MaxConcurrentPings)
 
 	peers := e.idProvider.Identities(filter.Not(filter.HasNodeID[flow.Identity](e.me.NodeID())))
-	for _, peer := range peers {
-		wg.Add(1)
-		go func(peer *flow.Identity) {
-			defer wg.Done()
-
-			pid := peer.ID()
-			delay := time.Duration(binary.BigEndian.Uint16(pid[:2])) % (PingInterval / time.Millisecond)
+	for i, peer := range peers {
+		peer := peer
+		g.Go(func() error {
+			delay := makeJitter(i)
 
 			select {
 			case <-ctx.Done():
-				return
+				return nil
 			case <-time.After(delay):
 			}
 
 			e.pingNode(ctx, peer)
-		}(peer)
+			return nil
+		})
 	}
 
-	wg.Wait()
+	_ = g.Wait()
 }
 
 // pingNode pings the given peer and updates the metrics with the result and the additional node information
@@ -136,6 +139,9 @@ func (e *Engine) pingNode(ctx context.Context, peer *flow.Identity) {
 		e.log.Error().Err(err).Str("peer", peer.String()).Msg("failed to get peer ID")
 		return
 	}
+
+	ctx, cancel := context.WithTimeout(ctx, PingTimeout)
+	defer cancel()
 
 	// ping the node
 	resp, rtt, pingErr := e.pingService.Ping(ctx, pid) // ping will timeout in PingTimeout seconds
@@ -155,4 +161,10 @@ func (e *Engine) pingNode(ctx context.Context, peer *flow.Identity) {
 	if pingErr == nil {
 		e.metrics.NodeInfo(peer, info, resp.Version, resp.BlockHeight, resp.HotstuffView)
 	}
+}
+
+// makeJitter returns a jitter between 0 and MaxJitter
+func makeJitter(offset int) time.Duration {
+	jitter := float64(MaxJitter) * float64(offset%MaxConcurrentPings) / float64(MaxConcurrentPings)
+	return time.Duration(jitter)
 }
