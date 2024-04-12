c6f238e03084eb3f6a02b0345ee106bfb611fbbc
Jordan Schalm <jordan@dapperlabs.com>
2024-04-12 13:04:20 -0400

    re-enable epoch integration tests

--- integration/tests/epochs/cohort2/epoch_join_and_leave_vn_test.go
+++ integration/tests/epochs/cohort2/epoch_join_and_leave_vn_test.go
@@ -8,11 +8,9 @@ import (
 
 	"github.com/onflow/flow-go/integration/tests/epochs"
 	"github.com/onflow/flow-go/model/flow"
-	"github.com/onflow/flow-go/utils/unittest"
 )
 
 func TestEpochJoinAndLeaveVN(t *testing.T) {
-	unittest.SkipUnless(t, unittest.TEST_TODO, "kvstore: sealing segment doesn't support multiple protocol states")
 	suite.Run(t, new(EpochJoinAndLeaveVNSuite))
 }
 
