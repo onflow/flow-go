2fdadaafa5853ad6d8756eaff045103c49bae1fb
Bjarte Stien Karlsen <bjarte@bjartek.org>
2024-04-19 09:57:29 +0200

    try to update to new middleware

--- engine/common/rpc/logging_interceptor.go
+++ engine/common/rpc/logging_interceptor.go
@@ -1,9 +1,10 @@
 package rpc
 
 import (
-	grpczerolog "github.com/grpc-ecosystem/go-grpc-middleware/providers/zerolog/v2"
+	"context"
+	"fmt"
+
 	"github.com/grpc-ecosystem/go-grpc-middleware/v2/interceptors/logging"
-	"github.com/grpc-ecosystem/go-grpc-middleware/v2/interceptors/tags"
 	"github.com/rs/zerolog"
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/codes"
@@ -19,7 +20,30 @@ func customClientCodeToLevel(c codes.Code) logging.Level {
 
 // LoggingInterceptor creates the logging interceptors to log incoming GRPC request and response (minus the payload body)
 func LoggingInterceptor(log zerolog.Logger) []grpc.UnaryServerInterceptor {
-	tagsInterceptor := tags.UnaryServerInterceptor(tags.WithFieldExtractor(tags.CodeGenRequestFieldExtractor))
-	loggingInterceptor := logging.UnaryServerInterceptor(grpczerolog.InterceptorLogger(log), logging.WithLevels(customClientCodeToLevel))
-	return []grpc.UnaryServerInterceptor{tagsInterceptor, loggingInterceptor}
+	//this code gen extractor was removed, is this needed? if it is we need to use context for passing these along or so they say
+	//
+	//tagsInterceptor := tags.UnaryServerInterceptor(tags.WithFieldExtractor(tags.CodeGenRequestFieldExtractor))
+	loggingInterceptor := logging.UnaryServerInterceptor(InterceptorLogger(log), logging.WithLevels(customClientCodeToLevel))
+	return []grpc.UnaryServerInterceptor{loggingInterceptor}
+}
+
+// InterceptorLogger adapts zerolog logger to interceptor logger.
+// This code is simple enough to be copied and not imported.
+func InterceptorLogger(l zerolog.Logger) logging.Logger {
+	return logging.LoggerFunc(func(ctx context.Context, lvl logging.Level, msg string, fields ...any) {
+		l := l.With().Fields(fields).Logger()
+
+		switch lvl {
+		case logging.LevelDebug:
+			l.Debug().Msg(msg)
+		case logging.LevelInfo:
+			l.Info().Msg(msg)
+		case logging.LevelWarn:
+			l.Warn().Msg(msg)
+		case logging.LevelError:
+			l.Error().Msg(msg)
+		default:
+			panic(fmt.Sprintf("unknown level %v", lvl))
+		}
+	})
 }
