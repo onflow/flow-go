5323b379bfad93691ab44a19828cd371d943b6aa
sideninja <75445744+sideninja@users.noreply.github.com>
2024-05-02 16:53:43 +0200

    cherry-pick handler test

--- fvm/evm/handler/handler_test.go
+++ fvm/evm/handler/handler_test.go
@@ -828,8 +828,9 @@ func TestHandler_TransactionRun(t *testing.T) {
 
 					gasConsumed := testutils.RandomGas(1000)
 					addr := testutils.RandomAddress(t)
-					result := func(tx *gethTypes.Transaction) *types.Result {
+					result := func(i uint16, tx *gethTypes.Transaction) *types.Result {
 						return &types.Result{
+							Index:                   i,
 							DeployedContractAddress: &addr,
 							ReturnedValue:           testutils.RandomData(t),
 							GasConsumed:             gasConsumed,
@@ -846,7 +847,7 @@ func TestHandler_TransactionRun(t *testing.T) {
 						BatchRunTransactionFunc: func(txs []*gethTypes.Transaction) ([]*types.Result, error) {
 							runResults = make([]*types.Result, len(txs))
 							for i, tx := range txs {
-								runResults[i] = result(tx)
+								runResults[i] = result(uint16(i), tx)
 							}
 							return runResults, nil
 						},
@@ -879,6 +880,8 @@ func TestHandler_TransactionRun(t *testing.T) {
 
 					events := backend.Events()
 					require.Len(t, events, batchSize+1) // +1 block event
+					var blockHeight uint64
+					var blockHash string
 
 					for i, event := range events {
 						if i == batchSize {
@@ -890,9 +893,26 @@ func TestHandler_TransactionRun(t *testing.T) {
 						cadenceEvent, ok := ev.(cadence.Event)
 						require.True(t, ok)
 
-						// TODO: add an event decoder in types.event
-						cadenceLogs := cadence.SearchFieldByName(cadenceEvent, "logs")
-						encodedLogs, err := hex.DecodeString(strings.ReplaceAll(cadenceLogs.String(), "\"", ""))
+						eventPayload, err := types.DecodeTransactionEventPayload(cadenceEvent)
+						require.NoError(t, err)
+
+						txBytes, err := hex.DecodeString(eventPayload.Payload)
+						require.NoError(t, err)
+						require.Equal(t, txs[i], txBytes)
+						require.Equal(t, uint16(i), eventPayload.Index)
+						require.Equal(t, types.ErrCodeNoError, types.ErrorCode(eventPayload.ErrorCode))
+
+						// make sure all block heights are same
+						if blockHeight == 0 {
+							blockHeight = eventPayload.BlockHeight
+							blockHash = eventPayload.BlockHash
+						}
+						require.Equal(t, eventPayload.BlockHeight, blockHeight)
+						require.Equal(t, eventPayload.BlockHash, blockHash)
+
+						require.Equal(t, addr.String(), eventPayload.ContractAddress)
+
+						encodedLogs, err := hex.DecodeString(eventPayload.Logs)
 						require.NoError(t, err)
 
 						var logs []*gethTypes.Log
