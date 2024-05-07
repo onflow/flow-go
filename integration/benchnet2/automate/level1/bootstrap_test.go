7064860d9d21f0fbf6e3a1c709f9e7372195b514
Jordan Schalm <jordan@dapperlabs.com>
2024-05-07 13:27:26 -0400

    committing wip, adding new testdata

--- integration/benchnet2/automate/level1/bootstrap_test.go
+++ integration/benchnet2/automate/level1/bootstrap_test.go
@@ -7,6 +7,8 @@ import (
 	"testing"
 
 	"github.com/stretchr/testify/require"
+
+	"github.com/onflow/flow-go/utils/unittest"
 )
 
 const BootstrapPath = "../testdata/level1/data/"
@@ -14,9 +16,15 @@ const ExpectedOutputPath = "../testdata/level1/expected"
 
 func TestGenerateBootstrap_DataTable(t *testing.T) {
 	testDataMap := map[string]testData{
-		"2 AN, 6 LN, 3 CN, 2 EN, 1 VN": {
-			bootstrapPath:  filepath.Join(BootstrapPath, "root-protocol-state-snapshot1.json"),
-			expectedOutput: filepath.Join(ExpectedOutputPath, "template-data-input1.json"),
+		//"2 AN, 6 LN, 3 CN, 2 EN, 1 VN": {
+		//	bootstrapPath:  filepath.Join(BootstrapPath, "root-protocol-state-snapshot1.json"),
+		//	expectedOutput: filepath.Join(ExpectedOutputPath, "template-data-input1.json"),
+		//	dockerTag:      "v0.27.6",
+		//	dockerRegistry: "gcr.io/flow-container-registry/",
+		//},
+		"testing new snapshot file": {
+			bootstrapPath:  filepath.Join(BootstrapPath, "snapshot.json"),
+			expectedOutput: filepath.Join(ExpectedOutputPath, "new-template-data-input.json"),
 			dockerTag:      "v0.27.6",
 			dockerRegistry: "gcr.io/flow-container-registry/",
 		},
@@ -28,6 +36,11 @@ func TestGenerateBootstrap_DataTable(t *testing.T) {
 			bootstrap := NewBootstrap(testData.bootstrapPath)
 			actualNodeData := bootstrap.GenTemplateData(false, testData.dockerTag, testData.dockerRegistry)
 
+			//bz, err := json.Marshal(actualNodeData)
+			//require.NoError(t, err)
+			//err = os.WriteFile(filepath.Join(ExpectedOutputPath, "new-template-data-input.json"), bz, 0660)
+			//require.NoError(t, err)
+			//
 			// load expected template data file
 			var expectedNodeData []NodeData
 			expectedDataBytes, err := os.ReadFile(testData.expectedOutput)
@@ -47,4 +60,12 @@ type testData struct {
 	expectedOutput string
 	dockerTag      string
 	dockerRegistry string
+}
+
+func TestGenerateTestData(t *testing.T) {
+	// 		"2 AN, 6 LN, 3 CN, 2 EN, 1 VN": {
+	participants := unittest.IdentityListFixture(10, unittest.WithAllRoles())
+	unittest.RootSnapshotFixture(participants)
+
+	json.NewEncoder(os.Stdout)
 }
