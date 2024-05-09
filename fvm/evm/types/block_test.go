b9cd5f04dc3fef381fa26fc59038d18f4b1a4136
sideninja <75445744+sideninja@users.noreply.github.com>
2024-05-09 13:28:12 +0200

    add all test types

--- fvm/evm/types/block_test.go
+++ fvm/evm/types/block_test.go
@@ -86,4 +86,86 @@ func Test_DecodeBlocks(t *testing.T) {
 	require.Empty(t, b.Timestamp)
 	require.Empty(t, b.TotalGasUsed)
 
+	bv2 := blockV2{
+		ParentBlockHash:   GenesisBlockHash,
+		Height:            1,
+		TotalSupply:       2,
+		StateRoot:         gethCommon.Hash{0x01},
+		ReceiptRoot:       gethCommon.Hash{0x02},
+		TransactionHashes: []gethCommon.Hash{{0x04}},
+	}
+
+	b2, err := gethRLP.EncodeToBytes(bv2)
+	require.NoError(t, err)
+
+	b = decodeBlockBreakingChanges(b2)
+
+	require.Equal(t, b.TotalSupply.Uint64(), bv2.TotalSupply)
+	require.Equal(t, b.Height, bv2.Height)
+	require.Equal(t, b.ParentBlockHash, bv2.ParentBlockHash)
+	require.Equal(t, b.TransactionHashes, bv2.TransactionHashes)
+	require.Empty(t, b.Timestamp)
+	require.Empty(t, b.TotalGasUsed)
+
+	bv3 := blockV3{
+		ParentBlockHash:   GenesisBlockHash,
+		Height:            1,
+		TotalSupply:       2,
+		ReceiptRoot:       gethCommon.Hash{0x02},
+		TransactionHashes: []gethCommon.Hash{{0x04}},
+	}
+
+	b3, err := gethRLP.EncodeToBytes(bv3)
+	require.NoError(t, err)
+
+	b = decodeBlockBreakingChanges(b3)
+
+	require.Equal(t, b.TotalSupply.Uint64(), bv3.TotalSupply)
+	require.Equal(t, b.Height, bv3.Height)
+	require.Equal(t, b.ParentBlockHash, bv3.ParentBlockHash)
+	require.Equal(t, b.TransactionHashes, bv3.TransactionHashes)
+	require.Empty(t, b.Timestamp)
+	require.Empty(t, b.TotalGasUsed)
+
+	bv4 := blockV4{
+		ParentBlockHash:   GenesisBlockHash,
+		Height:            1,
+		TotalSupply:       big.NewInt(4),
+		ReceiptRoot:       gethCommon.Hash{0x02},
+		TransactionHashes: []gethCommon.Hash{{0x04}},
+	}
+
+	b4, err := gethRLP.EncodeToBytes(bv4)
+	require.NoError(t, err)
+
+	b = decodeBlockBreakingChanges(b4)
+
+	require.Equal(t, b.TotalSupply, bv4.TotalSupply)
+	require.Equal(t, b.Height, bv4.Height)
+	require.Equal(t, b.ParentBlockHash, bv4.ParentBlockHash)
+	require.Equal(t, b.TransactionHashes, bv4.TransactionHashes)
+	require.Empty(t, b.Timestamp)
+	require.Empty(t, b.TotalGasUsed)
+
+	bv5 := blockV5{
+		ParentBlockHash:   GenesisBlockHash,
+		Height:            1,
+		TotalSupply:       big.NewInt(2),
+		ReceiptRoot:       gethCommon.Hash{0x02},
+		TransactionHashes: []gethCommon.Hash{{0x04}},
+		Timestamp:         100,
+	}
+
+	b5, err := gethRLP.EncodeToBytes(bv5)
+	require.NoError(t, err)
+
+	b = decodeBlockBreakingChanges(b5)
+
+	require.Equal(t, b.Timestamp, bv5.Timestamp)
+	require.Equal(t, b.TotalSupply.Uint64(), bv5.TotalSupply)
+	require.Equal(t, b.Height, bv5.Height)
+	require.Equal(t, b.ParentBlockHash, bv5.ParentBlockHash)
+	require.Equal(t, b.TransactionHashes, bv5.TransactionHashes)
+	require.Empty(t, b.Timestamp)
+	require.Empty(t, b.TotalGasUsed)
 }
