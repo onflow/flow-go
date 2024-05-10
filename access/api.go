ac51bcc2ff942efbb63eedf259f687ce841ad5e1
Leo Zhang (zhangchiqing) <zhangchiqing@gmail.com>
2024-05-10 14:01:42 -0700

    update api

--- access/api.go
+++ access/api.go
@@ -25,6 +25,7 @@ type API interface {
 	GetBlockByID(ctx context.Context, id flow.Identifier) (*flow.Block, flow.BlockStatus, error)
 
 	GetCollectionByID(ctx context.Context, id flow.Identifier) (*flow.LightCollection, error)
+	GetFullCollectionByID(ctx context.Context, id flow.Identifier) (*flow.Collection, error)
 
 	SendTransaction(ctx context.Context, tx *flow.TransactionBody) error
 	GetTransaction(ctx context.Context, id flow.Identifier) (*flow.TransactionBody, error)
