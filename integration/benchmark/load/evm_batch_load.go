89110cf314050be0141c4776b18b843061a6c01f
Janez Podhostnik <janez.podhostnik@gmail.com>
2024-05-14 19:17:26 +0200

    change batch size

--- integration/benchmark/load/evm_batch_load.go
+++ integration/benchmark/load/evm_batch_load.go
@@ -54,7 +54,7 @@ func NewEVMBatchTransferLoad(log zerolog.Logger) *EVMBatchTransferLoad {
 		eoaChan:                 make(chan *eoa, 1_000_000),
 		doneChan:                make(chan struct{}),
 		PreCreateEOAAccounts:    5000,
-		TransfersPerTransaction: 100,
+		TransfersPerTransaction: 500,
 	}
 
 	go load.reportStatus()
