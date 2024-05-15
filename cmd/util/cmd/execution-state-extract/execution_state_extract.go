6b423c330f5c1fb831eba8208d8309c7ce91e81c
Bastian Müller <bastian@turbolent.com>
2024-05-14 17:01:32 -0700

    use buffered channels

--- cmd/util/cmd/execution-state-extract/execution_state_extract.go
+++ cmd/util/cmd/execution-state-extract/execution_state_extract.go
@@ -428,8 +428,8 @@ func newByAccountRegistersFromPayloadAccountGrouping(
 ) {
 	g, ctx := errgroup.WithContext(context.Background())
 
-	jobs := make(chan *util.PayloadAccountGroup)
-	results := make(chan *registers.AccountRegisters)
+	jobs := make(chan *util.PayloadAccountGroup, nWorker)
+	results := make(chan *registers.AccountRegisters, nWorker)
 
 	g.Go(func() error {
 		defer close(jobs)
