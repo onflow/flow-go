74faee760346715c8edd6673ff206e21a3f14802
Leo Zhang (zhangchiqing) <zhangchiqing@gmail.com>
2024-05-16 15:18:21 -0700

    use info log

--- engine/execution/ingestion/fetcher/fetcher.go
+++ engine/execution/ingestion/fetcher/fetcher.go
@@ -71,7 +71,7 @@ func (e *CollectionFetcher) FetchCollection(blockID flow.Identifier, height uint
 		})
 	}
 
-	e.log.Debug().Bool("onflowOnlyLNs", e.onflowOnlyLNs).
+	e.log.Info().Bool("onflowOnlyLNs", e.onflowOnlyLNs).
 		Msgf("queued collection %v for block %v, height %v from guarantors: %v",
 			guarantee.ID(), blockID, height, guarantors)
 
