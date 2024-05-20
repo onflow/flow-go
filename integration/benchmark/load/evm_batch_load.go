93832b5cb3879484a3a68e4edc0af41e579363a5
Janez Podhostnik <janez.podhostnik@gmail.com>
2024-05-20 15:38:58 +0200

    next measurement

--- integration/benchmark/load/evm_batch_load.go
+++ integration/benchmark/load/evm_batch_load.go
@@ -54,7 +54,7 @@ func NewEVMBatchTransferLoad(log zerolog.Logger) *EVMBatchTransferLoad {
 		eoaChan:                 make(chan *eoa, 1_000_000),
 		doneChan:                make(chan struct{}),
 		PreCreateEOAAccounts:    5000,
-		TransfersPerTransaction: 50,
+		TransfersPerTransaction: 1,
 	}
 
 	go load.reportStatus()
