5c91caa114a701079b2b3650b417f28ec431481e
Andrii Slisarchuk <andriyslisarchuk@gmail.com>
2024-05-27 13:11:08 +0300

    Added comments

--- engine/access/ingestion/version/version_control.go
+++ engine/access/ingestion/version/version_control.go
@@ -16,32 +16,41 @@ import (
 	"github.com/onflow/flow-go/storage"
 )
 
+// VersionControlConsumer defines a function type that consumes version control updates.
+// It is called with the block height and the corresponding sem-version.
 type VersionControlConsumer func(height uint64, semver string)
 
-var AnyHeight = uint64(math.MaxUint64)
+// NoHeight represents the maximum possible height for blocks.
+var NoHeight = uint64(math.MaxUint64)
 
-// VersionControl TODO
+// VersionControl manages the version control system for the node.
+// It consumes BlockFinalized events and updates the node's version control based on the latest version beacon.
+//
+// VersionControl implements the protocol.Consumer and component.Component interfaces.
 type VersionControl struct {
-	// Version control needs to consume only BlockFinalized events.
-	// adding psEvents.Noop makes it a protocol.Consumer without unnecessary implementations
+	// Noop implements the protocol.Consumer interface with no operations.
 	psEvents.Noop
 	sync.RWMutex
 	component.Component
 
-	log            zerolog.Logger
+	log zerolog.Logger
+	// Storages
 	headers        storage.Headers
 	versionBeacons storage.VersionBeacons
 
-	// nodeVersion could be nil right now. See NewVersionControl.
+	// nodeVersion stores the node's current version.
+	// It could be nil if the node version is not available.
 	nodeVersion *semver.Version
-	// last seen version beacon, used to detect version beacon changes
+	// versionBeacon stores the last handled version beacon.
 	versionBeacon *flow.SealedVersionBeacon
 
-	// array of consumers
+	// consumers stores the list of consumers for version updates.
 	consumers []VersionControlConsumer
 
+	// blockFinalizedChan is a channel for receiving block finalized events.
 	blockFinalizedChan chan *flow.Header
 
+	// startHeight and endHeight define the height boundaries for version compatibility.
 	startHeight uint64
 	endHeight   uint64
 }
@@ -49,11 +58,11 @@ type VersionControl struct {
 var _ protocol.Consumer = (*VersionControl)(nil)
 var _ component.Component = (*VersionControl)(nil)
 
-// NewVersionControl creates new VersionControl.
+// NewVersionControl creates a new VersionControl instance.
 //
 // We currently have no strong guarantee that the node version is a valid semver.
-// See build.SemverV2 for more details. That is why nil is a valid input for node version
-// without a node version, the stop control can still be used for manual stopping.
+// See build.SemverV2 for more details. That is why nil is a valid input for node version.
+// Without a node version, the stop control can still be used for manual stopping.
 func NewVersionControl(
 	log zerolog.Logger,
 	headers storage.Headers,
@@ -61,8 +70,7 @@ func NewVersionControl(
 	nodeVersion *semver.Version,
 	latestFinalizedBlock *flow.Header,
 ) *VersionControl {
-	// We should not miss block finalized events, and we should be able to handle them
-	// faster than they are produced anyway.
+	// blockFinalizedChan is buffered to handle block finalized events.
 	blockFinalizedChan := make(chan *flow.Header, 1000)
 
 	vc := &VersionControl{
@@ -75,8 +83,8 @@ func NewVersionControl(
 		headers:        headers,
 		nodeVersion:    nodeVersion,
 		versionBeacons: versionBeacons,
-		startHeight:    AnyHeight,
-		endHeight:      AnyHeight,
+		startHeight:    NoHeight,
+		endHeight:      NoHeight,
 	}
 
 	if vc.nodeVersion != nil {
@@ -87,6 +95,7 @@ func NewVersionControl(
 
 	log.Info().Msgf("Created")
 
+	// Setup component manager for handling worker functions.
 	cm := component.NewComponentManagerBuilder()
 	cm.AddWorker(vc.processEvents)
 	cm.AddWorker(func(ctx irrecoverable.SignalerContext, ready component.ReadyFunc) {
@@ -95,54 +104,11 @@ func NewVersionControl(
 
 	vc.Component = cm.Build()
 
-	// TODO: handle version beacon already indicating a stop
-	// right now the stop will happen on first BlockFinalized
-	// which is fine, but ideally we would stop right away.
-
 	return vc
 }
 
-// BlockFinalized is called when a block is finalized.
-//
-// This is a protocol event consumer. See protocol.Consumer.
-func (v *VersionControl) BlockFinalized(h *flow.Header) {
-	v.blockFinalizedChan <- h
-}
-
-func (v *VersionControl) CompatibleAtBlock(height uint64) bool {
-	return (v.startHeight == AnyHeight || height >= v.startHeight) &&
-		(v.endHeight == AnyHeight || height <= v.endHeight)
-}
-
-func (v *VersionControl) AddVersionUpdatesConsumer(consumer VersionControlConsumer) {
-	v.Lock()
-	defer v.Unlock()
-
-	v.consumers = append(v.consumers, consumer)
-}
-
-// processEvents is a worker that processes block finalized events.
-func (v *VersionControl) processEvents(
-	ctx irrecoverable.SignalerContext,
-	ready component.ReadyFunc,
-) {
-	ready()
-
-	for {
-		select {
-		case <-ctx.Done():
-			return
-		case h := <-v.blockFinalizedChan:
-			v.blockFinalized(ctx, h)
-		}
-	}
-}
-
-// BlockFinalizedForTesting is used for testing	only.
-func (v *VersionControl) BlockFinalizedForTesting(h *flow.Header) {
-	v.blockFinalized(irrecoverable.MockSignalerContext{}, h)
-}
-
+// checkInitialVersionBeacon checks the initial version beacon at the latest finalized block.
+// It ensures the component is not ready until the initial version beacon is checked.
 func (v *VersionControl) checkInitialVersionBeacon(
 	ctx irrecoverable.SignalerContext,
 	ready component.ReadyFunc,
@@ -198,7 +164,51 @@ func (v *VersionControl) checkInitialVersionBeacon(
 	}
 }
 
-// blockFinalized is called when a block is marked as finalized
+// BlockFinalized is called when a block is finalized.
+// It implements the protocol.Consumer interface.
+func (v *VersionControl) BlockFinalized(h *flow.Header) {
+	v.blockFinalizedChan <- h
+}
+
+// BlockFinalizedForTesting is used for testing purposes only.
+// It simulates a block finalized event for testing the version control logic.
+func (v *VersionControl) BlockFinalizedForTesting(h *flow.Header) {
+	v.blockFinalized(irrecoverable.MockSignalerContext{}, h)
+}
+
+// CompatibleAtBlock checks if the node's version is compatible at a given block height.
+// It returns true if the node's version is compatible within the specified height range.
+func (v *VersionControl) CompatibleAtBlock(height uint64) bool {
+	return (v.startHeight == NoHeight || height >= v.startHeight) &&
+		(v.endHeight == NoHeight || height <= v.endHeight)
+}
+
+// AddVersionUpdatesConsumer adds a consumer for version update events.
+func (v *VersionControl) AddVersionUpdatesConsumer(consumer VersionControlConsumer) {
+	v.Lock()
+	defer v.Unlock()
+
+	v.consumers = append(v.consumers, consumer)
+}
+
+// processEvents is a worker that processes block finalized events.
+func (v *VersionControl) processEvents(
+	ctx irrecoverable.SignalerContext,
+	ready component.ReadyFunc,
+) {
+	ready()
+
+	for {
+		select {
+		case <-ctx.Done():
+			return
+		case h := <-v.blockFinalizedChan:
+			v.blockFinalized(ctx, h)
+		}
+	}
+}
+
+// blockFinalized processes a block finalized event and updates the version control state.
 func (v *VersionControl) blockFinalized(
 	ctx irrecoverable.SignalerContext,
 	h *flow.Header,
