ab62426198ff1e493597d2b4ba44dee28aad6a33
sideninja <75445744+sideninja@users.noreply.github.com>
2024-05-31 19:32:49 +0200

    add uploader test

--- fvm/evm/debug/uploader_test.go
+++ fvm/evm/debug/uploader_test.go
@@ -3,25 +3,28 @@ package debug
 import (
 	"context"
 	"encoding/json"
+	"fmt"
 	"io"
+	"math/rand"
+	"os"
 	"testing"
 
 	"cloud.google.com/go/storage"
 	"github.com/stretchr/testify/require"
-
-	testutils "github.com/onflow/flow-go/utils/unittest"
 )
 
 func Test_Uploader(t *testing.T) {
 	testutils.SkipUnless(t, testutils.TEST_REQUIRES_GCP_ACCESS, "requires GCP Bucket setup")
 
 	t.Run("successfuly upload traces", func(t *testing.T) {
+		err := os.Setenv("STORAGE_EMULATOR_HOST", "localhost:9023")
+
 		uploader, err := NewGCPUploader()
 		require.NoError(t, err)
 
 		const testID = "test_1"
 
-		data := json.RawMessage(`{ "test": true }`)
+		data := json.RawMessage(fmt.Sprintf(`{ "test": %d }`, rand.Int()))
 
 		err = uploader.Upload(testID, data)
 		require.NoError(t, err)
@@ -37,7 +40,7 @@ func Test_Uploader(t *testing.T) {
 		readBytes, err := io.ReadAll(reader)
 		require.NoError(t, err)
 
-		require.Equal(t, data, readBytes)
+		require.Equal(t, []byte(data), readBytes)
 	})
 
 }
