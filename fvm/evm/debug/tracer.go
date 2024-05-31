07bc58339b9f1ae364e324ce517a95155376af77
sideninja <75445744+sideninja@users.noreply.github.com>
2024-05-31 15:56:57 +0200

    add import for side-effects

--- fvm/evm/debug/tracer.go
+++ fvm/evm/debug/tracer.go
@@ -6,6 +6,10 @@ import (
 
 	gethCommon "github.com/onflow/go-ethereum/common"
 	"github.com/onflow/go-ethereum/eth/tracers"
+
+	// this import is needed for side-effects, because the
+	// tracers.DefaultDirectory is relying on the init function
+	_ "github.com/onflow/go-ethereum/eth/tracers/native"
 )
 
 type EVMTracer interface {
