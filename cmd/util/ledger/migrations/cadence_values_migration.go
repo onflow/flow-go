c8f682b452fb7feff65488c891f8c5698932b333
Bastian Müller <bastian@turbolent.com>
2024-06-03 15:23:32 -0700

    add new type key migration

--- cmd/util/ledger/migrations/cadence_values_migration.go
+++ cmd/util/ledger/migrations/cadence_values_migration.go
@@ -14,6 +14,7 @@ import (
 	"github.com/onflow/cadence/migrations/entitlements"
 	"github.com/onflow/cadence/migrations/statictypes"
 	"github.com/onflow/cadence/migrations/string_normalization"
+	"github.com/onflow/cadence/migrations/type_keys"
 	"github.com/onflow/cadence/runtime"
 	"github.com/onflow/cadence/runtime/common"
 	cadenceErrors "github.com/onflow/cadence/runtime/errors"
@@ -266,6 +267,7 @@ func NewCadence1ValueMigration(
 					WithCompositeTypeConverter(compositeTypeConverter).
 					WithInterfaceTypeConverter(interfaceTypeConverter),
 				entitlements.NewEntitlementsMigration(inter),
+				type_keys.NewTypeKeyMigration(),
 				string_normalization.NewStringNormalizingMigration(),
 			}
 		},
