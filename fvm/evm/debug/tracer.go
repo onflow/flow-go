02f4d9dcf37fac0343b767247485cdf29894d7fa
sideninja <75445744+sideninja@users.noreply.github.com>
2024-06-04 19:45:19 +0200

    Revert "remove unneeded code"
    This reverts commit 646bc47432d22fde0a90b734ea14fd1fcbc1cb45.

--- fvm/evm/debug/tracer.go
+++ fvm/evm/debug/tracer.go
@@ -1,6 +1,7 @@
 package debug
 
 import (
+	"context"
 	"encoding/json"
 	"fmt"
 	"time"
@@ -8,6 +9,8 @@ import (
 	gethCommon "github.com/onflow/go-ethereum/common"
 	"github.com/onflow/go-ethereum/eth/tracers"
 	"github.com/rs/zerolog"
+	"github.com/sethvargo/go-retry"
+
 	// this import is needed for side-effects, because the
 	// tracers.DefaultDirectory is relying on the init function
 	_ "github.com/onflow/go-ethereum/eth/tracers/native"
@@ -99,8 +102,6 @@ func (t *TracerManager) OnFinalizedBlock(block *model.Block) {
 	}
 }
 
-// Collect will collect the tracer and push it to the upload queue.
-// This call is non-blocking and handles any possible panics gracefully.
 func (t *TracerManager) Collect(id gethCommon.Hash, tracer *CallTracer) {
 	l := t.logger.With().Str("tx-id", id.String()).Logger()
 
@@ -139,3 +140,63 @@ type CallTracer struct {
 	evmID  gethCommon.Hash
 	header *flow.Header
 }
+
+func (t *CallTracer) Collect(id gethCommon.Hash) {
+	// upload is concurrent and it doesn't produce any errors, as the
+	// client doesn't expect it, we don't want to break execution flow,
+	// in case there are errors we retry, and if we fail after retries
+	// we log them and continue.
+	go func() {
+		l := t.logger.With().Str("tx-id", id.String()).Logger()
+
+		defer func() {
+			if r := recover(); r != nil {
+				err, ok := r.(error)
+				if !ok {
+					err = fmt.Errorf("panic: %v", r)
+				}
+
+				l.Err(err).
+					Stack().
+					Msg("failed to collect EVM traces")
+			}
+		}()
+
+		res, err := t.tracer.GetResult()
+		if err != nil {
+			l.Error().Err(err).Msg("failed to produce trace results")
+		}
+
+		backoff := retry.NewFibonacci(initialTimeout)
+		backoff = retry.WithMaxRetries(maxRetryNumber, backoff)
+
+		err = retry.Do(context.Background(), backoff, func(ctx context.Context) error {
+			err = t.uploader.Upload(id.String(), res)
+			if err != nil {
+				l.Error().Err(err).Msg("failed to upload trace results, retrying")
+				err = retry.RetryableError(err) // all upload errors should be retried
+			}
+			return err
+		})
+		if err != nil {
+			l.Error().Err(err).
+				Str("traces", string(res)).
+				Msg("failed to upload trace results, no more retries")
+			return
+		}
+
+		l.Debug().Msg("evm traces uploaded successfully")
+	}()
+}
+
+var NopTracer = &nopTracer{}
+
+var _ EVMTracer = &nopTracer{}
+
+type nopTracer struct{}
+
+func (n nopTracer) TxTracer() tracers.Tracer {
+	return nil
+}
+
+func (n nopTracer) Collect(_ gethCommon.Hash) {}
