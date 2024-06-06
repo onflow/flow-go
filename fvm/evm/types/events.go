6cd68023e8c6417287d9ad67803c4354956c64a2
ramtinms <ramtin.seraj@dapperlabs.com>
2024-06-06 11:48:20 -0700

    rename

--- fvm/evm/types/events.go
+++ fvm/evm/types/events.go
@@ -99,7 +99,7 @@ func (p *transactionEvent) ToCadence(location common.Location) (cadence.Event, e
 			// todo we can remove hash and just reference block by height (evm-gateway dependency)
 			cadence.NewField("blockHash", cadence.StringType),
 			cadence.NewField("errorMessage", cadence.StringType),
-			cadence.NewField("returnedValue", cadence.StringType),
+			cadence.NewField("returnedData", cadence.StringType),
 		},
 		nil,
 	)
@@ -204,7 +204,7 @@ type TransactionEventPayload struct {
 	BlockHeight     uint64 `cadence:"blockHeight"`
 	BlockHash       string `cadence:"blockHash"`
 	ErrorMessage    string `cadence:"errorMessage"`
-	ReturnedValue   string `cadence:"returnedValue"`
+	ReturnedData    string `cadence:"returnedData"`
 }
 
 // DecodeTransactionEventPayload decodes Cadence event into transaction event payload.
