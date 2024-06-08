bf9426b320482cf2927711e61605474474fcce40
Leo Zhang (zhangchiqing) <zhangchiqing@gmail.com>
2024-06-07 21:18:53 -0700

    add output-dir

--- cmd/util/cmd/find-trie-root/cmd.go
+++ cmd/util/cmd/find-trie-root/cmd.go
@@ -21,6 +21,7 @@ var (
 	flagRootHash          string
 	flagFrom              int
 	flagTo                int
+	flagOutputDir         string
 )
 
 var Cmd = &cobra.Command{
@@ -30,7 +31,7 @@ var Cmd = &cobra.Command{
 }
 
 func init() {
-	Cmd.Flags().StringVarP(&flagExecutionStateDir, "execution-state-dir", "e", "/var/flow/data/execution", "directory to the execution state")
+	Cmd.Flags().StringVar(&flagExecutionStateDir, "execution-state-dir", "/var/flow/data/execution", "directory to the execution state")
 	_ = Cmd.MarkFlagRequired("execution-state-dir")
 
 	Cmd.Flags().StringVar(&flagRootHash, "root-hash", "",
@@ -39,6 +40,8 @@ func init() {
 
 	Cmd.Flags().IntVar(&flagFrom, "from", 0, "from segment")
 	Cmd.Flags().IntVar(&flagTo, "to", math.MaxInt32, "to segment")
+
+	Cmd.Flags().StringVar(&flagOutputDir, "output-dir", "", "output directory")
 }
 
 func run(*cobra.Command, []string) {
@@ -52,6 +55,17 @@ func run(*cobra.Command, []string) {
 		log.Fatal().Err(err).Msg("cannot find root hash in segments")
 	}
 	log.Info().Msgf("found root hash in segment %d at offset %d", segment, offset)
+
+	if len(flagOutputDir) == 0 {
+		return
+	}
+
+	err = copyWAL(flagExecutionStateDir, flagOutputDir, segment, rootHash)
+	if err != nil {
+		log.Fatal().Err(err).Msg("cannot copy WAL")
+	}
+
+	log.Info().Msgf("copied WAL to %s", flagOutputDir)
 }
 
 func parseInput(rootHashStr string) (ledger.RootHash, error) {
@@ -148,4 +162,61 @@ func searchRootHashInSegments(
 	}
 
 	return 0, 0, fmt.Errorf("finish reading all segment files from %d to %d, but not found", from, to)
+}
+
+func copyWAL(dir, outputDir string, segment int, expectedRoot ledger.RootHash) error {
+	writer, err := prometheusWAL.NewSize(log.Logger, prometheus.DefaultRegisterer, outputDir, wal.SegmentSize, false)
+	if err != nil {
+		return fmt.Errorf("cannot create writer WAL: %w", err)
+	}
+
+	defer writer.Close()
+
+	w, err := prometheusWAL.NewSize(log.Logger, prometheus.DefaultRegisterer, dir, wal.SegmentSize, false)
+	if err != nil {
+		return fmt.Errorf("cannot create WAL: %w", err)
+	}
+
+	sr, err := prometheusWAL.NewSegmentsRangeReader(log.Logger, prometheusWAL.SegmentRange{
+		Dir:   w.Dir(),
+		First: segment,
+		Last:  segment,
+	})
+	if err != nil {
+		return fmt.Errorf("cannot create WAL segments reader: %w", err)
+	}
+
+	defer sr.Close()
+
+	reader := prometheusWAL.NewReader(sr)
+
+	for reader.Next() {
+		record := reader.Record()
+		operation, rootHash, update, err := wal.Decode(record)
+		if err != nil {
+			return fmt.Errorf("cannot decode LedgerWAL record: %w", err)
+		}
+
+		bytes := wal.EncodeUpdate(update)
+		_, err = writer.Log(bytes)
+		if err != nil {
+			return fmt.Errorf("cannot write LedgerWAL record: %w", err)
+		}
+
+		switch operation {
+		case wal.WALUpdate:
+			if rootHash == expectedRoot {
+				log.Info().Msgf("found expected trie root hash %x, finish writing", rootHash)
+				return nil
+			}
+		default:
+		}
+
+		err = reader.Err()
+		if err != nil {
+			return fmt.Errorf("cannot read LedgerWAL: %w", err)
+		}
+	}
+
+	return fmt.Errorf("finish reading all segment files from %d to %d, but not found", segment, segment)
 }
