6b3678af7c4a6db63e530107b4c28e5ff96d559e
Andrii <andriy.dyachuk95@gmail.com>
2024-06-12 14:26:47 +0300

    Reduced the indentation

--- module/execution/scripts_test.go
+++ module/execution/scripts_test.go
@@ -396,47 +396,48 @@ func newBlockHeadersStorage(blocks []*flow.Block) storage.Headers {
 
 func transferTokensTx(chain flow.Chain) *flow.TransactionBody {
 	sc := systemcontracts.SystemContractsForChain(chain.ChainID())
+
 	return flow.NewTransactionBody().
 		SetScript([]byte(fmt.Sprintf(
 			`
-							// This transaction is a template for a transaction that
-							// could be used by anyone to send tokens to another account
-							// that has been set up to receive tokens.
-							//
-							// The withdraw amount and the account from getAccount
-							// would be the parameters to the transaction
+	// This transaction is a template for a transaction that
+	// could be used by anyone to send tokens to another account
+	// that has been set up to receive tokens.
+	//
+	// The withdraw amount and the account from getAccount
+	// would be the parameters to the transaction
 
-							import FungibleToken from 0x%s
-							import FlowToken from 0x%s
+	import FungibleToken from 0x%s
+	import FlowToken from 0x%s
 
-							transaction(amount: UFix64, to: Address) {
+	transaction(amount: UFix64, to: Address) {
 
-								// The Vault resource that holds the tokens that are being transferred
-								let sentVault: @{FungibleToken.Vault}
+	// The Vault resource that holds the tokens that are being transferred
+	let sentVault: @{FungibleToken.Vault}
 
-								prepare(signer: auth(BorrowValue) &Account) {
+	prepare(signer: auth(BorrowValue) &Account) {
 
-									// Get a reference to the signer's stored vault
-									let vaultRef = signer.storage.borrow<auth(FungibleToken.Withdraw) &FlowToken.Vault>(from: /storage/flowTokenVault)
-										?? panic("Could not borrow reference to the owner's Vault!")
+	// Get a reference to the signer's stored vault
+	let vaultRef = signer.storage.borrow<auth(FungibleToken.Withdraw) &FlowToken.Vault>(from: /storage/flowTokenVault)
+	?? panic("Could not borrow reference to the owner's Vault!")
 
-									// Withdraw tokens from the signer's stored vault
-									self.sentVault <- vaultRef.withdraw(amount: amount)
-								}
+	// Withdraw tokens from the signer's stored vault
+	self.sentVault <- vaultRef.withdraw(amount: amount)
+	}
 
-								execute {
+	execute {
 
-									// Get the recipient's public account object
-									let recipient = getAccount(to)
+	// Get the recipient's public account object
+	let recipient = getAccount(to)
 
-									// Get a reference to the recipient's Receiver
-									let receiverRef = recipient.capabilities.borrow<&{FungibleToken.Receiver}>(/public/flowTokenReceiver)
-										?? panic("Could not borrow receiver reference to the recipient's Vault")
+	// Get a reference to the recipient's Receiver
+	let receiverRef = recipient.capabilities.borrow<&{FungibleToken.Receiver}>(/public/flowTokenReceiver)
+	?? panic("Could not borrow receiver reference to the recipient's Vault")
 
-									// Deposit the withdrawn tokens in the recipient's receiver
-									receiverRef.deposit(from: <-self.sentVault)
-								}
-							}`,
+	// Deposit the withdrawn tokens in the recipient's receiver
+	receiverRef.deposit(from: <-self.sentVault)
+	}
+	}`,
 			sc.FungibleToken.Address.Hex(),
 			sc.FlowToken.Address.Hex(),
 		)),
