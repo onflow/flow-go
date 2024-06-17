65c72683d60c10b114f9990390f7c8434ac44cb2
Leo Zhang (zhangchiqing) <zhangchiqing@gmail.com>
2024-06-17 14:13:47 -0700

    add pebble qcs

--- storage/pebble/qcs.go
+++ storage/pebble/qcs.go
@@ -1,19 +1,18 @@
-package badger
+package pebble
 
 import (
-	"github.com/dgraph-io/badger/v2"
+	"github.com/cockroachdb/pebble"
 
 	"github.com/onflow/flow-go/model/flow"
 	"github.com/onflow/flow-go/module"
 	"github.com/onflow/flow-go/module/metrics"
 	"github.com/onflow/flow-go/storage"
-	"github.com/onflow/flow-go/storage/badger/operation"
-	"github.com/onflow/flow-go/storage/badger/transaction"
+	"github.com/onflow/flow-go/storage/pebble/operation"
 )
 
 // QuorumCertificates implements persistent storage for quorum certificates.
 type QuorumCertificates struct {
-	db    *badger.DB
+	db    *pebble.DB
 	cache *Cache[flow.Identifier, *flow.QuorumCertificate]
 }
 
@@ -21,13 +20,13 @@ var _ storage.QuorumCertificates = (*QuorumCertificates)(nil)
 
 // NewQuorumCertificates Creates QuorumCertificates instance which is a database of quorum certificates
 // which supports storing, caching and retrieving by block ID.
-func NewQuorumCertificates(collector module.CacheMetrics, db *badger.DB, cacheSize uint) *QuorumCertificates {
-	store := func(_ flow.Identifier, qc *flow.QuorumCertificate) func(*transaction.Tx) error {
-		return transaction.WithTx(operation.InsertQuorumCertificate(qc))
+func NewQuorumCertificates(collector module.CacheMetrics, db *pebble.DB, cacheSize uint) *QuorumCertificates {
+	store := func(_ flow.Identifier, qc *flow.QuorumCertificate) func(operation.PebbleReaderWriter) error {
+		return operation.OnlyWrite(operation.InsertQuorumCertificate(qc))
 	}
 
-	retrieve := func(blockID flow.Identifier) func(tx *badger.Txn) (*flow.QuorumCertificate, error) {
-		return func(tx *badger.Txn) (*flow.QuorumCertificate, error) {
+	retrieve := func(blockID flow.Identifier) func(tx pebble.Reader) (*flow.QuorumCertificate, error) {
+		return func(tx pebble.Reader) (*flow.QuorumCertificate, error) {
 			var qc flow.QuorumCertificate
 			err := operation.RetrieveQuorumCertificate(blockID, &qc)(tx)
 			return &qc, err
@@ -48,13 +47,11 @@ func (q *QuorumCertificates) StoreTx(qc *flow.QuorumCertificate) func(interface{
 }
 
 func (q *QuorumCertificates) ByBlockID(blockID flow.Identifier) (*flow.QuorumCertificate, error) {
-	tx := q.db.NewTransaction(false)
-	defer tx.Discard()
-	return q.retrieveTx(blockID)(tx)
+	return q.retrieveTx(blockID)(q.db)
 }
 
-func (q *QuorumCertificates) retrieveTx(blockID flow.Identifier) func(*badger.Txn) (*flow.QuorumCertificate, error) {
-	return func(tx *badger.Txn) (*flow.QuorumCertificate, error) {
+func (q *QuorumCertificates) retrieveTx(blockID flow.Identifier) func(pebble.Reader) (*flow.QuorumCertificate, error) {
+	return func(tx pebble.Reader) (*flow.QuorumCertificate, error) {
 		val, err := q.cache.Get(blockID)(tx)
 		if err != nil {
 			return nil, err
--- storage/qcs.go
+++ storage/qcs.go
@@ -2,7 +2,6 @@ package storage
 
 import (
 	"github.com/onflow/flow-go/model/flow"
-	"github.com/onflow/flow-go/storage/badger/transaction"
 )
 
 // QuorumCertificates represents storage for Quorum Certificates.
@@ -13,7 +12,7 @@ import (
 type QuorumCertificates interface {
 	// StoreTx stores a Quorum Certificate as part of database transaction QC is indexed by QC.BlockID.
 	// * storage.ErrAlreadyExists if any QC for blockID is already stored
-	StoreTx(qc *flow.QuorumCertificate) func(*transaction.Tx) error
+	StoreTx(qc *flow.QuorumCertificate) func(interface{}) error
 	// ByBlockID returns QC that certifies block referred by blockID.
 	// * storage.ErrNotFound if no QC for blockID doesn't exist.
 	ByBlockID(blockID flow.Identifier) (*flow.QuorumCertificate, error)
