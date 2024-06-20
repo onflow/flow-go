6f610a1e2afdec0a8dc81a73d495feb0be2ddc16
Leo Zhang (zhangchiqing) <zhangchiqing@gmail.com>
2024-06-20 14:47:11 -0700

    movee tests

---
+++ storage/transaction_key_test.go
@@ -0,0 +1,30 @@
+package storage_test
+
+import (
+	"math/rand"
+	"testing"
+
+	"github.com/onflow/flow-go/storage"
+	"github.com/onflow/flow-go/utils/unittest"
+	"github.com/stretchr/testify/require"
+)
+
+func TestKeyConversion(t *testing.T) {
+	blockID := unittest.IdentifierFixture()
+	txID := unittest.IdentifierFixture()
+	key := storage.KeyFromBlockIDTransactionID(blockID, txID)
+	bID, tID, err := storage.KeyToBlockIDTransactionID(key)
+	require.NoError(t, err)
+	require.Equal(t, blockID, bID)
+	require.Equal(t, txID, tID)
+}
+
+func TestIndexKeyConversion(t *testing.T) {
+	blockID := unittest.IdentifierFixture()
+	txIndex := rand.Uint32()
+	key := storage.KeyFromBlockIDIndex(blockID, txIndex)
+	bID, tID, err := storage.KeyToBlockIDIndex(key)
+	require.NoError(t, err)
+	require.Equal(t, blockID, bID)
+	require.Equal(t, txIndex, tID)
+}
