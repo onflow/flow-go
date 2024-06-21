5740ec456110f3af6e1c6d51ada2395eedc91d2f
Jordan Schalm <jordan@dapperlabs.com>
2024-06-21 09:09:12 -0700

    remove debug log

--- utils/unittest/matchers.go
+++ utils/unittest/matchers.go
@@ -1,8 +1,6 @@
 package unittest
 
 import (
-	"fmt"
-
 	"github.com/stretchr/testify/mock"
 
 	"github.com/onflow/flow-go/model/flow"
@@ -11,8 +9,6 @@ import (
 // MatchEpochExtension matches an epoch extension argument passed to a mocked component.
 func MatchEpochExtension(finalView, extensionLen uint64) any {
 	return mock.MatchedBy(func(extension flow.EpochExtension) bool {
-		fmt.Println("## ", extension.FirstView, extension.FinalView)
-		fmt.Println("$$ ", finalView+1, finalView+extensionLen)
 		return extension.FirstView == finalView+1 && extension.FinalView == finalView+extensionLen
 	})
 }
