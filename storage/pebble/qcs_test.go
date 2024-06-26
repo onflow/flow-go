39fd0a7892d83a4521522ec2551c7be5898de0cb
Leo Zhang (zhangchiqing) <zhangchiqing@gmail.com>
2024-06-26 10:35:43 -0700

    fix qc tests

--- storage/pebble/qcs_test.go
+++ storage/pebble/qcs_test.go
@@ -6,6 +6,7 @@ import (
 	"github.com/cockroachdb/pebble"
 	"github.com/stretchr/testify/require"
 
+	"github.com/onflow/flow-go/model/flow"
 	"github.com/onflow/flow-go/module/metrics"
 	"github.com/onflow/flow-go/storage"
 	bstorage "github.com/onflow/flow-go/storage/pebble"
@@ -30,32 +31,32 @@ func TestQuorumCertificates_StoreTx(t *testing.T) {
 	})
 }
 
-// // TestQuorumCertificates_StoreTx_OtherQC checks if storing other QC for same blockID results in
-// // expected storage error and already stored value is not overwritten.
-//
-//	func TestQuorumCertificates_StoreTx_OtherQC(t *testing.T) {
-//		unittest.RunWithPebbleDB(t, func(db *pebble.DB) {
-//			metrics := metrics.NewNoopCollector()
-//			store := bstorage.NewQuorumCertificates(metrics, db, 10)
-//			qc := unittest.QuorumCertificateFixture()
-//			otherQC := unittest.QuorumCertificateFixture(func(otherQC *flow.QuorumCertificate) {
-//				otherQC.View = qc.View
-//				otherQC.BlockID = qc.BlockID
-//			})
-//
-//			err := db, transaction.Update, transaction.ToTx(store.StoreTx(qc)))
-//			require.NoError(t, err)
-//
-//			err = operation.RetryOnConflictTx(db, transaction.Update, transaction.ToTx(store.StoreTx(otherQC)))
-//			require.ErrorIs(t, err, storage.ErrAlreadyExists)
-//
-//			actual, err := store.ByBlockID(otherQC.BlockID)
-//			require.NoError(t, err)
-//
-//			require.Equal(t, qc, actual)
-//		})
-//	}
-//
+// TestQuorumCertificates_StoreTx_OtherQC checks if storing other QC for same blockID results in
+// expected storage error and already stored value is not overwritten.
+
+func TestQuorumCertificates_StoreTx_OtherQC(t *testing.T) {
+	unittest.RunWithPebbleDB(t, func(db *pebble.DB) {
+		metrics := metrics.NewNoopCollector()
+		store := bstorage.NewQuorumCertificates(metrics, db, 10)
+		qc := unittest.QuorumCertificateFixture()
+		otherQC := unittest.QuorumCertificateFixture(func(otherQC *flow.QuorumCertificate) {
+			otherQC.View = qc.View
+			otherQC.BlockID = qc.BlockID
+		})
+
+		err := operation.WithReaderBatchWriter(db, store.StorePebble(qc))
+		require.NoError(t, err)
+
+		err = operation.WithReaderBatchWriter(db, store.StorePebble(otherQC))
+		require.ErrorIs(t, err, storage.ErrAlreadyExists)
+
+		actual, err := store.ByBlockID(otherQC.BlockID)
+		require.NoError(t, err)
+
+		require.Equal(t, qc, actual)
+	})
+}
+
 // TestQuorumCertificates_ByBlockID that ByBlockID returns correct sentinel error if no QC for given block ID has been found
 func TestQuorumCertificates_ByBlockID(t *testing.T) {
 	unittest.RunWithPebbleDB(t, func(db *pebble.DB) {
