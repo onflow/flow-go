6f32346e41d7a7c2bccaeb50966d02dde788224b
ramtinms <ramtin.seraj@dapperlabs.com>
2024-06-26 16:04:40 -0700

    add test

--- fvm/evm/precompiles/replayer.go
+++ fvm/evm/precompiles/replayer.go
@@ -9,12 +9,24 @@ import (
 )
 
 var (
+	// errInvalidPrecompiledContractCalls is returned when an invalid list of precompiled contract calls
+	// is passed
 	errInvalidPrecompiledContractCalls = fmt.Errorf("invalid list of precompiled contract calls")
 	// errUnexpectedCall is returned when a call to the precompile is not expected
 	errUnexpectedCall = fmt.Errorf("unexpected call")
-	// more calls were expected
+	// this error is returned where more calls were expected before CapturedCalls call
 	errMoreCallsWereExpected = fmt.Errorf("expecting more call")
 )
+
+// converts an aggregated set of precompile calls
+// into a list of replayer precompiled contract
+func AggregatedPrecompiledCallsToPrecompiledContracts(apc types.AggregatedPrecompiledCalls) []types.PrecompiledContract {
+	res := make([]types.PrecompiledContract, 0)
+	for _, ap := range apc {
+		res = append(res, NewReplayerPrecompiledContract(&ap))
+	}
+	return res
+}
 
 // ReplayerPrecompiledContract is a precompiled contract
 // that replay the outputs based on the input
@@ -26,7 +38,7 @@ type ReplayerPrecompiledContract struct {
 // NewReplayerPrecompiledContract constructs a ReplayerPrecompiledContract
 func NewReplayerPrecompiledContract(
 	expectedCalls *types.PrecompiledCalls,
-) types.PrecompiledContract {
+) *ReplayerPrecompiledContract {
 	if expectedCalls == nil {
 		panic(errInvalidPrecompiledContractCalls)
 	}
@@ -46,23 +58,24 @@ func (p *ReplayerPrecompiledContract) RequiredGas(input []byte) (output uint64) 
 	if !bytes.Equal(p.expectedCalls.RequiredGasCalls[p.requiredGasIndex].Input, input) {
 		panic(errUnexpectedCall)
 	}
+	output = p.expectedCalls.RequiredGasCalls[p.requiredGasIndex].Output
 	p.requiredGasIndex++
-	return p.expectedCalls.RequiredGasCalls[p.requiredGasIndex].Output
+	return
 }
 
 func (p *ReplayerPrecompiledContract) Run(input []byte) (output []byte, err error) {
 	if p.runIndex > len(p.expectedCalls.RunCalls) {
 		panic(errUnexpectedCall)
 	}
-	if !bytes.Equal(p.expectedCalls.RunCalls[p.requiredGasIndex].Input, input) {
+	if !bytes.Equal(p.expectedCalls.RunCalls[p.runIndex].Input, input) {
 		panic(errUnexpectedCall)
 	}
-	p.runIndex++
 	output = p.expectedCalls.RunCalls[p.runIndex].Output
 	errMsg := p.expectedCalls.RunCalls[p.runIndex].ErrorMsg
 	if len(errMsg) > 0 {
 		err = errors.New(errMsg)
 	}
+	p.runIndex++
 	return
 }
 
@@ -70,10 +83,14 @@ func (p *ReplayerPrecompiledContract) IsCalled() bool {
 	return p.requiredGasIndex > 0 || p.runIndex > 0
 }
 
+func (p *ReplayerPrecompiledContract) HasReplayedAll() bool {
+	return len(p.expectedCalls.RequiredGasCalls) == p.requiredGasIndex &&
+		len(p.expectedCalls.RunCalls) == p.runIndex
+}
+
 func (p *ReplayerPrecompiledContract) CapturedCalls() *types.PrecompiledCalls {
 	// we didn't consume all calls
-	if len(p.expectedCalls.RequiredGasCalls) != p.requiredGasIndex ||
-		len(p.expectedCalls.RunCalls) != p.runIndex {
+	if !p.HasReplayedAll() {
 		panic(errMoreCallsWereExpected)
 	}
 	return p.expectedCalls
