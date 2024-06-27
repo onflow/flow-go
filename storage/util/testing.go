e7a5df0e006956d319fc1ce6086a2b893624cf83
Leo Zhang (zhangchiqing) <zhangchiqing@gmail.com>
2024-06-27 11:23:40 -0700

    fix linting

--- storage/util/testing.go
+++ storage/util/testing.go
@@ -5,14 +5,12 @@ import (
 	"path/filepath"
 	"testing"
 
-	"github.com/cockroachdb/pebble"
 	"github.com/dgraph-io/badger/v2"
 	"github.com/stretchr/testify/require"
 
 	"github.com/onflow/flow-go/module/metrics"
 	"github.com/onflow/flow-go/storage"
 	bstorage "github.com/onflow/flow-go/storage/badger"
-	pstorage "github.com/onflow/flow-go/storage/pebble"
 )
 
 func StorageLayer(_ testing.TB, db *badger.DB) *storage.All {
@@ -21,12 +19,11 @@ func StorageLayer(_ testing.TB, db *badger.DB) *storage.All {
 	return all
 }
 
-func PebbleStorageLayer(_ testing.TB, db *pebble.DB) *storage.All {
-	metrics := metrics.NewNoopCollector()
-	all := pstorage.InitAll(metrics, db)
-	return all
-}
-
+//	func PebbleStorageLayer(_ testing.TB, db *pebble.DB) *storage.All {
+//		metrics := metrics.NewNoopCollector()
+//		all := pstorage.InitAll(metrics, db)
+//		return all
+//	}
 func CreateFiles(t *testing.T, dir string, names ...string) {
 	for _, name := range names {
 		file, err := os.Create(filepath.Join(dir, name))
