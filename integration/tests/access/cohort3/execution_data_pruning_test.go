905bb5f05d0bf4caf4b0780c5804ddd600f2354f
UlyanaAndrukhiv <u.andrukhiv@gmail.com>
2024-07-02 12:20:53 +0300

    Updated integraation test for execution data pruning

--- integration/tests/access/cohort3/execution_data_pruning_test.go
+++ integration/tests/access/cohort3/execution_data_pruning_test.go
@@ -11,16 +11,21 @@ import (
 	"github.com/rs/zerolog"
 	"github.com/stretchr/testify/require"
 	"github.com/stretchr/testify/suite"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials/insecure"
 
-	"github.com/onflow/flow-go/engine/ghost/client"
+	sdk "github.com/onflow/flow-go-sdk"
 	"github.com/onflow/flow-go/integration/testnet"
-	"github.com/onflow/flow-go/integration/tests/lib"
 	"github.com/onflow/flow-go/model/flow"
 	"github.com/onflow/flow-go/module/blobs"
 	"github.com/onflow/flow-go/module/executiondatasync/execution_data"
 	"github.com/onflow/flow-go/module/metrics"
 	storage "github.com/onflow/flow-go/storage/badger"
 	"github.com/onflow/flow-go/utils/unittest"
+
+	accessproto "github.com/onflow/flow/protobuf/go/flow/access"
+	"github.com/onflow/flow/protobuf/go/flow/entities"
+	"github.com/onflow/flow/protobuf/go/flow/executiondata"
 )
 
 func TestExecutionDataPruning(t *testing.T) {
@@ -29,12 +34,11 @@ func TestExecutionDataPruning(t *testing.T) {
 
 type ExecutionDataPruningSuite struct {
 	suite.Suite
-	lib.TestnetStateTracker
 
 	log zerolog.Logger
 
-	ghostID      flow.Identifier
-	observerName string
+	accessNodeName   string
+	observerNodeName string
 	// threshold defines the maximum height range and how frequently pruning is performed.
 	threshold uint64
 
@@ -60,6 +64,7 @@ func (s *ExecutionDataPruningSuite) SetupTest() {
 	}()
 
 	// access node
+	s.accessNodeName = testnet.PrimaryAN
 	accessNodeConfig := testnet.NewNodeConfig(
 		flow.RoleAccess,
 		testnet.WithLogLevel(zerolog.FatalLevel),
@@ -69,17 +74,11 @@ func (s *ExecutionDataPruningSuite) SetupTest() {
 		testnet.WithAdditionalFlag("--execution-data-retry-delay=1s"),
 		testnet.WithAdditionalFlag("--execution-data-indexing-enabled=true"),
 		testnet.WithAdditionalFlagf("--execution-state-dir=%s", testnet.DefaultExecutionStateDir),
-		testnet.WithAdditionalFlag("--event-query-mode=execution-nodes-only"),
 		testnet.WithAdditionalFlagf("--public-network-execution-data-sync-enabled=true"),
+		testnet.WithAdditionalFlagf("--event-query-mode=local-only"),
+		testnet.WithAdditionalFlagf("--execution-data-height-range-target=%d", 100),
+		testnet.WithAdditionalFlagf("--execution-data-height-range-threshold=%d", s.threshold),
 	)
-
-	// add the ghost (access) node config
-	s.ghostID = unittest.IdentifierFixture()
-	ghostNode := testnet.NewNodeConfig(
-		flow.RoleAccess,
-		testnet.WithID(s.ghostID),
-		testnet.WithLogLevel(zerolog.FatalLevel),
-		testnet.AsGhost())
 
 	consensusConfigs := []func(config *testnet.NodeConfig){
 		testnet.WithAdditionalFlag("--cruise-ctl-fallback-proposal-duration=400ms"),
@@ -98,16 +97,15 @@ func (s *ExecutionDataPruningSuite) SetupTest() {
 		testnet.NewNodeConfig(flow.RoleConsensus, consensusConfigs...),
 		testnet.NewNodeConfig(flow.RoleVerification, testnet.WithLogLevel(zerolog.FatalLevel)),
 		accessNodeConfig, // access_1
-		ghostNode,        // access ghost
 	}
 
 	// add the observer node config
-	s.observerName = testnet.PrimaryON
+	s.observerNodeName = testnet.PrimaryON
 	s.threshold = 50
 
 	observers := []testnet.ObserverConfig{{
-		ContainerName: s.observerName,
-		LogLevel:      zerolog.WarnLevel,
+		ContainerName: s.observerNodeName,
+		LogLevel:      zerolog.InfoLevel,
 		AdditionalFlags: []string{
 			fmt.Sprintf("--execution-data-dir=%s", testnet.DefaultExecutionDataServiceDir),
 			fmt.Sprintf("--execution-state-dir=%s", testnet.DefaultExecutionStateDir),
@@ -115,15 +113,10 @@ func (s *ExecutionDataPruningSuite) SetupTest() {
 			"--execution-data-indexing-enabled=true",
 			"--execution-data-retry-delay=1s",
 			"--event-query-mode=local-only",
-			fmt.Sprintf("--execution-data-height-range-target=%d", 100),            //400
-			fmt.Sprintf("--execution-data-height-range-threshold=%d", s.threshold), //100
+			fmt.Sprintf("--execution-data-height-range-target=%d", 100),
+			fmt.Sprintf("--execution-data-height-range-threshold=%d", s.threshold),
 		},
-	},
-		{
-			ContainerName: "observer_2",
-			LogLevel:      zerolog.InfoLevel,
-		},
-	}
+	}}
 
 	conf := testnet.NewNetworkConfig("execution_data_pruning", nodeConfigs, testnet.WithObservers(observers...))
 	s.net = testnet.PrepareFlowNetwork(s.T(), conf, flow.Localnet)
@@ -133,57 +126,119 @@ func (s *ExecutionDataPruningSuite) SetupTest() {
 	s.ctx, s.cancel = context.WithCancel(context.Background())
 
 	s.net.Start(s.ctx)
-	s.Track(s.T(), s.ctx, s.Ghost())
 }
 
-func (s *ExecutionDataPruningSuite) Ghost() *client.GhostClient {
-	client, err := s.net.ContainerByID(s.ghostID).GhostClient()
-	require.NoError(s.T(), err, "could not get ghost client")
-	return client
+// getGRPCClient is the helper func to create an access api client
+func (s *ExecutionDataPruningSuite) getGRPCClient(address string) (accessproto.AccessAPIClient, error) {
+	conn, err := grpc.Dial(address, grpc.WithTransportCredentials(insecure.NewCredentials()))
+	if err != nil {
+		return nil, err
+	}
+
+	client := accessproto.NewAccessAPIClient(conn)
+	return client, nil
 }
 
 func (s *ExecutionDataPruningSuite) TestHappyPath() {
-	// pause until the observer node is progressing
-	//TODO: UlianaAndrukhiv: remove Sleep
-	time.Sleep(2 * time.Minute)
+	accessNode := s.net.ContainerByName(s.accessNodeName)
+	observerNode := s.net.ContainerByName(s.observerNodeName)
 
+	// creating grpc client
+	grpcClient, err := s.getGRPCClient(observerNode.Addr(testnet.GRPCPort))
+	s.Require().NoError(err)
+
+	// creating execution data api client
+	client, err := getClient(fmt.Sprintf("localhost:%s", observerNode.Port(testnet.ExecutionStatePort)))
+	s.Require().NoError(err)
+
+	// pause until the observer node start indexing blocks
+	s.Require().Eventually(func() bool {
+		_, err := grpcClient.GetEventsForHeightRange(s.ctx, &accessproto.GetEventsForHeightRangeRequest{
+			Type:                 sdk.EventAccountCreated,
+			StartHeight:          1,
+			EndHeight:            1,
+			EventEncodingVersion: entities.EventEncodingVersion_CCF_V0,
+		})
+
+		return err == nil
+	}, 2*time.Minute, 10*time.Second)
+
+	// subscribe on events till waitingBlockHeight to make sure that block indexed till that height and prune
+	// pruned execution data at least once
+	waitingBlockHeight := uint64(200)
+
+	stream, err := client.SubscribeEventsFromStartHeight(s.ctx, &executiondata.SubscribeEventsFromStartHeightRequest{
+		EventEncodingVersion: entities.EventEncodingVersion_CCF_V0,
+		Filter:               &executiondata.EventFilter{},
+		HeartbeatInterval:    1,
+		StartBlockHeight:     0,
+	})
+	s.Require().NoError(err)
+	eventsChan, errChan, err := SubscribeHandler(s.ctx, stream.Recv, eventsResponseHandler)
+	s.Require().NoError(err)
+
+	var canCheckResults bool
+	for {
+		select {
+		case err := <-errChan:
+			s.Require().NoErrorf(err, "unexpected %s error", s.observerNodeName)
+		case event := <-eventsChan:
+			if event.Height > waitingBlockHeight {
+				canCheckResults = true
+			}
+		}
+
+		if canCheckResults {
+			break
+		}
+	}
 	s.net.StopContainers()
 
 	metrics := metrics.NewNoopCollector()
 
-	an := s.net.ContainerByName(testnet.PrimaryAN)
+	// start an execution data service using the Access Node's execution data db
+	anEds := s.nodeExecutionDataStore(accessNode)
+
 	// setup storage objects needed to get the execution data id
-	anDB, err := an.DB()
+	anDB, err := accessNode.DB()
 	require.NoError(s.T(), err, "could not open db")
 
+	anHeaders := storage.NewHeaders(metrics, anDB)
 	anResults := storage.NewExecutionResults(metrics, anDB)
 
-	on := s.net.ContainerByName(s.observerName)
-	onEds := s.nodeExecutionDataStore(on)
+	// start an execution data service using the Observer Node's execution data db
+
+	onEds := s.nodeExecutionDataStore(observerNode)
 	// setup storage objects needed to get the execution data id
-	onDB, err := on.DB()
+	onDB, err := observerNode.DB()
 	require.NoError(s.T(), err, "could not open db")
 
-	onHeaders := storage.NewHeaders(metrics, onDB)
 	onResults := storage.NewExecutionResults(metrics, onDB)
 
-	// Loop through blocks and verify the execution data was pruned correctly
+	// checking execution results from 1 block
 	startBlockHeight := uint64(1)
 
+	// Loop through blocks and verify the execution data was pruned correctly
+	// execution data till height equal threshold + 1 should be pruned
 	for i := startBlockHeight; i <= s.threshold+1; i++ {
-		header, err := onHeaders.ByHeight(i)
-		require.NoError(s.T(), err, "%s: could not get header", s.observerName)
+		header, err := anHeaders.ByHeight(i)
+		require.NoError(s.T(), err, "%s: could not get header", s.accessNodeName)
 
 		result, err := anResults.ByBlockID(header.ID())
-		require.NoError(s.T(), err, "%s: could not get sealed result", testnet.PrimaryAN)
+		require.NoError(s.T(), err, "%s: could not get sealed result", s.accessNodeName)
+
+		// verify AN execution data
+		_, err = anEds.Get(s.ctx, result.ExecutionDataID)
+		require.Error(s.T(), err, "%s: could not prune execution data for height %v", s.accessNodeName, i)
+		require.ErrorContains(s.T(), err, "not found")
 
 		result, err = onResults.ByID(result.ID())
-		require.NoError(s.T(), err, "%s: could not get sealed result from ON`s storage", testnet.PrimaryON)
+		require.NoError(s.T(), err, "%s: could not get sealed result from ON`s storage", s.observerNodeName)
 
+		// verify ON execution data
 		_, err = onEds.Get(s.ctx, result.ExecutionDataID)
-		require.Error(s.T(), err, "%s: could not prune execution data for height %v", s.observerName, i)
+		require.Error(s.T(), err, "%s: could not prune execution data for height %v", s.observerNodeName, i)
 		require.ErrorContains(s.T(), err, "not found")
-		s.T().Logf("%s: execution data for height was pruned %d, err: %v", s.observerName, i, err)
 	}
 }
 
