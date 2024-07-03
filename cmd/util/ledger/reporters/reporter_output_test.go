c5bfc47a8dcee27b65ec280de79151d4a6ebbc0f
Faye Amacker <33205765+fxamacker@users.noreply.github.com>
2024-07-03 16:15:34 -0500

    Add support for JSONL report format
    Currently, reports.ReportWriter outputs messages to a JSON array,
    which can cause JSON parsers like jq to crash on large reports.
    
    This commit adds support for JSONL format, so each JSON object can
    be parsed independently (rather than requiring every object in
    the array to be parsed).
    
    While at it, modify the diff-states command to use JSONL format.

--- cmd/util/ledger/reporters/reporter_output_test.go
+++ cmd/util/ledger/reporters/reporter_output_test.go
@@ -1,6 +1,7 @@
 package reporters_test
 
 import (
+	"fmt"
 	"os"
 	"path"
 	"sync"
@@ -12,7 +13,7 @@ import (
 	"github.com/onflow/flow-go/cmd/util/ledger/reporters"
 )
 
-func TestReportFileWriter(t *testing.T) {
+func TestReportFileWriterJSONArray(t *testing.T) {
 	dir := t.TempDir()
 
 	filename := path.Join(dir, "test.json")
@@ -30,20 +31,20 @@ func TestReportFileWriter(t *testing.T) {
 	}
 
 	t.Run("Open & Close - empty json array", func(t *testing.T) {
-		rw := reporters.NewReportFileWriter(filename, log)
+		rw := reporters.NewReportFileWriter(filename, log, reporters.ReportFormatJSONArray)
 		rw.Close()
 
 		requireFileContains(t, "[]")
 	})
 	t.Run("Open & Write One & Close - json array with one element", func(t *testing.T) {
-		rw := reporters.NewReportFileWriter(filename, log)
+		rw := reporters.NewReportFileWriter(filename, log, reporters.ReportFormatJSONArray)
 		rw.Write(testData{TestField: "something"})
 		rw.Close()
 
 		requireFileContains(t, "[{\"TestField\":\"something\"}]")
 	})
 	t.Run("Open & Write Many & Close - json array with many elements", func(t *testing.T) {
-		rw := reporters.NewReportFileWriter(filename, log)
+		rw := reporters.NewReportFileWriter(filename, log, reporters.ReportFormatJSONArray)
 		rw.Write(testData{TestField: "something0"})
 		rw.Write(testData{TestField: "something1"})
 		rw.Write(testData{TestField: "something2"})
@@ -55,7 +56,7 @@ func TestReportFileWriter(t *testing.T) {
 	})
 
 	t.Run("Open & Write Many in threads & Close", func(t *testing.T) {
-		rw := reporters.NewReportFileWriter(filename, log)
+		rw := reporters.NewReportFileWriter(filename, log, reporters.ReportFormatJSONArray)
 
 		wg := &sync.WaitGroup{}
 		for i := 0; i < 3; i++ {
@@ -72,5 +73,72 @@ func TestReportFileWriter(t *testing.T) {
 
 		requireFileContains(t,
 			"[{\"TestField\":\"something\"},{\"TestField\":\"something\"},{\"TestField\":\"something\"}]")
+	})
+}
+
+func TestReportFileWriterJSONL(t *testing.T) {
+	dir := t.TempDir()
+
+	filename := path.Join(dir, "test.jsonl")
+	log := zerolog.Logger{}
+
+	requireFileContains := func(t *testing.T, expected string) {
+		dat, err := os.ReadFile(filename)
+		require.NoError(t, err)
+
+		fmt.Printf("filename: %s\n", filename)
+
+		require.Equal(t, []byte(expected), dat)
+	}
+
+	type testData struct {
+		TestField string
+	}
+
+	t.Run("Open & Close", func(t *testing.T) {
+		rw := reporters.NewReportFileWriter(filename, log, reporters.ReportFormatJSONL)
+		rw.Close()
+
+		requireFileContains(t, "")
+	})
+
+	t.Run("Open & Write One & Close", func(t *testing.T) {
+		rw := reporters.NewReportFileWriter(filename, log, reporters.ReportFormatJSONL)
+		rw.Write(testData{TestField: "something"})
+		rw.Close()
+
+		requireFileContains(t, "{\"TestField\":\"something\"}")
+	})
+
+	t.Run("Open & Write Many & Close", func(t *testing.T) {
+		rw := reporters.NewReportFileWriter(filename, log, reporters.ReportFormatJSONL)
+		rw.Write(testData{TestField: "something0"})
+		rw.Write(testData{TestField: "something1"})
+		rw.Write(testData{TestField: "something2"})
+
+		rw.Close()
+
+		requireFileContains(t,
+			"{\"TestField\":\"something0\"}\n{\"TestField\":\"something1\"}\n{\"TestField\":\"something2\"}")
+	})
+
+	t.Run("Open & Write Many in threads & Close", func(t *testing.T) {
+		rw := reporters.NewReportFileWriter(filename, log, reporters.ReportFormatJSONL)
+
+		wg := &sync.WaitGroup{}
+		for i := 0; i < 3; i++ {
+			wg.Add(1)
+			go func() {
+				rw.Write(testData{TestField: "something"})
+				wg.Done()
+			}()
+		}
+
+		wg.Wait()
+
+		rw.Close()
+
+		requireFileContains(t,
+			"{\"TestField\":\"something\"}\n{\"TestField\":\"something\"}\n{\"TestField\":\"something\"}")
 	})
 }
