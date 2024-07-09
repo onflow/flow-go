4192ee394591b8e996bab02967215b24978ccaed
ramtinms <ramtin.seraj@dapperlabs.com>
2024-07-08 22:13:12 -0700

    update test

--- fvm/evm/types/utils.go
+++ fvm/evm/types/utils.go
@@ -20,12 +20,19 @@ func BytesToCadenceUInt8ArrayValue(b []byte) cadence.Array {
 var cadenceArrayTypeOfUInt8 = cadence.NewVariableSizedArrayType(cadence.UInt8Type)
 
 // CadenceUInt8ArrayValueToBytes converts a Cadence array of type UInt8 into a byte slice
-func CadenceUInt8ArrayValueToBytes(a cadence.Array) ([]byte, error) {
-	if !a.ArrayType.Equal(cadenceArrayTypeOfUInt8) {
+func CadenceUInt8ArrayValueToBytes(a cadence.Value) ([]byte, error) {
+	aa, ok := a.(cadence.Array)
+	if !ok {
+		return nil, fmt.Errorf("value is not an array")
+	}
+
+	// if array type is empty, continue
+	if aa.Type() != nil && !aa.Type().Equal(cadenceArrayTypeOfUInt8) {
 		return nil, fmt.Errorf("invalid array type")
 	}
-	values := make([]byte, len(a.Values))
-	for i, v := range a.Values {
+
+	values := make([]byte, len(aa.Values))
+	for i, v := range aa.Values {
 		values[i] = byte(v.(cadence.UInt8))
 	}
 	return values, nil
