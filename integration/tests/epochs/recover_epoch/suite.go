b64a5ca8512cc818af1fbfda224912b6d8bc84ad
Khalil Claybon <khalil.claybon@dapperlabs.com>
2024-07-09 23:33:08 -0400

    add short dkg len comment

--- integration/tests/epochs/recover_epoch/suite.go
+++ integration/tests/epochs/recover_epoch/suite.go
@@ -24,6 +24,7 @@ type Suite struct {
 func (s *Suite) SetupTest() {
 	// use a shorter staking auction because we don't have staking operations in this case
 	s.StakingAuctionLen = 2
+	// to manually trigger EFM we assign very short dkg phase len ensuring the dkg will fail
 	s.DKGPhaseLen = 10
 	s.EpochLen = 80
 	s.EpochCommitSafetyThreshold = 20
