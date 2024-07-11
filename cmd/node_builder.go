b777c5e2c001039b05dfa31aa9c44a06e0bb05e7
Leo Zhang (zhangchiqing) <zhangchiqing@gmail.com>
2024-07-11 13:15:36 -0700

    update mocks

--- cmd/node_builder.go
+++ cmd/node_builder.go
@@ -5,6 +5,7 @@ import (
 	"time"
 
 	"github.com/cockroachdb/pebble"
+	"github.com/dgraph-io/badger/v2"
 	madns "github.com/multiformats/go-multiaddr-dns"
 	"github.com/prometheus/client_golang/prometheus"
 	"github.com/rs/zerolog"
