409687ea88800cc7aa351ae6f31ee9e05b15d517
UlyanaAndrukhiv <u.andrukhiv@gmail.com>
2024-07-17 17:08:23 +0300

    Added ExecutionDataDBMode

--- module/executiondatasync/execution_data/execution_data.go
+++ module/executiondatasync/execution_data/execution_data.go
@@ -1,9 +1,45 @@
 package execution_data
 
 import (
+	"errors"
+
 	"github.com/onflow/flow-go/ledger"
 	"github.com/onflow/flow-go/model/flow"
 )
+
+// ExecutionDataDBMode controls which db type to use.
+type ExecutionDataDBMode int
+
+const (
+	// ExecutionDataDBModeBadger uses badger db
+	ExecutionDataDBModeBadger ExecutionDataDBMode = iota + 1
+
+	// ExecutionDataDBModePebble uses pebble db
+	// execution nodes
+	ExecutionDataDBModePebble
+)
+
+func ParseExecutionDataDBMode(s string) (ExecutionDataDBMode, error) {
+	switch s {
+	case ExecutionDataDBModeBadger.String():
+		return ExecutionDataDBModeBadger, nil
+	case ExecutionDataDBModePebble.String():
+		return ExecutionDataDBModePebble, nil
+	default:
+		return 0, errors.New("invalid execution data DB mode")
+	}
+}
+
+func (m ExecutionDataDBMode) String() string {
+	switch m {
+	case ExecutionDataDBModeBadger:
+		return "badger"
+	case ExecutionDataDBModePebble:
+		return "pebble"
+	default:
+		return ""
+	}
+}
 
 // DefaultMaxBlobSize is the default maximum size of a blob.
 // This is calibrated to fit within a libp2p message and not exceed the max size recommended by bitswap.
