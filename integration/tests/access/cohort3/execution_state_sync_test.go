83c00f016c9ee33216e750f79abf83ca501dbbcd
UlyanaAndrukhiv <u.andrukhiv@gmail.com>
2024-07-18 18:18:05 +0300

    Removed flag from integration test

--- integration/tests/access/cohort3/execution_state_sync_test.go
+++ integration/tests/access/cohort3/execution_state_sync_test.go
@@ -82,7 +82,6 @@ func (s *ExecutionStateSyncSuite) buildNetworkConfig() {
 		testnet.WithAdditionalFlag(fmt.Sprintf("--execution-data-dir=%s", testnet.DefaultExecutionDataServiceDir)),
 		testnet.WithAdditionalFlag("--execution-data-retry-delay=1s"),
 		testnet.WithAdditionalFlagf("--public-network-execution-data-sync-enabled=true"),
-		testnet.WithAdditionalFlag(fmt.Sprintf("--execution-data-db=%s", execution_data.ExecutionDataDBModePebble.String())),
 	)
 
 	// add the ghost (access) node config
