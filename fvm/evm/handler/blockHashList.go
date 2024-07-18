b817af67b9b54ba6fbae7024e05db1288158dc62
ramtinms <ramtin.seraj@dapperlabs.com>
2024-07-17 19:13:03 -0700

    lint fix

--- fvm/evm/handler/blockHashList.go
+++ fvm/evm/handler/blockHashList.go
@@ -4,9 +4,10 @@ import (
 	"encoding/binary"
 	"fmt"
 
+	gethCommon "github.com/onflow/go-ethereum/common"
+
 	"github.com/onflow/flow-go/fvm/evm/types"
 	"github.com/onflow/flow-go/model/flow"
-	gethCommon "github.com/onflow/go-ethereum/common"
 )
 
 const (
