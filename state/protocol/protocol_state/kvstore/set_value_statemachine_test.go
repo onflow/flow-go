0a7920e43b3bc750f11af916df7493411d97801a
Yurii Oleksyshyn <yuraolex@gmail.com>
2024-07-26 14:52:29 +0300

    Added test suite for set kv store value. Added a few tests.

---
+++ state/protocol/protocol_state/kvstore/set_value_statemachine_test.go
@@ -0,0 +1,99 @@
+package kvstore_test
+
+import (
+	"github.com/onflow/flow-go/state/protocol"
+	"testing"
+
+	"github.com/stretchr/testify/require"
+	"github.com/stretchr/testify/suite"
+
+	"github.com/onflow/flow-go/model/flow"
+	mockprotocol "github.com/onflow/flow-go/state/protocol/mock"
+	"github.com/onflow/flow-go/state/protocol/protocol_state/kvstore"
+	"github.com/onflow/flow-go/state/protocol/protocol_state/mock"
+	mocks "github.com/stretchr/testify/mock"
+)
+
+func TestSetKeyValueStoreValueStateMachine(t *testing.T) {
+	suite.Run(t, new(SetKeyValueStoreValueStateMachineSuite))
+}
+
+// SetKeyValueStoreValueStateMachineSuite is a dedicated test suite for testing KV store state machine.
+type SetKeyValueStoreValueStateMachineSuite struct {
+	suite.Suite
+
+	view        uint64
+	parentState *mockprotocol.KVStoreReader
+	mutator     *mock.KVStoreMutator
+	telemetry   *mock.StateMachineTelemetryConsumer
+
+	stateMachine *kvstore.SetValueStateMachine
+}
+
+func (s *SetKeyValueStoreValueStateMachineSuite) SetupTest() {
+	s.telemetry = mock.NewStateMachineTelemetryConsumer(s.T())
+	s.parentState = mockprotocol.NewKVStoreReader(s.T())
+	s.mutator = mock.NewKVStoreMutator(s.T())
+	s.view = 1000
+
+	s.parentState.On("GetEpochCommitSafetyThreshold").Return(uint64(100)).Maybe()
+
+	s.stateMachine = kvstore.NewSetValueStateMachine(s.telemetry, s.view, s.parentState, s.mutator)
+	require.NotNil(s.T(), s.stateMachine)
+}
+
+// TestInitialInvariants ensures that initial state machine invariants are met.
+// It checks that state machine has the correct candidateView and parent state.
+func (s *SetKeyValueStoreValueStateMachineSuite) TestInitialInvariants() {
+	require.Equal(s.T(), s.view, s.stateMachine.View())
+	require.Equal(s.T(), s.parentState, s.stateMachine.ParentState())
+}
+
+// TestEvolveState_ProtocolStateVersionUpgrade ensures that state machine can process protocol state version upgrade event.
+// It checks several cases including
+// * happy path - valid upgrade version and activation view
+// * invalid upgrade version - has to return sentinel error since version is invalid
+// * invalid activation view - has to return sentinel error since activation view doesn't meet threshold.
+func (s *SetKeyValueStoreValueStateMachineSuite) TestEvolveState_ProtocolStateVersionUpgrade() {
+	s.Run("happy-path", func() {
+		ev := &flow.SetEpochExtensionViewCount{
+			Value: 1000,
+		}
+
+		s.mutator.On("SetEpochExtensionViewCount", ev.Value).Return(nil)
+		err := s.stateMachine.EvolveState([]flow.ServiceEvent{ev.ServiceEvent()})
+		require.NoError(s.T(), err)
+	})
+	// process two events, one is valid and one is invalid, ensure:
+	// 1. valid event is processed
+	// 2. invalid event is ignored and reported
+	s.Run("invalid-value", func() {
+		s.mutator = mock.NewKVStoreMutator(s.T())
+		s.stateMachine = kvstore.NewSetValueStateMachine(s.telemetry, s.view, s.parentState, s.mutator)
+
+		valid := &flow.SetEpochExtensionViewCount{
+			Value: 1000,
+		}
+		invalid := &flow.SetEpochExtensionViewCount{
+			Value: 50,
+		}
+
+		s.mutator.On("SetEpochExtensionViewCount", valid.Value).Return(nil).Once()
+		s.mutator.On("SetEpochExtensionViewCount", invalid.Value).Return(kvstore.ErrInvalidValue).Once()
+		s.telemetry.On("OnInvalidServiceEvent", invalid.ServiceEvent(),
+			mocks.MatchedBy(protocol.IsInvalidServiceEventError)).Return().Once()
+
+		err := s.stateMachine.EvolveState([]flow.ServiceEvent{invalid.ServiceEvent(), valid.ServiceEvent()})
+		require.NoError(s.T(), err, "sentinel error has to be handled internally")
+	})
+	s.Run("exception", func() {
+
+	})
+}
+
+// TestBuild ensures that state machine returns empty list of deferred operations.
+func (s *SetKeyValueStoreValueStateMachineSuite) TestBuild() {
+	dbOps, err := s.stateMachine.Build()
+	require.NoError(s.T(), err)
+	require.True(s.T(), dbOps.IsEmpty())
+}
