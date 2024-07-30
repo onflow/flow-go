513d6be32f3ea8cb18d34cdeb8470e02d47aeee4
UlyanaAndrukhiv <u.andrukhiv@gmail.com>
2024-07-30 15:08:28 +0300

    Updated documentation for integration test

--- integration/tests/access/cohort3/pebble_execution_state_sync_test.go
+++ integration/tests/access/cohort3/pebble_execution_state_sync_test.go
@@ -20,7 +20,7 @@ func (s *PebbleExecutionStateSync) SetupTest() {
 	s.setup(execution_data.ExecutionDataDBModePebble)
 }
 
-// TestBadgerDBHappyPath tests that Execution Nodes generate execution data, and Access Nodes are able to
+// TestPebbleDBHappyPath+ tests that Execution Nodes generate execution data, and Access Nodes are able to
 // successfully sync the data to pebble DB
 func (s *PebbleExecutionStateSync) TestPebbleDBHappyPath() {
 	s.executionStateSyncTest()
