fb1bd4b13a08263cf6f47f73320253dddcd6fff6
ramtinms <ramtin.seraj@dapperlabs.com>
2024-08-13 00:08:44 -0700

    update test

--- fvm/evm/debug/tracer_test.go
+++ fvm/evm/debug/tracer_test.go
@@ -50,17 +50,37 @@ func Test_CallTracer(t *testing.T) {
 		tr.OnExit(0, []byte{0x02}, 200, nil, false)
 		tr.OnTxEnd(&gethTypes.Receipt{TxHash: tx.Hash()}, nil)
 		tracer.Collect(tx.Hash())
+	})
 
-		// second transaction
-		tx = gethTypes.NewTransaction(nonce, to, amount, 200, big.NewInt(20), data)
-		tr.OnTxStart(nil, tx, from)
-		tr.OnEnter(0, 0, from, to, []byte{0x01, 0x02}, 10, big.NewInt(1))
-		tr.OnExit(0, []byte{0x02}, 200, nil, false)
-		tr.OnTxEnd(&gethTypes.Receipt{TxHash: tx.Hash()}, nil)
-		tracer.Collect(tx.Hash())
+	t.Run("collect traces and upload them (after a failed transaction)", func(t *testing.T) {
+		txID := gethCommon.Hash{0x05}
+		blockID := flow.Identifier{0x01}
+		var res json.RawMessage
+
+		mockUpload := &testutils.MockUploader{
+			UploadFunc: func(id string, message json.RawMessage) error {
+				require.Equal(t, TraceID(txID, blockID), id)
+				require.Equal(t, res, message)
+				return nil
+			},
+		}
+
+		tracer, err := NewEVMCallTracer(mockUpload, zerolog.Nop())
+		require.NoError(t, err)
+		tracer.WithBlockID(blockID)
+
+		from := gethCommon.HexToAddress("0x01")
+		to := gethCommon.HexToAddress("0x02")
+		nonce := uint64(10)
+		data := []byte{0x02, 0x04}
+		amount := big.NewInt(1)
+
+		// first transaction
+		tr := tracer.TxTracer()
+		require.NotNil(t, tr)
 
 		// failed transaction (no exit and collect call)
-		tx = gethTypes.NewTransaction(nonce, to, amount, 300, big.NewInt(30), data)
+		tx := gethTypes.NewTransaction(nonce, to, amount, 300, big.NewInt(30), data)
 		tr.OnTxStart(nil, tx, from)
 		tr.OnEnter(0, 0, from, to, []byte{0x01, 0x02}, 10, big.NewInt(1))
 		tr.OnEnter(1, byte(vm.ADD), from, to, data, 20, big.NewInt(2))
