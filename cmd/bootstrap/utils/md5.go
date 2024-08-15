30fb9f703656d48e18372f5d34a84300154f798f
Joshua Foster <joshua@quiknode.io>
2024-08-15 14:45:39 -0400

    Allow concurrent downloads Verify existing files to the MD5 hash (skip if match)

---
+++ cmd/bootstrap/utils/md5.go
@@ -0,0 +1,22 @@
+package utils
+
+import (
+	"crypto/md5"
+	"io"
+	"os"
+)
+
+func CalcMd5(outpath string) []byte {
+	f, err := os.Open(outpath)
+	if err != nil {
+		return nil
+	}
+	defer f.Close()
+
+	h := md5.New()
+	if _, err := io.Copy(h, f); err != nil {
+		return nil
+	}
+
+	return h.Sum(nil)
+}
