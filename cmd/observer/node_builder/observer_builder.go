0d05d2fac95261962012b2a05699ef4c817bbdf9
Andrii <andriy.dyachuk95@gmail.com>
2024-08-19 12:45:28 +0300

    Added indexReporter to observer builder

--- cmd/observer/node_builder/observer_builder.go
+++ cmd/observer/node_builder/observer_builder.go
@@ -1904,6 +1904,12 @@ func (builder *ObserverServiceBuilder) enqueueRPCServer() {
 			return nil, fmt.Errorf("failed to initialize block tracker: %w", err)
 		}
 
+		// If execution data syncing and indexing is disabled, pass nil indexReporter
+		var indexReporter state_synchronization.IndexReporter
+		if builder.executionDataSyncEnabled && builder.executionDataIndexingEnabled {
+			indexReporter = builder.Reporter
+		}
+
 		backendParams := backend.Params{
 			State:                     node.State,
 			Blocks:                    node.Storage.Blocks,
@@ -1930,6 +1936,7 @@ func (builder *ObserverServiceBuilder) enqueueRPCServer() {
 				builder.stateStreamConf.ResponseLimit,
 				builder.stateStreamConf.ClientSendBufferSize,
 			),
+			IndexReporter: indexReporter,
 		}
 
 		if builder.localServiceAPIEnabled {
@@ -1955,12 +1962,6 @@ func (builder *ObserverServiceBuilder) enqueueRPCServer() {
 			node.RootChainID.Chain())
 		if err != nil {
 			return nil, err
-		}
-
-		// If execution data syncing and indexing is disabled, pass nil indexReporter
-		var indexReporter state_synchronization.IndexReporter
-		if builder.executionDataSyncEnabled && builder.executionDataIndexingEnabled {
-			indexReporter = builder.Reporter
 		}
 
 		engineBuilder, err := rpc.NewBuilder(
