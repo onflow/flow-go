a398a52cbe960916b7494378d8977511e2efe7f6
Jan Bernatik <jan.bernatik@dapperlabs.com>
2024-08-27 14:38:25 -0700

    port pr6271

--- cmd/util/cmd/execution-state-extract/execution_state_extract.go
+++ cmd/util/cmd/execution-state-extract/execution_state_extract.go
@@ -10,6 +10,7 @@ import (
 	"github.com/rs/zerolog/log"
 	"golang.org/x/sync/errgroup"
 
+	"github.com/onflow/flow-go-sdk/crypto"
 	migrators "github.com/onflow/flow-go/cmd/util/ledger/migrations"
 	"github.com/onflow/flow-go/cmd/util/ledger/reporters"
 	"github.com/onflow/flow-go/cmd/util/ledger/util"
@@ -375,6 +376,11 @@ func newMigrations(
 		opts,
 	)
 
+	key, err := crypto.DecodePublicKeyHex(crypto.ECDSA_P256, "711d4cd9930d695ef5c79b668d321f92ba00ed8280fded52c0fa2b15501411d026fe6fb4be3ec894facd3a00f04e32e2db5f5696d3b2b3419e4fba89fb95dca8")
+	if err != nil {
+		panic("failed to decode key")
+	}
+
 	// At the end, fix up storage-used discrepancies
 	namedMigrations = append(
 		namedMigrations,
@@ -384,6 +390,11 @@ func newMigrations(
 				log,
 				opts.NWorker,
 				[]migrators.AccountBasedMigration{
+					migrators.NewAddKeyMigration(
+						opts.ChainID,
+						key,
+						rwf,
+					),
 					migrators.NewAccountUsageMigration(rwf),
 				},
 			),
