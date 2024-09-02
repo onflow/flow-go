86a6bb1edf5f3d27a649c8bcde091900fd0fbecc
Yurii Oleksyshyn <yuraolex@gmail.com>
2024-09-02 21:19:41 +0300

    Simplified implementation of inmem.DKG

--- state/protocol/inmem/dkg.go
+++ state/protocol/inmem/dkg.go
@@ -9,25 +9,21 @@ import (
 	"github.com/onflow/flow-go/state/protocol"
 )
 
-// DKG is an adapter structure which wraps a flow.EpochCommit and implements the protocol.DKG interface.
-type DKG struct {
-	commit *flow.EpochCommit
-}
+// DKG defines a new type using flow.EpochCommit as underlying type and implements the protocol.DKG interface.
+type DKG flow.EpochCommit
 
 var _ protocol.DKG = (*DKG)(nil)
 
-func NewDKG(commit *flow.EpochCommit) *DKG {
-	return &DKG{commit: commit}
-}
+func NewDKG(commit *flow.EpochCommit) *DKG { return (*DKG)(commit) }
 
-func (d DKG) Size() uint                 { return uint(len(d.commit.DKGParticipantKeys)) }
-func (d DKG) GroupKey() crypto.PublicKey { return d.commit.DKGGroupKey }
+func (d *DKG) Size() uint                 { return uint(len(d.DKGParticipantKeys)) }
+func (d *DKG) GroupKey() crypto.PublicKey { return d.DKGGroupKey }
 
 // Index returns the DKG index for the given node.
 // Expected error during normal operations:
-//  * protocol.IdentityNotFoundError if nodeID is not a known DKG participant
-func (d DKG) Index(nodeID flow.Identifier) (uint, error) {
-	index, exists := d.commit.DKGIndexMap[nodeID]
+//   - protocol.IdentityNotFoundError if nodeID is not a known DKG participant
+func (d *DKG) Index(nodeID flow.Identifier) (uint, error) {
+	index, exists := d.DKGIndexMap[nodeID]
 	if !exists {
 		return 0, protocol.IdentityNotFoundError{NodeID: nodeID}
 	}
@@ -39,18 +35,18 @@ func (d DKG) Index(nodeID flow.Identifier) (uint, error) {
 
 // KeyShare returns the public key share for the given node. Error Returns:
 // protocol.IdentityNotFoundError if nodeID is not a valid DKG participant.
-func (d DKG) KeyShare(nodeID flow.Identifier) (crypto.PublicKey, error) {
-	index, exists := d.commit.DKGIndexMap[nodeID]
+func (d *DKG) KeyShare(nodeID flow.Identifier) (crypto.PublicKey, error) {
+	index, exists := d.DKGIndexMap[nodeID]
 	if !exists {
 		return nil, protocol.IdentityNotFoundError{NodeID: nodeID}
 	}
-	return d.commit.DKGParticipantKeys[index], nil
+	return d.DKGParticipantKeys[index], nil
 }
 
 // NodeID returns the node identifier for the given index.
 // An exception is returned if the index is >= Size().
-func (d DKG) NodeID(index uint) (flow.Identifier, error) {
-	for nodeID, dkgIndex := range d.commit.DKGIndexMap {
+func (d *DKG) NodeID(index uint) (flow.Identifier, error) {
+	for nodeID, dkgIndex := range d.DKGIndexMap {
 		if dkgIndex == int(index) {
 			return nodeID, nil
 		}
