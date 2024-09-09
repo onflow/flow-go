9413cb516367b97b394177764ce76b70b7d2c30f
Bastian Müller <bastian@turbolent.com>
2024-09-09 11:55:38 -0700

    improve calculation of new authorization, handle edge cases

--- cmd/util/cmd/generate-authorization-fixes/entitlements_test.go
+++ cmd/util/cmd/generate-authorization-fixes/entitlements_test.go
@@ -1,14 +1,36 @@
 package generate_authorization_fixes
 
 import (
+	"errors"
 	"testing"
 
+	"github.com/onflow/cadence/runtime/common"
+	"github.com/onflow/cadence/runtime/interpreter"
 	"github.com/onflow/cadence/runtime/sema"
 	. "github.com/onflow/cadence/runtime/tests/checker"
+	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
 )
 
-func TestFindMinimalEntitlementsSet(t *testing.T) {
+func newEntitlementSetAuthorizationFromEntitlementTypes(
+	entitlements []*sema.EntitlementType,
+	kind sema.EntitlementSetKind,
+) interpreter.EntitlementSetAuthorization {
+	return interpreter.NewEntitlementSetAuthorization(
+		nil,
+		func() []common.TypeID {
+			typeIDs := make([]common.TypeID, len(entitlements))
+			for i, e := range entitlements {
+				typeIDs[i] = e.ID()
+			}
+			return typeIDs
+		},
+		len(entitlements),
+		kind,
+	)
+}
+
+func TestFindMinimalAuthorization(t *testing.T) {
 
 	t.Parallel()
 
@@ -16,110 +38,188 @@ func TestFindMinimalEntitlementsSet(t *testing.T) {
       entitlement E1
       entitlement E2
       entitlement E3
-      resource interface I1 {
-          access(E1) fun method1()
+
+      struct S {
+          access(all) fun accessAll() {}
+          access(self) fun accessSelf() {}
+          access(contract) fun accessContract() {}
+          access(account) fun accessAccount() {}
+
+          access(E1) fun accessE1() {}
+          access(E2) fun accessE2() {}
+          access(E1, E2) fun accessE1AndE2() {}
+          access(E1 | E2) fun accessE1OrE2() {}
       }
-      resource interface I2 {
-          access(E1, E2) fun method2()
-          access(E2) fun method3()
-      }
-      resource interface I3 {
-          access(E3) fun method4()
-      }
-      // Assume this is the intersection-type that is left now.
-      // It may have some set of entitlements, but we are not interested:
-      // We are going to re-derive the minimal set of entitlements needed,
-      // depending on what methods to keep.
-      var intersectionType: &{I1, I2, I3}?  = nil
 	`)
 	require.NoError(t, err)
 
-	vValueType := RequireGlobalValue(t, checker.Elaboration, "intersectionType")
-
-	require.IsType(t, &sema.OptionalType{}, vValueType)
-	optionalType := vValueType.(*sema.OptionalType)
-
-	require.IsType(t, &sema.ReferenceType{}, optionalType.Type)
-	referenceType := optionalType.Type.(*sema.ReferenceType)
-
-	require.IsType(t, &sema.IntersectionType{}, referenceType.Type)
-	intersectionType := referenceType.Type.(*sema.IntersectionType)
+	ty := RequireGlobalType(t, checker.Elaboration, "S")
 
 	e1 := RequireGlobalType(t, checker.Elaboration, "E1").(*sema.EntitlementType)
 	e2 := RequireGlobalType(t, checker.Elaboration, "E2").(*sema.EntitlementType)
-	e3 := RequireGlobalType(t, checker.Elaboration, "E3").(*sema.EntitlementType)
 
-	t.Run("method1, method2", func(t *testing.T) {
-		entitlements := findMinimalEntitlementSet(
-			intersectionType,
-			[]string{
-				"method1",
-				"method2",
+	t.Run("accessAll, accessSelf, accessContract, accessAccount", func(t *testing.T) {
+		t.Parallel()
+
+		authorization, unresolved := findMinimalAuthorization(
+			ty,
+			map[string]struct{}{
+				"accessAll":      {},
+				"accessSelf":     {},
+				"accessContract": {},
+				"accessAccount":  {},
+				"undefined":      {},
 			},
 		)
-		require.Equal(t,
-			[]*sema.EntitlementType{e1},
-			entitlements,
+		assert.Equal(t,
+			interpreter.UnauthorizedAccess,
+			authorization,
+		)
+		assert.Equal(t,
+			map[string]error{
+				"accessSelf":     errors.New("member is inaccessible (access(self))"),
+				"accessContract": errors.New("member is inaccessible (access(contract))"),
+				"accessAccount":  errors.New("member is inaccessible (access(account))"),
+				"undefined":      errors.New("member does not exist"),
+			},
+			unresolved,
 		)
 	})
 
-	t.Run("method1, method2, method3", func(t *testing.T) {
-		entitlements := findMinimalEntitlementSet(
-			intersectionType,
-			[]string{
-				"method1",
-				"method2",
-				"method3",
+	t.Run("accessE1", func(t *testing.T) {
+		t.Parallel()
+
+		authorization, unresolved := findMinimalAuthorization(
+			ty,
+			map[string]struct{}{
+				"accessE1":  {},
+				"undefined": {},
 			},
 		)
-		require.Equal(t,
-			[]*sema.EntitlementType{e1, e2},
-			entitlements,
+		assert.Equal(t,
+			newEntitlementSetAuthorizationFromEntitlementTypes(
+				[]*sema.EntitlementType{
+					e1,
+				},
+				sema.Conjunction,
+			),
+			authorization,
+		)
+		assert.Equal(t,
+			map[string]error{
+				"undefined": errors.New("member does not exist"),
+			},
+			unresolved,
 		)
 	})
 
-	t.Run("method1, method2, method4", func(t *testing.T) {
-		entitlements := findMinimalEntitlementSet(
-			intersectionType,
-			[]string{
-				"method1",
-				"method2",
-				"method4",
+	t.Run("accessE1, accessE2", func(t *testing.T) {
+		t.Parallel()
+
+		authorization, unresolved := findMinimalAuthorization(
+			ty,
+			map[string]struct{}{
+				"accessE1":  {},
+				"accessE2":  {},
+				"undefined": {},
 			},
 		)
-		require.Equal(
-			t,
-			[]*sema.EntitlementType{e1, e3},
-			entitlements,
+		assert.Equal(t,
+			newEntitlementSetAuthorizationFromEntitlementTypes(
+				[]*sema.EntitlementType{
+					e1, e2,
+				},
+				sema.Conjunction,
+			),
+			authorization,
+		)
+		assert.Equal(t,
+			map[string]error{
+				"undefined": errors.New("member does not exist"),
+			},
+			unresolved,
 		)
 	})
 
-	t.Run("method1, method2, method3, method4", func(t *testing.T) {
-		entitlements := findMinimalEntitlementSet(
-			intersectionType,
-			[]string{
-				"method1",
-				"method2",
-				"method3",
-				"method4",
+	t.Run("accessE1AndE2", func(t *testing.T) {
+		t.Parallel()
+
+		authorization, unresolved := findMinimalAuthorization(
+			ty,
+			map[string]struct{}{
+				"accessE1AndE2": {},
+				"undefined":     {},
 			},
 		)
-		require.Equal(t,
-			[]*sema.EntitlementType{e1, e2, e3},
-			entitlements,
+		assert.Equal(t,
+			newEntitlementSetAuthorizationFromEntitlementTypes(
+				[]*sema.EntitlementType{
+					e1, e2,
+				},
+				sema.Conjunction,
+			),
+			authorization,
+		)
+		assert.Equal(t,
+			map[string]error{
+				"undefined": errors.New("member does not exist"),
+			},
+			unresolved,
 		)
 	})
 
-	t.Run("method4", func(t *testing.T) {
-		entitlements := findMinimalEntitlementSet(
-			intersectionType,
-			[]string{
-				"method4",
+	t.Run("accessE1OrE2", func(t *testing.T) {
+		t.Parallel()
+
+		authorization, unresolved := findMinimalAuthorization(
+			ty,
+			map[string]struct{}{
+				"accessE1OrE2": {},
+				"undefined":    {},
 			},
 		)
-		require.Equal(t,
-			[]*sema.EntitlementType{e3},
-			entitlements,
+		assert.Equal(t,
+			newEntitlementSetAuthorizationFromEntitlementTypes(
+				[]*sema.EntitlementType{
+					e1,
+				},
+				sema.Conjunction,
+			),
+			authorization,
+		)
+		assert.Equal(t,
+			map[string]error{
+				"undefined": errors.New("member does not exist"),
+			},
+			unresolved,
+		)
+	})
+
+	t.Run("accessE1OrE2, accessE1AndE2", func(t *testing.T) {
+		t.Parallel()
+
+		authorization, unresolved := findMinimalAuthorization(
+			ty,
+			map[string]struct{}{
+				"accessE1OrE2":  {},
+				"accessE1AndE2": {},
+				"undefined":     {},
+			},
+		)
+		assert.Equal(t,
+			newEntitlementSetAuthorizationFromEntitlementTypes(
+				[]*sema.EntitlementType{
+					e1, e2,
+				},
+				sema.Conjunction,
+			),
+			authorization,
+		)
+		assert.Equal(t,
+			map[string]error{
+				"undefined": errors.New("member does not exist"),
+			},
+			unresolved,
 		)
 	})
 }
