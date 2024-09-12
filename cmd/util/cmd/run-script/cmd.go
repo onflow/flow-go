1c944d08fadcce02b64b5399ca5dc5e7acdbf4b9
Bastian Müller <bastian@turbolent.com>
2024-09-12 11:36:11 -0700

    add REST API to run-script command

--- cmd/util/cmd/run-script/cmd.go
+++ cmd/util/cmd/run-script/cmd.go
@@ -1,19 +1,29 @@
 package run_script
 
 import (
+	"context"
+	"errors"
+	"fmt"
 	"io"
 	"os"
 
 	jsoncdc "github.com/onflow/cadence/encoding/json"
+	"github.com/onflow/flow/protobuf/go/flow/entities"
 	"github.com/rs/zerolog/log"
 	"github.com/spf13/cobra"
 
+	"github.com/onflow/flow-go/access"
 	"github.com/onflow/flow-go/cmd/util/ledger/util"
 	"github.com/onflow/flow-go/cmd/util/ledger/util/registers"
+	"github.com/onflow/flow-go/engine/access/rest"
+	"github.com/onflow/flow-go/engine/access/state_stream/backend"
+	"github.com/onflow/flow-go/engine/access/subscription"
 	"github.com/onflow/flow-go/engine/execution/computation"
 	"github.com/onflow/flow-go/fvm"
+	"github.com/onflow/flow-go/fvm/storage/snapshot"
 	"github.com/onflow/flow-go/ledger"
 	"github.com/onflow/flow-go/model/flow"
+	"github.com/onflow/flow-go/module/metrics"
 )
 
 var (
@@ -21,6 +31,8 @@ var (
 	flagState           string
 	flagStateCommitment string
 	flagChain           string
+	flagServe           bool
+	flagPort            int
 )
 
 var Cmd = &cobra.Command{
@@ -60,6 +72,20 @@ func init() {
 		"Chain name",
 	)
 	_ = Cmd.MarkFlagRequired("chain")
+
+	Cmd.Flags().BoolVar(
+		&flagServe,
+		"serve",
+		false,
+		"serve with an HTTP server",
+	)
+
+	Cmd.Flags().IntVar(
+		&flagPort,
+		"port",
+		8000,
+		"port for HTTP server",
+	)
 }
 
 func run(*cobra.Command, []string) {
@@ -75,15 +101,14 @@ func run(*cobra.Command, []string) {
 
 	chainID := flow.ChainID(flagChain)
 	// Validate chain ID
-	_ = chainID.Chain()
+	chain := chainID.Chain()
 
-	code, err := io.ReadAll(os.Stdin)
-	if err != nil {
-		log.Fatal().Msgf("failed to read script: %s", err)
-	}
+	log.Info().Msg("loading state ...")
 
-	var payloads []*ledger.Payload
-
+	var (
+		err      error
+		payloads []*ledger.Payload
+	)
 	if flagPayloads != "" {
 		_, payloads, err = util.ReadPayloadFile(log.Logger, flagPayloads)
 	} else {
@@ -125,23 +150,390 @@ func run(*cobra.Command, []string) {
 
 	vm := fvm.NewVirtualMachine()
 
+	if flagServe {
+
+		api := &api{
+			chainID:         chainID,
+			vm:              vm,
+			ctx:             ctx,
+			storageSnapshot: storageSnapshot,
+		}
+
+		server, err := rest.NewServer(
+			api,
+			rest.Config{
+				ListenAddress: fmt.Sprintf(":%d", flagPort),
+			},
+			log.Logger,
+			chain,
+			metrics.NewNoopCollector(),
+			nil,
+			backend.Config{},
+		)
+		if err != nil {
+			log.Fatal().Err(err).Msg("failed to create server")
+		}
+
+		log.Info().Msgf("serving on port %d", flagPort)
+
+		err = server.ListenAndServe()
+		if err != nil {
+			log.Info().Msg("server stopped")
+		}
+	} else {
+		code, err := io.ReadAll(os.Stdin)
+		if err != nil {
+			log.Fatal().Msgf("failed to read script: %s", err)
+		}
+
+		encodedResult, err := runScript(vm, ctx, storageSnapshot, code, nil)
+		if err != nil {
+			log.Fatal().Err(err).Msg("failed to run script")
+		}
+
+		_, _ = os.Stdout.Write(encodedResult)
+	}
+}
+
+func runScript(
+	vm *fvm.VirtualMachine,
+	ctx fvm.Context,
+	storageSnapshot snapshot.StorageSnapshot,
+	code []byte,
+	arguments [][]byte,
+) (
+	encodedResult []byte,
+	err error,
+) {
 	_, res, err := vm.Run(
 		ctx,
-		fvm.Script(code),
+		fvm.Script(code).WithArguments(arguments...),
 		storageSnapshot,
 	)
 	if err != nil {
-		log.Fatal().Msgf("failed to run script: %s", err)
+		return nil, err
 	}
 
 	if res.Err != nil {
-		log.Fatal().Msgf("script failed: %s", res.Err)
+		return nil, res.Err
 	}
 
 	encoded, err := jsoncdc.Encode(res.Value)
 	if err != nil {
-		log.Fatal().Msgf("failed to encode result: %s", err)
+		return nil, err
 	}
 
-	_, _ = os.Stdout.Write(encoded)
+	return encoded, nil
+}
+
+type api struct {
+	chainID         flow.ChainID
+	vm              *fvm.VirtualMachine
+	ctx             fvm.Context
+	storageSnapshot registers.StorageSnapshot
+}
+
+var _ access.API = &api{}
+
+func (*api) Ping(_ context.Context) error {
+	return nil
+}
+
+func (a *api) GetNetworkParameters(_ context.Context) access.NetworkParameters {
+	return access.NetworkParameters{
+		ChainID: a.chainID,
+	}
+}
+
+func (*api) GetNodeVersionInfo(_ context.Context) (*access.NodeVersionInfo, error) {
+	return nil, errors.New("unimplemented")
+}
+
+func (*api) GetLatestBlockHeader(_ context.Context, _ bool) (*flow.Header, flow.BlockStatus, error) {
+	return nil, flow.BlockStatusUnknown, errors.New("unimplemented")
+}
+
+func (*api) GetBlockHeaderByHeight(_ context.Context, _ uint64) (*flow.Header, flow.BlockStatus, error) {
+	return nil, flow.BlockStatusUnknown, errors.New("unimplemented")
+}
+
+func (*api) GetBlockHeaderByID(_ context.Context, _ flow.Identifier) (*flow.Header, flow.BlockStatus, error) {
+	return nil, flow.BlockStatusUnknown, errors.New("unimplemented")
+}
+
+func (*api) GetLatestBlock(_ context.Context, _ bool) (*flow.Block, flow.BlockStatus, error) {
+	return nil, flow.BlockStatusUnknown, errors.New("unimplemented")
+}
+
+func (*api) GetBlockByHeight(_ context.Context, _ uint64) (*flow.Block, flow.BlockStatus, error) {
+	return nil, flow.BlockStatusUnknown, errors.New("unimplemented")
+}
+
+func (*api) GetBlockByID(_ context.Context, _ flow.Identifier) (*flow.Block, flow.BlockStatus, error) {
+	return nil, flow.BlockStatusUnknown, errors.New("unimplemented")
+}
+
+func (*api) GetCollectionByID(_ context.Context, _ flow.Identifier) (*flow.LightCollection, error) {
+	return nil, errors.New("unimplemented")
+}
+
+func (*api) GetFullCollectionByID(_ context.Context, _ flow.Identifier) (*flow.Collection, error) {
+	return nil, errors.New("unimplemented")
+}
+
+func (*api) SendTransaction(_ context.Context, _ *flow.TransactionBody) error {
+	return errors.New("unimplemented")
+}
+
+func (*api) GetTransaction(_ context.Context, _ flow.Identifier) (*flow.TransactionBody, error) {
+	return nil, errors.New("unimplemented")
+}
+
+func (*api) GetTransactionsByBlockID(_ context.Context, _ flow.Identifier) ([]*flow.TransactionBody, error) {
+	return nil, errors.New("unimplemented")
+}
+
+func (*api) GetTransactionResult(
+	_ context.Context,
+	_ flow.Identifier,
+	_ flow.Identifier,
+	_ flow.Identifier,
+	_ entities.EventEncodingVersion,
+) (*access.TransactionResult, error) {
+	return nil, errors.New("unimplemented")
+}
+
+func (*api) GetTransactionResultByIndex(
+	_ context.Context,
+	_ flow.Identifier,
+	_ uint32,
+	_ entities.EventEncodingVersion,
+) (*access.TransactionResult, error) {
+	return nil, errors.New("unimplemented")
+}
+
+func (*api) GetTransactionResultsByBlockID(
+	_ context.Context,
+	_ flow.Identifier,
+	_ entities.EventEncodingVersion,
+) ([]*access.TransactionResult, error) {
+	return nil, errors.New("unimplemented")
+}
+
+func (*api) GetSystemTransaction(
+	_ context.Context,
+	_ flow.Identifier,
+) (*flow.TransactionBody, error) {
+	return nil, errors.New("unimplemented")
+}
+
+func (*api) GetSystemTransactionResult(
+	_ context.Context,
+	_ flow.Identifier,
+	_ entities.EventEncodingVersion,
+) (*access.TransactionResult, error) {
+	return nil, errors.New("unimplemented")
+}
+
+func (*api) GetAccount(_ context.Context, _ flow.Address) (*flow.Account, error) {
+	return nil, errors.New("unimplemented")
+}
+
+func (*api) GetAccountAtLatestBlock(_ context.Context, _ flow.Address) (*flow.Account, error) {
+	return nil, errors.New("unimplemented")
+}
+
+func (*api) GetAccountAtBlockHeight(_ context.Context, _ flow.Address, _ uint64) (*flow.Account, error) {
+	return nil, errors.New("unimplemented")
+}
+
+func (*api) GetAccountBalanceAtLatestBlock(_ context.Context, _ flow.Address) (uint64, error) {
+	return 0, errors.New("unimplemented")
+}
+
+func (*api) GetAccountBalanceAtBlockHeight(
+	_ context.Context,
+	_ flow.Address,
+	_ uint64,
+) (uint64, error) {
+	return 0, errors.New("unimplemented")
+}
+
+func (*api) GetAccountKeyAtLatestBlock(
+	_ context.Context,
+	_ flow.Address,
+	_ uint32,
+) (*flow.AccountPublicKey, error) {
+	return nil, errors.New("unimplemented")
+}
+
+func (*api) GetAccountKeyAtBlockHeight(
+	_ context.Context,
+	_ flow.Address,
+	_ uint32,
+	_ uint64,
+) (*flow.AccountPublicKey, error) {
+	return nil, errors.New("unimplemented")
+}
+
+func (*api) GetAccountKeysAtLatestBlock(
+	_ context.Context,
+	_ flow.Address,
+) ([]flow.AccountPublicKey, error) {
+	return nil, errors.New("unimplemented")
+}
+
+func (*api) GetAccountKeysAtBlockHeight(
+	_ context.Context,
+	_ flow.Address,
+	_ uint64,
+) ([]flow.AccountPublicKey, error) {
+	return nil, errors.New("unimplemented")
+}
+
+func (a *api) ExecuteScriptAtLatestBlock(
+	_ context.Context,
+	script []byte,
+	arguments [][]byte,
+) ([]byte, error) {
+	return runScript(
+		a.vm,
+		a.ctx,
+		a.storageSnapshot,
+		script,
+		arguments,
+	)
+}
+
+func (*api) ExecuteScriptAtBlockHeight(
+	_ context.Context,
+	_ uint64,
+	_ []byte,
+	_ [][]byte,
+) ([]byte, error) {
+	return nil, errors.New("unimplemented")
+}
+
+func (*api) ExecuteScriptAtBlockID(
+	_ context.Context,
+	_ flow.Identifier,
+	_ []byte,
+	_ [][]byte,
+) ([]byte, error) {
+	return nil, errors.New("unimplemented")
+}
+
+func (a *api) GetEventsForHeightRange(
+	_ context.Context,
+	_ string,
+	_, _ uint64,
+	_ entities.EventEncodingVersion,
+) ([]flow.BlockEvents, error) {
+	return nil, errors.New("unimplemented")
+}
+
+func (a *api) GetEventsForBlockIDs(
+	_ context.Context,
+	_ string,
+	_ []flow.Identifier,
+	_ entities.EventEncodingVersion,
+) ([]flow.BlockEvents, error) {
+	return nil, errors.New("unimplemented")
+}
+
+func (*api) GetLatestProtocolStateSnapshot(_ context.Context) ([]byte, error) {
+	return nil, errors.New("unimplemented")
+}
+
+func (*api) GetProtocolStateSnapshotByBlockID(_ context.Context, _ flow.Identifier) ([]byte, error) {
+	return nil, errors.New("unimplemented")
+}
+
+func (*api) GetProtocolStateSnapshotByHeight(_ context.Context, _ uint64) ([]byte, error) {
+	return nil, errors.New("unimplemented")
+}
+
+func (*api) GetExecutionResultForBlockID(_ context.Context, _ flow.Identifier) (*flow.ExecutionResult, error) {
+	return nil, errors.New("unimplemented")
+}
+
+func (*api) GetExecutionResultByID(_ context.Context, _ flow.Identifier) (*flow.ExecutionResult, error) {
+	return nil, errors.New("unimplemented")
+}
+
+func (*api) SubscribeBlocksFromStartBlockID(
+	_ context.Context,
+	_ flow.Identifier,
+	_ flow.BlockStatus,
+) subscription.Subscription {
+	return nil
+}
+
+func (*api) SubscribeBlocksFromStartHeight(
+	_ context.Context,
+	_ uint64,
+	_ flow.BlockStatus,
+) subscription.Subscription {
+	return nil
+}
+
+func (*api) SubscribeBlocksFromLatest(
+	_ context.Context,
+	_ flow.BlockStatus,
+) subscription.Subscription {
+	return nil
+}
+
+func (*api) SubscribeBlockHeadersFromStartBlockID(
+	_ context.Context,
+	_ flow.Identifier,
+	_ flow.BlockStatus,
+) subscription.Subscription {
+	return nil
+}
+
+func (*api) SubscribeBlockHeadersFromStartHeight(
+	_ context.Context,
+	_ uint64,
+	_ flow.BlockStatus,
+) subscription.Subscription {
+	return nil
+}
+
+func (*api) SubscribeBlockHeadersFromLatest(
+	_ context.Context,
+	_ flow.BlockStatus,
+) subscription.Subscription {
+	return nil
+}
+
+func (*api) SubscribeBlockDigestsFromStartBlockID(
+	_ context.Context,
+	_ flow.Identifier,
+	_ flow.BlockStatus,
+) subscription.Subscription {
+	return nil
+}
+
+func (*api) SubscribeBlockDigestsFromStartHeight(
+	_ context.Context,
+	_ uint64,
+	_ flow.BlockStatus,
+) subscription.Subscription {
+	return nil
+}
+
+func (*api) SubscribeBlockDigestsFromLatest(
+	_ context.Context,
+	_ flow.BlockStatus,
+) subscription.Subscription {
+	return nil
+}
+
+func (*api) SubscribeTransactionStatuses(
+	_ context.Context,
+	_ *flow.TransactionBody,
+	_ entities.EventEncodingVersion,
+) subscription.Subscription {
+	return nil
 }
