2f15072f9ddd70d11086169c7cc5466d729e85d3
Leo Zhang (zhangchiqing) <zhangchiqing@gmail.com>
2024-09-16 10:23:48 -0700

    add iterator benchmark

--- storage/operation/writes_bench_test.go
+++ storage/operation/writes_bench_test.go
@@ -10,20 +10,6 @@ import (
 	"github.com/onflow/flow-go/storage/operation/dbtest"
 )
 
-func BenchmarkRetrieve(t *testing.B) {
-	dbtest.BenchWithStorages(t, func(t *testing.B, r storage.Reader, withWriter dbtest.WithWriter) {
-		e := Entity{ID: 1337}
-		require.NoError(t, withWriter(operation.Upsert(e.Key(), e)))
-
-		t.ResetTimer()
-
-		for i := 0; i < t.N; i++ {
-			var readBack Entity
-			require.NoError(t, operation.Retrieve(e.Key(), &readBack)(r))
-		}
-	})
-}
-
 func BenchmarkUpsert(t *testing.B) {
 	dbtest.BenchWithStorages(t, func(t *testing.B, r storage.Reader, withWriter dbtest.WithWriter) {
 		for i := 0; i < t.N; i++ {
