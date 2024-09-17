910768bd46f7e5bb436c1e55b75198e5759ad90c
UlyanaAndrukhiv <u.andrukhiv@gmail.com>
2024-09-17 16:49:49 +0300

    Added tests for storing transaction result error messages

---
+++ storage/badger/transaction_result_error_messages_test.go
@@ -0,0 +1,93 @@
+package badger_test
+
+import (
+	"fmt"
+	"testing"
+
+	"github.com/dgraph-io/badger/v2"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+	"golang.org/x/exp/rand"
+
+	"github.com/onflow/flow-go/model/flow"
+	"github.com/onflow/flow-go/module/metrics"
+	"github.com/onflow/flow-go/storage"
+	"github.com/onflow/flow-go/utils/unittest"
+
+	bstorage "github.com/onflow/flow-go/storage/badger"
+)
+
+func TestStoringTransactionResultErrorMessages(t *testing.T) {
+	unittest.RunWithBadgerDB(t, func(db *badger.DB) {
+		metrics := metrics.NewNoopCollector()
+		store := bstorage.NewTransactionResultErrorMessages(metrics, db, 1000)
+
+		blockID := unittest.IdentifierFixture()
+		txErrorMessages := make([]flow.TransactionResultErrorMessage, 0)
+		for i := 0; i < 10; i++ {
+			expected := flow.TransactionResultErrorMessage{
+				TransactionID: unittest.IdentifierFixture(),
+				ErrorMessage:  fmt.Sprintf("a runtime error %d", i),
+				ExecutorID:    unittest.IdentifierFixture(),
+			}
+			txErrorMessages = append(txErrorMessages, expected)
+		}
+		err := store.Store(blockID, txErrorMessages)
+		require.NoError(t, err)
+
+		// check retrieving by ByBlockIDTransactionID
+		for _, txErrorMessage := range txErrorMessages {
+			actual, err := store.ByBlockIDTransactionID(blockID, txErrorMessage.TransactionID)
+			require.NoError(t, err)
+			assert.Equal(t, txErrorMessage, *actual)
+		}
+
+		// check retrieving by ByBlockIDTransactionIndex
+		for i, txErrorMessage := range txErrorMessages {
+			actual, err := store.ByBlockIDTransactionIndex(blockID, uint32(i))
+			require.NoError(t, err)
+			assert.Equal(t, txErrorMessage, *actual)
+		}
+
+		// check retrieving by ByBlockID
+		actual, err := store.ByBlockID(blockID)
+		require.NoError(t, err)
+		assert.Equal(t, txErrorMessages, actual)
+
+		// test loading from database
+		newStore := bstorage.NewTransactionResultErrorMessages(metrics, db, 1000)
+		for _, txErrorMessage := range txErrorMessages {
+			actual, err := newStore.ByBlockIDTransactionID(blockID, txErrorMessage.TransactionID)
+			require.NoError(t, err)
+			assert.Equal(t, txErrorMessage, *actual)
+		}
+
+		// check retrieving by index from both cache and db
+		for i := len(txErrorMessages) - 1; i >= 0; i-- {
+			actual, err := store.ByBlockIDTransactionIndex(blockID, uint32(i))
+			require.NoError(t, err)
+			assert.Equal(t, txErrorMessages[i], *actual)
+
+			actual, err = newStore.ByBlockIDTransactionIndex(blockID, uint32(i))
+			require.NoError(t, err)
+			assert.Equal(t, txErrorMessages[i], *actual)
+		}
+	})
+}
+
+func TestReadingNotStoreTransactionResultErrorMessage(t *testing.T) {
+	unittest.RunWithBadgerDB(t, func(db *badger.DB) {
+		metrics := metrics.NewNoopCollector()
+		store := bstorage.NewTransactionResultErrorMessages(metrics, db, 1000)
+
+		blockID := unittest.IdentifierFixture()
+		txID := unittest.IdentifierFixture()
+		txIndex := rand.Uint32()
+
+		_, err := store.ByBlockIDTransactionID(blockID, txID)
+		assert.ErrorIs(t, err, storage.ErrNotFound)
+
+		_, err = store.ByBlockIDTransactionIndex(blockID, txIndex)
+		assert.ErrorIs(t, err, storage.ErrNotFound)
+	})
+}
