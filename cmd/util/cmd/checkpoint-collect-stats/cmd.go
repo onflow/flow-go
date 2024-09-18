7cf36e89a375082b39fc1be557f13943dc202d03
Faye Amacker <33205765+fxamacker@users.noreply.github.com>
2024-09-18 18:53:58 -0500

    Support all register types in checkpoint-collect-stats
    This commit adds support for all register types, including
    - Cadence 1.0 domain registers, such as path_cap and cap_con
    - evm registers, such as block list hash bucket
    - fvm registers, such as uuid_%d
    - internal registers
    - etc.
    
    This commit also collects stats for all Cadence domains registers,
    in addition to individual domains.  This can be used to track
    domain register growth.

--- cmd/util/cmd/checkpoint-collect-stats/cmd.go
+++ cmd/util/cmd/checkpoint-collect-stats/cmd.go
@@ -2,6 +2,7 @@ package checkpoint_collect_stats
 
 import (
 	"math"
+	"slices"
 	"strings"
 
 	"github.com/montanaflynn/stats"
@@ -15,6 +16,8 @@ import (
 
 	"github.com/onflow/flow-go/cmd/util/ledger/reporters"
 	"github.com/onflow/flow-go/cmd/util/ledger/util"
+	"github.com/onflow/flow-go/fvm/evm/emulator/state"
+	"github.com/onflow/flow-go/fvm/evm/handler"
 	"github.com/onflow/flow-go/ledger"
 	"github.com/onflow/flow-go/ledger/common/pathfinder"
 	"github.com/onflow/flow-go/ledger/complete"
@@ -35,6 +38,14 @@ var (
 
 const (
 	ledgerStatsReportName = "ledger-stats"
+
+	domainTypePrefix = "domain "
+)
+
+const (
+	// EVM register keys from fvm/evm/handler/blockHashList.go
+	blockHashListMetaKey         = "BlockHashListMeta"
+	blockHashListBucketKeyPrefix = "BlockHashListBucket"
 )
 
 var Cmd = &cobra.Command{
@@ -79,15 +90,16 @@ type PayloadStats struct {
 }
 
 type RegisterStatsByTypes struct {
-	Type                    string  `json:"type"`
-	Counts                  uint64  `json:"counts"`
-	ValueSizeTotal          float64 `json:"value_size_total"`
-	ValueSizeMin            float64 `json:"value_size_min"`
-	ValueSize25thPercentile float64 `json:"value_size_25th_percentile"`
-	ValueSizeMedian         float64 `json:"value_size_median"`
-	ValueSize75thPercentile float64 `json:"value_size_75th_percentile"`
-	ValueSize95thPercentile float64 `json:"value_size_95th_percentile"`
-	ValueSizeMax            float64 `json:"value_size_max"`
+	Type                    string                 `json:"type"`
+	Counts                  uint64                 `json:"counts"`
+	ValueSizeTotal          float64                `json:"value_size_total"`
+	ValueSizeMin            float64                `json:"value_size_min"`
+	ValueSize25thPercentile float64                `json:"value_size_25th_percentile"`
+	ValueSizeMedian         float64                `json:"value_size_median"`
+	ValueSize75thPercentile float64                `json:"value_size_75th_percentile"`
+	ValueSize95thPercentile float64                `json:"value_size_95th_percentile"`
+	ValueSizeMax            float64                `json:"value_size_max"`
+	SubTypes                []RegisterStatsByTypes `json:"subtypes,omitempty"`
 }
 
 type sizesByType map[string][]float64
@@ -123,7 +135,9 @@ func run(*cobra.Command, []string) {
 		valueSize := value.Size()
 		totalPayloadSize += uint64(size)
 		totalPayloadValueSize += uint64(valueSize)
-		valueSizesByType[getType(key)] = append(valueSizesByType[getType(key)], float64(valueSize))
+
+		typ := getType(key)
+		valueSizesByType[typ] = append(valueSizesByType[typ], float64(valueSize))
 	}
 
 	var ledgerStats *complete.LedgerStats
@@ -224,58 +238,76 @@ func getPayloadStatsFromCheckpoint(payloadCallBack func(payload *ledger.Payload)
 	return ledgerStats
 }
 
+func getTypeStats(t string, values []float64) RegisterStatsByTypes {
+	sum, err := stats.Sum(values)
+	if err != nil {
+		log.Fatal().Err(err).Msg("cannot compute the sum of values")
+	}
+
+	min, err := stats.Min(values)
+	if err != nil {
+		log.Fatal().Err(err).Msg("cannot compute the min of values")
+	}
+
+	percentile25, err := stats.Percentile(values, 25)
+	if err != nil {
+		log.Fatal().Err(err).Msg("cannot compute the 25th percentile of values")
+	}
+
+	median, err := stats.Median(values)
+	if err != nil {
+		log.Fatal().Err(err).Msg("cannot compute the median of values")
+	}
+
+	percentile75, err := stats.Percentile(values, 75)
+	if err != nil {
+		log.Fatal().Err(err).Msg("cannot compute the 75th percentile of values")
+	}
+
+	percentile95, err := stats.Percentile(values, 95)
+	if err != nil {
+		log.Fatal().Err(err).Msg("cannot compute the 95th percentile of values")
+	}
+
+	max, err := stats.Max(values)
+	if err != nil {
+		log.Fatal().Err(err).Msg("cannot compute the max of values")
+	}
+
+	return RegisterStatsByTypes{
+		Type:                    t,
+		Counts:                  uint64(len(values)),
+		ValueSizeTotal:          sum,
+		ValueSizeMin:            min,
+		ValueSize25thPercentile: percentile25,
+		ValueSizeMedian:         median,
+		ValueSize75thPercentile: percentile75,
+		ValueSize95thPercentile: percentile95,
+		ValueSizeMax:            max,
+	}
+}
+
 func getStats(valueSizesByType sizesByType) []RegisterStatsByTypes {
-	statsByTypes := make([]RegisterStatsByTypes, 0)
+	domainStats := make([]RegisterStatsByTypes, 0, len(util.StorageMapDomains))
+	var allDomainSizes []float64
+
+	statsByTypes := make([]RegisterStatsByTypes, 0, len(valueSizesByType))
 	for t, values := range valueSizesByType {
 
-		sum, err := stats.Sum(values)
-		if err != nil {
-			log.Fatal().Err(err).Msg("cannot compute the sum of values")
-		}
+		stats := getTypeStats(t, values)
 
-		min, err := stats.Min(values)
-		if err != nil {
-			log.Fatal().Err(err).Msg("cannot compute the min of values")
+		if isDomainType(t) {
+			domainStats = append(domainStats, stats)
+			allDomainSizes = append(allDomainSizes, values...)
+		} else {
+			statsByTypes = append(statsByTypes, stats)
 		}
-
-		percentile25, err := stats.Percentile(values, 25)
-		if err != nil {
-			log.Fatal().Err(err).Msg("cannot compute the 25th percentile of values")
-		}
-
-		median, err := stats.Median(values)
-		if err != nil {
-			log.Fatal().Err(err).Msg("cannot compute the median of values")
-		}
-
-		percentile75, err := stats.Percentile(values, 75)
-		if err != nil {
-			log.Fatal().Err(err).Msg("cannot compute the 75th percentile of values")
-		}
-
-		percentile95, err := stats.Percentile(values, 95)
-		if err != nil {
-			log.Fatal().Err(err).Msg("cannot compute the 95th percentile of values")
-		}
-
-		max, err := stats.Max(values)
-		if err != nil {
-			log.Fatal().Err(err).Msg("cannot compute the max of values")
-		}
-
-		statsByTypes = append(statsByTypes,
-			RegisterStatsByTypes{
-				Type:                    t,
-				Counts:                  uint64(len(values)),
-				ValueSizeTotal:          sum,
-				ValueSizeMin:            min,
-				ValueSize25thPercentile: percentile25,
-				ValueSizeMedian:         median,
-				ValueSize75thPercentile: percentile75,
-				ValueSize95thPercentile: percentile95,
-				ValueSizeMax:            max,
-			})
 	}
+
+	allDomainStats := getTypeStats("domain", allDomainSizes)
+	allDomainStats.SubTypes = domainStats
+
+	statsByTypes = append(statsByTypes, allDomainStats)
 
 	return statsByTypes
 }
@@ -288,38 +320,60 @@ func writeStats(reportName string, stats interface{}) {
 	rw.Write(stats)
 }
 
+func isDomainType(typ string) bool {
+	return strings.HasPrefix(typ, domainTypePrefix)
+}
+
 func getType(key ledger.Key) string {
 	k := key.KeyParts[1].Value
 	kstr := string(k)
 
 	if atree.LedgerKeyIsSlabKey(kstr) {
-		return "slab"
+		return "atree slab"
+	}
+
+	isDomain := slices.Contains(util.StorageMapDomains, kstr)
+	if isDomain {
+		return domainTypePrefix + kstr
 	}
 
 	switch kstr {
-	case "storage":
-		return "account's cadence storage domain map"
-	case "private":
-		return "account's cadence private domain map"
-	case "public":
-		return "account's cadence public domain map"
-	case "contract":
-		return "account's cadence contract domain map"
 	case flow.ContractNamesKey:
 		return "contract names"
 	case flow.AccountStatusKey:
 		return "account status"
-	case "uuid":
-		return "uuid generator state"
-	case "account_address_state":
+	case flow.AddressStateKey:
 		return "address generator state"
+	case state.AccountsStorageIDKey:
+		return "account storage ID"
+	case state.CodesStorageIDKey:
+		return "code storage ID"
+	case handler.BlockStoreLatestBlockKey:
+		return "latest block"
+	case handler.BlockStoreLatestBlockProposalKey:
+		return "latest block proposal"
 	}
+
 	// other fvm registers
+	if kstr == "uuid" || strings.HasPrefix(kstr, "uuid_") {
+		return "uuid generator state"
+	}
 	if strings.HasPrefix(kstr, "public_key_") {
 		return "public key"
 	}
 	if strings.HasPrefix(kstr, flow.CodeKeyPrefix) {
 		return "contract content"
 	}
+
+	// other evm registers
+	if strings.HasPrefix(kstr, blockHashListBucketKeyPrefix) {
+		return "block hash list bucket"
+	}
+	if strings.HasPrefix(kstr, blockHashListMetaKey) {
+		return "block hash list meta"
+	}
+
+	log.Warn().Msgf("unknown payload key: %s", kstr)
+
 	return "others"
 }
