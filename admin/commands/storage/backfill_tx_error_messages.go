87451ce99b46871bb128f45999c3a4bee86106f4
UlyanaAndrukhiv <u.andrukhiv@gmail.com>
2024-10-03 14:28:07 +0300

    Updated validation for command

--- admin/commands/storage/backfill_tx_error_messages.go
+++ admin/commands/storage/backfill_tx_error_messages.go
@@ -3,18 +3,19 @@ package storage
 import (
 	"context"
 	"fmt"
-	"strings"
+
+	execproto "github.com/onflow/flow/protobuf/go/flow/execution"
 
 	"github.com/onflow/flow-go/admin"
 	"github.com/onflow/flow-go/admin/commands"
+	"github.com/onflow/flow-go/engine/access/index"
+	"github.com/onflow/flow-go/engine/access/rpc/backend"
 	commonrpc "github.com/onflow/flow-go/engine/common/rpc"
+	"github.com/onflow/flow-go/engine/common/rpc/convert"
 	"github.com/onflow/flow-go/model/flow"
+	"github.com/onflow/flow-go/model/flow/filter"
 	"github.com/onflow/flow-go/state/protocol"
 	"github.com/onflow/flow-go/storage"
-)
-
-const (
-	ANY = "any"
 )
 
 var _ commands.AdminCommand = (*BackfillTxErrorMessagesCommand)(nil)
@@ -22,24 +23,27 @@ var _ commands.AdminCommand = (*BackfillTxErrorMessagesCommand)(nil)
 type backfillTxErrorMessagesRequest struct {
 	startHeight      uint64
 	endHeight        uint64
-	executionNodeIds flow.IdentifierList
+	executionNodeIds flow.IdentityList
 }
 
 type BackfillTxErrorMessagesCommand struct {
 	state           protocol.State
-	txResults       storage.TransactionResults
+	txResultsIndex  *index.TransactionResultsIndex
 	txErrorMessages storage.TransactionResultErrorMessages
+	backend         *backend.Backend
 }
 
 func NewBackfillTxErrorMessagesCommand(
 	state protocol.State,
-	txResults storage.TransactionResults,
+	txResultsIndex *index.TransactionResultsIndex,
 	txErrorMessages storage.TransactionResultErrorMessages,
+	backend *backend.Backend,
 ) commands.AdminCommand {
 	return &BackfillTxErrorMessagesCommand{
 		state:           state,
-		txResults:       txResults,
+		txResultsIndex:  txResultsIndex,
 		txErrorMessages: txErrorMessages,
+		backend:         backend,
 	}
 }
 
@@ -53,55 +57,49 @@ func (b *BackfillTxErrorMessagesCommand) Validator(request *admin.CommandRequest
 
 	data := &backfillTxErrorMessagesRequest{}
 
-	if startHeight, ok := input["start-height"]; ok {
-		if startHeight, err := parseN(startHeight); err != nil {
-			return admin.NewInvalidAdminReqErrorf("invalid 'startHeight' field: %w", err)
-		} else {
+	rootHeight := b.state.Params().SealedRoot().Height
+	data.startHeight = rootHeight // Default value
+
+	if startHeightIn, ok := input["start-height"]; ok {
+		if startHeight, err := parseN(startHeightIn); err != nil {
+			return admin.NewInvalidAdminReqErrorf("invalid 'start-height' field: %w", err)
+		} else if startHeight > rootHeight {
 			data.startHeight = startHeight
 		}
-	} else {
-		data.startHeight = b.state.Params().SealedRoot().Height
 	}
 
-	if endHeight, ok := input["end-height"]; ok {
-		if endHeight, err := parseN(endHeight); err != nil {
-			return admin.NewInvalidAdminReqErrorf("invalid 'endHeight' field: %w", err)
-		} else {
+	sealed, err := b.state.Sealed().Head()
+	if err != nil {
+		return fmt.Errorf("failed to lookup sealed header: %w", err)
+	}
+	data.endHeight = sealed.Height // Default value
+
+	if endHeightIn, ok := input["end-height"]; ok {
+		if endHeight, err := parseN(endHeightIn); err != nil {
+			return admin.NewInvalidAdminReqErrorf("invalid 'end-height' field: %w", err)
+		} else if endHeight < sealed.Height {
 			data.endHeight = endHeight
 		}
-	} else {
-		header, err := b.state.Sealed().Head()
-		if err != nil {
-			return fmt.Errorf("failed to lookup sealed header: %w", err)
-		}
-
-		data.endHeight = header.Height
 	}
 
 	if data.endHeight < data.startHeight {
-		return admin.NewInvalidAdminReqErrorf("end-height %v should not be smaller than start-height %v", data.endHeight, data.startHeight)
+		return admin.NewInvalidAdminReqErrorf("start-height %d should not be smaller than end-height %d", data.startHeight, data.endHeight)
 	}
 
-	if inExecutionNodeIds, ok := input["execution-node-ids"]; ok {
-		switch executionNodeIds := inExecutionNodeIds.(type) {
-		case string:
-			executionNodeIds = strings.ToLower(strings.TrimSpace(executionNodeIds))
-			if executionNodeIds == ANY {
-				data.executionNodeIds = flow.IdentifierList{}
-			} else {
-				return admin.NewInvalidAdminReqErrorf("invalid 'execution-node-ids' field: should be 'any' or list of ids")
-			}
-		case []string:
-			ids, err := commonrpc.IdentifierList(executionNodeIds)
-			if err != nil {
-				return admin.NewInvalidAdminReqErrorf("invalid 'execution-node-ids' field: %w", err)
-			}
-			data.executionNodeIds = ids
-		default:
-			return admin.NewInvalidAdminReqParameterError("execution-node-ids", "must be string or number", inExecutionNodeIds)
+	identities, err := b.state.Final().Identities(filter.HasRole[flow.Identity](flow.RoleExecution))
+	if err != nil {
+		return fmt.Errorf("failed to retreive execution IDs: %w", err)
+	}
+
+	if executionNodeIdsIn, ok := input["execution-node-ids"]; ok {
+		executionNodeIds, err := b.parseExecutionNodeIds(executionNodeIdsIn, identities)
+		if err != nil {
+			return err
 		}
+		data.executionNodeIds = executionNodeIds
 	} else {
-		return fmt.Errorf("'execution-node-ids' field should be provided")
+		// in case no execution node ids provided, the command will use any valid execution node
+		data.executionNodeIds = identities
 	}
 
 	request.ValidatorData = data
@@ -109,6 +107,113 @@ func (b *BackfillTxErrorMessagesCommand) Validator(request *admin.CommandRequest
 	return nil
 }
 
-func (b *BackfillTxErrorMessagesCommand) Handler(ctx context.Context, req *admin.CommandRequest) (interface{}, error) {
+func (b *BackfillTxErrorMessagesCommand) Handler(ctx context.Context, request *admin.CommandRequest) (interface{}, error) {
+	if b.txErrorMessages == nil {
+		return nil, fmt.Errorf("failed to backfill, could not get transaction error messages storage")
+	}
+
+	data := request.ValidatorData.(*backfillTxErrorMessagesRequest)
+
+	for height := data.startHeight; height <= data.endHeight; height++ {
+		header, err := b.state.AtHeight(height).Head()
+		if err != nil {
+			return nil, fmt.Errorf("failed to get block header: %w", err)
+		}
+
+		blockID := header.ID()
+
+		exists, err := b.txErrorMessages.Exists(blockID)
+		if err != nil {
+			return nil, fmt.Errorf("could not check existance of transaction result error messages: %w", err)
+		}
+
+		if exists {
+			continue
+		}
+
+		results, err := b.txResultsIndex.ByBlockID(blockID, height)
+		if err != nil {
+			return nil, fmt.Errorf("failed to get result by block ID: %w", err)
+		}
+
+		fetchTxErrorMessages := false
+		for _, txResult := range results {
+			if txResult.Failed {
+				fetchTxErrorMessages = true
+			}
+		}
+
+		if !fetchTxErrorMessages {
+			continue
+		}
+
+		req := &execproto.GetTransactionErrorMessagesByBlockIDRequest{
+			BlockId: convert.IdentifierToMessage(blockID),
+		}
+
+		resp, execNode, err := b.backend.GetTransactionErrorMessagesFromAnyEN(ctx, data.executionNodeIds.ToSkeleton(), req)
+		if err != nil {
+			return nil, fmt.Errorf("failed to retrieve transaction error messages for block id %#v: %w", blockID, err)
+		}
+
+		err = b.storeTransactionResultErrorMessages(blockID, resp, execNode)
+		if err != nil {
+			return nil, fmt.Errorf("could not store error messages: %w", err)
+		}
+	}
+
 	return nil, nil
+}
+
+func (b *BackfillTxErrorMessagesCommand) parseExecutionNodeIds(executionNodeIdsIn interface{}, allIdentities flow.IdentityList) (flow.IdentityList, error) {
+	var ids flow.IdentityList
+
+	switch executionNodeIds := executionNodeIdsIn.(type) {
+	case []string:
+		if len(executionNodeIds) == 0 {
+			return nil, admin.NewInvalidAdminReqParameterError("execution-node-ids", "must be a non empty list of string", executionNodeIdsIn)
+		}
+		requestedENIdentifiers, err := commonrpc.IdentifierList(executionNodeIds)
+		if err != nil {
+			return nil, admin.NewInvalidAdminReqParameterError("execution-node-ids", err.Error(), executionNodeIdsIn)
+		}
+
+		for _, en := range requestedENIdentifiers {
+			id, exists := allIdentities.ByNodeID(en)
+			if !exists {
+				return nil, admin.NewInvalidAdminReqParameterError("execution-node-ids", "could not found execution nodes by provided ids", executionNodeIdsIn)
+			}
+			ids = append(ids, id)
+		}
+	default:
+		return nil, admin.NewInvalidAdminReqParameterError("execution-node-ids", "must be a list of string", executionNodeIdsIn)
+	}
+
+	return ids, nil
+}
+
+func (b *BackfillTxErrorMessagesCommand) storeTransactionResultErrorMessages(
+	blockID flow.Identifier,
+	errorMessagesResponses []*execproto.GetTransactionErrorMessagesResponse_Result,
+	execNode *flow.IdentitySkeleton,
+) error {
+	errorMessages := make([]flow.TransactionResultErrorMessage, 0)
+	for _, value := range errorMessagesResponses {
+		errorMessage := flow.TransactionResultErrorMessage{
+			ErrorMessage:  value.ErrorMessage,
+			TransactionID: convert.MessageToIdentifier(value.TransactionId),
+			Index:         value.Index,
+			ExecutorID:    execNode.NodeID,
+		}
+		errorMessages = append(errorMessages, errorMessage)
+	}
+
+	if len(errorMessages) > 0 {
+		err := b.txErrorMessages.Store(blockID, errorMessages)
+		if err != nil {
+			return fmt.Errorf("failed to store transaction error messages: %w", err)
+		}
+	}
+
+	return nil
 }
