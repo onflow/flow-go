64ba3b3d4b6b5bad8087649859c1d57a6cd3ed16
UlyanaAndrukhiv <u.andrukhiv@gmail.com>
2024-10-16 12:43:33 +0300

    Updated naming and flag description

--- engine/access/rpc/backend/backend_transactions_test.go
+++ engine/access/rpc/backend/backend_transactions_test.go
@@ -513,7 +513,7 @@ func (suite *Suite) TestLookupTransactionErrorMessageByTransactionID_FailedToFet
 		// Perform the lookup and expect the failed error message to be returned.
 		errMsg, err := backend.LookupErrorMessageByTransactionID(context.Background(), blockId, block.Header.Height, failedTxId)
 		suite.Require().NoError(err)
-		suite.Require().Equal(errMsg, FailedErrorMessage)
+		suite.Require().Equal(errMsg, DefaultFailedErrorMessage)
 		suite.assertAllExpectations()
 	})
 }
@@ -698,7 +698,7 @@ func (suite *Suite) TestLookupTransactionErrorMessageByIndex_FailedToFetch() {
 		// Perform the lookup and expect the failed error message to be returned.
 		errMsg, err := backend.LookupErrorMessageByIndex(context.Background(), blockId, block.Header.Height, failedTxIndex)
 		suite.Require().NoError(err)
-		suite.Require().Equal(errMsg, FailedErrorMessage)
+		suite.Require().Equal(errMsg, DefaultFailedErrorMessage)
 		suite.assertAllExpectations()
 	})
 }
@@ -924,7 +924,7 @@ func (suite *Suite) TestLookupTransactionErrorMessagesByBlockID_FailedToFetch() 
 		expectedTxErrorMessages := make(map[flow.Identifier]string)
 		for _, result := range failedResultsByBlockID {
 			if result.Failed {
-				expectedTxErrorMessages[result.TransactionID] = FailedErrorMessage
+				expectedTxErrorMessages[result.TransactionID] = DefaultFailedErrorMessage
 			}
 		}
 
