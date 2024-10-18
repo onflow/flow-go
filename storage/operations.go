4b211fb523d858202c4090e8dc2dfe125cf56203
Leo Zhang (zhangchiqing) <zhangchiqing@gmail.com>
2024-10-17 17:27:06 -0700

    add batch

--- storage/operations.go
+++ storage/operations.go
@@ -144,6 +144,19 @@ type DB interface {
 	// atomic batch updates to the database.
 	// Any error returned are considered fatal and the batch is not committed.
 	WithReaderBatchWriter(func(ReaderBatchWriter) error) error
+
+	// NewBatch create a new batch for writing.
+	NewBatch() Batch
+}
+
+// Batch is an interface for a batch of writes to a storage backend.
+// The batch is pending until it is committed.
+// Useful for dynamically adding writes to the batch
+type Batch interface {
+	ReaderBatchWriter
+
+	// Commit applies the batched updates to the database.
+	Commit() error
 }
 
 // OnlyWriter is an adapter to convert a function that takes a Writer
