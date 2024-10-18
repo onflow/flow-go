e25393f2ad24f008d04db5c1c55c2f17224aef27
Leo Zhang (zhangchiqing) <zhangchiqing@gmail.com>
2024-10-17 17:27:06 -0700

    add batch

--- storage/operation/pebbleimpl/dbstore.go
+++ storage/operation/pebbleimpl/dbstore.go
@@ -21,3 +21,7 @@ func (b *dbStore) Reader() storage.Reader {
 func (b *dbStore) WithReaderBatchWriter(fn func(storage.ReaderBatchWriter) error) error {
 	return WithReaderBatchWriter(b.db, fn)
 }
+
+func (b *dbStore) NewBatch() storage.Batch {
+	return NewReaderBatchWriter(b.db)
+}
