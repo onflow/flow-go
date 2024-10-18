d7da9d5df03f2ecd254d14822e75cf25ce1b5835
ramtinms <ramtin.seraj@dapperlabs.com>
2024-10-18 10:37:00 -0700

    doc update

--- fvm/evm/offchain/sync/replayer.go
+++ fvm/evm/offchain/sync/replayer.go
@@ -49,6 +49,10 @@ func NewReplayer(
 // which include all the context data for re-executing the transactions, and returns the replay result.
 // this method can be called concurrently if underlying storage
 // tracer and block snapshot provider support concurrency.
+//
+// Warning! the list of transaction events has to be sorted based on their
+// execution, sometimes the access node might return events out of order
+// it needs to be sorted by txIndex and eventIndex respectively.
 func (cr *Replayer) ReplayBlock(
 	transactionEvents []events.TransactionEventPayload,
 	blockEvent *events.BlockEventPayload,
