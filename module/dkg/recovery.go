2fbdd6d319b8ae3b7b3c4d2ccd2a743992b79321
Yurii Oleksyshyn <yuraolex@gmail.com>
2024-10-28 14:59:19 +0200

    Added godoc

--- module/dkg/recovery.go
+++ module/dkg/recovery.go
@@ -14,6 +14,14 @@ import (
 	"github.com/rs/zerolog"
 )
 
+// BeaconKeyRecovery is a specific module that performs automatic recovery of the random beacon private key
+// when exiting the epoch fallback mode.
+// When following the happy path of the protocol, each node that takes part in the DKG eventually obtains a random beacon
+// private key which is stored in storage.DKGState. When the network enters EFM, then eventually the network will be recovered
+// by submitting the recovery epoch which will effectively exit the EFM. When recovering the next epoch,
+// the operator will use a result of last successful DKG. However, each node needs to track this event and correctly fill
+// the storage.DKGState using the last obtained 'my beacon key' for recovered(next) epoch.
+// This module checks if there is a valid 'my beacon key' for the next epoch, and if not, it tries to recover it from the previous epoch.
 type BeaconKeyRecovery struct {
 	events.Noop
 	log           zerolog.Logger
@@ -24,6 +32,10 @@ type BeaconKeyRecovery struct {
 
 var _ protocol.Consumer = &BeaconKeyRecovery{}
 
+// NewBeaconKeyRecovery creates a new BeaconKeyRecovery instance and tries to recover the random beacon private key.
+// This method ensures that we try to recover the random beacon private key even if we have missed the `EpochFallbackModeExited`
+// protocol event (this could happen if the node has crashed between emitting and delivering the event).
+// No errors are expected during normal operations.
 func NewBeaconKeyRecovery(
 	log zerolog.Logger,
 	local module.Local,
@@ -46,6 +58,8 @@ func NewBeaconKeyRecovery(
 	return recovery, nil
 }
 
+// EpochFallbackModeExited implements handler from protocol.Consumer to perform recovery of the beacon private key when
+// this node has exited the epoch fallback mode.
 func (b *BeaconKeyRecovery) EpochFallbackModeExited(epochCounter uint64, _ *flow.Header) {
 	b.log.Info().Msgf("epoch fallback mode exited for epoch %d", epochCounter)
 	err := b.tryRecoverMyBeaconPrivateKey(b.state.Final())
@@ -54,6 +68,13 @@ func (b *BeaconKeyRecovery) EpochFallbackModeExited(epochCounter uint64, _ *flow
 	}
 }
 
+// tryRecoverMyBeaconPrivateKey performs the recovery of the random beacon private key for the next epoch by trying to use
+// a safe 'my beacon key' from the current epoch (it is expected that this method will be called before entering the recovered epoch).
+// If a safe 'my beacon key' is found, it will be stored in the storage.EpochRecoveryMyBeaconKey for the next epoch
+// concluding the 'my beacon key' recovery.
+// If there is a safe 'my beacon key' for the next epoch, or we are not in committed phase (DKG for next epoch is not available)
+// then calling this method is no-op.
+// No errors are expected during normal operations.
 func (b *BeaconKeyRecovery) tryRecoverMyBeaconPrivateKey(final protocol.Snapshot) error {
 	head, err := final.Head()
 	if err != nil {
@@ -81,23 +102,23 @@ func (b *BeaconKeyRecovery) tryRecoverMyBeaconPrivateKey(final protocol.Snapshot
 	}
 	_, safe, err := b.localDKGState.RetrieveMyBeaconPrivateKey(nextEpochCounter)
 	if err != nil && !errors.Is(err, storage.ErrNotFound) {
-		return fmt.Errorf("could not retrieve my beacon private key for next epoch: %w", err)
+		return fmt.Errorf("could not retrieve my beacon private key for the next epoch: %w", err)
 	}
 	if safe {
-		log.Info().Msg("my beacon private key for next epoch is safe, nothing to do")
+		log.Info().Msg("my beacon private key for the next epoch is safe, nothing to do")
 		return nil
 	}
 
 	myBeaconPrivateKey, safe, err := b.localDKGState.RetrieveMyBeaconPrivateKey(currentEpochCounter)
 	if err != nil {
 		if errors.Is(err, storage.ErrNotFound) {
-			log.Warn().Str(logging.KeySuspicious, "true").Msgf("no my beacon key for current epoch has been found")
+			log.Warn().Str(logging.KeySuspicious, "true").Msgf("no my beacon key for the current epoch has been found")
 			return nil
 		}
-		return fmt.Errorf("could not retrieve my beacon private key for current epoch: %w", err)
+		return fmt.Errorf("could not retrieve my beacon private key for the current epoch: %w", err)
 	}
 	if !safe {
-		log.Warn().Str(logging.KeySuspicious, "true").Msgf("my beacon key for current epoch is not safe")
+		log.Warn().Str(logging.KeySuspicious, "true").Msgf("my beacon key for the current epoch is not safe")
 		return nil
 	}
 
@@ -118,7 +139,7 @@ func (b *BeaconKeyRecovery) tryRecoverMyBeaconPrivateKey(final protocol.Snapshot
 		if err != nil {
 			return fmt.Errorf("could not overwrite my beacon private key for the next epoch: %w", err)
 		}
-		log.Info().Msgf("succesfully recovered my beacon private key for next epoch")
+		log.Info().Msgf("succesfully recovered my beacon private key for the next epoch")
 	} else {
 		log.Warn().Str(logging.KeySuspicious, "true").Msgf("available my beacon key is not part of the next epoch DKG")
 	}
