3bec34426850622e5f592b43e39f6617cdae710c
UlyanaAndrukhiv <u.andrukhiv@gmail.com>
2024-10-30 18:37:26 +0200

    Separated websocket models

--- engine/access/rest/routes/websocket_broker.go
+++ engine/access/rest/routes/websocket_broker.go
@@ -29,65 +29,6 @@ const (
 	DefaultSendMessageTimeout            = 10 * time.Second // Default timeout for sending messages
 )
 
-// BaseMessageRequest represents a base structure for incoming messages.
-type BaseMessageRequest struct {
-	Action string `json:"action"` // Action type of the request
-}
-
-// BaseMessageResponse represents a base structure for outgoing messages.
-type BaseMessageResponse struct {
-	Action       string `json:"action,omitempty"`        // Action type of the response
-	Success      bool   `json:"success"`                 // Indicates success or failure
-	ErrorMessage string `json:"error_message,omitempty"` // Error message, if any
-
-}
-
-// SubscribeMessageRequest represents a request to subscribe to a topic.
-type SubscribeMessageRequest struct {
-	BaseMessageRequest
-	Topic     string                 `json:"topic"`     // Topic to subscribe to
-	Arguments map[string]interface{} `json:"arguments"` // Additional arguments for subscription
-}
-
-// SubscribeMessageResponse represents the response to a subscription request.
-type SubscribeMessageResponse struct {
-	BaseMessageResponse
-	Topic string `json:"topic"` // Topic of the subscription
-	ID    string `json:"id"`    // Unique subscription ID
-}
-
-// UnsubscribeMessageRequest represents a request to unsubscribe from a topic.
-type UnsubscribeMessageRequest struct {
-	BaseMessageRequest
-	Topic string `json:"topic"` // Topic to unsubscribe from
-	ID    string `json:"id"`    // Unique subscription ID
-}
-
-// UnsubscribeMessageResponse represents the response to an unsubscription request.
-type UnsubscribeMessageResponse struct {
-	BaseMessageResponse
-	Topic string `json:"topic"` // Topic of the unsubscription
-	ID    string `json:"id"`    // Unique subscription ID
-}
-
-// ListSubscriptionsMessageRequest represents a request to list active subscriptions.
-type ListSubscriptionsMessageRequest struct {
-	BaseMessageRequest
-}
-
-// SubscriptionEntry represents an active subscription entry.
-type SubscriptionEntry struct {
-	Topic string `json:"topic,omitempty"` // Topic of the subscription
-	ID    string `json:"id,omitempty"`    // Unique subscription ID
-}
-
-// ListSubscriptionsMessageResponse is the structure used to respond to list_subscriptions requests.
-// It contains a list of active subscriptions for the current WebSocket connection.
-type ListSubscriptionsMessageResponse struct {
-	BaseMessageResponse
-	Subscriptions []*SubscriptionEntry `json:"subscriptions,omitempty"`
-}
-
 // WebsocketConfig holds configuration for the WebSocketBroker connection.
 type WebsocketConfig struct {
 	MaxSubscriptionsPerConnection uint64
