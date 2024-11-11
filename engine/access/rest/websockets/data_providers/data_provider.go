9f9154015655a31a2726755c566a7b20e6c3f7fd
UlyanaAndrukhiv <u.andrukhiv@gmail.com>
2024-11-11 10:40:34 +0200

    Added godoc for DataProvider interface

--- engine/access/rest/websockets/data_providers/data_provider.go
+++ engine/access/rest/websockets/data_providers/data_provider.go
@@ -1,7 +1,11 @@
 package data_providers
 
+// The DataProvider is the interface abstracts of the actual subscriptions used by the WebSocketCollector.
 type DataProvider interface {
 	BaseDataProvider
 
+	// Run starts processing the subscription and handles responses.
+	//
+	// No errors are expected during normal operations.
 	Run() error
 }
