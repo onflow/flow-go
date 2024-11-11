7502a798ab9f74b3a5f482408259428716f94a2b
UlyanaAndrukhiv <u.andrukhiv@gmail.com>
2024-11-11 11:30:02 +0200

    Added godoc for factory

--- engine/access/rest/websockets/data_providers/factory.go
+++ engine/access/rest/websockets/data_providers/factory.go
@@ -10,6 +10,8 @@ import (
 	"github.com/onflow/flow-go/engine/access/state_stream"
 )
 
+// Constants defining various topic names used to specify different types of
+// data providers.
 const (
 	EventsTopic              = "events"
 	AccountStatusesTopic     = "account_statuses"
@@ -22,6 +24,9 @@ const (
 	BlocksFromLatestTopic           = "blocks_from_latest"
 )
 
+// DataProviderFactory is responsible for creating data providers based on the
+// requested topic. It manages access to logging, state stream configuration,
+// and relevant APIs needed to retrieve data.
 type DataProviderFactory struct {
 	logger            zerolog.Logger
 	eventFilterConfig state_stream.EventFilterConfig
@@ -30,6 +35,13 @@ type DataProviderFactory struct {
 	accessApi      access.API
 }
 
+// NewDataProviderFactory creates a new DataProviderFactory
+//
+// Parameters:
+// - logger: Used for logging within the data providers.
+// - eventFilterConfig: Configuration for filtering events from state streams.
+// - stateStreamApi: API for accessing data from the Flow state stream API.
+// - accessApi: API for accessing data from the Flow Access API.
 func NewDataProviderFactory(
 	logger zerolog.Logger,
 	eventFilterConfig state_stream.EventFilterConfig,
@@ -44,6 +56,16 @@ func NewDataProviderFactory(
 	}
 }
 
+// NewDataProvider creates a new data provider based on the specified topic
+// and configuration parameters.
+//
+// Parameters:
+// - ctx: Context for managing request lifetime and cancellation.
+// - topic: The topic for which a data provider is to be created.
+// - arguments: Configuration arguments for the data provider.
+// - ch: Channel to which the data provider sends data.
+//
+// No errors are expected during normal operations.
 func (s *DataProviderFactory) NewDataProvider(
 	ctx context.Context,
 	topic string,
