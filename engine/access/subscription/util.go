e9c6ac8c149b7483414a10672929941cd5a4ca9c
UlyanaAndrukhiv <u.andrukhiv@gmail.com>
2024-11-12 11:08:44 +0200

    Redactored HandleSubscription to use it in the rest subscription data providers

--- engine/access/subscription/util.go
+++ engine/access/subscription/util.go
@@ -1,8 +1,10 @@
 package subscription
 
 import (
+	"context"
+	"fmt"
+
 	"google.golang.org/grpc/codes"
-	"google.golang.org/grpc/status"
 
 	"github.com/onflow/flow-go/engine/common/rpc"
 )
@@ -11,29 +13,50 @@ import (
 // handles the received responses, and sends the processed information to the client via the provided stream using handleResponse.
 //
 // Parameters:
+// - ctx: Context for the operation.
+// - sub: The subscription.
+// - handleResponse: The function responsible for handling the response of the subscribed type.
+//
+// No errors are expected during normal operations.
+func HandleSubscription[T any](ctx context.Context, sub Subscription, handleResponse func(resp T) error) error {
+	for {
+		select {
+		case v, ok := <-sub.Channel():
+			if !ok {
+				if sub.Err() != nil {
+					return fmt.Errorf("stream encountered an error: %w", sub.Err())
+				}
+				return nil
+			}
+
+			resp, ok := v.(T)
+			if !ok {
+				return fmt.Errorf("unexpected response type: %T", v)
+			}
+
+			err := handleResponse(resp)
+			if err != nil {
+				return err
+			}
+		case <-ctx.Done():
+			return nil
+		}
+	}
+}
+
+// HandleRPCSubscription is a generic handler for subscriptions to a specific type for rpc calls.
+//
+// Parameters:
 // - sub: The subscription.
 // - handleResponse: The function responsible for handling the response of the subscribed type.
 //
 // Expected errors during normal operation:
 //   - codes.Internal: If the subscription encounters an error or gets an unexpected response.
-func HandleSubscription[T any](sub Subscription, handleResponse func(resp T) error) error {
-	for {
-		v, ok := <-sub.Channel()
-		if !ok {
-			if sub.Err() != nil {
-				return rpc.ConvertError(sub.Err(), "stream encountered an error", codes.Internal)
-			}
-			return nil
-		}
-
-		resp, ok := v.(T)
-		if !ok {
-			return status.Errorf(codes.Internal, "unexpected response type: %T", v)
-		}
-
-		err := handleResponse(resp)
-		if err != nil {
-			return err
-		}
+func HandleRPCSubscription[T any](sub Subscription, handleResponse func(resp T) error) error {
+	err := HandleSubscription(nil, sub, handleResponse)
+	if err != nil {
+		return rpc.ConvertError(err, "handle subscription error", codes.Internal)
 	}
+
+	return nil
 }
