63b2325fa601d551a0e728fe74134ebf8cd03381
Ardit Marku <markoupetr@gmail.com>
2024-11-17 15:56:26 +0200

    Delete account with zero address before gas fee collector switch

--- fvm/evm/offchain/sync/replay.go
+++ fvm/evm/offchain/sync/replay.go
@@ -5,11 +5,13 @@ import (
 	"fmt"
 
 	"github.com/onflow/atree"
+	gethCommon "github.com/onflow/go-ethereum/common"
 	gethTypes "github.com/onflow/go-ethereum/core/types"
 	gethTracer "github.com/onflow/go-ethereum/eth/tracers"
 	gethTrie "github.com/onflow/go-ethereum/trie"
 
 	"github.com/onflow/flow-go/fvm/evm/emulator"
+	"github.com/onflow/flow-go/fvm/evm/emulator/state"
 	"github.com/onflow/flow-go/fvm/evm/events"
 	"github.com/onflow/flow-go/fvm/evm/precompiles"
 	"github.com/onflow/flow-go/fvm/evm/types"
@@ -105,6 +107,20 @@ func replayTransactionExecution(
 			return fmt.Errorf("error decoding precompiled calls [%x]: %w", txEvent.Payload, err)
 		}
 		ctx.ExtraPrecompiledContracts = precompiles.AggregatedPrecompiledCallsToPrecompiledContracts(pcs)
+	}
+
+	if ctx.BlockNumber == 8008603 && ctx.ChainID == types.FlowEVMTestNetChainID {
+		//delete 0 address to fix gasUsage issue on testnet
+		base, err := state.NewBaseView(ledger, rootAddr)
+		if err != nil {
+			return err
+		}
+		if err := base.DeleteAccount(gethCommon.Address{}); err != nil {
+			return err
+		}
+		if err := base.Commit(); err != nil {
+			return err
+		}
 	}
 
 	// create a new block view
