73070f9b5fd4860e1aa4dfd3b1bb07145db8b6c0
Leo Zhang (zhangchiqing) <zhangchiqing@gmail.com>
2024-11-26 22:00:51 -0800

    add debugging tests

---
+++ cmd/util/cmd/verify-evm-offchain-replay/verify_test.go
@@ -0,0 +1,38 @@
+package verify
+
+import (
+	"testing"
+
+	"github.com/rs/zerolog/log"
+	"github.com/stretchr/testify/require"
+
+	"github.com/onflow/flow-go/model/flow"
+)
+
+func TestTestnetFromRootToLatest(t *testing.T) {
+	// verify Testnet51
+	require.NoError(t, Verify(log.Logger, 211176671, 218215348,
+		flow.Testnet, "/var/flow/data/protocol", "/var/flow/data/execution_data",
+		"/var/flow/nov26_testnet_evm_state_gob", 100_000),
+	)
+
+	// verify Testnet52
+	require.NoError(t, Verify(log.Logger, 218215349, 228901661,
+		flow.Testnet, "/var/flow52/data/protocol", "/var/flow52/data/execution_data",
+		"/var/flow/nov26_testnet_evm_state_gob", 100_000),
+	)
+}
+
+func TestMainnetFromRootToLatest(t *testing.T) {
+	// verify Mainnet25
+	require.NoError(t, Verify(log.Logger, 85981135, 88226266,
+		flow.Testnet, "/var/flow/data/protocol", "/var/flow/data/execution_data",
+		"/var/flow/nov26_mainnet_evm_state_gob", 100_000),
+	)
+
+	// verify Mainnet26
+	require.NoError(t, Verify(log.Logger, 88226267, 94800210,
+		flow.Testnet, "/var/flow52/data/protocol", "/var/flow52/data/execution_data",
+		"/var/flow/nov26_mainnet_evm_state_gob", 100_000),
+	)
+}
