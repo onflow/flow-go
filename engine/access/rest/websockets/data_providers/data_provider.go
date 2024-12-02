437af4f6cdb52aaddc8b07cb80943b93c88dceed
UlyanaAndrukhiv <u.andrukhiv@gmail.com>
2024-12-02 14:57:31 +0200

    Added additional description for websocket data providers Run method

--- engine/access/rest/websockets/data_providers/data_provider.go
+++ engine/access/rest/websockets/data_providers/data_provider.go
@@ -17,6 +17,17 @@ type DataProvider interface {
 	Close() error
 	// Run starts processing the subscription and handles responses.
 	//
+	// The separation of the data provider's creation and its Run() method
+	// allows for better control over the subscription lifecycle. By doing so,
+	// a confirmation message can be sent to the client immediately upon
+	// successful subscription creation or failure. This ensures any required
+	// setup or preparation steps can be handled prior to initiating the
+	// subscription and data streaming process.
+	//
+	// Run() begins the actual processing of the subscription. At this point,
+	// the context used for provider creation is no longer needed, as all
+	// necessary preparation steps should have been completed.
+	//
 	// No errors are expected during normal operations.
 	Run() error
 }
