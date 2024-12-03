bb7ae2e80be91c050f316335fd0243ee448b07c7
Bastian Müller <bastian@turbolent.com>
2024-12-03 15:30:51 -0800

    adjust code to Cadence API changes related to storage map domains

--- cmd/util/ledger/migrations/utils.go
+++ cmd/util/ledger/migrations/utils.go
@@ -1,10 +1,6 @@
 package migrations
 
 import (
-	"github.com/onflow/cadence/common"
-	"github.com/onflow/cadence/runtime"
-	"github.com/onflow/cadence/stdlib"
-
 	"github.com/onflow/flow-go/cmd/util/ledger/util/registers"
 )
 
@@ -13,23 +9,4 @@ type RegistersMigration func(registersByAccount *registers.ByAccount) error
 type NamedMigration struct {
 	Name    string
 	Migrate RegistersMigration
-}
-
-var AllStorageMapDomains = []string{
-	common.PathDomainStorage.Identifier(),
-	common.PathDomainPrivate.Identifier(),
-	common.PathDomainPublic.Identifier(),
-	runtime.StorageDomainContract,
-	stdlib.InboxStorageDomain,
-	stdlib.CapabilityControllerStorageDomain,
-	stdlib.PathCapabilityStorageDomain,
-	stdlib.AccountCapabilityStorageDomain,
-}
-
-var allStorageMapDomainsSet = map[string]struct{}{}
-
-func init() {
-	for _, domain := range AllStorageMapDomains {
-		allStorageMapDomainsSet[domain] = struct{}{}
-	}
 }
