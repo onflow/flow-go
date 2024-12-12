69c99b059d9be9969b8df8cc39346ee10867ca6c
Tim Barry <tim.barry@flowfoundation.org>
2024-12-12 10:49:39 -0800

    Replace AsyncUploader fifoqueue with channel
    Instead of using a notifier and fifoqueue with metrics, use a buffered channel.
    Block when the channel is full. (Reasoning: we want to ensure no uploads get dropped.)
    Buffer size of 100 was chosen arbitrarily. All AsyncUploader tests now pass.

--- engine/execution/ingestion/uploader/uploader.go
+++ engine/execution/ingestion/uploader/uploader.go
@@ -2,13 +2,10 @@ package uploader
 
 import (
 	"context"
-	"fmt"
 	"time"
 
 	"github.com/rs/zerolog"
 
-	"github.com/onflow/flow-go/engine"
-	"github.com/onflow/flow-go/engine/common/fifoqueue"
 	"github.com/onflow/flow-go/engine/execution"
 	"github.com/onflow/flow-go/module"
 	"github.com/onflow/flow-go/module/component"
@@ -30,16 +27,13 @@ func NewAsyncUploader(uploader Uploader,
 	maxRetryNumber uint64,
 	log zerolog.Logger,
 	metrics module.ExecutionMetrics) *AsyncUploader {
-	// TODO queue size, add length metrics and check error
-	queue, _ := fifoqueue.NewFifoQueue(1000)
 	a := &AsyncUploader{
 		uploader:            uploader,
 		log:                 log.With().Str("component", "block_data_uploader").Logger(),
 		metrics:             metrics,
 		retryInitialTimeout: retryInitialTimeout,
 		maxRetryNumber:      maxRetryNumber,
-		queue:               queue,
-		notifier:            engine.NewNotifier(),
+		queue:               make(chan *execution.ComputationResult, 100),
 	}
 	builder := component.NewComponentManagerBuilder()
 	for range 3 {
@@ -58,8 +52,7 @@ type AsyncUploader struct {
 	retryInitialTimeout time.Duration
 	maxRetryNumber      uint64
 	onComplete          OnCompleteFunc // callback function called after Upload is completed
-	queue               *fifoqueue.FifoQueue
-	notifier            engine.Notifier
+	queue               chan *execution.ComputationResult
 	cm                  *component.ComponentManager
 	component.Component
 	// TODO Replace fifoqueue with channel, and make Upload() blocking
@@ -71,40 +64,12 @@ func (a *AsyncUploader) UploadWorker(ctx irrecoverable.SignalerContext, ready co
 	ready()
 
 	done := ctx.Done()
-	wake := a.notifier.Channel()
 	for {
 		select {
 		case <-done:
 			return
-		case <-wake:
-			err := a.processUploadTasks(ctx)
-			if err != nil {
-				ctx.Throw(err)
-			}
-		}
-	}
-}
-
-// processUploadTasks processes any available tasks from the queue.
-// Only returns when the queue is empty (or the component is terminated).
-// No errors expected during normal operation.
-func (a *AsyncUploader) processUploadTasks(ctx context.Context) error {
-	for {
-		item, ok := a.queue.Pop()
-		if !ok {
-			return nil
-		}
-
-		computationResult, ok := item.(*execution.ComputationResult)
-		if !ok {
-			return fmt.Errorf("invalid type in AsyncUploader queue")
-		}
-
-		a.UploadTask(ctx, computationResult)
-		select {
-		case <-ctx.Done():
-			return nil
-		default:
+		case computationResult := <-a.queue:
+			a.UploadTask(ctx, computationResult)
 		}
 	}
 }
@@ -117,11 +82,7 @@ func (a *AsyncUploader) SetOnCompleteCallback(onComplete OnCompleteFunc) {
 // ensuring that multiple uploads can be run in parallel.
 // No errors expected during normal operation.
 func (a *AsyncUploader) Upload(computationResult *execution.ComputationResult) error {
-	if a.queue.Push(computationResult) {
-		a.notifier.Notify()
-	} else {
-		// TODO record in metrics
-	}
+	a.queue <- computationResult
 	return nil
 }
 
