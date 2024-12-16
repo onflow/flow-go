f285e90f123f3aab4ca5862788fd80763fa515de
Leo Zhang <zhangchiqing@gmail.com>
2024-12-16 15:26:06 -0800

    Apply suggestions from code review
    Co-authored-by: Jordan Schalm <jordan.schalm@flowfoundation.org>

--- utils/merr/closer.go
+++ utils/merr/closer.go
@@ -9,20 +9,18 @@ import (
 // CloseAndMergeError close the closable and merge the closeErr with the given err into a multierror
 // Note: when using this function in a defer function, don't use as below:
 // func XXX() (
-//
 //	err error,
 //	) {
-//		def func() {
-//			// bad, because the definition of err might get overwritten
+//		defer func() {
+//			// bad, because the definition of err might get overwritten by another deferred function
 //			err = closeAndMergeError(closable, err)
 //		}()
 //
 // Better to use as below:
 // func XXX() (
-//
 //	errToReturn error,
 //	) {
-//		def func() {
+//		defer func() {
 //			// good, because the error to returned is only updated here, and guaranteed to be returned
 //			errToReturn = closeAndMergeError(closable, errToReturn)
 //		}()
