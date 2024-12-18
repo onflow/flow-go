7b8ee773e4ec1a51863dcc8cb7ed530b9a2e51c7
Andrii <andriy.dyachuk95@gmail.com>
2024-12-18 15:27:29 +0200

    Initializing msg index by 0

--- engine/access/rest/websockets/data_providers/account_statuses_provider.go
+++ engine/access/rest/websockets/data_providers/account_statuses_provider.go
@@ -99,7 +99,7 @@ func (p *AccountStatusesDataProvider) createSubscription(ctx context.Context, ar
 // No errors are expected during normal operations.
 func (p *AccountStatusesDataProvider) handleResponse() func(accountStatusesResponse *backend.AccountStatusesResponse) error {
 	blocksSinceLastMessage := uint64(0)
-	messageIndex := counters.NewMonotonousCounter(1)
+	messageIndex := counters.NewMonotonousCounter(0)
 
 	return func(accountStatusesResponse *backend.AccountStatusesResponse) error {
 		// check if there are any events in the response. if not, do not send a message unless the last
@@ -112,10 +112,10 @@ func (p *AccountStatusesDataProvider) handleResponse() func(accountStatusesRespo
 			blocksSinceLastMessage = 0
 		}
 
-		index := messageIndex.Value()
 		if ok := messageIndex.Set(messageIndex.Value() + 1); !ok {
 			return status.Errorf(codes.Internal, "message index already incremented to %d", messageIndex.Value())
 		}
+		index := messageIndex.Value()
 
 		p.send <- &models.AccountStatusesResponse{
 			BlockID:       accountStatusesResponse.BlockID.String(),
