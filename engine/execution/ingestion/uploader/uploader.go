602a4cc71fa2b3db17c209950d5f7f38044c0ec4
Tim Barry <21149133+tim-barry@users.noreply.github.com>
2024-12-20 10:04:49 -0800

    Apply suggestions from code review
    Channel size of 20000 and worker count of 100 suggested by Leo. 20000 is approximately equal to 4 hours of execution results.
    
    Co-authored-by: Leo Zhang <zhangchiqing@gmail.com>

--- engine/execution/ingestion/uploader/uploader.go
+++ engine/execution/ingestion/uploader/uploader.go
@@ -35,10 +35,10 @@ func NewAsyncUploader(uploader Uploader,
 		maxRetryNumber:      maxRetryNumber,
 		// we use a channel rather than a Fifoqueue here because a Fifoqueue might drop items when full,
 		// but it is not acceptable to skip uploading an execution result
-		queue:               make(chan *execution.ComputationResult, 100),
+		queue:               make(chan *execution.ComputationResult, 20000),
 	}
 	builder := component.NewComponentManagerBuilder()
-	for i := 0; i < 3; i++ {
+	for i := 0; i < 10; i++ {
 		builder.AddWorker(a.UploadWorker)
 	}
 	a.cm = builder.Build()
