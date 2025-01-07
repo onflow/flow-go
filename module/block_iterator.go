c78325c667920c314daf1d7e8929e7a8b3a03f7d
Leo Zhang (zhangchiqing) <zhangchiqing@gmail.com>
2025-01-07 04:44:34 -0800

    add block iterator

---
+++ module/block_iterator.go
@@ -0,0 +1,23 @@
+package module
+
+import "github.com/onflow/flow-go/model/flow"
+
+// IterateJob defines the height range of blocks to iterate over
+type IterateJob struct {
+	Start uint64 // the start height of the range
+	End   uint64 // the end height of the range
+}
+
+// BlockIterator is an interface for iterating over blocks
+type BlockIterator interface {
+	// Next returns the next block in the iterator
+	// Note: a block will only be iterated once in a single iteration, however
+	// if the iteration is interrupted (e.g. by a restart), the iterator can be
+	// resumed from the last checkpoint, which might result in the same block being
+	// iterated again.
+	Next() (blockID flow.Identifier, hasNext bool, exception error)
+
+	// Checkpoint saves the current state of the iterator
+	// so that it can be resumed later
+	Checkpoint() error
+}
