79ac35edca480413f75736cd46f54e7345d5dfa6
Ardit Marku <markoupetr@gmail.com>
2025-01-14 13:31:58 +0200

    Better naming for BalanceInAttFlowValidForFlowVault and fix logical condition

--- fvm/evm/emulator/emulator.go
+++ fvm/evm/emulator/emulator.go
@@ -381,7 +381,7 @@ func (proc *procedure) withdrawFrom(
 		), nil
 	}
 
-	if types.BalanceInAttFlowValidForFlowVault(call.Value) {
+	if !types.AttoFlowBalanceValidForFlowVault(call.Value) {
 		return types.NewInvalidResult(
 			call.Transaction(),
 			types.ErrWithdrawBalanceRounding,
