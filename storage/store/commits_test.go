67825993e9c94a7dc6c4b904fa9e5c113c42c5c6
Leo Zhang (zhangchiqing) <zhangchiqing@gmail.com>
2025-01-14 13:43:03 -0800

    refactor commits

--- storage/store/commits_test.go
+++ storage/store/commits_test.go
@@ -1,10 +1,9 @@
-package badger_test
+package store_test
 
 import (
 	"errors"
 	"testing"
 
-	"github.com/dgraph-io/badger/v2"
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
 
@@ -12,32 +11,33 @@ import (
 	"github.com/onflow/flow-go/storage"
 	"github.com/onflow/flow-go/utils/unittest"
 
-	badgerstorage "github.com/onflow/flow-go/storage/badger"
+	"github.com/onflow/flow-go/storage/operation/dbtest"
+	"github.com/onflow/flow-go/storage/store"
 )
 
-// TestCommitsStoreAndRetrieve tests that a commit can be stored, retrieved and attempted to be stored again without an error
+// TestCommitsStoreAndRetrieve tests that a commit can be store1d, retrieved and attempted to be stored again without an error
 func TestCommitsStoreAndRetrieve(t *testing.T) {
-	unittest.RunWithBadgerDB(t, func(db *badger.DB) {
+	dbtest.RunWithDB(t, func(t *testing.T, db storage.DB) {
 		metrics := metrics.NewNoopCollector()
-		store := badgerstorage.NewCommits(metrics, db)
+		store1 := store.NewCommits(metrics, db)
 
 		// attempt to get a invalid commit
-		_, err := store.ByBlockID(unittest.IdentifierFixture())
+		_, err := store1.ByBlockID(unittest.IdentifierFixture())
 		assert.True(t, errors.Is(err, storage.ErrNotFound))
 
-		// store a commit in db
+		// store1 a commit in db
 		blockID := unittest.IdentifierFixture()
 		expected := unittest.StateCommitmentFixture()
-		err = store.Store(blockID, expected)
+		err = store1.Store(blockID, expected)
 		require.NoError(t, err)
 
 		// retrieve the commit by ID
-		actual, err := store.ByBlockID(blockID)
+		actual, err := store1.ByBlockID(blockID)
 		require.NoError(t, err)
 		assert.Equal(t, expected, actual)
 
 		// re-insert the commit - should be idempotent
-		err = store.Store(blockID, expected)
+		err = store1.Store(blockID, expected)
 		require.NoError(t, err)
 	})
 }
