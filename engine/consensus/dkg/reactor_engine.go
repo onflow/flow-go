04bd4a6b672e2ce7d936bbdeb0594230da05c8a1
Jordan Schalm <jordan@dapperlabs.com>
2025-01-17 14:43:53 -0800

    add sanity check: re-check committed beacon key

--- engine/consensus/dkg/reactor_engine.go
+++ engine/consensus/dkg/reactor_engine.go
@@ -284,13 +284,16 @@ func (e *ReactorEngine) handleEpochCommittedPhaseStarted(currentEpochCounter uin
 
 		return
 	}
-	if currentState != flow.DKGStateCompleted {
-		if currentState == flow.RandomBeaconKeyCommitted {
-			// this can happen if a healthy node which succeeded the DKG restarts in the EpochCommit phase
-			log.Debug().Msg("checking beacon key consistency after EpochCommit: observed committed beacon key for most recent dkg - exiting")
-		} else {
-			log.Warn().Msgf("checking beacon key consistency after EpochCommit: exiting because dkg didn't reach completed state: %s", currentState.String())
-		}
+	// (i) if I have a key (currentState == flow.DKGStateCompleted) which is consistent with the EpochCommit service event,
+	// then commit the key or (ii) if the key is already committed (currentState == flow.RandomBeaconKeyCommitted), then we
+	// expect it to be consistent with the EpochCommit service event. While (ii) is a sanity check, we have a severe problem
+	// if it is violated, because a node signing with an invalid Random beacon key will be slashed - so we better check!
+	// Our logic for committing a key is idempotent: it is a no-op when stating that a key `k` should be committed that previously
+	// has already been committed; while it errors if `k` is different from the previously-committed key. In other words, the
+	// sanity check (ii) is already included in the happy-path logic for (i). So we just repeat the happy-path logic also for
+	// currentState == flow.RandomBeaconKeyCommitted, because repeated calls only occur due to node crashes, which are rare.
+	if currentState != flow.DKGStateCompleted && currentState != flow.RandomBeaconKeyCommitted {
+		log.Warn().Msgf("checking beacon key consistency after EpochCommit: exiting because dkg didn't reach completed state: %s", currentState.String())
 		return
 	}
 
