748f5a3622dd5c2de9a40d9a242ea7840f2f5c8f
Leo Zhang (zhangchiqing) <zhangchiqing@gmail.com>
2025-01-23 14:01:30 -0800

    update initializer to take root

--- module/block_iterator/initializer.go
+++ module/block_iterator/initializer.go
@@ -10,27 +10,22 @@ import (
 
 type ProgressInitializer struct {
 	progress storage.ConsumerProgress
-	getRoot  func() (uint64, error)
+	root     uint64
 }
 
 var _ module.IterateProgressInitializer = (*ProgressInitializer)(nil)
 
-func NewInitializer(progress storage.ConsumerProgress, getRoot func() (uint64, error)) *ProgressInitializer {
+func NewInitializer(progress storage.ConsumerProgress, root uint64) *ProgressInitializer {
 	return &ProgressInitializer{
 		progress: progress,
-		getRoot:  getRoot,
+		root:     root,
 	}
 }
 
 func (h *ProgressInitializer) Init() (module.IterateProgressReader, module.IterateProgressWriter, error) {
 	_, err := h.progress.ProcessedIndex()
 	if errors.Is(err, storage.ErrNotFound) {
-		root, err := h.getRoot()
-		if err != nil {
-			return nil, nil, fmt.Errorf("failed to get root block: %w", err)
-		}
-
-		next := root + 1
+		next := h.root + 1
 		err = h.progress.InitProcessedIndex(next)
 		if err != nil {
 			return nil, nil, fmt.Errorf("failed to init processed index: %w", err)
