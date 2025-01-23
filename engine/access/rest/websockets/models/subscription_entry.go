7413e0281bdc30b827ed927256d028c623ec1436
Peter Argue <89119817+peterargue@users.noreply.github.com>
2025-01-23 10:46:08 -0800

    Update engine/access/rest/websockets/models/subscription_entry.go

--- engine/access/rest/websockets/models/subscription_entry.go
+++ engine/access/rest/websockets/models/subscription_entry.go
@@ -3,6 +3,6 @@ package models
 // SubscriptionEntry represents an active subscription entry.
 type SubscriptionEntry struct {
 	SubscriptionID string    `json:"subscription_id"` // ID is a client generated UUID for subscription
-	Topic          string    `json:"topic"`     // Topic of the subscription
+	Topic          string    `json:"topic"`           // Topic of the subscription
 	Arguments      Arguments `json:"arguments"`
 }
