92ac8d18e6721e6d63d8f6b0326639c43e5ea01c
Leo Zhang (zhangchiqing) <zhangchiqing@gmail.com>
2025-01-24 10:54:55 -0800

    add headers.BlockIDByView methods

--- storage/badger/headers_test.go
+++ storage/badger/headers_test.go
@@ -50,3 +50,34 @@ func TestHeaderRetrieveWithoutStore(t *testing.T) {
 		require.True(t, errors.Is(err, storage.ErrNotFound))
 	})
 }
+
+func TestHeaderGetByView(t *testing.T) {
+
+	unittest.RunWithBadgerDB(t, func(db *badger.DB) {
+		metrics := metrics.NewNoopCollector()
+		headers := badgerstorage.NewHeaders(metrics, db)
+
+		block := unittest.BlockFixture()
+
+		// store header
+		err := headers.Store(block.Header)
+		require.NoError(t, err)
+
+		// verify storing the block doesn't not index the view automatically
+		_, err = headers.BlockIDByView(block.Header.View)
+		require.ErrorIs(t, err, storage.ErrNotFound)
+
+		// index block by view
+		require.NoError(t, db.Update(operation.IndexBlockView(block.Header.View, block.ID())))
+
+		// verify that the block ID can be retrieved by view
+		indexedID, err := headers.BlockIDByView(block.Header.View)
+		require.NoError(t, err)
+		require.Equal(t, block.ID(), indexedID)
+
+		// verify that the block header can be retrieved by view
+		actual, err := headers.ByView(block.Header.View)
+		require.NoError(t, err)
+		require.Equal(t, block.Header, actual)
+	})
+}
