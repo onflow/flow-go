8ac6287c6ee3cf6e7ba28905a464334660e9380b
Leo Zhang (zhangchiqing) <zhangchiqing@gmail.com>
2025-01-27 11:16:04 -0800

    add executor aggregator

---
+++ module/block_iterator/executor/aggregator.go
@@ -0,0 +1,28 @@
+package executor
+
+import (
+	"github.com/onflow/flow-go/model/flow"
+	"github.com/onflow/flow-go/storage"
+)
+
+type AggregatedExecutor struct {
+	executors []IterationExecutor
+}
+
+var _ IterationExecutor = (*AggregatedExecutor)(nil)
+
+func NewAggregatedExecutor(executors []IterationExecutor) IterationExecutor {
+	return &AggregatedExecutor{
+		executors: executors,
+	}
+}
+
+func (a *AggregatedExecutor) ExecuteByBlockID(blockID flow.Identifier, batch storage.ReaderBatchWriter) (exception error) {
+	for _, executor := range a.executors {
+		exception = executor.ExecuteByBlockID(blockID, batch)
+		if exception != nil {
+			return exception
+		}
+	}
+	return nil
+}
