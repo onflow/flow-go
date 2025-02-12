8662256b989fdf8cfcf24f35b4ff891613e30670
Tim Barry <tim.barry@flowfoundation.org>
2025-02-12 11:15:28 -0800

    Update doc comments for errors
    Co-authored-by: Jordan Schalm <jordan.schalm@flowfoundation.org>

--- state/protocol/badger/snapshot.go
+++ state/protocol/badger/snapshot.go
@@ -383,6 +383,7 @@ type EpochQuery struct {
 var _ protocol.EpochQuery = (*EpochQuery)(nil)
 
 // Current returns the current epoch.
+// No errors are expected during normal operation.
 func (q *EpochQuery) Current() (protocol.CommittedEpoch, error) {
 	// all errors returned from storage reads here are unexpected, because all
 	// snapshots reside within a current epoch, which must be queryable
@@ -403,9 +404,13 @@ func (q *EpochQuery) Current() (protocol.CommittedEpoch, error) {
 	return inmem.NewCommittedEpoch(setup, epochState.EpochExtensions(), commit), nil
 }
 
-// NextUnsafe returns the next epoch, if it has been setup but not yet committed.
+// NextUnsafe returns the next epoch, if it has been set up but not yet committed.
+// Error returns:
+//   - protocol.ErrNextEpochNotSetup if the next epoch has not yet been set up as of the snapshot's reference block
+//     (the reference block resides in the EpochStaking phase)
+//   - protocol.ErrNextEpochAlreadyCommitted if the next epoch has already been committed at the snapshot's reference block
+//     (the reference block resides in the EpochCommitted phase)
 func (q *EpochQuery) NextUnsafe() (protocol.TentativeEpoch, error) {
-
 	epochState, err := q.snap.state.protocolState.EpochStateAtBlockID(q.snap.blockID)
 	if err != nil {
 		return nil, fmt.Errorf("could not get protocol state snapshot at block %x: %w", q.snap.blockID, err)
@@ -430,6 +435,9 @@ func (q *EpochQuery) NextUnsafe() (protocol.TentativeEpoch, error) {
 }
 
 // NextCommitted returns the next epoch as of this snapshot, only if it has been committed already.
+// Error returns:
+//   - protocol.ErrNextEpochNotCommitted if the next epoch has not yet been committed at the snapshot's reference block
+//     (the reference block does not reside in the EpochCommitted phase)
 func (q *EpochQuery) NextCommitted() (protocol.CommittedEpoch, error) {
 	epochState, err := q.snap.state.protocolState.EpochStateAtBlockID(q.snap.blockID)
 	if err != nil {
@@ -454,10 +462,9 @@ func (q *EpochQuery) NextCommitted() (protocol.CommittedEpoch, error) {
 }
 
 // Previous returns the previous epoch. During the first epoch after the root
-// block, this returns a sentinel error (since there is no previous epoch).
+// block, this returns protocol.ErrNoPreviousEpoch (since there is no previous epoch).
 // For all other epochs, returns the previous epoch.
 func (q *EpochQuery) Previous() (protocol.CommittedEpoch, error) {
-
 	epochState, err := q.snap.state.protocolState.EpochStateAtBlockID(q.snap.blockID)
 	if err != nil {
 		return nil, fmt.Errorf("could not get protocol state snapshot at block %x: %w", q.snap.blockID, err)
