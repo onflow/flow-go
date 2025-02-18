58e379953390cc3f497aa5e1eb9af042e4074a59
Ardit Marku <markoupetr@gmail.com>
2025-02-18 19:29:01 +0200

    Fix typo in GetBlockView function description
    Co-authored-by: Peter Argue <89119817+peterargue@users.noreply.github.com>

--- fvm/evm/offchain/query/viewProvider.go
+++ fvm/evm/offchain/query/viewProvider.go
@@ -36,7 +36,7 @@ func NewViewProvider(
 // GetBlockView returns the block view for the given height (at the end of a block!)
 // The `GetSnapshotAt` function of `storageProvider`, will return
 // the block state at its start, before any transaction executions.
-// This is the indented functionality, when replaying & verifying blocks.
+// This is the intended functionality, when replaying & verifying blocks.
 // However, when reading the state from a block, we are interested
 // in its end state, after all transaction executions.
 // That is why we fetch the block snapshot at the next height.
