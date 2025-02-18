2372da73b0bb5d5202018acb6fc5a2a713f6f1ef
Tim Barry <tim.barry@flowfoundation.org>
2025-02-18 10:33:41 -0800

    [Malleability] Seal: remove duplicated test

--- model/flow/seal_test.go
+++ model/flow/seal_test.go
@@ -3,8 +3,6 @@ package flow_test
 import (
 	"testing"
 
-	"github.com/stretchr/testify/assert"
-
 	"github.com/onflow/flow-go/utils/unittest"
 )
 
@@ -14,21 +12,4 @@ import (
 func TestSealMalleability(t *testing.T) {
 	seal := unittest.Seal.Fixture()
 	unittest.RequireEntityNotMalleable(t, seal)
-}
-
-// Test_SealID checks that two seals that only differ in their approval
-// signatures have different IDs. This is REQUIRED FOR the STORAGE layer
-// to correctly retrieve the block payload!
-func Test_SealID(t *testing.T) {
-
-	seal := unittest.Seal.Fixture()
-	id := seal.ID()
-	cs := seal.Checksum()
-
-	// Change signatures of first chunk
-	seal.AggregatedApprovalSigs[0] = unittest.Seal.AggregatedSignatureFixture()
-
-	// The ID of the seal should now be different
-	assert.NotEqual(t, id, seal.ID())
-	assert.NotEqual(t, cs, seal.Checksum())
 }
