51798a16b1339fbacf7c83d98501c618b9fe7499
Leo Zhang (zhangchiqing) <zhangchiqing@gmail.com>
2025-02-18 13:26:33 -0800

    fix lint

--- engine/testutil/nodes.go
+++ engine/testutil/nodes.go
@@ -315,7 +315,7 @@ func CollectionNode(t *testing.T, hub *stub.Hub, identity bootstrap.NodeInfo, ro
 		retrieve)
 	require.NoError(t, err)
 
-	pusherEngine, err := pusher.New(node.Log, node.Net, node.State, node.Metrics, node.Metrics, node.Me, collections, transactions)
+	pusherEngine, err := pusher.New(node.Log, node.Net, node.State, node.Metrics, node.Metrics, node.Me)
 	require.NoError(t, err)
 
 	clusterStateFactory, err := factories.NewClusterStateFactory(
