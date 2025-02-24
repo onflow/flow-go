6f5e71507ee47f81f3557950a829d112f2629a11
Leo Zhang (zhangchiqing) <zhangchiqing@gmail.com>
2025-02-24 11:27:07 -0800

    refactor read chunk data pack in utils

--- cmd/util/cmd/read-badger/cmd/chunk_data_pack.go
+++ cmd/util/cmd/read-badger/cmd/chunk_data_pack.go
@@ -1,11 +1,19 @@
 package cmd
 
 import (
+	"fmt"
+
+	"github.com/cockroachdb/pebble"
+	"github.com/dgraph-io/badger/v2"
 	"github.com/rs/zerolog/log"
 	"github.com/spf13/cobra"
 
 	"github.com/onflow/flow-go/cmd/util/cmd/common"
 	"github.com/onflow/flow-go/model/flow"
+	"github.com/onflow/flow-go/module/metrics"
+	badgerstorage "github.com/onflow/flow-go/storage/badger"
+	"github.com/onflow/flow-go/storage/operation/pebbleimpl"
+	"github.com/onflow/flow-go/storage/store"
 )
 
 var flagChunkID string
@@ -21,23 +29,31 @@ var chunkDataPackCmd = &cobra.Command{
 	Use:   "chunk-data",
 	Short: "get chunk data pack by chunk ID",
 	Run: func(cmd *cobra.Command, args []string) {
-		storages, db := InitStorages()
-		defer db.Close()
+		err := WithBadgerAndPebble(func(bdb *badger.DB, pdb *pebble.DB) error {
+			log.Info().Msgf("got flag chunk id: %s", flagChunkID)
+			chunkID, err := flow.HexStringToIdentifier(flagChunkID)
+			if err != nil {
+				return fmt.Errorf("malformed chunk id: %w", err)
+			}
 
-		log.Info().Msgf("got flag chunk id: %s", flagChunkID)
-		chunkID, err := flow.HexStringToIdentifier(flagChunkID)
+			metrics := metrics.NewNoopCollector()
+			collections := badgerstorage.NewCollections(bdb, badgerstorage.NewTransactions(metrics, bdb))
+			chunkDataPacks := store.NewChunkDataPacks(metrics,
+				pebbleimpl.ToDB(pdb), collections, 0)
+
+			log.Info().Msgf("getting chunk data pack by chunk id: %v", chunkID)
+			chunkDataPack, err := chunkDataPacks.ByChunkID(chunkID)
+			if err != nil {
+				log.Error().Err(err).Msgf("could not get chunk data pack with chunk id: %v", chunkID)
+				return nil
+			}
+
+			common.PrettyPrintEntity(chunkDataPack)
+			return nil
+		})
+
 		if err != nil {
-			log.Error().Err(err).Msg("malformed chunk id")
-			return
+			log.Error().Err(err).Msg("could not get chunk data pack")
 		}
-
-		log.Info().Msgf("getting chunk data pack by chunk id: %v", chunkID)
-		chunkDataPack, err := storages.ChunkDataPacks.ByChunkID(chunkID)
-		if err != nil {
-			log.Error().Err(err).Msgf("could not get chunk data pack with chunk id: %v", chunkID)
-			return
-		}
-
-		common.PrettyPrintEntity(chunkDataPack)
 	},
 }
