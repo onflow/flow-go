667bf8d3b492a0f481292ab0dced12ccd5e8c3d5
Andrii <andriy.dyachuk95@gmail.com>
2025-02-26 13:42:50 +0200

    Refactored eject.go, options.go and Backenmd struct to generics

--- module/mempool/stdmap/options.go
+++ module/mempool/stdmap/options.go
@@ -6,13 +6,13 @@ import (
 
 // OptionFunc is a function that can be provided to the backend on creation in
 // order to set a certain custom option.
-type OptionFunc func(*Backend)
+type OptionFunc[K comparable, V any] func(backend *Backend[K, V])
 
 // WithLimit can be provided to the backend on creation in order to set a point
 // where it's time to check for ejection conditions.  The actual size may continue
 // to rise by the threshold for batch ejection (currently 128)
-func WithLimit(limit uint) OptionFunc {
-	return func(be *Backend) {
+func WithLimit[K comparable, V any](limit uint) OptionFunc[K, V] {
+	return func(be *Backend[K, V]) {
 		be.guaranteedCapacity = limit
 	}
 }
@@ -20,8 +20,8 @@ func WithLimit(limit uint) OptionFunc {
 // WithEject can be provided to the backend on creation in order to set a custom
 // eject function to pick the entity to be evicted upon overflow, as well as
 // hooking into it for additional cleanup work.
-func WithEject(eject EjectFunc) OptionFunc {
-	return func(be *Backend) {
+func WithEject[K comparable, V any](eject EjectFunc[K, V]) OptionFunc[K, V] {
+	return func(be *Backend[K, V]) {
 		be.eject = eject
 		be.batchEject = nil
 	}
@@ -30,8 +30,8 @@ func WithEject(eject EjectFunc) OptionFunc {
 // WithBackData sets the underlying backdata of the backend.
 // BackData represents the underlying data structure that is utilized by mempool.Backend, as the
 // core structure of maintaining data on memory-pools.
-func WithBackData(backdata mempool.BackData) OptionFunc {
-	return func(be *Backend) {
+func WithBackData[K comparable, V any](backdata mempool.BackData[K, V]) OptionFunc[K, V] {
+	return func(be *Backend[K, V]) {
 		be.backData = backdata
 	}
 }
