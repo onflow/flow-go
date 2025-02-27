6690d39b2a7b75f07ce01c78d9b28dacef483f96
UlyanaAndrukhiv <u.andrukhiv@gmail.com>
2025-02-27 15:39:09 +0200

    Updated Guarantees memory pool

--- module/mempool/stdmap/guarantees.go
+++ module/mempool/stdmap/guarantees.go
@@ -7,30 +7,29 @@ import (
 // Guarantees implements the collections memory pool of the consensus nodes,
 // used to store collection guarantees and to generate block payloads.
 type Guarantees struct {
-	*Backend
+	*Backend[flow.Identifier, *flow.CollectionGuarantee]
 }
 
 // NewGuarantees creates a new memory pool for collection guarantees.
 func NewGuarantees(limit uint) (*Guarantees, error) {
 	g := &Guarantees{
-		Backend: NewBackend(WithLimit(limit)),
+		Backend: NewBackend[flow.Identifier, *flow.CollectionGuarantee](WithLimit[flow.Identifier, *flow.CollectionGuarantee](limit)),
 	}
 
 	return g, nil
 }
 
-// Add adds a collection guarantee guarantee to the mempool.
+// Add adds a collection guarantee to the mempool.
 func (g *Guarantees) Add(guarantee *flow.CollectionGuarantee) bool {
-	return g.Backend.Add(guarantee)
+	return g.Backend.Add(guarantee.ID(), guarantee)
 }
 
 // ByID returns the collection guarantee with the given ID from the mempool.
 func (g *Guarantees) ByID(collID flow.Identifier) (*flow.CollectionGuarantee, bool) {
-	entity, exists := g.Backend.ByID(collID)
+	guarantee, exists := g.Backend.ByID(collID)
 	if !exists {
 		return nil, false
 	}
-	guarantee := entity.(*flow.CollectionGuarantee)
 	return guarantee, true
 }
 
@@ -38,8 +37,8 @@ func (g *Guarantees) ByID(collID flow.Identifier) (*flow.CollectionGuarantee, bo
 func (g *Guarantees) All() []*flow.CollectionGuarantee {
 	entities := g.Backend.All()
 	guarantees := make([]*flow.CollectionGuarantee, 0, len(entities))
-	for _, entity := range entities {
-		guarantees = append(guarantees, entity.(*flow.CollectionGuarantee))
+	for _, guarantee := range entities {
+		guarantees = append(guarantees, guarantee)
 	}
 	return guarantees
 }
