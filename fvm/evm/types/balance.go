9566a28a2fd7de60e56ba4bdc4f4759e1675ffae
Ardit Marku <markoupetr@gmail.com>
2025-02-27 11:52:00 +0200

    Update scientific notation in AttoFlowBalanceValidForFlowVault description
    Co-authored-by: Janez Podhostnik <67895329+janezpodhostnik@users.noreply.github.com>

--- fvm/evm/types/balance.go
+++ fvm/evm/types/balance.go
@@ -98,7 +98,7 @@ func BalanceConversionToUFix64ProneToRoundingError(bal Balance) bool {
 // represented as atto Flow, can be stored in a Flow Vault.
 //
 // Warning! The smallest unit of Flow token that a Flow Vault (Cadence)
-// can store, is 1e10^-8 .
+// can store, is 1e-8 .
 // That means the minimum balance, in atto Flow, that can be stored in a Flow Vault,
 // is 1e10^10 .
 func AttoFlowBalanceValidForFlowVault(bal *big.Int) bool {
