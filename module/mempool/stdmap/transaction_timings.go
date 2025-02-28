3ee6c74ff0d82cbdbd8906cb4ab77f4d0ab71dd5
UlyanaAndrukhiv <u.andrukhiv@gmail.com>
2025-02-28 17:32:37 +0200

    Updated TransactionTimings memory pool

--- module/mempool/stdmap/transaction_timings.go
+++ module/mempool/stdmap/transaction_timings.go
@@ -7,13 +7,13 @@ import (
 // TransactionTimings implements the transaction timings memory pool of access nodes,
 // used to store transaction timings to report the timing of individual transactions
 type TransactionTimings struct {
-	*Backend
+	*Backend[flow.Identifier, *flow.TransactionTiming]
 }
 
 // NewTransactionTimings creates a new memory pool for transaction timings
 func NewTransactionTimings(limit uint) (*TransactionTimings, error) {
 	t := &TransactionTimings{
-		Backend: NewBackend(WithLimit(limit)),
+		Backend: NewBackend[flow.Identifier, *flow.TransactionTiming](WithLimit[flow.Identifier, *flow.TransactionTiming](limit)),
 	}
 
 	return t, nil
@@ -21,17 +21,13 @@ func NewTransactionTimings(limit uint) (*TransactionTimings, error) {
 
 // Add adds a transaction timing to the mempool.
 func (t *TransactionTimings) Add(tx *flow.TransactionTiming) bool {
-	return t.Backend.Add(tx)
+	return t.Backend.Add(tx.ID(), tx)
 }
 
 // ByID returns the transaction timing with the given ID from the mempool.
 func (t *TransactionTimings) ByID(txID flow.Identifier) (*flow.TransactionTiming, bool) {
-	entity, exists := t.Backend.ByID(txID)
+	tt, exists := t.Backend.ByID(txID)
 	if !exists {
-		return nil, false
-	}
-	tt, ok := entity.(*flow.TransactionTiming)
-	if !ok {
 		return nil, false
 	}
 	return tt, true
@@ -39,20 +35,12 @@ func (t *TransactionTimings) ByID(txID flow.Identifier) (*flow.TransactionTiming
 
 // Adjust will adjust the transaction timing using the given function if the given key can be found.
 // Returns a bool which indicates whether the value was updated as well as the updated value.
-func (t *TransactionTimings) Adjust(txID flow.Identifier, f func(*flow.TransactionTiming) *flow.TransactionTiming) (
+func (t *TransactionTimings) Adjust(txID flow.Identifier, f func(*flow.TransactionTiming) (flow.Identifier, *flow.TransactionTiming)) (
 	*flow.TransactionTiming, bool) {
-	e, updated := t.Backend.Adjust(txID, func(e flow.Entity) flow.Entity {
-		tt, ok := e.(*flow.TransactionTiming)
-		if !ok {
-			return nil
-		}
+	tt, updated := t.Backend.Adjust(txID, func(tt *flow.TransactionTiming) (flow.Identifier, *flow.TransactionTiming) {
 		return f(tt)
 	})
 	if !updated {
-		return nil, false
-	}
-	tt, ok := e.(*flow.TransactionTiming)
-	if !ok {
 		return nil, false
 	}
 	return tt, updated
@@ -62,8 +50,8 @@ func (t *TransactionTimings) Adjust(txID flow.Identifier, f func(*flow.Transacti
 func (t *TransactionTimings) All() []*flow.TransactionTiming {
 	entities := t.Backend.All()
 	txs := make([]*flow.TransactionTiming, 0, len(entities))
-	for _, entity := range entities {
-		txs = append(txs, entity.(*flow.TransactionTiming))
+	for _, tx := range entities {
+		txs = append(txs, tx)
 	}
 	return txs
 }
