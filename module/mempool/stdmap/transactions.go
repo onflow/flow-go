d21dc8acd114c024e543acc82189c9ef4597866a
UlyanaAndrukhiv <u.andrukhiv@gmail.com>
2025-02-28 14:08:54 +0200

    Updated Transactions memory pool

--- module/mempool/stdmap/transactions.go
+++ module/mempool/stdmap/transactions.go
@@ -1,22 +1,20 @@
 package stdmap
 
 import (
-	"fmt"
-
 	"github.com/onflow/flow-go/model/flow"
 )
 
 // Transactions implements the transactions memory pool of the consensus nodes,
 // used to store transactions and to generate block payloads.
 type Transactions struct {
-	*Backend
+	*Backend[flow.Identifier, *flow.TransactionBody]
 }
 
 // NewTransactions creates a new memory pool for transactions.
 // Deprecated: use herocache.Transactions instead.
 func NewTransactions(limit uint) *Transactions {
 	t := &Transactions{
-		Backend: NewBackend(WithLimit(limit)),
+		Backend: NewBackend[flow.Identifier, *flow.TransactionBody](WithLimit[flow.Identifier, *flow.TransactionBody](limit)),
 	}
 
 	return t
@@ -24,18 +22,14 @@ func NewTransactions(limit uint) *Transactions {
 
 // Add adds a transaction to the mempool.
 func (t *Transactions) Add(tx *flow.TransactionBody) bool {
-	return t.Backend.Add(tx)
+	return t.Backend.Add(tx.ID(), tx)
 }
 
 // ByID returns the transaction with the given ID from the mempool.
 func (t *Transactions) ByID(txID flow.Identifier) (*flow.TransactionBody, bool) {
-	entity, exists := t.Backend.ByID(txID)
+	tx, exists := t.Backend.ByID(txID)
 	if !exists {
 		return nil, false
-	}
-	tx, ok := entity.(*flow.TransactionBody)
-	if !ok {
-		panic(fmt.Sprintf("invalid entity in transaction pool (%T)", entity))
 	}
 	return tx, true
 }
@@ -44,8 +38,8 @@ func (t *Transactions) ByID(txID flow.Identifier) (*flow.TransactionBody, bool) 
 func (t *Transactions) All() []*flow.TransactionBody {
 	entities := t.Backend.All()
 	txs := make([]*flow.TransactionBody, 0, len(entities))
-	for _, entity := range entities {
-		txs = append(txs, entity.(*flow.TransactionBody))
+	for _, tx := range entities {
+		txs = append(txs, tx)
 	}
 	return txs
 }
