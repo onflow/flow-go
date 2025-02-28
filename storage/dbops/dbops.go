3c77397e7e1bf8119a29043f5ee249a5616fb4d7
Leo Zhang (zhangchiqing) <zhangchiqing@gmail.com>
2025-02-28 10:57:07 -0800

    revert dbops changes

--- storage/dbops/dbops.go
+++ storage/dbops/dbops.go
@@ -2,7 +2,11 @@ package dbops
 
 // The dbops feature flag is used to toggle between different database update operations.
 // Currently, the existing database update operations use badger-transaction, which is default and deprecated.
-// As part of the refactoring process to eventually transition to batch-update,
+// As part of the refactoring process to eventually transition to pebble-batch updates,
+// an intermediate step is required to switch to badger-batch.
+// This is why the feature flag has three possible values to facilitate the transition.
+const DB_OPS_MSG = "database operations to use (badger-transaction, badger-batch, pebble-batch)"
+const DB_OPS_DEFAULT = string(BadgerTransaction)
 
 type DBOps string
 
@@ -10,13 +14,7 @@ const (
 	// BadgerTransaction uses badger transactions (default and deprecated)
 	BadgerTransaction DBOps = "badger-transaction"
 	// BadgerBatch uses badger batch updates
-	BatchUpdate DBOps = "batch-update"
+	BadgerBatch DBOps = "badger-batch"
+	// PebbleBatch uses pebble batch updates
+	PebbleBatch DBOps = "pebble-batch"
 )
-
-func IsBadgerTransaction(op string) bool {
-	return op == string(BadgerTransaction)
-}
-
-func IsBatchUpdate(op string) bool {
-	return op == string(BatchUpdate)
-}
