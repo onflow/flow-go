a9027d9ac220934a458951c37bf9a7b9a1c4d10b
Illia Malachyn <illia.malachyn@gmail.com>
2025-03-17 17:21:20 +0200

    remove EventType.Parse() in favor of factory method

--- engine/access/rest/common/parser/event_type.go
+++ engine/access/rest/common/parser/event_type.go
@@ -17,14 +17,6 @@ func NewEventType(raw string) (EventType, error) {
 	return EventType(raw), nil
 }
 
-func (e *EventType) Parse(raw string) error {
-	if !basicEventRe.MatchString(raw) && !flowEventRe.MatchString(raw) {
-		return fmt.Errorf("invalid event type format")
-	}
-	*e = EventType(raw)
-	return nil
-}
-
 func (e EventType) Flow() string {
 	return string(e)
 }
@@ -47,27 +39,6 @@ func NewEventTypes(raw []string) (EventTypes, error) {
 	}
 
 	return eventTypes, nil
-}
-
-func (e *EventTypes) Parse(raw []string) error {
-	// make a map to have only unique values as keys
-	eventTypes := make(EventTypes, 0)
-	uniqueTypes := make(map[string]bool)
-	for i, r := range raw {
-		var eType EventType
-		err := eType.Parse(r)
-		if err != nil {
-			return fmt.Errorf("error at index %d: %w", i, err)
-		}
-
-		if !uniqueTypes[eType.Flow()] {
-			uniqueTypes[eType.Flow()] = true
-			eventTypes = append(eventTypes, eType)
-		}
-	}
-
-	*e = eventTypes
-	return nil
 }
 
 func (e EventTypes) Flow() []string {
