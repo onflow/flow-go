7acca485b27cbf8aa534b1f6e7fb88a4359d4eb1
Jordan Schalm <jordan@dapperlabs.com>
2025-03-17 17:10:10 -0700

    add basic leader selection tool

---
+++ cmd/util/cmd/leaders/cmd.go
@@ -0,0 +1,76 @@
+package leaders
+
+import (
+	"encoding/json"
+	"os"
+
+	"github.com/rs/zerolog/log"
+	"github.com/spf13/cobra"
+
+	"github.com/onflow/flow-go/consensus/hotstuff/committees/leader"
+	"github.com/onflow/flow-go/model/flow"
+	"github.com/onflow/flow-go/state/protocol/inmem"
+)
+
+var (
+	flagStartView uint64
+	flagEndView   uint64
+)
+var Cmd = &cobra.Command{
+	Use:   "leaders",
+	Short: "Get leader selection for a view range",
+	Run:   run,
+}
+
+func init() {
+
+	Cmd.Flags().Uint64Var(&flagStartView, "start-view", 0, "")
+	Cmd.Flags().Uint64Var(&flagEndView, "end-view", 0, "")
+	Cmd.MarkFlagRequired("start-view")
+	Cmd.MarkFlagRequired("end-view")
+}
+
+func run(*cobra.Command, []string) {
+
+	var snapshot inmem.EncodableSnapshot
+	err := json.NewDecoder(os.Stdin).Decode(&snapshot)
+	if err != nil {
+		log.Fatal().Err(err).Msg("Failed to read snapshot from stdin")
+	}
+
+	snap := inmem.SnapshotFromEncodable(snapshot)
+	epoch, err := snap.Epochs().Current()
+	if err != nil {
+		log.Fatal().Err(err).Msg("Failed to read current epoch")
+	}
+
+	// Should match https://github.com/onflow/flow-go/blob/48b6db32d4491903aa0ffa541377c8f239da3bcc/consensus/hotstuff/committees/consensus_committee.go#L74-L78
+	selection, err := leader.SelectionForConsensus(
+		epoch.InitialIdentities(),
+		epoch.RandomSource(),
+		epoch.FirstView(),
+		epoch.FinalView(),
+	)
+	if err != nil {
+		log.Fatal().Err(err).Msg("Failed to read current leader selection")
+	}
+
+	type LeaderForView struct {
+		View     uint64
+		LeaderID flow.Identifier
+	}
+
+	leaders := make([]LeaderForView, 0, flagEndView-flagStartView+1)
+	for view := flagStartView; view <= flagEndView; view++ {
+		leaderID, err := selection.LeaderForView(view)
+		if err != nil {
+			log.Fatal().Err(err).Msg("Failed to read leader for view")
+		}
+		leaders = append(leaders, LeaderForView{View: view, LeaderID: leaderID})
+	}
+
+	err = json.NewEncoder(os.Stdout).Encode(leaders)
+	if err != nil {
+		log.Fatal().Err(err).Msg("Failed to encode leaders")
+	}
+}
