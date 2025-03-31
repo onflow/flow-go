ce94822d6a78d2a8e01c8f23b0598d06f5a187f5
Andrii Diachuk <andriy.dyachuk95@gmail.com>
2025-03-31 14:53:04 +0300

    Update utils/unittest/util.go
    Co-authored-by: Jordan Schalm <jordan.schalm@flowfoundation.org>

--- utils/unittest/util.go
+++ utils/unittest/util.go
@@ -4,8 +4,10 @@ import (
 	"github.com/onflow/flow-go/model/flow"
 )
 
-// MockEntity is a simple struct used for testing mempools that work with entities.
-// It provides an Identifier and a Nonce field to simulate basic entity behavior.
+// MockEntity is a simple struct used for testing mempools constrained to Identifier-typed keys.
+// The Identifier field is the key used to store the entity in the mempool. 
+// The Nonce field can be used to simulate modifying the entity value stored in the mempool,
+// for example via `Adjust`/`AdjustWithInit` methods.
 // This allows for controlled testing of how mempools handle entity storage and updates.
 type MockEntity struct {
 	Identifier flow.Identifier
