027abfd79d1228a5ffb0a955acdc176bc668f837
UlyanaAndrukhiv <u.andrukhiv@gmail.com>
2025-04-01 13:06:07 +0300

    Removed TestDistinctChunkIDs_EmptyChunks and TestDistinctChunkIDs_FullChunks according to suggestion

--- model/flow/chunk_test.go
+++ model/flow/chunk_test.go
@@ -29,58 +29,6 @@ func TestChunkList_ByIndex(t *testing.T) {
 	require.True(t, ok)
 }
 
-// TestDistinctChunkIDs_EmptyChunks evaluates that two empty chunks
-// with the distinct block ids would have distinct chunk ids.
-func TestDistinctChunkIDs_EmptyChunks(t *testing.T) {
-	// generates two random block ids and requires them
-	// being distinct
-	blockIdA := unittest.IdentifierFixture()
-	blockIdB := unittest.IdentifierFixture()
-	require.NotEqual(t, blockIdA, blockIdB)
-
-	// generates a chunk associated with each block id
-	chunkA := &flow.Chunk{
-		ChunkBody: flow.ChunkBody{
-			BlockID: blockIdA,
-		},
-	}
-
-	chunkB := &flow.Chunk{
-		ChunkBody: flow.ChunkBody{
-			BlockID: blockIdB,
-		},
-	}
-
-	require.NotEqual(t, chunkA.ID(), chunkB.ID())
-}
-
-// TestDistinctChunkIDs_FullChunks evaluates that two full chunks
-// with completely identical fields but distinct block ids have
-// distinct chunk ids.
-func TestDistinctChunkIDs_FullChunks(t *testing.T) {
-	// generates two random block ids and requires them
-	// being distinct
-	blockIdA := unittest.IdentifierFixture()
-	blockIdB := unittest.IdentifierFixture()
-	require.NotEqual(t, blockIdA, blockIdB)
-
-	// generates a chunk associated with blockA
-	chunkA := unittest.ChunkFixture(blockIdA, 42, unittest.StateCommitmentFixture())
-
-	// generates a deep copy of chunkA in chunkB
-	chunkB := *chunkA
-
-	// since chunkB is a deep copy of chunkA their
-	// chunk ids should be the same
-	require.Equal(t, chunkA.ID(), chunkB.ID())
-
-	// changes block id in chunkB
-	chunkB.BlockID = blockIdB
-
-	// chunks with distinct block ids should have distinct chunk ids
-	require.NotEqual(t, chunkA.ID(), chunkB.ID())
-}
-
 // TestChunkList_Indices evaluates the Indices method of ChunkList on lists of different sizes.
 func TestChunkList_Indices(t *testing.T) {
 	cl := unittest.ChunkListFixture(5, unittest.IdentifierFixture(), unittest.StateCommitmentFixture())
