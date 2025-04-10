b88c99169538b28bf32cb00d6d164614742c7bfd
Illia Malachyn <illia.malachyn@gmail.com>
2025-04-10 15:43:20 +0300

    add godoc comment to interface

--- storage/store/inmemory/unsynchronized/persister.go
+++ storage/store/inmemory/unsynchronized/persister.go
@@ -4,6 +4,7 @@ import (
 	"github.com/onflow/flow-go/storage"
 )
 
+// Persister is responsible for persisting in-memory storages to given DB.
 type Persister interface {
 	AddToBatch(batch storage.ReaderBatchWriter) error
 }
