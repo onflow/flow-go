4f38622aa115ab3b8f0a1fd55ebc449ef8d30094
Tim Barry <tim.barry@flowfoundation.org>
2025-04-17 13:15:40 -0700

    Update error return comments for storage/blocks

--- storage/blocks.go
+++ storage/blocks.go
@@ -16,27 +16,55 @@ import (
 type Blocks interface {
 
 	// Store will atomically store a block with all its dependencies.
+	//
+	// Error returns:
+	//   - storage.ErrAlreadyExists if the blockID already exists in the database.
+	//   - generic error in case of unexpected failure from the database layer or encoding failure.
 	Store(proposal *flow.BlockProposal) error
 
 	// StoreTx allows us to store a new block, including its payload & header,
 	// as part of a DB transaction, while still going through the caching layer.
+	//
+	// Error returns:
+	//   - storage.ErrAlreadyExists if the blockID already exists in the database.
+	//   - generic error in case of unexpected failure from the database layer or encoding failure.
 	StoreTx(proposal *flow.BlockProposal) func(*transaction.Tx) error
 
 	// ByID returns the block with the given hash. It is available for all incorporated blocks (validated blocks
 	// that have been appended to any of the known forks) no matter whether the block has been finalized or not.
+	//
+	// Error returns:
+	//   - storage.ErrNotFound if no block with the corresponding ID was found
+	//   - generic error in case of unexpected failure from the database layer, or failure
+	//     to decode an existing database value
 	ByID(blockID flow.Identifier) (*flow.Block, error)
 
 	// ProposalByID returns the block with the given ID, along with the proposer's signature on it.
 	// It is available for all incorporated blocks (validated blocks that have been appended to any
 	// of the known forks) no matter whether the block has been finalized or not.
+	//
+	// Error returns:
+	//   - storage.ErrNotFound if no block with the corresponding ID was found
+	//   - generic error in case of unexpected failure from the database layer, or failure
+	//     to decode an existing database value
 	ProposalByID(blockID flow.Identifier) (*flow.BlockProposal, error)
 
 	// ByHeight returns the block at the given height. It is only available
 	// for finalized blocks.
+	//
+	// Error returns:
+	//   - storage.ErrNotFound if no block for the corresponding height was found
+	//   - generic error in case of unexpected failure from the database layer, or failure
+	//     to decode an existing database value
 	ByHeight(height uint64) (*flow.Block, error)
 
 	// ProposalByHeight returns the block at the given height, along with the proposer's
 	// signature on it. It is only available for finalized blocks.
+	//
+	// Error returns:
+	//   - storage.ErrNotFound if no block proposal for the corresponding height was found
+	//   - generic error in case of unexpected failure from the database layer, or failure
+	//     to decode an existing database value
 	ProposalByHeight(height uint64) (*flow.BlockProposal, error)
 
 	// ByCollectionID returns the block for the given collection ID.
@@ -45,6 +73,11 @@ type Blocks interface {
 	// each different fork can contain a recent collection once. Therefore, we must wait for
 	// finality.
 	// CAUTION: this method is not backed by a cache and therefore comparatively slow!
+	//
+	// Error returns:
+	//   - storage.ErrNotFound if the collection ID was not found
+	//   - generic error in case of unexpected failure from the database layer, or failure
+	//     to decode an existing database value
 	ByCollectionID(collID flow.Identifier) (*flow.Block, error)
 
 	// IndexBlockForCollections is only to be called for *finalized* blocks. For each
@@ -52,5 +85,8 @@ type Blocks interface {
 	// While consensus nodes verify that collections are not repeated within the same fork,
 	// each different fork can contain a recent collection once. Therefore, we must wait for
 	// finality.
+	//
+	// Error returns:
+	//   - generic error in case of unexpected failure from the database layer or encoding failure.
 	IndexBlockForCollections(blockID flow.Identifier, collIDs []flow.Identifier) error
 }
