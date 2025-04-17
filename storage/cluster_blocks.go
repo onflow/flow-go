a5e6dac03f9a80c4dbd0b8ba987a04a8c015291e
Tim Barry <tim.barry@flowfoundation.org>
2025-04-17 16:31:41 -0700

    Update error return comments for storage/cluster_blocks

--- storage/cluster_blocks.go
+++ storage/cluster_blocks.go
@@ -13,14 +13,28 @@ import (
 type ClusterBlocks interface {
 
 	// Store stores the proposed collection (aka cluster block).
+	//
+	// Error returns:
+	//   - storage.ErrAlreadyExists if the blockID already exists in the database.
+	//   - generic error in case of unexpected failure from the database layer or encoding failure.
 	Store(proposal *cluster.BlockProposal) error
 
 	// ProposalByID returns the collection with the given ID, along with the proposer's signature on it.
 	// It is available for all incorporated collections (validated blocks that have been appended to any
 	// of the known forks) no matter whether the collection has been finalized or not.
+	//
+	// Error returns:
+	//   - storage.ErrNotFound if the block ID was not found
+	//   - generic error in case of unexpected failure from the database layer, or failure
+	//     to decode an existing database value
 	ProposalByID(blockID flow.Identifier) (*cluster.BlockProposal, error)
 
 	// ProposalByHeight returns the collection at the given height, along with the proposer's
 	// signature on it. It is only available for finalized collections.
+	//
+	// Error returns:
+	//   - storage.ErrNotFound if the block height or block ID was not found
+	//   - generic error in case of unexpected failure from the database layer, or failure
+	//     to decode an existing database value
 	ProposalByHeight(height uint64) (*cluster.BlockProposal, error)
 }
