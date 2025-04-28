673da3a36aa89ff1d72ad33eac92ae8393827ccb
Uliana Andrukhiv <u.andrukhiv@gmail.com>
2025-04-28 13:22:36 +0300

    Updated tests according to suggestions from code review
    Co-authored-by: Tim Barry <21149133+tim-barry@users.noreply.github.com>

--- model/cluster/block_test.go
+++ model/cluster/block_test.go
@@ -16,8 +16,7 @@ func TestClusterBlockMalleability(t *testing.T) {
 	unittest.RequireEntityNonMalleable(t, &block,
 		unittest.WithFieldGenerator("Header.Timestamp", func() time.Time { return time.Now().UTC() }),
 		unittest.WithFieldGenerator("Payload.Collection", func() flow.Collection {
-			block.Payload.Collection = unittest.CollectionFixture(3)
-			return block.Payload.Collection
+			return unittest.CollectionFixture(3)
 		}),
 	)
 }
