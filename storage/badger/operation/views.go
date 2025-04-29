6e784af77b85d1257d24f9b2f6d77418d1e00e4b
Leo Zhang (zhangchiqing) <zhangchiqing@gmail.com>
2025-04-29 09:39:35 -0700

    add InsertSafetyData methods back

---
+++ storage/badger/operation/views.go
@@ -0,0 +1,17 @@
+package operation
+
+import (
+	"github.com/dgraph-io/badger/v2"
+	"github.com/onflow/flow-go/consensus/hotstuff"
+	"github.com/onflow/flow-go/model/flow"
+)
+
+// InsertSafetyData inserts safety data into the database.
+func InsertSafetyData(chainID flow.ChainID, safetyData *hotstuff.SafetyData) func(*badger.Txn) error {
+	return insert(makePrefix(codeSafetyData, chainID), safetyData)
+}
+
+// InsertLivenessData inserts liveness data into the database.
+func InsertLivenessData(chainID flow.ChainID, livenessData *hotstuff.LivenessData) func(*badger.Txn) error {
+	return insert(makePrefix(codeLivenessData, chainID), livenessData)
+}
