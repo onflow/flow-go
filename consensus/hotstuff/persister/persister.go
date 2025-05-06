fca2fe4b75c9deb0ba472bad73f3ba8735ea7b0f
Leo Zhang (zhangchiqing) <zhangchiqing@gmail.com>
2025-05-06 08:56:44 -0700

    ensure persister has been bootstrapped during initialization

--- consensus/hotstuff/persister/persister.go
+++ consensus/hotstuff/persister/persister.go
@@ -1,6 +1,8 @@
 package persister
 
 import (
+	"fmt"
+
 	"github.com/onflow/flow-go/consensus/hotstuff"
 	"github.com/onflow/flow-go/model/flow"
 	"github.com/onflow/flow-go/storage"
@@ -9,10 +11,6 @@ import (
 
 // Persister is responsible for persisting minimal critical safety and liveness data for HotStuff:
 // specifically [hotstuff.LivenessData] and [hotstuff.SafetyData].
-//
-// Persister depends on protocol.State and cluster.State bootstrapping to set initial values for
-// SafetyData and LivenessData, for each distinct chain ID. This bootstrapping must be complete
-// before constructing a Persister instance with New (otherwise it will return an error).
 type Persister struct {
 	db      storage.DB
 	chainID flow.ChainID
@@ -22,16 +20,41 @@ var _ hotstuff.Persister = (*Persister)(nil)
 var _ hotstuff.PersisterReader = (*Persister)(nil)
 
 // New creates a new Persister.
-//
-// Persister depends on protocol.State and cluster.State bootstrapping to set initial values for
-// SafetyData and LivenessData, for each distinct chain ID. This bootstrapping must be completed
-// before first using a Persister instance.
 func New(db storage.DB, chainID flow.ChainID) (*Persister, error) {
+	err := ensureSafetyDataAndLivenessDataAreBootstrapped(db, chainID)
+	if err != nil {
+		return nil, fmt.Errorf("fail to check persister was properly bootstrapped: %w", err)
+	}
+
 	p := &Persister{
 		db:      db,
 		chainID: chainID,
 	}
 	return p, nil
+}
+
+// ensureSafetyDataAndLivenessDataAreBootstrapped checks if the safety and liveness data are
+// bootstrapped for the given chain ID. If not, it returns an error.
+// The Flow Protocol mandates that SafetyData and LivenessData is provided as part of the bootstrapping
+// data. For a node, the SafetyData and LivenessData is among the most safety-critical data. We require
+// the protocol.State's or cluster.State's bootstrapping logic to properly initialize these values in the
+// database.
+// It is highly recommended that components using the Persister read the current SafetyData
+// and/or LivenessData (depending on what they need) within their constructor as a sanity check.
+func ensureSafetyDataAndLivenessDataAreBootstrapped(db storage.DB, chainID flow.ChainID) error {
+	var safetyData hotstuff.SafetyData
+	err := operation.RetrieveSafetyData(db.Reader(), chainID, &safetyData)
+	if err != nil {
+		return fmt.Errorf("fail to retrieve safety data: %w", err)
+	}
+
+	var livenessData hotstuff.LivenessData
+	err = operation.RetrieveLivenessData(db.Reader(), chainID, &livenessData)
+	if err != nil {
+		return fmt.Errorf("fail to retrieve liveness data: %w", err)
+	}
+
+	return nil
 }
 
 // NewReader returns a new Persister as a PersisterReader type (only read methods accessible).
