2950517b7ed9022afc18eb06091cb220cc3d79c2
Uliana Andrukhiv <u.andrukhiv@gmail.com>
2025-05-15 12:41:57 +0300

    Apply suggestions from code review
    Co-authored-by: Tim Barry <21149133+tim-barry@users.noreply.github.com>

--- integration/tests/ghost/ghost_node_example_test.go
+++ integration/tests/ghost/ghost_node_example_test.go
@@ -125,7 +125,7 @@ func TestGhostNodeExample_Subscribe(t *testing.T) {
 		// the following switch should be similar to the one defined in the actual node that is being emulated
 		switch v := event.(type) {
 		case *messages.UntrustedProposal:
-			fmt.Printf("Received block proposal: %s from %s\n", v.Block.ToHeader().ID().String(), from.String())
+			fmt.Printf("Received block proposal: %s from %s\n", v.Block.ID().String(), from.String())
 			i++
 		default:
 			t.Logf(" ignoring event: :%T: %v", v, v)
