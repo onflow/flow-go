4b4b3facb74d6e805f20b4b271451a0c789197df
Leo Zhang (zhangchiqing) <zhangchiqing@gmail.com>
2025-05-26 22:06:20 -0700

    fix procedure cluster tests

--- storage/procedure/cluster_test.go
+++ storage/procedure/cluster_test.go
@@ -3,24 +3,28 @@ package procedure
 import (
 	"testing"
 
-	"github.com/dgraph-io/badger/v2"
 	"github.com/stretchr/testify/require"
 
 	"github.com/onflow/flow-go/model/cluster"
 	"github.com/onflow/flow-go/model/flow"
-	"github.com/onflow/flow-go/storage/badger/operation"
+	"github.com/onflow/flow-go/storage"
+	"github.com/onflow/flow-go/storage/operation"
+	"github.com/onflow/flow-go/storage/operation/dbtest"
 	"github.com/onflow/flow-go/utils/unittest"
 )
 
 func TestInsertRetrieveClusterBlock(t *testing.T) {
-	unittest.RunWithBadgerDB(t, func(db *badger.DB) {
+	dbtest.RunWithDB(t, func(t *testing.T, db storage.DB) {
 		block := unittest.ClusterBlockFixture()
 
-		err := db.Update(InsertClusterBlock(&block))
-		require.NoError(t, err)
+		_, lctx := unittest.LockManagerWithContext(t, storage.LockInsertClusterBlock)
+		defer lctx.Release()
+		require.NoError(t, db.WithReaderBatchWriter(func(rw storage.ReaderBatchWriter) error {
+			return InsertClusterBlock(lctx, rw, &block)
+		}))
 
 		var retrieved cluster.Block
-		err = db.View(RetrieveClusterBlock(block.Header.ID(), &retrieved))
+		err := RetrieveClusterBlock(db.Reader(), block.Header.ID(), &retrieved)
 		require.NoError(t, err)
 
 		require.Equal(t, block, retrieved)
@@ -28,30 +32,36 @@ func TestInsertRetrieveClusterBlock(t *testing.T) {
 }
 
 func TestFinalizeClusterBlock(t *testing.T) {
-	unittest.RunWithBadgerDB(t, func(db *badger.DB) {
+	dbtest.RunWithDB(t, func(t *testing.T, db storage.DB) {
 		parent := unittest.ClusterBlockFixture()
 
 		block := unittest.ClusterBlockWithParent(&parent)
 
-		err := db.Update(InsertClusterBlock(&block))
-		require.NoError(t, err)
+		_, lctx := unittest.LockManagerWithContext(t, storage.LockInsertClusterBlock)
+		defer lctx.Release()
+		require.NoError(t, db.WithReaderBatchWriter(func(rw storage.ReaderBatchWriter) error {
+			return InsertClusterBlock(lctx, rw, &block)
+		}))
 
-		err = db.Update(operation.IndexClusterBlockHeight(block.Header.ChainID, parent.Header.Height, parent.ID()))
-		require.NoError(t, err)
+		require.NoError(t, db.WithReaderBatchWriter(func(rw storage.ReaderBatchWriter) error {
+			return operation.IndexClusterBlockHeight(rw.Writer(), block.Header.ChainID, parent.Header.Height, parent.ID())
+		}))
 
-		err = db.Update(operation.InsertClusterFinalizedHeight(block.Header.ChainID, parent.Header.Height))
-		require.NoError(t, err)
+		require.NoError(t, db.WithReaderBatchWriter(func(rw storage.ReaderBatchWriter) error {
+			return operation.UpsertClusterFinalizedHeight(rw.Writer(), block.Header.ChainID, parent.Header.Height)
+		}))
 
-		err = db.Update(FinalizeClusterBlock(block.Header.ID()))
-		require.NoError(t, err)
+		require.NoError(t, db.WithReaderBatchWriter(func(rw storage.ReaderBatchWriter) error {
+			return FinalizeClusterBlock(rw, block.Header.ID())
+		}))
 
 		var boundary uint64
-		err = db.View(operation.RetrieveClusterFinalizedHeight(block.Header.ChainID, &boundary))
+		err := operation.RetrieveClusterFinalizedHeight(db.Reader(), block.Header.ChainID, &boundary)
 		require.NoError(t, err)
 		require.Equal(t, block.Header.Height, boundary)
 
 		var headID flow.Identifier
-		err = db.View(operation.LookupClusterBlockHeight(block.Header.ChainID, boundary, &headID))
+		err = operation.LookupClusterBlockHeight(db.Reader(), block.Header.ChainID, boundary, &headID)
 		require.NoError(t, err)
 		require.Equal(t, block.ID(), headID)
 	})
