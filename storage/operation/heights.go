00b1a6c4cb1a66063887b0b65153cfb467f6e326
Leo Zhang (zhangchiqing) <zhangchiqing@gmail.com>
2025-06-02 08:46:20 -0700

    remove height operation

--- storage/operation/heights.go
+++
@@ -1,7 +0,0 @@
-package operation
-
-import "github.com/onflow/flow-go/storage"
-
-func RetrieveFinalizedHeight(r storage.Reader, height *uint64) error {
-	return RetrieveByKey(r, MakePrefix(codeFinalizedHeight), height)
-}
