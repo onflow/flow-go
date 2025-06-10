7886ee5f7806011ca3bec5e772062b6a99c31b80
Andrii <andriy.dyachuk95@gmail.com>
2025-06-10 16:12:35 +0300

    Linted

--- utils/unittest/fixtures.go
+++ utils/unittest/fixtures.go
@@ -1688,8 +1688,7 @@ func WithChunkID(chunkID flow.Identifier) func(*verification.ChunkDataPackReques
 // and height of zero.
 // Use options to customize the request.
 func ChunkDataPackRequestFixture(opts ...func(*verification.ChunkDataPackRequest)) *verification.
-ChunkDataPackRequest {
-
+	ChunkDataPackRequest {
 	req := &verification.ChunkDataPackRequest{
 		Locator: chunks.Locator{
 			ResultID: IdentifierFixture(),
