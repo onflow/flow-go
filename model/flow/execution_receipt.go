84809430bd4b21c867bf4a086d32fb347791563d
UlyanaAndrukhiv <u.andrukhiv@gmail.com>
2025-06-24 20:07:00 +0300

    Reverted back ExecutionResult validation to UnsignedExecutionReceipt constructor

--- model/flow/execution_receipt.go
+++ model/flow/execution_receipt.go
@@ -90,16 +90,17 @@ func NewUnsignedExecutionReceipt(untrusted UntrustedUnsignedExecutionReceipt) (*
 	if untrusted.ExecutorID == ZeroID {
 		return nil, fmt.Errorf("executor ID must not be zero")
 	}
-	executionResult, err := NewExecutionResult(UntrustedExecutionResult(untrusted.ExecutionResult))
-	if err != nil {
-		return nil, fmt.Errorf("invalid execution result: %w", err)
-	}
+	//TODO: uncomment this code then constructor for ExecutionResult will be added (when PR #7525 will be merged)
+	//executionResult, err := NewExecutionResult(UntrustedExecutionResult(untrusted.ExecutionResult))
+	//if err != nil {
+	//	return nil, fmt.Errorf("invalid execution result: %w", err)
+	//}
 	if len(untrusted.Spocks) == 0 {
 		return nil, fmt.Errorf("spocks must not be empty")
 	}
 	return &UnsignedExecutionReceipt{
 		ExecutorID:      untrusted.ExecutorID,
-		ExecutionResult: *executionResult,
+		ExecutionResult: untrusted.ExecutionResult,
 		Spocks:          untrusted.Spocks,
 	}, nil
 }
