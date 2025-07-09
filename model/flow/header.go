4a344d7dc02ea25843e3194fd213f9c98f66ea9c
Andrii Diachuk <andriy.dyachuk95@gmail.com>
2025-07-09 11:59:18 +0300

    Update model/flow/header.go
    Co-authored-by: Yurii Oleksyshyn <yuraolex@gmail.com>

--- model/flow/header.go
+++ model/flow/header.go
@@ -117,7 +117,6 @@ func NewRootHeaderBody(untrusted UntrustedHeaderBody) (*HeaderBody, error) {
 	if untrusted.ChainID == "" {
 		return nil, fmt.Errorf("ChainID of root header body must not be empty")
 	}
-
 	if untrusted.ParentView != 0 {
 		return nil, fmt.Errorf("ParentView of root header body must be zero")
 	}
