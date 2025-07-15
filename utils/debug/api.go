9b70dc41abe2eb91aefd303b7c06a04bd0b1fae1
Andrii <andriy.dyachuk95@gmail.com>
2025-07-15 15:23:15 +0300

    Fixed api methods

--- utils/debug/api.go
+++ utils/debug/api.go
@@ -2,12 +2,10 @@ package debug
 
 import (
 	"context"
-	"fmt"
 
 	"github.com/onflow/flow/protobuf/go/flow/access"
 	"github.com/onflow/flow/protobuf/go/flow/execution"
 
-	"github.com/onflow/flow-go/access/legacy/convert"
 	rpcConvert "github.com/onflow/flow-go/engine/common/rpc/convert"
 	"github.com/onflow/flow-go/model/flow"
 )
@@ -29,46 +27,9 @@ func GetExecutionAPIBlockHeader(
 		return nil, err
 	}
 
-	blockHeader, err := rpcConvert.MessageToBlockHeader(resp.Block)
+	header, err := rpcConvert.MessageToBlockHeader(resp.Block)
 	if err != nil {
 		return nil, err
-	}
-	untrusted := flow.UntrustedHeaderBody{
-		ChainID:            blockHeader.ChainID,
-		ParentID:           blockHeader.ParentID,
-		Height:             blockHeader.Height,
-		Timestamp:          blockHeader.Timestamp,
-		ParentVoterIndices: blockHeader.ParentVoterIndices,
-		ParentVoterSigData: blockHeader.ParentVoterSigData,
-		ProposerID:         blockHeader.ProposerID,
-		LastViewTC:         blockHeader.LastViewTC,
-	}
-	if !blockHeader.ContainsParentQC() {
-		rootHeaderBody, err := flow.NewRootHeaderBody(untrusted)
-		if err != nil {
-			return nil, fmt.Errorf("could not build root header body: %w", err)
-		}
-		rootHeader, err := flow.NewHeader(flow.UntrustedHeader{
-			HeaderBody:  *rootHeaderBody,
-			PayloadHash: convert.MessageToIdentifier(resp.Block.PayloadHash),
-		})
-		if err != nil {
-			return nil, fmt.Errorf("could not build root header: %w", err)
-		}
-
-		return rootHeader, nil
-	}
-
-	headerBody, err := flow.NewHeaderBody(untrusted)
-	if err != nil {
-		return nil, fmt.Errorf("could not build header body: %w", err)
-	}
-	header, err := flow.NewHeader(flow.UntrustedHeader{
-		HeaderBody:  *headerBody,
-		PayloadHash: blockHeader.PayloadHash,
-	})
-	if err != nil {
-		return nil, fmt.Errorf("could not build header: %w", err)
 	}
 
 	return header, nil
@@ -91,46 +52,9 @@ func GetAccessAPIBlockHeader(
 		return nil, err
 	}
 
-	blockHeader, err := rpcConvert.MessageToBlockHeader(resp.Block)
+	header, err := rpcConvert.MessageToBlockHeader(resp.Block)
 	if err != nil {
 		return nil, err
-	}
-	untrusted := flow.UntrustedHeaderBody{
-		ChainID:            blockHeader.ChainID,
-		ParentID:           blockHeader.ParentID,
-		Height:             blockHeader.Height,
-		Timestamp:          blockHeader.Timestamp,
-		ParentVoterIndices: blockHeader.ParentVoterIndices,
-		ParentVoterSigData: blockHeader.ParentVoterSigData,
-		ProposerID:         blockHeader.ProposerID,
-		LastViewTC:         blockHeader.LastViewTC,
-	}
-	if !blockHeader.ContainsParentQC() {
-		rootHeaderBody, err := flow.NewRootHeaderBody(untrusted)
-		if err != nil {
-			return nil, fmt.Errorf("could not build root header body: %w", err)
-		}
-		rootHeader, err := flow.NewHeader(flow.UntrustedHeader{
-			HeaderBody:  *rootHeaderBody,
-			PayloadHash: convert.MessageToIdentifier(resp.Block.PayloadHash),
-		})
-		if err != nil {
-			return nil, fmt.Errorf("could not build root header: %w", err)
-		}
-
-		return rootHeader, nil
-	}
-
-	headerBody, err := flow.NewHeaderBody(untrusted)
-	if err != nil {
-		return nil, fmt.Errorf("could not build header body: %w", err)
-	}
-	header, err := flow.NewHeader(flow.UntrustedHeader{
-		HeaderBody:  *headerBody,
-		PayloadHash: blockHeader.PayloadHash,
-	})
-	if err != nil {
-		return nil, fmt.Errorf("could not build header: %w", err)
 	}
 
 	return header, nil
