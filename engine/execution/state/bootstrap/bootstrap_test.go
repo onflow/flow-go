0ada66b1c3eae8f435d709f83024af0d8f419f5f
Bastian Müller <bastian@turbolent.com>
2025-07-22 14:55:25 -0700

    adjust state commitments

--- engine/execution/state/bootstrap/bootstrap_test.go
+++ engine/execution/state/bootstrap/bootstrap_test.go
@@ -57,7 +57,7 @@ func TestBootstrapLedger(t *testing.T) {
 }
 
 func TestBootstrapLedger_ZeroTokenSupply(t *testing.T) {
-	expectedStateCommitmentBytes, _ := hex.DecodeString("86417e6e6e67954dfb8be38f001be052bfca92d8335b0e20943896ce39106b73")
+	expectedStateCommitmentBytes, _ := hex.DecodeString("c253e10c066f517b25cf11b0381e8958e8f59bb6e8c45e3f571bd109129400bd")
 	expectedStateCommitment, err := flow.ToStateCommitment(expectedStateCommitmentBytes)
 	require.NoError(t, err)
 
@@ -104,7 +104,7 @@ func TestBootstrapLedger_ZeroTokenSupply(t *testing.T) {
 // - transaction fee deduction
 // This tests that the state commitment has not changed for the bookkeeping parts of the transaction.
 func TestBootstrapLedger_EmptyTransaction(t *testing.T) {
-	expectedStateCommitmentBytes, _ := hex.DecodeString("69312ac010d291189c83e9f1bb8145ca214a31d6b604850c87eb042a7cf20a15")
+	expectedStateCommitmentBytes, _ := hex.DecodeString("b4c12b01497a2a69d4e65a7f87a98f6347a561d30a2450aa5e87bd58f1f8d407")
 	expectedStateCommitment, err := flow.ToStateCommitment(expectedStateCommitmentBytes)
 	require.NoError(t, err)
 
