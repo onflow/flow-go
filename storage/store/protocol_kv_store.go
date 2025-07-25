e435b62611fe975a93a445cf60f82e705d6da7da
Leo Zhang (zhangchiqing) <zhangchiqing@gmail.com>
2025-07-25 08:56:17 -0700

    ix protocol kv store

--- storage/store/protocol_kv_store.go
+++ storage/store/protocol_kv_store.go
@@ -129,8 +129,14 @@ func (s *ProtocolKVStore) BatchIndex(rw storage.ReaderBatchWriter, blockID flow.
 	// TODO(7355): lockctx
 	rw.Lock(s.indexing)
 
-	_, err := s.byBlockIdCache.Get(s.db.Reader(), blockID)
+	existingStateID, err := s.byBlockIdCache.Get(s.db.Reader(), blockID)
 	if err == nil {
+
+		// if it's about to index the same state ID, then we can skip the operation
+		if existingStateID == stateID {
+			return nil
+		}
+
 		return fmt.Errorf("kv-store snapshot with block id (%x) already exists: %w", blockID[:], storage.ErrAlreadyExists)
 	}
 	if !errors.Is(err, storage.ErrNotFound) {
