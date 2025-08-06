c67e486414d443decdeb4244ff7227442409df11
Leo Zhang (zhangchiqing) <zhangchiqing@gmail.com>
2025-08-06 15:03:46 -0700

    remove badger.Headers and fix tests

--- module/finalizedreader/finalizedreader_test.go
+++ module/finalizedreader/finalizedreader_test.go
@@ -4,34 +4,52 @@ import (
 	"errors"
 	"testing"
 
-	"github.com/dgraph-io/badger/v2"
+	"github.com/jordanschalm/lockctx"
 	"github.com/stretchr/testify/require"
 
 	"github.com/onflow/flow-go/module/metrics"
 	"github.com/onflow/flow-go/storage"
-	"github.com/onflow/flow-go/storage/badger/operation"
+	"github.com/onflow/flow-go/storage/operation"
+	"github.com/onflow/flow-go/storage/operation/dbtest"
+	"github.com/onflow/flow-go/storage/store"
 	"github.com/onflow/flow-go/utils/unittest"
-
-	badgerstorage "github.com/onflow/flow-go/storage/badger"
 )
 
+func withLock(t *testing.T, manager lockctx.Manager, lockID string, fn func(lctx lockctx.Context) error) {
+	t.Helper()
+	lctx := manager.NewContext()
+	require.NoError(t, lctx.AcquireLock(lockID))
+	defer lctx.Release()
+	require.NoError(t, fn(lctx))
+}
+
 func TestFinalizedReader(t *testing.T) {
-	unittest.RunWithBadgerDB(t, func(db *badger.DB) {
+	dbtest.RunWithDB(t, func(t *testing.T, db storage.DB) {
 		// prepare the storage.Headers instance
 		metrics := metrics.NewNoopCollector()
-		headers := badgerstorage.NewHeaders(metrics, db)
+		all := store.InitAll(metrics, db)
+		blocks := all.Blocks
+		headers := all.Headers
 		block := unittest.BlockFixture()
 
-		// store header
-		err := headers.Store(block.Header)
-		require.NoError(t, err)
+		// store block 1
+		lockManager := storage.NewTestingLockManager()
+		withLock(t, lockManager, storage.LockInsertBlock, func(lctx lockctx.Context) error {
+			return db.WithReaderBatchWriter(func(rw storage.ReaderBatchWriter) error {
+				return blocks.BatchStore(lctx, rw, &block)
+			})
+		})
 
-		// index the header
-		err = db.Update(operation.IndexFinalizedBlockByHeight(block.Header.Height, block.ID()))
-		require.NoError(t, err)
+		// finalize block 1
+		withLock(t, lockManager, storage.LockFinalizeBlock, func(lctx lockctx.Context) error {
+			return db.WithReaderBatchWriter(func(rw storage.ReaderBatchWriter) error {
+				return operation.IndexFinalizedBlockByHeight(lctx, rw, block.Header.Height, block.ID())
+			})
+		})
 
 		// verify is able to reader the finalized block ID
 		reader := NewFinalizedReader(headers, block.Header.Height)
+
 		finalized, err := reader.FinalizedBlockIDAtHeight(block.Header.Height)
 		require.NoError(t, err)
 		require.Equal(t, block.ID(), finalized)
@@ -43,9 +61,19 @@ func TestFinalizedReader(t *testing.T) {
 
 		// finalize one more block
 		block2 := unittest.BlockWithParentFixture(block.Header)
-		require.NoError(t, headers.Store(block2.Header))
-		err = db.Update(operation.IndexFinalizedBlockByHeight(block2.Header.Height, block2.ID()))
-		require.NoError(t, err)
+
+		withLock(t, lockManager, storage.LockInsertBlock, func(lctx lockctx.Context) error {
+			return db.WithReaderBatchWriter(func(rw storage.ReaderBatchWriter) error {
+				return blocks.BatchStore(lctx, rw, block2)
+			})
+		})
+
+		withLock(t, lockManager, storage.LockFinalizeBlock, func(lctx lockctx.Context) error {
+			return db.WithReaderBatchWriter(func(rw storage.ReaderBatchWriter) error {
+				return operation.IndexFinalizedBlockByHeight(lctx, rw, block2.Header.Height, block2.ID())
+			})
+		})
+
 		reader.BlockFinalized(block2.Header)
 
 		// should be able to retrieve the block
