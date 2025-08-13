2ba91e2c77b7ebd69822b01d077f93dfd927876f
gregor <75445744+devbugging@users.noreply.github.com>
2025-08-13 12:01:39 +0200

    Use sealed blocks for account

--- integration/testnet/client.go
+++ integration/testnet/client.go
@@ -459,7 +459,7 @@ func (c *Client) GetEventsForBlockIDs(
 }
 
 func getAccount(ctx context.Context, client *client.Client, address sdk.Address) (*sdk.Account, error) {
-	header, err := client.GetLatestBlockHeader(ctx, false) // todo change back to true when sealing is done
+	header, err := client.GetLatestBlockHeader(ctx, true)
 	if err != nil {
 		return nil, fmt.Errorf("could not get latest block header: %w", err)
 	}
