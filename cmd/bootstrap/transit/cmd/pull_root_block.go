01695abc225ec3d21bd87549b098d6079e357e3a
sjonpaulbrown <sjonpaulbrown@gmail.com>
2025-08-18 11:51:34 -0600

    Update pull-root-block command to conditionally use output directory flag

--- cmd/bootstrap/transit/cmd/pull_root_block.go
+++ cmd/bootstrap/transit/cmd/pull_root_block.go
@@ -52,25 +52,40 @@ func pullRootBlock(c *cobra.Command, args []string) {
 	log.Info().Msg("downloading root block")
 
 	rootBlockFile := filepath.Join(flagToken, bootstrap.PathRootBlockData)
-	fullOutpath := filepath.Join(flagBootDir, bootstrap.PathRootBlockData)
+	fullRootBlockPath := filepath.Join(flagBootDir, bootstrap.PathRootBlockData)
+	if flagOutputDir != "" {
+		fullRootBlockPath = filepath.Join(flagOutputDir, "root-block.json")
+	}
 
-	log.Info().Str("source", rootBlockFile).Str("dest", fullOutpath).Msgf("downloading root block file from transit servers")
-	err = bucket.DownloadFile(ctx, client, fullOutpath, rootBlockFile)
+	log.Info().Str("source", rootBlockFile).Str("dest", fullRootBlockPath).Msgf("downloading root block file from transit servers")
+	err = bucket.DownloadFile(ctx, client, fullRootBlockPath, rootBlockFile)
 	if err != nil {
 		log.Fatal().Err(err).Msgf("could not download google bucket file")
 	}
 
 	objectName := filepath.Join(flagToken, fmt.Sprintf(FilenameRandomBeaconCipher, nodeID))
-	fullOutpath = filepath.Join(flagBootDir, filepath.Base(objectName))
+
+	// By default, use the bootstrap directory for the random beacon download & unwrapping
+	fullRandomBeaconPath := filepath.Join(flagBootDir, filepath.Base(objectName))
+	unWrappedRandomBeaconPath := filepath.Join(
+		flagBootDir,
+		fmt.Sprintf(bootstrap.PathRandomBeaconPriv, nodeID),
+	)
+
+	// If output directory is specified, use it for the random beacon path
+	// this will set the path used to download the random beacon file and unwrap it
+	if flagOutputDir != "" {
+		fullRandomBeaconPath = filepath.Join(flagOutputDir, filepath.Base(objectName))
+		unWrappedRandomBeaconPath = filepath.Join(flagOutputDir, filepath.Base(objectName))
+	}
 
 	log.Info().Msgf("downloading random beacon key: %s", objectName)
 
-	err = bucket.DownloadFile(ctx, client, fullOutpath, objectName)
+	err = bucket.DownloadFile(ctx, client, fullRandomBeaconPath, objectName)
 	if err != nil {
 		log.Fatal().Err(err).Msg("could not download file from google bucket")
 	}
-
-	err = unWrapFile(flagBootDir, nodeID)
+	err = unWrapFile(flagBootDir, nodeID, flagOutputDir, unWrappedRandomBeaconPath)
 	if err != nil {
 		log.Fatal().Err(err).Msg("could not unwrap random beacon file")
 	}
