48879d7f13227fc8bdf1fdb386d587fdbff9e7eb
Leo Zhang (zhangchiqing) <zhangchiqing@gmail.com>
2025-08-19 15:33:03 -0700

    fix indexer core

--- module/metrics.go
+++ module/metrics.go
@@ -1181,8 +1181,8 @@ type DHTMetrics interface {
 }
 
 type CollectionExecutedMetric interface {
-	CollectionFinalized(light flow.LightCollection)
-	CollectionExecuted(light flow.LightCollection)
+	CollectionFinalized(light *flow.LightCollection)
+	CollectionExecuted(light *flow.LightCollection)
 	BlockFinalized(block *flow.Block)
 	ExecutionReceiptReceived(r *flow.ExecutionReceipt)
 	UpdateLastFullBlockHeight(height uint64)
