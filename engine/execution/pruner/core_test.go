98abc1d30f3b379eff236125814fe64b1ca47f31
Leo Zhang (zhangchiqing) <zhangchiqing@gmail.com>
2025-08-19 15:47:01 -0700

    resolve pruner core test

--- engine/execution/pruner/core_test.go
+++ engine/execution/pruner/core_test.go
@@ -48,28 +48,22 @@ func TestLoopPruneExecutionDataFromRootToLatestSealed(t *testing.T) {
 			// indexed by height
 			chunks := make([]*verification.VerifiableChunkData, lastFinalizedHeight+2)
 			parentID := genesis.ID()
-<<<<<<< HEAD
 			manager, lctx := unittest.LockManagerWithContext(t, storage.LockInsertBlock)
 			require.NoError(t, db.WithReaderBatchWriter(func(rw storage.ReaderBatchWriter) error {
-				return blockstore.BatchStore(lctx, rw, genesis)
+				return blockstore.BatchStore(lctx, rw, &flow.Proposal{Block: *genesis, ProposerSigData: nil})
 			}))
 			lctx.Release()
 
-=======
-			// By convention, root block has no proposer signature - implementation has to handle this edge case
-			require.NoError(t, headers.Store(&flow.ProposalHeader{Header: genesis.ToHeader(), ProposerSigData: nil}))
->>>>>>> feature/malleability
 			for i := 1; i <= lastFinalizedHeight; i++ {
 				chunk, block := unittest.VerifiableChunkDataFixture(0, func(headerBody *flow.HeaderBody) {
 					headerBody.Height = uint64(i)
 					headerBody.ParentID = parentID
 				})
 				chunks[i] = chunk // index by height
-<<<<<<< HEAD
 				lctx := manager.NewContext()
 				require.NoError(t, lctx.AcquireLock(storage.LockInsertBlock))
 				require.NoError(t, db.WithReaderBatchWriter(func(rw storage.ReaderBatchWriter) error {
-					return blockstore.BatchStore(lctx, rw, block)
+					return blockstore.BatchStore(lctx, rw, unittest.ProposalFromBlock(block))
 				}))
 				lctx.Release()
 				lctx = manager.NewContext()
@@ -78,10 +72,6 @@ func TestLoopPruneExecutionDataFromRootToLatestSealed(t *testing.T) {
 					return operation.IndexFinalizedBlockByHeight(lctx, rw, chunk.Header.Height, chunk.Header.ID())
 				}))
 				lctx.Release()
-=======
-				require.NoError(t, headers.Store(unittest.ProposalHeaderFromHeader(chunk.Header)))
-				require.NoError(t, bdb.Update(operation.IndexBlockHeight(chunk.Header.Height, chunk.Header.ID())))
->>>>>>> feature/malleability
 				require.NoError(t, results.Store(chunk.Result))
 				require.NoError(t, results.Index(chunk.Result.BlockID, chunk.Result.ID()))
 				require.NoError(t, chunkDataPacks.Store([]*flow.ChunkDataPack{chunk.ChunkDataPack}))
