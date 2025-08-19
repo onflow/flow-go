7fe0698a460744ce18e757ad8b4d69dd5dc9ee92
Leo Zhang (zhangchiqing) <zhangchiqing@gmail.com>
2025-08-19 10:33:44 -0700

    resolve conflicts in storage

--- storage/badger/all.go
+++ storage/badger/all.go
@@ -10,10 +10,9 @@ import (
 )
 
 func InitAll(metrics module.CacheMetrics, db *badger.DB) *storage.All {
-<<<<<<< HEAD
 	sdb := badgerimpl.ToDB(db)
 	headers := store.NewHeaders(metrics, sdb)
-	guarantees := store.NewGuarantees(metrics, sdb, DefaultCacheSize)
+	guarantees := store.NewGuarantees(metrics, sdb, DefaultCacheSize, DefaultCacheSize)
 	seals := store.NewSeals(metrics, sdb)
 	index := store.NewIndex(metrics, sdb)
 	results := store.NewExecutionResults(metrics, sdb)
@@ -27,23 +26,6 @@ func InitAll(metrics module.CacheMetrics, db *badger.DB) *storage.All {
 		store.DefaultEpochProtocolStateCacheSize, store.DefaultProtocolStateIndexCacheSize)
 	protocolKVStore := store.NewProtocolKVStore(metrics, sdb, store.DefaultProtocolKVStoreCacheSize, store.DefaultProtocolKVStoreByBlockIDCacheSize)
 	versionBeacons := store.NewVersionBeacons(sdb)
-=======
-	headers := NewHeaders(metrics, db)
-	guarantees := NewGuarantees(metrics, db, DefaultCacheSize, DefaultCacheSize)
-	seals := NewSeals(metrics, db)
-	index := NewIndex(metrics, db)
-	results := NewExecutionResults(metrics, db)
-	receipts := NewExecutionReceipts(metrics, db, results, DefaultCacheSize)
-	payloads := NewPayloads(db, index, guarantees, seals, receipts, results)
-	blocks := NewBlocks(db, headers, payloads)
-	qcs := NewQuorumCertificates(metrics, db, DefaultCacheSize)
-	setups := NewEpochSetups(metrics, db)
-	epochCommits := NewEpochCommits(metrics, db)
-	epochProtocolStateEntries := NewEpochProtocolStateEntries(metrics, setups, epochCommits, db,
-		DefaultEpochProtocolStateCacheSize, DefaultProtocolStateIndexCacheSize)
-	protocolKVStore := NewProtocolKVStore(metrics, db, DefaultProtocolKVStoreCacheSize, DefaultProtocolKVStoreByBlockIDCacheSize)
-	versionBeacons := store.NewVersionBeacons(badgerimpl.ToDB(db))
->>>>>>> feature/malleability
 
 	transactions := store.NewTransactions(metrics, sdb)
 	collections := store.NewCollections(sdb, transactions)
