0fb902a12f828a52761a10659e998565efe99101
Leo Zhang (zhangchiqing) <zhangchiqing@gmail.com>
2025-08-19 15:08:22 -0700

    resolve read protocol state snapshot

--- cmd/util/cmd/read-protocol-state/cmd/snapshot.go
+++ cmd/util/cmd/read-protocol-state/cmd/snapshot.go
@@ -102,20 +102,12 @@ func runSnapshotE(*cobra.Command, []string) error {
 			return fmt.Errorf("fail to get block of snapshot: %w", err)
 		}
 
-<<<<<<< HEAD
 		log.Info().Msgf("creating snapshot for block height %v, id %v", head.Height, head.ID())
 
 		serializable, err := inmem.FromSnapshot(snapshot)
 		if err != nil {
 			return fmt.Errorf("fail to serialize snapshot: %w", err)
 		}
-=======
-	log.Info().Msgf("snapshot created, sealed height %v, id %v",
-		sealingSegment.Sealed().Height, sealingSegment.Sealed().ID())
-
-	log.Info().Msgf("highest finalized height %v, id %v",
-		sealingSegment.Highest().Height, sealingSegment.Highest().ID())
->>>>>>> @{-1}
 
 		sealingSegment, err := serializable.SealingSegment()
 		if err != nil {
@@ -123,10 +115,10 @@ func runSnapshotE(*cobra.Command, []string) error {
 		}
 
 		log.Info().Msgf("snapshot created, sealed height %v, id %v",
-			sealingSegment.Sealed().Header.Height, sealingSegment.Sealed().Header.ID())
+			sealingSegment.Sealed().Height, sealingSegment.Sealed().ID())
 
 		log.Info().Msgf("highest finalized height %v, id %v",
-			sealingSegment.Highest().Header.Height, sealingSegment.Highest().Header.ID())
+			sealingSegment.Highest().Height, sealingSegment.Highest().ID())
 
 		encoded := serializable.Encodable()
 		common.PrettyPrint(encoded)
