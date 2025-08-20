b608838b79857429c53e87a2d93f54ac7054fc1b
Leo Zhang (zhangchiqing) <zhangchiqing@gmail.com>
2025-08-20 14:39:26 -0700

    fix prefix code for badger

--- storage/operation/prefix.go
+++ storage/operation/prefix.go
@@ -79,6 +79,7 @@ const (
 	codeBeaconPrivateKey   = 63 // BeaconPrivateKey, keyed by epoch counter
 	codeDKGStarted         = 64 // flag that the DKG for an epoch has been started
 	codeDKGEnded           = 65 // flag that the DKG for an epoch has ended (stores end state)
+	codeDKGState           = 66 // current state of Recoverable Random Beacon State Machine for given epoch
 	codeVersionBeacon      = 67 // flag for storing version beacons
 	codeEpochProtocolState = 68
 	codeProtocolKVStore    = 69
