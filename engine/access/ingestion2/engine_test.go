2eded9d7de26e934a0056143e7eebad6cec7636d
Leo Zhang (zhangchiqing) <zhangchiqing@gmail.com>
2025-08-20 14:29:46 -0700

    resolve ingestion engine2 tests

--- engine/access/ingestion2/engine_test.go
+++ engine/access/ingestion2/engine_test.go
@@ -447,10 +447,12 @@ func (s *Suite) TestExecutionReceiptsAreIndexed() {
 
 	// we should store the light collection and index its transactions
 	s.collections.On("StoreAndIndexByTransaction", &collection).Return(light, nil).Once()
-	block := &flow.Block{
-		Header:  &flow.Header{Height: 0},
-		Payload: &flow.Payload{Guarantees: []*flow.CollectionGuarantee{}},
-	}
+	block := unittest.BlockFixture(
+		unittest.Block.WithHeight(0),
+		unittest.Block.WithPayload(
+			unittest.PayloadFixture(unittest.WithGuarantees([]*flow.CollectionGuarantee{}...)),
+		),
+	)
 	s.blocks.On("ByID", mock.Anything).Return(block, nil)
 
 	// for each transaction in the collection, we should store it
