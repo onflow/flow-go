d2f3f930bed30180ef9365c037827f8961454f6b
Andrii Diachuk <andriy.dyachuk95@gmail.com>
2025-08-20 13:47:48 +0300

    Update model/messages/untrusted_message.go
    Co-authored-by: Jordan Schalm <jordan.schalm@flowfoundation.org>

--- model/messages/untrusted_message.go
+++ model/messages/untrusted_message.go
@@ -5,9 +5,15 @@ import (
 	"github.com/onflow/flow-go/model/flow"
 )
 
-// UntrustedMessage is the decode target for anything that came over the wire.
-// ToInternal must validate and construct the internal, trusted model.
-// Implementations must validate and construct the trusted, internal model.
+// UntrustedMessage represents the set of allowed decode target types for messages received over the network.
+// Conceptually, an UntrustedMessage implementation makes no guarantees whatsoever about its contents.
+// UntrustedMessage's must implement a ToInternal method, which converts the network message to a corresponding internal type (typically in the model/flow package)
+// This conversion provides an opportunity to:
+//    - perform basic structural validity checks (required fields are non-nil, fields reference one another in a consistent way)
+//    - attach auxiliary information (for example, caching the hash of a model)
+// Internal models abide by basic structural validity requirements, but are not trusted.
+// They may still represent invalid or Byzantine inputs in the context of the broader application state.
+// It is the responsibility of engines operating on these models to fully validate them.
 type UntrustedMessage interface {
 
 	// ToInternal returns the validated internal type (from flow.* constructors)
