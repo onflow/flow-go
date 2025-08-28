c7cd9bad02f71d66637abe749a8577413aacc58a
Andrii <andriy.dyachuk95@gmail.com>
2025-08-28 21:45:18 +0300

    Created internal models, fixed tests

--- model/messages/exchange.go
+++ model/messages/exchange.go
@@ -9,10 +9,7 @@ import (
 // specified here. In the typical case, the identifier is simply the ID of the
 // entity being requested, but more complex identifier-entity relationships can
 // be used as well.
-type EntityRequest struct {
-	Nonce     uint64
-	EntityIDs []flow.Identifier
-}
+type EntityRequest flow.EntityRequest
 
 // ToInternal converts the untrusted EntityRequest into its trusted internal
 // representation.
@@ -21,26 +18,18 @@ type EntityRequest struct {
 // must perform validation checks and return a constructed internal
 // object.
 func (e *EntityRequest) ToInternal() (any, error) {
-	// TODO(malleability, #7719) implement with validation checks
-	return e, nil
+	return (*flow.EntityRequest)(e), nil
+	//return e, nil
 }
 
 // EntityResponse is a response to an entity request, containing a set of
 // serialized entities and the identifiers used to request them. The returned
 // entity set may be empty or incomplete.
-type EntityResponse struct {
-	Nonce     uint64
-	EntityIDs []flow.Identifier
-	Blobs     [][]byte
-}
+type EntityResponse flow.EntityResponse
 
 // ToInternal converts the untrusted EntityResponse into its trusted internal
 // representation.
-//
-// This stub returns the receiver unchanged. A proper implementation
-// must perform validation checks and return a constructed internal
-// object.
 func (e *EntityResponse) ToInternal() (any, error) {
-	// TODO(malleability, #7720) implement with validation checks
-	return e, nil
+	return (*flow.EntityResponse)(e), nil
+	//return e, nil
 }
