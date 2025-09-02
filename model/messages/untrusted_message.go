8989a1046ecc898d316f5303cc754bd45a560c5d
UlyanaAndrukhiv <u.andrukhiv@gmail.com>
2025-09-02 19:41:30 +0300

    Fixed HandleIncomingEvent for corruptnet processor

--- model/messages/untrusted_message.go
+++ model/messages/untrusted_message.go
@@ -1,5 +1,12 @@
 package messages
 
+import (
+	"fmt"
+
+	"github.com/onflow/flow-go/model/cluster"
+	"github.com/onflow/flow-go/model/flow"
+)
+
 // UntrustedMessage represents the set of allowed decode target types for messages received over the network.
 // Conceptually, an UntrustedMessage implementation makes no guarantees whatsoever about its contents.
 // UntrustedMessage's must implement a ToInternal method, which converts the network message to a corresponding internal type (typically in the model/flow package)
@@ -15,4 +22,46 @@ type UntrustedMessage interface {
 	// ToInternal returns the internal type (from flow.* constructors) representation.
 	// All errors indicate that the decode target contains a structurally invalid representation of the internal model.
 	ToInternal() (any, error)
+}
+
+// InternalToMessage converts an internal types into the
+// corresponding UntrustedMessage for network.
+//
+// This is the inverse of ToInternal: instead of decoding a network
+// message into an internal model, it wraps or casts internal objects
+// so they can be encoded and sent over the network. Encoding and always
+// requires an UntrustedMessage.
+//
+// No errors are expected during normal operation.
+// TODO: investigate how to eliminate this workaround in both ghost/rpc.go and corruptnet/message_processor.go
+func InternalToMessage(event interface{}) (UntrustedMessage, error) {
+	switch internal := event.(type) {
+	case *flow.Proposal:
+		return (*Proposal)(internal), nil
+	case *cluster.Proposal:
+		return (*ClusterProposal)(internal), nil
+	case *flow.CollectionGuarantee:
+		return (*CollectionGuarantee)(internal), nil
+	case *flow.SyncRequest:
+		return (*SyncRequest)(internal), nil
+	case *flow.SyncResponse:
+		return (*SyncResponse)(internal), nil
+	case *flow.BatchRequest:
+		return (*BatchRequest)(internal), nil
+	case *flow.ChunkDataRequest:
+		return (*ChunkDataRequest)(internal), nil
+	case *flow.ChunkDataResponse:
+		return &ChunkDataResponse{
+			ChunkDataPack: flow.UntrustedChunkDataPack(internal.ChunkDataPack),
+			Nonce:         internal.Nonce,
+		}, nil
+	case *flow.RangeRequest:
+		return (*RangeRequest)(internal), nil
+	case UntrustedMessage:
+		// Already a valid UntrustedMessage
+		// TODO(immutable M2): expand when ToInternal changes for other M2 types
+		return internal, nil
+	default:
+		return nil, fmt.Errorf("cannot convert unsupported type %T", event)
+	}
 }
