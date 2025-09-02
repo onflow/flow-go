022302665f5ef20a5ee06c1b166880a4a1377e50
Leo Zhang (zhangchiqing) <zhangchiqing@gmail.com>
2025-09-02 16:50:45 -0700

    remove testing.ExecutionStorageLayer

--- storage/util/testing.go
+++ storage/util/testing.go
@@ -6,28 +6,7 @@ import (
 	"testing"
 
 	"github.com/stretchr/testify/require"
-
-	"github.com/onflow/flow-go/module/metrics"
-	"github.com/onflow/flow-go/storage"
-	"github.com/onflow/flow-go/storage/store"
 )
-
-func ExecutionStorageLayer(_ testing.TB, db storage.DB) *storage.Execution {
-	metrics := metrics.NewNoopCollector()
-
-	results := store.NewExecutionResults(metrics, db)
-	receipts := store.NewExecutionReceipts(metrics, db, results, store.DefaultCacheSize)
-	commits := store.NewCommits(metrics, db)
-	transactionResults := store.NewTransactionResults(metrics, db, store.DefaultCacheSize)
-	events := store.NewEvents(metrics, db)
-	return &storage.Execution{
-		Results:            results,
-		Receipts:           receipts,
-		Commits:            commits,
-		TransactionResults: transactionResults,
-		Events:             events,
-	}
-}
 
 func CreateFiles(t *testing.T, dir string, names ...string) {
 	for _, name := range names {
