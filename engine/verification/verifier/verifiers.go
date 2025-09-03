0d3bf70cb08bcd3a77a0c5cdaf8d2cae9f3a7271
gregor <75445744+devbugging@users.noreply.github.com>
2025-09-03 17:45:39 +0200

    Resovle conflict

--- engine/verification/verifier/verifiers.go
+++ engine/verification/verifier/verifiers.go
@@ -337,18 +337,14 @@ func makeVerifier(
 	)
 
 	// TODO(JanezP): cleanup creation of fvm context github.com/onflow/flow-go/issues/5249
-<<<<<<< HEAD
-	fvmOptions = append(fvmOptions, computation.DefaultFVMOptions(chainID, false, false, scheduledCallbacksEnabled)...)
-=======
 	fvmOptions = append(
 		fvmOptions,
 		computation.DefaultFVMOptions(
 			chainID,
 			false,
-			true,
+			scheduledCallbacksEnabled,
 		)...,
 	)
->>>>>>> master
 	vmCtx := fvm.NewContext(fvmOptions...)
 
 	chunkVerifier := chunks.NewChunkVerifier(vm, vmCtx, logger)
