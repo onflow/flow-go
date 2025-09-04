ca4e6ec8af03b995cf8e2fe9bd4149b732629595
Leo Zhang (zhangchiqing) <zhangchiqing@gmail.com>
2025-09-04 11:52:48 -0700

    update comment

--- storage/operation/spork.go
+++ storage/operation/spork.go
@@ -5,11 +5,18 @@ import (
 	"github.com/onflow/flow-go/storage"
 )
 
+// IndexSporkRootBlock indexes the spork root block ID for the present spork.
+//
+// Error returns:
+//   - generic error in case of unexpected failure from the database layer or encoding failure.
 func IndexSporkRootBlock(w storage.Writer, blockID flow.Identifier) error {
 	return UpsertByKey(w, MakePrefix(codeSporkRootBlockID), blockID)
 }
 
 // RetrieveSporkRootBlockID retrieves the spork root block ID for the present spork.
+//
+// Error returns:
+//   - generic error in case of unexpected failure from the database layer or encoding failure.
 func RetrieveSporkRootBlockID(r storage.Reader, blockID *flow.Identifier) error {
 	return RetrieveByKey(r, MakePrefix(codeSporkRootBlockID), blockID)
 }
