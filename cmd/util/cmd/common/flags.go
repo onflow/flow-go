ddbe8f9ff0c90c17945534b7c4f15d0212ef2b9f
Leo Zhang (zhangchiqing) <zhangchiqing@gmail.com>
2025-09-18 08:46:58 -0700

    deprecate pebble-dir and use-db flags

--- cmd/util/cmd/common/flags.go
+++ cmd/util/cmd/common/flags.go
@@ -6,32 +6,21 @@ import (
 )
 
 var (
-	flagBadgerDir string
-	flagPebbleDir string
-	flagUseDB     string
+	flagProtocolDBDir string
 )
 
 type DBFlags struct {
-	BadgerDir string
-	PebbleDir string
-	UseDB     string
+	ProtocolDBDir string
 }
 
-const DefaultBadgerDir = "/var/flow/data/protocol"
-const DefaultPebbleDir = "/var/flow/data/protocol-pebble"
-const DefaultDB = "pebble" // "badger|pebble"
+const DefaultProtocolDBDir = "/var/flow/data/protocol"
 
 // InitWithDBFlags initializes the command with the database flags
 // and sets the default values for the flags
 // this function is usually used in the init() function of a cmd
 func InitWithDBFlags(cmd *cobra.Command) {
-	cmd.PersistentFlags().StringVarP(&flagBadgerDir, "datadir", "d", DefaultBadgerDir,
-		"directory to the badger dababase")
-
-	cmd.PersistentFlags().StringVar(&flagPebbleDir, "pebble-dir", DefaultPebbleDir,
-		"directory to the pebble dababase")
-
-	cmd.PersistentFlags().StringVar(&flagUseDB, "use-db", DefaultDB, "the database type to use, badger or pebble")
+	cmd.PersistentFlags().StringVarP(&flagProtocolDBDir, "datadir", "d", DefaultProtocolDBDir,
+		"directory to the protocol db dababase")
 }
 
 // ReadDBFlags is to read the database flags
@@ -40,14 +29,10 @@ func InitWithDBFlags(cmd *cobra.Command) {
 // only in the cobra.Command.Run function
 func ReadDBFlags() DBFlags {
 	log.Info().
-		Str("datadir", flagBadgerDir).
-		Str("pebble-dir", flagPebbleDir).
-		Str("use-db", flagUseDB).
-		Msgf("read initialized db flags")
+		Str("datadir", flagProtocolDBDir).
+		Msgf("read initialized protocol db flag")
 
 	return DBFlags{
-		BadgerDir: flagBadgerDir,
-		PebbleDir: flagPebbleDir,
-		UseDB:     flagUseDB,
+		ProtocolDBDir: flagProtocolDBDir,
 	}
 }
