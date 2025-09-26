796a78834ae498c378ab5808c69c68fe389d1462
Peter Argue <89119817+peterargue@users.noreply.github.com>
2025-09-25 18:09:37 -0700

    Refactor notifier_test to use synctest

--- engine/notifier_test.go
+++ engine/notifier_test.go
@@ -1,12 +1,13 @@
 package engine
 
 import (
+	"context"
 	"fmt"
 	"sync"
 	"testing"
-	"time"
+	"testing/synctest"
 
-	"github.com/stretchr/testify/require"
+	"github.com/stretchr/testify/assert"
 	"go.uber.org/atomic"
 )
 
@@ -38,7 +39,7 @@ func TestNotifier_NoNotificationsInitialization(t *testing.T) {
 	select {
 	case <-notifier.Channel():
 		t.Fail()
-	default: //expected
+	default: // expected
 	}
 }
 
@@ -51,14 +52,15 @@ func TestNotifier_ManyNotifications(t *testing.T) {
 	t.Parallel()
 	notifier := NewNotifier()
 
+	// send 10 notifications
 	var counter sync.WaitGroup
-	for i := 0; i < 10; i++ {
-		counter.Add(1)
-		go func() {
+	for range 10 {
+		counter.Go(func() {
 			notifier.Notify()
-			counter.Done()
-		}()
+		})
 	}
+
+	// wait for all gorountines to complete
 	counter.Wait()
 
 	// attempt to consume first notification:
@@ -67,15 +69,15 @@ func TestNotifier_ManyNotifications(t *testing.T) {
 	select {
 	case <-c: // expected
 	default:
-		t.Fail()
+		t.Error("expected one notification to be available")
 	}
 
-	// attempt to consume first notification
+	// attempt to consume second notification
 	// expect that no notification is available
 	select {
 	case <-c:
-		t.Fail()
-	default: //expected
+		t.Error("expected only one notification to be available")
+	default: // expected
 	}
 }
 
@@ -85,123 +87,113 @@ func TestNotifier_ManyNotifications(t *testing.T) {
 func TestNotifier_ManyConsumers(t *testing.T) {
 	singleTestRun := func(t *testing.T) {
 		t.Parallel()
-		notifier := NewNotifier()
-		c := notifier.Channel()
+		synctest.Test(t, func(t *testing.T) {
+			notifier := NewNotifier()
+			c := notifier.Channel()
 
-		// spawn 100 worker routines to each wait for a notification
-		var startingWorkers sync.WaitGroup
-		pendingWorkers := atomic.NewInt32(100)
-		for i := 0; i < 100; i++ {
-			startingWorkers.Add(1)
-			go func() {
-				startingWorkers.Done()
-				<-c
-				pendingWorkers.Dec()
-			}()
-		}
-		startingWorkers.Wait()
+			// spawn 100 worker routines to each wait for a notification
+			pendingWorkers := atomic.NewInt32(100)
+			for range 100 {
+				go func() {
+					<-c
+					pendingWorkers.Dec()
+				}()
+			}
 
-		// send 100 notifications, with small delays
-		for i := 0; i < 100; i++ {
-			notifier.Notify()
-			time.Sleep(100 * time.Millisecond)
-		}
+			// wait until all workers are blocked on the channel
+			synctest.Wait()
 
-		// require that all workers got a notification
-		if !conditionEventuallySatisfied(func() bool { return pendingWorkers.Load() == 0 }, 3*time.Second, 100*time.Millisecond) {
-			require.Fail(t, "timed out", "still awaiting %d workers to get notification", pendingWorkers.Load())
-		}
+			for range 100 {
+				notifier.Notify()
+			}
+
+			// wait until all workers are done
+			synctest.Wait()
+
+			// require that all workers got a notification
+			assert.Equal(t, int32(0), pendingWorkers.Load())
+		})
 	}
 
-	for r := 0; r < 100; r++ {
+	for r := range 100 {
 		t.Run(fmt.Sprintf("run %d", r), singleTestRun)
 	}
 }
 
 // TestNotifier_AllWorkProcessed spans many routines pushing work and fewer
-// routines consuming work. We require that all worker is eventually processed.
+// routines consuming work. We require that all work is eventually processed.
 func TestNotifier_AllWorkProcessed(t *testing.T) {
 	singleTestRun := func(t *testing.T) {
 		t.Parallel()
-		notifier := NewNotifier()
+		synctest.Test(t, func(t *testing.T) {
+			ctx, cancel := context.WithCancel(context.Background())
+			defer cancel()
 
-		totalWork := int32(100)
-		pendingWorkQueue := make(chan struct{}, totalWork)
-		scheduledWork := atomic.NewInt32(0)
-		consumedWork := atomic.NewInt32(0)
+			notifier := NewNotifier()
 
-		// starts the consuming first, because if we starts the production first instead, then
-		// we might finish pushing all jobs, before any of our consumer has started listening
-		// to the queue.
-		var consumersAllReady sync.WaitGroup
-		consumersAllReady.Add(5)
+			totalWork := int32(100)
+			pendingWorkQueue := make(chan struct{}, totalWork)
+			scheduledWork := atomic.NewInt32(0)
+			consumedWork := atomic.NewInt32(0)
 
-		// 5 routines consuming work
-		for i := 0; i < 5; i++ {
-			go func() {
-				consumersAllReady.Done()
-				for consumedWork.Load() < totalWork {
-					<-notifier.Channel()
-				L:
-					for {
-						select {
-						case <-pendingWorkQueue:
-							consumedWork.Inc()
-						default:
-							break L
-						}
+			// starts the consumers first, because if we start the production first instead, then
+			// we might finish pushing all jobs, before any of our consumer has started listening
+			// to the queue.
+
+			processAllPending := func() {
+				for {
+					select {
+					case <-pendingWorkQueue:
+						consumedWork.Inc()
+					default:
+						return
 					}
 				}
-			}()
-		}
-
-		// wait long enough for all consumer to be ready for new notification.
-		consumersAllReady.Wait()
-
-		var workersAllReady sync.WaitGroup
-		workersAllReady.Add(10)
-
-		// 10 routines pushing work
-		for i := 0; i < 10; i++ {
-			go func() {
-				workersAllReady.Done()
-				for scheduledWork.Inc() <= totalWork {
-					pendingWorkQueue <- struct{}{}
-					notifier.Notify()
-				}
-			}()
-		}
-
-		// wait long enough for all workers to be started.
-		workersAllReady.Wait()
-
-		// require that all work is eventually consumed
-		if !conditionEventuallySatisfied(func() bool { return consumedWork.Load() == totalWork }, 3*time.Second, 100*time.Millisecond) {
-			require.Fail(t, "timed out", "only consumed %d units of work but expecting %d", consumedWork.Load(), totalWork)
-		}
-	}
-
-	for r := 0; r < 100; r++ {
-		t.Run(fmt.Sprintf("run %d", r), singleTestRun)
-	}
-}
-
-func conditionEventuallySatisfied(condition func() bool, waitFor time.Duration, tick time.Duration) bool {
-	done := make(chan struct{})
-
-	go func() {
-		for range time.Tick(tick) {
-			if condition() {
-				close(done)
-				return
 			}
-		}
-	}()
 
-	select {
-	case <-time.After(waitFor):
-		return false
-	case <-done:
-		return true
+			// 5 routines consuming work
+			for range 5 {
+				go func() {
+					for {
+						select {
+						case <-ctx.Done():
+							return
+						case <-notifier.Channel():
+							processAllPending()
+						}
+					}
+				}()
+			}
+
+			// wait for all consumer to be ready for new notification.
+			synctest.Wait()
+
+			// 10 routines pushing work
+			for range 10 {
+				go func() {
+					for scheduledWork.Load() <= totalWork {
+						pendingWorkQueue <- struct{}{}
+						scheduledWork.Inc()
+						notifier.Notify()
+					}
+				}()
+			}
+
+			// wait for all producers and consumers to block. at this point, all jobs should be completed.
+			synctest.Wait()
+
+			// at least the expected number of jobs should have been scheduled, and all jobs that were
+			// scheduled should have been consumed.
+			assert.GreaterOrEqual(t, scheduledWork.Load(), int32(totalWork))
+			assert.Equal(t, scheduledWork.Load(), consumedWork.Load())
+
+			// shutdown blocked consumers and wait for them to complete
+			cancel()
+			synctest.Wait()
+		})
+	}
+
+	for r := range 100 {
+		t.Run(fmt.Sprintf("run %d", r), singleTestRun)
 	}
 }
