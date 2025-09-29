edd6ad1eccea66438bf311c3b8813485c40da85a
Alex Hentschel <alex.hentschel@flowfoundation.org>
2025-09-29 11:11:52 -0700

    extended usage of newly created invalid cluster snapshot

--- state/cluster/snapshot.go
+++ state/cluster/snapshot.go
@@ -5,22 +5,32 @@ import (
 )
 
 // Snapshot represents an immutable snapshot at a specific point in the cluster
-// state history.
+// state history. Specifically, a valid snapshot references a [cluster.Block],
+// which designates the head of one specific cluster-consensus fork. This head
+// has
 type Snapshot interface {
 
-	// Collection returns the collection generated in this step of the cluster
-	// state history.
+	// Collection returns the collection designated as the reference for this
+	// snapshot. Technically, this is a portion of the payload of a cluster block.
+	//
+	// Expected error returns during normal operations:
+	//  - If the snapshot is for an unknown collection [state.ErrUnknownSnapshotReference]
 	Collection() (*flow.Collection, error)
 
-	// Head returns the latest block at the selected point of the cluster state
-	// history. If the snapshot was selected by block ID, returns the header
-	// with that block ID. If the snapshot was selected as final, returns the
-	// latest finalized block.
+	// Head returns the header of the collection that designated as the reference for this
+	// snapshot. Technically, this is the header of a [cluster.Block]
+	//
+	// Expected error returns during normal operations:
+	//  - If the snapshot is for an unknown collection [state.ErrUnknownSnapshotReference]
 	Head() (*flow.Header, error)
 
-	// Pending returns the IDs of all blocks descending from the snapshot head, which thus were
-	// potential extensions of the protocol state at this snapshot. The result
-	// is ordered such that parents are included before their children. These
-	// are NOT guaranteed to have been validated by HotStuff.
+	// Pending returns the IDs of all collections descending from the snapshot's head collection.
+	// The result is ordered such that parents are included before their children. While only valid
+	// descendants will be returned, note that the descendants may not be finalized yet.
+	//
+	// CAUTION: the list of descendants returned by this function is
+	//
+	// Expected error returns during normal operations:
+	//  - If the snapshot is for an unknown collection [state.ErrUnknownSnapshotReference]
 	Pending() ([]flow.Identifier, error)
 }
