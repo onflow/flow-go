ab51fd0a91750d9fcbceb41fd124ec150a23c60d
Alex Hentschel <alex.hentschel@flowfoundation.org>
2025-09-29 16:09:53 -0700

    fixed incomplete goDoc

--- state/cluster/snapshot.go
+++ state/cluster/snapshot.go
@@ -5,7 +5,7 @@ import (
 )
 
 // Snapshot pertains to a specific fork of the collector cluster consensus. Specifically,
-// it references one block denoted as the Head block. This Snapshot type is for collector
+// it references one block denoted as the `Head`. This Snapshot type is for collector
 // clusters, so we are referencing a cluster block, aka collection, here.
 type Snapshot interface {
 
@@ -16,8 +16,8 @@ type Snapshot interface {
 	//  - If the snapshot is for an unknown collection [state.ErrUnknownSnapshotReference]
 	Collection() (*flow.Collection, error)
 
-	// Head returns the header of the collection that designated as the reference for this
-	// snapshot. Technically, this is the header of a [cluster.Block]
+	// Head returns the header of the collection that is designated as the reference for
+	// this snapshot. Technically, this is the header of a [cluster.Block]
 	//
 	// Expected error returns during normal operations:
 	//  - If the snapshot is for an unknown collection [state.ErrUnknownSnapshotReference]
@@ -27,7 +27,8 @@ type Snapshot interface {
 	// The result is ordered such that parents are included before their children. While only valid
 	// descendants will be returned, note that the descendants may not be finalized yet.
 	//
-	// CAUTION: the list of descendants returned by this function is
+	// CAUTION: the list of descendants returned is constructed for each call via database reads,
+	// and may be expensive to compute, especially if the reference collection is older.
 	//
 	// Expected error returns during normal operations:
 	//  - If the snapshot is for an unknown collection [state.ErrUnknownSnapshotReference]
