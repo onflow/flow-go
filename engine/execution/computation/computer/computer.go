df8ae979c04557a382c51af5db087affe48a1f3a
gregor <75445744+devbugging@users.noreply.github.com>
2025-10-01 11:44:45 +0200

    Only log process execution failure

--- engine/execution/computation/computer/computer.go
+++ engine/execution/computation/computer/computer.go
@@ -647,10 +647,13 @@ func (e *blockComputer) executeProcessCallback(
 	}
 
 	if txn.Output().Err != nil {
-		return nil, 0, fmt.Errorf(
-			"process callback transaction %s error: %v",
-			request.txnIdStr,
-			txn.Output().Err)
+		// if the process transaction fails we log the critical error but don't return an error
+		// so that block execution continues and only the scheduled transactions halt
+		callbackLogger.Error().
+			Err(txn.Output().Err).
+			Bool("critical_error", true).
+			Uint64("height", request.ctx.BlockHeader.Height).
+			Msg("system process transaction output error")
 	}
 
 	callbackTxs, err := blueprints.ExecuteCallbacksTransactions(e.vmCtx.Chain, txn.Output().Events)
