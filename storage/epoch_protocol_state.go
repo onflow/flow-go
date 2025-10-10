c26fc13dccb2355160065d72bf49d8baca8ae306
Leo Zhang (zhangchiqing) <zhangchiqing@gmail.com>
2025-10-10 08:14:07 -0700

    update comments

--- storage/epoch_protocol_state.go
+++ storage/epoch_protocol_state.go
@@ -34,13 +34,13 @@ type EpochProtocolStateEntries interface {
 	//
 	// CAUTION:
 	//   - The caller must acquire the lock [storage.LockInsertBlock] and hold it until the database write has been committed.
-//   - OVERWRITES existing data (potential for data corruption):
-//     The lock proof serves as a reminder that the CALLER is responsible to ensure that the DEDUPLICATION CHECK is done elsewhere
-//     ATOMICALLY within this write operation. Currently it's done by operation.InsertHeader where it performs a check
-//     to ensure the blockID is new, therefore any data indexed by this blockID is new as well.
+	//   - OVERWRITES existing data (potential for data corruption):
+	//     The lock proof serves as a reminder that the CALLER is responsible to ensure that the DEDUPLICATION CHECK is done elsewhere
+	//     ATOMICALLY within this write operation. Currently it's done by operation.InsertHeader where it performs a check
+	//     to ensure the blockID is new, therefore any data indexed by this blockID is new as well.
 	//
 	// Expected errors during normal operations:
-// No expected errors during normal operations.
+	// No expected errors during normal operations.
 	BatchIndex(lctx lockctx.Proof, rw ReaderBatchWriter, blockID flow.Identifier, epochProtocolStateID flow.Identifier) error
 
 	// ByID returns the flow.RichEpochStateEntry by its ID.
