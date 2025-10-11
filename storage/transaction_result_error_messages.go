127db87fbc3072e4f7dc622f7278935e0999cb8c
Alex Hentschel <alex.hentschel@flowfoundation.org>
2025-10-10 19:45:21 -0700

    extending documentation

--- storage/transaction_result_error_messages.go
+++ storage/transaction_result_error_messages.go
@@ -10,10 +10,18 @@ import (
 type TransactionResultErrorMessagesReader interface {
 	// Exists returns true if transaction result error messages for the given ID have been stored.
 	//
+	// Note that transaction error messages are auxiliary data provided by the Execution Nodes on a goodwill basis and
+	// not protected by the protocol. Execution Error messages might be non-deterministic, i.e. potentially different
+	// for different execution nodes.
+	//
 	// No errors are expected during normal operation.
 	Exists(blockID flow.Identifier) (bool, error)
 
 	// ByBlockIDTransactionID returns the transaction result error message for the given block ID and transaction ID.
+	//
+	// Note that transaction error messages are auxiliary data provided by the Execution Nodes on a goodwill basis and
+	// not protected by the protocol. Execution Error messages might be non-deterministic, i.e. potentially different
+	// for different execution nodes.
 	//
 	// Expected errors during normal operation:
 	//   - [storage.ErrNotFound] if no transaction error message is known at given block and transaction id.
@@ -21,12 +29,20 @@ type TransactionResultErrorMessagesReader interface {
 
 	// ByBlockIDTransactionIndex returns the transaction result error message for the given blockID and transaction index.
 	//
+	// Note that transaction error messages are auxiliary data provided by the Execution Nodes on a goodwill basis and
+	// not protected by the protocol. Execution Error messages might be non-deterministic, i.e. potentially different
+	// for different execution nodes.
+	//
 	// Expected errors during normal operation:
 	//   - [storage.ErrNotFound] if no transaction error message is known at given block and transaction index.
 	ByBlockIDTransactionIndex(blockID flow.Identifier, txIndex uint32) (*flow.TransactionResultErrorMessage, error)
 
 	// ByBlockID gets all transaction result error messages for a block, ordered by transaction index.
-	// Note: This method will return an empty slice both if the block is not indexed yet and if the block does not have any errors.
+	// CAUTION: This method will return an empty slice both if the block is not indexed yet and if the block does not have any errors.
+	//
+	// Note that transaction error messages are auxiliary data provided by the Execution Nodes on a goodwill basis and
+	// not protected by the protocol. Execution Error messages might be non-deterministic, i.e. potentially different
+	// for different execution nodes.
 	//
 	// No errors are expected during normal operations.
 	ByBlockID(id flow.Identifier) ([]flow.TransactionResultErrorMessage, error)
