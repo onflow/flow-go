257f514db959a578381635738abd6cb789825e79
Andrii <andriy.dyachuk95@gmail.com>
2025-10-16 12:29:28 +0300

    Moved init of executionResultInfoProvider to the top of the Build function

--- cmd/access/node_builder/access_node_builder.go
+++ cmd/access/node_builder/access_node_builder.go
@@ -653,33 +653,33 @@ func (builder *FlowAccessNodeBuilder) BuildExecutionSyncComponents() *FlowAccess
 			return nil
 		}).
 		Module("execution state cache", func(node *cmd.NodeConfig) error {
-			backendConfig := builder.rpcConf.BackendConfig
-
-			preferredENIdentifiers, err := flow.IdentifierListFromHex(backendConfig.PreferredExecutionNodeIDs)
-			if err != nil {
-				return fmt.Errorf("failed to convert node id string to Flow Identifier for preferred EN map: %w", err)
-			}
-
-			fixedENIdentifiers, err := flow.IdentifierListFromHex(backendConfig.FixedExecutionNodeIDs)
-			if err != nil {
-				return fmt.Errorf("failed to convert node id string to Flow Identifier for fixed EN map: %w", err)
-			}
-
-			execNodeSelector := execution_result.NewExecutionNodeSelector(
-				preferredENIdentifiers,
-				fixedENIdentifiers,
-			)
-
-			builder.executionResultInfoProvider = execution_result.NewExecutionResultInfoProvider(
-				node.Logger,
-				node.State,
-				node.Storage.Receipts,
-				execNodeSelector,
-				optimistic_sync.DefaultCriteria,
-			)
-			if err != nil {
-				return fmt.Errorf("failed to create execution result provider: %w", err)
-			}
+			//backendConfig := builder.rpcConf.BackendConfig
+			//
+			//preferredENIdentifiers, err := flow.IdentifierListFromHex(backendConfig.PreferredExecutionNodeIDs)
+			//if err != nil {
+			//	return fmt.Errorf("failed to convert node id string to Flow Identifier for preferred EN map: %w", err)
+			//}
+			//
+			//fixedENIdentifiers, err := flow.IdentifierListFromHex(backendConfig.FixedExecutionNodeIDs)
+			//if err != nil {
+			//	return fmt.Errorf("failed to convert node id string to Flow Identifier for fixed EN map: %w", err)
+			//}
+			//
+			//execNodeSelector := execution_result.NewExecutionNodeSelector(
+			//	preferredENIdentifiers,
+			//	fixedENIdentifiers,
+			//)
+			//
+			//builder.executionResultInfoProvider = execution_result.NewExecutionResultInfoProvider(
+			//	node.Logger,
+			//	node.State,
+			//	node.Storage.Receipts,
+			//	execNodeSelector,
+			//	optimistic_sync.DefaultCriteria,
+			//)
+			//if err != nil {
+			//	return fmt.Errorf("failed to create execution result provider: %w", err)
+			//}
 
 			// TODO: use real objects instead of mocks once they're implemented
 			snapshot := osyncsnapshot.NewSnapshotMock(
@@ -1767,6 +1767,38 @@ func (builder *FlowAccessNodeBuilder) Build() (cmd.Node, error) {
 
 	builder.Module("events storage", func(node *cmd.NodeConfig) error {
 		builder.events = store.NewEvents(node.Metrics.Cache, node.ProtocolDB)
+		return nil
+	})
+
+	builder.Module("execution result info provider", func(node *cmd.NodeConfig) error {
+		backendConfig := builder.rpcConf.BackendConfig
+
+		preferredENIdentifiers, err := flow.IdentifierListFromHex(backendConfig.PreferredExecutionNodeIDs)
+		if err != nil {
+			return fmt.Errorf("failed to convert node id string to Flow Identifier for preferred EN map: %w", err)
+		}
+
+		fixedENIdentifiers, err := flow.IdentifierListFromHex(backendConfig.FixedExecutionNodeIDs)
+		if err != nil {
+			return fmt.Errorf("failed to convert node id string to Flow Identifier for fixed EN map: %w", err)
+		}
+
+		execNodeSelector := execution_result.NewExecutionNodeSelector(
+			preferredENIdentifiers,
+			fixedENIdentifiers,
+		)
+
+		builder.executionResultInfoProvider = execution_result.NewExecutionResultInfoProvider(
+			node.Logger,
+			node.State,
+			node.Storage.Receipts,
+			execNodeSelector,
+			optimistic_sync.DefaultCriteria,
+		)
+		if err != nil {
+			return fmt.Errorf("failed to create execution result provider: %w", err)
+		}
+
 		return nil
 	})
 
