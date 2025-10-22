824226a0a48d3f00576839d215a2ee53e31c14d8
Peter Argue <89119817+peterargue@users.noreply.github.com>
2025-10-22 05:55:50 -0700

    fix test name

--- engine/common/rpc/convert/transactions_test.go
+++ engine/common/rpc/convert/transactions_test.go
@@ -33,13 +33,13 @@ func TestConvertTransaction(t *testing.T) {
 	assert.Equal(t, tx.ID(), converted.ID())
 }
 
-// TestConvertSystemTransaction1 tests that converting a system transaction to a protobuf message and
+// TestConvertSystemTransaction tests that converting a system transaction to a protobuf message and
 // back results in the same transaction body.
 //
 // System and scheduled transactions have nil/empty values for some fields. This test ensures that
 // these fields are properly handled when converting to and from protobuf messages and the resulting
 // transaction body identical.
-func TestConvertSystemTransaction1(t *testing.T) {
+func TestConvertSystemTransaction(t *testing.T) {
 	t.Parallel()
 
 	g := fixtures.NewGeneratorSuite()
