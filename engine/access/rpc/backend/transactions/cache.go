0ef2e2182aa43b0cb10d6c0c77fe71e6cf9505c5
Peter Argue <89119817+peterargue@users.noreply.github.com>
2025-10-30 07:11:52 -0700

    updates from code review

--- engine/access/rpc/backend/transactions/cache.go
+++ engine/access/rpc/backend/transactions/cache.go
@@ -8,7 +8,12 @@ import (
 // TxResultCache is a cache for transaction results used by the API to avoid unnecessary lookups on
 // historical access nodes.
 type TxResultCache interface {
+	// Get retrieves a cached transaction result by transaction ID,	returns true if it exists in the
+	// cache, otherwise false.
 	Get(flow.Identifier) (*accessmodel.TransactionResult, bool)
+
+	// Add adds a transaction result to the cache keyed by its transaction ID, and returns true if
+	// an entry was evicted in the process.
 	Add(flow.Identifier, *accessmodel.TransactionResult) bool
 }
 
@@ -22,10 +27,18 @@ func NewNoopTxResultCache() *NoopTxResultCache {
 	return &NoopTxResultCache{}
 }
 
+// Get retrieves a cached transaction result by transaction ID,	returns true if it exists in the
+// cache, otherwise false.
+//
+// This is a no-op implementation and always returns nil and false
 func (n *NoopTxResultCache) Get(flow.Identifier) (*accessmodel.TransactionResult, bool) {
 	return nil, false
 }
 
+// Add adds a transaction result to the cache keyed by its transaction ID, and returns true if
+// an entry was evicted in the process.
+//
+// This is a no-op implementation which simply ignores the inputs and returns false.
 func (n *NoopTxResultCache) Add(flow.Identifier, *accessmodel.TransactionResult) bool {
 	return false
 }
