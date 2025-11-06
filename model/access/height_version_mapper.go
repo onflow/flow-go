a3c21d6162f06a2426c9e916240f8f90b023bbf2
gregor <75445744+devbugging@users.noreply.github.com>
2025-11-06 16:28:10 +0100

    height version mapper

--- model/access/height_version_mapper.go
+++ model/access/height_version_mapper.go
@@ -7,16 +7,16 @@ import (
 )
 
 type HeightVersionMapper interface {
-	GetVersion(height uint64) (string, error)
-	VersionExists(version string) bool
+	GetVersion(height uint64) (Version, error)
+	VersionExists(version Version) bool
 }
 
 type StaticHeightVersionMapper struct {
-	heightVersionBoundaries map[uint64]string
+	heightVersionBoundaries map[uint64]Version
 	boundaries              []uint64
 }
 
-func NewStaticHeightVersionMapper(heightVersionBoundaries map[uint64]string) *StaticHeightVersionMapper {
+func NewStaticHeightVersionMapper(heightVersionBoundaries map[uint64]Version) *StaticHeightVersionMapper {
 	boundaries := slices.Collect(maps.Keys(heightVersionBoundaries))
 	slices.Sort(boundaries)
 	slices.Reverse(boundaries)
@@ -27,17 +27,17 @@ func NewStaticHeightVersionMapper(heightVersionBoundaries map[uint64]string) *St
 	}
 }
 
-func (s *StaticHeightVersionMapper) GetVersion(height uint64) (string, error) {
+func (s *StaticHeightVersionMapper) GetVersion(height uint64) (Version, error) {
 	for _, boundary := range s.boundaries {
 		if height >= boundary {
 			return s.heightVersionBoundaries[boundary], nil
 		}
 	}
 
-	return "", fmt.Errorf("height %d is before any known version boundary", height)
+	return 0, fmt.Errorf("height %d is before any known version boundary", height)
 }
 
-func (s *StaticHeightVersionMapper) VersionExists(version string) bool {
+func (s *StaticHeightVersionMapper) VersionExists(version Version) bool {
 	for _, v := range s.heightVersionBoundaries {
 		if v == version {
 			return true
