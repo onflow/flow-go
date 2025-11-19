e0205ab1e7db4a0fec6673e15b410367b5b5a6d6
Leo Zhang <zhangchiqing@gmail.com>
2025-11-19 10:14:29 -0800

    Update consensus/hotstuff/distributor.go
    Co-authored-by: Jordan Schalm <jordan.schalm@flowfoundation.org>

--- consensus/hotstuff/distributor.go
+++ consensus/hotstuff/distributor.go
@@ -2,14 +2,16 @@ package hotstuff
 
 import "github.com/onflow/flow-go/consensus/hotstuff/model"
 
-// Distributor is an interface for distributing HotStuff consensus events to registered consumers.
+// FinalizationRegistrar is an interface for registering consumers of HotStuff events.
 // It provides a pub-sub mechanism where components can register callbacks to be notified
 // when blocks are finalized or incorporated into the chain.
 //
-// The distributor pattern centralizes event distribution and ensures that all registered
-// consumers are notified of consensus events. This prevents callers from forgetting to
-// register callbacks and provides a consistent way to handle consensus notifications.
-type Distributor interface {
+// Components which must be subscribed to finalization events for correct functionality should:
+//    - accept a FinalizationRegistrar argument in their constructor
+//    - register their event consumer functions within their constructor
+// Following this pattern of coupling construction with subscription prevents callers from forgetting
+// to register callbacks and provides a consistent way to handle consensus notifications.
+type FinalizationRegistrar interface {
 	// AddOnBlockFinalizedConsumer registers a callback function that will be invoked
 	// whenever a block is finalized. Finalized blocks have achieved consensus finality
 	// and are considered immutable.
