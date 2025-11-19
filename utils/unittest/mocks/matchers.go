c0a88715f48ce9c52f3416d5ca912c0e7904ca65
Peter Argue <89119817+peterargue@users.noreply.github.com>
2025-11-19 09:16:48 -0800

    add godocs for MatchLock

--- utils/unittest/mocks/matchers.go
+++ utils/unittest/mocks/matchers.go
@@ -5,6 +5,18 @@ import (
 	"github.com/stretchr/testify/mock"
 )
 
+// MatchLock returns an argument matcher that checks if the argument is a `lockctx.Proof` that holds
+// the provided lock.
+//
+// Example:
+//
+//	events.
+//		On("BatchStore", mocks.MatchLock(storage.LockInsertEvent), blockID, expectedEvents, mock.Anything).
+//		Return(func(lctx lockctx.Proof, blockID flow.Identifier, events []flow.EventsList, batch storage.ReaderBatchWriter) error {
+//			require.NotNil(t, batch)
+//			return nil
+//		}).
+//		Once()
 func MatchLock(lock string) interface{} {
 	return mock.MatchedBy(func(lctx lockctx.Proof) bool { return lctx.HoldsLock(lock) })
 }
