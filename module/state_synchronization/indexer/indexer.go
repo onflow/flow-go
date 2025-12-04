c8f71b92bcd801148b70fdb8b6a1fce64582cd85
Leo Zhang (zhangchiqing) <zhangchiqing@gmail.com>
2025-12-03 20:18:06 -0800

    log indexer look up

--- module/state_synchronization/indexer/indexer.go
+++ module/state_synchronization/indexer/indexer.go
@@ -158,6 +158,7 @@ func (i *Indexer) onBlockIndexed() error {
 	highestIndexedHeight := i.jobConsumer.LastProcessedIndex()
 
 	if lastProcessedHeight < highestIndexedHeight {
+		i.log.Info().Msgf("notifying processed heights from %d to %d", lastProcessedHeight+1, highestIndexedHeight)
 		// we need loop here because it's possible for a height to be missed here,
 		// we should guarantee all heights are processed
 		for height := lastProcessedHeight + 1; height <= highestIndexedHeight; height++ {
