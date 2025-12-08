14919611c7c297f2007be090880ca6bdee834911
UlyanaAndrukhiv <u.andrukhiv@gmail.com>
2025-12-08 12:29:32 +0200

    Udded Unwrap method to optimistic_sync errors

--- module/executiondatasync/optimistic_sync/errors.go
+++ module/executiondatasync/optimistic_sync/errors.go
@@ -10,21 +10,21 @@ import (
 // RequiredExecutorsCountExceededError indicates that the requested number of required executors
 // exceeds the total available execution nodes.
 type RequiredExecutorsCountExceededError struct {
-	requiredExecutorsCount  int
-	availableExecutorsCount int
+	err error
 }
 
 func NewRequiredExecutorsCountExceededError(requiredExecutorsCount int, availableExecutorsCount int) *RequiredExecutorsCountExceededError {
 	return &RequiredExecutorsCountExceededError{
-		requiredExecutorsCount:  requiredExecutorsCount,
-		availableExecutorsCount: availableExecutorsCount,
+		err: fmt.Errorf("required executors count exceeded: required %d, available %d", requiredExecutorsCount, availableExecutorsCount),
 	}
 }
 
-func (e *RequiredExecutorsCountExceededError) Error() string {
-	return fmt.Sprintf("required executors count exceeded: required %d, available %d",
-		e.requiredExecutorsCount, e.availableExecutorsCount,
-	)
+func (e RequiredExecutorsCountExceededError) Error() string {
+	return e.err.Error()
+}
+
+func (e RequiredExecutorsCountExceededError) Unwrap() error {
+	return e.err
 }
 
 func IsRequiredExecutorsCountExceededError(err error) bool {
@@ -35,17 +35,21 @@ func IsRequiredExecutorsCountExceededError(err error) bool {
 // UnknownRequiredExecutorError indicates that a required executor ID is not present
 // in the list of active execution nodes.
 type UnknownRequiredExecutorError struct {
-	executorID flow.Identifier
+	err error
 }
 
 func NewUnknownRequiredExecutorError(executorID flow.Identifier) *UnknownRequiredExecutorError {
 	return &UnknownRequiredExecutorError{
-		executorID: executorID,
+		err: fmt.Errorf("unknown required executor ID %s", executorID.String()),
 	}
 }
 
-func (e *UnknownRequiredExecutorError) Error() string {
-	return fmt.Sprintf("unknown required executor ID: %s", e.executorID.String())
+func (e UnknownRequiredExecutorError) Error() string {
+	return e.err.Error()
+}
+
+func (e UnknownRequiredExecutorError) Unwrap() error {
+	return e.err
 }
 
 func IsUnknownRequiredExecutorError(err error) bool {
@@ -56,17 +60,21 @@ func IsUnknownRequiredExecutorError(err error) bool {
 // CriteriaNotMetError indicates that the execution result criteria could not be
 // satisfied for a given block, when the block is already sealed.
 type CriteriaNotMetError struct {
-	blockID flow.Identifier
+	err error
 }
 
 func NewCriteriaNotMetError(blockID flow.Identifier) *CriteriaNotMetError {
 	return &CriteriaNotMetError{
-		blockID: blockID,
+		err: fmt.Errorf("block %s is already sealed but the criteria is still not met", blockID),
 	}
 }
 
-func (e *CriteriaNotMetError) Error() string {
-	return fmt.Sprintf("block %s is already sealed but the criteria is still not met,", e.blockID)
+func (e CriteriaNotMetError) Error() string {
+	return e.err.Error()
+}
+
+func (e CriteriaNotMetError) Unwrap() error {
+	return e.err
 }
 
 func IsCriteriaNotMetError(err error) bool {
