cbe7f85b0094b294ba809d65bbcd6328d3ff4377
Faye Amacker <33205765+fxamacker@users.noreply.github.com>
2025-12-28 16:08:33 -0600

    Fix mock

--- fvm/environment/mock/environment.go
+++ fvm/environment/mock/environment.go
@@ -925,36 +925,6 @@ func (_m *Environment) GetCurrentBlockHeight() (uint64, error) {
 	return r0, r1
 }
 
-// GetRestrictedAccounts provides a mock function with no fields
-func (_m *Environment) GetRestrictedAccounts() (map[flow.Address]struct{}, error) {
-	ret := _m.Called()
-
-	if len(ret) == 0 {
-		panic("no return value specified for GetRestrictedAccounts")
-	}
-
-	var r0 map[flow.Address]struct{}
-	var r1 error
-	if rf, ok := ret.Get(0).(func() (map[flow.Address]struct{}, error)); ok {
-		return rf()
-	}
-	if rf, ok := ret.Get(0).(func() map[flow.Address]struct{}); ok {
-		r0 = rf()
-	} else {
-		if ret.Get(0) != nil {
-			r0 = ret.Get(0).(map[flow.Address]struct{})
-		}
-	}
-
-	if rf, ok := ret.Get(1).(func() error); ok {
-		r1 = rf()
-	} else {
-		r1 = ret.Error(1)
-	}
-
-	return r0, r1
-}
-
 // GetOrLoadProgram provides a mock function with given fields: location, load
 func (_m *Environment) GetOrLoadProgram(location runtime.Location, load func() (*runtime.Program, error)) (*runtime.Program, error) {
 	ret := _m.Called(location, load)
@@ -978,6 +948,36 @@ func (_m *Environment) GetOrLoadProgram(location runtime.Location, load func() (
 
 	if rf, ok := ret.Get(1).(func(runtime.Location, func() (*runtime.Program, error)) error); ok {
 		r1 = rf(location, load)
+	} else {
+		r1 = ret.Error(1)
+	}
+
+	return r0, r1
+}
+
+// GetRestrictedAccounts provides a mock function with no fields
+func (_m *Environment) GetRestrictedAccounts() (map[flow.Address]struct{}, error) {
+	ret := _m.Called()
+
+	if len(ret) == 0 {
+		panic("no return value specified for GetRestrictedAccounts")
+	}
+
+	var r0 map[flow.Address]struct{}
+	var r1 error
+	if rf, ok := ret.Get(0).(func() (map[flow.Address]struct{}, error)); ok {
+		return rf()
+	}
+	if rf, ok := ret.Get(0).(func() map[flow.Address]struct{}); ok {
+		r0 = rf()
+	} else {
+		if ret.Get(0) != nil {
+			r0 = ret.Get(0).(map[flow.Address]struct{})
+		}
+	}
+
+	if rf, ok := ret.Get(1).(func() error); ok {
+		r1 = rf()
 	} else {
 		r1 = ret.Error(1)
 	}
