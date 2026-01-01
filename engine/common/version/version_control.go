43b1f183335931a328f8d24f00b5ac45fd8408cf
Jan Bernatik <jan.bernatik@flowfoundation.org>
2026-01-01 07:49:13 -0800

    add AN compatibility for v0.44.9

--- engine/common/version/version_control.go
+++ engine/common/version/version_control.go
@@ -57,6 +57,7 @@ var defaultCompatibilityOverrides = map[string]struct{}{
 	"0.44.0":  {}, // mainnet, testnet
 	"0.44.1":  {}, // mainnet, testnet
 	"0.44.7":  {}, // mainnet, testnet
+	"0.44.9":  {}, // mainnet, testnet
 }
 
 // VersionControl manages the version control system for the node.
