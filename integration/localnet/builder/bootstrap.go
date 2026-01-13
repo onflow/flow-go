70203fef3ea023a8cd5eb59ddd2fab67cc4ce09a
Leo Zhang (zhangchiqing) <zhangchiqing@gmail.com>
2026-01-13 14:43:10 -0800

    rename capacity to mtrie-cache-size

--- integration/localnet/builder/bootstrap.go
+++ integration/localnet/builder/bootstrap.go
@@ -854,7 +854,7 @@ func prepareLedgerService(dockerServices Services, flowNodeContainerConfigs []te
 		Command: []string{
 			"--triedir=/trie",
 			fmt.Sprintf("--grpc-addr=0.0.0.0:%s", testnet.GRPCPort),
-			"--capacity=100",
+			"--mtrie-cache-size=100",
 			"--checkpoint-distance=100",
 			"--checkpoints-to-keep=3",
 			fmt.Sprintf("--loglevel=%s", logLevel),
