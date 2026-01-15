bef8264c433c6576a464cc6b372c3d1e7d8945b5
Leo Zhang (zhangchiqing) <zhangchiqing@gmail.com>
2026-01-14 18:26:06 -0800

    remove grpc compression

--- ledger/remote/factory.go
+++ ledger/remote/factory.go
@@ -8,34 +8,30 @@ import (
 
 // RemoteLedgerFactory creates remote ledger instances via gRPC.
 type RemoteLedgerFactory struct {
-	grpcAddr          string
-	logger            zerolog.Logger
-	maxRequestSize    uint
-	maxResponseSize   uint
-	enableCompression bool
+	grpcAddr        string
+	logger          zerolog.Logger
+	maxRequestSize  uint
+	maxResponseSize uint
 }
 
 // NewRemoteLedgerFactory creates a new factory for remote ledger instances.
 // maxRequestSize and maxResponseSize specify the maximum message sizes in bytes.
 // If both are 0, defaults to 1 GiB for both requests and responses.
-// enableCompression enables gzip compression for proof operations (useful for testing/benchmarking).
 func NewRemoteLedgerFactory(
 	grpcAddr string,
 	logger zerolog.Logger,
 	maxRequestSize, maxResponseSize uint,
-	enableCompression bool,
 ) ledger.Factory {
 	return &RemoteLedgerFactory{
-		grpcAddr:          grpcAddr,
-		logger:            logger,
-		maxRequestSize:    maxRequestSize,
-		maxResponseSize:   maxResponseSize,
-		enableCompression: enableCompression,
+		grpcAddr:        grpcAddr,
+		logger:          logger,
+		maxRequestSize:  maxRequestSize,
+		maxResponseSize: maxResponseSize,
 	}
 }
 
 func (f *RemoteLedgerFactory) NewLedger() (ledger.Ledger, error) {
-	client, err := NewClient(f.grpcAddr, f.logger, f.maxRequestSize, f.maxResponseSize, f.enableCompression)
+	client, err := NewClient(f.grpcAddr, f.logger, f.maxRequestSize, f.maxResponseSize)
 	if err != nil {
 		return nil, err
 	}
