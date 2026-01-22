a2ad334bb6073063f2ae595f0592b26b67f75f81
Ardit Marku <markoupetr@gmail.com>
2026-01-22 14:37:52 +0200

    Fix comment typo

--- fvm/evm/handler/precompiles.go
+++ fvm/evm/handler/precompiles.go
@@ -27,7 +27,7 @@ import (
 // such special errors occur. This way, any potential bugs will not go unnoticed.
 // The Cadence runtime recovers any Go crashers (index out of bounds, nil
 // dereferences, etc.) and fails the transaction gracefully, so a panic in the
-// precompiled contract is not indicate a node/runtime crash.
+// precompiled contract does not indicate a node/runtime crash.
 
 func preparePrecompiledContracts(
 	evmContractAddress flow.Address,
