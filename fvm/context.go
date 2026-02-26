74349c7a877dc483a1320c4fdffa76c4116807c4
Leo Zhang (zhangchiqing) <zhangchiqing@gmail.com>
2026-02-26 09:10:27 -0800

    add token movements inspect

--- fvm/context.go
+++ fvm/context.go
@@ -430,3 +430,10 @@ func WithInspectors(inspectors []inspection.Inspector) Option {
 		return ctx
 	}
 }
+
+func WithInspectors(inspectors []inspection.Inspector) Option {
+	return func(ctx Context) Context {
+		ctx.Inspectors = inspectors
+		return ctx
+	}
+}
