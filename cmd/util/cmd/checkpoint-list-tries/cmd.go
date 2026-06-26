e1ceeee7ab57c765a425b60531e09a9a5b4d1b85
Leo Zhang (zhangchiqing) <zhangchiqing@gmail.com>
2026-06-26 10:08:25 -0700

    update checkpoint list tries

--- cmd/util/cmd/checkpoint-list-tries/cmd.go
+++ cmd/util/cmd/checkpoint-list-tries/cmd.go
@@ -2,10 +2,14 @@ package checkpoint_list_tries
 
 import (
 	"fmt"
+	"path/filepath"
+	"strings"
 
+	"github.com/rs/zerolog"
 	"github.com/rs/zerolog/log"
 	"github.com/spf13/cobra"
 
+	"github.com/onflow/flow-go/ledger"
 	"github.com/onflow/flow-go/ledger/complete/wal"
 )
 
@@ -28,14 +32,32 @@ func init() {
 
 func run(*cobra.Command, []string) {
 
-	log.Info().Msgf("loading checkpoint %v", flagCheckpoint)
-	tries, err := wal.LoadCheckpoint(flagCheckpoint, log.Logger)
-	if err != nil {
-		log.Fatal().Err(err).Msg("error while loading checkpoint")
-	}
-	log.Info().Msgf("checkpoint loaded, total tries: %v", len(tries))
+	log.Info().Msgf("reading trie root hashes from checkpoint %v", flagCheckpoint)
 
-	for _, trie := range tries {
-		fmt.Printf("trie root hash: %s\n", trie.RootHash())
+	hashes, err := readTrieRootHashes(log.Logger, flagCheckpoint)
+	if err != nil {
+		log.Fatal().Err(err).Msg("error while reading trie root hashes from checkpoint")
 	}
+	log.Info().Msgf("checkpoint read, total tries: %v", len(hashes))
+
+	for _, h := range hashes {
+		fmt.Printf("trie root hash: %s\n", h)
+	}
+}
+
+// readTrieRootHashes reads only the trie root hashes from the checkpoint file at
+// the given path, without materializing the full trie forest. Only the top-trie
+// part file (containing the trie root records) is read.
+//
+// Both V6 and V7 (payloadless) checkpoints are supported; the version is
+// determined by the V7 filename suffix ([wal.V7FileSuffix]). The root hashes are
+// returned in the order they are stored in the checkpoint.
+//
+// No error returns are expected during normal operation.
+func readTrieRootHashes(logger zerolog.Logger, checkpointFilePath string) ([]ledger.RootHash, error) {
+	dir, fileName := filepath.Split(checkpointFilePath)
+	if strings.HasSuffix(fileName, wal.V7FileSuffix) {
+		return wal.ReadTriesRootHashV7(logger, dir, fileName)
+	}
+	return wal.ReadTriesRootHash(logger, dir, fileName)
 }
